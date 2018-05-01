166a06e51af606dabe2449be9898c2f0c7d57c22
Nick Schrock <schrockn@gmail.com>
2018-05-01 10:09:28 -0700

    complete modularization

--- python/solidic-pandas/setup.py
+++ python/solidic-pandas/setup.py
@@ -9,14 +9,13 @@ else:
     import builtins
 
 setup(
-    name='dagster',
+    name='solidic-pandas',
     license='MIT',
-    packages=find_packages(exclude=['dagster_tests']),
+    packages=find_packages(exclude=['solidic_pandas_tests']),
     install_requires=[
         # standard python 2/3 compatability things
         'enum34>=1.1.6',
         'future>=0.16.0',
-
-        # solidic (not explicitly expressed atm)
+        'pandas>=0.22.0',
     ],
 )
