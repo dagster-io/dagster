cf4b04d724051452317275e1a55621c8431c3ab8
Nick Schrock <schrockn@gmail.com>
2018-05-02 11:27:27 -0700

    add convenience api to pandas kernal

--- python/solidic-pandas/solidic_pandas/__init__.py
+++ python/solidic-pandas/solidic_pandas/__init__.py
@@ -1,2 +1,30 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 from builtins import *  # pylint: disable=W0622,W0401
+
+import check
+
+from solidic.definitions import Solid
+from .definitions import (create_solidic_pandas_csv_input, create_solidic_pandas_csv_output)
+
+
+def solid(**kwargs):
+    return Solid(**kwargs)
+
+
+def tabular_solid(**kwargs):
+    # will add parquet and other standardized formats
+    return Solid(output_type_defs=[csv_output()], **kwargs)
+
+
+def single_path_arg(input_name, path):
+    check.str_param(input_name, 'input_name')
+    check.str_param(path, 'path')
+    return {input_name: {'path': path}}
+
+
+def csv_input(name):
+    return create_solidic_pandas_csv_input(name)
+
+
+def csv_output():
+    return create_solidic_pandas_csv_output()
