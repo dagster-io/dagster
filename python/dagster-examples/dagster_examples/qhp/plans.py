c0601abc89143d46a99f6a76d810188c46a01a3a
Nick Schrock <schrockn@gmail.com>
2018-05-10 12:20:20 -0700

    remove plans.py

--- python/dagster-examples/dagster_examples/qhp/plans.py
+++
@@ -1,4 +0,0 @@
-# from solid_core import (SolidInt64, SolidString, SolidBool, SolidFloat64)
-# import solid_pandas_kernel as solid_pd
-from .qhp_input import define_qhp_input
-import solidic_pandas as solidic_pd
