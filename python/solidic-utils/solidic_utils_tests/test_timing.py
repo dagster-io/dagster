2665d09a8e81925a3b610b4827cd1d2ac1a9bf1e
Nick Schrock <schrockn@gmail.com>
2018-05-11 13:48:45 -0700

    timer

---
+++ python/solidic-utils/solidic_utils_tests/test_timing.py
@@ -0,0 +1,22 @@
+import pytest
+
+from solidic_utils.timing import (time_execution_scope, TimerResult)
+
+
+def test_basic_usage():
+    with time_execution_scope() as timer_result:
+        pass
+
+    assert timer_result
+    assert isinstance(timer_result.millis, float)
+    assert isinstance(timer_result.seconds, float)
+
+
+def test_wrong_usage():
+    with pytest.raises(Exception):
+        timer_result = TimerResult()
+        assert timer_result.millis
+
+    with pytest.raises(Exception):
+        timer_result = TimerResult()
+        assert timer_result.seconds
