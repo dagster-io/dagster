99f701a88ce131478c58f9613a108b701efb090d
Nick Schrock <schrockn@gmail.com>
2018-05-10 18:25:13 -0700

    things are executing from the command line

--- python/dagster/dagster/embedded_cli.py
+++ python/dagster/dagster/embedded_cli.py
@@ -1,10 +1,12 @@
+from collections import defaultdict
+
 import click
 
 import check
-
-from solidic.execution import (SolidPipeline, execute_pipeline, SolidExecutionContext)
+from solidic.execution import (SolidExecutionContext, SolidPipeline, execute_pipeline)
 
 from .graphviz import build_graphviz_graph
+from .structured_flags import structure_flags
 
 
 @click.command(name='graphviz')
@@ -18,7 +20,7 @@ def embedded_dagster_graphviz_command(cxt):
 @click.option('--input', multiple=True)
 @click.option('--output', multiple=True)
 @click.pass_context
-def embedded_dagster_execute_command(cxt, input, output):
+def embedded_dagster_execute_command(cxt, input, output):  # pylint: disable=W0622
     check.tuple_param(input, 'input')
     check.tuple_param(output, 'output')
 
@@ -27,7 +29,17 @@ def embedded_dagster_execute_command(cxt, input, output):
 
     pipeline = check.inst(cxt.obj['pipeline'], SolidPipeline)
 
-    input_arg_dicts = {'qhp_json_input': {'path': 'providers-771.json'}}
+    structured_flags = structure_flags(input_list)
+
+    if structured_flags.single_argument or structured_flags.named_arguments:
+        check.failed('only supporting named key arguments right now')
+
+    input_arg_dicts = defaultdict(lambda: {})
+
+    for nka in structured_flags.named_key_arguments:
+        input_arg_dicts[nka.name][nka.key] = nka.value
+
+    # input_arg_dicts = {'qhp_json_input': {'path': 'providers-771.json'}}
 
     for result in execute_pipeline(
         SolidExecutionContext(), pipeline, input_arg_dicts, through_solids=output_list
@@ -35,8 +47,7 @@ def embedded_dagster_execute_command(cxt, input, output):
         if not result.success:
             raise result.exception
 
-        if result.name in output_list:
-            print(result.materialized_output)
+        print(f'processed {result.name}')
 
 
 def embedded_dagster_cli_main(argv, pipeline):
