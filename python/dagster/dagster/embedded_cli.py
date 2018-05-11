d184b20f03171283e2dcf7fc6eeb450c640895ed
Nick Schrock <schrockn@gmail.com>
2018-05-10 18:08:04 -0700

    execute scaffodling

--- python/dagster/dagster/embedded_cli.py
+++ python/dagster/dagster/embedded_cli.py
@@ -2,7 +2,7 @@ import click
 
 import check
 
-from solidic.execution import SolidPipeline
+from solidic.execution import (SolidPipeline, execute_pipeline, SolidExecutionContext)
 
 from .graphviz import build_graphviz_graph
 
@@ -14,10 +14,36 @@ def embedded_dagster_graphviz_command(cxt):
     build_graphviz_graph(pipeline).view(cleanup=True)
 
 
+@click.command(name='execute')
+@click.option('--input', multiple=True)
+@click.option('--output', multiple=True)
+@click.pass_context
+def embedded_dagster_execute_command(cxt, input, output):
+    check.tuple_param(input, 'input')
+    check.tuple_param(output, 'output')
+
+    input_list = list(input)
+    output_list = list(output)
+
+    pipeline = check.inst(cxt.obj['pipeline'], SolidPipeline)
+
+    input_arg_dicts = {'qhp_json_input': {'path': 'providers-771.json'}}
+
+    for result in execute_pipeline(
+        SolidExecutionContext(), pipeline, input_arg_dicts, through_solids=output_list
+    ):
+        if not result.success:
+            raise result.exception
+
+        if result.name in output_list:
+            print(result.materialized_output)
+
+
 def embedded_dagster_cli_main(argv, pipeline):
     check.list_param(argv, 'argv', of_type=str)
     check.inst_param(pipeline, 'pipeline', SolidPipeline)
 
     dagster_command_group = click.Group(name='dagster')
     dagster_command_group.add_command(embedded_dagster_graphviz_command)
+    dagster_command_group.add_command(embedded_dagster_execute_command)
     dagster_command_group(argv[1:], obj={'pipeline': pipeline})
