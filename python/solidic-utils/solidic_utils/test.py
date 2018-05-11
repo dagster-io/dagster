fc084148790e9fc99a663a8407bd052fc994b256
Nick Schrock <schrockn@gmail.com>
2018-05-11 09:53:04 -0700

    test subgraph execution

--- python/solidic-utils/solidic_utils/test.py
+++ python/solidic-utils/solidic_utils/test.py
@@ -1,4 +1,5 @@
 import inspect
+import itertools
 import os
 import tempfile
 from contextlib import contextmanager
@@ -20,10 +21,34 @@ def script_relative_path(file_path):
     return os.path.join(os.path.dirname(os.path.abspath(scriptdir)), file_path)
 
 
+def _unlink_swallow_errors(path):
+    check.str_param(path, 'path')
+    try:
+        os.unlink(path)
+    except:  # pylint: disable=W0702
+        pass
+
+
 @contextmanager
 def get_temp_file_name():
     temp_file_name = tempfile.mkstemp()[1]
     try:
         yield temp_file_name
     finally:
-        os.unlink(temp_file_name)
+        _unlink_swallow_errors(temp_file_name)
+
+
+@contextmanager
+def get_temp_file_names(number):
+    check.int_param(number, 'number')
+
+    temp_file_names = list()
+    for _ in itertools.repeat(None, number):
+        temp_file_name = tempfile.mkstemp()[1]
+        temp_file_names.append(temp_file_name)
+
+    try:
+        yield tuple(temp_file_names)
+    finally:
+        for temp_file_name in temp_file_names:
+            _unlink_swallow_errors(temp_file_name)
