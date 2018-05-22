82647df1e7bbf41b1f85f3817aab814bc9dd6b6e
Nick Schrock <schrockn@gmail.com>
2018-05-22 07:44:53 -0700

    delete python scaffold

--- python-scaffold/bin/python_scaffold_driver.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=E0401, C0413
-
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import python_scaffold
-python_scaffold.main(sys.argv)
