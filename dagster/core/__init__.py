2e5e292248dde97924bda052170050b93650a096
Abe <abegong@gmail.com>
2018-05-23 09:52:27 -0700

    create_json_input

--- dagster/core/__init__.py
+++ dagster/core/__init__.py
@@ -1,15 +1,14 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 from builtins import *  # pylint: disable=W0622,W0401
 
+import json
+
 from dagster import check
 from dagster.core import types
 from dagster.core.execution import DagsterExecutionContext
 
 from .definitions import (InputDefinition, Solid, create_dagster_single_file_input)
 from .graph import DagsterPipeline
-
-from dagster.core.execution import DagsterExecutionContext
-import json
 
 
 def pipeline(**kwargs):
@@ -26,7 +25,6 @@ def file_input_definition(argument_def_dict=None, **kwargs):
 
 
 def create_json_input(name):
-    import json
     check.str_param(name, 'name')
 
     #Note: I don't understand the function of check_path.
