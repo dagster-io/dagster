36ade7ac8ffa0803f8797f9b516dbdb753f79a73
Nick Schrock <schrockn@gmail.com>
2018-05-24 14:53:25 -0700

    filed-based sql works

--- dagster/sqlalchemy_kernel/__init__.py
+++ dagster/sqlalchemy_kernel/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 from builtins import *  # pylint: disable=W0622,W0401
 
+import os
+
 import sqlalchemy as sa
 
 from dagster import check
@@ -9,6 +11,7 @@ import dagster.core
 from dagster.core import types
 from dagster.core.execution import (DagsterExecutionContext)
 from dagster.core.definitions import (Solid, InputDefinition, OutputDefinition)
+from dagster.transform_only_solid import (dep_only_input, no_args_transform_solid)
 
 
 class DagsterSqlAlchemyExecutionContext(DagsterExecutionContext):
@@ -103,3 +106,36 @@ def create_sql_solid(name, inputs, sql_text):
         transform_fn=create_sql_transform(sql_text),
         outputs=[create_table_output()],
     )
+
+
+def _create_sql_alchemy_transform_fn(sql_text):
+    check.str_param(sql_text, 'sql_text')
+
+    def transform_fn(context):
+        conn = context.engine.connect()
+        conn.execute(sql_text)
+        conn.close()
+
+    return transform_fn
+
+
+def create_sql_statement_solid(name, sql_text, inputs=None):
+    check.str_param(name, 'name')
+    check.str_param(sql_text, 'sql_text')
+    check.opt_list_param(inputs, 'inputs', of_type=InputDefinition)
+    return no_args_transform_solid(
+        name,
+        no_args_transform_fn=_create_sql_alchemy_transform_fn(sql_text),
+        inputs=inputs,
+    )
+
+
+def sql_file_solid(path, inputs=None):
+    check.str_param(path, 'path')
+    check.opt_list_param(inputs, 'inputs', of_type=InputDefinition)
+
+    basename = os.path.basename(path)
+    name = os.path.splitext(basename)[0]
+
+    with open(path, 'r') as ff:
+        return create_sql_statement_solid(name, ff.read(), inputs)
