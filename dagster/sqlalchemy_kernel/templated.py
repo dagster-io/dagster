1f793ab1dab925d3916dd424eefec807c7d0f56a
Nick Schrock <schrockn@gmail.com>
2018-05-28 14:04:07 -0700

    slightly api refactor + docs

--- dagster/sqlalchemy_kernel/templated.py
+++ dagster/sqlalchemy_kernel/templated.py
@@ -16,12 +16,78 @@ def _create_table_input(name, depends_on=None):
     )
 
 
-def create_templated_sql_transform_solid(name, sql, table_arguments, dependencies, output):
+def create_templated_sql_transform_solid(name, sql, table_arguments, output, dependencies=None):
+    '''
+    Create a solid that is a templated sql statement. This assumes that the sql statement
+    is creating or modifying a table, and that that table will be used downstream in the pipeline
+    for further transformations
+
+    Usage example:
+
+    sum_sql_template = 'CREATE TABLE {{sum_table.name}} AS ' + \
+        'SELECT num1, num2, num1 + num2 as sum FROM {{num_table.name}}'
+
+    sum_solid = create_templated_sql_transform_solid(
+        name='sum_solid',
+        sql=sum_sql_template,
+        table_arguments=['sum_table', 'num_table'],
+        output='sum_table',
+    )
+
+    'Table arguments' are inputs that represent a table. They have a string property "name". Each
+    table argument ends up as an InputDefinition into the solid.
+
+    To invoke this you would configure an argument dictionary to look like:
+
+    input_arg_dict = {'sum_table': {'name': 'a_sum_table'}, 'num_table': {'name': 'a_num_table'}}
+
+    dagster.execute_pipeline(context, pipeline, input_arg_dict)
+
+    The output table is what is flowed to the *next* solid.
+
+    So imagine, we built another solid which depended on the previous one.
+
+    sum_sq_sql_template = 'CREATE TABLE {{sum_sq_table.name}} AS ' + \
+        'SELECT num1, num2, sum, sum * sum as sum_sq FROM {{sum_solid.name}}'
+
+    sum_sq_solid = create_templated_sql_transform_solid(
+        name='sum_sq_solid',
+        sql=sum_sq_sql_template,
+        table_arguments=['sum_sq_table'],
+        output='sum_sq_table',
+        dependencies=[sum_solid],
+    )
+
+    This solid has two inputs, one is a table_argument directly passed in, and one is
+    the table from the previous solid.
+
+    Note: there is current awkwardness in this api because all inputs and solids in pipeline
+    must have a different name. This is why the template variable in the "sum_sq_solid" is
+    named "sum_solid" rather than "sum_table".
+
+    To invoke this you would now specifiy:
+
+    input_arg_dict = {
+        'sum_table': {'name': 'a_sum_table'},
+        'num_table': {'name': 'a_num_table'},
+        'sum_sq_table': {'name': 'a_sum_sq_table'},
+    }
+
+    If you wanted to test the sum_sq_table in isolation, you could skip the num_table and just
+    specify the output of the sum_solid:
+
+    input_arg_dict = {
+        'sum_solid': {'name': 'a_sum_table'},
+        'sum_sq_table': {'name': 'a_sum_sq_table'},
+    }
+
+    As noted above the naming is not very intuitive right row, but the functionality works.
+    '''
     check.str_param(name, 'name')
     check.str_param(sql, 'sql')
     check.list_param(table_arguments, 'table_arguments', of_type=str)
-    check.list_param(dependencies, 'dependencies', of_type=Solid)
     check.str_param(output, 'output')
+    dependencies = check.opt_list_param(dependencies, 'dependencies', of_type=Solid)
 
     table_inputs = [_create_table_input(table) for table in table_arguments]
     dep_inputs = [_create_table_input(dep.name, depends_on=dep) for dep in dependencies]
