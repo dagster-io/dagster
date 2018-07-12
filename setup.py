86181befd4cc090f50190752d8c9d41553b821b5
Nick Schrock <schrockn@gmail.com>
2018-07-12 08:16:10 -0600

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(here, "README.rst"), "r") as fh:
 
 setup(
     name='dagster',
-    version="0.1.1",
+    version="0.1.2",
     author="Elementl",
     author_email="schrockn@elementl.com",
     license='Apache-2.0',
