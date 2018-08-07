6f829af81121945e763d0d3491d86550ce382df0
Nick Schrock <schrockn@gmail.com>
2018-08-07 07:40:50 -0700

    fix setup.py

--- python_modules/dagster-ui/setup.py
+++ python_modules/dagster-ui/setup.py
@@ -17,8 +17,8 @@ def long_description():
 
 
 setup(
-    name='dagster',
-    version='0.1.2',
+    name='dagster-ui',
+    version='0.0.0',
     author='Elementl',
     author_email='schrockn@elementl.com',
     license='Apache-2.0',
