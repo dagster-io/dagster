7a7b2c70c990b6c59542cd98f6c5f8227d3ac757
Mikhail Novikov <freiksenet@gmail.com>
2018-08-20 15:30:51 +0300

    Make dagit production ready
    * Only one command - "dagit" now
    * Make sure files are included in python
    * Serve static files
    * Serve index.html in all other cases (for push state support)

--- python_modules/dagit/dagit/app.py
+++ python_modules/dagit/dagit/app.py
@@ -1,5 +1,6 @@
+import os
 from graphql.execution.executors.asyncio import AsyncioExecutor
-from flask import Flask
+from flask import Flask, send_file, send_from_directory
 from flask_graphql import GraphQLView
 from flask_cors import CORS
 
@@ -15,19 +16,37 @@ class DagsterGraphQLView(GraphQLView):
         return {'pipeline_config': self.pipeline_config}
 
 
+def static_view(path, file):
+    return send_from_directory(
+        os.path.join(os.path.dirname(__file__), './webapp/build/static/', path), file
+    )
+
+
+def index_view(_path):
+    return send_file(os.path.join(os.path.dirname(__file__), './webapp/build/index.html'))
+
+
 def create_app(pipeline_config):
     app = Flask('dagster-ui')
 
     schema = create_schema()
     app.add_url_rule(
-        '/graphql',
-        view_func=DagsterGraphQLView.as_view(
+        '/graphql', 'graphql',
+        DagsterGraphQLView.as_view(
             'graphql',
             schema=schema,
             graphiql=True,
             executor=AsyncioExecutor(),
             pipeline_config=pipeline_config,
         )
+    )
+    app.add_url_rule('/static/<path:path>/<string:file>', 'static_view', static_view)
+    app.add_url_rule('/<path:_path>', 'index_catchall', index_view)
+    app.add_url_rule(
+        '/',
+        'index',
+        index_view,
+        defaults={'_path': ''},
     )
 
     CORS(app)
