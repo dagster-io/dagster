9a8c9943abfe0d3737f9b20053d78878280d3eef
Nicholas Schrock <schrockn@elementl.com>
2018-08-28 15:36:40 -0700

    bump to version 0.1.8

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -18,7 +18,7 @@ def long_description():
 
 setup(
     name='dagster',
-    version='0.1.7',
+    version='0.1.8',
     author='Elementl',
     author_email='schrockn@elementl.com',
     license='Apache-2.0',
