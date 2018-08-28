3352e159be9671bd957935d1e9f1810b121381ec
Mikhail Novikov <freiksenet@gmail.com>
2018-08-28 14:09:23 +0300

    Dagit v0.1.11

--- python_modules/dagit/setup.py
+++ python_modules/dagit/setup.py
@@ -18,7 +18,7 @@ def long_description():
 
 setup(
     name='dagit',
-    version='0.1.10',
+    version='0.1.11',
     author='Elementl',
     author_email='schrockn@elementl.com',
     license='Apache-2.0',
