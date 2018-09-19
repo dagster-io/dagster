f134eafb51528b50bee0e4c24fabeff9d1483a3b
Nicholas Schrock <schrockn@elementl.com>
2018-09-19 09:36:48 -0700

    WIP merge for dagstermill
    This creates an actual API for creating dagstermill solids, improves the
    in-notebook performance, and adds some tests.

--- python_modules/dagstermill/dagstermill/__init__.py
+++ python_modules/dagstermill/dagstermill/__init__.py
@@ -1,10 +1,21 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 from builtins import *  # pylint: disable=W0622,W0401
 
+import os
+import uuid
+
 import base64
 import pickle
 
 import papermill as pm
+
+from dagster import (
+    InputDefinition,
+    OutputDefinition,
+    Result,
+    SolidDefinition,
+    check,
+)
 
 CACHE = {}
 
@@ -12,23 +23,108 @@ CACHE = {}
 # python3 -m ipykernel install --user
 
 
+class InMemoryInputs:
+    def __init__(self, args):
+        self.args = args
+
+
 def define_inputs(**kwargs):
-    return base64.b64encode(pickle.dumps(kwargs))
+    return InMemoryInputs(kwargs)
 
 
 def _get_cached_inputs_dict(inputs):
+    if isinstance(inputs, InMemoryInputs):
+        return inputs.args
+
     if inputs in CACHE:
         return CACHE[inputs]
 
-    ddict = pickle.loads(base64.b64decode(inputs))
+    ddict = deserialize_dm_object(inputs)
     CACHE[inputs] = ddict
     return ddict
 
 
-def get_input(inputs, input_name):
-    ddict = _get_cached_inputs_dict(inputs)
-    return ddict[input_name]
+def deserialize_dm_object(dm_object):
+    return pickle.loads(base64.b64decode(dm_object.encode('ascii')))
+
+
+def serialize_dm_object(dm_object):
+    return base64.b64encode(pickle.dumps(dm_object)).decode('ascii')
+
+
+def get_input(serialized_inputs, input_name):
+    inputs_dict = _get_cached_inputs_dict(serialized_inputs)
+    return inputs_dict[input_name]
+
+
+def get_inputs(serialized_inputs, *input_names):
+    inputs_dict = _get_cached_inputs_dict(serialized_inputs)
+    return tuple([inputs_dict[input_name] for input_name in input_names])
+
+
+class InMemoryConfig:
+    def __init__(self, value):
+        self.value = value
+
+
+def define_config(value):
+    return InMemoryConfig(value)
+
+
+def get_config(value):
+    if isinstance(value, InMemoryConfig):
+        return value.value
+    return deserialize_dm_object(value)
 
 
 def yield_result(value, output_name='result'):
     pm.record(output_name, value)
+
+
+def define_dagstermill_solid(
+    name,
+    notebook_path,
+    input_defs=None,
+    output_defs=None,
+    config_def=None,
+):
+    check.str_param(name, 'name')
+    check.str_param(notebook_path, 'notebook_path')
+    input_defs = check.opt_list_param(input_defs, 'input_defs', of_type=InputDefinition)
+    output_defs = check.opt_list_param(output_defs, 'output_defs', of_type=OutputDefinition)
+
+    def _t_fn(info, inputs):
+        if not os.path.exists('/tmp/dagstermill/'):
+            os.mkdir('/tmp/dagstermill/')
+
+        temp_path = '/tmp/dagstermill/{prefix}-out.ipynb'.format(prefix=str(uuid.uuid4()))
+
+        try:
+            _source_nb = pm.execute_notebook(
+                notebook_path,
+                temp_path,
+                parameters=dict(
+                    inputs=serialize_dm_object(inputs),
+                    config=serialize_dm_object(info.config),
+                ),
+            )
+
+            output_nb = pm.read_notebook(temp_path)
+
+            for output_def in info.solid_def.output_defs:
+                if output_def.name in output_nb.data:
+                    yield Result(output_nb.data[output_def.name], output_def.name)
+
+        finally:
+            pass
+            # if os.path.exists(temp_path):
+            #     os.remove(temp_path)
+
+    return SolidDefinition(
+        name=name,
+        inputs=input_defs,
+        transform_fn=_t_fn,
+        outputs=output_defs,
+        config_def=config_def,
+        description='This solid is backed by the notebook at {path}'.format(path=notebook_path),
+    )
--- python_modules/dagstermill/dagstermill_tests/test_serializer.py
+++ python_modules/dagstermill/dagstermill_tests/test_serializer.py
@@ -1,14 +1,15 @@
 import sys
 import pytest
 
-import papermill as pm
-
 from dagster import (
-    execute_pipeline,
-    SolidDefinition,
+    DependencyDefinition,
+    InputDefinition,
     OutputDefinition,
     PipelineDefinition,
-    Result,
+    config,
+    execute_pipeline,
+    lambda_solid,
+    types,
 )
 
 from dagster.utils import script_relative_path
@@ -16,9 +17,48 @@ from dagster.utils import script_relative_path
 import dagstermill as dm
 
 
-def test_basic():
+def test_basic_get_in_memory_input():
     inputs = dm.define_inputs(a=1)
     assert dm.get_input(inputs, 'a') == 1
+
+
+def test_basic_get_in_memory_inputs():
+    inputs = dm.define_inputs(a=1, b=2)
+    assert dm.get_input(inputs, 'a') == 1
+    assert dm.get_input(inputs, 'b') == 2
+
+    a, b = dm.get_inputs(inputs, 'a', 'b')
+
+    assert a == 1
+    assert b == 2
+
+
+def test_basic_get_serialized_inputs():
+    inputs = dm.serialize_dm_object(dict(a=1, b=2))
+    assert dm.get_input(inputs, 'a') == 1
+    assert dm.get_input(inputs, 'b') == 2
+
+    a, b = dm.get_inputs(inputs, 'a', 'b')
+
+    assert a == 1
+    assert b == 2
+
+
+def test_basic_in_memory_config():
+    value = {'path': 'some_path.csv'}
+    config_ = dm.define_config(value)
+    assert dm.get_config(config_) == value
+
+
+def test_basic_serialized_config():
+    value = {'path': 'some_path.csv'}
+    config_ = dm.serialize_dm_object(value)
+    assert dm.get_config(config_) == value
+
+
+def test_serialize_unserialize():
+    value = {'a': 1, 'b': 2}
+    assert dm.deserialize_dm_object(dm.serialize_dm_object(value)) == value
 
 
 def nb_test_path(name):
@@ -26,37 +66,15 @@ def nb_test_path(name):
 
 
 def define_hello_world_solid():
-    def _transform(_info, _inputs):
-        pm.execute_notebook(nb_test_path('hello_world'), '/tmp/out.ipynb', parameters=dict())
-
-    return SolidDefinition(
-        name='test',
-        inputs=[],
-        transform_fn=_transform,
-        outputs=[],
-        config_def=None,
-    )
+    return dm.define_dagstermill_solid('test', nb_test_path('hello_world'))
 
 
 def define_hello_world_with_output():
-    def _transform(info, _inputs):
-        pm.execute_notebook(
-            nb_test_path('hello_world_output'),
-            '/tmp/out.ipynb',
-            parameters=dict(),
-        )
-
-        nb = pm.read_notebook('/tmp/out.ipynb')
-        for output_def in info.solid_def.output_defs:
-            if output_def.name in nb.data:
-                yield Result(nb.data[output_def.name], output_def.name)
-
-    return SolidDefinition(
-        name='test',
-        inputs=[],
-        transform_fn=_transform,
-        outputs=[OutputDefinition()],
-        config_def=None,
+    return dm.define_dagstermill_solid(
+        'test',
+        nb_test_path('hello_world_output'),
+        [],
+        [OutputDefinition()],
     )
 
 
@@ -85,3 +103,106 @@ def test_hello_world_with_output():
     result = execute_pipeline(pipeline)
     assert result.success
     assert result.result_for_solid('test').transformed_value() == 'hello, world'
+
+
+def add_two_numbers_pm_solid(name):
+    return dm.define_dagstermill_solid(
+        name,
+        nb_test_path('add_two_numbers'),
+        [
+            InputDefinition(name='a', dagster_type=types.Int),
+            InputDefinition(name='b', dagster_type=types.Int),
+        ],
+        [OutputDefinition(types.Int)],
+    )
+
+
+def mult_two_numbers_pm_solid(name):
+    return dm.define_dagstermill_solid(
+        name,
+        nb_test_path('mult_two_numbers'),
+        [
+            InputDefinition(name='a', dagster_type=types.Int),
+            InputDefinition(name='b', dagster_type=types.Int),
+        ],
+        [OutputDefinition(types.Int)],
+    )
+
+
+@lambda_solid
+def return_one():
+    return 1
+
+
+@lambda_solid
+def return_two():
+    return 2
+
+
+def define_hello_world_inputs_pipeline():
+    with_inputs_solid = add_two_numbers_pm_solid('with_inputs')
+    return PipelineDefinition(
+        solids=[return_one, return_two, with_inputs_solid],
+        dependencies={
+            with_inputs_solid.name: {
+                'a': DependencyDefinition('return_one'),
+                'b': DependencyDefinition('return_two'),
+            }
+        }
+    )
+
+
+@notebook_test
+def test_hello_world_inputs():
+    pipeline = define_hello_world_inputs_pipeline()
+    result = execute_pipeline(pipeline)
+    assert result.success
+    assert result.result_for_solid('with_inputs').transformed_value() == 3
+
+
+@notebook_test
+def test_hello_world_config():
+    with_config_solid = dm.define_dagstermill_solid(
+        'with_config',
+        nb_test_path('hello_world_with_config'),
+        [],
+        [OutputDefinition()],
+    )
+
+    pipeline = PipelineDefinition(solids=[with_config_solid])
+    pipeline_result = execute_pipeline(
+        pipeline,
+        config.Environment(solids={'with_config': config.Solid(script_relative_path('num.csv'))}),
+    )
+
+    assert pipeline_result.success
+    assert pipeline_result.result_for_solid('with_config').transformed_value() == 100
+
+
+def define_test_notebook_dag_pipeline():
+    return PipelineDefinition(
+        solids=[
+            return_one,
+            return_two,
+            add_two_numbers_pm_solid('add_two'),
+            mult_two_numbers_pm_solid('mult_two'),
+        ],
+        dependencies={
+            'add_two': {
+                'a': DependencyDefinition('return_one'),
+                'b': DependencyDefinition('return_two'),
+            },
+            'mult_two': {
+                'a': DependencyDefinition('add_two'),
+                'b': DependencyDefinition('return_two'),
+            },
+        },
+    )
+
+
+@notebook_test
+def test_notebook_dag():
+    pipeline_result = execute_pipeline(define_test_notebook_dag_pipeline())
+    assert pipeline_result.success
+    assert pipeline_result.result_for_solid('add_two').transformed_value() == 3
+    assert pipeline_result.result_for_solid('mult_two').transformed_value() == 6
