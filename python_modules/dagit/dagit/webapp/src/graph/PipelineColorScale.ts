b7e9ce29e5b8aa5ed3892181bbf1132aa156aa8b
Ben Gotow <ben@foundry376.com>
2018-11-01 14:41:07 -0700

    Initial rendreing work for additional components in DAG

--- python_modules/dagit/dagit/webapp/src/graph/PipelineColorScale.ts
+++ python_modules/dagit/dagit/webapp/src/graph/PipelineColorScale.ts
@@ -2,11 +2,19 @@ import { Colors } from "@blueprintjs/core";
 import { scaleOrdinal } from "@vx/scale";
 
 const PipelineColorScale = scaleOrdinal({
-  domain: ["source", "input", "solid", "output", "materializations"],
+  domain: [
+    "source",
+    "input",
+    "solid",
+    "solidDarker",
+    "output",
+    "materializations"
+  ],
   range: [
     Colors.TURQUOISE5,
     Colors.TURQUOISE3,
     "#DBE6EE",
+    "#7D8C97",
     Colors.BLUE3,
     Colors.ORANGE5
   ]
--- python_modules/dagit/dagit/webapp/src/graph/SolidNode.tsx
+++ python_modules/dagit/dagit/webapp/src/graph/SolidNode.tsx
@@ -11,6 +11,7 @@ import { IFullSolidLayout, ILayout } from "./getFullSolidLayout";
 import {
   SVGEllipseInRect,
   SVGFlowLayoutRect,
+  SVGFlowLayoutFiller,
   SVGMonospaceText
 } from "./SVGComponents";
 
@@ -23,6 +24,120 @@ interface ISolidNodeProps {
   onClick?: (solid: string) => void;
   onDoubleClick?: (solid: string) => void;
 }
+
+interface ISolidTagsProps {
+  x: number;
+  y: number;
+  width: number;
+  minified: boolean;
+  tags: string[];
+}
+
+function hueForTag(text = "") {
+  return (
+    text
+      .split("")
+      .map(c => c.charCodeAt(0))
+      .reduce((n, a) => n + a) % 360
+  );
+}
+
+const SolidTags: React.SFC<ISolidTagsProps> = ({
+  tags,
+  x,
+  y,
+  width,
+  minified
+}) => {
+  const height = minified ? 32 : 20;
+  const overhang = 6;
+
+  return (
+    <SVGFlowLayoutRect
+      x={x}
+      y={y - (height - overhang)}
+      width={width}
+      height={height}
+      fill={"transparent"}
+      stroke={"transparent"}
+      spacing={minified ? 8 : 4}
+      padding={0}
+    >
+      <SVGFlowLayoutFiller />
+      {tags.map(tag => {
+        const hue = hueForTag(tag);
+        return (
+          <SVGFlowLayoutRect
+            key={tag}
+            rx={0}
+            ry={0}
+            height={height}
+            padding={minified ? 8 : 4}
+            fill={`hsl(${hue}, 10%, 95%)`}
+            stroke={`hsl(${hue}, 55%, 55%)`}
+            strokeWidth={1}
+            spacing={0}
+          >
+            <SVGMonospaceText
+              text={tag}
+              fill={`hsl(${hue}, 55%, 55%)`}
+              size={minified ? 24 : 14}
+            />
+          </SVGFlowLayoutRect>
+        );
+      })}
+    </SVGFlowLayoutRect>
+  );
+};
+
+interface ISolidConfigNubProps {
+  x: number;
+  y: number;
+  minified: boolean;
+}
+
+const SolidConfigNub: React.SFC<ISolidConfigNubProps> = ({
+  x,
+  y,
+  minified
+}) => {
+  return (
+    <>
+      <SVGEllipseInRect
+        x={x}
+        y={y}
+        width={26}
+        height={26}
+        stroke={Colors.GRAY3}
+        fill={PipelineColorScale("solid")}
+        pathLength={100}
+        strokeWidth={1}
+        strokeDasharray={`0 50 0`}
+      />
+      <SVGEllipseInRect
+        x={x + 3}
+        y={y + 3}
+        width={20}
+        height={20}
+        stroke={Colors.WHITE}
+        fill={PipelineColorScale("solidDarker")}
+        pathLength={100}
+        strokeWidth={2}
+      />
+      {!minified && (
+        <text
+          x={x + 8}
+          y={y + 7.5}
+          style={{ font: `14px "Arial", san-serif` }}
+          fill={Colors.WHITE}
+          dominantBaseline="hanging"
+        >
+          C
+        </text>
+      )}
+    </>
+  );
+};
 
 export default class SolidNode extends React.Component<ISolidNodeProps> {
   static fragments = {
@@ -100,7 +215,13 @@ export default class SolidNode extends React.Component<ISolidNodeProps> {
             spacing={7}
             height={height}
           >
-            <SVGEllipseInRect width={14} height={14} />
+            <SVGEllipseInRect
+              width={14}
+              height={14}
+              fill="rgba(0, 0, 0, 0.3)"
+              stroke="white"
+              strokeWidth={1.5}
+            />
             {showText && (
               <SVGMonospaceText
                 text={`${input!.definition.name}:`}
@@ -168,18 +289,27 @@ export default class SolidNode extends React.Component<ISolidNodeProps> {
   }
 
   public render() {
-    const { solid, layout } = this.props;
+    const { solid, layout, dim, selected, minified } = this.props;
+    const { x, y, width, height } = layout.solid;
 
     return (
       <g
         onClick={this.handleClick}
         onDoubleClick={this.handleDoubleClick}
-        opacity={this.props.dim ? 0.3 : 1}
+        opacity={dim ? 0.3 : 1}
       >
-        {this.props.selected && this.renderSelectedBox()}
+        {selected && this.renderSelectedBox()}
         {this.renderSolid()}
         {this.renderIO("input", solid.inputs, layout.inputs)}
         {this.renderIO("output", solid.outputs, layout.outputs)}
+        <SolidConfigNub minified={minified} x={x + width - 33} y={y - 13} />
+        <SolidTags
+          x={x}
+          y={y + height}
+          width={width + 5}
+          minified={minified}
+          tags={["ipynb", "bla"]}
+        />
       </g>
     );
   }
