69ad1f9c081038bcf0c1c4b173da94294db0f1e3
Max Gasner <max.gasner@gmail.com>
2018-12-07 09:55:15 -0800

    Images for polyglot CircleCI builds

---
+++ .circleci/images/build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+docker build -f Dockerfile-2.7 -t dagster/cci-test:2.7 .
+docker build -f Dockerfile-3.5 -t dagster/cci-test:3.5 .
+docker build -f Dockerfile-3.6 -t dagster/cci-test:3.6 .
+
+docker push dagster/cci-test:2.7
+docker push dagster/cci-test:3.5
+docker push dagster/cci-test:3.6
