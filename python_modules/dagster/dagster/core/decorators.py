f373577f2245189221f604fac68a0afb442ac983
mgasner <max.gasner@gmail.com>
2018-12-11 11:21:45 -0800

    Improve error messages (#386)
    * Fix return values
    
    * Better error messages
    
    Improve error messages
    
    * Use DagsterInvariantViolationError

--- python_modules/dagster/dagster/core/decorators.py
+++ python_modules/dagster/dagster/core/decorators.py
@@ -11,6 +11,7 @@ from .definitions import (
     SolidDefinition,
     check,
 )
+from .errors import DagsterInvariantViolationError
 
 if hasattr(inspect, 'signature'):
     funcsigs = inspect
@@ -331,8 +332,23 @@ def _create_solid_transform_wrapper(fn, input_defs, output_defs):
             elif len(output_defs) == 1:
                 yield Result(value=result, output_name=output_defs[0].name)
             elif result is not None:
-                # XXX(freiksenet)
-                raise Exception('Output for a solid without an output.')
+                raise DagsterInvariantViolationError(
+                    'Solid unexpectedly returned output {result} of type {type_}. Should be a '
+                    'MultipleResults object, or a generator, containing or yielding {n_results} '
+                    'results: {{{expected_results}}}.'.format(
+                        result=result,
+                        type_=type(result),
+                        n_results=len(output_defs),
+                        expected_results=', '.join(
+                            [
+                                '\'{result_name}\': {dagster_type}'.format(
+                                    result_name=output_def.name,
+                                    dagster_type=output_def.dagster_type
+                                ) for output_def in output_defs
+                            ]
+                        )
+                    )
+                )
 
     return transform
 
