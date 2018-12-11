3cc5a50d9a63197999b2c7e9a5f98a98b4d76b80
Nicholas Schrock <schrockn@elementl.com>
2018-12-11 14:49:38 -0800

    tests

---
+++ python_modules/dagster/dagster/utils/utils_tests/test_yaml.py
@@ -0,0 +1,43 @@
+from dagster.utils.yaml_utils import (
+    load_yaml_from_path,
+    load_yaml_from_glob_list,
+)
+
+from dagster.utils import script_relative_path
+
+
+def test_load_yaml():
+    assert load_yaml_from_path(script_relative_path('yamls/yaml_one.yaml')) == {
+        'key_one': {
+            'key_one_one': 'value_one',
+        },
+    }
+
+
+def test_from_glob_list():
+    assert load_yaml_from_glob_list([script_relative_path('yamls/yaml_one.yaml')]) == {
+        'key_one': {
+            'key_one_one': 'value_one',
+        },
+    }
+
+    assert load_yaml_from_glob_list(
+        [
+            script_relative_path('yamls/yaml_one.yaml'),
+            script_relative_path('yamls/yaml_two.yaml'),
+        ]
+    ) == {
+        'key_one': {
+            'key_one_one': 'value_one',
+            'key_one_two': 'value_two',
+        },
+    }
+
+    assert load_yaml_from_glob_list([
+        script_relative_path('yamls/*.yaml'),
+    ]) == {
+        'key_one': {
+            'key_one_one': 'value_one',
+            'key_one_two': 'value_two',
+        },
+    }
