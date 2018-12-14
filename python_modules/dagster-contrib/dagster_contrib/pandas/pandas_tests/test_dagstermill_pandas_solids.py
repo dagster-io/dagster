46cfe5e52313a471a1b96062dd7dd36a8857aa80
Nicholas Schrock <schrockn@elementl.com>
2018-12-14 04:07:33 -0800

    Mark all notebook tests with tag

--- python_modules/dagster-contrib/dagster_contrib/pandas/pandas_tests/test_dagstermill_pandas_solids.py
+++ python_modules/dagster-contrib/dagster_contrib/pandas/pandas_tests/test_dagstermill_pandas_solids.py
@@ -26,13 +26,17 @@ def nb_test_path(name):
 
 
 def notebook_test(f):
-    return pytest.mark.skipif(
-        sys.version_info < (3, 5),
-        reason='''Notebooks execute in their own process and hardcode what "kernel" they use.
+    # mark this with the "notebook_test" tag so that they can be all be skipped
+    # (for performance reasons) and mark them as python3 only
+    return pytest.mark.notebook_test(
+        pytest.mark.skipif(
+            sys.version_info < (3, 5),
+            reason='''Notebooks execute in their own process and hardcode what "kernel" they use.
         All of the development notebooks currently use the python3 "kernel" so they will
         not be executable in a container that only have python2.7 (e.g. in CircleCI)
         ''',
-    )(f)
+        )(f)
+    )
 
 
 def define_pandas_input_transform_test_solid():
