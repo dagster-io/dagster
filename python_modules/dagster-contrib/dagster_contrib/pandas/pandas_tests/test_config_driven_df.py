107319a226ee8536852719545c904eeca3ba16d9
Nicholas Schrock <schrockn@elementl.com>
2018-12-17 06:50:21 -0800

    through some witchcraft this worked on the first shot 🙌🏻🙌🏻🙌🏻

---
+++ python_modules/dagster-contrib/dagster_contrib/pandas/pandas_tests/test_config_driven_df.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+from dagster import (
+    PipelineDefinition,
+    execute_pipeline,
+    solid,
+    types,
+)
+
+from dagster.utils import script_relative_path
+
+from dagster_contrib.pandas import DataFrame
+
+
+def test_dataframe():
+    assert DataFrame.name == 'PandasDataFrame'
+
+
+def test_dataframe_csv_from_config():
+    called = {}
+
+    @solid(config_field=types.Field(DataFrame))
+    def df_as_config(info):
+        assert info.config.to_dict('list') == {
+            'num1': [1, 3],
+            'num2': [2, 4],
+        }
+        called['yup'] = True
+
+    pipeline = PipelineDefinition(solids=[df_as_config])
+
+    result = execute_pipeline(
+        pipeline,
+        {
+            'solids': {
+                'df_as_config': {
+                    'config': {
+                        'csv': {
+                            'path': script_relative_path('num.csv'),
+                        },
+                    },
+                },
+            },
+        },
+    )
+
+    assert result.success
+    assert called['yup']
