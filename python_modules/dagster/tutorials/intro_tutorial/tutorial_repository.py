ef7e105b34b156551e64b2a92c3974573625d12a
Max Gasner <max.gasner@gmail.com>
2018-12-17 17:23:42 -0800

    Tutorial cp

--- python_modules/dagster/tutorials/intro_tutorial/tutorial_repository.py
+++ python_modules/dagster/tutorials/intro_tutorial/tutorial_repository.py
@@ -4,6 +4,7 @@ from part_one import define_hello_world_pipeline
 from part_two import define_hello_dag_pipeline
 from part_three import define_diamond_dag_pipeline
 from part_four import define_configurable_hello_world_pipeline
+from part_five import define_execution_context_pipeline_step_two
 
 
 def define_repository():
@@ -14,5 +15,6 @@ def define_repository():
             'part_two_pipeline': define_hello_dag_pipeline,
             'part_three_pipeline': define_diamond_dag_pipeline,
             'part_four_pipeline': define_configurable_hello_world_pipeline,
+            'part_five_pipeline': define_execution_context_pipeline_step_two,
         },
     )
