eb4117b7d2980bee30c0c3eefead29ab6d828032
Mikhail Novikov <freiksenet@gmail.com>
2018-12-21 16:03:17 +0200

    Another attempt

--- python_modules/dagit/dagit/dagit_tests/test_smoke.py
+++ python_modules/dagit/dagit/dagit_tests/test_smoke.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 import json
 from dagit import app
 from dagit.pipeline_run_storage import PipelineRunStorage
