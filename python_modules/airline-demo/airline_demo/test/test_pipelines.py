d100904abf9755ef8ac875f877d0c468c4433434
Nicholas Schrock <schrockn@elementl.com>
2018-12-26 11:09:31 -0800

    final cleanup

--- python_modules/airline-demo/airline_demo/test/test_pipelines.py
+++ python_modules/airline-demo/airline_demo/test/test_pipelines.py
@@ -4,15 +4,15 @@ import pytest
 
 from dagster import execute_pipeline
 
+from dagster.utils import (
+    load_yaml_from_globs,
+    script_relative_path,
+)
+
 from airline_demo.pipelines import (
     define_airline_demo_download_pipeline,
     define_airline_demo_ingest_pipeline,
     define_airline_demo_warehouse_pipeline,
-)
-
-from dagster.utils import (
-    load_yaml_from_glob_list,
-    script_relative_path,
 )
 
 
@@ -21,14 +21,9 @@ from dagster.utils import (
 @pytest.mark.db
 @pytest.mark.py3
 def test_pipelines():
-    now = datetime.datetime.utcnow()
-    timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
-
-    download_config_object = load_yaml_from_glob_list(
-        [
-            script_relative_path('../../environments/local_base.yml'),
-            script_relative_path('../../environments/local_fast_download.yml'),
-        ]
+    download_config_object = load_yaml_from_globs(
+        script_relative_path('../../environments/local_base.yml'),
+        script_relative_path('../../environments/local_fast_download.yml'),
     )
 
     result_download = execute_pipeline(
@@ -38,11 +33,9 @@ def test_pipelines():
 
     assert result_download.success
 
-    ingest_config_object = load_yaml_from_glob_list(
-        [
-            script_relative_path('../../environments/local_base.yml'),
-            script_relative_path('../../environments/local_fast_ingest.yml'),
-        ]
+    ingest_config_object = load_yaml_from_globs(
+        script_relative_path('../../environments/local_base.yml'),
+        script_relative_path('../../environments/local_fast_ingest.yml'),
     )
 
     result_ingest = execute_pipeline(
@@ -52,11 +45,9 @@ def test_pipelines():
 
     assert result_ingest.success
 
-    warehouse_config_object = load_yaml_from_glob_list(
-        [
-            script_relative_path('../../environments/local_base.yml'),
-            script_relative_path('../../environments/local_fast_warehouse.yml'),
-        ]
+    warehouse_config_object = load_yaml_from_globs(
+        script_relative_path('../../environments/local_base.yml'),
+        script_relative_path('../../environments/local_fast_warehouse.yml'),
     )
 
     result_warehouse = execute_pipeline(
