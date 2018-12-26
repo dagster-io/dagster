0f8e3406cd6995d714525815866b9403dd1c027b
Nicholas Schrock <schrockn@elementl.com>
2018-12-26 11:11:26 -0800

    add test

--- python_modules/dagster/dagster/utils/utils_tests/test_yaml.py
+++ python_modules/dagster/dagster/utils/utils_tests/test_yaml.py
@@ -1,6 +1,7 @@
 from dagster.utils.yaml_utils import (
-    load_yaml_from_path,
     load_yaml_from_glob_list,
+    load_yaml_from_globs,
+    load_yaml_from_path,
 )
 
 from dagster.utils import script_relative_path
@@ -36,6 +37,16 @@ def test_from_glob_list():
     assert load_yaml_from_glob_list([
         script_relative_path('yamls/*.yaml'),
     ]) == {
+        'key_one': {
+            'key_one_one': 'value_one',
+            'key_one_two': 'value_two',
+        },
+    }
+
+    assert load_yaml_from_globs(
+        script_relative_path('yamls/yaml_one.yaml'),
+        script_relative_path('yamls/yaml_two.yaml'),
+    ) == {
         'key_one': {
             'key_one_one': 'value_one',
             'key_one_two': 'value_two',
