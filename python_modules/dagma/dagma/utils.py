1ad41305c673525dc5fd81a7ae1201b874a727aa
Max Gasner <max.gasner@gmail.com>
2018-12-27 15:54:12 -0800

    Typo

--- python_modules/dagma/dagma/utils.py
+++ python_modules/dagma/dagma/utils.py
@@ -22,7 +22,7 @@ def get_resources_key(context):
 
 
 LambdaInvocationPayload = namedtuple(
-    'LambdaInvocationPayload', 'run_id step_idx key s3_bucket s3_key_inputs s3_key_body'
+    'LambdaInvocationPayload', 'run_id step_idx key s3_bucket s3_key_inputs s3_key_body '
     's3_key_resources s3_key_outputs'
 )
 
