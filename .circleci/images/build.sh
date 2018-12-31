38dde1a6d27c54c9d761d8e87708bb8ebac38e68
Nicholas Schrock <schrockn@elementl.com>
2018-12-31 13:40:55 -0800

    cp

--- .circleci/images/build.sh
+++ .circleci/images/build.sh
@@ -8,3 +8,5 @@ docker build -f Dockerfile-3.7.2 -t dagster/cci-test:3.7.2 .
 docker push dagster/cci-test:2.7
 docker push dagster/cci-test:3.5
 docker push dagster/cci-test:3.6
+# Pending resolution of creds issue
+# docker push dagster/cci-test:3.7.2
