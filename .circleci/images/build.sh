0c46ccb179a28cf9c3b9b3c6da919511d4eb133b
Nicholas Schrock <schrockn@elementl.com>
2018-12-31 13:34:44 -0800

    take a stab at 37 circleci

--- .circleci/images/build.sh
+++ .circleci/images/build.sh
@@ -3,6 +3,7 @@
 docker build -f Dockerfile-2.7 -t dagster/cci-test:2.7 .
 docker build -f Dockerfile-3.5 -t dagster/cci-test:3.5 .
 docker build -f Dockerfile-3.6 -t dagster/cci-test:3.6 .
+docker build -f Dockerfile-3.7.2 -t dagster/cci-test:3.7.2 .
 
 docker push dagster/cci-test:2.7
 docker push dagster/cci-test:3.5
--- airline_demo_env_setup.sh
+++ airline_demo_env_setup.sh
@@ -1,6 +1,7 @@
+# Requires docker running on your system
+#
 # This took 23 minutes first time it ran
 pushd python_modules/airline-demo
 docker-compose up --detach
 popd
-
 
