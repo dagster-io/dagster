0807394b2cf2c849dd8fa4570225f7eaf66d85c2
Nicholas Schrock <schrockn@elementl.com>
2019-01-02 14:36:27 -0800

    cp

--- python_modules/dagster/dagster/dagster_tests/test_formatting.py
+++ python_modules/dagster/dagster/dagster_tests/test_formatting.py
@@ -15,8 +15,6 @@ from dagster.utils import script_relative_path
     ''',
 )
 def test_formatting():
-    # cwd = os.getcwd()
-    # try:
     cmd = 'black {target} --diff --line-length 100 -S --fast'.format(
         target=script_relative_path('..')
     )
@@ -26,9 +24,3 @@ def test_formatting():
 
     assert p_status == 0
     assert 'reformatted' not in output.decode('utf-8')
-
-    print(output)
-    print(p_status)
-    #     pass
-    # finally:
-    #     os.chdir(cwd)
