7e65a739c57ce5c31161327276163c1efe70c340
Nicholas Schrock <schrockn@elementl.com>
2019-01-02 13:49:16 -0800

    fix dagster-ge

--- python_modules/dagster-ge/dagster_ge/__init__.py
+++ python_modules/dagster-ge/dagster_ge/__init__.py
@@ -19,10 +19,11 @@ def json_config_expectation(name, file_path):
             # on the same dataframe instance, changing the type. But a
             # ge can't be copied, because it causes an error.
             # The error is
-            #  AttributeError: 'PandasDataset' object has no attribute 'discard_subset_failing_expectations'
+            #  AttributeError: 'PandasDataset' object has no attribute
+            #  'discard_subset_failing_expectations'
             # See https://github.com/great-expectations/great_expectations/issues/342
             df_copy = copy.deepcopy(df)
-            ge_df = ge.from_pandas(df_copy, expt_config)
+            ge_df = ge.from_pandas(df_copy, expectations_config=expt_config)
             validate_result = ge_df.validate()
             check.invariant('success' in validate_result)
             check.invariant('results' in validate_result)
