a540a7298277d9b9ce96868824ceab53a3261121
Ben Gotow <bengotow@gmail.com>
2019-01-02 17:54:24 -0800

    Switch to mutation observer for more reliable scroll-to-bottom behavior #438  (#515)
    This PR also fixes a small bug that caused cell heights to get "stuck" when you filtered the list or otherwise caused the cells at certain positions to change.

--- python_modules/dagster-contrib/dagster_contrib/pandas/__init__.py
+++ python_modules/dagster-contrib/dagster_contrib/pandas/__init__.py
@@ -40,9 +40,13 @@ def define_csv_dict_field():
     )
 
 
-class _PandasDataFrameMaterializationConfigSchema(ConfigurableSelectorFromDict):
+class _PandasDataFrameMaterializationConfigSchema(
+    ConfigurableSelectorFromDict,
+    types.DagsterType
+):
     def __init__(self):
         super(_PandasDataFrameMaterializationConfigSchema, self).__init__(
+            name='PandasDataFrameMaterializationConfigSchema',
             fields={
                 'csv': define_csv_dict_field(),
                 'parquet': define_path_dict_field(),
