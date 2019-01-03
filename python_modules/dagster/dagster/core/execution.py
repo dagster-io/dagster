779374668fdf83bf193a83630801c157bf6f6974
Nicholas Schrock <schrockn@elementl.com>
2019-01-03 09:08:07 -0800

    Add subset execution to execute_pipeline (#500)
    Adding solid_subset to the top-level execution API.

--- python_modules/dagster/dagster/core/execution.py
+++ python_modules/dagster/dagster/core/execution.py
@@ -482,6 +482,7 @@ def execute_pipeline_iterator(
     environment=None,
     throw_on_error=True,
     reentrant_info=None,
+    solid_subset=None,
 ):
     '''Returns iterator that yields :py:class:`SolidExecutionResult` for each
     solid executed in the pipeline.
@@ -498,10 +499,16 @@ def execute_pipeline_iterator(
     typed_environment = create_typed_environment(pipeline, environment)
     check.bool_param(throw_on_error, 'throw_on_error')
     check.opt_inst_param(reentrant_info, 'reentrant_info', ReentrantInfo)
+    check.opt_list_param(solid_subset, 'solid_subset', of_type=str)
 
-    with yield_context(pipeline, typed_environment, reentrant_info) as context:
+    pipeline_to_execute = pipeline if solid_subset is None else build_sub_pipeline(
+        pipeline,
+        solid_subset,
+    )
+
+    with yield_context(pipeline_to_execute, typed_environment, reentrant_info) as context:
         for solid_result in _do_iterate_pipeline(
-            pipeline,
+            pipeline_to_execute,
             context,
             typed_environment,
             throw_on_error,
@@ -581,6 +588,7 @@ def execute_pipeline(
     environment=None,
     throw_on_error=True,
     reentrant_info=None,
+    solid_subset=None,
 ):
     '''
     "Synchronous" version of :py:function:`execute_pipeline_iterator`.
@@ -603,10 +611,16 @@ def execute_pipeline(
     check.opt_dict_param(environment, 'environment')
     check.bool_param(throw_on_error, 'throw_on_error')
     check.opt_inst_param(reentrant_info, 'reentrant_info', ReentrantInfo)
+    check.opt_list_param(solid_subset, 'solid_subset', of_type=str)
 
     typed_environment = create_typed_environment(pipeline, environment)
-
-    return execute_reentrant_pipeline(pipeline, typed_environment, throw_on_error, reentrant_info)
+    return execute_reentrant_pipeline(
+        pipeline,
+        typed_environment,
+        throw_on_error,
+        reentrant_info,
+        solid_subset,
+    )
 
 
 def _dep_key_of(solid):
@@ -655,16 +669,30 @@ def execute_reentrant_pipeline(
     typed_environment,
     throw_on_error,
     reentrant_info,
+    solid_subset=None,
 ):
     check.inst_param(pipeline, 'pipeline', PipelineDefinition)
     check.inst_param(typed_environment, 'typed_environment', config.Environment)
     check.opt_inst_param(reentrant_info, 'reentrant_info', ReentrantInfo)
+    check.opt_list_param(solid_subset, 'solid_subset', of_type=str)
 
-    with yield_context(pipeline, typed_environment, reentrant_info) as context:
+    pipeline_to_execute = pipeline if solid_subset is None else build_sub_pipeline(
+        pipeline,
+        solid_subset,
+    )
+
+    with yield_context(pipeline_to_execute, typed_environment, reentrant_info) as context:
         return PipelineExecutionResult(
-            pipeline,
+            pipeline_to_execute,
             context,
-            list(_do_iterate_pipeline(pipeline, context, typed_environment, throw_on_error)),
+            list(
+                _do_iterate_pipeline(
+                    pipeline_to_execute,
+                    context,
+                    typed_environment,
+                    throw_on_error,
+                )
+            ),
         )
 
 
