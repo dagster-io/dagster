a59d200cb248ec77646701c985374ae6d4f66db4
Max Gasner <max.gasner@gmail.com>
2019-01-10 11:22:13 -0800

    Add snapshot test for doc build

--- python_modules/dagster/docs/test_doc_build.py
+++ python_modules/dagster/docs/test_doc_build.py
@@ -1,13 +1,20 @@
 import os
 import subprocess
+
+import pytest
+
 from dagster.utils import script_relative_path
 
 
-def test_build_all_docs():
+BUILT_DOCS_RELATIVE_PATH = '_build/'
+
+def test_build_all_docs(snapshot):
     pwd = os.getcwd()
     try:
         os.chdir(script_relative_path('.'))
         subprocess.check_output(['make', 'clean'])
         subprocess.check_output(['make', 'html'])
+        os.chdir(script_relative_path(BUILT_DOCS_RELATIVE_PATH))
+        snapshot.assert_match(list(os.walk('.')))
     finally:
         os.chdir(pwd)
