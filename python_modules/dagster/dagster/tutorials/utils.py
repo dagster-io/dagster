6a4ea494b2cca2239388aedcfd01835341af8dba
Max Gasner <max.gasner@gmail.com>
2019-01-10 11:57:57 -0800

    Black

--- python_modules/dagster/dagster/tutorials/utils.py
+++ python_modules/dagster/dagster/tutorials/utils.py
@@ -6,7 +6,17 @@ def check_script(path):
 
 
 def check_cli_execute_file_pipeline(path, pipeline_fn_name, env_file=None):
-    cli_cmd = ['python', '-m', 'dagster', 'pipeline', 'execute', '-f', path, '-n', pipeline_fn_name]
+    cli_cmd = [
+        'python',
+        '-m',
+        'dagster',
+        'pipeline',
+        'execute',
+        '-f',
+        path,
+        '-n',
+        pipeline_fn_name,
+    ]
 
     if env_file:
         cli_cmd.append('-e')
