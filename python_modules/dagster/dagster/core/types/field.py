e99bf61af7472f5635aa9c413ae298f2aa162d60
Nicholas Schrock <schrockn@elementl.com>
2019-01-13 09:44:55 -0800

    extract default applier

--- python_modules/dagster/dagster/core/types/field.py
+++ python_modules/dagster/dagster/core/types/field.py
@@ -12,6 +12,7 @@ from .config import (
     DEFAULT_TYPE_ATTRIBUTES,
 )
 from .dagster_type import check_dagster_type_param
+from .default_applier import apply_default_values
 
 
 class __FieldValueSentinel:
@@ -76,9 +77,7 @@ class Field:
         if is_optional == INFER_OPTIONAL_COMPOSITE_FIELD:
             is_optional = all_optional_type(self.config_type)
             if is_optional is True:
-                from .evaluator import hard_create_config_value
-
-                self._default_value = lambda: hard_create_config_value(self.config_type, None)
+                self._default_value = lambda: apply_default_values(self.config_type, None)
             else:
                 self._default_value = default_value
         else:
