fb3c16c9c350f72df0b256cb052dc7b1c60ee9eb
Nicholas Schrock <schrockn@elementl.com>
2019-01-15 12:40:56 -0800

    tests done

--- python_modules/dagster/dagster/tutorials/test_intro_tutorial_part_ten.py
+++ python_modules/dagster/dagster/tutorials/test_intro_tutorial_part_ten.py
@@ -1,26 +1,22 @@
-# pylint: disable=W0622,W0614,W0401
 import pytest
 
 from dagster import (
     DagsterExpectationFailedError,
-    DependencyDefinition,
     ExpectationDefinition,
     ExpectationResult,
-    Field,
     InputDefinition,
     Int,
     OutputDefinition,
     PipelineDefinition,
     execute_pipeline,
     lambda_solid,
-    solid,
 )
 
 
-@solid(
-    config_field=Field(Int),
-    outputs=[
-        OutputDefinition(
+@lambda_solid(
+    inputs=[
+        InputDefinition(
+            'num_one',
             Int,
             expectations=[
                 ExpectationDefinition(
@@ -30,45 +26,34 @@ from dagster import (
                     ),
                 )
             ],
-        )
+        ),
+        InputDefinition('num_two', Int),
     ],
-)
-def ingest_a(info):
-    return info.config
-
-
-@solid(config_field=Field(Int), outputs=[OutputDefinition(Int)])
-def ingest_b(info):
-    return info.config
-
-
-@lambda_solid(
-    inputs=[InputDefinition('num_one', Int), InputDefinition('num_two', Int)],
     output=OutputDefinition(Int),
 )
 def add_ints(num_one, num_two):
     return num_one + num_two
 
 
-def define_part_ten_step_one_pipeline():
+def define_expectations_tutorial_pipeline():
     return PipelineDefinition(
-        name='part_ten_step_one_pipeline',
-        solids=[ingest_a, ingest_b, add_ints],
-        dependencies={
-            'add_ints': {
-                'num_one': DependencyDefinition('ingest_a'),
-                'num_two': DependencyDefinition('ingest_b'),
-            }
-        },
+        name='part_ten_step_one_pipeline', solids=[add_ints]
     )
 
 
 def test_intro_tutorial_part_ten_step_one():
     result = execute_pipeline(
-        define_part_ten_step_one_pipeline(),
+        define_expectations_tutorial_pipeline(),
         {
             'context': {'default': {'config': {'log_level': 'DEBUG'}}},
-            'solids': {'ingest_a': {'config': 2}, 'ingest_b': {'config': 3}},
+            'solids': {
+                'add_ints': {
+                    'inputs': {
+                        'num_one': {'value': 2},
+                        'num_two': {'value': 3},
+                    }
+                }
+            },
         },
     )
 
@@ -78,34 +63,27 @@ def test_intro_tutorial_part_ten_step_one():
 def define_failing_environment_config():
     return {
         'context': {'default': {'config': {'log_level': 'DEBUG'}}},
-        'solids': {'ingest_a': {'config': -2}, 'ingest_b': {'config': 3}},
+        'solids': {
+            'add_ints': {
+                'inputs': {'num_one': {'value': -2}, 'num_two': {'value': 3}}
+            }
+        },
     }
 
 
 def test_intro_tutorial_part_ten_step_two_fails_hard():
     with pytest.raises(DagsterExpectationFailedError):
         execute_pipeline(
-            define_part_ten_step_one_pipeline(),
+            define_expectations_tutorial_pipeline(),
             define_failing_environment_config(),
         )
 
 
 def test_intro_tutorial_part_ten_step_two_fails_soft():
     result = execute_pipeline(
-        define_part_ten_step_one_pipeline(),
+        define_expectations_tutorial_pipeline(),
         define_failing_environment_config(),
         throw_on_error=False,
     )
 
     assert not result.success
-
-
-if __name__ == '__main__':
-    execute_pipeline(
-        define_part_ten_step_one_pipeline(),
-        {
-            'context': {'default': {'config': {'log_level': 'DEBUG'}}},
-            'solids': {'ingest_a': {'config': -2}, 'ingest_b': {'config': 3}},
-            'expectations': {'evaluate': True},
-        },
-    )
