aa0a07a2067a3d937cda6165fe8268f8a6f9a5cf
Max Gasner <max.gasner@gmail.com>
2019-01-17 14:04:22 -0800

    black

--- python_modules/dagster/dagster_tests/cli_tests/test_version.py
+++ python_modules/dagster/dagster_tests/cli_tests/test_version.py
@@ -9,4 +9,3 @@ def test_version():
     assert subprocess.check_output(['dagster', '--version']) == bytes(
         ('dagster, version {version}\n'.format(version=__version__)).encode('utf-8')
     )
-
