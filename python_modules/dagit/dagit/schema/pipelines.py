95e094daba53db4b22f1ba8e3d2964f0911552cf
Nicholas Schrock <schrockn@elementl.com>
2019-01-22 12:51:14 -0800

    fix

--- python_modules/dagit/dagit/schema/pipelines.py
+++ python_modules/dagit/dagit/schema/pipelines.py
@@ -15,7 +15,7 @@ from dagster.core.definitions import Solid, SolidInputHandle, SolidOutputHandle
 
 from dagit.schema import dauphin
 
-from .config_types types import to_dauphin_config_type
+from .config_types import to_dauphin_config_type
 from .legacy_types import all_types
 
 
