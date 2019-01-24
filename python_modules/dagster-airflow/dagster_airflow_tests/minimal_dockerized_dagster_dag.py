dbc0d0c9030827aa8b6f0742195c4147f7a23304
Max Gasner <max.gasner@gmail.com>
2019-01-24 12:05:21 -0800

    Cp

--- python_modules/dagster-airflow/dagster_airflow_tests/minimal_dockerized_dagster_dag.py
+++ python_modules/dagster-airflow/dagster_airflow_tests/minimal_dockerized_dagster_dag.py
@@ -1,12 +1,17 @@
+# The strings 'airflow' and 'DAG' must both appear in this file in order for it to be parsed by
+# airflow as a DAG definition.
+
 import errno
 import os
 
 from datetime import datetime, timedelta
 
-from airflow import DAG
-from airflow.operators.dagster_plugin import ModifiedDockerOperator
+# from airflow import DAG
+from dagster.utils import load_yaml_from_path, script_relative_path
 
 from dagster_airflow.scaffold import scaffold_airflow_dag
+
+from .test_project.dagster_airflow_demo import define_demo_execution_pipeline
 
 
 default_args = {
@@ -20,4 +25,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = scaffold_airflow_dag()
+pipeline = define_demo_execution_pipeline()
+env_config = load_yaml_from_path(script_relative_path('test_project/env.yml'))
+
+dag = scaffold_airflow_dag(pipeline, env_config)
