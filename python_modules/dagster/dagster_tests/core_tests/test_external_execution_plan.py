eb132fe36f1b96b6f39bc84434b6c7713f8e0dd7
Nicholas Schrock <schrockn@elementl.com>
2019-01-24 12:57:34 -0800

    cp

---
+++ python_modules/dagster/dagster_tests/core_tests/test_external_execution_plan.py
@@ -0,0 +1,55 @@
+from dagster import (
+    DependencyDefinition,
+    InputDefinition,
+    Int,
+    OutputDefinition,
+    PipelineDefinition,
+    lambda_solid,
+)
+from dagster.core.execution import execute_externalized_plan
+from dagster.core.execution_plan.objects import StepTag
+from dagster.core.types.runtime import resolve_to_runtime_type
+
+from dagster.utils.test import get_temp_file_name
+
+
+def test_basic_pipeline_external_plan_execution():
+    @lambda_solid
+    def return_one():
+        return 1
+
+    @lambda_solid(inputs=[InputDefinition('num', Int)], output=OutputDefinition(Int))
+    def add_one(num):
+        return num + 1
+
+    pipeline = PipelineDefinition(
+        name='basic_external_plan_execution',
+        solids=[return_one, add_one],
+        dependencies={'add_one': {'num': DependencyDefinition('return_one')}},
+    )
+
+    with get_temp_file_name() as temp_path:
+
+        int_type = resolve_to_runtime_type(Int)
+
+        int_type.marshalling_strategy.marshal_value(5, temp_path)
+
+        results = execute_externalized_plan(
+            pipeline,
+            ['add_one.transform'],
+            inputs_to_marshal={'add_one.transform': {'num': temp_path}},
+        )
+
+    assert len(results) == 2
+
+    thunk_step_result = results[0]
+
+    assert thunk_step_result.tag == StepTag.VALUE_THUNK
+
+    transform_step_result = results[1]
+    print(transform_step_result)
+    assert transform_step_result.tag == StepTag.TRANSFORM
+    assert transform_step_result.success
+    assert transform_step_result.success_data.output_name == 'result'
+    assert transform_step_result.success_data.value == 6
+
