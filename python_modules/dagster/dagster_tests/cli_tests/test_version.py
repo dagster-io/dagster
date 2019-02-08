c21c29466362a98c9455951a4389d1dd2668292f
Max Gasner <max.gasner@gmail.com>
2019-02-08 14:31:46 -0500

    Boo hiss

--- python_modules/dagster/dagster_tests/cli_tests/test_version.py
+++ python_modules/dagster/dagster_tests/cli_tests/test_version.py
@@ -6,6 +6,6 @@ from dagster.version import __version__
 
 
 def test_version():
-    assert subprocess.check_output(['dagster', '--version']).strip('\n').strip('\r') == bytes(
-        ('dagster, version {version}'.format(version=__version__)).encode('utf-8')
-    )
+    assert subprocess.check_output(['dagster', '--version']).decode('utf-8').strip('\n').strip(
+        '\r'
+    ) == 'dagster, version {version}'.format(version=__version__)
