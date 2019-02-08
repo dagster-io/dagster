c7c2ca313f3001d554190b246a1effa501e1416a
Max Gasner <max.gasner@gmail.com>
2019-02-08 16:29:47 -0500

    Windows compat

--- python_modules/dagit/dagit_tests/test_version.py
+++ python_modules/dagit/dagit_tests/test_version.py
@@ -6,6 +6,6 @@ from dagit.version import __version__
 
 
 def test_version():
-    assert subprocess.check_output(['dagit', '--version']) == bytes(
-        ('dagit, version {version}\n'.format(version=__version__)).encode('utf-8')
-    )
+    assert subprocess.check_output(['dagit', '--version']).decode('utf-8').strip('\n').strip(
+        '\r'
+    ) == 'dagit, version {version}'.format(version=__version__)
