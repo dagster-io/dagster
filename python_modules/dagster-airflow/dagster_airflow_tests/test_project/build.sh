2505c0c1f9523891d3a938742bd4b6ee52d4b696
Max Gasner <max.gasner@gmail.com>
2019-02-11 11:42:19 -0800

    Install locally

--- python_modules/dagster-airflow/dagster_airflow_tests/test_project/build.sh
+++ python_modules/dagster-airflow/dagster_airflow_tests/test_project/build.sh
@@ -1,1 +1,8 @@
+#! /bin/bash
+# For the avoidance of doubt, this script is meant to be run with the test_project directory as pwd
+
+cp ../../../dagster .
+cp ../../../dagit .
 docker build -t dagster-airflow-demo .
+rm -r dagster
+rm -r dagit
