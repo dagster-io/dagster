aa4228e5255f69c84b758dbdcec582eb9dc948f2
Nicholas Schrock <schrockn@elementl.com>
2019-03-12 16:01:00 -0700

    moar tests

--- python_modules/dagstermill/dagstermill_tests/test_test_utils.py
+++ python_modules/dagstermill/dagstermill_tests/test_test_utils.py
@@ -1,5 +1,20 @@
+import pytest
+
 from dagstermill import DagstermillError
 from dagstermill.test_utils import notebook_test
+
+
+def test_normal_test():
+    has_been_called = {}
+
+    @notebook_test
+    def normal_test():
+        has_been_called['yup'] = True
+        assert True
+
+    normal_test()
+
+    assert 'yup' in has_been_called
 
 
 def test_retry_logic():
@@ -16,3 +31,18 @@ def test_retry_logic():
 
     assert has_been_called['yup']
     assert has_been_called['times'] == 2
+
+
+def test_double_fail():
+    has_been_called = {'times': 0}
+
+    @notebook_test
+    def always_fail():
+        has_been_called['times'] = has_been_called['times'] + 1
+        raise DagstermillError('Kernel died before replying to kernel_info')
+
+    with pytest.raises(DagstermillError):
+        always_fail()
+
+    assert has_been_called['times'] == 2
+
