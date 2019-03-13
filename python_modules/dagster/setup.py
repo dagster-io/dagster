d16c43120a6fcf84c097148c348f3022e0fe8afb
Max Gasner <max@elementl.com>
2019-03-13 14:14:41 -0700

    Bump pyyaml to 4.2b1

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -62,7 +62,7 @@ def _do_setup(name='dagster'):
             'coloredlogs>=10.0',
             'graphviz>=0.8.3',
             # pyyaml pinned for compatibility with docker-compose
-            'pyyaml==3.13',
+            'pyyaml==4.2b1',
             # core (not explicitly expressed atm)
             'six>=1.11.0',
             'toposort>=1.5',
