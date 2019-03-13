f0fe758ef250267fc3d2d7723bd73187e4dbab46
mgasner <max.gasner@gmail.com>
2019-03-13 12:34:24 -0700

    Pin pyyaml for docker-compose compatibility (#911)

--- python_modules/dagit/setup.py
+++ python_modules/dagit/setup.py
@@ -60,7 +60,8 @@ def _do_setup(name='dagit'):
             # 'click>=6.7',
             # 'coloredlogs>=10.0',
             # 'graphviz>=0.8.3',
-            'pyyaml>=3.12',
+            # pyyaml pinned for compatibility with docker-compose
+            'pyyaml==3.13',
             # core (not explicitly expressed atm)
             'six>=1.11.0',
             # cli
