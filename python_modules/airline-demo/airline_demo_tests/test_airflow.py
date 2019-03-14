af2fa6be740c110ea5f9ab6865bf3738c8794545
Max Gasner <max.gasner@gmail.com>
2019-03-14 14:28:43 -0700

    More extensive uncontainerized test

--- python_modules/airline-demo/airline_demo_tests/test_airflow.py
+++ python_modules/airline-demo/airline_demo_tests/test_airflow.py
@@ -2,10 +2,20 @@ import datetime
 import logging
 import os
 
+from collections import defaultdict
+
 from airflow.models import TaskInstance
 
-from dagster import execute_pipeline
+from dagster import execute_pipeline, RunConfig
+from dagster.core.execute_marshalling import execute_marshalling, MarshalledOutput
+from dagster.core.execution import yield_pipeline_execution_context
+from dagster.core.execution_plan import create_execution_plan_core
 from dagster.utils import load_yaml_from_glob_list, script_relative_path
+from dagster_airflow.scaffold import (
+    coalesce_execution_steps,
+    _key_for_marshalled_result,
+    _step_executions_key,
+)
 
 from airline_demo.pipelines import (
     define_airline_demo_download_pipeline,
@@ -31,6 +41,7 @@ def test_uncontainerized_download_dag_execution_with_airflow_config():
 
 
 def test_uncontainerized_ingest_dag_execution_with_airflow_config():
+    pipeline = define_airline_demo_ingest_pipeline()
     config_object = load_yaml_from_glob_list(
         [
             script_relative_path('../environments/airflow_base.yml'),
@@ -38,9 +49,65 @@ def test_uncontainerized_ingest_dag_execution_with_airflow_config():
         ]
     )
 
-    result = execute_pipeline(define_airline_demo_ingest_pipeline(), config_object)
+    with yield_pipeline_execution_context(pipeline, config_object, RunConfig()) as pipeline_context:
+        execution_plan = create_execution_plan_core(pipeline_context)
 
-    assert result.success
+    execution_steps = coalesce_execution_steps(execution_plan)
+
+    for (solid_name, solid_steps) in execution_steps:
+        step_output_keys = set([])
+        for step in solid_steps:
+            for step_output in step.step_outputs:
+                step_output_keys.add((step.key, step_output.name))
+
+        # this is a nested dict of step_key -> input_name -> marshalling_key
+        inputs_to_marshal = defaultdict(lambda: defaultdict(dict))
+
+
+        for step in solid_steps:
+            for step_input in step.step_inputs:
+                step_input_key = (
+                    step_input.prev_output_handle.step_key,
+                    step_input.prev_output_handle.output_name,
+                )
+                if step_input_key in step_output_keys:
+                    continue
+
+                inputs_to_marshal[step.key][step_input.name] = _key_for_marshalled_result(
+                    *step_input_key, prepend_run_id=False
+                ).format(tmp='/tmp/results', sep='/')
+
+        # this is a dict of step_key -> [MarshalledOutput]
+        outputs_to_marshal = defaultdict(list)
+
+        for step in solid_steps:
+            for step_output in step.step_outputs:
+                outputs_to_marshal[step.key].append(
+                    MarshalledOutput(
+                        step_output.name, _key_for_marshalled_result(
+                            step.key, step_output.name, prepend_run_id=False
+                        ).format(tmp='/tmp/results', sep='/')
+                    )
+                )
+
+        step_keys = [step.key for step in solid_steps]
+
+        # this is [ExecutionStepEvent(event_type=ExecutionStepEventType.STEP_OUTPUT)]
+        result = execute_marshalling(
+            pipeline,
+            step_keys,
+            inputs_to_marshal=inputs_to_marshal,
+            outputs_to_marshal=outputs_to_marshal,
+            environment_dict=config_object,
+        )
+
+        for execution_step_event in result:
+            assert execution_step_event.step_failure_data is None
+            key = _key_for_marshalled_result(
+                execution_step_event.step_key,
+                execution_step_event.step_output_data.output_name,
+                prepend_run_id=False
+            ).format(tmp='/tmp/results', sep='/')
 
 
 def test_uncontainerized_warehouse_dag_execution_with_airflow_config():
