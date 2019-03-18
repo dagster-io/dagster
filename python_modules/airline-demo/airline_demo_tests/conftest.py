3377b0035606d617712af0736cce6289a4382ca0
Max Gasner <max.gasner@gmail.com>
2019-03-18 10:00:55 -0700

    Black

--- python_modules/airline-demo/airline_demo_tests/conftest.py
+++ python_modules/airline-demo/airline_demo_tests/conftest.py
@@ -188,7 +188,7 @@ def scaffold_dag(request, airflow_test):
         image=docker_image,
         output_path=tempdir,
         dag_kwargs={'default_args': {'start_date': datetime.datetime(1900, 1, 1)}},
-        operator_kwargs={'network_mode': 'container:db'} if CIRCLECI else {}
+        operator_kwargs={'network_mode': 'container:db'} if CIRCLECI else {},
     )
 
     # Ensure that the scaffolded files parse correctly
