0e956a824b221f2f939ccfafc926541597a47458
Max Gasner <max.gasner@gmail.com>
2019-03-18 16:11:53 -0700

    Add test for warehouse pipeline

--- python_modules/airline-demo/airline_demo_tests/test_airflow.py
+++ python_modules/airline-demo/airline_demo_tests/test_airflow.py
@@ -217,3 +217,40 @@ class TestInMemoryAirflow_1IngestExecution:
 
 
 ####################################################################################################
+
+
+@airflow
+class TestInMemoryAirflow_2WarehouseExecution:
+    pipeline = define_airline_demo_ingest_pipeline()
+    config = [
+        script_relative_path(os.path.join('..', 'environments', 'airflow_base.yml')),
+        script_relative_path(os.path.join('..', 'environments', 'local_warehouse.yml')),
+    ]
+
+    def test_airflow_run_warehouse_pipeline(self, scaffold_dag):
+        _n, _p, _d, static_path, editable_path = scaffold_dag
+
+        execution_date = datetime.datetime.utcnow()
+
+        import_module_from_path('airline_demo_warehouse_pipeline_static__scaffold', static_path)
+        demo_pipeline = import_module_from_path('demo_pipeline', editable_path)
+
+        _dag, tasks = demo_pipeline.make_dag(
+            dag_id=demo_pipeline.DAG_ID,
+            dag_description=demo_pipeline.DAG_DESCRIPTION,
+            dag_kwargs=dict(default_args=demo_pipeline.DEFAULT_ARGS, **demo_pipeline.DAG_KWARGS),
+            s3_conn_id=demo_pipeline.S3_CONN_ID,
+            operator_kwargs={
+                'persist_intermediate_results_to_s3': True,
+                's3_bucket_name': 'dagster-lambda-execution',
+                'network_mode': 'container:db',
+            },
+            host_tmp_dir=demo_pipeline.HOST_TMP_DIR,
+        )
+
+        # These are in topo order already
+        for task in tasks:
+            ti = TaskInstance(task=task, execution_date=execution_date)
+            context = ti.get_template_context()
+            task._log = logging  # pylint: disable=protected-access
+            task.execute(context)
