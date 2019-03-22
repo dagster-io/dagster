a179b3a20621413c1bbacfb744b6aadf9e697db6
Alex Langenfeld <alangenfeld@fb.com>
2019-03-22 07:35:48 -0700

    break down test_graphql (#981)
    Breaks up parts of test_graphql in to a setup/util file and then a collection of smaller tests grouped around the queries they use. Its not a perfect organization but its the best i came up with.

---
+++ python_modules/dagit/dagit_tests/test_subset.py
@@ -0,0 +1,117 @@
+from .setup import define_context, execute_dagster_graphql
+
+PIPELINE_OR_ERROR_SUBSET_QUERY = '''
+query PipelineQuery($name: String! $solidSubset: [String!])
+{
+    pipelineOrError(params: { name: $name, solidSubset: $solidSubset }) {
+        __typename
+        ... on Pipeline {
+            name
+            solids {
+                name
+            }
+            configTypes {
+                __typename
+                name
+                ... on CompositeConfigType {
+                    fields {
+                    name
+                    configType {
+                        name
+                        __typename
+                    }
+                    __typename
+                    }
+                    __typename
+                }
+            }
+        }
+        ... on SolidNotFoundError {
+            solidName
+        }
+    }
+}
+'''
+PIPELINE_SUBSET_QUERY = '''
+query PipelineQuery($name: String! $solidSubset: [String!])
+{
+    pipeline(params: { name: $name, solidSubset: $solidSubset }) {
+        name
+        solids {
+            name
+        }
+        configTypes {
+          __typename
+          name
+          ... on CompositeConfigType {
+            fields {
+              name
+              configType {
+                name
+                __typename
+              }
+              __typename
+            }
+            __typename
+          }
+        }
+    }
+}
+'''
+
+
+def field_names_of(type_dict, typename):
+    return {field_data['name'] for field_data in type_dict[typename]['fields']}
+
+
+def types_dict_of_result(subset_result, top_key):
+    return {
+        type_data['name']: type_data for type_data in subset_result.data[top_key]['configTypes']
+    }
+
+
+def do_test_subset(query, top_key):
+    subset_result = execute_dagster_graphql(
+        define_context(), query, {'name': 'pandas_hello_world', 'solidSubset': ['sum_sq_solid']}
+    )
+
+    assert not subset_result.errors
+    assert subset_result.data
+
+    assert [solid_data['name'] for solid_data in subset_result.data[top_key]['solids']] == [
+        'sum_sq_solid'
+    ]
+
+    subset_types_dict = types_dict_of_result(subset_result, top_key)
+    assert field_names_of(subset_types_dict, 'PandasHelloWorld.SumSqSolid.Inputs') == {'sum_df'}
+    assert 'PandasHelloWorld.SumSolid.Inputs' not in subset_types_dict
+
+    full_types_dict = types_dict_of_result(
+        execute_dagster_graphql(define_context(), query, {'name': 'pandas_hello_world'}), top_key
+    )
+
+    assert 'PandasHelloWorld.SumSolid.Inputs' in full_types_dict
+    assert 'PandasHelloWorld.SumSqSolid.Inputs' not in full_types_dict
+
+    assert field_names_of(full_types_dict, 'PandasHelloWorld.SumSolid.Inputs') == {'num'}
+
+
+def test_pandas_hello_world_pipeline_subset():
+    do_test_subset(PIPELINE_SUBSET_QUERY, 'pipeline')
+
+
+def test_pandas_hello_world_pipeline_or_error_subset():
+    do_test_subset(PIPELINE_OR_ERROR_SUBSET_QUERY, 'pipelineOrError')
+
+
+def test_pandas_hello_world_pipeline_or_error_subset_wrong_solid_name():
+    result = execute_dagster_graphql(
+        define_context(),
+        PIPELINE_OR_ERROR_SUBSET_QUERY,
+        {'name': 'pandas_hello_world', 'solidSubset': ['nope']},
+    )
+
+    assert not result.errors
+    assert result.data
+    assert result.data['pipelineOrError']['__typename'] == 'SolidNotFoundError'
+    assert result.data['pipelineOrError']['solidName'] == 'nope'
