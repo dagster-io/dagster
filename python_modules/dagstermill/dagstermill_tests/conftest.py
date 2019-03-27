7593adf5eeae8fb08bb46d50a65174d137a855eb
Max Gasner <max.gasner@gmail.com>
2019-03-27 10:20:50 -0700

    Don't install kernel if it is already present

--- python_modules/dagstermill/dagstermill_tests/conftest.py
+++ python_modules/dagstermill/dagstermill_tests/conftest.py
@@ -1,8 +1,25 @@
+import re
 import subprocess
 
 import pytest
 
 
+# Dagstermill tests invoke notebooks that look for an ipython kernel called dagster -- if this is
+# not already present, then the tests fail. This fixture creates the kernel if it is not already
+# present before tests run.
 @pytest.fixture(scope='session', autouse=True)
 def kernel():
-    subprocess.check_output(['ipython', 'kernel', 'install', '--name', 'dagster', '--user'])
+    installed_kernels = [
+        x.split(' ')[0]
+        for x in [
+            re.sub('  +', ' ', x.strip(' '))
+            for x in filter(
+                lambda x: x.startswith('  '),
+                subprocess.check_output(['ipython', 'kernelspec', 'list'])
+                .decode('utf-8')
+                .split('\n'),
+            )
+        ]
+    ]
+    if 'dagster' not in installed_kernels:
+        subprocess.check_output(['ipython', 'kernel', 'install', '--name', 'dagster', '--user'])
