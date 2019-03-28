19c3951463d951bfaffe1a4d1ad5f8929e601a67
Max Gasner <max.gasner@gmail.com>
2019-03-28 14:59:54 -0700

    Add note on database fixture

--- python_modules/airline-demo/airline_demo_tests/conftest.py
+++ python_modules/airline-demo/airline_demo_tests/conftest.py
@@ -10,6 +10,10 @@ from dagster.utils import pushd, script_relative_path
 CIRCLECI = os.getenv('CIRCLECI')
 
 
+# Spins up a database using docker-compose and tears it down after tests complete.
+# We disable this on CircleCI when running the py37 tests -- airflow is not compatible with
+# py37; as a consequence, on this build we use the Circle docker executor, rathr than the
+# machine executor, and spin the database up directly from circleci/postgres:9.6.2-alpine.
 @pytest.fixture(scope='session')
 def docker_compose_db():
     if sys.version_info.major == 3 and sys.version_info.minor == 7 and CIRCLECI:
