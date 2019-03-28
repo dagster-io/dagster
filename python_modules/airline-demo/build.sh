154f3a3f44c0cf317dd4a6b0928b36a2c91b63c2
Max Gasner <max.gasner@gmail.com>
2019-03-27 17:31:22 -0700

    Update build script

--- python_modules/airline-demo/build.sh
+++ python_modules/airline-demo/build.sh
@@ -22,16 +22,16 @@ docker build -t airline-demo-airflow . && \
 docker tag airline-demo-airflow dagster/airline-demo-airflow && \
 \
 # pip uninstall -y dagster-airflow && \
-pip install -e ../dagster-airflow && \
-\
-dagster-airflow scaffold airline_demo_download_pipeline --image airline-demo-airflow \
--e environments/local_base.yml -e environments/local_fast_download.yml --install && \
-\
-dagster-airflow scaffold airline_demo_ingest_pipeline --image airline-demo-airflow \
--e environments/local_base.yml -e environments/local_ingest.yml --install && \
-\
-dagster-airflow scaffold airline_demo_warehouse_pipeline --image airline-demo-airflow \
--e environments/local_base.yml -e environments/local_warehouse.yml --install
+# pip install -e ../dagster-airflow && \
+# \
+# dagster-airflow scaffold airline_demo_download_pipeline --image airline-demo-airflow \
+# -e environments/local_base.yml -e environments/local_fast_download.yml --install && \
+# \
+# dagster-airflow scaffold airline_demo_ingest_pipeline --image airline-demo-airflow \
+# -e environments/local_base.yml -e environments/local_ingest.yml --install && \
+# \
+# dagster-airflow scaffold airline_demo_warehouse_pipeline --image airline-demo-airflow \
+# -e environments/local_base.yml -e environments/local_warehouse.yml --install
 
 rm -rf dagster && \
 rm -rf dagit && \
