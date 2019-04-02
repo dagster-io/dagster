82e8475d2366e4726d0194aa9c31bbdb5f828a46
Max Gasner <max.gasner@gmail.com>
2019-04-02 11:15:13 -0700

    Add test helper

--- python_modules/dagster-airflow/dagster_airflow_tests/test_factory.py
+++ python_modules/dagster-airflow/dagster_airflow_tests/test_factory.py
@@ -5,56 +5,35 @@ import json
 import re
 import uuid
 
-from collections import namedtuple
+from dagster.utils import script_relative_path
 
-from airflow import DAG
-from airflow.models import TaskInstance
-from airflow.operators.python_operator import PythonOperator
-
-from dagster.utils import load_yaml_from_path, script_relative_path
-
-from dagster_airflow.factory import make_airflow_dag
+from dagster_airflow.test_helpers import dagster_airflow_python_operator_pipeline
 
 from .test_project.dagster_airflow_demo import define_demo_execution_pipeline
 
-PIPELINE = define_demo_execution_pipeline()
 
-ENV_CONFIG = load_yaml_from_path(script_relative_path('test_project/env.yml'))
-
-
-def test_make_dag():
-    dag, tasks = make_airflow_dag(PIPELINE, ENV_CONFIG)
-
-    assert isinstance(dag, DAG)
-    for task in tasks:
-        assert isinstance(task, PythonOperator)
-
-
-def test_execute_dag():
-    _dag, tasks = make_airflow_dag(PIPELINE, ENV_CONFIG)
+class TestExecuteDag(object):
+    pipeline = define_demo_execution_pipeline()
+    config_yaml = [script_relative_path('test_project/env.yml')]
     run_id = str(uuid.uuid4())
     execution_date = datetime.datetime.utcnow()
 
-    expected_results = {
-        'multiply_the_word': '\'barbar\'',
-        'count_letters': '{\'b\': 2, \'a\': 2, \'r\': 2}',
-    }
-    for task in tasks:
-        ti = TaskInstance(task=task, execution_date=execution_date)
-        context = ti.get_template_context()
-        context['dag_run'] = namedtuple('_', 'run_id')(run_id=run_id)
-
-        res = task.execute(context)
-        json_res = json.loads(res)
-        assert 'data' in json_res
-        assert 'executePlan' in json_res['data']
-        assert '__typename' in json_res['data']['executePlan']
-        assert json_res['data']['executePlan']['__typename'] == 'ExecutePlanSuccess'
-        result = list(
-            filter(
-                lambda x: x['outputName'] == 'result', json_res['data']['executePlan']['stepEvents']
-            )
-        )[0]
-        assert json.loads(
-            re.sub('^u\'', '\'', result['valueRepr']).replace('\'', '"')
-        ) == json.loads(expected_results[task.task_id].replace('\'', '"'))
+    def test_execute_dag(self, dagster_airflow_python_operator_pipeline):
+        expected_results = {
+            'multiply_the_word': '\'barbar\'',
+            'count_letters': '{\'b\': 2, \'a\': 2, \'r\': 2}',
+        }
+        for result in dagster_airflow_python_operator_pipeline:
+            assert 'data' in result
+            assert 'executePlan' in result['data']
+            assert '__typename' in result['data']['executePlan']
+            assert result['data']['executePlan']['__typename'] == 'ExecutePlanSuccess'
+            result = list(
+                filter(
+                    lambda x: x['outputName'] == 'result',
+                    result['data']['executePlan']['stepEvents'],
+                )
+            )[0]
+            assert json.loads(
+                re.sub('^u\'', '\'', result['valueRepr']).replace('\'', '"')
+            ) == json.loads(expected_results[result['step']['solid']['name']].replace('\'', '"'))
