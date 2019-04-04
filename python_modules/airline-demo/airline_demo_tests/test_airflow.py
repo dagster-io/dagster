b7b6b9ce2390c61bd9181b41814c6f048527430f
Max Gasner <max.gasner@gmail.com>
2019-04-03 17:11:40 -0700

    Mark airflow tests slow

--- python_modules/airline-demo/airline_demo_tests/test_airflow.py
+++ python_modules/airline-demo/airline_demo_tests/test_airflow.py
@@ -7,13 +7,14 @@ from airline_demo.pipelines import (
     define_airline_demo_warehouse_pipeline,
 )
 
-from .marks import airflow
+from .marks import airflow, slow
 
 
 ####################################################################################################
 # These tests are "in-memory" because although they use the Airflow APIs to execute dockerized
 # DAG nodes, they don't run through the full Airflow machinery (a separate scheduler and executor
 # process, or even the single-node out-of-process invocation of `airflow test`)
+@slow
 @airflow
 class TestInMemoryAirflow_0IngestExecution:
     pipeline = define_airline_demo_ingest_pipeline()
@@ -26,7 +27,7 @@ class TestInMemoryAirflow_0IngestExecution:
     def test_airflow_run_ingest_pipeline(self, in_memory_airflow_run, clean_results_dir):
         pass
 
-
+@slow
 @airflow
 class TestInMemoryAirflow_1WarehouseExecution:
     pipeline = define_airline_demo_warehouse_pipeline()
