b2dbd96aed3e806378535005b40364e6eb2d526a
Max Gasner <max.gasner@gmail.com>
2019-04-04 12:10:40 -0700

    Update Docker images

--- .circleci/images/build.sh
+++ .circleci/images/build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-docker build -f Dockerfile-2.7 -t dagster/cci-test:2.7 .
-docker build -f Dockerfile-3.5 -t dagster/cci-test:3.5 .
-docker build -f Dockerfile-3.6 -t dagster/cci-test:3.6 .
-docker build -f Dockerfile-3.7.2 -t dagster/cci-test:3.7.2 .
+docker build -f Dockerfile-2.7.16 -t dagster/cci-test:2.7.16 .
+docker build -f Dockerfile-3.5.7 -t dagster/cci-test:3.5.7 .
+docker build -f Dockerfile-3.6.8 -t dagster/cci-test:3.6.8 .
+docker build -f Dockerfile-3.7.3 -t dagster/cci-test:3.7.3 .
 
-docker push dagster/cci-test:2.7
-docker push dagster/cci-test:3.5
-docker push dagster/cci-test:3.6
-docker push dagster/cci-test:3.7.2
+docker push dagster/cci-test:2.7.16
+docker push dagster/cci-test:3.5.7
+docker push dagster/cci-test:3.6.8
+docker push dagster/cci-test:3.7.3
