3480d5be47e4dac4616b18564eab4a6e2e148016
Max Gasner <max.gasner@gmail.com>
2019-04-09 18:39:02 -0400

    Fix set comprehension

--- python_modules/dagster/dagster/core/object_store.py
+++ python_modules/dagster/dagster/core/object_store.py
@@ -86,7 +86,7 @@ class ObjectStore(six.with_metaclass(ABCMeta)):
 
     def _check_for_unsupported_composite_overrides(self, runtime_type):
         composite_overrides = {
-            t.name in self.TYPE_STORAGE_PLUGIN_REGISTRY
+            t.name
             for t in runtime_type.inner_types
             if t.name in self.TYPE_STORAGE_PLUGIN_REGISTRY
         }
