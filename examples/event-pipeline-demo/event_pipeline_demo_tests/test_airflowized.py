c846851fecf6878c9fbdb0c3e4988ba813a747cc
alangenfeld <alex@elementl.com>
2019-04-09 14:32:41 -0700

    move events pipeline to examples

--- examples/event-pipeline-demo/event_pipeline_demo_tests/test_airflowized.py
+++ examples/event-pipeline-demo/event_pipeline_demo_tests/test_airflowized.py
@@ -3,7 +3,9 @@ import pytest
 from dagster.utils import script_relative_path
 
 # pylint: disable=unused-import
-from dagster_airflow.test_fixtures import dagster_airflow_python_operator_pipeline
+from dagster_airflow.test_fixtures import (
+    dagster_airflow_python_operator_pipeline,
+)
 
 from event_pipeline_demo.pipelines import define_event_ingest_pipeline
 
@@ -14,5 +16,7 @@ class TestAirflowizedEventPipeline(object):
     pipeline = define_event_ingest_pipeline()
 
     # pylint: disable=redefined-outer-name
-    def test_airflowized_event_pipeline(self, dagster_airflow_python_operator_pipeline):
+    def test_airflowized_event_pipeline(
+        self, dagster_airflow_python_operator_pipeline
+    ):
         pass
