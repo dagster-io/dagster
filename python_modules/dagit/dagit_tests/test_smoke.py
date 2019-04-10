ab9ef81650abba07365c396ffb09efe1f6e5408d
Max Gasner <max.gasner@gmail.com>
2019-04-10 15:06:43 -0700

    Nits

--- python_modules/dagit/dagit_tests/test_smoke.py
+++ python_modules/dagit/dagit_tests/test_smoke.py
@@ -38,7 +38,7 @@ def test_smoke_app():
 
     result = client.get('/dagit/open?path=foo.bar')
     assert result.status_code == 400
-    assert 'does not exist.' in result.data.decode('utf-8')
+    assert 'does not exist' in result.data.decode('utf-8')
 
     result = client.get('/dagit/notebook?path=foo.bar')
     assert result.status_code == 400
--- python_modules/dagster/dagster_tests/utils_tests/test_safe_isfile.py
+++ python_modules/dagster/dagster_tests/utils_tests/test_safe_isfile.py
@@ -4,4 +4,3 @@ from dagster.utils import safe_isfile, script_relative_path
 def test_safe_isfile():
     assert safe_isfile(script_relative_path('test_safe_isfile.py'))
     assert not safe_isfile(script_relative_path('test_safe_isfile_foobar.py'))
-    assert not safe_isfile(b'\0')
