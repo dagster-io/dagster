024d52b3b9aea991b4a6df67b495b09ddfbef521
Nicholas Schrock <schrockn@elementl.com>
2019-04-12 14:50:28 -0700

    cp

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_execution_plan.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_execution_plan.py
@@ -18,14 +18,14 @@ snapshots['test_success_whole_execution_plan 1'] = {
             {
                 '__typename': 'ExecutionStepStartEvent',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 }
             },
             {
                 '__typename': 'ExecutionStepOutputEvent',
                 'outputName': 'input_thunk_output',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 },
                 'valueRepr': '''   num1  num2
 0     1     2
@@ -34,7 +34,7 @@ snapshots['test_success_whole_execution_plan 1'] = {
             {
                 '__typename': 'ExecutionStepSuccessEvent',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 }
             },
             {
@@ -96,14 +96,14 @@ snapshots['test_success_whole_execution_plan_with_filesystem_config 1'] = {
             {
                 '__typename': 'ExecutionStepStartEvent',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 }
             },
             {
                 '__typename': 'ExecutionStepOutputEvent',
                 'outputName': 'input_thunk_output',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 },
                 'valueRepr': '''   num1  num2
 0     1     2
@@ -112,7 +112,7 @@ snapshots['test_success_whole_execution_plan_with_filesystem_config 1'] = {
             {
                 '__typename': 'ExecutionStepSuccessEvent',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 }
             },
             {
@@ -174,14 +174,14 @@ snapshots['test_success_whole_execution_plan_with_in_memory_config 1'] = {
             {
                 '__typename': 'ExecutionStepStartEvent',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 }
             },
             {
                 '__typename': 'ExecutionStepOutputEvent',
                 'outputName': 'input_thunk_output',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 },
                 'valueRepr': '''   num1  num2
 0     1     2
@@ -190,7 +190,7 @@ snapshots['test_success_whole_execution_plan_with_in_memory_config 1'] = {
             {
                 '__typename': 'ExecutionStepSuccessEvent',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 }
             },
             {
@@ -241,62 +241,6 @@ snapshots['test_success_whole_execution_plan_with_in_memory_config 1'] = {
     }
 }
 
-snapshots['test_successful_one_part_execute_plan 1'] = {
-    'executePlan': {
-        '__typename': 'ExecutePlanSuccess',
-        'hasFailures': False,
-        'pipeline': {
-            'name': 'pandas_hello_world'
-        },
-        'stepEvents': [
-            {
-                '__typename': 'ExecutionStepStartEvent',
-                'step': {
-                    'key': 'sum_solid.num.input_thunk'
-                }
-            },
-            {
-                '__typename': 'ExecutionStepOutputEvent',
-                'outputName': 'input_thunk_output',
-                'step': {
-                    'key': 'sum_solid.num.input_thunk'
-                },
-                'valueRepr': '''   num1  num2
-0     1     2
-1     3     4'''
-            },
-            {
-                '__typename': 'ExecutionStepSuccessEvent',
-                'step': {
-                    'key': 'sum_solid.num.input_thunk'
-                }
-            },
-            {
-                '__typename': 'ExecutionStepStartEvent',
-                'step': {
-                    'key': 'sum_solid.transform'
-                }
-            },
-            {
-                '__typename': 'ExecutionStepOutputEvent',
-                'outputName': 'result',
-                'step': {
-                    'key': 'sum_solid.transform'
-                },
-                'valueRepr': '''   num1  num2  sum
-0     1     2    3
-1     3     4    7'''
-            },
-            {
-                '__typename': 'ExecutionStepSuccessEvent',
-                'step': {
-                    'key': 'sum_solid.transform'
-                }
-            }
-        ]
-    }
-}
-
 snapshots['test_successful_two_part_execute_plan 1'] = {
     'executePlan': {
         '__typename': 'ExecutePlanSuccess',
@@ -308,14 +252,14 @@ snapshots['test_successful_two_part_execute_plan 1'] = {
             {
                 '__typename': 'ExecutionStepStartEvent',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 }
             },
             {
                 '__typename': 'ExecutionStepOutputEvent',
                 'outputName': 'input_thunk_output',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 },
                 'valueRepr': '''   num1  num2
 0     1     2
@@ -324,7 +268,7 @@ snapshots['test_successful_two_part_execute_plan 1'] = {
             {
                 '__typename': 'ExecutionStepSuccessEvent',
                 'step': {
-                    'key': 'sum_solid.num.input_thunk'
+                    'key': 'sum_solid.num.read'
                 }
             },
             {
@@ -405,5 +349,61 @@ snapshots['test_pipeline_not_found_error_execute_plan 1'] = {
     'executePlan': {
         '__typename': 'PipelineNotFoundError',
         'pipelineName': 'nope'
+    }
+}
+
+snapshots['test_successful_one_part_execute_plan 1'] = {
+    'executePlan': {
+        '__typename': 'ExecutePlanSuccess',
+        'hasFailures': False,
+        'pipeline': {
+            'name': 'pandas_hello_world'
+        },
+        'stepEvents': [
+            {
+                '__typename': 'ExecutionStepStartEvent',
+                'step': {
+                    'key': 'sum_solid.num.read'
+                }
+            },
+            {
+                '__typename': 'ExecutionStepOutputEvent',
+                'outputName': 'input_thunk_output',
+                'step': {
+                    'key': 'sum_solid.num.read'
+                },
+                'valueRepr': '''   num1  num2
+0     1     2
+1     3     4'''
+            },
+            {
+                '__typename': 'ExecutionStepSuccessEvent',
+                'step': {
+                    'key': 'sum_solid.num.read'
+                }
+            },
+            {
+                '__typename': 'ExecutionStepStartEvent',
+                'step': {
+                    'key': 'sum_solid.transform'
+                }
+            },
+            {
+                '__typename': 'ExecutionStepOutputEvent',
+                'outputName': 'result',
+                'step': {
+                    'key': 'sum_solid.transform'
+                },
+                'valueRepr': '''   num1  num2  sum
+0     1     2    3
+1     3     4    7'''
+            },
+            {
+                '__typename': 'ExecutionStepSuccessEvent',
+                'step': {
+                    'key': 'sum_solid.transform'
+                }
+            }
+        ]
     }
 }
