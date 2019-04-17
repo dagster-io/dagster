03d97f9c91e3d263ba59c844d8f82ad3917b9f88
Max Gasner <max.gasner@gmail.com>
2019-04-17 09:57:41 -0700

    Fix test

--- python_modules/dagit/dagit_tests/test_cli.py
+++ python_modules/dagit/dagit_tests/test_cli.py
@@ -6,4 +6,4 @@ from dagit.cli import ui
 def test_invoke_ui():
     runner = CliRunner()
     result = runner.invoke(ui, ['--version'])
-    assert result.output == 'dagit, version 0.3.5\n'
+    assert 'dagit, version' in result.output
