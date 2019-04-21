973485573fd9b31a04e93099b5284717beb189be
Nate Kupp <nate@elementl.com>
2019-04-20 18:00:16 -0700

    typos

--- python_modules/libraries/dagster-slack/setup.py
+++ python_modules/libraries/dagster-slack/setup.py
@@ -12,12 +12,12 @@ else:
 
 def get_version(name):
     version = {}
-    with open("dagster_gcp/version.py") as fp:
+    with open("dagster_slack/version.py") as fp:
         exec(fp.read(), version)  # pylint: disable=W0122
 
-    if name == 'dagster-gcp':
+    if name == 'dagster-slack':
         return version['__version__']
-    elif name == 'dagster-gcp-nightly':
+    elif name == 'dagster-slack-nightly':
         return version['__nightly__']
     else:
         raise Exception('Shouldn\'t be here: bad package name {name}'.format(name=name))
@@ -27,14 +27,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--nightly', action='store_true')
 
 
-def _do_setup(name='dagster-gcp'):
+def _do_setup(name='dagster-slack'):
     setup(
-        name='dagster_gcp',
+        name='dagster_slack',
         version=get_version(name),
         author='Elementl',
         license='Apache-2.0',
-        description='Package for GCP-specific Dagster framework solid and resource components.',
-        url='https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-gcp',
+        description='A Slack client resource for posting to Slack',
+        url='https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-slack',
         classifiers=[
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.5',
@@ -52,6 +52,6 @@ if __name__ == '__main__':
     parsed, unparsed = parser.parse_known_args()
     sys.argv = [sys.argv[0]] + unparsed
     if parsed.nightly:
-        _do_setup('dagster-gcp-nightly')
+        _do_setup('dagster-slack-nightly')
     else:
-        _do_setup('dagster-gcp')
+        _do_setup('dagster-slack')
