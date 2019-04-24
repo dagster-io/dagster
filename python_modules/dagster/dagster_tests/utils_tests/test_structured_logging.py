8d5b488c67174da7cffa9355bc724d68467a3c03
Max Gasner <max.gasner@gmail.com>
2019-04-24 15:13:32 -0500

    Test using context

--- python_modules/dagster/dagster_tests/utils_tests/test_structured_logging.py
+++ python_modules/dagster/dagster_tests/utils_tests/test_structured_logging.py
@@ -37,3 +37,23 @@ def test_construct_event_record():
     assert len(messages) == 1
     message = messages[0]
     assert isinstance(message, LogMessageRecord)
+
+
+def test_structured_logger_in_context_with_bad_log_level():
+    messages = []
+
+    def _append_message(logger_message):
+        messages.append(logger_message)
+
+    logger = define_structured_logger('some_name', _append_message, level=logging.DEBUG)
+    context = create_test_pipeline_execution_context(loggers=[logger])
+    context.log.gargle('from_context', foo=2)
+    assert len(messages) == 1
+    message = messages[0]
+    assert message.name == 'some_name'
+    assert message.level == logging.ERROR
+    assert message.meta['foo'] == 2
+    assert message.meta['orig_message'] == (
+        'Unexpected log level: User code attempted to log at level \'GARGLE\', but no logger was '
+        'configured to handle that level. Original message: \'from_context\''
+    )
