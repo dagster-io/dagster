205ab372644242c00683dcb491895c3f0670364b
alangenfeld <alex@elementl.com>
2019-04-25 14:54:51 -0700

    run black on examples as well
    Summary: Not sure if this was intentional, but we should autoformat examples.
    
    Test Plan: CI passes
    
    Reviewers: schrockn, max, natekupp
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D12

--- examples/event-pipeline-demo/event_pipeline_demo_tests/test_airflowized.py
+++ examples/event-pipeline-demo/event_pipeline_demo_tests/test_airflowized.py
@@ -3,9 +3,7 @@ import pytest
 from dagster.utils import script_relative_path
 
 # pylint: disable=unused-import
-from dagster_airflow.test_fixtures import (
-    dagster_airflow_python_operator_pipeline,
-)
+from dagster_airflow.test_fixtures import dagster_airflow_python_operator_pipeline
 
 from event_pipeline_demo.pipelines import define_event_ingest_pipeline
 
@@ -16,7 +14,5 @@ class TestAirflowizedEventPipeline(object):
     pipeline = define_event_ingest_pipeline()
 
     # pylint: disable=redefined-outer-name
-    def test_airflowized_event_pipeline(
-        self, dagster_airflow_python_operator_pipeline
-    ):
+    def test_airflowized_event_pipeline(self, dagster_airflow_python_operator_pipeline):
         pass
