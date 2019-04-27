b3636b01cfb1447370682ae95d29ce68fb92391d
Max Gasner <max.gasner@gmail.com>
2019-04-27 14:47:15 -0500

    Fix docstring

--- python_modules/dagster/dagster_tests/core_tests/test_logging.py
+++ python_modules/dagster/dagster_tests/core_tests/test_logging.py
@@ -21,7 +21,6 @@ DAGSTER_DEFAULT_LOGGER = 'dagster'
 
 @contextmanager
 def _setup_logger(name, log_levels=None, register_levels=True):
-    log_levels = check.opt_dict_param(log_levels, 'log_levels')
     '''Test helper that creates a new logger.
 
     Args:
@@ -32,6 +31,7 @@ def _setup_logger(name, log_levels=None, register_levels=True):
             module. Default True.
 
     '''
+    log_levels = check.opt_dict_param(log_levels, 'log_levels')
 
     def add_log_level(value, name):
         logging.addLevelName(value, name)
