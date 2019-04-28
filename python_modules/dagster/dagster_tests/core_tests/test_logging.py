92cd3358f526993746a333c3618d798d95c00a22
Max Gasner <max.gasner@gmail.com>
2019-04-27 22:19:12 -0500

    Lint

--- python_modules/dagster/dagster_tests/core_tests/test_logging.py
+++ python_modules/dagster/dagster_tests/core_tests/test_logging.py
@@ -147,7 +147,7 @@ def test_logging_integer_log_levels():
 
 
 def test_logging_bad_custom_log_levels():
-    with _setup_logger('test') as (captured_results, logger):
+    with _setup_logger('test') as (_, logger):
 
         dl = DagsterLogManager('123', {}, [logger])
         with pytest.raises(check.CheckError):
@@ -155,7 +155,7 @@ def test_logging_bad_custom_log_levels():
 
 
 def test_logging_unregistered_custom_log_levels():
-    with _setup_logger('test', {'FOO': 3}, register_levels=False) as (captured_results, logger):
+    with _setup_logger('test', {'FOO': 3}, register_levels=False) as (_, logger):
 
         dl = DagsterLogManager('123', {}, [logger])
         with pytest.raises(check.CheckError):
