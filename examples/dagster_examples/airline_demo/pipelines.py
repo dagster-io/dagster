693a4023cc9341cabd9f98afef50a251db749d79
Nicholas Schrock <schrockn@elementl.com>
2019-06-03 19:03:47 -0700

    (airline-composites-3) Create composite for all on time data processing
    Summary: Continuing the organization and composition of the pipeline.
    
    Test Plan: Run in dagit. buildkite
    
    Reviewers: max
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D294

--- examples/dagster_examples/airline_demo/pipelines.py
+++ examples/dagster_examples/airline_demo/pipelines.py
@@ -1,6 +1,7 @@
 """Pipeline definitions for the airline_demo."""
 
 from dagster import (
+    CompositeSolidDefinition,
     DependencyDefinition,
     ModeDefinition,
     PipelineDefinition,
@@ -69,40 +70,46 @@ prod_mode = ModeDefinition(
     },
 )
 
-
-def define_airline_demo_ingest_pipeline():
-    solids = [
-        download_from_s3_to_bytes,
-        ingest_csv_to_spark,
-        load_data_to_database_from_spark,
-        process_q2_data,
-        process_sfo_weather_data,
-        s3_to_dw_table,
-        s3_to_df,
-    ]
-    dependencies = {
-        SolidInstance('s3_to_dw_table', alias='process_q2_coupon_data'): {},
-        SolidInstance('s3_to_dw_table', alias='process_q2_market_data'): {},
-        SolidInstance('s3_to_dw_table', alias='process_q2_ticket_data'): {},
+process_on_time_data = CompositeSolidDefinition(
+    name='process_on_time_data',
+    solids=[s3_to_df, process_q2_data, load_data_to_database_from_spark],
+    dependencies={
         SolidInstance('s3_to_df', alias='april_on_time_s3_to_df'): {},
         SolidInstance('s3_to_df', alias='may_on_time_s3_to_df'): {},
         SolidInstance('s3_to_df', alias='june_on_time_s3_to_df'): {},
         SolidInstance('s3_to_df', alias='master_cord_s3_to_df'): {},
-        SolidInstance('download_from_s3_to_bytes', alias='download_q2_sfo_weather'): {},
-        SolidInstance('ingest_csv_to_spark', alias='ingest_q2_sfo_weather'): {
-            'input_csv_file': DependencyDefinition('download_q2_sfo_weather')
-        },
         'process_q2_data': {
             'april_data': DependencyDefinition('april_on_time_s3_to_df'),
             'may_data': DependencyDefinition('may_on_time_s3_to_df'),
             'june_data': DependencyDefinition('june_on_time_s3_to_df'),
             'master_cord_data': DependencyDefinition('master_cord_s3_to_df'),
         },
-        'process_sfo_weather_data': {
-            'sfo_weather_data': DependencyDefinition('ingest_q2_sfo_weather')
-        },
         SolidInstance('load_data_to_database_from_spark', alias='load_q2_on_time_data'): {
             'data_frame': DependencyDefinition('process_q2_data')
+        },
+    },
+)
+
+
+def define_airline_demo_ingest_pipeline():
+    solids = [
+        download_from_s3_to_bytes,
+        ingest_csv_to_spark,
+        load_data_to_database_from_spark,
+        process_on_time_data,
+        process_sfo_weather_data,
+        s3_to_dw_table,
+    ]
+    dependencies = {
+        SolidInstance('s3_to_dw_table', alias='process_q2_coupon_data'): {},
+        SolidInstance('s3_to_dw_table', alias='process_q2_market_data'): {},
+        SolidInstance('s3_to_dw_table', alias='process_q2_ticket_data'): {},
+        SolidInstance('download_from_s3_to_bytes', alias='download_q2_sfo_weather'): {},
+        SolidInstance('ingest_csv_to_spark', alias='ingest_q2_sfo_weather'): {
+            'input_csv_file': DependencyDefinition('download_q2_sfo_weather')
+        },
+        'process_sfo_weather_data': {
+            'sfo_weather_data': DependencyDefinition('ingest_q2_sfo_weather')
         },
         SolidInstance('load_data_to_database_from_spark', alias='load_q2_sfo_weather'): {
             'data_frame': DependencyDefinition('process_sfo_weather_data')
