d5998c9cb6a1deaf8da7ab612b038e88c903ae76
Nicholas Schrock <schrockn@elementl.com>
2019-06-05 11:38:08 -0700

    (airline-demo-refactor-3) Add unit test for load_data_to_database_from_spark
    Summary: Use mocked resources and inputs to test the load_data_to_database_from_spark
    
    Test Plan: Unit test
    
    Reviewers: max
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D304

---
+++ examples/dagster_examples_tests/airline_demo_tests/unit_tests/test_load_data_from_spark.py
@@ -0,0 +1,40 @@
+from dagster import PipelineDefinition, ModeDefinition, ResourceDefinition, execute_solid
+
+from pyspark.sql import DataFrame
+
+from dagster_examples.airline_demo.solids import load_data_to_database_from_spark
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
+
+def test_airline_demo_load_df():
+    pipeline_def = PipelineDefinition(
+        name='load_df_test',
+        solids=[load_data_to_database_from_spark],
+        mode_definitions=[
+            ModeDefinition(
+                resources={'db_info': ResourceDefinition.hardcoded_resource(mock.MagicMock())}
+            )
+        ],
+    )
+
+    solid_result = execute_solid(
+        pipeline_def,
+        'load_data_to_database_from_spark',
+        inputs={'data_frame': mock.MagicMock(spec=DataFrame)},
+        environment_dict={
+            'solids': {'load_data_to_database_from_spark': {'config': {'table_name': 'foo'}}}
+        },
+    )
+    assert solid_result.success
+    mats = solid_result.materializations_during_compute
+    assert len(mats) == 1
+    mat = mats[0]
+    # This is obviously a dumb thing to materialize
+    # Can improve when https://github.com/dagster-io/dagster/issues/1438 is complete
+    assert mat.event_specific_data.materialization.path == 'Persisted Db Table: foo'
+
+    assert solid_result.transformed_value('table_name') == 'foo'
