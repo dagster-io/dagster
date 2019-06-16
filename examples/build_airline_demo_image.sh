faa02a72a4d279f983766705dc4f1ec935cf4bf3
Nicholas Schrock <schrockn@elementl.com>
2019-06-16 10:16:03 -0700

    Minor build improvements
    Summary: Bump airline demo to v3 and optimize pip installs
    
    Test Plan: Buildkite. Monitor logs to make sure nothing is fishy.
    
    Reviewers: natekupp
    
    Reviewed By: natekupp
    
    Differential Revision: https://dagster.phacility.com/D415

--- examples/build_airline_demo_image.sh
+++ examples/build_airline_demo_image.sh
@@ -7,9 +7,12 @@ pushd $ROOT/examples
 
 pip install --upgrade pip
 
+# dagster-pandas required. See https://github.com/dagster-io/dagster/issues/1485
+
 function cleanup {
     rm -rf dagster
     rm -rf dagster-graphql
+    rm -rf dagster-pandas
     rm -rf dagstermill
     rm -rf dagster-aws
     rm -rf dagster-spark
@@ -20,6 +23,7 @@ trap cleanup EXIT
 
 cp -R ../python_modules/dagster .
 cp -R ../python_modules/dagster-graphql .
+cp -R ../python_modules/libraries/dagster-pandas .
 cp -R ../python_modules/dagstermill .
 cp -R ../python_modules/libraries/dagster-aws .
 cp -R ../python_modules/libraries/dagster-spark .
@@ -27,6 +31,7 @@ cp -R ../python_modules/libraries/dagster-pyspark .
 
 rm -rf dagster/.tox
 rm -rf dagster-graphql/.tox
+rm -rf dagster-pandas/.tox
 rm -rf dagstermill/.tox
 rm -rf dagster-aws/.tox
 rm -rf dagster-spark/.tox
@@ -34,6 +39,7 @@ rm -rf dagster-pyspark/.tox
 
 rm -rf dagster/dist
 rm -rf dagster-graphql/dist
+rm -rf dagster-pandas/dist
 rm -rf dagstermill/dist
 rm -rf dagster-aws/dist
 rm -rf dagster-spark/dist
