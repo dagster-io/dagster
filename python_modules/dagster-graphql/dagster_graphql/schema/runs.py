b2996c0d9935896d14e76f96d982f93ec18acddb
Sashank Thupukari <s@thupukari.com>
2019-07-24 14:50:38 -0700

    Remove debugging comment in runs.py
    Test Plan: unit
    
    Reviewers: #ft, max
    
    Reviewed By: #ft, max
    
    Differential Revision: https://dagster.phacility.com/D686

--- python_modules/dagster-graphql/dagster_graphql/schema/runs.py
+++ python_modules/dagster-graphql/dagster_graphql/schema/runs.py
@@ -520,7 +520,6 @@ def from_dagster_event_record(graphene_info, event_record, dauphin_pipeline, exe
             pipeline=dauphin_pipeline, **basic_params
         )
     elif dagster_event.event_type == DagsterEventType.PIPELINE_PROCESS_START:
-        # raise Exception(dir(dagster_event))
         process_data = dagster_event.pipeline_process_start_data
         return graphene_info.schema.type_named('PipelineProcessStartEvent')(
             pipeline=dauphin_pipeline,
