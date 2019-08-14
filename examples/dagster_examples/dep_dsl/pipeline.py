dfa0e8210378e4b0e47221b5a6511caf434455cf
Nicholas Schrock <schrockn@elementl.com>
2019-08-14 07:08:42 -0700

    Throwing up example yaml-driven deps
    Summary: People are asking for this so have an example
    
    Test Plan: Local testing using dagit
    
    Reviewers: #ft
    
    Reviewed By: #ft
    
    Subscribers: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D813

---
+++ examples/dagster_examples/dep_dsl/pipeline.py
@@ -0,0 +1,67 @@
+'''
+This demo example is to demonstrate that it is possible to construct
+the dependencies of a pipeline definition from a yaml file or similar.
+This is not the "blessed" file format nor is it our recommendation for
+doing things. However we've been asked a number of times if this is possible
+and this should serve as a reasonable template to build upon. 
+'''
+from dagster import (
+    SolidInvocation,
+    PipelineDefinition,
+    DependencyDefinition,
+    lambda_solid,
+    file_relative_path,
+    check,
+    InputDefinition,
+    OutputDefinition,
+)
+from dagster.utils import load_yaml_from_path
+
+
+@lambda_solid(input_defs=[InputDefinition('num', int)], output_def=OutputDefinition(int))
+def add_one(num: int) -> int:
+    return num + 1
+
+
+@lambda_solid(input_defs=[InputDefinition('num', int)], output_def=OutputDefinition(int))
+def add_two(num: int) -> int:
+    return num + 2
+
+
+@lambda_solid(
+    input_defs=[InputDefinition('left', int), InputDefinition('right', int)],
+    output_def=OutputDefinition(int),
+)
+def add(left: int, right: int) -> int:
+    return left + right
+
+
+def construct_pipeline_with_yaml(yaml_file, solid_defs):
+    yaml_data = load_yaml_from_path(yaml_file)
+    solid_def_dict = {s.name: s for s in solid_defs}
+
+    deps = {}
+
+    for solid_yaml_data in yaml_data['pipeline']['solids']:
+        check.invariant(solid_yaml_data['def'] in solid_def_dict)
+        def_name = solid_yaml_data['def']
+        alias = solid_yaml_data.get('alias', def_name)
+        solid_deps_entry = {}
+        for input_name, input_data in solid_yaml_data.get('deps', {}).items():
+            solid_deps_entry[input_name] = DependencyDefinition(
+                solid=input_data['solid'], output=input_data.get('output', 'result')
+            )
+        deps[SolidInvocation(name=def_name, alias=alias)] = solid_deps_entry
+
+    return PipelineDefinition(
+        name=yaml_data['pipeline']['name'],
+        description=yaml_data['pipeline'].get('description'),
+        solid_defs=solid_defs,
+        dependencies=deps,
+    )
+
+
+def define_dep_dsl_pipeline():
+    return construct_pipeline_with_yaml(
+        file_relative_path(__file__, 'example.yaml'), [add_one, add_two, add]
+    )
