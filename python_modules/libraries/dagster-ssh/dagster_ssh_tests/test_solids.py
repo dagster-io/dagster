a2e28cc8c793f508beb25dbbd902c45c4c0f5aa6
Nate Kupp <nate@elementl.com>
2019-08-20 14:38:17 -0700

    Fix SFTP solid test
    Summary: This fixes the SFTP solid test by using an SFTP pytest mock
    
    Test Plan: unit
    
    Reviewers: #ft, schrockn
    
    Reviewed By: #ft, schrockn
    
    Differential Revision: https://dagster.phacility.com/D849

--- python_modules/libraries/dagster-ssh/dagster_ssh_tests/test_solids.py
+++ python_modules/libraries/dagster-ssh/dagster_ssh_tests/test_solids.py
@@ -6,39 +6,39 @@ from dagster.seven import get_system_temp_directory
 from dagster_ssh import sftp_solid, ssh_resource
 
 
-def test_sftp_solid():
+def test_sftp_solid(sftpserver):
     tmp_path = get_system_temp_directory()
     readme_file = os.path.join(tmp_path, 'readme.txt')
 
-    result = execute_solid(
-        sftp_solid,
-        ModeDefinition(resource_defs={'ssh_resource': ssh_resource}),
-        environment_dict={
-            'solids': {
-                'sftp_solid': {
-                    'config': {
-                        'local_filepath': readme_file,
-                        'remote_filepath': 'readme.txt',
-                        'operation': 'GET',
+    with sftpserver.serve_content({'a_dir': {'readme.txt': 'hello, world'}}):
+        result = execute_solid(
+            sftp_solid,
+            ModeDefinition(resource_defs={'ssh_resource': ssh_resource}),
+            environment_dict={
+                'solids': {
+                    'sftp_solid': {
+                        'config': {
+                            'local_filepath': readme_file,
+                            'remote_filepath': 'a_dir/readme.txt',
+                            'operation': 'GET',
+                        }
                     }
-                }
-            },
-            'resources': {
-                'ssh_resource': {
-                    'config': {
-                        # Per https://www.sftp.net/public-online-sftp-servers
-                        'remote_host': 'test.rebex.net',
-                        'remote_port': 22,
-                        'username': 'demo',
-                        'password': 'password',
-                        'no_host_key_check': True,
+                },
+                'resources': {
+                    'ssh_resource': {
+                        'config': {
+                            'remote_host': sftpserver.host,
+                            'remote_port': sftpserver.port,
+                            'username': 'user',
+                            'password': 'pw',
+                            'no_host_key_check': True,
+                        }
                     }
-                }
+                },
             },
-        },
-    )
-    assert result.success
+        )
+        assert result.success
 
     with open(readme_file, 'rb') as f:
         contents = f.read()
-        assert b'you are connected to an FTP or SFTP server' in contents
+        assert b'hello, world' in contents
