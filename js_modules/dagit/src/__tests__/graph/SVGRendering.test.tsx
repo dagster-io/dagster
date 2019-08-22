be9541ed0363ae51ba661fe6dabd8cfaf501d94a
Ben Gotow <ben@foundry376.com>
2019-08-22 15:59:34 -0500

    Add a test for a composite, add package cache so the tests pass

--- js_modules/dagit/src/__tests__/graph/SVGRendering.test.tsx
+++ js_modules/dagit/src/__tests__/graph/SVGRendering.test.tsx
@@ -10,6 +10,7 @@ import { PipelineGraphContents } from "../../graph/PipelineGraph";
 import { getDagrePipelineLayout } from "../../graph/getFullSolidLayout";
 import { PipelineExplorerRootQuery_pipelineOrError_Pipeline } from "../../types/PipelineExplorerRootQuery";
 import { PIPELINE_EXPLORER_ROOT_QUERY } from "../../PipelineExplorerRoot";
+import { PipelineGraphSolidFragment } from "../../graph/types/PipelineGraphSolidFragment";
 
 const PipelineNames = [
   "airline_demo_ingest_pipeline",
@@ -37,56 +38,88 @@ fs.writeFileSync(
     ).join("\n\n")
 );
 
+function pipelineNamed(name: string) {
+  const result = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/${name}.json`).toString()
+  );
+  return result.data
+    .pipelineOrError as PipelineExplorerRootQuery_pipelineOrError_Pipeline;
+}
+
+function svgForPipeline(
+  name: string,
+  solids: PipelineGraphSolidFragment[],
+  parent?: PipelineGraphSolidFragment
+) {
+  // render the pipeline explorer's viewport contents to SVG and capture
+  // styled-component styles into a <div>
+  const layout = getDagrePipelineLayout(solids, parent);
+  const div = document.createElement("div");
+  const svgContent = ReactDOM.renderToStaticMarkup(
+    <StyleSheetManager target={div}>
+      <PipelineGraphContents
+        minified={false}
+        solids={solids}
+        parentSolid={parent}
+        pipelineName={name}
+        layout={layout}
+        backgroundColor={Colors.LIGHT_GRAY5}
+        highlightedSolids={[]}
+      />
+    </StyleSheetManager>
+  );
+
+  return pretty(
+    `<svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewbox="0 0 ${layout.width} ${layout.height}"
+      width="${layout.width}"
+      height="${layout.height}"
+    >
+      ${div.innerHTML}
+      ${svgContent}
+    </svg>`
+  );
+}
+
 PipelineNames.forEach(name => {
   it(`${name}: renders the expected SVG`, () => {
     // load the GraphQL response and pull out the first layer of solids
-    const result = JSON.parse(
-      fs.readFileSync(`${__dirname}/data/${name}.json`).toString()
-    );
-    const pipeline = result.data
-      .pipelineOrError as PipelineExplorerRootQuery_pipelineOrError_Pipeline;
-    const solids = pipeline.solidHandles
-      .filter(h => !h.parent)
+    const solids = pipelineNamed(name)
+      .solidHandles.filter(h => !h.parent)
       .map(h => h.solid);
 
-    // render the pipeline explorer's viewport contents to SVG and capture
-    // styled-component styles into a <div>
-    const layout = getDagrePipelineLayout(solids);
-    const div = document.createElement("div");
-    const svgContent = ReactDOM.renderToStaticMarkup(
-      <StyleSheetManager target={div}>
-        <PipelineGraphContents
-          minified={false}
-          solids={solids}
-          pipelineName={pipeline.name}
-          layout={layout}
-          backgroundColor={Colors.LIGHT_GRAY5}
-          highlightedSolids={[]}
-        />
-      </StyleSheetManager>
-    );
+    const expectedPath = `${__dirname}/svg-snapshots/${name}.svg`;
+    const actualPath = `${__dirname}/svg-snapshots/${name}.actual.svg`;
+    const actual = svgForPipeline(name, solids);
 
-    const expectedPath = `${__dirname}/svg-snapshots/${pipeline.name}.svg`;
-    const actualPath = `${__dirname}/svg-snapshots/${pipeline.name}.actual.svg`;
-    const actual = pretty(
-      `<svg
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewbox="0 0 ${layout.width} ${layout.height}"
-          width="${layout.width}"
-          height="${layout.height}"
-        >
-          ${div.innerHTML}
-          ${svgContent}
-        </svg>`
-    );
-
-    // write out the actual result for easy visual comparison
+    // write out the actual result for easy visual comparison and compare to existing
     fs.writeFileSync(actualPath, actual);
-
-    // compare to expected data
     if (fs.existsSync(expectedPath)) {
       expect(fs.readFileSync(expectedPath).toString()).toEqual(actual);
     }
   });
+});
+
+it(`renders the expected SVG when viewing a composite`, () => {
+  // load the GraphQL response and pull out the first layer of solids
+  const pipeline = pipelineNamed("airline_demo_ingest_pipeline");
+  const parentId = "master_cord_s3_to_df";
+  const parent = pipeline.solidHandles.find(h => h.handleID === parentId)!
+    .solid;
+
+  const solids = pipeline.solidHandles
+    .filter(h => h.parent && h.parent.handleID === parentId)
+    .map(h => h.solid);
+
+  const expectedPath = `${__dirname}/svg-snapshots/airline-composite.svg`;
+  const actualPath = `${__dirname}/svg-snapshots/airline-composite.actual.svg`;
+  const actual = svgForPipeline(name, solids, parent);
+
+  // write out the actual result for easy visual comparison and compare to existing
+  fs.writeFileSync(actualPath, actual);
+  if (fs.existsSync(expectedPath)) {
+    expect(fs.readFileSync(expectedPath).toString()).toEqual(actual);
+  }
 });
