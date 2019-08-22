2c6feebac70f65881845b8129a5318bc05d78b42
Ben Gotow <ben@foundry376.com>
2019-08-21 23:54:02 -0500

    Correctly memoize new joined path rendering

--- js_modules/dagit/src/graph/SolidLinks.tsx
+++ js_modules/dagit/src/graph/SolidLinks.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import { Colors } from "@blueprintjs/core";
-import { LinkVertical as Link } from "@vx/shape";
+import { pathVerticalDiagonal } from "@vx/shape";
 import { ILayoutConnection, IFullPipelineLayout } from "./getFullSolidLayout";
 import styled from "styled-components";
 
 export type Edge = { a: string; b: string };
+
+const buildSVGPath = pathVerticalDiagonal({
+  source: (s: any) => s.source,
+  target: (s: any) => s.target,
+  x: (s: any) => s.x,
+  y: (s: any) => s.y
+});
 
 export const SolidLinks = React.memo(
   (props: {
@@ -14,34 +21,21 @@ export const SolidLinks = React.memo(
     onHighlight: (arr: Edge[]) => void;
   }) => {
     const solids = props.layout.solids;
-
-    return (
-      <g style={{ opacity: props.opacity }}>
-        {props.connections.map(({ from, to }, i) => (
-          <g
-            key={i}
-            onMouseLeave={() => props.onHighlight([])}
-            onMouseEnter={() =>
-              props.onHighlight([{ a: from.solidName, b: to.solidName }])
-            }
-          >
-            <StyledLink
-              data={{
-                // can also use from.point for the "Dagre" closest point on node
-                source: solids[from.solidName].outputs[from.edgeName].port,
-                target: solids[to.solidName].inputs[to.edgeName].port
-              }}
-            />
-          </g>
-        ))}
-      </g>
+    const paths = props.connections.map(({ from, to }, i) =>
+      buildSVGPath({
+        // can also use from.point for the "Dagre" closest point on node
+        source: solids[from.solidName].outputs[from.edgeName].port,
+        target: solids[to.solidName].inputs[to.edgeName].port
+      })
     );
+
+    return <StyledPath d={paths.join("")} style={{ opacity: props.opacity }} />;
   }
 );
 
 SolidLinks.displayName = "SolidLinks";
 
-const StyledLink = styled(Link)`
+const StyledPath = styled("path")`
   stroke-width: 6;
   stroke: ${Colors.BLACK}
   fill: none;
