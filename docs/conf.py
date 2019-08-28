484b1c12cca2d5198a95adbb576d9b22f8baf744
Nate Kupp <nate@elementl.com>
2019-08-28 15:11:15 -0700

    Change docs author
    Summary: This shows up as a top result in Google Search for Dagster https://buildmedia.readthedocs.org/media/pdf/dagster/master/dagster.pdf
    
    Test Plan: docs only
    
    Reviewers: alangenfeld
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D903

--- docs/conf.py
+++ docs/conf.py
@@ -139,9 +139,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'Dagster.tex', 'Dagster Documentation', 'Nicholas Schrock', 'manual')
-]
+latex_documents = [(master_doc, 'Dagster.tex', 'Dagster Documentation', 'Elementl', 'manual')]
 
 # -- Options for manual page output ------------------------------------------
 
