c261e35648e1f0400f5f33c476b62101549e45ed
Ben Gotow <ben@foundry376.com>
2019-08-28 14:34:22 -0700

    Move the hack out of the main pipeline execution page

--- js_modules/dagit/src/execute/PipelineExecutionRoot.tsx
+++ js_modules/dagit/src/execute/PipelineExecutionRoot.tsx
@@ -1,17 +1,12 @@
 import * as React from "react";
 import gql from "graphql-tag";
-import { match, Redirect } from "react-router";
+import { match } from "react-router";
 import PipelineExecutionContainer from "./PipelineExecutionContainer";
 import { QueryResult, Query } from "react-apollo";
 import { NonIdealState } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
-import {
-  useStorage,
-  applyCreateSession,
-  IExecutionSession
-} from "../LocalStorage";
+import { useStorage } from "../LocalStorage";
 import { PipelineExecutionRootQuery } from "./types/PipelineExecutionRootQuery";
-import * as querystring from "query-string";
 
 interface IPipelineExecutionRootProps {
   match: match<{ pipelineName: string }>;
@@ -26,27 +21,6 @@ export const PipelineExecutionRoot: React.FunctionComponent<
     solidSubset: data.sessions[data.current].solidSubset,
     mode: data.sessions[data.current].mode
   };
-
-  // If the user has passed config via a query string, write a new session with the
-  // incoming config and redirect to remove it from the URL bar.
-  const qs = querystring.parse(window.location.search);
-  if (qs.config || qs.mode || qs.solidSubset) {
-    const newSession: Partial<IExecutionSession> = {};
-    if (typeof qs.config === "string") {
-      newSession.environmentConfigYaml = qs.config;
-    }
-    if (typeof qs.mode === "string") {
-      newSession.mode = qs.mode;
-    }
-    if (qs.solidSubset instanceof Array) {
-      newSession.solidSubset = qs.solidSubset;
-    } else if (typeof qs.solidSubset === "string") {
-      newSession.solidSubset = [qs.solidSubset];
-    }
-
-    onSave(applyCreateSession(data, newSession));
-    return <Redirect to={`/${params.pipelineName}/execute`} />;
-  }
 
   return (
     <Query
