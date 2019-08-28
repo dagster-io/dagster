46e19a6f4564d5340b39286d54337b3a2524f3c3
Ben Gotow <ben@foundry376.com>
2019-08-28 14:18:48 -0700

    Switch <StorageProvider> to a hook to clean up the redirect

--- js_modules/dagit/src/execute/PipelineExecutionRoot.tsx
+++ js_modules/dagit/src/execute/PipelineExecutionRoot.tsx
@@ -6,7 +6,7 @@ import { QueryResult, Query } from "react-apollo";
 import { NonIdealState } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import {
-  StorageProvider,
+  useStorage,
   applyCreateSession,
   IExecutionSession
 } from "../LocalStorage";
@@ -19,95 +19,89 @@ interface IPipelineExecutionRootProps {
 
 export const PipelineExecutionRoot: React.FunctionComponent<
   IPipelineExecutionRootProps
-> = ({ match: { params } }) => (
-  <StorageProvider namespace={params.pipelineName} key={params.pipelineName}>
-    {({ data, onSave }) => {
-      const vars = {
-        name: params.pipelineName,
-        solidSubset: data.sessions[data.current].solidSubset,
-        mode: data.sessions[data.current].mode
-      };
+> = ({ match: { params } }) => {
+  const [data, onSave] = useStorage({ namespace: params.pipelineName });
+  const vars = {
+    name: params.pipelineName,
+    solidSubset: data.sessions[data.current].solidSubset,
+    mode: data.sessions[data.current].mode
+  };
 
-      // If the user has passed config via a query string, write a new session with the
-      // incoming config and redirect to remove it from the URL bar.  Note: This is not
-      // technically where a side effect like this should live, but placing it here
-      // (before the query but after we have our StorageProvider) prevents the UI from
-      // loading twice.
-      const qs = querystring.parse(window.location.search);
-      if (qs.config || qs.mode || qs.solidSubset) {
-        const newSession: Partial<IExecutionSession> = {};
-        if (typeof qs.config === "string") {
-          newSession.environmentConfigYaml = qs.config;
+  // If the user has passed config via a query string, write a new session with the
+  // incoming config and redirect to remove it from the URL bar.
+  const qs = querystring.parse(window.location.search);
+  if (qs.config || qs.mode || qs.solidSubset) {
+    const newSession: Partial<IExecutionSession> = {};
+    if (typeof qs.config === "string") {
+      newSession.environmentConfigYaml = qs.config;
+    }
+    if (typeof qs.mode === "string") {
+      newSession.mode = qs.mode;
+    }
+    if (qs.solidSubset instanceof Array) {
+      newSession.solidSubset = qs.solidSubset;
+    } else if (typeof qs.solidSubset === "string") {
+      newSession.solidSubset = [qs.solidSubset];
+    }
+
+    onSave(applyCreateSession(data, newSession));
+    return <Redirect to={`/${params.pipelineName}/execute`} />;
+  }
+
+  return (
+    <Query
+      // never serve cached Pipeline given new vars by forcing teardown of the Query.
+      // Apollo's behaviors are sort of whacky, even with no-cache. Should just use
+      // window.fetch...
+      key={JSON.stringify(vars)}
+      query={PIPELINE_EXECUTION_ROOT_QUERY}
+      fetchPolicy="cache-and-network"
+      partialRefetch={true}
+      variables={vars}
+    >
+      {(result: QueryResult<PipelineExecutionRootQuery, any>) => {
+        const pipelineOrError = result.data && result.data.pipelineOrError;
+
+        if (
+          !pipelineOrError ||
+          pipelineOrError.__typename === "PipelineNotFoundError"
+        ) {
+          return (
+            <NonIdealState
+              icon={IconNames.FLOW_BRANCH}
+              title="Pipeline Not Found"
+              description={
+                pipelineOrError
+                  ? pipelineOrError.message
+                  : "No data returned from GraphQL"
+              }
+            />
+          );
         }
-        if (typeof qs.mode === "string") {
-          newSession.mode = qs.mode;
+
+        if (pipelineOrError.__typename === "PythonError") {
+          return (
+            <NonIdealState
+              icon={IconNames.ERROR}
+              title="Python Error"
+              description={pipelineOrError.message}
+            />
+          );
         }
-        if (qs.solidSubset instanceof Array) {
-          newSession.solidSubset = qs.solidSubset;
-        } else if (typeof qs.solidSubset === "string") {
-          newSession.solidSubset = [qs.solidSubset];
-        }
 
-        onSave(applyCreateSession(data, newSession));
-        return <Redirect to={`/${params.pipelineName}/execute`} />;
-      }
-
-      return (
-        <Query
-          // never serve cached Pipeline given new vars by forcing teardown of the Query.
-          // Apollo's behaviors are sort of whacky, even with no-cache. Should just use
-          // window.fetch...
-          key={JSON.stringify(vars)}
-          query={PIPELINE_EXECUTION_ROOT_QUERY}
-          fetchPolicy="cache-and-network"
-          partialRefetch={true}
-          variables={vars}
-        >
-          {(result: QueryResult<PipelineExecutionRootQuery, any>) => {
-            const pipelineOrError = result.data && result.data.pipelineOrError;
-
-            if (
-              !pipelineOrError ||
-              pipelineOrError.__typename === "PipelineNotFoundError"
-            ) {
-              return (
-                <NonIdealState
-                  icon={IconNames.FLOW_BRANCH}
-                  title="Pipeline Not Found"
-                  description={
-                    pipelineOrError
-                      ? pipelineOrError.message
-                      : "No data returned from GraphQL"
-                  }
-                />
-              );
-            }
-
-            if (pipelineOrError.__typename === "PythonError") {
-              return (
-                <NonIdealState
-                  icon={IconNames.ERROR}
-                  title="Python Error"
-                  description={pipelineOrError.message}
-                />
-              );
-            }
-
-            return (
-              <PipelineExecutionContainer
-                data={data}
-                onSave={onSave}
-                pipelineOrError={pipelineOrError}
-                pipelineName={params.pipelineName}
-                currentSession={data.sessions[data.current]}
-              />
-            );
-          }}
-        </Query>
-      );
-    }}
-  </StorageProvider>
-);
+        return (
+          <PipelineExecutionContainer
+            data={data}
+            onSave={onSave}
+            pipelineOrError={pipelineOrError}
+            pipelineName={params.pipelineName}
+            currentSession={data.sessions[data.current]}
+          />
+        );
+      }}
+    </Query>
+  );
+};
 
 export const PIPELINE_EXECUTION_ROOT_QUERY = gql`
   query PipelineExecutionRootQuery(
