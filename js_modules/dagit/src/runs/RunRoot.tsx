87e4e5983fb030bf53af4775e1fd3ca4f0ae2c3c
Ben Gotow <ben@foundry376.com>
2019-09-03 13:23:54 -0500

    Fix hook issues, stop serializing modal contents to JSON
    Summary: This diff fixes the new modals. Allowing the presenter to pass a React tree as the content of the alert makes the rendering fully flexible but didn't work because the internal implementation was serializing the alert to JSON.
    
    This diff also fixes several regressions caused by the previous patch that moved a few things to the Hook syntax and exposed places where we need better test coverage. (It's surprising to me that the previous patch passed all the tests, actually.) I will write some additional tests in another diff.
    
    Test Plan: Run tests
    
    Reviewers: #ft
    
    Subscribers:

--- js_modules/dagit/src/runs/RunRoot.tsx
+++ js_modules/dagit/src/runs/RunRoot.tsx
@@ -11,31 +11,29 @@ interface IRunRootProps {
   match: match<{ runId: string }>;
 }
 
-export default class RunRoot extends React.Component<IRunRootProps> {
-  render() {
-    const { runId } = this.props.match.params;
-    const client = useApolloClient();
-    const { data } = useQuery<RunRootQuery>(RUN_ROOT_QUERY, {
-      fetchPolicy: "cache-and-network",
-      partialRefetch: true,
-      variables: { runId }
-    });
+export const RunRoot: React.FunctionComponent<IRunRootProps> = props => {
+  const { runId } = props.match.params;
+  const client = useApolloClient();
+  const { data } = useQuery<RunRootQuery>(RUN_ROOT_QUERY, {
+    fetchPolicy: "cache-and-network",
+    partialRefetch: true,
+    variables: { runId }
+  });
 
-    return !data || !data.pipelineRunOrError ? (
-      <Run client={client} run={undefined} />
-    ) : data.pipelineRunOrError.__typename === "PipelineRun" ? (
-      <Run client={client} run={data.pipelineRunOrError} />
-    ) : (
-      <NonIdealState
-        icon={IconNames.SEND_TO_GRAPH}
-        title="No Run"
-        description={
-          "The run with this ID does not exist or has been cleaned up."
-        }
-      />
-    );
-  }
-}
+  return !data || !data.pipelineRunOrError ? (
+    <Run client={client} run={undefined} />
+  ) : data.pipelineRunOrError.__typename === "PipelineRun" ? (
+    <Run client={client} run={data.pipelineRunOrError} />
+  ) : (
+    <NonIdealState
+      icon={IconNames.SEND_TO_GRAPH}
+      title="No Run"
+      description={
+        "The run with this ID does not exist or has been cleaned up."
+      }
+    />
+  );
+};
 
 export const RUN_ROOT_QUERY = gql`
   query RunRootQuery($runId: ID!) {
