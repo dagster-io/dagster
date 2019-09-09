d7b21fea2acd48af286dd9c1a3de9979ab713cdc
Max Gasner <max.gasner@gmail.com>
2019-09-09 11:43:05 -0700

    rearrange imports

--- python_modules/dagster/dagster/seven/__init__.py
+++ python_modules/dagster/dagster/seven/__init__.py
@@ -4,8 +4,15 @@ import os
 import sys
 import time
 
+try:
+    # Python 2 tempfile doesn't have tempfile.TemporaryDirectory
+    import backports.tempfile as tempfile
+except ImportError:
+    import tempfile
+
+
 from .json import JSONDecodeError, dump, dumps
-from .temp_dir import get_system_temp_directory, tempfile
+from .temp_dir import get_system_temp_directory
 
 TemporaryDirectory = tempfile.TemporaryDirectory
 
