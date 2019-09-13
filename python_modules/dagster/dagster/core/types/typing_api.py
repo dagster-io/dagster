c40886bed6a3a090cf290d96fe65cab74065d8d6
Nicholas Schrock <schrockn@elementl.com>
2019-09-13 10:35:00 -0700

    (new-dict-api-6) Make functions callable in python 2 and add is_closed_python_dict_type
    Summary:
    This renaming/refactor makes it clear that it is in reality a check against
    vanilla types. The typehint variants only exist to gate py3
    
    Test Plan: BK
    
    Reviewers: #ft, alangenfeld
    
    Reviewed By: #ft, alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D1032

--- python_modules/dagster/dagster/core/types/typing_api.py
+++ python_modules/dagster/dagster/core/types/typing_api.py
@@ -6,23 +6,65 @@ import typing
 
 from dagster import check
 
+
+def _get_origin(ttype):
+    return getattr(ttype, '__origin__', None)
+
+
+def is_python_list_type(ttype):
+    '''Returns true for list, List, and List[T]'''
+    if ttype is list or ttype is typing.List:
+        return True
+    origin = _get_origin(ttype)
+    return origin == typing.List or origin == list
+
+
+def is_closed_python_optional_type(ttype):
+    # Optional[X] is Union[X, NoneType] which is what we match against here
+    origin = _get_origin(ttype)
+    return origin == typing.Union and len(ttype.__args__) == 2 and ttype.__args__[1] == type(None)
+
+
+def is_python_dict_type(ttype):
+    if ttype is dict or ttype is typing.Dict:
+        return True
+    if not ttype:
+        return False
+
+    origin = _get_origin(ttype)
+    # py37 origin is typing.Dict, pre-37 is dict
+    return origin == typing.Dict or origin == dict
+
+
+def is_closed_python_dict_type(ttype):
+    if not ttype:
+        return False
+    if ttype is typing.Dict:
+        return False
+    if not hasattr(ttype, '__args__'):
+        return False
+    if ttype.__args__ is None or len(ttype.__args__) != 2:
+        return False
+
+    key_type, value_type = ttype.__args__
+    origin = _get_origin(ttype)
+
+    # when it is a raw typing.Dict the arguments are instances of TypeVars
+    return (
+        # py37 origin is typing.Dict, pre-37 is dict
+        (origin == typing.Dict or origin is dict)
+        and not isinstance(key_type, typing.TypeVar)
+        and not isinstance(value_type, typing.TypeVar)
+    )
+
+
 if sys.version_info.major >= 3:
 
     def is_python_list_typehint(type_annotation):
-        '''Returns true for list, List, and List[T]'''
-        if type_annotation is list or type_annotation is typing.List:
-            return True
-        origin = getattr(type_annotation, '__origin__', None)
-        return origin == typing.List or origin == list
+        return is_python_list_type(type_annotation)
 
     def is_closed_python_optional_typehint(type_annotation):
-        # Optional[X] is Union[X, NoneType] which is what we match against here
-        origin = getattr(type_annotation, '__origin__', None)
-        return (
-            origin == typing.Union
-            and len(type_annotation.__args__) == 2
-            and type_annotation.__args__[1] == type(None)
-        )
+        return is_closed_python_optional_type(type_annotation)
 
     def get_optional_inner_type(type_annotation):
         check.invariant(
--- python_modules/dagster/dagster_tests/core_tests/types_tests/test_typing_api.py
+++ python_modules/dagster/dagster_tests/core_tests/types_tests/test_typing_api.py
@@ -1,10 +1,11 @@
 import sys
-from typing import List, Optional
+import typing
 
 import pytest
 
 from dagster.core.types.typing_api import (
     get_optional_inner_type,
+    is_closed_python_dict_type,
     is_closed_python_optional_typehint,
     is_python_list_typehint,
 )
@@ -12,10 +13,22 @@ from dagster.core.types.typing_api import (
 REASON_STRING = 'typing_api only valid for python 3'
 
 
+def test_closed_python_dict():
+    assert is_closed_python_dict_type(typing.Dict[str, int]) is True
+
+    assert is_closed_python_dict_type(dict) is False
+    assert is_closed_python_dict_type(typing.Dict) is False
+    assert is_closed_python_dict_type(None) is False
+    assert is_closed_python_dict_type(1) is False
+    assert is_closed_python_dict_type('foobar') is False
+    assert is_closed_python_dict_type(typing.Optional) is False
+    assert is_closed_python_dict_type(typing.List) is False
+
+
 @pytest.mark.skipif(sys.version_info.major < 3, reason=REASON_STRING)
 def test_is_typing_list_py_3():
-    assert is_python_list_typehint(List[int])
-    assert is_python_list_typehint(List)
+    assert is_python_list_typehint(typing.List[int])
+    assert is_python_list_typehint(typing.List)
     assert is_python_list_typehint(list)
     assert not is_python_list_typehint(None)
     assert not is_python_list_typehint(int)
@@ -24,8 +37,8 @@ def test_is_typing_list_py_3():
 
 @pytest.mark.skipif(sys.version_info.major < 3, reason=REASON_STRING)
 def test_is_typing_optional_py_3():
-    assert is_closed_python_optional_typehint(Optional[int])
-    assert not is_closed_python_optional_typehint(Optional)
+    assert is_closed_python_optional_typehint(typing.Optional[int])
+    assert not is_closed_python_optional_typehint(typing.Optional)
     assert not is_closed_python_optional_typehint(None)
     assert not is_closed_python_optional_typehint(int)
     assert not is_closed_python_optional_typehint(list)
@@ -34,7 +47,7 @@ def test_is_typing_optional_py_3():
 
 @pytest.mark.skipif(sys.version_info.major < 3, reason=REASON_STRING)
 def test_get_inner_optional_py_3():
-    assert get_optional_inner_type(Optional[int]) is int
+    assert get_optional_inner_type(typing.Optional[int]) is int
 
 
 PY_2_REASON_STRING = 'Only checking python 2 implementations'
@@ -43,10 +56,10 @@ PY_2_REASON_STRING = 'Only checking python 2 implementations'
 @pytest.mark.skipif(sys.version_info.major >= 3, reason=PY_2_REASON_STRING)
 def test_is_typing_list_py_2():
     with pytest.raises(NotImplementedError):
-        is_python_list_typehint(List[int])
+        is_python_list_typehint(typing.List[int])
 
 
 @pytest.mark.skipif(sys.version_info.major >= 3, reason=PY_2_REASON_STRING)
 def test_is_typing_optional_py_2():
     with pytest.raises(NotImplementedError):
-        is_closed_python_optional_typehint(Optional[int])
+        is_closed_python_optional_typehint(typing.Optional[int])
