af6c3fdaaf3e3c066690c319195294a7d91bdf24
Max Gasner <max.gasner@gmail.com>
2019-09-27 10:15:03 -0700

    Unset $DAGSTER_HOME for tests
    Summary: Avoid polluting $DAGSTER_HOME with CLI test artifacts
    
    Test Plan: Unit
    
    Reviewers: #ft, sashank, alangenfeld
    
    Reviewed By: #ft, alangenfeld
    
    Subscribers: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D1101

---
+++ python_modules/dagster/dagster_tests/conftest.py
@@ -0,0 +1,13 @@
+import os
+
+import pytest
+
+
+@pytest.fixture(scope='session', autouse=True)
+def unset_dagster_home():
+    old_env = os.getenv('DAGSTER_HOME')
+    if old_env is not None:
+        del os.environ['DAGSTER_HOME']
+    yield
+    if old_env is not None:
+        os.environ['DAGSTER_HOME'] = old_env
