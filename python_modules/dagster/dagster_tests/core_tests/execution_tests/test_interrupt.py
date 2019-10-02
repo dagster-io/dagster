a80eef7b476caa7e97aea20e0f079fd5865fdb16
Nicholas Schrock <schrockn@elementl.com>
2019-10-01 18:33:32 -0700

    Make test_interrupt not flaky
    Summary:
    Use a temp file to make the timing of this deterministic. The solid writes a temp file and loops.
    The thread waits for that temp file to exists and then throws the interrupt.
    
    Test Plan: BK
    
    Reviewers: natekupp, max, alangenfeld
    
    Reviewed By: natekupp
    
    Differential Revision: https://dagster.phacility.com/D1152

--- python_modules/dagster/dagster_tests/core_tests/execution_tests/test_interrupt.py
+++ python_modules/dagster/dagster_tests/core_tests/execution_tests/test_interrupt.py
@@ -1,7 +1,10 @@
+import os
 import time
+from contextlib import contextmanager
 from threading import Thread
 
 from dagster import DagsterEventType, execute_pipeline_iterator, lambda_solid, pipeline
+from dagster.utils.test import get_temp_file_name
 
 try:
     import _thread as thread
@@ -9,36 +12,51 @@ except ImportError:
     import thread
 
 
-@lambda_solid
-def sleep():
-    time.sleep(0.1)
+# Consolidate with implementation in dagster.utils.test once D1118 is merged
+@contextmanager
+def get_temp_file_location():
+    with get_temp_file_name() as path:
+        os.unlink(path)
+        yield path
+        if os.path.exists(path):
+            os.unlink(path)
 
 
-@pipeline
-def sleepy():
-    sleep()
+def _send_kbd_int(temp_file):
+    while not os.path.exists(temp_file):
+        time.sleep(0.1)
 
-
-def _send_kbd_int():
-    time.sleep(0.05)
     thread.interrupt_main()
 
 
 def test_interrupt():
-    results = []
-    for result in execute_pipeline_iterator(sleepy):
-        results.append(result.event_type)
+    with get_temp_file_location() as success_tempfile:
 
-    assert DagsterEventType.PIPELINE_SUCCESS in results
+        @lambda_solid
+        def write_a_file():
+            with open(success_tempfile, 'w') as ff:
+                ff.write('yup')
 
-    results = []
-    Thread(target=_send_kbd_int).start()
+            while True:
+                time.sleep(0.1)
 
-    try:
-        for result in execute_pipeline_iterator(sleepy):
-            results.append(result.event_type)
-    except KeyboardInterrupt:
-        pass
+        @pipeline
+        def write_a_file_pipeline():
+            write_a_file()
 
-    assert DagsterEventType.STEP_FAILURE in results
-    assert DagsterEventType.PIPELINE_FAILURE in results
+        # launch a thread the waits until the file is written to launch an interrupt
+        Thread(target=_send_kbd_int, args=(success_tempfile,)).start()
+
+        results = []
+        try:
+            # launch a pipeline that writes a file and loops infinitely
+            # next time the launched thread wakes up it will send a keyboard
+            # interrupt
+            for result in execute_pipeline_iterator(write_a_file_pipeline):
+                results.append(result.event_type)
+            assert False  # should never reach
+        except KeyboardInterrupt:
+            pass
+
+        assert DagsterEventType.STEP_FAILURE in results
+        assert DagsterEventType.PIPELINE_FAILURE in results
