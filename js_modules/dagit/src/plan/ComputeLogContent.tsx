4ce5516f5638942a8ef67786f0ce4a775d29f628
prha <prha@elementl.com>
2019-10-02 22:15:39 -0700

    use tabbed logs instead of split view
    Summary: {F46615}
    
    Test Plan: bk, made sure that scroll position got maintained
    
    Reviewers: #ft, alangenfeld
    
    Reviewed By: #ft, alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D1188

--- js_modules/dagit/src/plan/ComputeLogContent.tsx
+++ js_modules/dagit/src/plan/ComputeLogContent.tsx
@@ -37,22 +37,12 @@ export class ComputeLogContent extends React.Component<
   };
 
   state = {
-    stdoutVisible: true,
-    stderrVisible: true
+    selected: "stderr"
   };
 
-  close = (e: React.SyntheticEvent, type: string) => {
+  close = (e: React.SyntheticEvent) => {
     e.stopPropagation();
-    const { stderrVisible, stdoutVisible } = this.state;
-    const keepModalOpen = type === "stdout" ? stderrVisible : stdoutVisible;
-    if (keepModalOpen) {
-      this.setState({
-        stderrVisible: type !== "stderr",
-        stdoutVisible: type !== "stdout"
-      });
-    } else {
-      this.props.onRequestClose();
-    }
+    this.props.onRequestClose();
   };
 
   renderStatus() {
@@ -68,56 +58,65 @@ export class ComputeLogContent extends React.Component<
     );
   }
 
-  renderFile(type: string) {
+  render() {
     const { computeLogs } = this.props;
-    const { stdoutVisible, stderrVisible } = this.state;
-    const visible = type === "stdout" ? stdoutVisible : stderrVisible;
-    if (!visible) {
+    const { selected } = this.state;
+
+    const logData = computeLogs[selected];
+    if (!logData) {
       return null;
     }
 
-    const content = computeLogs[type] ? computeLogs[type].data : "";
-    const path = computeLogs[type] ? computeLogs[type].path : null;
+    const { path, downloadUrl } = logData;
     const isRelativeUrl = (x?: string) => x && x.startsWith("/");
-    const downloadUrl = computeLogs[type]
-      ? isRelativeUrl(computeLogs[type].downloadUrl)
-        ? ROOT_SERVER_URI + computeLogs[type].downloadUrl
-        : computeLogs[type].downloadUrl
-      : null;
+    const url = isRelativeUrl(downloadUrl)
+      ? ROOT_SERVER_URI + downloadUrl
+      : downloadUrl;
 
-    return (
-      <FileContainer>
-        <FileHeader>
-          <Row>
-            {this.renderStatus()}
-            <Title>{type}</Title>
-          </Row>
-          <Row>
-            <Link
-              aria-label="Download link"
-              className="bp3-button bp3-minimal bp3-icon-download"
-              href={downloadUrl}
-              download
-            >
-              <LinkText>Download {type}</LinkText>
-            </Link>
-            <button
-              onClick={e => this.close(e, type)}
-              className="bp3-dialog-close-button bp3-button bp3-minimal bp3-icon-cross"
-            ></button>
-          </Row>
-        </FileHeader>
-        <FileContent content={content} />
-        <FileFooter>{path}</FileFooter>
-      </FileContainer>
-    );
-  }
-
-  render() {
     return (
       <Container>
-        {this.renderFile("stdout")}
-        {this.renderFile("stderr")}
+        <FileContainer>
+          <FileHeader>
+            <Row>
+              <Tab
+                selected={selected === "stderr"}
+                onClick={() => this.setState({ selected: "stderr" })}
+              >
+                stderr
+              </Tab>
+              <Tab
+                selected={selected === "stdout"}
+                onClick={() => this.setState({ selected: "stdout" })}
+              >
+                stdout
+              </Tab>
+            </Row>
+            <Row>
+              {this.renderStatus()}
+              <Link
+                aria-label="Download link"
+                className="bp3-button bp3-minimal bp3-icon-download"
+                href={url}
+                download
+              >
+                <LinkText>Download {selected}</LinkText>
+              </Link>
+              <button
+                onClick={this.close}
+                className="bp3-dialog-close-button bp3-button bp3-minimal bp3-icon-cross"
+              ></button>
+            </Row>
+          </FileHeader>
+          <FileContent
+            content={(computeLogs.stdout && computeLogs.stdout.data) || ""}
+            selected={selected === "stdout"}
+          />
+          <FileContent
+            content={(computeLogs.stderr && computeLogs.stderr.data) || ""}
+            selected={selected === "stderr"}
+          />
+          <FileFooter>{path}</FileFooter>
+        </FileContainer>
       </Container>
     );
   }
@@ -206,9 +205,6 @@ const LineNumbers = (props: IScrollContainerProps) => {
   );
 };
 
-const Title = styled.div`
-  margin-left: 10px;
-`;
 const Row = styled.div`
   display: flex;
   flex-direction: row;
@@ -223,6 +219,7 @@ const LinkText = styled.span`
   top: -10px;
 `;
 const Link = styled.a`
+  margin-left: 10px;
   ::before {
     margin: 0 !important;
   }
@@ -252,7 +249,7 @@ const FileHeader = styled.div`
   border-bottom: 0.5px solid #5c7080;
   color: ${({ color }) => color || "#ffffff"};
   font-weight: 600;
-  padding: 2px 10px;
+  padding: 0 10px;
   position: absolute;
   top: 0;
   left: 0;
@@ -268,6 +265,8 @@ const FileContent = styled(ScrollContainer)`
   font-family: Consolas, Menlo, monospace;
   white-space: pre;
   overflow: auto;
+  ${({ selected }: { selected: boolean }) =>
+    selected ? null : "visibility: hidden;"}
 `;
 const FileFooter = styled.div`
   display: flex;
@@ -327,5 +326,36 @@ const SolarizedColors = sc.createGlobalStyle`
   }
   .ansi-white {
     color: #eee8d5;
+  }
+`;
+const Tab = styled.div`
+  background-color: ${({ selected }: { selected: boolean }) =>
+    selected ? "#333333" : "#444444"};
+  cursor: pointer;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 150px;
+  margin-right: 1px;
+  margin-bottom: ${({ selected }: { selected: boolean }) =>
+    selected ? "-10px" : "-9px"};
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+  border-top: 0.5px solid
+    ${({ selected }: { selected: boolean }) =>
+      selected ? "#5c7080" : "transparent"};
+  border-left: 0.5px solid
+    ${({ selected }: { selected: boolean }) =>
+      selected ? "#5c7080" : "transparent"};
+  border-right: 0.5px solid
+    ${({ selected }: { selected: boolean }) =>
+      selected ? "#5c7080" : "transparent"};
+  &:hover {
+    border-top: 0.5px solid #5c7080;
+    border-left: 0.5px solid #5c7080;
+    border-right: 0.5px solid #5c7080;
+    height: ${({ selected }: { selected: boolean }) =>
+      selected ? "30px" : "28px"};
   }
 `;
