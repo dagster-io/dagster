a2e276d265b6336ba35f1a36558d87f240987a19
Nicholas Schrock <schrockn@elementl.com>
2019-11-12 15:21:32 -0800

    Merge test and code files in tutorial
    Summary:
    In order to completely avoid the thorn issues
    of importing things in and outside of packages, we instead
    are just going to have the tests in the same file as
    the example code. This allows the code to be included directly
    verbatim from the file without dealing with the fact that in
    our tests, we are executing in packages, whereas the user
    will probably not be doing that in the tutorial context.
    
    Test Plan: Run pytest manually against hello_cereal and complex_pipeline
    
    Reviewers: max
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D1408

--- examples/dagster_examples/intro_tutorial/complex_pipeline.py
+++ examples/dagster_examples/intro_tutorial/complex_pipeline.py
@@ -67,3 +67,11 @@ def complex_pipeline():
 if __name__ == '__main__':
     result = execute_pipeline(complex_pipeline)
     assert result.success
+
+
+def test_complex_pipeline():
+    res = execute_pipeline(complex_pipeline)
+    assert res.success
+    assert len(res.solid_result_list) == 4
+    for solid_res in res.solid_result_list:
+        assert solid_res.success
