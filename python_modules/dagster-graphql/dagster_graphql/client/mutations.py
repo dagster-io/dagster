fbe046bce56408f8e035b8192e91e66a7bded4f4
alangenfeld <alex@elementl.com>
2019-11-21 08:55:52 -0800

    [dagster-graphql] remove dead mutation
    Summary: remove the old variant of execution used by airflow/dask/etc that has been replaced by `execute_plan`
    
    Test Plan: buildkite
    
    Reviewers: max, natekupp, schrockn
    
    Reviewed By: natekupp
    
    Differential Revision: https://dagster.phacility.com/D1421

--- python_modules/dagster-graphql/dagster_graphql/client/mutations.py
+++ python_modules/dagster-graphql/dagster_graphql/client/mutations.py
@@ -5,31 +5,13 @@ from dagster.core.events.log import DagsterEventRecord
 from dagster.core.instance import DagsterInstance
 from dagster.core.serdes import deserialize_json_to_dagster_namedtuple
 
-from .query import (
-    EXECUTE_PLAN_MUTATION,
-    RAW_EXECUTE_PLAN_MUTATION,
-    START_PIPELINE_EXECUTION_MUTATION,
-)
+from .query import EXECUTE_PLAN_MUTATION, RAW_EXECUTE_PLAN_MUTATION
 from .util import HANDLED_EVENTS, dagster_event_from_dict
 
 
 class DagsterGraphQLClientError(DagsterError):
     '''Indicates that some error has occurred when executing a GraphQL query against
     dagster-graphql'''
-
-
-def execute_start_pipeline_execution_mutation(handle, variables, instance_ref=None):
-    res = execute_query(
-        handle,
-        START_PIPELINE_EXECUTION_MUTATION,
-        variables,
-        use_sync_executor=True,
-        instance=DagsterInstance.from_ref(instance_ref)
-        if instance_ref
-        else DagsterInstance.ephemeral(),
-    )
-    handle_execution_errors(res, 'startPipelineExecution')
-    return handle_start_pipeline_execution_result(res)
 
 
 def execute_execute_plan_mutation(handle, variables, instance_ref=None):
@@ -67,55 +49,6 @@ def execute_execute_plan_mutation_raw(handle, variables, instance_ref=None):
     res = execute_query(handle, RAW_EXECUTE_PLAN_MUTATION, variables, instance=instance)
     handle_execution_errors(res, 'executePlan')
     return handle_execute_plan_result_raw(res)
-
-
-def handle_start_pipeline_execution_result(res):
-    res_data = res['data']['startPipelineExecution']
-
-    res_type = res_data['__typename']
-
-    if res_type == 'InvalidStepError':
-        raise DagsterGraphQLClientError(
-            'invalid step {step_key}'.format(step_key=res_data['invalidStepKey'])
-        )
-
-    if res_type == 'InvalidOutputError':
-        raise DagsterGraphQLClientError(
-            'invalid output {output} for step {step_key}'.format(
-                output=res_data['invalidOutputName'], step_key=res_data['stepKey']
-            )
-        )
-
-    if res_type == 'PipelineConfigValidationInvalid':
-        errors = [err['message'] for err in res_data['errors']]
-        raise DagsterGraphQLClientError(
-            'Pipeline configuration invalid:\n{errors}'.format(errors='\n'.join(errors))
-        )
-
-    if res_type == 'PipelineNotFoundError':
-        raise DagsterGraphQLClientError(
-            'Pipeline "{pipeline_name}" not found: {message}:'.format(
-                pipeline_name=res_data['pipelineName'], message=res_data['message']
-            )
-        )
-
-    if res_type == 'PythonError':
-        raise DagsterGraphQLClientError(
-            'Subplan execution failed: {message}\n{stack}'.format(
-                message=res_data['message'], stack=res_data['stack']
-            )
-        )
-
-    if res_type == 'StartPipelineExecutionSuccess':
-        pipeline_name = res_data['run']['pipeline']['name']
-
-        return [
-            dagster_event_from_dict(e, pipeline_name)
-            for e in res_data['run']['logs']['nodes']
-            if e['__typename'] in HANDLED_EVENTS
-        ]
-
-    raise DagsterGraphQLClientError('Unexpected result type')
 
 
 def handle_error_states(res_type, res_data):
--- python_modules/dagster-graphql/dagster_graphql_tests/client_tests/test_mutations.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/client_tests/test_mutations.py
@@ -3,7 +3,6 @@ import uuid
 from dagster_graphql.client.mutations import (
     execute_execute_plan_mutation,
     execute_execute_plan_mutation_raw,
-    execute_start_pipeline_execution_mutation,
 )
 
 from dagster import ExecutionTargetHandle
@@ -35,27 +34,6 @@ EXPECTED_EVENTS = {
     ('STEP_SUCCESS', 'sleeper_4.compute'),
     ('STEP_SUCCESS', 'total.compute'),
 }
-
-
-def test_execute_start_pipeline_execution_mutation():
-    pipeline_name = 'sleepy_pipeline'
-    handle = ExecutionTargetHandle.for_pipeline_module(
-        'dagster_examples.toys.sleepy', pipeline_name
-    )
-    variables = {
-        'executionParams': {
-            'environmentConfigData': {},
-            'mode': 'default',
-            'selector': {'name': pipeline_name},
-            'executionMetadata': {'runId': '12345'},
-        }
-    }
-    result = execute_start_pipeline_execution_mutation(handle, variables)
-    seen_events = set()
-    for event in result:
-        seen_events.add((event.event_type_value, event.step_key))
-
-    assert seen_events == EXPECTED_EVENTS
 
 
 def test_execute_execute_plan_mutation():
