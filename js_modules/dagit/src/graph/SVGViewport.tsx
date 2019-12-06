e52793e222e90b5bc3fde6277b40890bb191d52f
Ben Gotow <ben@foundry376.com>
2019-12-05 17:10:13 -0800

    Replace current two-step zoom with smooth zoom
    Summary: Pretty self-explanatory!
    
    Test Plan: Run tests
    
    Reviewers: @schrockn, @alangenfeld
    
    Subscribers:

--- js_modules/dagit/src/graph/SVGViewport.tsx
+++ js_modules/dagit/src/graph/SVGViewport.tsx
@@ -34,6 +34,7 @@ interface Point {
 
 export const DETAIL_ZOOM = 0.75;
 export const MAX_OVERVIEW_ZOOM = 0.39;
+export const MIN_OVERVIEW_ZOOM = 0.15;
 
 const PanAndZoomInteractor: SVGViewportInteractor = {
   onMouseDown(viewport: SVGViewport, event: React.MouseEvent<HTMLDivElement>) {
@@ -66,24 +67,18 @@ const PanAndZoomInteractor: SVGViewportInteractor = {
   },
 
   onWheel(viewport: SVGViewport, event: React.WheelEvent<HTMLDivElement>) {
-    // Because of inertial scrolling on macOS, we receive wheel events for ~1000ms
-    // after we trigger the zoom and this can cause a second zoom.
-    const wheelWasIdle = Date.now() - viewport._lastWheelTime > 2000;
-    const wheelChangedDir = viewport._lastWheelDir !== Math.sign(event.deltaY);
+    const offset = viewport.screenToSVGCoords(viewport.getOffsetXY(event));
+    let { x, y, scale } = viewport.state;
+    const oldScale = scale;
 
-    // Ignore very gentle wheel interaction, could be accidental
-    if (Math.abs(event.deltaY) <= 2) return;
+    scale = Math.max(
+      0.015,
+      Math.min(DETAIL_ZOOM, scale * (1 - event.deltaY * 0.0025))
+    );
+    x = x + (offset.x * oldScale - offset.x * scale);
+    y = y + (offset.y * oldScale - offset.y * scale);
 
-    if (wheelWasIdle || wheelChangedDir) {
-      if (event.deltaY < 0) {
-        viewport.onZoomAndCenter(event);
-      } else {
-        viewport.autocenter(true);
-      }
-    }
-
-    viewport._lastWheelTime = Date.now();
-    viewport._lastWheelDir = Math.sign(event.deltaY);
+    viewport.setState({ x, y, scale });
   }
 };
 
@@ -119,18 +114,28 @@ export default class SVGViewport extends React.Component<
 
     const dw = ownerRect.width / this.props.graphWidth;
     const dh = ownerRect.height / this.props.graphHeight;
-    const scale = Math.min(dw, dh, MAX_OVERVIEW_ZOOM);
+    const desiredScale = Math.min(dw, dh);
+    const boundedScale = Math.max(
+      Math.min(desiredScale, MAX_OVERVIEW_ZOOM),
+      MIN_OVERVIEW_ZOOM
+    );
 
+    if (this.state.scale < boundedScale && desiredScale !== boundedScale) {
+      // If the user is zoomed out past where they're going to land, AND where they're going to land
+      // is not a view of the entire DAG but instead a view of some zoomed section, autocentering is
+      // undesirable and should do nothing.
+      return;
+    }
     const target = {
-      x: -(this.props.graphWidth / 2) * scale + ownerRect.width / 2,
-      y: -(this.props.graphHeight / 2) * scale + ownerRect.height / 2,
-      scale: scale
+      x: -(this.props.graphWidth / 2) * boundedScale + ownerRect.width / 2,
+      y: -(this.props.graphHeight / 2) * boundedScale + ownerRect.height / 2,
+      scale: boundedScale
     };
 
     if (animate) {
       this.smoothZoom(target);
     } else {
-      this.setState(Object.assign(target, { minScale: scale }));
+      this.setState(Object.assign(target, { minScale: boundedScale }));
     }
   }
 
