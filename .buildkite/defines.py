434ee0d4f395484c1d2f3389897b91d8f81629fa
Nate Kupp <nate@elementl.com>
2019-12-06 12:04:44 -0800

    Python 3.8 support in buildkite
    Summary:
    This enables Python 3.8 tests for most things in Dagster.
    
    See: https://github.com/dagster-io/dagster/issues/1960 for notes on what's not currently covered.
    
    Test Plan: unit
    
    Reviewers: schrockn, alangenfeld, max, prha
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D1560

--- .buildkite/defines.py
+++ .buildkite/defines.py
@@ -1,11 +1,13 @@
 # This should be an enum once we make our own buildkite AMI with py3
 class SupportedPython(object):
+    V3_8 = "3.8.0"
     V3_7 = "3.7.4"
     V3_6 = "3.6.9"
     V3_5 = "3.5.7"
     V2_7 = "2.7.16"
 
 
+# See: https://github.com/dagster-io/dagster/issues/1960
 SupportedPythons = [
     SupportedPython.V3_7,
     SupportedPython.V3_6,
@@ -13,4 +15,5 @@ SupportedPythons = [
     SupportedPython.V2_7,
 ]
 
+# See: https://github.com/dagster-io/dagster/issues/1960
 SupportedPython3s = [SupportedPython.V3_7, SupportedPython.V3_6, SupportedPython.V3_5]
--- .buildkite/images/docker/build.sh
+++ .buildkite/images/docker/build.sh
@@ -24,6 +24,7 @@ IMAGE_TYPE=$2
 
 function cleanup {
     rm -rf scala_modules
+    rm -rf pyarrow-0.15.1.dev403+gfe41a6822-cp38-cp38-linux_x86_64.whl
 }
 
 # ensure cleanup happens on error or normal exit
@@ -31,10 +32,21 @@ trap cleanup EXIT
 
 rsync -av --exclude='*target*' --exclude='*.idea*' --exclude='*.class' $ROOT/scala_modules .
 
+# pyarrow for Debian Buster
+# this was built in a Buster Docker container using the instructions here:
+# https://arrow.apache.org/docs/developers/python.html
+gsutil cp gs://elementl-deploy-artifacts-9c3ff00/pyarrow-0.15.1.dev403+gfe41a6822-cp38-cp38-linux_x86_64.whl .
+
+DEBIAN_VERSION="stretch"
+if [ $PYTHON_MAJMIN == "38" ]; then
+    DEBIAN_VERSION="buster"
+fi
+
 if [ $IMAGE_TYPE == "integration" ]; then
     # Build the integration image
     docker build . \
         --no-cache \
+        --build-arg DEBIAN_VERSION=$DEBIAN_VERSION \
         --build-arg PYTHON_VERSION=$PYTHON_VERSION \
         --build-arg PYTHON_MAJOR_VERSION=$PYTHON_MAJOR_VERSION \
         --target dagster-integration-image \
