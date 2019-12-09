7d28e6cc3b2bc2abe71326f28dce897d91d9a934
alangenfeld <alex@elementl.com>
2019-12-09 08:19:01 -0800

    [nightly] fix aws and pyspark
    Test Plan: bk
    
    Reviewers: nate, max
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D1603

--- python_modules/libraries/dagster-aws/setup.py
+++ python_modules/libraries/dagster-aws/setup.py
@@ -23,7 +23,7 @@ parser.add_argument('--nightly', action='store_true')
 
 def _do_setup(name='dagster-aws'):
     setup(
-        name='dagster_aws',
+        name=name,
         version=get_version(name),
         author='Elementl',
         license='Apache-2.0',
