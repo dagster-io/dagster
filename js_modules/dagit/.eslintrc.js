8bb54ff8c5cb1400c89924c68e1bd86107d55af3
prha <prha@elementl.com>
2019-12-12 13:05:23 -0800

    Add web worker to calculate dagre layout for large graphs
    Summary: Hooks based approaches use `useEffect`, which only runs side effects *after* the first render call
    
    Test Plan: Rendered multiple pipelines in `Explore`
    
    Reviewers: bengotow, alangenfeld
    
    Reviewed By: bengotow, alangenfeld
    
    Subscribers: bengotow
    
    Differential Revision: https://dagster.phacility.com/D1657

--- js_modules/dagit/.eslintrc.js
+++ js_modules/dagit/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  plugins: ["react-hooks"],
+  plugins: ["react-hooks", "import"],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
---
+++ js_modules/dagit/src/workers/dagre_layout.worker.ts
@@ -0,0 +1,9 @@
+import { layoutPipeline } from "../graph/layout";
+// eslint-disable-next-line no-restricted-globals
+const ctx: Worker = self as any;
+
+ctx.addEventListener("message", async event => {
+  const { solids, parentSolid } = event.data;
+  const layout = layoutPipeline(solids, parentSolid);
+  ctx.postMessage(layout);
+});
