5ce40c9f61fa4dbfa4b26f13ace8541724a8f7f4
Nicholas Schrock <schrockn@elementl.com>
2019-12-18 10:17:38 -0800

    Fix lakehouse demo
    Summary: This wasn't loading
    
    Test Plan: load dagit in lakehouse_test
    
    Reviewers: max
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D1700

--- python_modules/lakehouse/lakehouse_tests/repo.py
+++ python_modules/lakehouse/lakehouse_tests/repo.py
@@ -1,6 +1,6 @@
-from dagster import RepositoryDefinition
+from test_typed_pyspark_lakehouse import typed_lakehouse_pipeline
 
-from .test_typed_pyspark_lakehouse import typed_lakehouse_pipeline
+from dagster import RepositoryDefinition
 
 
 def lakehouse_test_repo():
