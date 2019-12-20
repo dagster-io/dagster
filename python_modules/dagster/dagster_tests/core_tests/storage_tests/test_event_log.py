912aa5c82ff4f5f5d99fd7bac4d9db01e02b845d
Sashank Thupukari <s@thupukari.com>
2019-12-20 13:40:29 -0500

    Parameterize event storage tests
    Summary:
    We previously duplicated tests for the in-memory and sqlite implementations of event log storage.
    
    This diff simply uses the pytest parameterize feature to consolidate the duplicate tests.
    
    Test Plan: unit
    
    Reviewers: max, schrockn
    
    Reviewed By: schrockn
    
    Differential Revision: https://dagster.phacility.com/D1725

--- python_modules/dagster/dagster_tests/core_tests/storage_tests/test_event_log.py
+++ python_modules/dagster/dagster_tests/core_tests/storage_tests/test_event_log.py
@@ -1,5 +1,6 @@
 import os
 import time
+from contextlib import contextmanager
 
 import pytest
 import sqlalchemy
@@ -17,106 +18,43 @@ from dagster.core.storage.event_log import (
 from dagster.core.storage.sql import create_engine
 
 
-def test_init_in_memory_event_log_storage():
-    storage = InMemoryEventLogStorage()
-    assert not storage.is_persistent
+@contextmanager
+def create_in_memory_event_log_storage():
+    yield InMemoryEventLogStorage()
 
 
-def test_in_memory_event_log_storage_run_not_found():
-    storage = InMemoryEventLogStorage()
-    assert storage.get_logs_for_run('bar') == []
-
-
-def test_in_memory_event_log_storage_store_events_and_wipe():
-    storage = InMemoryEventLogStorage()
-    assert len(storage.get_logs_for_run('foo')) == 0
-    storage.store_event(
-        DagsterEventRecord(
-            None,
-            'Message2',
-            'debug',
-            '',
-            'foo',
-            time.time(),
-            dagster_event=DagsterEvent(
-                DagsterEventType.ENGINE_EVENT.value,
-                'nonce',
-                event_specific_data=EngineEventData.in_process(999),
-            ),
-        )
-    )
-    assert len(storage.get_logs_for_run('foo')) == 1
-    assert storage.get_stats_for_run('foo')
-    storage.wipe()
-    assert len(storage.get_logs_for_run('foo')) == 0
-
-
-def test_in_memory_event_log_storage_watch():
-    def evt(name):
-        return DagsterEventRecord(
-            None,
-            name,
-            'debug',
-            '',
-            'foo',
-            time.time(),
-            dagster_event=DagsterEvent(
-                DagsterEventType.ENGINE_EVENT.value,
-                'nonce',
-                event_specific_data=EngineEventData.in_process(999),
-            ),
-        )
-
-    watched = []
-    watcher = lambda x: watched.append(x)  # pylint: disable=unnecessary-lambda
-
-    storage = InMemoryEventLogStorage()
-    assert len(storage.get_logs_for_run('foo')) == 0
-
-    storage.store_event(evt('Message1'))
-    assert len(storage.get_logs_for_run('foo')) == 1
-    assert len(watched) == 0
-
-    storage.watch('foo', None, watcher)
-    storage.store_event(evt('Message2'))
-    assert len(storage.get_logs_for_run('foo')) == 2
-    assert len(watched) == 1
-
-    storage.end_watch('foo', lambda event: None)
-    storage.store_event(evt('Message3'))
-    assert len(storage.get_logs_for_run('foo')) == 3
-    assert len(watched) == 2
-
-    storage.end_watch('bar', lambda event: None)
-    storage.store_event(evt('Message4'))
-    assert len(storage.get_logs_for_run('foo')) == 4
-    assert len(watched) == 3
-
-    storage.end_watch('foo', watcher)
-    storage.store_event(evt('Message5'))
-    assert len(storage.get_logs_for_run('foo')) == 5
-    assert len(watched) == 3
-
-    storage.delete_events('foo')
-    assert len(storage.get_logs_for_run('foo')) == 0
-    assert len(watched) == 3
-
-
-def test_init_filesystem_event_log_storage():
+@contextmanager
+def create_sqlite_run_event_logstorage():
     with seven.TemporaryDirectory() as tmpdir_path:
-        storage = SqliteEventLogStorage(tmpdir_path)
-        assert storage.is_persistent
+        yield SqliteEventLogStorage(tmpdir_path)
 
 
-def test_filesystem_event_log_storage_run_not_found():
-    with seven.TemporaryDirectory() as tmpdir_path:
-        storage = SqliteEventLogStorage(tmpdir_path)
+event_storage_test = pytest.mark.parametrize(
+    'event_storage_factory_cm_fn',
+    [create_in_memory_event_log_storage, create_sqlite_run_event_logstorage],
+)
+
+
+@event_storage_test
+def test_init_log_storage(event_storage_factory_cm_fn):
+    with event_storage_factory_cm_fn() as storage:
+        if isinstance(storage, InMemoryEventLogStorage):
+            assert not storage.is_persistent
+        elif isinstance(storage, SqliteEventLogStorage):
+            assert storage.is_persistent
+        else:
+            raise Exception("Invalid event storage type")
+
+
+@event_storage_test
+def test_log_storage_run_not_found(event_storage_factory_cm_fn):
+    with event_storage_factory_cm_fn() as storage:
         assert storage.get_logs_for_run('bar') == []
 
 
-def test_filesystem_event_log_storage_store_events_and_wipe():
-    with seven.TemporaryDirectory() as tmpdir_path:
-        storage = SqliteEventLogStorage(tmpdir_path)
+@event_storage_test
+def test_event_log_storage_store_events_and_wipe(event_storage_factory_cm_fn):
+    with event_storage_factory_cm_fn() as storage:
         assert len(storage.get_logs_for_run('foo')) == 0
         storage.store_event(
             DagsterEventRecord(
@@ -134,11 +72,13 @@ def test_filesystem_event_log_storage_store_events_and_wipe():
             )
         )
         assert len(storage.get_logs_for_run('foo')) == 1
+        assert storage.get_stats_for_run('foo')
         storage.wipe()
         assert len(storage.get_logs_for_run('foo')) == 0
 
 
-def test_filesystem_event_log_storage_watch():
+@event_storage_test
+def test_event_log_storage_watch(event_storage_factory_cm_fn):
     def evt(name):
         return DagsterEventRecord(
             None,
@@ -154,36 +94,34 @@ def test_filesystem_event_log_storage_watch():
             ),
         )
 
-    watched = []
+    with event_storage_factory_cm_fn() as storage:
+        watched = []
+        watcher = lambda x: watched.append(x)  # pylint: disable=unnecessary-lambda
 
-    def callback(evt):
-        watched.append(evt)
-
-    noop = lambda evt: None
-
-    with seven.TemporaryDirectory() as tmpdir_path:
-        storage = SqliteEventLogStorage(tmpdir_path)
-
+        storage = InMemoryEventLogStorage()
         assert len(storage.get_logs_for_run('foo')) == 0
 
         storage.store_event(evt('Message1'))
         assert len(storage.get_logs_for_run('foo')) == 1
         assert len(watched) == 0
 
-        storage.watch('foo', 2, callback)
+        storage.watch('foo', None, watcher)
         storage.store_event(evt('Message2'))
         assert len(storage.get_logs_for_run('foo')) == 2
+        assert len(watched) == 1
 
-        storage.end_watch('foo', noop)
+        storage.end_watch('foo', lambda event: None)
         storage.store_event(evt('Message3'))
         assert len(storage.get_logs_for_run('foo')) == 3
+        assert len(watched) == 2
 
-        storage.end_watch('bar', noop)
+        storage.end_watch('bar', lambda event: None)
         storage.store_event(evt('Message4'))
         assert len(storage.get_logs_for_run('foo')) == 4
+        assert len(watched) == 3
 
         time.sleep(0.5)  # this value scientifically selected from a range of attractive values
-        storage.end_watch('foo', callback)
+        storage.end_watch('foo', watcher)
         time.sleep(0.5)
         storage.store_event(evt('Message5'))
         assert len(storage.get_logs_for_run('foo')) == 5
@@ -194,9 +132,9 @@ def test_filesystem_event_log_storage_watch():
         assert len(watched) == 3
 
 
-def test_event_log_delete():
-    with seven.TemporaryDirectory() as tmpdir_path:
-        storage = SqliteEventLogStorage(tmpdir_path)
+@event_storage_test
+def test_event_log_delete(event_storage_factory_cm_fn):
+    with event_storage_factory_cm_fn() as storage:
         assert len(storage.get_logs_for_run('foo')) == 0
         storage.store_event(
             DagsterEventRecord(
@@ -217,6 +155,16 @@ def test_event_log_delete():
         assert storage.get_stats_for_run('foo')
         storage.delete_events('foo')
         assert len(storage.get_logs_for_run('foo')) == 0
+
+
+@event_storage_test
+def test_event_log_get_stats_without_start_and_success(event_storage_factory_cm_fn):
+    # When an event log doesn't have a PIPELINE_START or PIPELINE_SUCCESS | PIPELINE_FAILURE event,
+    # we want to ensure storage.get_stats_for_run(...) doesn't throw an error.
+
+    with event_storage_factory_cm_fn() as storage:
+        assert len(storage.get_logs_for_run('foo')) == 0
+        assert storage.get_stats_for_run('foo')
 
 
 def test_filesystem_event_log_storage_run_corrupted():
