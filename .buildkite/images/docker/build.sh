0e0a7a633d409955930475c6841347e8268df2ac
Nate Kupp <nate@elementl.com>
2019-12-28 13:49:34 -0800

    Remove public images
    Summary:
    Per discussion on D1704, removing our public images for now - we can bring these back later in lockstep with our versioned releases.
    
    Also, planning to continue iterating on integration images and this'll make it easier to update the Dockerfile.
    
    Test Plan: buildkite
    
    Reviewers: schrockn
    
    Reviewed By: schrockn
    
    Differential Revision: https://dagster.phacility.com/D1749

--- .buildkite/images/docker/build.sh
+++ .buildkite/images/docker/build.sh
@@ -4,9 +4,9 @@ cd $ROOT/.buildkite/images/docker/
 set -eux
 
 
-if [ "$#" -ne 3 ]; then
-    echo "Error: Must specify a Python version and image type along with version.\n" 1>&2
-    echo "Usage: ./build.sh 3.7.4 integration v6" 1>&2
+if [ "$#" -ne 2 ]; then
+    echo "Error: Must specify a Python version and image version.\n" 1>&2
+    echo "Usage: ./build.sh 3.7.4 v6" 1>&2
     exit 1
 fi
 
@@ -17,10 +17,8 @@ PYTHON_MAJOR_VERSION="${PYTHON_VERSION:0:1}"
 # e.g. 37
 PYTHON_MAJMIN=`echo "${PYTHON_VERSION:0:3}" | sed 's/\.//'`
 
-IMAGE_TYPE=$2
-
 # Version of the buildkite integration image
-IMAGE_VERSION=$3
+IMAGE_VERSION=$2
 
 function cleanup {
     rm -rf scala_modules
@@ -36,21 +34,10 @@ if [ $PYTHON_MAJMIN == "38" ]; then
     DEBIAN_VERSION="buster"
 fi
 
-if [ $IMAGE_TYPE == "integration" ]; then
-    # Build the integration image
-    docker build . \
-        --no-cache \
-        --build-arg DEBIAN_VERSION=$DEBIAN_VERSION \
-        --build-arg PYTHON_VERSION=$PYTHON_VERSION \
-        --build-arg PYTHON_MAJOR_VERSION=$PYTHON_MAJOR_VERSION \
-        --target dagster-integration-image \
-        -t "dagster/buildkite-integration:py${PYTHON_VERSION}-${IMAGE_VERSION}"
-else
-    # Build the public image
-    docker build . \
-        --no-cache \
-        --build-arg DEBIAN_VERSION=$DEBIAN_VERSION \
-        --build-arg PYTHON_VERSION=$PYTHON_VERSION \
-        --target dagster-public-image \
-        -t "dagster/dagster-py${PYTHON_MAJMIN}"
-fi
+# Build the integration image
+docker build . \
+    --no-cache \
+    --build-arg DEBIAN_VERSION=$DEBIAN_VERSION \
+    --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+    --build-arg PYTHON_MAJOR_VERSION=$PYTHON_MAJOR_VERSION \
+    -t "dagster/buildkite-integration:py${PYTHON_VERSION}-${IMAGE_VERSION}"
--- .buildkite/images/docker/push.sh
+++ .buildkite/images/docker/push.sh
@@ -3,9 +3,9 @@ ROOT=$(git rev-parse --show-toplevel)
 cd $ROOT/.buildkite/images/docker/
 set -eux
 
-if [ "$#" -ne 3 ]; then
-    echo "Error: Must specify a Python version and image type along with version.\n" 1>&2
-    echo "Usage: ./build.sh 3.7.4 integration v6" 1>&2
+if [ "$#" -ne 2 ]; then
+    echo "Error: Must specify a Python version and image version.\n" 1>&2
+    echo "Usage: ./push.sh 3.7.4 v6" 1>&2
     exit 1
 fi
 
@@ -16,22 +16,15 @@ PYTHON_MAJOR_VERSION="${PYTHON_VERSION:0:1}"
 # e.g. 37
 PYTHON_MAJMIN=`echo "${PYTHON_VERSION:0:3}" | sed 's/\.//'`
 
-IMAGE_TYPE=$2
-
 # Version of the buildkite integration image
-IMAGE_VERSION=$3
+IMAGE_VERSION=$2
 
 TAG=`date '+%Y-%m-%d'`
 
-if [ $IMAGE_TYPE == "integration" ]; then
-    docker tag "dagster/buildkite-integration:py${PYTHON_VERSION}-${IMAGE_VERSION}" \
-        "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-1.amazonaws.com/buildkite-integration:py${PYTHON_VERSION}-${IMAGE_VERSION}"
+# Log into ECR
+aws ecr get-login --no-include-email --region us-west-1 | sh
 
-    docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-1.amazonaws.com/buildkite-integration:py${PYTHON_VERSION}-${IMAGE_VERSION}"
-else
-    docker tag "dagster/dagster-py${PYTHON_MAJMIN}" "dagster/dagster-py${PYTHON_MAJMIN}:${TAG}"
-    docker tag "dagster/dagster-py${PYTHON_MAJMIN}" "dagster/dagster-py${PYTHON_MAJMIN}:latest"
+docker tag "dagster/buildkite-integration:py${PYTHON_VERSION}-${IMAGE_VERSION}" \
+    "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-1.amazonaws.com/buildkite-integration:py${PYTHON_VERSION}-${IMAGE_VERSION}"
 
-    docker push "dagster/dagster-py${PYTHON_MAJMIN}:${TAG}"
-    docker push "dagster/dagster-py${PYTHON_MAJMIN}:latest"
-fi
+docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-1.amazonaws.com/buildkite-integration:py${PYTHON_VERSION}-${IMAGE_VERSION}"
