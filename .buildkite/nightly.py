f84aab273120516e708ca195a38a806847eed893
Nate Kupp <nate@elementl.com>
2019-12-30 13:52:49 -0800

    Remove publish docker from nightly build
    Summary: Fixes nightly build failure
    
    Test Plan: buildkite
    
    Reviewers: schrockn
    
    Reviewed By: schrockn
    
    Differential Revision: https://dagster.phacility.com/D1760

--- .buildkite/nightly.py
+++ .buildkite/nightly.py
@@ -2,36 +2,14 @@ import os
 import sys
 
 import yaml
-from defines import SupportedPython, SupportedPythons
-from step_builder import BuildkiteQueue, StepBuilder
+from defines import SupportedPython
+from step_builder import StepBuilder
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(SCRIPT_PATH)
 
-
-def publish_docker_images():
-    # e.g. 27, 35, 36, 37
-    python_versions = [''.join(py_version.split('.')[:2]) for py_version in SupportedPythons]
-
-    return [
-        StepBuilder("docker image %s" % version)
-        .run(
-            r"aws s3 cp s3://\${BUILDKITE_SECRETS_BUCKET}/dockerhub-creds /tmp/dockerhub-creds",
-            "cat /tmp/dockerhub-creds | docker login --username elementldevtools --password-stdin",
-            "pushd /workdir/.buildkite/images/",
-            "make VERSION=\"public\" build-public-{version}".format(version=version),
-            "make VERSION=\"public\" push-public-{version}".format(version=version),
-        )
-        .on_integration_image(SupportedPython.V3_7, ['BUILDKITE_SECRETS_BUCKET'])
-        .on_queue(BuildkiteQueue.DOCKER)
-        .with_timeout(30)
-        .build()
-        for version in python_versions
-    ]
-
-
 if __name__ == "__main__":
-    steps = publish_docker_images() + [
+    steps = [
         StepBuilder('publish nightlies')
         .on_integration_image(
             SupportedPython.V3_7, ['SLACK_RELEASE_BOT_TOKEN', 'PYPI_USERNAME', 'PYPI_PASSWORD']
