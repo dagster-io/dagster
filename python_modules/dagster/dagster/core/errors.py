94e90a72955a14a8490ed2805d7f187612dba511
Max Gasner <max.gasner@gmail.com>
2020-01-27 12:47:28 -0800

    Poor man's race condition fix
    Summary: We catch the IntegrityError here in the absence of a better locking scheme.
    
    Test Plan: Prod
    
    Reviewers: alangenfeld
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D1889

--- python_modules/dagster/dagster/core/errors.py
+++ python_modules/dagster/dagster/core/errors.py
@@ -268,3 +268,7 @@ class DagsterInstanceMigrationRequired(DagsterError):
                 ),
             )
         )
+
+
+class DagsterRunAlreadyExists(DagsterError):
+    '''Indicates that a pipeline run already exists in a run storage.'''
