69e82eaec3a3372d175a6821ddc20270289334cb
alangenfeld <alex@elementl.com>
2020-02-04 09:53:17 -0800

    fix dagster-k8s publish
    Test Plan: grep for setup\(\n.*name='
    
    Reviewers: nate, max
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D1952

--- python_modules/libraries/dagster-k8s/setup.py
+++ python_modules/libraries/dagster-k8s/setup.py
@@ -23,7 +23,7 @@ parser.add_argument('--nightly', action='store_true')
 
 def _do_setup(name='dagster-k8s'):
     setup(
-        name='dagster_k8s',
+        name=name,
         version=get_version(name),
         author='Elementl',
         license='Apache-2.0',
