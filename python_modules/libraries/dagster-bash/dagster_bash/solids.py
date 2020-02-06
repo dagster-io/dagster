dbcb64688fb0d574505e7404e58be756514cc569
Nate Kupp <nate@elementl.com>
2020-02-06 11:42:49 -0800

    Improvements to the dagster-bash bash solid factory
    Summary: Related thread in #general: https://dagster.slack.com/archives/CCCR6P2UR/p1580849984100300
    
    Test Plan: unit
    
    Reviewers: alangenfeld, prha
    
    Reviewed By: prha
    
    Differential Revision: https://dagster.phacility.com/D1968

--- python_modules/libraries/dagster-bash/dagster_bash/solids.py
+++ python_modules/libraries/dagster-bash/dagster_bash/solids.py
@@ -3,23 +3,72 @@ import signal
 from subprocess import PIPE, STDOUT, Popen
 from tempfile import NamedTemporaryFile
 
-from dagster import Enum, EnumValue, Failure, Field, Permissive, check, seven, solid
+from dagster import (
+    Enum,
+    EnumValue,
+    Failure,
+    Field,
+    InputDefinition,
+    Nothing,
+    OutputDefinition,
+    Permissive,
+    check,
+    seven,
+    solid,
+)
 
 
-def bash_command_solid(bash_command, name=None, output_encoding=None):
-    '''Execute a Bash command.
+def bash_command_solid(
+    bash_command, name='bash_solid', input_defs=None, output_encoding='utf-8', **kwargs
+):
+    '''This function is a factory which constructs a solid that will execute a Bash command.
+
+    Any kwargs passed to this function will be passed along to the underlying @solid decorator.
+    However, note that overriding config or output_defs is not supported. You might consider using
+    @composite_solid to wrap this solid in the cases where you'd like to configure the bash solid
+    with different config fields.
+
+    Args:
+        bash_command (str): The shell command to execute.
+        name (str, optional): The name of this solid. Defaults to "bash_solid".
+        input_defs (List[InputDefinition], optional): input definitions for the solid. Defaults to
+            a single Nothing input.
+        output_encoding (str, optional): The output encoding of the shell, used when reading back
+            logs produced by shell script execution. Defaults to "utf-8".
+
+    Raises:
+        Failure: Raised when the shell command returns a non-zero exit code.
+
+    Returns:
+        SolidDefinition: Returns the constructed solid definition.
     '''
     check.str_param(bash_command, 'bash_command')
-    name = check.opt_str_param(name, 'name', default='bash_solid')
-    output_encoding = check.opt_str_param(output_encoding, 'output_encoding', default='utf-8')
+    name = check.str_param(name, 'name')
+    check.str_param(output_encoding, 'output_encoding')
+    check.opt_list_param(input_defs, 'input_defs', of_type=InputDefinition)
+
+    if 'output_defs' in kwargs:
+        raise TypeError('Overriding output_defs for bash solid is not supported.')
+
+    if 'config' in kwargs:
+        raise TypeError('Overriding config for bash solid is not supported.')
 
     @solid(
         name=name,
+        description=kwargs.pop('description', 'A solid to invoke a bash command.'),
+        input_defs=input_defs or [InputDefinition('start', Nothing)],
+        output_defs=[OutputDefinition(str, 'result')],
         config={
+            'env': Field(
+                Permissive(),
+                default_value=None,
+                is_required=False,
+                description='An optional dict of environment variables to pass to the subprocess.',
+            ),
             'output_logging': Field(
                 Enum(
-                    'OutputType',
-                    [
+                    name='OutputType',
+                    enum_values=[
                         EnumValue('STREAM', description='Stream script stdout/stderr.'),
                         EnumValue(
                             'BUFFER',
@@ -29,16 +78,10 @@ def bash_command_solid(bash_command, name=None, output_encoding=None):
                     ],
                 ),
                 is_required=False,
-                default_value='STREAM',
-            ),
-            'env': Field(
-                Permissive(),
-                description='Environment variables to pass to the child process; if not provided, '
-                'the current process environment will be passed.',
-                is_required=False,
-                default_value=None,
+                default_value='BUFFER',
             ),
         },
+        **kwargs
     )
     def _bash_solid(context):
         '''This logic is ported from the Airflow BashOperator implementation.
@@ -46,11 +89,8 @@ def bash_command_solid(bash_command, name=None, output_encoding=None):
         https://github.com/apache/airflow/blob/master/airflow/operators/bash_operator.py
         '''
 
-        def log_info_msg(log_str):
-            context.log.info('[bash][{name}] '.format(name=name) + log_str)
-
         tmp_path = seven.get_system_temp_directory()
-        log_info_msg('using temporary directory: %s' % tmp_path)
+        context.log.info('using temporary directory: %s' % tmp_path)
 
         env = (
             context.solid_config['env']
@@ -58,11 +98,16 @@ def bash_command_solid(bash_command, name=None, output_encoding=None):
             else os.environ.copy()
         )
 
+        # Solid will return the string result of reading stdout of the shell command
+        res = ''
+
         with NamedTemporaryFile(dir=tmp_path, prefix=name) as tmp_file:
             tmp_file.write(bytes(bash_command.encode('utf-8')))
             tmp_file.flush()
             script_location = os.path.abspath(tmp_file.name)
-            log_info_msg('Temporary script location: {location}'.format(location=script_location))
+            context.log.info(
+                'Temporary script location: {location}'.format(location=script_location)
+            )
 
             def pre_exec():
                 # Restore default signal disposition and invoke setsid
@@ -71,7 +116,7 @@ def bash_command_solid(bash_command, name=None, output_encoding=None):
                         signal.signal(getattr(signal, sig), signal.SIG_DFL)
                 os.setsid()
 
-            log_info_msg('Running command: {command}'.format(command=bash_command))
+            context.log.info('Running command: {command}'.format(command=bash_command))
 
             # pylint: disable=subprocess-popen-preexec-fn
             sub_process = Popen(
@@ -85,42 +130,55 @@ def bash_command_solid(bash_command, name=None, output_encoding=None):
 
             # Stream back logs as they are emitted
             if context.solid_config['output_logging'] == 'STREAM':
-                line = ''
                 for raw_line in iter(sub_process.stdout.readline, b''):
                     line = raw_line.decode(output_encoding).rstrip()
-                    log_info_msg(line)
+                    context.log.info(line)
+                    res += line
 
             sub_process.wait()
 
             # Collect and buffer all logs, then emit
             if context.solid_config['output_logging'] == 'BUFFER':
-                line = ''
-                for raw_line in iter(sub_process.stdout.readline, b''):
-                    line += raw_line.decode(output_encoding)
-                log_info_msg(line)
+                res = ''.join(
+                    [
+                        raw_line.decode(output_encoding)
+                        for raw_line in iter(sub_process.stdout.readline, b'')
+                    ]
+                )
+                context.log.info(res)
 
             # no logging in this case
             elif context.solid_config['output_logging'] == 'NONE':
                 pass
 
-            log_info_msg(
+            context.log.info(
                 'Command exited with return code {retcode}'.format(retcode=sub_process.returncode)
             )
 
             if sub_process.returncode:
-                raise Failure(description='[bash][{name}] Bash command failed'.format(name=name))
-
-        return line
+                raise Failure(
+                    description='Bash command failed {command}'.format(command=bash_command)
+                )
+        return res
 
     return _bash_solid
 
 
-def bash_script_solid(bash_script_path, name=None, output_encoding=None):
-    '''run a bash command for a script file.
+def bash_script_solid(
+    bash_script_path, name='bash_solid', input_defs=None, output_encoding='utf-8', **kwargs
+):
+    '''This function is a factory which constructs a solid that will execute a Bash command read
+    from a script file. See :py:func:`bash_command_solid` for more information on usage.
     '''
     check.str_param(bash_script_path, 'bash_script_path')
     # name checked in bash_solid_for_command
     # output_encoding checked in bash_solid_for_command
 
     with open(bash_script_path, 'rb') as f:
-        return bash_command_solid(f.read().decode('utf-8'), name, output_encoding)
+        return bash_command_solid(
+            bash_command=f.read().decode('utf-8'),
+            name=name,
+            input_defs=input_defs,
+            output_encoding=output_encoding,
+            **kwargs
+        )
