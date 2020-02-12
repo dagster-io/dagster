2546a42b69a36d6a1584c7cd7b05fd1700a46336
Ben Gotow <ben@foundry376.com>
2020-02-12 16:36:53 -0600

    Prevent default Gaant options from being mutated when merged with provided options [2148]
    Summary: Fix for https://github.com/dagster-io/dagster/issues/2148
    
    Test Plan: Run tests
    
    Reviewers: alangenfeld, prha, schrockn
    
    Subscribers:

--- js_modules/dagit/src/gaant/GaantChart.tsx
+++ js_modules/dagit/src/gaant/GaantChart.tsx
@@ -146,7 +146,7 @@ export class GaantChart extends React.Component<
 
     this.state = {
       query: "*",
-      options: Object.assign(DEFAULT_OPTIONS, props.options)
+      options: Object.assign({}, DEFAULT_OPTIONS, props.options)
     };
   }
 
