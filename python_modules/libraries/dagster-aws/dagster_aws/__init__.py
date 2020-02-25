e13575dc5fedfd3f5dde0eeef4cf290179340a18
David Wallace <david.wallace@goodeggs.com>
2020-02-25 12:51:15 -0800

    make S3FileHandle importable

--- python_modules/libraries/dagster-aws/dagster_aws/__init__.py
+++ python_modules/libraries/dagster-aws/dagster_aws/__init__.py
@@ -1,5 +1,6 @@
 from .s3.compute_log_manager import S3ComputeLogManager
 from .s3.file_cache import S3FileCache
+from .s3.file_manager import S3FileHandle
 from .s3.resources import S3Resource, s3_resource
 from .s3.s3_fake_resource import S3FakeSession, create_s3_fake_resource
 from .s3.solids import S3Coordinate
@@ -15,5 +16,6 @@ __all__ = [
     'S3Coordinate',
     'S3FakeSession',
     'S3FileCache',
+    'S3FileHandle',
     'S3Resource',
 ]
