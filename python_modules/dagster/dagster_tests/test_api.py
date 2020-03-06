a4eda2334650325bf5e9fd914bec39081f290acf
Max Gasner <max.gasner@gmail.com>
2020-03-06 14:00:06 -0800

    Fix __all__
    Summary: Make sure public API is consistent
    
    Test Plan: N/A
    
    Reviewers: alangenfeld, schrockn
    
    Reviewed By: schrockn
    
    Differential Revision: https://dagster.phacility.com/D2213

---
+++ python_modules/dagster/dagster_tests/test_api.py
@@ -0,0 +1,12 @@
+import inspect
+
+import dagster
+
+
+def test_all():
+    dagster_dir = dir(dagster)
+    for each in dagster.__all__:
+        assert each in dagster_dir
+    for exported in dagster_dir:
+        if not exported.startswith('_') and not inspect.ismodule(getattr(dagster, exported)):
+            assert exported in dagster.__all__
