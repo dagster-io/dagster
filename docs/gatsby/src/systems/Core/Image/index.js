6574cc09216417f89e4fd726e3870db74b6cab6b
Max Gasner <max.gasner@gmail.com>
2020-03-06 13:11:43 -0800

    Adds a drop shadow to our modal images
    Test Plan: Manual
    
    Reviewers: sashank
    
    Reviewed By: sashank
    
    Differential Revision: https://dagster.phacility.com/D2212

--- docs/gatsby/src/systems/Core/Image/index.js
+++ docs/gatsby/src/systems/Core/Image/index.js
@@ -4,21 +4,33 @@ import Img from "gatsby-image";
 import styleToObj from "style-to-object";
 import ModalImage from "react-modal-image";
 
+export const imgStyle = {
+  border: "1px solid #333",
+  "-webkit-box-shadow": "5px 5px 5px 0px rgba(0, 0, 0, 0.75)",
+  "-moz-box-shadow": "5px 5px 5px 0px rgba(0, 0, 0, 0.75)",
+  boxShadow: "5px 5px 5px 0px rgba(0, 0, 0, 0.75)"
+};
+
 export const Image = ({ src, width = "100%", images, style, ...props }) => {
   const img = images.find(({ node }) => node.absolutePath.includes(src));
-  const st = typeof style === "string" ? styleToObj(style) : style;
+  const st = {
+    ...imgStyle,
+    ...(typeof style === "string" ? styleToObj(style) : style)
+  };
 
-  return img ? (
-    <ModalImage
-      hideZoom
-      imageBackgroundColor="transparent"
-      small={img.node.childImageSharp.fluid.src}
-      smallSrcSet={img.node.childImageSharp.fluid.srcSet}
-      large={img.node.childImageSharp.original.src}
-      sx={{ width }}
-      style={st}
-    />
-  ) : (
-    <img src={src} {...props} alt={props.alt} style={st} />
-  );
+  if (img) {
+    return (
+      <ModalImage
+        hideZoom
+        imageBackgroundColor="transparent"
+        small={img.node.childImageSharp.fluid.src}
+        smallSrcSet={img.node.childImageSharp.fluid.srcSet}
+        large={img.node.childImageSharp.original.src}
+        sx={{ width, ...st }}
+      />
+    );
+  } else {
+    props.style = { ...imgStyle, ...props.style };
+    return <img src={src} {...props} alt={props.alt} style={st} />;
+  }
 };
