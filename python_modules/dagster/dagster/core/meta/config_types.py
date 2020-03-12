5ed8e8c73490c5a09e252a700e5b502ffbd93be0
Nicholas Schrock <schrockn@elementl.com>
2020-03-12 14:11:09 -0700

    (config-type-database-1) Add ConfigSchemaSnapshot
    Summary:
    This is step one in the process to allow dagit to serve graphql requests
    for pipeline metadata from a serialized format, rather than querying
    the definition objects directly.
    
    What this does is to construct a "snapshot" of metadata objects
    representing the config schema.
    
    The `ConfigTypeSnapshot` class itself is serialiable
    This diff does this via a couple steps:
    
    1) Revive the config type meta classes (this is what will be
    serialized) but change them to refer to other types by key only rather than the
    existing complicated scenario where we expand closed generic types inline.
    
    Instead we will list out all closed generic types in the top-level config dictionary
    just like all the other normal types, simplifying this considerably.
    
    2) Create a `ConfigSchemaSnapshot` class and a factory function that takes a
    pipeline definition and construct all the config types available in all
    modes.
    
    Test Plan: BK
    
    Reviewers: alangenfeld, sashank, themissinghlink
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D2238

--- python_modules/dagster/dagster/core/meta/config_types.py
+++ python_modules/dagster/dagster/core/meta/config_types.py
@@ -3,13 +3,36 @@ from collections import namedtuple
 from dagster import check
 from dagster.config.config_type import ConfigType, ConfigTypeKind
 from dagster.config.field import Field
+from dagster.core.definitions.pipeline import PipelineDefinition
 from dagster.core.serdes import whitelist_for_serdes
+from dagster.utils import merge_dicts
+
+
+def build_config_schema_snapshot(pipeline_def):
+    check.inst_param(pipeline_def, 'pipeline_def', PipelineDefinition)
+    all_config_metas_by_key = {}
+    for mode in pipeline_def.available_modes:
+        environment_schema = pipeline_def.get_environment_schema(mode)
+        all_config_metas_by_key = merge_dicts(
+            all_config_metas_by_key,
+            {ct.key: meta_from_config_type(ct) for ct in environment_schema.all_config_types()},
+        )
+
+    return ConfigSchemaSnapshot(all_config_metas_by_key)
 
 
 @whitelist_for_serdes
-class NonGenericTypeRefMeta(namedtuple('_NonGenericTypeRefMeta', 'key')):
-    def __new__(cls, key):
-        return super(NonGenericTypeRefMeta, cls).__new__(cls, check.str_param(key, 'key'))
+class ConfigSchemaSnapshot(namedtuple('_ConfigSchemaSnapshot', 'all_config_metas_by_key')):
+    def __new__(cls, all_config_metas_by_key):
+        return super(ConfigSchemaSnapshot, cls).__new__(
+            cls,
+            all_config_metas_by_key=check.dict_param(
+                all_config_metas_by_key,
+                'all_config_metas_by_key',
+                key_type=str,
+                value_type=ConfigTypeMeta,
+            ),
+        )
 
 
 @whitelist_for_serdes
@@ -17,22 +40,22 @@ class ConfigTypeMeta(
     namedtuple(
         '_ConfigTypeMeta',
         'kind key given_name description '
-        'type_param_refs '  # only valid for closed generics (Set, Tuple, List, Optional)
+        'type_param_keys '  # only valid for closed generics (Set, Tuple, List, Optional)
         'enum_values '  # only valid for enums
         'fields',  # only valid for dicts and selectors
     )
 ):
     def __new__(
-        cls, kind, key, given_name, type_param_refs, enum_values, fields, description,
+        cls, kind, key, given_name, type_param_keys, enum_values, fields, description,
     ):
         return super(ConfigTypeMeta, cls).__new__(
             cls,
             kind=check.inst_param(kind, 'kind', ConfigTypeKind),
             key=check.str_param(key, 'key'),
             given_name=check.opt_str_param(given_name, 'given_name'),
-            type_param_refs=None
-            if type_param_refs is None
-            else check.list_param(type_param_refs, 'type_param_refs', of_type=TypeRef),
+            type_param_keys=None
+            if type_param_keys is None
+            else check.list_param(type_param_keys, 'type_param_keys', of_type=str),
             enum_values=None
             if enum_values is None
             else check.list_param(enum_values, 'enum_values', of_type=ConfigEnumValueMeta),
@@ -43,63 +66,29 @@ class ConfigTypeMeta(
         )
 
     @property
-    def inner_type_refs(self):
-        '''
-        This recurses through the type references with non-generic types as leaves.
-        '''
+    def inner_type_key(self):
+        # valid for Noneable and Array
+        check.invariant(self.kind == ConfigTypeKind.NONEABLE or self.kind == ConfigTypeKind.ARRAY)
+        check.invariant(len(self.type_param_keys) == 1)
+        return self.type_param_keys[0]
 
-        def _doit():
-            next_level_refs = _get_next_level_refs(self)
-            if next_level_refs:
-                for next_level in next_level_refs:
-                    for inner_ref in _recurse_through_generics(next_level):
-                        yield inner_ref
+    def get_field(self, name):
+        check.str_param(name, 'name')
+        check.invariant(ConfigTypeKind.has_fields(self.kind))
 
-        # there might be duplicate keys (esp for scalars)
-        refs_by_key = {}
+        for f in self.fields:
+            if f.name == name:
+                return f
 
-        for ref in _doit():
-            if ref.key not in refs_by_key:
-                refs_by_key[ref.key] = ref
+        check.failed('Field {name} not found'.format(name=name))
 
-        return list(refs_by_key.values())
-
-
-# This function is used by the recursive descent
-# through all the inner types. This does *not*
-# recursively descend through the type parameters
-# of generic types. It just gets the next level of
-# types. Either the direct type parameters of a
-# generic type. Or the type refs of all the fields
-# if it is a type with fields.
-def _get_next_level_refs(ref):
-    # if a generic type, get type params
-    # if a type with fields, get refs of the fields
-    if ConfigTypeKind.is_closed_generic(ref.kind):
-        return ref.type_param_refs
-    elif (
-        ConfigTypeKind.has_fields(ref.kind) and ref.fields
-    ):  # still check fields because permissive
-        return [field_meta.type_ref for field_meta in ref.fields]
-
-
-def _recurse_through_generics(ref):
-    yield ref
-    if isinstance(ref, ConfigTypeMeta) and ConfigTypeKind.is_closed_generic(ref.kind):
-        for type_param_ref in ref.type_param_refs:
-            for inner_ref in _recurse_through_generics(type_param_ref):
-                yield inner_ref
-
-
-# A type reference in these serializable data structures are one of two things
-# 1) A closed generic type (e.g. List[Int] of Optional[Set[str]])
-# 2) Or a reference to a non-generic type, such as Dict, Selector, or a Scalar.
-# Upon deserialization and when hydrated back to the graphql query, it will
-# be the responsibility of that module to maintain a dictionary of the
-# non-generic types and then do lookups into the dictionary in order to
-# to explode the entire type hierarchy requested by the client
-
-TypeRef = (ConfigTypeMeta, NonGenericTypeRefMeta)
+    def get_child_type_keys(self):
+        if ConfigTypeKind.is_closed_generic(self.kind):
+            return self.type_param_keys
+        elif ConfigTypeKind.has_fields(self.kind):
+            return [field.type_key for field in self.fields]
+        else:
+            return []
 
 
 @whitelist_for_serdes
@@ -116,16 +105,16 @@ class ConfigEnumValueMeta(namedtuple('_ConfigEnumValueMeta', 'value description'
 class ConfigFieldMeta(
     namedtuple(
         '_ConfigFieldMeta',
-        'name type_ref is_required default_provided default_value_as_str description',
+        'name type_key is_required default_provided default_value_as_str description',
     )
 ):
     def __new__(
-        cls, name, type_ref, is_required, default_provided, default_value_as_str, description
+        cls, name, type_key, is_required, default_provided, default_value_as_str, description
     ):
         return super(ConfigFieldMeta, cls).__new__(
             cls,
             name=check.opt_str_param(name, 'name'),
-            type_ref=check.inst_param(type_ref, 'type_ref', TypeRef),
+            type_key=check.str_param(type_key, 'type_key'),
             is_required=check.bool_param(is_required, 'is_required'),
             default_provided=check.bool_param(default_provided, 'default_provided'),
             default_value_as_str=check.opt_str_param(default_value_as_str, 'default_value_as_str'),
@@ -138,24 +127,12 @@ def meta_from_field(name, field):
     check.inst_param(field, 'field', Field)
     return ConfigFieldMeta(
         name=name,
-        type_ref=type_ref_of(field.config_type),
+        type_key=field.config_type.key,
         is_required=field.is_required,
         default_provided=field.default_provided,
         default_value_as_str=field.default_value_as_str if field.default_provided else None,
         description=field.description,
     )
-
-
-def type_ref_of(config_type):
-    check.inst_param(config_type, 'config_type', ConfigType)
-    if ConfigTypeKind.is_closed_generic(config_type.kind):
-        return meta_from_config_type(config_type)
-    else:
-        return NonGenericTypeRefMeta(key=config_type.key)
-
-
-def type_refs_of(type_list):
-    return list(map(type_ref_of, type_list)) if type_list is not None else None
 
 
 def meta_from_config_type(config_type):
@@ -165,7 +142,9 @@ def meta_from_config_type(config_type):
         given_name=config_type.given_name,
         kind=config_type.kind,
         description=config_type.description,
-        type_param_refs=type_refs_of(config_type.type_params),
+        type_param_keys=[ct.key for ct in config_type.type_params]
+        if config_type.type_params
+        else None,
         enum_values=[
             ConfigEnumValueMeta(ev.config_value, ev.description) for ev in config_type.enum_values
         ]
--- python_modules/dagster/dagster_tests/core_tests/meta_tests/test_config_type_metas.py
+++ python_modules/dagster/dagster_tests/core_tests/meta_tests/test_config_type_metas.py
@@ -1,9 +1,18 @@
-from dagster import Array, Field, Noneable, Selector, Shape
+from dagster import (
+    Array,
+    Field,
+    ModeDefinition,
+    Noneable,
+    Selector,
+    Shape,
+    pipeline,
+    resource,
+    solid,
+)
 from dagster.config.field import resolve_to_config_type
 from dagster.core.meta.config_types import (
     ConfigTypeKind,
-    ConfigTypeMeta,
-    NonGenericTypeRefMeta,
+    build_config_schema_snapshot,
     meta_from_config_type,
 )
 from dagster.core.serdes import deserialize_json_to_dagster_namedtuple, serialize_dagster_namedtuple
@@ -18,7 +27,6 @@ def test_basic_int_meta():
     assert int_meta.given_name == 'Int'
     assert int_meta.key == 'Int'
     assert int_meta.kind == ConfigTypeKind.SCALAR
-    assert int_meta.inner_type_refs == []
     assert int_meta.enum_values is None
     assert int_meta.fields is None
 
@@ -27,10 +35,11 @@ def test_basic_dict():
     dict_meta = meta_from_dagster_type({'foo': int})
     assert dict_meta.key.startswith('Shape.')
     assert dict_meta.given_name is None
-    assert dict_meta.inner_type_refs
-    assert len(dict_meta.inner_type_refs) == 1
-    assert dict_meta.inner_type_refs[0].key == 'Int'
-    assert dict_meta.enum_values is None
+    child_type_keys = dict_meta.get_child_type_keys()
+    assert child_type_keys
+    assert len(child_type_keys) == 1
+    assert child_type_keys[0] == 'Int'
+    assert child_type_keys[0]
 
     assert dict_meta.fields and len(dict_meta.fields) == 1
 
@@ -66,20 +75,20 @@ def test_field_things():
 def test_basic_list():
     list_meta = meta_from_dagster_type(Array(int))
     assert list_meta.key.startswith('Array')
-    assert list_meta.inner_type_refs
-    assert len(list_meta.inner_type_refs) == 1
-    assert list_meta.inner_type_refs[0].key == 'Int'
-    assert list_meta.enum_values is None
+    child_type_keys = list_meta.get_child_type_keys()
+    assert child_type_keys
+    assert len(child_type_keys) == 1
+    assert child_type_keys[0] == 'Int'
 
 
 def test_basic_optional():
     optional_meta = meta_from_dagster_type(Noneable(int))
     assert optional_meta.key.startswith('Noneable')
-    assert optional_meta.inner_type_refs
-    assert len(optional_meta.inner_type_refs) == 1
-    assert optional_meta.inner_type_refs[0].key == 'Int'
-    # https://github.com/dagster-io/dagster/issues/1933
-    # TODO reconcile names
+
+    child_type_keys = optional_meta.get_child_type_keys()
+    assert child_type_keys
+    assert len(child_type_keys) == 1
+    assert child_type_keys[0] == 'Int'
     assert optional_meta.kind == ConfigTypeKind.NONEABLE
     assert optional_meta.enum_values is None
 
@@ -87,21 +96,11 @@ def test_basic_optional():
 def test_basic_list_list():
     list_meta = meta_from_dagster_type([[int]])
     assert list_meta.key.startswith('Array')
-    assert list_meta.inner_type_refs
-    assert len(list_meta.inner_type_refs) == 2
-    refs = {ref.key: ref for ref in list_meta.inner_type_refs}
-    assert (
-        len(refs['Array.Int'].inner_type_refs) == 1
-        and isinstance(refs['Array.Int'], ConfigTypeMeta)
-        and refs['Array.Int'].inner_type_refs[0].key == 'Int'
-    )
-    assert refs['Int'].key == 'Int'
+    child_type_keys = list_meta.get_child_type_keys()
+    assert child_type_keys
+    assert len(child_type_keys) == 1
+    assert child_type_keys[0] == 'Array.Int'
     assert list_meta.enum_values is None
-
-    assert (
-        len(list_meta.type_param_refs) == 1
-        and list_meta.type_param_refs[0].kind == ConfigTypeKind.ARRAY
-    )
 
 
 def test_list_of_dict():
@@ -109,18 +108,10 @@ def test_list_of_dict():
     list_of_dict_meta = meta_from_dagster_type([inner_dict_dagster_type])
 
     assert list_of_dict_meta.key.startswith('Array')
-    assert list_of_dict_meta.inner_type_refs
-    assert len(list_of_dict_meta.inner_type_refs) == 1
-    # Both Shape[...] and str are NonGenericTypeRefMetas in this schema
-    dict_ref = list_of_dict_meta.type_param_refs[0]
-    assert isinstance(dict_ref, NonGenericTypeRefMeta)
-    assert dict_ref.key.startswith('Shape')
-
-    assert (
-        len(list_of_dict_meta.type_param_refs) == 1
-        and list_of_dict_meta.type_param_refs[0].key
-        == resolve_to_config_type(inner_dict_dagster_type).key
-    )
+    child_type_keys = list_of_dict_meta.get_child_type_keys()
+    assert child_type_keys
+    assert len(child_type_keys) == 1
+    assert child_type_keys[0].startswith('Shape')
 
 
 def test_selector_of_things():
@@ -130,7 +121,7 @@ def test_selector_of_things():
     assert selector_meta.fields and len(selector_meta.fields) == 1
     field_meta = selector_meta.fields[0]
     assert field_meta.name == 'bar'
-    assert field_meta.type_ref.key == 'Int'
+    assert field_meta.type_key == 'Int'
 
 
 def test_kitchen_sink():
@@ -156,29 +147,151 @@ def test_kitchen_sink():
     assert kitchen_sink_meta == rehydrated_meta
 
 
+def test_simple_pipeline_smoke_test():
+    @solid
+    def solid_without_config(_):
+        pass
+
+    @pipeline
+    def single_solid_pipeline():
+        solid_without_config()
+
+    config_schema_snapshot = build_config_schema_snapshot(single_solid_pipeline)
+    assert config_schema_snapshot.all_config_metas_by_key
+
+    serialized = serialize_dagster_namedtuple(config_schema_snapshot)
+    rehydrated_config_schema_snapshot = deserialize_json_to_dagster_namedtuple(serialized)
+    assert config_schema_snapshot == rehydrated_config_schema_snapshot
+
+
+def test_check_solid_config_correct():
+    @solid(config={'foo': str})
+    def solid_with_config(_):
+        pass
+
+    @pipeline
+    def single_solid_pipeline():
+        solid_with_config()
+
+    solid_config_key = solid_with_config.config_field.config_type.key
+
+    config_metas = build_config_schema_snapshot(single_solid_pipeline).all_config_metas_by_key
+
+    assert solid_config_key in config_metas
+
+    solid_config_meta = config_metas[solid_config_key]
+
+    assert solid_config_meta.kind == ConfigTypeKind.STRICT_SHAPE
+    assert len(solid_config_meta.fields) == 1
+
+    foo_field = solid_config_meta.fields[0]
+
+    assert foo_field.name == 'foo'
+    assert foo_field.type_key == 'String'
+
+
+def test_check_solid_list_list_config_correct():
+    @solid(config={'list_list_int': [[{'bar': int}]]})
+    def solid_with_config(_):
+        pass
+
+    @pipeline
+    def single_solid_pipeline():
+        solid_with_config()
+
+    solid_config_key = solid_with_config.config_field.config_type.key
+
+    config_metas = build_config_schema_snapshot(single_solid_pipeline).all_config_metas_by_key
+    assert solid_config_key in config_metas
+    solid_config_meta = config_metas[solid_config_key]
+
+    assert solid_config_meta.kind == ConfigTypeKind.STRICT_SHAPE
+    assert len(solid_config_meta.fields) == 1
+
+    list_list_field = solid_config_meta.fields[0]
+
+    list_list_type_key = list_list_field.type_key
+
+    assert list_list_type_key.startswith('Array.Array.')
+
+    list_list_type = config_metas[list_list_type_key]
+
+    assert list_list_type.kind == ConfigTypeKind.ARRAY
+    list_meta = config_metas[list_list_type.inner_type_key]
+    assert list_meta.kind == ConfigTypeKind.ARRAY
+    assert config_metas[list_meta.inner_type_key].kind == ConfigTypeKind.STRICT_SHAPE
+
+
 def test_kitchen_sink_break_out():
-    nested_dict_cls = resolve_to_config_type(
-        {
-            'list_list': [[int]],
-            'nested_selector': Selector({'some_field': int, 'list': Noneable([bool])}),
-        }
+    @solid(
+        config=[
+            {
+                'opt_list_of_int': Field([int], is_required=False),
+                'nested_dict': {
+                    'list_list': [[int]],
+                    'nested_selector': Selector(
+                        {'some_field': int, 'noneable_list': Noneable([bool])}
+                    ),
+                },
+            }
+        ]
     )
-    dict_within_list_cls = resolve_to_config_type(
-        {'opt_list_of_int': Field([int], is_required=False), 'nested_dict': Field(nested_dict_cls)}
-    )
-    kitchen_sink = Array(dict_within_list_cls)
+    def solid_with_kitchen_sink_config(_):
+        pass
 
-    dict_within_list_key = dict_within_list_cls.key
-    kitchen_sink_meta = meta_from_dagster_type(kitchen_sink)
+    @pipeline
+    def single_solid_pipeline():
+        solid_with_kitchen_sink_config()
 
-    assert len(kitchen_sink_meta.type_param_refs) == 1
-    assert kitchen_sink_meta.type_param_refs[0].key == dict_within_list_key
-    assert len(kitchen_sink_meta.inner_type_refs) == 1
-    assert kitchen_sink_meta.inner_type_refs[0].key == dict_within_list_key
-    dict_within_list_meta = meta_from_dagster_type(dict_within_list_cls)
-    assert dict_within_list_meta.type_param_refs is None
-    # List[int], Int, Shape.XXX
-    assert len(dict_within_list_meta.inner_type_refs) == 3
-    assert sorted([type_ref.key for type_ref in dict_within_list_meta.inner_type_refs]) == sorted(
-        [nested_dict_cls.key, 'Int', 'Array.Int']
+    config_metas = build_config_schema_snapshot(single_solid_pipeline).all_config_metas_by_key
+
+    solid_config_key = solid_with_kitchen_sink_config.config_field.config_type.key
+    assert solid_config_key in config_metas
+    solid_config_meta = config_metas[solid_config_key]
+
+    assert solid_config_meta.kind == ConfigTypeKind.ARRAY
+
+    dict_within_list = config_metas[solid_config_meta.inner_type_key]
+
+    assert len(dict_within_list.fields) == 2
+
+    opt_field = dict_within_list.get_field('opt_list_of_int')
+
+    assert opt_field.is_required is False
+    assert config_metas[opt_field.type_key].kind == ConfigTypeKind.ARRAY
+
+    nested_dict = config_metas[dict_within_list.get_field('nested_dict').type_key]
+    assert len(nested_dict.fields) == 2
+    nested_selector = config_metas[nested_dict.get_field('nested_selector').type_key]
+    noneable_list_bool = config_metas[nested_selector.get_field('noneable_list').type_key]
+    assert noneable_list_bool.kind == ConfigTypeKind.NONEABLE
+    list_bool = config_metas[noneable_list_bool.inner_type_key]
+    assert list_bool.kind == ConfigTypeKind.ARRAY
+
+
+def test_multiple_modes():
+    @solid
+    def noop_solid(_):
+        pass
+
+    @resource(config={'a': int})
+    def a_resource(_):
+        pass
+
+    @resource(config={'b': int})
+    def b_resource(_):
+        pass
+
+    @pipeline(
+        mode_defs=[
+            ModeDefinition(name='mode_a', resource_defs={'resource': a_resource}),
+            ModeDefinition(name='mode_b', resource_defs={'resource': b_resource}),
+        ]
     )
+    def modez():
+        noop_solid()
+
+    config_metas = build_config_schema_snapshot(modez).all_config_metas_by_key
+
+    assert a_resource.config_field.config_type.key in config_metas
+    assert b_resource.config_field.config_type.key in config_metas
