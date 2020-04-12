2df9a8d04c5f2802b3f3bd0e10e40b8353a15613
Nicholas Schrock <schrockn@elementl.com>
2020-04-12 11:24:53 -0700

    (add-snapshot-persistence-7) Refresh all snapshots
    Summary:
    It's good to occasionally refresh snapshot tests by blowing
    away the folder to get rid of old tests etc.
    
    Depends on D2535
    
    Test Plan: BK
    
    Reviewers: max, sashank, themissinghlink
    
    Reviewed By: themissinghlink
    
    Differential Revision: https://dagster.phacility.com/D2534

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_execution_plan.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_execution_plan.py
@@ -6,131 +6,6 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_successful_two_part_execute_plan 1'] = {
-    'executePlan': {
-        '__typename': 'ExecutePlanSuccess',
-        'hasFailures': False,
-        'pipeline': {
-            'name': 'csv_hello_world'
-        },
-        'stepEvents': [
-            {
-                '__typename': 'ExecutionStepStartEvent',
-                'message': 'Started execution of step "sum_solid.compute".',
-                'step': {
-                    'key': 'sum_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ExecutionStepInputEvent',
-                'message': 'Got input "num" of type "PoorMansDataFrame". (Type check passed).',
-                'step': {
-                    'key': 'sum_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ExecutionStepOutputEvent',
-                'message': 'Yielded output "result" of type "PoorMansDataFrame". (Type check passed).',
-                'outputName': 'result',
-                'step': {
-                    'key': 'sum_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ObjectStoreOperationEvent',
-                'message': 'Stored intermediate object for output result in filesystem object store using pickle.',
-                'step': {
-                    'key': 'sum_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ExecutionStepSuccessEvent',
-                'message': 'Finished execution of step "sum_solid.compute" in {N}ms.',
-                'step': {
-                    'key': 'sum_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            }
-        ]
-    }
-}
-
-snapshots['test_successful_two_part_execute_plan 2'] = {
-    'executePlan': {
-        '__typename': 'ExecutePlanSuccess',
-        'hasFailures': False,
-        'pipeline': {
-            'name': 'csv_hello_world'
-        },
-        'stepEvents': [
-            {
-                '__typename': 'ExecutionStepStartEvent',
-                'message': 'Started execution of step "sum_sq_solid.compute".',
-                'step': {
-                    'key': 'sum_sq_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ObjectStoreOperationEvent',
-                'message': 'Retrieved intermediate object for input sum_df in filesystem object store using pickle.',
-                'step': {
-                    'key': 'sum_sq_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ExecutionStepInputEvent',
-                'message': 'Got input "sum_df" of type "PoorMansDataFrame". (Type check passed).',
-                'step': {
-                    'key': 'sum_sq_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ExecutionStepOutputEvent',
-                'message': 'Yielded output "result" of type "PoorMansDataFrame". (Type check passed).',
-                'outputName': 'result',
-                'step': {
-                    'key': 'sum_sq_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ObjectStoreOperationEvent',
-                'message': 'Stored intermediate object for output result in filesystem object store using pickle.',
-                'step': {
-                    'key': 'sum_sq_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            },
-            {
-                '__typename': 'ExecutionStepSuccessEvent',
-                'message': 'Finished execution of step "sum_sq_solid.compute" in {N}ms.',
-                'step': {
-                    'key': 'sum_sq_solid.compute',
-                    'metadata': [
-                    ]
-                }
-            }
-        ]
-    }
-}
-
 snapshots['test_success_whole_execution_plan 1'] = {
     'executePlan': {
         '__typename': 'ExecutePlanSuccess',
@@ -493,6 +368,131 @@ snapshots['test_successful_one_part_execute_plan 1'] = {
                 'message': 'Finished execution of step "sum_solid.compute" in {N}ms.',
                 'step': {
                     'key': 'sum_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            }
+        ]
+    }
+}
+
+snapshots['test_successful_two_part_execute_plan 1'] = {
+    'executePlan': {
+        '__typename': 'ExecutePlanSuccess',
+        'hasFailures': False,
+        'pipeline': {
+            'name': 'csv_hello_world'
+        },
+        'stepEvents': [
+            {
+                '__typename': 'ExecutionStepStartEvent',
+                'message': 'Started execution of step "sum_solid.compute".',
+                'step': {
+                    'key': 'sum_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ExecutionStepInputEvent',
+                'message': 'Got input "num" of type "PoorMansDataFrame". (Type check passed).',
+                'step': {
+                    'key': 'sum_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ExecutionStepOutputEvent',
+                'message': 'Yielded output "result" of type "PoorMansDataFrame". (Type check passed).',
+                'outputName': 'result',
+                'step': {
+                    'key': 'sum_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ObjectStoreOperationEvent',
+                'message': 'Stored intermediate object for output result in filesystem object store using pickle.',
+                'step': {
+                    'key': 'sum_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ExecutionStepSuccessEvent',
+                'message': 'Finished execution of step "sum_solid.compute" in {N}ms.',
+                'step': {
+                    'key': 'sum_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            }
+        ]
+    }
+}
+
+snapshots['test_successful_two_part_execute_plan 2'] = {
+    'executePlan': {
+        '__typename': 'ExecutePlanSuccess',
+        'hasFailures': False,
+        'pipeline': {
+            'name': 'csv_hello_world'
+        },
+        'stepEvents': [
+            {
+                '__typename': 'ExecutionStepStartEvent',
+                'message': 'Started execution of step "sum_sq_solid.compute".',
+                'step': {
+                    'key': 'sum_sq_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ObjectStoreOperationEvent',
+                'message': 'Retrieved intermediate object for input sum_df in filesystem object store using pickle.',
+                'step': {
+                    'key': 'sum_sq_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ExecutionStepInputEvent',
+                'message': 'Got input "sum_df" of type "PoorMansDataFrame". (Type check passed).',
+                'step': {
+                    'key': 'sum_sq_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ExecutionStepOutputEvent',
+                'message': 'Yielded output "result" of type "PoorMansDataFrame". (Type check passed).',
+                'outputName': 'result',
+                'step': {
+                    'key': 'sum_sq_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ObjectStoreOperationEvent',
+                'message': 'Stored intermediate object for output result in filesystem object store using pickle.',
+                'step': {
+                    'key': 'sum_sq_solid.compute',
+                    'metadata': [
+                    ]
+                }
+            },
+            {
+                '__typename': 'ExecutionStepSuccessEvent',
+                'message': 'Finished execution of step "sum_sq_solid.compute" in {N}ms.',
+                'step': {
+                    'key': 'sum_sq_solid.compute',
                     'metadata': [
                     ]
                 }
