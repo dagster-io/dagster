8703baa4805fc12731edfe64803a02f5983a60db
Sashank Thupukari <s@thupukari.com>
2020-04-23 15:17:22 -0400

    Add launch_pipeline_execution_for_created_run
    Test Plan: test coverage is in next diff
    
    Reviewers: max, yuhan
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D2666

--- python_modules/dagster-graphql/dagster_graphql/implementation/execution/launch_execution.py
+++ python_modules/dagster-graphql/dagster_graphql/implementation/execution/launch_execution.py
@@ -1,10 +1,16 @@
 from __future__ import absolute_import
 
+from dagster_graphql.schema.errors import DauphinPipelineConfigValidationInvalid
 from graphql.execution.base import ResolveInfo
 
 from dagster import check
+from dagster.config.validate import validate_config
+from dagster.core.definitions.environment_schema import create_environment_schema
+from dagster.core.errors import DagsterInvalidConfigError
+from dagster.core.events import EngineEventData
 from dagster.core.execution.api import create_execution_plan
 from dagster.core.snap.execution_plan_snapshot import snapshot_from_execution_plan
+from dagster.utils.error import SerializableErrorInfo
 
 from ..fetch_pipelines import get_pipeline_def_from_selector
 from ..fetch_runs import get_validated_config
@@ -70,6 +76,63 @@ def _launch_pipeline_execution(graphene_info, execution_params, is_reexecuted=Fa
 
     run = instance.launch_run(pipeline_run)
 
+    return graphene_info.schema.type_named('LaunchPipelineRunSuccess')(
+        run=graphene_info.schema.type_named('PipelineRun')(run)
+    )
+
+
+def _launch_pipeline_execution_for_created_run(graphene_info, run_id):
+    check.inst_param(graphene_info, 'graphene_info', ResolveInfo)
+    check.str_param(run_id, 'run_id')
+
+    # First retrieve the pipeline run
+    instance = graphene_info.context.instance
+    pipeline_run = instance.get_run_by_id(run_id)
+    if not pipeline_run:
+        return graphene_info.schema.type_named('PipelineRunNotFoundError')(run_id)
+
+    pipeline_def = get_pipeline_def_from_selector(graphene_info, pipeline_run.selector)
+
+    # Run config valudation
+    # If there are any config errors, then inject them into the event log
+    environment_schema = create_environment_schema(pipeline_def, pipeline_run.mode)
+    validated_config = validate_config(
+        environment_schema.environment_type, pipeline_run.environment_dict
+    )
+    if not validated_config.success:
+        # If the config is invalid, we construct a DagsterInvalidConfigError exception and
+        # insert it into the event log. We also return a PipelineConfigValidationInvalid user facing
+        # graphql error.
+
+        # We currently re-use the engine events machinery to add the error to the event log, but
+        # may need to create a new event type and instance method to handle these erros.
+        invalid_config_exception = DagsterInvalidConfigError(
+            'Error in config for pipeline {}'.format(pipeline_def.name),
+            validated_config.errors,
+            pipeline_run.environment_dict,
+        )
+
+        instance.report_engine_event(
+            str(invalid_config_exception.message),
+            pipeline_run,
+            EngineEventData.engine_error(
+                SerializableErrorInfo(
+                    invalid_config_exception.message,
+                    [],
+                    DagsterInvalidConfigError.__class__.__name__,
+                    None,
+                )
+            ),
+        )
+
+        # TODO: also insert a pipeline init failure event
+        # https://github.com/dagster-io/dagster/issues/2385
+
+        return DauphinPipelineConfigValidationInvalid.for_validation_errors(
+            pipeline_def, validated_config.errors
+        )
+
+    run = instance.launch_run(pipeline_run)
     return graphene_info.schema.type_named('LaunchPipelineRunSuccess')(
         run=graphene_info.schema.type_named('PipelineRun')(run)
     )
