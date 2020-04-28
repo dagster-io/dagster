31c79b88068f0d404368eba734b659c0f5291b8a
Max Gasner <max.gasner@gmail.com>
2020-04-28 10:33:08 -0700

    Require requests (for telemetry)
    Test Plan: Manual - pip install dagster in a new python Docker image
    
    Reviewers: catherinewu, schrockn
    
    Reviewed By: catherinewu, schrockn
    
    Differential Revision: https://dagster.phacility.com/D2710

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -77,6 +77,7 @@ def _do_setup(name='dagster'):
             'gevent',
             'pyrsistent>=0.14.8',
             'python-dateutil',
+            'requests',
             'rx<=1.6.1',  # 3.0 was a breaking change. No py2 compatability as well.
             'six',
             'tqdm',
