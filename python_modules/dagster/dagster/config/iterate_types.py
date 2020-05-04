e1bc8e9faaa903e823c23794bc694ae7fc14ec33
Nicholas Schrock <schrockn@elementl.com>
2020-05-04 07:57:25 -0700

    (external-config-schema-7) Eliminate list iteration in iterate_config_types
    Summary:
    This never made much sense and appears to be an unexercised
    code path
    
    Depeonds on D2760
    
    Test Plan: BK
    
    Reviewers: sashank, alangenfeld
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D2762

--- python_modules/dagster/dagster/config/iterate_types.py
+++ python_modules/dagster/dagster/config/iterate_types.py
@@ -4,14 +4,6 @@ from .config_type import ConfigType, ConfigTypeKind
 
 
 def iterate_config_types(config_type):
-    # TODO: Investigate during config refactor. Very sketchy -- schrockn 12/05/19
-    # Looping over this should be done at callsites
-    if isinstance(config_type, list):
-        check.list_param(config_type, 'config_type', of_type=ConfigType)
-        for config_type_item in config_type:
-            for inner_type in iterate_config_types(config_type_item):
-                yield inner_type
-
     check.inst_param(config_type, 'config_type', ConfigType)
     if config_type.kind == ConfigTypeKind.ARRAY or config_type.kind == ConfigTypeKind.NONEABLE:
         for inner_type in iterate_config_types(config_type.inner_type):
