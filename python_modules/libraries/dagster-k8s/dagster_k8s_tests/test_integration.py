ba9fe778d24a378e785107935f1364daba449147
Nicholas Schrock <schrockn@elementl.com>
2020-05-11 15:17:32 -0700

    Readd disabled celery test
    Summary:
    Readding celery test that was disabled in D2834
    
    https://github.com/dagster-io/dagster/issues/2439
    
    Test Plan: BK
    
    Reviewers: alangenfeld, nate
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D2871

--- python_modules/libraries/dagster-k8s/dagster_k8s_tests/test_integration.py
+++ python_modules/libraries/dagster-k8s/dagster_k8s_tests/test_integration.py
@@ -1,8 +1,10 @@
 import os
 
 import pytest
+from dagster_k8s import get_celery_engine_config
 
-from dagster.utils import load_yaml_from_path
+from dagster.utils import load_yaml_from_path, merge_dicts
+from dagster.utils.yaml_utils import merge_yamls
 
 from .test_project import test_project_environments_path
 from .utils import wait_for_job_and_get_logs
@@ -34,46 +36,40 @@ def test_k8s_run_launcher_default(dagster_instance, helm_namespace):
 
 
 @pytest.mark.integration
-@pytest.mark.skip('https://github.com/dagster-io/dagster/issues/2439')
 def test_k8s_run_launcher_celery(dagster_instance, helm_namespace):
-    assert dagster_instance
-    assert helm_namespace
-    # from dagster_k8s import get_celery_engine_config
 
-    # from dagster.utils import load_yaml_from_path, merge_dicts
-    # from dagster.utils.yaml_utils import merge_yamls
-    # environment_dict = merge_dicts(
-    #     merge_yamls(
-    #         [
-    #             os.path.join(test_project_environments_path(), 'env.yaml'),
-    #             os.path.join(test_project_environments_path(), 'env_filesystem.yaml'),
-    #         ]
-    #     ),
-    #     get_celery_engine_config(),
-    # )
+    environment_dict = merge_dicts(
+        merge_yamls(
+            [
+                os.path.join(test_project_environments_path(), 'env.yaml'),
+                os.path.join(test_project_environments_path(), 'env_filesystem.yaml'),
+            ]
+        ),
+        get_celery_engine_config(),
+    )
 
-    # assert 'celery' in environment_dict['execution']
+    assert 'celery' in environment_dict['execution']
 
-    # pipeline_name = 'demo_pipeline_celery'
-    # tags = {'key': 'value'}
-    # run = dagster_instance.create_run(
-    #     pipeline_name=pipeline_name, environment_dict=environment_dict, tags=tags, mode='default'
-    # )
+    pipeline_name = 'demo_pipeline_celery'
+    tags = {'key': 'value'}
+    run = dagster_instance.create_run(
+        pipeline_name=pipeline_name, environment_dict=environment_dict, tags=tags, mode='default'
+    )
 
-    # dagster_instance.launch_run(run.run_id)
-    # result = wait_for_job_and_get_logs(
-    #     job_name='dagster-run-%s' % run.run_id, namespace=helm_namespace
-    # )
+    dagster_instance.launch_run(run.run_id)
+    result = wait_for_job_and_get_logs(
+        job_name='dagster-run-%s' % run.run_id, namespace=helm_namespace
+    )
 
-    # assert not result.get('errors')
-    # assert result['data']
-    # # this is bad test but proves that we got celery configured properly
-    # # to get it working would involve relying on s3 / gcs for storage
-    # assert result['data']['startPipelineExecutionForCreatedRun']['__typename'] == 'PythonError'
-    # assert (
-    #     'Must use S3 or GCS storage with non-local Celery broker: pyamqp://test:test@dagster-rabbitmq:5672// and backend: amqp'
-    #     in result['data']['startPipelineExecutionForCreatedRun']['message']
-    # )
+    assert not result.get('errors')
+    assert result['data']
+    # This is  not an ideal test but proves that we got celery configured properly.
+    # We detect that by seeing an error that we know happens after config.
+    assert result['data']['startPipelineExecutionForCreatedRun']['__typename'] == 'PythonError'
+    assert (
+        'When executing count_letters.compute discovered required outputs missing from previous step'
+        in result['data']['startPipelineExecutionForCreatedRun']['message']
+    )
 
 
 @pytest.mark.integration
