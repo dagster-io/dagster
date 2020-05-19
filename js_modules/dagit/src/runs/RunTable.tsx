ede61a57d1c6a4166eeeb244062443a9e4f0957d
prha <prha@elementl.com>
2020-05-19 16:06:05 -0700

    make sure menu items are each on their own line
    Test Plan: Rendered run action menu on runs page
    
    Reviewers: max, yuhan
    
    Reviewed By: yuhan
    
    Differential Revision: https://dagster.phacility.com/D2998

--- js_modules/dagit/src/runs/RunTable.tsx
+++ js_modules/dagit/src/runs/RunTable.tsx
@@ -383,6 +383,7 @@ const RunActionsMenu: React.FunctionComponent<{
             content={OPEN_PLAYGROUND_UNKNOWN}
             position={Position.BOTTOM}
             disabled={infoReady}
+            wrapperTagName="div"
           >
             <MenuItem
               text="Open in Playground..."
@@ -403,6 +404,7 @@ const RunActionsMenu: React.FunctionComponent<{
             content={REEXECUTE_PIPELINE_UNKNOWN}
             position={Position.BOTTOM}
             disabled={infoReady}
+            wrapperTagName="div"
           >
             <MenuItem
               text="Re-execute"
