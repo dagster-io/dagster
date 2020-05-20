dd7883bdabda52076b1e3558a69c564c3dfc94e7
Nicholas Schrock <schrockn@elementl.com>
2020-05-20 13:28:16 -0700

    (dagster-cli-api-run-launcher-1) Move RunLauncher to base.py
    Summary: Going to introduce a subclass as a peer, so we need to make this not be in `__init__.py`
    
    Test Plan: BK
    
    Reviewers: max, alangenfeld
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D3008

---
+++ python_modules/dagster/dagster/core/launcher/base.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+
+import six
+
+
+class RunLauncher(six.with_metaclass(ABCMeta)):
+    @abstractmethod
+    def launch_run(self, instance, run):
+        '''Launch a run.
+
+        This method should begin the execution of the specified run, and may emit engine events.
+        Runs should be created in the instance (e.g., by calling
+        ``DagsterInstance.create_run()``) *before* this method is called, and
+        should be in the ``PipelineRunStatus.NOT_STARTED`` state. Typically, this method will
+        not be invoked directly, but should be invoked through ``DagsterInstance.launch_run()``.
+
+        Args:
+            instance (DagsterInstance): The instance in which the run has been created.
+            run (PipelineRun): The PipelineRun to launch.
+
+        Returns:
+            PipelineRun: The launched run. This should be in the ``PipelineRunStatus.STARTED``
+                state, or, if a synchronous failure occurs, the ``PipelineRunStatus.FAILURE`` state.
+        '''
