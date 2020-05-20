03a1029f7bbc2e0b55c761e23df936ef060bcf2e
Nicholas Schrock <schrockn@elementl.com>
2020-05-20 13:56:45 -0700

    (dagster-cli-api-run-launcher-3) Add CliApiRunLauncher
    Summary:
    Adds the CliApiRunLauncher. This is largely insprired
    (copied) from the PipelineExecutionManager, which will
    eventually be deleted. This does the same monitoring and
    running of pipelines as that, but uses python.threading
    instead of gevent, subprocess.pOpen instead of multiprocessing, etc.
    
    Depends on D3009
    
    Test Plan: BK
    
    Reviewers: alangenfeld, max, sashank
    
    Differential Revision: https://dagster.phacility.com/D3011

--- python_modules/dagster/dagster/core/launcher/__init__.py
+++ python_modules/dagster/dagster/core/launcher/__init__.py
@@ -1,1 +1,2 @@
 from .base import RunLauncher
+from .cli_api_run_launcher import CliApiRunLauncher
---
+++ python_modules/dagster/dagster/core/launcher/cli_api_run_launcher.py
@@ -0,0 +1,195 @@
+import os
+import threading
+import time
+
+from dagster import check
+from dagster.api.execute_run import cli_api_execute_run
+from dagster.core.host_representation import ExternalPipeline
+from dagster.core.instance import DagsterInstance
+from dagster.core.storage.pipeline_run import PipelineRun
+from dagster.seven.temp_dir import get_system_temp_directory
+
+from .base import RunLauncher
+
+SUBPROCESS_TICK = 0.5
+
+
+def _is_alive(popen):
+    return popen.poll() is None
+
+
+class CliApiRunLauncher(RunLauncher):
+    '''
+    This run launcher launches a new process which invokes
+    the command `dagster api execute_run`.
+    '''
+
+    def __init__(self):
+        self._instance = None
+        self._living_process_by_run_id = {}
+        self._output_files_by_run_id = {}
+        self._processes_lock = None
+        self._thread = None
+
+    def initialize(self, instance):
+        check.inst_param(instance, 'instance', DagsterInstance)
+        check.invariant(self._instance is None, 'Must only call initialize once')
+        self._instance = instance
+
+        self._processes_lock = threading.Lock()
+        self._thread = threading.Thread(target=self._clock, args=())
+        self._thread.daemon = True
+        self._thread.start()
+
+    def _generate_synthetic_error_from_crash(self, run):
+        message = 'Pipeline execution process for {run_id} unexpectedly exited.'.format(
+            run_id=run.run_id
+        )
+        self._instance.report_engine_event(message, run, cls=self.__class__)
+        self._instance.report_run_failed(run)
+
+    def _living_process_snapshot(self):
+        with self._processes_lock:
+            return {run_id: process for run_id, process in self._living_process_by_run_id.items()}
+
+    def _clock(self):
+        '''
+        This function polls the instance to synchronize it with the state of processes managed
+        by this manager instance. On every tick (every 0.5 seconds currently) it checks for zombie
+        processes
+        '''
+        while True:
+            self._check_for_zombies()
+
+            time.sleep(SUBPROCESS_TICK)
+
+    def _check_for_zombies(self):
+        '''
+        Checks the current index of run_id => process and sees if any of them are dead. If they are,
+        it queries the instance to see if the runs are in a proper terminal state (success or
+        failure). If not, then we can assume that the underlying process died unexpected and clean
+        everything. In either case, the dead process is removed from the run_id => process index.
+        '''
+        runs_to_clear = []
+
+        living_process_snapshot = self._living_process_snapshot()
+
+        for run_id, process in living_process_snapshot.items():
+            if not _is_alive(process):
+                run = self._instance.get_run_by_id(run_id)
+                if not run:  # defensive
+                    continue
+
+                runs_to_clear.append(run_id)
+
+                # expected terminal state. it's fine for process to be dead
+                if run.is_finished:
+                    continue
+
+                # the process died in an unexpected manner. inform the system
+                self._generate_synthetic_error_from_crash(run)
+
+        with self._processes_lock:
+            for run_to_clear_id in runs_to_clear:
+                self._delete_process(run_to_clear_id)
+
+    # always call this within lock
+    def _delete_process(self, run_id):
+        del self._living_process_by_run_id[run_id]
+        output_file = self._output_files_by_run_id[run_id]
+        try:
+            if os.path.exists(output_file):
+                os.unlink(output_file)
+        finally:
+            del self._output_files_by_run_id[run_id]
+
+    def launch_run(self, instance, run, external_pipeline=None):
+        '''Subclasses must implement this method.'''
+
+        check.inst_param(instance, 'instance', DagsterInstance)
+        check.inst_param(run, 'run', PipelineRun)
+        check.inst_param(external_pipeline, 'external_pipeline', ExternalPipeline)
+        env_handle = external_pipeline.handle.repository_handle.environment_handle
+        check.param_invariant(
+            env_handle.in_process_origin.repo_yaml,
+            'external_pipeline',
+            'Must come from repo_yaml for now',
+        )
+
+        # initialize when the first run happens
+        if not self._instance:
+            self.initialize(instance)
+
+        output_file = os.path.join(
+            get_system_temp_directory(), 'cli-api-execute-run-{}'.format(run.run_id)
+        )
+
+        process = cli_api_execute_run(
+            output_file=output_file,
+            instance=self._instance,
+            repo_yaml=env_handle.in_process_origin.repo_yaml,
+            pipeline_run=run,
+        )
+
+        with self._processes_lock:
+            self._living_process_by_run_id[run.run_id] = process
+            self._output_files_by_run_id[run.run_id] = output_file
+
+    def join(self):
+        with self._processes_lock:
+            for run_id, process in self._living_process_by_run_id.items():
+                if _is_alive(process):
+                    _stdout, _std_error = process.communicate()
+
+                run = self._instance.get_run_by_id(run_id)
+
+                if run and not run.is_finished:
+                    self._generate_synthetic_error_from_crash(run)
+
+            for output_file in self._output_files_by_run_id.values():
+                if os.path.exists(output_file):
+                    os.unlink(output_file)
+
+    def _get_process(self, run_id):
+        with self._processes_lock:
+            return self._living_process_by_run_id.get(run_id)
+
+    def is_process_running(self, run_id):
+        check.str_param(run_id, 'run_id')
+        process = self._get_process(run_id)
+        return _is_alive(process) if process else False
+
+    def can_terminate(self, run_id):
+        check.str_param(run_id, 'run_id')
+
+        process = self._get_process(run_id)
+
+        if not process:
+            return False
+
+        if not _is_alive(process):
+            return False
+
+        return True
+
+    def terminate(self, run_id):
+        check.str_param(run_id, 'run_id')
+
+        process = self._get_process(run_id)
+
+        if not process:
+            return False
+
+        if not _is_alive(process):
+            return False
+
+        process.kill()
+        return True
+
+    def get_active_run_count(self):
+        with self._processes_lock:
+            return len(self._living_process_by_run_id)
+
+    def is_active(self, run_id):
+        with self._processes_lock:
+            return run_id in self._living_process_by_run_id
