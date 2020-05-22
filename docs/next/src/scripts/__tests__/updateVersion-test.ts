be13b8b3818ecdb9fe300d7cfe49f38b8c198586
yuhan <yuhan@elementl.com>
2020-05-21 23:05:19 -0700

    [docs] deploy 2/ convert versions page to json and update script
    Summary: update script to take json and then update json `updateVersionsJson` + generate mdx `generateVersionsMDX`
    
    Test Plan:
    - unit
    - before and after look identical
    {F143275}
    
    Reviewers: nate, sashank
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D3054

--- docs/next/src/scripts/__tests__/updateVersion-test.ts
+++ docs/next/src/scripts/__tests__/updateVersion-test.ts
@@ -1,47 +1,120 @@
-import { updateVersion } from '../updateVersion';
+import { updateVersionsJson, generateVersionsMDX } from '../updateVersion';
 
-const MOCK_CONTENT = `# Dagster Versions
+const MOCK_JSON_CONTENT = {
+  currentVersion: {
+    version: '0.7.13',
+    documentation: '/docs/install',
+    releaseNotes: 'https://github.com/dagster-io/dagster/releases/tag/0.7.13',
+  },
+  allVersions: [
+    {
+      version: '0.7.13',
+      documentation: '/0.7.13/docs/install',
+      releaseNotes: 'https://github.com/dagster-io/dagster/releases/tag/0.7.13',
+    },
+    {
+      version: '0.7.12',
+      documentation: '/0.7.12/docs/install',
+      releaseNotes: 'https://github.com/dagster-io/dagster/releases/tag/0.7.12',
+    },
+    {
+      version: '0.7.11',
+      documentation: '/0.7.11/docs/install',
+      releaseNotes: 'https://github.com/dagster-io/dagster/releases/tag/0.7.11',
+    },
+    {
+      version: '0.7.10',
+      documentation: '/0.7.10/docs/install',
+      releaseNotes: 'https://github.com/dagster-io/dagster/releases/tag/0.7.10',
+    },
+    {
+      version: '0.7.9',
+      documentation: '/0.7.9/docs/install',
+      releaseNotes: 'https://github.com/dagster-io/dagster/releases/tag/0.7.9',
+    },
+    {
+      version: '0.7.8',
+      documentation: 'https://dagster.readthedocs.io/en/0.7.8/',
+      releaseNotes: 'https://github.com/dagster-io/dagster/releases/tag/0.7.8',
+      note: 'Documentation is on external site',
+    },
+  ],
+};
 
-## Current version (Stable)
+test('updateVersionsJson works', () => {
+  const { oldVersion, newJsonContent } = updateVersionsJson(
+    JSON.stringify(MOCK_JSON_CONTENT),
+    '0.7.100',
+  );
+  expect(oldVersion).toEqual('0.7.13');
+  expect(newJsonContent).toMatchInlineSnapshot(`
+    "{
+        \\"currentVersion\\": {
+            \\"version\\": \\"0.7.100\\",
+            \\"documentation\\": \\"/docs/install\\",
+            \\"releaseNotes\\": \\"https://github.com/dagster-io/dagster/releases/tag/0.7.100\\"
+        },
+        \\"allVersions\\": [
+            {
+                \\"version\\": \\"0.7.100\\",
+                \\"documentation\\": \\"/0.7.100/docs/install\\",
+                \\"releaseNotes\\": \\"https://github.com/dagster-io/dagster/releases/tag/0.7.100\\"
+            },
+            {
+                \\"version\\": \\"0.7.13\\",
+                \\"documentation\\": \\"/0.7.13/docs/install\\",
+                \\"releaseNotes\\": \\"https://github.com/dagster-io/dagster/releases/tag/0.7.13\\"
+            },
+            {
+                \\"version\\": \\"0.7.12\\",
+                \\"documentation\\": \\"/0.7.12/docs/install\\",
+                \\"releaseNotes\\": \\"https://github.com/dagster-io/dagster/releases/tag/0.7.12\\"
+            },
+            {
+                \\"version\\": \\"0.7.11\\",
+                \\"documentation\\": \\"/0.7.11/docs/install\\",
+                \\"releaseNotes\\": \\"https://github.com/dagster-io/dagster/releases/tag/0.7.11\\"
+            },
+            {
+                \\"version\\": \\"0.7.10\\",
+                \\"documentation\\": \\"/0.7.10/docs/install\\",
+                \\"releaseNotes\\": \\"https://github.com/dagster-io/dagster/releases/tag/0.7.10\\"
+            },
+            {
+                \\"version\\": \\"0.7.9\\",
+                \\"documentation\\": \\"/0.7.9/docs/install\\",
+                \\"releaseNotes\\": \\"https://github.com/dagster-io/dagster/releases/tag/0.7.9\\"
+            },
+            {
+                \\"version\\": \\"0.7.8\\",
+                \\"documentation\\": \\"https://dagster.readthedocs.io/en/0.7.8/\\",
+                \\"releaseNotes\\": \\"https://github.com/dagster-io/dagster/releases/tag/0.7.8\\",
+                \\"note\\": \\"Documentation is on external site\\"
+            }
+        ]
+    }"
+  `);
+});
 
-| **0.7.12** | [Documentation](/docs/install) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.12) |
-| ---------- | ------------------------------ | -------------------------------------------------------------------------- |
+test('updateMDX works', () => {
+  expect(generateVersionsMDX(JSON.stringify(MOCK_JSON_CONTENT)).mdxContent)
+    .toMatchInlineSnapshot(`
+    "# Dagster Versions
+
+    ## Current version (Stable)
+
+    | **0.7.13** | [Documentation](/docs/install) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.13) |
+    | ---- | ---- | ---- |
 
 
-## All Versions
+    ## All Versions
 
-| **0.7.12** | [Documentation](/0.7.12/docs/install)                     | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.12) |                                         |
-| ---------- | --------------------------------------------------------- | -------------------------------------------------------------------------- | --------------------------------------- |
-| **0.7.11** | [Documentation](/0.7.11/docs/install)                     | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.11) |                                         |
-| **0.7.10** | [Documentation](/0.7.10/docs/install)                     | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.10) |                                         |
-| **0.7.9**  | [Documentation](/0.7.9/docs/install)                      | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.9)  |                                         |
-| **0.7.8**  | [Documentation](https://dagster.readthedocs.io/en/0.7.8/) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.8)  | Note: Documentation is on external site |
-| **0.7.7**  | [Documentation](https://dagster.readthedocs.io/en/0.7.7/) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.7)  | Note: Documentation is on external site |
-`;
-
-const MOCK_NEW_CONTENT = `# Dagster Versions
-
-## Current version (Stable)
-
-| **0.7.100** | [Documentation](/docs/install) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.100) |
-| ---------- | ------------------------------ | -------------------------------------------------------------------------- |
-
-
-## All Versions
-
-| **0.7.100** | [Documentation](/0.7.100/docs/install)                     | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.100) |                                         |
-| ---------- | --------------------------------------------------------- | -------------------------------------------------------------------------- | --------------------------------------- |
-| **0.7.12** | [Documentation](/0.7.12/docs/install)                     | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.12) |                                         |
-| **0.7.11** | [Documentation](/0.7.11/docs/install)                     | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.11) |                                         |
-| **0.7.10** | [Documentation](/0.7.10/docs/install)                     | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.10) |                                         |
-| **0.7.9**  | [Documentation](/0.7.9/docs/install)                      | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.9)  |                                         |
-| **0.7.8**  | [Documentation](https://dagster.readthedocs.io/en/0.7.8/) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.8)  | Note: Documentation is on external site |
-| **0.7.7**  | [Documentation](https://dagster.readthedocs.io/en/0.7.7/) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.7)  | Note: Documentation is on external site |
-`;
-
-test('it works', () => {
-  expect(updateVersion(MOCK_CONTENT, '0.7.100')).toEqual({
-    oldVersion: '0.7.12',
-    newContent: MOCK_NEW_CONTENT,
-  });
+    | **0.7.13** | [Documentation](/0.7.13/docs/install) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.13) |   |
+    | ---- | ---- | ---- | ---- |
+    | **0.7.12** | [Documentation](/0.7.12/docs/install) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.12) |   |
+    | **0.7.11** | [Documentation](/0.7.11/docs/install) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.11) |   |
+    | **0.7.10** | [Documentation](/0.7.10/docs/install) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.10) |   |
+    | **0.7.9** | [Documentation](/0.7.9/docs/install) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.9) |   |
+    | **0.7.8** | [Documentation](https://dagster.readthedocs.io/en/0.7.8/) | [Release Notes](https://github.com/dagster-io/dagster/releases/tag/0.7.8) | Documentation is on external site |"
+  `);
 });
--- docs/next/src/scripts/updateVersion.ts
+++ docs/next/src/scripts/updateVersion.ts
@@ -1,11 +1,122 @@
 import fs from 'fs';
 import path from 'path';
 
-const TARGET_FILE_PATH = path.resolve(
-  __dirname,
-  '..',
-  'pages/versions/index.mdx',
-);
+export interface Versions {
+  currentVersion: Version;
+  allVersions: Version[];
+}
+export interface Version {
+  version: string;
+  documentation: string;
+  releaseNotes: string;
+  note?: string;
+}
+
+const JSON_FILE_PATH = path.resolve(__dirname, '..', 'versions.json');
+const MDX_FILE_PATH = path.resolve(__dirname, '..', 'pages/versions/index.mdx');
+
+const JSON_CONTENT = {
+  currentVersion: {
+    version: '<version>',
+    documentation: '/docs/install',
+    releaseNotes:
+      'https://github.com/dagster-io/dagster/releases/tag/<version>',
+  },
+  allVersion: {
+    version: '<version>',
+    documentation: '/<version>/docs/install',
+    releaseNotes:
+      'https://github.com/dagster-io/dagster/releases/tag/<version>',
+  },
+};
+
+const currVersionRow = (
+  version: string,
+  documentation: string,
+  releaseNotes: string,
+) =>
+  `| **${version}** | [Documentation](${documentation}) | [Release Notes](${releaseNotes}) |`;
+
+const allVersionRow = (
+  version: string,
+  documentation: string,
+  releaseNotes: string,
+  note: string = ' ',
+) =>
+  `| **${version}** | [Documentation](${documentation}) | [Release Notes](${releaseNotes}) | ${note} |`;
+
+export function updateVersionsJson(
+  jsonString: string,
+  newVersion: string,
+): {
+  oldVersion: string;
+  newJsonContent: string;
+} {
+  const jsonContent: Versions = JSON.parse(jsonString);
+  const oldVersion = jsonContent.currentVersion.version;
+  jsonContent.currentVersion = JSON.parse(
+    JSON.stringify(JSON_CONTENT.currentVersion).replace(
+      /<version>/g,
+      newVersion,
+    ),
+  );
+  jsonContent.allVersions.unshift(
+    JSON.parse(
+      JSON.stringify(JSON_CONTENT.allVersion).replace(/<version>/g, newVersion),
+    ),
+  );
+  return {
+    oldVersion: oldVersion,
+    newJsonContent: JSON.stringify(jsonContent, null, 4),
+  };
+}
+
+export function generateVersionsMDX(
+  jsonString: string,
+): {
+  mdxContent: string;
+} {
+  const jsonContent: Versions = JSON.parse(jsonString);
+  return {
+    mdxContent: [
+      '# Dagster Versions',
+      '',
+      '## Current version (Stable)',
+      '',
+      currVersionRow(
+        jsonContent.currentVersion.version,
+        jsonContent.currentVersion.documentation,
+        jsonContent.currentVersion.releaseNotes,
+      ),
+      '| ---- | ---- | ---- |',
+      '',
+      '',
+      '## All Versions',
+      '',
+      ...jsonContent.allVersions
+        .slice(0, 1)
+        .map((obj) =>
+          allVersionRow(
+            obj.version,
+            obj.documentation,
+            obj.releaseNotes,
+            obj.note,
+          ),
+        ),
+      '| ---- | ---- | ---- | ---- |',
+      ...jsonContent.allVersions
+        .slice(1)
+        .map((obj) =>
+          allVersionRow(
+            obj.version,
+            obj.documentation,
+            obj.releaseNotes,
+            obj.note,
+          ),
+        ),
+    ].join('\n'),
+  };
+}
 
 // equivalent of `if __name__ == '__main__'`
 if (require.main === module) {
@@ -18,57 +129,22 @@ function main() {
   }
 
   const newVersion = process.argv[2];
-  const currentContent = fs.readFileSync(TARGET_FILE_PATH, 'utf-8');
+  const currentJsonContent = fs.readFileSync(JSON_FILE_PATH, 'utf-8');
 
-  const { oldVersion, newContent } = updateVersion(currentContent, newVersion);
+  const { oldVersion, newJsonContent } = updateVersionsJson(
+    currentJsonContent,
+    newVersion,
+  );
+  const { mdxContent } = generateVersionsMDX(newJsonContent);
+
   console.log(
     `Updating the current version from ${oldVersion} to ${newVersion}...\n`,
   );
+  // write to versions.json
+  fs.writeFileSync(JSON_FILE_PATH, newJsonContent, 'utf-8');
+  console.log(`✅ Successfully updated ${JSON_FILE_PATH}.\n`);
 
-  fs.writeFileSync(TARGET_FILE_PATH, newContent, 'utf-8');
-  console.log(`✅ Successfully updated ${TARGET_FILE_PATH}.\n`);
-}
-
-export function updateVersion(
-  fileContent: string,
-  newVersion: string,
-): {
-  oldVersion: string;
-  newContent: string;
-} {
-  const lines = fileContent.split('\n');
-
-  // find old version
-  const versionRegex = /\| +\*\*(\d+\.\d+\.\d+)\*\* +\|/;
-  const oldVersion = lines
-    .map((line) => line.match(versionRegex)?.[1])
-    .find(Boolean);
-
-  if (!oldVersion) {
-    throw new Error('❌ Invalid input content');
-  }
-
-  // find lines with old version
-  let oldVersionLines: { index: number; line: string }[] = [];
-  for (let index = 0; index < lines.length; index++) {
-    const line = lines[index];
-    if (line.includes(oldVersion)) {
-      oldVersionLines.push({ index, line });
-    }
-  }
-
-  // update versions
-  const { index: oldVersionIndex, line: oldVersionLine } = oldVersionLines[1];
-  oldVersionLines.map(({ index }) => {
-    lines[index] = lines[index].replace(
-      new RegExp(oldVersion, 'g'),
-      newVersion,
-    );
-  });
-  lines.splice(oldVersionIndex + 2, 0, oldVersionLine);
-
-  return {
-    oldVersion: oldVersion,
-    newContent: lines.join('\n'),
-  };
+  // write to versions/index.mdx
+  fs.writeFileSync(MDX_FILE_PATH, mdxContent, 'utf-8');
+  console.log(`✅ Successfully updated ${MDX_FILE_PATH}.\n`);
 }
