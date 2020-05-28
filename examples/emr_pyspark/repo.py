336441d54fc37070de06b67fb152b77143a86031
Sandy Ryza <sandy@remix.com>
2020-05-28 14:19:57 -0700

    PySpark EMR example
    Summary: Example that demonstrates how to use an emr_pyspark_step_launcher
    
    Test Plan: bk
    
    Reviewers: nate, sashank, yuhan
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D3106

---
+++ examples/emr_pyspark/repo.py
@@ -0,0 +1,35 @@
+from dagster_aws.emr.emr_pyspark_step_launcher import emr_pyspark_step_launcher
+from dagster_aws.s3 import s3_plus_default_storage_defs, s3_resource
+from dagster_pyspark import pyspark_resource
+
+from dagster import ModeDefinition, PresetDefinition, RepositoryDefinition, pipeline, solid
+
+mode = ModeDefinition(
+    name='prod',
+    resource_defs={
+        'pyspark_step_launcher': emr_pyspark_step_launcher,
+        'pyspark': pyspark_resource,
+        's3': s3_resource,
+    },
+    system_storage_defs=s3_plus_default_storage_defs,
+)
+
+preset = PresetDefinition.from_files(
+    name='prod', mode='prod', environment_files=['prod_resources.yaml', 's3_storage.yaml'],
+)
+
+
+@solid(required_resource_keys={'pyspark_step_launcher'})
+def hello(_):
+    return 1
+
+
+@pipeline(
+    mode_defs=[mode], preset_defs=[preset],
+)
+def my_pipeline():
+    hello()
+
+
+def define_repository():
+    return RepositoryDefinition('emr_pyspark', pipeline_defs=[my_pipeline])
