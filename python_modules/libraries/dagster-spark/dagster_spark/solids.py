3531da4613ad096c36251f4f72b594e4961757ec
Sandy Ryza <sandy@remix.com>
2020-06-04 11:39:02 -0700

    Have create_spark_solid accept required_resource_keys
    Summary:
    :Summary:
    This allows it to be used with emr_pyspark_step_launcher
    
    Test Plan: bk
    
    Reviewers: nate
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D3277

--- python_modules/libraries/dagster-spark/dagster_spark/solids.py
+++ python_modules/libraries/dagster-spark/dagster_spark/solids.py
@@ -110,10 +110,13 @@ class SparkSolidDefinition(SolidDefinition):
         )
 
 
-def create_spark_solid(name, main_class, description=None):
+def create_spark_solid(
+    name, main_class, description=None, required_resource_keys=frozenset(['spark'])
+):
     check.str_param(name, 'name')
     check.str_param(main_class, 'main_class')
     check.opt_str_param(description, 'description', 'A parameterized Spark job.')
+    check.set_param(required_resource_keys, 'required_resource_keys')
 
     @solid(
         name=name,
@@ -122,7 +125,7 @@ def create_spark_solid(name, main_class, description=None):
         input_defs=[InputDefinition('start', Nothing)],
         output_defs=[OutputDefinition(Nothing)],
         tags={'kind': 'spark', 'main_class': main_class},
-        required_resource_keys={'spark'},
+        required_resource_keys=required_resource_keys,
     )
     def spark_solid(context):  # pylint: disable=unused-argument
         context.resources.spark.run_spark_job(context.solid_config, main_class)
