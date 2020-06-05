b68c43e3e393c8f3a9262a4a39c556a095e676f1
Sandy Ryza <sandy@remix.com>
2020-06-05 10:47:56 -0700

    set __all__ for dagster-pyspark
    Test Plan: bk
    
    Reviewers: nate
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D3303

--- python_modules/libraries/dagster-pyspark/dagster_pyspark/__init__.py
+++ python_modules/libraries/dagster-pyspark/dagster_pyspark/__init__.py
@@ -5,3 +5,8 @@ from .types import DataFrame
 from .version import __version__
 
 check_dagster_package_version('dagster-pyspark', __version__)
+
+__all__ = [
+    'DataFrame',
+    'pyspark_resource',
+]
