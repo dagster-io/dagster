5e927abc05309cc70af8ad3719eff34b63c582c5
Nate Kupp <nate@elementl.com>
2020-06-05 11:46:50 -0700

    Docs scheduling cleanup
    Summary: Fixes #2310
    
    Test Plan: docs only
    
    Reviewers: sashank, yuhan
    
    Reviewed By: yuhan
    
    Differential Revision: https://dagster.phacility.com/D3304

--- docs/next/src/pages/docs/deploying/docker/demo.py
+++ docs/next/src/pages/docs/deploying/docker/demo.py
@@ -1,6 +1,16 @@
-from dagster import repository
+from dagster import pipeline, repository, solid
+
+
+@solid
+def hello(_):
+    print('hello, world!')
+
+
+@pipeline
+def simple_pipeline():
+    hello()
 
 
 @repository
 def demo_repo():
-    return []
+    return [simple_pipeline]
