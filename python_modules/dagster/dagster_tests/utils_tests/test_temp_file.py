b400b40b3326939e1a085e4fb77b5e078a5332ca
alangenfeld <alex@elementl.com>
2020-06-15 16:36:06 -0500

    fix windows tests

--- python_modules/dagster/dagster_tests/utils_tests/test_temp_file.py
+++ python_modules/dagster/dagster_tests/utils_tests/test_temp_file.py
@@ -1,9 +1,12 @@
-import resource
+import pytest
 
 from dagster.utils.temp_file import get_temp_file_name, get_temp_file_names
 
 
+@pytest.mark.skipif('"win" in sys.platform', reason="resource module not available in windows")
 def test_get_temp_file_name_leak_file_descriptors():
+    import resource
+
     resource.setrlimit(resource.RLIMIT_NOFILE, (100, 100))
     for _ in range(100):
         with get_temp_file_name() as _:
