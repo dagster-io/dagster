8d7dfedae353a08cd9e8d76b394a1ec3fe283a6a
Sashank Thupukari <s@thupukari.com>
2020-06-15 13:06:45 -0400

    [docs] Add nested table of contents
    Test Plan: manual
    
    Reviewers: yuhan
    
    Reviewed By: yuhan
    
    Differential Revision: https://dagster.phacility.com/D3493

--- docs/next/src/components/Sidebar/Nav.tsx
+++ docs/next/src/components/Sidebar/Nav.tsx
@@ -84,32 +84,114 @@ const Nav: React.FC<NavProps> = ({ className, isMobile }) => {
                 router.asPath.startsWith(c.path) &&
                 router.asPath.length - c.path.length < 2;
 
+              const subChildSelected =
+                (c.children &&
+                  c.children.reduce((accumulator, child) => {
+                    return (
+                      accumulator ||
+                      (router.asPath.startsWith(child.path) &&
+                        router.asPath.length - child.path.length < 2)
+                    );
+                  }, false)) ||
+                false;
+
               return (
-                <VersionedLink key={`${c.path}-${c.name}`} href={c.path}>
-                  <a
-                    className={cx(
-                      `group flex justify-between items-center px-3 py-2 text-sm leading-5 font-medium text-gray-600 rounded-md focus:outline-none transition ease-in-out duration-150`,
-                      {
-                        'font-semibold text-blue-700 bg-blue-100': subSelected,
-                        'hover:font-semibold': !subSelected,
-                      },
-                    )}
-                  >
-                    <div className="truncatej">{c.name}</div>
-                    <div>
-                      {c.isExternal && (
-                        <svg
-                          className="w-4 h-4 bg-gray-50"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
-                          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-                        </svg>
+                <div>
+                  <VersionedLink key={`${c.path}-${c.name}`} href={c.path}>
+                    <a
+                      className={cx(
+                        `group flex justify-between items-center px-3 py-2 text-sm leading-5 font-medium text-gray-600 rounded-md focus:outline-none transition ease-in-out duration-150`,
+                        {
+                          'font-semibold text-blue-700 bg-blue-100': subSelected,
+                          'hover:font-semibold': !subSelected,
+                        },
                       )}
-                    </div>
-                  </a>
-                </VersionedLink>
+                    >
+                      <div className="truncate">{c.name}</div>
+                      <div>
+                        {c.isExternal && (
+                          <svg
+                            className="w-4 h-4 bg-gray-50"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                          >
+                            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+                            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+                          </svg>
+                        )}
+                      </div>
+                      {c.children && (
+                        <div>
+                          {subSelected || subChildSelected ? (
+                            <svg
+                              className="w-4 h-4 bg-gray-50"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd"
+                              ></path>
+                            </svg>
+                          ) : (
+                            <svg
+                              className="w-4 h-4 bg-gray-50"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd"
+                              ></path>
+                            </svg>
+                          )}
+                        </div>
+                      )}
+                    </a>
+                  </VersionedLink>
+                  {(subSelected || subChildSelected) &&
+                    c.children &&
+                    c.children.map((child) => {
+                      const childSelected =
+                        router.asPath.startsWith(child.path) &&
+                        router.asPath.length - child.path.length < 2;
+
+                      return (
+                        <div className="ml-4 border-l">
+                          <VersionedLink
+                            key={`${child.path}-${child.name}`}
+                            href={child.path}
+                          >
+                            <a
+                              className={cx(
+                                `group flex justify-between items-center px-3 py-2 text-sm leading-5 ml-1 font-medium text-gray-600 rounded-md focus:outline-none transition ease-in-out duration-150`,
+                                {
+                                  'font-semibold text-blue-700 bg-blue-100': childSelected,
+                                  'hover:font-semibold': !childSelected,
+                                },
+                              )}
+                            >
+                              <div className="truncate">{child.name}</div>
+                              <div>
+                                {child.isExternal && (
+                                  <svg
+                                    className="w-4 h-4 bg-gray-50"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                  >
+                                    <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+                                    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+                                  </svg>
+                                )}
+                              </div>
+                            </a>
+                          </VersionedLink>
+                        </div>
+                      );
+                    })}
+                </div>
               );
             })}
           </div>
