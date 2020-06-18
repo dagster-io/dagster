4162aa2e121407fee8e580feacf1cbd225ac5ca2
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2020-06-19 01:33:24 +0300

    Add test_types.py

---
+++ python_modules/libraries/dagster-pyspark/dagster_pyspark_tests/test_types.py
@@ -0,0 +1,70 @@
+import shutil
+
+import pytest
+from dagster_pyspark import DataFrame as DagsterPySparkDataFrame
+from pyspark.sql import DataFrame as NativeSparkDataFrame
+from pyspark.sql import Row, SparkSession
+
+from dagster import OutputDefinition, execute_solid, make_python_type_usable_as_dagster_type, solid
+from dagster.utils.test import get_temp_dir
+
+
+spark = SparkSession.builder.getOrCreate()
+
+
+def create_pyspark_df() -> NativeSparkDataFrame:
+    data = [Row(_c0=str(i), _c1=str(i)) for i in range(100)]
+    return spark.createDataFrame(data)
+
+
+@pytest.mark.parametrize(
+    'file_type,read',
+    [
+        pytest.param('csv', spark.read.csv, id='csv'),
+        pytest.param('parquet', spark.read.parquet, id='parquet'),
+        pytest.param('json', spark.read.json, id='json'),
+    ],
+)
+def test_dataframe_outputs(file_type, read):
+    df = create_pyspark_df()
+
+    @solid(output_defs=[OutputDefinition(dagster_type=DagsterPySparkDataFrame, name='df')])
+    def return_df(context):
+        return df
+
+    with get_temp_dir() as temp_path:
+        shutil.rmtree(temp_path)
+
+        result = execute_solid(
+            return_df,
+            run_config={
+                'solids': {'return_df': {'outputs': [{'df': {file_type: {'path': temp_path}}}]}}
+            },
+        )
+        assert result.success
+        actual = read(temp_path)
+        assert sorted(df.collect()) == sorted(actual.collect())
+
+        result = execute_solid(
+            return_df,
+            run_config={
+                'solids': {
+                    'return_df': {
+                        'outputs': [
+                            {
+                                'df': {
+                                    file_type: {
+                                        'path': temp_path,
+                                        'mode': 'overwrite',
+                                        'compression': 'gzip',
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+        )
+        assert result.success
+        actual = read(temp_path)
+        assert sorted(df.collect()) == sorted(actual.collect())
