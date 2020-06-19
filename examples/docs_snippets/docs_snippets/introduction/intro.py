ca0d3d0e05949b390c5c1b9e8184640a1e17c42c
Sandy Ryza <sandy@elementl.com>
2020-06-19 09:02:50 -0700

    [docs] Add code example for intro page
    Summary:
    The thinking behind the code is to provide something minimal but realistic.  For that realism, I think using a Pandas DataFrame (vs. some primitive type) is important.
    
    Test Plan: bk
    
    Reviewers: max, sashank, yuhan, schrockn
    
    Reviewed By: schrockn
    
    Differential Revision: https://dagster.phacility.com/D3567

---
+++ examples/docs_snippets/docs_snippets/introduction/intro.py
@@ -0,0 +1,64 @@
+from dagster_pandas import DataFrame as DagsterPandasDataFrame
+from pandas import DataFrame
+
+from dagster import (
+    execute_pipeline,
+    make_python_type_usable_as_dagster_type,
+    pipeline,
+    repository,
+    solid,
+)
+
+make_python_type_usable_as_dagster_type(python_type=DataFrame, dagster_type=DagsterPandasDataFrame)
+
+
+@solid(description='Calculates the grams of sugar per cup of each kind of cereal.')
+def sugar_by_volume(_, cereals: DataFrame) -> DataFrame:
+    df = cereals[['name']].copy()
+    df['sugar_per_cup'] = cereals['sugars'] / cereals['cups']
+    return df
+
+
+@solid(
+    description='Finds the sugar-per-cup cutoff that separates the top quartile of cereals from the rest.'
+)
+def top_quartile_cutoff(_, cereals: DataFrame) -> float:
+    return cereals['sugar_per_cup'].quantile(0.75)
+
+
+@solid(
+    description='''
+    Selects cereals whose sugar-per-cup exceeds the given cutoff.
+
+    Inputs:
+    - **cereals** - A DataFrame of cereal names with sugar per cup.
+    - **cutoff** - The sugar-per-cup cutoff for inclusion.
+'''
+)
+def sugariest_cereals(_, cereals: DataFrame, cutoff: float) -> DataFrame:
+    return cereals[cereals['sugar_per_cup'] > cutoff]
+
+
+@pipeline
+def sugariest_pipeline():
+    sugar_by_vol = sugar_by_volume()
+    sugariest_cereals(sugar_by_vol, top_quartile_cutoff(sugar_by_vol))
+
+
+if __name__ == '__main__':
+    result = execute_pipeline(
+        sugariest_pipeline,
+        run_config={
+            'solids': {
+                'sugar_by_volume': {'inputs': {'cereals': {'csv': {'path': 'cereal.csv'}}}},
+                'sugariest_cereals': {
+                    'outputs': [{'result': {'csv': {'path': 'sugariest_cereal.csv'}}}]
+                },
+            },
+        },
+    )
+
+
+@repository
+def intro_repo():
+    return [sugariest_pipeline]
