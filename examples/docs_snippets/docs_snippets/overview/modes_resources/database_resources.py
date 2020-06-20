f56bfad142117f232d0cf3977a0f121cb3dfe6c5
Sandy Ryza <sandy@elementl.com>
2020-06-19 17:16:09 -0700

    [docs] modes & resources overview
    Summary:
    {F173910}
    {F173911}
    {F173912}
    {F173913}
    
    Test Plan: inspection of docs site
    
    Reviewers: sashank, max, yuhan
    
    Reviewed By: sashank
    
    Differential Revision: https://dagster.phacility.com/D3609

---
+++ examples/docs_snippets/docs_snippets/overview/modes_resources/database_resources.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine
+
+from dagster import IntSource, StringSource, resource
+
+
+@resource
+def sqlite_database(_):
+    class SQLiteDatabase:
+        def execute_query(self, query):
+            engine = create_engine('sqlite:///tmp.db')
+            with engine.connect() as conn:
+                conn.execute(query)
+
+    return SQLiteDatabase()
+
+
+@resource(
+    config={
+        'hostname': StringSource,
+        'port': IntSource,
+        'username': StringSource,
+        'password': StringSource,
+        'db_name': StringSource,
+    }
+)
+def postgres_database(init_context):
+    class PostgresDatabase:
+        def __init__(self, resource_config):
+            self.hostname = resource_config['hostname']
+            self.port = resource_config['port']
+            self.username = resource_config['username']
+            self.password = resource_config['password']
+            self.db_name = resource_config['db_name']
+
+        def execute_query(self, query):
+            engine = create_engine(
+                f'postgresql://{self.username}:{self.password}@{self.hostname}:{self.port}/{self.db_name}'
+            )
+            with engine.connect() as conn:
+                conn.execute(query)
+
+    return PostgresDatabase(init_context.resource_config)
---
+++ examples/docs_snippets/docs_snippets/overview/modes_resources/pipeline_with_modes.py
@@ -0,0 +1,15 @@
+from dagster import ModeDefinition, pipeline
+
+from .database_resources import postgres_database, sqlite_database
+from .solids_with_resources import generate_table_1, generate_table_2
+
+
+@pipeline(
+    mode_defs=[
+        ModeDefinition('local_dev', resource_defs={'database': sqlite_database}),
+        ModeDefinition('prod', resource_defs={'database': postgres_database}),
+    ],
+)
+def generate_tables_pipeline():
+    generate_table_1()
+    generate_table_2()
