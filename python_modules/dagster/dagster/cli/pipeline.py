c1a1aec9019b83ad7b84b1d9bf99d3fe5b04a83b
alangenfeld <alex@elementl.com>
2020-06-22 16:50:23 -0500

    fix backfill cli help text
    Summary: resolves #2631
    
    Test Plan: eyes
    
    Reviewers: yuhan, johann, catherinewu
    
    Reviewed By: yuhan
    
    Differential Revision: https://dagster.phacility.com/D3598

--- python_modules/dagster/dagster/cli/pipeline.py
+++ python_modules/dagster/dagster/cli/pipeline.py
@@ -152,8 +152,8 @@ def get_partitioned_pipeline_instructions(command_name):
     return (
         'This commands targets a partitioned pipeline. The pipeline and partition set must be '
         'defined in a repository, which can be specified in a number of ways:'
-        '\n\n1. dagster pipeline {command_name} <<pipeline_name>> (works if .{default_filename} exists)'
-        '\n\n2. dagster pipeline {command_name} <<pipeline_name>> -w path/to/{default_filename}'
+        '\n\n1. dagster pipeline {command_name} -p <<pipeline_name>> (works if .{default_filename} exists)'
+        '\n\n2. dagster pipeline {command_name} -p <<pipeline_name>> -w path/to/{default_filename}'
         '\n\n3. dagster pipeline {command_name} -f /path/to/file.py -a define_some_repo -p <<pipeline_name>>'
         '\n\n4. dagster pipeline {command_name} -m a_module.submodule -a define_some_repo -p <<pipeline_name>>'
     ).format(command_name=command_name, default_filename=DEFAULT_WORKSPACE_YAML_FILENAME)
