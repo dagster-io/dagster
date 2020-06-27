ec7f124a747e4ef27be1d162f9eb286c5c58cc09
Sandy Ryza <sandy@elementl.com>
2020-06-26 17:44:16 -0700

    [docs] Nothing example
    Summary: {F179029}
    
    Test Plan: manual inspection, bk
    
    Reviewers: sashank, schrockn, yuhan, nate
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D3672

---
+++ examples/nothing/repo.py
@@ -0,0 +1,29 @@
+from dagster import InputDefinition, Nothing, pipeline, repository, solid
+
+
+def get_database_connection():
+    class Database:
+        def execute(self, query):
+            pass
+
+    return Database()
+
+
+@solid
+def create_table_1(_) -> Nothing:
+    get_database_connection().execute('create table_1 as select * from some_source_table')
+
+
+@solid(input_defs=[InputDefinition('start', Nothing)])
+def create_table_2(_):
+    get_database_connection().execute('create table_2 as select * from table_1')
+
+
+@pipeline
+def my_pipeline():
+    create_table_2(create_table_1())
+
+
+@repository
+def nothing_example_repository():
+    return [my_pipeline]
