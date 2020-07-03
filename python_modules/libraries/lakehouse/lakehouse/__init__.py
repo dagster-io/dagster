2a872753d1218e36b18eb53d7dfd0bc7a59f1690
Sandy Ryza <sandy@elementl.com>
2020-07-03 09:55:46 -0700

    (lakehouse-table-2) lakehouse table
    Summary: Depends on D3756.
    
    Test Plan: unit tests
    
    Reviewers: schrockn, alangenfeld, yuhan
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D3758

--- python_modules/libraries/lakehouse/lakehouse/__init__.py
+++ python_modules/libraries/lakehouse/lakehouse/__init__.py
@@ -8,5 +8,6 @@ its assets.
 
 from .asset import Asset, source_asset
 from .computation import Computation
-from .decorators import computed_asset
+from .decorators import computed_asset, computed_table
 from .house import Lakehouse, TypeStoragePolicy
+from .table import Column, Table, source_table
--- python_modules/libraries/lakehouse/lakehouse/decorators.py
+++ python_modules/libraries/lakehouse/lakehouse/decorators.py
@@ -5,12 +5,13 @@ from dagster.seven import funcsigs
 
 from .asset import Asset
 from .computation import AssetDependency, Computation
+from .table import Table
 
 
 def computed_asset(storage_key, path=None, input_assets=None):
     '''Create an Asset with its computation built from the decorated function.
 
-    The type annotations on the arguments and return value of the decorated functioon are use to
+    The type annotations on the arguments and return value of the decorated function are use to
     determine which TypeStoragePolicy will be used to load and save its outputs and inputs.
 
     Args:
@@ -35,7 +36,7 @@ def computed_asset(storage_key, path=None, input_assets=None):
             def asia_orders_asset(orders: pyspark.sql.DataFrame) -> pyspark.sql.DataFrame:
                 return orders.filter(orders['continent'] == 'asia')
 
-            @computed_asset(storage_key='filesystem', input_assets=[orders_asset, users_assett])
+            @computed_asset(storage_key='filesystem', input_assets=[orders_asset, users_asset])
             def user_orders_asset(
                 orders: pyspark.sql.DataFrame, users: pyspark.sql.DataFrame
             ) -> pyspark.sql.DataFrame:
@@ -54,35 +55,7 @@ def computed_asset(storage_key, path=None, input_assets=None):
     def _computed_asset(fn):
         _path = path or (fn.__name__,)
         _input_assets = input_assets or []
-
-        kwarg_types = _infer_kwarg_types(fn)
-        if isinstance(_input_assets, list):
-            check.invariant(
-                len(kwarg_types) == len(_input_assets),
-                'For {fn_name}, input_assets length "{input_assets_len}"" must match number of '
-                'keyword args "{num_kwargs}"'.format(
-                    fn_name=fn.__name__,
-                    input_assets_len=len(_input_assets),
-                    num_kwargs=len(kwarg_types),
-                ),
-            )
-            kwarg_deps = {
-                kwarg: AssetDependency(input_asset, kwarg_types[kwarg])
-                for kwarg, input_asset in zip(kwarg_types.keys(), _input_assets)
-            }
-        elif isinstance(_input_assets, dict):
-            check.invariant(
-                kwarg_types.keys() == _input_assets.keys(),
-                'input_assets keys {kwarg_deps_keys} must match keyword args {kwargs}'.format(
-                    kwarg_deps_keys=_input_assets.keys(), kwargs=kwarg_types.keys(),
-                ),
-            )
-            kwarg_deps = {
-                kwarg: AssetDependency(_input_assets[kwarg], kwarg_types[kwarg])
-                for kwarg in kwarg_types.keys()
-            }
-        else:
-            check.failed('input_assets must be a list or a dict')
+        kwarg_deps = _deps_by_arg_name(_input_assets, fn)
 
         return Asset(
             storage_key=storage_key,
@@ -93,6 +66,79 @@ def computed_asset(storage_key, path=None, input_assets=None):
         )
 
     return _computed_asset
+
+
+def computed_table(storage_key, path=None, input_assets=None, columns=None):
+    '''Create a Table with its computation built from the decorated function.
+
+    The type annotations on the arguments and return value of the decorated function are use to
+    determine which TypeStoragePolicy will be used to load and save its outputs and inputs.
+
+    Args:
+        storage_key (str): The key of the storage used to persist the asset.
+        path (Optional[Tuple[str, ...]]): The path of the asset within the storage_key. If not
+            given, the name of the decorated function is used.
+        input_assets (Optional[Union[List[Asset], Dict[str, Asset]]]): The assets that this asset
+            depends on, mapped to the arguments of the decorated function.  If a dictionary is
+            passed, the keys should be the same as the names of the decorated function's arguments.
+            If a list is passed, the first asset in the list is mapped to the first argument of the
+            decorated function, and so on.
+        columns (Optional[List[Column]]): The table's columns.
+    '''
+
+    def _computed_table(fn):
+        _path = path or (fn.__name__,)
+        _input_assets = input_assets or []
+        kwarg_deps = _deps_by_arg_name(_input_assets, fn)
+
+        return Table(
+            storage_key=storage_key,
+            path=_path,
+            computation=Computation(
+                compute_fn=fn, deps=kwarg_deps, output_in_memory_type=_infer_output_type(fn),
+            ),
+            columns=columns,
+        )
+
+    return _computed_table
+
+
+def _deps_by_arg_name(input_assets, fn):
+    '''
+    Args:
+        input_assets (Optional[Union[List[Asset], Dict[str, Asset]]])
+        fn (Callable)
+
+    Returns (Dict[str, AssetDependency])
+    '''
+    kwarg_types = _infer_kwarg_types(fn)
+    if isinstance(input_assets, list):
+        check.invariant(
+            len(kwarg_types) == len(input_assets),
+            'For {fn_name}, input_assets length "{input_assets_len}"" must match number of '
+            'keyword args "{num_kwargs}"'.format(
+                fn_name=fn.__name__,
+                input_assets_len=len(input_assets),
+                num_kwargs=len(kwarg_types),
+            ),
+        )
+        return {
+            kwarg: AssetDependency(input_asset, kwarg_types[kwarg])
+            for kwarg, input_asset in zip(kwarg_types.keys(), input_assets)
+        }
+    elif isinstance(input_assets, dict):
+        check.invariant(
+            kwarg_types.keys() == input_assets.keys(),
+            'input_assets keys {kwarg_deps_keys} must match keyword args {kwargs}'.format(
+                kwarg_deps_keys=input_assets.keys(), kwargs=kwarg_types.keys(),
+            ),
+        )
+        return {
+            kwarg: AssetDependency(input_assets[kwarg], kwarg_types[kwarg])
+            for kwarg in kwarg_types.keys()
+        }
+    else:
+        check.failed('input_assets must be a list or a dict')
 
 
 def _infer_kwarg_types(fn):
