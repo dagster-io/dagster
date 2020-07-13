4d63a63e721a0adc43633218bfd94e61402bde4b
Max Gasner <max.gasner@gmail.com>
2020-07-13 16:33:32 -0400

    psutil

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -90,7 +90,7 @@ def _do_setup(name='dagster'):
             'backports.tempfile; python_version<"3"',
             'toposort>=1.0',
             'watchdog>=0.8.3',
-            'psutil >= 1.0; platform_system=="Windows"',
+            'psutil >= 1.0',
             # https://github.com/mhammond/pywin32/issues/1439
             'pywin32 != 226; platform_system=="Windows"',
             'pytz',
