6de50e262322ae85f4cd69207281d4c9b5569c37
Nicholas Schrock <schrockn@elementl.com>
2020-07-14 21:47:14 -0700

    Checkin repro for https://github.com/dagster-io/dagster/issues/2725
    Summary: I'm fairly scandalized by this bug and am shocked it is in the system. We should fix this asap.
    
    Test Plan: BK
    
    Reviewers: sandyryza, alangenfeld, max
    
    Reviewed By: sandyryza
    
    Differential Revision: https://dagster.phacility.com/D3894

---
+++ python_modules/dagster/dagster_tests/core_tests/config_types_tests/test_config_default_required.py
@@ -0,0 +1,26 @@
+import pytest
+
+from dagster import Field, execute_solid, solid
+
+
+# Repro for https://github.com/dagster-io/dagster/issues/2725
+@pytest.mark.xfail
+def test_default_implies_not_required_field_correct():
+    @solid(config_schema={'default_to_one': Field(int, default_value=1)})
+    def return_default_to_one(context):
+        return context.solid_config['default_to_one']
+
+    default_to_one_field = return_default_to_one.config_field.config_type.fields['default_to_one']
+    assert default_to_one_field.is_required is False
+
+
+# Repro for https://github.com/dagster-io/dagster/issues/2725
+@pytest.mark.xfail
+def test_default_implies_not_required_execute_solid():
+    @solid(config_schema={'default_to_one': Field(int, default_value=1)})
+    def return_default_to_one(context):
+        return context.solid_config['default_to_one']
+
+    # This currently throws. It should not require configuration as the
+    # the default value should make it *not* required.
+    execute_solid(return_default_to_one)
