fce255ba0132197ab3e773c3823eb8102a5cc2d5
prha <prha@elementl.com>
2020-08-12 16:25:25 -0700

    toys longitudinal
    Summary: add longitudinal pipeline back into toys
    
    Test Plan: inspection
    
    Reviewers: sashank, bengotow
    
    Reviewed By: sashank
    
    Differential Revision: https://dagster.phacility.com/D4185

--- python_modules/dagster-test/dagster_test/toys/repo.py
+++ python_modules/dagster-test/dagster_test/toys/repo.py
@@ -3,6 +3,7 @@ from dagster_test.toys.error_monster import error_monster
 from dagster_test.toys.fan_in_fan_out import fan_in_fan_out_pipeline
 from dagster_test.toys.hammer import hammer_pipeline
 from dagster_test.toys.log_spew import log_spew
+from dagster_test.toys.longitudinal import longitudinal_pipeline
 from dagster_test.toys.many_events import many_events
 from dagster_test.toys.sleepy import sleepy_pipeline
 
@@ -19,6 +20,7 @@ def toys_repository():
         hammer_pipeline,
         fan_in_fan_out_pipeline,
         log_spew,
+        longitudinal_pipeline,
         many_events,
         sleepy_pipeline,
     ] + get_toys_schedules()
