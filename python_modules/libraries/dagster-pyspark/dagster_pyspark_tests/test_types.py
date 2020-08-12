2f10ffb3d5b24db20f00a69148f38b0ea9103b75
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2020-08-12 10:05:24 +0300

    add tests

--- python_modules/libraries/dagster-pyspark/dagster_pyspark_tests/test_types.py
+++ python_modules/libraries/dagster-pyspark/dagster_pyspark_tests/test_types.py
@@ -2,9 +2,17 @@ import shutil
 
 import pytest
 from dagster_pyspark import DataFrame as DagsterPySparkDataFrame
+from dagster_pyspark import pyspark_resource
 from pyspark.sql import Row, SparkSession
 
-from dagster import OutputDefinition, execute_solid, solid
+from dagster import (
+    InputDefinition,
+    ModeDefinition,
+    OutputDefinition,
+    execute_solid,
+    file_relative_path,
+    solid,
+)
 from dagster.utils.test import get_temp_dir
 
 spark = SparkSession.builder.getOrCreate()
@@ -66,3 +74,28 @@ def test_dataframe_outputs(file_type, read):
         assert result.success
         actual = read(temp_path)
         assert sorted(df.collect()) == sorted(actual.collect())
+
+
+@pytest.mark.parametrize(
+    'file_type,read',
+    [
+        pytest.param('csv', spark.read.csv, id='csv'),
+        pytest.param('parquet', spark.read.parquet, id='parquet'),
+        pytest.param('json', spark.read.json, id='json'),
+    ],
+)
+def test_dataframe_inputs(file_type, read):
+    @solid(input_defs=[InputDefinition(dagster_type=DagsterPySparkDataFrame, name='input_df')],)
+    def return_df(_, input_df):
+        return input_df
+
+    file_name = file_relative_path(__file__, "num.{file_type}".format(file_type=file_type))
+    result = execute_solid(
+        return_df,
+        mode_def=ModeDefinition(resource_defs={'pyspark': pyspark_resource}),
+        run_config={
+            'solids': {'return_df': {'inputs': {'input_df': {file_type: {'path': file_name}}}}}
+        },
+    )
+    assert result.success
+    assert sorted(result.output_value().collect()) == sorted(read(file_name).collect())
