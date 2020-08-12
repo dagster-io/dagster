bfe5694859e24a679ab52be45e7738f26fb62d32
dpeng817 <cdguitar817@gmail.com>
2020-08-12 12:17:03 -0400

    Retitled the simple lakehouse example in the doc to be more idiomatic to the others. Changed setup.py for the example as well to remove some inaccurate information.
    Summary: Simple lakehouse example needed renaming, as it was currently titled `simple_lakehouse` in the doc. There was also some incorrect information in the setup.py file.
    
    Test Plan: Using existing simple lakehouse tests.
    
    Reviewers: sandyryza
    
    Reviewed By: sandyryza
    
    Differential Revision: https://dagster.phacility.com/D4164

--- examples/simple_lakehouse/setup.py
+++ examples/simple_lakehouse/setup.py
@@ -3,15 +3,14 @@ from setuptools import setup
 setup(
     name='simple_lakehouse',
     version='dev',
-    author_email='chris@elementl.com',
+    author_email='hello@elementl.com',
     packages=['simple_lakehouse'],  # same as name
     install_requires=['dagster'],  # external packages as dependencies
     author='Elementl',
     license='Apache-2.0',
-    description='Dagster Examples',
+    description='Dagster example for using the Lakehouse API.',
     url='https://github.com/dagster-io/dagster/tree/master/examples/simple_lakehouse',
     classifiers=[
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
