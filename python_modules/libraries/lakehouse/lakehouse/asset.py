1cffce1d622d249375fa1b2b4333bd43e4643099
Sandy Ryza <sandy@elementl.com>
2020-08-19 16:51:01 -0700

    [lakehouse] accept strings and lists as asset paths
    Test Plan: added test
    
    Reviewers: cdecarolis, prha
    
    Reviewed By: prha
    
    Differential Revision: https://dagster.phacility.com/D4229

--- python_modules/libraries/lakehouse/lakehouse/asset.py
+++ python_modules/libraries/lakehouse/lakehouse/asset.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta
+from typing import List, Tuple, Union
 
 import six
 
@@ -10,7 +11,7 @@ from .computation import Computation
 class Asset(six.with_metaclass(ABCMeta)):
     def __init__(self, storage_key, path, computation):
         self._storage_key = check.str_param(storage_key, 'storage_key')
-        self._path = check.tuple_param(path, 'path', of_type=str)
+        self._path = canonicalize_path(path)
         self._computation = check.opt_inst_param(computation, 'computation', Computation)
         self._dagster_type = DagsterType(type_check_fn=lambda a, b: True, name='.'.join(self.path))
 
@@ -36,3 +37,16 @@ def source_asset(path, storage_key='default_storage'):
     may depend on it, but it has no dependencies that the lakehouse is aware of.
     '''
     return Asset(storage_key=storage_key, path=path, computation=None)
+
+
+def canonicalize_path(path: Union[Tuple[str, ...], List[str], str]) -> Tuple[str, ...]:
+    if isinstance(path, str):
+        return (path,)
+    elif isinstance(path, tuple):
+        check.tuple_param(path, 'path', of_type=str)
+        return path
+    elif isinstance(path, list):
+        check.list_param(path, 'path', of_type=str)
+        return tuple(path)
+    else:
+        check.failed('path param must be a string, list of strings, or tuple of strings')
---
+++ python_modules/libraries/lakehouse/lakehouse_tests/test_asset.py
@@ -0,0 +1,25 @@
+import pytest
+from lakehouse.asset import canonicalize_path
+
+from dagster.check import CheckError
+
+
+@pytest.mark.parametrize(
+    'path, expected',
+    [
+        ('a', ('a',)),
+        ('ab', ('ab',)),
+        (('a',), ('a',)),
+        (('ab', 'cd'), ('ab', 'cd')),
+        (['a'], ('a',)),
+        (['ab', 'cd'], ('ab', 'cd')),
+    ],
+)
+def test_canonicalize_path(path, expected):
+    assert canonicalize_path(path) == expected
+
+
+@pytest.mark.parametrize('path', [(1,), (1,), ((1,),), (('a', 1),), (['a', 1],)])
+def test_unacceptable_paths(path):
+    with pytest.raises(CheckError):
+        canonicalize_path(path)
