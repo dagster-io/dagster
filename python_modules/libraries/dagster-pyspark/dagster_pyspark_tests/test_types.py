acfcbba63814b9787d177d07a977ee9756128dbb
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2020-08-20 19:13:18 +0300

    Add pyspark dataframe_loader (#2790)
    * add dataframe_loader
    
    * add tests
    
    * add file_type 'other'
    
    * add tests
    
    * fix errors

--- python_modules/libraries/dagster-pyspark/dagster_pyspark_tests/test_types.py
+++ python_modules/libraries/dagster-pyspark/dagster_pyspark_tests/test_types.py
@@ -2,12 +2,34 @@ import shutil
 
 import pytest
 from dagster_pyspark import DataFrame as DagsterPySparkDataFrame
+from dagster_pyspark import pyspark_resource
 from pyspark.sql import Row, SparkSession
 
-from dagster import OutputDefinition, execute_solid, solid
+from dagster import (
+    InputDefinition,
+    ModeDefinition,
+    OutputDefinition,
+    execute_solid,
+    file_relative_path,
+    solid,
+)
 from dagster.utils.test import get_temp_dir
 
 spark = SparkSession.builder.getOrCreate()
+
+dataframe_parametrize_argnames = 'file_type,read,other'
+dataframe_parametrize_argvalues = [
+    pytest.param('csv', spark.read.csv, False, id='csv'),
+    pytest.param('parquet', spark.read.parquet, False, id='parquet'),
+    pytest.param('json', spark.read.json, False, id='json'),
+    pytest.param('csv', spark.read.load, True, id='other_csv'),
+    pytest.param('parquet', spark.read.load, True, id='other_parquet'),
+    pytest.param('json', spark.read.load, True, id='other_json'),
+]
+
+
+def dict_without_keys(ddict, *keys):
+    return {key: value for key, value in ddict.items() if key not in set(keys)}
 
 
 def create_pyspark_df():
@@ -15,15 +37,8 @@ def create_pyspark_df():
     return spark.createDataFrame(data)
 
 
-@pytest.mark.parametrize(
-    'file_type,read',
-    [
-        pytest.param('csv', spark.read.csv, id='csv'),
-        pytest.param('parquet', spark.read.parquet, id='parquet'),
-        pytest.param('json', spark.read.json, id='json'),
-    ],
-)
-def test_dataframe_outputs(file_type, read):
+@pytest.mark.parametrize(dataframe_parametrize_argnames, dataframe_parametrize_argvalues)
+def test_dataframe_outputs(file_type, read, other):
     df = create_pyspark_df()
 
     @solid(output_defs=[OutputDefinition(dagster_type=DagsterPySparkDataFrame, name='df')])
@@ -33,14 +48,17 @@ def test_dataframe_outputs(file_type, read):
     with get_temp_dir() as temp_path:
         shutil.rmtree(temp_path)
 
+        options = {'path': temp_path}
+        if other:
+            options['format'] = file_type
+            file_type = 'other'
+
         result = execute_solid(
             return_df,
-            run_config={
-                'solids': {'return_df': {'outputs': [{'df': {file_type: {'path': temp_path}}}]}}
-            },
+            run_config={'solids': {'return_df': {'outputs': [{'df': {file_type: options}}]}}},
         )
         assert result.success
-        actual = read(temp_path)
+        actual = read(options['path'], **dict_without_keys(options, 'path'))
         assert sorted(df.collect()) == sorted(actual.collect())
 
         result = execute_solid(
@@ -51,11 +69,9 @@ def test_dataframe_outputs(file_type, read):
                         'outputs': [
                             {
                                 'df': {
-                                    file_type: {
-                                        'path': temp_path,
-                                        'mode': 'overwrite',
-                                        'compression': 'gzip',
-                                    }
+                                    file_type: dict(
+                                        {'mode': 'overwrite', 'compression': 'gzip',}, **options
+                                    )
                                 }
                             }
                         ]
@@ -64,5 +80,26 @@ def test_dataframe_outputs(file_type, read):
             },
         )
         assert result.success
-        actual = read(temp_path)
+        actual = read(options['path'], **dict_without_keys(options, 'path'))
         assert sorted(df.collect()) == sorted(actual.collect())
+
+
+@pytest.mark.parametrize(dataframe_parametrize_argnames, dataframe_parametrize_argvalues)
+def test_dataframe_inputs(file_type, read, other):
+    @solid(input_defs=[InputDefinition(dagster_type=DagsterPySparkDataFrame, name='input_df')],)
+    def return_df(_, input_df):
+        return input_df
+
+    options = {'path': file_relative_path(__file__, "num.{file_type}".format(file_type=file_type))}
+    if other:
+        options['format'] = file_type
+        file_type = 'other'
+
+    result = execute_solid(
+        return_df,
+        mode_def=ModeDefinition(resource_defs={'pyspark': pyspark_resource}),
+        run_config={'solids': {'return_df': {'inputs': {'input_df': {file_type: options}}}}},
+    )
+    assert result.success
+    actual = read(options['path'], **dict_without_keys(options, 'path'))
+    assert sorted(result.output_value().collect()) == sorted(actual.collect())
