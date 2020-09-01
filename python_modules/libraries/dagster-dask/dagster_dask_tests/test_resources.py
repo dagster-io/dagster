749596bb18e26bd3a71bd815e156ac5bc5f867f0
King Chung Huang <kinghuang@mac.com>
2020-09-01 15:54:03 -0600

    Test local cluster configuration
    Run a pipeline with a custom Dask local cluster config and test that
    the client’s scheduler has the configured numbered of workers and
    threads per worker.

--- python_modules/libraries/dagster-dask/dagster_dask_tests/test_resources.py
+++ python_modules/libraries/dagster-dask/dagster_dask_tests/test_resources.py
@@ -1,20 +1,57 @@
-import pytest
+from dagster import (
+    DagsterInstance,
+    Dict,
+    InputDefinition,
+    ModeDefinition,
+    Output,
+    OutputDefinition,
+    ResourceDefinition,
+    execute_pipeline,
+    file_relative_path,
+    pipeline,
+    solid,
+)
+import dask.dataframe as dd
+from dask.dataframe.utils import assert_eq
+
 from dagster_dask import DataFrame, dask_resource
 
 
 @solid(
-    input_defs=[InputDefinition(dagster_type=DataFrame, name="df")],
-    output_defs=[OutputDefinition(dagster_type=DataFrame, name="df")],
+    output_defs=[
+        OutputDefinition(dagster_type=Dict, name="scheduler_info"),
+        OutputDefinition(dagster_type=Dict, name="nthreads"),
+    ],
     required_resource_keys={"dask"},
 )
-def passthrough(_, df):
-    return df
+def scheduler_info_solid(context):
+    client = context.resources.dask.client
+
+    yield Output(client.scheduler_info(), "scheduler_info")
+    yield Output(client.nthreads(), "nthreads")
 
 
 @pipeline(
     mode_defs=[
-        ModeDefinition(resource_defs={"dask": ResourceDefinition.hardcoded_resource(dask_resource)})
+        ModeDefinition(resource_defs={"dask": dask_resource})
     ]
 )
-def dask_pipeline():
-    return passthrough()
+def scheduler_info_pipeline():
+    return scheduler_info_solid()
+
+
+def test_custom_local_cluster():
+    cluster_config = {
+        "n_workers": 2,
+        "threads_per_worker": 3,
+        "dashboard_address": None,
+    }
+
+    run_config = {"resources": {"dask": {"config": {"cluster": {"local": cluster_config}}}}}
+    result = execute_pipeline(scheduler_info_pipeline, run_config=run_config, instance=DagsterInstance.local_temp())
+
+    scheduler_info = result.result_for_solid("scheduler_info_solid").output_value("scheduler_info")
+    assert len(scheduler_info["workers"]) == cluster_config["n_workers"]
+
+    nthreads = result.result_for_solid("scheduler_info_solid").output_value("nthreads")
+    assert all(v == cluster_config["threads_per_worker"] for v in nthreads.values())
