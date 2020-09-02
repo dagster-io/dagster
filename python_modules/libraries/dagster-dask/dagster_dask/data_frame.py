1eddd114a6596b0de31a904873a1726a3fb26b44
King Chung Huang <kinghuang@mac.com>
2020-09-02 12:39:24 -0600

    Move DataFrameUtilities to utils file

--- python_modules/libraries/dagster-dask/dagster_dask/data_frame.py
+++ python_modules/libraries/dagster-dask/dagster_dask/data_frame.py
@@ -23,7 +23,7 @@ from dagster import (
 )
 import dask.dataframe as dd
 
-from .utils import sanitize_column_names
+from .utils import DataFrameUtilities
 
 
 WriteCompressionTextOptions = Enum(
@@ -337,35 +337,6 @@ DataFrameToTypes = {
             ),
         },
     },
-}
-
-
-DataFrameUtilities = {
-    "sample": {
-        "function": dd.DataFrame.sample,
-        "options": Field(Float, is_required=False, description="Sample a random fraction of items.")
-    },
-    "repartition": {
-        "function": dd.DataFrame.repartition,
-        "options": Field(
-            Selector(
-                {
-                    "npartitions": Field(Int, description="Number of partitions of output."),
-                    "partition_size": Field(Any, description="Max number of bytes of memory for each partition."),
-                }
-            ),
-            is_required=False,
-            description="Repartition dataframe along new divisions.",
-        )  
-    },
-    "reset_index": {
-        "function": dd.DataFrame.reset_index,
-        "options": Field(Bool, is_required=False, description="Reset the index to the default index."),
-    },
-    "sanitize_column_names": {
-        "function": sanitize_column_names,
-        "options": Field(Bool, is_required=False, description="Modify column names for greater compatibility."),   
-    }
 }
 
 
--- python_modules/libraries/dagster-dask/dagster_dask/utils.py
+++ python_modules/libraries/dagster-dask/dagster_dask/utils.py
@@ -1,3 +1,12 @@
+from dagster import (
+    Any, 
+    Bool,
+    Field,
+    Int,
+    Permissive,
+    Selector,
+    String,
+)
 import dask.dataframe as dd
 
 
@@ -5,3 +14,32 @@ def sanitize_column_names(df: dd.DataFrame) -> dd.DataFrame:
     df.columns = map(str.lower, df.columns)
     
     return df
+
+
+DataFrameUtilities = {
+    "sample": {
+        "function": dd.DataFrame.sample,
+        "options": Field(Float, is_required=False, description="Sample a random fraction of items.")
+    },
+    "repartition": {
+        "function": dd.DataFrame.repartition,
+        "options": Field(
+            Selector(
+                {
+                    "npartitions": Field(Int, description="Number of partitions of output."),
+                    "partition_size": Field(Any, description="Max number of bytes of memory for each partition."),
+                }
+            ),
+            is_required=False,
+            description="Repartition dataframe along new divisions.",
+        )  
+    },
+    "reset_index": {
+        "function": dd.DataFrame.reset_index,
+        "options": Field(Bool, is_required=False, description="Reset the index to the default index."),
+    },
+    "sanitize_column_names": {
+        "function": sanitize_column_names,
+        "options": Field(Bool, is_required=False, description="Modify column names for greater compatibility."),   
+    }
+}
