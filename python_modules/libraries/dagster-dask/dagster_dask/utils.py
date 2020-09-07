16bdb7f54cb77e83f3d2187051d435e9035a7bf8
King Chung Huang <kinghuang@mac.com>
2020-09-07 17:54:56 -0600

    Read enabled flag
    The sanitize_column_names config is defined as a flag. Take in the flag
    value, and don't do anything if it is False.

--- python_modules/libraries/dagster-dask/dagster_dask/utils.py
+++ python_modules/libraries/dagster-dask/dagster_dask/utils.py
@@ -12,8 +12,9 @@ from dagster import (
 import dask.dataframe as dd
 
 
-def sanitize_column_names(df: dd.DataFrame) -> dd.DataFrame:
-    df.columns = map(str.lower, df.columns)
+def sanitize_column_names(df: dd.DataFrame, enabled) -> dd.DataFrame:
+    if enabled:
+        df.columns = map(str.lower, df.columns)
     
     return df
 
