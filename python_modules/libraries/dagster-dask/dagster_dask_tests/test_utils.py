4ebb17cc9a308584e415f9e3c6238e773f2fc639
King Chung Huang <kinghuang@mac.com>
2020-09-07 17:19:02 -0600

    Refactor test and validate input with output

--- python_modules/libraries/dagster-dask/dagster_dask_tests/test_utils.py
+++ python_modules/libraries/dagster-dask/dagster_dask_tests/test_utils.py
@@ -4,6 +4,7 @@ from dagster import (
     file_relative_path,
     solid,
 )
+import dask.dataframe as dd
 
 from dagster_dask import DataFrame
 
@@ -13,29 +14,33 @@ def passthrough(_, input_df: DataFrame) -> DataFrame:
     return input_df
 
 
-def test_single_utilities():
-    file_name = file_relative_path(__file__, "num.csv")
-    
-    def generate_config(**df_opts):
-        return {
-            "solids": {
-                "passthrough": {
-                    "inputs": {
-                        "input_df": {
-                            "read": {
-                                "csv": {
-                                    "path": file_name,
-                                },
+def generate_config(path, **df_opts):
+    return {
+        "solids": {
+            "passthrough": {
+                "inputs": {
+                    "input_df": {
+                        "read": {
+                            "csv": {
+                                "path": path,
                             },
-                            **df_opts,
                         },
+                        **df_opts,
                     },
                 },
             },
-        }
+        },
+    }
 
-    run_config = generate_config(sample={"frac": 0.5})
+
+def test_sample_utility():
+    path = file_relative_path(__file__, "num.csv")
+    frac = 0.5
+
+    input_df = dd.read_csv(path)
+
+    run_config = generate_config(path, sample={"frac": 0.5})
     result = execute_solid(passthrough, run_config=run_config)
+    output_df = result.output_value()
 
-    df = result.output_value()
-    assert len(df) == 1
+    assert len(input_df) * frac == len(output_df)
