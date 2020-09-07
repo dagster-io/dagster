b283d35050d1c31257054c0f0c2a1057a1088df1
King Chung Huang <kinghuang@mac.com>
2020-09-07 16:13:17 -0600

    Map all method options
    For the utilities that are direct mappings of DataFrame methods, adjust
    the options to comprehensively match the method args and kwargs.

--- python_modules/libraries/dagster-dask/dagster_dask/utils.py
+++ python_modules/libraries/dagster-dask/dagster_dask/utils.py
@@ -2,9 +2,11 @@ from dagster import (
     Any, 
     Bool,
     Field,
+    Float,
     Int,
     Permissive,
     Selector,
+    Shape,
     String,
 )
 import dask.dataframe as dd
@@ -19,38 +21,39 @@ def sanitize_column_names(df: dd.DataFrame) -> dd.DataFrame:
 DataFrameUtilities = {
     "sample": {
         "function": dd.DataFrame.sample,
-        "options": Field(Float, is_required=False, description="Sample a random fraction of items."),
+        "options": Shape({
+            "frac": Field(Float, is_required=False, description="Fraction of axis items to return."),
+            "replace": Field(Bool, is_required=False, description="Sample with or without replacement."),
+            "random_state": Field(Int, is_required=False, description="Create RandomState with this as the seed."),
+        }),
     },
     "set_index": {
         "function": dd.DataFrame.set_index,
-        "options": Permissive(
-            {
-                "other": Field(String, is_required=True, description="Set index to specified column."),
-                "drop": Field(Bool, is_required=False, description="Delete columns to be used as the new index."),
-                "sorted": Field(Bool, is_required=False, description="If the index column is already sorted in increasing order."),
-                "divisions": Field(Any, is_required=False, description="Known values on which to separate index values of the partitions."),
-            }
-        ),
+        "options": Permissive({
+            "other": Field(String, is_required=True, description="Set index to specified column."),
+            "drop": Field(Bool, is_required=False, description="Delete columns to be used as the new index."),
+            "sorted": Field(Bool, is_required=False, description="If the index column is already sorted in increasing order."),
+            "divisions": Field(Any, is_required=False, description="Known values on which to separate index values of the partitions."),   
+        }),
     },
     "repartition": {
         "function": dd.DataFrame.repartition,
-        "options": Field(
-            Selector(
-                {
-                    "npartitions": Field(Int, description="Number of partitions of output."),
-                    "partition_size": Field(Any, description="Max number of bytes of memory for each partition."),
-                }
-            ),
-            is_required=False,
-            description="Repartition dataframe along new divisions.",
-        )  
+        "options": Shape({
+            "divisions": Field(Any, is_required=False, description="List of partitions to be used."),
+            "npartitions": Field(Int, is_required=False, description="Number of partitions of output."),
+            "partition_size": Field(Any, is_required=False, description="Max number of bytes of memory for each partition."),
+            "freq": Field(String, is_required=False, description="A period on which to partition timeseries data."),
+            "force": Field(Bool, is_required=False, description="Allows the expansion of the existing divisions."),
+        })
     },
     "reset_index": {
         "function": dd.DataFrame.reset_index,
-        "options": Field(Bool, is_required=False, description="Reset the index to the default index."),
+        "options": Shape({
+            "drop": Field(Bool, is_required=False, description="Do not try to insert index into dataframe columns."),
+        })
     },
     "sanitize_column_names": {
         "function": sanitize_column_names,
         "options": Field(Bool, is_required=False, description="Modify column names for greater compatibility."),   
-    }
+    },
 }
