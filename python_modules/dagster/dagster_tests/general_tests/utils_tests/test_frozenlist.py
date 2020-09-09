b5746f11b7593e8e85f0d26b2742ed64f1af563b
King Chung Huang <kinghuang@mac.com>
2020-09-08 20:35:18 -0600

    Test pickling frozenlist

---
+++ python_modules/dagster/dagster_tests/general_tests/utils_tests/test_frozenlist.py
@@ -0,0 +1,12 @@
+import pickle
+import pytest
+
+from dagster.utils import frozendict
+
+
+def test_pickle_frozenlist():
+    orig_list = [1, "a", {}]
+    data = pickle.dumps(orig_list)
+    loaded_list = pickle.loads(data)
+
+    assert orig_list == loaded_list
