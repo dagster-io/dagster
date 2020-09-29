edb84ce04bf2e4afc478f6f420e96bf0e9449d5f
Bob Chen <bob@elementl.com>
2020-09-29 15:53:56 -0400

    Adds scrolling and line wrapping to Examples index page.
    Summary:
    Fixes web UI issues on Examples index page, as described in [[ https://dagster.slack.com/archives/C012YCX5P8X/p1601398854000200 | this Slack message ]].
    
    **Before**
    {F305672}
    - Unable to scroll down to more examples when the viewport is short (notice there is no scrollbar).
    - Unable to scroll right to the "Open" link when the viewport is thin. This is a symptom of long titles and descriptions.
    
    **After removing "position:fixed"**
    {F305669}
    - Able to scroll down to more examples when the viewport is short (notice there is now a scrollbar).
    - Still unable to scroll right to the "Open" link when the viewport is thin. This is a symptom of long titles and descriptions.
    
    **After removing "whitespace-no-wrap"**
    {F305675}
    - Able to scroll down to more examples when the viewport is short (notice there is now a scrollbar).
    - Able to see "Open" link when viewport is thin. The sacrifice here is that long titles and descriptions will wrap, which causes inconsistent row heights in the table.
    
    Test Plan: Run documentation website locally and visually check across browsers && buildkite ofc
    
    Reviewers: sashank, yuhan, nate
    
    Reviewed By: yuhan
    
    Differential Revision: https://dagster.phacility.com/D4592

--- docs/next/src/pages/examples/index.tsx
+++ docs/next/src/pages/examples/index.tsx
@@ -18,7 +18,7 @@ const Modules: NextPage<{ body: string }> = (props) => {
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block sm:px-6 lg:px-8">
             <div className="border overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="fixed divide-y divide-gray-200 border border-rounded-md">
+              <table className="divide-y divide-gray-200 border border-rounded-md">
                 <thead>
                   <tr>
                     <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
@@ -34,10 +34,10 @@ const Modules: NextPage<{ body: string }> = (props) => {
                 <tbody>
                   {body.map((example: any, i: number) => (
                     <tr className={i % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
-                      <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
+                      <td className="px-6 py-4 text-sm leading-5 font-medium text-gray-900">
                         {example.title}
                       </td>
-                      <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                      <td className="px-6 py-4 text-sm leading-5 text-gray-500">
                         {example.description}
                       </td>
 
