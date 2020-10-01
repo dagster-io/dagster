a3db99ac8238f5e84e576ea8ef3f7fa912547810
alangenfeld <alex@elementl.com>
2020-10-01 16:31:53 -0500

    preset deep merge test
    Summary: test for PR 2911
    
    Test Plan: bk after rebase
    
    Reviewers: sandyryza, bob, rexledesma
    
    Reviewed By: sandyryza
    
    Differential Revision: https://dagster.phacility.com/D4615

--- python_modules/dagster/dagster_tests/core_tests/test_presets.py
+++ python_modules/dagster/dagster_tests/core_tests/test_presets.py
@@ -19,3 +19,35 @@ def test_empty_preset():
     empty_preset = PresetDefinition("empty")
     assert empty_preset.run_config == None
     assert empty_preset.get_environment_yaml() == "{}\n"
+
+
+def test_merge():
+    base_preset = PresetDefinition(
+        "base",
+        run_config={
+            "context": {
+                "unittest": {
+                    "resources": {
+                        "db_resource": {
+                            "config": {"user": "some_user", "password": "some_password"}
+                        }
+                    }
+                }
+            }
+        },
+    )
+
+    new_preset = base_preset.with_additional_config(
+        {"context": {"unittest": {"resources": {"another": {"config": "not_sensitive"}}}}}
+    )
+
+    assert new_preset.run_config == {
+        "context": {
+            "unittest": {
+                "resources": {
+                    "db_resource": {"config": {"user": "some_user", "password": "some_password"}},
+                    "another": {"config": "not_sensitive"},
+                }
+            }
+        }
+    }
