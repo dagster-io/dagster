363a20c869b1430933a649e949845a406a7cb2ca
Rex Ledesma <rex@elementl.com>
2020-10-02 11:43:25 -0700

    Add missing packaging dependency in dagster-aws
    Summary: As the title.
    
    Test Plan: bk, eks dev works
    
    Reviewers: johann, alangenfeld, nate
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D4651

--- python_modules/libraries/dagster-aws/setup.py
+++ python_modules/libraries/dagster-aws/setup.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         ],
         packages=find_packages(exclude=["test"]),
         include_package_data=True,
-        install_requires=["boto3>=1.9", "dagster", "psycopg2-binary", "requests",],
+        install_requires=["boto3>=1.9", "dagster", "packaging", "psycopg2-binary", "requests"],
         extras_require={"pyspark": ["dagster-pyspark"]},
         zip_safe=False,
     )
