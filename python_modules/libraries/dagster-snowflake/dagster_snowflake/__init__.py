83302c2544fe14208cf0f4ffc083b8a2edc6aec1
Sandy Ryza <sandy@elementl.com>
2020-10-02 09:52:41 -0700

    include SnowflakeConnection in API docs
    Test Plan: manual inspection
    
    Reviewers: sashank, yuhan, nate
    
    Reviewed By: yuhan
    
    Differential Revision: https://dagster.phacility.com/D4638

--- python_modules/libraries/dagster-snowflake/dagster_snowflake/__init__.py
+++ python_modules/libraries/dagster-snowflake/dagster_snowflake/__init__.py
@@ -1,9 +1,9 @@
 from dagster.core.utils import check_dagster_package_version
 
-from .resources import snowflake_resource
+from .resources import SnowflakeConnection, snowflake_resource
 from .solids import snowflake_solid_for_query
 from .version import __version__
 
 check_dagster_package_version("dagster-snowflake", __version__)
 
-__all__ = ["snowflake_solid_for_query", "snowflake_resource"]
+__all__ = ["snowflake_solid_for_query", "snowflake_resource", "SnowflakeConnection"]
