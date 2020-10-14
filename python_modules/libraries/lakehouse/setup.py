c6cee64f699dbb1fe26e6117656089dcf590ce4c
Sandy Ryza <sandy@elementl.com>
2020-10-14 13:37:25 -0700

    publish lakehouse module
    Summary: This wasn't working last time.  I discovered it's because the setup.py for lakehouse wasn't doing all the fancy stuff that the other setup.pys are doing.
    
    Test Plan:
    ```
    Python 3.6.8 (default, Apr  3 2020, 10:47:04)
    [GCC 4.2.1 Compatible Apple LLVM 10.0.0 (clang-1000.11.45.5)] on darwin
    Type "help", "copyright", "credits" or "license" for more information.
    >>> from automation.release.dagster_module import DagsterModule
    >>> lakehouse = DagsterModule(“lakehouse”, is_library=True)
    >>> lakehouse.publish(dry_run=True)
    >>> lakehouse.publish(dry_run=False)
    ```
    
    Reviewers: nate, dgibson
    
    Reviewed By: nate
    
    Differential Revision: https://dagster.phacility.com/D4640

--- python_modules/libraries/lakehouse/setup.py
+++ python_modules/libraries/lakehouse/setup.py
@@ -1,11 +1,30 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+
+def get_version():
+    version = {}
+    with open("lakehouse/version.py") as fp:
+        exec(fp.read(), version)  # pylint: disable=W0122
+
+    return version["__version__"]
+
 
 if __name__ == "__main__":
     setup(
         name="lakehouse",
+        version=get_version(),
         author="Elementl",
         author_email="hello@elementl.com",
         license="Apache-2.0",
+        url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/lakehouse",
         install_requires=["dagster"],
+        packages=find_packages(exclude=["test"]),
         entry_points={"console_scripts": ["house = lakehouse.cli:main"]},
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        zip_safe=False,
     )
