6a537fbf25860c0d2b91540d6288daaa3b75a9a3
Дмитрий Матасов <dmatasov@bestplace.pro>
2020-10-14 23:58:29 +0300

    isort

--- python_modules/libraries/dagster-pandas/dagster_pandas_tests/test_config_driven_df.py
+++ python_modules/libraries/dagster-pandas/dagster_pandas_tests/test_config_driven_df.py
@@ -4,6 +4,8 @@ import os
 
 import pandas as pd
 import pytest
+from dagster_pandas import DataFrame
+
 from dagster import (
     DagsterInvalidConfigError,
     InputDefinition,
@@ -14,7 +16,6 @@ from dagster import (
 )
 from dagster.utils import file_relative_path
 from dagster.utils.test import get_temp_file_name
-from dagster_pandas import DataFrame
 
 
 def check_parquet_support():
