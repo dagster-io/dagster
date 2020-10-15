84a37997d50899deda766a5f53abae58542a36b0
Isaac Hellendag <dish@elementl.com>
2020-10-14 19:11:44 -0500

    [dagit] Use parent snapshot ID to determine current-ness of snapshot
    Summary:
    In cases where a snapshot is a child of a parent snapshot, use that information to determine whether to tag the snapshot as "current". That is, if a parent snapshot is the current state of the pipeline, it should be labeled as "current".
    
    - Add nullable `parentSnapshotId` to `PipelineSnapshot` in GraphQL
    - Break apart PipelineNav
      - Query for parent snapshot in snapshot pipeline nav, with a brief loading state and a set width to avoid making the tabs jump
    
    Test Plan: View snapshot pages for different pipelines, verify that the "current"/"snapshot" tag shows up correctly.
    
    Reviewers: alangenfeld, max, sandyryza
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D4789

--- js_modules/dagit/src/nav/PipelineNav.tsx
+++ js_modules/dagit/src/nav/PipelineNav.tsx
@@ -1,13 +1,25 @@
-import {IBreadcrumbProps, Tag} from '@blueprintjs/core';
+import {IBreadcrumbProps, IconName, Tag} from '@blueprintjs/core';
+import gql from 'graphql-tag';
 import React from 'react';
+import {useQuery} from 'react-apollo';
 import {useRouteMatch} from 'react-router-dom';
 import styled from 'styled-components';
 
-import {useRepository} from 'src/DagsterRepositoryContext';
-import {explorerPathFromString, explorerPathToString} from 'src/PipelinePathUtils';
+import {useActivePipelineForName, useRepository} from 'src/DagsterRepositoryContext';
+import {
+  explorerPathFromString,
+  explorerPathToString,
+  PipelineExplorerPath,
+} from 'src/PipelinePathUtils';
 import {TopNav} from 'src/nav/TopNav';
 
-const pipelineTabs = {
+interface Tab {
+  title: string;
+  pathComponent: string;
+  icon: IconName;
+}
+
+const pipelineTabs: {[key: string]: Tab} = {
   overview: {title: 'Overview', pathComponent: 'overview', icon: 'dashboard'},
   definition: {title: 'Definition', pathComponent: '', icon: 'diagram-tree'},
   playground: {
@@ -30,7 +42,7 @@ const pipelineTabs = {
 const snapshotOrder = ['definition', 'runs'];
 const currentOrder = ['overview', 'definition', 'playground', 'runs', 'partitions'];
 
-export function tabForPipelinePathComponent(component?: string) {
+export function tabForPipelinePathComponent(component?: string): Tab {
   const tabList = Object.keys(pipelineTabs);
   const match =
     tabList.find((t) => pipelineTabs[t].pathComponent === component) ||
@@ -38,80 +50,132 @@ export function tabForPipelinePathComponent(component?: string) {
   return pipelineTabs[match];
 }
 
-interface PipelineNavProps {
-  isHistorical: boolean;
-  isSnapshot: boolean;
-}
-
-export const PipelineNav: React.FunctionComponent<PipelineNavProps> = (props: PipelineNavProps) => {
-  const {isHistorical, isSnapshot} = props;
-  const repository = useRepository();
-  const match = useRouteMatch<{tab: string; selector: string}>(['/pipeline/:selector/:tab?']);
-
-  const active = tabForPipelinePathComponent(match.params.tab);
-  const explorerPath = explorerPathFromString(match.params.selector);
-  const hasPartitionSet = repository?.partitionSets
-    .map((x) => x.pipelineName)
-    .includes(explorerPath.pipelineName);
-
-  // When you click one of the top tabs, it resets the snapshot you may be looking at
-  // in the Definition tab and also clears solids from the path
+const tabForKey = (explorerPath: PipelineExplorerPath) => {
   const explorerPathForTab = explorerPathToString({
     ...explorerPath,
     pathSolids: [],
   });
 
-  const breadcrumbs: IBreadcrumbProps[] = [{text: 'Pipelines', icon: 'diagram-tree'}];
+  // When you click one of the top tabs, it resets the snapshot you may be looking at
+  // in the Definition tab and also clears solids from the path
+  return (key: string) => {
+    const tab = pipelineTabs[key];
+    return {
+      text: tab.title,
+      href: `/pipeline/${explorerPathForTab}${tab.pathComponent}`,
+    };
+  };
+};
 
-  if (isSnapshot) {
-    const tag = isHistorical ? (
-      <Tag intent="warning" minimal>
-        Historical snapshot
-      </Tag>
-    ) : (
-      <Tag intent="success" minimal>
-        Current snapshot
-      </Tag>
-    );
-
-    breadcrumbs.push(
-      {
-        text: explorerPath.pipelineName,
-        href: `/pipeline/${explorerPath.pipelineName}`,
-      },
-      {
-        text: (
-          <div style={{alignItems: 'center', display: 'flex', flexDirection: 'row'}}>
-            <Mono>{explorerPath.snapshotId}</Mono>
-            {tag}
-          </div>
-        ),
-      },
-    );
-  } else {
-    breadcrumbs.push({
-      text: explorerPath.pipelineName,
-    });
+const SNAPSHOT_PARENT_QUERY = gql`
+  query SnapshotQuery($snapshotId: String!) {
+    pipelineSnapshotOrError(snapshotId: $snapshotId) {
+      ... on PipelineSnapshot {
+        parentSnapshotId
+      }
+    }
   }
+`;
 
-  const tabForKey = React.useCallback(
-    (key) => {
-      const tab = pipelineTabs[key];
-      return {
-        text: tab.title,
-        href: `/pipeline/${explorerPathForTab}${tab.pathComponent}`,
-      };
+interface SnapshotNavProps {
+  active: Tab;
+  explorerPath: PipelineExplorerPath;
+}
+
+const SnapshotNav = (props: SnapshotNavProps) => {
+  const {active, explorerPath} = props;
+  const {pipelineName, snapshotId} = explorerPath;
+
+  const currentPipelineState = useActivePipelineForName(pipelineName);
+  const currentSnapshotID = currentPipelineState?.pipelineSnapshotId;
+
+  const {data, loading} = useQuery(SNAPSHOT_PARENT_QUERY, {
+    variables: {snapshotId},
+  });
+
+  const tag = () => {
+    if (loading) {
+      return (
+        <Tag intent="none" minimal>
+          ...
+        </Tag>
+      );
+    }
+
+    if (
+      !currentSnapshotID ||
+      (currentSnapshotID !== snapshotId &&
+        data?.pipelineSnapshotOrError?.parentSnapshotId !== currentSnapshotID)
+    ) {
+      return (
+        <Tag intent="warning" minimal>
+          Snapshot
+        </Tag>
+      );
+    }
+
+    return (
+      <Tag intent="success" minimal>
+        Current
+      </Tag>
+    );
+  };
+
+  const breadcrumbs: IBreadcrumbProps[] = [
+    {text: 'Pipelines', icon: 'diagram-tree'},
+    {
+      text: explorerPath.pipelineName,
+      href: `/pipeline/${explorerPath.pipelineName}`,
     },
-    [explorerPathForTab],
-  );
+    {
+      text: (
+        <div style={{alignItems: 'center', display: 'flex', flexDirection: 'row'}}>
+          <Mono>{explorerPath.snapshotId}</Mono>
+          <div style={{width: '70px'}}>{tag()}</div>
+        </div>
+      ),
+    },
+  ];
 
-  const tabs = React.useMemo(() => {
-    return isSnapshot
-      ? snapshotOrder.map(tabForKey)
-      : currentOrder.filter((key) => hasPartitionSet || key !== 'partitions').map(tabForKey);
-  }, [hasPartitionSet, isSnapshot, tabForKey]);
+  const tabs = snapshotOrder.map(tabForKey(explorerPath));
+  return <TopNav activeTab={active.title} breadcrumbs={breadcrumbs} tabs={tabs} />;
+};
+
+interface CurrentPipelineNavProps {
+  active: Tab;
+  explorerPath: PipelineExplorerPath;
+}
+
+const CurrentPipelineNav = (props: CurrentPipelineNavProps) => {
+  const {active, explorerPath} = props;
+  const repository = useRepository();
+
+  const hasPartitionSet = repository?.partitionSets
+    .map((x) => x.pipelineName)
+    .includes(explorerPath.pipelineName);
+
+  const breadcrumbs: IBreadcrumbProps[] = [
+    {text: 'Pipelines', icon: 'diagram-tree'},
+    {text: explorerPath.pipelineName},
+  ];
+
+  const tabs = currentOrder
+    .filter((key) => hasPartitionSet || key !== 'partitions')
+    .map(tabForKey(explorerPath));
 
   return <TopNav activeTab={active.title} breadcrumbs={breadcrumbs} tabs={tabs} />;
+};
+
+export const PipelineNav: React.FunctionComponent<{}> = () => {
+  const match = useRouteMatch<{tab: string; selector: string}>(['/pipeline/:selector/:tab?']);
+  const active = tabForPipelinePathComponent(match.params.tab);
+  const explorerPath = explorerPathFromString(match.params.selector);
+  const {snapshotId} = explorerPath;
+
+  if (snapshotId) {
+    return <SnapshotNav active={active} explorerPath={explorerPath} />;
+  }
+  return <CurrentPipelineNav active={active} explorerPath={explorerPath} />;
 };
 
 const Mono = styled.div`
---
+++ js_modules/dagit/src/nav/types/SnapshotQuery.ts
@@ -0,0 +1,27 @@
+// @generated
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: SnapshotQuery
+// ====================================================
+
+export interface SnapshotQuery_pipelineSnapshotOrError_PipelineSnapshotNotFoundError {
+  __typename: "PipelineSnapshotNotFoundError" | "PipelineNotFoundError" | "PythonError";
+}
+
+export interface SnapshotQuery_pipelineSnapshotOrError_PipelineSnapshot {
+  __typename: "PipelineSnapshot";
+  parentSnapshotId: string | null;
+}
+
+export type SnapshotQuery_pipelineSnapshotOrError = SnapshotQuery_pipelineSnapshotOrError_PipelineSnapshotNotFoundError | SnapshotQuery_pipelineSnapshotOrError_PipelineSnapshot;
+
+export interface SnapshotQuery {
+  pipelineSnapshotOrError: SnapshotQuery_pipelineSnapshotOrError;
+}
+
+export interface SnapshotQueryVariables {
+  snapshotId: string;
+}
