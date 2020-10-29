f0244c807da42ef4d1c20a01eec21175b8867b7a
Nicholas Schrock <schrockn@elementl.com>
2020-10-29 15:06:25 -0700

    use instance_for_test in test_event_callback function
    Summary: I was looking at this test investigating flakiness and I notice it was using the forbidden method for testing (DagsterInstance.local_temp()) I doubt this will fix the issue described in #3169 but a good excuse to replace this callsite.
    
    Test Plan: BK
    
    Reviewers: max, dgibson
    
    Reviewed By: dgibson
    
    Differential Revision: https://dagster.phacility.com/D4972

--- python_modules/libraries/dagstermill/dagstermill_tests/test_event_callback.py
+++ python_modules/libraries/dagstermill/dagstermill_tests/test_event_callback.py
@@ -5,7 +5,7 @@ from dagster.core.definitions.reconstructable import ReconstructablePipeline
 from dagster.core.events import DagsterEventType
 from dagster.core.events.log import EventRecord
 from dagster.core.execution.api import execute_run
-from dagster.core.instance import DagsterInstance
+from dagster.core.test_utils import instance_for_test
 
 
 def test_event_callback_logging():
@@ -20,23 +20,23 @@ def test_event_callback_logging():
         "dagstermill.examples.repository", "define_hello_logging_pipeline",
     )
     pipeline_def = pipeline.get_definition()
-    instance = DagsterInstance.local_temp()
+    with instance_for_test() as instance:
 
-    pipeline_run = instance.create_run_for_pipeline(pipeline_def)
+        pipeline_run = instance.create_run_for_pipeline(pipeline_def)
 
-    instance.watch_event_logs(pipeline_run.run_id, -1, _event_callback)
+        instance.watch_event_logs(pipeline_run.run_id, -1, _event_callback)
 
-    execute_run(pipeline, pipeline_run, instance)
+        execute_run(pipeline, pipeline_run, instance)
 
-    passed_before_timeout = False
-    retries = 5
-    while retries > 0:
-        time.sleep(0.333)
-        if DagsterEventType.PIPELINE_FAILURE in events.keys():
-            break
-        if DagsterEventType.PIPELINE_SUCCESS in events.keys():
-            passed_before_timeout = True
-            break
-        retries -= 1
+        passed_before_timeout = False
+        retries = 5
+        while retries > 0:
+            time.sleep(0.333)
+            if DagsterEventType.PIPELINE_FAILURE in events.keys():
+                break
+            if DagsterEventType.PIPELINE_SUCCESS in events.keys():
+                passed_before_timeout = True
+                break
+            retries -= 1
 
-    assert passed_before_timeout
+        assert passed_before_timeout
