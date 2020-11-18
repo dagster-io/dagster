57ec7630de6b1c4fd3b30bba76217f5f831f2b28
Jordan Sanders <10291790+jmsanders@users.noreply.github.com>
2020-11-18 09:02:58 -0600

    Mock S3

--- python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_s3_file_cache.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_s3_file_cache.py
@@ -1,27 +1,19 @@
 import io
 
-import boto3
 from dagster_aws.s3 import S3FileCache, S3FileHandle
-from moto import mock_s3
 
 
-@mock_s3
-def test_s3_file_cache_file_not_present():
-    s3 = boto3.client("s3")
-    s3.create_bucket(Bucket="some-bucket")
+def test_s3_file_cache_file_not_present(s3, bucket):
     file_store = S3FileCache(
-        s3_bucket="some-bucket", s3_key="some-key", s3_session=s3, overwrite=False
+        s3_bucket=bucket.name, s3_key="some-key", s3_session=s3.meta.client, overwrite=False
     )
 
     assert not file_store.has_file_object("foo")
 
 
-@mock_s3
-def test_s3_file_cache_file_present():
-    s3 = boto3.client("s3")
-    s3.create_bucket(Bucket="some-bucket")
+def test_s3_file_cache_file_present(s3, bucket):
     file_store = S3FileCache(
-        s3_bucket="some-bucket", s3_key="some-key", s3_session=s3, overwrite=False
+        s3_bucket=bucket.name, s3_key="some-key", s3_session=s3.meta.client, overwrite=False
     )
 
     assert not file_store.has_file_object("foo")
@@ -31,23 +23,17 @@ def test_s3_file_cache_file_present():
     assert file_store.has_file_object("foo")
 
 
-@mock_s3
-def test_s3_file_cache_correct_handle():
-    s3 = boto3.client("s3")
-    s3.create_bucket(Bucket="some-bucket")
+def test_s3_file_cache_correct_handle(s3, bucket):
     file_store = S3FileCache(
-        s3_bucket="some-bucket", s3_key="some-key", s3_session=s3, overwrite=False
+        s3_bucket=bucket.name, s3_key="some-key", s3_session=s3.meta.client, overwrite=False
     )
 
     assert isinstance(file_store.get_file_handle("foo"), S3FileHandle)
 
 
-@mock_s3
-def test_s3_file_cache_write_file_object():
-    s3 = boto3.client("s3")
-    s3.create_bucket(Bucket="some-bucket")
+def test_s3_file_cache_write_file_object(s3, bucket):
     file_store = S3FileCache(
-        s3_bucket="some-bucket", s3_key="some-key", s3_session=s3, overwrite=False
+        s3_bucket=bucket.name, s3_key="some-key", s3_session=s3.meta.client, overwrite=False
     )
 
     stream = io.BytesIO("content".encode())
