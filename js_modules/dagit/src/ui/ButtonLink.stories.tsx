bdc16e726fd89ef253cb3f326a4a7253660bc967
Isaac Hellendag <dish@elementl.com>
2020-11-18 15:51:41 -0600

    ButtonLink
    Summary: Create a new generic component: a button that looks like a link. This is a common pattern for things that are not actually links to anything, but have onclick behavior and should *look* like links.
    
    Test Plan: View storybook examples, verify correct rendering and hover/focus behavior.
    
    Reviewers: bengotow, sashank
    
    Reviewed By: sashank
    
    Differential Revision: https://dagster.phacility.com/D5194

---
+++ js_modules/dagit/src/ui/ButtonLink.stories.tsx
@@ -0,0 +1,59 @@
+import {Colors} from '@blueprintjs/core';
+import {Story, Meta} from '@storybook/react/types-6-0';
+import * as React from 'react';
+
+import {Box} from 'src/ui/Box';
+import {ButtonLink, Props} from 'src/ui/ButtonLink';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: 'ButtonLink',
+  component: ButtonLink,
+} as Meta;
+
+const Template: Story<Props> = (props) => <ButtonLink {...props} />;
+
+export const ColorString = Template.bind({});
+ColorString.args = {
+  children: 'Hello world',
+  color: Colors.COBALT4,
+};
+
+export const ColorMap = Template.bind({});
+ColorMap.args = {
+  children: 'Hello world',
+  color: {
+    link: Colors.COBALT4,
+    hover: Colors.COBALT1,
+    active: Colors.ORANGE1,
+  },
+};
+
+export const HoverUnderline = Template.bind({});
+HoverUnderline.args = {
+  children: 'Hello world',
+  color: Colors.COBALT4,
+  underline: 'hover',
+};
+
+export const WhiteLinkOnBlack = () => {
+  return (
+    <Box background={Colors.BLACK} padding={16}>
+      <ButtonLink color={{link: Colors.WHITE, hover: Colors.LIGHT_GRAY1}} underline="always">
+        Hello world
+      </ButtonLink>
+    </Box>
+  );
+};
+
+export const WithinText = () => {
+  return (
+    <div>
+      Lorem ipsum{' '}
+      <ButtonLink color={{link: Colors.COBALT4, hover: Colors.BLUE1}} underline="always">
+        dolor sit
+      </ButtonLink>{' '}
+      amet edipiscing.
+    </div>
+  );
+};
---
+++ js_modules/dagit/src/ui/ButtonLink.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import styled, {css} from 'styled-components';
+
+type Color =
+  | string
+  | {
+      link: string;
+      hover?: string;
+      active?: string;
+    };
+
+type Underline = 'never' | 'always' | 'hover';
+
+export interface Props {
+  color: Color;
+  underline?: Underline;
+}
+
+const fontColor = (color: Color) => {
+  if (typeof color === 'string') {
+    return css`
+      color: ${color};
+    `;
+  }
+
+  const {link, hover, active} = color;
+  return css`
+    color: ${link};
+    ${hover ? `&:hover { color: ${hover}; }` : null}
+    ${active ? `&:active { color: ${active}; }` : null}
+  `;
+};
+
+const outlineColor = (color: Color) => {
+  if (typeof color === 'string') {
+    return color;
+  }
+  return color.link;
+};
+
+const textDecoration = (underline: Underline) => {
+  switch (underline) {
+    case 'always':
+      return css`
+        text-decoration: underline;
+      `;
+    case 'hover':
+      return css`
+        &:hover {
+          text-decoration: underline;
+        }
+      `;
+    case 'never':
+    default:
+      return null;
+  }
+};
+
+export const ButtonLink = styled(({color, underline, ...rest}) => <button {...rest} />)<Props>`
+  background: transparent;
+  border: 0;
+  cursor: pointer;
+  font-size: inherit;
+  padding: 0;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+
+  &:focus-visible {
+    outline: 1px auto ${({color}) => outlineColor(color)};
+    outline-offset: 2px;
+  }
+
+  transition: 30ms color linear;
+
+  ${({color}) => fontColor(color)}
+  ${({underline}) => textDecoration(underline)}
+`;
