1d9c4aaa48c09b0bb5bd66feccdd5b01355e856b
Isaac Hellendag <dish@elementl.com>
2020-11-20 17:16:46 -0600

    [Dagit] Add filter tabs to Runs page
    Summary: Introduce filter tabs on `/instance/runs`, essentially shortcuts to view filtered slices of runs. For any filter states where `status` does not match one of the predetermined tab states, it defaults to "All runs".
    
    Test Plan: View `/instance/runs`, verify proper rendering and behavior, including tab clicks and filter population.
    
    Reviewers: johann, prha, dgibson
    
    Reviewed By: johann
    
    Differential Revision: https://dagster.phacility.com/D5467

--- js_modules/dagit/src/runs/RunsRoot.tsx
+++ js_modules/dagit/src/runs/RunsRoot.tsx
@@ -1,11 +1,13 @@
 import {gql} from '@apollo/client';
-import {NonIdealState} from '@blueprintjs/core';
+import {Colors, NonIdealState, Tab, Tabs} from '@blueprintjs/core';
 import {IconNames} from '@blueprintjs/icons';
 import * as React from 'react';
 import {RouteComponentProps} from 'react-router';
+import styled from 'styled-components';
 
 import {CursorPaginationControls} from 'src/CursorControls';
 import {Loading} from 'src/Loading';
+import {TokenizingFieldValue} from 'src/TokenizingField';
 import {useDocumentTitle} from 'src/hooks/useDocumentTitle';
 import {RunTable} from 'src/runs/RunTable';
 import {RunsQueryRefetchContext} from 'src/runs/RunUtils';
@@ -16,11 +18,32 @@ import {
 } from 'src/runs/RunsFilter';
 import {RunsRootQuery, RunsRootQueryVariables} from 'src/runs/types/RunsRootQuery';
 import {useCursorPaginatedQuery} from 'src/runs/useCursorPaginatedQuery';
+import {PipelineRunStatus} from 'src/types/globalTypes';
+import {Box} from 'src/ui/Box';
+import {ButtonLink} from 'src/ui/ButtonLink';
 import {Group} from 'src/ui/Group';
 import {Page} from 'src/ui/Page';
-import {PageHeader} from 'src/ui/PageHeader';
+import {Heading} from 'src/ui/Text';
 
 const PAGE_SIZE = 25;
+
+const selectedTabId = (filterTokens: TokenizingFieldValue[]) => {
+  const statusTokens = new Set(
+    filterTokens.filter((token) => token.token === 'status').map((token) => token.value),
+  );
+  if (statusTokens.size === 1 && statusTokens.has('QUEUED')) {
+    return 'queued';
+  }
+  if (statusTokens.size === 2) {
+    if (statusTokens.has('STARTED') && statusTokens.has('NOT_STARTED')) {
+      return 'in-progress';
+    }
+    if (statusTokens.has('FAILURE') && statusTokens.has('SUCCESS')) {
+      return 'done';
+    }
+  }
+  return 'all';
+};
 
 export const RunsRoot: React.FunctionComponent<RouteComponentProps> = () => {
   useDocumentTitle('Runs');
@@ -46,10 +69,76 @@ export const RunsRoot: React.FunctionComponent<RouteComponentProps> = () => {
     pageSize: PAGE_SIZE,
   });
 
+  const setStatusFilter = (statuses: PipelineRunStatus[]) => {
+    const tokensMinusStatus = filterTokens.filter((token) => token.token !== 'status');
+    const statusTokens = statuses.map((status) => ({token: 'status', value: status}));
+    setFilterTokens([...statusTokens, ...tokensMinusStatus]);
+  };
+
+  const selectedTab = selectedTabId(filterTokens);
+  const tabColor = (match: string) =>
+    selectedTab === match ? Colors.BLUE1 : {link: Colors.GRAY2, hover: Colors.BLUE1};
+
   return (
     <Page>
-      <PageHeader text="Runs" />
       <Group direction="vertical" spacing={8}>
+        <Heading>Runs</Heading>
+        <Box border={{side: 'bottom', width: 1, color: Colors.LIGHT_GRAY3}}>
+          <Tabs selectedTabId={selectedTab} id="run-tabs">
+            <Tab
+              title={
+                <TabButton
+                  color={tabColor('all')}
+                  underline="never"
+                  onClick={() => setStatusFilter([])}
+                >
+                  All runs
+                </TabButton>
+              }
+              id="all"
+            />
+            <Tab
+              title={
+                <TabButton
+                  color={tabColor('queued')}
+                  underline="never"
+                  onClick={() => setStatusFilter([PipelineRunStatus.QUEUED])}
+                >
+                  Queued
+                </TabButton>
+              }
+              id="queued"
+            />
+            <Tab
+              title={
+                <TabButton
+                  color={tabColor('in-progress')}
+                  underline="never"
+                  onClick={() =>
+                    setStatusFilter([PipelineRunStatus.STARTED, PipelineRunStatus.NOT_STARTED])
+                  }
+                >
+                  In progress
+                </TabButton>
+              }
+              id="in-progress"
+            />
+            <Tab
+              title={
+                <TabButton
+                  color={tabColor('done')}
+                  underline="never"
+                  onClick={() =>
+                    setStatusFilter([PipelineRunStatus.FAILURE, PipelineRunStatus.SUCCESS])
+                  }
+                >
+                  Done
+                </TabButton>
+              }
+              id="done"
+            />
+          </Tabs>
+        </Box>
         <RunsFilter
           tokens={filterTokens}
           onChange={setFilterTokens}
@@ -107,4 +196,8 @@ export const RUNS_ROOT_QUERY = gql`
   }
 
   ${RunTable.fragments.RunTableRunFragment}
+`;
+
+const TabButton = styled(ButtonLink)`
+  line-height: 30px;
 `;
