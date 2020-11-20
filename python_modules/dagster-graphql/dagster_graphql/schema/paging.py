7981c266acfde68a54bf36f875457d135b3a5f2d
Sashank Thupukari <s@thupukari.com>
2020-11-20 18:14:50 -0500

    Remove PageInfo gqp types
    Summary: This type is not used anywhere
    
    Test Plan: bk
    
    Reviewers: dish, dgibson
    
    Reviewed By: dish
    
    Differential Revision: https://dagster.phacility.com/D5231

--- python_modules/dagster-graphql/dagster_graphql/schema/paging.py
+++ python_modules/dagster-graphql/dagster_graphql/schema/paging.py
@@ -4,14 +4,3 @@ from dagster_graphql import dauphin
 class DauphinCursor(dauphin.Int, dauphin.Scalar):
     class Meta(object):
         name = "Cursor"
-
-
-class DauphinPageInfo(dauphin.ObjectType):
-    class Meta(object):
-        name = "PageInfo"
-
-    lastCursor = dauphin.Field("Cursor")
-    hasNextPage = dauphin.Field(dauphin.Boolean)
-    hasPreviousPage = dauphin.Field(dauphin.Boolean)
-    count = dauphin.NonNull(dauphin.Int)
-    totalCount = dauphin.NonNull(dauphin.Int)
