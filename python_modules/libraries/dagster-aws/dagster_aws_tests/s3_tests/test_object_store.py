b4b87ea90e5a7b7841c03b18d45bd05257650c52
Jordan Sanders <10291790+jmsanders@users.noreply.github.com>
2020-11-22 10:16:31 -0600

    Move S3 mocks into parent directory
    Also, rename s3 and bucket to mock_s3 and mock_s3_bucket to make it more
    explicitly clear when we're using a mocked resource vs. when we're using
    a real one.

--- python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_object_store.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_object_store.py
@@ -2,12 +2,12 @@ from dagster.core.storage.object_store import DEFAULT_SERIALIZATION_STRATEGY
 from dagster_aws.s3 import S3ObjectStore
 
 
-def test_s3_object_store(bucket, caplog):
+def test_s3_object_store(mock_s3_bucket, caplog):
     key = "foo"
 
-    s3_obj_store = S3ObjectStore(bucket.name)
+    s3_obj_store = S3ObjectStore(mock_s3_bucket.name)
     res_key = s3_obj_store.set_object(key, True, DEFAULT_SERIALIZATION_STRATEGY)
-    assert res_key == "s3://{s3_bucket}/{key}".format(s3_bucket=bucket.name, key=key)
+    assert res_key == "s3://{s3_bucket}/{key}".format(s3_bucket=mock_s3_bucket.name, key=key)
 
     s3_obj_store.set_object(key, True, DEFAULT_SERIALIZATION_STRATEGY)
     assert "Removing existing S3 key" in caplog.text
@@ -22,5 +22,5 @@ def test_s3_object_store(bucket, caplog):
     assert not s3_obj_store.has_object(key)
 
     assert s3_obj_store.uri_for_key(key) == "s3://{s3_bucket}/{key}".format(
-        s3_bucket=bucket.name, key=key
+        s3_bucket=mock_s3_bucket.name, key=key
     )
--- python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_s3_file_cache.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_s3_file_cache.py
@@ -3,17 +3,23 @@ import io
 from dagster_aws.s3 import S3FileCache, S3FileHandle
 
 
-def test_s3_file_cache_file_not_present(s3, bucket):
+def test_s3_file_cache_file_not_present(mock_s3_resource, mock_s3_bucket):
     file_store = S3FileCache(
-        s3_bucket=bucket.name, s3_key="some-key", s3_session=s3.meta.client, overwrite=False
+        s3_bucket=mock_s3_bucket.name,
+        s3_key="some-key",
+        s3_session=mock_s3_resource.meta.client,
+        overwrite=False,
     )
 
     assert not file_store.has_file_object("foo")
 
 
-def test_s3_file_cache_file_present(s3, bucket):
+def test_s3_file_cache_file_present(mock_s3_resource, mock_s3_bucket):
     file_store = S3FileCache(
-        s3_bucket=bucket.name, s3_key="some-key", s3_session=s3.meta.client, overwrite=False
+        s3_bucket=mock_s3_bucket.name,
+        s3_key="some-key",
+        s3_session=mock_s3_resource.meta.client,
+        overwrite=False,
     )
 
     assert not file_store.has_file_object("foo")
@@ -23,17 +29,23 @@ def test_s3_file_cache_file_present(s3, bucket):
     assert file_store.has_file_object("foo")
 
 
-def test_s3_file_cache_correct_handle(s3, bucket):
+def test_s3_file_cache_correct_handle(mock_s3_resource, mock_s3_bucket):
     file_store = S3FileCache(
-        s3_bucket=bucket.name, s3_key="some-key", s3_session=s3.meta.client, overwrite=False
+        s3_bucket=mock_s3_bucket.name,
+        s3_key="some-key",
+        s3_session=mock_s3_resource.meta.client,
+        overwrite=False,
     )
 
     assert isinstance(file_store.get_file_handle("foo"), S3FileHandle)
 
 
-def test_s3_file_cache_write_file_object(s3, bucket):
+def test_s3_file_cache_write_file_object(mock_s3_resource, mock_s3_bucket):
     file_store = S3FileCache(
-        s3_bucket=bucket.name, s3_key="some-key", s3_session=s3.meta.client, overwrite=False
+        s3_bucket=mock_s3_bucket.name,
+        s3_key="some-key",
+        s3_session=mock_s3_resource.meta.client,
+        overwrite=False,
     )
 
     stream = io.BytesIO("content".encode())
