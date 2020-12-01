ac7ed8b7084a7407a790bdd9fd06013f357731c8
Jordan Sanders <10291790+jmsanders@users.noreply.github.com>
2020-12-01 08:24:31 -0600

    Fix IllegalLocationConstraintException
    Bumping to moto 1.3.16 to get the updated Athena endpoints introduces
    this change:
    
    https://github.com/spulec/moto/pull/2717
    
    Which (correctly) throws when S3 buckets are created outside the default
    region and a specific region is not provided.

--- python_modules/libraries/dagster-aws/dagster_aws_tests/conftest.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/conftest.py
@@ -6,7 +6,7 @@ from moto import mock_s3
 @pytest.fixture
 def mock_s3_resource():
     with mock_s3():
-        yield boto3.resource("s3")
+        yield boto3.resource("s3", region_name="us-east-1")
 
 
 @pytest.fixture
