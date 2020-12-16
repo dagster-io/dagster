c24edab9f6ab848b779fdb0d2c8307880129c229
gibsondan <daniel@elementl.com>
2020-12-16 14:12:32 -0600

    Log error/interrupt events in the execute_run CLI call
    Summary: Getting errors surfaced here would be extremely useful fro debugging.
    
    Test Plan: Integration
    
    Reviewers: johann, nate, alangenfeld
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D5646

--- python_modules/dagster/dagster/cli/api.py
+++ python_modules/dagster/dagster/cli/api.py
@@ -142,22 +142,26 @@ def _execute_run_command_body(recon_pipeline, pipeline_run_id, instance, write_s
         for event in execute_run_iterator(recon_pipeline, pipeline_run, instance):
             write_stream_fn(event)
     except (KeyboardInterrupt, DagsterExecutionInterruptedError):
-        instance.report_engine_event(
-            message="Pipeline execution terminated by interrupt", pipeline_run=pipeline_run,
+        write_stream_fn(
+            instance.report_engine_event(
+                message="Pipeline execution terminated by interrupt", pipeline_run=pipeline_run,
+            )
         )
         pipeline_run = instance.get_run_by_id(pipeline_run_id)
         if pipeline_run and (not pipeline_run.is_finished):
-            instance.report_run_canceled(pipeline_run)
+            write_stream_fn(instance.report_run_canceled(pipeline_run))
     except Exception:  # pylint: disable=broad-except
-        instance.report_engine_event(
-            "An exception was thrown during execution that is likely a framework error, "
-            "rather than an error in user code.",
-            pipeline_run,
-            EngineEventData.engine_error(serializable_error_info_from_exc_info(sys.exc_info())),
+        write_stream_fn(
+            instance.report_engine_event(
+                "An exception was thrown during execution that is likely a framework error, "
+                "rather than an error in user code.",
+                pipeline_run,
+                EngineEventData.engine_error(serializable_error_info_from_exc_info(sys.exc_info())),
+            )
         )
         pipeline_run = instance.get_run_by_id(pipeline_run_id)
         if pipeline_run and (not pipeline_run.is_finished):
-            instance.report_run_failed(pipeline_run)
+            write_stream_fn(instance.report_run_failed(pipeline_run))
     finally:
         instance.report_engine_event(
             "Process for pipeline exited (pid: {pid}).".format(pid=pid), pipeline_run,
