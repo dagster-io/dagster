11760a5296a8b1d98530d8d564baf50a2433f8cf
Isaac Hellendag <dish@elementl.com>
2021-01-07 11:27:36 -0600

    [dagit] Make sections of Instance config page linkable
    Summary:
    Enable linking to specific sections of yaml in the configuration section of "Instance details". We can use this to direct users to their configuration settings from elsewhere in Dagit, e.g. schedules.
    
    For each top-level config section, there is a link that uses a hash to jump to that section. The hash-identified section will be highlighted in light gray. If no hash (or a missing hash) is used, nothing is highlighted.
    
    Test Plan: View `/instance/config`, verify correct rendering of yaml sections. Use link icon to highlight a section. Reload the page with that hash in place on the URL, verify that the browser jumps to it.
    
    Reviewers: johann, prha, dgibson, bengotow
    
    Reviewed By: bengotow
    
    Differential Revision: https://dagster.phacility.com/D5856

--- js_modules/dagit/src/instance/InstanceConfig.tsx
+++ js_modules/dagit/src/instance/InstanceConfig.tsx
@@ -1,10 +1,12 @@
 import {gql, useQuery} from '@apollo/client';
-import {Colors, Spinner} from '@blueprintjs/core';
+import {Colors, Icon, Spinner} from '@blueprintjs/core';
 import * as React from 'react';
-import {createGlobalStyle} from 'styled-components/macro';
+import {Link, useHistory} from 'react-router-dom';
+import styled, {createGlobalStyle, css} from 'styled-components';
 
 import {HighlightedCodeBlock} from 'src/HighlightedCodeBlock';
 import {InstanceConfigQuery} from 'src/instance/types/InstanceConfigQuery';
+import {Box} from 'src/ui/Box';
 import {Group} from 'src/ui/Group';
 import {Subheading} from 'src/ui/Text';
 
@@ -19,21 +21,87 @@ const YamlShimStyle = createGlobalStyle`
   }
 `;
 
-export const InstanceConfig = () => {
+export const InstanceConfig = React.memo(() => {
+  const history = useHistory();
   const {data} = useQuery<InstanceConfigQuery>(INSTANCE_CONFIG_QUERY, {
     fetchPolicy: 'cache-and-network',
   });
+  const [hash, setHash] = React.useState(() => document.location.hash);
 
-  return data ? (
-    <Group direction="column" spacing={12}>
+  React.useEffect(() => {
+    // Once data has finished loading and rendering, scroll to hash
+    if (data) {
+      const documentHash = document.location.hash;
+      if (documentHash) {
+        const target = documentHash.slice(1);
+        document.getElementById(target)?.scrollIntoView({
+          block: 'start',
+          inline: 'nearest',
+        });
+      }
+    }
+  }, [data]);
+
+  React.useEffect(() => {
+    const unlisten = history.listen((location) => {
+      setHash(location.hash);
+    });
+
+    return () => unlisten();
+  }, [history]);
+
+  if (!data) {
+    return <Spinner size={35} />;
+  }
+
+  // Split by top-level yaml keys
+  const sections = data.instance.info.split(/\n(?=\w)/g);
+
+  return (
+    <Group direction="column" spacing={16}>
       <Subheading>{`Dagster ${data.version}`}</Subheading>
       <YamlShimStyle />
-      <HighlightedCodeBlock value={data.instance.info} language="yaml" />
+      {sections.map((section) => {
+        const [id] = section.split(/\:/);
+        const hashForSection = `#${id}`;
+        return (
+          <Box flex={{direction: 'row', alignItems: 'flex-start'}} key={id} id={id}>
+            <ConfigLink to={`/instance/config${hashForSection}`} key={id}>
+              <Icon icon="link" color={Colors.GRAY4} iconSize={11} />
+            </ConfigLink>
+            <ConfigSection highlighted={hash === hashForSection}>
+              <HighlightedCodeBlock value={section} language="yaml" />
+            </ConfigSection>
+          </Box>
+        );
+      })}
     </Group>
-  ) : (
-    <Spinner size={35} />
   );
-};
+});
+
+const ConfigLink = styled(Link)`
+  margin-right: 12px;
+  margin-top: -1px;
+  user-select: none;
+  transition: filter ease-in-out 100ms;
+
+  &:hover {
+    filter: brightness(0.4);
+  }
+`;
+
+const ConfigSection = styled.div<{highlighted: boolean}>`
+  flex-grow: 1;
+
+  ${({highlighted}) =>
+    highlighted
+      ? css`
+          background-color: ${Colors.LIGHT_GRAY3};
+          margin: -8px;
+          padding: 8px;
+        `
+      : null};
+`;
 
 export const INSTANCE_CONFIG_QUERY = gql`
   query InstanceConfigQuery {
