3d5076a71ca1cf674e010df327141f5a0ffae91b
catherinewu <cat@elementl.com>
2021-01-13 15:46:43 -0800

    Add mailing list subscription button (via Revue)
    Summary:
    Add mailing list subscription button.
    
    I briefly evaluated Revue and Mailchimp. Revue seemed more aesthetic and easy to use. Also, see https://www.getrevue.co/profile/dagster vs http://eepurl.com/hnYH7r. It's easy to switch between providers later on so just going to go with Revue
    
    Open to using a different icon
    
    Test Plan: previewed docs locally
    
    Reviewers: yuhan
    
    Reviewed By: yuhan
    
    Subscribers: rexledesma
    
    Differential Revision: https://dagster.phacility.com/D5986

--- docs/next/src/components/CommunityLinks.tsx
+++ docs/next/src/components/CommunityLinks.tsx
@@ -20,22 +20,29 @@ const Icon: React.FC<{ href: string; src: string }> = ({ href, src }) => {
 const CommunityLinks: React.FC<CommunityLinksProps> = ({ className }) => {
   return (
     <div className={cx('flex flex-row nowrap justify-around', className)}>
-      <a href="https://github.com/dagster-io/dagster">
+      <a href="https://github.com/dagster-io/dagster" target="_blank">
         <VersionedImage
           className="h-6"
           src="/assets/images/logos/github-icon.svg"
         />
       </a>
-      <a href="https://dagster-slackin.herokuapp.com/">
+      <a href="https://dagster-slackin.herokuapp.com/" target="_blank">
         <VersionedImage
           className="h-6"
           src="/assets/images/logos/slack-icon.svg"
         />
       </a>
-      <a href="https://stackoverflow.com/questions/tagged/dagster">
+      <a href="https://stackoverflow.com/questions/tagged/dagster" target="_blank">
         <VersionedImage
           className="h-6"
           src="/assets/images/logos/stack-overflow-icon.svg"
+        />
+      </a>
+      {/* FontAwesome requires that their usage license is included: https://fontawesome.com/license */}
+      <a href="https://www.getrevue.co/profile/dagster" target="_blank">
+        <VersionedImage
+          className="h-6"
+          src="/assets/images/logos/email-icon.svg"
         />
       </a>
     </div>
