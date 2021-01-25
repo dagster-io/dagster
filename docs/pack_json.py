95525e909d8cd5958fb4219a2b7b33a9ba3e6db3
Sashank Thupukari <s@thupukari.com>
2021-01-25 03:16:59 -0500

    (docs-infra-2/n) Write script to pack API docs data
    Summary: This diff creates a script `pack_json.py`, that packs all the JSON data generated by sphinx into a single file instead of 10s of files. This makes it easier to maintain the souce API data across versions and dynamically render the API data for any version in a single site.
    
    Test Plan: manual
    
    Reviewers: yuhan
    
    Reviewed By: yuhan
    
    Differential Revision: https://dagster.phacility.com/D6138

---
+++ docs/pack_json.py
@@ -0,0 +1,66 @@
+import json
+import os
+
+from dagster.utils import file_relative_path
+
+
+def read_json(filename):
+    with open(filename) as f:
+        data = json.load(f)
+        return data
+
+
+def write_json(filename, data):
+    with open(filename, "w") as f:
+        json.dump(data, f, sort_keys=True)
+
+
+def extract_route_from_path(path_to_folder, root, file):
+    sub_path = root.replace(path_to_folder, "")[1:]
+    route = sub_path.split("/") + [file.replace(".fjson", "")]
+    return route
+
+
+def add_data_at_route(root_data, route, data):
+    curr = root_data
+
+    for part in route[:-1]:
+        if not part in curr:
+            curr[part] = {}
+
+        curr = curr[part]
+
+    last = route[-1]
+    curr[last] = data
+
+
+def pack_directory_json(path_to_folder: str):
+    root_data = {}
+
+    for (root, _, files) in os.walk(path_to_folder):
+        for file in files:
+            if file.endswith(".fjson"):
+                route = extract_route_from_path(path_to_folder, root, file)
+                data = read_json(os.path.join(root, file))
+                add_data_at_route(root_data, route, data)
+
+    return root_data
+
+
+def main():
+    # TODO: Update content directory to path in Next.js folder
+    json_directory = file_relative_path(__file__, "sphinx/_build/json")
+    content_master_directory = file_relative_path(__file__, ".")
+
+    directories_to_pack = {
+        os.path.join(json_directory, "sections"): "sections.json",
+        os.path.join(json_directory, "_modules"): "modules.json",
+    }
+
+    for directory, output_file in directories_to_pack.items():
+        data = pack_directory_json(directory)
+        write_json(os.path.join(content_master_directory, output_file), data)
+
+
+if __name__ == "__main__":
+    main()
