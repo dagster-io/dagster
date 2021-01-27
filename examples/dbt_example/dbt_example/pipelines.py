45f67be56f8fffd08d6f6e1196384aeda236f2b5
yuhan <yuhan@elementl.com>
2021-01-26 16:35:30 -0800

    improve dagster-dbt example doc #3558
    Summary:
    write up about executing the pipeline, how dbt solid is created, and refer to api reference and overview when needed + dagit screenshots
    
    https://github.com/dagster-io/dagster/issues/3558
    
    Test Plan:
    screenshot of the entire page:
    {F525683}
    
    Reviewers: #docs, catherinewu, bob
    
    Reviewed By: bob
    
    Differential Revision: https://dagster.phacility.com/D6171

--- examples/dbt_example/dbt_example/pipelines.py
+++ examples/dbt_example/dbt_example/pipelines.py
@@ -13,7 +13,7 @@ from .solids import (
 
 CEREALS_DATASET_URL = "https://gist.githubusercontent.com/mgasner/bd2c0f66dff4a9f01855cfa6870b1fce/raw/2de62a57fb08da7c58d6480c987077cf91c783a1/cereal.csv"
 
-
+# start_pipeline_marker_0
 @pipeline(
     mode_defs=[
         ModeDefinition(
@@ -77,3 +77,6 @@ def dbt_example_pipeline():
     run_results = run_cereals_models(start_after=loaded)
     test_cereals_models(start_after=run_results)
     post_plot_to_slack(analyze_cereals(run_results))
+
+
+# end_pipeline_marker_0
