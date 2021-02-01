4e7ec38525cbf2b35bcfc682ec721b6f502c1298
gibsondan <daniel@elementl.com>
2021-02-01 09:33:52 -0600

    move dagit webapp commands to tox
    Summary: Goal here is to make it easy to locally simulate the test suite. Bit strange to have a suite for a library with no setup.py though?
    
    Test Plan: BK, introduce change that would break test suite and verify it breaks
    
    Reviewers: nate, max, dish, alangenfeld
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D6225

--- .buildkite/dagster-buildkite/dagster_buildkite/steps/dagit.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/dagit.py
@@ -8,21 +8,8 @@ def dagit_steps() -> List[dict]:
     return [
         StepBuilder("dagit webapp tests")
         .run(
-            "pip install -e python_modules/dagster[test] -qqq",
-            "pip install -e python_modules/dagster-graphql -qqq",
-            "pip install -e python_modules/libraries/dagster-cron -qqq",
-            "pip install -e python_modules/libraries/dagster-slack -qqq",
-            "pip install -e python_modules/dagit -qqq",
-            "pip install -e examples/legacy_examples -qqq",
             "cd js_modules/dagit",
-            "yarn install",
-            "yarn run ts",
-            "yarn run jest --collectCoverage --watchAll=false",
-            "yarn run check-prettier",
-            "yarn run check-lint",
-            "yarn run download-schema",
-            "yarn run generate-types",
-            "git diff --exit-code",
+            "tox -vv -e py37",
             "mv coverage/lcov.info lcov.dagit.$BUILDKITE_BUILD_ID.info",
             "buildkite-agent artifact upload lcov.dagit.$BUILDKITE_BUILD_ID.info",
         )
