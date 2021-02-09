508c16ffaffe4abfbc38bb74c5533b2039e982bd
Sashank Thupukari <s@thupukari.com>
2021-02-09 16:35:35 -0500

    Fix snapshot script to update all files

--- docs/next/scripts/snapshot.ts
+++ docs/next/scripts/snapshot.ts
@@ -1,36 +1,40 @@
 import { read, write } from "to-vfile";
-import remark from "remark";
-import mdx from "remark-mdx";
-import codeTransformer from "../util/codeTransformer";
 
-// This is to keep front matter in place
-import frontmatter from "remark-frontmatter";
+import codeTransformer from "../util/codeTransformer";
 import extract from "remark-extract-frontmatter";
+import fg from "fast-glob";
+import frontmatter from "remark-frontmatter";
+import mdx from "remark-mdx";
+import remark from "remark";
 import { parse as yaml } from "yaml";
 
 // Main
 (async () => {
-  const path = "./content/concepts/solids-pipelines/solids.mdx";
-  const file = await read(path);
-  const contents = await remark()
-    .use(frontmatter)
-    .use(extract, { yaml: yaml })
-    .use({
-      // This is for compatability with Prettier settings
-      settings: {
-        bullet: "-",
-        emphasis: "_",
-        strong: "*",
-        listItemIndent: "one",
-        rule: "-",
-      },
-    })
-    .use(mdx)
-    .use(codeTransformer)
-    .process(file);
+  const stream = fg.stream(["./content/**/*.mdx"]);
+  for await (const path of stream) {
+    console.log("Updating file", path);
 
-  await write({
-    path,
-    contents: Buffer.from(contents.toString()),
-  });
+    const file = await read(path);
+    const contents = await remark()
+      .use(frontmatter)
+      .use(extract, { yaml: yaml })
+      .use({
+        // This is for compatability with Prettier settings
+        settings: {
+          bullet: "-",
+          emphasis: "_",
+          strong: "*",
+          listItemIndent: "one",
+          rule: "-",
+        },
+      })
+      .use(mdx)
+      .use(codeTransformer)
+      .process(file);
+
+    await write({
+      path,
+      contents: Buffer.from(contents.toString()),
+    });
+  }
 })();
