49424274627ec7d40312bba1bd20f1d5d73981cb
Sidharth Menon <menon.sid.k@gmail.com>
2021-02-09 19:16:17 -0500

    [buildkite][core] Add BK script to check DB schemas for cross-DBAPI compatability
    Summary:
    Added checks for MySQL compatability, including:
    	1. No plain db.String columns
    	2. No db.Text + unique constraint columns
    
    Test Plan: N/A
    
    Reviewers: catherinewu, rexledesma, bob
    
    Reviewed By: rexledesma
    
    Differential Revision: https://dagster.phacility.com/D6399

--- scripts/check_schemas.py
+++ scripts/check_schemas.py
@@ -3,7 +3,8 @@ import importlib
 import pkgutil
 
 import dagster
-import sqlalchemy as sa
+import sqlalchemy as db
+from sqlalchemy.sql.schema import Column
 
 
 def check_schema_compat(schema):
@@ -12,17 +13,47 @@ def check_schema_compat(schema):
     """
     for name in dir(schema):
         obj = getattr(schema, name)
-        if isinstance(obj, sa.Table):
+        if isinstance(obj, db.Table):
             print(name, obj)
             for column in obj.columns:
-                print(f"  -{column}: {column.type}")
-                if isinstance(column.type, sa.types.VARCHAR):
-                    raise Exception(
-                        f"Column {column} is type VARCHAR; cannot use bare db.String type as "
-                        "it is incompatible with certain databases (MySQL). Use either a "
-                        "fixed-length db.String(123) or db.Text instead."
-                    )
+                print(f"  -{column}: {column.type};")
+                if isinstance(column, Column):
+                    validate_column(column)
             print()
+
+
+# These columns (in dagster/core/storage/*/schema.py) need
+# migration or a compiles directive to be compatible with MySQL.
+MYSQL_MIGRATION_PLANNED_COLUMNS = frozenset(["secondary_indexes.name", "asset_keys.asset_key"])
+
+
+def validate_column(column: Column):
+    """This function is used to validate individual DB columns in a a schema for cross-DBAPI compatibility
+    i.e.:
+        1. plain db.String not allowed (MySQL compatability)
+        2. db.Text + unique=True not allowed (MySQL compatability)
+    """
+    if (
+        isinstance(column.type, db.String)
+        and not isinstance(column.type, (db.Text, db.Unicode))
+        and column.type.length is None
+    ):
+        raise Exception(
+            f"Column {column} is type VARCHAR; cannot use bare db.String type as "
+            "it is incompatible with certain databases (MySQL). Use either a "
+            "fixed-length db.String(123) or db.Text instead."
+        )
+    elif (
+        isinstance(column.type, db.Text)
+        and column.unique
+        and str(column) not in MYSQL_MIGRATION_PLANNED_COLUMNS
+    ):
+        raise Exception(
+            f"Column {column} is type TEXT and has a UNIQUE constraint; "
+            "cannot use bare db.Text type w/ a UNIQUE constaint "
+            "since it is incompatible with certain databases (MySQL). "
+            "Use a fixed-length db.String(123) instead."
+        )
 
 
 if __name__ == "__main__":
