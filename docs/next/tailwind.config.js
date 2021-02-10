ce6d58df01a3829220b92f071be9fe38b418331b
Sashank Thupukari <s@thupukari.com>
2021-02-10 03:48:06 -0500

    better prose colors

--- docs/next/tailwind.config.js
+++ docs/next/tailwind.config.js
@@ -56,46 +56,65 @@ module.exports = {
           },
         },
         dark: {
-          css: {
-            color: theme("colors.gray.300"),
-            a: {
-              color: theme("colors.gray.200"),
-              "&:hover": {
-                color: theme("colors.gray.100"),
+          css: [
+            {
+              color: theme("colors.gray.300"),
+              '[class~="lead"]': {
+                color: theme("colors.gray.300"),
+              },
+              a: {
+                color: theme("colors.white"),
+              },
+              strong: {
+                color: theme("colors.white"),
+              },
+              "ol > li::before": {
+                color: theme("colors.gray.400"),
+              },
+              "ul > li::before": {
+                backgroundColor: theme("colors.gray.600"),
+              },
+              hr: {
+                borderColor: theme("colors.gray.200"),
+              },
+              blockquote: {
+                color: theme("colors.gray.200"),
+                borderLeftColor: theme("colors.gray.600"),
+              },
+              h1: {
+                color: theme("colors.white"),
+              },
+              h2: {
+                color: theme("colors.white"),
+              },
+              h3: {
+                color: theme("colors.white"),
+              },
+              h4: {
+                color: theme("colors.white"),
+              },
+              "figure figcaption": {
+                color: theme("colors.gray.400"),
+              },
+              code: {
+                color: theme("colors.white"),
+              },
+              "a code": {
+                color: theme("colors.white"),
+              },
+              pre: {
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
+              },
+              thead: {
+                color: theme("colors.white"),
+                borderBottomColor: theme("colors.gray.400"),
+              },
+              "tbody tr": {
+                borderBottomColor: theme("colors.gray.600"),
               },
             },
-
-            h1: {
-              color: theme("colors.gray.300"),
-            },
-            h2: {
-              color: theme("colors.gray.300"),
-            },
-            h3: {
-              color: theme("colors.gray.300"),
-            },
-            h4: {
-              color: theme("colors.gray.300"),
-            },
-            h5: {
-              color: theme("colors.gray.300"),
-            },
-            h6: {
-              color: theme("colors.gray.300"),
-            },
-
-            strong: {
-              color: theme("colors.gray.300"),
-            },
-
-            code: {
-              color: theme("colors.gray.300"),
-            },
-
-            figcaption: {
-              color: theme("colors.gray.500"),
-            },
-          },
+          ],
         },
       }),
     },
