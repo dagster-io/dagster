ced3b603d11f4161d9cdef06dce3aaea27af2868
yuhan <yuhan@elementl.com>
2021-02-11 13:52:44 -0800

    [new-docs] seo - mark all pages to noindex in dark launch
    Summary: https://github.com/garmeeh/next-seo#dangerouslySetAllPagesToNoIndex
    
    Test Plan:
    DEFAULT_SEO config applied to all pages
    {F566081}
    
    Reviewers: sashank
    
    Reviewed By: sashank
    
    Differential Revision: https://dagster.phacility.com/D6439

--- docs/next/pages/_app.tsx
+++ docs/next/pages/_app.tsx
@@ -1,15 +1,29 @@
 import "../styles/globals.css";
 import "../styles/prism.css";
 import Layout from "../layouts/MainLayout";
+import { DefaultSeo } from "next-seo";
 
 import type { AppProps } from "next/app";
+import { Container } from "next/app";
+
+const DEFAULT_SEO = {
+  title: "Dagster Docs",
+  // TODO: unset this
+  // while dark launch, we mark all pages to noindex
+  dangerouslySetAllPagesToNoIndex: true,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   const getLayout =
     // @ts-ignore
     Component.getLayout || ((page) => <Layout children={page} />);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <Container>
+      <DefaultSeo {...DEFAULT_SEO} />
+      {getLayout(<Component {...pageProps} />)}
+    </Container>
+  );
 }
 
 export default MyApp;
