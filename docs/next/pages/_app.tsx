73860c92dd7bb8396e50d5c0178b9739cdb13f59
Sashank Thupukari <s@thupukari.com>
2021-02-15 22:38:32 -0500

    Fix lint warnings

--- docs/next/pages/_app.tsx
+++ docs/next/pages/_app.tsx
@@ -1,10 +1,9 @@
 import "../styles/globals.css";
 import "../styles/prism.css";
-import Layout from "../layouts/MainLayout";
-import { DefaultSeo } from "next-seo";
 
 import type { AppProps } from "next/app";
-import { Container } from "next/app";
+import { DefaultSeo } from "next-seo";
+import Layout from "../layouts/MainLayout";
 
 const DEFAULT_SEO = {
   title: "Dagster Docs",
@@ -19,10 +18,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     Component.getLayout || ((page) => <Layout children={page} />);
 
   return (
-    <Container>
+    <>
       <DefaultSeo {...DEFAULT_SEO} />
       {getLayout(<Component {...pageProps} />)}
-    </Container>
+    </>
   );
 }
 
