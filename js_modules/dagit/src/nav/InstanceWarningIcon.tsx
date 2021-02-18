572c503f63eabca1fa23fb596b8fd563225fda6e
Isaac Hellendag <dish@elementl.com>
2021-02-18 11:53:26 -0600

    [dagit] Show a tooltip on InstanceWarningIcon
    Summary:
    In the updated left nav, the Instance warning icon could be a little more informative. This adds a tooltip with a summary of what's prompting us to show the icon at all.
    
    This way, if a user notices it, they can quickly see what's going on and know whether or not to investigate further.
    
    Test Plan: Load Dagit, force a repo error and keep daemons not running. Verify correct tooltip rendering. See screenshots.
    
    Reviewers: bengotow, prha, dgibson, johann
    
    Reviewed By: prha
    
    Differential Revision: https://dagster.phacility.com/D6574

--- js_modules/dagit/src/nav/InstanceWarningIcon.tsx
+++ js_modules/dagit/src/nav/InstanceWarningIcon.tsx
@@ -1,10 +1,11 @@
 import {gql, useQuery} from '@apollo/client';
-import {Colors, Icon} from '@blueprintjs/core';
+import {Colors, Icon, Tooltip} from '@blueprintjs/core';
 import * as React from 'react';
 import styled from 'styled-components';
 
 import {INSTANCE_HEALTH_FRAGMENT} from 'src/instance/InstanceHealthFragment';
 import {InstanceWarningQuery} from 'src/nav/types/InstanceWarningQuery';
+import {Group} from 'src/ui/Group';
 import {WorkspaceContext} from 'src/workspace/WorkspaceContext';
 
 export const InstanceWarningIcon = React.memo(() => {
@@ -15,14 +16,35 @@ export const InstanceWarningIcon = React.memo(() => {
     pollInterval: 15 * 1000,
   });
 
-  const repoErrors = locations.some((node) => node.__typename === 'RepositoryLocationLoadFailure');
-  const daemonErrors = healthData?.instance.daemonHealth.allDaemonStatuses.some(
-    (daemon) => !daemon.healthy && daemon.required,
+  const repoErrors = locations.filter(
+    (node) => node.__typename === 'RepositoryLocationLoadFailure',
   );
+  const daemonErrors =
+    healthData?.instance.daemonHealth.allDaemonStatuses.filter(
+      (daemon) => !daemon.healthy && daemon.required,
+    ) || [];
 
-  if (repoErrors || daemonErrors) {
+  if (repoErrors.length || daemonErrors.length) {
+    const content = (
+      <Group direction="column" spacing={4}>
+        {repoErrors.length ? (
+          <div>{`${repoErrors.length} ${
+            repoErrors.length === 1
+              ? 'repository location failed to load'
+              : 'repository locations failed to load'
+          }`}</div>
+        ) : null}
+        {daemonErrors.length ? (
+          <div>{`${daemonErrors.length} ${
+            daemonErrors.length === 1 ? 'daemon not running' : 'daemons not running'
+          }`}</div>
+        ) : null}
+      </Group>
+    );
     return (
-      <WarningIcon icon="warning-sign" iconSize={12} color={Colors.GOLD3} title="Warnings found" />
+      <WarningTooltip content={content} position="right">
+        <Icon icon="warning-sign" iconSize={12} color={Colors.GOLD3} title="Warnings found" />
+      </WarningTooltip>
     );
   }
 
@@ -38,6 +60,11 @@ const INSTANCE_WARNING_QUERY = gql`
   ${INSTANCE_HEALTH_FRAGMENT}
 `;
 
-const WarningIcon = styled(Icon)`
+const WarningTooltip = styled(Tooltip)`
   display: block;
+
+  .bp3-popover-target,
+  .bp3-icon {
+    display: block;
+  }
 `;
