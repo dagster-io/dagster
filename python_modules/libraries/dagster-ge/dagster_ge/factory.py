0a3cc3ec9a8c98ae14e4c3197799523d11e4f8fa
Andres Sopena Perez <andres.sopena@trivago.com>
2021-02-19 13:04:51 +0100

    [dagster-ge] Update to latest version of Great Expectations (isort applied)

--- python_modules/libraries/dagster-ge/dagster_ge/factory.py
+++ python_modules/libraries/dagster-ge/dagster_ge/factory.py
@@ -14,6 +14,9 @@ from dagster import (
     solid,
 )
 from dagster_pandas import DataFrame
+from great_expectations.render.page_renderer_util import (
+    render_multiple_validation_result_pages_markdown,
+)
 
 try:
     # ge < v0.13.0
@@ -22,9 +25,6 @@ except ImportError:
     # ge >= v0.13.0
     from great_expectations.core.util import convert_to_json_serializable
 
-from great_expectations.render.page_renderer_util import (
-    render_multiple_validation_result_pages_markdown,
-)
 
 
 @resource(config_schema={"ge_root_dir": Noneable(StringSource)})
