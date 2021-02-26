ac7bbd22d93790ee9c68a32e5386a2c274697b39
Sidharth Menon <menon.sid.k@gmail.com>
2021-02-26 17:34:09 -0500

    [dagster-postgres] cleaning up ScheduleStorage suite
    Summary:
    I noticed that the fixture `clean_storage` was only used in one place so did a bit of housekeeping to make the code simpler
    
    This diff exists for the same reason as D6714 - namely @prha's comments in D6617 about the MySQL schedule_storage tests (e.g. why the fixture if it's only used once).
    
    Test Plan: bk
    
    Reviewers: bob, catherinewu, rexledesma, prha
    
    Reviewed By: prha
    
    Subscribers: prha
    
    Differential Revision: https://dagster.phacility.com/D6715

--- python_modules/libraries/dagster-postgres/dagster_postgres_tests/conftest.py
+++ python_modules/libraries/dagster-postgres/dagster_postgres_tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from dagster.utils import file_relative_path
 from dagster.utils.test.postgres_instance import TestPostgresInstance
-from dagster_postgres.schedule_storage.schedule_storage import PostgresScheduleStorage
 
 
 @pytest.fixture(scope="session")
@@ -15,10 +14,3 @@ def conn_string():  # pylint: disable=redefined-outer-name, unused-argument
         file_relative_path(__file__, "docker-compose.yml"), "test-postgres-db"
     ) as conn_str:
         yield conn_str
-
-
-@pytest.fixture(scope="function")
-def clean_schedule_storage(conn_string):  # pylint: disable=redefined-outer-name
-    storage = PostgresScheduleStorage.create_clean_storage(conn_string)
-    assert storage
-    return storage
--- python_modules/libraries/dagster-postgres/dagster_postgres_tests/test_schedule_storage.py
+++ python_modules/libraries/dagster-postgres/dagster_postgres_tests/test_schedule_storage.py
@@ -1,10 +1,13 @@
 import pytest
 from dagster.utils.test.schedule_storage import TestScheduleStorage
+from dagster_postgres.schedule_storage import PostgresScheduleStorage
 
 
 class TestPostgresScheduleStorage(TestScheduleStorage):
     __test__ = True
 
     @pytest.fixture(scope="function", name="storage")
-    def schedule_storage(self, clean_schedule_storage):  # pylint: disable=arguments-differ
-        return clean_schedule_storage
+    def schedule_storage(self, conn_string):  # pylint: disable=arguments-differ
+        storage = PostgresScheduleStorage.create_clean_storage(conn_string)
+        assert storage
+        return storage
