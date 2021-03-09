8a4eba41f67d2b399efa7126334bb05847dd20e7
yuhan <yuhan@elementl.com>
2021-03-09 15:55:40 -0800

    [docs-infra] SEO - open graph & canonical url
    Summary:
    - canonical
    - open graph
    - twitter
    - favicon
    
    images size cheatsheet: https://blog.hubspot.com/marketing/ultimate-guide-social-media-image-dimensions-infographic
    
    Test Plan:
    added 0.10.9 for testing - will remove later
    
    all historical versions will point to the latest as its canonical link:
    {F625268}
    
    Reviewers: sashank
    
    Reviewed By: sashank
    
    Differential Revision: https://dagster.phacility.com/D6850

--- docs/next/pages/_app.tsx
+++ docs/next/pages/_app.tsx
@@ -4,22 +4,48 @@ import "../styles/prism.css";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import Layout from "../layouts/MainLayout";
+import { normalizeVersionPath, useVersion } from "../util/useVersion";
 
+// TODO: update BASE_URL before launching
+// const BASE_URL = "https://docs.dagster.io";
+const BASE_URL = "https://dagster.vercel.app";
 const DEFAULT_SEO = {
   title: "Dagster Docs",
   // TODO: unset this
   // while dark launch, we mark all pages to noindex
   dangerouslySetAllPagesToNoIndex: true,
+  twitter: {
+    site: "@dagsterio",
+    cardType: "summary_large_image",
+    images: {
+      url: `${BASE_URL}/assets/shared/dagster-og-share.png`,
+      alt: "Dagster Docs",
+    },
+  },
+  openGraph: {
+    url: BASE_URL,
+    title: "Dagster Docs",
+    type: "website",
+    description: "A data orchestrator for machine learning, analytics, and ETL",
+    images: [
+      {
+        url: `${BASE_URL}/assets/shared/dagster-og-share.png`,
+        alt: "Dagster Docs",
+      },
+    ],
+  },
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { asPath } = useVersion();
   const getLayout =
     // @ts-ignore
     Component.getLayout || ((page) => <Layout children={page} />);
+  const canonicalUrl = `${BASE_URL}${asPath}`;
 
   return (
     <>
-      <DefaultSeo {...DEFAULT_SEO} />
+      <DefaultSeo canonical={canonicalUrl} {...DEFAULT_SEO} />
       {getLayout(<Component {...pageProps} />)}
     </>
   );
