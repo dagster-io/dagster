6d4a7cd46a73ea0efcd022816963c04fb8d90f50
gibsondan <daniel@elementl.com>
2021-03-10 22:38:14 -0600

    Show reload all even when there are no entries in the workspace
    Summary: We still want to be able to reload when we go from 0 to 1.
    
    Test Plan: Launch an empty workspace, add a new entry to workspace.yaml, press reload, see new entry
    
    Reviewers: dish
    
    Reviewed By: dish
    
    Differential Revision: https://dagster.phacility.com/D6891

--- js_modules/dagit/src/workspace/ReloadAllButton.tsx
+++ js_modules/dagit/src/workspace/ReloadAllButton.tsx
@@ -1,15 +1,10 @@
 import {Button, Icon} from '@blueprintjs/core';
 import * as React from 'react';
 
-import {WorkspaceContext} from 'src/workspace/WorkspaceContext';
 import {useReloadWorkspace} from 'src/workspace/useReloadWorkspace';
 
 export const ReloadAllButton = () => {
-  const {locations} = React.useContext(WorkspaceContext);
   const {reloading, onClick} = useReloadWorkspace();
-  if (!locations.length) {
-    return null;
-  }
 
   return (
     <Button
