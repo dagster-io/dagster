8821e504413c78019bc73dfe3540c0ab5a1d17eb
Rex Ledesma <rex@elementl.com>
2021-03-12 15:28:22 -0800

    Fix sensors docs to use context.get_instance()
    Summary: As the title.
    
    Test Plan: pytest
    
    Reviewers: #docs, prha, sashank
    
    Reviewed By: #docs, sashank
    
    Subscribers: sashank
    
    Differential Revision: https://dagster.phacility.com/D6927

--- examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/sensors/sensor_alert.py
+++ examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/sensors/sensor_alert.py
@@ -29,15 +29,16 @@ def failure_alert_pipeline():
 
 @sensor(pipeline_name="failure_alert_pipeline", mode="prod")
 def pipeline_failure_sensor(context):
-    runs = context.instance.get_runs(
-        filters=PipelineRunsFilter(
-            pipeline_name="your_pipeline_name",
-            statuses=[PipelineRunStatus.FAILURE],
-        ),
-    )
-    for run in runs:
-        # Use the id of the failed run as run_key to avoid duplicate alerts.
-        yield RunRequest(run_key=str(run.run_id))
+    with context.get_instance() as instance:
+        runs = instance.get_runs(
+            filters=PipelineRunsFilter(
+                pipeline_name="your_pipeline_name",
+                statuses=[PipelineRunStatus.FAILURE],
+            ),
+        )
+        for run in runs:
+            # Use the id of the failed run as run_key to avoid duplicate alerts.
+            yield RunRequest(run_key=str(run.run_id))
 
 
 # end_alert_sensor_marker
--- examples/docs_snippets/docs_snippets_tests/concepts_tests/partitions_schedules_sensors_tests/test_sensors.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/partitions_schedules_sensors_tests/test_sensors.py
@@ -1,7 +1,11 @@
-from dagster import execute_pipeline
+from contextlib import suppress
+
+from dagster import SensorExecutionContext, execute_pipeline, pipeline, reconstructable, solid
 from dagster.core.definitions.job import RunRequest
+from dagster.core.test_utils import instance_for_test
 from docs_snippets.concepts.partitions_schedules_sensors.sensors.sensor_alert import (
     failure_alert_pipeline,
+    pipeline_failure_sensor,
 )
 from docs_snippets.concepts.partitions_schedules_sensors.sensors.sensors import (
     isolated_run_request,
@@ -10,6 +14,17 @@ from docs_snippets.concepts.partitions_schedules_sensors.sensors.sensors import 
     sensor_A,
     sensor_B,
 )
+
+
+@solid(config_schema={"fail": bool})
+def foo(context):
+    if context.solid_config["fail"]:
+        raise Exception("This will always fail!")
+
+
+@pipeline
+def your_pipeline_name():
+    return foo()
 
 
 def test_failure_alert_pipeline():
@@ -39,3 +54,36 @@ def test_interval_sensors():
     # TODO: Actually test
     assert sensor_A
     assert sensor_B
+
+
+def test_pipeline_failure_sensor_has_request():
+    with instance_for_test() as instance:
+        with suppress(Exception):
+            execute_pipeline(
+                reconstructable(your_pipeline_name),
+                run_config={"solids": {"foo": {"config": {"fail": True}}}},
+                instance=instance,
+            )
+
+        context = SensorExecutionContext(
+            instance_ref=instance.get_ref(), last_run_key=None, last_completion_time=None
+        )
+
+        requests = pipeline_failure_sensor.get_execution_data(context)
+        assert len(requests) == 1
+
+
+def test_pipeline_failure_sensor_has_no_request():
+    with instance_for_test() as instance:
+        execute_pipeline(
+            reconstructable(your_pipeline_name),
+            run_config={"solids": {"foo": {"config": {"fail": False}}}},
+            instance=instance,
+        )
+
+        context = SensorExecutionContext(
+            instance_ref=instance.get_ref(), last_run_key=None, last_completion_time=None
+        )
+
+        requests = pipeline_failure_sensor.get_execution_data(context)
+        assert len(requests) == 0
