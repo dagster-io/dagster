215c14a82cc9aefce94b62bf8f0481bada7ac9fb
King Chung Huang <kinghuang@mac.com>
2021-03-15 14:40:55 -0600

    Briefly describe dask_resource

--- python_modules/libraries/dagster-dask/dagster_dask/resources.py
+++ python_modules/libraries/dagster-dask/dagster_dask/resources.py
@@ -28,6 +28,17 @@ class DaskResource:
     config_schema=DaskConfigSchema,
 )
 def dask_resource(context):
+    """Resource for a Dask cluster.
+
+    The Dask resource gives solids access a Dask cluster for computations. This is different
+    from the Dask executor, which runs an entire Dagster pipeline as a graph of futures on
+    Dask.
+
+    The resource provides two properties for accessing configured ``cluster`` and ``client``
+    objects. Typically there is no need to interact with these directly. Simply declaring a
+    Dask resource will make its client the current Dask client when running a pipeline. Solids
+    can work with Dask DataFrames and other objects as normal.
+    """
     res = DaskResource(context)
 
     yield res
