907e73c11a46719bac943e12b74a645cbcdb30a3
yuhan <yuhan@elementl.com>
2021-03-19 12:57:18 -0700

    [docs] issue: aws and gcp io manager mixed up
    Summary: https://dagster.slack.com/archives/CHJEUKYQM/p1616140467002900
    
    Test Plan: eys
    
    Reviewers: sashank, sandyryza
    
    Reviewed By: sashank
    
    Differential Revision: https://dagster.phacility.com/D7075

--- examples/docs_snippets/docs_snippets/deploying/gcp/io_manager.py
+++ examples/docs_snippets/docs_snippets/deploying/gcp/io_manager.py
@@ -1,8 +1,8 @@
 from dagster import ModeDefinition
-from dagster_aws.s3.io_manager import s3_pickle_io_manager
-from dagster_aws.s3.resources import s3_resource
+from dagster_gcp.gcs.io_manager import gcs_pickle_io_manager
+from dagster_gcp.gcs.resources import gcs_resource
 
 prod_mode = ModeDefinition(
     name="prod",
-    resource_defs={"s3": s3_resource, "io_manager": s3_pickle_io_manager},
+    resource_defs={"gcs": gcs_resource, "io_manager": gcs_pickle_io_manager},
 )
