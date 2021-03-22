3030538ce4110b819a0e809290a28d166f2848c7
yuhan <yuhan@elementl.com>
2021-03-22 15:20:53 -0700

    [docs-infra] feedback "cancel" button
    Summary: https://github.com/dagster-io/dagster/issues/3926
    
    Test Plan: {F652861}
    
    Reviewers: sashank, johann
    
    Reviewed By: sashank, johann
    
    Differential Revision: https://dagster.phacility.com/D7104

--- docs/next/layouts/MainLayout.tsx
+++ docs/next/layouts/MainLayout.tsx
@@ -308,6 +308,7 @@ export const FeedbackModal = ({
               <div className="flex-shrink-0 px-4 py-4 flex justify-end">
                 <button
                   type="button"
+                  onClick={closeFeedback}
                   className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Cancel
