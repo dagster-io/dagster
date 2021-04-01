55076f943f2ef05d10d36f66e3a5a77930491b12
alangenfeld <alex@elementl.com>
2021-04-01 15:56:00 -0500

    update scrapbook dep
    Summary: resolve https://github.com/dagster-io/dagster/issues/3753
    
    Test Plan: bk
    
    Reviewers: max
    
    Reviewed By: max
    
    Differential Revision: https://dagster.phacility.com/D7264

--- python_modules/libraries/dagstermill/setup.py
+++ python_modules/libraries/dagstermill/setup.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
             # https://github.com/ipython/ipykernel/issues/568
             "ipykernel>=4.9.0,!=5.4.0,!=5.4.1",
             "nbconvert>=5.4.0,<6.0.0",
-            "nteract-scrapbook>=0.2.0",
+            "scrapbook >=0.5.0",
             "papermill>=1.0.0,<2.0.0",
         ],
         extras_require={"test": ["matplotlib", "scikit-learn>=0.19.0"]},
