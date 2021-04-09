d3bdc43a94c24e33ab2e0cd1fb4f62e83caf1334
Isaac Hellendag <dish@elementl.com>
2021-04-09 10:52:17 -0500

    [dagit] Delete commented code in AssetView
    Test Plan: Buildkite
    
    Reviewers: prha
    
    Reviewed By: prha
    
    Differential Revision: https://dagster.phacility.com/D7368

--- js_modules/dagit/packages/core/src/assets/AssetView.tsx
+++ js_modules/dagit/packages/core/src/assets/AssetView.tsx
@@ -11,20 +11,6 @@ export const AssetView: React.FC<{assetKey: AssetKey}> = ({assetKey}) => {
   const assetPath = assetKey.path.join(' \u203A ');
   useDocumentTitle(`Asset: ${assetPath}`);
 
-  // return (
-  //   <Loading queryResult={queryResult}>
-  //     {({assetOrError}) => {
-  //       if (assetOrError.__typename !== 'Asset') {
-  //         return null;
-  //       }
-  //       if (!assetOrError.assetMaterializations.length) {
-  //         return <p>This asset has never been materialized.</p>;
-  //       }
-  //       return <AssetViewWithData asset={assetOrError} />;
-  //     }}
-  //   </Loading>
-  // );
-
   return (
     <Group spacing={24} direction="column">
       <AssetDetails assetKey={assetKey} />
