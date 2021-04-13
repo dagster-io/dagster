915881b6c618793b1d0bfca1b7f8fd3509c4b371
Sidharth Menon <menon.sid.k@gmail.com>
2021-04-13 13:35:03 -0400

    [dagster-graphql] Dagster GraphQL Python Client [3/4]
    Summary: Adding `submit_pipeline_execution` to the Dagster GraphQL client
    
    Test Plan: adding tests to check that 1) success conditions are handled correctly and 2) failure conditions (both within client and from server) are handled gracefully
    
    Differential Revision: https://dagster.phacility.com/D7424

--- python_modules/dagster-graphql/dagster_graphql/client/utils.py
+++ python_modules/dagster-graphql/dagster_graphql/client/utils.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import NamedTuple, Optional
+from typing import Any, Dict, List, NamedTuple, Optional
 
 
 class DagsterGraphQLClientError(Exception):
@@ -32,3 +32,22 @@ class ReloadRepositoryLocationInfo(NamedTuple):
 
     status: ReloadRepositoryLocationStatus
     message: Optional[str] = None
+
+
+class PipelineInfo(NamedTuple):
+    repository_location_name: str
+    repository_name: str
+    pipeline_name: str
+
+    @staticmethod
+    def from_node(node: Dict[str, Any]) -> List["PipelineInfo"]:
+        repo_name = node["name"]
+        repo_location_name = node["location"]["name"]
+        return [
+            PipelineInfo(
+                repository_location_name=repo_location_name,
+                repository_name=repo_name,
+                pipeline_name=pipeline["name"],
+            )
+            for pipeline in node["pipelines"]
+        ]
