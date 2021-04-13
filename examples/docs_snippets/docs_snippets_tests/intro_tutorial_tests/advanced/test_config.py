f829072011c0c8e7500b46348d97fb44f45a2e20
Sandy Ryza <sandy@elementl.com>
2021-04-13 09:27:24 -0700

    advanced tutorial changes
    Summary:
    A few unrelated changes:
    * Moves configuration to the advanced section and adds words to clarify when it makes sense to use it.
    * Renames "Advanced: Pipelines" to "Advanced: Modes, Resources, and Presets".
    * Takes out the "Advanced" prefix for the individual section names.  They're already under headings that communicate that they're advanced.
    * Removes the "Re-use and composition" advanced tutorial section. The reasoning behind this is basically that I don't think we have good recommendations on when to use these features.  The examples are also a bit contrived.  I don't think it's clear that well-written solids should be reusable.  We still have a composite solids concepts section that covers composition.
    
    Test Plan: manual inspection
    
    Reviewers: yuhan, schrockn, max
    
    Reviewed By: schrockn
    
    Differential Revision: https://dagster.phacility.com/D7407

--- examples/docs_snippets/docs_snippets_tests/intro_tutorial_tests/advanced/test_config.py
+++ examples/docs_snippets/docs_snippets_tests/intro_tutorial_tests/advanced/test_config.py
@@ -1,13 +1,13 @@
 from dagster import execute_pipeline
 from dagster.utils import pushd, script_relative_path
-from docs_snippets.intro_tutorial.basics.configuring_solids.configurable_pipeline import (
+from docs_snippets.intro_tutorial.advanced.configuring_solids.configurable_pipeline import (
     configurable_pipeline,
 )
 
 
 def test_tutorial_config_schema():
     with pushd(
-        script_relative_path("../../../docs_snippets/intro_tutorial/basics/configuring_solids/")
+        script_relative_path("../../../docs_snippets/intro_tutorial/advanced/configuring_solids/")
     ):
         result = execute_pipeline(
             configurable_pipeline,
