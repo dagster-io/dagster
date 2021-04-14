a4bcdf82b568ae999386d34b5936d377d7c3cf9b
Sidharth Menon <menon.sid.k@gmail.com>
2021-04-14 02:10:28 -0400

    [dagster-graphql] Dagster GraphQL Python Client [2/4]
    Summary:
    This diff creates the `dagster-graphql-client` CLI:
    
    	1. Adds BK checks to ensure that current GraphQL queries by the client are included in a legacy query `query_snapshot` folder via `dagster-graphql-client query check`
    		a. It also adds the `dagster-graphql-client query snapshot` command to add the GraphQL client's queries to the `query_snapshot` directory
    	2. Tests to ensure that each query in the `client_backcompat` directory is still supported by the GraphQL server (so at a minimum, we can be intentional about breaking changes that arise)
    
    Test Plan: bk script checks + new backcompat tests (this diff is mostly tests)
    
    Differential Revision: https://dagster.phacility.com/D7421

---
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/client_backcompat/test_backcompat_with_python_client.py
@@ -0,0 +1,24 @@
+import os
+from typing import Iterator
+
+import pytest
+from dagster_graphql.schema import create_schema
+from gql import Client, gql
+
+
+@pytest.fixture(scope="session", name="validator_client")
+def get_validator_client() -> Client:
+    return Client(schema=create_schema())
+
+
+def generate_legacy_query_strings() -> Iterator[str]:
+    for (dir_name, _, file_names) in os.walk("./query_snapshots"):
+        for file_name in file_names:
+            if file_name.endswith(".graphql"):
+                with open(os.path.join(dir_name, file_name)) as f:
+                    yield f.read()
+
+
+@pytest.mark.parametrize("query_str", generate_legacy_query_strings())
+def test_backcompat(validator_client: Client, query_str: str):
+    validator_client.validate(gql(query_str))
