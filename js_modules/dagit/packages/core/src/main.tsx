f83bb37e555c393aea06971b2988fe179fab9794
Isaac Hellendag <dish@elementl.com>
2021-05-06 17:59:13 -0500

    [dagit] Add a few more main exports
    Summary: Add a couple `main` exports to `@dagit/core`.
    
    Test Plan: BK
    
    Reviewers: bengotow, dgibson
    
    Reviewed By: dgibson
    
    Differential Revision: https://dagster.phacility.com/D7781

--- js_modules/dagit/packages/core/src/main.tsx
+++ js_modules/dagit/packages/core/src/main.tsx
@@ -3,6 +3,10 @@ export * from './app/App';
 export * from './app/AppProvider';
 export * from './app/AppTopNav';
 
+// Workspace
+export * from './workspace/WorkspaceContext';
+
 // UI
 export * from './ui/Box';
 export * from './ui/Group';
+export * from './ui/Spinner';
