8e3f761525f7f25a203ca22007b80f3892d6b448
Johann Miller <johann@elementl.com>
2021-05-12 20:26:44 -0400

    Put run coordinator instance yaml under test
    Test Plan: unit
    
    Reviewers: sashank
    
    Reviewed By: sashank
    
    Differential Revision: https://dagster.phacility.com/D7892

--- examples/docs_snippets/docs_snippets_tests/deploying_tests/test_concurrency_limits.py
+++ examples/docs_snippets/docs_snippets_tests/deploying_tests/test_concurrency_limits.py
@@ -1,4 +1,8 @@
-from docs_snippets.deploying.concurrency_limits import (  # pylint: disable=import-error
+import os
+
+from dagster.core.instance.ref import InstanceRef
+from dagster.core.run_coordinator import QueuedRunCoordinator
+from docs_snippets.deploying.concurrency_limits.concurrency_limits import (  # pylint: disable=import-error
     important_pipeline,
     less_important_schedule,
 )
@@ -7,3 +11,14 @@ from docs_snippets.deploying.concurrency_limits import (  # pylint: disable=impo
 def test_inclusion():
     assert important_pipeline
     assert less_important_schedule
+
+
+def test_instance_yaml(docs_snippets_folder):
+    intance_yaml_folder = os.path.join(
+        docs_snippets_folder,
+        "deploying",
+        "concurrency_limits",
+    )
+    assert isinstance(
+        InstanceRef.from_dir(intance_yaml_folder).run_coordinator, QueuedRunCoordinator
+    )
