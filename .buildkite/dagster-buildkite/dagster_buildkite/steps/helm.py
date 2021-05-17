117d734e8447f035921546e854217a502f6bd55a
Rex Ledesma <rex@elementl.com>
2021-05-17 09:31:38 -0700

    Add more retries for Helm tests
    Summary:
    Because these tests call to the $ref's of the JSON Schema,
    they are subject to failure. Bump up the retry number on
    Buildkite.
    
    Also only run the tests on python 3.8. Running on other
    versions of Python is repetitive and is a source of flakiness
    Helm tests are just template tests is not an importable
    library in Dagster.
    
    Test Plan: bk
    
    Reviewers: alangenfeld
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D7935

--- .buildkite/dagster-buildkite/dagster_buildkite/steps/helm.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/helm.py
@@ -13,7 +13,7 @@ def helm_lint_steps() -> List[dict]:
             "pip install yamllint",
             "make yamllint",
         )
-        .on_integration_image(SupportedPython.V3_7)
+        .on_integration_image(SupportedPython.V3_8)
         .build(),
         StepBuilder(":helm: dagster-json-schema")
         .run(
@@ -21,21 +21,21 @@ def helm_lint_steps() -> List[dict]:
             "dagster-helm schema apply",
             "git diff --exit-code",
         )
-        .on_integration_image(SupportedPython.V3_7)
+        .on_integration_image(SupportedPython.V3_8)
         .build(),
         StepBuilder(":helm: dagster :lint-roller:")
         .run(
             "helm lint helm/dagster --with-subcharts --strict",
         )
-        .on_integration_image(SupportedPython.V3_7)
-        .with_retry(1)
+        .on_integration_image(SupportedPython.V3_8)
+        .with_retry(2)
         .build(),
         StepBuilder(":helm: dagster dependency build")
         .run(
             "helm repo add bitnami https://charts.bitnami.com/bitnami",
             "helm dependency build helm/dagster",
         )
-        .on_integration_image(SupportedPython.V3_7)
+        .on_integration_image(SupportedPython.V3_8)
         .build(),
     ]
 
@@ -45,9 +45,10 @@ def helm_steps() -> List[dict]:
     tests += helm_lint_steps()
     tests += ModuleBuildSpec(
         os.path.join("helm", "dagster", "schema"),
+        supported_pythons=[SupportedPython.V3_8],
         buildkite_label="dagster-helm-schema",
         upload_coverage=False,
-        retries=1,
+        retries=2,
     ).get_tox_build_steps()
 
     return tests
