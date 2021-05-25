2e81aaa99a84b4c4439540914df9a6f01810eb4b
hugo.lima <hugo.lima@eigentech.com>
2021-05-25 21:16:50 +0100

    added mlflow resource and boiler plate stuff for packaging

---
+++ python_modules/libraries/dagster-mlflow/dagster_mlflow/__init__.py
@@ -0,0 +1,8 @@
+from dagster.core.utils import check_dagster_package_version
+
+from .resources import datadog_resource
+from .version import __version__
+
+check_dagster_package_version("dagster-datadog", __version__)
+
+__all__ = ["datadog_resource"]
