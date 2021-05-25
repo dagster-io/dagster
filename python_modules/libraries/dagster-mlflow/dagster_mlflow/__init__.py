00dad0d81ba865f7348eb4113b7837cd81db564d
hug0l1ma <hugo.lima@eigentech.com>
2021-05-25 21:55:23 +0100

    fixed init file

--- python_modules/libraries/dagster-mlflow/dagster_mlflow/__init__.py
+++ python_modules/libraries/dagster-mlflow/dagster_mlflow/__init__.py
@@ -1,8 +1,8 @@
 from dagster.core.utils import check_dagster_package_version
 
-from .resources import datadog_resource
+from .resources import mlflow_tracking
 from .version import __version__
 
-check_dagster_package_version("dagster-datadog", __version__)
+check_dagster_package_version("dagster-mlflow", __version__)
 
-__all__ = ["datadog_resource"]
+__all__ = ["mlflow_tracking"]
