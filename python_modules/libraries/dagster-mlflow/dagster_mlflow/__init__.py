4f0e2f58d9667bca0d7a15dc9b2917a3b94ce191
hug0l1ma <hugo.lima@eigentech.com>
2021-05-26 01:15:11 +0100

    updated docstring with more detailed usage instructions

--- python_modules/libraries/dagster-mlflow/dagster_mlflow/__init__.py
+++ python_modules/libraries/dagster-mlflow/dagster_mlflow/__init__.py
@@ -1,8 +1,8 @@
 from dagster.core.utils import check_dagster_package_version
 
-from .resources import mlflow_tracking
+from .resources import mlflow_tracking, cleanup_on_success, cleanup_on_dagit_failure
 from .version import __version__
 
 check_dagster_package_version("dagster-mlflow", __version__)
 
-__all__ = ["mlflow_tracking"]
+__all__ = ["mlflow_tracking", "cleanup_on_success", "cleanup_on_dagit_failure"]
