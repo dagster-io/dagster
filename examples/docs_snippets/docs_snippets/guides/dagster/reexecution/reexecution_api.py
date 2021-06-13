957f8d6022a1b3979c702eb7a05e459093345c2e
sarahmk125 <sarahmk125@gmail.com>
2021-06-13 11:01:21 -0400

    updating docs from pr comments

--- examples/docs_snippets/docs_snippets/guides/dagster/reexecution/reexecution_api.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/reexecution/reexecution_api.py
@@ -2,18 +2,25 @@ from dagster import DagsterInstance, execute_pipeline, reexecute_pipeline
 from reexecution.pipeline.unreliable_pipeline import unreliable_pipeline
 
 
-def reexecution_api_example():
-    instance = DagsterInstance.ephemeral()
-    run_config = {"intermediate_storage": {"filesystem": {}}}
+instance = DagsterInstance.ephemeral()
 
-    # Initial execution
-    pipeline_result_full = execute_pipeline(unreliable_pipeline, run_config=run_config, instance=instance)
+# Initial execution
+pipeline_result_full = execute_pipeline(unreliable_pipeline, instance=instance)
 
-    if not pipeline_result_full.success:
-        # Re-execution: Entire pipeline
-        reexecution_result_full = reexecute_pipeline(
-            unreliable_pipeline,
-            parent_run_id=pipeline_result_full.run_id,
-            run_config=run_config,
-            instance=instance,
-        )
+if not pipeline_result_full.success:
+    # Re-execution: Entire pipeline
+    reexecution_result_full = reexecute_pipeline(
+        unreliable_pipeline,
+        parent_run_id=pipeline_result_full.run_id,
+        instance=instance,
+    )
+
+# end_initial_execution_marker
+
+# Re-execution: Starting with the "unreliable" solid and all its descendents
+reexecution_result_specific_selection = reexecute_pipeline(
+    unreliable_pipeline,
+    parent_run_id=pipeline_result_full.run_id,
+    instance=instance,
+    solid_selection=['unreliable+*']
+)
--- examples/docs_snippets/docs_snippets/guides/dagster/reexecution/reexecution_api_steps.py
+++
@@ -1,8 +0,0 @@
-# Re-execution: Starting with the "unreliable" solid and all its descendents
-reexecution_result_specific_selection = reexecute_pipeline(
-    unreliable_pipeline,
-    parent_run_id=pipeline_result_full.run_id,
-    run_config=run_config,
-    instance=instance,
-    solid_selection=['unreliable+*']
-)
