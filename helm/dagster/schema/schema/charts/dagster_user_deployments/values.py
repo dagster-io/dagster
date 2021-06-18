b10af425c5a6313cd756e4413bf327e61bee5e25
Michel Rouly <michel@rouly.net>
2021-06-18 16:36:15 -0400

    update schema for user deployments

--- helm/dagster/schema/schema/charts/dagster_user_deployments/values.py
+++ helm/dagster/schema/schema/charts/dagster_user_deployments/values.py
@@ -1,11 +1,18 @@
 from typing import List
 
-from pydantic import BaseModel  # pylint: disable=no-name-in-module
+from pydantic import BaseModel, Field  # pylint: disable=no-name-in-module
 
 from .subschema.user_deployments import UserDeployment
+from ..dagster.subschema import Global, ServiceAccount
+from ..utils import kubernetes
 
 
 class DagsterUserDeploymentsHelmValues(BaseModel):
     __doc__ = "@" + "generated"
 
+    dagsterHome: str
+    postgresqlSecretName: str
     deployments: List[UserDeployment]
+    imagePullSecrets: List[kubernetes.SecretRef]
+    serviceAccount: ServiceAccount
+    global_: Global = Field(..., alias="global")
