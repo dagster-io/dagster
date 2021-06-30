b9a50717cf677199e322e147ad0afc13945c4eb4
Michel Rouly <michel@rouly.net>
2021-06-30 17:13:35 -0400

    Add imagePullSecrets to the subchart schema

--- helm/dagster/schema/schema/charts/dagster_user_deployments/values.py
+++ helm/dagster/schema/schema/charts/dagster_user_deployments/values.py
@@ -3,9 +3,11 @@ from typing import List
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
 from .subschema.user_deployments import UserDeployment
+from ..utils import kubernetes
 
 
 class DagsterUserDeploymentsHelmValues(BaseModel):
     __doc__ = "@" + "generated"
 
     deployments: List[UserDeployment]
+    imagePullSecrets: List[kubernetes.SecretRef]
