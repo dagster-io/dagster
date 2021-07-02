c1b8731745612dbe96e71538b422deecd91a2004
helloworld <s@thupukari.com>
2021-07-02 11:12:28 -0400

    Unpin urllib3 in automation
    Summary: This pin was causing the pip resolver to be really slow. This diff unpins urllib3 since it doesn't seem necessary anymore.
    
    Test Plan: bk
    
    Reviewers: sidkmenon, jordansanders, dgibson
    
    Reviewed By: sidkmenon, dgibson
    
    Differential Revision: https://dagster.phacility.com/D8672

--- python_modules/automation/setup.py
+++ python_modules/automation/setup.py
@@ -28,7 +28,7 @@ setup(
         "twine==1.15.0",
         "virtualenv==16.5.0",
         "wheel==0.33.6",
-        "urllib3==1.26.5",
+        "urllib3",
         # resolve issue with aiohttp pin of chardet for aiohttp<=3.7.3, req'd by slackclient
         # https://github.com/dagster-io/dagster/issues/3539
         "chardet<4.0",
