f188334fc683b1235b7e36715e67483909ec57be
Iswariya Manivannan <iswariya.m17@gmail.com>
2021-07-03 15:39:53 +0200

    Add pipeline failure msteams sensor

--- python_modules/libraries/dagster-msteams/dagster_msteams/__init__.py
+++ python_modules/libraries/dagster-msteams/dagster_msteams/__init__.py
@@ -3,6 +3,7 @@ from dagster.core.utils import check_dagster_package_version
 from .hooks import teams_on_failure, teams_on_success
 from .resources import msteams_resource
 from .card import Card
+from .sensors import make_teams_on_pipeline_failure_sensor
 from .version import __version__
 
 check_dagster_package_version("dagster-msteams", __version__)
