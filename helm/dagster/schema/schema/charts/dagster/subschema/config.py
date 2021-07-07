19ccd5c87d17f417794b11e82f49eb93d271087b
Rex Ledesma <rex@elementl.com>
2021-07-07 12:48:07 -0400

    feat!: add schema in Helm for run coordinator
    Summary:
    Previously, the run coordinator was allowed a custom class, but its configuration did not
    fit the convention of "type:, config:" since that would be a breaking change. Here, we migrate
    the schema to this convention.
    
    Will change `queuedRunCoordinator` to `runCoordinator` in a followup.
    
    Test Plan:
    pytest
    integration
    
    Reviewers: johann, sidkmenon
    
    Reviewed By: johann
    
    Differential Revision: https://dagster.phacility.com/D8737

--- helm/dagster/schema/schema/charts/dagster/subschema/config.py
+++ helm/dagster/schema/schema/charts/dagster/subschema/config.py
@@ -3,11 +3,12 @@ from typing import Union
 from pydantic import BaseModel, Extra  # pylint: disable=no-name-in-module
 
 
-class _StringSource(BaseModel):
+class Source(BaseModel):
     env: str
 
     class Config:
         extra = Extra.forbid
 
 
-StringSource = Union[str, _StringSource]
+StringSource = Union[str, Source]
+IntSource = Union[int, Source]
--- helm/dagster/schema/schema_tests/test_instance.py
+++ helm/dagster/schema/schema_tests/test_instance.py
@@ -1,5 +1,6 @@
 import pytest
 import yaml
+from dagster.core.run_coordinator import QueuedRunCoordinator
 from dagster_aws.s3.compute_log_manager import S3ComputeLogManager
 from dagster_azure.blob.compute_log_manager import AzureBlobComputeLogManager
 from dagster_gcp.gcs.compute_log_manager import GCSComputeLogManager
@@ -13,7 +14,15 @@ from schema.charts.dagster.subschema.compute_log_manager import (
 from schema.charts.dagster.subschema.compute_log_manager import (
     S3ComputeLogManager as S3ComputeLogManagerModel,
 )
-from schema.charts.dagster.subschema.daemon import Daemon, QueuedRunCoordinator
+from schema.charts.dagster.subschema.daemon import (
+    ConfigurableClass,
+    Daemon,
+    QueuedRunCoordinatorConfig,
+    RunCoordinator,
+    RunCoordinatorConfig,
+    RunCoordinatorType,
+    TagConcurrencyLimit,
+)
 from schema.charts.dagster.subschema.postgresql import PostgreSQL, Service
 from schema.charts.dagster.values import DagsterHelmValues
 from schema.utils.helm_template import HelmTemplate
@@ -74,14 +83,22 @@ def test_storage_postgres_db_config(template: HelmTemplate, storage: str):
 
 
 @pytest.mark.parametrize("enabled", [True, False])
-def test_run_coordinator_config(template: HelmTemplate, enabled: bool):
-    module_name = "dagster.core.run_coordinator"
-    class_name = "QueuedRunCoordinator"
-
+def test_queued_run_coordinator_config(template: HelmTemplate, enabled: bool):
+    max_concurrent_runs = 50
+    tag_concurrency_limits = [TagConcurrencyLimit(key="key", value="value", limit=10)]
+    dequeue_interval_seconds = 50
     helm_values = DagsterHelmValues.construct(
         dagsterDaemon=Daemon.construct(
-            queuedRunCoordinator=QueuedRunCoordinator.construct(
-                enabled=enabled, module=module_name, class_name=class_name
+            queuedRunCoordinator=RunCoordinator.construct(
+                enabled=enabled,
+                type=RunCoordinatorType.QUEUED,
+                config=RunCoordinatorConfig.construct(
+                    queuedRunCoordinator=QueuedRunCoordinatorConfig.construct(
+                        maxConcurrentRuns=max_concurrent_runs,
+                        tagConcurrencyLimits=tag_concurrency_limits,
+                        dequeueIntervalSeconds=dequeue_interval_seconds,
+                    )
+                ),
             )
         )
     )
@@ -92,8 +109,50 @@ def test_run_coordinator_config(template: HelmTemplate, enabled: bool):
 
     assert ("run_coordinator" in instance) == enabled
     if enabled:
-        assert instance["run_coordinator"]["module"] == module_name
-        assert instance["run_coordinator"]["class"] == class_name
+        assert instance["run_coordinator"]["module"] == "dagster.core.run_coordinator"
+        assert instance["run_coordinator"]["class"] == "QueuedRunCoordinator"
+        assert instance["run_coordinator"]["config"]
+
+        run_coordinator_config = instance["run_coordinator"]["config"]
+
+        assert run_coordinator_config["max_concurrent_runs"] == max_concurrent_runs
+        assert run_coordinator_config["dequeue_interval_seconds"] == dequeue_interval_seconds
+
+        assert len(run_coordinator_config["tag_concurrency_limits"]) == len(tag_concurrency_limits)
+        assert run_coordinator_config["tag_concurrency_limits"] == [
+            tag_concurrency_limit.dict() for tag_concurrency_limit in tag_concurrency_limits
+        ]
+
+
+def test_custom_run_coordinator_config(template: HelmTemplate):
+    module = "a_module"
+    class_ = "Class"
+    config_field_one = "1"
+    config_field_two = "two"
+    config = {"config_field_one": config_field_one, "config_field_two": config_field_two}
+    helm_values = DagsterHelmValues.construct(
+        dagsterDaemon=Daemon.construct(
+            queuedRunCoordinator=RunCoordinator.construct(
+                enabled=True,
+                type=RunCoordinatorType.CUSTOM,
+                config=RunCoordinatorConfig.construct(
+                    customRunCoordinator=ConfigurableClass.construct(
+                        module=module,
+                        class_=class_,
+                        config=config,
+                    )
+                ),
+            )
+        )
+    )
+    configmaps = template.render(helm_values)
+    assert len(configmaps) == 1
+
+    instance = yaml.full_load(configmaps[0].data["dagster.yaml"])
+
+    assert instance["run_coordinator"]["module"] == module
+    assert instance["run_coordinator"]["class"] == class_
+    assert instance["run_coordinator"]["config"] == config
 
 
 @pytest.mark.parametrize(
@@ -111,3 +170,16 @@ def test_compute_log_manager_has_schema(json_schema_model, compute_log_manager_c
     )
 
     assert json_schema_fields == compute_log_manager_fields
+
+
+@pytest.mark.parametrize(
+    argnames=["json_schema_model", "run_coordinator_class"],
+    argvalues=[
+        (QueuedRunCoordinatorConfig, QueuedRunCoordinator),
+    ],
+)
+def test_run_coordinator_has_schema(json_schema_model, run_coordinator_class):
+    json_schema_fields = json_schema_model.schema()["properties"].keys()
+    run_coordinator_fields = set(map(to_camel_case, run_coordinator_class.config_type().keys()))
+
+    assert json_schema_fields == run_coordinator_fields
