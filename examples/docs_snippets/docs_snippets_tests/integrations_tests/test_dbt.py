f36963e133ac61535553716a5a485a4166e18bd3
Owen Kephart <owen@elementl.com>
2021-07-16 16:28:45 -0700

    Better tests for dbt integration guide, and slightly modify example
    Summary: Wanted to modify the pattern we were recommending (tbh I'm not even sure the original would work). Also noticed that the existing test didn't really do anything as defined, so I made it do something
    
    Test Plan: bk
    
    Reviewers: alangenfeld, cdecarolis
    
    Reviewed By: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D8939

--- examples/docs_snippets/docs_snippets_tests/integrations_tests/test_dbt.py
+++ examples/docs_snippets/docs_snippets_tests/integrations_tests/test_dbt.py
@@ -1,2 +1,53 @@
-# Checks that Python code snippets have valid syntax.
-import docs_snippets.integrations.dbt  # pylint: disable=unused-import
+import pytest
+from docs_snippets.integrations.dbt import (
+    scope_dbt_cli_config_exclude_models,
+    scope_dbt_cli_config_executable,
+    scope_dbt_cli_config_profile_and_target,
+    scope_dbt_cli_config_select_models,
+    scope_dbt_cli_config_vars,
+    scope_dbt_cli_profile_modes,
+    scope_dbt_cli_resource_config,
+    scope_dbt_cli_run,
+    scope_dbt_cli_run_after_another_solid,
+    scope_dbt_cli_run_specific_models,
+    scope_dbt_cli_run_specific_models_runtime,
+    scope_dbt_rpc_and_wait_config_polling_interval,
+    scope_dbt_rpc_config_disable_assets,
+    scope_dbt_rpc_config_exclude_models,
+    scope_dbt_rpc_config_select_models,
+    scope_dbt_rpc_resource,
+    scope_dbt_rpc_resource_example,
+    scope_dbt_rpc_run,
+    scope_dbt_rpc_run_and_wait,
+    scope_dbt_rpc_run_specific_models,
+)
+
+
+@pytest.mark.parametrize(
+    "scope",
+    [
+        scope_dbt_cli_resource_config,
+        scope_dbt_cli_run,
+        scope_dbt_cli_run_specific_models,
+        scope_dbt_cli_run_specific_models_runtime,
+        scope_dbt_cli_profile_modes,
+        scope_dbt_cli_run_after_another_solid,
+        scope_dbt_rpc_resource,
+        scope_dbt_rpc_run,
+        scope_dbt_rpc_run_specific_models,
+        scope_dbt_rpc_run_and_wait,
+        scope_dbt_cli_config_profile_and_target,
+        scope_dbt_cli_config_executable,
+        scope_dbt_cli_config_select_models,
+        scope_dbt_cli_config_exclude_models,
+        scope_dbt_cli_config_vars,
+        scope_dbt_rpc_resource_example,
+        scope_dbt_rpc_config_select_models,
+        scope_dbt_rpc_config_exclude_models,
+        scope_dbt_rpc_and_wait_config_polling_interval,
+        scope_dbt_rpc_config_disable_assets,
+    ],
+)
+def test_valid_scope(scope):
+    # test that the scope has valid syntax
+    scope()
