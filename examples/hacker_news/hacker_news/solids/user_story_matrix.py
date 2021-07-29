8863ab297f3b83361ed060819677bb2ec755af85
Sandy Ryza <sandy@elementl.com>
2021-07-29 10:50:43 -0700

    indexed coo matrix in story recommender pipeline
    Summary: I wish I had thought to do this when implementing the pipeline initially.  It simplifies the code and makes the pipeline easier to assetize.
    
    Test Plan: bk
    
    Reviewers: owen
    
    Reviewed By: owen
    
    Differential Revision: https://dagster.phacility.com/D9146

--- examples/hacker_news/hacker_news/solids/user_story_matrix.py
+++ examples/hacker_news/hacker_news/solids/user_story_matrix.py
@@ -1,31 +1,27 @@
+from typing import NamedTuple
+
 import numpy as np
-from dagster import Output, OutputDefinition, solid
+from dagster import OutputDefinition, solid
 from pandas import DataFrame, Series
 from scipy.sparse import coo_matrix
 
 
-@solid(
-    output_defs=[
-        OutputDefinition(
-            Series,
-            name="row_users",
-            description="A series whose index corresponds to the row indices in the user story "
-            "matrix and whose values are the corresponding user IDs.",
-            metadata={"key": "row_users"},
-        ),
-        OutputDefinition(
-            Series,
-            name="col_stories",
-            description="A series whose index corresponds to the column indices in the user story "
-            "matrix and whose values are the corresponding story IDs.",
-            metadata={"key": "col_stories"},
-        ),
-        OutputDefinition(
-            coo_matrix, name="user_story_matrix", metadata={"key": "user_story_matrix"}
-        ),
-    ]
-)
-def build_user_story_matrix(comment_stories: DataFrame):
+class IndexedCooMatrix(NamedTuple):
+    """A matrix with indexes for the rows and columns"""
+
+    matrix: coo_matrix
+
+    row_index: Series
+    """A Series whose index corresponds to the row numbers in the
+    matrix and whose values are the corresponding values in the index."""
+
+    col_index: Series
+    """A Series whose index corresponds to the col numbers in the
+    matrix and whose values are the corresponding values in this index."""
+
+
+@solid(output_defs=[OutputDefinition(metadata={"key": "user_story_matrix"})])
+def build_user_story_matrix(comment_stories: DataFrame) -> IndexedCooMatrix:
     """
     Builds a sparse matrix where the rows are users, the columns are stories, and the values
     are whether the user commented on the story.
@@ -41,15 +37,10 @@ def build_user_story_matrix(comment_stories: DataFrame):
     sparse_cols = story_col_indices[deduplicated["story_id"]]
     sparse_data = np.ones(len(sparse_rows))
 
-    yield Output(
-        Series(user_row_indices.index.values, index=user_row_indices),
-        output_name="row_users",
-    )
-    yield Output(
-        Series(story_col_indices.index.values, index=story_col_indices),
-        output_name="col_stories",
-    )
-    yield Output(
-        coo_matrix((sparse_data, (sparse_rows, sparse_cols)), shape=(len(users), len(stories))),
-        output_name="user_story_matrix",
+    return IndexedCooMatrix(
+        matrix=coo_matrix(
+            (sparse_data, (sparse_rows, sparse_cols)), shape=(len(users), len(stories))
+        ),
+        row_index=Series(user_row_indices.index.values, index=user_row_indices),
+        col_index=Series(story_col_indices.index.values, index=story_col_indices),
     )
