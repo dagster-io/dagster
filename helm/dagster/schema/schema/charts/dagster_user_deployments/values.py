757dd5025ccd960976978672602e164226998bc3
Michel Rouly <michel@rouly.net>
2021-08-02 14:57:39 -0400

    feat(helm): add serviceAccount.name to the user deployment subchart and schema (#4329)
    * add serviceAccount.name to the chart and schema
    
    add tests for serviceAccount in subchart
    
    * keep both test fixtures around
    
    * prefix standalone tests with standalone
    
    * make black

--- helm/dagster/schema/schema/charts/dagster_user_deployments/values.py
+++ helm/dagster/schema/schema/charts/dagster_user_deployments/values.py
@@ -2,6 +2,7 @@ from typing import List
 
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
+from ..dagster.subschema import Global, ServiceAccount
 from ..utils import kubernetes
 from .subschema.user_deployments import UserDeployment
 
@@ -11,3 +12,4 @@ class DagsterUserDeploymentsHelmValues(BaseModel):
 
     deployments: List[UserDeployment]
     imagePullSecrets: List[kubernetes.SecretRef]
+    serviceAccount: ServiceAccount
