6a75c1259d781e4298308e8ebe8b0fe674ee62c7
Jordan Sanders <jordan@elementl.com>
2021-08-04 10:15:29 -0500

    Test termination with ECS eventual consistency
    Prior to 85b999bf71095762c537cb5f71f16bc2f5a8769b, this would have
    raised:
    
    ```
    List index out of range
    ```

--- python_modules/libraries/dagster-aws/dagster_aws_tests/ecs_tests/launcher_tests/test_termination.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/ecs_tests/launcher_tests/test_termination.py
@@ -1,5 +1,4 @@
 def test_termination(instance, workspace, run):
-
     assert not instance.run_launcher.can_terminate(run.run_id)
 
     instance.launch_run(run.run_id, workspace)
@@ -8,3 +7,27 @@ def test_termination(instance, workspace, run):
     assert instance.run_launcher.terminate(run.run_id)
     assert not instance.run_launcher.can_terminate(run.run_id)
     assert not instance.run_launcher.terminate(run.run_id)
+
+
+def test_eventual_consistency(instance, workspace, run, monkeypatch):
+    instance.launch_run(run.run_id, workspace)
+
+    def empty(*_args, **_kwargs):
+        return {"tasks": []}
+
+    original = instance.run_launcher.ecs.describe_tasks
+
+    # The ECS API is eventually consistent:
+    # https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_RunTask.html
+    # describe_tasks might initially return nothing even if a task exists.
+    monkeypatch.setattr(instance.run_launcher.ecs, "describe_tasks", empty)
+    assert not instance.run_launcher.can_terminate(run.run_id)
+
+    monkeypatch.setattr(instance.run_launcher.ecs, "describe_tasks", original)
+    assert instance.run_launcher.can_terminate(run.run_id)
+
+    monkeypatch.setattr(instance.run_launcher.ecs, "describe_tasks", empty)
+    assert not instance.run_launcher.terminate(run.run_id)
+
+    monkeypatch.setattr(instance.run_launcher.ecs, "describe_tasks", original)
+    assert instance.run_launcher.terminate(run.run_id)
