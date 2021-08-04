0db24c03e11e2baaa65f4f446a791234082e1383
Sandy Ryza <sandy@elementl.com>
2021-08-04 10:44:58 -0700

    tutorial test

---
+++ examples/docs_snippets/docs_snippets_tests/intro_tutorial_tests/basics/test_test_complex_pipeline.py
@@ -0,0 +1,9 @@
+from docs_snippets.intro_tutorial.basics.e04_quality.test_complex_pipeline import (
+    test_complex_pipeline,
+    test_find_highest_calorie_cereal,
+)
+
+
+def test_example_tests():
+    test_complex_pipeline()
+    test_find_highest_calorie_cereal()
