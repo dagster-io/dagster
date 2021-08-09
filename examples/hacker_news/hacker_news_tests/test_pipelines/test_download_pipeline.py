c1c75887b743227f6974ef6ee8737f81241492fe
Sandy Ryza <sandy@elementl.com>
2021-08-09 15:40:56 -0700

    [demo pipelines] remove dynamic download pipeline
    Summary:
    The goal of the demo repo is to simulate what a "real" set of data application would look like, and a real data application would not include two parallel implementations of the same functionality.  I think there are many opportunities for confusion if someone is trying to copy/paste code or figure out what patterns they themselves should follow.
    
    If we want to showcase dynamic orchestration, we could add an additional pipeline or set of steps that require it.  Maybe some hyperparameter search for the story recommender or something.
    
    Test Plan: bk
    
    Reviewers: owen
    
    Reviewed By: owen
    
    Differential Revision: https://dagster.phacility.com/D9215

--- examples/hacker_news/hacker_news_tests/test_pipelines/test_download_pipeline.py
+++ examples/hacker_news/hacker_news_tests/test_pipelines/test_download_pipeline.py
@@ -1,7 +1,7 @@
 import tempfile
 
 from dagster import execute_pipeline
-from hacker_news.pipelines.download_pipeline import download_pipeline, dynamic_download_pipeline
+from hacker_news.pipelines.download_pipeline import download_pipeline
 
 
 def test_download():
@@ -14,30 +14,6 @@ def test_download():
                     "partition_end": {"config": "2020-12-30 01:00:00"},
                     "parquet_io_manager": {"config": {"base_path": temp_dir}},
                 }
-            },
-            mode="test_local_data",
-        )
-
-        assert result.success
-
-
-def test_dynamic_download():
-    with tempfile.TemporaryDirectory() as temp_dir:
-        result = execute_pipeline(
-            dynamic_download_pipeline,
-            run_config={
-                "resources": {
-                    "partition_start": {"config": "2020-12-30 00:00:00"},
-                    "partition_end": {"config": "2020-12-30 01:00:00"},
-                    "parquet_io_manager": {"config": {"base_path": temp_dir}},
-                },
-                "solids": {
-                    "dynamic_id_ranges_for_time": {
-                        "config": {
-                            "batch_size": 100,
-                        }
-                    },
-                },
             },
             mode="test_local_data",
         )
