60daec290d424b6425f02b7122f739911cb4b06b
prha <prha@elementl.com>
2021-08-12 13:41:17 -0700

    add download schedule

--- examples/hacker_news_assets/hacker_news_assets/repo.py
+++ examples/hacker_news_assets/hacker_news_assets/repo.py
@@ -1,20 +1,16 @@
 from dagster import repository
 
-from .pipelines.download_pipeline import (
-    download_comments_and_stories_dev,
-    download_comments_and_stories_prod,
-)
-from .pipelines.story_recommender import story_recommender_dev, story_recommender_prod
+from .pipelines.download_pipeline import download_comments_and_stories_prod
+from .pipelines.story_recommender import story_recommender_prod
 from .sensors.hn_tables_updated_sensor import story_recommender_on_hn_table_update
 from .sensors.slack_on_pipeline_failure_sensor import make_pipeline_failure_sensor
+from .schedules.hourly_hn_download_schedule import create_hourly_hn_download_schedule
 
 
 @repository
 def hacker_news_repository():
     pipelines = [
-        # download_comments_and_stories_dev,
         download_comments_and_stories_prod,
-        # story_recommender_dev,
         story_recommender_prod,
     ]
     sensors = [
@@ -22,4 +18,4 @@ def hacker_news_repository():
         story_recommender_on_hn_table_update,
     ]
 
-    return pipelines + sensors
+    return pipelines + sensors + [create_hourly_hn_download_schedule()]
