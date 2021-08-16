278fc3bc94976e95ee924344ece149101229a3f9
Isaac Hellendag <dish@elementl.com>
2021-08-16 12:55:44 -0500

    [dagit] Better styling for ReactMarkdown use cases

---
+++ js_modules/dagit/packages/core/src/ui/Markdown.tsx
@@ -0,0 +1,40 @@
+import {Colors} from '@blueprintjs/core';
+import * as React from 'react';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import styled from 'styled-components/macro';
+
+import {FontFamily} from './styles';
+
+interface Props {
+  children: string;
+}
+
+export const Markdown: React.FC<Props> = (props) => {
+  return (
+    <Container>
+      <ReactMarkdown remarkPlugins={[gfm]} {...props} />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  table {
+    font-family: ${FontFamily.monospace};
+  }
+
+  table tr th {
+    color: ${Colors.GRAY3};
+    font-weight: normal;
+    padding: 4px 16px 4px 0;
+  }
+
+  table tr td {
+    padding: 2px 16px 2px 0;
+  }
+
+  table tr th:last-child,
+  table tr td:last-child {
+    padding-right: 0;
+  }
+`;
