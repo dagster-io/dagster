56a75724878afe2b9e0a74b00fa3eff37cd29e31
Sandy Ryza <sandy@elementl.com>
2021-08-25 14:21:22 -0700

    graph/job/op toys package #4632

---
+++ python_modules/dagster-test/dagster_test/graph_job_op_toys/log_file.py
@@ -0,0 +1,37 @@
+import os
+
+from dagster import AssetKey, AssetMaterialization, EventMetadata, Field, Output, pipeline, solid
+
+
+@solid(
+    config_schema={
+        "filename": Field(str, is_required=True),
+        "directory": Field(str, is_required=True),
+    }
+)
+def read_file(context):
+    relative_filename = context.solid_config["filename"]
+    directory = context.solid_config["directory"]
+    filename = os.path.join(directory, relative_filename)
+    try:
+        fstats = os.stat(filename)
+        context.log.info("Found file {}".format(relative_filename))
+        yield AssetMaterialization(
+            asset_key=AssetKey(["log_file", relative_filename]),
+            metadata={
+                "path": EventMetadata.path(filename),
+                "File status": {
+                    "size": fstats.st_size,
+                    "ctime": fstats.st_ctime,
+                    "mtime": fstats.st_mtime,
+                },
+            },
+        )
+        yield Output(relative_filename)
+    except FileNotFoundError:
+        context.log.error("No file found: {}".format(relative_filename))
+
+
+@pipeline(description="Demo pipeline that spits out some file info, given a path")
+def log_file_pipeline():
+    read_file()
---
+++ python_modules/dagster-test/dagster_test/graph_job_op_toys/sensors.py
@@ -0,0 +1,131 @@
+import os
+
+from dagster import AssetKey, RunRequest, SkipReason, check, sensor
+from dagster.core.definitions.decorators.sensor import asset_sensor
+from dagster.core.definitions.pipeline_sensor import (
+    PipelineFailureSensorContext,
+    pipeline_failure_sensor,
+)
+from slack import WebClient
+
+
+def get_directory_files(directory_name, since=None):
+    check.str_param(directory_name, "directory_name")
+    if not os.path.isdir(directory_name):
+        return []
+
+    try:
+        since = float(since)
+    except (TypeError, ValueError):
+        since = None
+
+    files = []
+    for filename in os.listdir(directory_name):
+        filepath = os.path.join(directory_name, filename)
+        if not os.path.isfile(filepath):
+            continue
+        fstats = os.stat(filepath)
+        if not since or fstats.st_mtime > since:
+            files.append((filename, fstats.st_mtime))
+
+    return files
+
+
+def get_toys_sensors():
+
+    directory_name = os.environ.get("DAGSTER_TOY_SENSOR_DIRECTORY")
+
+    @sensor(pipeline_name="log_file_pipeline")
+    def toy_file_sensor(context):
+        if not directory_name:
+            yield SkipReason(
+                "No directory specified at environment variable `DAGSTER_TOY_SENSOR_DIRECTORY`"
+            )
+            return
+
+        if not os.path.isdir(directory_name):
+            yield SkipReason(f"Directory {directory_name} not found")
+            return
+
+        directory_files = get_directory_files(directory_name, context.cursor)
+        if not directory_files:
+            yield SkipReason(f"No new files found in {directory_name} (after {context.cursor})")
+            return
+
+        for filename, mtime in directory_files:
+            yield RunRequest(
+                run_key="{}:{}".format(filename, str(mtime)),
+                run_config={
+                    "solids": {
+                        "read_file": {"config": {"directory": directory_name, "filename": filename}}
+                    }
+                },
+            )
+
+    bucket = os.environ.get("DAGSTER_TOY_SENSOR_S3_BUCKET")
+
+    from dagster_aws.s3.sensor import get_s3_keys
+
+    @sensor(pipeline_name="log_s3_pipeline")
+    def toy_s3_sensor(context):
+        if not bucket:
+            raise Exception(
+                "S3 bucket not specified at environment variable `DAGSTER_TOY_SENSOR_S3_BUCKET`."
+            )
+
+        new_s3_keys = get_s3_keys(bucket, since_key=context.last_run_key)
+        if not new_s3_keys:
+            yield SkipReason(f"No s3 updates found for bucket {bucket}.")
+            return
+
+        for s3_key in new_s3_keys:
+            yield RunRequest(
+                run_key=s3_key,
+                run_config={
+                    "solids": {"read_s3_key": {"config": {"bucket": bucket, "s3_key": s3_key}}}
+                },
+            )
+
+    @pipeline_failure_sensor(pipeline_selection=["error_monster"])
+    def custom_slack_on_pipeline_failure(context: PipelineFailureSensorContext):
+
+        base_url = "http://localhost:3000"
+
+        # TBD: support resources?
+        slack_client = WebClient(token=os.environ["SLACK_DAGSTER_ETL_BOT_TOKEN"])
+
+        run_page_url = f"{base_url}/instance/runs/{context.pipeline_run.run_id}"
+        channel = "#yuhan-test"
+        message = "\n".join(
+            [
+                f'Pipeline "{context.pipeline_run.pipeline_name}" failed.',
+                f"error: {context.failure_event.message}",
+                f"mode: {context.pipeline_run.mode}",
+                f"run_page_url: {run_page_url}",
+            ]
+        )
+
+        slack_client.chat_postMessage(
+            channel=channel,
+            blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": message}}],
+        )
+
+    @asset_sensor(asset_key=AssetKey("model"), pipeline_name="log_asset_pipeline")
+    def toy_asset_sensor(context, asset_event):
+        yield RunRequest(
+            run_key=context.cursor,
+            run_config={
+                "solids": {
+                    "read_materialization": {
+                        "config": {"asset_key": ["model"], "pipeline": asset_event.pipeline_name}
+                    }
+                }
+            },
+        )
+
+    return [
+        toy_file_sensor,
+        toy_asset_sensor,
+        toy_s3_sensor,
+        custom_slack_on_pipeline_failure,
+    ]
