66fa94ebaba108a6efba27a3199082557743976e
prha <1040172+prha@users.noreply.github.com>
2021-09-02 10:14:41 -0700

    rename job_failure_sensor to be run_failure_sensor (#4715)

--- examples/docs_snippets_crag/docs_snippets_crag_tests/concepts_tests/partitions_schedules_sensors_tests/test_sensors.py
+++ examples/docs_snippets_crag/docs_snippets_crag_tests/concepts_tests/partitions_schedules_sensors_tests/test_sensors.py
@@ -1,10 +1,10 @@
 from dagster import execute_pipeline, pipeline, repository, solid
 from dagster.core.definitions.run_request import RunRequest
 from docs_snippets_crag.concepts.partitions_schedules_sensors.sensors.sensor_alert import (
-    email_on_job_failure,
-    my_slack_on_job_failure,
-    my_slack_on_job_success,
-    slack_on_job_failure,
+    email_on_run_failure,
+    my_slack_on_run_failure,
+    my_slack_on_run_success,
+    slack_on_run_failure,
 )
 from docs_snippets_crag.concepts.partitions_schedules_sensors.sensors.sensors import (
     isolated_run_request,
@@ -52,20 +52,20 @@ def test_interval_sensors():
     assert sensor_B
 
 
-def test_job_failure_sensor_def():
+def test_run_failure_sensor_def():
     @repository
     def my_repo():
         return [
-            my_slack_on_job_failure,
-            slack_on_job_failure,
-            email_on_job_failure,
-            my_slack_on_job_success,
+            my_slack_on_run_failure,
+            slack_on_run_failure,
+            email_on_run_failure,
+            my_slack_on_run_success,
         ]
 
-    assert my_repo.has_sensor_def("my_slack_on_job_failure")
-    assert my_repo.has_sensor_def("slack_on_job_failure")
-    assert my_repo.has_sensor_def("email_on_job_failure")
-    assert my_repo.has_sensor_def("my_slack_on_job_success")
+    assert my_repo.has_sensor_def("my_slack_on_run_failure")
+    assert my_repo.has_sensor_def("slack_on_run_failure")
+    assert my_repo.has_sensor_def("email_on_run_failure")
+    assert my_repo.has_sensor_def("my_slack_on_run_success")
 
 
 def test_sensor_testing_example():
