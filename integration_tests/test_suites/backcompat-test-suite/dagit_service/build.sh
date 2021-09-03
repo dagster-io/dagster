b856e502fe2e79e50dd9eb68e30afb1dd3fb339c
Chris DeCarolis <cdguitar817@gmail.com>
2021-09-03 19:28:11 -0400

    Split between release and source

--- integration_tests/test_suites/backcompat-test-suite/dagit_service/build.sh
+++ integration_tests/test_suites/backcompat-test-suite/dagit_service/build.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
+DAGIT_BACKCOMPAT_VERSION=$1
+USER_CODE_BACKCOMPAT_VERSION=$2
 
-pushd "/Users/christopherdecarolis/dagster/integration_tests/test_suites/backcompat-test-suite/dagit_service"
 
-docker-compose build
+ROOT=$(git rev-parse --show-toplevel)
+BASE_DIR="${ROOT}/integration_tests/test_suites/backcompat-test-suite/dagit_service"
+
+function cleanup {
+    rm -rf "${BASE_DIR}/python_modules"
+    set +ux
+    shopt -u expand_aliases
+}
+
+cleanup
+
+# ensure cleanup happens on error or normal exit
+trap cleanup INT TERM EXIT ERR
+
+set -ux
+shopt -s expand_aliases
+
+pushd $BASE_DIR
+
+mkdir -p python_modules
+mkdir -p python_modules/libraries/
+
+echo -e "--- \033[32m:truck: Copying files...\033[0m"
+alias copy_py="rsync -av \
+      --progress \
+      --exclude *.egginfo \
+      --exclude *.tox \
+      --exclude dist \
+      --exclude __pycache__ \
+      --exclude *.pyc \
+      --exclude .coverage"
+
+copy_py $ROOT/python_modules/dagster \
+        $ROOT/python_modules/dagit \
+        $ROOT/python_modules/dagster-graphql \
+        python_modules/
+
+copy_py $ROOT/python_modules/libraries/dagster-postgres \
+        $ROOT/python_modules/libraries/dagster-docker \
+        python_modules/libraries/
+
+echo -e "--- \033[32m:docker: Building Docker images\033[0m"
+docker-compose build \
+    --build-arg DAGIT_BACKCOMPAT_VERSION=${DAGIT_BACKCOMPAT_VERSION} \
+    --build-arg USER_CODE_BACKCOMPAT_VERSION=${USER_CODE_BACKCOMPAT_VERSION}
--- integration_tests/test_suites/backcompat-test-suite/test_backcompat.py
+++ integration_tests/test_suites/backcompat-test-suite/test_backcompat.py
@@ -1,16 +1,36 @@
-import pytest
-from contextlib import contextmanager
-from dagster import file_relative_path
-from dagster_graphql import DagsterGraphQLClient
+import os
 import subprocess
+import time
+from contextlib import contextmanager
+
+import pytest
+from dagster import file_relative_path
+from dagster.core.storage.pipeline_run import PipelineRunStatus
+from dagster_graphql import DagsterGraphQLClient
+
+MAX_TIMEOUT_SECONDS = 20
 
 pytest_plugins = ["dagster_test.fixtures"]
 
 # pylint: disable=redefined-outer-name
 RELEASE_TEST_MAP = {
-    "dagit__0.12.0": ["0.12.0", "current_branch"],
-    "user_code__0.12.0": ["current_branch", "0.12.0"],
+    "dagit__0.12.8": ["0.12.8", "current_branch"],
+    "user_code__0.12.8": ["current_branch", "0.12.8"],
 }
+
+
+def assert_run_success(client, run_id: int):
+    start_time = time.time()
+    while True:
+        if time.time() - start_time > MAX_TIMEOUT_SECONDS:
+            raise Exception("Timed out waiting for launched run to complete")
+
+        status = client.get_run_status(run_id)
+        assert status and status != PipelineRunStatus.FAILURE
+        if status == PipelineRunStatus.SUCCESS:
+            break
+
+        time.sleep(1)
 
 
 @pytest.fixture(
@@ -61,11 +81,41 @@ def graphql_client(release_test_map, retrying_requests):
     dagit_version = release_test_map["dagit"]
     user_code_version = release_test_map["user_code"]
 
-    with docker_service_up(file_relative_path(__file__, "./dagit_service/docker-compose.yml")):
+    if dagit_version == "current_branch":
+        os.environ["DAGIT_DOCKERFILE"] = "./Dockerfile_dagit_source"
+    else:
+        os.environ["DAGIT_DOCKERFILE"] = "./Dockerfile_dagit_release"
+
+    if user_code_version == "current_branch":
+        os.environ["USER_CODE_DOCKERFILE"] = "./Dockerfile_grpc_source"
+    else:
+        os.environ["USER_CODE_DOCKERFILE"] = "./Dockerfile_grpc_release"
+
+    with docker_service_up(
+        file_relative_path(__file__, "./dagit_service/docker-compose.yml"),
+        build_args=[dagit_version, user_code_version],
+    ):
         result = retrying_requests.get("http://127.0.0.1:3000/dagit_info")
         assert result.json().get("dagit_version")
         yield DagsterGraphQLClient("127.0.0.1", port_number=3000)
 
 
-def test_backcompat_deploy(graphql_client):
-    assert graphql_client
+def test_backcompat_deployed_pipeline(graphql_client):
+    assert_runs_and_exists(graphql_client, "the_pipeline")
+
+
+def test_backcompat_deployed_job(graphql_client):
+    assert_runs_and_exists(graphql_client, "the_job")
+
+
+def assert_runs_and_exists(client, name):
+    run_id = client.submit_pipeline_execution(pipeline_name=name, mode="default", run_config={})
+    assert_run_success(client, run_id)
+
+    locations = (
+        client._get_repo_locations_and_names_with_pipeline(  # pylint: disable=protected-access
+            pipeline_name=name
+        )
+    )
+    assert len(locations) == 1
+    assert locations[0].pipeline_name == name
