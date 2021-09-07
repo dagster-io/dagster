4c9e13bbbbe420009bf2e3517e33573a87e95159
alangenfeld <alex@elementl.com>
2021-09-07 16:52:41 -0500

    chunk event log subscription initial load

--- python_modules/dagit/dagit_tests/test_subscriptions.py
+++ python_modules/dagit/dagit_tests/test_subscriptions.py
@@ -5,7 +5,7 @@ from unittest import mock
 import objgraph
 from dagit.subscription_server import DagsterSubscriptionServer
 from dagster import execute_pipeline, pipeline, solid
-from dagster.core.test_utils import instance_for_test
+from dagster.core.test_utils import environ, instance_for_test
 from dagster.core.workspace.context import WorkspaceProcessContext
 from dagster.core.workspace.load_target import WorkspaceFileTarget
 from dagster.utils import file_relative_path
@@ -108,6 +108,28 @@ def test_event_log_subscription():
             assert len(objgraph.by_type("PipelineRunObservableSubscribe")) == 1
             end_subscription(server, context)
             gc.collect()
+            assert len(objgraph.by_type("SubscriptionObserver")) == 0
+            assert len(objgraph.by_type("PipelineRunObservableSubscribe")) == 0
+
+
+def test_event_log_subscription_chunked():
+    schema = create_schema()
+    server = DagsterSubscriptionServer(schema=schema)
+
+    with instance_for_test() as instance, environ({"DAGIT_EVENT_LOAD_CHUNK_SIZE": "2"}):
+        run = execute_pipeline(example_pipeline, instance=instance)
+        assert run.success
+        assert run.run_id
+
+        with create_subscription_context(instance) as context:
+            start_subscription(server, context, EVENT_LOG_SUBSCRIPTION, {"runId": run.run_id})
+            gc.collect()
+            assert len(objgraph.by_type("SubscriptionObserver")) == 1
+            assert len(objgraph.by_type("PipelineRunObservableSubscribe")) == 1
+
+            end_subscription(server, context)
+            gc.collect()
+
             assert len(objgraph.by_type("SubscriptionObserver")) == 0
             assert len(objgraph.by_type("PipelineRunObservableSubscribe")) == 0
 
