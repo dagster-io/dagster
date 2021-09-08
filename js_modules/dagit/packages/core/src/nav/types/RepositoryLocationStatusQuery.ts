02fa3c3b87c8dfc91d40b56864919dc2ac167398
Isaac Hellendag <dish@elementl.com>
2021-09-08 13:39:53 -0500

    [dagit] Poll for repo location status in reload workflow

---
+++ js_modules/dagit/packages/core/src/nav/types/RepositoryLocationStatusQuery.ts
@@ -0,0 +1,67 @@
+// @generated
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { RepositoryLocationLoadStatus } from "./../../types/globalTypes";
+
+// ====================================================
+// GraphQL query operation: RepositoryLocationStatusQuery
+// ====================================================
+
+export interface RepositoryLocationStatusQuery_workspaceOrError_PythonError {
+  __typename: "PythonError";
+}
+
+export interface RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_RepositoryLocation_repositories_pipelines {
+  __typename: "Pipeline";
+  id: string;
+  name: string;
+}
+
+export interface RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_RepositoryLocation_repositories {
+  __typename: "Repository";
+  id: string;
+  name: string;
+  pipelines: RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_RepositoryLocation_repositories_pipelines[];
+}
+
+export interface RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_RepositoryLocation {
+  __typename: "RepositoryLocation";
+  id: string;
+  repositories: RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_RepositoryLocation_repositories[];
+}
+
+export interface RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_PythonError_cause {
+  __typename: "PythonError";
+  message: string;
+  stack: string[];
+}
+
+export interface RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_PythonError {
+  __typename: "PythonError";
+  message: string;
+  stack: string[];
+  cause: RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_PythonError_cause | null;
+}
+
+export type RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError = RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_RepositoryLocation | RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError_PythonError;
+
+export interface RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries {
+  __typename: "WorkspaceLocationEntry";
+  id: string;
+  loadStatus: RepositoryLocationLoadStatus;
+  locationOrLoadError: RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries_locationOrLoadError | null;
+}
+
+export interface RepositoryLocationStatusQuery_workspaceOrError_Workspace {
+  __typename: "Workspace";
+  locationEntries: RepositoryLocationStatusQuery_workspaceOrError_Workspace_locationEntries[];
+}
+
+export type RepositoryLocationStatusQuery_workspaceOrError = RepositoryLocationStatusQuery_workspaceOrError_PythonError | RepositoryLocationStatusQuery_workspaceOrError_Workspace;
+
+export interface RepositoryLocationStatusQuery {
+  workspaceOrError: RepositoryLocationStatusQuery_workspaceOrError;
+}
