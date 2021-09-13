015d99c7ea166c8a857c09d4fdedc3db63b95ae5
Chris DeCarolis <cdguitar817@gmail.com>
2021-09-13 09:12:25 -0400

    Fix tests and snapshots

--- examples/docs_snippets_crag/docs_snippets_crag/concepts/configuration/execute_with_config.py
+++ examples/docs_snippets_crag/docs_snippets_crag/concepts/configuration/execute_with_config.py
@@ -5,13 +5,13 @@ from docs_snippets_crag.concepts.configuration.configurable_op_with_schema impor
 def execute_with_config():
     # start_define_job_with_config
     config_example_job = config_example.to_job(
-        run_config={"ops": {"config_example": {"config": {"iterations": 1}}}}
+        config={"ops": {"uses_config": {"config": {"iterations": 1}}}}
     )
     # end_define_job_with_config
     assert config_example_job.execute_in_process().success
     # start_execute_with_config
-    result = config_example.execute_in_process(
-        run_config={"ops": {"config_example": {"config": {"iterations": 1}}}}
+    result = config_example.to_job().execute_in_process(
+        run_config={"ops": {"uses_config": {"config": {"iterations": 1}}}}
     )
     # end_execute_with_config
     assert result.success
@@ -19,7 +19,7 @@ def execute_with_config():
 
 def execute_with_bad_config():
     # start_execute_with_bad_config
-    result = nests_configurable.execute_in_process(
+    result = nests_configurable.to_job().execute_in_process(
         run_config={
             "ops": {"configurable_with_schema": {"config": {"nonexistent_config_value": 1}}}
         }
