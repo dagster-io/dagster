141701f8bfa17f8dc48378cffe6e9f4118eaa3d6
acrulopez <a.cruz-lopez@kayrros.com>
2021-09-13 15:43:13 +0200

    allow to set configuration per worker on celeryK8sRunLauncher

--- helm/dagster/schema/schema/charts/dagster/subschema/run_launcher.py
+++ helm/dagster/schema/schema/charts/dagster/subschema/run_launcher.py
@@ -18,6 +18,7 @@ class CeleryWorkerQueue(BaseModel):
     name: str
     labels: Optional[kubernetes.Labels]
     nodeSelector: Optional[kubernetes.NodeSelector]
+    configSource: dict
 
     class Config:
         extra = Extra.forbid
