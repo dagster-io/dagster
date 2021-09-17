cb7a2325884493694339ccd9831225f59fd3c376
Christopher DeCarolis <cdguitar817@gmail.com>
2021-09-17 07:51:31 -0600

    Revert "Backcompat integration tests (#4739)"
    This reverts commit 12d499dfb4e4a3a4a783580ccc8ecc7680e43ea9.

--- .buildkite/dagster-buildkite/setup.py
+++ .buildkite/dagster-buildkite/setup.py
@@ -16,7 +16,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=find_packages(exclude=["test"]),
-    install_requires=["PyYAML", "packaging>=20.9", "requests"],
+    install_requires=["PyYAML"],
     entry_points={
         "console_scripts": [
             "dagster-buildkite = dagster_buildkite.cli:dagster",
