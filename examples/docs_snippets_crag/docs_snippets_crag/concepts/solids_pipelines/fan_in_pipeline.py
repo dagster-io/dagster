0f6f20d3954b3d29148264bf4e0deafd92f256ae
Sandy Ryza <sandy@elementl.com>
2021-09-17 13:03:51 -0400

    docs concept pages for graph and job

--- examples/docs_snippets_crag/docs_snippets_crag/concepts/solids_pipelines/fan_in_pipeline.py
+++ examples/docs_snippets_crag/docs_snippets_crag/concepts/solids_pipelines/fan_in_pipeline.py
@@ -2,21 +2,21 @@
 
 from typing import List
 
-from dagster import pipeline, solid
+from dagster import graph, op
 
 
-@solid
+@op
 def return_one() -> int:
     return 1
 
 
-@solid
+@op
 def sum_fan_in(nums: List[int]) -> int:
     return sum(nums)
 
 
-@pipeline
-def fan_in_pipeline():
+@graph
+def fan_in():
     fan_outs = []
     for i in range(0, 10):
         fan_outs.append(return_one.alias(f"return_one_{i}")())
