41a67b588f1ae99d16b26cb0cdb27edf1c604bd7
Isaac Hellendag <hellendag@users.noreply.github.com>
2021-09-24 14:59:23 -0500

    [dagit-bb] Menu (#4906)

---
+++ js_modules/dagit/packages/core/src/ui/Menu.stories.tsx
@@ -0,0 +1,46 @@
+import {Meta} from '@storybook/react/types-6-0';
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+import {Group} from './Group';
+import {MenuWIP as Menu, MenuItemWIP as MenuItem, MenuDividerWIP as MenuDivider} from './Menu';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: 'Menu',
+  component: Menu,
+} as Meta;
+
+export const Default = () => {
+  return (
+    <Group direction="column" spacing={8} padding={8}>
+      <Container style={{width: '180px'}}>
+        <Menu>
+          <MenuItem text="Item 01" />
+          <MenuItem text="Item 02" />
+          <MenuItem text="Item 03" />
+        </Menu>
+      </Container>
+      <Container style={{width: '180px'}}>
+        <Menu>
+          <MenuItem icon="folder" text="Item 01" />
+          <MenuItem icon="location_on" text="Item 02" />
+          <MenuItem icon="download_for_offline" text="Item 03" />
+          <MenuDivider />
+          <MenuItem icon="attach_file" text="Item 04" />
+        </Menu>
+      </Container>
+      <Container style={{width: '180px'}}>
+        <Menu>
+          <MenuItem icon="download_for_offline" text="Save" />
+          <MenuItem icon="attach_file" text="Attach" />
+          <MenuItem intent="danger" icon="delete" text="Delete" />
+        </Menu>
+      </Container>
+    </Group>
+  );
+};
+
+const Container = styled.div`
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 12px;
+`;
