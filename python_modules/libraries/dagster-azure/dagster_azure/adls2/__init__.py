44efde61ce35aefa944d70bdbfb1b74892a999a3
prha <prha@elementl.com>
2021-09-27 16:43:43 -0400

    cragify azure

--- python_modules/libraries/dagster-azure/dagster_azure/adls2/__init__.py
+++ python_modules/libraries/dagster-azure/dagster_azure/adls2/__init__.py
@@ -7,5 +7,3 @@ from .object_store import ADLS2ObjectStore
 from .resources import adls2_file_manager, adls2_resource
 from .system_storage import adls2_intermediate_storage, adls2_plus_default_intermediate_storage_defs
 from .utils import create_adls2_client
-
-# from .solids import ADLS2Coordinate, file_handle_to_adls2
--- python_modules/libraries/dagster-azure/dagster_azure_tests/blob_tests/test_compute_log_manager.py
+++ python_modules/libraries/dagster-azure/dagster_azure_tests/blob_tests/test_compute_log_manager.py
@@ -3,7 +3,7 @@ import sys
 import tempfile
 from unittest import mock
 
-from dagster import DagsterEventType, execute_pipeline, pipeline, solid
+from dagster import DagsterEventType, graph, op
 from dagster.core.instance import DagsterInstance, InstanceType
 from dagster.core.launcher.sync_in_memory_run_launcher import SyncInMemoryRunLauncher
 from dagster.core.run_coordinator import DefaultRunCoordinator
@@ -31,9 +31,9 @@ def test_compute_log_manager(
     fake_client = FakeBlobServiceClient(storage_account)
     mock_create_blob_client.return_value = fake_client
 
-    @pipeline
+    @graph
     def simple():
-        @solid
+        @op
         def easy(context):
             context.log.info("easy")
             print(HELLO_WORLD)  # pylint: disable=print-call
@@ -60,7 +60,7 @@ def test_compute_log_manager(
             run_coordinator=DefaultRunCoordinator(),
             run_launcher=SyncInMemoryRunLauncher(),
         )
-        result = execute_pipeline(simple, instance=instance)
+        result = simple.execute_in_process(instance=instance)
         compute_steps = [
             event.step_key
             for event in result.step_event_list
