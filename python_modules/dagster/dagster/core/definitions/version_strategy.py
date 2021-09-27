a665317b38d22c1e7bdb221adc2aa9c64effacc5
RBrossard <remy.brossard@outlook.com>
2021-09-27 16:00:44 +0200

    give VersionStrategy.get_X_version access to X config (#4830)
    * give VersionStrategy.get_X_version access to X config
    
    * fix black, isort, pylint
    
    Co-authored-by: Remy Brossard <remy@anotherbrain.ai>

--- python_modules/dagster/dagster/core/definitions/version_strategy.py
+++ python_modules/dagster/dagster/core/definitions/version_strategy.py
@@ -1,9 +1,64 @@
 from abc import ABC, abstractmethod
+from collections import namedtuple
 from typing import TYPE_CHECKING, Optional
+
+from dagster import check
 
 if TYPE_CHECKING:
     from .solid import SolidDefinition
     from .resource import ResourceDefinition
+
+
+class SolidVersionContext(
+    namedtuple(
+        "SolidVersionContext",
+        "solid_def solid_config",
+    )
+):
+    """Version-specific solid context.
+    Attributes:
+        solid_def (SolidDefinition): The definition of the versioned solid
+        solid_config (Any): The parsed config received by the versioned solid
+    """
+
+    def __new__(
+        cls,
+        solid_def,
+        solid_config,
+    ):
+        if TYPE_CHECKING:
+            solid_def = check.inst_param(
+                solid_def, "solid_def", SolidDefinition  # pylint: disable=E0601
+            )
+        return super(SolidVersionContext, cls).__new__(
+            cls, solid_def=solid_def, solid_config=solid_config
+        )
+
+
+class ResourceVersionContext(
+    namedtuple(
+        "SolidVersionContext",
+        "solid_def solid_config",
+    )
+):
+    """Version-specific solid context.
+    Attributes:
+        resource_def (ResourceDefinition): The definition of the versioned resource
+        resource_config (Any): The parsed config received by the versioned resource
+    """
+
+    def __new__(
+        cls,
+        resource_def,
+        resource_config,
+    ):
+        if TYPE_CHECKING:
+            resource_def = check.inst_param(
+                resource_def, "resource_def", ResourceDefinition  # pylint: disable=E0601
+            )
+        return super(ResourceVersionContext, cls).__new__(
+            cls, solid_def=resource_def, solid_config=resource_config
+        )
 
 
 class VersionStrategy(ABC):
@@ -12,18 +67,18 @@ class VersionStrategy(ABC):
     When subclassing, `get_solid_version` must be implemented, and `get_resource_version` can be
     optionally implemented.
 
-    `get_solid_version` should ingest a SolidDefinition, and `get_resource_version` should ingest a
-    ResourceDefinition. From that,  each synthesize a unique string called a `version`, which will
+    `get_solid_version` should ingest a SolidVersionContext, and `get_resource_version` should ingest a
+    ResourceVersionContext. From that,  each synthesize a unique string called a `version`, which will
     be tagged to outputs of that solid in the pipeline. Providing a `VersionStrategy` instance to a
     job will enable memoization on that job, such that only steps whose outputs do not have an
     up-to-date version will run.
     """
 
     @abstractmethod
-    def get_solid_version(self, solid_def: "SolidDefinition") -> str:
+    def get_solid_version(self, context: SolidVersionContext) -> str:
         pass
 
     def get_resource_version(
-        self, resource_def: "ResourceDefinition"  # pylint: disable=unused-argument
+        self, context: ResourceVersionContext  # pylint: disable=unused-argument
     ) -> Optional[str]:
         return None
