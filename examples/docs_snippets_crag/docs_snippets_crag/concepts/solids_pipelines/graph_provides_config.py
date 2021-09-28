dd4a07568b3e851a7b4e310f311e37335a781217
Sandy Ryza <sandy@elementl.com>
2021-09-28 10:51:58 -0700

    nesting doc

---
+++ examples/docs_snippets_crag/docs_snippets_crag/concepts/solids_pipelines/graph_provides_config.py
@@ -0,0 +1,16 @@
+from dagster import graph, op
+
+
+@op(config_schema={"n": float})
+def add_n(context, number):
+    return number + context.solid_config["n"]
+
+
+@op(config_schema={"m": float})
+def multiply_by_m(context, number):
+    return number * context.solid_config["m"]
+
+
+@graph(config={"multiply_by_m": {"config": {"m": 1.8}}, "add_n": {"config": {"n": 32}}})
+def celsius_to_fahrenheit(number):
+    return multiply_by_m(add_n(number))
