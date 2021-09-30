11aed399b546c62ee7536cc18cc1d2b37a282429
Isaac Hellendag <dish@elementl.com>
2021-09-30 16:21:24 -0500

    Delete some commented code

--- js_modules/dagit/packages/core/src/ui/Tag.tsx
+++ js_modules/dagit/packages/core/src/ui/Tag.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-restricted-imports
 import * as React from 'react';
 import styled from 'styled-components/macro';
 
@@ -36,29 +35,3 @@ const TagButton = styled.button`
     outline: none;
   }
 `;
-
-// interface TagChildProps {
-//   isDagsterTag?: boolean;
-// }
-
-// const TagElement = styled(({isDagsterTag, ...rest}) => <TagWIP {...rest} />)<TagChildProps>`
-//   padding: 1px !important;
-//   margin: 1px 2px !important;
-//   overflow: hidden;
-//   background-color: ${({isDagsterTag}) => (isDagsterTag ? Colors.LIGHT_GRAY2 : Colors.GRAY1)};
-//   border: ${({isDagsterTag}) =>
-//     isDagsterTag ? `1px solid ${Colors.LIGHT_GRAY1}` : `1px solid ${Colors.GRAY1}`};
-//   color: ${({isDagsterTag}) => (isDagsterTag ? Colors.DARK_GRAY1 : Colors.WHITE)};
-//   cursor: ${({onClick}) => (onClick ? `pointer` : 'default')};
-//   max-width: 400px;
-// `;
-
-// const TagKey = styled.span`
-//   padding: 2px 4px;
-// `;
-
-// const TagValue = styled.span<TagChildProps>`
-//   background-color: ${({isDagsterTag}) => (isDagsterTag ? Colors.LIGHT_GRAY4 : Colors.GRAY3)};
-//   border-radius: 3px;
-//   padding: 2px 4px;
-// `;
