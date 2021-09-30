22fbc8747ce76dee9c277ac7d13f1143d24c6c6a
Owen Kephart <owen@elementl.com>
2021-09-30 09:28:45 -0700

    pr

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/logging_tests/test_instance_config.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/logging_tests/test_instance_config.py
@@ -14,6 +14,17 @@ def test_valid_managed_loggers_instance_yaml():
     assert instance.managed_python_loggers == ["root"]
 
 
+def test_valid_managed_loggers_instance_yaml_2():
+    ref = InstanceRef.from_dir(
+        base_dir=file_relative_path(
+            __file__, "../../../docs_snippets/concepts/logging"
+        ),
+        config_filename="python_logging_managed_loggers_config2.yaml",
+    )
+    instance = DagsterInstance.from_ref(ref)
+    assert instance.managed_python_loggers == ["my_logger", "my_other_logger"]
+
+
 def test_valid_log_level_instance_yaml():
     ref = InstanceRef.from_dir(
         base_dir=file_relative_path(
@@ -22,7 +33,7 @@ def test_valid_log_level_instance_yaml():
         config_filename="python_logging_python_log_level_config.yaml",
     )
     instance = DagsterInstance.from_ref(ref)
-    assert instance.python_log_level == "ERROR"
+    assert instance.python_log_level == "INFO"
 
 
 def test_valid_handler_instance_yaml():
