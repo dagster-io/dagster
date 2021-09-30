685ee58d5d195f640c40fa167966fe747dc36728
gibsondan <daniel@elementl.com>
2021-09-30 15:45:51 -0500

    remove version pin from dagster-test
    Summary:
    This library never has its version upgraded, so it doesn't make sense to pin it. My hunch is this may help with weird test failures in the RC branch.

--- python_modules/dagster-test/setup.py
+++ python_modules/dagster-test/setup.py
@@ -1,23 +1,9 @@
-from typing import Dict
-
 from setuptools import find_packages, setup  # type: ignore
 
-
-def get_version() -> str:
-    version: Dict[str, str] = {}
-    with open("dagster_test/version.py") as fp:
-        exec(fp.read(), version)  # pylint: disable=W0122
-
-    return version["__version__"]
-
-
 if __name__ == "__main__":
-    ver = get_version()
-    # dont pin dev installs to avoid pip dep resolver issues
-    pin = "" if ver == "dev" else f"=={ver}"
     setup(
         name="dagster-test",
-        version=ver,
+        version="dev",
         author="Elementl",
         author_email="hello@elementl.com",
         license="Apache-2.0",
@@ -32,7 +18,7 @@ if __name__ == "__main__":
         ],
         packages=find_packages(exclude=["test"]),
         install_requires=[
-            f"dagster{pin}",
+            "dagster",
             "pyspark",
         ],
         zip_safe=False,
