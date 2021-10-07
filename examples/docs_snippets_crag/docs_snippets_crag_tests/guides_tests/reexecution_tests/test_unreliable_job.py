3dec8e40472a3b914a316f133324c5dc85b01d92
Rex Ledesma <rex@elementl.com>
2021-10-07 11:31:13 -0700

    test: do not raise on error for unreliable job (#5069)

--- examples/docs_snippets_crag/docs_snippets_crag_tests/guides_tests/reexecution_tests/test_unreliable_job.py
+++ examples/docs_snippets_crag/docs_snippets_crag_tests/guides_tests/reexecution_tests/test_unreliable_job.py
@@ -2,5 +2,5 @@ from docs_snippets_crag.guides.dagster.reexecution.unreliable_job import unrelia
 
 
 def test_job_compiles_and_executes():
-    result = unreliable_job.execute_in_process()
+    result = unreliable_job.execute_in_process(raise_on_error=False)
     assert result
