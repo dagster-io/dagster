ceb7b5927df15c90d28f77a5794d3501035c231f
Sandy Ryza <sandy@elementl.com>
2021-10-08 16:31:29 -0700

    HN demo pipeline code improvements

--- examples/hacker_news/hacker_news/repo.py
+++ examples/hacker_news/hacker_news/repo.py
@@ -1,26 +1,19 @@
 from dagster import repository, schedule_from_partitions
 
+from .jobs.dbt_metrics import dbt_prod_job, dbt_staging_job
 from .jobs.hacker_news_api_download import download_prod_job, download_staging_job
-from .sensors.download_finished_sensor import (
-    dbt_on_hn_download_finished_prod,
-    dbt_on_hn_download_finished_staging,
-)
-from .sensors.hn_tables_updated_sensor import (
-    story_recommender_on_hn_table_update_prod,
-    story_recommender_on_hn_table_update_staging,
-)
-from .sensors.slack_on_failure_sensor import make_job_failure_sensor
+from .jobs.story_recommender import story_recommender_prod_job, story_recommender_staging_job
+from .sensors.hn_tables_updated_sensor import make_hn_tables_updated_sensor
+from .sensors.slack_on_failure_sensor import make_slack_on_failure_sensor
 
 
 @repository
 def hacker_news_prod():
     return [
         schedule_from_partitions(download_prod_job),
-        make_job_failure_sensor(
-            base_url="my_dagit_url.com", job_selection=[download_prod_job.name]
-        ),
-        story_recommender_on_hn_table_update_prod,
-        dbt_on_hn_download_finished_prod,
+        make_slack_on_failure_sensor(base_url="my_prod_dagit_url.com"),
+        make_hn_tables_updated_sensor(story_recommender_prod_job),
+        make_hn_tables_updated_sensor(dbt_prod_job),
     ]
 
 
@@ -28,9 +21,7 @@ def hacker_news_prod():
 def hacker_news_staging():
     return [
         schedule_from_partitions(download_staging_job),
-        make_job_failure_sensor(
-            base_url="my_dagit_url.com", job_selection=[download_staging_job.name]
-        ),
-        story_recommender_on_hn_table_update_staging,
-        dbt_on_hn_download_finished_staging,
+        make_slack_on_failure_sensor(base_url="my_staging_dagit_url.com"),
+        make_hn_tables_updated_sensor(story_recommender_staging_job),
+        make_hn_tables_updated_sensor(dbt_staging_job),
     ]
