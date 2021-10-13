26f1cd5885508941b28df7c90a284f62ddd5d295
prha <1040172+prha@users.noreply.github.com>
2021-10-13 13:55:57 -0700

    ensure we perform run migrations in chunks (#5175)

--- python_modules/dagster/dagster/core/storage/runs/migration.py
+++ python_modules/dagster/dagster/core/storage/runs/migration.py
@@ -1,3 +1,5 @@
+from contextlib import ExitStack
+
 import pendulum
 from tqdm import tqdm
 
@@ -12,24 +14,47 @@ RUN_DATA_MIGRATIONS = {
     MODE_MIGRATION: lambda: migrate_mode_column,
 }
 
+RUN_CHUNK_SIZE = 100
 
-def migrate_run_partition(instance, print_fn=None):
+
+def chunked_run_iterator(storage, print_fn=None, chunk_size=RUN_CHUNK_SIZE):
+    with ExitStack() as stack:
+        if print_fn:
+            run_count = storage.get_runs_count()
+            progress = stack.enter_context(tqdm(total=run_count))
+        else:
+            progress = None
+
+        cursor = None
+        has_more = True
+
+        while has_more:
+            chunk = storage.get_runs(cursor=cursor, limit=chunk_size)
+            has_more = chunk_size and len(chunk) >= chunk_size
+
+            for run in chunk:
+                cursor = run.run_id
+                yield run
+
+            if progress:
+                progress.update(len(chunk))
+
+
+def migrate_run_partition(storage, print_fn=None):
     """
     Utility method to build an asset key index from the data in existing event log records.
     Takes in event_log_storage, and a print_fn to keep track of progress.
     """
     if print_fn:
         print_fn("Querying run storage.")
-    runs = instance.get_runs()
-    if print_fn:
-        runs = tqdm(runs)
-    for run in runs:
+
+    for run in chunked_run_iterator(storage, print_fn):
         if PARTITION_NAME_TAG not in run.tags:
             continue
         if PARTITION_SET_TAG not in run.tags:
             continue
 
-        instance.add_run_tags(run.run_id, run.tags)
+        storage.add_run_tags(run.run_id, run.tags)
 
 
 def migrate_mode_column(storage, print_fn=None):
@@ -40,11 +65,9 @@ def migrate_mode_column(storage, print_fn=None):
 
     if print_fn:
         print_fn("Querying run storage.")
+
     with storage.connect() as conn:
-        runs = storage.get_runs()
-        if print_fn:
-            runs = tqdm(runs)
-        for run in runs:
+        for run in chunked_run_iterator(storage, print_fn):
             conn.execute(
                 RunsTable.update()  # pylint: disable=no-value-for-parameter
                 .where(RunsTable.c.run_id == run.run_id)
