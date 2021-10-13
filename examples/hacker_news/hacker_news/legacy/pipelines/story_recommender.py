8378b96d523832128f0d51070955f28cccb1e1af
Owen Kephart <owen@elementl.com>
2021-10-13 11:27:33 -0700

    update legacy as well

--- examples/hacker_news/hacker_news/legacy/pipelines/story_recommender.py
+++ examples/hacker_news/hacker_news/legacy/pipelines/story_recommender.py
@@ -1,6 +1,7 @@
 from dagster import ModeDefinition, fs_io_manager, pipeline
 from dagster.core.storage.file_manager import local_file_manager
 from dagster_aws.s3 import s3_file_manager
+from dagstermill.io_managers import local_output_notebook_io_manager
 from hacker_news.ops.comment_stories import build_comment_stories
 from hacker_news.ops.recommender_model import (
     build_component_top_stories,
@@ -10,6 +11,7 @@ from hacker_news.ops.recommender_model import (
 from hacker_news.ops.user_story_matrix import build_user_story_matrix
 from hacker_news.ops.user_top_recommended_stories import build_user_top_recommended_stories
 from hacker_news.resources.fixed_s3_pickle_io_manager import fixed_s3_pickle_io_manager
+from hacker_news.resources.s3_notebook_io_manager import s3_notebook_io_manager
 from hacker_news.resources.snowflake_io_manager import snowflake_io_manager
 
 snowflake_manager = snowflake_io_manager.configured(
@@ -29,7 +31,7 @@ DEV_MODE = ModeDefinition(
         "io_manager": fs_io_manager,
         "warehouse_io_manager": fs_io_manager,
         "warehouse_loader": snowflake_manager,
-        "file_manager": local_file_manager,
+        "output_notebook_io_manager": local_output_notebook_io_manager,
     },
 )
 
@@ -40,7 +42,9 @@ PROD_MODE = ModeDefinition(
         "io_manager": fixed_s3_pickle_io_manager.configured({"bucket": "hackernews-elementl-prod"}),
         "warehouse_io_manager": snowflake_manager,
         "warehouse_loader": snowflake_manager,
-        "file_manager": s3_file_manager.configured({"s3_bucket": "hackernews-elementl-prod"}),
+        "output_notebook_io_manager": s3_notebook_io_manager.configured(
+            {"bucket": "hackernews-elementl-prod"}
+        ),
     },
 )
 
