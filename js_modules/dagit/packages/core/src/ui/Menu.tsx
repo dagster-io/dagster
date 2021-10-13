983c91435fea68e531fbd1796ac6de4b6aade109
Isaac Hellendag <dish@elementl.com>
2021-10-13 15:56:51 -0500

    Lint against Blueprint Menu, remove a console.log

--- js_modules/dagit/packages/core/src/ui/Menu.tsx
+++ js_modules/dagit/packages/core/src/ui/Menu.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-restricted-imports
 import {Menu, MenuDivider, MenuItem} from '@blueprintjs/core';
 import * as React from 'react';
 import styled from 'styled-components/macro';
