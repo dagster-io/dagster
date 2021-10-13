d1db10a3ff9f716593845fe34d1e5f81e93e3476
Owen Kephart <owen@elementl.com>
2021-10-13 09:32:43 -0700

    switch to output_notebook_io_manager

--- examples/hacker_news/hacker_news/jobs/story_recommender.py
+++ examples/hacker_news/hacker_news/jobs/story_recommender.py
@@ -1,5 +1,6 @@
 from dagster import fs_io_manager, graph
-from dagster_aws.s3 import s3_pickle_io_manager, s3_resource
+from dagster_aws.s3 import s3_resource
+from dagstermill.io_managers import local_output_notebook_io_manager
 from hacker_news.ops.comment_stories import build_comment_stories
 from hacker_news.ops.recommender_model import (
     build_component_top_stories,
@@ -9,6 +10,7 @@ from hacker_news.ops.recommender_model import (
 from hacker_news.ops.user_story_matrix import build_user_story_matrix
 from hacker_news.ops.user_top_recommended_stories import build_user_top_recommended_stories
 from hacker_news.resources.fixed_s3_pickle_io_manager import fixed_s3_pickle_io_manager
+from hacker_news.resources.s3_notebook_io_manager import s3_notebook_io_manager
 from hacker_news.resources.snowflake_io_manager import snowflake_io_manager
 
 snowflake_manager = snowflake_io_manager.configured(
@@ -25,7 +27,7 @@ STORY_RECOMMENDER_RESOURCES_DEV = {
     "io_manager": fs_io_manager,
     "warehouse_io_manager": fs_io_manager,
     "warehouse_loader": snowflake_manager,
-    "output_notebook_io_manager": fs_io_manager,
+    "output_notebook_io_manager": local_output_notebook_io_manager,
 }
 
 STORY_RECOMMENDER_RESOURCES_STAGING = {
@@ -33,8 +35,8 @@ STORY_RECOMMENDER_RESOURCES_STAGING = {
     "warehouse_io_manager": snowflake_manager,
     "warehouse_loader": snowflake_manager,
     "s3": s3_resource,
-    "output_notebook_io_manager": s3_pickle_io_manager.configured(
-        {"s3_bucket": "hackernews-elementl-dev", "s3_prefix": "notebooks"}
+    "output_notebook_io_manager": s3_notebook_io_manager.configured(
+        {"bucket": "hackernews-elementl-dev"}
     ),
 }
 
@@ -43,8 +45,8 @@ STORY_RECOMMENDER_RESOURCES_PROD = {
     "warehouse_io_manager": snowflake_manager,
     "warehouse_loader": snowflake_manager,
     "s3": s3_resource,
-    "output_notebook_io_manager": s3_pickle_io_manager.configured(
-        {"s3_bucket": "hackernews-elementl-prod", "s3_prefix": "notebooks"}
+    "output_notebook_io_manager": s3_notebook_io_manager.configured(
+        {"bucket": "hackernews-elementl-prod"}
     ),
 }
 
