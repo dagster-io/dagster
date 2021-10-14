50170f76a2b9021d5b5b2e21e6326c47364c6dc8
Jordan Sanders <jordan@elementl.com>
2021-10-14 09:33:03 -0500

    Switch to using .execute_in_process() in all tests

--- python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_io_manager.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_io_manager.py
@@ -1,36 +1,14 @@
 from dagster import (
-    DagsterInstance,
     In,
     Int,
     Out,
     Output,
-    PipelineRun,
-    build_input_context,
-    build_output_context,
     job,
     op,
     resource,
 )
-from dagster.core.definitions.pipeline_base import InMemoryPipeline
-from dagster.core.events import DagsterEventType
-from dagster.core.execution.api import execute_plan
-from dagster.core.execution.plan.outputs import StepOutputHandle
-from dagster.core.execution.plan.plan import ExecutionPlan
-from dagster.core.system_config.objects import ResolvedRunConfig
-from dagster.core.utils import make_new_run_id
-from dagster_aws.s3.io_manager import PickledObjectS3IOManager, s3_pickle_io_manager
+from dagster_aws.s3.io_manager import s3_pickle_io_manager
 from dagster_aws.s3.utils import construct_s3_client
-
-
-def get_step_output(step_events, step_key, output_name="result"):
-    for step_event in step_events:
-        if (
-            step_event.event_type == DagsterEventType.STEP_OUTPUT
-            and step_event.step_key == step_key
-            and step_event.step_output_data.output_name == output_name
-        ):
-            return step_event
-    return None
 
 
 def define_inty_job():
@@ -62,67 +40,17 @@ def define_inty_job():
 
 
 def test_s3_pickle_io_manager_execution(mock_s3_bucket):
+    assert not len(list(mock_s3_bucket.objects.all()))
     inty_job = define_inty_job()
 
     run_config = {"resources": {"io_manager": {"config": {"s3_bucket": mock_s3_bucket.name}}}}
 
-    run_id = make_new_run_id()
+    result = inty_job.execute_in_process(run_config)
 
-    resolved_run_config = ResolvedRunConfig.build(inty_job, run_config=run_config)
-    execution_plan = ExecutionPlan.build(InMemoryPipeline(inty_job), resolved_run_config)
+    assert result.output_for_node("return_one") == 1
+    assert result.output_for_node("add_one") == 2
 
-    assert execution_plan.get_step_by_key("return_one")
-
-    step_keys = ["return_one"]
-    instance = DagsterInstance.ephemeral()
-    pipeline_run = PipelineRun(pipeline_name=inty_job.name, run_id=run_id, run_config=run_config)
-
-    return_one_step_events = list(
-        execute_plan(
-            execution_plan.build_subset_plan(step_keys, inty_job, resolved_run_config),
-            pipeline=InMemoryPipeline(inty_job),
-            run_config=run_config,
-            pipeline_run=pipeline_run,
-            instance=instance,
-        )
-    )
-
-    assert get_step_output(return_one_step_events, "return_one")
-
-    io_manager = PickledObjectS3IOManager(
-        mock_s3_bucket.name, construct_s3_client(max_attempts=5), s3_prefix="dagster"
-    )
-    step_output_handle = StepOutputHandle("return_one")
-    context = build_input_context(
-        upstream_output=build_output_context(
-            step_key=step_output_handle.step_key,
-            name=step_output_handle.output_name,
-            run_id=run_id,
-        )
-    )
-    assert io_manager.load_input(context) == 1
-
-    add_one_step_events = list(
-        execute_plan(
-            execution_plan.build_subset_plan(["add_one"], inty_job, resolved_run_config),
-            pipeline=InMemoryPipeline(inty_job),
-            run_config=run_config,
-            pipeline_run=pipeline_run,
-            instance=instance,
-        )
-    )
-
-    step_output_handle = StepOutputHandle("add_one")
-    context = build_input_context(
-        upstream_output=build_output_context(
-            step_key=step_output_handle.step_key,
-            name=step_output_handle.output_name,
-            run_id=run_id,
-        )
-    )
-
-    assert get_step_output(add_one_step_events, "add_one")
-    assert io_manager.load_input(context) == 2
+    assert len(list(mock_s3_bucket.objects.all())) == 2
 
 
 def define_multiple_output_job():
