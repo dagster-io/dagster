a56898c929a1dbac78da19eb85de5769f400f793
Jordan Sanders <jordan@elementl.com>
2021-10-14 11:07:08 -0500

    Fixup prefixed IO manager tests (#5189)
    This test was failing initially because of a syntax error. But once I
    fixed that, it continued to fail because no outputs were actually being
    written.
    
    I switched from the existing pattern of building and traversing the
    execution plan by hand to our new `job.execute_in_process()` method and
    it helped pinpoint the issue to us not yielding an `Output()` from our
    job.
    
    I like this test implementation because I think it makes the behavior
    under test easier to understand - we expect to see two keys written to
    S3 and we expect them to contain the appropriate outputs.

--- python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_io_manager.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_io_manager.py
@@ -1,35 +1,6 @@
-from dagster import (
-    DagsterInstance,
-    In,
-    Int,
-    Out,
-    PipelineRun,
-    build_input_context,
-    build_output_context,
-    job,
-    op,
-    resource,
-)
-from dagster.core.definitions.pipeline_base import InMemoryPipeline
-from dagster.core.events import DagsterEventType
-from dagster.core.execution.api import execute_plan
-from dagster.core.execution.plan.outputs import StepOutputHandle
-from dagster.core.execution.plan.plan import ExecutionPlan
-from dagster.core.system_config.objects import ResolvedRunConfig
-from dagster.core.utils import make_new_run_id
-from dagster_aws.s3.io_manager import PickledObjectS3IOManager, s3_pickle_io_manager
+from dagster import In, Int, Out, Output, job, op, resource
+from dagster_aws.s3.io_manager import s3_pickle_io_manager
 from dagster_aws.s3.utils import construct_s3_client
-
-
-def get_step_output(step_events, step_key, output_name="result"):
-    for step_event in step_events:
-        if (
-            step_event.event_type == DagsterEventType.STEP_OUTPUT
-            and step_event.step_key == step_key
-            and step_event.step_output_data.output_name == output_name
-        ):
-            return step_event
-    return None
 
 
 def define_inty_job():
@@ -61,67 +32,17 @@ def define_inty_job():
 
 
 def test_s3_pickle_io_manager_execution(mock_s3_bucket):
+    assert not len(list(mock_s3_bucket.objects.all()))
     inty_job = define_inty_job()
 
     run_config = {"resources": {"io_manager": {"config": {"s3_bucket": mock_s3_bucket.name}}}}
 
-    run_id = make_new_run_id()
+    result = inty_job.execute_in_process(run_config)
 
-    resolved_run_config = ResolvedRunConfig.build(inty_job, run_config=run_config)
-    execution_plan = ExecutionPlan.build(InMemoryPipeline(inty_job), resolved_run_config)
+    assert result.output_for_node("return_one") == 1
+    assert result.output_for_node("add_one") == 2
 
-    assert execution_plan.get_step_by_key("return_one")
-
-    step_keys = ["return_one"]
-    instance = DagsterInstance.ephemeral()
-    pipeline_run = PipelineRun(pipeline_name=inty_job.name, run_id=run_id, run_config=run_config)
-
-    return_one_step_events = list(
-        execute_plan(
-            execution_plan.build_subset_plan(step_keys, inty_job, resolved_run_config),
-            pipeline=InMemoryPipeline(inty_job),
-            run_config=run_config,
-            pipeline_run=pipeline_run,
-            instance=instance,
-        )
-    )
-
-    assert get_step_output(return_one_step_events, "return_one")
-
-    io_manager = PickledObjectS3IOManager(
-        mock_s3_bucket.name, construct_s3_client(max_attempts=5), s3_prefix="dagster"
-    )
-    step_output_handle = StepOutputHandle("return_one")
-    context = build_input_context(
-        upstream_output=build_output_context(
-            step_key=step_output_handle.step_key,
-            name=step_output_handle.output_name,
-            run_id=run_id,
-        )
-    )
-    assert io_manager.load_input(context) == 1
-
-    add_one_step_events = list(
-        execute_plan(
-            execution_plan.build_subset_plan(["add_one"], inty_job, resolved_run_config),
-            pipeline=InMemoryPipeline(inty_job),
-            run_config=run_config,
-            pipeline_run=pipeline_run,
-            instance=instance,
-        )
-    )
-
-    step_output_handle = StepOutputHandle("add_one")
-    context = build_input_context(
-        upstream_output=build_output_context(
-            step_key=step_output_handle.step_key,
-            name=step_output_handle.output_name,
-            run_id=run_id,
-        )
-    )
-
-    assert get_step_output(add_one_step_events, "add_one")
-    assert io_manager.load_input(context) == 2
+    assert len(list(mock_s3_bucket.objects.all())) == 2
 
 
 def define_multiple_output_job():
@@ -131,77 +52,31 @@ def define_multiple_output_job():
 
     @op(
         out={
-            "foo": Out(Int, io_manager_key="io_manager"),
-            "foobar": Out(Int, io_manager_key="io_manager"),
+            "foo": Out(Int),
+            "foobar": Out(Int),
         }
     )
-    def baz():
-        yield 5
-        yield 10
+    def return_two_outputs():
+        yield Output(10, "foobar")
+        yield Output(5, "foo")
 
-    
-    @job(
-        resource_defs={
-            "io_manager" = s3_pickle_io_manager,
-            "s3": test_s3_resource
-            }
-    )
+    @job(resource_defs={"io_manager": s3_pickle_io_manager, "s3": test_s3_resource})
     def output_prefix_execution_plan():
-        baz()
+        return_two_outputs()
 
     return output_prefix_execution_plan
 
 
 def test_s3_pickle_io_manager_prefix(mock_s3_bucket):
+    assert not len(list(mock_s3_bucket.objects.all()))
 
     prefixy_job = define_multiple_output_job()
 
     run_config = {"resources": {"io_manager": {"config": {"s3_bucket": mock_s3_bucket.name}}}}
 
-    run_id = make_new_run_id()
+    result = prefixy_job.execute_in_process(run_config)
 
-    resolved_run_config = ResolvedRunConfig.build(prefixy_job, run_config=run_config)
-    execution_plan = ExecutionPlan.build(InMemoryPipeline(prefixy_job), resolved_run_config)
+    assert result.output_for_node("return_two_outputs", "foo") == 5
+    assert result.output_for_node("return_two_outputs", "foobar") == 10
 
-    assert execution_plan.get_step_by_key("baz")
-
-    instance = DagsterInstance.ephemeral()
-
-
-    step_keys = ["baz"]
-    instance = DagsterInstance.ephemeral()
-    pipeline_run = PipelineRun(pipeline_name=prefixy_job.name, run_id=run_id, run_config=run_config)
-
-    baz_step_events = list(
-        execute_plan(
-            execution_plan.build_subset_plan(step_keys, prefixy_job, resolved_run_config),
-            pipeline=InMemoryPipeline(prefixy_job),
-            run_config=run_config,
-            pipeline_run=pipeline_run,
-            instance=instance,
-        )
-    )
-    
-    five = get_step_output(baz_step_events, "baz", "foo")
-    ten = get_step_output(baz_step_events, "baz", "foobar")
-
-    assert five and ten
-    assert five == 5 and ten == 10
-
-    mocked_s3_client = construct_s3_client(max_attempts=5)
-    io_manager = PickledObjectS3IOManager(
-        mock_s3_bucket.name, mocked_s3_client, s3_prefix="dagster"
-    )
-
-    step_output_handle = StepOutputHandle("baz")
-    context = build_output_context(
-            step_key = step_output_handle.step_key,
-            name = step_output_handle.output_name,
-            run_id = run_id
-    )
-    io_manager.handle_output(context, five)
-    io_manager.handle_output(context, ten)
-    
-    assert mocked_s3_client.get_object(Bucket=mock_s3_bucket.name, Key="five")
-    assert mocked_s3_client.get_object(Bucket=mock_s3_bucket.name, Key="ten")
-
+    assert len(list(mock_s3_bucket.objects.all())) == 2
