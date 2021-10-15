e32c56f8a6d627fb4072b9f23e48b40b36ff177e
Claire <clairelin135@gmail.com>
2021-10-15 11:32:47 -0700

    add moto to tox imports

--- examples/docs_snippets_crag/docs_snippets_crag_tests/concepts_tests/io_management_tests/test_io_manager_per_output.py
+++ examples/docs_snippets_crag/docs_snippets_crag_tests/concepts_tests/io_management_tests/test_io_manager_per_output.py
@@ -1,8 +1,8 @@
-from tempfile import TemporaryDirectory
-
+from dagster_aws_tests.conftest import (  # pylint: disable=unused-import
+    mock_s3_bucket,
+    mock_s3_resource,
+)
 from docs_snippets_crag.concepts.io_management.io_manager_per_output import my_job
-
-from dagster_aws_tests.conftest import mock_s3_bucket, mock_s3_resource
 
 
 def test_io_manager_per_output(mock_s3_bucket):
