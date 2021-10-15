4dc458d54e35f4903babb35333bf8bdbd3a46e75
Claire <clairelin135@gmail.com>
2021-10-15 12:47:30 -0700

    pylint

--- examples/docs_snippets_crag/docs_snippets_crag_tests/concepts_tests/io_management_tests/test_io_manager_per_output.py
+++ examples/docs_snippets_crag/docs_snippets_crag_tests/concepts_tests/io_management_tests/test_io_manager_per_output.py
@@ -5,7 +5,7 @@ from dagster_aws_tests.conftest import (  # pylint: disable=unused-import
 from docs_snippets_crag.concepts.io_management.io_manager_per_output import my_job
 
 
-def test_io_manager_per_output(mock_s3_bucket):
+def test_io_manager_per_output(mock_s3_bucket):  # pylint: disable=redefined-outer-name
     my_job.execute_in_process(
         run_config={"resources": {"s3_io": {"config": {"s3_bucket": mock_s3_bucket.name}}}},
     )
