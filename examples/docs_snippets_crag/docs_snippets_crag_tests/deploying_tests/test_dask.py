e4e6bdfdf56f40f14e8f528a18687f05f5022849
gibsondan <daniel@elementl.com>
2021-10-15 16:16:53 -0500

    Unbreak docs build/tests
    Summary:
    One bad test from me, one bad snapshot from some other PR

--- examples/docs_snippets_crag/docs_snippets_crag_tests/deploying_tests/test_dask.py
+++ examples/docs_snippets_crag/docs_snippets_crag_tests/deploying_tests/test_dask.py
@@ -1,17 +1,13 @@
-from dagster import execute_pipeline, reconstructable
 from dagster.core.test_utils import instance_for_test
 from docs_snippets_crag.deploying.dask_hello_world import (  # pylint: disable=import-error
-    dask_pipeline,
+    local_dask_job,
 )
 
 
 def test_local_dask_pipeline():
     with instance_for_test() as instance:
-        result = execute_pipeline(
-            reconstructable(dask_pipeline),
-            mode="local",
-            run_config={"execution": {"multiprocess": {}}},
+        result = local_dask_job.execute_in_process(
             instance=instance,
         )
         assert result.success
-        assert result.result_for_solid("hello_world").output_value() == "Hello, World!"
+        assert result.output_for_node("hello_world") == "Hello, World!"
