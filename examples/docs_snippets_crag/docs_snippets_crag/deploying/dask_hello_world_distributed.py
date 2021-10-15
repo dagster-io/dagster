7b680262102872fc98a63514a8f6034971a61778
gibsondan <daniel@elementl.com>
2021-10-15 15:14:45 -0500

    CRAGify dask docs (#5193)

---
+++ examples/docs_snippets_crag/docs_snippets_crag/deploying/dask_hello_world_distributed.py
@@ -0,0 +1,22 @@
+# start_distributed_job_marker
+
+from dagster import job, op
+from dagster_aws.s3.io_manager import s3_pickle_io_manager
+from dagster_aws.s3.resources import s3_resource
+from dagster_dask import dask_executor
+
+
+@op
+def hello_world():
+    return "Hello, World!"
+
+
+@job(
+    executor_def=dask_executor,
+    resource_defs={"io_manager": s3_pickle_io_manager, "s3": s3_resource},
+)
+def distributed_dask_job():
+    hello_world()
+
+
+# end_distributed_job_marker
