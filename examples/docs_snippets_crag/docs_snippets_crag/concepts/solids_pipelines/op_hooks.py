105fb7059c331544218ed614e6430dac5252e3db
Claire Lin <clairelin135@gmail.com>
2021-10-15 17:05:05 -0700

    [Crag] Update Op Hooks Doc (#4817)

--- examples/docs_snippets_crag/docs_snippets_crag/concepts/solids_pipelines/op_hooks.py
+++ examples/docs_snippets_crag/docs_snippets_crag/concepts/solids_pipelines/op_hooks.py
@@ -3,13 +3,12 @@ import yaml
 from unittest import mock
 from dagster_slack import slack_resource
 from dagster import (
-    ModeDefinition,
     ResourceDefinition,
-    execute_pipeline,
     file_relative_path,
-    pipeline,
+    graph,
+    job,
+    op,
     repository,
-    solid,
 )
 
 # start_repo_marker_0
@@ -18,13 +17,13 @@ from dagster import HookContext, failure_hook, success_hook
 
 @success_hook(required_resource_keys={"slack"})
 def slack_message_on_success(context: HookContext):
-    message = f"Solid {context.solid.name} finished successfully"
+    message = f"Op {context.op.name} finished successfully"
     context.resources.slack.chat.post_message(channel="#foo", text=message)
 
 
 @failure_hook(required_resource_keys={"slack"})
 def slack_message_on_failure(context: HookContext):
-    message = f"Solid {context.solid.name} failed"
+    message = f"Op {context.op.name} failed"
     context.resources.slack.chat.post_message(channel="#foo", text=message)
 
 
@@ -33,47 +32,58 @@ def slack_message_on_failure(context: HookContext):
 slack_resource_mock = mock.MagicMock()
 
 
-@solid
+@op
 def a():
     pass
 
 
-@solid
+@op
 def b():
     raise Exception()
 
 
-# start_repo_marker_3
-mode_defs = [
-    ModeDefinition(
-        "dev",
-        resource_defs={
-            "slack": ResourceDefinition.hardcoded_resource(
-                slack_resource_mock, "do not send messages in dev"
-            )
-        },
-    ),
-    ModeDefinition("prod", resource_defs={"slack": slack_resource}),
-]
-# end_repo_marker_3
-
 # start_repo_marker_1
-@slack_message_on_failure
-@pipeline(mode_defs=mode_defs)
+@job(resource_defs={"slack": slack_resource}, hooks={slack_message_on_failure})
 def notif_all():
-    # the hook "slack_message_on_failure" is applied on every solid instance within this pipeline
+    # the hook "slack_message_on_failure" is applied on every op instance within this graph
     a()
     b()
 
 
 # end_repo_marker_1
 
+
+# start_repo_marker_3
+@graph
+def slack_notif_all():
+    a()
+    b()
+
+
+notif_all_prod = slack_notif_all.to_job(
+    name="notif_all_prod",
+    resource_defs={
+        "slack": ResourceDefinition.hardcoded_resource(
+            slack_resource_mock, "do not send messages in dev"
+        )
+    },
+    hooks={slack_message_on_failure},
+)
+
+notif_all_dev = slack_notif_all.to_job(
+    name="notif_all_dev", resource_defs={"slack": slack_resource}, hooks={slack_message_on_failure}
+)
+
+
+# end_repo_marker_3
+
+
 # start_repo_marker_2
-@pipeline(mode_defs=mode_defs)
+@job(resource_defs={"slack": slack_resource})
 def selective_notif():
-    # only solid "a" triggers hooks: a slack message will be sent when it fails or succeeds
+    # only op "a" triggers hooks: a slack message will be sent when it fails or succeeds
     a.with_hooks({slack_message_on_failure, slack_message_on_success})()
-    # solid "b" won't trigger any hooks
+    # op "b" won't trigger any hooks
     b()
 
 
@@ -88,11 +98,11 @@ def repo():
 # start_repo_main
 if __name__ == "__main__":
     with open(
-        file_relative_path(__file__, "prod.yaml"),
+        file_relative_path(__file__, "prod_op_hooks.yaml"),
         "r",
     ) as fd:
         run_config = yaml.safe_load(fd.read())
-    result = execute_pipeline(notif_all, run_config=run_config, mode="prod")
+    result = notif_all_dev.execute_in_process(run_config=run_config, raise_on_error=False)
 # end_repo_main
 
 
