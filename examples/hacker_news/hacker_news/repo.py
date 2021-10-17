7c1a13435e1a3c3f1182f3201b96b091f4fe73a0
OwenKephart <owen@elementl.com>
2021-10-17 16:07:32 -0700

    add local hn crag repo (#5229)

--- examples/hacker_news/hacker_news/repo.py
+++ examples/hacker_news/hacker_news/repo.py
@@ -1,10 +1,23 @@
 from dagster import repository, schedule_from_partitions
 
 from .jobs.dbt_metrics import dbt_prod_job, dbt_staging_job
-from .jobs.hacker_news_api_download import download_prod_job, download_staging_job
+from .jobs.hacker_news_api_download import (
+    download_local_job,
+    download_prod_job,
+    download_staging_job,
+)
 from .jobs.story_recommender import story_recommender_prod_job, story_recommender_staging_job
 from .sensors.hn_tables_updated_sensor import make_hn_tables_updated_sensor
 from .sensors.slack_on_failure_sensor import make_slack_on_failure_sensor
+
+
+@repository
+def hacker_news_local():
+    return [
+        download_local_job,
+        story_recommender_staging_job,
+        dbt_staging_job,
+    ]
 
 
 @repository
