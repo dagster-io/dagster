f430e742134af00ad09f6dc7fcf9f1c9d5dae419
Sandy Ryza <sandy@elementl.com>
2021-10-17 13:39:32 -0700

    job versions of repository and workspace pages

--- examples/docs_snippets_crag/docs_snippets_crag_tests/concepts_tests/repositories_workspaces_tests/test_repository.py
+++ examples/docs_snippets_crag/docs_snippets_crag_tests/concepts_tests/repositories_workspaces_tests/test_repository.py
@@ -1,28 +1,26 @@
-from dagster import execute_pipeline
 from docs_snippets_crag.concepts.repositories_workspaces.lazy_repository_definition import (
     my_lazy_repository,
 )
 from docs_snippets_crag.concepts.repositories_workspaces.repository_definition import (
-    addition_pipeline,
+    job1,
+    job2,
     my_repository,
-    subtraction_pipeline,
 )
 
 
-def test_pipelines():
-    result = execute_pipeline(addition_pipeline)
+def test_jobs():
+    result = job1.execute_in_process()
     assert result.success
-    assert result.result_for_solid("add").output_value() == 3
 
-    result = execute_pipeline(subtraction_pipeline)
+    result = job2.execute_in_process()
     assert result.success
-    assert result.result_for_solid("subtract").output_value() == -1
 
 
 def test_my_repository():
     assert my_repository
-    assert len(my_repository.get_all_pipelines()) == 2
+    assert len(my_repository.get_all_pipelines()) == 3
     assert len(my_repository.schedule_defs) == 1
+    assert len(my_repository.sensor_defs) == 1
 
 
 def test_my_lazy_repository():
