dfe03ac26671e3b8dfb6c5894c4b3c80d1bbaf45
prha <1040172+prha@users.noreply.github.com>
2021-10-19 16:44:45 -0700

    change dagster-ge to use ops/job, update examples (#5183)
    * change dagster-ge to use ops/job, update examples
    
    * fix import
    
    * remove preset ref

--- examples/ge_example/ge_example_tests/test_ge_example.py
+++ examples/ge_example/ge_example_tests/test_ge_example.py
@@ -1,12 +1,34 @@
-from dagster import execute_pipeline
-from ge_example.ge_demo import payroll_data_pipeline
+import pytest
+from dagster.utils import file_relative_path
+from ge_example.ge_demo import payroll_data
 
 
 def test_pipeline_success():
-    res = execute_pipeline(payroll_data_pipeline, preset="sample_preset_success")
+    res = payroll_data.execute_in_process()
     assert res.success
 
 
 def test_pipeline_failure():
-    res = execute_pipeline(payroll_data_pipeline, preset="sample_preset_fail", raise_on_error=False)
-    assert not res.success
+    with pytest.raises(ValueError):
+        payroll_data.execute_in_process(
+            run_config={
+                "resources": {
+                    "ge_data_context": {
+                        "config": {
+                            "ge_root_dir": file_relative_path(
+                                __file__, "../ge_example/great_expectations"
+                            )
+                        }
+                    }
+                },
+                "solids": {
+                    "read_in_datafile": {
+                        "inputs": {
+                            "csv_path": {
+                                "value": file_relative_path(__file__, "../ge_example/data/fail.csv")
+                            }
+                        }
+                    }
+                },
+            }
+        )
