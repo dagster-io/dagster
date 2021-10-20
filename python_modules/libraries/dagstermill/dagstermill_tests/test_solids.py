fa02785d37dd09fa2a996a156f69c6c87e350673
yuhan <yuhan@elementl.com>
2021-10-20 15:14:19 -0700

    up

--- python_modules/libraries/dagstermill/dagstermill_tests/test_solids.py
+++ python_modules/libraries/dagstermill/dagstermill_tests/test_solids.py
@@ -210,44 +210,6 @@ def test_double_add_pipeline():
 
 
 @pytest.mark.notebook_test
-def test_fan_in_notebook_pipeline_legacy():
-    with exec_for_test(
-        "fan_in_notebook_pipeline_legacy",
-        {
-            "execution": {"multiprocess": {}},
-            "solids": {
-                "solid_1": {"inputs": {"obj": "hello"}},
-                "solid_2": {"inputs": {"obj": "world"}},
-            },
-        },
-    ) as result:
-        assert result.success
-        assert result.result_for_solid("solid_1").output_value() == "hello"
-        assert result.result_for_solid("solid_2").output_value() == "world"
-        assert result.result_for_solid("fan_in_legacy").output_value() == "hello world"
-
-
-@pytest.mark.notebook_test
-def test_composite_pipeline_legacy():
-    with exec_for_test(
-        "composite_pipeline_legacy",
-        {
-            "execution": {"multiprocess": {}},
-            "solids": {
-                "outer_legacy": {"solids": {"yield_something_legacy": {"inputs": {"obj": "hello"}}}}
-            },
-        },
-    ) as result:
-        assert result.success
-        assert (
-            result.result_for_solid("outer_legacy")
-            .result_for_solid("yield_something_legacy")
-            .output_value()
-            == "hello"
-        )
-
-
-@pytest.mark.notebook_test
 def test_fan_in_notebook_pipeline():
     with exec_for_test(
         "fan_in_notebook_pipeline",
