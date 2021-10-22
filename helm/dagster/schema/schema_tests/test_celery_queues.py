ac8db94aa61acb486815df8d7b83d429122c0396
gibsondan <daniel@elementl.com>
2021-10-22 11:45:02 -0500

    Why is this helm test failing??

--- helm/dagster/schema/schema_tests/test_celery_queues.py
+++ helm/dagster/schema/schema_tests/test_celery_queues.py
@@ -2,6 +2,7 @@ import pytest
 from kubernetes.client import models
 from schema.charts.dagster.subschema.run_launcher import (
     CeleryK8sRunLauncherConfig,
+    CeleryWorkerQueue,
     RunLauncher,
     RunLauncherConfig,
     RunLauncherType,
@@ -18,6 +19,16 @@ def deployment_helm_template() -> HelmTemplate:
         subchart_paths=["charts/dagster-user-deployments"],
         output="templates/deployment-celery-queues.yaml",
         model=models.V1Deployment,
+    )
+
+
+@pytest.fixture(name="celery_queue_configmap_template")
+def celery_queue_configmap_helm_template() -> HelmTemplate:
+    return HelmTemplate(
+        helm_dir_path="helm/dagster",
+        subchart_paths=["charts/dagster-user-deployments"],
+        output="templates/configmap-celery.yaml",
+        model=models.V1ConfigMap,
     )
 
 
@@ -62,3 +73,48 @@ def test_celery_queue_image(deployment_template: HelmTemplate):
 
     assert image_name == repository
     assert image_tag == tag
+
+
+def test_celery_queue_inherit_config_source(
+    deployment_template: HelmTemplate, celery_queue_configmap_template: HelmTemplate
+):
+
+    configSource = {
+        "broker_transport_options": {"priority_steps": [9]},
+        "worker_concurrency": 1,
+    }
+
+    workerQueues = [
+        {"name": "extra-queue-1", "replicaCount": 1},
+        {"name": "dagster", "replicaCount": 2},
+    ]
+
+    helm_values = DagsterHelmValues.construct(
+        runLauncher=RunLauncher.construct(
+            type=RunLauncherType.CELERY,
+            config=RunLauncherConfig.construct(
+                celeryK8sRunLauncher=CeleryK8sRunLauncherConfig.construct(
+                    configSource=configSource,
+                    workerQueues=[
+                        CeleryWorkerQueue.construct(**workerQueue) for workerQueue in workerQueues
+                    ],
+                )
+            ),
+        )
+    )
+
+    print("HI")
+
+    print(str(helm_values))
+
+    celery_queue_deployments = deployment_template.render(helm_values)
+
+    print("DEPLOYMENTS:\n\n\n\n\n")
+    print(str(celery_queue_deployments))
+
+    assert len(celery_queue_deployments) == 2
+
+    celery_queue_configmaps = celery_queue_configmap_template.render(helm_values)
+
+    print("CONFIG MAPS: \n\n\n\n\n\n\n")
+    print(str(celery_queue_configmaps))
