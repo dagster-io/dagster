785eede7b053fd97847e0f812e4cfccc2886ad89
Josh Braun <josh.braun.4@gmail.com>
2021-10-27 17:38:49 -0400

    style bug fixes

--- docs/next/layouts/MainLayout.tsx
+++ docs/next/layouts/MainLayout.tsx
@@ -418,7 +418,7 @@ const Header = ({ openFeedback, isDarkMode, setDarkMode }) => {
             onClick={() => setDarkMode(!isDarkMode)}
             type="button"
             className={cx(
-              "hidden relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500",
+              "hidden relative flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500",
               {
                 "bg-gray-800": isDarkMode,
                 "bg-gray-200": !isDarkMode,
