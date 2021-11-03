ff8513cbb55343543daccb46c6e70bb67cc373b0
Mark Kudryk <mark.kudryk@drillinginfo.com>
2021-11-03 14:57:40 -0600

    Specifying subpackages for AWS clusters.

--- python_modules/libraries/dagster-dask/dagster_dask/resources.py
+++ python_modules/libraries/dagster-dask/dagster_dask/resources.py
@@ -15,8 +15,8 @@ DaskClusterTypes = {
         "oar": ("OAR", "dask_jobqueue", "OARCluster"),
         "kube": ("Kubernetes", "dask_kubernetes", "KubeCluster"),
         "azureml": ("Azure ML", "dask_cloudprovider", "AzureMLCluster"),
-        "ecs": ("AWS ECS", "dask_cloudprovider", "ECSCluster"),
-        "fargate": ("AWS ECS on Fargate", "dask_cloudprovider", "FargateCluster"),
+        "ecs": ("AWS ECS", "dask_cloudprovider.aws", "ECSCluster"),
+        "fargate": ("AWS ECS on Fargate", "dask_cloudprovider.aws", "FargateCluster"),
     }.items()
 }
 
