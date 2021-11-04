3d730495f430621712bc8e2c2fc5d86254ec2d4d
gibsondan <daniel@elementl.com>
2021-11-03 19:23:16 -0500

    Some small make dev_install improvements
    Summary:
    Two small changes here to the make dev_install script:
    - At one point we were installing our modules twice, once without deps, and then once with. But then the --no-deps arg was removed, so now we're just running the exact same command twice for no good reason :)
    - Remove tensorflow, which isn't being used and contributes literal GB to the make dev_install build

--- examples/airline_demo/setup.py
+++ examples/airline_demo/setup.py
@@ -43,7 +43,6 @@ setup(
             "seaborn",
             "sqlalchemy-redshift>=0.7.2",
             "SQLAlchemy-Utils==0.33.8",
-            'tensorflow; python_version < "3.9"',
         ],
         "airflow": [
             "dagster_airflow",
