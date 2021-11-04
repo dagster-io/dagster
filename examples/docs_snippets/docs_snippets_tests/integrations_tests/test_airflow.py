69a98202291f136d7eaad3f0c6ddfc2c4171dfc4
yuhan <yuhan@elementl.com>
2021-11-03 18:40:30 -0700

    disbale tests for now

--- examples/docs_snippets/docs_snippets_tests/integrations_tests/test_airflow.py
+++ examples/docs_snippets/docs_snippets_tests/integrations_tests/test_airflow.py
@@ -1,5 +1,3 @@
-from dagster_airflow.operators.docker_operator import DagsterDockerOperator
-from dagster_airflow.operators.python_operator import DagsterPythonOperator
 from docs_snippets.integrations.airflow.hello_cereal import hello_cereal_job
 
 
@@ -7,19 +5,22 @@ def test_hello_cereal():
     assert hello_cereal_job.execute_in_process().success
 
 
-def test_hello_cereal_dag():
-    from docs_snippets.integrations.airflow.hello_cereal_dag import dag, tasks
+# https://github.com/dagster-io/dagster/issues/5534
+# def test_hello_cereal_dag():
+#     from docs_snippets.integrations.airflow.hello_cereal_dag import dag, tasks
+#     from dagster_airflow.operators.python_operator import DagsterPythonOperator
 
-    assert dag.dag_id == "hello_cereal_job"
+#     assert dag.dag_id == "hello_cereal_job"
 
-    for task in tasks:
-        assert isinstance(task, DagsterPythonOperator)
+#     for task in tasks:
+#         assert isinstance(task, DagsterPythonOperator)
 
 
-def test_containerized():
-    from docs_snippets.integrations.airflow.containerized import dag, steps
+# def test_containerized():
+#     from docs_snippets.integrations.airflow.containerized import dag, steps
+#     from dagster_airflow.operators.docker_operator import DagsterDockerOperator
 
-    assert dag.dag_id == "hello_cereal_job"
+#     assert dag.dag_id == "hello_cereal_job"
 
-    for step in steps:
-        assert isinstance(step, DagsterDockerOperator)
+#     for step in steps:
+#         assert isinstance(step, DagsterDockerOperator)
