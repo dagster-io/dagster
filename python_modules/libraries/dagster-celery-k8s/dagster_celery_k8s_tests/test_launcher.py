5188d2c2cbaa4976c580e5da3e2237da003ba1b2
Johann Miller <johann@elementl.com>
2021-11-08 07:54:56 -0500

    Support job run config in CeleryK8s (#5552)

--- python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s_tests/test_launcher.py
+++ python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s_tests/test_launcher.py
@@ -3,6 +3,7 @@ from unittest import mock
 
 import pytest
 from dagster import pipeline, reconstructable
+from dagster.check import CheckError
 from dagster.core.errors import DagsterInvariantViolationError
 from dagster.core.host_representation import RepositoryHandle
 from dagster.core.launcher import LaunchRunContext
@@ -14,7 +15,7 @@ from dagster.core.test_utils import (
     instance_for_test,
 )
 from dagster.utils.hosted_user_process import external_pipeline_from_recon_pipeline
-from dagster_celery_k8s.config import get_celery_engine_config
+from dagster_celery_k8s.config import get_celery_engine_config, get_celery_engine_job_config
 from dagster_celery_k8s.executor import CELERY_K8S_CONFIG_KEY
 from dagster_celery_k8s.launcher import (
     CeleryK8sRunLauncher,
@@ -60,10 +61,127 @@ def test_get_validated_celery_k8s_executor_config():
     }
 
     with pytest.raises(
-        DagsterInvariantViolationError,
-        match="celery-k8s execution configuration must be present in the run config to use the CeleryK8sRunLauncher.",
+        CheckError,
+        match="Incorrect execution schema provided",
     ):
-        _get_validated_celery_k8s_executor_config({})
+        _get_validated_celery_k8s_executor_config(
+            {"execution": {"celery-k8s": {"config": {"foo": "bar"}}}}
+        )
+
+    with environ(
+        {
+            "DAGSTER_K8S_PIPELINE_RUN_IMAGE": "foo",
+            "DAGSTER_K8S_PIPELINE_RUN_NAMESPACE": "default",
+            "DAGSTER_K8S_PIPELINE_RUN_IMAGE_PULL_POLICY": "Always",
+            "DAGSTER_K8S_PIPELINE_RUN_ENV_CONFIGMAP": "config-pipeline-env",
+        }
+    ):
+        cfg = get_celery_engine_job_config()
+        res = _get_validated_celery_k8s_executor_config(cfg)
+        assert res == {
+            "backend": "rpc://",
+            "retries": {"enabled": {}},
+            "job_image": "foo",
+            "image_pull_policy": "Always",
+            "env_config_maps": ["config-pipeline-env"],
+            "load_incluster_config": True,
+            "job_namespace": "default",
+            "repo_location_name": "<<in_process>>",
+            "job_wait_timeout": DEFAULT_WAIT_TIMEOUT,
+            "volume_mounts": [],
+            "volumes": [],
+        }
+
+    # Test setting all possible config fields
+    with environ(
+        {
+            "TEST_PIPELINE_RUN_NAMESPACE": "default",
+            "TEST_CELERY_BROKER": "redis://some-redis-host:6379/0",
+            "TEST_CELERY_BACKEND": "redis://some-redis-host:6379/0",
+            "TEST_PIPELINE_RUN_IMAGE": "foo",
+            "TEST_PIPELINE_RUN_IMAGE_PULL_POLICY": "Always",
+            "TEST_K8S_PULL_SECRET_1": "super-secret-1",
+            "TEST_K8S_PULL_SECRET_2": "super-secret-2",
+            "TEST_SERVICE_ACCOUNT_NAME": "my-cool-service-acccount",
+            "TEST_PIPELINE_RUN_ENV_CONFIGMAP": "config-pipeline-env",
+            "TEST_SECRET": "config-secret-env",
+        }
+    ):
+
+        cfg = {
+            "execution": {
+                "config": {
+                    "repo_location_name": "<<in_process>>",
+                    "load_incluster_config": False,
+                    "kubeconfig_file": "/some/kubeconfig/file",
+                    "job_namespace": {"env": "TEST_PIPELINE_RUN_NAMESPACE"},
+                    "broker": {"env": "TEST_CELERY_BROKER"},
+                    "backend": {"env": "TEST_CELERY_BACKEND"},
+                    "include": ["dagster", "dagit"],
+                    "config_source": {
+                        "task_annotations": """{'*': {'on_failure': my_on_failure}}"""
+                    },
+                    "retries": {"disabled": {}},
+                    "job_image": {"env": "TEST_PIPELINE_RUN_IMAGE"},
+                    "image_pull_policy": {"env": "TEST_PIPELINE_RUN_IMAGE_PULL_POLICY"},
+                    "image_pull_secrets": [
+                        {"name": {"env": "TEST_K8S_PULL_SECRET_1"}},
+                        {"name": {"env": "TEST_K8S_PULL_SECRET_2"}},
+                    ],
+                    "service_account_name": {"env": "TEST_SERVICE_ACCOUNT_NAME"},
+                    "env_config_maps": [{"env": "TEST_PIPELINE_RUN_ENV_CONFIGMAP"}],
+                    "env_secrets": [{"env": "TEST_SECRET"}],
+                    "job_wait_timeout": DEFAULT_WAIT_TIMEOUT,
+                    "volume_mounts": [],
+                    "volumes": [],
+                }
+            }
+        }
+
+        res = _get_validated_celery_k8s_executor_config(cfg)
+        assert res == {
+            "repo_location_name": "<<in_process>>",
+            "load_incluster_config": False,
+            "kubeconfig_file": "/some/kubeconfig/file",
+            "job_namespace": "default",
+            "backend": "redis://some-redis-host:6379/0",
+            "broker": "redis://some-redis-host:6379/0",
+            "include": ["dagster", "dagit"],
+            "config_source": {"task_annotations": """{'*': {'on_failure': my_on_failure}}"""},
+            "retries": {"disabled": {}},
+            "job_image": "foo",
+            "image_pull_policy": "Always",
+            "image_pull_secrets": [{"name": "super-secret-1"}, {"name": "super-secret-2"}],
+            "service_account_name": "my-cool-service-acccount",
+            "env_config_maps": ["config-pipeline-env"],
+            "env_secrets": ["config-secret-env"],
+            "job_wait_timeout": DEFAULT_WAIT_TIMEOUT,
+            "volume_mounts": [],
+            "volumes": [],
+        }
+
+
+def test_get_validated_celery_k8s_executor_config_for_job():
+    res = _get_validated_celery_k8s_executor_config({"execution": {"config": {"job_image": "foo"}}})
+
+    assert res == {
+        "backend": "rpc://",
+        "retries": {"enabled": {}},
+        "job_image": "foo",
+        "image_pull_policy": "IfNotPresent",
+        "load_incluster_config": True,
+        "job_namespace": "default",
+        "repo_location_name": "<<in_process>>",
+        "job_wait_timeout": DEFAULT_WAIT_TIMEOUT,
+        "volume_mounts": [],
+        "volumes": [],
+    }
+
+    with pytest.raises(
+        CheckError,
+        match="Incorrect execution schema provided",
+    ):
+        _get_validated_celery_k8s_executor_config({"execution": {"config": {"foo": "bar"}}})
 
     with environ(
         {
