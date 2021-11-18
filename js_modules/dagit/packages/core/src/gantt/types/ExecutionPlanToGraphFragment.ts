0de914b5477c7044f6fd6af6772ceeba0be88ce3
Ben Gotow <ben@foundry376.com>
2021-11-18 14:21:56 -0600

    [dagit] Cut unused data from execution plan query to speed up Run details

--- js_modules/dagit/packages/core/src/gantt/types/ExecutionPlanToGraphFragment.ts
+++ js_modules/dagit/packages/core/src/gantt/types/ExecutionPlanToGraphFragment.ts
@@ -9,22 +9,10 @@ import { StepKind } from "./../../types/globalTypes";
 // GraphQL fragment: ExecutionPlanToGraphFragment
 // ====================================================
 
-export interface ExecutionPlanToGraphFragment_steps_inputs_dependsOn_outputs_type {
-  __typename: "RegularDagsterType" | "ListDagsterType" | "NullableDagsterType";
-  name: string | null;
-}
-
-export interface ExecutionPlanToGraphFragment_steps_inputs_dependsOn_outputs {
-  __typename: "ExecutionStepOutput";
-  name: string;
-  type: ExecutionPlanToGraphFragment_steps_inputs_dependsOn_outputs_type;
-}
-
 export interface ExecutionPlanToGraphFragment_steps_inputs_dependsOn {
   __typename: "ExecutionStep";
   key: string;
   kind: StepKind;
-  outputs: ExecutionPlanToGraphFragment_steps_inputs_dependsOn_outputs[];
 }
 
 export interface ExecutionPlanToGraphFragment_steps_inputs {
@@ -42,5 +30,4 @@ export interface ExecutionPlanToGraphFragment_steps {
 export interface ExecutionPlanToGraphFragment {
   __typename: "ExecutionPlan";
   steps: ExecutionPlanToGraphFragment_steps[];
-  artifactsPersisted: boolean;
 }
