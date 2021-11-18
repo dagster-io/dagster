ce06948cdbe48e920a915f78a1f10d60daba3111
yuhan <yuhan@elementl.com>
2021-11-18 13:38:11 -0800

    examples: df_to_csv_io_manager + io manager unit test

--- examples/bite_sized/tests/test_conditional_branching.py
+++ examples/bite_sized/tests/test_conditional_branching.py
@@ -1,6 +1,9 @@
+import os
 import tempfile
 
-from bite_sized.conditional_branching import conditional_branching
+import pandas as pd
+from bite_sized.conditional_branching import conditional_branching, df_to_csv_io_manager
+from dagster import build_init_resource_context, build_input_context, build_output_context
 from dagster.core.test_utils import instance_for_test
 
 
@@ -17,3 +20,21 @@ def test_conditional_branching():
                     "resources": {"slack": {"config": {"token": "nonce"}}},
                 },
             ).success
+
+
+def test_df_to_csv_io_manager():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        my_io_manager = df_to_csv_io_manager(
+            build_init_resource_context(config={"base_dir": temp_dir})
+        )
+        test_df = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]})
+        # test handle_output
+        output_context = build_output_context(name="abc", step_key="123")
+        my_io_manager.handle_output(output_context, test_df)
+        output_path = my_io_manager._get_path(output_context)  # pylint:disable=protected-access
+        assert os.path.exists(output_path)
+        assert test_df.equals(pd.read_csv(output_path))
+
+        # test load_input
+        input_context = build_input_context(upstream_output=output_context)
+        assert test_df.equals(my_io_manager.load_input(input_context))
