7623870551d9e5db700cc8ee5637b520659c6b62
Owen Kephart <owen@elementl.com>
2021-11-22 13:56:03 -0800

    removed some jankiness

--- docs/sphinx/_ext/autodoc_configurable.py
+++ docs/sphinx/_ext/autodoc_configurable.py
@@ -1,22 +1,28 @@
 import json
 import textwrap
-from typing import Any, Optional
+from typing import Any, List
+
+from dagster import BoolSource, IntSource, StringSource
+from dagster.config.config_type import ConfigType, ConfigTypeKind
 from dagster.core.definitions.configurable import ConfigurableDefinition
-from dagster.config.config_type import ConfigTypeKind
-from dagster import StringSource, IntSource, BoolSource
 from sphinx.ext.autodoc import DataDocumenter
 
 
-def type_repr(config_type):
+def type_repr(config_type: ConfigType) -> str:
+    """Generate a human-readable name for a given dagster ConfigType"""
+
+    # Use given name if possible
     if config_type.given_name:
         return config_type.given_name
+
+    # The xSource types are a particular kind of selector that is very common, special case these
     if config_type == StringSource:
         return "dagster.StringSource"
-    if config_type == BoolSource:
+    elif config_type == BoolSource:
         return "dagster.BoolSource"
-    if config_type == IntSource:
+    elif config_type == IntSource:
         return "dagster.IntSource"
-    if config_type.kind == ConfigTypeKind.ANY:
+    elif config_type.kind == ConfigTypeKind.ANY:
         return "Any"
     elif config_type.kind == ConfigTypeKind.SCALAR:
         return config_type.scalar_kind.name.lower()
@@ -24,7 +30,6 @@ def type_repr(config_type):
         return "Enum{" + ", ".join(str(val) for val in config_type.config_values) + "}"
     elif config_type.kind == ConfigTypeKind.ARRAY:
         return "List[{}]".format(type_repr(config_type.inner_type))
-    # TODO not sure what to do for this one
     elif config_type.kind == ConfigTypeKind.SELECTOR:
         return "selector"
     elif config_type.kind == ConfigTypeKind.STRICT_SHAPE:
@@ -39,21 +44,27 @@ def type_repr(config_type):
         return f"Union[{type_repr(config_type.inner_type)}, None]"
 
 
-def config_field_to_lines(field, name=None):
+def config_field_to_lines(field, name=None) -> List[str]:
+    """Given a config field, turn it into a list of lines to add to the documentation"""
     lines = [""]
 
+    # The only unnamed field will be the top level config schema wrapper
     if name:
         type_str = type_repr(field.config_type)
         if not field.is_required:
             type_str += ", optional"
         lines.append(f":{name} ({type_str}):")
         if field.description:
+            # trim / normalize whitespace. some of our config descriptions misuse triple-quote blocks
+            # so this makes them look nicer
             for l in field.description.split("\n"):
+                # escape '*' characters because they get interpreted as emphasis markers in rst
                 lines.append(" " * 4 + textwrap.dedent(l.replace("*", "\\*")))
             lines.append("")
 
         if field.default_provided:
             val = field.default_value
+            # for fields with dictionary default vals, hide them in collapsible block
             if isinstance(val, dict):
                 ls = json.dumps(val, indent=4).split("\n")
                 lines.append("")
@@ -67,13 +78,17 @@ def config_field_to_lines(field, name=None):
                 lines.append("")
                 lines.append(f"    **Default Value:** {repr(val)}")
 
+    # if field has subfields, recurse
     if hasattr(field.config_type, "fields") and len(field.config_type.fields) > 0:
         lines.append("")
-        lines.append("    .. collapse:: Config Schema:")
+        # for the top level, no need to indent
+        indent = "    " if name else ""
+        lines.append(indent + ".. collapse:: Config Schema:")
         for name, subfield in field.config_type.fields.items():
-            lines += ["    " + line for line in config_field_to_lines(subfield, name=name)]
+            # indent all of these lines to fit under the collapse block
+            lines += [indent + "    " + line for line in config_field_to_lines(subfield, name=name)]
 
-    return ["    " + line for line in lines]
+    return lines
 
 
 class ConfigurableDocumenter(DataDocumenter):
@@ -87,15 +102,15 @@ class ConfigurableDocumenter(DataDocumenter):
     def add_content(self, more_content, no_docstring: bool = False) -> None:
         source_name = self.get_sourcename()
         self.add_line("", source_name)
+        # explicit visual linebreak
+        self.add_line("|", source_name)
+        self.add_line("", source_name)
 
-        lines = config_field_to_lines(self.object.config_schema.as_field())
-        lines = textwrap.dedent("\n".join(lines)).split("\n")
-
-        for line in lines:
-            self.add_line(line.replace('"', ""), source_name)
+        for line in config_field_to_lines(self.object.config_schema.as_field()):
+            self.add_line(line, source_name)
 
         self.add_line("", source_name)
-        self.add_line("", source_name)
+        # do this call at the bottom so that config schema is first thing in documentation
         super().add_content(more_content, no_docstring)
 
 
