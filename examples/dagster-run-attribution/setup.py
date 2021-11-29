b909ecf1ab0e517c20e85d3e4c50c5074ac1bca3
yuhan <yuhan@elementl.com>
2021-11-28 21:02:07 -0800

    examples: make each example folder standalone - clone, run, and deploy 1/

--- examples/dagster-run-attribution/setup.py
+++ examples/dagster-run-attribution/setup.py
@@ -9,7 +9,7 @@ setup(
     author="Elementl",
     license="Apache-2.0",
     description="Dagster example for performing run attribution by reading from HTTP headers in the RunCoordinator",
-    url="https://github.com/dagster-io/dagster/tree/master/examples/run_attribution_example",
+    url="https://github.com/dagster-io/dagster/tree/master/examples/dagster-run-attribution",
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
---
+++ examples/with-pyspark/README.md
@@ -0,0 +1,3 @@
+# Example with Pyspark
+
+View this example in the Dagster docs at https://docs.dagster.io/integrations/spark.
