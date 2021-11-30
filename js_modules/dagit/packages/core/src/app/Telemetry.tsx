7cf892b41b8226ae3f8e3925f72870a9ef87efb2
Chris DeCarolis <cdguitar817@gmail.com>
2021-11-29 17:11:48 -0800

    Use bare fetch for telemetry

--- js_modules/dagit/packages/core/src/app/Telemetry.tsx
+++ js_modules/dagit/packages/core/src/app/Telemetry.tsx
@@ -1,4 +1,6 @@
-import {gql, useMutation, useQuery} from '@apollo/client';
+import {gql} from '@apollo/client';
+import {extractPathPrefix} from '@dagit/app/src/extractPathPrefix';
+import {print} from 'graphql';
 
 import {DagitTelemetryEnabledQuery} from './types/DagitTelemetryEnabledQuery';
 
@@ -14,6 +16,8 @@ const TELEMETRY_ENABLED_QUERY = gql`
   }
 `;
 
+const GRAPHQL_PATH = `${extractPathPrefix() || ''}/graphql`;
+
 const LOG_TELEMETRY_MUTATION = gql`
   mutation LogTelemetryMutation($action: String!, $metadata: String!, $clientTime: String!) {
     logTelemetry(action: $action, metadata: $metadata, clientTime: $clientTime) {
@@ -28,19 +32,43 @@ const LOG_TELEMETRY_MUTATION = gql`
   }
 `;
 
-export const useTelemetryAction = (action: TelemetryAction) => {
-  const [telemetryRequest] = useMutation(LOG_TELEMETRY_MUTATION);
-  const {data} = useQuery<DagitTelemetryEnabledQuery>(TELEMETRY_ENABLED_QUERY);
+let __DAGIT_TELEMETRY_FLAG: boolean | undefined = undefined;
 
-  return (metadata: {[key: string]: string | null | undefined} = {}) => {
-    if (data && data.instance.dagitTelemetryEnabled) {
-      telemetryRequest({
-        variables: {
-          action: action.toString(),
-          metadata: JSON.stringify(metadata),
-          clientTime: Date.now(),
-        },
-      });
-    }
-  };
-};
+export async function logTelemetry(
+  action: TelemetryAction,
+  metadata: {[key: string]: string | null | undefined} = {},
+) {
+  if (__DAGIT_TELEMETRY_FLAG === undefined) {
+    const enabledResponse = await fetch(GRAPHQL_PATH, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        query: print(TELEMETRY_ENABLED_QUERY),
+      }),
+    });
+    const payload = (await enabledResponse.json()) as {data: DagitTelemetryEnabledQuery};
+    const data = payload.data;
+    __DAGIT_TELEMETRY_FLAG = data.instance.dagitTelemetryEnabled;
+  }
+  if (!__DAGIT_TELEMETRY_FLAG) {
+    return;
+  }
+  return fetch(GRAPHQL_PATH, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify({
+      query: print(LOG_TELEMETRY_MUTATION),
+      variables: {
+        action,
+        metadata: JSON.stringify(metadata),
+        clientTime: Date.now(),
+      },
+    }),
+  });
+}
