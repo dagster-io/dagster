798fdfd057feb08e2b9c0e2e280d24462057c150
pcherednichenko <paulcheredn@gmail.com>
2021-11-30 13:09:29 +0100

    backwards compatible localion_name

--- helm/dagster/schema/schema_tests/test_workspace.py
+++ helm/dagster/schema/schema_tests/test_workspace.py
@@ -122,6 +122,42 @@ def test_workspace_renders_from_helm_dagit(template: HelmTemplate):
         assert grpc_server["grpc_server"]["location_name"] == server.name
 
 
+def test_workspace_server_location_name_renders_from_helm_dagit(template: HelmTemplate):
+    servers = [
+        Server(host="another-deployment-one", port=4000),
+        Server(host="another-deployment-two", port=4001, name="deployment two"),
+    ]
+    helm_values = DagsterHelmValues.construct(
+        dagit=Dagit.construct(workspace=Workspace(enabled=True, servers=servers)),
+        dagsterUserDeployments=UserDeployments(
+            enabled=True,
+            enableSubchart=True,
+            deployments=[
+                create_simple_user_deployment("deployment-one"),
+                create_simple_user_deployment("deployment-two"),
+            ],
+        ),
+    )
+
+    workspace_templates = template.render(helm_values)
+
+    assert len(workspace_templates) == 1
+
+    workspace_template = workspace_templates[0]
+
+    workspace = yaml.full_load(workspace_template.data["workspace.yaml"])
+    grpc_servers = workspace["load_from"]
+
+    assert len(grpc_servers) == len(servers)
+
+    for grpc_server, server in zip(grpc_servers, servers):
+        assert grpc_servers["grpc_server"]["host"] == server.host
+        assert grpc_server["grpc_server"]["port"] == server.port
+
+    assert grpc_servers[0]["grpc_server"]["location_name"] == servers[0].host
+    assert grpc_servers[1]["grpc_server"]["location_name"] == servers[1].name
+
+
 def test_workspace_renders_empty(template: HelmTemplate):
     servers: List[Server] = []
     helm_values = DagsterHelmValues.construct(
