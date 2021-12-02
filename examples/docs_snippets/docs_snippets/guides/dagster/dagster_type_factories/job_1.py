f6d61102fbf06834e02d1fad458ddb1270047a33
Sean Mackesey <s.mackesey@gmail.com>
2021-12-02 08:42:58 -0800

    Add Dagster Type factory pandas validation guide (#5631)
    * Adds a guide illustrating Dagster Type factories for data validation

---
+++ examples/docs_snippets/docs_snippets/guides/dagster/dagster_type_factories/job_1.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from dagster import AssetMaterialization, job, op
+
+
+@op
+def load_trips():
+    return pd.read_csv(
+        "./ebike_trips.csv",
+        parse_dates=["start_time", "end_time"],
+    )
+
+
+@op
+def generate_plot(trips):
+    minute_lengths = [x.total_seconds() / 60 for x in trips.end_time - trips.start_time]
+    bin_edges = np.histogram_bin_edges(minute_lengths, 15)
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.set(title="Trip lengths", xlabel="Minutes", ylabel="Count")
+    ax.hist(minute_lengths, bins=bin_edges)
+    fig.savefig("trip_lengths.png")
+    yield AssetMaterialization(
+        asset_key="trip_dist_plot", description="Distribution of trip lengths."
+    )
+    yield Output(None)
+
+
+@job
+def generate_trip_distribution_plot():
+    generate_plot(load_trips())
---
+++ examples/docs_snippets/docs_snippets_tests/guides_tests/dagster_type_factories_tests/test_pandas_dagster_types_examples.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+
+import docs_snippets.guides.dagster.dagster_type_factories as example_root
+import pytest
+from dagster import check_dagster_type
+from dagster.core.errors import DagsterTypeCheckDidNotPass
+from docs_snippets.guides.dagster.dagster_type_factories.job_1 import (
+    generate_trip_distribution_plot as job_1,
+)
+from docs_snippets.guides.dagster.dagster_type_factories.job_2 import (
+    generate_trip_distribution_plot as job_2,
+)
+from docs_snippets.guides.dagster.dagster_type_factories.simple_example import (
+    set_containing_1,
+    set_has_element_type_factory,
+)
+
+EBIKE_TRIPS_PATH = os.path.join(example_root.__path__[0], "ebike_trips.csv")
+
+
+def test_simple_example_one_off():
+    assert check_dagster_type(set_containing_1, {1, 2}).success
+
+
+def test_simple_example_factory():
+    set_containing_2 = set_has_element_type_factory(2)
+    assert check_dagster_type(set_containing_2, {1, 2}).success
+
+
+@pytest.fixture(scope="function")
+def in_tmpdir(monkeypatch, tmp_path_factory):
+    path = tmp_path_factory.mktemp("ebike_trips")
+    shutil.copy(EBIKE_TRIPS_PATH, path)
+    monkeypatch.chdir(path)
+
+
+@pytest.mark.usefixtures("in_tmpdir")
+def test_job_1_fails():
+    with pytest.raises(ValueError):
+        job_1.execute_in_process()
+
+
+@pytest.mark.usefixtures("in_tmpdir")
+def test_job_2_no_clean_fails():
+    with pytest.raises(DagsterTypeCheckDidNotPass):
+        job_2.execute_in_process()
+
+
+@pytest.mark.usefixtures("in_tmpdir")
+def test_job_2_no_clean_succeeds():
+    assert job_2.execute_in_process(
+        run_config={"ops": {"load_trips": {"config": {"clean": True}}}}
+    ).success
+    assert os.path.exists("./trip_lengths.png")
