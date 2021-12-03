cd5aeb247ab0c0dcf199aaf3825fe1d0348a466b
Isaac Hellendag <dish@elementl.com>
2021-12-03 09:47:39 -0600

    [dagit] Query for config tab availability

---
+++ js_modules/dagit/packages/core/src/instance/types/InstanceConfigHasInfo.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: InstanceConfigHasInfo
+// ====================================================
+
+export interface InstanceConfigHasInfo_instance {
+  __typename: "Instance";
+  hasInfo: boolean;
+}
+
+export interface InstanceConfigHasInfo {
+  instance: InstanceConfigHasInfo_instance;
+}
