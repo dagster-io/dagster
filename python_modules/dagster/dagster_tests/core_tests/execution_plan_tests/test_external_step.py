bd3380245797875d8315066b0665db4a7290d601
OwenKephart <owen@elementl.com>
2021-12-09 16:40:05 -0500

    update external step launchers to work with dynamic execution (#5909)

--- python_modules/dagster/dagster_tests/core_tests/execution_plan_tests/test_external_step.py
+++ python_modules/dagster/dagster_tests/core_tests/execution_plan_tests/test_external_step.py
@@ -6,6 +6,8 @@ from threading import Thread
 
 import pytest
 from dagster import (
+    DynamicOut,
+    DynamicOutput,
     Field,
     ModeDefinition,
     RetryRequested,
@@ -13,6 +15,8 @@ from dagster import (
     execute_pipeline,
     execute_pipeline_iterator,
     fs_io_manager,
+    job,
+    op,
     pipeline,
     reconstructable,
     resource,
@@ -31,6 +35,7 @@ from dagster.core.execution.plan.external_step import (
 from dagster.core.execution.retries import RetryMode
 from dagster.core.instance import DagsterInstance
 from dagster.core.storage.pipeline_run import PipelineRun
+from dagster.core.test_utils import instance_for_test
 from dagster.utils import safe_tempfile_path, send_interrupt
 from dagster.utils.merger import deep_merge_dicts, merge_dicts
 
@@ -69,6 +74,36 @@ class RequestRetryLocalExternalStepLauncher(LocalExternalStepLauncher):
 @resource(config_schema=local_external_step_launcher.config_schema)
 def request_retry_local_external_step_launcher(context):
     return RequestRetryLocalExternalStepLauncher(**context.resource_config)
+
+
+def define_dynamic_job():
+    from typing import List
+
+    @op(required_resource_keys={"first_step_launcher"}, out=DynamicOut(int))
+    def dynamic_outs():
+        for i in range(0, 3):
+            yield DynamicOutput(value=i, mapping_key=f"num_{i}")
+
+    @op(required_resource_keys={"second_step_launcher"})
+    def increment(i):
+        return i + 1
+
+    @op
+    def total(ins: List[int]):
+        return sum(ins)
+
+    @job(
+        resource_defs={
+            "first_step_launcher": local_external_step_launcher,
+            "second_step_launcher": local_external_step_launcher,
+            "io_manager": fs_io_manager,
+        }
+    )
+    def my_job():
+        all_incs = dynamic_outs().map(increment)
+        total(all_incs.collect())
+
+    return my_job
 
 
 def define_basic_pipeline():
@@ -215,6 +250,27 @@ def test_pipeline(mode):
         )
         assert result.result_for_solid("return_two").output_value() == 2
         assert result.result_for_solid("add_one").output_value() == 3
+
+
+def test_dynamic_job():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with instance_for_test() as instance:
+            result = execute_pipeline(
+                pipeline=reconstructable(define_dynamic_job),
+                run_config={
+                    "resources": {
+                        "first_step_launcher": {
+                            "config": {"scratch_dir": tmpdir},
+                        },
+                        "second_step_launcher": {
+                            "config": {"scratch_dir": tmpdir},
+                        },
+                        "io_manager": {"config": {"base_dir": tmpdir}},
+                    }
+                },
+                instance=instance,
+            )
+            assert result.result_for_solid("total").output_value() == 6
 
 
 def test_launcher_requests_retry():
