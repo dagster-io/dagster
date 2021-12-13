ef28693e1cf51a1a8bc3ccc7dd1b529ae45da57f
Isaac Hellendag <dish@elementl.com>
2021-12-13 09:22:30 -0600

    [dagit] Fix Open in Playground link from schedule

--- js_modules/dagit/packages/core/src/pipelines/PipelineOrJobDisambiguationRoot.tsx
+++ js_modules/dagit/packages/core/src/pipelines/PipelineOrJobDisambiguationRoot.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Redirect, RouteComponentProps} from 'react-router-dom';
+import {Redirect, RouteComponentProps, useLocation} from 'react-router-dom';
 
 import {isThisThingAJob, useRepository} from '../workspace/WorkspaceContext';
 import {RepoAddress} from '../workspace/types';
@@ -11,13 +11,18 @@ interface Props extends RouteComponentProps<{repoPath: string; pipelinePath: str
 }
 
 export const PipelineOrJobDisambiguationRoot: React.FC<Props> = (props) => {
+  const location = useLocation();
   const {repoAddress} = props;
   const {pipelinePath} = props.match.params;
   const {pipelineName: pipelineOrJobName} = explorerPathFromString(pipelinePath);
   const repo = useRepository(repoAddress);
   const isJob = isThisThingAJob(repo, pipelineOrJobName);
+  const {search} = location;
 
-  return (
-    <Redirect to={props.match.url.replace('/pipeline_or_job/', isJob ? '/jobs/' : '/pipelines/')} />
+  const replacedPath = props.match.url.replace(
+    '/pipeline_or_job/',
+    isJob ? '/jobs/' : '/pipelines/',
   );
+
+  return <Redirect to={`${replacedPath}${search}`} />;
 };
