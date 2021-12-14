a0577206d0b6a717ceaa21bf7cc1e517694b3f31
Isaac Hellendag <dish@elementl.com>
2021-12-14 14:11:04 -0600

    [dagit] Don't show warning icon for certain sensor/schedule daemon states

--- js_modules/dagit/packages/core/src/nav/InstanceWarningIcon.test.tsx
+++ js_modules/dagit/packages/core/src/nav/InstanceWarningIcon.test.tsx
@@ -48,4 +48,180 @@ describe('InstanceWarningIcon', () => {
       expect(screen.queryByLabelText('warning')).toBeNull();
     });
   });
+
+  describe('Schedule/sensor errors', () => {
+    describe('Schedule error', () => {
+      const scheduleErrorMocks = {
+        DaemonHealth: () => ({
+          allDaemonStatuses: () => [
+            {daemonType: () => 'SCHEDULER', healthy: () => false, required: () => true},
+            {daemonType: () => 'SENSOR', healthy: () => true, required: () => true},
+            {daemonType: () => 'OTHER', healthy: () => true, required: () => true},
+          ],
+        }),
+      };
+
+      it('does not display if there are no schedules, and only a scheduler error', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [],
+            schedules: () => [],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, scheduleErrorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeNull();
+        });
+      });
+
+      it('displays if there are schedules, and only a scheduler error', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [],
+            schedules: () => [1],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, scheduleErrorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeVisible();
+        });
+      });
+    });
+
+    describe('Sensor error', () => {
+      const sensorErrorMocks = {
+        DaemonHealth: () => ({
+          allDaemonStatuses: () => [
+            {daemonType: () => 'SCHEDULER', healthy: () => true, required: () => true},
+            {daemonType: () => 'SENSOR', healthy: () => false, required: () => true},
+            {daemonType: () => 'OTHER', healthy: () => true, required: () => true},
+          ],
+        }),
+      };
+
+      it('does not display if there are no sensors, and only a sensor error', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [],
+            schedules: () => [],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, sensorErrorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeNull();
+        });
+      });
+
+      it('displays if there are sensors, and only a sensor error', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [1],
+            schedules: () => [],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, sensorErrorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeVisible();
+        });
+      });
+    });
+
+    describe('Schedule and Sensor error', () => {
+      const errorMocks = {
+        DaemonHealth: () => ({
+          allDaemonStatuses: () => [
+            {daemonType: () => 'SCHEDULER', healthy: () => false, required: () => true},
+            {daemonType: () => 'SENSOR', healthy: () => false, required: () => true},
+            {daemonType: () => 'OTHER', healthy: () => true, required: () => true},
+          ],
+        }),
+      };
+
+      it('does not display if there are no sensors/schedules, and only (both) sensor/schedule errors', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [],
+            schedules: () => [],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, errorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeNull();
+        });
+      });
+
+      it('displays if there are sensors, and only (both) sensor/schedule errors', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [1],
+            schedules: () => [],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, errorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeVisible();
+        });
+      });
+
+      it('displays if there are schedules, and only (both) sensor/schedule errors', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [],
+            schedules: () => [1],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, errorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeVisible();
+        });
+      });
+
+      it('displays if there are schedules and sensors, and only (both) sensor/schedule errors', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [1],
+            schedules: () => [1],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, errorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeVisible();
+        });
+      });
+    });
+
+    describe('Other error', () => {
+      const otherErrorMocks = {
+        DaemonHealth: () => ({
+          allDaemonStatuses: () => [
+            {daemonType: () => 'SCHEDULER', healthy: () => true, required: () => true},
+            {daemonType: () => 'SENSOR', healthy: () => true, required: () => true},
+            {daemonType: () => 'OTHER', healthy: () => false, required: () => true},
+          ],
+        }),
+      };
+
+      it('displays even if there are no sensors or schedules', async () => {
+        const pipelineMocks = {
+          Pipeline: () => ({
+            sensors: () => [],
+            schedules: () => [],
+          }),
+        };
+
+        render(<Test mocks={[defaultMocks, otherErrorMocks, pipelineMocks]} />);
+        await waitFor(() => {
+          expect(screen.queryByLabelText('warning')).toBeNull();
+        });
+      });
+    });
+  });
 });
