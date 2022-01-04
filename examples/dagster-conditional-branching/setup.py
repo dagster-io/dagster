e4b198d54d5202ac54ec1482590d7f29e49e818c
alangenfeld <alex@elementl.com>
2022-01-04 11:13:36 -0600

    [WIP] python 3.9 support

--- examples/dagster-conditional-branching/setup.py
+++ examples/dagster-conditional-branching/setup.py
@@ -18,6 +18,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
--- examples/hacker_news/setup.py
+++ examples/hacker_news/setup.py
@@ -9,6 +9,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(exclude=["test"]),
