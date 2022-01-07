f82a72fe161244e3e99dd6cce2bb3e02428e4ec0
Chris DeCarolis <cdguitar817@gmail.com>
2022-01-07 10:24:18 -0800

    Fix dagit test

--- python_modules/dagit/dagit_tests/test_app.py
+++ python_modules/dagit/dagit_tests/test_app.py
@@ -279,7 +279,7 @@ def test_dagit_logs(
     caplog,
 ):
     with tempfile.TemporaryDirectory() as temp_dir:
-        with instance_for_test(temp_dir=temp_dir):
+        with instance_for_test(temp_dir=temp_dir, overrides={"telemetry": {"enabled": True}}):
             runner = CliRunner(env={"DAGSTER_HOME": temp_dir})
             workspace_path = file_relative_path(__file__, "telemetry_repository.yaml")
             result = runner.invoke(
