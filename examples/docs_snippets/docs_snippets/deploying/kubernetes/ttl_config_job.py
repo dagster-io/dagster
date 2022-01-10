3bfadacb9c9c2a751ccbc524f4fbcc6be21a0be6
Joseph Van Drunen <josephvandrunen@gmail.com>
2022-01-10 15:48:39 -0500

    avoid black collapsing config

--- examples/docs_snippets/docs_snippets/deploying/kubernetes/ttl_config_job.py
+++ examples/docs_snippets/docs_snippets/deploying/kubernetes/ttl_config_job.py
@@ -1,19 +1,23 @@
 from dagster import job, op
 
+
 @op
 def my_op():
     print("foo")
 
+
+# fmt: off
 # start_ttl
 @job(
-  tags = {
-    'dagster-k8s/config': {
-      'job_spec_config': {
-        'ttl_seconds_after_finished': 7200
-      }
+    tags = {
+        'dagster-k8s/config': {
+            'job_spec_config': {
+                'ttl_seconds_after_finished': 7200
+            }
+        }
     }
-  }
 )
 def my_job():
-  my_op()
+    my_op()
 # end_ttl
+# fmt: on
