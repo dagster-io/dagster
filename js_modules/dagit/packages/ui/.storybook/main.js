7b093643d2799d73dcc14805a31a7fb398a9b889
Isaac Hellendag <hellendag@users.noreply.github.com>
2022-01-20 10:39:40 -0600

    [dagster-io/ui] Disallow wrapping inside Button (#6257)

--- js_modules/dagit/packages/ui/.storybook/main.js
+++ js_modules/dagit/packages/ui/.storybook/main.js
@@ -10,5 +10,9 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "webpack5"
+  },
+  // https://storybook.js.org/docs/react/configure/webpack#bundle-splitting
+  features: {
+    storyStoreV7: true,
   }
 }
