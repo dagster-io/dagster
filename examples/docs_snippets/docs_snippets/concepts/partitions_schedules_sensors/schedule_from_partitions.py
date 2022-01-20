c0d759787fc53684cfae60637f03a0fbceb4ae68
gibsondan <daniel@elementl.com>
2022-01-19 20:44:44 -0600

    Explain how to use end_offset to achieve what used to be possible with the partition_days_offset field in partitioned schedules docs (#6233)

--- examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/schedule_from_partitions.py
+++ examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/schedule_from_partitions.py
@@ -1,9 +1,18 @@
 """isort:skip_file"""
-from .partitioned_job import do_stuff_partitioned
+
+from .partitioned_job import my_partitioned_config
 
 # start_marker
-from dagster import build_schedule_from_partitioned_job
+from dagster import build_schedule_from_partitioned_job, job
 
-do_stuff_partitioned_schedule = build_schedule_from_partitioned_job(do_stuff_partitioned)
+
+@job(config=my_partitioned_config)
+def do_stuff_partitioned():
+    ...
+
+
+do_stuff_partitioned_schedule = build_schedule_from_partitioned_job(
+    do_stuff_partitioned,
+)
 
 # end_marker
