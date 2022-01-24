82abdd6f9d619af132a614330f12d20a94e16d95
Chris DeCarolis <cdguitar817@gmail.com>
2022-01-24 10:05:39 -0800

    Change implementation for postgres

--- python_modules/libraries/dagster-postgres/dagster_postgres/run_storage/run_storage.py
+++ python_modules/libraries/dagster-postgres/dagster_postgres/run_storage/run_storage.py
@@ -54,11 +54,12 @@ class PostgresRunStorage(SqlRunStorage, ConfigurableClass):
 
         # Stamp and create tables if the main table does not exist (we can't check alembic
         # revision because alembic config may be shared with other storage classes)
-        if self.should_autocreate_tables and (
-            "runs" not in table_names or "telemetry_info" not in table_names
-        ):
+        if self.should_autocreate_tables and "runs" not in table_names:
             retry_pg_creation_fn(self._init_db)
             self.build_missing_indexes()
+
+        if "telemetry_info" not in table_names:
+            RunStorageSqlMetadata.create_all(self._engine)
 
         super().__init__()
 
