3251e5bc50a2be5baacac86786653b7b9d0f7f59
alangenfeld <alex@elementl.com>
2022-01-27 12:00:08 -0600

    [dag-star] fix graphql http handling

--- python_modules/dagit/dagit_tests/starlette/test_app.py
+++ python_modules/dagit/dagit_tests/starlette/test_app.py
@@ -5,22 +5,50 @@ from dagit.graphql import GraphQLWS
 from dagit.starlette import ROOT_ADDRESS_STATIC_RESOURCES
 from dagit.version import __version__ as dagit_version
 from dagster import __version__ as dagster_version
-from dagster import execute_pipeline, graph, op, pipeline, reconstructable
+from dagster import job, op
+from dagster.seven import json
 from dagster_graphql.version import __version__ as dagster_graphql_version
 from starlette.testclient import TestClient
 
 EVENT_LOG_SUBSCRIPTION = """
-    subscription PipelineRunLogsSubscription($runId: ID!) {
-        pipelineRunLogs(runId: $runId) {
-            __typename
+subscription PipelineRunLogsSubscription($runId: ID!) {
+    pipelineRunLogs(runId: $runId) {
+        __typename
+    }
+}
+"""
+
+RUN_QUERY = """
+query RunQuery($runId: ID!) {
+    pipelineRunOrError(runId: $runId) {
+        __typename
+        ... on Run {
+            id
+        }
+        ... on PythonError {
+            message
         }
     }
+}
 """
 
 
-def test_dagit_info(empty_app):
-    client = TestClient(empty_app)
-    response = client.get("/dagit_info")
+def _add_run(instance):
+    @op
+    def my_op():
+        print("STDOUT RULEZ")  # pylint: disable=print-call
+
+    @job
+    def _simple_job():
+        my_op()
+
+    result = _simple_job.execute_in_process(instance=instance)
+    assert result.success
+    return result.run_id
+
+
+def test_dagit_info(test_client: TestClient):
+    response = test_client.get("/dagit_info")
     assert response.status_code == 200
     assert response.json() == {
         "dagit_version": dagit_version,
@@ -29,50 +57,109 @@ def test_dagit_info(empty_app):
     }
 
 
-def test_static_resources(empty_app):
-    client = TestClient(empty_app)
-
+def test_static_resources(test_client: TestClient):
     # make sure we did not fallback to the index html
     # for static resources at /
     for address in ROOT_ADDRESS_STATIC_RESOURCES:
-        response = client.get(address)
+        response = test_client.get(address)
         assert response.status_code == 200, response.text
         assert response.headers["content-type"] != "text/html"
 
-    response = client.get("/vendor/graphql-playground/middleware.js")
+    response = test_client.get("/vendor/graphql-playground/middleware.js")
     assert response.status_code == 200, response.text
     assert response.headers["content-type"] != "application/js"
 
 
-def test_graphql_get(empty_app):
-    client = TestClient(empty_app)
-    response = client.get(
-        "/graphql?query={__typename}",
-    )
-    assert response.status_code == 200, response.text
-    assert response.json() == {"data": {"__typename": "DagitQuery"}}
+# https://graphql.org/learn/serving-over-http/
 
 
-def test_graphql_post(empty_app):
-    client = TestClient(empty_app)
-    response = client.post(
-        "/graphql?query={__typename}",
-    )
-    assert response.status_code == 200, response.text
-    assert response.json() == {"data": {"__typename": "DagitQuery"}}
-
-    response = client.post(
+def test_graphql_get(instance, test_client: TestClient):
+    # base case
+    response = test_client.get(
         "/graphql",
-        json={"query": "{__typename}"},
+        params={"query": "{__typename}"},
+    )
+    assert response.status_code == 200, response.text
+    assert response.json() == {"data": {"__typename": "DagitQuery"}}
+
+    # missing
+    response = test_client.get("/graphql")
+    assert response.status_code == 400, response.text
+
+    # variables
+    var_str = json.dumps({"runId": "missing"})
+    response = test_client.get(
+        "/graphql",
+        params={"query": RUN_QUERY, "variables": var_str},
+    )
+    assert response.status_code == 200, response.text
+    assert response.json() == {"data": {"pipelineRunOrError": {"__typename": "RunNotFoundError"}}}
+
+    # malformed vars
+    response = test_client.get(
+        "/graphql",
+        params={
+            "query": RUN_QUERY,
+            "variables": var_str[:-2],  # malform by trimming
+        },
+    )
+    assert response.status_code == 400, response.text
+
+
+def test_graphql_post(test_client: TestClient):
+    # base case
+    response = test_client.post(
+        "/graphql",
+        params={"query": "{__typename}"},
+    )
+    assert response.status_code == 200, response.text
+    assert response.json() == {"data": {"__typename": "DagitQuery"}}
+
+    # missing
+    response = test_client.post("/graphql")
+    assert response.status_code == 400, response.text
+
+    # variables
+    var_str = json.dumps({"runId": "missing"})
+    response = test_client.post(
+        "/graphql",
+        params={"query": RUN_QUERY, "variables": var_str},
+    )
+    assert response.status_code == 200, response.text
+    assert response.json() == {"data": {"pipelineRunOrError": {"__typename": "RunNotFoundError"}}}
+
+    # malformed vars
+    response = test_client.post(
+        "/graphql",
+        params={
+            "query": RUN_QUERY,
+            "variables": var_str[:-2],  # malform by trimming
+        },
+    )
+    assert response.status_code == 400, response.text
+
+    # application/json
+    response = test_client.post(
+        "/graphql",
+        json={"query": RUN_QUERY, "variables": {"runId": "missing"}},
+    )
+    assert response.status_code == 200, response.text
+    assert response.json() == {"data": {"pipelineRunOrError": {"__typename": "RunNotFoundError"}}}
+
+    # application/graphql
+    response = test_client.post(
+        "/graphql",
+        data="{__typename}",
+        headers={"Content-type": "application/graphql"},
     )
     assert response.status_code == 200, response.text
     assert response.json() == {"data": {"__typename": "DagitQuery"}}
 
 
-def test_graphql_ws_error(empty_app):
+def test_graphql_ws_error(test_client: TestClient):
     # wtf pylint
     # pylint: disable=not-context-manager
-    with TestClient(empty_app).websocket_connect("/graphql", str(GraphQLWS.PROTOCOL)) as ws:
+    with test_client.websocket_connect("/graphql", str(GraphQLWS.PROTOCOL)) as ws:
         ws.send_json({"type": GraphQLWS.CONNECTION_INIT})
         ws.send_json(
             {
@@ -91,16 +178,11 @@ def test_graphql_ws_error(empty_app):
         assert response["type"] == GraphQLWS.ERROR
 
 
-def test_graphql_ws_success(instance, empty_app):
-    @pipeline
-    def _test():
-        pass
-
-    result = execute_pipeline(_test, instance=instance)
-    run_id = result.run_id
+def test_graphql_ws_success(instance, test_client: TestClient):
+    run_id = _add_run(instance)
     # wtf pylint
     # pylint: disable=not-context-manager
-    with TestClient(empty_app).websocket_connect("/graphql", GraphQLWS.PROTOCOL) as ws:
+    with test_client.websocket_connect("/graphql", GraphQLWS.PROTOCOL) as ws:
         ws.send_json({"type": GraphQLWS.CONNECTION_INIT})
         ws.send_json(
             {
@@ -126,38 +208,20 @@ def test_graphql_ws_success(instance, empty_app):
     assert len(objgraph.by_type("PipelineRunObservableSubscribe")) == 0
 
 
-def test_download_debug_file(instance, empty_app):
-    @pipeline
-    def _test():
-        pass
+def test_download_debug_file(instance, test_client: TestClient):
+    run_id = _add_run(instance)
 
-    result = execute_pipeline(_test, instance=instance)
-    run_id = result.run_id
-
-    response = TestClient(empty_app).get(f"/download_debug/{run_id}")
+    response = test_client.get(f"/download_debug/{run_id}")
     assert response.status_code == 200
     assert response.headers["content-type"] == "application/gzip"
 
 
-def _simple_job():
-    @op
-    def my_op():
-        print("STDOUT RULEZ")  # pylint: disable=print-call
+def test_download_compute(instance, test_client: TestClient):
+    run_id = _add_run(instance)
 
-    @graph
-    def my_graph():
-        my_op()
-
-    return my_graph.to_job()
-
-
-def test_download_compute(instance, empty_app):
-    result = execute_pipeline(reconstructable(_simple_job), instance=instance)
-    run_id = result.run_id
-
-    response = TestClient(empty_app).get(f"/download/{run_id}/my_op/stdout")
+    response = test_client.get(f"/download/{run_id}/my_op/stdout")
     assert response.status_code == 200
     assert "STDOUT RULEZ" in str(response.content)
 
-    response = TestClient(empty_app).get(f"/download/{run_id}/jonx/stdout")
+    response = test_client.get(f"/download/{run_id}/jonx/stdout")
     assert response.status_code == 404
