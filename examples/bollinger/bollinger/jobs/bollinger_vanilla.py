71577a270e4735e56b42dc73482feec0e87cc313
Sean Mackesey <s.mackesey@gmail.com>
2022-01-28 11:37:43 -0800

    [jpm-example] fix/format bollinger_vanilla anom events

--- examples/bollinger/bollinger/jobs/bollinger_vanilla.py
+++ examples/bollinger/bollinger/jobs/bollinger_vanilla.py
@@ -1,8 +1,17 @@
 from dagster import In, Out, job, op
-from ..lib import AnomalousEventsDgType, Sp500PricesDgType, load_sp500_prices, compute_bollinger_multi, compute_anomalous_events, BollingerDgType
+
+from ..lib import (
+    AnomalousEventsDgType,
+    BollingerDgType,
+    Sp500PricesDgType,
+    compute_anomalous_events,
+    compute_bollinger_multi,
+    load_sp500_prices,
+)
 
 # ****************************************************************************
 # ***** OP VERSION ***********************************************************
+
 
 @op(
     out=Out(dagster_type=Sp500PricesDgType),
@@ -10,24 +19,27 @@ from ..lib import AnomalousEventsDgType, Sp500PricesDgType, load_sp500_prices, c
 def van_sp500_prices():
     return load_sp500_prices()
 
+
 @op(
     ins={
         "sp500_prices": In(dagster_type=Sp500PricesDgType),
     },
-    out=Out(dagster_type=BollingerDgType)
+    out=Out(dagster_type=BollingerDgType),
 )
 def van_bollinger(sp500_prices):
     return compute_bollinger_multi(sp500_prices)
+
 
 @op(
     ins={
         "bollinger": In(dagster_type=BollingerDgType),
     },
-    out=Out(dagster_type=AnomalousEventsDgType)
+    out=Out(dagster_type=AnomalousEventsDgType),
 )
 def van_anomalous_events(bollinger):
-    return compute_anomalous_events(bollinger())
-    
+    return compute_anomalous_events(bollinger)
+
+
 @job
 def bollinger_vanilla():
     van_anomalous_events(van_bollinger(van_sp500_prices()))
