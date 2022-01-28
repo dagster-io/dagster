82d8bfa26e14bc28835253972767007b1e71070d
prha <prha@elementl.com>
2022-01-27 16:22:15 -0800

    change down revision

--- python_modules/libraries/dagster-mysql/dagster_mysql/alembic/versions/17154c80d885_add_range_index.py
+++ python_modules/libraries/dagster-mysql/dagster_mysql/alembic/versions/17154c80d885_add_range_index.py
@@ -1,7 +1,7 @@
 """add range index
 
 Revision ID: 17154c80d885
-Revises: 29a8e9d74220
+Revises: f78059038d01
 Create Date: 2022-01-20 11:45:26.092743
 
 """
@@ -9,7 +9,7 @@ from dagster.core.storage.migration.utils import create_run_range_indices
 
 # revision identifiers, used by Alembic.
 revision = "17154c80d885"
-down_revision = "29a8e9d74220"
+down_revision = "f78059038d01"
 branch_labels = None
 depends_on = None
 
