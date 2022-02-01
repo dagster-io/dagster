45efed7b0a8a87e0445147098282e25d9238aa05
alangenfeld <alex@elementl.com>
2022-02-01 15:52:06 -0600

    [dagit] use system temp for instance root dir

--- python_modules/dagster/dagster/cli/utils.py
+++ python_modules/dagster/dagster/cli/utils.py
@@ -1,8 +1,8 @@
-import os
 import tempfile
 from contextlib import contextmanager
 
 import click
+from dagster import seven
 from dagster.core.instance import DagsterInstance, is_dagster_home_set
 
 
@@ -12,9 +12,9 @@ def get_instance_for_service(service_name):
         with DagsterInstance.get() as instance:
             yield instance
     else:
-        # make the temp dir in the cwd since default temp dir roots
+        # make the temp dir in system temp since default temp dir roots
         # have issues with FS notif based event log watching
-        with tempfile.TemporaryDirectory(dir=os.getcwd()) as tempdir:
+        with tempfile.TemporaryDirectory(dir=seven.get_system_temp_directory()) as tempdir:
             click.echo(
                 f"Using temporary directory {tempdir} for storage. This will be removed when {service_name} exits.\n"
                 "To persist information across sessions, set the environment variable DAGSTER_HOME to a directory to use.\n"
