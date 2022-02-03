84817fd0e6bc0c549590dd89316991136ad00597
Johann Miller <johann@elementl.com>
2022-02-03 13:30:51 -0500

    Fix sanitize_k8s_label regex (#6478)
    <!--- Hello Dagster contributor! It's great to have you with us! -->
    <!-- Make sure to read https://docs.dagster.io/community/contributing -->
    
    ## Summary
    <!-- Describe your changes here, include the motivation/context, test coverage, -->
    <!-- the type of change i.e. breaking change, new feature, or bug fix -->
    <!-- and related GitHub issue or screenshots (if applicable). -->
    
    
    
    
    ## Test Plan
    <!--- Please describe the tests you have added and your testing environment (if applicable). -->
    
    
    
    
    ## Checklist
    <!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask in our Slack. -->
    
    - [ ] My change requires a change to the documentation and I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.

--- python_modules/libraries/dagster-k8s/dagster_k8s/utils.py
+++ python_modules/libraries/dagster-k8s/dagster_k8s/utils.py
@@ -16,7 +16,7 @@ def sanitize_k8s_label(label_name: str):
     # Truncate too long label values to fit into 63-characters limit and avoid invalid characters.
     # https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#syntax-and-character-set
     label_name = label_name[:63]
-    return re.sub(r"[^a-z0-9\-_\.]", "-", label_name).strip("-")
+    return re.sub(r"[^a-zA-Z0-9\-_\.]", "-", label_name).strip("-").strip("_")
 
 
 def retrieve_pod_logs(pod_name, namespace):
