91b99249847541eecb8c75713a7e76a4f8e9ca31
prha <prha@elementl.com>
2022-02-03 17:45:12 -0800

    add 3.9.10 python

--- .buildkite/dagster-buildkite/dagster_buildkite/defines.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/defines.py
@@ -1,4 +1,5 @@
 class SupportedPython:
+    V3_9 = "3.9.10"
     V3_8 = "3.8.7"
     V3_7 = "3.7.9"
     V3_6 = "3.6.12"
@@ -8,11 +9,13 @@ SupportedPythons = [
     SupportedPython.V3_6,
     SupportedPython.V3_7,
     SupportedPython.V3_8,
+    SupportedPython.V3_9,
 ]
 
 ExamplePythons = [SupportedPython.V3_8]
 
 TOX_MAP = {
+    SupportedPython.V3_9: "py39",
     SupportedPython.V3_8: "py38",
     SupportedPython.V3_7: "py37",
     SupportedPython.V3_6: "py36",
