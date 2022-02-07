b1dd39a76e6366ff713165f7b6ca89a4b793151d
prha <1040172+prha@users.noreply.github.com>
2022-02-07 09:24:25 -0800

    add python 3.9.10 to BK test suite (#6491)
    * update versions, add images
    
    * add 3.9.10 python
    
    * repush images with updated timestamp tags
    
    * fix installs for py39 tests; pyarrow
    
    * rebuild images with cpython, python-dev, libevent-dev
    
    * add python-dev / libevent-dev to unit for smoke test
    
    * keep gcc installed for pipenv install in smoke test
    
    * remove libevent shenanigans
    
    * ignore airflow on py39
    
    * airflow py39
    
    * simplify base image

--- .buildkite/dagster-buildkite/setup.py
+++ .buildkite/dagster-buildkite/setup.py
@@ -12,6 +12,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
