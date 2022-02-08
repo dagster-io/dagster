262f73fcf3d4bfc1ce47717b5fb41bc4660167d2
prha <prha@elementl.com>
2022-02-07 17:03:45 -0800

    mysql back compat tests

---
+++ python_modules/libraries/dagster-mysql/dagster_mysql_tests/compat_tests/test_back_compat.py
@@ -0,0 +1,44 @@
+# pylint: disable=protected-access
+
+import os
+import subprocess
+import tempfile
+
+from dagster.core.instance import DagsterInstance
+from dagster.utils import file_relative_path
+
+
+def _reconstruct_from_file(hostname, path, username="root", password="test"):
+    env = os.environ.copy()
+    env["MYSQL_PWD"] = "test"
+    subprocess.check_call(f"mysql -uroot -h{hostname} test < {path}", shell=True, env=env)
+
+
+def test_0_13_17_mysql_convert_float_cols(hostname, conn_string):
+    _reconstruct_from_file(
+        hostname,
+        file_relative_path(__file__, "snapshot_0_13_18_start_end_timestamp.sql"),
+    )
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        with open(file_relative_path(__file__, "dagster.yaml"), "r") as template_fd:
+            with open(os.path.join(tempdir, "dagster.yaml"), "w") as target_fd:
+                template = template_fd.read().format(hostname=hostname)
+                target_fd.write(template)
+
+        instance = DagsterInstance.from_config(tempdir)
+        record = instance.get_run_records(limit=1)[0]
+        assert record.start_timestamp is None
+        assert record.end_timestamp is None
+
+        instance.upgrade()
+
+        record = instance.get_run_records(limit=1)[0]
+        assert record.start_timestamp is None
+        assert record.end_timestamp is None
+
+        instance.reindex()
+
+        record = instance.get_run_records(limit=1)[0]
+        assert int(record.start_timestamp.timestamp()) == 1643817629
+        assert int(record.end_timestamp.timestamp()) == 1643817634
