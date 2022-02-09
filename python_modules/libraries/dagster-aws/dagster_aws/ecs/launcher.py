81381e2c9fdf3badd64836c929af73ce27bb0fea
gibsondan <daniel@elementl.com>
2022-02-09 10:59:26 -0600

    Allow noneable secrets_tag that doesn't hit secretsmanager (#6529)
    Summary:
    A user reported they don't have access to secrets_manager. Defaulting to a 'dagster' tag is probably fine, but should include a way for them to set the tag to empty if they want zero secret manager calls.
    
    Test Plan: Bk

--- python_modules/libraries/dagster-aws/dagster_aws/ecs/launcher.py
+++ python_modules/libraries/dagster-aws/dagster_aws/ecs/launcher.py
@@ -2,11 +2,12 @@ from collections import namedtuple
 
 import boto3
 from botocore.exceptions import ClientError
-from dagster import Array, Field, StringSource, check
+from dagster import Array, Field, Noneable, StringSource, check
 from dagster.core.events import EngineEventData, EventMetadataEntry
 from dagster.core.launcher.base import LaunchRunContext, RunLauncher
 from dagster.grpc.types import ExecuteRunArgs
 from dagster.serdes import ConfigurableClass
+from dagster.utils import merge_dicts
 from dagster.utils.backcompat import experimental
 
 from ..secretsmanager import get_secrets_from_arns, get_tagged_secrets
@@ -81,7 +82,7 @@ class EcsRunLauncher(RunLauncher, ConfigurableClass):
                 ),
             ),
             "secrets_tag": Field(
-                StringSource,
+                Noneable(StringSource),
                 is_required=False,
                 default_value="dagster",
                 description=(
@@ -234,10 +235,14 @@ class EcsRunLauncher(RunLauncher, ConfigurableClass):
             metadata,
             image,
             self.container_name,
-            secrets={
-                **get_tagged_secrets(self.secrets_manager, self.secrets_tag),
-                **get_secrets_from_arns(self.secrets_manager, self.secrets),
-            },
+            secrets=merge_dicts(
+                (
+                    get_tagged_secrets(self.secrets_manager, self.secrets_tag)
+                    if self.secrets_tag
+                    else {}
+                ),
+                get_secrets_from_arns(self.secrets_manager, self.secrets),
+            ),
         )
 
     def _task_metadata(self):
