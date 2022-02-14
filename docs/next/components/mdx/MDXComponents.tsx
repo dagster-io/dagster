4e1a43eac9a876065b80d4b9a62f5a3eb48ea9d9
Sean Mackesey <s.mackesey@gmail.com>
2022-02-14 13:44:21 -0800

    remove commented-out old Warning implementation

--- docs/next/components/mdx/MDXComponents.tsx
+++ docs/next/components/mdx/MDXComponents.tsx
@@ -235,34 +235,6 @@ const Warning = ({ children }) => {
   return <Admonition style="warning">{children}</Admonition>;
 };
 
-// const Warning = ({ children }) => {
-//   return (
-//     <div className="bg-yellow-50 border-l-4 border-yellow-400 px-4 my-4">
-//       <div className="flex items-center">
-//         <div className="flex-shrink-0">
-//           {/* Heroicon name: solid/exclamation */}
-//           <svg
-//             className="h-5 w-5 text-yellow-400"
-//             xmlns="http://www.w3.org/2000/svg"
-//             viewBox="0 0 20 20"
-//             fill="currentColor"
-//             aria-hidden="true"
-//           >
-//             <path
-//               fillRule="evenodd"
-//               d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-//               clipRule="evenodd"
-//             />
-//           </svg>
-//         </div>
-//         <div className="ml-3">
-//           <p className="text-sm text-yellow-700">{children}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
 const CodeReferenceLink = ({ filePath, isInline, children }) => {
   const { version } = useVersion();
   const url = `https://github.com/dagster-io/dagster/tree/${version}/${filePath}`;
