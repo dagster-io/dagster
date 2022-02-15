5b8f564590c7d76ea7cf894972581a128483f2af
OwenKephart <owen@elementl.com>
2022-02-15 11:52:14 -0800

    [dagster-dbt] dbt asset improvements (#6600)

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/test_asset_defs.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/test_asset_defs.py
@@ -5,7 +5,8 @@ from dagster import AssetKey, EventMetadataEntry, ResourceDefinition
 from dagster.core.asset_defs import build_assets_job
 from dagster.core.asset_defs.decorators import ASSET_DEPENDENCY_METADATA_KEY
 from dagster.utils import file_relative_path
-from dagster_dbt.asset_defs import load_assets_from_dbt_manifest
+from dagster_dbt import dbt_cli_resource
+from dagster_dbt.asset_defs import load_assets_from_dbt_manifest, load_assets_from_dbt_project
 from dagster_dbt.types import DbtOutput
 
 
@@ -75,7 +76,7 @@ def assert_assets_match_project(dbt_assets):
     assets_op = dbt_assets[0].op
     assert assets_op.tags == {"kind": "dbt"}
     assert len(assets_op.input_defs) == 0
-    assert assets_op.output_dict.keys() == {
+    assert set(assets_op.output_dict.keys()) == {
         "sort_by_calories",
         "least_caloric",
         "sort_hot_cereals_by_calories",
@@ -93,3 +94,114 @@ def assert_assets_match_project(dbt_assets):
     root_out_def = assets_op.output_dict.get("sort_by_calories")
     assert root_out_def.hardcoded_asset_key == AssetKey(["sort_by_calories"])
     assert not root_out_def.metadata[ASSET_DEPENDENCY_METADATA_KEY]
+
+
+def test_basic(
+    dbt_seed, conn_string, test_project_dir, dbt_config_dir
+):  # pylint: disable=unused-argument
+
+    dbt_assets = load_assets_from_dbt_project(test_project_dir, dbt_config_dir)
+
+    result = build_assets_job(
+        "test_job",
+        dbt_assets,
+        resource_defs={
+            "dbt": dbt_cli_resource.configured(
+                {"project_dir": test_project_dir, "profiles_dir": dbt_config_dir}
+            )
+        },
+    ).execute_in_process()
+
+    assert result.success
+    materializations = [
+        event.event_specific_data.materialization
+        for event in result.events_for_node(dbt_assets[0].op.name)
+        if event.event_type_value == "ASSET_MATERIALIZATION"
+    ]
+    assert len(materializations) == 4
+
+
+def test_select_from_project(
+    dbt_seed, conn_string, test_project_dir, dbt_config_dir
+):  # pylint: disable=unused-argument
+
+    dbt_assets = load_assets_from_dbt_project(
+        test_project_dir, dbt_config_dir, select="sort_by_calories"
+    )
+
+    result = build_assets_job(
+        "test_job",
+        dbt_assets,
+        resource_defs={
+            "dbt": dbt_cli_resource.configured(
+                {"project_dir": test_project_dir, "profiles_dir": dbt_config_dir}
+            )
+        },
+    ).execute_in_process()
+
+    assert result.success
+    materializations = [
+        event.event_specific_data.materialization
+        for event in result.events_for_node(dbt_assets[0].op.name)
+        if event.event_type_value == "ASSET_MATERIALIZATION"
+    ]
+    assert len(materializations) == 1
+
+
+def test_select_from_manifest(
+    dbt_seed, conn_string, test_project_dir, dbt_config_dir
+):  # pylint: disable=unused-argument
+
+    manifest_path = file_relative_path(__file__, "sample_manifest.json")
+    with open(manifest_path, "r") as f:
+        manifest_json = json.load(f)
+    dbt_assets = load_assets_from_dbt_manifest(
+        manifest_json, selected_unique_ids={"model.dagster_dbt_test_project.sort_by_calories"}
+    )
+
+    result = build_assets_job(
+        "test_job",
+        dbt_assets,
+        resource_defs={
+            "dbt": dbt_cli_resource.configured(
+                {"project_dir": test_project_dir, "profiles_dir": dbt_config_dir}
+            )
+        },
+    ).execute_in_process()
+
+    assert result.success
+    materializations = [
+        event.event_specific_data.materialization
+        for event in result.events_for_node(dbt_assets[0].op.name)
+        if event.event_type_value == "ASSET_MATERIALIZATION"
+    ]
+    assert len(materializations) == 1
+
+
+def test_node_info_to_asset_key(
+    dbt_seed, conn_string, test_project_dir, dbt_config_dir
+):  # pylint: disable=unused-argument
+    dbt_assets = load_assets_from_dbt_project(
+        test_project_dir,
+        dbt_config_dir,
+        node_info_to_asset_key=lambda node_info: AssetKey(["foo", node_info["name"]]),
+    )
+
+    result = build_assets_job(
+        "test_job",
+        dbt_assets,
+        resource_defs={
+            "dbt": dbt_cli_resource.configured(
+                {"project_dir": test_project_dir, "profiles_dir": dbt_config_dir}
+            )
+        },
+    ).execute_in_process()
+
+    assert result.success
+    materializations = [
+        event.event_specific_data.materialization
+        for event in result.events_for_node(dbt_assets[0].op.name)
+        if event.event_type_value == "ASSET_MATERIALIZATION"
+    ]
+    assert len(materializations) == 4
+    assert materializations[0].asset_key == AssetKey(["foo", "sort_by_calories"])
