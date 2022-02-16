df13b76008f6df55d5306e242155017b95028e58
Sean Mackesey <s.mackesey@gmail.com>
2022-02-16 10:35:48 -0800

    make breaking-version variable

--- python_modules/dagster/dagster/__init__.py
+++ python_modules/dagster/dagster/__init__.py
@@ -253,31 +253,32 @@ from .version import __version__
 from dagster.config.source import BoolSource, StringSource, IntSource  # isort:skip
 
 _DEPRECATED = {
-    "EventMetadataEntry": ("MetadataEntry", MetadataEntry),
-    "EventMetadata": ("MetadataValue", MetadataValue),
-    "TextMetadataEntryData": ("TextMetadataValue", TextMetadataValue),
-    "UrlMetadataEntryData": ("UrlMetadataValue", UrlMetadataValue),
-    "PathMetadataEntryData": ("PathMetadataValue", PathMetadataValue),
-    "JsonMetadataEntryData": ("JsonMetadataValue", JsonMetadataValue),
-    "MarkdownMetadataEntryData": ("MarkdownMetadataValue", MarkdownMetadataValue),
-    "PythonArtifactMetadataEntryData": ("PythonArtifactMetadataValue", PythonArtifactMetadataValue),
-    "FloatMetadataEntryData": ("FloatMetadataValue", FloatMetadataValue),
-    "IntMetadataEntryData": ("IntMetadataValue", IntMetadataValue),
+    "EventMetadataEntry": ("MetadataEntry", MetadataEntry, "0.15.0"),
+    "EventMetadata": ("MetadataValue", MetadataValue, "0.15.0"),
+    "TextMetadataEntryData": ("TextMetadataValue", TextMetadataValue, "0.15.0"),
+    "UrlMetadataEntryData": ("UrlMetadataValue", UrlMetadataValue, "0.15.0"),
+    "PathMetadataEntryData": ("PathMetadataValue", PathMetadataValue, "0.15.0"),
+    "JsonMetadataEntryData": ("JsonMetadataValue", JsonMetadataValue, "0.15.0"),
+    "MarkdownMetadataEntryData": ("MarkdownMetadataValue", MarkdownMetadataValue, "0.15.0"),
+    "PythonArtifactMetadataEntryData": ("PythonArtifactMetadataValue", PythonArtifactMetadataValue, "0.15.0"),
+    "FloatMetadataEntryData": ("FloatMetadataValue", FloatMetadataValue, "0.15.0"),
+    "IntMetadataEntryData": ("IntMetadataValue", IntMetadataValue, "0.15.0"),
     "DagsterPipelineRunMetadataEntryData": (
         "DagsterPipelineRunMetadataValue",
         DagsterPipelineRunMetadataValue,
+        "0.15.0",
     ),
-    "DagsterAssetMetadataEntryData": ("DagsterAssetMetadataValue", DagsterAssetMetadataValue),
-    "TableMetadataEntryData": ("TableMetadataValue", TableMetadataValue),
-    "TableSchemaMetadataEntryData": ("TableSchemaMetadataValue", TableSchemaMetadataValue),
+    "DagsterAssetMetadataEntryData": ("DagsterAssetMetadataValue", DagsterAssetMetadataValue, "0.15.0"),
+    "TableMetadataEntryData": ("TableMetadataValue", TableMetadataValue, "0.15.0"),
+    "TableSchemaMetadataEntryData": ("TableSchemaMetadataValue", TableSchemaMetadataValue, "0.15.0"),
 }
 
 
 def __getattr__(name):
     if name in _DEPRECATED:
-        new_name, value = _DEPRECATED[name]
+        new_name, value, breaking_version = _DEPRECATED[name]
         stacklevel = 3 if sys.version_info >= (3, 7) else 4
-        rename_warning(new_name, name, "0.15.0", stacklevel=stacklevel)
+        rename_warning(new_name, name, breaking_version, stacklevel=stacklevel)
         return value
     else:
         raise AttributeError("module '{}' has no attribute '{}'".format(__name__, name))
