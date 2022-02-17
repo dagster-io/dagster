f03f9e106f6a117dab51784b6882dc27a5c01753
Sean Mackesey <s.mackesey@gmail.com>
2022-02-16 22:24:40 -0800

    Merge sean/02-16-change_deprecation_strategy into master (#6670)
    * change deprecation strategy

--- python_modules/dagster/dagster_tests/general_tests/test_api.py
+++ python_modules/dagster/dagster_tests/general_tests/test_api.py
@@ -1,8 +1,6 @@
 import inspect
-import re
 
 import dagster
-import pytest
 
 
 def test_all():
@@ -10,20 +8,5 @@ def test_all():
     for each in dagster.__all__:
         assert each in dagster_dir
     for exported in dagster_dir:
-        if (
-            not exported.startswith("_")
-            and not inspect.ismodule(getattr(dagster, exported))
-            and not exported in dagster._DEPRECATED
-        ):
+        if not exported.startswith("_") and not inspect.ismodule(getattr(dagster, exported)):
             assert exported in dagster.__all__
-
-
-def test_deprecated_imports(monkeypatch):
-
-    # Using `EventMetadataEntry` as a standin for the rest since monkeypatch does not work on 3.6
-    from dagster import MetadataEntry
-
-    with pytest.warns(DeprecationWarning, match=re.escape('"EventMetadataEntry" is deprecated')):
-        from dagster import EventMetadataEntry  # pylint: disable=no-name-in-module
-
-    assert EventMetadataEntry is MetadataEntry
