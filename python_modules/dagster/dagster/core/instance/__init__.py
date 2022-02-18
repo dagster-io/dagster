fe61ba5d1784e4e55d20ceaf09b03e0830199b4f
gibsondan <daniel@elementl.com>
2022-02-18 13:28:46 -0600

    Remove bad invariant from back when sensor status was a fixed status, not a default status (#6711)
    * Remove bad invariant from back when sensor status was a fixed status, not a default status
    
    Summary:
    Plus add some missing test coverage for start_sensor when the default status is set to RUNNING in code, will do a similar pass for schedules separately now
    
    * revert failing snapshot tests
    
    Co-authored-by: prha <prha@elementl.com>

--- python_modules/dagster/dagster/core/instance/__init__.py
+++ python_modules/dagster/dagster/core/instance/__init__.py
@@ -1693,12 +1693,14 @@ records = instance.get_event_records(
         )
         from dagster.core.definitions.run_request import InstigatorType
 
-        check.invariant(
-            not external_sensor.default_status,
-            "Can only manually start a sensor that does not have its status set in code",
-        )
-
         state = self.get_instigator_state(external_sensor.get_external_origin_id())
+
+        if external_sensor.get_current_instigator_state(state).is_running:
+            raise Exception(
+                "You have attempted to start sensor {name}, but it is already running".format(
+                    name=external_sensor.name
+                )
+            )
 
         if not state:
             return self.add_instigator_state(
