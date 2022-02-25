bfe067be86727ec1dea14412ffb96c0d2843b630
JamieDeMaria <jamie@elementl.com>
2022-02-25 11:07:07 -0500

    docs updates

--- examples/docs_snippets/docs_snippets/concepts/assets/asset_w_context.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_w_context.py
@@ -3,11 +3,10 @@ from dagster import asset
 # start_w_context
 
 
-@asset(required_resource_keys={"alerter"})
+@asset(required_resource_keys={"api"})
 def my_asset(context):
-    # creates an asset and sends an alert to notify of its creation
-    context.resources.alerter.send_asset_create_alert("my_asset")
-    return [1, 2, 3]
+    # fetches contents of an asset
+    return context.resources.api.fetch_table("my_asset")
 
 
 # end_w_context
