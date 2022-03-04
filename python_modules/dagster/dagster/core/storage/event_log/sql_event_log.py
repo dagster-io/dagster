6fb4bcf13888e3c8ad8f1393b57f73109e0cab7f
prha <prha@elementl.com>
2022-03-04 10:00:42 -0800

    black

--- python_modules/dagster/dagster/core/storage/event_log/sql_event_log.py
+++ python_modules/dagster/dagster/core/storage/event_log/sql_event_log.py
@@ -1163,9 +1163,10 @@ class SqlEventLogStorage(EventLogStorage):
 
         return materialization_count_by_partition
 
+
 def _get_from_row(row, column):
-    """ utility function for extracting a column from a sqlalchemy row proxy, since '_asdict' is not
-    supported in sqlalchemy 1.3 """
+    """utility function for extracting a column from a sqlalchemy row proxy, since '_asdict' is not
+    supported in sqlalchemy 1.3"""
     if not row.has_key(column):
         return None
     return row[column]
