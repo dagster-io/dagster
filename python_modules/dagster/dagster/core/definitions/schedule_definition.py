75d486b7e576b4778a63a9ba25c699a542aa0964
Sean Mackesey <s.mackesey@gmail.com>
2022-03-04 20:28:34 -0800

    pendulum.tz.timezone

--- python_modules/dagster/dagster/core/definitions/schedule_definition.py
+++ python_modules/dagster/dagster/core/definitions/schedule_definition.py
@@ -321,7 +321,7 @@ class ScheduleDefinition:
         if self._execution_timezone:
             try:
                 # Verify that the timezone can be loaded
-                pendulum.timezone(self._execution_timezone)
+                pendulum.tz.timezone(self._execution_timezone)
             except Exception as e:
                 raise DagsterInvalidDefinitionError(
                     f"Invalid execution timezone {self._execution_timezone} for {name}"
