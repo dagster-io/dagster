544432d996b012868ed526646f20a776e34773e2
Sean Mackesey <s.mackesey@gmail.com>
2022-03-11 07:28:53 -0800

    sort reqs

--- python_modules/libraries/dagstermill/setup.py
+++ python_modules/libraries/dagstermill/setup.py
@@ -38,15 +38,15 @@ if __name__ == "__main__":
             # https://github.com/nteract/papermill/issues/519,
             # https://github.com/ipython/ipykernel/issues/568
             "ipykernel>=4.9.0,!=5.4.0,!=5.4.1",
-            "scrapbook>=0.5.0",
-            "packaging>=20.5",
-            "papermill>=1.0.0",
             # See: https://github.com/mu-editor/mu/pull/1844
             # ipykernel<6 depends on ipython_genutils, but it isn't explicitly
             # declared as a dependency. It also depends on traitlets, which
             # incidentally brought ipython_genutils, but in v5.1 it was dropped, so as
             # a workaround we need to manually specify it here
             "ipython_genutils>=0.2.0",
+            "packaging>=20.5",
+            "papermill>=1.0.0",
+            "scrapbook>=0.5.0",
         ],
         extras_require={
             "test": [
