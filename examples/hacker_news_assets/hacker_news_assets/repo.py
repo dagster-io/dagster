be9073ee7eebb5a5df1576e6a997978380e83cc5
Sandy Ryza <sandy@elementl.com>
2022-03-11 14:21:43 -0800

    hn local repo that uses duckdb as warehouse

--- examples/hacker_news_assets/hacker_news_assets/repo.py
+++ examples/hacker_news_assets/hacker_news_assets/repo.py
@@ -1,9 +1,21 @@
 from dagster import repository, schedule_from_partitions
 
-from .assets import prod_assets, staging_assets
-from .jobs.activity_stats import activity_stats_prod_job, activity_stats_staging_job
-from .jobs.hacker_news_api_download import download_prod_job, download_staging_job
-from .jobs.story_recommender import story_recommender_prod_job, story_recommender_staging_job
+from .assets import local_assets, prod_assets, staging_assets
+from .jobs.activity_stats import (
+    activity_stats_local_job,
+    activity_stats_prod_job,
+    activity_stats_staging_job,
+)
+from .jobs.hacker_news_api_download import (
+    download_local_job,
+    download_prod_job,
+    download_staging_job,
+)
+from .jobs.story_recommender import (
+    story_recommender_local_job,
+    story_recommender_prod_job,
+    story_recommender_staging_job,
+)
 from .sensors.hn_tables_updated_sensor import make_hn_tables_updated_sensor
 from .sensors.slack_on_failure_sensor import make_slack_on_failure_sensor
 
@@ -28,3 +40,8 @@ def hacker_news_assets_staging():
         make_hn_tables_updated_sensor(activity_stats_staging_job),
         make_hn_tables_updated_sensor(story_recommender_staging_job),
     ]
+
+
+@repository
+def hacker_news_assets_local():
+    return [local_assets, download_local_job, activity_stats_local_job, story_recommender_local_job]
--- examples/hacker_news_assets/hacker_news_assets/resources/__init__.py
+++ examples/hacker_news_assets/hacker_news_assets/resources/__init__.py
@@ -8,7 +8,8 @@ from dagster import ResourceDefinition
 from dagster.utils import file_relative_path
 
 from .common_bucket_s3_pickle_io_manager import common_bucket_s3_pickle_io_manager
-from .hn_resource import hn_api_subsample_client, hn_snapshot_client
+from .duckdb_parquet_io_manager import duckdb_partitioned_parquet_io_manager
+from .hn_resource import hn_api_client, hn_api_subsample_client
 from .parquet_io_manager import (
     local_partitioned_parquet_io_manager,
     s3_partitioned_parquet_io_manager,
@@ -17,6 +18,9 @@ from .snowflake_io_manager import snowflake_io_manager
 
 DBT_PROJECT_DIR = file_relative_path(__file__, "../../hacker_news_dbt")
 DBT_PROFILES_DIR = DBT_PROJECT_DIR + "/config"
+dbt_local_resource = dbt_cli_resource.configured(
+    {"profiles_dir": DBT_PROFILES_DIR, "project_dir": DBT_PROJECT_DIR, "target": "local"}
+)
 dbt_staging_resource = dbt_cli_resource.configured(
     {"profiles-dir": DBT_PROFILES_DIR, "project-dir": DBT_PROJECT_DIR, "target": "staging"}
 )
@@ -76,8 +80,10 @@ RESOURCES_STAGING = {
 
 RESOURCES_LOCAL = {
     "parquet_io_manager": local_partitioned_parquet_io_manager,
-    "warehouse_io_manager": local_partitioned_parquet_io_manager,
+    "warehouse_io_manager": duckdb_partitioned_parquet_io_manager.configured(
+        {"duckdb_path": os.path.join(DBT_PROJECT_DIR, "hackernews.duckdb")},
+    ),
     "pyspark": configured_pyspark,
-    "hn_client": hn_snapshot_client,
-    "dbt": ResourceDefinition.none_resource(),
+    "hn_client": hn_api_client,
+    "dbt": dbt_local_resource,
 }
