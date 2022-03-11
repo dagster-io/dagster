c3a291d8691bdc0071801514898dd54ca3a15603
Sandy Ryza <sandy@elementl.com>
2022-03-11 11:46:28 -0800

    Move dbt metadata collection into snowflake io manager

--- examples/hacker_news_assets/hacker_news_assets/assets/activity_stats.py
+++ examples/hacker_news_assets/hacker_news_assets/assets/activity_stats.py
@@ -1,15 +1,9 @@
 import json
 import os
 
-import pandas as pd
 from dagster_dbt import dbt_cli_resource
 from dagster_dbt.asset_defs import load_assets_from_dbt_manifest
-from hacker_news_assets.resources.snowflake_io_manager import (
-    SHARED_SNOWFLAKE_CONF,
-    connect_snowflake,
-)
 
-from dagster import MetadataValue
 from dagster.utils import file_relative_path
 
 DBT_PROJECT_DIR = file_relative_path(__file__, "../../hacker_news_dbt")
@@ -22,18 +16,7 @@ dbt_prod_resource = dbt_cli_resource.configured(
 )
 
 
-def asset_metadata(_context, model_info):
-    config = dict(SHARED_SNOWFLAKE_CONF)
-    config["schema"] = model_info["schema"]
-    with connect_snowflake(config=config) as con:
-        df = pd.read_sql(f"SELECT * FROM {model_info['name']} LIMIT 5", con=con)
-        num_rows = con.execute(f"SELECT COUNT(*) FROM {model_info['name']}").fetchone()
-
-    return {"Data sample": MetadataValue.md(df.to_markdown()), "Rows": num_rows[0]}
-
-
 assets = load_assets_from_dbt_manifest(
     json.load(open(os.path.join(DBT_PROJECT_DIR, "target", "manifest.json"))),
-    runtime_metadata_fn=asset_metadata,
     io_manager_key="warehouse_io_manager",
 )
