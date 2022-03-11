24be050d52c21b8f3dbb153f007a196f1824f873
Johann Miller <johann@elementl.com>
2022-03-11 12:52:02 -0800

    Fault tolerance docs

--- examples/docs_snippets/docs_snippets_tests/deploying_tests/dagster_instance_tests/snapshots/snap_test_dagster_instance.py
+++ examples/docs_snippets/docs_snippets_tests/deploying_tests/dagster_instance_tests/snapshots/snap_test_dagster_instance.py
@@ -12,6 +12,7 @@ snapshots["test_instance_yaml 1"] = [
     "local_artifact_storage",
     "run_coordinator",
     "run_launcher",
+    "run_monitoring",
     "run_storage",
     "schedule_storage",
     "scheduler",
