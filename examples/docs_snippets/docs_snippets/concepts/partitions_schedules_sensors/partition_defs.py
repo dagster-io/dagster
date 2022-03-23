90265b459b389c105575280bc7315f6440b582da
JamieDeMaria <jamie@elementl.com>
2022-03-23 19:40:35 -0400

    add docs

--- examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/partition_defs.py
+++ examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/partition_defs.py
@@ -1,14 +1,108 @@
-# from dagster import (
-#     hourly_partitioned_config,
-#     daily_partitioned_config,
-#     weekly_partitioned_config,
-#     monthly_partitioned_config,
-# )
+from datetime import datetime
 
-# # start_hourly_partition_def
+from dagster import (
+    daily_partitioned_config,
+    hourly_partitioned_config,
+    monthly_partitioned_config,
+    weekly_partitioned_config,
+)
 
-# @hourly_partitioned_config(start_date=datetime(2022, 3, 20))
-# def my_hourly_partition_config():
+# start_hourly_partition_def
 
 
-# # end_hourly_partition_def
+@hourly_partitioned_config(start_date=datetime(2022, 3, 12))
+def my_hourly_partitioned_config(start: datetime, _end: datetime):
+    return {
+        "ops": {
+            "process_data_for_date": {"config": {"date": start.strftime("%Y-%m-%d")}}
+        }
+    }
+
+
+# end_hourly_partition_def
+
+# start_daily_partition_def
+
+
+@daily_partitioned_config(start_date="2022-03-12")
+def my_daily_partitioned_config(start: datetime, _end: datetime):
+    return {
+        "ops": {
+            "process_data_for_date": {"config": {"date": start.strftime("%Y-%m-%d")}}
+        }
+    }
+
+
+# end_daily_partition_def
+
+# start_weekly_partition_def
+
+
+@weekly_partitioned_config(start_date="2022-03-12")
+def my_weekly_partitioned_config(start: datetime, _end: datetime):
+    return {
+        "ops": {
+            "process_data_for_date": {"config": {"date": start.strftime("%Y-%m-%d")}}
+        }
+    }
+
+
+# end_weekly_partition_def
+
+# start_monthly_partition_def
+
+
+@monthly_partitioned_config(start_date="2022-03-12")
+def my_monthly_partitioned_config(start: datetime, _end: datetime):
+    return {
+        "ops": {
+            "process_data_for_date": {"config": {"date": start.strftime("%Y-%m-%d")}}
+        }
+    }
+
+
+# end_monthly_partition_def
+
+# start_hourly_offset_partition_def
+
+
+@hourly_partitioned_config(start_date=datetime(2022, 3, 12), minute_offset=15)
+def my_hourly_offset_partitioned_config(start: datetime, _end: datetime):
+    return {
+        "ops": {
+            "process_data_for_date": {"config": {"date": start.strftime("%Y-%m-%d")}}
+        }
+    }
+
+
+# end_hourly_offset_partition_def
+
+# start_weekly_offset_partition_def
+
+
+@weekly_partitioned_config(
+    start_date="2022-03-12", minute_offset=15, hour_offset=16, day_offset=3
+)
+def my_weekly_offset_partitioned_config(start: datetime, _end: datetime):
+    return {
+        "ops": {
+            "process_data_for_date": {"config": {"date": start.strftime("%Y-%m-%d")}}
+        }
+    }
+
+
+# end_weekly_offset_partition_def
+
+# start_monthly_offset_partition_def
+
+
+@monthly_partitioned_config(start_date="2022-03-12", day_offset=15)
+def my_monthly_offset_partitioned_config(start: datetime, _end: datetime):
+    return {
+        "ops": {
+            "process_data_for_date": {"config": {"date": start.strftime("%Y-%m-%d")}}
+        }
+    }
+
+
+# end_monthly_offset_partition_def
