e7267b1a2097cfc9ac6e115a8df83cc43526be65
Ben Gotow <ben@foundry376.com>
2022-03-25 16:05:06 -0500

    [dagit] Always collapse the left Jobs navigation by default

--- js_modules/dagit/packages/core/src/app/LayoutProvider.tsx
+++ js_modules/dagit/packages/core/src/app/LayoutProvider.tsx
@@ -1,22 +1,6 @@
 import * as React from 'react';
 import {useLocation} from 'react-router-dom';
 
-function useMatchMedia(query: string) {
-  const match = React.useRef(matchMedia(query));
-  const [result, setResult] = React.useState(match.current.matches);
-
-  React.useEffect(() => {
-    const matcher = match.current;
-    const onChange = () => setResult(matcher.matches);
-    matcher.addEventListener('change', onChange);
-    return () => {
-      matcher.removeEventListener('change', onChange);
-    };
-  }, [query]);
-
-  return result;
-}
-
 type LayoutContextValue = {
   nav: {
     isOpen: boolean;
@@ -38,8 +22,6 @@ export const LayoutContext = React.createContext<LayoutContextValue>({
 export const LayoutProvider: React.FC = (props) => {
   const [navOpen, setNavOpen] = React.useState(false);
   const location = useLocation();
-  const isSmallScreen = useMatchMedia('(max-width: 1440px)');
-  const isInstancePage = location.pathname.startsWith('/instance');
 
   React.useEffect(() => {
     setNavOpen(false);
@@ -49,12 +31,12 @@ export const LayoutProvider: React.FC = (props) => {
     () => ({
       nav: {
         isOpen: navOpen,
-        isCollapsible: isInstancePage || isSmallScreen,
+        isCollapsible: true,
         open: () => setNavOpen(true),
         close: () => setNavOpen(false),
       },
     }),
-    [navOpen, isInstancePage, isSmallScreen],
+    [navOpen],
   );
 
   return <LayoutContext.Provider value={value}>{props.children}</LayoutContext.Provider>;
