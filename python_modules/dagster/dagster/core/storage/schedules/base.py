592f56177975a8d684a74d62fcb20e40b29d59ed
prha <prha@elementl.com>
2022-03-25 13:31:07 -0700

    change base

--- python_modules/dagster/dagster/core/storage/schedules/base.py
+++ python_modules/dagster/dagster/core/storage/schedules/base.py
@@ -24,15 +24,17 @@ class ScheduleStorage(abc.ABC, MayHaveInstanceWeakref):
 
         Args:
             repository_origin_id (Optional[str]): The ExternalRepository target id to scope results to
+            repository_name (Optional[str]): The repository name to scope results to
             instigator_type (Optional[InstigatorType]): The InstigatorType to scope results to
         """
 
     @abc.abstractmethod
-    def get_instigator_state(self, origin_id: str) -> InstigatorState:
+    def get_instigator_state(self, origin_id: str, selector_id: str) -> InstigatorState:
         """Return the instigator state for the given id
 
         Args:
             origin_id (str): The unique instigator identifier
+            selector_id (str): The logical instigator identifier
         """
 
     @abc.abstractmethod
@@ -57,6 +59,7 @@ class ScheduleStorage(abc.ABC, MayHaveInstanceWeakref):
 
         Args:
             origin_id (str): The id of the instigator target to delete
+            selector_id (str): The logical instigator identifier
         """
 
     @property
@@ -65,7 +68,7 @@ class ScheduleStorage(abc.ABC, MayHaveInstanceWeakref):
 
     def get_batch_ticks(
         self,
-        origin_ids: Sequence[str],
+        selector_ids: Sequence[str],
         limit: Optional[int] = None,
         statuses: Optional[Sequence[TickStatus]] = None,
     ) -> Mapping[str, Iterable[InstigatorTick]]:
@@ -75,6 +78,7 @@ class ScheduleStorage(abc.ABC, MayHaveInstanceWeakref):
     def get_ticks(
         self,
         origin_id: str,
+        selector_id: str,
         before: Optional[float] = None,
         after: Optional[float] = None,
         limit: Optional[int] = None,
@@ -84,6 +88,7 @@ class ScheduleStorage(abc.ABC, MayHaveInstanceWeakref):
 
         Args:
             origin_id (str): The id of the instigator target
+            selector_id (str): The logical instigator identifier
         """
 
     @abc.abstractmethod
@@ -103,11 +108,12 @@ class ScheduleStorage(abc.ABC, MayHaveInstanceWeakref):
         """
 
     @abc.abstractmethod
-    def purge_ticks(self, origin_id: str, tick_status: TickStatus, before: float):
+    def purge_ticks(self, origin_id: str, selector_id: str, tick_status: TickStatus, before: float):
         """Wipe ticks for an instigator for a certain status and timestamp.
 
         Args:
             origin_id (str): The id of the instigator target to delete
+            selector_id (str): The logical instigator identifier
             tick_status (TickStatus): The tick status to wipe
             before (datetime): All ticks before this datetime will get purged
         """
