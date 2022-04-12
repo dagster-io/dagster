c8f1e3b55c795e4762477154827dff3d5ce09476
JamieDeMaria <jamie@elementl.com>
2022-04-12 10:02:39 -0400

    update

--- examples/tags_example/ops.py
+++ examples/tags_example/ops.py
@@ -1,38 +1,53 @@
-from dagster import (
-    op,
-    MetadataEntry,
-    MetadataValue,
-    AssetMaterialization,
-    AssetKey
-)
 from time import time
+
+from dagster import AssetKey, AssetMaterialization, MetadataEntry, MetadataValue, job, op
+
+###################
 
 
 @op
 def does_nothing():
     return None
 
+
+@job(metadata=[MetadataEntry.text(label="team", value="core", searchable=True)])
+def job_with_searchable_metadata():
+    does_nothing()
+
+
+###################
+
+
 @op(
     metdata=[
         MetadataEntry.text(label="team", value="core", searchable=True),
-        MetadataEntry("")
     ],
 )
 def op_with_definition_metadata():
     return None
 
 
+###################
+
+
 def get_dataframe():
     pass
 
+
 @op
-def op_with_runtime_metadata():
+def op_with_runtime_metadata(context):
     s = time()
     df = get_dataframe()
     yield AssetMaterialization(
         asset_key=AssetKey("my_table"),
-        metadata_entries=[
-            MetadataEntry.table_schema(label="my_table_schema", value=df.table_schema, searchable=False)
-        ]
+        metadata=[
+            MetadataEntry.table_schema(
+                label="my_table_schema", value=df.table_schema, searchable=False
+            )
+        ],
     )
-    yield MetadataEntry.float(label="duration", value=time() - s)
+    yield MetadataEntry.float(label="duration", value=time() - s, searchable=False)
+    # or potentially
+    context.log_metadata(
+        [MetadataEntry.float(label="duration", value=time() - s, searchable=False)]
+    )
