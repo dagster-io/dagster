0a456d9d549e403b2a37d272eaa0573b02f4f996
Sean Mackesey <s.mackesey@gmail.com>
2022-04-22 09:37:25 -0700

    use 3.25.0

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
                 "pytest==6.1.1",
                 "responses==0.10.*",
                 "snapshottest==0.6.0",
-                "tox==3.24.5",
+                "tox==3.25.0",
                 "tqdm==4.48.0",  # pylint crash 48.1+
                 "yamllint",
                 "flake8>=3.7.8",
