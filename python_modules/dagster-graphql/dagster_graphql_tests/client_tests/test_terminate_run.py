9520b9a3cbcda26f2bb771100cc321d0631fb180
javier162380 <javierllorente16@gmail.com>
2022-04-25 13:49:59 +0200

    testing exceptions using a different approach

--- python_modules/dagster-graphql/dagster_graphql_tests/client_tests/test_terminate_run.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/client_tests/test_terminate_run.py
@@ -18,32 +18,32 @@ def test_terminate_run_status_success(mock_client: MockClient):
 
 @python_client_test_suite
 def test_terminate_run_not_failure(mock_client: MockClient):
-    expected_result = "Unable to terminate run"
-    response = {"terminateRun": {"__typename": "TerminateRunFailure", "message": expected_result}}
+    error_type, error_message = "TerminateRunFailure", "Unable to terminate run"
+    response = {"terminateRun": {"__typename": "TerminateRunFailure", "message": error_message}}
     mock_client.mock_gql_client.execute.return_value = response
 
     with pytest.raises(DagsterGraphQLClientError) as e:
         mock_client.python_client.terminate_run(RUN_ID)
-        assert e.value == expected_result
+    assert e.value.args == (error_type, error_message)
 
 
 @python_client_test_suite
 def test_terminate_run_not_found(mock_client: MockClient):
-    expected_result = "Run Id foo not found"
-    response = {"terminateRun": {"__typename": "RunNotFoundError", "runId": expected_result}}
+    error_type, error_message = "RunNotFoundError", "Run Id foo not found"
+    response = {"terminateRun": {"__typename": "RunNotFoundError", "runId": error_message}}
 
     with pytest.raises(DagsterGraphQLClientError) as e:
         mock_client.mock_gql_client.execute.return_value = response
         mock_client.python_client.terminate_run(RUN_ID)
-        assert e.value == expected_result
+    assert e.value.args == (error_type, error_message)
 
 
 @python_client_test_suite
 def test_terminate_run_python_error(mock_client: MockClient):
-    expected_result = "Unable to terminate run"
-    response = {"terminateRun": {"__typename": "PythonError", "message": expected_result}}
+    error_type, error_message = "PythonError", "Unable to terminate run"
+    response = {"terminateRun": {"__typename": "PythonError", "message": error_message}}
 
     with pytest.raises(DagsterGraphQLClientError) as e:
         mock_client.mock_gql_client.execute.return_value = response
         mock_client.python_client.terminate_run(RUN_ID)
-        assert e.value  == expected_result
+    assert e.value.args == (error_type, error_message)
