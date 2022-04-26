00d1b83a27b8cc1eb098a2c9ef8427c7ef4c4982
Sean Mackesey <s.mackesey@gmail.com>
2022-04-26 13:52:48 -0700

    add ConfigSchemaType and adjust decorators

--- python_modules/dagster/dagster/config/config_schema.py
+++ python_modules/dagster/dagster/config/config_schema.py
@@ -1,3 +1,25 @@
+from typing import Dict, List, Type, Union
+from typing_extensions import TypeAlias
+
+from dagster.config.config_type import ConfigType
+from dagster.config.field import Field
+
+# Eventually, the below `ConfigSchemaType` should be renamed to `ConfigSchema` and the class
+# definition should be dropped. The reason we don't do this now is that sphinx autodoc doesn't
+# support type aliases, so there is no good way to gracefully attach a docstring to this and have it
+# show up in the docs. See: https://github.com/sphinx-doc/sphinx/issues/8934
+#
+# Unfortunately mypy doesn't support recursive types, which would be used to properly define the
+# List/Dict elements of this union: `Dict[str, ConfigSchema]`, `List[ConfigSchema]`.
+ConfigSchemaType: TypeAlias = Union[
+    Type[Union[bool, float, int, str]],
+    Type[Union[dict, list]],
+    ConfigType,
+    Field,
+    Dict[str, object],
+    List,
+]
+
 class ConfigSchema:
     """This is a placeholder type. Any time that it appears in documentation, it means that any of
     the following types are acceptable:
