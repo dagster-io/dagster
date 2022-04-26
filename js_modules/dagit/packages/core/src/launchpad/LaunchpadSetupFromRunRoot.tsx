22e0267d5e3ae7ed61b3b832b667cca50b124141
Isaac Hellendag <hellendag@users.noreply.github.com>
2022-04-26 11:00:10 -0700

    [dagit] Refactor ExecutionSessionStorage (#7559)

--- js_modules/dagit/packages/core/src/launchpad/LaunchpadSetupFromRunRoot.tsx
+++ js_modules/dagit/packages/core/src/launchpad/LaunchpadSetupFromRunRoot.tsx
@@ -62,7 +62,7 @@ const LaunchpadSetupFromRunAllowedRoot: React.FC<Props> = (props) => {
 
   useJobTitle(explorerPath, isJob);
 
-  const [storageData, onSave] = useExecutionSessionStorage(repoAddress.name, pipelineName);
+  const [storageData, onSave] = useExecutionSessionStorage(repoAddress, pipelineName);
 
   const {data, loading} = useQuery<ConfigForRunQuery, ConfigForRunQueryVariables>(
     CONFIG_FOR_RUN_QUERY,
