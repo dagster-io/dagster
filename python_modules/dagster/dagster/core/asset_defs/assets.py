17f9c1d5333b9aed53d15fe48b4fffa21e0094f8
Claire Lin <claire@elementl.com>
2022-04-27 14:55:32 -0700

    cherry-pick

--- python_modules/dagster/dagster/core/asset_defs/assets.py
+++ python_modules/dagster/dagster/core/asset_defs/assets.py
@@ -4,6 +4,7 @@ from dagster import check
 from dagster.core.definitions import NodeDefinition, OpDefinition
 from dagster.core.definitions.events import AssetKey
 from dagster.core.definitions.partition import PartitionsDefinition
+from dagster.core.errors import DagsterInvalidDefinitionError
 
 from .partition_mapping import PartitionMapping
 
@@ -42,6 +43,24 @@ class AssetsDefinition:
 
     def __call__(self, *args, **kwargs):
         return self._node_def(*args, **kwargs)
+
+    @staticmethod
+    def from_graph(
+        graph_def: GraphDefinition,
+        asset_keys_by_input_name: Mapping[str, AssetKey],
+        asset_keys_by_output_name: Mapping[str, AssetKey],
+        internal_asset_deps: Optional[Mapping[str, SetAssetKey]] = None,
+    ):
+        AssetsDefinition(
+            asset_keys_by_input_name=_infer_asset_keys_by_input_names(
+                graph_def,
+                asset_keys_by_input_name or {},
+            ),
+            asset_keys_by_output_name=_infer_asset_keys_by_output_names(
+                graph_def, asset_keys_by_output_name or {}
+            ),
+            op=graph_def,  # TODO: change this arg name to node_def once dependent PR merged
+        )
 
     @property
     def op(self) -> OpDefinition:
@@ -83,3 +102,53 @@ class AssetsDefinition:
             in_asset_key,
             self._partitions_def.get_default_partition_mapping(),
         )
+
+
+def _infer_asset_keys_by_input_names(
+    graph_def: GraphDefinition, asset_keys_by_input_name: Mapping[str, AssetKey]
+) -> Mapping[str, AssetKey]:
+    all_input_names = {graph_input.definition.name for graph_input in graph_def.input_mappings}
+    for in_key in asset_keys_by_input_name.keys():
+        if in_key not in all_input_names:
+            raise DagsterInvalidDefinitionError(
+                f"Key '{in_key}' in provided asset_keys_by_input_name dict does not correspond to "
+                "any key provided in the ins dictionary of the decorated op or any argument "
+                "to the decorated function"
+            )
+
+    # If asset key is not supplied in asset_keys_by_input_name, create asset key
+    # from input name
+    inferred_input_names_by_asset_key: Dict[str, AssetKey] = {
+        input_name: asset_keys_by_input_name.get(input_name, AssetKey([input_name]))
+        for input_name in all_input_names
+    }
+
+    return inferred_input_names_by_asset_key
+
+
+def _infer_asset_keys_by_output_names(
+    graph_def: GraphDefinition, asset_keys_by_output_name: Mapping[str, AssetKey]
+) -> Mapping[str, AssetKey]:
+    inferred_asset_keys_by_output_names: Dict[AssetKey, str] = asset_keys_by_output_name.copy()
+    output_names = [output.definition.name for output in graph_def.output_mappings]
+
+    if (
+        len(output_names) == 1
+        and output_names[0] not in asset_keys_by_output_name
+        and output_names[0] == "result"
+    ):
+        # If there is only one output and the name is the default "result", generate asset key
+        # from the name of the node
+        inferred_asset_keys_by_output_names[output_names[0]] = AssetKey([node_def.name])
+
+    for output_name in asset_keys_by_output_name.keys():
+        if output_name not in output_names:
+            raise DagsterInvalidDefinitionError(
+                f"Key {output_name} in provided asset_keys_by_output_name does not correspond "
+                "to any key provided in the out dictionary of the decorated op"
+            )
+
+    for output_name in output_names:
+        if output_name not in inferred_asset_keys_by_output_names:
+            inferred_asset_keys_by_output_names[output_name] = AssetKey([output_name])
+    return inferred_asset_keys_by_output_names
