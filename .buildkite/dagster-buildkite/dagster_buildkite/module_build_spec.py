8ab7bfb4923d3f8cca757f509c6206e7aa8578cb
Sean Mackesey <s.mackesey@gmail.com>
2022-04-28 14:41:09 -0700

    enable mypy checking on dagit (#7626)

--- .buildkite/dagster-buildkite/dagster_buildkite/module_build_spec.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/module_build_spec.py
@@ -5,7 +5,6 @@ from .step_builder import StepBuilder
 from .utils import get_python_versions_for_branch
 
 MYPY_EXCLUDES = [
-    "python_modules/dagit",
     "python_modules/automation",
     "python_modules/libraries/dagster-databricks",
     "python_modules/libraries/dagster-dbt",
