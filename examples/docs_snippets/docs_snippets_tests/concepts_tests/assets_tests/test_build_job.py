ebc4295b51a2ab4f9efb04ecac735b498fa2b5c3
Sandy Ryza <sandy@elementl.com>
2022-05-14 16:03:32 -0700

    asset concepts page without AssetGroups

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_build_job.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_build_job.py
@@ -1,11 +1,6 @@
-from docs_snippets.concepts.assets.build_job import (
-    all_assets,
-    downstream_assets,
-    upstream_and_downstream_1,
-)
+from docs_snippets.concepts.assets.build_job import repo
 
 
-def test_jobs():
-    all_assets.execute_in_process()
-    downstream_assets.execute_in_process()
-    upstream_and_downstream_1.execute_in_process()
+def test():
+    assert repo.get_job("all_assets_job").execute_in_process().success
+    assert repo.get_job("asset1_job").execute_in_process().success
