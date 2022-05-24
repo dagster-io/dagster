dbdf830cc2202e73f42f709fa0d5b76196154f37
Claire Lin <claire@elementl.com>
2022-05-24 16:01:49 -0700

    add source assets

--- python_modules/dagster/dagster_tests/core_tests/asset_defs_tests/test_assets_job.py
+++ python_modules/dagster/dagster_tests/core_tests/asset_defs_tests/test_assets_job.py
@@ -1173,3 +1173,31 @@ def test_raise_error_on_incomplete_graph_asset_subset():
     with instance_for_test() as instance:
         with pytest.raises(DagsterInvalidSubsetError, match="complicated_graph"):
             job.execute_in_process(instance=instance, asset_selection=[AssetKey("comments_table")])
+
+
+def test_subset_with_source_asset():
+    class MyIOManager(IOManager):
+        def handle_output(self, context, obj):
+            pass
+
+        def load_input(self, context):
+            return 5
+
+    @io_manager
+    def the_manager():
+        return MyIOManager()
+
+    my_source_asset = SourceAsset(key=AssetKey("my_source_asset"), io_manager_key="the_manager")
+
+    @asset
+    def my_derived_asset(my_source_asset):
+        return my_source_asset + 4
+
+    source_asset_job = AssetGroup(
+        assets=[my_derived_asset],
+        source_assets=[my_source_asset],
+        resource_defs={"the_manager": the_manager},
+    ).build_job("source_asset_job")
+
+    result = source_asset_job.execute_in_process(asset_selection=[AssetKey("my_derived_asset")])
+    assert result.success
