5e8c2f5f375f6c68b3323a7714b57a452e453418
gibsondan <daniel@elementl.com>
2022-05-24 14:09:49 -0500

    Load all code artifacts on grpc server startup, not just pipelines/jobs
    Summary:
    User reported a race condition with partition set loading - we have logic in place to prevent this for jobs/pipelines, but not for the other code artifacts. Fix that by adding a new method that pulls in any lazy code artifacts at grpc server startup time.
    
    Test Plan:
    Add log outputs to validate that partition sets are loaded as soon as the grpc server is started up

--- python_modules/dagster/dagster/grpc/server.py
+++ python_modules/dagster/dagster/grpc/server.py
@@ -111,8 +111,10 @@ class LoadedRepositories:
                 entry_point=entry_point,
             )
             repo_def = recon_repo.get_definition()
-            # force load of all lazy constructed jobs/pipelines
-            repo_def.get_all_pipelines()
+            # force load of all lazy constructed code artifacts to prevent
+            # any thread-safety issues loading them later on when serving
+            # definitions from multiple threads
+            repo_def.load_all_definitions()
 
             self._code_pointers_by_repo_name[repo_def.name] = pointer
             self._recon_repos_by_name[repo_def.name] = recon_repo
