c7d8878cba46c58168bd09cad1e3997bcf94f9cd
Erin Cochran <erin.k.cochran@gmail.com>
2022-06-01 16:49:05 -0400

    Run scripts

--- examples/docs_snippets/docs_snippets/concepts/assets/asset_config.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_config.py
@@ -1,12 +1,13 @@
 from dagster import asset
 
-
 # start_example
+
 
 @asset(config_schema={"api_endpoint": str})
 def my_configurable_asset(context):
     api_endpoint = context.op_config["api_endpoint"]
     data = requests.get(f"{api_endpoint}/data").json()
     return data
+
 
 # end_example
