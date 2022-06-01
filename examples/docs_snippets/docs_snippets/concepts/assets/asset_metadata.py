5e54f0a31aff85326a1ee5303cdcbe012fc1560e
Erin Cochran <erin.k.cochran@gmail.com>
2022-06-01 11:36:23 -0400

    Update example, run snapshot

--- examples/docs_snippets/docs_snippets/concepts/assets/asset_metadata.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_metadata.py
@@ -1,30 +1,7 @@
-# isort: skip_file
-# pylint: disable=unused-argument,reimported,unnecessary-ellipsis
-
-# start_simple_metadata
-
 from dagster import asset
 
-@asset(
-    metadata={
-        "cereal_name": "Sugar Sprinkles"
-    },
-)
-
-# end_simple_metadata
-
-
-# start_typed_metadata
-
-from dagster import asset, MetadataValue
-
-@asset(
-    metadata={
-        "cereal_name": "Sugar Sparkles",
-        "description": "Sparkly, sugary delight!",
-        "path": MetadataValue.path("/sugar_sparkles"),
-        "sales_dashboard_url": MetadataValue.url("http://mycoolsite.com/sugar_sparkle_sales")
-    },
-)
-
-# end_typed_metadata
+# start_example
+@asset(metadata={"cereal_name": "Sugar Sprinkles"})
+def cereal_asset():
+    return 5
+# end_example
