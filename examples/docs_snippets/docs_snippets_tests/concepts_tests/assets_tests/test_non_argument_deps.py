28a28f1519a4fb6dd5cfe3c9336c9409f259693a
Erin Cochran <erin.k.cochran@gmail.com>
2022-06-01 13:19:45 -0400

    Run formatting scripts

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_non_argument_deps.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_non_argument_deps.py
@@ -1,5 +1,8 @@
 from dagster import AssetGroup
-from docs_snippets.concepts.assets.non_argument_deps import upstream_asset, downstream_asset
+from docs_snippets.concepts.assets.non_argument_deps import (
+    downstream_asset,
+    upstream_asset,
+)
 
 
 def test_non_argument_deps():
