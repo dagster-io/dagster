1c70d51eae4955e062aabcd96ceb79836bd52623
Erin Cochran <erin.k.cochran@gmail.com>
2022-06-02 11:33:44 -0400

    Addressing review comments

--- examples/docs_snippets/docs_snippets/concepts/assets/explicit_asset_dependency.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/explicit_asset_dependency.py
@@ -1,6 +1,7 @@
 from dagster import AssetIn, asset
 
 
+# start_string_example
 @asset
 def upstream_asset():
     return [1, 2, 3]
@@ -9,3 +10,27 @@ def upstream_asset():
 @asset(ins={"upstream": AssetIn("upstream_asset")})
 def downstream_asset(upstream):
     return upstream + [4]
+
+
+# end_string_example
+
+
+# start_explicit_asset_key_example
+
+
+@asset
+def upstream_asset():
+    return [1, 2, 3]
+
+
+@asset(ins={"upstream": AssetIn(asset_key="upstream_asset")})
+def downstream_asset(upstream):
+    return upstream + [4]
+
+
+@asset(ins={"upstream": AssetIn(asset_key=AssetKey("upstream_asset"))})
+def another_downstream_asset(upstream):
+    return upstream + [10]
+
+
+# end_explicit_asset_key_example
--- examples/docs_snippets/docs_snippets/concepts/assets/source_asset.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/source_asset.py
@@ -2,14 +2,14 @@
 # start_marker
 from dagster import AssetGroup, AssetKey, SourceAsset, asset
 
-my_source_asset = SourceAsset(key=AssetKey("my_source_asset"))
+my_source_asset = SourceAsset(key=AssetKey("a_source_asset"))
 
 
 @asset
-def my_derived_asset(my_source_asset):
-    return my_source_asset + [4]
+def my_derived_asset(a_source_asset):
+    return a_source_asset + [4]
 
 
-asset_group = AssetGroup(assets=[my_derived_asset], source_assets=[my_source_asset])
+asset_group = AssetGroup(assets=[my_derived_asset], source_assets=[a_source_asset])
 
 # end_marker
