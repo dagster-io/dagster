c8e5fa4642b410e857c7272b12a8cc4ed4d9cbf3
Isaac Hellendag <hellendag@users.noreply.github.com>
2022-06-03 13:20:32 -0500

    [dagit] Make the left nav stay open/closed (#8173)
    ### Summary & Motivation
    
    Modify the logic and behavior for rendering the left nav as open or closed.
    
    - Remove the flag and any route-based logic for hiding the nav.
    - If the user is using a small viewport:
      - The hamburger button will open and close the nav. Same as before.
      - When navigating, the nav will close.
    - If the user is using a large viewport:
      - The hamburger button is now present, and shows/hides the nav. The main contents are rendered next to the nav, not beneath it.
      - There is no animation for opening/closing the nav.
      - The nav will not hide when navigating.
      - Open/closed state is tracked in localStorage. This way, the user's preference is tracked across sessions.
      - If the user switches to a small viewport, the large viewport preference is saved, but behavior will go back to small viewport.
      - If the user switches back to a large viewport, the large viewport preference is used.
    
    ### How I Tested These Changes
    
    Test the situations described above. Verify that the nav behaves as expected, and writes to localStorage properly.

--- js_modules/dagit/packages/core/src/app/LayoutProvider.tsx
+++ js_modules/dagit/packages/core/src/app/LayoutProvider.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {useLocation} from 'react-router-dom';
 
-import {useFeatureFlags} from './Flags';
+import {useStateWithStorage} from '../hooks/useStateWithStorage';
 
 function useMatchMedia(query: string) {
   const match = React.useRef(matchMedia(query));
@@ -22,7 +22,7 @@ function useMatchMedia(query: string) {
 type LayoutContextValue = {
   nav: {
     isOpen: boolean;
-    isCollapsible: boolean;
+    isSmallScreen: boolean;
     open: () => void;
     close: () => void;
   };
@@ -31,34 +31,59 @@ type LayoutContextValue = {
 export const LayoutContext = React.createContext<LayoutContextValue>({
   nav: {
     isOpen: false,
-    isCollapsible: false,
+    isSmallScreen: false,
     open: () => {},
     close: () => {},
   },
 });
 
+const STORAGE_KEY = 'large-screen-nav-open';
+
 export const LayoutProvider: React.FC = (props) => {
-  const [navOpen, setNavOpen] = React.useState(false);
-  const flags = useFeatureFlags();
+  const [navOpenIfLargeScreen, setNavOpenIfLargeScreen] = useStateWithStorage(
+    STORAGE_KEY,
+    (json: any) => {
+      if (typeof json !== 'boolean') {
+        return false;
+      }
+      return json;
+    },
+  );
+
+  const [navOpenIfSmallScreen, setNavOpenIfSmallScreen] = React.useState(false);
   const location = useLocation();
   const isSmallScreen = useMatchMedia('(max-width: 1440px)');
-  const isInstancePage = location.pathname.startsWith('/instance');
-  const isCollapsible = flags.flagAlwaysCollapseNavigation || isInstancePage || isSmallScreen;
+
+  const open = React.useCallback(() => {
+    setNavOpenIfSmallScreen(true);
+    if (!isSmallScreen) {
+      setNavOpenIfLargeScreen(true);
+    }
+  }, [isSmallScreen, setNavOpenIfLargeScreen]);
+
+  const close = React.useCallback(() => {
+    setNavOpenIfSmallScreen(false);
+    if (!isSmallScreen) {
+      setNavOpenIfLargeScreen(false);
+    }
+  }, [isSmallScreen, setNavOpenIfLargeScreen]);
 
   React.useEffect(() => {
-    setNavOpen(false);
+    setNavOpenIfSmallScreen(false);
   }, [location]);
+
+  const isOpen = isSmallScreen ? navOpenIfSmallScreen : navOpenIfLargeScreen;
 
   const value = React.useMemo(
     () => ({
       nav: {
-        isOpen: navOpen,
-        isCollapsible,
-        open: () => setNavOpen(true),
-        close: () => setNavOpen(false),
+        isOpen,
+        isSmallScreen,
+        open,
+        close,
       },
     }),
-    [navOpen, isCollapsible],
+    [isOpen, isSmallScreen, open, close],
   );
 
   return <LayoutContext.Provider value={value}>{props.children}</LayoutContext.Provider>;
