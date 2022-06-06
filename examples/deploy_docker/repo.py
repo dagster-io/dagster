e5792fafa77ae7a3f931ecfdad5cf03130a52286
gibsondan <daniel@elementl.com>
2022-06-06 13:37:55 -0500

    Add docker executor example to deploy_docker
    Summary:
    I needed to test the docker executor, and a user just asked about how to do this with the deploy_docker example.
    
    Test Plan: Bk, run deploy_docker example locally

--- examples/deploy_docker/repo.py
+++ examples/deploy_docker/repo.py
@@ -1,4 +1,6 @@
-from dagster import job, op, repository, schedule
+from dagster_docker import docker_executor
+
+from dagster import fs_io_manager, graph, op, repository, schedule
 
 
 @op
@@ -6,9 +8,25 @@ def hello():
     return 1
 
 
-@job
-def my_job():
-    hello()
+@op
+def goodbye(foo):
+    if foo != 1:
+        raise Exception("Bad io manager")
+    return foo * 2
+
+
+@graph
+def my_graph():
+    goodbye(hello())
+
+
+my_job = my_graph.to_job(name="my_job")
+
+my_step_isolated_job = my_graph.to_job(
+    name="my_step_isolated_job",
+    executor_def=docker_executor,
+    resource_defs={"io_manager": fs_io_manager.configured({"base_dir": "/tmp/io_manager_storage"})},
+)
 
 
 @schedule(cron_schedule="* * * * *", job=my_job, execution_timezone="US/Central")
@@ -18,4 +36,4 @@ def my_schedule(_context):
 
 @repository
 def deploy_docker_repository():
-    return [my_job, my_schedule]
+    return [my_job, my_step_isolated_job, my_schedule]
