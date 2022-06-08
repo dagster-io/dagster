acc0f9bc4eeb1e4b38aa749ae8f2af71da4f44eb
Erin Cochran <erin.k.cochran@gmail.com>
2022-06-08 12:15:26 -0400

    Split example into two files; rerun snapshot

--- examples/docs_snippets/docs_snippets/concepts/assets/explicit_asset_dependency_asset_keys.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/explicit_asset_dependency_asset_keys.py
@@ -1,22 +1,5 @@
 from dagster import AssetIn, AssetKey, asset
 
-
-# start_string_example
-@asset
-def upstream_asset():
-    return [1, 2, 3]
-
-
-@asset(ins={"upstream": AssetIn("upstream_asset")})
-def downstream_asset(upstream):
-    return upstream + [4]
-
-
-# end_string_example
-
-
-# start_explicit_asset_key_example
-
 # One way of providing explicit asset keys:
 
 
@@ -31,6 +14,3 @@ def downstream_asset(upstream):
 @asset(ins={"upstream": AssetIn(asset_key=AssetKey("upstream_asset"))})
 def another_downstream_asset(upstream):
     return upstream + [10]
-
-
-# end_explicit_asset_key_example
