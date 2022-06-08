ff3392a5478de28abedd369893d8af695a110e1a
gibsondan <daniel@elementl.com>
2022-06-08 16:34:33 -0500

    add an "environment" key to EcsRunLauncher / EcsContainerContext that sets env vars (#8243)
    Summary:
    Lets you pass in a set of key-value pairs to be injected into the container.

---
+++ python_modules/dagster/dagster_tests/core_tests/test_utils.py
@@ -0,0 +1,28 @@
+import pytest
+
+from dagster.core.test_utils import environ
+from dagster.core.utils import parse_env_var
+
+
+def test_parse_env_var_no_equals():
+
+    env_var = "FOO_ENV_VAR"
+
+    with pytest.raises(
+        Exception, match="Tried to load environment variable FOO_ENV_VAR, but it was not set"
+    ):
+        parse_env_var(env_var)
+
+    with environ({"FOO_ENV_VAR": "BAR_VALUE"}):
+        assert parse_env_var(env_var) == ("FOO_ENV_VAR", "BAR_VALUE")
+
+
+def test_parse_env_var_equals():
+    env_var = "FOO_ENV_VAR=BAR_VALUE"
+    assert parse_env_var(env_var) == ("FOO_ENV_VAR", "BAR_VALUE")
+
+
+def test_parse_env_var_containing_equals():
+    env_var = "FOO_ENV_VAR=HERE_COMES_THE_EQUALS=THERE_IT_WENT"
+
+    assert parse_env_var(env_var) == ("FOO_ENV_VAR", "HERE_COMES_THE_EQUALS=THERE_IT_WENT")
