2e0680b5bc44ab3959105188a89ebc130f1fcf0a
yuhan <yuhan@elementl.com>
2022-06-08 14:03:50 -0700

    switch back to prod index name before landing

--- docs/next/components/Search.tsx
+++ docs/next/components/Search.tsx
@@ -119,7 +119,7 @@ export function Search() {
               distinct: 1,
             }}
             onClose={onClose}
-            indexName="test_oss_docs"
+            indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME}
             apiKey={process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY}
             appId={process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}
             navigator={{
