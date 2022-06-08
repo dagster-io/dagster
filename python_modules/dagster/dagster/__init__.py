ca009fc56f269076da1c40fba5b8095e04a111a5
Christopher DeCarolis <cdguitar817@gmail.com>
2022-06-08 13:17:13 -0700

    with_resources top level export (#8264)

--- python_modules/dagster/dagster/__init__.py
+++ python_modules/dagster/dagster/__init__.py
@@ -14,7 +14,6 @@ sys.meta_path.insert(
         }
     ),
 )
-
 from dagster.builtins import Any, Bool, Float, Int, Nothing, String
 from dagster.config import Enum, EnumValue, Field, Map, Permissive, Selector, Shape
 from dagster.config.config_schema import ConfigSchema
@@ -227,6 +226,7 @@ from dagster.core.execution.results import (
     SolidExecutionResult,
 )
 from dagster.core.execution.validate_run_config import validate_run_config
+from dagster.core.execution.with_resources import with_resources
 from dagster.core.executor.base import Executor
 from dagster.core.executor.init import InitExecutorContext
 from dagster.core.instance import DagsterInstance
@@ -489,6 +489,7 @@ __all__ = [
     "OpExecutionContext",
     "PipelineExecutionResult",
     "RetryRequested",
+    "with_resources",
     "build_resources",
     "SolidExecutionResult",
     "SolidExecutionContext",
