b9de066928abab27ba0af07afdba99ab3a2a87ea
Johann Miller <johann@elementl.com>
2022-06-09 10:13:27 -0400

    Add max_retries to user editable tags (#8285)

--- python_modules/dagster/dagster/core/storage/tags.py
+++ python_modules/dagster/dagster/core/storage/tags.py
@@ -41,7 +41,9 @@ PRIORITY_TAG = "{prefix}priority".format(prefix=SYSTEM_TAG_PREFIX)
 
 DOCKER_IMAGE_TAG = "{prefix}image".format(prefix=SYSTEM_TAG_PREFIX)
 
-USER_EDITABLE_SYSTEM_TAGS = [PRIORITY_TAG]
+MAX_RETRIES_TAG = "{prefix}max_retries".format(prefix=SYSTEM_TAG_PREFIX)
+
+USER_EDITABLE_SYSTEM_TAGS = [PRIORITY_TAG, MAX_RETRIES_TAG]
 
 
 class TagType(Enum):
