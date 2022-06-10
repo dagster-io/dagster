6c12910be3284a00e1f60b3a5f2bd495622011ee
Erin Cochran <erin.k.cochran@gmail.com>
2022-06-10 03:07:16 -0400

    Consolidate examples

--- examples/docs_snippets/docs_snippets/guides/dagster/asset_tutorial/cereal.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/asset_tutorial/cereal.py
@@ -1,5 +1,10 @@
 """isort:skip_file"""
 
+import csv
+import requests
+from dagster import asset
+
+
 # start_asset_marker
 import csv
 import requests
@@ -16,6 +21,27 @@ def cereals():
 
 
 # end_asset_marker
+
+# start_multiple_assets
+import csv
+import requests
+from dagster import asset
+
+
+@asset
+def cereals():
+    response = requests.get("https://docs.dagster.io/assets/cereal.csv")
+    lines = response.text.split("\n")
+    return [row for row in csv.DictReader(lines)]
+
+
+@asset
+def nabisco_cereals(cereals):
+    """Cereals manufactured by Nabisco"""
+    return [row for row in cereals if row["mfr"] == "N"]
+
+
+# end_multiple_assets
 
 # start_materialize_marker
 from dagster import materialize
