b1f1855d93f0cd05b61ac250c3b539e7b605fb37
Erin Cochran <erin.k.cochran@gmail.com>
2022-06-10 02:47:20 -0400

    Fixing tests

--- examples/docs_snippets/docs_snippets/intro_tutorial/basics/materialize_cereal.py
+++ examples/docs_snippets/docs_snippets/intro_tutorial/basics/materialize_cereal.py
@@ -1,4 +1,14 @@
+from doc_snippets.guides.dagster.asset_tutorial.serial_asset_graph import (
+    cereals,
+    nabisco_cereals,
+)
+
+# start_example
+
 from dagster import materialize
+
 
 if __name__ == "__main__":
     materialize([cereals, nabisco_cereals])
+
+# end_example
