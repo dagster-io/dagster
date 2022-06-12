de07a17490e0e3ca8f58525a8547190ac728128e
Rex Ledesma <rex@elementl.com>
2022-06-12 00:48:55 -0700

    test(graphql): ensure type names do not leak graphene

--- python_modules/dagster-graphql/dagster_graphql_tests/test_schema.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/test_schema.py
@@ -1,11 +1,37 @@
-from dagster_graphql.schema import types
+import json
+
+import pytest
+from click.testing import CliRunner
+from dagster_graphql.cli import ui
 
 
-def test_schema_naming_invariant():
-    violations = []
-    for type_ in types():
-        if type_.__name__ != f"Graphene{type_._meta.name}":  # pylint: disable=protected-access
-            violations.append(
-                (type_.__name__, f"Graphene{type_._meta.name}")  # pylint: disable=protected-access
-            )
-    assert not violations, violations
+@pytest.fixture(name="runner")
+def runner_fixture():
+    yield CliRunner()
+
+
+def test_schema_type_names_without_graphene(runner):
+    query = """
+    query GetSchemaTypeNames {
+        __schema {
+            types {
+                name
+            }
+        }
+    }
+    """
+
+    result = runner.invoke(
+        ui,
+        ["--empty-workspace", "--ephemeral-instance", "-t", query],
+    )
+    graphql_schema_types = json.loads(result.output)["data"]["__schema"]["types"]
+
+    violations = [
+        graphql_type["name"]
+        for graphql_type in graphql_schema_types
+        if "Graphene".casefold() in graphql_type["name"].casefold()
+    ]
+    assert (
+        not violations
+    ), f"'Graphene' cannot be included in the GraphQL type name. Violating types: {violations}."
