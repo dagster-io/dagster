c0a4864da4ec61af189ec7599389902c5c636668
salazarm <salazarm@users.noreply.github.com>
2022-06-13 12:01:48 -0400

    Update Table.tsx

--- js_modules/dagit/packages/ui/src/components/Table.tsx
+++ js_modules/dagit/packages/ui/src/components/Table.tsx
@@ -14,11 +14,6 @@ export const Table = styled(HTMLTable)<TableProps>`
   border: none;
   width: 100%;
 
-  & tr th,
-  & tr td {
-    box-shadow: inset 0 1px 0 ${Colors.KeylineGray}, inset 1px 0 0 ${Colors.KeylineGray} !important;
-  }
-
   & tr th {
     color: ${Colors.Gray500};
     font-family: ${FontFamily.default};
