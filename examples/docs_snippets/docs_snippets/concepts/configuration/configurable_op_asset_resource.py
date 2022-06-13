0a58c12edf266d8d0c4ebc8a0be2d0024489fe71
Sean Mackesey <s.mackesey@gmail.com>
2022-06-13 19:02:46 -0400

    update to config docs to use asset

---
+++ examples/docs_snippets/docs_snippets/concepts/configuration/configurable_op_asset_resource.py
@@ -0,0 +1,26 @@
+from dagster import asset, op, resource
+
+
+class MyDatabaseConnection:
+    def __init__(self, url):
+        self.url = url
+
+
+# start_marker
+@op(config_schema={"person_name": str})
+def op_using_config(context):
+    return f'hello {context.op_config["person_name"]}'
+
+
+@asset(config_schema={"person_name": str})
+def asset_using_config(context):
+    # Note how asset config is also accessed with context.op_config
+    return f'hello {context.op_config["person_name"]}'
+
+
+@resource(config_schema={"url": str})
+def resource_using_config(context):
+    return MyDatabaseConnection(context.resource_config["url"])
+
+
+# end_marker
