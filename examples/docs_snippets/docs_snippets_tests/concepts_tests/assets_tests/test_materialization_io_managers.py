531e5daebc344f7a05a0ff3767c9eb287a2261c5
Sandy Ryza <sandy@elementl.com>
2022-06-14 12:13:50 -0700

    Fix problems from "remove old asset lineage from docs (#8382)" (#8389)

---
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_materialization_io_managers.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+from dagster import ModeDefinition, execute_pipeline, io_manager, pipeline, solid
+from docs_snippets.concepts.assets.materialization_io_managers import (
+    PandasCsvIOManager,
+    PandasCsvIOManagerWithAsset,
+)
+
+
+class DummyClass(pd.DataFrame):
+    def to_csv(self, _path):  # pylint: disable=arguments-differ
+        return
+
+
+def _generate_pipeline_for_io_manager(manager, config_schema=None):
+    @io_manager(output_config_schema=config_schema or {})
+    def custom_io_manager(_):
+        return manager
+
+    @solid
+    def dummy_solid():
+        return DummyClass.from_dict({"some_column": [2]})
+
+    @pipeline(
+        mode_defs=[ModeDefinition(resource_defs={"io_manager": custom_io_manager})]
+    )
+    def dummy_pipeline():
+        dummy_solid()
+
+    return dummy_pipeline
+
+
+def test_pipelines_compile_and_execute():
+    managers = [
+        PandasCsvIOManager(),
+        PandasCsvIOManagerWithAsset(),
+    ]
+    for manager in managers:
+        result = execute_pipeline(_generate_pipeline_for_io_manager(manager))
+        assert result
+        assert result.success
