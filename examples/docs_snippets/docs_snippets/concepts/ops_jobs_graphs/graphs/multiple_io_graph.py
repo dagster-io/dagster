13b87355b70a39c8caede7b614446f992cc72b4a
Erin Cochran <erin.k.cochran@gmail.com>
2022-06-14 12:19:51 -0400

    [docs] - New Jobs & Graphs structure [CON-289] (#8035)
    * First pass at job page restructuring
    
    * Update sidenav
    
    * Another pass at job page clean up
    
    * Run snapshot
    
    * Fix issue from snapshot
    
    * Placeholder graph page
    
    * Remove subsections from sidenav
    
    * Moving things around
    
    * First pass at graph page structure
    
    * Move content from Jobs to Graphs
    
    * Moving more content over to Graphs
    
    * Moving nesting and dynamic graphs around
    
    * `jobs-graphs.mdx` > `jobs.mdx`
    
    * Update sidenav for new jobs page
    
    * Update links to go to the right places
    
    * Update redirect
    
    * Updating examples and tests for graphs
    
    * Another pass at graph content
    
    * Clean up dependencies section
    
    * Add copy for graph-backed assets
    
    * Run snapshot
    
    * Add assets link to jobs intro
    
    * Move SDA jobs to the SDA page
    
    * First pass at graph intro
    
    * Add copy to Graph Jobs section
    
    * Add copy to other pattern sections
    
    * Running snapshot
    
    * Update docs/content/concepts/ops-jobs-graphs/jobs.mdx
    
    * Update docs/content/concepts/ops-jobs-graphs/jobs.mdx
    
    * Review comments
    
    * Fix link
    
    * Fixing links
    
    * Review comment for partitioned jobs
    
    * Fix links
    
    * Cleaning up SDA
    
    * Fix link and snapshot
    
    * Remove broken/unused examples from dbt guide
    
    * Revert "Remove broken/unused examples from dbt guide"
    
    This reverts commit 5c05fb634163df3e9cb85be328915da3748c6739.

---
+++ examples/docs_snippets/docs_snippets/concepts/ops_jobs_graphs/graphs/multiple_io_graph.py
@@ -0,0 +1,30 @@
+# pylint: disable=unused-argument, no-value-for-parameter
+
+# start_marker
+from dagster import graph, op
+
+
+@op
+def return_one(context) -> int:
+    return 1
+
+
+@op
+def add_one(context, number: int):
+    return number + 1
+
+
+@op
+def adder(context, a: int, b: int) -> int:
+    return a + b
+
+
+@graph
+def inputs_and_outputs():
+    value = return_one()
+    a = add_one(value)
+    b = add_one(value)
+    adder(a, b)
+
+
+# end_marker
