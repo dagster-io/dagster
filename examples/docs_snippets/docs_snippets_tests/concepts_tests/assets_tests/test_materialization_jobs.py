ea9d2386c2da6a1a39581f9da6a963f93cecd812
Sandy Ryza <sandy@elementl.com>
2022-06-14 08:59:34 -0700

    remove old asset lineage from docs

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_materialization_jobs.py
+++
@@ -1,8 +0,0 @@
-from docs_snippets.concepts.assets.materialization_jobs import my_user_model_job
-
-
-def test_pipelines_compile_and_execute():
-    jobs = [my_user_model_job]
-    for job in jobs:
-        result = job.execute_in_process()
-        assert result.success
