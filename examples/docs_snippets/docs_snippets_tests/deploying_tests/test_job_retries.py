14696ae217b6a5670b8d19e4f3d5564f29e048e3
Johann Miller <johann@elementl.com>
2022-06-14 11:45:48 -0400

    Run retries docs (#8367)
    ### Summary & Motivation
    
    ### How I Tested These Changes

---
+++ examples/docs_snippets/docs_snippets_tests/deploying_tests/test_job_retries.py
@@ -0,0 +1,25 @@
+import os
+
+from dagster import DagsterInstance
+from dagster.core.execution.plan.resume_retry import ReexecutionStrategy
+from dagster.core.instance.ref import InstanceRef
+from dagster.core.storage.tags import MAX_RETRIES_TAG, RETRY_STRATEGY_TAG
+from docs_snippets.deploying.job_retries import other_sample_sample_job, sample_job
+
+
+def test_tags():
+    assert sample_job.tags[MAX_RETRIES_TAG] == "3"
+    assert other_sample_sample_job.tags[MAX_RETRIES_TAG] == "3"
+    assert (
+        other_sample_sample_job.tags[RETRY_STRATEGY_TAG]
+        == ReexecutionStrategy.ALL_STEPS.value
+    )
+
+
+def test_instance(docs_snippets_folder):
+    ref = InstanceRef.from_dir(
+        os.path.join(docs_snippets_folder, "deploying", "dagster_instance")
+    )
+
+    assert ref.settings["run_retries"]["enabled"] == True
+    assert ref.settings["run_retries"]["max_retries"] == 3
