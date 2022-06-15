268a0644447fdf163499f6d94075623a6057bbce
Christopher DeCarolis <cdguitar817@gmail.com>
2022-06-15 10:27:04 -0700

    Update python_modules/dagster/dagster/core/definitions/decorators/op_decorator.py

--- python_modules/dagster/dagster/core/definitions/decorators/op_decorator.py
+++ python_modules/dagster/dagster/core/definitions/decorators/op_decorator.py
@@ -214,7 +214,7 @@ def op(
     retry_policy: Optional[RetryPolicy] = None,
     input_defs: Optional[List[InputDefinition]] = None,
     output_defs: Optional[List[OutputDefinition]] = None,
-) -> Union[OpDefinition, _Op]:
+) -> Union["OpDefinition", _Op]:
     """
     Create an op with the specified parameters from the decorated function.
 
