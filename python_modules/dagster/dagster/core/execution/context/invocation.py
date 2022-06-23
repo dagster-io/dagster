8b5124955639704fca6c7ddf74ae69924081b481
Chris DeCarolis <cdguitar817@gmail.com>
2022-06-23 16:00:23 -0700

    Allow invocation when resource is required but no context is set

--- python_modules/dagster/dagster/core/execution/context/invocation.py
+++ python_modules/dagster/dagster/core/execution/context/invocation.py
@@ -301,10 +301,10 @@ def _validate_resource_requirements(
     resource_defs: Mapping[str, ResourceDefinition], solid_def: SolidDefinition
 ) -> None:
     """Validate correctness of resources against required resource keys"""
-
-    for requirement in solid_def.get_resource_requirements():
-        if not requirement.is_io_manager_requirement:
-            ensure_requirements_satisfied(resource_defs, [requirement])
+    if solid_def.compute_fn.has_context_arg():
+        for requirement in solid_def.get_resource_requirements():
+            if not requirement.is_io_manager_requirement:
+                ensure_requirements_satisfied(resource_defs, [requirement])
 
 
 def _resolve_bound_config(solid_config: Any, solid_def: SolidDefinition) -> Any:
