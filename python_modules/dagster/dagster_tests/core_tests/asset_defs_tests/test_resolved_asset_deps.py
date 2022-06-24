22f59cfe9f999c593d41f9fe3c337821ff0c1206
Sandy Ryza <sandy@elementl.com>
2022-06-24 13:27:58 -0700

    fix asset dep resolution bug when two assets in group have same name (#8609)

---
+++ python_modules/dagster/dagster_tests/core_tests/asset_defs_tests/test_resolved_asset_deps.py
@@ -0,0 +1,18 @@
+from dagster import AssetIn, asset
+from dagster.core.asset_defs.resolved_asset_deps import resolve_assets_def_deps
+
+
+def test_same_name_twice_and_downstream():
+    @asset(name="apple", key_prefix="a")
+    def asset1():
+        ...
+
+    @asset(name="apple", key_prefix="b")
+    def asset2():
+        ...
+
+    @asset(ins={"apple": AssetIn(key_prefix="a")})
+    def asset3(apple):
+        del apple
+
+    assert len(resolve_assets_def_deps([asset1, asset2, asset3], [])) == 0
