eb399654992b6b15fd08049cd8feb640f7e02770
Isaac Hellendag <hellendag@users.noreply.github.com>
2022-06-24 10:05:18 -0500

    [dagit] Repair dialog ordering for confirmation/alert dialogs (#8570)
    ### Summary & Motivation
    
    Resolves #8423.
    
    The Dialog components used for `CustomConfirmationProvider` and `CustomAlertProvider` are reused singleton components that, once rendered initially, remain in the DOM forever.
    
    The problem is that when one of these is opened from another Dialog after already having been rendered before (and therefore already existing in the DOM), the DOM order causes the confirmation or alert Dialog to sit behind the Dialog that opened it.
    
    We could strap on our helmets and start some z-index wars, but I think I prefer this instead: don't leave the confirmation or alert Dialog sitting in the DOM once we're done with it. Unmount it after a delay, then remount it when a new confirmation/alert arrives. The delay allows the fade-out animation to be visible to the user before the unmount occurs.
    
    Open to thoughts on this.
    
    ### How I Tested These Changes
    
    View asset config dialog that requires some scaffolding. Open scaffold Dialog (confirmation), then close both dialogs. Repeat, and verify that the confirmation dialog now properly sits on top of the config dialog.
    
    I also added a jest test that includes testing the unmount behavior.

--- js_modules/dagit/packages/core/src/app/CustomAlertProvider.tsx
+++ js_modules/dagit/packages/core/src/app/CustomAlertProvider.tsx
@@ -23,7 +23,10 @@ export const showCustomAlert = (opts: Partial<ICustomAlert>) => {
   setCustomAlert(Object.assign({body: '', title: 'Error'}, opts));
 };
 
+const REMOVAL_DELAY = 500;
+
 export const CustomAlertProvider = () => {
+  const [mounted, setMounted] = React.useState(false);
   const [alert, setAlert] = React.useState(() => CurrentAlert);
   const body = React.useRef<HTMLDivElement>(null);
 
@@ -35,6 +38,19 @@ export const CustomAlertProvider = () => {
     return () => document.removeEventListener(CURRENT_ALERT_CHANGED, setter);
   }, []);
 
+  // When an alert is set, allow the Dialog to be mounted. When it is cleared,
+  // remove the Dialog from the tree so that its root node doesn't linger in the DOM
+  // and cause subsequent z-index bugs.
+  React.useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+    if (!!alert) {
+      setMounted(true);
+    } else {
+      timer = setTimeout(() => setMounted(false), REMOVAL_DELAY);
+    }
+    return () => timer && clearTimeout(timer);
+  }, [alert]);
+
   const onCopy = React.useCallback(
     (e: React.MouseEvent) => {
       const copyElement = copySelector ? body.current!.querySelector(copySelector) : body.current;
@@ -44,6 +60,10 @@ export const CustomAlertProvider = () => {
     },
     [copySelector],
   );
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Dialog
