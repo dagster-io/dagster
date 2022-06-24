269db762d1aed6eb885eb2c31ebb01fe64183893
Chris DeCarolis <cdguitar817@gmail.com>
2022-06-24 10:47:35 -0700

    Add partition_key arg to materialize

--- python_modules/dagster/dagster_tests/core_tests/asset_defs_tests/test_materialize_to_memory.py
+++ python_modules/dagster/dagster_tests/core_tests/asset_defs_tests/test_materialize_to_memory.py
@@ -251,7 +251,7 @@ def test_materialize_multi_asset():
 def test_materialize_to_memory_partition_key():
     @asset(partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"))
     def the_asset(context):
-        context.asset_partition_key_for_output()
+        assert context.asset_partition_key_for_output() == "2022-02-02"
 
-    result = materialize_to_memory([the_asset], partition_key="2022-01-01")
+    result = materialize_to_memory([the_asset], partition_key="2022-02-02")
     assert result.success
