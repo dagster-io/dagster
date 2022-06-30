f3b79cd6e9e7b6e597cd95aabcfaa5e28cd56801
Sean Mackesey <s.mackesey@gmail.com>
2022-06-30 10:28:51 -0400

    revert toy change

--- python_modules/dagster-test/dagster_test/graph_job_op_toys/resources.py
+++ python_modules/dagster-test/dagster_test/graph_job_op_toys/resources.py
@@ -12,7 +12,7 @@ from dagster import (
 
 
 def define_resource(num):
-    @resource(config_schema=Field(Int, is_required=True))
+    @resource(config_schema=Field(Int, is_required=False))
     def a_resource(context):
         return num if context.resource_config is None else context.resource_config
 
