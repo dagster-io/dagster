22901864cb911f9df1e9ef825dd45d4c1de9111c
Brad Alexander <10731572+brad-alexander@users.noreply.github.com>
2022-07-03 12:16:00 -0700

    fix undefined name

--- python_modules/dagster/dagster/daemon/monitoring/monitoring_daemon.py
+++ python_modules/dagster/dagster/daemon/monitoring/monitoring_daemon.py
@@ -61,7 +61,7 @@ def monitor_started_run(instance: DagsterInstance, workspace, run, logger):
             if instance.run_launcher.supports_resume_run:
                 msg = (
                     f"Detected run worker status {check_health_result}. Marking run {run.run_id} as "
-                    f"failed, because it has surpassed the configured maximum attempts to resume the run: {max_resume_run_attempts}."
+                    f"failed, because it has surpassed the configured maximum attempts to resume the run: {instance.run_monitoring_max_resume_run_attempts}."
                 )
             else:
                 msg = (
