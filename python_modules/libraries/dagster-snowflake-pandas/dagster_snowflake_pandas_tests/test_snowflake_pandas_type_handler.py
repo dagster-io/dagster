b721c70dd446f4767b897ee89478f682217a9e50
jamiedemaria <jamie@elementl.com>
2022-07-08 14:24:14 -0400

    Snowflake IO Manager handles pandas timestamps (#8760)

--- python_modules/libraries/dagster-snowflake-pandas/dagster_snowflake_pandas_tests/test_snowflake_pandas_type_handler.py
+++ python_modules/libraries/dagster-snowflake-pandas/dagster_snowflake_pandas_tests/test_snowflake_pandas_type_handler.py
@@ -11,6 +11,10 @@ from dagster_snowflake import build_snowflake_io_manager
 from dagster_snowflake.resources import SnowflakeConnection
 from dagster_snowflake.snowflake_io_manager import TableSlice
 from dagster_snowflake_pandas import SnowflakePandasTypeHandler
+from dagster_snowflake_pandas.snowflake_pandas_type_handler import (
+    _convert_string_to_timestamp,
+    _convert_timestamp_to_string,
+)
 from pandas import DataFrame
 
 from dagster import (
@@ -43,13 +47,13 @@ SHARED_BUILDKITE_SNOWFLAKE_CONF = {
 
 
 @contextmanager
-def temporary_snowflake_table(schema_name: str, db_name: str) -> Iterator[str]:
+def temporary_snowflake_table(schema_name: str, db_name: str, column_str: str) -> Iterator[str]:
     snowflake_config = dict(database=db_name, **SHARED_BUILDKITE_SNOWFLAKE_CONF)
     table_name = "test_io_manager_" + str(uuid.uuid4()).replace("-", "_")
     with SnowflakeConnection(
         snowflake_config, logging.getLogger("temporary_snowflake_table")
     ).get_connection() as conn:
-        conn.cursor().execute(f"create table {schema_name}.{table_name} (foo string, quux integer)")
+        conn.cursor().execute(f"create table {schema_name}.{table_name} ({column_str})")
         try:
             yield table_name
         finally:
@@ -84,7 +88,7 @@ def test_handle_output():
 
 def test_load_input():
     with patch("dagster_snowflake_pandas.snowflake_pandas_type_handler._connect_snowflake"), patch(
-        "dagster_snowflake_pandas.snowflake_pandas_type_handler.read_sql"
+        "dagster_snowflake_pandas.snowflake_pandas_type_handler.pd.read_sql"
     ) as mock_read_sql:
         mock_read_sql.return_value = DataFrame([{"COL1": "a", "COL2": 1}])
 
@@ -104,10 +108,37 @@ def test_load_input():
         assert df.equals(DataFrame([{"col1": "a", "col2": 1}]))
 
 
+def test_type_conversions():
+    # no timestamp data
+    no_time = pandas.Series([1, 2, 3, 4, 5])
+    converted = _convert_string_to_timestamp(_convert_timestamp_to_string(no_time))
+
+    assert (converted == no_time).all()
+
+    # timestamp data
+    with_time = pandas.Series(
+        [
+            pandas.Timestamp("2017-01-01T12:30:45.35"),
+            pandas.Timestamp("2017-02-01T12:30:45.35"),
+            pandas.Timestamp("2017-03-01T12:30:45.35"),
+        ]
+    )
+    time_converted = _convert_string_to_timestamp(_convert_timestamp_to_string(with_time))
+
+    assert (with_time == time_converted).all()
+
+    # string that isn't a time
+    string_data = pandas.Series(["not", "a", "timestamp"])
+
+    assert (_convert_string_to_timestamp(string_data) == string_data).all()
+
+
 @pytest.mark.skipif(not IS_BUILDKITE, reason="Requires access to the BUILDKITE snowflake DB")
 def test_io_manager_with_snowflake_pandas():
     with temporary_snowflake_table(
-        schema_name="SNOWFLAKE_IO_MANAGER_SCHEMA", db_name="TEST_SNOWFLAKE_IO_MANAGER"
+        schema_name="SNOWFLAKE_IO_MANAGER_SCHEMA",
+        db_name="TEST_SNOWFLAKE_IO_MANAGER",
+        column_str="foo string, quux integer",
     ) as table_name:
 
         # Create a job with the temporary table name as an output, so that it will write to that table
@@ -147,4 +178,59 @@ def test_io_manager_with_snowflake_pandas():
             read_pandas_df(emit_pandas_df())
 
         res = io_manager_test_pipeline.execute_in_process()
+        assert res.success
+
+
+@pytest.mark.skipif(not IS_BUILDKITE, reason="Requires access to the BUILDKITE snowflake DB")
+def test_io_manager_with_snowflake_pandas_timestamp_data():
+    with temporary_snowflake_table(
+        schema_name="SNOWFLAKE_IO_MANAGER_SCHEMA",
+        db_name="TEST_SNOWFLAKE_IO_MANAGER",
+        column_str="foo string, date TIMESTAMP_NTZ(9)",
+    ) as table_name:
+
+        time_df = pandas.DataFrame(
+            {
+                "foo": ["bar", "baz"],
+                "date": [
+                    pandas.Timestamp("2017-01-01T12:30:45.350"),
+                    pandas.Timestamp("2017-02-01T12:30:45.350"),
+                ],
+            }
+        )
+
+        @op(
+            out={
+                table_name: Out(
+                    io_manager_key="snowflake", metadata={"schema": "SNOWFLAKE_IO_MANAGER_SCHEMA"}
+                )
+            }
+        )
+        def emit_time_df(_):
+            return time_df
+
+        @op
+        def read_time_df(df: pandas.DataFrame):
+            assert set(df.columns) == {"foo", "date"}
+            assert (df["date"] == time_df["date"]).all()
+
+        snowflake_io_manager = build_snowflake_io_manager([SnowflakePandasTypeHandler()])
+
+        @job(
+            resource_defs={"snowflake": snowflake_io_manager},
+            config={
+                "resources": {
+                    "snowflake": {
+                        "config": {
+                            **SHARED_BUILDKITE_SNOWFLAKE_CONF,
+                            "database": "TEST_SNOWFLAKE_IO_MANAGER",
+                        }
+                    }
+                }
+            },
+        )
+        def io_manager_timestamp_test_job():
+            read_time_df(emit_time_df())
+
+        res = io_manager_timestamp_test_job.execute_in_process()
         assert res.success
