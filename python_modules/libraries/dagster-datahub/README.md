2f15fbfa3f5d2f683e7b1c050cc124ff3f55aac1
Jordan Wolinsky <jordan@zephyrai.bio>
2022-07-14 18:26:15 -0400

    dagster-datahub Rest and Kafka Emitter integration (#8764)
    * datahub dagster emitter integration
    
    * isort, pylint fix, some review comments
    
    * Extra String Source and update unit tests for such
    
    * more StringSource
    
    * remove dagster spark from tox dependencies
    
    * remove redshift from extras
    
    * add dagster-datahub documentation
    
    * more docs
    
    * add datahub to navigation, remove test code
    
    * caps
    
    * remove comments
    
    * fix unit test.  add http:// to schema registry url
    
    * make black
    
    * fix docs build, which needed to install the lib deps instead of mocking
    
    Co-authored-by: prha <prha@elementl.com>

---
+++ python_modules/libraries/dagster-datahub/README.md
@@ -0,0 +1,4 @@
+# dagster-datahub
+
+The docs for `dagster-datahub` can be found
+[here](https://docs.dagster.io/_apidocs/libraries/dagster-datahub).
---
+++ python_modules/libraries/dagster-datahub/setup.py
@@ -0,0 +1,44 @@
+from typing import Dict
+
+from setuptools import find_packages, setup
+
+
+def get_version() -> str:
+    version: Dict[str, str] = {}
+    with open("dagster_datahub/version.py", encoding="utf8") as fp:
+        exec(fp.read(), version)  # pylint: disable=W0122
+
+    return version["__version__"]
+
+
+if __name__ == "__main__":
+    ver = get_version()
+    # dont pin dev installs to avoid pip dep resolver issues
+    pin = "" if ver == "0+dev" else f"=={ver}"
+    setup(
+        name="dagster-datahub",
+        version=ver,
+        author="Elementl",
+        author_email="hello@elementl.com",
+        license="Apache-2.0",
+        description="Package for Datahub-specific Dagster framework solid and resource components.",
+        url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-datahub",
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        packages=find_packages(exclude=["dagster_datahub_tests*"]),
+        include_package_data=True,
+        install_requires=[
+            "acryl-datahub[datahub-rest, datahub-kafka]",
+            f"dagster{pin}",
+            "packaging",
+            "requests",
+        ],
+        extras_require={},
+        zip_safe=False,
+    )
