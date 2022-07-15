010108541a944b58aef77d6f22a5afeb21d131b7
Sean Mackesey <s.mackesey@gmail.com>
2022-07-14 21:34:17 -0400

    fixes [INTERNAL_BRANCH=sean/rename-dagster-api-cli-config]

--- python_modules/dagster/dagster/_config/primitive_mapping.py
+++ python_modules/dagster/dagster/_config/primitive_mapping.py
@@ -5,14 +5,16 @@ from .config_type import Array, ConfigAnyInstance
 from .field import resolve_to_config_type
 from .field_utils import Permissive
 
-SUPPORTED_CONFIG_BUILTINS = {
+SUPPORTED_CONFIG_BUILTIN_MAP = {
     int: Int,
     float: Float,
     bool: Bool,
     str: String,
-    list: Array(ConfigAnyInstance),
+    list: Array(ConfigAnyInstance),  # type: ignore
     dict: Permissive(),
 }
+
+SUPPORTED_CONFIG_BUILTINS = [*SUPPORTED_CONFIG_BUILTIN_MAP.keys()]
 
 
 def is_supported_config_python_builtin(ttype):
@@ -25,4 +27,4 @@ def remap_python_builtin_for_config(ttype):
     """
     check.param_invariant(is_supported_config_python_builtin(ttype), "ttype")
 
-    return resolve_to_config_type(SUPPORTED_CONFIG_BUILTINS[ttype])
+    return resolve_to_config_type(SUPPORTED_CONFIG_BUILTIN_MAP[ttype])
