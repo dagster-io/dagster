8dfd731f4ab6eada591bd606fc113d23d710f9ae
Claire Lin <claire@elementl.com>
2022-07-21 09:48:09 -0700

    pylint

--- python_modules/dagster/dagster/core/definitions/assets_job.py
+++ python_modules/dagster/dagster/core/definitions/assets_job.py
@@ -131,10 +131,10 @@ def build_assets_job(
             partitions_def=partitions_def,
             asset_layer=asset_layer,
             _asset_selection_data=_asset_selection_data,
-            _metadata_entries=original_job._metadata_entries,
+            _metadata_entries=original_job._metadata_entries,  # pylint: disable=protected-access
             logger_defs=original_job.get_mode_definition().loggers,
             hooks=original_job.hook_defs,
-            op_retry_policy=original_job._solid_retry_policy,
+            op_retry_policy=original_job._solid_retry_policy,  # pylint: disable=protected-access
             version_strategy=original_job.version_strategy,
         )
 
