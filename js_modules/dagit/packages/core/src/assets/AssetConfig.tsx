1e77c6b222fcd5b97170220ba102f305d6b6033c
Sean Mackesey <s.mackesey@gmail.com>
2022-07-22 14:48:50 -0400

    remove special-handling code for asset config schema

--- js_modules/dagit/packages/core/src/assets/AssetConfig.tsx
+++ js_modules/dagit/packages/core/src/assets/AssetConfig.tsx
@@ -1,27 +1,6 @@
 import {gql} from '@apollo/client';
 
-import {
-  SidebarAssetQuery_assetNodeOrError_AssetNode_configField as ConfigField,
-  SidebarAssetQuery_assetNodeOrError_AssetNode_configField_configType_CompositeConfigType as ConfigSchema,
-} from '../asset-graph/types/SidebarAssetQuery';
 import {CONFIG_TYPE_SCHEMA_FRAGMENT} from '../typeexplorer/ConfigTypeSchema';
-
-export interface HasConfigField {
-  configField: ConfigField | null;
-}
-
-export function configSchemaForAssetNode(obj: HasConfigField): ConfigSchema | null {
-  if (obj.configField) {
-    const configSchema = obj.configField.configType as ConfigSchema;
-    if (configSchema.fields) {
-      return configSchema.fields.length > 1 ? configSchema : null;
-    } else {
-      return null;
-    }
-  } else {
-    return null;
-  }
-}
 
 export const ASSET_NODE_CONFIG_FRAGMENT = gql`
   fragment AssetNodeConfigFragment on AssetNode {
