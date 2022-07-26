6f0c14a868a3d0322801fe6ab0240cc89bb86545
Chris DeCarolis <cdguitar817@gmail.com>
2022-07-26 16:33:15 -0700

    Update mlflow, shell, and slack integrations

--- python_modules/libraries/dagster-shell/dagster_shell/__init__.py
+++ python_modules/libraries/dagster-shell/dagster_shell/__init__.py
@@ -2,20 +2,14 @@ from dagster._core.utils import check_dagster_package_version
 
 from .solids import (
     create_shell_command_op,
-    create_shell_command_solid,
     create_shell_script_op,
-    create_shell_script_solid,
     shell_op,
-    shell_solid,
 )
 from .version import __version__
 
 check_dagster_package_version("dagster-shell", __version__)
 
 __all__ = [
-    "create_shell_command_solid",
-    "create_shell_script_solid",
-    "shell_solid",
     "create_shell_command_op",
     "create_shell_script_op",
     "shell_op",
