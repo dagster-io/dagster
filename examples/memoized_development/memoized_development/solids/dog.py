90ea7172dee0f39f9a1543a785c4a6827683539f
Christopher DeCarolis <cdguitar817@gmail.com>
2022-07-27 11:21:52 -0700

    remove legacy dbt docs example, fix graphql client example (#9055)
    * remove legacy dbt docs example, fix graphql client example
    
    * fix actual snapshots, add dagstermill example
    
    * remove legacy memoization example
    
    * fix bk

--- examples/memoized_development/memoized_development/solids/dog.py
+++
@@ -1,9 +0,0 @@
-from memoized_development.solids.solid_utils import get_hash_for_file
-
-from dagster._legacy import solid
-
-
-@solid(version=get_hash_for_file(__file__), config_schema={"dog_breed": str})
-def emit_dog(context):
-    breed = context.solid_config["dog_breed"]
-    return breed
