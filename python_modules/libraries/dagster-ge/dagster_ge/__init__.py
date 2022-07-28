725a3aaf312900b46df4e86cabb86ee548fac250
Christopher DeCarolis <cdguitar817@gmail.com>
2022-07-28 16:26:24 -0700

    [1.0] remove legacy imports from dagster-ge (#9051)
    remove legacy imports from dagster-ge

--- python_modules/libraries/dagster-ge/dagster_ge/__init__.py
+++ python_modules/libraries/dagster-ge/dagster_ge/__init__.py
@@ -1,3 +1,3 @@
-from .factory import ge_validation_op_factory, ge_validation_solid_factory
+from .factory import ge_validation_op_factory
 
-__all__ = ["ge_validation_solid_factory"]
+__all__ = ["ge_validation_op_factory"]
