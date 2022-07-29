f4b652f5e7b7be1578bf9b5c6141f1e91061c0f2
Chris DeCarolis <cdguitar817@gmail.com>
2022-07-29 09:53:20 -0700

    delete snowflake_solid_for_query

--- python_modules/libraries/dagster-snowflake/dagster_snowflake/__init__.py
+++ python_modules/libraries/dagster-snowflake/dagster_snowflake/__init__.py
@@ -2,14 +2,13 @@ from dagster._core.utils import check_dagster_package_version
 
 from .resources import SnowflakeConnection, snowflake_resource
 from .snowflake_io_manager import DbTypeHandler, build_snowflake_io_manager
-from .solids import snowflake_op_for_query, snowflake_solid_for_query
+from .solids import snowflake_op_for_query
 from .version import __version__
 
 check_dagster_package_version("dagster-snowflake", __version__)
 
 __all__ = [
     "snowflake_op_for_query",
-    "snowflake_solid_for_query",
     "snowflake_resource",
     "build_snowflake_io_manager",
     "SnowflakeConnection",
