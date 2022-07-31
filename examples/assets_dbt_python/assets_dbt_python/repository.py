aa1795ffde89ff15815b9a1129ca873de6c8c490
yuhan <yuhan@elementl.com>
2022-07-31 01:54:52 -0700

    isort

--- examples/assets_dbt_python/assets_dbt_python/repository.py
+++ examples/assets_dbt_python/assets_dbt_python/repository.py
@@ -1,9 +1,8 @@
 import os
 
-from assets_dbt_python.assets import forecasting
-from assets_dbt_python.assets import raw_data
-
+from assets_dbt_python.assets import forecasting, raw_data
 from assets_dbt_python.resources import duckdb_io_manager
+from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
 
 from dagster import (
     ScheduleDefinition,
@@ -14,7 +13,6 @@ from dagster import (
     with_resources,
 )
 from dagster._utils import file_relative_path
-from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
 
 DBT_PROJECT_DIR = file_relative_path(__file__, "../dbt_project")
 DBT_PROFILES_DIR = file_relative_path(__file__, "../dbt_project/config")
