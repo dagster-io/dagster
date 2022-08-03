52cfd0b6e0953d6c88100ad46ff7e9a11573f483
yuhan <yuhan@elementl.com>
2022-08-02 21:40:08 -0700

    add datasets back, remove old sda example

--- examples/software_defined_assets/software_defined_assets_tests/test_spark_weather_assets.py
+++
@@ -1,9 +0,0 @@
-from software_defined_assets.spark_weather_assets import spark_weather_assets
-
-from dagster import materialize
-from dagster._core.test_utils import instance_for_test
-
-
-def test_airport_weather_assets():
-    with instance_for_test() as instance:
-        assert materialize(spark_weather_assets, instance=instance).success
