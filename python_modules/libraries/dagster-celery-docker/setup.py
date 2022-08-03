1455a76c358e17bc96d72b469d9cb15afc596830
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2022-08-03 10:45:45 -0700

    drop python 3.6 from bk (#9173)
    * drop python 3.6 from bk
    * update setup.py - drop 3.6, add 3.10

--- python_modules/libraries/dagster-celery-docker/setup.py
+++ python_modules/libraries/dagster-celery-docker/setup.py
@@ -23,10 +23,10 @@ if __name__ == "__main__":
         description="A Dagster integration for celery-docker",
         url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-celery-docker",
         classifiers=[
-            "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "License :: OSI Approved :: Apache Software License",
             "Operating System :: OS Independent",
         ],
