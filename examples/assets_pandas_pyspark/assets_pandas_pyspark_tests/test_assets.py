964177d79d397379f1dd6609970d4f1e16fb35e7
yuhan <yuhan@elementl.com>
2022-08-02 20:42:35 -0700

    update test names

--- examples/assets_pandas_pyspark/assets_pandas_pyspark_tests/test_assets.py
+++ examples/assets_pandas_pyspark/assets_pandas_pyspark_tests/test_assets.py
@@ -5,11 +5,11 @@ from dagster import materialize
 from dagster._core.test_utils import instance_for_test
 
 
-def test_airport_weather_assets():
+def test_weather_assets():
     with instance_for_test() as instance:
         assert materialize(weather_assets, instance=instance).success
 
 
-def test_airport_weather_assets():
+def test_spark_weather_assets():
     with instance_for_test() as instance:
         assert materialize(spark_weather_assets, instance=instance).success
