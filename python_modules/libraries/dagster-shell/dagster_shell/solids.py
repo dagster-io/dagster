32195904f32a293172106fd5432110d7e31e12d5
Chris DeCarolis <cdguitar817@gmail.com>
2022-08-02 17:23:19 -0700

    fix dagster-shell (remove ins)

--- python_modules/libraries/dagster-shell/dagster_shell/solids.py
+++ python_modules/libraries/dagster-shell/dagster_shell/solids.py
@@ -1,9 +1,8 @@
 import os
 
-from dagster import Enum, EnumValue, Failure, Field, Noneable, Nothing, Permissive
+from dagster import Enum, EnumValue, Failure, Field, In, Noneable, Nothing, Out, Permissive
 from dagster import _check as check
 from dagster import op
-from dagster._legacy import InputDefinition, OutputDefinition, solid
 
 from .utils import execute, execute_script_file
 
@@ -39,38 +38,29 @@ def shell_op_config():
     }
 
 
-def core_shell(dagster_decorator, decorator_name):
-    @dagster_decorator(
-        name=f"shell_{decorator_name}",
-        description=(
-            f"This {decorator_name} executes a shell command it receives as input.\n\n"
-            f"This {decorator_name} is suitable for uses where the command to execute is generated dynamically by "
-            f"upstream {decorator_name}. If you know the command to execute at pipeline construction time, "
-            f"consider `shell_command_{decorator_name}` instead."
-        ),
-        input_defs=[InputDefinition("shell_command", str)],
-        output_defs=[OutputDefinition(str, "result")],
-        config_schema=shell_op_config(),
-    )
-    def shell_fn(context, shell_command):
-        op_config = context.op_config.copy()
-        op_config["env"] = {**os.environ, **op_config.get("env", {})}
-        output, return_code = execute(shell_command=shell_command, log=context.log, **op_config)
+@op(
+    name="shell_op",
+    description=(
+        "This op executes a shell command it receives as input.\n\n"
+        "This op is suitable for uses where the command to execute is generated dynamically by "
+        "upstream ops. If you know the command to execute at pipeline construction time, "
+        "consider `shell_command_op` instead."
+    ),
+    ins={"shell_command": In(str)},
+    out=Out(str),
+    config_schema=shell_op_config(),
+)
+def shell_op(context, shell_command):
+    op_config = context.op_config.copy()
+    op_config["env"] = {**os.environ, **op_config.get("env", {})}
+    output, return_code = execute(shell_command=shell_command, log=context.log, **op_config)
 
-        if return_code:
-            raise Failure(
-                description="Shell command execution failed with output: {output}".format(
-                    output=output
-                )
-            )
+    if return_code:
+        raise Failure(
+            description="Shell command execution failed with output: {output}".format(output=output)
+        )
 
-        return output
-
-    return shell_fn
-
-
-shell_solid = core_shell(solid, "solid")
-shell_op = core_shell(op, "op")
+    return output
 
 
 def create_shell_command_op(
@@ -110,79 +100,12 @@ def create_shell_command_op(
     Returns:
         OpDefinition: Returns the constructed op definition.
     """
-    return core_create_shell_command(
-        op,
-        shell_command=shell_command,
+
+    @op(
         name=name,
         description=description,
-        required_resource_keys=required_resource_keys,
-        tags=tags,
-    )
-
-
-def create_shell_command_solid(
-    shell_command,
-    name,
-    description=None,
-    required_resource_keys=None,
-    tags=None,
-):
-    """This function is a factory that constructs solids to execute a shell command.
-
-    Note that you can only use ``shell_command_solid`` if you know the command you'd like to execute
-    at pipeline construction time. If you'd like to construct shell commands dynamically during
-    pipeline execution and pass them between solids, you should use ``shell_solid`` instead.
-
-    Examples:
-
-    .. literalinclude:: ../../../../../../python_modules/libraries/dagster-shell/dagster_shell_tests/example_shell_command_solid.py
-       :language: python
-
-
-    Args:
-        shell_command (str): The shell command that the constructed solid will execute.
-        name (str): The name of the constructed solid.
-        description (Optional[str]): Human-readable description of this solid.
-        required_resource_keys (Optional[Set[str]]): Set of resource handles required by this solid.
-            Setting this ensures that resource spin up for the required resources will occur before
-            the shell command is executed.
-        tags (Optional[Dict[str, Any]]): Arbitrary metadata for the solid. Frameworks may
-            expect and require certain metadata to be attached to a solid. Users should generally
-            not set metadata directly. Values that are not strings will be json encoded and must meet
-            the criteria that `json.loads(json.dumps(value)) == value`.
-
-    Raises:
-        Failure: Raised when the shell command returns a non-zero exit code.
-
-    Returns:
-        SolidDefinition: Returns the constructed solid definition.
-    """
-    return core_create_shell_command(
-        solid,
-        shell_command=shell_command,
-        name=name,
-        description=description,
-        required_resource_keys=required_resource_keys,
-        tags=tags,
-    )
-
-
-def core_create_shell_command(
-    dagster_decorator,
-    shell_command,
-    name,
-    description=None,
-    required_resource_keys=None,
-    tags=None,
-):
-    check.str_param(shell_command, "shell_command")
-    name = check.str_param(name, "name")
-
-    @dagster_decorator(
-        name=name,
-        description=description,
-        input_defs=[InputDefinition("start", Nothing)],
-        output_defs=[OutputDefinition(str, "result")],
+        ins={"start": In(Nothing)},
+        out=Out(str),
         config_schema=shell_op_config(),
         required_resource_keys=required_resource_keys,
         tags=tags,
@@ -204,9 +127,7 @@ def core_create_shell_command(
     return _shell_fn
 
 
-def create_shell_script_op(
-    shell_script_path, name="create_shell_script_op", input_defs=None, **kwargs
-):
+def create_shell_script_op(shell_script_path, name="create_shell_script_op", ins=None, **kwargs):
     """This function is a factory which constructs an op that will execute a shell command read
     from a script file.
 
@@ -226,8 +147,8 @@ def create_shell_script_op(
 
     Args:
         shell_script_path (str): The script file to execute.
-        name (str, optional): The name of this op. Defaults to "create_shell_script_op".
-        input_defs (List[InputDefinition], optional): input definitions for the op. Defaults to
+        name (Optional[str]): The name of this op. Defaults to "create_shell_script_op".
+        ins (Optional[Mapping[str, In]]): Ins for the op. Defaults to
             a single Nothing input.
 
     Raises:
@@ -236,81 +157,18 @@ def create_shell_script_op(
     Returns:
         OpDefinition: Returns the constructed op definition.
     """
-    return core_create_shell_script(
-        dagster_decorator=solid,
-        decorator_name="solid",
-        shell_script_path=shell_script_path,
-        name=name,
-        input_defs=input_defs,
-        **kwargs,
-    )
-
-
-def create_shell_script_solid(
-    shell_script_path, name="create_shell_script_solid", input_defs=None, **kwargs
-):
-    """This function is a factory which constructs a solid that will execute a shell command read
-    from a script file.
-
-    Any kwargs passed to this function will be passed along to the underlying :func:`@solid
-    <dagster.solid>` decorator. However, note that overriding ``config`` or ``output_defs`` is not
-    supported.
-
-    You might consider using :func:`@composite_solid <dagster.composite_solid>` to wrap this solid
-    in the cases where you'd like to configure the shell solid with different config fields.
-
-
-    Examples:
-
-    .. literalinclude:: ../../../../../../python_modules/libraries/dagster-shell/dagster_shell_tests/example_shell_script_solid.py
-       :language: python
-
-
-    Args:
-        shell_script_path (str): The script file to execute.
-        name (str, optional): The name of this solid. Defaults to "create_shell_script_solid".
-        input_defs (List[InputDefinition], optional): input definitions for the solid. Defaults to
-            a single Nothing input.
-
-    Raises:
-        Failure: Raised when the shell command returns a non-zero exit code.
-
-    Returns:
-        SolidDefinition: Returns the constructed solid definition.
-    """
-    return core_create_shell_script(
-        dagster_decorator=solid,
-        decorator_name="solid",
-        shell_script_path=shell_script_path,
-        name=name,
-        input_defs=input_defs,
-        **kwargs,
-    )
-
-
-def core_create_shell_script(
-    dagster_decorator,
-    decorator_name,
-    shell_script_path,
-    name="create_shell_script_solid",
-    input_defs=None,
-    **kwargs,
-):
     check.str_param(shell_script_path, "shell_script_path")
     name = check.str_param(name, "name")
-    check.opt_list_param(input_defs, "input_defs", of_type=InputDefinition)
-
-    if "output_defs" in kwargs:
-        raise TypeError(f"Overriding output_defs for shell {decorator_name} is not supported.")
+    check.opt_mapping_param(ins, "ins", value_type=In)
 
     if "config" in kwargs:
-        raise TypeError(f"Overriding config for shell {decorator_name} is not supported.")
+        raise TypeError("Overriding config for shell op is not supported.")
 
-    @dagster_decorator(
+    @op(
         name=name,
-        description=kwargs.pop("description", f"A {decorator_name} to invoke a shell command."),
-        input_defs=input_defs or [InputDefinition("start", Nothing)],
-        output_defs=[OutputDefinition(str, "result")],
+        description=kwargs.pop("description", "An op to invoke a shell command."),
+        ins=ins or {"start": In(Nothing)},
+        out=Out(str),
         config_schema=shell_op_config(),
         **kwargs,
     )
--- python_modules/libraries/dagster-shell/dagster_shell_tests/test_examples.py
+++ python_modules/libraries/dagster-shell/dagster_shell_tests/test_examples.py
@@ -1,22 +1,3 @@
-from dagster._legacy import execute_pipeline
-
-
-def test_example_shell_command_solid():
-    from .example_shell_command_solid import pipe
-
-    res = execute_pipeline(pipe)
-    assert res.success
-    assert res.result_for_solid("a").output_value() == "hello, world!\n"
-
-
-def test_example_shell_script_solid():
-    from .example_shell_script_solid import pipe
-
-    res = execute_pipeline(pipe)
-    assert res.success
-    assert res.result_for_solid("a").output_value() == "hello, world!\n"
-
-
 def test_example_shell_command_op():
     from .example_shell_command_op import my_graph
 
