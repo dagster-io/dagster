2e7fd2cd25ec51695712d49e7e695664f6467f72
Claire Lin <claire@elementl.com>
2022-08-04 12:01:49 -0700

    add new examples

--- examples/docs_snippets/docs_snippets/concepts/configuration/make_values_resource_any.py
+++ examples/docs_snippets/docs_snippets/concepts/configuration/make_values_resource_any.py
@@ -1,22 +1,34 @@
+from dagster import job, op
+import os
+
+# start_file_example
+
+
 from dagster import job, make_values_resource, op
 
 
-@op(required_resource_keys={"value"})
-def needs_value(context):
-    context.log.info(f"value: {context.resources.value}")
+@op(required_resource_keys={"file_dir"})
+def add_file(context):
+    filename = f"{context.resources.file_dir}/new_file.txt"
+    open(filename, "x").close()
+
+    context.log.info(f"Created file: {filename}")
 
 
-@op(required_resource_keys={"value"})
-def also_needs_value(context):
-    context.log.info(f"value: {context.resources.value}")
+@op(required_resource_keys={"file_dir"})
+def total_num_files(context):
+    files_in_dir = os.listdir(context.resources.file_dir)
+    context.log.info(f"Total number of files: {len(files_in_dir)}")
 
 
-@job(resource_defs={"value": make_values_resource()})
-def basic_job():
-    needs_value()
-    also_needs_value()
+@job(resource_defs={"file_dir": make_values_resource()})
+def file_dir_job():
+    add_file()
+    total_num_files()
 
 
-basic_result = basic_job.execute_in_process(
-    run_config={"resources": {"value": {"config": "some_value"}}}
+result = file_dir_job.execute_in_process(
+    run_config={"resources": {"file_dir": {"config": "/my_files/"}}}
 )
+
+# end_file_example
--- examples/docs_snippets/docs_snippets/concepts/configuration/make_values_resource_config_schema.py
+++ examples/docs_snippets/docs_snippets/concepts/configuration/make_values_resource_config_schema.py
@@ -1,22 +1,42 @@
+import os
+
+# start_file_example
+
+
 from dagster import job, make_values_resource, op
 
 
-@op(required_resource_keys={"values"})
-def needs_value(context):
-    context.log.info(f"my str: {context.resources.values['my_str']}")
+@op(required_resource_keys={"file_dirs"})
+def write_file(context):
+    filename = f"{context.resources.file_dirs['write_file_dir']}/new_file.txt"
+    open(filename, "x").close()
+
+    context.log.info(f"Created file: {filename}")
 
 
-@op(required_resource_keys={"values"})
-def needs_different_value(context):
-    context.log.info(f"my int: {context.resources.values['my_int']}")
+@op(required_resource_keys={"file_dirs"})
+def total_num_files(context):
+    files_in_dir = os.listdir(context.resources.file_dirs['count_file_dir'])
+    context.log.info(f"Total number of files: {len(files_in_dir)}")
 
 
-@job(resource_defs={"values": make_values_resource(my_str=str, my_int=int)})
-def different_values_job():
-    needs_value()
-    needs_different_value()
+@job(resource_defs={"file_dirs": make_values_resource(write_file_dir=str, count_file_dir=str)})
+def file_dir_job():
+    write_file()
+    total_num_files()
 
 
-result = different_values_job.execute_in_process(
-    run_config={"resources": {"values": {"config": {"my_str": "foo", "my_int": 1}}}}
+result = file_dir_job.execute_in_process(
+    run_config={
+        "resources": {
+            "file_dirs": {
+                "config": {
+                    "write_file_dir": "/write_files/",
+                    "count_file_dir": "/count_files/",
+                }
+            }
+        }
+    }
 )
+
+# end_file_example
