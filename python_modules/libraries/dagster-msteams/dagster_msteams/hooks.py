e2462db8b230e9d10314216545bbdde80d0b7c53
Christopher DeCarolis <cdguitar817@gmail.com>
2022-08-04 10:17:00 -0700

    remove legacy args from build_x_context (#9201)
    * remove legacy args from build_x_context
    
    * remove mode_def from build_hook_context
    
    * fix tests, mlflow

--- python_modules/libraries/dagster-msteams/dagster_msteams/hooks.py
+++ python_modules/libraries/dagster-msteams/dagster_msteams/hooks.py
@@ -7,12 +7,7 @@ from dagster._core.execution.context.hook import HookContext
 
 
 def _default_status_message(context: HookContext, status: str) -> str:
-    return "Solid {solid_name} on pipeline {pipeline_name} {status}!\nRun ID: {run_id}".format(
-        solid_name=context.solid.name,
-        pipeline_name=context.pipeline_name,
-        run_id=context.run_id,
-        status=status,
-    )
+    return f"Op {context.op.name} on job {context.job_name} {status}!\nRun ID: {context.run_id}"
 
 
 def _default_failure_message(context: HookContext) -> str:
@@ -33,31 +28,29 @@ def teams_on_failure(
         message_fn (Optional(Callable[[HookContext], str])): Function which takes in the
             HookContext outputs the message you want to send.
         dagit_base_url: (Optional[str]): The base url of your Dagit instance. Specify this
-            to allow messages to include deeplinks to the specific pipeline run that triggered
+            to allow messages to include deeplinks to the specific run that triggered
             the hook.
 
     Examples:
         .. code-block:: python
 
             @teams_on_failure(dagit_base_url="http://localhost:3000")
-            @pipeline(...)
-            def my_pipeline():
+            @job(...)
+            def my_job():
                 pass
 
         .. code-block:: python
 
             def my_message_fn(context: HookContext) -> str:
-                return "Solid {solid_name} failed!".format(
-                    solid_name=context.solid
-                )
+                return f"Op {context.op.name} failed!"
 
-            @solid
-            def a_solid(context):
+            @op
+            def a_op(context):
                 pass
 
-            @pipeline(...)
-            def my_pipeline():
-                a_solid.with_hooks(hook_defs={teams_on_failure("#foo", my_message_fn)})
+            @job(...)
+            def my_job():
+                a_op.with_hooks(hook_defs={teams_on_failure("#foo", my_message_fn)})
 
     """
 
@@ -86,31 +79,29 @@ def teams_on_success(
         message_fn (Optional(Callable[[HookContext], str])): Function which takes in the
             HookContext outputs the message you want to send.
         dagit_base_url: (Optional[str]): The base url of your Dagit instance. Specify this
-            to allow messages to include deeplinks to the specific pipeline run that triggered
+            to allow messages to include deeplinks to the specific run that triggered
             the hook.
 
     Examples:
         .. code-block:: python
 
             @teams_on_success(dagit_base_url="http://localhost:3000")
-            @pipeline(...)
-            def my_pipeline():
+            @job(...)
+            def my_job():
                 pass
 
         .. code-block:: python
 
             def my_message_fn(context: HookContext) -> str:
-                return "Solid {solid_name} failed!".format(
-                    solid_name=context.solid
-                )
+                return f"Op {context.op.name} failed!"
 
-            @solid
-            def a_solid(context):
+            @op
+            def a_op(context):
                 pass
 
-            @pipeline(...)
-            def my_pipeline():
-                a_solid.with_hooks(hook_defs={teams_on_success("#foo", my_message_fn)})
+            @job(...)
+            def my_job():
+                a_op.with_hooks(hook_defs={teams_on_success("#foo", my_message_fn)})
 
     """
 
