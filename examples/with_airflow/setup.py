cfa1c739b9b8cd244bafe53a3b7a11d53965e353
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2022-08-05 10:58:01 -0700

    example with_airflow: pin markupsafe (#9242)

--- examples/with_airflow/setup.py
+++ examples/with_airflow/setup.py
@@ -11,5 +11,7 @@ if __name__ == "__main__":
             "dagster_airflow",
             # See https://github.com/dagster-io/dagster/issues/2701
             "apache-airflow==1.10.10",
+            # Conflicts with `Jinja2` which is used in dagster cli that dagster_airflow depends on
+            "markupsafe<=2.0.1",
         ],
     )
