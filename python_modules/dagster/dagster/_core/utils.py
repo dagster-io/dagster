c854253aaa985ee283bc78bbff7e065926188db0
Sean Mackesey <s.mackesey@gmail.com>
2022-08-05 09:45:19 -0400

    add tests

--- python_modules/dagster/dagster/_core/utils.py
+++ python_modules/dagster/dagster/_core/utils.py
@@ -2,16 +2,14 @@ import os
 import random
 import string
 import uuid
+import warnings
 from collections import OrderedDict
 from typing import Tuple, Union, cast
-import warnings
 
-import packaging.version
 import toposort as toposort_
 
 import dagster._check as check
-from dagster._utils import frozendict, library_version_from_core_version
-from dagster.version import __version__
+from dagster._utils import frozendict, library_version_from_core_version, parse_package_version
 
 BACKFILL_TAG_LENGTH = 8
 
@@ -76,20 +74,25 @@ def str_format_set(items):
     return "[{items}]".format(items=", ".join(["'{item}'".format(item=item) for item in items]))
 
 
-def check_dagster_package_version(library_name: str, library_version: str):
-    parsed_lib_version = packaging.version.parse(library_version)
-    assert isinstance(parsed_lib_version, packaging.version.Version)
+def check_dagster_package_version(library_name: str, library_version: str) -> None:
+    # This import must be internal in order for this function to be testable
+    from dagster.version import __version__
+
+    parsed_lib_version = parse_package_version(library_version)
     if parsed_lib_version.release[0] >= 1:
         if library_version != __version__:
-            message = "Found version mismatch between `dagster` ({}) and `{}` ({})".format(
-                __version__, library_name, library_version
+            message = (
+                f"Found version mismatch between `dagster` ({__version__})"
+                f"and `{library_name}` ({library_version})"
             )
             warnings.warn(message)
     else:
         target_version = library_version_from_core_version(__version__)
         if library_version != target_version:
-            message = "Found version mismatch between `dagster` ({}) and `{}` ({}). Library version should be {}.".format(
-                __version__, library_name, library_version, target_version
+            message = (
+                f"Found version mismatch between `dagster` ({__version__}) "
+                f"expected library version ({target_version} "
+                f"and `{library_name}` ({library_version})."
             )
             warnings.warn(message)
 
--- python_modules/dagster/dagster_tests/core_tests/test_utils.py
+++ python_modules/dagster/dagster_tests/core_tests/test_utils.py
@@ -1,7 +1,10 @@
+import warnings
+
 import pytest
 
+import dagster.version
 from dagster._core.test_utils import environ
-from dagster._core.utils import parse_env_var
+from dagster._core.utils import check_dagster_package_version, parse_env_var
 
 
 def test_parse_env_var_no_equals():
@@ -26,3 +29,24 @@ def test_parse_env_var_containing_equals():
     env_var = "FOO_ENV_VAR=HERE_COMES_THE_EQUALS=THERE_IT_WENT"
 
     assert parse_env_var(env_var) == ("FOO_ENV_VAR", "HERE_COMES_THE_EQUALS=THERE_IT_WENT")
+
+
+def test_check_dagster_package_version(monkeypatch):
+
+    monkeypatch.setattr(dagster.version, "__version__", "1.1.0")
+
+    # Ensure no warning emitted
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        check_dagster_package_version("foo", "1.1.0")
+
+    # Lib version matching 1.1.0-- see dagster._utils.library_version_from_core_version
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        check_dagster_package_version("foo", "0.17.0")
+
+    with pytest.warns():
+        check_dagster_package_version("foo", "1.0.0")
+
+    with pytest.warns():
+        check_dagster_package_version("foo", "0.16.5")
