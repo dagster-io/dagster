d1b91a51e5a82bd3f758e4e1ef50932bfb61febc
yuhan <yuhan@elementl.com>
2022-08-05 18:04:06 -0700

    docs infra: new header + open graph img

---
+++ docs/next/components/SignInButton.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+export const SignInButton = () => {
+  const onClick = () => {
+    const url = new URL("https://dagster.cloud");
+    const anonymousId = window.analytics?.user().anonymousId();
+    if (anonymousId) {
+      const params = new URLSearchParams();
+      params.append("ajs_aid", anonymousId);
+      url.search = params.toString();
+    }
+    window.location.assign(url.toString());
+  };
+
+  return (
+    <button
+      className="hidden xl:block py-2 my-2 rounded-xl px-4 bg-lavender hover:text-gable-green-darker bg-opacity-0 hover:border-2 hover:bg-opacity-50 focus:outline-none focus:text-gable-green-darker transition duration-150 ease-in-out bg-transparent"
+      onClick={onClick}
+    >
+      Sign in
+    </button>
+  );
+};
--- docs/next/tailwind.config.js
+++ docs/next/tailwind.config.js
@@ -1,6 +1,4 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
-const { red } = require("tailwindcss/colors");
 
 module.exports = {
   important: "html",
@@ -24,39 +22,43 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Neue Montreal", ...defaultTheme.fontFamily.sans],
-        mono: ["Diatype Mono", ...defaultTheme.fontFamily.mono]
+        mono: ["Diatype Mono", ...defaultTheme.fontFamily.mono],
       },
       borderRadius: {
-        'xl': '36px'
+        xl: "36px",
       },
       boxShadow: {
-        inner: 'inset 5px 5px 15px 0 rgba(0, 0, 0, 0.05)',
+        inner: "inset 5px 5px 15px 0 rgba(0, 0, 0, 0.05)",
       },
       colors: {
-        gray: colors.trueGray, //remove later
-        cyan: colors.cyan, //remove later
-        "gable-green": "#163B36",
+        "gable-green": "#21463D",
+        "gable-green-08": "rgba(22, 59, 54, 0.08)",
+        "gable-green-darker": "#122F2B",
         "sea-foam": "#A7FFBF",
-        "blurple": "#4F43DD",
-        "lavender": "#DEDDFF",
+        blurple: "#4F43DD",
+        "blurple-darker": "#3F36B1",
+        lavender: "#DEDDFF",
+        "lavender-darker": "#C9C6FA",
         primary: {
-          "900":"#0E0CA7",
-          "500":"#4F43DD",
-          "300":"#B9B4F1",
-          "100": "#EDECFC"
+          900: "#09086E",
+          700: "#0E0CA7",
+          500: "#4F43DD",
+          300: "#B9B4F1",
+          100: "#EDECFC",
         },
         gray: {
-          "900": '#231F1B',
-          "800": '#3A3631',
-          "700": '#524E48',
-          "600": '#6B6762',
-          "500": '#86837F',
-          "400": '#A19D99',
-          "300": '#BDBAB7',
-          "200": '#DAD8D6',
-          "100": '#F5F4F2',
-          "50": '#FEFDFB'
-        }
+          900: "#231F1B",
+          800: "#3A3631",
+          700: "#524E48",
+          600: "#6B6762",
+          500: "#86837F",
+          400: "#A19D99",
+          300: "#BDBAB7",
+          200: "#DAD8D6",
+          150: "#F1F1EF",
+          100: "#F5F4F2",
+          50: "#FAF9F7",
+        },
       },
       animation: {
         wiggle: "wiggle 1s ease-in-out infinite",
@@ -104,7 +106,7 @@ module.exports = {
               backgroundColor: theme("colors.primary.100"),
               marginRight: 8,
             },
-            "a": {
+            a: {
               color: theme("colors.primary.900"),
               transition: ".3s all",
               textDecoration: "none",
@@ -116,14 +118,14 @@ module.exports = {
             ".scroll-margin-top a": {
               boxShadow: "none",
             },
-            "a strong":{
+            "a strong": {
               color: theme("colors.primary.900"),
             },
-            "a strong:hover":{
+            "a strong:hover": {
               textDecoration: "underline",
-            }
+            },
           },
-        }
+        },
       }),
     },
   },
