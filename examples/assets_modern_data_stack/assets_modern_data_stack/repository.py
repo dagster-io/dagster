6c03cad39d6e8d2ee3cd0c60cba66a47ee532611
OwenKephart <owen@elementl.com>
2022-08-08 18:48:21 -0700

    [mds-demo] add daily schedule job (#9308)

--- examples/assets_modern_data_stack/assets_modern_data_stack/repository.py
+++ examples/assets_modern_data_stack/assets_modern_data_stack/repository.py
@@ -1,7 +1,13 @@
 from dagster_airbyte import airbyte_resource
 from dagster_dbt import dbt_cli_resource
 
-from dagster import load_assets_from_package_module, repository, with_resources
+from dagster import (
+    ScheduleDefinition,
+    define_asset_job,
+    load_assets_from_package_module,
+    repository,
+    with_resources,
+)
 
 from . import assets
 from .db_io_manager import db_io_manager
@@ -10,11 +16,17 @@ from .utils.constants import AIRBYTE_CONFIG, DBT_CONFIG, POSTGRES_CONFIG
 
 @repository
 def assets_modern_data_stack():
-    return with_resources(
-        load_assets_from_package_module(assets),
-        resource_defs={
-            "airbyte": airbyte_resource.configured(AIRBYTE_CONFIG),
-            "dbt": dbt_cli_resource.configured(DBT_CONFIG),
-            "db_io_manager": db_io_manager.configured(POSTGRES_CONFIG),
-        },
-    )
+    return [
+        with_resources(
+            load_assets_from_package_module(assets),
+            resource_defs={
+                "airbyte": airbyte_resource.configured(AIRBYTE_CONFIG),
+                "dbt": dbt_cli_resource.configured(DBT_CONFIG),
+                "db_io_manager": db_io_manager.configured(POSTGRES_CONFIG),
+            },
+        ),
+        # update all assets once a day
+        ScheduleDefinition(
+            job=define_asset_job("all_assets", selection="*"), cron_schedule="@daily"
+        ),
+    ]
