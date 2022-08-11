18606c9cce936479d027ac15d49efb4bc6e7b73c
fahadkh <fahad@zephyrai.bio>
2022-08-11 15:57:21 -0500

    pylint

--- python_modules/libraries/dagster-shell/dagster_shell_tests/example_shell_command_utility.py
+++ python_modules/libraries/dagster-shell/dagster_shell_tests/example_shell_command_utility.py
@@ -7,6 +7,6 @@ from dagster import OpExecutionContext, op
 @op
 def my_shell_op(context: OpExecutionContext, data: str):
     temp_file = "/tmp/data.txt"
-    with open(temp_file, "w") as temp_file_writer:
+    with open(temp_file, "w", encoding="utf-8") as temp_file_writer:
         temp_file_writer.write(data)
         execute_shell_command(f"cat {temp_file}", output_logging="STREAM", log=context.log)
