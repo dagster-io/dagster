2da797a6217e4fcaa8d3bd53d3db273431fa9794
Chris DeCarolis <cdguitar817@gmail.com>
2022-08-11 18:10:14 -0700

    remove deprecated APIs from dagster-spark

--- python_modules/libraries/dagster-spark/dagster_spark/ops.py
+++ python_modules/libraries/dagster-spark/dagster_spark/ops.py
@@ -1,57 +1,28 @@
-from dagster import Nothing
+from dagster import In, Nothing, Out
 from dagster import _check as check
 from dagster import op
-from dagster._legacy import InputDefinition, OutputDefinition, solid
 
 from .configs import define_spark_config
 
 
-def create_spark_solid(
-    name, main_class, description=None, required_resource_keys=frozenset(["spark"])
-):
-    return core_create_spark(
-        dagster_decorator=solid,
-        name=name,
-        main_class=main_class,
-        description=description,
-        required_resource_keys=required_resource_keys,
-    )
-
-
 def create_spark_op(
     name, main_class, description=None, required_resource_keys=frozenset(["spark"])
-):
-    return core_create_spark(
-        dagster_decorator=op,
-        name=name,
-        main_class=main_class,
-        description=description,
-        required_resource_keys=required_resource_keys,
-    )
-
-
-def core_create_spark(
-    dagster_decorator,
-    name,
-    main_class,
-    description=None,
-    required_resource_keys=frozenset(["spark"]),
 ):
     check.str_param(name, "name")
     check.str_param(main_class, "main_class")
     check.opt_str_param(description, "description", "A parameterized Spark job.")
     check.set_param(required_resource_keys, "required_resource_keys")
 
-    @dagster_decorator(
+    @op(
         name=name,
         description=description,
         config_schema=define_spark_config(),
-        input_defs=[InputDefinition("start", Nothing)],
-        output_defs=[OutputDefinition(Nothing)],
+        ins={"start": In(Nothing)},
+        out=Out(Nothing),
         tags={"kind": "spark", "main_class": main_class},
         required_resource_keys=required_resource_keys,
     )
-    def spark_solid(context):  # pylint: disable=unused-argument
-        context.resources.spark.run_spark_job(context.solid_config, main_class)
+    def spark_op(context):  # pylint: disable=unused-argument
+        context.resources.spark.run_spark_job(context.op_config, main_class)
 
-    return spark_solid
+    return spark_op
