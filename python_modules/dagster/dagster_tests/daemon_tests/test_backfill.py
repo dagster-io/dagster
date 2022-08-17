36cb0e2e4e9e390b61e4bf7222daedf68456f6e5
Owen Kephart <owen@elementl.com>
2022-08-17 14:22:55 -0700

    bump timeout

--- python_modules/dagster/dagster_tests/daemon_tests/test_backfill.py
+++ python_modules/dagster/dagster_tests/daemon_tests/test_backfill.py
@@ -749,9 +749,9 @@ def test_backfill_with_asset_selection():
                 instance, workspace, get_default_daemon_logger("BackfillDaemon")
             )
         )
-        wait_for_all_runs_to_start(instance)
+        wait_for_all_runs_to_start(instance, timeout=30)
         assert instance.get_runs_count() == 3
-        wait_for_all_runs_to_finish(instance, timeout=45)
+        wait_for_all_runs_to_finish(instance, timeout=30)
 
         assert instance.get_runs_count() == 3
         runs = reversed(instance.get_runs())
