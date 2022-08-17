9da8d861959397eb2eaebe4ad7c6ebc8ec91dfaf
Claire Lin <claire@elementl.com>
2022-08-17 15:24:13 -0700

    add new page

--- docs/next/pages/search.tsx
+++ docs/next/pages/search.tsx
@@ -19,7 +19,13 @@ import visit from "unist-util-visit";
 import { Shimmer } from "components/Shimmer";
 import algoliasearch from "algoliasearch";
 import { useCallback, useEffect, useRef, useState } from "react";
-import { Highlight } from "react-instantsearch-dom";
+import {
+  Highlight,
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+} from "react-instantsearch-dom";
 
 const client = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
@@ -27,21 +33,69 @@ const client = algoliasearch(
 );
 
 const index = client.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME);
-// index.setSettings({ attributesToHighlight: ["*"] });
+
+function HitComponent(hit) {
+  function parsedLevel(name) {
+    if (name == "Title" || name == null) {
+      return "";
+    }
+    return name;
+  }
+
+  const lvl0 = parsedLevel(hit.hierarchy.lvl0);
+  const lvl1 = parsedLevel(hit.hierarchy.lvl1);
+  const lvl2 = parsedLevel(hit.hierarchy.lvl2);
+
+  let highlightAttr = hit.content != null ? "content" : null;
+
+  let section = null;
+
+  for (const [key, val] of Object.entries(hit.hierarchy)) {
+    if (val != "Title" && val != null) {
+      section = `hierarchy.${key}`;
+    }
+  }
+
+  const path = `${lvl0} ${lvl1 ? "|" : ""} ${lvl1} ${lvl2 ? "|" : ""} ${lvl2}`;
+
+  const a = document.createElement("a");
+  a.href = hit.url;
+  const hash = a.hash === "#content-wrapper" ? "" : a.hash;
+  let url = `${a.pathname}${hash}`;
+
+  return (
+    <a href={url}>
+      <div className="SearchHit">
+        <Highlight hit={hit} attribute={section} tagName="mark" />
+        <p className="SearchPath">{path}</p>
+        {highlightAttr && (
+          <Highlight
+            hit={hit}
+            attribute={highlightAttr}
+            tagName="mark"
+            className="SearchSnippet"
+          />
+        )}
+      </div>
+    </a>
+  );
+}
+
+const Hit = ({ hit }) => HitComponent(hit);
 
 export default function SearchPage() {
-  const [query, setQuery] = useState("");
-  const [hits, setHits] = useState([]);
-  useEffect(() => {
-    const path = window.location.href;
-    const urlQuery = decodeURIComponent(path.substring(path.indexOf("=") + 1));
+  // const [query, setQuery] = useState("");
+  // const [hits, setHits] = useState([]);
+  // useEffect(() => {
+  //   const path = window.location.href;
+  //   const urlQuery = decodeURIComponent(path.substring(path.indexOf("=") + 1));
 
-    setQuery(urlQuery);
-    index.search(urlQuery, { hitsPerPage: 50 }).then(({ hits }) => {
-      console.log(hits);
-      setHits(hits);
-    });
-  }, []);
+  //   setQuery(urlQuery);
+  //   index.search(urlQuery, { hitsPerPage: 50 }).then(({ hits }) => {
+  //     console.log(hits);
+  //     setHits(hits);
+  //   });
+  // }, []);
 
   const router = useRouter();
 
@@ -51,48 +105,16 @@ export default function SearchPage() {
     return <Shimmer />;
   }
 
-  function parsedLevel(name) {
-    if (name == "Title" || name == null) {
-      return "";
-    }
-    return name;
-  }
-
-  function renderHit(hit) {
-    const hierarchy = hit._highlightResult.hierarchy_camel[0];
-    let highlightedLevel = "";
-    for (let key in hierarchy) {
-      if (hierarchy[key].matchedWords.length > 0) {
-        highlightedLevel = key;
-        break;
-      }
-    }
-
-    const lvl0 = parsedLevel(hit.hierarchy.lvl0);
-    const lvl1 = parsedLevel(hit.hierarchy.lvl1);
-    const lvl2 = parsedLevel(hit.hierarchy.lvl2);
-
-    const page = `${lvl0} ${lvl1 ? ">" : ""} ${lvl1} ${
-      lvl2 ? ">" : ""
-    } ${lvl2}`;
-
-    // let element = document.create
-    return <Highlight hit={hit} attribute="name" />;
-  }
-
-  function onChange(e) {
-    setQuery(e.target.value);
-    index.search(query, { hitsPerPage: 50 }).then(({ hits }) => {
-      setHits(hits);
-    });
-  }
-
   return (
-    <div>
-      <input type="text" value={query} onChange={onChange} />
-      {hits.map((hit) => {
-        return renderHit(hit);
-      })}
+    <div className="w-full py-4">
+      <InstantSearch
+        searchClient={client}
+        indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME}
+      >
+        <SearchBox />
+        <Hits hitComponent={Hit} />
+        <Pagination className="mt-5 mb-5" />
+      </InstantSearch>
     </div>
   );
 }
