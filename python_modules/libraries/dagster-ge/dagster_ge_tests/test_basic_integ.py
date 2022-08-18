14dd488147957a55962cd71ff70900cbbf8dfc64
Christopher DeCarolis <cdguitar817@gmail.com>
2022-08-17 17:14:39 -0700

    Move dagster-ge onto new APIs (#9363)
    ### Summary & Motivation
    
    ### How I Tested These Changes

--- python_modules/libraries/dagster-ge/dagster_ge_tests/test_basic_integ.py
+++ python_modules/libraries/dagster-ge/dagster_ge_tests/test_basic_integ.py
@@ -10,18 +10,16 @@ from dagster_pyspark import DataFrame as DagsterPySparkDataFrame
 from dagster_pyspark import pyspark_resource
 from pandas import read_csv
 
-from dagster import Output, reconstructable
-from dagster._core.test_utils import instance_for_test
-from dagster._legacy import InputDefinition, ModeDefinition, execute_pipeline, pipeline, solid
+from dagster import In, Output, job, op
 from dagster._utils import file_relative_path
 
 
-@solid
+@op
 def pandas_yielder(_):
     return read_csv(file_relative_path(__file__, "./basic.csv"))
 
 
-@solid(required_resource_keys={"pyspark"})
+@op(required_resource_keys={"pyspark"})
 def pyspark_yielder(context):
     return (
         context.resources.pyspark.spark_session.read.format("csv")
@@ -30,27 +28,23 @@ def pyspark_yielder(context):
     )
 
 
-@solid(input_defs=[InputDefinition(name="res")])
+@op(ins={"res": In()})
 def reyielder(_context, res):
     yield Output((res["statistics"], res["results"]))
 
 
-@pipeline(
-    mode_defs=[ModeDefinition("basic", resource_defs={"ge_data_context": ge_data_context})],
-)
-def hello_world_pandas_pipeline_v2():
-    return reyielder(
-        ge_validation_op_factory("ge_validation_solid", "getest", "basic.warning")(pandas_yielder())
+@job(resource_defs={"ge_data_context": ge_data_context})
+def hello_world_pandas_job_v2():
+    reyielder(
+        ge_validation_op_factory("ge_validation_op", "getest", "basic.warning")(pandas_yielder())
     )
 
 
-@pipeline(
-    mode_defs=[ModeDefinition("basic", resource_defs={"ge_data_context": ge_data_context})],
-)
-def hello_world_pandas_pipeline_v3():
-    return reyielder(
+@job(resource_defs={"ge_data_context": ge_data_context})
+def hello_world_pandas_job_v3():
+    reyielder(
         ge_validation_op_factory_v3(
-            name="ge_validation_solid",
+            name="ge_validation_op",
             datasource_name="getest",
             data_connector_name="my_runtime_data_connector",
             data_asset_name="test_asset",
@@ -60,57 +54,44 @@ def hello_world_pandas_pipeline_v3():
     )
 
 
-@pipeline(
-    mode_defs=[
-        ModeDefinition(
-            "basic",
-            resource_defs={
-                "ge_data_context": ge_data_context,
-                "pyspark": pyspark_resource,
-            },
-        )
-    ],
+@job(
+    resource_defs={
+        "ge_data_context": ge_data_context,
+        "pyspark": pyspark_resource,
+    }
 )
-def hello_world_pyspark_pipeline():
+def hello_world_pyspark_job():
     validate = ge_validation_op_factory(
-        "ge_validation_solid",
+        "ge_validation_op",
         "getestspark",
         "basic.warning",
         input_dagster_type=DagsterPySparkDataFrame,
     )
-    return reyielder(validate(pyspark_yielder()))
+    reyielder(validate(pyspark_yielder()))
 
 
 @pytest.mark.parametrize(
-    "pipe, ge_dir",
+    "job_def, ge_dir",
     [
-        (hello_world_pandas_pipeline_v2, "./great_expectations"),
-        (hello_world_pandas_pipeline_v3, "./great_expectations_v3"),
+        (hello_world_pandas_job_v2, "./great_expectations"),
+        (hello_world_pandas_job_v3, "./great_expectations_v3"),
     ],
 )
-def test_yielded_results_config_pandas(snapshot, pipe, ge_dir):
+def test_yielded_results_config_pandas(snapshot, job_def, ge_dir):
     run_config = {
         "resources": {
             "ge_data_context": {"config": {"ge_root_dir": file_relative_path(__file__, ge_dir)}}
         }
     }
-    with instance_for_test() as instance:
-        result = execute_pipeline(
-            reconstructable(pipe),
-            run_config=run_config,
-            mode="basic",
-            instance=instance,
-        )
-        assert result.result_for_solid("reyielder").output_value()[0]["success_percent"] == 100
-        expectations = result.result_for_solid(
-            "ge_validation_solid"
-        ).expectation_results_during_compute
-        assert len(expectations) == 1
-        mainexpect = expectations[0]
-        assert mainexpect.success
-        # purge system specific metadata for testing
-        metadata = mainexpect.metadata_entries[0].entry_data.md_str.split("### Info")[0]
-        snapshot.assert_match(metadata)
+    result = job_def.execute_in_process(run_config=run_config)
+    assert result.output_for_node("reyielder")[0]["success_percent"] == 100
+    expectations = result.expectation_results_for_node("ge_validation_op")
+    assert len(expectations) == 1
+    mainexpect = expectations[0]
+    assert mainexpect.success
+    # purge system specific metadata for testing
+    metadata = mainexpect.metadata_entries[0].entry_data.md_str.split("### Info")[0]
+    snapshot.assert_match(metadata)
 
 
 def test_yielded_results_config_pyspark_v2(snapshot):  # pylint:disable=unused-argument
@@ -121,20 +102,12 @@ def test_yielded_results_config_pyspark_v2(snapshot):  # pylint:disable=unused-a
             }
         }
     }
-    with instance_for_test() as instance:
-        result = execute_pipeline(
-            reconstructable(hello_world_pyspark_pipeline),
-            run_config=run_config,
-            mode="basic",
-            instance=instance,
-        )
-        assert result.result_for_solid("reyielder").output_value()[0]["success_percent"] == 100
-        expectations = result.result_for_solid(
-            "ge_validation_solid"
-        ).expectation_results_during_compute
-        assert len(expectations) == 1
-        mainexpect = expectations[0]
-        assert mainexpect.success
-        # purge system specific metadata for testing
-        metadata = mainexpect.metadata_entries[0].entry_data.md_str.split("### Info")[0]
-        snapshot.assert_match(metadata)
+    result = hello_world_pyspark_job.execute_in_process(run_config=run_config)
+    assert result.output_for_node("reyielder")[0]["success_percent"] == 100
+    expectations = result.expectation_results_for_node("ge_validation_op")
+    assert len(expectations) == 1
+    mainexpect = expectations[0]
+    assert mainexpect.success
+    # purge system specific metadata for testing
+    metadata = mainexpect.metadata_entries[0].entry_data.md_str.split("### Info")[0]
+    snapshot.assert_match(metadata)
