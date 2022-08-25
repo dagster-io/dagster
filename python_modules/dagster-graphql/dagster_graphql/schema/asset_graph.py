ae250e7b6b63bc0aed583acdec55b657824f39b7
Ben Gotow <ben@foundry376.com>
2022-08-25 15:17:16 -0500

    [dagit] Treat duplicate asset definitions as a fatal error when launching materializations (#9453)
    * [dagit] More aggressively error when duplicate asset definitions exist
    
    * [dagit] More aggressively error when duplicate asset definitions exist
    
    Co-authored-by: bengotow <bgotow@elementl.com>

--- python_modules/dagster-graphql/dagster_graphql/schema/asset_graph.py
+++ python_modules/dagster-graphql/dagster_graphql/schema/asset_graph.py
@@ -92,6 +92,14 @@ class GrapheneAssetLatestInfo(graphene.ObjectType):
         name = "AssetLatestInfo"
 
 
+class GrapheneAssetNodeDefinitionCollision(graphene.ObjectType):
+    assetKey = graphene.NonNull(GrapheneAssetKey)
+    repositories = non_null_list(lambda: external.GrapheneRepository)
+
+    class Meta:
+        name = "AssetNodeDefinitionCollision"
+
+
 class GrapheneAssetNode(graphene.ObjectType):
 
     _depended_by_loader: Optional[CrossRepoAssetDependedByLoader]
