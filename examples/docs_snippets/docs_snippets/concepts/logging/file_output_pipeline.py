93deab01d08dbdbfe9cf3d756632df27fcf630d1
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2022-09-07 09:17:41 -0700

    docs: no pipelines/solids in python logging examples (#9608)
    ### Summary & Motivation
    resolves https://github.com/dagster-io/dagster/issues/9595
    ### How I Tested These Changes
    https://dagster-git-yuhan-09-06-docsnopipelinessolidsin-dde98c-elementl.vercel.app/concepts/logging/python-logging#example-outputting-dagster-logs-to-a-file

--- examples/docs_snippets/docs_snippets/concepts/logging/file_output_pipeline.py
+++ examples/docs_snippets/docs_snippets/concepts/logging/file_output_pipeline.py
@@ -1,17 +1,17 @@
 # isort: skip_file
-from dagster._legacy import pipeline, solid
+from dagster import job, op
 
 # start_custom_file_output_log
 
 
-@solid
-def file_log_solid(context):
+@op
+def file_log_op(context):
     context.log.info("Hello world!")
 
 
-@pipeline
-def file_log_pipeline():
-    file_log_solid()
+@job
+def file_log_job():
+    file_log_op()
 
 
 # end_custom_file_output_log
