690cbcd34c95026852a417424ea5009044de009e
yuhan <yuhan@elementl.com>
2022-09-06 21:25:46 -0700

    docs: no pipelines/solids in python logging examples

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/logging_tests/test_instance_config.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/logging_tests/test_instance_config.py
@@ -5,9 +5,7 @@ from dagster._utils import file_relative_path
 
 def test_valid_managed_loggers_instance_yaml():
     ref = InstanceRef.from_dir(
-        base_dir=file_relative_path(
-            __file__, "../../../docs_snippets/concepts/logging"
-        ),
+        base_dir=file_relative_path(__file__, "../../../docs_snippets/concepts/logging"),
         config_filename="python_logging_managed_loggers_config.yaml",
     )
     instance = DagsterInstance.from_ref(ref)
@@ -16,9 +14,7 @@ def test_valid_managed_loggers_instance_yaml():
 
 def test_valid_log_level_instance_yaml():
     ref = InstanceRef.from_dir(
-        base_dir=file_relative_path(
-            __file__, "../../../docs_snippets/concepts/logging"
-        ),
+        base_dir=file_relative_path(__file__, "../../../docs_snippets/concepts/logging"),
         config_filename="python_logging_python_log_level_config.yaml",
     )
     instance = DagsterInstance.from_ref(ref)
@@ -27,10 +23,17 @@ def test_valid_log_level_instance_yaml():
 
 def test_valid_handler_instance_yaml():
     ref = InstanceRef.from_dir(
-        base_dir=file_relative_path(
-            __file__, "../../../docs_snippets/concepts/logging"
-        ),
+        base_dir=file_relative_path(__file__, "../../../docs_snippets/concepts/logging"),
         config_filename="python_logging_handler_config.yaml",
+    )
+    instance = DagsterInstance.from_ref(ref)
+    assert len(instance.get_handlers()) == 2
+
+
+def test_valid_file_instance_yaml():
+    ref = InstanceRef.from_dir(
+        base_dir=file_relative_path(__file__, "../../../docs_snippets/concepts/logging"),
+        config_filename="python_logging_file_output_config.yaml",
     )
     instance = DagsterInstance.from_ref(ref)
     assert len(instance.get_handlers()) == 2
