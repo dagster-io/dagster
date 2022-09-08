8f674c4c69b3492f3cb59c4462b8e37041a923f5
elementl <devtools@elementl.com>
2022-09-08 20:04:03 +0000

    1.0.8

--- python_modules/libraries/dagster-pyspark/setup.py
+++ python_modules/libraries/dagster-pyspark/setup.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
         ],
         packages=find_packages(exclude=["dagster_pyspark_tests*"]),
         install_requires=[
-            f"dagster{pin}",
-            f"dagster_spark{pin}",
+            "dagster==1.0.8",
+            "dagster_spark==0.16.8",
             # Pyspark 2.x is incompatible with Python 3.8+
             'pyspark>=3.0.0; python_version >= "3.8"',
             'pyspark>=2.0.2; python_version < "3.8"',
