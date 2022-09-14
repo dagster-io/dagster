968a3c3fc84c6b902f94e560084d27a0b05beb60
Ben Pankow <ben@elementl.com>
2022-09-14 14:03:26 -0700

    [dagster-airbyte] Support representing normalization-created tables as Assets (#9655)
    ## Summary
    
    Adds two new optional parameters to `build_airbyte_assets`.
    
    - `upstream_assets` optionally allows a user to specify one or more upstream `AssetKey`s that the Airbyte assets should depend on, this can be used to e.g. represent the source data dependency.
    - `normalization_tables` is a Mapping which maps a destination table to a set of one or more derived tables created by the [Airbyte basic normalization](https://docs.airbyte.com/understanding-airbyte/basic-normalization/#nesting) process. These normalization-generated tables will be shown as Assets downstream of their parent table, and materializations will be triggered by the parent table materializing.
    
    <img width="695" alt="Screen Shot 2022-09-09 at 4 14 47 PM" src="https://user-images.githubusercontent.com/10215173/189457596-99ecde10-982c-4b8f-8019-0c04673b1c67.png">
    
    
    
    ## Test Plan
    
    Tested locally; new unit test.

--- python_modules/libraries/dagster-airbyte/dagster_airbyte/asset_defs.py
+++ python_modules/libraries/dagster-airbyte/dagster_airbyte/asset_defs.py
@@ -1,4 +1,5 @@
-from typing import List, Optional
+from itertools import chain
+from typing import List, Mapping, Optional, Set
 
 from dagster_airbyte.utils import generate_materializations
 
@@ -13,6 +14,8 @@ def build_airbyte_assets(
     connection_id: str,
     destination_tables: List[str],
     asset_key_prefix: Optional[List[str]] = None,
+    normalization_tables: Optional[Mapping[str, Set[str]]] = None,
+    upstream_assets: Optional[Set[AssetKey]] = None,
 ) -> List[AssetsDefinition]:
     """
     Builds a set of assets representing the tables created by an Airbyte sync operation.
@@ -23,18 +26,41 @@ def build_airbyte_assets(
         destination_tables (List[str]): The names of the tables that you want to be represented
             in the Dagster asset graph for this sync. This will generally map to the name of the
             stream in Airbyte, unless a stream prefix has been specified in Airbyte.
+        normalization_tables (Optional[Mapping[str, List[str]]]): If you are using Airbyte's
+            normalization feature, you may specify a mapping of destination table to a list of
+            derived tables that will be created by the normalization process.
         asset_key_prefix (Optional[List[str]]): A prefix for the asset keys inside this asset.
             If left blank, assets will have a key of `AssetKey([table_name])`.
+        upstream_assets (Optional[Set[AssetKey]]): A list of assets to add as sources.
     """
 
     asset_key_prefix = check.opt_list_param(asset_key_prefix, "asset_key_prefix", of_type=str)
 
+    # Generate a list of outputs, the set of destination tables plus any affiliated
+    # normalization tables
+    tables = chain.from_iterable(
+        chain([destination_tables], normalization_tables.values() if normalization_tables else [])
+    )
+    outputs = {table: AssetOut(key=AssetKey(asset_key_prefix + [table])) for table in tables}
+
+    internal_deps = {}
+
+    # If normalization tables are specified, we need to add a dependency from the destination table
+    # to the affilitated normalization table
+    if normalization_tables:
+        for base_table, derived_tables in normalization_tables.items():
+            for derived_table in derived_tables:
+                internal_deps[derived_table] = {AssetKey(asset_key_prefix + [base_table])}
+
+    # All non-normalization tables depend on any user-provided upstream assets
+    for table in destination_tables:
+        internal_deps[table] = upstream_assets or set()
+
     @multi_asset(
         name=f"airbyte_sync_{connection_id[:5]}",
-        outs={
-            table: AssetOut(key=AssetKey(asset_key_prefix + [table]))
-            for table in destination_tables
-        },
+        non_argument_deps=upstream_assets or set(),
+        outs=outputs,
+        internal_asset_deps=internal_deps,
         required_resource_keys={"airbyte"},
         compute_kind="airbyte",
     )
@@ -50,6 +76,14 @@ def build_airbyte_assets(
                         entry.label: entry.entry_data for entry in materialization.metadata_entries
                     },
                 )
+                # Also materialize any normalization tables affiliated with this destination
+                # e.g. nested objects, lists etc
+                if normalization_tables:
+                    for dependent_table in normalization_tables.get(table_name, set()):
+                        yield Output(
+                            value=None,
+                            output_name=dependent_table,
+                        )
             else:
                 yield materialization
 
