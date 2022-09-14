274449b183e07ef221fb8930cb9ef536a89882c1
Ben Pankow <ben@elementl.com>
2022-09-14 14:03:26 -0700

    [dagster-airbyte] Support representing normalization-created tables as Assets (#9655)
    ## Summary
    
    Adds two new optional parameters to `build_airbyte_assets`.
    
    - `upstream_assets` optionally allows a user to specify one or more upstream `AssetKey`s that the Airbyte assets should depend on, this can be used to e.g. represent the source data dependency.
    - `normalization_tables` is a Mapping which maps a destination table to a set of one or more derived tables created by the [Airbyte basic normalization](https://docs.airbyte.com/understanding-airbyte/basic-normalization/#nesting) process. These normalization-generated tables will be shown as Assets downstream of their parent table, and materializations will be triggered by the parent table materializing.
    
    <img width="695" alt="Screen Shot 2022-09-09 at 4 14 47 PM" src="https://user-images.githubusercontent.com/10215173/189457596-99ecde10-982c-4b8f-8019-0c04673b1c67.png">
    
    
    
    ## Test Plan
    
    Tested locally; new unit test.

--- python_modules/libraries/dagster-airbyte/dagster_airbyte_tests/test_asset_defs.py
+++ python_modules/libraries/dagster-airbyte/dagster_airbyte_tests/test_asset_defs.py
@@ -3,6 +3,7 @@ import responses
 from dagster_airbyte import airbyte_resource, build_airbyte_assets
 
 from dagster import AssetKey, MetadataEntry, TableColumn, TableSchema, build_init_resource_context
+from dagster._core.definitions.source_asset import SourceAsset
 from dagster._legacy import build_assets_job
 
 from .utils import get_sample_connection_json, get_sample_job_json
@@ -91,3 +92,102 @@ def test_assets(schema_prefix):
         )
         in materializations[0].metadata_entries
     )
+
+
+@responses.activate
+@pytest.mark.parametrize("schema_prefix", ["", "the_prefix_"])
+@pytest.mark.parametrize("source_asset", [None, "my_source_asset_key"])
+def test_assets_with_normalization(schema_prefix, source_asset):
+
+    ab_resource = airbyte_resource(
+        build_init_resource_context(
+            config={
+                "host": "some_host",
+                "port": "8000",
+            }
+        )
+    )
+    destination_tables = ["foo", "bar"]
+    if schema_prefix:
+        destination_tables = [schema_prefix + t for t in destination_tables]
+
+    bar_normalization_tables = {schema_prefix + "bar_baz", schema_prefix + "bar_qux"}
+    ab_assets = build_airbyte_assets(
+        "12345",
+        destination_tables=destination_tables,
+        normalization_tables={destination_tables[1]: bar_normalization_tables},
+        asset_key_prefix=["some", "prefix"],
+        upstream_assets={AssetKey(source_asset)} if source_asset else None,
+    )
+
+    assert ab_assets[0].keys == {AssetKey(["some", "prefix", t]) for t in destination_tables} | {
+        AssetKey(["some", "prefix", t]) for t in bar_normalization_tables
+    }
+    assert len(ab_assets[0].op.output_defs) == 4
+
+    responses.add(
+        method=responses.POST,
+        url=ab_resource.api_base_url + "/connections/get",
+        json=get_sample_connection_json(prefix=schema_prefix),
+        status=200,
+    )
+    responses.add(
+        method=responses.POST,
+        url=ab_resource.api_base_url + "/connections/sync",
+        json={"job": {"id": 1}},
+        status=200,
+    )
+    responses.add(
+        method=responses.POST,
+        url=ab_resource.api_base_url + "/jobs/get",
+        json=get_sample_job_json(schema_prefix=schema_prefix),
+        status=200,
+    )
+
+    ab_job = build_assets_job(
+        "ab_job",
+        ab_assets,
+        source_assets=[SourceAsset(AssetKey(source_asset))] if source_asset else None,
+        resource_defs={
+            "airbyte": airbyte_resource.configured(
+                {
+                    "host": "some_host",
+                    "port": "8000",
+                }
+            )
+        },
+    )
+
+    res = ab_job.execute_in_process()
+
+    materializations = [
+        event.event_specific_data.materialization
+        for event in res.events_for_node("airbyte_sync_12345")
+        if event.event_type_value == "ASSET_MATERIALIZATION"
+    ]
+    assert len(materializations) == 5
+    assert {m.asset_key for m in materializations} == {
+        AssetKey(["some", "prefix", schema_prefix + "foo"]),
+        AssetKey(["some", "prefix", schema_prefix + "bar"]),
+        AssetKey(["some", "prefix", schema_prefix + "baz"]),
+        # Normalized materializations are there
+        AssetKey(["some", "prefix", schema_prefix + "bar_baz"]),
+        AssetKey(["some", "prefix", schema_prefix + "bar_qux"]),
+    }
+    assert MetadataEntry("bytesEmitted", value=1234) in materializations[0].metadata_entries
+    assert MetadataEntry("recordsCommitted", value=4321) in materializations[0].metadata_entries
+    assert (
+        MetadataEntry(
+            "schema",
+            value=TableSchema(
+                columns=[
+                    TableColumn(name="a", type="str"),
+                    TableColumn(name="b", type="int"),
+                ]
+            ),
+        )
+        in materializations[0].metadata_entries
+    )
+
+    # No metadata for normalized materializations, for now
+    assert not materializations[3].metadata_entries
