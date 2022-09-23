75fbce0a99fe7506a01f58aa2320bd314fa1a1ca
Sean Mackesey <s.mackesey@gmail.com>
2022-09-23 09:12:10 -0400

    fix

--- python_modules/automation/automation/scaffold/cli.py
+++ python_modules/automation/automation/scaffold/cli.py
@@ -8,31 +8,12 @@ from automation.git import get_most_recent_git_tag
 
 from .utils import copy_directory
 
-EXAMPLES_JSON_PATH = "docs/next/src/pages/examples/examples.json"
-
 # Location of the template assets used here
 ASSETS_PATH = os.path.join(os.path.dirname(__file__), "assets")
 
 CLI_HELP = """This CLI is used for generating new skeleton examples or libraries with all of the
 necessary files like tox.ini, etc.
 """
-
-
-def add_to_examples_json(name: str) -> None:
-    with open(EXAMPLES_JSON_PATH, "r", encoding="utf8") as examples_file:
-        examples = json.load(examples_file)
-
-    if name in {example["name"] for example in examples}:
-        raise click.UsageError(
-            "Example with name {name} already exists in {path}".format(
-                name=name, path=EXAMPLES_JSON_PATH
-            )
-        )
-
-    examples.append({"name": name, "title": "", "description": ""})
-
-    with open(EXAMPLES_JSON_PATH, "w", encoding="utf8") as examples_file:
-        json.dump(examples, examples_file, indent=4)
 
 
 @click.group(help=CLI_HELP)
@@ -84,22 +65,16 @@ def library(name: str):
 def example(name):
     """Scaffolds a Dagster example in the top-level examples/ folder."""
     template_library_path = os.path.join(ASSETS_PATH, "dagster-example-tmpl")
-    new_template_library_path = os.path.abspath("examples/{name}".format(name=name))
-    doc_path = os.path.abspath("./docs/next/src/pages/examples/{name}.mdx".format(name=name))
+    new_template_example_path = os.path.abspath("examples/{name}".format(name=name))
 
-    if os.path.exists(new_template_library_path):
+    if os.path.exists(new_template_example_path):
         raise click.UsageError("Example with name {name} already exists".format(name=name))
 
-    if os.path.exists(doc_path):
-        raise click.UsageError("Docs page already exists: {doc_path}".format(doc_path=doc_path))
-
-    add_to_examples_json(name)
-
-    copy_directory(template_library_path, new_template_library_path)
+    copy_directory(template_library_path, new_template_example_path)
 
     version = get_most_recent_git_tag()
 
-    for dname, _, files in os.walk(new_template_library_path):
+    for dname, _, files in os.walk(new_template_example_path):
         for fname in files:
             fpath = os.path.join(dname, fname)
             with open(fpath, encoding="utf8") as f:
@@ -117,11 +92,7 @@ def example(name):
         new_dname = dname.replace("example-tmpl", name)
         shutil.move(dname, new_dname)
 
-    shutil.move(os.path.join(new_template_library_path, "README.mdx"), doc_path)
-
-    print("Example created at {path}".format(path=new_template_library_path))
-    print("Documentation stub created at {path}".format(path=doc_path))
-    print("Added metadata to {path}".format(path=EXAMPLES_JSON_PATH))
+    print("Example created at {path}".format(path=new_template_example_path))
 
 
 def main() -> None:
