c9200befc13a45f115546842e671762b0985b1e1
Jordan Sanders <jordan@elementl.com>
2022-09-29 14:50:19 -0500

    fixup! Pass through all BUILDKITE* envvars

--- .buildkite/dagster-buildkite/dagster_buildkite/step_builder.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/step_builder.py
@@ -74,7 +74,7 @@ class CommandStepBuilder:
         settings["network"] = "kind"
 
         # Pass through all BUILDKITE* envvars so our test analytics are properly tagged
-        buildkite_envvars = [env for env in list(os.environ.keys) if env.startswith("BUILDKITE")]
+        buildkite_envvars = [env for env in list(os.environ.keys()) if env.startswith("BUILDKITE")]
 
         # Set PYTEST_DEBUG_TEMPROOT to our mounted /tmp volume. Any time the
         # pytest `tmp_path` or `tmpdir` fixtures are used used, the temporary
