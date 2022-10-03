0067286a89aa72f3fe99e78a32833bd56700186b
JamieDeMaria <jamie@elementl.com>
2022-10-03 16:11:10 -0400

    typo

--- python_modules/dagster-test/dagster_test/toys/asset_sensors.py
+++ python_modules/dagster-test/dagster_test/toys/asset_sensors.py
@@ -173,7 +173,7 @@ def get_asset_sensors_repo():
         runs_long,
         waits,
         build_asset_reconciliation_sensor(
-            selection=AssetSelection.assets(downstream, waits),
+            asset_selection=AssetSelection.assets(downstream, waits),
             name="generated_sensor",
             wait_for_all_upstream=True,
             wait_for_in_progress_runs=True,
