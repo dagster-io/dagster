448a7ef0bed1b2fb54c01a84d64b4f0f5fc8161b
Sean Mackesey <s.mackesey@gmail.com>
2022-10-05 16:07:45 -0400

    updates

--- examples/assets_reconciliation/assets_reconciliation/repo.py
+++ examples/assets_reconciliation/assets_reconciliation/repo.py
@@ -1,42 +1,53 @@
 import datetime
-from dagster import asset, repository, source_asset, Field
 
-from .lib import (
-    BollingerBandsDgType,
-    StockPricesDgType,
-    compute_anomalous_events,
-    compute_bollinger_bands_multi,
-    load_sp500_prices,
-)
+from dagster import AssetIn, Field, asset, input_manager, repository, source_asset, with_resources
 
+# from .lib import (
+#     BollingerBandsDgType,
+#     StockPricesDgType,
+#     compute_anomalous_events,
+#     compute_bollinger_bands_multi,
+#     load_dummy_source,
+# )
 
 
 @source_asset
-def sp500_prices(context):
-    return datetime.datetime.now()
+def dummy_source(_context):
+    for _ in range(10):
+        print("RUNNING SOURCE ASSET VERSION FN")
+    return str(datetime.datetime.now())
+
+
+@input_manager
+def source_asset_input_manager():
+    return 100
+
 
 @asset(
-    dagster_type=BollingerBandsDgType,
-    config_schema={
-        "rate": Field(int, default_value=30, description="Size of sliding window in days"),
-        "sigma": Field(
-            float, default_value=2.0, description="Width of envelope in standard deviations"
-        ),
-    },
-    metadata={"owner": "alice@example.com"},
-    reconcile=True,
+    # dagster_type=BollingerBandsDgType,
+    ins={"dummy_source": AssetIn(input_manager_key="source_asset_input_manager")},
+    # config_schema={
+    #     "rate": Field(int, default_value=30, description="Size of sliding window in days"),
+    #     "sigma": Field(
+    #         float, default_value=2.0, description="Width of envelope in standard deviations"
+    #     ),
+    # },
+    # metadata={"owner": "alice@example.com"},
+    versioned=True,
 )
-def sp500_bollinger_bands(context, sp500_prices):
+def dummy_asset(context, dummy_source):
     """Bollinger bands for the S&amp;P 500 stock prices."""
-    return compute_bollinger_bands_multi(
-        sp500_prices, rate=context.op_config["rate"], sigma=context.op_config["sigma"]
-    )
-
+    print("RUNNING $$$DUMMY ASSET$$$")
+    return dummy_source + 100
 
 
 @repository
 def repo():
     return [
-        sp500_prices,
-        sp500_bollinger_bands
+        # dummy_source,
+        # dummy_asset,
+        *with_resources(
+            [dummy_source, dummy_asset],
+            {"source_asset_input_manager": source_asset_input_manager},
+        )
     ]
