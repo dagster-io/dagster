c6db186c00530b332f1409656a4d96cc35c8c2cb
Joseph Van Drunen <josephvandrunen@gmail.com>
2022-10-06 13:39:50 -0400

    wip

--- examples/docs_snippets/docs_snippets/integrations/airflow/operator.py
+++ examples/docs_snippets/docs_snippets/integrations/airflow/operator.py
@@ -4,13 +4,13 @@ from airflow import DAG
 from dagster_airflow import DagsterCloudOperator
 
 with DAG(
-    dag_id='dagster_cloud',
+    dag_id="dagster_cloud",
     start_date=datetime(2022, 5, 28),
-    schedule_interval='*/5 * * * *',
+    schedule_interval="*/5 * * * *",
     catchup=False,
 ) as dag:
     DagsterCloudOperator(
-        task_id='new_dagster_assets',
+        task_id="new_dagster_assets",
         repostitory_location_name="example_location",
         repository_name="my_dagster_project",
         job_name="all_assets_job",
