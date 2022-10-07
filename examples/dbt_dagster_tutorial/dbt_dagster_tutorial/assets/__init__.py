0d02c09b6fe6b70dd1c123a229586a77a91a2980
JamieDeMaria <jamie@elementl.com>
2022-10-07 14:06:18 -0400

    reorg code snippets to match scaffolded project layout

--- examples/dbt_dagster_tutorial/dbt_dagster_tutorial/assets/__init__.py
+++ examples/dbt_dagster_tutorial/dbt_dagster_tutorial/assets/__init__.py
@@ -9,20 +9,18 @@ from dagster._utils import file_relative_path
 # These assets would be part of the first stage of the tutorial
 @asset(key_prefix=["jaffle_shop"], group_name="staging")
 def customers_raw() -> pd.DataFrame:
-    data = pd.read_csv("s3://dbt-tutorial-public/jaffle_shop_customers.csv")
-    # data = pd.read_csv("https://docs.dagster.io/assets/customers.csv") TODO replace ^ with this
+    data = pd.read_csv("https://docs.dagster.io/assets/customers.csv")
     return data
 
 
 @asset(key_prefix=["jaffle_shop"], group_name="staging")
 def orders_raw() -> pd.DataFrame:
-    data = pd.read_csv("s3://dbt-tutorial-public/jaffle_shop_orders.csv")
-    # data = pd.read_csv("https://docs.dagster.io/assets/orders.csv") TODO replace ^ with this
+    data = pd.read_csv("https://docs.dagster.io/assets/orders.csv")
     return data
 
 
-DBT_PROJECT_PATH = file_relative_path(__file__, "../jaffle_shop")
-DBT_PROFILES = file_relative_path(__file__, "../jaffle_shop/config")
+DBT_PROJECT_PATH = file_relative_path(__file__, "../../jaffle_shop")
+DBT_PROFILES = file_relative_path(__file__, "../../jaffle_shop/config")
 
 # if larger project use load_assets_from_dbt_manifest
 # dbt_assets = load_assets_from_dbt_manifest(json.load(DBT_PROJECT_PATH + "manifest.json", encoding="utf8"))
@@ -33,10 +31,7 @@ dbt_assets = load_assets_from_dbt_project(
 # end first stage of tutorial - at this point you can run dagit and launch a materialization
 
 # This asset would be the second stage of the tutorial
-@asset(
-    ins={"customers": AssetIn(key_prefix=["jaffle_shop"])},
-    group_name="staging",
-)
+@asset(ins={"customers": AssetIn(key_prefix=["jaffle_shop"])}, group_name="staging")
 def order_count_chart(context, customers: pd.DataFrame):
     fig = px.histogram(customers, x="number_of_orders")
     fig.update_layout(bargap=0.2)
