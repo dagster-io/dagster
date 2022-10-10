dcf201548db7917a3f684cf578bfe8bb9c04ba7f
Erin Cochran <erin.k.cochran@gmail.com>
2022-10-10 13:35:17 -0400

    Addressing some review comments

--- examples/dbt_dagster_tutorial/jaffle_shop/models/staging/stg_customers.sql
+++ examples/dbt_dagster_tutorial/jaffle_shop/models/staging/stg_customers.sql
@@ -2,5 +2,4 @@ select
     id as customer_id,
     first_name,
     last_name
-
 from {{ source('jaffle_shop', 'customers_raw') }}
--- examples/dbt_dagster_tutorial/jaffle_shop/models/staging/stg_orders.sql
+++ examples/dbt_dagster_tutorial/jaffle_shop/models/staging/stg_orders.sql
@@ -3,5 +3,4 @@ select
     user_id as customer_id,
     order_date,
     status
-
 from {{ source('jaffle_shop', 'orders_raw') }}
