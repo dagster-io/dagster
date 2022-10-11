0523253d901600de3965a2cf60837c5ff25821ac
Claire Lin <claire@elementl.com>
2022-10-11 13:00:06 -0700

    black

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_io_managers_prod_local.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_io_managers_prod_local.py
@@ -7,14 +7,18 @@ from docs_snippets.concepts.assets.asset_io_manager_prod_local import my_reposit
 @mock.patch.dict(os.environ, {"ENV": "prod"})
 def test_prod_assets():
     assert (
-        len(my_repository._assets_defs_by_key.keys())
-        == 2  # pylint: disable=protected-access
+        len(
+            my_repository._assets_defs_by_key.keys()  # pylint: disable=protected-access
+        )
+        == 2
     )
 
 
 @mock.patch.dict(os.environ, {"ENV": "local"})
 def test_local_assets():
     assert (
-        len(my_repository._assets_defs_by_key.keys())
-        == 2  # pylint: disable=protected-access
+        len(
+            my_repository._assets_defs_by_key.keys()  # pylint: disable=protected-access
+        )
+        == 2
     )
