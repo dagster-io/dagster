bbb7dd362ea19e7ad5db570e2ab885a7ece8d3bb
JamieDeMaria <jamie@elementl.com>
2022-10-12 12:33:15 -0400

    pr comments

--- js_modules/dagit/packages/core/src/plugins/ipynb.tsx
+++ js_modules/dagit/packages/core/src/plugins/ipynb.tsx
@@ -1,4 +1,4 @@
-import {Button, DialogBody, DialogFooter, Dialog, Icon} from '@dagster-io/ui';
+import {Button, DialogBody, DialogFooter, Dialog, Icon, ExternalAnchorButton} from '@dagster-io/ui';
 import * as React from 'react';
 
 import {AppContext} from '../app/AppContext';
@@ -16,56 +16,56 @@ export const SidebarComponent: React.FC<IPluginSidebarProps> = (props) => {
 
   const metadata = props.definition.metadata;
   const notebookPath = metadata.find((m) => m.key === 'notebook_path');
-  const notebookKind = metadata.find((m) => m.key === 'kind');
   const repoLocName = props.repoAddress?.location;
 
   if (!notebookPath) {
     return <span />;
   }
 
-  if (notebookKind && notebookKind.value === 'noteable') {
-    return (
-      <div>
-        <Button
-          icon={<Icon name="content_copy" />}
-          onClick={() => window.open(notebookPath.value, '_blank')}
-        >
-          View Notebook
-        </Button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <Button icon={<Icon name="content_copy" />} onClick={() => setOpen(true)}>
-          View Notebook
-        </Button>
-        <Dialog
-          icon="info"
-          onClose={() => setOpen(false)}
-          style={{width: '80vw', maxWidth: 900}}
-          title={notebookPath.value.split('/').pop()}
-          usePortal={true}
-          isOpen={open}
-        >
-          <DialogBody>
-            <iframe
-              title={notebookPath.value}
-              src={`${rootServerURI}/dagit/notebook?path=${encodeURIComponent(
-                notebookPath.value,
-              )}&repoLocName=${repoLocName}`}
-              sandbox=""
-              style={{border: 0, background: 'white'}}
-              seamless={true}
-              width="100%"
-              height={500}
-            />
-          </DialogBody>
-          <DialogFooter>
-            <Button onClick={() => setOpen(false)}>Close</Button>
-          </DialogFooter>
-        </Dialog>
-      </div>
-    );
+  const value = notebookPath.value;
+  const url = React.useMemo(() => {
+    try {
+      const url = new URL(value);
+      return url.toString();
+    } catch (e) {
+      // Not a full valid URL
+      return null;
+    }
+  }, [value]);
+
+  if (url) {
+    return <ExternalAnchorButton href={url}>View Notebook</ExternalAnchorButton>;
   }
+  return (
+    <div>
+      <Button icon={<Icon name="content_copy" />} onClick={() => setOpen(true)}>
+        View Notebook
+      </Button>
+      <Dialog
+        icon="info"
+        onClose={() => setOpen(false)}
+        style={{width: '80vw', maxWidth: 900}}
+        title={notebookPath.value.split('/').pop()}
+        usePortal={true}
+        isOpen={open}
+      >
+        <DialogBody>
+          <iframe
+            title={notebookPath.value}
+            src={`${rootServerURI}/dagit/notebook?path=${encodeURIComponent(
+              notebookPath.value,
+            )}&repoLocName=${repoLocName}`}
+            sandbox=""
+            style={{border: 0, background: 'white'}}
+            seamless={true}
+            width="100%"
+            height={500}
+          />
+        </DialogBody>
+        <DialogFooter>
+          <Button onClick={() => setOpen(false)}>Close</Button>
+        </DialogFooter>
+      </Dialog>
+    </div>
+  );
 };
