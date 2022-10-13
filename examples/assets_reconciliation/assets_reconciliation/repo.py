8cfd0686287fc672471391aee9e1ec0c213cc36a
Sean Mackesey <s.mackesey@gmail.com>
2022-10-13 07:22:41 -0400

    update

--- examples/assets_reconciliation/assets_reconciliation/repo.py
+++ examples/assets_reconciliation/assets_reconciliation/repo.py
@@ -1,21 +1,21 @@
 import datetime
 
-from dagster import AssetIn, Field, asset, input_manager, repository, source_asset, with_resources
-
-# from .lib import (
-#     BollingerBandsDgType,
-#     StockPricesDgType,
-#     compute_anomalous_events,
-#     compute_bollinger_bands_multi,
-#     load_dummy_source,
-# )
+from dagster import AssetIn, asset, input_manager, repository, source_asset, with_resources
+from dagster._core.definitions.logical_version import LogicalVersion
+from dagster._core.definitions.source_asset import SourceAsset
 
 
 @source_asset
-def dummy_source(_context):
-    for _ in range(10):
-        print("RUNNING SOURCE ASSET VERSION FN")
-    return str(datetime.datetime.now())
+def foo(_context):
+    return LogicalVersion(str(datetime.datetime.now()))
+
+
+@source_asset
+def bar(_context):
+    return LogicalVersion(str(datetime.datetime.now()))
+
+
+baz = SourceAsset("baz")
 
 
 @input_manager
@@ -24,30 +24,45 @@ def source_asset_input_manager():
 
 
 @asset(
-    # dagster_type=BollingerBandsDgType,
-    ins={"dummy_source": AssetIn(input_manager_key="source_asset_input_manager")},
-    # config_schema={
-    #     "rate": Field(int, default_value=30, description="Size of sliding window in days"),
-    #     "sigma": Field(
-    #         float, default_value=2.0, description="Width of envelope in standard deviations"
-    #     ),
-    # },
-    # metadata={"owner": "alice@example.com"},
-    versioned=True,
+    ins={"foo": AssetIn(input_manager_key="source_asset_input_manager")},
+    version="1",
 )
-def dummy_asset(context, dummy_source):
-    """Bollinger bands for the S&amp;P 500 stock prices."""
-    print("RUNNING $$$DUMMY ASSET$$$")
-    return dummy_source + 100
+def alpha(context, foo):
+    return foo + 100
+
+
+@asset(
+    ins={"bar": AssetIn(input_manager_key="source_asset_input_manager")},
+    version=True,
+)
+def beta(context, bar):
+    return bar + 100
+
+
+@asset(
+    ins={
+        "foo": AssetIn(input_manager_key="source_asset_input_manager"),
+        "bar": AssetIn(input_manager_key="source_asset_input_manager"),
+        "baz": AssetIn(input_manager_key="source_asset_input_manager"),
+    },
+    version=True,
+)
+def delta(context, foo, bar, baz):
+    return foo + bar + baz + 100
+
+
+@asset(
+    version=True,
+)
+def epsilon(context, alpha):
+    return alpha + 100
 
 
 @repository
 def repo():
     return [
-        # dummy_source,
-        # dummy_asset,
         *with_resources(
-            [dummy_source, dummy_asset],
+            [foo, bar, baz, alpha, beta, delta, epsilon],
             {"source_asset_input_manager": source_asset_input_manager},
         )
     ]
