867f00755cd52528f6ee6145ed7d233e664baf47
Jordan Sanders <jordan@elementl.com>
2022-10-14 08:43:37 -0500

    Add logging

--- .buildkite/dagster-buildkite/dagster_buildkite/utils.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/utils.py
@@ -300,10 +300,12 @@ def skip_if_no_docs_changes():
 
     # If anything changes in the docs directory
     if any(Path("docs") in path.parents for path in get_changed_files()):
+        logging.info("Run docs steps because files in the docs directory changed")
         return None
 
     # If anything changes that uses the literalinclude docstring
     if any("literalinclude" in path.read_text() for path in get_changed_files()):
+        logging.info("Run docs steps because files with the literalinclude docstring changed")
         return None
 
     return "No docs changes"
