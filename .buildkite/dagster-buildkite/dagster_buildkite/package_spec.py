21b2264fa4cf6e0b08f8aeb99b0131cd9a86d244
Jordan Sanders <jordan@elementl.com>
2022-10-19 14:41:03 -0500

    Fixup flow control

--- .buildkite/dagster-buildkite/dagster_buildkite/package_spec.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/package_spec.py
@@ -214,13 +214,12 @@ class PackageSpec(
                     else:
                         extra_commands_post = []
 
+                    dependencies = []
                     if not self.skip_reason:
                         if isinstance(self.pytest_step_dependencies, list):
                             dependencies = self.pytest_step_dependencies
                         elif callable(self.pytest_step_dependencies):
                             dependencies = self.pytest_step_dependencies(py_version, other_factor)
-                    else:
-                        dependencies = []
 
                     steps.append(
                         build_tox_step(
