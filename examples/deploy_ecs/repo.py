fabb254e442a1732a66e273d670321c4fe7b27fa
prha <prha@elementl.com>
2022-10-20 09:24:09 -0700

    wip

--- examples/deploy_ecs/repo.py
+++ examples/deploy_ecs/repo.py
@@ -1,11 +1,28 @@
 import time
 
+import boto3
+
 from dagster import graph, op, repository
 
 
 @op
-def my_op():
-    time.sleep(30)
+def my_op(context):
+    for i in range(200):
+        time.sleep(0.5)
+        print(f"stdout {i}")
+        if i % 1000 == 420:
+            context.log.error(f"Error message seq={i}")
+        elif i % 100 == 0:
+            context.log.warning(f"Warning message seq={i}")
+        elif i % 10 == 0:
+            context.log.info(f"Info message seq={i}")
+        else:
+            context.log.debug(f"Debug message seq={i}")
+
+    ecs = boto3.client("ecs")
+    print(ecs.list_tasks())
+    print(context.pipeline_run.tags)
+
     return True
 
 
