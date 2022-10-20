957228a924dae7275a9e6513dd5d5a650f8a9c92
Jordan Sanders <jordan@elementl.com>
2022-10-20 13:54:33 -0500

    Parametrize Buildkite queues (#10007)
    So we can globally switch queues without needing all in flight
    branches to be rebased (obviously this benefit will only take
    affect _after_ existing branches rebase to include _this_ change).
    
    This will make it easier to migrate a bunch of builds to a new queue
    without needing to keep the old queue active for a long tail of older
    builds.
    
    NO_SKIP

--- .buildkite/dagster-buildkite/dagster_buildkite/step_builder.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/step_builder.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional
 
 from .images.versions import BUILDKITE_TEST_IMAGE_VERSION
 from .python_version import AvailablePythonVersion
-from .utils import CommandStep, message_contains
+from .utils import CommandStep, message_contains, safe_getenv
 
 DEFAULT_TIMEOUT_IN_MIN = 20
 
@@ -17,9 +17,9 @@ AWS_ECR_REGION = "us-west-2"
 
 
 class BuildkiteQueue(Enum):
-    DOCKER = "docker-p"
-    MEDIUM = "buildkite-medium-v5-0-1"
-    WINDOWS = "windows-medium"
+    DOCKER = safe_getenv("BUILDKITE_DOCKER_QUEUE")
+    MEDIUM = safe_getenv("BUILDKITE_MEDIUM_QUEUE")
+    WINDOWS = safe_getenv("BUILDKITE_WINDOWS_QUEUE")
 
     @classmethod
     def contains(cls, value: object) -> bool:
