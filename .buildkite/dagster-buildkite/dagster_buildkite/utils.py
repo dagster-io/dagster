5f741221637f5ee04a196b3cf7ea23cecc8f3125
Jordan Sanders <jordan@elementl.com>
2022-10-20 12:28:51 -0500

    Fixup NO_SKIP behavior

--- .buildkite/dagster-buildkite/dagster_buildkite/utils.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/utils.py
@@ -291,4 +291,4 @@ def changed_python_package_names():
 
 
 def message_contains(substring: str) -> bool:
-    return substring in os.getenv("BUILDKITE_MESSAGE")
+    return substring in os.getenv("BUILDKITE_MESSAGE", "")
