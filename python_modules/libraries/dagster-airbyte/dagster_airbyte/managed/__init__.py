01edf1d4362fc77126aff307a7a16eab54b21725
Ben Pankow <ben@elementl.com>
2022-10-25 09:53:01 -0700

    [dagster-airbyte] Airbyte managed elements impl (#10013)
    * [dagster-airbyte][wip] Airbyte stacks impl
    
    * fix import
    
    * Add integration test, some cleanup
    
    * fix pytest?
    
    * split off integration tests
    
    * increase timeout to be yuge
    
    * reduce timeout, fix port?
    
    * cleanup, comments
    
    * stack -> element
    
    * mypy
    
    * lint
    
    * pytest fix?
    
    * respond to review comments
    
    * fix tox test
    
    * fix bk tox
    
    * potential bk tox fix?
    
    * runtime type checks
    
    * [dagster-airbyte] Add utility method for loading from instance & checking against config (#10055)
    
    * [dagster-airbyte] Add utility method for loading from instance & checking against config
    
    * runtime check

---
+++ python_modules/libraries/dagster-airbyte/dagster_airbyte/managed/__init__.py
@@ -0,0 +1,2 @@
+from .reconciliation import AirbyteManagedElementReconciler, load_assets_from_connections
+from .types import AirbyteConnection, AirbyteDestination, AirbyteSource, AirbyteSyncMode
---
+++ python_modules/libraries/dagster-airbyte/dagster_airbyte_tests/example_stacks/empty_airbyte_stack.py
@@ -0,0 +1,17 @@
+import os
+
+from dagster_airbyte import AirbyteManagedElementReconciler, airbyte_resource
+
+airbyte_instance = airbyte_resource.configured(
+    {
+        "host": os.getenv("AIRBYTE_HOSTNAME", "localhost"),
+        "port": os.getenv("AIRBYTE_PORT", "80"),
+    }
+)
+
+
+reconciler = AirbyteManagedElementReconciler(
+    airbyte=airbyte_instance,
+    connections=[],
+    delete_unmented_resources=True,
+)
