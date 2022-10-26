f1fb3dc468f866903da31ebae9c757f97abe88a4
benpankow <ben@elementl.com>
2022-10-26 16:02:15 -0500

    [dagster-airbyte][docs] Reorganize Airbyte guide

--- examples/docs_snippets/docs_snippets/integrations/airbyte/airbyte.py
+++ examples/docs_snippets/docs_snippets/integrations/airbyte/airbyte.py
@@ -2,6 +2,22 @@
 # pylint: disable=unused-variable
 
 
+def scope_define_instance():
+    # start_define_instance
+    from dagster import airbyte_resource
+
+    airbyte_instance = airbyte_resource.configured(
+        {
+            "host": "localhost",
+            "port": 8000,
+            # If using basic auth, include username and password:
+            "username": "airbyte",
+            "password": {"env": "AIRBYTE_PASSWORD"},
+        }
+    )
+    # end_define_instance
+
+
 def scope_load_assets_from_airbyte_project():
     # start_load_assets_from_airbyte_project
     from dagster_airbyte import load_assets_from_airbyte_project
@@ -13,8 +29,7 @@ def scope_load_assets_from_airbyte_project():
 
 
 def scope_load_assets_from_airbyte_instance():
-    # start_load_assets_from_airbyte_instance
-    from dagster_airbyte import airbyte_resource, load_assets_from_airbyte_instance
+    from dagster_airbyte import airbyte_resource
 
     airbyte_instance = airbyte_resource.configured(
         {
@@ -25,29 +40,33 @@ def scope_load_assets_from_airbyte_instance():
             "password": {"env": "AIRBYTE_PASSWORD"},
         }
     )
+    # start_load_assets_from_airbyte_instance
+    from dagster_airbyte import load_assets_from_airbyte_instance
+
     airbyte_assets = load_assets_from_airbyte_instance(airbyte_instance)
     # end_load_assets_from_airbyte_instance
 
 
 def scope_airbyte_project_config():
+    from dagster_airbyte import airbyte_resource
+
+    airbyte_instance = airbyte_resource.configured(
+        {
+            "host": "localhost",
+            "port": "8000",
+            # If using basic auth, include username and password:
+            "username": "airbyte",
+            "password": {"env": "AIRBYTE_PASSWORD"},
+        }
+    )
     # start_airbyte_project_config
-    from dagster_airbyte import airbyte_resource, load_assets_from_airbyte_project
+    from dagster_airbyte import load_assets_from_airbyte_project
 
     from dagster import with_resources
 
     airbyte_assets = with_resources(
         [load_assets_from_airbyte_project(project_dir="path/to/airbyte/project")],
-        {
-            "airbyte": airbyte_resource.configured(
-                {
-                    "host": "localhost",
-                    "port": "8000",
-                    # If using basic auth, include username and password:
-                    "username": "airbyte",
-                    "password": {"env": "AIRBYTE_PASSWORD"},
-                }
-            )
-        },
+        {"airbyte": airbyte_instance},
     )
     # end_airbyte_project_config
 
@@ -64,8 +83,19 @@ def scope_manually_define_airbyte_assets():
 
 
 def scope_airbyte_manual_config():
+    from dagster_airbyte import airbyte_resource
+
+    airbyte_instance = airbyte_resource.configured(
+        {
+            "host": "localhost",
+            "port": "8000",
+            # If using basic auth, include username and password:
+            "username": "airbyte",
+            "password": {"env": "AIRBYTE_PASSWORD"},
+        }
+    )
     # start_airbyte_manual_config
-    from dagster_airbyte import airbyte_resource, build_airbyte_assets
+    from dagster_airbyte import build_airbyte_assets
 
     from dagster import with_resources
 
@@ -74,19 +104,11 @@ def scope_airbyte_manual_config():
             connection_id="87b7fe85-a22c-420e-8d74-b30e7ede77df",
             destination_tables=["releases", "tags", "teams"],
         ),
-        {
-            "airbyte": airbyte_resource.configured(
-                {
-                    "host": "localhost",
-                    "port": "8000",
-                    # If using basic auth, include username and password:
-                    "username": "airbyte",
-                    "password": {"env": "AIRBYTE_PASSWORD"},
-                }
-            )
-        },
+        {"airbyte": airbyte_instance},
     )
     # end_airbyte_manual_config
+
+
 
 
 def scope_schedule_assets():
