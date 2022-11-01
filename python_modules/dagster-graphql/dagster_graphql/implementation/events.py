7c1b2b2b46acd2d1d4fa3c4e83c4b00e5076a2af
JamieDeMaria <jamie@elementl.com>
2022-11-01 09:40:13 -0400

    add notebook metadata type to dagit

--- python_modules/dagster-graphql/dagster_graphql/implementation/events.py
+++ python_modules/dagster-graphql/dagster_graphql/implementation/events.py
@@ -373,10 +373,8 @@ def from_dagster_event_record(event_record: EventLogEntry, pipeline_name: str) -
         )
     elif dagster_event.event_type == DagsterEventType.LOGS_CAPTURED:
         return GrapheneLogsCapturedEvent(
-            fileKey=dagster_event.logs_captured_data.file_key,
-            logKey=dagster_event.logs_captured_data.file_key,
+            logKey=dagster_event.logs_captured_data.log_key,
             stepKeys=dagster_event.logs_captured_data.step_keys,
-            externalUrl=dagster_event.logs_captured_data.external_url,
             pid=dagster_event.pid,
             **basic_params,
         )
