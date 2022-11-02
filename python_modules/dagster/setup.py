bb79c5e2d5f07c5fa2b18649dadf3c0d3de9613a
pragbatra-mendel <prag.b@mendel.ai>
2022-11-02 16:58:58 -0700

    Bump dagster grpcio dependency to natively support M1 mac

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -58,8 +58,8 @@ setup(
         "croniter>=0.3.34",
         # grpcio 1.48.1 has hanging/crashing issues: https://github.com/grpc/grpc/issues/30843
         # ensure version we require is >= that with which we generated the grpc code (set in dev-requirements)
-        "grpcio>=1.32.0,<1.48.1",
-        "grpcio-health-checking>=1.32.0,<1.44.0",
+        "grpcio>=1.50",
+        "grpcio-health-checking>=1.50",
         "packaging>=20.9",
         "pendulum",
         "protobuf>=3.13.0,<4",  # ensure version we require is >= that with which we generated the proto code (set in dev-requirements)
