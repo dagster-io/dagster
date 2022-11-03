dee0aa9248457e26b187f02e6b2f7d63b1dfb00b
Joseph Van Drunen <josephvandrunen@gmail.com>
2022-11-03 12:50:29 -0400

    fixup

--- examples/with_airflow/setup.py
+++ examples/with_airflow/setup.py
@@ -1,15 +1,14 @@
 from setuptools import find_packages, setup
 
-if __name__ == "__main__":
-    setup(
-        name="with_airflow",
-        packages=find_packages(exclude=["with_airflow_tests"]),
-        install_requires=[
-            "dagster",
-            "dagster_airflow",
-            "apache-airflow==2.3.0",
-            # pin jinja2 to version compatible with dagit and airflow
-            "jinja2==3.0.3",
-        ],
-        extras_require={"dev": ["dagit", "pytest"]},
-    )
+setup(
+    name="with_airflow",
+    packages=find_packages(exclude=["with_airflow_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster_airflow",
+        "apache-airflow==2.3.0",
+        # pin jinja2 to version compatible with dagit and airflow
+        "jinja2==3.0.3",
+    ],
+    extras_require={"dev": ["dagit", "pytest"]},
+)
