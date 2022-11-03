e06e02acdcacc9be9219b7b2459753dd869af47f
Joseph Van Drunen <josephvandrunen@gmail.com>
2022-11-03 13:02:42 -0400

    fixup

--- examples/with_airflow/setup.py
+++ examples/with_airflow/setup.py
@@ -9,6 +9,7 @@ setup(
         "apache-airflow==2.3.0",
         # pin jinja2 to version compatible with dagit and airflow
         "jinja2==3.0.3",
+        # for the kubernetes operator
         "apache-airflow-providers-cncf-kubernetes>=4.4.0",
         "apache-airflow-providers-docker>=3.1.0",
     ],
