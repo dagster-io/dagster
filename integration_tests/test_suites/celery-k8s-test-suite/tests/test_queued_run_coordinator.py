79d1f122cfb9f7cd277393ca970a00ee8ec01db0
Jordan Sanders <jordan@elementl.com>
2022-11-04 16:12:00 -0500

    Revert "Mark celery-k8s-test-suite as flaky" (#10384)
    Revert "Mark celery-k8s-test-suite as flaky (#10204)"
    
    This reverts commit cefab8246326c9d7e13942429e34103a1e5f1734.

--- integration_tests/test_suites/celery-k8s-test-suite/tests/test_queued_run_coordinator.py
+++ integration_tests/test_suites/celery-k8s-test-suite/tests/test_queued_run_coordinator.py
@@ -1,6 +1,5 @@
 import os
 
-import pytest
 from dagster_k8s.test import wait_for_job_and_get_raw_logs
 from dagster_k8s_test_infra.integration_utils import image_pull_policy, launch_run_over_graphql
 from dagster_test.test_project import get_test_project_environments_path
@@ -33,7 +32,6 @@ def assert_events_in_order(logs, expected_events):
 
 
 @mark_daemon
-@pytest.mark.flaky(reruns=2)
 def test_execute_queued_run_on_celery_k8s(  # pylint: disable=redefined-outer-name
     dagster_docker_image,
     dagster_instance_for_daemon,
