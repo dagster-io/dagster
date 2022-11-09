4d670623e6479503c9c151fbd01747064f602e9a
JamieDeMaria <jamie@elementl.com>
2022-11-09 17:12:03 -0500

    formatting

--- examples/tutorial_notebook_assets/tutorial_finished/assets/__init__.py
+++ examples/tutorial_notebook_assets/tutorial_finished/assets/__init__.py
@@ -6,9 +6,7 @@ from dagster import AssetIn, Field, Int, asset, file_relative_path
 
 
 # fetch the iris dataset
-@asset(
-    group_name="finished_tutorial"
-)
+@asset(group_name="finished_tutorial")
 def iris_dataset():
     return pd.read_csv(
         "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",
@@ -28,7 +26,7 @@ iris_kmeans_jupyter_notebook = define_dagstermill_asset(
     name="iris_kmeans_jupyter",
     notebook_path=file_relative_path(__file__, "../notebooks/iris-kmeans.ipynb"),
     ins={"iris": AssetIn("iris_dataset")},
-    group_name="finished_tutorial"
+    group_name="finished_tutorial",
 )
 
 
@@ -39,5 +37,5 @@ iris_kmeans_noteable_notebook = define_noteable_dagster_asset(
     name="iris_kmeans_noteable",
     notebook_id=notebook_id,
     ins={"iris": AssetIn("iris_dataset")},
-    group_name="finished_tutorial"
+    group_name="finished_tutorial",
 )
--- examples/tutorial_notebook_assets/tutorial_template/repository.py
+++ examples/tutorial_notebook_assets/tutorial_template/repository.py
@@ -1,8 +1,9 @@
 from dagstermill import local_output_notebook_io_manager
-from . import assets
-from .jobs import ping_noteable
 
 from dagster import load_assets_from_package_module, repository, with_resources
+
+from . import assets
+from .jobs import ping_noteable
 
 
 @repository
