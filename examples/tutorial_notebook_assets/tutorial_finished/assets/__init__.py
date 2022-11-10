bf1e78a01c8a5a7ba36d10018e698f17a237f5b2
JamieDeMaria <jamie@elementl.com>
2022-11-10 10:22:43 -0500

    fix asset names

--- examples/tutorial_notebook_assets/tutorial_finished/assets/__init__.py
+++ examples/tutorial_notebook_assets/tutorial_finished/assets/__init__.py
@@ -7,7 +7,7 @@ from dagster import AssetIn, Field, Int, asset, file_relative_path
 
 # fetch the iris dataset
 @asset(group_name="finished_tutorial")
-def iris_dataset():
+def iris_dataset_finished():
     return pd.read_csv(
         "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",
         names=[
@@ -22,10 +22,10 @@ def iris_dataset():
 
 # Asset backed by a Jupyter notebook
 
-iris_kmeans_jupyter_notebook = define_dagstermill_asset(
-    name="iris_kmeans_jupyter",
+iris_kmeans_jupyter_notebook_finished = define_dagstermill_asset(
+    name="iris_kmeans_jupyter_finished",
     notebook_path=file_relative_path(__file__, "../notebooks/iris-kmeans.ipynb"),
-    ins={"iris": AssetIn("iris_dataset")},
+    ins={"iris": AssetIn("iris_dataset_finished")},
     group_name="finished_tutorial",
 )
 
@@ -33,9 +33,9 @@ iris_kmeans_jupyter_notebook = define_dagstermill_asset(
 # Asset backed by a Noteable notebook
 
 notebook_id = "<your-noteable-notebook-id>"
-iris_kmeans_noteable_notebook = define_noteable_dagster_asset(
-    name="iris_kmeans_noteable",
+iris_kmeans_noteable_notebook_finished = define_noteable_dagster_asset(
+    name="iris_kmeans_noteable_finished",
     notebook_id=notebook_id,
-    ins={"iris": AssetIn("iris_dataset")},
+    ins={"iris": AssetIn("iris_dataset_finished")},
     group_name="finished_tutorial",
 )
