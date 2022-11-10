2a5fffa666aeb45000fa34e42d5abc88b8087dbe
Owen Kephart <owen@elementl.com>
2022-11-10 11:01:52 -0800

    add missing file

---
+++ python_modules/dagster-graphql/dagster_graphql/schema/freshness_policy.py
@@ -0,0 +1,27 @@
+import graphene
+from dagster._core.definitions.freshness_policy import FreshnessPolicy
+import dagster._check as check
+
+
+class GrapheneAssetFreshnessInfo(graphene.ObjectType):
+    currentMinutesLate = graphene.Field(graphene.Float)
+    latestMaterializationMinutesLate = graphene.Field(graphene.Float)
+
+    class Meta:
+        name = "AssetFreshnessInfo"
+
+
+class GrapheneFreshnessPolicy(graphene.ObjectType):
+    maximumLagMinutes = graphene.NonNull(graphene.Float)
+    cronSchedule = graphene.Field(graphene.String)
+
+    class Meta:
+        name = "FreshnessPolicy"
+
+    def __init__(self, freshness_policy: FreshnessPolicy):
+        freshness_policy = check.inst_param(freshness_policy, "freshness_policy", FreshnessPolicy)
+
+        super().__init__(
+            maximumLagMinutes=freshness_policy.maximum_lag_minutes,
+            cronSchedule=freshness_policy.cron_schedule,
+        )
