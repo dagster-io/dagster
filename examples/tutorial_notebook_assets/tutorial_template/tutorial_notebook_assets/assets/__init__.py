d11d3f774923061354275831aae49f644ae5624d
jamiedemaria <demaria.jamie@gmail.com>
2022-11-10 12:55:19 -0500

    Notebook + assets example pt 2 (#10415)

--- examples/tutorial_notebook_assets/tutorial_template/tutorial_notebook_assets/assets/__init__.py
+++
@@ -1,6 +0,0 @@
-from dagster import AssetIn, AssetKey, asset, file_relative_path
-
-# from dagstermill import define_dagstermill_asset
-# from papermill_origami import define_noteable_dagster_asset
-
-# TODO create an asset from a Jupyter notebook
