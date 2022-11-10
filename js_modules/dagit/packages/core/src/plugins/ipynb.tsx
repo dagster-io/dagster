6f8453873ce7e5364e7a33597a03064e7f605002
jamiedemaria <demaria.jamie@gmail.com>
2022-11-10 10:21:33 -0500

    [dagit] Clarify notebook buttons (#10451)

--- js_modules/dagit/packages/core/src/plugins/ipynb.tsx
+++ js_modules/dagit/packages/core/src/plugins/ipynb.tsx
@@ -1,3 +1,4 @@
+import {Box} from '@dagster-io/ui';
 import * as React from 'react';
 
 import {IPluginSidebarProps} from '../plugins';
@@ -8,5 +9,13 @@ export const SidebarComponent: React.FC<IPluginSidebarProps> = (props) => {
   const notebookPath = metadata.find((m) => m.key === 'notebook_path');
   const repoLocName = props.repoAddress?.location;
 
-  return <NotebookButton path={notebookPath?.value} repoLocation={repoLocName || ''} />;
+  return (
+    <Box padding={16}>
+      <NotebookButton
+        path={notebookPath?.value}
+        repoLocation={repoLocName || ''}
+        label="View Source Notebook"
+      />
+    </Box>
+  );
 };
