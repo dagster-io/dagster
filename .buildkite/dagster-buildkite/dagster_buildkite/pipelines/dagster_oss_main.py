af8a5de9f6e317fcce96f9da28818387435c21ea
Jordan Sanders <jordan@elementl.com>
2022-11-10 16:10:10 -0600

    Don't kick of internal builds
    I'm using this branch to isolate which parts of the build could be moved
    to a more public buildkite pipeline.
    
    [NO_SKIP=true]

--- .buildkite/dagster-buildkite/dagster_buildkite/pipelines/dagster_oss_main.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/pipelines/dagster_oss_main.py
@@ -5,18 +5,17 @@ from typing import List, Optional, Tuple
 
 from dagster_buildkite.defines import DO_COVERAGE
 from dagster_buildkite.steps.coverage import build_coverage_step
-from dagster_buildkite.steps.dagit_ui import build_dagit_ui_steps, skip_if_no_dagit_changes
+from dagster_buildkite.steps.dagit_ui import build_dagit_ui_steps
 from dagster_buildkite.steps.dagster import build_dagster_steps, build_repo_wide_steps
 from dagster_buildkite.steps.docs import build_docs_steps
-from dagster_buildkite.steps.trigger import build_trigger_step
 from dagster_buildkite.steps.wait import build_wait_step
-from dagster_buildkite.utils import BuildkiteStep, is_release_branch, safe_getenv
+from dagster_buildkite.utils import BuildkiteStep, safe_getenv
 
 
 def build_dagster_oss_main_steps() -> List[BuildkiteStep]:
 
-    branch_name = safe_getenv("BUILDKITE_BRANCH")
-    commit_hash = safe_getenv("BUILDKITE_COMMIT")
+    # branch_name = safe_getenv("BUILDKITE_BRANCH")
+    # commit_hash = safe_getenv("BUILDKITE_COMMIT")
     do_coverage = DO_COVERAGE
 
     steps: List[BuildkiteStep] = []
@@ -27,27 +26,27 @@ def build_dagster_oss_main_steps() -> List[BuildkiteStep]:
     # overridden by setting the `INTERNAL_BRANCH` environment variable or passing
     # `[INTERNAL_BRANCH=<branch>]` in the commit message. Master/release branches
     # always run on the matching internal branch.
-    if branch_name == "master" or is_release_branch(branch_name):
-        pipeline_name = "internal"
-        trigger_branch = branch_name  # build on matching internal release branch
-        async_step = True
-    else:  # feature branch
-        pipeline_name = "oss-internal-compatibility"
-        trigger_branch = _get_internal_branch_specifier() or "master"
-        async_step = False
+    # if branch_name == "master" or is_release_branch(branch_name):
+    #     pipeline_name = "internal"
+    #     trigger_branch = branch_name  # build on matching internal release branch
+    #     async_step = True
+    # else:  # feature branch
+    #     pipeline_name = "oss-internal-compatibility"
+    #     trigger_branch = _get_internal_branch_specifier() or "master"
+    #     async_step = False
 
-    steps.append(
-        build_trigger_step(
-            pipeline=pipeline_name,
-            trigger_branch=trigger_branch,
-            async_step=async_step,
-            env={
-                "DAGSTER_BRANCH": branch_name,
-                "DAGSTER_COMMIT_HASH": commit_hash,
-                "DAGIT_ONLY_OSS_CHANGE": "1" if not skip_if_no_dagit_changes else "",
-            },
-        ),
-    )
+    # steps.append(
+    #     build_trigger_step(
+    #         pipeline=pipeline_name,
+    #         trigger_branch=trigger_branch,
+    #         async_step=async_step,
+    #         env={
+    #             "DAGSTER_BRANCH": branch_name,
+    #             "DAGSTER_COMMIT_HASH": commit_hash,
+    #             "DAGIT_ONLY_OSS_CHANGE": "1" if not skip_if_no_dagit_changes else "",
+    #         },
+    #     ),
+    # )
 
     # Full pipeline.
     steps += build_repo_wide_steps()
