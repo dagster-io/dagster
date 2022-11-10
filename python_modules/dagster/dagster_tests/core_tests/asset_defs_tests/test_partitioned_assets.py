16b11ef5feaffed4ad705467b3f9e15ca2462f7f
Sandy Ryza <sandy@elementl.com>
2022-11-09 16:32:24 -0800

    enable launching a run that targets a range of asset partitions (#10441)
    enable launching runs that target a range of partitions

--- python_modules/dagster/dagster_tests/core_tests/asset_defs_tests/test_partitioned_assets.py
+++ python_modules/dagster/dagster_tests/core_tests/asset_defs_tests/test_partitioned_assets.py
@@ -28,6 +28,10 @@ from dagster._core.definitions.asset_partitions import (
 from dagster._core.definitions.events import AssetKey
 from dagster._core.definitions.partition_key_range import PartitionKeyRange
 from dagster._core.definitions.time_window_partitions import TimeWindow
+from dagster._core.storage.tags import (
+    ASSET_PARTITION_RANGE_END_TAG,
+    ASSET_PARTITION_RANGE_START_TAG,
+)
 
 
 @pytest.fixture(autouse=True)
@@ -468,3 +472,40 @@ def test_mismatched_job_partitioned_config_with_asset_partitions():
         match="Can't supply a PartitionedConfig for 'config' with a different PartitionsDefinition than supplied for 'partitions_def'.",
     ):
         define_asset_job("job", config=myconfig).resolve([asset1], [])
+
+
+def test_partition_range_single_run():
+    partitions_def = DailyPartitionsDefinition(start_date="2020-01-01")
+
+    @asset(partitions_def=partitions_def)
+    def upstream_asset(context) -> None:
+        assert context.asset_partition_key_range_for_output() == PartitionKeyRange(
+            start="2020-01-01", end="2020-01-03"
+        )
+
+    @asset(partitions_def=partitions_def, non_argument_deps={"upstream_asset"})
+    def downstream_asset(context) -> None:
+        assert context.asset_partition_key_range_for_input("upstream_asset") == PartitionKeyRange(
+            start="2020-01-01", end="2020-01-03"
+        )
+        assert context.asset_partition_key_range_for_output() == PartitionKeyRange(
+            start="2020-01-01", end="2020-01-03"
+        )
+
+    the_job = define_asset_job("job").resolve([upstream_asset, downstream_asset], [])
+
+    result = the_job.execute_in_process(
+        tags={
+            ASSET_PARTITION_RANGE_START_TAG: "2020-01-01",
+            ASSET_PARTITION_RANGE_END_TAG: "2020-01-03",
+        }
+    )
+
+    assert {
+        materialization.partition
+        for materialization in result.asset_materializations_for_node("upstream_asset")
+    } == {"2020-01-01", "2020-01-02", "2020-01-03"}
+    assert {
+        materialization.partition
+        for materialization in result.asset_materializations_for_node("downstream_asset")
+    } == {"2020-01-01", "2020-01-02", "2020-01-03"}
