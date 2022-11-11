1b15a68906e167b3953580221b362faf51754184
Sean Mackesey <s.mackesey@gmail.com>
2022-11-11 15:30:33 -0500

    fix

--- examples/assets_reconciliation/assets_reconciliation/repo.py
+++ examples/assets_reconciliation/assets_reconciliation/repo.py
@@ -1,16 +1,16 @@
 import datetime
 
-from dagster import AssetIn, asset, input_manager, repository, source_asset, with_resources
+from dagster import AssetIn, asset, input_manager, repository, observable_source_asset, with_resources
 from dagster._core.definitions.logical_version import LogicalVersion
 from dagster._core.definitions.source_asset import SourceAsset
 
 
-@source_asset
+@observable_source_asset
 def foo(_context):
     return LogicalVersion(str(datetime.datetime.now()))
 
 
-@source_asset
+@observable_source_asset
 def bar(_context):
     return LogicalVersion(str(datetime.datetime.now()))
 
@@ -25,7 +25,7 @@ def source_asset_input_manager():
 
 @asset(
     ins={"foo": AssetIn(input_manager_key="source_asset_input_manager")},
-    version="1",
+    op_version="1",
 )
 def alpha(context, foo):
     return foo + 100
@@ -33,7 +33,7 @@ def alpha(context, foo):
 
 @asset(
     ins={"bar": AssetIn(input_manager_key="source_asset_input_manager")},
-    version=True,
+    op_version="1",
 )
 def beta(context, bar):
     return bar + 100
@@ -45,14 +45,13 @@ def beta(context, bar):
         "bar": AssetIn(input_manager_key="source_asset_input_manager"),
         "baz": AssetIn(input_manager_key="source_asset_input_manager"),
     },
-    version=True,
 )
 def delta(context, foo, bar, baz):
     return foo + bar + baz + 100
 
 
 @asset(
-    version=True,
+    op_version="1",
 )
 def epsilon(context, alpha):
     return alpha + 100
