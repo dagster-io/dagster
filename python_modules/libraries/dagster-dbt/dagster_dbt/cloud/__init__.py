243421114806bba8018d97771e13c50cd86b9843
Rex Ledesma <rex@elementl.com>
2022-11-11 17:13:34 -0500

    feat(dbt-cloud): generate asset dependencies from a cloud job (#10437)

--- python_modules/libraries/dagster-dbt/dagster_dbt/cloud/__init__.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/cloud/__init__.py
@@ -1,3 +1,4 @@
+from .asset_defs import load_assets_from_dbt_cloud_job
 from .ops import dbt_cloud_run_op
 from .resources import DbtCloudResourceV2, dbt_cloud_resource
 from .types import DbtCloudOutput
