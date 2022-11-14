e84bfeda4394a742a311c4ebe2de430921f342b6
bengotow <bgotow@elementl.com>
2022-11-14 14:01:01 -0600

    [dagit] Tweak GraphQL schema so single- and mulit- dim assets are queried the same way

--- js_modules/dagit/packages/core/src/assets/types/PartitionHealthQuery.ts
+++ js_modules/dagit/packages/core/src/assets/types/PartitionHealthQuery.ts
@@ -13,26 +13,28 @@ export interface PartitionHealthQuery_assetNodeOrError_AssetNotFoundError {
   __typename: "AssetNotFoundError";
 }
 
+export interface PartitionHealthQuery_assetNodeOrError_AssetNode_partitionKeysByDimension {
+  __typename: "DimensionPartitionKeys";
+  name: string;
+  partitionKeys: string[];
+}
+
 export interface PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountGroupedByDimension {
   __typename: "MaterializationCountGroupedByDimension";
+  materializationCountsGrouped: number[][];
 }
 
-export interface PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountByPartition_partitionsCounts {
-  __typename: "PartitionMaterializationCount";
-  partition: string;
-  materializationCount: number;
+export interface PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountSingleDimension {
+  __typename: "MaterializationCountSingleDimension";
+  materializationCounts: number[];
 }
 
-export interface PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountByPartition {
-  __typename: "MaterializationCountByPartition";
-  partitionsCounts: PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountByPartition_partitionsCounts[];
-}
-
-export type PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts = PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountGroupedByDimension | PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountByPartition;
+export type PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts = PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountGroupedByDimension | PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts_MaterializationCountSingleDimension;
 
 export interface PartitionHealthQuery_assetNodeOrError_AssetNode {
   __typename: "AssetNode";
   id: string;
+  partitionKeysByDimension: PartitionHealthQuery_assetNodeOrError_AssetNode_partitionKeysByDimension[];
   partitionMaterializationCounts: PartitionHealthQuery_assetNodeOrError_AssetNode_partitionMaterializationCounts | null;
 }
 
