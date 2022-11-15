7fceafcc26abf54697d73afc17c86961af9fab98
Nicholas Schrock <schrockn@elementl.com>
2022-11-15 11:50:03 -0500

    ready for commit

--- python_modules/dagster/dagster/_core/definitions/register_definitions.py
+++ python_modules/dagster/dagster/_core/definitions/register_definitions.py
@@ -1,23 +1,51 @@
 import inspect
+import sys
+from collections.abc import Generator
+from contextlib import contextmanager
 from types import ModuleType
-from typing import Any, Iterable, Mapping, Optional, Union
+from typing import Any, Iterable, Mapping, Optional, Union, Dict
 
 import dagster._check as check
+
+from dagster._core.execution.with_resources import with_resources
 
 from .assets import AssetsDefinition, SourceAsset
 from .cacheable_assets import CacheableAssetsDefinition
 
-# from .decorators import repository
+from .decorators import repository
 from .job_definition import JobDefinition
 
-# from .resource_definition import ResourceDefinition
+from .resource_definition import ResourceDefinition
 from .schedule_definition import ScheduleDefinition
 from .sensor_definition import SensorDefinition
 
-# from .repository_definition import PendingRepositoryDefinition, RepositoryDefinition
+from .repository_definition import PendingRepositoryDefinition, RepositoryDefinition
 
 
 MAGIC_REGISTERED_DEFINITIONS_KEY = "__registered_definitions"
+
+
+class ModuleHasRegisteredDefinitionsError(Exception):
+    pass
+
+
+# invoke this function to get the module name the function that called the current
+# scope
+def get_module_name_of_caller() -> str:
+    # based on https://stackoverflow.com/questions/2000861/retrieve-module-object-from-stack-frame
+    # two f_backs to get past get_module_name_of_caller frame
+
+    # Need to do none checking because of:
+    # https://docs.python.org/3/library/inspect.html#inspect.currentframe
+    # CPython implementation detail: This function relies on Python stack frame
+    # support in the interpreter, which isn’t guaranteed to exist in all
+    # implementations of Python. If running in an implementation without
+    # Python stack frame support this function returns None.
+
+    frame = check.not_none(inspect.currentframe(), NO_STACK_FRAME_ERROR_MSG)
+    back_frame = check.not_none(frame.f_back, NO_STACK_FRAME_ERROR_MSG)
+    back_back_frame = check.not_none(back_frame.f_back, NO_STACK_FRAME_ERROR_MSG)
+    return back_back_frame.f_globals["__name__"]
 
 
 def register_definitions(
@@ -68,25 +96,74 @@ def register_definitions(
     Invoking this function dynamically injects an attribute named "__registered_definitions" into
     the calling module that contains a handle to the definitions.
     """
-    raise NotImplementedError("Not implemented yet")
+    module_name = get_module_name_of_caller()
+    python_module = sys.modules[module_name]
+
+    if MAGIC_REGISTERED_DEFINITIONS_KEY in python_module.__dict__:
+        raise ModuleHasRegisteredDefinitionsError(
+            f"Python module {module_name} already has registered definitions, likely because of a previous call to register_definitions"
+        )
+
+    # This grabs last component of a module name (typically the name
+    # of the file) and uses it for the repository name. Code locations
+    # defined with register_definitions will only have 1 repository defined. And
+    # the web UI will not expose the concept of repository anywhere. In those
+    # cases the name of the repository is immaterial. If the legacy UI is loaded
+    # the repository will have a sensible enough name, but explicitly overriding
+    # that name is not worth polluting this new, cleaner API.
+    repo_name = module_name.split(".")[-1] if "." in module_name else module_name
+
+    resource_defs = coerce_resources_to_defs(resources or {})
+
+    @repository(name=repo_name)
+    def registered_repository():
+        return [
+            *with_resources(assets or [], resource_defs),
+            *(schedules or []),
+            *(sensors or []),
+            *(jobs or []),
+        ]
+
+    python_module.__dict__[MAGIC_REGISTERED_DEFINITIONS_KEY] = registered_repository
 
 
 NO_STACK_FRAME_ERROR_MSG = "Python interpreter must support Python stack frames. Python interpreters that are not CPython do not necessarily implement the necessary APIs."
 
-# invoke this function to get the module name the function that called the current
-# scope
-def get_module_name_of_caller() -> str:
-    # based on https://stackoverflow.com/questions/2000861/retrieve-module-object-from-stack-frame
-    # two f_backs to get past get_module_name_of_caller frame
 
-    # Need to do none checking because of:
-    # https://docs.python.org/3/library/inspect.html#inspect.currentframe
-    # CPython implementation detail: This function relies on Python stack frame
-    # support in the interpreter, which isn’t guaranteed to exist in all
-    # implementations of Python. If running in an implementation without
-    # Python stack frame support this function returns None.
+@contextmanager
+def register_definitions_test_scope(dundername: str) -> Generator[None, None, None]:
+    """
+    A testing utility. Intended usage:
 
-    frame = check.not_none(inspect.currentframe(), NO_STACK_FRAME_ERROR_MSG)
-    back_frame = check.not_none(frame.f_back, NO_STACK_FRAME_ERROR_MSG)
-    back_back_frame = check.not_none(back_frame.f_back, NO_STACK_FRAME_ERROR_MSG)
-    return back_back_frame.f_globals["__name__"]
+    with register_definitions_test_scope(__name__):
+        register_definitions(...)
+
+    This function ensures that no definitions have been registered
+    in the current module. Then in the with block the test can invoke
+    register_definitions. Upon exiting the with both the registered definitions
+    in that module are "unregistered" by deleting the magic attribute.
+    """
+    parent_mod = sys.modules[dundername]
+    assert MAGIC_REGISTERED_DEFINITIONS_KEY not in parent_mod.__dict__
+    try:
+        yield
+    finally:
+        if MAGIC_REGISTERED_DEFINITIONS_KEY in parent_mod.__dict__:
+            del parent_mod.__dict__[MAGIC_REGISTERED_DEFINITIONS_KEY]
+
+
+def get_registered_repository_in_module(
+    module_name: str,
+) -> Union[RepositoryDefinition, PendingRepositoryDefinition]:
+    return sys.modules[module_name].__dict__[MAGIC_REGISTERED_DEFINITIONS_KEY]
+
+
+def coerce_resources_to_defs(resources: Mapping[str, Any]) -> Dict[str, ResourceDefinition]:
+    resource_defs = {}
+    for key, resource_obj in resources.items():
+        resource_defs[key] = (
+            resource_obj
+            if isinstance(resource_obj, ResourceDefinition)
+            else ResourceDefinition.hardcoded_resource(resource_obj)
+        )
+    return resource_defs
--- python_modules/dagster/dagster_tests/core_tests/definitions_tests/test_register_definitions.py
+++ python_modules/dagster/dagster_tests/core_tests/definitions_tests/test_register_definitions.py
@@ -1,6 +1,33 @@
-from dagster import register_definitions
+import sys
+import pytest
 
-from dagster._core.definitions.register_definitions import get_module_name_of_caller
+from dagster import (
+    op,
+    AssetKey,
+    asset,
+    register_definitions,
+    ScheduleDefinition,
+    define_asset_job,
+    sensor,
+    repository,
+    AssetsDefinition,
+    ResourceDefinition,
+)
+from dagster._core.definitions.register_definitions import (
+    MAGIC_REGISTERED_DEFINITIONS_KEY,
+    get_module_name_of_caller,
+    register_definitions_test_scope,
+    ModuleHasRegisteredDefinitionsError,
+    get_registered_repository_in_module,
+)
+from dagster._core.definitions.repository_definition import (
+    PendingRepositoryDefinition,
+    RepositoryDefinition,
+)
+from dagster._core.definitions.cacheable_assets import (
+    AssetsDefinitionCacheableData,
+    CacheableAssetsDefinition,
+)
 
 
 def test_include_register_definition_works():
@@ -16,3 +43,192 @@ def test_module_name_of_caller():
     assert (
         test_module_name == "dagster_tests.core_tests.definitions_tests.test_register_definitions"
     )
+
+
+def test_register_definitions_magic_global():
+    register_definitions()
+    assert MAGIC_REGISTERED_DEFINITIONS_KEY in sys.modules[__name__].__dict__
+    # clean up so subsequent test cases work
+    del sys.modules[__name__].__dict__[MAGIC_REGISTERED_DEFINITIONS_KEY]
+    assert MAGIC_REGISTERED_DEFINITIONS_KEY not in sys.modules[__name__].__dict__
+
+
+def test_register_definitions_test_scope():
+    assert MAGIC_REGISTERED_DEFINITIONS_KEY not in sys.modules[__name__].__dict__
+    with register_definitions_test_scope(__name__):
+        register_definitions()
+        assert MAGIC_REGISTERED_DEFINITIONS_KEY in sys.modules[__name__].__dict__
+
+    assert MAGIC_REGISTERED_DEFINITIONS_KEY not in sys.modules[__name__].__dict__
+
+    with pytest.raises(Exception, match="test cleanup"):
+        with register_definitions_test_scope(__name__):
+            register_definitions()
+            raise Exception("test cleanup")
+
+    assert MAGIC_REGISTERED_DEFINITIONS_KEY not in sys.modules[__name__].__dict__
+
+
+def test_register_definitions_called_twice():
+    with register_definitions_test_scope(__name__):
+        register_definitions()
+        with pytest.raises(ModuleHasRegisteredDefinitionsError):
+            register_definitions()
+
+
+def get_all_assets_from_repo(repo):
+    # could not find public method on repository to do this
+    return list(repo._assets_defs_by_key.values())  # pylint: disable=protected-access
+
+
+def get_resolved_repository_in_scope() -> RepositoryDefinition:
+    repo_or_caching_repo = get_registered_repository_in_module(__name__)
+    return (
+        repo_or_caching_repo.compute_repository_definition()
+        if isinstance(repo_or_caching_repo, PendingRepositoryDefinition)
+        else repo_or_caching_repo
+    )
+
+
+def test_basic_asset_definitions():
+    with register_definitions_test_scope(__name__):
+
+        @asset
+        def an_asset():
+            pass
+
+        register_definitions(assets=[an_asset])
+
+        repo = get_resolved_repository_in_scope()
+        all_assets = get_all_assets_from_repo(repo)
+        # all_assets = list(repo._assets_defs_by_key.values())  # pylint: disable=protected-access
+        assert len(all_assets) == 1
+        assert all_assets[0].key.to_user_string() == "an_asset"
+
+
+def test_basic_schedule_definition():
+    with register_definitions_test_scope(__name__):
+
+        @asset
+        def an_asset():
+            pass
+
+        register_definitions(
+            assets=[an_asset],
+            schedules=[
+                ScheduleDefinition(
+                    name="daily_an_asset_schedule",
+                    job=define_asset_job(name="an_asset_job"),
+                    cron_schedule="@daily",
+                )
+            ],
+        )
+
+        repo = get_resolved_repository_in_scope()
+        assert repo.get_schedule_def("daily_an_asset_schedule")
+
+
+def test_basic_sensor_definition():
+    with register_definitions_test_scope(__name__):
+
+        @asset
+        def an_asset():
+            pass
+
+        an_asset_job = define_asset_job(name="an_asset_job")
+
+        @sensor(name="an_asset_sensor", job=an_asset_job)
+        def a_sensor():
+            raise NotImplementedError()
+
+        register_definitions(
+            assets=[an_asset],
+            sensors=[a_sensor],
+        )
+
+        repo = get_resolved_repository_in_scope()
+
+        assert repo.get_sensor_def("an_asset_sensor")
+
+
+def test_with_resource_binding():
+    with register_definitions_test_scope(__name__):
+
+        executed = {}
+
+        @asset(required_resource_keys={"foo"})
+        def requires_foo(context):
+            assert context.resources.foo == "wrapped"
+            executed["yes"] = True
+
+        register_definitions(
+            assets=[requires_foo],
+            resources={"foo": ResourceDefinition.hardcoded_resource("wrapped")},
+        )
+
+        repo = get_resolved_repository_in_scope()
+
+        asset_job = repo.get_all_jobs()[0]
+        asset_job.execute_in_process()
+        assert executed["yes"]
+
+
+def test_resource_coercion():
+    with register_definitions_test_scope(__name__):
+
+        executed = {}
+
+        @asset(required_resource_keys={"foo"})
+        def requires_foo(context):
+            assert context.resources.foo == "object-to-coerce"
+            executed["yes"] = True
+
+        register_definitions(
+            assets=[requires_foo],
+            resources={"foo": "object-to-coerce"},
+        )
+
+        repo = get_resolved_repository_in_scope()
+
+        asset_job = repo.get_all_jobs()[0]
+        asset_job.execute_in_process()
+        assert executed["yes"]
+
+
+def test_pending_definitions():
+    class MyCacheableAssetsDefinition(CacheableAssetsDefinition):
+        def compute_cacheable_data(self):
+            return [
+                AssetsDefinitionCacheableData(
+                    keys_by_input_name={}, keys_by_output_name={"result": AssetKey(self.unique_id)}
+                )
+            ]
+
+        def build_definitions(self, data):
+            @op
+            def my_op():
+                return 1
+
+            return [
+                AssetsDefinition.from_op(
+                    my_op,
+                    keys_by_input_name=cd.keys_by_input_name,
+                    keys_by_output_name=cd.keys_by_output_name,
+                )
+                for cd in data
+            ]
+
+    @repository
+    def a_pending_repo():
+        return [MyCacheableAssetsDefinition("foobar")]
+
+    assert isinstance(a_pending_repo, PendingRepositoryDefinition)
+
+    assert isinstance(a_pending_repo.compute_repository_definition(), RepositoryDefinition)
+
+    with register_definitions_test_scope(__name__):
+        register_definitions(assets=[MyCacheableAssetsDefinition("foobar")])
+        repo = get_resolved_repository_in_scope()
+        all_assets = get_all_assets_from_repo(repo)
+        assert len(all_assets) == 1
+        assert all_assets[0].key.to_user_string() == "foobar"
