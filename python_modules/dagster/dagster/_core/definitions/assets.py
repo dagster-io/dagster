6e25effbca596798857c8fc9f94c9fd09f732b8f
Owen Kephart <owen@elementl.com>
2022-11-16 12:32:05 -0800

    oops

--- python_modules/dagster/dagster/_core/definitions/assets.py
+++ python_modules/dagster/dagster/_core/definitions/assets.py
@@ -161,7 +161,7 @@ class AssetsDefinition(ResourceAddable):
             )
         for key, freshness_policy in (freshness_policies_by_key or {}).items():
             check.param_invariant(
-                freshness_policy is not None and self._partitions_def is not None,
+                freshness_policy and self._partitions_def,
                 "freshness_policies_by_key",
                 "FreshnessPolicies are currently unsupported for partitioned assets.",
             )
