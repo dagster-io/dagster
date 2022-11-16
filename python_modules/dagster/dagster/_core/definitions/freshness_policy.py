e0a11fcc30302f4f1cbe4cb4dd1c19dcfdf5bf94
Owen Kephart <owen@elementl.com>
2022-11-16 15:19:25 -0800

    extra expl

--- python_modules/dagster/dagster/_core/definitions/freshness_policy.py
+++ python_modules/dagster/dagster/_core/definitions/freshness_policy.py
@@ -40,6 +40,10 @@ class FreshnessPolicy(
     ago. This calculation is recursive: any given asset is expected to incorporate up-to-date
     data from all of its upstream assets.
 
+    Currently, it is assumed that all root asset definitions (asset definitions with no upstream
+    asset definitions) consume from some always-updating source. That is, if you execute a root
+    asset at time T, that root asset will now incorporate all data up to time T.
+
     If `cron_schedule` is not defined, the given asset will be expected to incorporate upstream
     data from no more than `maximum_lag_minutes` ago at all points in time. For example, "The events
     table should always have data from at most 1 hour ago".
