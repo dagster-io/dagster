5da0fbf0ea58574edc32265e4c5d517a9432e601
Erin Cochran <erin.k.cochran@gmail.com>
2022-11-17 13:11:58 -0500

    Round 3

--- docs/next/components/mdx/MDXComponents.tsx
+++ docs/next/components/mdx/MDXComponents.tsx
@@ -25,8 +25,8 @@ import EnvironmentVariablesIntro from './includes/EnvironmentVariablesIntro.mdx'
 import AddGitubRepositorySecret from './includes/dagster-cloud/AddGitubRepositorySecret.mdx';
 import BDCreateConfigureAgent from './includes/dagster-cloud/BDCreateConfigureAgent.mdx';
 import GenerateAgentToken from './includes/dagster-cloud/GenerateAgentToken.mdx';
-import DockerEnvVarsConfiguration from './includes/dagster-cloud/agents/DockerEnvVarsConfiguration.mdx';
 import AmazonEcsEnvVarsConfiguration from './includes/dagster-cloud/agents/AmazonEcsEnvVarsConfiguration.mdx';
+import DockerEnvVarsConfiguration from './includes/dagster-cloud/agents/DockerEnvVarsConfiguration.mdx';
 import K8sEnvVarsConfiguration from './includes/dagster-cloud/agents/K8sEnvVarsConfiguration.mdx';
 import DbtModelAssetExplanation from './includes/dagster/integrations/DbtModelAssetExplanation.mdx';
 
