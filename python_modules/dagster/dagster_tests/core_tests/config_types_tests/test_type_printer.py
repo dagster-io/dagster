56f29b432f714e2d115ae2521e96079a1a7f9ea3
Chris DeCarolis <cdguitar817@gmail.com>
2022-11-22 13:27:49 -0800

    Update API usage in core_tests/config_types_tests (straggler)

--- python_modules/dagster/dagster_tests/core_tests/config_types_tests/test_type_printer.py
+++ python_modules/dagster/dagster_tests/core_tests/config_types_tests/test_type_printer.py
@@ -1,11 +1,10 @@
-from dagster import Field, Int, Map, Noneable, ScalarUnion, String
+from dagster import GraphDefinition, op, Field, Int, Map, Noneable, ScalarUnion, String
 from dagster._config import (
     get_recursive_type_keys,
     print_config_type_to_string,
     resolve_to_config_type,
     snap_from_config_type,
 )
-from dagster._legacy import PipelineDefinition, solid
 
 
 def assert_inner_types(parent_type, *dagster_types):
@@ -101,7 +100,10 @@ def test_double_map_type_print():
 
 def test_list_map_nullable_combos():
     # Don't care about newlines here for brevity's sake, those are tested elsewhere
-    assert print_config_type_to_string({str: [int]}, with_lines=False) == "{ [String]: [Int] }"
+    assert (
+        print_config_type_to_string({str: [int]}, with_lines=False)
+        == "{ [String]: [Int] }"
+    )
     assert (
         print_config_type_to_string(Noneable({str: [int]}), with_lines=False)
         == "{ [String]: [Int] }?"
@@ -119,7 +121,9 @@ def test_list_map_nullable_combos():
         == "{ [String]: [Int?] }?"
     )
     assert (
-        print_config_type_to_string(Noneable({str: Noneable([Noneable(int)])}), with_lines=False)
+        print_config_type_to_string(
+            Noneable({str: Noneable([Noneable(int)])}), with_lines=False
+        )
         == "{ [String]: [Int?]? }?"
     )
 
@@ -195,7 +199,9 @@ def test_single_level_dict_lists_maps_and_nullable():
 
 
 def test_nested_dicts_and_maps():
-    output = print_config_type_to_string({"field_one": {str: {"field_two": {str: int}}}})
+    output = print_config_type_to_string(
+        {"field_one": {str: {"field_two": {str: int}}}}
+    )
     expected = """{
   field_one: {
     [String]: {
@@ -238,19 +244,19 @@ def test_test_type_pipeline_construction():
 
 
 def define_solid_for_test_type(name, config):
-    @solid(name=name, config_schema=config, input_defs=[], output_defs=[])
-    def a_solid(_):
+    @op(name=name, config_schema=config, ins={}, out={})
+    def a_op(_):
         return None
 
-    return a_solid
+    return a_op
 
 
 # launch in dagit with this command:
 # dagit -f test_type_printer.py -n define_test_type_pipeline
 def define_test_type_pipeline():
-    return PipelineDefinition(
+    return GraphDefinition(
         name="test_type_pipeline",
-        solid_defs=[
+        node_defs=[
             define_solid_for_test_type("int_config", int),
             define_solid_for_test_type("list_of_int_config", [int]),
             define_solid_for_test_type("nullable_list_of_int_config", Noneable([int])),
@@ -258,7 +264,9 @@ def define_test_type_pipeline():
             define_solid_for_test_type(
                 "nullable_list_of_nullable_int_config", Noneable([Noneable(int)])
             ),
-            define_solid_for_test_type("simple_dict", {"int_field": int, "string_field": str}),
+            define_solid_for_test_type(
+                "simple_dict", {"int_field": int, "string_field": str}
+            ),
             define_solid_for_test_type(
                 "dict_with_optional_field",
                 {
@@ -269,4 +277,4 @@ def define_test_type_pipeline():
             ),
             define_solid_for_test_type("nested_dict", {"nested": {"int_field": int}}),
         ],
-    )
+    ).to_job()
