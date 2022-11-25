67544beecd234f0be6870377012685bc205f35de
Nicholas Schrock <schrockn@elementl.com>
2022-11-25 11:22:30 -0500

    [𝘀𝗽𝗿] changes to master this commit is based on
    Created using spr 1.3.4
    
    [skip ci]

--- examples/docs_snippets/docs_snippets/guides/dagster/development_to_production/test_assets.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/development_to_production/test_assets.py
@@ -3,7 +3,7 @@ import pandas as pd
 from dagster import build_op_context
 
 from .assets_v2 import items
-from .resources.resources_v2 import StubHNClient, stub_hn_client
+from .resources.resources_v2 import StubHNClient
 
 # start
 # test_assets.py
@@ -11,15 +11,13 @@ from .resources.resources_v2 import StubHNClient, stub_hn_client
 
 def test_items():
     context = build_op_context(
-        resources={"hn_client": stub_hn_client},
+        resources={"hn_client": StubHNClient()},
         op_config={"N": StubHNClient().fetch_max_item_id()},
     )
     hn_dataset = items(context)
     assert isinstance(hn_dataset, pd.DataFrame)
 
-    expected_data = pd.DataFrame(StubHNClient().data.values()).rename(
-        columns={"by": "user_id"}
-    )
+    expected_data = pd.DataFrame(StubHNClient().data.values()).rename(columns={"by": "user_id"})
 
     assert (hn_dataset == expected_data).all().all()
 
