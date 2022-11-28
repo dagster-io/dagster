41d019aeb5cccd380542b0bcd603e5cb46c8b717
Sean Lopp <lopp@Seans-MacBook-Pro.local>
2022-11-28 13:48:55 -0700

    pr feedback

--- examples/docs_snippets/docs_snippets/getting-started/hello-dagster/hello-dagster.py
+++ examples/docs_snippets/docs_snippets/getting-started/hello-dagster/hello-dagster.py
@@ -16,6 +16,7 @@ def hackernews_top_story_ids():
     return top_story_ids[:10]
 
 
+# asset dependencies can be inferred from inputs
 @asset
 def hackernews_top_stories(hackernews_top_story_ids):
     """Get items based on story ids from the HackerNews items endpoint"""
@@ -28,6 +29,8 @@ def hackernews_top_stories(hackernews_top_story_ids):
         results.append(item)
 
     df = pd.DataFrame(results)
+
+    # recorded metadata can be customized
     metadata = {
         "num_records": len(df),
         "preview": MetadataValue.md(df.to_markdown()),
