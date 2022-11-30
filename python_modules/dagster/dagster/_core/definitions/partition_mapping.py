e99227ce6be7662af10b459f068fad5d82185e1a
Claire Lin <claire@elementl.com>
2022-11-30 14:26:20 -0800

    need to add partitionkeysubset

--- python_modules/dagster/dagster/_core/definitions/partition_mapping.py
+++ python_modules/dagster/dagster/_core/definitions/partition_mapping.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, cast
 
 import dagster._check as check
 from dagster._annotations import experimental, public
 from dagster._core.definitions.partition import PartitionsDefinition
 from dagster._core.definitions.partition_key_range import PartitionKeyRange
+from dagster._core.definitions.multi_dimensional_partitions import (
+    MultiPartitionsDefinition,
+    MultiPartitionKey,
+)
 
 
 @experimental
@@ -116,6 +120,111 @@ class LastPartitionMapping(PartitionMapping):
         upstream_partitions_def: PartitionsDefinition,
     ) -> PartitionKeyRange:
         raise NotImplementedError()
+
+
+@experimental
+class SingleDimensionToMultiPartitionMapping(PartitionMapping):
+    """
+    Defines a correspondence between an upstream single-dimensional partitions definition
+    and a downstream MultiPartitionsDefinition. The upstream partitions definition must be
+    a dimension of the downstream MultiPartitionsDefinition.
+
+    Args:
+        partition_dimension_name (str): The name of the partition dimension in the downstream
+            MultiPartitionsDefinition that matches the upstream partitions definition.
+    """
+
+    def __init__(self, partition_dimension_name: str):
+        self.partition_dimension_name = partition_dimension_name
+
+    def get_upstream_partitions_for_partition_range(
+        self,
+        downstream_partition_key_range: Optional[PartitionKeyRange],
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        if downstream_partitions_def is None or downstream_partition_key_range is None:
+            check.failed("downstream asset is not partitioned")
+
+        if not isinstance(downstream_partitions_def, MultiPartitionsDefinition):
+            check.failed("downstream asset is not multi-partitioned")
+        if not (
+            isinstance(downstream_partition_key_range.start, MultiPartitionKey)
+            and isinstance(downstream_partition_key_range.end, MultiPartitionKey)
+        ):
+            check.failed(
+                "Start and end fields of downstream partition key range must be MultiPartitionKeys"
+            )
+
+        matching_dimensions = [
+            partitions_def
+            for partitions_def in downstream_partitions_def.partitions_defs
+            if partitions_def.name == self.partition_dimension_name
+        ]
+        if len(matching_dimensions) != 1:
+            check.failed(f"Partition dimension '{self.partition_dimension_name}' not found")
+        matching_dimension_def = next(iter(matching_dimensions))
+
+        if upstream_partitions_def != matching_dimension_def.partitions_def:
+            check.failed(
+                "The upstream partitions definition does not have the same partitions definition "
+                f"as dimension {matching_dimension_def.name}"
+            )
+
+        range_start = cast(MultiPartitionKey, downstream_partition_key_range.start)
+        range_end = cast(MultiPartitionKey, downstream_partition_key_range.end)
+        return PartitionKeyRange(
+            range_start.keys_by_dimension[self.partition_dimension_name],
+            range_end.keys_by_dimension[self.partition_dimension_name],
+        )
+
+    def get_downstream_partitions_for_partition_range(
+        self,
+        upstream_partition_key_range: PartitionKeyRange,
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        if downstream_partitions_def is None or not isinstance(
+            downstream_partitions_def, MultiPartitionsDefinition
+        ):
+            check.failed("downstream asset is not multi-partitioned")
+
+        matching_dimensions = [
+            partitions_def
+            for partitions_def in downstream_partitions_def.partitions_defs
+            if partitions_def.name == self.partition_dimension_name
+        ]
+        if len(matching_dimensions) != 1:
+            check.failed(f"Partition dimension '{self.partition_dimension_name}' not found")
+        matching_dimension_def = next(iter(matching_dimensions))
+
+        if upstream_partitions_def != matching_dimension_def.partitions_def:
+            check.failed(
+                "The upstream partitions definition does not have the same partitions definition "
+                f"as dimension {matching_dimension_def.name}"
+            )
+
+        dimension_partition_keys = matching_dimension_def.partitions_def.get_partition_keys()
+        check.invariant(
+            upstream_partition_key_range.start in dimension_partition_keys
+            and upstream_partition_key_range.end in dimension_partition_keys,
+            f"Invalid upstream partition key range provided. The range must be a subset of the partition keys of the downstream {self.partition_dimension_name} dimension.",
+        )
+
+        range_start = [
+            key
+            for key in downstream_partitions_def.get_partition_keys()
+            if cast(MultiPartitionKey, key).keys_by_dimension[self.partition_dimension_name]
+            == upstream_partition_key_range.start
+        ][0]
+        range_end = [
+            key
+            for key in downstream_partitions_def.get_partition_keys()
+            if cast(MultiPartitionKey, key).keys_by_dimension[self.partition_dimension_name]
+            == upstream_partition_key_range.end
+        ][-1]
+
+        return PartitionKeyRange(range_start, range_end)
 
 
 def infer_partition_mapping(
---
+++ python_modules/dagster/dagster_tests/core_tests/asset_defs_tests/partition_mapping_tests/test_multipartition_partition_mapping.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+from dagster import (
+    DailyPartitionsDefinition,
+    HourlyPartitionsDefinition,
+    MonthlyPartitionsDefinition,
+    TimeWindowPartitionsDefinition,
+    WeeklyPartitionsDefinition,
+    StaticPartitionsDefinition,
+    MultiPartitionsDefinition,
+    MultiPartitionKey,
+)
+from dagster._core.definitions.partition_key_range import PartitionKeyRange
+from dagster._core.definitions.partition_mapping import SingleDimensionToMultiPartitionMapping
+
+
+def test_single_dimension_to_multi_partition_mapping():
+    upstream_partitions_def = StaticPartitionsDefinition(["a", "b", "c"])
+    downstream_partitions_def = MultiPartitionsDefinition(
+        {"abc": upstream_partitions_def, "123": StaticPartitionsDefinition(["1", "2", "3"])}
+    )
+
+    result = SingleDimensionToMultiPartitionMapping(
+        partition_dimension_name="abc"
+    ).get_downstream_partitions_for_partition_range(
+        upstream_partition_key_range=PartitionKeyRange("a", "a"),
+        downstream_partitions_def=downstream_partitions_def,
+        upstream_partitions_def=upstream_partitions_def,
+    )
+    assert result == PartitionKeyRange(
+        MultiPartitionKey({"abc": "a", "123": "1"}), MultiPartitionKey({"abc": "a", "123": "3"})
+    )
+    assert len(downstream_partitions_def.get_partition_keys_in_range(result)) == 3
+
+    downstream_partitions_def = MultiPartitionsDefinition(
+        {"abc": upstream_partitions_def, "xyz": StaticPartitionsDefinition(["x", "y", "z"])}
+    )
+
+    result = SingleDimensionToMultiPartitionMapping(
+        partition_dimension_name="abc"
+    ).get_downstream_partitions_for_partition_range(
+        upstream_partition_key_range=PartitionKeyRange("b", "b"),
+        downstream_partitions_def=downstream_partitions_def,
+        upstream_partitions_def=upstream_partitions_def,
+    )
+    assert result == PartitionKeyRange(
+        MultiPartitionKey({"abc": "b", "xyz": "x"}), MultiPartitionKey({"abc": "b", "xyz": "z"})
+    )
+    assert len(downstream_partitions_def.get_partition_keys_in_range(result)) == 3
