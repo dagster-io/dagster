fb146dee2e33a4d687c0b3fb04f7d2e6a2adbe2a
JamieDeMaria <jamie@elementl.com>
2022-11-30 16:08:17 -0500

    remove code snippet duplication

--- examples/docs_snippets/docs_snippets/integrations/snowflake/load_downstream.py
+++ examples/docs_snippets/docs_snippets/integrations/snowflake/load_downstream.py
@@ -1,21 +1,11 @@
+iris_dataset = None
+
+# start_example
+
 import pandas as pd
-from dagster_snowflake_pandas import snowflake_pandas_io_manager
+from dagster import asset
 
-from dagster import asset, repository, with_resources
-
-
-@asset
-def iris_dataset() -> pd.DataFrame:
-    return pd.read_csv(
-        "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",
-        names=[
-            "Sepal length (cm)",
-            "Sepal width (cm)",
-            "Petal length (cm)",
-            "Petal width (cm)",
-            "Species",
-        ],
-    )
+# this example uses the iris_dataset asset from Step 2
 
 
 @asset
@@ -23,19 +13,4 @@ def iris_cleaned(iris_dataset: pd.DataFrame):
     return iris_dataset.dropna().drop_duplicates()
 
 
-@repository
-def flowers_analysis_repository():
-    return with_resources(
-        [iris_dataset, iris_cleaned],
-        resource_defs={
-            "io_manager": snowflake_pandas_io_manager.configured(
-                {
-                    "database": "FLOWERS",
-                    "schema": "IRIS",
-                    "account": "abc1234.us-east-1",
-                    "user": {"env": "SNOWFLAKE_USER"},
-                    "password": {"env": "SNOWFLAKE_PASSWORD"},
-                }
-            )
-        },
-    )
+# end_example
--- examples/docs_snippets/docs_snippets/integrations/snowflake/source_asset.py
+++ examples/docs_snippets/docs_snippets/integrations/snowflake/source_asset.py
@@ -2,18 +2,18 @@ from dagster_snowflake_pandas import snowflake_pandas_io_manager
 
 from dagster import SourceAsset, repository, with_resources
 
-daffodil_dataset = SourceAsset(key="daffodil_dataset")
+iris_harvest_data = SourceAsset(key="iris_harvest_data")
 
 
 @repository
 def flowers_analysis_repository():
     return with_resources(
-        [daffodil_dataset],
+        [iris_harvest_data],
         resource_defs={
             "io_manager": snowflake_pandas_io_manager.configured(
                 {
                     "database": "FLOWERS",
-                    "schema": "DAFFODIL",
+                    "schema": "IRIS",
                     "account": "abc1234.us-east-1",
                     "user": {"env": "SNOWFLAKE_USER"},
                     "password": {"env": "SNOWFLAKE_PASSWORD"},
