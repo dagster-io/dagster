a7880a9a0d463a1d33755fc37fd7ccfb1bc3551d
Sean Mackesey <s.mackesey@gmail.com>
2022-12-01 09:55:28 -0500

    add test

--- python_modules/dagster/dagster_tests/execution_tests/test_logical_versions.py
+++ python_modules/dagster/dagster_tests/execution_tests/test_logical_versions.py
@@ -12,6 +12,10 @@ from dagster import (
     io_manager,
     materialize,
 )
+from dagster._core.definitions.asset_out import AssetOut
+from dagster._core.definitions.decorators.asset_decorator import multi_asset
+from dagster._core.definitions.events import AssetKey, Output
+from dagster._core.definitions.logical_version import CODE_VERSION_TAG_KEY, INPUT_EVENT_POINTER_TAG_KEY_PREFIX, INPUT_LOGICAL_VERSION_TAG_KEY_PREFIX, LOGICAL_VERSION_TAG_KEY
 from dagster._core.execution.execute_in_process_result import ExecuteInProcessResult
 
 
@@ -28,7 +32,16 @@ def mock_io_manager():
     return MockIOManager()
 
 
-def get_materialization_from_result(
+def get_mat_from_sequence(
+    key: AssetKey, sequence: Sequence[AssetMaterialization]
+) -> AssetMaterialization:
+    for output in sequence:
+        if output.asset_key == key:
+            return output
+    assert False
+
+
+def get_mat_from_result(
     result: ExecuteInProcessResult, node_str: str
 ) -> AssetMaterialization:
     mats = result.asset_materializations_for_node(node_str)
@@ -36,25 +49,43 @@ def get_materialization_from_result(
     assert isinstance(mats[0], AssetMaterialization)
     return mats[0]
 
+def get_upstream_version_from_mat_provenance(mat: AssetMaterialization, upstream_asset_key: AssetKey) -> str:
+    assert mat.tags
+    return mat.tags[f"{INPUT_LOGICAL_VERSION_TAG_KEY_PREFIX}/{upstream_asset_key.to_user_string()}"]
+
+def get_version_from_mat(mat: AssetMaterialization) -> str:
+    assert mat.tags
+    return mat.tags[LOGICAL_VERSION_TAG_KEY]
+
 
 def assert_same_versions(
     mat1: AssetMaterialization, mat2: AssetMaterialization, code_version: str
 ) -> None:
     assert mat1.tags
-    assert mat1.tags["dagster/code_version"] == code_version
-    assert mat1.tags["dagster/logical_version"] is not None
+    assert mat1.tags[CODE_VERSION_TAG_KEY] == code_version
+    assert mat1.tags[LOGICAL_VERSION_TAG_KEY] is not None
     assert mat2.tags
-    assert mat2.tags["dagster/code_version"] == code_version
-    assert mat2.tags["dagster/logical_version"] == mat1.tags["dagster/logical_version"]
+    assert mat2.tags[CODE_VERSION_TAG_KEY] == code_version
+    assert mat2.tags[LOGICAL_VERSION_TAG_KEY] == mat1.tags["dagster/logical_version"]
 
 
 def assert_different_versions(mat1: AssetMaterialization, mat2: AssetMaterialization) -> None:
     assert mat1.tags
-    assert mat1.tags["dagster/code_version"] is not None
-    assert mat1.tags["dagster/logical_version"] is not None
+    assert mat1.tags[CODE_VERSION_TAG_KEY] is not None
+    assert mat1.tags[LOGICAL_VERSION_TAG_KEY] is not None
     assert mat2.tags
-    assert mat2.tags["dagster/logical_version"] != mat1.tags["dagster/logical_version"]
+    assert mat2.tags[LOGICAL_VERSION_TAG_KEY] != mat1.tags["dagster/logical_version"]
 
+def assert_provenance_match(mat: AssetMaterialization, upstream_mat: AssetMaterialization) -> None:
+    mat_prov_lv = get_upstream_version_from_mat_provenance(mat, upstream_mat.asset_key)
+    upstream_mat_lv = get_version_from_mat(upstream_mat)
+    assert mat_prov_lv == upstream_mat_lv
+
+# Check that mat references upstream mat in its provenance
+def assert_provenance_no_match(mat: AssetMaterialization, upstream_mat: AssetMaterialization) -> None:
+    mat_prov_lv = get_upstream_version_from_mat_provenance(mat, upstream_mat.asset_key)
+    upstream_mat_lv = get_version_from_mat(upstream_mat)
+    assert mat_prov_lv != upstream_mat_lv
 
 def materialize_asset(
     all_assets: Sequence[Union[AssetsDefinition, SourceAsset]],
@@ -74,22 +105,26 @@ def materialize_asset(
 
     result = materialize(assets, instance=instance, resources={"io_manager": mock_io_manager})
     node_str = asset_to_materialize.key.path[-1]
-    mat = get_materialization_from_result(result, node_str)
+    mat = get_mat_from_result(result, node_str)
 
     assert isinstance(mat, AssetMaterialization)
     return mat
 
 
-def materialize_assets(assets, instance) -> Iterable[AssetMaterialization]:
+def materialize_assets(
+    assets: Sequence[AssetsDefinition], instance: DagsterInstance
+) -> Iterable[AssetMaterialization]:
     result = materialize(assets, instance=instance, resources={"io_manager": mock_io_manager})
     for asset_def in assets:
-        if isinstance(asset_def, AssetsDefinition):
-            node_str = asset_def.key.path[-1]
-            yield cast(AssetMaterialization, get_materialization_from_result(result, node_str))
+        node_str = asset_def.node_def.name if asset_def.node_def else asset_def.key.path[-1]
+        for mat in result.asset_materializations_for_node(node_str):
+            yield cast(AssetMaterialization, mat)
 
 
 def materialize_twice(
-    all_assets, asset_to_materialize, instance
+    all_assets: Sequence[Union[AssetsDefinition, SourceAsset]],
+    asset_to_materialize: AssetsDefinition,
+    instance: DagsterInstance,
 ) -> Tuple[AssetMaterialization, AssetMaterialization]:
     mat1 = materialize_asset(all_assets, asset_to_materialize, instance)
     mat2 = materialize_asset(all_assets, asset_to_materialize, instance)
@@ -218,3 +253,41 @@ def test_unversioned_after_versioned():
     asset2_mat2 = materialize_asset(all_assets, asset2, instance)
 
     assert_different_versions(asset2_mat1, asset2_mat2)
+
+
+# def test_multi_asset():
+#     @asset
+#     def start():
+#         return 1
+#
+#     @multi_asset(
+#         outs={
+#             "a": AssetOut(is_required=False),
+#             "b": AssetOut(is_required=False),
+#             "c": AssetOut(is_required=False),
+#         },
+#         internal_asset_deps={
+#             "a": {AssetKey("start")},
+#             "b": {AssetKey("a")},
+#             "c": {AssetKey("a")},
+#         },
+#         can_subset=True,
+#     )
+#     def abc_(context, start):
+#         a = (start + 1) if start else 1
+#         b = a + 1
+#         c = a + 2
+#         out_values = {"a": a, "b": b, "c": c}
+#         outputs_to_return = context.selected_output_names
+#         for output_name in outputs_to_return:
+#             yield Output(out_values[output_name], output_name)
+#
+#     instance = DagsterInstance.ephemeral()
+#     mats_1 = list(materialize_assets([start, abc_], instance))
+#     mat_a_1 = get_mat_from_sequence(AssetKey("a"), mats_1)
+#     mats_2 = list(materialize_assets([start, abc_], instance))
+#     mat_b_2 = get_mat_from_sequence(AssetKey("a"), mats_2)
+#     from IPython import embed; embed()
+#     assert_provenance_no_match(mat_b_2, mat_a_1)
+#
+#
