aadf887204f86ce550193f978710d9321553b7b3
JamieDeMaria <jamie@elementl.com>
2022-12-01 13:20:42 -0500

    hopefully fix bk

--- examples/docs_snippets/docs_snippets_tests/integrations_tests/test_dagstermill.py
+++ examples/docs_snippets/docs_snippets_tests/integrations_tests/test_dagstermill.py
@@ -1,3 +1,4 @@
+import os
 from contextlib import contextmanager
 
 from dagstermill_tests.test_ops import cleanup_result_notebook
@@ -5,6 +6,8 @@ from dagstermill_tests.test_ops import cleanup_result_notebook
 from dagster._core.definitions.reconstruct import ReconstructablePipeline
 from dagster._core.test_utils import instance_for_test
 from dagster._legacy import execute_pipeline
+
+IS_BUILDKITE = os.getenv("BUILDKITE") is not None
 
 
 @contextmanager
@@ -28,24 +31,36 @@ def exec_for_test(module_name, fn_name, env=None, raise_on_error=True, **kwargs)
 
 
 def test_config_asset():
+    module_path = "docs_snippets.integrations.dagstermill.iris_notebook_config"
+    if not IS_BUILDKITE:
+        module_path = "examples.docs_snippets." + module_path
+
     with exec_for_test(
-        module_name="examples.docs_snippets.docs_snippets.integrations.dagstermill.iris_notebook_config",
+        module_name=module_path,
         fn_name="config_asset_job",
     ) as result:
         assert result.success
 
 
 def test_iris_classify_job():
+    module_path = "docs_snippets.integrations.dagstermill.iris_notebook_op"
+    if not IS_BUILDKITE:
+        module_path = "examples.docs_snippets." + module_path
+
     with exec_for_test(
-        module_name="examples.docs_snippets.docs_snippets.integrations.dagstermill.iris_notebook_op",
+        module_name=module_path,
         fn_name="iris_classify",
     ) as result:
         assert result.success
 
 
 def test_outputs_job():
+    module_path = "docs_snippets.integrations.dagstermill.notebook_outputs"
+    if not IS_BUILDKITE:
+        module_path = "examples.docs_snippets." + module_path
+
     with exec_for_test(
-        module_name="examples.docs_snippets.docs_snippets.integrations.dagstermill.notebook_outputs",
+        module_name=module_path,
         fn_name="my_job",
     ) as result:
         assert result.success
