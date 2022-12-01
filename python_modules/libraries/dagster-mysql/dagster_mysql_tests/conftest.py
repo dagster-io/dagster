1feacfa8918de00366d7be214109701bed8ed94f
prha <prha@elementl.com>
2022-11-30 17:32:26 -0800

    add support for older versions of mysql

--- python_modules/libraries/dagster-mysql/dagster_mysql_tests/conftest.py
+++ python_modules/libraries/dagster-mysql/dagster_mysql_tests/conftest.py
@@ -9,9 +9,12 @@ def hostname(conn_string):  # pylint: disable=redefined-outer-name, unused-argum
     return TestMySQLInstance.get_hostname()
 
 
-@pytest.fixture(scope="session")
-def conn_string():  # pylint: disable=redefined-outer-name, unused-argument
+@pytest.fixture(
+    scope="session", params=[("test-mysql-db", {}), ("test-mysql-db-backcompat", {"port": 3307})]
+)
+def conn_string(request):  # pylint: disable=redefined-outer-name, unused-argument
+    service, conn_args = request.param
     with TestMySQLInstance.docker_service_up_or_skip(
-        file_relative_path(__file__, "docker-compose.yml"), "test-mysql-db"
+        file_relative_path(__file__, "docker-compose.yml"), service, conn_args
     ) as conn_str:
         yield conn_str
