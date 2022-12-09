7d5fabc8ad830e0518eec9fee7e783eed8a3fdaf
Nicholas Schrock <schrockn@elementl.com>
2022-12-09 09:16:58 -0500

    [convert-deployment-to-production-3] Convert @repository to Definitions
    Summary & Motivation: Use Definitions instead of repository
    
    Test Plan: BK. Load locally.
    
    Reviewers: yuhan
    
    Reviewed By: yuhan
    
    Pull Request: https://github.com/dagster-io/dagster/pull/10740

--- examples/development_to_production/development_to_production/__init__.py
+++ examples/development_to_production/development_to_production/__init__.py
@@ -4,51 +4,49 @@ from dagster_snowflake_pandas import snowflake_pandas_io_manager
 from development_to_production.assets import comments, items, stories
 from development_to_production.resources import hn_api_client
 
-from dagster import repository, with_resources
+from dagster import Definitions
 
+resource_defs = {
+    "local": {
+        "hn_client": hn_api_client,
+        "snowflake_io_manager": snowflake_pandas_io_manager.configured(
+            {
+                "account": "abc1234.us-east-1",
+                "user": {"env": "DEV_SNOWFLAKE_USER"},
+                "password": {"env": "DEV_SNOWFLAKE_PASSWORD"},
+                "database": "LOCAL",
+                "schema": {"env": "DEV_SNOWFLAKE_SCHEMA"},
+            }
+        ),
+    },
+    "staging": {
+        "hn_client": hn_api_client,
+        "snowflake_io_manager": snowflake_pandas_io_manager.configured(
+            {
+                "account": "abc1234.us-east-1",
+                "user": "system@company.com",
+                "password": {"env": "SYSTEM_SNOWFLAKE_PASSWORD"},
+                "database": "STAGING",
+                "schema": "HACKER_NEWS",
+            }
+        ),
+    },
+    "production": {
+        "hn_client": hn_api_client,
+        "snowflake_io_manager": snowflake_pandas_io_manager.configured(
+            {
+                "account": "abc1234.us-east-1",
+                "user": "system@company.com",
+                "password": {"env": "SYSTEM_SNOWFLAKE_PASSWORD"},
+                "database": "PRODUCTION",
+                "schema": "HACKER_NEWS",
+            }
+        ),
+    },
+}
+deployment_name = os.getenv("DAGSTER_DEPLOYMENT", "local")
 
-@repository
-def repo():
-    resource_defs = {
-        "local": {
-            "hn_client": hn_api_client,
-            "snowflake_io_manager": snowflake_pandas_io_manager.configured(
-                {
-                    "account": "abc1234.us-east-1",
-                    "user": {"env": "DEV_SNOWFLAKE_USER"},
-                    "password": {"env": "DEV_SNOWFLAKE_PASSWORD"},
-                    "database": "LOCAL",
-                    "schema": {"env": "DEV_SNOWFLAKE_SCHEMA"},
-                }
-            ),
-        },
-        "staging": {
-            "hn_client": hn_api_client,
-            "snowflake_io_manager": snowflake_pandas_io_manager.configured(
-                {
-                    "account": "abc1234.us-east-1",
-                    "user": "system@company.com",
-                    "password": {"env": "SYSTEM_SNOWFLAKE_PASSWORD"},
-                    "database": "STAGING",
-                    "schema": "HACKER_NEWS",
-                }
-            ),
-        },
-        "production": {
-            "hn_client": hn_api_client,
-            "snowflake_io_manager": snowflake_pandas_io_manager.configured(
-                {
-                    "account": "abc1234.us-east-1",
-                    "user": "system@company.com",
-                    "password": {"env": "SYSTEM_SNOWFLAKE_PASSWORD"},
-                    "database": "PRODUCTION",
-                    "schema": "HACKER_NEWS",
-                }
-            ),
-        },
-    }
-    deployment_name = os.getenv("DAGSTER_DEPLOYMENT", "local")
-
-    return [
-        *with_resources([items, comments, stories], resource_defs=resource_defs[deployment_name])
-    ]
+defs = Definitions(
+    assets=[items, comments, stories],
+    resources=resource_defs[deployment_name],
+)
