fdbeaf6394e3eaec0ab78d9390487733bf843539
Joseph Van Drunen <josephvandrunen@gmail.com>
2022-12-09 12:47:54 -0500

    fixup

--- examples/docs_snippets/docs_snippets/integrations/airflow/migrate_repo.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from dagster_airflow import make_dagster_repo_from_airflow_dags_path
-
-migrated_airflow_repo = make_dagster_repo_from_airflow_dags_path(
-    os.path.join(os.getenv("AIRFLOW_HOME"), "dags"),
-    "migrated_airflow_repo",
-)
