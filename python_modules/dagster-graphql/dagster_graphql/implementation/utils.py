f1319ba52f55a5d6a5cbdfd54ed69794dbc13d11
Sean Mackesey <s.mackesey@gmail.com>
2022-12-11 10:59:47 -0500

    [dagster-graphql] capture_error sig fix [INTERNAL_BRANCH=sean/release-pipeline-image]

--- python_modules/dagster-graphql/dagster_graphql/implementation/utils.py
+++ python_modules/dagster-graphql/dagster_graphql/implementation/utils.py
@@ -19,6 +19,7 @@ from typing import (
     Tuple,
     Type,
     TypeVar,
+    Union,
     cast,
 )
 
@@ -29,6 +30,8 @@ from dagster._core.workspace.context import BaseWorkspaceRequestContext
 from dagster._utils.error import serializable_error_info_from_exc_info
 from graphene import ResolveInfo
 from typing_extensions import ParamSpec, TypeAlias
+
+from dagster_graphql.schema.errors import GrapheneError
 
 if TYPE_CHECKING:
     from dagster_graphql.schema.errors import GraphenePythonError
@@ -119,8 +122,8 @@ class ErrorCapture:
             ErrorCapture.observer.reset(token)
 
 
-def capture_error(fn: T_Callable) -> T_Callable:
-    def _fn(*args, **kwargs):
+def capture_error(fn: Callable[P, T]) -> Callable[P, Union[T, GrapheneError, GraphenePythonError]]:
+    def _fn(*args: P.args, **kwargs: P.kwargs):
         try:
             return fn(*args, **kwargs)
         except UserFacingGraphQLError as de_exception:
@@ -129,7 +132,7 @@ def capture_error(fn: T_Callable) -> T_Callable:
             ErrorCapture.observer.get()(exc)
             return ErrorCapture.on_exception(sys.exc_info())  # type: ignore
 
-    return cast(T_Callable, _fn)
+    return _fn
 
 
 class UserFacingGraphQLError(Exception):
