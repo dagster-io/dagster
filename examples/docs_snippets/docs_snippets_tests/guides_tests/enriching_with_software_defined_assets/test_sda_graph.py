a609aebc8b98a0a92bef69d2d6f38bc0f5485928
Erin Cochran <erin.k.cochran@gmail.com>
2022-12-12 12:53:30 -0500

    Update tests

--- examples/docs_snippets/docs_snippets_tests/guides_tests/enriching_with_software_defined_assets/test_sda_graph.py
+++ examples/docs_snippets/docs_snippets_tests/guides_tests/enriching_with_software_defined_assets/test_sda_graph.py
@@ -4,4 +4,4 @@ from docs_snippets.guides.dagster.enriching_with_software_defined_assets.sda_gra
 
 
 def test_sda_graph():
-    assert defs.get_job("products_and_categories_job").execute_in_process().success
+    assert defs.get_job_def("products_and_categories_job").execute_in_process().success
--- examples/docs_snippets/docs_snippets_tests/guides_tests/enriching_with_software_defined_assets/test_sda_nothing.py
+++ examples/docs_snippets/docs_snippets_tests/guides_tests/enriching_with_software_defined_assets/test_sda_nothing.py
@@ -4,4 +4,4 @@ from docs_snippets.guides.dagster.enriching_with_software_defined_assets.sda_not
 
 
 def test_sda_nothing():
-    assert defs.get_job("users_recommender_job").execute_in_process().success
+    assert defs.get_job_def("users_recommender_job").execute_in_process().success
