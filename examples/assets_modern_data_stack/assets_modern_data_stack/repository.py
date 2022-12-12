f5a3d4c0a83dd7d27d9adb526c5d543112374889
yuhan <yuhan@elementl.com>
2022-12-12 15:58:12 -0800

    convert-examples assets_modern_data_stack 2.1/ repo -> defs, pyproject

--- examples/assets_modern_data_stack/assets_modern_data_stack/repository.py
+++ examples/assets_modern_data_stack/assets_modern_data_stack/repository.py
@@ -5,8 +5,7 @@ from dagster import (
     ScheduleDefinition,
     define_asset_job,
     load_assets_from_package_module,
-    repository,
-    with_resources,
+    Definitions,
 )
 
 from . import assets
@@ -14,19 +13,17 @@ from .db_io_manager import db_io_manager
 from .utils.constants import AIRBYTE_CONFIG, DBT_CONFIG, POSTGRES_CONFIG
 
 
-@repository
-def assets_modern_data_stack():
-    return [
-        with_resources(
-            load_assets_from_package_module(assets),
-            resource_defs={
-                "airbyte": airbyte_resource.configured(AIRBYTE_CONFIG),
-                "dbt": dbt_cli_resource.configured(DBT_CONFIG),
-                "db_io_manager": db_io_manager.configured(POSTGRES_CONFIG),
-            },
-        ),
+defs = Definitions(
+    assets=load_assets_from_package_module(assets),
+    resources={
+        "airbyte": airbyte_resource.configured(AIRBYTE_CONFIG),
+        "dbt": dbt_cli_resource.configured(DBT_CONFIG),
+        "db_io_manager": db_io_manager.configured(POSTGRES_CONFIG),
+    },
+    schedules=[
         # update all assets once a day
         ScheduleDefinition(
             job=define_asset_job("all_assets", selection="*"), cron_schedule="@daily"
         ),
-    ]
+    ],
+)
--- examples/assets_modern_data_stack/assets_modern_data_stack_tests/test_repo_loads.py
+++ examples/assets_modern_data_stack/assets_modern_data_stack_tests/test_repo_loads.py
@@ -1,11 +1,7 @@
-from assets_modern_data_stack import assets_modern_data_stack
+from assets_modern_data_stack import defs
 
 
-def test_repo_can_load():
-    assets_modern_data_stack.load_all_definitions()
-
+def test_defs_can_load():
     # Repo should have only one "default" asset group, which is represented a "__ASSET_JOB" job
-    assert [job.name for job in assets_modern_data_stack.get_all_jobs()] == [
-        "__ASSET_JOB",
-        "all_assets",
-    ]
+    assert defs.get_job_def("__ASSET_JOB")
+    assert defs.get_job_def("all_assets")
