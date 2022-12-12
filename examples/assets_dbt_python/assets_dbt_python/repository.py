72e457112d92811c7a3ec6a205ee1104fd280438
yuhan <yuhan@elementl.com>
2022-12-12 15:48:22 -0800

    convert-examples assets_dbt_python 1.1/ defs + pyproject

--- examples/assets_dbt_python/assets_dbt_python/repository.py
+++ examples/assets_dbt_python/assets_dbt_python/repository.py
@@ -9,8 +9,7 @@ from dagster import (
     define_asset_job,
     fs_io_manager,
     load_assets_from_package_module,
-    repository,
-    with_resources,
+    Definitions,
 )
 from dagster._utils import file_relative_path
 
@@ -44,25 +43,24 @@ forecasting_assets = load_assets_from_package_module(
 everything_job = define_asset_job("everything_everywhere_job", selection="*")
 forecast_job = define_asset_job("refresh_forecast_model_job", selection="*order_forecast_model")
 
+resources = {
+    # this io_manager allows us to load dbt models as pandas dataframes
+    "io_manager": duckdb_pandas_io_manager.configured(
+        {"database": os.path.join(DBT_PROJECT_DIR, "example.duckdb")}
+    ),
+    # this io_manager is responsible for storing/loading our pickled machine learning model
+    "model_io_manager": fs_io_manager,
+    # this resource is used to execute dbt cli commands
+    "dbt": dbt_cli_resource.configured(
+        {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROFILES_DIR}
+    ),
+}
 
-@repository
-def assets_dbt_python():
-    return with_resources(
-        dbt_assets + raw_data_assets + forecasting_assets,
-        resource_defs={
-            # this io_manager allows us to load dbt models as pandas dataframes
-            "io_manager": duckdb_pandas_io_manager.configured(
-                {"database": os.path.join(DBT_PROJECT_DIR, "example.duckdb")}
-            ),
-            # this io_manager is responsible for storing/loading our pickled machine learning model
-            "model_io_manager": fs_io_manager,
-            # this resource is used to execute dbt cli commands
-            "dbt": dbt_cli_resource.configured(
-                {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROFILES_DIR}
-            ),
-        },
-    ) + [
-        # run everything once a week, but update the forecast model daily
+defs = Definitions(
+    assets=dbt_assets + raw_data_assets + forecasting_assets,
+    resources=resources,
+    schedules=[
         ScheduleDefinition(job=everything_job, cron_schedule="@weekly"),
         ScheduleDefinition(job=forecast_job, cron_schedule="@daily"),
-    ]
+    ],
+)
