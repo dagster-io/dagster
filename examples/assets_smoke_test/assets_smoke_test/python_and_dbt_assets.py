5fa9abc8182b8f10ceafcbec3b2985ef380f84cc
yuhan <yuhan@elementl.com>
2022-12-12 23:26:53 -0800

    convert-examples 5/ assets_smoke_test repository -> definitions

--- examples/assets_smoke_test/assets_smoke_test/python_and_dbt_assets.py
+++ examples/assets_smoke_test/assets_smoke_test/python_and_dbt_assets.py
@@ -3,11 +3,11 @@ from dagster_snowflake_pandas import snowflake_pandas_io_manager
 from pandas import DataFrame
 
 from dagster import (
+    Definitions,
     SourceAsset,
     TableSchema,
     asset,
     load_assets_from_current_module,
-    repository,
     with_resources,
 )
 from dagster._utils import file_relative_path
@@ -43,22 +43,20 @@ def country_stats(country_populations: DataFrame, continent_stats: DataFrame) ->
 dbt_assets = load_assets_from_dbt_project(DBT_PROJECT_DIR, DBT_PROFILES_DIR)
 
 
-@repository
-def repo():
-    return with_resources(
-        load_assets_from_current_module(),
-        resource_defs={
-            "io_manager": snowflake_pandas_io_manager.configured(
-                {
-                    "account": {"env": "SNOWFLAKE_ACCOUNT"},
-                    "user": {"env": "SNOWFLAKE_USER"},
-                    "password": {"env": "SNOWFLAKE_PASSWORD"},
-                    "database": "DEV_SANDY",
-                    "warehouse": "ELEMENTL",
-                }
-            ),
-            "dbt": dbt_cli_resource.configured(
-                {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROFILES_DIR}
-            ),
-        },
-    )
+defs = Definitions(
+    assets=load_assets_from_current_module(),
+    resources={
+        "io_manager": snowflake_pandas_io_manager.configured(
+            {
+                "account": {"env": "SNOWFLAKE_ACCOUNT"},
+                "user": {"env": "SNOWFLAKE_USER"},
+                "password": {"env": "SNOWFLAKE_PASSWORD"},
+                "database": "DEV_SANDY",
+                "warehouse": "ELEMENTL",
+            }
+        ),
+        "dbt": dbt_cli_resource.configured(
+            {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROFILES_DIR}
+        ),
+    },
+)
