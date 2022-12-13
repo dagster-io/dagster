432abcc5126500af437acea6a3996446ad8ae15f
yuhan <yuhan@elementl.com>
2022-12-12 23:59:56 -0800

    convert-examples ?? with_airflow

--- examples/with_airflow/with_airflow/repository.py
+++ examples/with_airflow/with_airflow/repository.py
@@ -10,17 +10,13 @@ from with_airflow.airflow_complex_dag import complex_dag
 from with_airflow.airflow_kubernetes_dag import kubernetes_dag
 from with_airflow.airflow_simple_dag import simple_dag
 
-from dagster import repository
+from dagster import Definitions
 
 airflow_simple_dag = make_dagster_job_from_airflow_dag(simple_dag)
 airflow_complex_dag = make_dagster_job_from_airflow_dag(complex_dag)
 airflow_kubernetes_dag = make_dagster_job_from_airflow_dag(kubernetes_dag, mock_xcom=True)
 
-
-@repository
-def with_airflow():
-    return [airflow_complex_dag, airflow_simple_dag, airflow_kubernetes_dag]
-
+defs = Definitions(jobs=[airflow_complex_dag, airflow_simple_dag, airflow_kubernetes_dag])
 
 # end_repo_marker_0
 
