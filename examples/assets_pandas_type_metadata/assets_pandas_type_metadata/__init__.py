79b592b0a98cde0357af3326c7cd5b0e3356b1f7
yuhan <yuhan@elementl.com>
2022-12-12 23:14:01 -0800

    convert-examples 4.2/ assets_pandas_type_metadata repository.py -> __init__.py

--- examples/assets_pandas_type_metadata/assets_pandas_type_metadata/__init__.py
+++ examples/assets_pandas_type_metadata/assets_pandas_type_metadata/__init__.py
@@ -1,2 +1,8 @@
-from . import lib
-from .repository import defs
+from dagster import Definitions, load_assets_from_package_module
+
+from . import assets, lib
+from .resources.csv_io_manager import local_csv_io_manager
+
+defs = Definitions(
+    assets=load_assets_from_package_module(assets), resources={"io_manager": local_csv_io_manager}
+)
--- examples/assets_pandas_type_metadata/assets_pandas_type_metadata/repository.py
+++
@@ -1,8 +0,0 @@
-from assets_pandas_type_metadata import assets
-from assets_pandas_type_metadata.resources.csv_io_manager import local_csv_io_manager
-
-from dagster import Definitions, load_assets_from_package_module
-
-defs = Definitions(
-    assets=load_assets_from_package_module(assets), resources={"io_manager": local_csv_io_manager}
-)
