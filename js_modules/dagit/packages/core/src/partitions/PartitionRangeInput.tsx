5ebe03e37c5dade99835f9823db82f64beb5d8ac
Ben Gotow <ben@foundry376.com>
2022-12-14 11:58:25 -0600

    [dagit] Persist AssetPartitions state to URL, open Materialize panel with same selection (#11042)
    * [dagit] Persist AssetPartitions state to URL, open Materialize panel with same selection
    
    * Address PR feedback, add tests
    
    Co-authored-by: bengotow <bgotow@elementl.com>

--- js_modules/dagit/packages/core/src/partitions/PartitionRangeInput.tsx
+++ js_modules/dagit/packages/core/src/partitions/PartitionRangeInput.tsx
@@ -4,6 +4,8 @@ import * as React from 'react';
 import {showCustomAlert} from '../app/CustomAlertProvider';
 import {ClearButton} from '../ui/ClearButton';
 
+import {partitionsToText, textToPartitions} from './SpanRepresentation';
+
 export const PartitionRangeInput: React.FC<{
   value: string[];
   onChange: (partitionNames: string[]) => void;
@@ -56,30 +58,7 @@ export const PartitionRangeInput: React.FC<{
     />
   );
 };
-
-export function assembleIntoSpans<T>(keys: string[], keyTestFn: (key: string, idx: number) => T) {
-  const spans: {startIdx: number; endIdx: number; status: T}[] = [];
-
-  for (let ii = 0; ii < keys.length; ii++) {
-    const status = keyTestFn(keys[ii], ii);
-    if (!spans.length || spans[spans.length - 1].status !== status) {
-      spans.push({startIdx: ii, endIdx: ii, status});
-    } else {
-      spans[spans.length - 1].endIdx = ii;
-    }
-  }
-
-  return spans;
-}
-
-export function stringForSpan(
-  {startIdx, endIdx}: {startIdx: number; endIdx: number},
-  all: string[],
-) {
-  return startIdx === endIdx ? all[startIdx] : `[${all[startIdx]}...${all[endIdx]}]`;
-}
-
-function placeholderForPartitions(names: string[], isTimeseries: boolean) {
+export function placeholderForPartitions(names: string[], isTimeseries: boolean) {
   if (names.length === 0) {
     return '';
   }
@@ -87,41 +66,4 @@ function placeholderForPartitions(names: string[], isTimeseries: boolean) {
     return `ex: ${names[0]}, ${names[1]}`;
   }
   return `ex: ${names[0]}, ${names[1]}, [${names[2]}...${names[names.length - 1]}]`;
-}
-
-function textToPartitions(selected: string, all: string[]) {
-  const terms = selected.split(',').map((s) => s.trim());
-  const result = [];
-  for (const term of terms) {
-    if (term.length === 0) {
-      continue;
-    }
-    const rangeMatch = /^\[(.*)\.\.\.(.*)\]$/g.exec(term);
-    if (rangeMatch) {
-      const [, start, end] = rangeMatch;
-      const allStartIdx = all.indexOf(start);
-      const allEndIdx = all.indexOf(end);
-      if (allStartIdx === -1 || allEndIdx === -1) {
-        throw new Error(`Could not find partitions for provided range: ${start}...${end}`);
-      }
-      result.push(...all.slice(allStartIdx, allEndIdx + 1));
-    } else if (term.includes('*')) {
-      const [prefix, suffix] = term.split('*');
-      result.push(...all.filter((p) => p.startsWith(prefix) && p.endsWith(suffix)));
-    } else {
-      const idx = all.indexOf(term);
-      if (idx === -1) {
-        throw new Error(`Could not find partition: ${term}`);
-      }
-      result.push(term);
-    }
-  }
-  return result.sort((a, b) => all.indexOf(a) - all.indexOf(b));
-}
-
-function partitionsToText(selected: string[], all: string[]) {
-  return assembleIntoSpans(all, (key) => selected.includes(key))
-    .filter((s) => s.status)
-    .map((s) => stringForSpan(s, all))
-    .join(', ');
 }
