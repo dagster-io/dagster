62d984b820286dc553127f900e1ad7a10a64f7c6
yuhan <yuhan@elementl.com>
2022-12-14 14:53:18 -0800

    convert-examples-repo-to-defs cloud nux quickstart_snowflake

--- examples/quickstart_snowflake/quickstart_snowflake/__init__.py
+++ examples/quickstart_snowflake/quickstart_snowflake/__init__.py
@@ -1,1 +1,35 @@
-from .repository import quickstart_snowflake
+from dagster_snowflake import build_snowflake_io_manager
+from dagster_snowflake_pandas import SnowflakePandasTypeHandler
+
+from dagster import (
+    Definitions,
+    ScheduleDefinition,
+    define_asset_job,
+    load_assets_from_package_module,
+)
+
+from . import assets
+
+daily_refresh_schedule = ScheduleDefinition(
+    job=define_asset_job(name="all_assets_job"), cron_schedule="0 0 * * *"
+)
+
+
+defs = Definitions(
+    assets=load_assets_from_package_module(assets),
+    resources={
+        "io_manager": build_snowflake_io_manager([SnowflakePandasTypeHandler()]).configured(
+            # Read about using environment variables and secrets in Dagster:
+            # https://docs.dagster.io/guides/dagster/using-environment-variables-and-secrets
+            {
+                "account": {"env": "SNOWFLAKE_ACCOUNT"},
+                "user": {"env": "SNOWFLAKE_USER"},
+                "password": {"env": "SNOWFLAKE_PASSWORD"},
+                "warehouse": {"env": "SNOWFLAKE_WAREHOUSE"},
+                "database": {"env": "SNOWFLAKE_DATABASE"},
+                "schema": {"env": "SNOWFLAKE_SCHEMA"},
+            }
+        ),
+    },
+    schedules=[daily_refresh_schedule],
+)
