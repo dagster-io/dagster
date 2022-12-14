aaeab954e28de63c54741dcdff06d25973853990
yuhan <yuhan@elementl.com>
2022-12-13 23:53:41 -0800

    update tests

--- examples/assets_pandas_pyspark/assets_pandas_pyspark_tests/test_assets.py
+++ examples/assets_pandas_pyspark/assets_pandas_pyspark_tests/test_assets.py
@@ -7,21 +7,20 @@ from dagster._core.test_utils import instance_for_test
 def test_weather_assets():
     from assets_pandas_pyspark.local_filesystem_io_manager import local_filesystem_io_manager
 
-    weather_assets = with_resources(
-        load_assets_from_modules([table_assets]),
-        resource_defs={"io_manager": local_filesystem_io_manager},
-    )
     with instance_for_test() as instance:
-        assert materialize(weather_assets, instance=instance).success
+        assert materialize(
+            load_assets_from_modules([table_assets]),
+            instance=instance,
+            resources={"io_manager": local_filesystem_io_manager},
+        ).success
 
 
 def test_spark_weather_assets():
     from assets_pandas_pyspark.local_spark_filesystem_io_manager import local_filesystem_io_manager
 
-    spark_weather_assets = with_resources(
-        load_assets_from_modules([table_assets, spark_asset]),
-        resource_defs={"io_manager": local_filesystem_io_manager},
-    )
-
     with instance_for_test() as instance:
-        assert materialize(spark_weather_assets, instance=instance).success
+        assert materialize(
+            load_assets_from_modules([table_assets, spark_asset]),
+            instance=instance,
+            resources={"io_manager": local_filesystem_io_manager},
+        ).success
