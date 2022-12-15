598d719b4118546fbba530ef47bac491bf0bdcfa
Nicholas Schrock <schrockn@elementl.com>
2022-12-14 22:42:46 -0500

    lint
    Created using spr 1.3.4

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_asset_different_io_managers.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_asset_different_io_managers.py
@@ -2,7 +2,7 @@ from docs_snippets.concepts.assets.asset_different_io_managers import defs
 
 
 def test():
-    assert (
-        len(defs.get_repository_def()._assets_defs_by_key)
-        == 2  # pylint: disable=protected-access
+    assets_defs_by_key = (
+        defs.get_repository_def()._assets_defs_by_key  # pylint: disable=protected-access
     )
+    assert len(assets_defs_by_key) == 2
--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_asset_io_manager.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_asset_io_manager.py
@@ -2,7 +2,7 @@ from docs_snippets.concepts.assets.asset_io_manager import defs
 
 
 def test():
-    assert (
-        len(defs.get_repository_def()._assets_defs_by_key)
-        == 2  # pylint: disable=protected-access
+    assets_defs_by_key = (
+        defs.get_repository_def()._assets_defs_by_key  # pylint: disable=protected-access
     )
+    assert len(assets_defs_by_key) == 2
