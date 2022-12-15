84feccfc41b2b85b9d43f5714dfa5a69f0095d4f
Nicholas Schrock <schrockn@elementl.com>
2022-12-14 21:54:55 -0500

    fix test
    Created using spr 1.3.4

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_build_job.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_build_job.py
@@ -1,6 +1,6 @@
-from docs_snippets.concepts.assets.build_job import repo
+from docs_snippets.concepts.assets.build_job import defs
 
 
-def test():
-    assert repo.get_job("all_assets_job").execute_in_process().success
-    assert repo.get_job("asset1_job").execute_in_process().success
+def test_build_job_doc_snippet():
+    assert defs.get_job_def("all_assets_job").execute_in_process().success
+    assert defs.get_job_def("asset1_job").execute_in_process().success
