d863e3229172a722892d491e845fbc374d30c9f1
prha <prha@elementl.com>
2022-12-16 18:17:04 -0800

    split backfill table so partition status is fetched lazily

--- js_modules/dagit/packages/core/src/instance/types/BackfillTableFragment.ts
+++ js_modules/dagit/packages/core/src/instance/types/BackfillTableFragment.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { BulkActionStatus, RunStatus } from "./../../types/globalTypes";
+import { BulkActionStatus } from "./../../types/globalTypes";
 
 // ====================================================
 // GraphQL fragment: BackfillTableFragment
@@ -23,19 +23,6 @@ export interface BackfillTableFragment_partitionSet {
   mode: string;
   pipelineName: string;
   repositoryOrigin: BackfillTableFragment_partitionSet_repositoryOrigin;
-}
-
-export interface BackfillTableFragment_partitionStatuses_results {
-  __typename: "PartitionStatus";
-  id: string;
-  partitionName: string;
-  runId: string | null;
-  runStatus: RunStatus | null;
-}
-
-export interface BackfillTableFragment_partitionStatuses {
-  __typename: "PartitionStatuses";
-  results: BackfillTableFragment_partitionStatuses_results[];
 }
 
 export interface BackfillTableFragment_assetSelection {
@@ -66,7 +53,6 @@ export interface BackfillTableFragment {
   timestamp: number;
   partitionSetName: string;
   partitionSet: BackfillTableFragment_partitionSet | null;
-  partitionStatuses: BackfillTableFragment_partitionStatuses;
   assetSelection: BackfillTableFragment_assetSelection[] | null;
   error: BackfillTableFragment_error | null;
 }
