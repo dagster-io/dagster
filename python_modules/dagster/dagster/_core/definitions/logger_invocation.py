9bdf94c7b880138ddbffe0c6866879b15780f9e0
Nicholas Schrock <schrockn@elementl.com>
2022-12-17 18:21:57 -0500

    consolidate logger

--- python_modules/dagster/dagster/_core/definitions/logger_invocation.py
+++ python_modules/dagster/dagster/_core/definitions/logger_invocation.py
@@ -6,6 +6,8 @@ from ..._config import Shape
 from ..execution.context.logger import InitLoggerContext, UnboundInitLoggerContext
 from .logger_definition import LoggerDefinition
 
+from .resource_invocation import _resolve_bound_config
+
 
 def logger_invocation_result(logger_def: LoggerDefinition, init_context: UnboundInitLoggerContext):
     """Using the provided context, call the underlying `logger_fn` and return created logger."""
@@ -19,33 +21,30 @@ def logger_invocation_result(logger_def: LoggerDefinition, init_context: Unbound
     return logger_def.logger_fn(bound_context)
 
 
-def _resolve_bound_config(logger_config: Any, logger_def: "LoggerDefinition") -> Any:
-    from dagster._config import process_config
+# def _resolve_bound_config(config: Any, logger_def: "LoggerDefinition") -> Any:
+#     from dagster._config import process_config
 
-    validated_config = None
-    outer_config_shape = Shape({"config": logger_def.get_config_field()})
-    config_evr = process_config(
-        outer_config_shape, {"config": logger_config} if logger_config else {}
-    )
-    if not config_evr.success:
-        raise DagsterInvalidConfigError(
-            "Error in config for logger ",
-            config_evr.errors,
-            logger_config,
-        )
-    validated_config = cast(Dict, config_evr.value).get("config")
-    mapped_config_evr = logger_def.apply_config_mapping({"config": validated_config})
-    if not mapped_config_evr.success:
-        raise DagsterInvalidConfigError(
-            "Error in config mapping for logger ", mapped_config_evr.errors, validated_config
-        )
-    validated_config = cast(Dict, mapped_config_evr.value).get("config")
-    return validated_config
+#     outer_config_shape = Shape({"config": logger_def.get_config_field()})
+#     config_evr = process_config(outer_config_shape, {"config": config} if config else {})
+#     if not config_evr.success:
+#         raise DagsterInvalidConfigError(
+#             "Error in config for logger ",
+#             config_evr.errors,
+#             config,
+#         )
+#     validated_config = cast(Dict, config_evr.value).get("config")
+#     mapped_config_evr = logger_def.apply_config_mapping({"config": validated_config})
+#     if not mapped_config_evr.success:
+#         raise DagsterInvalidConfigError(
+#             "Error in config mapping for logger ", mapped_config_evr.errors, validated_config
+#         )
+#     validated_config = cast(Dict, mapped_config_evr.value).get("config")
+#     return validated_config
 
 
-def _get_default_if_exists(logger_def: LoggerDefinition):
-    return (
-        logger_def.config_field.default_value
-        if logger_def.config_field and logger_def.config_field.default_provided
-        else None
-    )
+# def _get_default_if_exists(logger_def: LoggerDefinition):
+#     return (
+#         logger_def.config_field.default_value
+#         if logger_def.config_field and logger_def.config_field.default_provided
+#         else None
+#     )
