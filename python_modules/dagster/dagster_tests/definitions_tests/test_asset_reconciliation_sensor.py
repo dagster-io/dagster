52e5dadec6818d6eec7f9dd38a4febec2e23bc34
Owen Kephart <owen@elementl.com>
2022-12-19 11:53:54 -0500

    remove unused tests

--- python_modules/dagster/dagster_tests/definitions_tests/test_asset_reconciliation_sensor.py
+++ python_modules/dagster/dagster_tests/definitions_tests/test_asset_reconciliation_sensor.py
@@ -959,33 +959,6 @@ def test_reconciliation(scenario):
 @pytest.mark.parametrize(
     "scenario",
     [
-        scenarios["freshness_complex_subsettable"],
-    ],
-)
-def test_reconciliation_no_tags(scenario):
-    # simulates an environment where asset_event_tags cannot be added
-    instance = DagsterInstance.ephemeral()
-
-    run_requests, _ = scenario.do_scenario(instance)
-
-    assert len(run_requests) == len(scenario.expected_run_requests)
-
-    def sort_run_request_key_fn(run_request):
-        return (min(run_request.asset_selection), run_request.partition_key)
-
-    sorted_run_requests = sorted(run_requests, key=sort_run_request_key_fn)
-    sorted_expected_run_requests = sorted(
-        scenario.expected_run_requests, key=sort_run_request_key_fn
-    )
-
-    for run_request, expected_run_request in zip(sorted_run_requests, sorted_expected_run_requests):
-        assert set(run_request.asset_selection) == set(expected_run_request.asset_selection)
-        assert run_request.partition_key == expected_run_request.partition_key
-
-
-@pytest.mark.parametrize(
-    "scenario",
-    [
         scenarios["diamond_never_materialized"],
         scenarios["one_asset_daily_partitions_never_materialized"],
     ],
