02d27fd8597543d4af34ad7aa8c2cc1504158d98
Nicholas Schrock <schrockn@elementl.com>
2022-12-20 10:43:26 -0500

    Move resource construction into functions

--- examples/project_fully_featured/project_fully_featured/__init__.py
+++ examples/project_fully_featured/project_fully_featured/__init__.py
@@ -4,16 +4,10 @@ from dagster import Definitions
 
 from .assets import activity_analytics_assets, core_assets, dbt_assets, recommender_assets
 from .jobs import activity_analytics_assets_sensor, core_assets_schedule, recommender_assets_sensor
-from .resources import RESOURCES_LOCAL, RESOURCES_PROD, RESOURCES_STAGING
+from .resources import get_local_resources, get_prod_resources, get_staging_resources
 from .sensors import make_slack_on_failure_sensor
 
 all_assets = [*core_assets, *recommender_assets, *dbt_assets, *activity_analytics_assets]
-
-resources_by_deployment_name = {
-    "prod": RESOURCES_PROD,
-    "staging": RESOURCES_STAGING,
-    "local": RESOURCES_LOCAL,
-}
 
 deployment_name = os.environ.get("DAGSTER_DEPLOYMENT", "local")
 
@@ -21,9 +15,21 @@ all_sensors = [activity_analytics_assets_sensor, recommender_assets_sensor]
 if deployment_name in ["prod", "staging"]:
     all_sensors.append(make_slack_on_failure_sensor(base_url="my_dagit_url"))
 
+
+def get_resources():
+    if deployment_name == "prod":
+        return get_prod_resources()
+    elif deployment_name == "staging":
+        return get_staging_resources()
+    elif deployment_name == "local":
+        return get_local_resources()
+
+    raise Exception(f"Invalid deployment name {deployment_name}")
+
+
 defs = Definitions(
     assets=all_assets,
-    resources=resources_by_deployment_name[deployment_name],
+    resources=get_resources(),
     schedules=[core_assets_schedule],
     sensors=all_sensors,
 )
--- examples/project_fully_featured/project_fully_featured/resources/__init__.py
+++ examples/project_fully_featured/project_fully_featured/resources/__init__.py
@@ -16,21 +16,23 @@ DBT_PROJECT_DIR = file_relative_path(__file__, "../../dbt_project")
 DBT_PROFILES_DIR = DBT_PROJECT_DIR + "/config"
 
 
-configured_pyspark = PySparkResource(
-    spark_conf={
-        "spark.jars.packages": ",".join(
-            [
-                "net.snowflake:snowflake-jdbc:3.8.0",
-                "net.snowflake:spark-snowflake_2.12:2.8.2-spark_3.0",
-                "com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.7",
-            ]
-        ),
-        "spark.hadoop.fs.s3.impl": "org.apache.hadoop.fs.s3native.NativeS3FileSystem",
-        "spark.hadoop.fs.s3.awsAccessKeyId": os.getenv("AWS_ACCESS_KEY_ID", ""),
-        "spark.hadoop.fs.s3.awsSecretAccessKey": os.getenv("AWS_SECRET_ACCESS_KEY", ""),
-        "spark.hadoop.fs.s3.buffer.dir": "/tmp",
-    }
-)
+def get_configured_pyspark_resource():
+    return PySparkResource(
+        spark_conf={
+            "spark.jars.packages": ",".join(
+                [
+                    "net.snowflake:snowflake-jdbc:3.8.0",
+                    "net.snowflake:spark-snowflake_2.12:2.8.2-spark_3.0",
+                    "com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.7",
+                ]
+            ),
+            "spark.hadoop.fs.s3.impl": "org.apache.hadoop.fs.s3native.NativeS3FileSystem",
+            "spark.hadoop.fs.s3.awsAccessKeyId": os.getenv("AWS_ACCESS_KEY_ID", ""),
+            "spark.hadoop.fs.s3.awsSecretAccessKey": os.getenv("AWS_SECRET_ACCESS_KEY", ""),
+            "spark.hadoop.fs.s3.buffer.dir": "/tmp",
+        }
+    )
+
 
 SHARED_SNOWFLAKE_CONF = {
     "account": os.getenv("SNOWFLAKE_ACCOUNT", ""),
@@ -39,55 +41,65 @@ SHARED_SNOWFLAKE_CONF = {
     "warehouse": "TINY_WAREHOUSE",
 }
 
-s3_session = build_s3_session()
 
-s3_prod_bucket = "hackernews-elementl-prod"
+def get_prod_resources():
+    s3_prod_bucket = "hackernews-elementl-prod"
 
-RESOURCES_PROD = {
-    "io_manager": deferred_io_manager(
-        lambda: PickledObjectS3IOManager(s3_bucket=s3_prod_bucket, s3_session=s3_session)
-    ),
-    "parquet_io_manager": PartitionedParquetIOManager(
-        base_path="s3://" + s3_prod_bucket,
-        pyspark_resource=configured_pyspark,
-    ),
-    "warehouse_io_manager": SnowflakeIOManager(database="DEMO_DB", **SHARED_SNOWFLAKE_CONF),
-    "hn_client": HNAPISubsampleClient(subsample_rate=10),
-    "dbt": DbtCliResource(
-        profiles_dir=DBT_PROFILES_DIR, project_dir=DBT_PROJECT_DIR, target="prod"
-    ),
-}
-
-s3_staging_bucket = "hackernews-elementl-dev"
-
-RESOURCES_STAGING = {
-    "io_manager": deferred_io_manager(
-        lambda: PickledObjectS3IOManager(s3_bucket=s3_staging_bucket, s3_session=s3_session)
-    ),
-    "parquet_io_manager": PartitionedParquetIOManager(
-        base_path="s3://" + s3_staging_bucket,
-        pyspark_resource=configured_pyspark,
-    ),
-    "warehouse_io_manager": SnowflakeIOManager(database="DEMO_DB_STAGIN", **SHARED_SNOWFLAKE_CONF),
-    "hn_client": HNAPISubsampleClient(subsample_rate=10),
-    "dbt": DbtCliResource(
-        profiles_dir=DBT_PROFILES_DIR, project_dir=DBT_PROJECT_DIR, target="staging"
-    ),
-}
+    return {
+        "io_manager": deferred_io_manager(
+            lambda: PickledObjectS3IOManager(
+                s3_bucket=s3_prod_bucket, s3_session=build_s3_session()
+            )
+        ),
+        "parquet_io_manager": PartitionedParquetIOManager(
+            base_path="s3://" + s3_prod_bucket,
+            pyspark_resource=get_configured_pyspark_resource(),
+        ),
+        "warehouse_io_manager": SnowflakeIOManager(database="DEMO_DB", **SHARED_SNOWFLAKE_CONF),
+        "hn_client": HNAPISubsampleClient(subsample_rate=10),
+        "dbt": DbtCliResource(
+            profiles_dir=DBT_PROFILES_DIR, project_dir=DBT_PROJECT_DIR, target="prod"
+        ),
+    }
 
 
-RESOURCES_LOCAL = {
-    "parquet_io_manager": PartitionedParquetIOManager(
-        base_path=get_system_temp_directory(),
-        pyspark_resource=configured_pyspark,
-    ),
-    "warehouse_io_manager": DuckDBPartitionedParquetIOManager(
-        base_path=get_system_temp_directory(),
-        duckdb_path=os.path.join(DBT_PROJECT_DIR, "hackernews.duckdb"),
-        pyspark_resource=configured_pyspark,
-    ),
-    "hn_client": HNAPIClient(),
-    "dbt": DbtCliResource(
-        profiles_dir=DBT_PROFILES_DIR, project_dir=DBT_PROJECT_DIR, target="local"
-    ),
-}
+def get_staging_resources():
+    s3_staging_bucket = "hackernews-elementl-dev"
+
+    return {
+        "io_manager": deferred_io_manager(
+            lambda: PickledObjectS3IOManager(
+                s3_bucket=s3_staging_bucket, s3_session=build_s3_session()
+            )
+        ),
+        "parquet_io_manager": PartitionedParquetIOManager(
+            base_path="s3://" + s3_staging_bucket,
+            pyspark_resource=get_configured_pyspark_resource(),
+        ),
+        "warehouse_io_manager": SnowflakeIOManager(
+            database="DEMO_DB_STAGIN", **SHARED_SNOWFLAKE_CONF
+        ),
+        "hn_client": HNAPISubsampleClient(subsample_rate=10),
+        "dbt": DbtCliResource(
+            profiles_dir=DBT_PROFILES_DIR, project_dir=DBT_PROJECT_DIR, target="staging"
+        ),
+    }
+
+
+def get_local_resources():
+    configured_pyspark = get_configured_pyspark_resource()
+    return {
+        "parquet_io_manager": PartitionedParquetIOManager(
+            base_path=get_system_temp_directory(),
+            pyspark_resource=configured_pyspark,
+        ),
+        "warehouse_io_manager": DuckDBPartitionedParquetIOManager(
+            base_path=get_system_temp_directory(),
+            duckdb_path=os.path.join(DBT_PROJECT_DIR, "hackernews.duckdb"),
+            pyspark_resource=configured_pyspark,
+        ),
+        "hn_client": HNAPIClient(),
+        "dbt": DbtCliResource(
+            profiles_dir=DBT_PROFILES_DIR, project_dir=DBT_PROJECT_DIR, target="local"
+        ),
+    }
