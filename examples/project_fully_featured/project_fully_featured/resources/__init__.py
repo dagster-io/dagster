a52a91857673d82d01930f3bbea88aa3307ed4a8
Nicholas Schrock <schrockn@elementl.com>
2022-12-19 21:47:33 -0500

    correct indentation for pyspark config

--- examples/project_fully_featured/project_fully_featured/resources/__init__.py
+++ examples/project_fully_featured/project_fully_featured/resources/__init__.py
@@ -28,19 +28,17 @@ dbt_prod_resource = dbt_cli_resource.configured(
 
 configured_pyspark = PySparkResource(
     spark_conf={
-        "spark_conf": {
-            "spark.jars.packages": ",".join(
-                [
-                    "net.snowflake:snowflake-jdbc:3.8.0",
-                    "net.snowflake:spark-snowflake_2.12:2.8.2-spark_3.0",
-                    "com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.7",
-                ]
-            ),
-            "spark.hadoop.fs.s3.impl": "org.apache.hadoop.fs.s3native.NativeS3FileSystem",
-            "spark.hadoop.fs.s3.awsAccessKeyId": os.getenv("AWS_ACCESS_KEY_ID", ""),
-            "spark.hadoop.fs.s3.awsSecretAccessKey": os.getenv("AWS_SECRET_ACCESS_KEY", ""),
-            "spark.hadoop.fs.s3.buffer.dir": "/tmp",
-        }
+        "spark.jars.packages": ",".join(
+            [
+                "net.snowflake:snowflake-jdbc:3.8.0",
+                "net.snowflake:spark-snowflake_2.12:2.8.2-spark_3.0",
+                "com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.7",
+            ]
+        ),
+        "spark.hadoop.fs.s3.impl": "org.apache.hadoop.fs.s3native.NativeS3FileSystem",
+        "spark.hadoop.fs.s3.awsAccessKeyId": os.getenv("AWS_ACCESS_KEY_ID", ""),
+        "spark.hadoop.fs.s3.awsSecretAccessKey": os.getenv("AWS_SECRET_ACCESS_KEY", ""),
+        "spark.hadoop.fs.s3.buffer.dir": "/tmp",
     }
 )
 
