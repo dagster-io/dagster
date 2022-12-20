10636fb64093293591dfc0f9485a106f505a73c9
Nicholas Schrock <schrockn@elementl.com>
2022-12-19 19:08:33 -0500

    cp

--- examples/project_fully_featured/project_fully_featured/resources/__init__.py
+++ examples/project_fully_featured/project_fully_featured/resources/__init__.py
@@ -1,19 +1,19 @@
 import os
+from typing import Optional
 
+from dagster._seven.temp_dir import get_system_temp_directory
 from dagster_aws.s3 import s3_resource
+from dagster_aws.s3.io_manager import PickledObjectS3IOManager
+from dagster_aws.s3.utils import construct_s3_client
 from dagster_dbt import dbt_cli_resource
-from dagster_pyspark import pyspark_resource
 from dagster_pyspark.resources import PySparkResource
 
 from dagster._utils import file_relative_path
 
 from .common_bucket_s3_pickle_io_manager import common_bucket_s3_pickle_io_manager
 from .duckdb_parquet_io_manager import duckdb_partitioned_parquet_io_manager
-from .hn_resource import hn_api_client, hn_api_subsample_client
-from .parquet_io_manager import (
-    local_partitioned_parquet_io_manager,
-    s3_partitioned_parquet_io_manager,
-)
+from .hn_resource import hn_api_client, hn_api_subsample_client, HNAPISubsampleClient
+from .parquet_io_manager import PartitionedParquetIOManager
 from .snowflake_io_manager import snowflake_io_manager
 
 DBT_PROJECT_DIR = file_relative_path(__file__, "../../dbt_project")
@@ -27,6 +27,23 @@ dbt_staging_resource = dbt_cli_resource.configured(
 dbt_prod_resource = dbt_cli_resource.configured(
     {"profiles_dir": DBT_PROFILES_DIR, "project_dir": DBT_PROJECT_DIR, "target": "prod"}
 )
+
+
+def create_local_partitioned_io_manager(
+    pyspark_resource: PySparkResource, base_path: Optional[str] = None
+) -> PartitionedParquetIOManager:
+    return PartitionedParquetIOManager(
+        base_path=base_path if base_path is not None else get_system_temp_directory(),
+        pyspark_resource=pyspark_resource,
+    )
+
+
+def create_s3_partitioned_parquet_io_manager(
+    pyspark_resource: PySparkResource, s3_bucket
+) -> PartitionedParquetIOManager:
+    return PartitionedParquetIOManager(
+        base_path="s3://" + s3_bucket, pyspark_resource=pyspark_resource
+    )
 
 
 def get_spark_conf():
@@ -45,39 +62,52 @@ def get_spark_conf():
     }
 
 
-configured_pyspark = pyspark_resource.configured({"spark_conf": get_spark_conf()})
+configured_pyspark = PySparkResource({"spark_conf": get_spark_conf()})
 
 
 snowflake_io_manager_prod = snowflake_io_manager.configured({"database": "DEMO_DB"})
 
-RESOURCES_PROD = {
-    "s3_bucket": "hackernews-elementl-prod",
-    "io_manager": common_bucket_s3_pickle_io_manager,
-    "s3": s3_resource,
-    "parquet_io_manager": s3_partitioned_parquet_io_manager,
-    "warehouse_io_manager": snowflake_io_manager_prod,
-    "pyspark": configured_pyspark,
-    "hn_client": hn_api_subsample_client.configured({"sample_rate": 10}),
-    "dbt": dbt_prod_resource,
-}
+
+def get_prod_resources():
+    s3_bucket = "hackernews-elementl-prod"
+    s3_session = construct_s3_client(max_attempts=5)
+    return {
+        "io_manager": PickledObjectS3IOManager(s3_bucket=s3_bucket, s3_session=s3_session),
+        "s3": s3_resource,
+        "parquet_io_manager": create_s3_partitioned_parquet_io_manager(
+            pyspark_resource=configured_pyspark, s3_bucket=s3_bucket
+        ),
+        "warehouse_io_manager": snowflake_io_manager_prod,
+        "pyspark": configured_pyspark,
+        "hn_client": HNAPISubsampleClient(subsample_rate=10),
+        "dbt": dbt_prod_resource,
+    }
+
+
+RESOURCES_PROD = get_prod_resources()
 
 snowflake_io_manager_staging = snowflake_io_manager.configured({"database": "DEMO_DB_STAGING"})
 
 
-RESOURCES_STAGING = {
-    "s3_bucket": "hackernews-elementl-dev",
-    "io_manager": common_bucket_s3_pickle_io_manager,
-    "s3": s3_resource,
-    "parquet_io_manager": s3_partitioned_parquet_io_manager,
-    "warehouse_io_manager": snowflake_io_manager_staging,
-    "pyspark": configured_pyspark,
-    "hn_client": hn_api_subsample_client.configured({"sample_rate": 10}),
-    "dbt": dbt_staging_resource,
-}
+def get_staging_resources():
+    s3_bucket = "hackernews-elementl-dev"
+    return {
+        "io_manager": PickledObjectS3IOManager(
+            s3_bucket=s3_bucket, s3_session=construct_s3_client(max_attempts=5)
+        ),
+        "s3": s3_resource,
+        "parquet_io_manager": create_s3_partitioned_parquet_io_manager(
+            pyspark_resource=configured_pyspark, s3_bucket=s3_bucket
+        ),
+        "warehouse_io_manager": snowflake_io_manager_staging,
+        "pyspark": configured_pyspark,
+        "hn_client": HNAPISubsampleClient(sample_rate=10),
+        "dbt": dbt_staging_resource,
+    }
 
 
 RESOURCES_LOCAL = {
-    "parquet_io_manager": local_partitioned_parquet_io_manager,
+    "parquet_io_manager": create_local_partitioned_io_manager(configured_pyspark),
     "warehouse_io_manager": duckdb_partitioned_parquet_io_manager.configured(
         {"duckdb_path": os.path.join(DBT_PROJECT_DIR, "hackernews.duckdb")},
     ),
--- examples/project_fully_featured/project_fully_featured/resources/common_bucket_s3_pickle_io_manager.py
+++ examples/project_fully_featured/project_fully_featured/resources/common_bucket_s3_pickle_io_manager.py
@@ -1,6 +1,6 @@
-from dagster_aws.s3 import s3_pickle_io_manager
-
-from dagster import build_init_resource_context, io_manager
+from dagster_aws.s3 import
+from dagster_aws.s3.io_manager import PickledObjectS3IOManager
+from dagster import io_manager
 
 
 @io_manager(required_resource_keys={"s3_bucket", "s3"})
@@ -8,9 +8,6 @@ def common_bucket_s3_pickle_io_manager(init_context):
     """
     A version of the s3_pickle_io_manager that gets its bucket from another resource.
     """
-    return s3_pickle_io_manager(
-        build_init_resource_context(
-            config={"s3_bucket": init_context.resources.s3_bucket},
-            resources={"s3": init_context.resources.s3},
-        )
+    return PickledObjectS3IOManager(
+        s3_bucket=init_context.resources.s3_bucket, s3_session=init_context.resources.s3
     )
