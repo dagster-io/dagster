40cd16a316b7c3a4ad2800bbf582b9863bc992dd
Ben Pankow <ben@elementl.com>
2022-12-21 11:46:22 -0600

    [structured config] Base structured config implementation (#11268)
    ## Summary
    
    Introduces the capability to specify config types for an Op or Asset by
    applying a type annotation to the new `config` keyword, e.g.
    
    ```python
    @op
    def greet_user(config: str):
        print(f"Hello, {config}")
    ```
    
    For more complex config schema, users can build a Pydantic type
    ("structured config") extending the new `Config` base class. This
    provides type hinting when using config in an asset or op body:
    
    ```python
    class GreetingConfig(Config):
        name: str
        should_print: bool
    
    @op 
    def greet_user(config: GreetingConfig):
        if config.should_print:
            print(f"Hello, {config.name}")
        else:
            ...
    ```
    
    ## Limitations
    
    This PR only handles the case of structured config which has primitive
    types. More complex config (defaults, descriptions, Permissive,
    Selector/Unions, etc) will be handled in stacked diffs.
    
    ## Test Plan
    
    Introduced a set of unit tests to cover basic config classes using
    primitives + nested structured config.
    
    Co-authored-by: Nicholas Schrock <schrockn@elementl.com>

---
+++ python_modules/dagster/dagster/_config/structured_config.py
@@ -0,0 +1,64 @@
+from typing import Any, Type
+
+from pydantic import BaseModel
+from pydantic.fields import SHAPE_SINGLETON, ModelField
+
+import dagster._check as check
+from dagster import Field, Shape
+from dagster._config.field_utils import convert_potential_field
+
+
+class Config(BaseModel):
+    """
+    Base class for Dagster configuration models.
+    """
+
+
+def _convert_pydantic_field(pydantic_field: ModelField) -> Field:
+    """
+    Transforms a Pydantic field into a corresponding Dagster config field.
+    """
+
+    if _safe_is_subclass(pydantic_field.type_, Config):
+        return infer_schema_from_config_class(pydantic_field.type_)
+
+    dagster_type = pydantic_field.type_
+    if pydantic_field.shape != SHAPE_SINGLETON:
+        raise NotImplementedError(f"Pydantic shape {pydantic_field.shape} not supported")
+
+    return convert_potential_field(dagster_type)
+
+
+def infer_schema_from_config_annotation(model_cls: Any) -> Field:
+    """
+    Parses a structured config class or primitive type and returns a corresponding Dagster config Field.
+    """
+
+    if _safe_is_subclass(model_cls, Config):
+        return infer_schema_from_config_class(model_cls)
+
+    return convert_potential_field(model_cls)
+
+
+def _safe_is_subclass(cls: Any, possible_parent_cls: Type) -> bool:
+    """Version of issubclass that returns False if cls is not a Type."""
+    if not isinstance(cls, type):
+        return False
+    return issubclass(cls, possible_parent_cls)
+
+
+def infer_schema_from_config_class(model_cls: Type[Config]) -> Field:
+    """
+    Parses a structured config class and returns a corresponding Dagster config Field.
+    """
+
+    check.param_invariant(
+        issubclass(model_cls, Config),
+        "Config type annotation must inherit from dagster._config.structured_config.Config",
+    )
+
+    fields = {}
+    for pydantic_field_name, pydantic_field in model_cls.__fields__.items():
+        fields[pydantic_field_name] = _convert_pydantic_field(pydantic_field)
+
+    return Field(config=Shape(fields))
