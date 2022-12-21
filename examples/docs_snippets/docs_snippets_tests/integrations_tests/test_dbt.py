ed212ed3d5306ea9d0b0dce8ad2fbc7fb799de80
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2022-12-20 23:11:36 -0800

    [docs] dbt cloud guide repository -> definitions (#11299)
    ### Summary & Motivation
    
    https://www.notion.so/dagster/docs-Definitions-Update-dbt-Cloud-guide-c496f04c6069407fb445d33b382766fe
    ### How I Tested These Changes
    unit + eyes

--- examples/docs_snippets/docs_snippets_tests/integrations_tests/test_dbt.py
+++ examples/docs_snippets/docs_snippets_tests/integrations_tests/test_dbt.py
@@ -1,7 +1,13 @@
-from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
+from dagster_dbt import (
+    dbt_cli_resource,
+    dbt_cloud_resource,
+    load_assets_from_dbt_cloud_job,
+    load_assets_from_dbt_project,
+)
 
 from dagster import file_relative_path, with_resources
 from docs_snippets.integrations.dbt.dbt import scope_schedule_assets
+from docs_snippets.integrations.dbt.dbt_cloud import scope_schedule_dbt_cloud_assets
 
 
 def test_scope_schedule_assets_can_load():
@@ -22,3 +28,15 @@ def test_scope_schedule_assets_can_load():
     )
 
     scope_schedule_assets(dbt_assets)
+
+
+def test_scope_schedule_dbt_cloud_assets_can_load():
+    dbt_cloud_instance = dbt_cloud_resource.configured(
+        {"auth_token": "foo", "account_id": 111}
+    )
+    dbt_cloud_assets = load_assets_from_dbt_cloud_job(
+        dbt_cloud=dbt_cloud_instance,
+        job_id=33333,
+    )
+
+    scope_schedule_dbt_cloud_assets(dbt_cloud_assets)
