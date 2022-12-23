d6b207d3947a37eedb68de60af2795bdfeee1347
Nicholas Schrock <schrockn@elementl.com>
2022-12-23 12:04:52 -0500

    pin mashumaro

--- python_modules/libraries/dagster-dbt/setup.py
+++ python_modules/libraries/dagster-dbt/setup.py
@@ -34,6 +34,7 @@ setup(
     packages=find_packages(exclude=["dagster_dbt_tests*"]),
     install_requires=[
         f"dagster{pin}",
+        "mashumaro<3.2",
         "dbt-core",
         "requests",
         "attrs",
