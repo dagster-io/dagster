58a56c5b6cc9bee405b32a22a49bf0c44ffad99a
benpankow <ben@elementl.com>
2022-12-23 11:07:37 -0600

    a few more tests

--- python_modules/dagster/dagster_tests/core_tests/resource_tests/test_struct_config_resources.py
+++ python_modules/dagster/dagster_tests/core_tests/resource_tests/test_struct_config_resources.py
@@ -1,8 +1,13 @@
+from abc import ABC, abstractmethod
 from functools import cached_property
+
+import pytest
+from pydantic import ValidationError
 
 from dagster import asset, job, op
 from dagster._config.structured_config import Resource, cached_method
 from dagster._core.definitions.assets_job import build_assets_job
+from dagster._core.definitions.resource_output import ResourceOutput
 
 
 def test_basic_structured_resource():
@@ -34,6 +39,16 @@ def test_basic_structured_resource():
 
     assert prefix_job.execute_in_process().success
     assert out_txt == ["greeting: hello, world!"]
+
+
+def test_invalid_config():
+    class MyResource(Resource):
+        foo: int
+
+    with pytest.raises(
+        ValidationError,
+    ):
+        MyResource(foo="why")
 
 
 def test_caching_within_resource():
@@ -105,3 +120,82 @@ def test_caching_within_resource():
 
     assert called["greeting"] == 1
     assert called["get_introduction"] == 2
+
+
+def test_abc_resource():
+
+    out_txt = []
+
+    class WriterResource(Resource, ABC):
+        @abstractmethod
+        def output(self, text: str) -> None:
+            pass
+
+    class PrefixedWriterResource(WriterResource):
+        prefix: str
+
+        def output(self, text: str) -> None:
+            out_txt.append(f"{self.prefix}{text}")
+
+    class RepetitiveWriterResource(WriterResource):
+        repetitions: int
+
+        def output(self, text: str) -> None:
+            out_txt.append(f"{text} " * self.repetitions)
+
+    @op
+    def hello_world_op(writer: WriterResource):
+        writer.output("hello, world!")
+
+    # Can't instantiate abstract class
+    with pytest.raises(TypeError):
+        WriterResource()
+
+    @job(resource_defs={"writer": PrefixedWriterResource(prefix="greeting: ")})
+    def prefixed_job():
+        hello_world_op()
+
+    assert prefixed_job.execute_in_process().success
+    assert out_txt == ["greeting: hello, world!"]
+
+    out_txt.clear()
+
+    @job(resource_defs={"writer": RepetitiveWriterResource(repetitions=3)})
+    def repetitive_writer_job():
+        hello_world_op()
+
+    assert repetitive_writer_job.execute_in_process().success
+    assert out_txt == ["hello, world! " * 3]
+
+
+def test_yield_in_resource_function():
+    called = []
+
+    class ResourceWithCleanup(Resource):
+        idx: int
+
+        def resource_function(self, context):
+            called.append(f"creation_{self.idx}")
+            yield True
+            called.append(f"cleanup_{self.idx}")
+
+    @op
+    def check_resource_created(
+        resource_with_cleanup_1: ResourceOutput[bool], resource_with_cleanup_2: ResourceOutput[bool]
+    ):
+        assert resource_with_cleanup_1 is True
+        assert resource_with_cleanup_2 is True
+        called.append("op")
+
+    @job(
+        resource_defs={
+            "resource_with_cleanup_1": ResourceWithCleanup(idx=1),
+            "resource_with_cleanup_2": ResourceWithCleanup(idx=2),
+        }
+    )
+    def the_job():
+        check_resource_created()
+
+    assert the_job.execute_in_process().success
+
+    assert called == ["creation_1", "creation_2", "op", "cleanup_2", "cleanup_1"]
