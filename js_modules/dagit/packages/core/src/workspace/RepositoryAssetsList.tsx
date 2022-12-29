ffe89bf32e02c9e7637b96c91ab896518b33e9b2
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2022-12-29 11:12:30 -0600

    [dagit] Refactor queries in src/workspace (#11418)
    ### Summary & Motivation
    
    Refactor queries and downstream types in src/workspace.
    
    ### How I Tested These Changes
    
    TS, lint, Jest.

--- js_modules/dagit/packages/core/src/workspace/RepositoryAssetsList.tsx
+++ js_modules/dagit/packages/core/src/workspace/RepositoryAssetsList.tsx
@@ -1,4 +1,4 @@
-import {gql, useQuery} from '@apollo/client';
+import {useQuery} from '@apollo/client';
 import {Box, Colors, NonIdealState, Table} from '@dagster-io/ui';
 import * as React from 'react';
 import {Link} from 'react-router-dom';
@@ -7,17 +7,14 @@ import styled from 'styled-components/macro';
 import {useTrackPageView} from '../app/analytics';
 import {displayNameForAssetKey} from '../asset-graph/Utils';
 import {assetDetailsPathForKey} from '../assets/assetDetailsPathForKey';
+import {graphql} from '../graphql';
 import {RepositoryLink} from '../nav/RepositoryLink';
 
 import {repoAddressAsHumanString} from './repoAddressAsString';
 import {repoAddressToSelector} from './repoAddressToSelector';
 import {RepoAddress} from './types';
-import {
-  RepositoryAssetsListQuery,
-  RepositoryAssetsListQueryVariables,
-} from './types/RepositoryAssetsListQuery';
 
-const REPOSITORY_ASSETS_LIST_QUERY = gql`
+const REPOSITORY_ASSETS_LIST_QUERY = graphql(`
   query RepositoryAssetsListQuery($repositorySelector: RepositorySelector!) {
     repositoryOrError(repositorySelector: $repositorySelector) {
       __typename
@@ -45,7 +42,7 @@ const REPOSITORY_ASSETS_LIST_QUERY = gql`
       }
     }
   }
-`;
+`);
 
 interface Props {
   repoAddress: RepoAddress;
@@ -57,10 +54,7 @@ export const RepositoryAssetsList: React.FC<Props> = (props) => {
   const {repoAddress} = props;
   const repositorySelector = repoAddressToSelector(repoAddress);
 
-  const {data, error, loading} = useQuery<
-    RepositoryAssetsListQuery,
-    RepositoryAssetsListQueryVariables
-  >(REPOSITORY_ASSETS_LIST_QUERY, {
+  const {data, error, loading} = useQuery(REPOSITORY_ASSETS_LIST_QUERY, {
     fetchPolicy: 'cache-and-network',
     variables: {repositorySelector},
   });
