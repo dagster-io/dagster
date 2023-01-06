dagster-io/dagster

commit 6eb867340555c9a8fbf2605bf859acc7c6e5aefb

Allow specifying resources in Op/Asset params list (#11322)
## Summary

Allows users to specify resource dependencies as parameters to an Asset
or Op function.

Resource dependencies are identified by type annotations which either
extend `ResourceDefinition` or are wrapped as `ResourceOutput[T]`.

The first pattern is useful for "new"-style resources, which are Python
classes that the user will interact with. In this case, the
`ResourceDefinition` and the output from the resource function are
identical. Here, a theoretical `S3Resource` implements
`ResourceDefinition` and provides various interactions:

```python
@asset
def my_asset(s3: S3Resource):
    with s3.open_bucket(...):
        .... 
```

The second pattern is useful for function-style resources, which may
output any arbitrary Python object. In this case, our `PostgresResource`
outputs a `DatabaseConnection` object:

```python
@op
def my_op(postgres: ResourceOutput[DatabaseConnection]):
    postgres.query(...)
```

## Implementation

For the prototype, this approach was chosen over an approach which would
use input names rather than annotations to identify which inputs are
resources and which are other op/asset dependencies for a few reasons:

- Clarity: By using annotations, it is immediately clear to the reader
which parameters are resources.
- Complexity: Asset dependencies are currently resolved immediately,
when the decorator is applied. It would require a significant rewrite to
delay this evaluation until the `Definitions` object containing the
asset is evaluated.


## Test Plan

Unit tests

python_modules/dagster/dagster/_core/definitions/resource_output.py
@@ -1,0 +1,25 @@
+from inspect import Parameter
+from typing import Sequence, TypeVar
+
+from typing_extensions import Annotated
+
+from dagster._core.decorator_utils import get_function_params
+from dagster._core.definitions.resource_definition import ResourceDefinition
+
+
+def get_resource_args(fn) -> Sequence[Parameter]:
+
+    return [param for param in get_function_params(fn) if _is_resource_annotated(param)]
+
+
+def _is_resource_annotated(param: Parameter) -> bool:
+    return (
+        isinstance(param.annotation, type) and issubclass(param.annotation, ResourceDefinition)
+    ) or (
+        hasattr(param.annotation, "__metadata__")
+        and getattr(param.annotation, "__metadata__") == ("resource_output",)
+    )
+
+
+T = TypeVar("T")
+ResourceOutput = Annotated[T, "resource_output"]

python_modules/dagster/dagster_tests/core_tests/resource_tests/test_resource_parameters.py
@@ -1,0 +1,296 @@
+from typing import Any
+
+import pytest
+
+from dagster import AssetsDefinition, ResourceDefinition, asset, job, op, resource, with_resources
+from dagster._check import ParameterCheckError
+from dagster._config.structured_config import Config
+from dagster._core.definitions.asset_out import AssetOut
+from dagster._core.definitions.assets_job import build_assets_job
+from dagster._core.definitions.decorators.asset_decorator import multi_asset
+from dagster._core.definitions.resource_output import ResourceOutput
+from dagster._core.errors import DagsterInvalidDefinitionError
+
+
+def test_filter_out_resources():
+    @op
+    def requires_resource_a(context, a: ResourceOutput[str]):
+        assert a
+        assert context.resources.a
+        assert not hasattr(context.resources, "b")
+
+    @op
+    def requires_resource_b(context, b: ResourceOutput[str]):
+        assert b
+        assert not hasattr(context.resources, "a")
+        assert context.resources.b
+
+    @op
+    def not_resources(context):
+        assert not hasattr(context.resources, "a")
+        assert not hasattr(context.resources, "b")
+
+    @job(
+        resource_defs={
+            "a": ResourceDefinition.hardcoded_resource("foo"),
+            "b": ResourceDefinition.hardcoded_resource("bar"),
+        },
+    )
+    def room_of_requirement():
+        requires_resource_a()
+        requires_resource_b()
+        not_resources()
+
+    room_of_requirement.execute_in_process()
+
+
+def test_init_resources():
+    resources_initted = {}
+
+    @resource
+    def resource_a(_):
+        resources_initted["a"] = True
+        yield "A"
+
+    @resource
+    def resource_b(_):
+        resources_initted["b"] = True
+        yield "B"
+
+    @op
+    def consumes_resource_a(a: ResourceOutput[str]):
+        assert a == "A"
+
+    @op
+    def consumes_resource_b(b: ResourceOutput[str]):
+        assert b == "B"
+
+    @job(
+        resource_defs={
+            "a": resource_a,
+            "b": resource_b,
+        },
+    )
+    def selective_init_test_job():
+        consumes_resource_a()
+        consumes_resource_b()
+
+    assert selective_init_test_job.execute_in_process().success
+
+    assert set(resources_initted.keys()) == {"a", "b"}
+
+
+def test_ops_with_dependencies():
+    completed = set()
+
+    @op
+    def first_op(foo: ResourceOutput[str]):
+        assert foo == "foo"
+        completed.add("first_op")
+        return "hello"
+
+    @op
+    def second_op(foo: ResourceOutput[str], first_op_result: str):
+        assert foo == "foo"
+        assert first_op_result == "hello"
+        completed.add("second_op")
+        return first_op_result + " world"
+
+    @op
+    def third_op():
+        completed.add("third_op")
+        return "!"
+
+    # Ensure ordering of resource args doesn't matter
+    @op
+    def fourth_op(
+        context, second_op_result: str, foo: ResourceOutput[str], third_op_result: str
+    ):  # pylint: disable=unused-argument
+        assert foo == "foo"
+        assert second_op_result == "hello world"
+        assert third_op_result == "!"
+        completed.add("fourth_op")
+        return second_op_result + third_op_result
+
+    @job(
+        resource_defs={"foo": ResourceDefinition.hardcoded_resource("foo")},
+    )
+    def op_dependencies_job():
+        fourth_op(second_op_result=second_op(first_op()), third_op_result=third_op())
+
+    assert op_dependencies_job.execute_in_process().success
+
+    assert completed == {"first_op", "second_op", "third_op", "fourth_op"}
+
+
+def test_assets():
+    executed = {}
+
+    @asset
+    def the_asset(context, foo: ResourceOutput[str]):
+        assert context.resources.foo == "blah"
+        assert foo == "blah"
+        executed["the_asset"] = True
+        return "hello"
+
+    @asset
+    def the_other_asset(context, the_asset, foo: ResourceOutput[str]):
+        assert context.resources.foo == "blah"
+        assert foo == "blah"
+        assert the_asset == "hello"
+        executed["the_other_asset"] = True
+        return "world"
+
+    # Ensure ordering of resource args doesn't matter
+    @asset
+    def the_third_asset(context, the_asset, foo: ResourceOutput[str], the_other_asset):
+        assert context.resources.foo == "blah"
+        assert foo == "blah"
+        assert the_asset == "hello"
+        assert the_other_asset == "world"
+        executed["the_third_asset"] = True
+
+    transformed_assets = with_resources(
+        [the_asset, the_other_asset, the_third_asset],
+        {"foo": ResourceDefinition.hardcoded_resource("blah")},
+    )
+
+    assert build_assets_job("the_job", transformed_assets).execute_in_process().success
+    assert executed["the_asset"]
+    assert executed["the_other_asset"]
+    assert executed["the_third_asset"]
+
+
+def test_multi_assets():
+    executed = {}
+
+    @multi_asset(outs={"a": AssetOut(key="asset_a"), "b": AssetOut(key="asset_b")})
+    def two_assets(context, foo: ResourceOutput[str]):
+        assert context.resources.foo == "blah"
+        assert foo == "blah"
+        executed["two_assets"] = True
+        return 1, 2
+
+    transformed_assets = with_resources(
+        [two_assets],
+        {"foo": ResourceDefinition.hardcoded_resource("blah")},
+    )[0]
+    assert isinstance(transformed_assets, AssetsDefinition)
+
+    assert build_assets_job("the_job", [transformed_assets]).execute_in_process().success
+    assert executed["two_assets"]
+
+
+def test_resource_not_provided():
+    @asset
+    def consumes_nonexistent_resource(
+        not_provided: ResourceOutput[str],  # pylint: disable=unused-argument
+    ):
+        pass
+
+    with pytest.raises(
+        DagsterInvalidDefinitionError,
+        match="resource with key 'not_provided' required by op 'consumes_nonexistent_resource'",
+    ):
+        with_resources([consumes_nonexistent_resource], {})
+
+
+def test_resource_class():
+
+    resource_called = {}
+
+    class MyResource(ResourceDefinition):
+        def __init__(self):
+            super().__init__(resource_fn=lambda *_, **__: self)
+
+        def do_something(self):
+            resource_called["called"] = True
+
+    @op
+    def do_something_op(my_resource: MyResource):
+        my_resource.do_something()
+
+    @job(resource_defs={"my_resource": MyResource()})
+    def my_job():
+        do_something_op()
+
+    assert my_job.execute_in_process().success
+    assert resource_called["called"]
+
+    @asset
+    def consumes_nonexistent_resource_class(
+        not_provided: MyResource,  # pylint: disable=unused-argument
+    ):
+        pass
+
+    with pytest.raises(
+        DagsterInvalidDefinitionError,
+        match="resource with key 'not_provided' required by op 'consumes_nonexistent_resource_class'",
+    ):
+        with_resources([consumes_nonexistent_resource_class], {})
+
+
+def test_both_decorator_and_argument_error():
+    with pytest.raises(
+        ParameterCheckError,
+        match="Invariant violation for parameter Cannot specify resource requirements in both @asset decorator and as arguments to the decorated function",
+    ):
+
+        @asset(required_resource_keys={"foo"})
+        def my_asset(bar: ResourceOutput[Any]):  # pylint: disable=unused-argument
+            pass
+
+    with pytest.raises(
+        ParameterCheckError,
+        match="Invariant violation for parameter Cannot specify resource requirements in both @multi_asset decorator and as arguments to the decorated function",
+    ):
+
+        @multi_asset(
+            outs={"a": AssetOut(key="asset_a"), "b": AssetOut(key="asset_b")},
+            required_resource_keys={"foo"},
+        )
+        def my_assets(bar: ResourceOutput[Any]):  # pylint: disable=unused-argument
+            pass
+
+    with pytest.raises(
+        ParameterCheckError,
+        match="Invariant violation for parameter Cannot specify resource requirements in both @op decorator and as arguments to the decorated function",
+    ):
+
+        @op(required_resource_keys={"foo"})
+        def my_op(bar: ResourceOutput[Any]):  # pylint: disable=unused-argument
+            pass
+
+
+def test_asset_with_structured_config():
+    class AnAssetConfig(Config):
+        a_string: str
+        an_int: int
+
+    executed = {}
+
+    @asset
+    def the_asset(context, config: AnAssetConfig, foo: ResourceOutput[str]):
+        assert context.resources.foo == "blah"
+        assert foo == "blah"
+        assert context.op_config["a_string"] == "foo"
+        assert config.a_string == "foo"
+        assert config.an_int == 2
+        executed["the_asset"] = True
+
+    transformed_asset = with_resources(
+        [the_asset],
+        {"foo": ResourceDefinition.hardcoded_resource("blah")},
+    )[0]
+    assert isinstance(transformed_asset, AssetsDefinition)
+
+    assert (
+        build_assets_job(
+            "the_job",
+            [transformed_asset],
+            config={"ops": {"the_asset": {"config": {"a_string": "foo", "an_int": 2}}}},
+        )
+        .execute_in_process()
+        .success
+    )
+    assert executed["the_asset"]

python_modules/dagster/dagster/_core/definitions/decorators/asset_decorator.py
@@ -18,6 +18,7 @@ from dagster._builtins import Nothing
 from dagster._config import UserConfigSchema
 from dagster._core.decorator_utils import get_function_params, get_valid_name_permutations
 from dagster._core.definitions.freshness_policy import FreshnessPolicy
+from dagster._core.definitions.resource_output import get_resource_args
 from dagster._core.errors import DagsterInvalidDefinitionError
 from dagster._core.storage.io_manager import IOManagerDefinition
 from dagster._core.types.dagster_type import DagsterType
@@ -263,8 +264,13 @@ class _Asset:
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=ExperimentalWarning)
 
-            required_resource_keys = set(self.required_resource_keys).union(
+            arg_resource_keys = {arg.name for arg in get_resource_args(fn)}
+            decorator_resource_keys = set(self.required_resource_keys).union(
                 set(self.resource_defs.keys())
+            )
+            check.param_invariant(
+                len(decorator_resource_keys) == 0 or len(arg_resource_keys) == 0,
+                "Cannot specify resource requirements in both @asset decorator and as arguments to the decorated function",
             )
 
             if isinstance(self.io_manager, str):
@@ -292,7 +298,9 @@ class _Asset:
                 description=self.description,
                 ins=dict(asset_ins.values()),
                 out=out,
-                required_resource_keys=required_resource_keys,
+                # Any resource requirements specified as arguments will be identified as
+                # part of the Op definition instantiation
+                required_resource_keys=decorator_resource_keys,
                 tags={
                     **({"kind": self.compute_kind} if self.compute_kind else {}),
                     **(self.op_tags or {}),
@@ -405,6 +413,7 @@ def multi_asset(
     )
 
     required_resource_keys = set(required_resource_keys).union(set(resource_defs.keys()))
+
     for out in outs.values():
         if isinstance(out, Out) and not isinstance(out, AssetOut):
             deprecation_warning(
@@ -420,6 +429,12 @@ def multi_asset(
             fn, ins or {}, non_argument_deps=_make_asset_keys(non_argument_deps)
         )
         asset_outs = build_asset_outs(outs)
+
+        arg_resource_keys = {arg.name for arg in get_resource_args(fn)}
+        check.param_invariant(
+            len(required_resource_keys or []) == 0 or len(arg_resource_keys) == 0,
+            "Cannot specify resource requirements in both @multi_asset decorator and as arguments to the decorated function",
+        )
 
         # validate that the asset_deps make sense
         valid_asset_deps = set(asset_ins.keys()) | set(asset_outs.keys())
@@ -522,17 +537,21 @@ def build_asset_ins(
     )
     input_params = params[1:] if is_context_provided else params
 
-    # Filter config
-    filtered_input_params = [param for param in input_params if param.name != "config"]
+    # Filter config, resource args
+    resource_arg_names = {arg.name for arg in get_resource_args(fn)}
+
+    new_input_args = []
+    for input_arg in input_params:
+        if input_arg.name != "config" and input_arg.name not in resource_arg_names:
+            new_input_args.append(input_arg)
+    input_params = new_input_args
 
     non_var_input_param_names = [
         param.name
-        for param in filtered_input_params
+        for param in new_input_args
         if param.kind == funcsigs.Parameter.POSITIONAL_OR_KEYWORD
     ]
-    has_kwargs = any(
-        param.kind == funcsigs.Parameter.VAR_KEYWORD for param in filtered_input_params
-    )
+    has_kwargs = any(param.kind == funcsigs.Parameter.VAR_KEYWORD for param in new_input_args)
 
     all_input_names = set(non_var_input_param_names) | asset_ins.keys()
 

python_modules/dagster/dagster/_core/definitions/decorators/op_decorator.py
@@ -83,6 +83,14 @@ class _Op:
         elif self.out is not None:
             outs = check.mapping_param(self.out, "out", key_type=str, value_type=Out)
 
+        arg_resource_keys = {arg.name for arg in compute_fn.get_resource_args()}
+        decorator_resource_keys = set(self.required_resource_keys or [])
+        check.param_invariant(
+            len(decorator_resource_keys) == 0 or len(arg_resource_keys) == 0,
+            "Cannot specify resource requirements in both @op decorator and as arguments to the decorated function",
+        )
+        resolved_resource_keys = decorator_resource_keys.union(arg_resource_keys)
+
         op_def = OpDefinition(
             name=self.name,
             ins=self.ins,
@@ -90,7 +98,7 @@ class _Op:
             compute_fn=compute_fn,
             config_schema=self.config_schema,
             description=self.description or format_docstring_for_description(fn),
-            required_resource_keys=self.required_resource_keys,
+            required_resource_keys=resolved_resource_keys,
             tags=self.tags,
             code_version=self.code_version,
             retry_policy=self.retry_policy,

python_modules/dagster/dagster/_core/definitions/decorators/solid_decorator.py
@@ -16,6 +16,7 @@ from typing import (
 import dagster._check as check
 from dagster._config import UserConfigSchema
 from dagster._core.decorator_utils import format_docstring_for_description
+from dagster._core.definitions.resource_output import get_resource_args
 from dagster._core.errors import DagsterInvalidDefinitionError
 from dagster._core.types.dagster_type import DagsterTypeKind
 from dagster._seven import funcsigs
@@ -60,10 +61,18 @@ class DecoratedOpFunction(NamedTuple):
 
         check.failed("Requested config arg on function that does not have one")
 
+    def get_resource_args(self) -> Sequence[funcsigs.Parameter]:
+        return get_resource_args(self.decorated_fn)
+
     def positional_inputs(self) -> Sequence[str]:
         params = self._get_function_params()
         input_args = params[1:] if self.has_context_arg() else params
-        input_args_filtered = [input_arg for input_arg in input_args if input_arg.name != "config"]
+        resource_arg_names = [arg.name for arg in self.get_resource_args()]
+        input_args_filtered = [
+            input_arg
+            for input_arg in input_args
+            if input_arg.name != "config" and input_arg.name not in resource_arg_names
+        ]
         return positional_arg_name_list(input_args_filtered)
 
     def has_var_kwargs(self) -> bool:
@@ -149,6 +158,14 @@ class _Solid:
             exclude_nothing=True,
         )
 
+        arg_resource_keys = {arg.name for arg in compute_fn.get_resource_args()}
+        decorator_resource_keys = set(self.required_resource_keys or [])
+        check.param_invariant(
+            len(decorator_resource_keys) == 0 or len(arg_resource_keys) == 0,
+            "Cannot specify resource requirements in both @op decorator and as arguments to the decorated function",
+        )
+        resolved_resource_keys = decorator_resource_keys.union(arg_resource_keys)
+
         solid_def = OpDefinition(
             name=self.name,
             ins={
@@ -158,7 +175,7 @@ class _Solid:
             compute_fn=compute_fn,
             config_schema=self.config_schema,
             description=self.description or format_docstring_for_description(fn),
-            required_resource_keys=self.required_resource_keys,
+            required_resource_keys=resolved_resource_keys,
             tags=self.tags,
             version=self.version,
             retry_policy=self.retry_policy,
@@ -345,6 +362,7 @@ def resolve_checked_solid_fn_inputs(
             arguments.
     """
 
+    explicit_names = set()
     if exclude_nothing:
         explicit_names = set(
             inp.name
@@ -365,10 +383,13 @@ def resolve_checked_solid_fn_inputs(
     input_args = params[1:] if compute_fn.has_context_arg() else params
 
     # filter out config arg
-    if compute_fn.has_config_arg():
+    resource_arg_names = {arg.name for arg in compute_fn.get_resource_args()}
+    explicit_names = explicit_names - resource_arg_names
+
+    if compute_fn.has_config_arg() or resource_arg_names:
         new_input_args = []
         for input_arg in input_args:
-            if input_arg.name != "config":
+            if input_arg.name != "config" and input_arg.name not in resource_arg_names:
                 new_input_args.append(input_arg)
         input_args = new_input_args
 

python_modules/dagster/dagster/_core/execution/plan/compute_generator.py
@@ -45,6 +45,7 @@ def create_solid_compute_wrapper(solid_def: OpDefinition):
     output_defs = solid_def.output_defs
     context_arg_provided = compute_fn.has_context_arg()
     config_arg_cls = compute_fn.get_config_arg().annotation if compute_fn.has_config_arg() else None
+    resource_arg_mapping = {arg.name: arg.name for arg in compute_fn.get_resource_args()}
 
     input_names = [
         input_def.name
@@ -64,7 +65,9 @@ def create_solid_compute_wrapper(solid_def: OpDefinition):
             or inspect.iscoroutinefunction(fn)
         ):
             # safe to execute the function, as doing so will not immediately execute user code
-            result = invoke_compute_fn(fn, context, kwargs, context_arg_provided, config_arg_cls)
+            result = invoke_compute_fn(
+                fn, context, kwargs, context_arg_provided, config_arg_cls, resource_arg_mapping
+            )
             if inspect.iscoroutine(result):
                 return _coerce_async_solid_to_async_gen(result, context, output_defs)
             # already a generator
@@ -73,7 +76,13 @@ def create_solid_compute_wrapper(solid_def: OpDefinition):
             # we have a regular function, do not execute it before we are in an iterator
             # (as we want all potential failures to happen inside iterators)
             return _coerce_solid_compute_fn_to_iterator(
-                fn, output_defs, context, context_arg_provided, kwargs, config_arg_cls
+                fn,
+                output_defs,
+                context,
+                context_arg_provided,
+                kwargs,
+                config_arg_cls,
+                resource_arg_mapping,
             )
 
     return compute
@@ -91,6 +100,7 @@ def invoke_compute_fn(
     kwargs: Dict[str, Any],
     context_arg_provided: bool,
     config_arg_cls: Optional[Type[Config]],
+    resource_args: Optional[Dict[str, str]] = None,
 ) -> Any:
     args_to_pass = kwargs
     if config_arg_cls:
@@ -99,13 +109,18 @@ def invoke_compute_fn(
             args_to_pass["config"] = config_arg_cls(**context.op_config)
         else:
             args_to_pass["config"] = context.op_config
+    if resource_args:
+        for resource_name, arg_name in resource_args.items():
+            args_to_pass[arg_name] = getattr(context.resources, resource_name)
     return fn(context, **args_to_pass) if context_arg_provided else fn(**args_to_pass)
 
 
 def _coerce_solid_compute_fn_to_iterator(
-    fn, output_defs, context, context_arg_provided, kwargs, config_arg_class
+    fn, output_defs, context, context_arg_provided, kwargs, config_arg_class, resource_arg_mapping
 ):
-    result = invoke_compute_fn(fn, context, kwargs, context_arg_provided, config_arg_class)
+    result = invoke_compute_fn(
+        fn, context, kwargs, context_arg_provided, config_arg_class, resource_arg_mapping
+    )
     for event in validate_and_coerce_op_result_to_iterator(result, context, output_defs):
         yield event
 

commit 351e713514f8ea8b359c8c4d05126e1eab958df5

[fix] Fix builtin type error related to resource annotations (#11551)
### Summary & Motivation

Addresses #11541, a bug introduced in #11322.

Users who are using Python >= 3.9 may use [builtin
types](https://peps.python.org/pep-0585/) for generic type annotations,
e.g. `list[dict[str, str]]` rather than `typing.List[typing.Dict[str,
str]]`.

Unfortunately, these builtin generics manage to get around the safety
checks in our subclass code - `isinstance(list[dict[str, str]], type) ==
True` and `inspect.isclass(list[dict[str, str]]) == True` but
`issubclass(list[dict[str, str]], Resource)` raises an error that
`list[dict[str, str]]` is not a class:

```python
TypeError: issubclass() arg 1 must be a class
```

This seems to be a specific issue related to `abc._abc_subclasscheck`,
since `Resource` is an ABC. The CPython implementation of the subclass
check seems to have different results than the python one, which uses
`isinstance(..., type)` under the hood:
https://bugs.python.org/issue44847

This PR adds a try/catch guard to areas where we make a subclass call to
avoid this issue.

## Test Plan

Add a test suite which is run for Python >= 3.9 to ensure builtin
generics can be used.

python_modules/dagster/dagster/_config/structured_config.py
@@ -313,7 +313,14 @@ def _safe_is_subclass(cls: Any, possible_parent_cls: Type) -> bool:
     """Version of issubclass that returns False if cls is not a Type."""
     if not isinstance(cls, type):
         return False
-    return issubclass(cls, possible_parent_cls)
+
+    try:
+        return issubclass(cls, possible_parent_cls)
+    except TypeError:
+        # Using builtin Python types in python 3.9+ will raise a TypeError when using issubclass
+        # even though the isinstance check will succeed (as will inspect.isclass), for example
+        # list[dict[str, str]] will raise a TypeError
+        return False
 
 
 def infer_schema_from_config_class(

python_modules/dagster/dagster/_core/definitions/resource_output.py
@@ -12,9 +12,18 @@ def get_resource_args(fn) -> Sequence[Parameter]:
 
 
 def _is_resource_annotated(param: Parameter) -> bool:
-    return (
-        isinstance(param.annotation, type) and issubclass(param.annotation, ResourceDefinition)
-    ) or (
+    extends_resource_definition = False
+    try:
+        extends_resource_definition = isinstance(param.annotation, type) and issubclass(
+            param.annotation, ResourceDefinition
+        )
+    except TypeError:
+        # Using builtin Python types in python 3.9+ will raise a TypeError when using issubclass
+        # even though the isinstance check will succeed (as will inspect.isclass), for example
+        # list[dict[str, str]] will raise a TypeError
+        pass
+
+    return (extends_resource_definition) or (
         hasattr(param.annotation, "__metadata__")
         and getattr(param.annotation, "__metadata__") == ("resource_output",)
     )

python_modules/dagster/dagster_tests/core_tests/resource_tests/test_resource_parameters.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Any
 
 import pytest
@@ -303,3 +304,37 @@ def test_asset_with_structured_config():
         .success
     )
     assert executed["the_asset"]
+
+
+# Disabled for Python versions <3.9 as builtin types do not support generics
+# until Python 3.9, https://peps.python.org/pep-0585/
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="requires python3.9")
+def test_no_err_builtin_annotations():
+    # Ensures that we can use Python builtin types without causing any issues, see
+    # https://github.com/dagster-io/dagster/issues/11541
+
+    executed = {}
+
+    @asset
+    def the_asset(context, foo: ResourceOutput[str]):
+        assert context.resources.foo == "blah"
+        assert foo == "blah"
+        executed["the_asset"] = True
+        return [{"hello": "world"}]
+
+    @asset
+    def the_other_asset(context, the_asset: list[dict[str, str]], foo: ResourceOutput[str]):
+        assert context.resources.foo == "blah"
+        assert foo == "blah"
+        assert the_asset == [{"hello": "world"}]
+        executed["the_other_asset"] = True
+        return "world"
+
+    transformed_assets = with_resources(
+        [the_asset, the_other_asset],
+        {"foo": ResourceDefinition.hardcoded_resource("blah")},
+    )
+
+    assert build_assets_job("the_job", transformed_assets).execute_in_process().success
+    assert executed["the_asset"]
+    assert executed["the_other_asset"]