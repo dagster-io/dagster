590cb20f49a94dec49d796eea640b0ae548006e2
Nicholas Schrock <schrockn@elementl.com>
2023-01-06 10:42:06 -0500

    example

---
+++ examples/experimental/script_to_assets_branching_io_manager/launch_branch.sh
@@ -0,0 +1,1 @@
+DAGSTER_DEPLOYMENT=my-feature-branch DAGSTER_HOME=/Users/schrockn/code/dagster-script-to-assets/dev_dagster_home/ dagit -f hn_dagster.py -p 3001
---
+++ examples/experimental/script_to_assets_branching_io_manager/launch_prod.sh
@@ -0,0 +1,1 @@
+DAGSTER_DEPLOYMENT=prod DAGSTER_HOME=/Users/schrockn/code/dagster-script-to-assets/prod_dagster_home/ dagit -f hn_dagster.py
