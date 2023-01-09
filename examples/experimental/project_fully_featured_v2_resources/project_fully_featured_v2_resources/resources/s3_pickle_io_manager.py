780b9be3fa14e4d04b69700b89627e792c6d0911
Nicholas Schrock <schrockn@elementl.com>
2023-01-09 16:21:26 -0500

    property

--- examples/experimental/project_fully_featured_v2_resources/project_fully_featured_v2_resources/resources/s3_pickle_io_manager.py
+++ examples/experimental/project_fully_featured_v2_resources/project_fully_featured_v2_resources/resources/s3_pickle_io_manager.py
@@ -6,5 +6,6 @@ from dagster_aws.s3.io_manager import s3_pickle_io_manager
 class S3PickledIOManagerAdapter(StructuredIOManagerAdapter):
     s3_bucket: str
 
+    @property
     def wrapped_io_manager(self) -> IOManagerDefinition:
         return s3_pickle_io_manager
