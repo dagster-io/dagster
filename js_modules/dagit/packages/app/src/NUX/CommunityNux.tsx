3e7f305b5bdb2619352b8f4268ccae1d165705c5
Marco Salazar <marco@elementl.com>
2023-01-09 16:48:05 -0500

    Make it an iframe

--- js_modules/dagit/packages/app/src/NUX/CommunityNux.tsx
+++ js_modules/dagit/packages/app/src/NUX/CommunityNux.tsx
@@ -1,18 +1,7 @@
 import {useStateWithStorage} from '@dagster-io/dagit-core/hooks/useStateWithStorage';
-import {
-  Body,
-  Box,
-  Button,
-  Checkbox,
-  Colors,
-  Dialog,
-  ExternalAnchorButton,
-  Heading,
-  Icon,
-  TextInput,
-} from '@dagster-io/ui';
+import {Dialog} from '@dagster-io/ui';
 import React from 'react';
-import isEmail from 'validator/lib/isEmail';
+import ReactDOM from 'react-dom';
 
 export const CommunityNux = () => {
   const [didDismissCommunityNux, dismissCommunityNux] = useStateWithStorage(
@@ -36,110 +25,139 @@ const CommunityNuxImpl: React.FC<{dismiss: () => void}> = ({dismiss}) => {
   React.useEffect(() => {
     const timeout = setTimeout(() => {
       setShouldShowNux(true);
-    }, 5000); // wait 5 seconds before showing Nux
+    }, 0); // wait 5 seconds before showing Nux
     return () => {
       clearTimeout(timeout);
     };
   }, []);
 
-  const [email, setEmail] = React.useState('');
-  const [newsletter, setNewsLetter] = React.useState(false);
+  const {preloadElement, loaded, renderInto, iframeRef} = useCommuniyNuxIframe({
+    height: '462px',
+    width: '680px',
+  });
 
-  const submit = async () => {
-    await fetch('http://dagster.cloud/email-sign-up', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email,
-        newsletter,
-      }),
-    });
-    dismiss();
-  };
+  React.useEffect(() => {
+    const iframe = iframeRef.current;
+    if (!iframe) {
+      return () => {};
+    }
+    const messageListener = (event: MessageEvent) => {
+      switch (event.data) {
+        case 'dismiss':
+          dismiss();
+          break;
+      }
+    };
 
-  const validEmail = isEmail(email);
-  const [emailChanged, setEmailChanged] = React.useState(false);
+    window.addEventListener('message', messageListener);
+    return () => {
+      window.removeEventListener('message', messageListener, false);
+    };
+  }, [dismiss, iframeRef]);
+
+  const [target, setTarget] = React.useState<HTMLDivElement | null>(null);
+  React.useEffect(() => {
+    if (shouldShowNux && loaded && target) {
+      renderInto(target);
+    }
+  }, [shouldShowNux, loaded, renderInto, target]);
 
   return (
-    <Dialog isOpen={shouldShowNux} style={{width: '680px'}}>
-      <Box flex={{direction: 'column', gap: 16}} style={{padding: '36px'}}>
-        <Box
-          flex={{direction: 'row', gap: 24, alignItems: 'center'}}
-          padding={{bottom: 24}}
-          border={{side: 'bottom', color: Colors.KeylineGray, width: 1}}
-        >
-          <Box flex={{direction: 'column', gap: 8}}>
-            <Heading>Join the Dagster community</Heading>
-            <Body style={{color: Colors.Gray700}}>
-              Connect with thousands of other data practitioners building with Dagster. Share
-              knowledge, get help, and contribute to the open-source project.
-            </Body>
-            <div>
-              <ExternalAnchorButton
-                icon={<Icon name="slack" />}
-                href="https://www.dagster.io/slack"
-              >
-                Join us on Slack
-              </ExternalAnchorButton>
-            </div>
-          </Box>
-          <video autoPlay muted loop playsInline width={120} height={120}>
-            <source src="/Dagster_world.mp4" type="video/mp4" />
-          </video>
-        </Box>
-        <Box flex={{direction: 'column', justifyContent: 'stretch', gap: 12}}>
-          <div>Notify me about Dagster security updates</div>
-          <TextInput
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-              setEmailChanged(true);
-            }}
-            placeholder="hello@dagster.io"
-            strokeColor={!emailChanged || validEmail ? undefined : Colors.Red500}
-            style={{width: '100%'}}
-          />
-          {emailChanged && !validEmail ? (
-            <div style={{paddingBottom: '12px', color: Colors.Red500, fontSize: '12px'}}>
-              Add your email to get updates from Dagster.
-            </div>
-          ) : null}
-          <Box flex={{direction: 'column', gap: 8}} padding={{bottom: 24}}>
-            <Box as="label" flex={{direction: 'row', gap: 8, alignItems: 'center'}}>
-              <Checkbox checked={true} disabled={true} onChange={() => {}} />
-              <span>Notify me about Dagster security updates</span>
-            </Box>
-            <Box as="label" flex={{direction: 'row', gap: 8, alignItems: 'center'}}>
-              <Checkbox
-                checked={newsletter}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setNewsLetter(e.target.checked);
-                }}
-              />{' '}
-              <span>Sign up for the Dagster newsletter</span>
-            </Box>
-          </Box>
-        </Box>
-        <Box flex={{direction: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-          <a href="https://dagster.io/privacy" target="_blank" rel="noreferrer">
-            Privacy Policy
-          </a>
-          <Box flex={{direction: 'row', gap: 8}}>
-            <Button
-              onClick={() => {
-                dismiss();
-              }}
-            >
-              Skip
-            </Button>
-            <Button onClick={submit} disabled={!validEmail} intent="primary">
-              Submit
-            </Button>
-          </Box>
-        </Box>
-      </Box>
-    </Dialog>
+    <>
+      <Dialog
+        isOpen={shouldShowNux && loaded}
+        style={{width: '680px', background: 'transparent', overflow: 'hidden', height: '462px'}}
+      >
+        <div
+          ref={(element: HTMLDivElement) => {
+            setTarget(element);
+          }}
+        />
+      </Dialog>
+      {/** We create a portal after the dialog so that the dialog can be positioned over the blueprint overlay */}
+      {ReactDOM.createPortal(preloadElement, document.body)}
+    </>
   );
+};
+
+const IFRAME_SRC = 'http://localhost:4000/dagit_iframes/community_nux';
+
+type Props = {
+  width: string;
+  height: string;
+};
+
+/**
+ * This iframe uses a bit of a hack to allow us to show the dialog only when the iframe is fully loaded.
+ * To do this we render the iframe offscreen then move it on screen. The problem we run into is two fold:
+ *  1) The container we render the iframe into will not be on screen until the iframe is ready, so the iframe can't be initially
+ *     put into its final location
+ *  2) If we move an iframe's DOM node then the iframe gets reloaded from scratch defeating the purpose of preloading it
+ *
+ * So instead we position the iframe absolutely and keep track of the position of the target element where we want the iframe to live.
+ *
+ */
+const useCommuniyNuxIframe = ({width, height}: Props) => {
+  const iframeRef = React.useRef<HTMLIFrameElement>(null);
+  const [loaded, setLoaded] = React.useState(false);
+  const [parentRect, setParentRect] = React.useState<DOMRect | null>(null);
+  const [parent, setParent] = React.useState<HTMLElement | null>(null);
+
+  React.useLayoutEffect(() => {
+    if (parent?.parentNode) {
+      const dialogFrame = parent.parentNode as HTMLDivElement;
+
+      const RO = window['ResizeObserver'] as any;
+      const observer = new RO(() => {
+        setParentRect(dialogFrame.getBoundingClientRect());
+      });
+      observer.observe(parent.parentNode);
+      observer.observe(document.documentElement);
+
+      const lastRect = dialogFrame.getBoundingClientRect();
+
+      // Blueprint animates the dialog, so we need to follow it for the animation
+      // It also doens't update every frame, so we give an allowance of up to 100 frames
+      // without any updates. After 100 frames of no updates we assume the animation is complete
+      // and we stop our measuring loop
+      const loopUntilAnimationFinishes = (max: number) => {
+        const nextRect = dialogFrame.getBoundingClientRect();
+        if (lastRect.left !== nextRect.left || lastRect.top !== nextRect.top || max > 0) {
+          setParentRect(nextRect);
+          requestAnimationFrame(() => loopUntilAnimationFinishes(Math.max(max - 1, 0)));
+        }
+      };
+      requestAnimationFrame(() => loopUntilAnimationFinishes(100));
+    }
+  }, [parent]);
+
+  return {
+    preloadElement: (
+      <iframe
+        style={
+          parentRect
+            ? {
+                width,
+                height,
+                position: 'absolute',
+                left: parentRect.left,
+                top: parentRect.top,
+                zIndex: 21,
+              }
+            : {width, height, left: '-999999px', position: 'absolute', zIndex: 0}
+        }
+        src={IFRAME_SRC}
+        ref={iframeRef}
+        onLoad={() => {
+          setLoaded(true);
+          console.log('loaded', iframeRef.current);
+        }}
+      />
+    ),
+    loaded,
+    iframeRef,
+    renderInto: React.useCallback((parent: HTMLElement) => {
+      setParent(parent);
+    }, []),
+  };
 };
