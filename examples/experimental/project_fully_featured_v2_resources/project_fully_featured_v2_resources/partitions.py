94b72472d5e3c7f3864e054796b92c5e8afdc2f1
Nicholas Schrock <schrockn@elementl.com>
2023-01-09 16:22:12 -0500

    old partitions

--- examples/experimental/project_fully_featured_v2_resources/project_fully_featured_v2_resources/partitions.py
+++ examples/experimental/project_fully_featured_v2_resources/project_fully_featured_v2_resources/partitions.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 from dagster import HourlyPartitionsDefinition
 
-hourly_partitions = HourlyPartitionsDefinition(start_date=datetime(2023, 1, 5, 20))
-# hourly_partitions = HourlyPartitionsDefinition(start_date=datetime(2020, 12, 1))
+# hourly_partitions = HourlyPartitionsDefinition(start_date=datetime(2023, 1, 5, 20))
+hourly_partitions = HourlyPartitionsDefinition(start_date=datetime(2020, 12, 1))
