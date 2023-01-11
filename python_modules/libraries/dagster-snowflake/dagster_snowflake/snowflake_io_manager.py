e4336f3804480298ef74b21ce0205aa8e67507b6
Josh Taylor <joshuataylorx@gmail.com>
2023-01-12 04:14:48 +0800

    [dagster-snowflake] Add private key authentication for Snowflake (#10750)

--- python_modules/libraries/dagster-snowflake/dagster_snowflake/snowflake_io_manager.py
+++ python_modules/libraries/dagster-snowflake/dagster_snowflake/snowflake_io_manager.py
@@ -88,14 +88,38 @@ def build_snowflake_io_manager(type_handlers: Sequence[DbTypeHandler]) -> IOMana
                 ),
             ),
             "user": Field(StringSource, description="User login name."),
-            "password": Field(StringSource, description="User password."),
+            "password": Field(StringSource, description="User password.", is_required=False),
             "warehouse": Field(
                 StringSource, description="Name of the warehouse to use.", is_required=False
             ),
             "schema": Field(
-                StringSource, description="Name of the schema to use", is_required=False
+                StringSource, description="Name of the schema to use.", is_required=False
             ),
-            "role": Field(StringSource, description="Name of the role to use", is_required=False),
+            "role": Field(StringSource, description="Name of the role to use.", is_required=False),
+            "private_key": Field(
+                StringSource,
+                description=(
+                    "Raw private key to use. See"
+                    " https://docs.snowflake.com/en/user-guide/key-pair-auth.html for details."
+                ),
+                is_required=False,
+            ),
+            "private_key_path": Field(
+                StringSource,
+                description=(
+                    "Path to the private key. See"
+                    " https://docs.snowflake.com/en/user-guide/key-pair-auth.html for details."
+                ),
+                is_required=False,
+            ),
+            "private_key_password": Field(
+                StringSource,
+                description=(
+                    "The password of the private key. See"
+                    " https://docs.snowflake.com/en/user-guide/key-pair-auth.html for details."
+                ),
+                is_required=False,
+            ),
         }
     )
     def snowflake_io_manager(init_context):
