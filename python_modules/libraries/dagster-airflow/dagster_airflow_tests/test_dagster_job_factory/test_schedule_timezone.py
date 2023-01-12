19a7a7fed682722fffa885a43d745676596843ae
Joseph Van Drunen <josephvandrunen@gmail.com>
2023-01-12 10:32:08 -0500

    wip

--- python_modules/libraries/dagster-airflow/dagster_airflow_tests/test_dagster_job_factory/test_schedule_timezone.py
+++ python_modules/libraries/dagster-airflow/dagster_airflow_tests/test_dagster_job_factory/test_schedule_timezone.py
@@ -1,29 +1,28 @@
 import pendulum
+import pytest
+from airflow import __version__ as airflow_version
 from airflow.models.dag import DAG
-from dagster_airflow.dagster_pipeline_factory import \
-    make_dagster_schedule_from_airflow_dag
+from dagster_airflow.dagster_pipeline_factory import make_dagster_schedule_from_airflow_dag
 
 from dagster import job
 
 
+@pytest.mark.skipif(airflow_version < "2.0.0", reason="requires airflow 2")
 def test_schedule_timezone():
     args = {
-        'owner': 'airflow',
-        'start_date': pendulum.today('Europe/London').add(days=-2),
+        "owner": "airflow",
+        "start_date": pendulum.today("Europe/London").add(days=-2),
     }
     dag = DAG(
-        dag_id='test_schedules',
+        dag_id="test_schedules",
         default_args=args,
-        schedule='0 0 * * *',
-
+        schedule="0 0 * * *",
     )
+
     @job
     def job_def():
         return
 
-    schedule = make_dagster_schedule_from_airflow_dag(
-        dag=dag,
-        job_def=job_def
-    )
-    assert schedule.cron_schedule == '0 0 * * *'
-    assert schedule.execution_timezone == 'Europe/London'
+    schedule = make_dagster_schedule_from_airflow_dag(dag=dag, job_def=job_def)
+    assert schedule.cron_schedule == "0 0 * * *"
+    assert schedule.execution_timezone == "Europe/London"
