9b4196a38ccab4f4f3896b202884bc28e5797013
gibsondan <daniel@elementl.com>
2023-01-12 13:13:55 -0600

    one more windows test fix for signals
    Summary:
    Underlying functionality is now working, but a test failure remains - will actually let the windows build finish this time
    
    Test Plan: Azure and BK

--- python_modules/dagster/dagster_tests/general_tests/utils_tests/test_signals.py
+++ python_modules/dagster/dagster_tests/general_tests/utils_tests/test_signals.py
@@ -6,11 +6,15 @@ from dagster._utils import get_run_crash_explanation, get_terminate_signal
 def test_get_run_crash_explanation():
     assert get_run_crash_explanation("foo", 1) == "foo unexpectedly exited with code 1."
     assert (
-        get_run_crash_explanation("foo", -Signals.SIGTERM)
-        == "foo was terminated by signal 15 (SIGTERM)."
+        get_run_crash_explanation("foo", -Signals.SIGINT)
+        == "foo was terminated by signal 2 (SIGINT)."
     )
+
+    term_signal = get_terminate_signal()
+
     assert (
-        "foo was terminated by signal 9 (SIGKILL). This usually indicates that the process was"
-        " killed by the operating system due to running out of memory."
+        f"foo was terminated by signal {term_signal} ({term_signal.name}). This usually"
+        " indicates that the process was killed by the operating system due to running out of"
+        " memory."
         in get_run_crash_explanation("foo", -get_terminate_signal())
     )
