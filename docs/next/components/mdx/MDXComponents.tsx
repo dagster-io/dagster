66c4e7daf3b7111249c214661f6bebd8d64c8979
Ben Pankow <ben@elementl.com>
2023-01-13 14:29:54 -0600

    [docs][dagit-screenshot] Add CLI command to generate asset SVGs from snippets (#9764)
    ## Summary
    
    First pass at a CLI script which reads a MDX file and generates asset
    graph SVGs for marked snippets.
    
    Does this by:
    - Loading snippet file in Dagit
    - Triggering SVG download from Dagit using Selenium
    - Adding necessary font info to the SVG
    - Adding link to SVG to the MDX file
    
    ## Test Plan
    
    Tested locally. See vercel preview for
    https://github.com/dagster-io/dagster/pull/9765 for an example in the
    asset tutorials:
    https://dagster-git-benpankow-update-tutorials-with-ass-ce5c5e-elementl.vercel.app/tutorial/assets/asset-graph

--- docs/next/components/mdx/MDXComponents.tsx
+++ docs/next/components/mdx/MDXComponents.tsx
@@ -471,7 +471,7 @@ const Code: React.FC<CodeProps> = ({children, dagimage, ...props}) => {
       </div>
       {dagimage && (
         <RenderedDAG
-          svgSrc="/images/asset-screenshots/my_assets.svg"
+          svgSrc={'/' + dagimage}
           mobileImgSrc="/images-2022-july/screenshots/python-assets2.png"
         />
       )}
