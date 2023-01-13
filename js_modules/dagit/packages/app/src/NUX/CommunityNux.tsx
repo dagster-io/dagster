a24aab16c612c72a3fa46cd9a29cefcb86a12c32
Marco Salazar <marco@elementl.com>
2023-01-13 13:38:37 -0500

    only load an iframe for recaptcha after use agrees to submit email

--- js_modules/dagit/packages/app/src/NUX/CommunityNux.tsx
+++ js_modules/dagit/packages/app/src/NUX/CommunityNux.tsx
@@ -1,7 +1,19 @@
 import {useStateWithStorage} from '@dagster-io/dagit-core/hooks/useStateWithStorage';
+import {
+  Body,
+  Box,
+  Button,
+  Checkbox,
+  Colors,
+  Dialog,
+  ExternalAnchorButton,
+  Heading,
+  Icon,
+  Spinner,
+  TextInput,
+} from '@dagster-io/ui';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {createGlobalStyle} from 'styled-components/macro';
+import isEmail from 'validator/lib/isEmail';
 
 export const CommunityNux = () => {
   const [didDismissCommunityNux, dismissCommunityNux] = useStateWithStorage(
@@ -28,12 +40,130 @@ const CommunityNuxImpl: React.FC<{dismiss: () => void}> = ({dismiss}) => {
   React.useEffect(() => {
     const timeout = setTimeout(() => {
       setShouldShowNux(true);
-    }, TIMEOUT); // wait 5 seconds before showing Nux
+    }, TIMEOUT);
     return () => {
       clearTimeout(timeout);
     };
   }, []);
 
+  const [iframeData, setIframeData] = React.useState<{email: string; newsletter: boolean} | null>(
+    null,
+  );
+  const submit = (email: string, newsletter: boolean) => {
+    setIframeData({email, newsletter});
+  };
+
+  console.log({iframeData});
+
+  return (
+    <Dialog isOpen={shouldShowNux} style={{width: '680px'}}>
+      {iframeData ? (
+        <RecaptchaIFrame
+          email={iframeData.email}
+          newsletter={iframeData.newsletter}
+          dismiss={dismiss}
+        />
+      ) : (
+        <Form dismiss={dismiss} submit={submit} />
+      )}
+    </Dialog>
+  );
+};
+
+const Form: React.FC<{
+  dismiss: () => void;
+  submit: (email: string, newsletter: boolean) => void;
+}> = ({dismiss, submit}) => {
+  const [email, setEmail] = React.useState('');
+  const [newsletter, setNewsLetter] = React.useState(false);
+  const validEmail = isEmail(email);
+  const [emailChanged, setEmailChanged] = React.useState(false);
+  const [blurred, setBlurred] = React.useState(false);
+
+  return (
+    <Box
+      flex={{direction: 'column', gap: 16}}
+      style={{padding: '36px', width: '680px', background: 'white'}}
+    >
+      <Box
+        flex={{direction: 'row', gap: 24, alignItems: 'center'}}
+        padding={{bottom: 24}}
+        border={{side: 'bottom', color: Colors.KeylineGray, width: 1}}
+      >
+        <Box flex={{direction: 'column', gap: 8, alignItems: 'start', justifyContent: 'start'}}>
+          <Heading>Join the Dagster community</Heading>
+          <Body style={{color: Colors.Gray700, marginBottom: '4px'}}>
+            Connect with thousands of other data practitioners building with Dagster. Share
+            knowledge, get help, and contribute to the open-source project.
+          </Body>
+          <ExternalAnchorButton icon={<Icon name="slack" />} href="https://www.dagster.io/slack">
+            Join us on Slack
+          </ExternalAnchorButton>
+        </Box>
+        <video autoPlay muted loop playsInline width={120} height={120}>
+          <source src="/Dagster_world.mp4" type="video/mp4" />
+        </video>
+      </Box>
+      <Box flex={{direction: 'column', justifyContent: 'stretch', gap: 12}}>
+        <div>Notify me about Dagster security updates</div>
+        <TextInput
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setEmailChanged(true);
+          }}
+          onBlur={() => setBlurred(true)}
+          placeholder="hello@dagster.io"
+          strokeColor={!emailChanged || validEmail ? undefined : Colors.Red500}
+          style={{width: '100%'}}
+        />
+        {emailChanged && blurred && !validEmail ? (
+          <div style={{paddingBottom: '12px', color: Colors.Red500, fontSize: '12px'}}>
+            Add your email to get updates from Dagster.
+          </div>
+        ) : null}
+        <Box as="label" flex={{direction: 'row', gap: 8, alignItems: 'center'}}>
+          <Checkbox
+            checked={newsletter}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setNewsLetter(e.target.checked);
+            }}
+          />{' '}
+          <span>Sign up for the Dagster newsletter</span>
+        </Box>
+      </Box>
+      <Box flex={{direction: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <a href="https://dagster.io/privacy" target="_blank" rel="noreferrer">
+          Privacy Policy
+        </a>
+        <Box flex={{direction: 'row', gap: 8}}>
+          <Button
+            onClick={() => {
+              dismiss();
+            }}
+          >
+            Skip
+          </Button>
+          <Button
+            onClick={() => {
+              submit(email, newsletter);
+            }}
+            disabled={!validEmail}
+            intent="primary"
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+const RecaptchaIFrame: React.FC<{
+  newsletter: boolean;
+  email: string;
+  dismiss: () => void;
+}> = ({dismiss, newsletter, email}) => {
   const [iframeLoaded, setIframeLoaded] = React.useState(false);
   const [width, setWidth] = React.useState(680);
   const [height, setHeight] = React.useState(462);
@@ -55,44 +185,30 @@ const CommunityNuxImpl: React.FC<{dismiss: () => void}> = ({dismiss}) => {
     };
   }, [dismiss]);
 
-  const isOpen = shouldShowNux && iframeLoaded;
-
-  return ReactDOM.createPortal(
-    <div className="bp3-portal dagit-portal">
-      <GlobalOffscreenStyle />
-      <div
-        className={`bp3-overlay ${isOpen ? 'bp3-overlay-open' : ''} bp3-overlay-scroll-container`}
-      >
-        {isOpen ? <div className="bp3-overlay-backdrop dagit-backdrop" /> : null}
-        <div className="bp3-dialog-container bp3-overlay-content">
-          <div
-            className={`bp3-dialog dagit-dialog ${isOpen ? '' : 'offscreen'}`}
-            style={{width: width + 'px', height: height + 'px'}}
-          >
-            <iframe
-              src={IFRAME_SRC}
-              width={width}
-              height={height}
-              style={{
-                border: 'none',
-              }}
-              onLoad={() => {
-                setIframeLoaded(true);
-              }}
-            />
-          </div>
-        </div>
-      </div>
-    </div>,
-    document.body,
+  return (
+    <Box padding={32} flex={{justifyContent: 'center', alignItems: 'center'}}>
+      {iframeLoaded ? null : <Spinner purpose="section" />}
+      <iframe
+        src={`${IFRAME_SRC}?email=${email}${newsletter ? '&newsletter=1' : ''}`}
+        width={width}
+        height={height}
+        style={{
+          border: 'none',
+          ...(iframeLoaded
+            ? {}
+            : {
+                position: 'absolute',
+                left: '-99999px',
+                width: '304px',
+                height: '78px',
+              }),
+        }}
+        onLoad={() => {
+          setIframeLoaded(true);
+        }}
+      />
+    </Box>
   );
 };
 
-const IFRAME_SRC = 'https://dagster.io/dagit_iframes/community_nux';
-
-const GlobalOffscreenStyle = createGlobalStyle`
-  .offscreen {
-    position: absolute;
-    left: -99999px;
-  }
-`;
+const IFRAME_SRC = '//localhost:4000/dagit_iframes/community_nux';
