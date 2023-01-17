2770e3437960fef210560dd43ed5cfc8d12d1de9
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2023-01-17 15:30:44 -0600

    [dagit] Strip down query for Scheduled Runs (#11751)
    ### Summary & Motivation
    
    The "Scheduled runs" page currently overfetches, pulling in run
    information that we don't actually need for this page.
    
    Use a couple of stripped-down fragments to get only the information we
    need.
    
    ### How I Tested These Changes
    
    View "Scheduled runs" as Cloud user with lots of schedules, verify that
    the page loads and the ticks render properly.

--- js_modules/dagit/packages/core/src/runs/types/ScheduledRunListRoot.types.ts
+++ js_modules/dagit/packages/core/src/runs/types/ScheduledRunListRoot.types.ts
@@ -2,9 +2,9 @@
 
 import * as Types from '../../graphql/types';
 
-export type SchedulerInfoQueryVariables = Types.Exact<{[key: string]: never}>;
+export type ScheduledRunsListQueryVariables = Types.Exact<{[key: string]: never}>;
 
-export type SchedulerInfoQuery = {
+export type ScheduledRunsListQuery = {
   __typename: 'DagitQuery';
   instance: {
     __typename: 'Instance';
@@ -54,63 +54,20 @@ export type SchedulerInfoQuery = {
             __typename: 'Schedule';
             id: string;
             name: string;
-            cronSchedule: string;
             executionTimezone: string | null;
-            pipelineName: string;
-            solidSelection: Array<string | null> | null;
             mode: string;
-            description: string | null;
-            partitionSet: {__typename: 'PartitionSet'; id: string; name: string} | null;
+            solidSelection: Array<string | null> | null;
+            pipelineName: string;
             scheduleState: {
               __typename: 'InstigationState';
               id: string;
-              selectorId: string;
-              name: string;
-              instigationType: Types.InstigationType;
               status: Types.InstigationStatus;
-              repositoryName: string;
-              repositoryLocationName: string;
-              runningCount: number;
-              typeSpecificData:
-                | {__typename: 'ScheduleData'; cronSchedule: string}
-                | {__typename: 'SensorData'; lastRunKey: string | null; lastCursor: string | null}
-                | null;
-              runs: Array<{
-                __typename: 'Run';
-                id: string;
-                runId: string;
-                status: Types.RunStatus;
-                startTime: number | null;
-                endTime: number | null;
-                updateTime: number | null;
-              }>;
-              ticks: Array<{
-                __typename: 'InstigationTick';
-                id: string;
-                cursor: string | null;
-                status: Types.InstigationTickStatus;
-                timestamp: number;
-                skipReason: string | null;
-                runIds: Array<string>;
-                runKeys: Array<string>;
-                error: {
-                  __typename: 'PythonError';
-                  message: string;
-                  stack: Array<string>;
-                  errorChain: Array<{
-                    __typename: 'ErrorChainLink';
-                    isExplicitLink: boolean;
-                    error: {__typename: 'PythonError'; message: string; stack: Array<string>};
-                  }>;
-                } | null;
-              }>;
             };
             futureTicks: {
               __typename: 'FutureInstigationTicks';
               results: Array<{__typename: 'FutureInstigationTick'; timestamp: number}>;
             };
           }>;
-          displayMetadata: Array<{__typename: 'RepositoryMetadata'; key: string; value: string}>;
         }>;
       };
 };
