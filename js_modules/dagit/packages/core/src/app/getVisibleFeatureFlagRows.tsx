c0c48113f3cc585a3602d708bfc7de5c3d050d62
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2023-01-17 09:46:39 -0600

    [dagit] Move User Settings into a Dialog (#11706)
    ### Summary & Motivation
    
    Begin moving "User settings" into a dialog.
    
    <img width="1223" alt="Screenshot 2023-01-12 at 4 14 38 PM" src="https://user-images.githubusercontent.com/2823852/212395674-e58604d6-d6ff-44b6-a3b8-9acdd38914ee.png">
    
    
    - Create `UserSettingsButton` to replace the `SettingsLink` defined in the `app` index file. This is because we no longer have a global route to refer to -- all of the user settings behavior needs to move to `core`.
    - Create a dialog component that contains the contentful pieces of User Settings configuration
    - Create `UserSettingsDialogContent`, which retrieves and renders time zone, feature flag, and keyboard shortcut management UI
    - Refactor `getFeatureFlagRows` to return key/value pairs for the flags that should be shown. This allows `UserSettingsDialogContent` to render checkboxes for us, instead of specifying them in this function.
      - In Cloud, we will be able to splat this array alongside any Cloud-specific flags, and pass that as `visibleFlags` to the `UserSettingsDialog` component 
    - Remove the OSS `/settings` route.
    
    Because toggling flags previously would reload the page, and now a fresh reload would leave us with no dialog, the user will be forced to click "Done" to continue from the dialog. If any changes have been made, the page reloads to incorporate the flag change. If no changes are made, "Done" just closes the dialog.
    
    ### How I Tested These Changes
    
    View Dagit, click on gear button. Verify that the dialog appears.
    
    - Make no changes, close the dialog with "Done". Verify that there is no page reload.
    - Reopen, toggle a flag, click "Done". Verify that the page reloads.
    
    View Cloud (master). Verify that the User Settings page renders and behaves properly using the new components, with no errors.

---
+++ js_modules/dagit/packages/core/src/app/getVisibleFeatureFlagRows.tsx
@@ -0,0 +1,19 @@
+import {FeatureFlag} from './Flags';
+
+/**
+ * Open-source feature flags to be displayed in Dagit "User settings"
+ */
+export const getVisibleFeatureFlagRows = () => [
+  {
+    key: 'Debug console logging',
+    flagType: FeatureFlag.flagDebugConsoleLogging,
+  },
+  {
+    key: 'Disable WebSockets',
+    flagType: FeatureFlag.flagDisableWebsockets,
+  },
+  {
+    key: 'Experimental schedule/sensor logging view',
+    flagType: FeatureFlag.flagSensorScheduleLogging,
+  },
+];
