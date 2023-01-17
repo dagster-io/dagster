5dbbb94554a83bd65f24881851a403c06649f176
Ben Gotow <ben@foundry376.com>
2023-01-17 13:19:45 -0600

    [dagit] Display source data used in specific asset materializations (#11736)
    ### Summary & Motivation
    
    This PR addresses #11174 - a new section of Dagit's materialization
    event / partition detail view shows the upstream materializations that
    were "source data" for the displayed event.
    
    This uses a new GraphQL resolver built on the underlying storage +
    fetching for asset versioning.
    
    
    ![image](https://user-images.githubusercontent.com/1037212/212723976-644bacfc-23a3-4a38-8aa8-cc5d667e1747.png)
    
    
    ### How I Tested These Changes
    
    There's a new unit test, but I also tested this on a large set of
    existing materializations, verified it does not appear for observations,
    and checked that navigating the referenced assets produces internally
    consistent views. (eg: clicking B in A->B->C->D still shows the same
    C->D)
    
    Co-authored-by: bengotow <bgotow@elementl.com>

---
+++ js_modules/dagit/packages/core/src/assets/types/AssetMaterializationUpstreamData.types.ts
@@ -0,0 +1,31 @@
+// Generated GraphQL types, do not edit manually.
+
+import * as Types from '../../graphql/types';
+
+export type AssetMaterializationUpstreamQueryVariables = Types.Exact<{
+  assetKey: Types.AssetKeyInput;
+  timestamp: Types.Scalars['String'];
+}>;
+
+export type AssetMaterializationUpstreamQuery = {
+  __typename: 'DagitQuery';
+  assetNodeOrError:
+    | {
+        __typename: 'AssetNode';
+        id: string;
+        assetMaterializationUsedData: Array<{
+          __typename: 'MaterializationUpstreamDataVersion';
+          timestamp: string;
+          assetKey: {__typename: 'AssetKey'; path: Array<string>};
+          downstreamAssetKey: {__typename: 'AssetKey'; path: Array<string>};
+        }>;
+      }
+    | {__typename: 'AssetNotFoundError'};
+};
+
+export type MaterializationUpstreamDataVersionFragment = {
+  __typename: 'MaterializationUpstreamDataVersion';
+  timestamp: string;
+  assetKey: {__typename: 'AssetKey'; path: Array<string>};
+  downstreamAssetKey: {__typename: 'AssetKey'; path: Array<string>};
+};
