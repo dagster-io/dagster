3759a25087f82b4b3480b60fc7b5b78a93e48ddb
Tim Castillo <tim@elementl.com>
2023-01-18 16:29:37 -0500

    rephrasing a section and remember doc snippet blocks from it

--- examples/docs_snippets/docs_snippets/intro_tutorial/basics/hackernews/analyze_hackernews.py
+++ examples/docs_snippets/docs_snippets/intro_tutorial/basics/hackernews/analyze_hackernews.py
@@ -1,13 +1,12 @@
-# start_hackernews_topstory_ids
 import requests
+import matplotlib.pyplot as plt
+import pandas as pd
+from wordcloud import STOPWORDS, WordCloud
+
 
 newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 top_100_newstories = requests.get(newstories_url).json()[:100]
-# end_hackernews_topstory_ids
 
-# start_hackernews_topstories
-import matplotlib.pyplot as plt
-import pandas as pd
 
 results = []
 for item_id in top_100_newstories:
@@ -18,22 +17,17 @@ for item_id in top_100_newstories:
         print(f"Got {len(results)} items so far.")
 
 df = pd.DataFrame(results)
-# end_hackernews_topstories
 
-
-# start_hackernews_topstories_word_cloud
-from wordcloud import STOPWORDS, WordCloud
 
 stopwords = set(STOPWORDS)
 stopwords.update(["Ask", "Show", "HN", "S"])
 titles_text = " ".join([str(item) for item in df["title"]])
 titles_cloud = WordCloud(stopwords=stopwords, background_color="white").generate(titles_text)
 
-# Generate the word cloud image
 plt.figure(figsize=(8, 8), facecolor=None)
 plt.imshow(titles_cloud, interpolation="bilinear")
 plt.axis("off")
 plt.tight_layout(pad=0)
 
+
 plt.show()
-# end_hackernews_topstories_word_cloud
