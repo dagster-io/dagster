3a68f020023352d6f3e6fa4407814490cda1f44b
benpankow <ben@elementl.com>
2023-01-20 11:29:59 -0800

    mypy

--- examples/experimental/project_fully_featured_v2_resources/project_fully_featured_v2_resources/assets/core/id_range_for_time.py
+++ examples/experimental/project_fully_featured_v2_resources/project_fully_featured_v2_resources/assets/core/id_range_for_time.py
@@ -97,4 +97,4 @@ def id_range_for_time(
     For the configured time partition, searches for the range of ids that were created in that time.
     """
     start, end = context.asset_partitions_time_window_for_output()
-    return _id_range_for_time(start.timestamp(), end.timestamp(), hn_client)
+    return _id_range_for_time(int(start.timestamp()), int(end.timestamp()), hn_client)
