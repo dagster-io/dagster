beb5e2f700943848b3619a69de79bf57f3785940
Nicholas Schrock <schrockn@elementl.com>
2023-01-22 11:56:08 -0500

    generic looking good

--- python_modules/dagster/dagster/_core/executor/per_step/per_step_executor.py
+++ python_modules/dagster/dagster/_core/executor/per_step/per_step_executor.py
@@ -1,4 +1,17 @@
-from typing import Any, Callable, Dict, Iterator, List, Optional
+from abc import abstractmethod
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Iterator,
+    List,
+    Mapping,
+    Optional,
+    Type,
+    TypeVar,
+    cast,
+)
 
 from dagster import _check as check
 from dagster._core.events import DagsterEvent
@@ -14,71 +27,96 @@ from dagster._core.executor.step_delegating.step_handler.base import (
 )
 
 
+def step_context_for_handler_context(step_handler_context: StepHandlerContext) -> IStepContext:
+    step_keys = step_handler_context.step_keys
+    check.invariant(len(step_keys) == 1)
+    return step_handler_context.get_step_context(step_keys[0])
+
+
 class StepHandlerForPerStepExecutor(StepHandler):
     def __init__(
         self,
-        step_executor_factory: Callable[[IStepContext], "StepExecutor"],
+        step_executor_factory: Callable[[StepHandlerContext, IStepContext], "StepExecutor"],
     ):
         self._step_executor_factory = step_executor_factory
+        self._step_executors: Dict[str, StepExecutor] = {}
 
     @property
     def name(self) -> str:
         return "StepHandlerForPerStepExecutor"
 
-    def handler_for_key(self, step_context: IStepContext) -> "StepExecutor":
-        return self._step_executor_factory(step_context)
-
-    def _get_step_context(self, step_handler_context: StepHandlerContext) -> IStepContext:
-        step_keys = step_handler_context.step_keys
-        check.invariant(len(step_keys) == 1)
-        return step_handler_context.get_step_context(step_keys[0])
+    def _get_step_executor(self, step_handler_context: StepHandlerContext) -> "StepExecutor":
+        step_context = step_context_for_handler_context(step_handler_context)
+        step_key = step_context.step.key
+        if step_key not in self._step_executors:
+            self._step_executors[step_key] = self._step_executor_factory(
+                step_handler_context, step_context
+            )
+        return self._step_executors[step_key]
 
     def launch_step(self, step_handler_context: StepHandlerContext) -> Iterator[DagsterEvent]:
-        step_context = self._get_step_context(step_handler_context)
-
-        remote_handler = self.handler_for_key(step_context)
-
+        step_executor = self._get_step_executor(step_handler_context)
+        step_context = step_executor.step_context
         yield DagsterEvent.step_worker_starting(
             step_context,
             message=f'Executing step "{step_context.step.key}" in external process',
             metadata_entries=[],
         )
 
-        iterator = remote_handler.execute(step_handler_context)
+        iterator = step_executor.execute()
         if iterator:
             yield from iterator
 
+    @property
+    def step_executors(self) -> Mapping[str, "StepExecutor"]:
+        return self._step_executors
+
     def check_step_health(self, step_handler_context: StepHandlerContext) -> CheckStepHealthResult:
-        step_context = self._get_step_context(step_handler_context)
-        return self.handler_for_key(step_context).check_step_health(step_handler_context)
+        return self._get_step_executor(step_handler_context).check_step_health()
 
     def terminate_step(self, step_handler_context: StepHandlerContext) -> Iterator[DagsterEvent]:
-        step_context = self._get_step_context(step_handler_context)
-        iterator = self.handler_for_key(step_context).terminate(step_handler_context)
+        iterator = self._get_step_executor(step_handler_context).terminate()
         if iterator:
             yield from iterator
 
 
 class StepExecutor:
-    def __init__(self, step_context: IStepContext):
-        self.step_context = step_context
+    def __init__(self, step_handler_context: StepHandlerContext, step_context: IStepContext):
+        # Users will inherit from this so doing runtime checks
+        self.step_handler_context = check.inst_param(
+            step_handler_context, "step_handler_context", StepHandlerContext
+        )
+        self.step_context = check.inst_param(step_context, "step_context", IStepContext)
 
-    def execute(self, step_handler_context: StepHandlerContext) -> Optional[Iterator[DagsterEvent]]:
+    @property
+    def step_key(self) -> str:
+        return self.step_context.step.key
+
+    def execute(self) -> Optional[Iterator[DagsterEvent]]:
         raise NotImplementedError()
 
-    def terminate(
-        self, step_handler_context: StepHandlerContext
-    ) -> Optional[Iterator[DagsterEvent]]:
+    def terminate(self) -> Optional[Iterator[DagsterEvent]]:
         raise NotImplementedError()
 
-    def check_step_health(self, step_handler_context: StepHandlerContext):
+    def check_step_health(self) -> CheckStepHealthResult:
         raise NotImplementedError()
 
 
-class StepExecutorBasedRunExecutor(Executor):
+class StepExecutorBasedExecutor(Executor):
+    @property
+    def step_count(self) -> int:
+        return len(self.step_executors)
+
+    @property
+    @abstractmethod
+    def step_executors(self) -> Mapping[str, "StepExecutor"]:
+        ...
+
+
+class PluggableStepExecutorBasedRunExecutor(StepExecutorBasedExecutor):
     def __init__(
         self,
-        step_executor_factory: Callable[[IStepContext], "StepExecutor"],
+        step_executor_factory: Callable[[StepHandlerContext, IStepContext], "StepExecutor"],
         retries: RetryMode,
         sleep_seconds: Optional[float] = None,
         check_step_health_interval_seconds: Optional[int] = None,
@@ -86,8 +124,9 @@ class StepExecutorBasedRunExecutor(Executor):
         tag_concurrency_limits: Optional[List[Dict[str, Any]]] = None,
         should_verify_step: bool = False,
     ):
+        self._step_handler = StepHandlerForPerStepExecutor(step_executor_factory)
         self._inner_executor = StepDelegatingExecutor(
-            StepHandlerForPerStepExecutor(step_executor_factory),
+            step_handler=self._step_handler,
             retries=retries,
             sleep_seconds=sleep_seconds,
             check_step_health_interval_seconds=check_step_health_interval_seconds,
@@ -102,3 +141,53 @@ class StepExecutorBasedRunExecutor(Executor):
     @property
     def retries(self) -> RetryMode:
         return self._inner_executor.retries
+
+    @property
+    def step_executors(self) -> Mapping[str, "StepExecutor"]:
+        return self._step_handler.step_executors
+
+
+TStepExecutor = TypeVar("TStepExecutor", bound=StepExecutor)
+
+
+class GenericRunExecutor(StepExecutorBasedExecutor, Generic[TStepExecutor]):
+    def _create_step_executor(
+        self, step_handler_context: StepHandlerContext, step_context: IStepContext
+    ) -> TStepExecutor:
+        return self._step_executor_cls(step_handler_context, step_context)
+
+    # from https://stackoverflow.com/a/70231012
+    # This seems to be the most reliable way to be able to access the generic type parameter at runtime
+    # Requires slightly different instantiation
+
+    def __init__(
+        self,
+        step_executor_cls: Type[TStepExecutor],
+        retries: RetryMode,
+        sleep_seconds: Optional[float] = None,
+        check_step_health_interval_seconds: Optional[int] = None,
+        max_concurrent: Optional[int] = None,
+        tag_concurrency_limits: Optional[List[Dict[str, Any]]] = None,
+        should_verify_step: bool = False,
+    ):
+        self._step_executor_cls = step_executor_cls
+        self._inner_executor = PluggableStepExecutorBasedRunExecutor(
+            self._create_step_executor,
+            retries=retries,
+            sleep_seconds=sleep_seconds,
+            check_step_health_interval_seconds=check_step_health_interval_seconds,
+            max_concurrent=max_concurrent,
+            tag_concurrency_limits=tag_concurrency_limits,
+            should_verify_step=should_verify_step,
+        )
+
+    def execute(self, plan_context: PlanOrchestrationContext, execution_plan: ExecutionPlan):
+        return self._inner_executor.execute(plan_context, execution_plan)
+
+    @property
+    def retries(self) -> RetryMode:
+        return self._inner_executor.retries
+
+    @property
+    def step_executors(self) -> Mapping[str, TStepExecutor]:
+        return cast(Mapping[str, TStepExecutor], self._inner_executor._step_handler.step_executors)
--- python_modules/dagster/dagster_tests/execution_tests/engine_tests/test_per_step_executor.py
+++ python_modules/dagster/dagster_tests/execution_tests/engine_tests/test_per_step_executor.py
@@ -1,104 +1,44 @@
 import subprocess
 from contextlib import contextmanager
-from typing import Callable, Dict, Iterator, List, Optional
+from typing import Callable, Iterator, Optional
 
 from dagster import Definitions, job, op, reconstructable
 from dagster._core.definitions.job_definition import JobDefinition
 from dagster._core.events import DagsterEvent
 from dagster._core.execution.api import execute_job
-from dagster._core.execution.context.system import IStepContext
 from dagster._core.execution.execute_job_result import ExecuteJobResult
 from dagster._core.execution.retries import RetryMode
 from dagster._core.executor.per_step.per_step_executor import (
+    GenericRunExecutor,
+    PluggableStepExecutorBasedRunExecutor,
     StepExecutor,
-    StepExecutorBasedRunExecutor,
 )
 from dagster._core.executor.step_delegating import (
     CheckStepHealthResult,
 )
-from dagster._core.executor.step_delegating.step_handler.base import (
-    StepHandlerContext,
-)
 from dagster._core.test_utils import instance_for_test
 
 
-class PerRunState:
-    def __init__(self):
-        self.processes: List[subprocess.Popen] = []
-        self.handler_dict: Dict[str, SubprocessStepExecutor] = {}
-
-    def wait_for_processes(self):
-        for p in self.processes:
-            p.wait(timeout=5)
-
-    @property
-    def step_count(self):
-        return len(self.handler_dict)
-
-
+# This is what it would look like for a user to write a step executor
 class SubprocessStepExecutor(StepExecutor):
-    def __init__(self, step_context: IStepContext, per_run_state: PerRunState):
-        super().__init__(step_context)
-        self.step_key = step_context.step.key
-        self.per_run_state = per_run_state
-
-    def execute(self, step_handler_context: StepHandlerContext) -> Optional[Iterator[DagsterEvent]]:
-        self.per_run_state.processes.append(
-            subprocess.Popen(step_handler_context.execute_step_args.get_command_args())
+    def execute(self) -> Optional[Iterator[DagsterEvent]]:
+        self._popen = subprocess.Popen(
+            self.step_handler_context.execute_step_args.get_command_args()
         )
 
-    def check_step_health(self, step_handler_context: StepHandlerContext):
+    def check_step_health(self):
         return CheckStepHealthResult.healthy()
 
-    def terminate(
-        self, step_handler_context: StepHandlerContext
-    ) -> Optional[Iterator[DagsterEvent]]:
+    def terminate(self) -> Optional[Iterator[DagsterEvent]]:
         raise NotImplementedError()
 
-
-def get_subprocess_executor_with_monkey_patched_process_collection():
-    per_run_state = PerRunState()
-
-    class SubprocessCurriedRunState(SubprocessStepExecutor):
-        def __init__(self, step_context: IStepContext):
-            super().__init__(step_context=step_context, per_run_state=per_run_state)
-
-    step_executors = {"subprocess": SubprocessCurriedRunState}
-
-    def _get_single_step_handler(step_context: IStepContext) -> SubprocessCurriedRunState:
-        if "step_executor" in step_context.step.tags:
-            step_executor_cls = step_executors[step_context.step.tags["step_executor"]]
-            step_handler = step_executor_cls(step_context)
-        else:
-            step_handler = SubprocessCurriedRunState(
-                step_context=step_context,
-            )
-        per_run_state.handler_dict[step_context.step.key] = step_handler
-        return step_handler
-
-    executor = StepExecutorBasedRunExecutor(
-        step_executor_factory=_get_single_step_handler,
-        retries=RetryMode.DISABLED,
-    )
-    setattr(executor, "__per_run_state_for_test", per_run_state)
-    return executor
+    def wait_for_completion(self):
+        return self._popen.wait(timeout=5)
 
 
-# def get_ergonomic_executor_construction():
-#     # return RunWorkerExecutor[SubprocessStepExecutor](
-#     #     retries=RetryMode.DISABLED
-#     # )
-
-#     executor = StepExecutorBasedExecutor[(
-#         step_executor_factory=_get_single_step_handler,
-#         retries=RetryMode.DISABLED,
-#     )
-
-
-def get_per_run_state(result: ExecuteJobResult) -> PerRunState:
+def get_executor(result: ExecuteJobResult) -> PluggableStepExecutorBasedRunExecutor:
     assert result.job_def.executor_def.executor_creation_fn
-    executor = result.job_def.executor_def.executor_creation_fn(None)  # type: ignore
-    return getattr(executor, "__per_run_state_for_test")
+    return result.job_def.executor_def.executor_creation_fn(None)  # type: ignore
 
 
 @contextmanager
@@ -108,14 +48,17 @@ def execute_and_complete_job(job_fn: Callable):
             reconstructable(job_fn),
             instance=instance,
         ) as result:
-            per_run_state = get_per_run_state(result)
-            per_run_state.wait_for_processes()
-            yield result
+            executor = get_executor(result)
+            for step_executor in executor.step_executors.values():
+                assert isinstance(step_executor, SubprocessStepExecutor)
+                step_executor.wait_for_completion()
+            yield result, executor
 
 
 def get_job_for_execution(job_def: JobDefinition):
     defs = Definitions(
-        jobs=[job_def], executor=get_subprocess_executor_with_monkey_patched_process_collection()
+        jobs=[job_def],
+        executor=GenericRunExecutor(SubprocessStepExecutor, retries=RetryMode.DISABLED),
     )
     return defs.get_job_def(job_def.name)
 
@@ -133,13 +76,12 @@ def return_single_op_job():
 
 
 def test_single_op_job():
-    with execute_and_complete_job(return_single_op_job) as result:
+    with execute_and_complete_job(return_single_op_job) as (result, executor):
         assert result.success
         assert result.output_for_node("return_one") == 1
 
-        per_run_state = get_per_run_state(result)
-        assert per_run_state.step_count == 1
-        assert isinstance(per_run_state.handler_dict["return_one"], SubprocessStepExecutor)
+        assert executor.step_count == 1
+        assert isinstance(executor.step_executors["return_one"], SubprocessStepExecutor)
 
 
 def return_multi_op_job():
@@ -163,15 +105,14 @@ def return_multi_op_job():
 
 
 def test_multi_op_job():
-    with execute_and_complete_job(return_multi_op_job) as result:
+    with execute_and_complete_job(return_multi_op_job) as (result, executor):
         assert result.success
         assert result.output_for_node("return_one") == 1
         assert result.output_for_node("return_two") == 2
         assert result.output_for_node("add") == 3
 
-        per_run_state = get_per_run_state(result)
-        assert per_run_state.step_count == 3
-        # we in fact get different object instances
-        assert per_run_state.handler_dict["return_one"].step_key == "return_one"
-        assert per_run_state.handler_dict["return_two"].step_key == "return_two"
-        assert per_run_state.handler_dict["add"].step_key == "add"
+        assert executor.step_count == 3
+        step_executors = executor.step_executors
+        assert step_executors["return_one"].step_key == "return_one"
+        assert step_executors["return_two"].step_key == "return_two"
+        assert step_executors["add"].step_key == "add"
