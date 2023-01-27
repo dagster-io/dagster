d85600fdbf23821d999f41e4f8ba3e78fb3f1372
prha <prha@elementl.com>
2023-01-27 08:16:00 -0800

    add explicit version check

--- python_modules/libraries/dagster-mysql/dagster_mysql_tests/test_run_storage.py
+++ python_modules/libraries/dagster-mysql/dagster_mysql_tests/test_run_storage.py
@@ -4,6 +4,7 @@ import pytest
 import yaml
 from dagster._core.test_utils import environ, instance_for_test
 from dagster_mysql.run_storage import MySQLRunStorage
+from dagster_mysql.utils import parse_mysql_version
 from dagster_tests.storage_tests.utils.run_storage import TestRunStorage
 
 TestRunStorage.__test__ = False
@@ -92,6 +93,7 @@ def test_mysql_version(conn_string):
             return "5.7.38-log"
 
     storage = FakeNonBucketing(conn_string)
+    assert parse_mysql_version("5.7.38-log") == (5, 7, 38)
     assert not storage.supports_bucket_queries
 
     class FakeBucketing(MySQLRunStorage):
@@ -100,4 +102,5 @@ def test_mysql_version(conn_string):
             return "8.0.31-google"
 
     storage = FakeBucketing(conn_string)
+    assert parse_mysql_version("8.0.31-google") == (8, 0, 31)
     assert storage.supports_bucket_queries
