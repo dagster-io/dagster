88978a3e35636ae70c64137ea2962a549a28c3cf
gibsondan <daniel@elementl.com>
2023-01-27 10:37:26 -0600

    Fix aws ssm tests (#11886)
    Summary:
    Moto added new validation that caught a real problem in our tests - we
    were missing a required parameter here.
    
    Test Plan: BK
    
    ### Summary & Motivation
    
    ### How I Tested These Changes

--- python_modules/libraries/dagster-aws/dagster_aws_tests/ssm_tests/test_parameters.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/ssm_tests/test_parameters.py
@@ -4,16 +4,21 @@ from dagster_aws.ssm.parameters import get_parameters_by_paths, get_parameters_b
 def test_get_parameters_from_tags(mock_ssm_client):
     mock_ssm_client.put_parameter(
         Name="foo_param1",
+        Type="String",
         Value="foo_value1",
         Tags=[{"Key": "foo_tag_key", "Value": "foo_tag_value1"}],
     )
     mock_ssm_client.put_parameter(
         Name="foo_param2",
+        Type="String",
         Value="foo_value2",
         Tags=[{"Key": "foo_tag_key", "Value": "foo_tag_value2"}],
     )
     mock_ssm_client.put_parameter(
-        Name="bar_param", Value="bar_value", Tags=[{"Key": "bar_tag_key", "Value": "bar_tag_value"}]
+        Name="bar_param",
+        Type="String",
+        Value="bar_value",
+        Tags=[{"Key": "bar_tag_key", "Value": "bar_tag_value"}],
     )
     result = get_parameters_by_tags(
         mock_ssm_client,
@@ -31,9 +36,9 @@ def test_get_parameters_from_tags(mock_ssm_client):
 
 
 def test_get_parameters_by_path(mock_ssm_client):
-    mock_ssm_client.put_parameter(Name="path/based/param1", Value="param1")
-    mock_ssm_client.put_parameter(Name="path/based/nested/param2", Value="param2")
-    mock_ssm_client.put_parameter(Name="path/not/matching/param3", Value="param3")
+    mock_ssm_client.put_parameter(Name="path/based/param1", Value="param1", Type="String")
+    mock_ssm_client.put_parameter(Name="path/based/nested/param2", Value="param2", Type="String")
+    mock_ssm_client.put_parameter(Name="path/not/matching/param3", Value="param3", Type="String")
 
     result = get_parameters_by_paths(
         mock_ssm_client, ["path/based"], with_decryption=True, recursive=True
