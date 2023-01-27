f7fbec8750ba32f430ad6676c18851e72efffea9
Rex Ledesma <rex@elementl.com>
2023-01-27 12:02:03 -0800

    test(dagster-dbt): add testing for python 3.10

--- .buildkite/dagster-buildkite/dagster_buildkite/steps/packages.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/packages.py
@@ -367,10 +367,6 @@ LIBRARY_PACKAGES_WITH_CUSTOM_CONFIG: List[PackageSpec] = [
     PackageSpec(
         "python_modules/libraries/dagster-dbt",
         pytest_extra_cmds=dbt_extra_cmds,
-        # dbt-core no longer supports does not yet support python 3.10
-        unsupported_python_versions=[
-            AvailablePythonVersion.V3_10,
-        ],
     ),
     PackageSpec(
         "python_modules/libraries/dagster-airbyte",
