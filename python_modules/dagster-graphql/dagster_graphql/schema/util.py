f9e0494a51a71b859b5ab67e9da1422f1428c2dc
Sean Mackesey <s.mackesey@gmail.com>
2023-01-27 12:15:17 -0500

    [gql] InputObject alias [INTERNAL_BRANCH=sean/release-pipeline-image]

--- python_modules/dagster-graphql/dagster_graphql/schema/util.py
+++ python_modules/dagster-graphql/dagster_graphql/schema/util.py
@@ -1,8 +1,13 @@
-from typing import cast
+from typing import Any, cast
 
 import graphene
 from dagster._core.workspace.context import WorkspaceRequestContext
-from typing_extensions import Protocol
+from typing_extensions import Protocol, TypeAlias
+
+# Unfortunately Graphene input objects do not play nicely with typing-- we can't type arguments to
+# resolvers with `GrapheneMyInputObjectType` and have it work. We use this `InputObject` type as a
+# stand-in until a better solution is found.
+InputObject: TypeAlias = Any
 
 class ResolveInfo(graphene.ResolveInfo):
     @property
