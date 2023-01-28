b31c4c3a4cd952fec1b7ee991e53cfdd0eecdf9c
Sean Mackesey <s.mackesey@gmail.com>
2023-01-27 19:58:07 -0500

    [pyright] examples/tutorial-notebook-assets (#11910)
    ### Summary & Motivation
    
    Remove dagster version lower bounds in
    `examples/tutorial-notebook-assets` (it is inconsistent with the rest of
    our examples)
    
    ### How I Tested These Changes
    
    BK

--- examples/tutorial_notebook_assets/setup.py
+++ examples/tutorial_notebook_assets/setup.py
@@ -4,8 +4,8 @@ setup(
     name="tutorial_notebook_assets",
     packages=find_packages(exclude=["tutorial_notebook_assets"]),
     install_requires=[
-        "dagster>=1.0.16",
-        "dagstermill>=0.16.16",
+        "dagster",
+        "dagstermill",
         "papermill-origami>=0.0.8",
         "pandas",
         "matplotlib",
