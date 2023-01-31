5de1241a8663f134cec0b9a0210d72f82f17ed2c
Sean Mackesey <s.mackesey@gmail.com>
2023-01-31 17:57:08 -0500

    remove from public API

--- python_modules/dagster/dagster/__init__.py
+++ python_modules/dagster/dagster/__init__.py
@@ -228,9 +228,6 @@ from dagster._core.definitions.multi_dimensional_partitions import (
     MultiPartitionKey as MultiPartitionKey,
     MultiPartitionsDefinition as MultiPartitionsDefinition,
 )
-from dagster._core.definitions.observe import (
-    observe as observe,
-)
 from dagster._core.definitions.op_definition import OpDefinition as OpDefinition
 from dagster._core.definitions.output import (
     DynamicOut as DynamicOut,
