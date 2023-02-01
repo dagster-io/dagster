17d8910e765199db1c7587f6e55018e460600960
Joseph Van Drunen <josephvandrunen@gmail.com>
2023-02-01 11:14:26 -0500

    test fixup

--- python_modules/libraries/dagster-airflow/dagster_airflow_tests/test_dagster_job_factory/test_load_assets_airflow_2.py
+++ python_modules/libraries/dagster-airflow/dagster_airflow_tests/test_dagster_job_factory/test_load_assets_airflow_2.py
@@ -1,40 +1,48 @@
 import imp
 
+import tempfile
+import os
 import pytest
 from airflow import __version__ as airflow_version
+from airflow.models import DagBag
 from dagster import AssetKey, asset, materialize
 from dagster_airflow import (
     load_assets_from_airflow_dag,
 )
 from dagster_airflow_tests.marks import requires_airflow_db
 
-from ..airflow_utils import COMPLEX_DAG_FILE_CONTENTS
+from ..airflow_utils import COMPLEX_DAG_FILE_CONTENTS_AIRFLOW_2
 
 
-@pytest.mark.skipif(airflow_version >= "2.0.0", reason="requires airflow 1")
+@pytest.mark.skipif(airflow_version < "2.0.0", reason="requires airflow 2")
 @requires_airflow_db
 def test_load_assets_from_airflow_dag():
-    dag_module = imp.new_module("dag_module")
-    exec(COMPLEX_DAG_FILE_CONTENTS, dag_module.__dict__)
+    with tempfile.TemporaryDirectory(suffix="assets") as tmpdir_path:
+        with open(os.path.join(tmpdir_path, "dag.py"), "wb") as f:
+            f.write(bytes(COMPLEX_DAG_FILE_CONTENTS_AIRFLOW_2.encode("utf-8")))
 
-    @asset
-    def new_upstream_asset():
-        return 1
+        dag_bag = DagBag(dag_folder=tmpdir_path)
+        complex_dag = dag_bag.get_dag(dag_id="example_complex")
 
-    assets = load_assets_from_airflow_dag(
-        dag=dag_module.complex_dag,
-        task_ids_by_asset_key={
-            AssetKey("foo_asset"): {"create_entry_group_result", "create_entry_group_result2"},
-            AssetKey("bar_asset"): {"create_entry_gcs_result", "create_entry_gcs_result2"},
-        },
-        upstream_dependencies_by_asset_key={
-            AssetKey("foo_asset"): {
-                AssetKey("new_upstream_asset"),
+        @asset
+        def new_upstream_asset():
+            return 1
+
+        assets = load_assets_from_airflow_dag(
+            dag=complex_dag,
+            task_ids_by_asset_key={
+                AssetKey("foo_asset"): {"create_entry_group_result", "create_entry_group_result2"},
+                AssetKey("bar_asset"): {"create_entry_gcs_result", "create_entry_gcs_result2"},
             },
-        },
-    )
+            upstream_dependencies_by_asset_key={
+                AssetKey("foo_asset"): {
+                    AssetKey("new_upstream_asset"),
+                },
+            },
+        )
 
-    result = materialize(
-        [*assets, new_upstream_asset], tags={"airflow_execution_date": "2021-01-01T00:00:00+00:00"}
-    )
-    assert result.success
+        result = materialize(
+            [*assets, new_upstream_asset],
+            partition_key="2023-02-01",
+        )
+        assert result.success
