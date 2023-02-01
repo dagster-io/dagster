47a92723a0402de6ee9531a10c0549c742683f85
Joseph Van Drunen <josephvandrunen@gmail.com>
2023-02-01 01:36:36 -0500

    tests

---
+++ python_modules/libraries/dagster-airflow/dagster_airflow_tests/test_dagster_job_factory/test_load_assets_airflow_2.py
@@ -0,0 +1,36 @@
+import imp
+
+import pytest
+from airflow import __version__ as airflow_version
+from dagster_airflow import (
+    load_assets_from_airflow_dag,
+)
+from dagster import AssetKey, asset, repository, materialize
+
+from ..airflow_utils import COMPLEX_DAG_FILE_CONTENTS
+
+
+@pytest.mark.skipif(airflow_version >= "2.0.0", reason="requires airflow 1")
+def test_asset_apis_airflow_2():
+    dag_module = imp.new_module("dag_module")
+    exec(COMPLEX_DAG_FILE_CONTENTS, dag_module.__dict__)
+
+    @asset
+    def new_upstream_asset():
+        return 1
+
+    assets = load_assets_from_airflow_dag(
+        dag=dag_module.complex_dag,
+        task_ids_by_asset_key={
+            AssetKey("foo_asset"): {"create_entry_group_result", "create_entry_group_result2"},
+            AssetKey("bar_asset"): {"create_entry_gcs_result", "create_entry_gcs_result2"},
+        },
+        upstream_dependencies_by_asset_key={
+            AssetKey("foo_asset"): {
+                AssetKey("new_upstream_asset"),
+            },
+        },
+    )
+
+    result = materialize([*assets, new_upstream_asset])
+    assert result.success
