7a1c5a85118b39ce4fb1abfc1668d3d3fe288cfc
JamieDeMaria <jamie@elementl.com>
2023-02-01 18:06:12 -0500

    debuggin

--- examples/tutorial_dbt_dagster/tutorial_finished/tutorial_dbt_dagster/assets/__init__.py
+++ examples/tutorial_dbt_dagster/tutorial_finished/tutorial_dbt_dagster/assets/__init__.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import plotly.express as px
-from dagster import AssetIn, MetadataValue, asset, file_relative_path
+from dagster import AssetIn, MetadataValue, asset, file_relative_path, define_asset_job
 from dagster_dbt import load_assets_from_dbt_project
 
 
-@asset(key_prefix=["jaffle_shop"], group_name="staging")
+@asset(key_prefix=["jaffle_shop"], group_name="staging", io_manager_key="duckdb")
 def customers_raw() -> pd.DataFrame:
     data = pd.read_csv("https://docs.dagster.io/assets/customers.csv")
     return data
 
 
-@asset(key_prefix=["jaffle_shop"], group_name="staging")
+@asset(key_prefix=["jaffle_shop"], group_name="staging", io_manager_key="duckdb")
 def orders_raw() -> pd.DataFrame:
     data = pd.read_csv("https://docs.dagster.io/assets/orders.csv")
     return data
@@ -26,11 +26,14 @@ dbt_assets = load_assets_from_dbt_project(
 )
 
 
-@asset(ins={"customers": AssetIn(key_prefix=["jaffle_shop"])}, group_name="staging")
-def order_count_chart(context, customers: pd.DataFrame):
+@asset(ins={"customers": AssetIn(key_prefix=["jaffle_shop"], input_manager_key="duckdb")}, group_name="staging", io_manager_key="duckdb")
+def order_count_chart(context, customers: pd.DataFrame) -> None:
     fig = px.histogram(customers, x="number_of_orders")
     fig.update_layout(bargap=0.2)
     save_chart_path = file_relative_path(__file__, "order_count_chart.html")
     fig.write_html(save_chart_path, auto_open=True)
 
     context.add_output_metadata({"plot_url": MetadataValue.url("file://" + save_chart_path)})
+
+
+all_assets_job = define_asset_job(name="all_assets_job")
--- examples/tutorial_dbt_dagster/tutorial_finished/tutorial_dbt_dagster/repository.py
+++ examples/tutorial_dbt_dagster/tutorial_finished/tutorial_dbt_dagster/repository.py
@@ -1,26 +1,29 @@
 import os
 
-from dagster import load_assets_from_package_module, repository, with_resources
 from dagster_dbt import dbt_cli_resource
 from dagster_duckdb_pandas import duckdb_pandas_io_manager
-
 from tutorial_dbt_dagster import assets
-from tutorial_dbt_dagster.assets import DBT_PROFILES, DBT_PROJECT_PATH
+from tutorial_dbt_dagster.assets import DBT_PROFILES, DBT_PROJECT_PATH, all_assets_job
+
+from dagster import load_assets_from_package_module, repository, with_resources
 
 
 @repository
 def tutorial_dbt_dagster():
-    return with_resources(
-        load_assets_from_package_module(assets),
-        {
-            "dbt": dbt_cli_resource.configured(
-                {
-                    "project_dir": DBT_PROJECT_PATH,
-                    "profiles_dir": DBT_PROFILES,
-                },
-            ),
-            "io_manager": duckdb_pandas_io_manager.configured(
-                {"database": os.path.join(DBT_PROJECT_PATH, "tutorial.duckdb")}
-            ),
-        },
-    )
+    return [
+        *with_resources(
+            load_assets_from_package_module(assets),
+            {
+                "dbt": dbt_cli_resource.configured(
+                    {
+                        "project_dir": DBT_PROJECT_PATH,
+                        "profiles_dir": DBT_PROFILES,
+                    },
+                ),
+                "duckdb": duckdb_pandas_io_manager.configured(
+                    {"database": os.path.join(DBT_PROJECT_PATH, "tutorial.duckdb")}
+                ),
+            },
+        ),
+        all_assets_job,
+    ]
