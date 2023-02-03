5533d0ff949ac166899fbac1a852de5af0baf990
Ben Pankow <ben@elementl.com>
2023-02-03 17:05:54 -0600

    [telemetry] fix setting __TELEMETRY_ENABLED__ flag on Dagit (#12092)
    ## Summary
    
    Noticed while trying to add another python-populated Dagit flag.
    
    The `"__TELEMETRY_ENABLED__"` placeholder string was being set to a
    string value `"True"` or `"False`", which the JS does not convert into a
    boolean value. This PR fixes the templating by replacing the entire
    placeholder string with a literal
    `true` or `false `JS bool.

--- python_modules/dagit/dagit/webserver.py
+++ python_modules/dagit/dagit/webserver.py
@@ -211,7 +211,9 @@ class DagitWebserver(GraphQLServer, Generic[T_IWorkspaceProcessContext]):
                     rendered_template.replace('href="/', f'href="{self._app_path_prefix}/')
                     .replace('src="/', f'src="{self._app_path_prefix}/')
                     .replace("__PATH_PREFIX__", self._app_path_prefix)
-                    .replace("__TELEMETRY_ENABLED__", str(context.instance.telemetry_enabled))
+                    .replace(
+                        '"__TELEMETRY_ENABLED__"', str(context.instance.telemetry_enabled).lower()
+                    )
                     .replace("NONCE-PLACEHOLDER", nonce),
                     headers=headers,
                 )
