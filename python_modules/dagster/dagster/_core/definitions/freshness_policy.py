d8ed1cf701f5083d888e9e21c699934dedf35bca
benpankow <ben@elementl.com>
2023-02-06 16:01:41 -0800

    Fix serdes on FreshnessPolicy object

--- python_modules/dagster/dagster/_core/definitions/freshness_policy.py
+++ python_modules/dagster/dagster/_core/definitions/freshness_policy.py
@@ -77,7 +77,7 @@ class FreshnessPolicy(
 
     """
 
-    def __new__(cls, *, maximum_lag_minutes: float, cron_schedule: Optional[str] = None):
+    def __new__(cls, maximum_lag_minutes: float, cron_schedule: Optional[str] = None):
         return super(FreshnessPolicy, cls).__new__(
             cls,
             maximum_lag_minutes=float(
