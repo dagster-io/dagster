670cc6e4682a8a359dacb27723034750685da209
Owen Kephart <owen@elementl.com>
2023-02-08 09:34:45 -0800

    update comment

--- .buildkite/dagster-buildkite/dagster_buildkite/steps/packages.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/packages.py
@@ -366,7 +366,7 @@ LIBRARY_PACKAGES_WITH_CUSTOM_CONFIG: List[PackageSpec] = [
     PackageSpec(
         "python_modules/libraries/dagster-dbt",
         pytest_extra_cmds=dbt_extra_cmds,
-        # dbt-core no longer supports does not yet support python 3.10
+        # dbt-core does not yet support python 3.10
         unsupported_python_versions=[
             AvailablePythonVersion.V3_10,
         ],
