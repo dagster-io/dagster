34d6da3e760d42a9e4a8700bb9f60bbfb7c51ac5
Joseph Van Drunen <josephvandrunen@gmail.com>
2023-02-08 20:19:22 -0500

    lint

--- examples/docs_snippets/docs_snippets/integrations/airflow/migrate_repo_connections.py
+++ examples/docs_snippets/docs_snippets/integrations/airflow/migrate_repo_connections.py
@@ -5,5 +5,7 @@ from dagster_airflow import make_dagster_definitions_from_airflow_dags_path
 
 migrated_airflow_definitions = make_dagster_definitions_from_airflow_dags_path(
     os.path.abspath("./dags/"),
-    connections=[Connection(conn_id="http_default", conn_type="uri", host="https://google.com")],
+    connections=[
+        Connection(conn_id="http_default", conn_type="uri", host="https://google.com")
+    ],
 )
