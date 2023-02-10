a22f8b75e157786e286c6629e080e5aadd564d42
Owen Kephart <owen@elementl.com>
2023-02-10 14:35:22 -0800

    update mocks

--- js_modules/dagit/packages/core/src/assets/AssetsCatalogTable.mocks.ts
+++ js_modules/dagit/packages/core/src/assets/AssetsCatalogTable.mocks.ts
@@ -180,6 +180,7 @@ export const SingleAssetQueryMaterializedStaleAndLate: MockedResponse<SingleAsse
           freshnessPolicy: {
             maximumLagMinutes: 2,
             cronSchedule: null,
+            scheduleTimezone: null,
             __typename: 'FreshnessPolicy',
           },
           freshnessInfo: {
