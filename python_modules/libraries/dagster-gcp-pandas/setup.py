656d35ab6e0f36ad00db8489108368ccecbe4c89
JamieDeMaria <jamie@elementl.com>
2023-02-16 14:11:45 -0500

    put dagster-pandas in tox

--- python_modules/libraries/dagster-gcp-pandas/setup.py
+++ python_modules/libraries/dagster-gcp-pandas/setup.py
@@ -33,6 +33,6 @@ setup(
     ],
     packages=find_packages(exclude=["dagster_gcp_pandas_tests*"]),
     include_package_data=True,
-    install_requires=[f"dagster{pin}", f"dagster-gcp{pin}", "pandas", "pandas-gbq"],
+    install_requires=[f"dagster{pin}", f"dagster-gcp{pin}", "pandas-gbq"],
     zip_safe=False,
 )
