3af8cf374403a2d45c4434c2c72c3bdfe9da10e6
Sandy Ryza <sandy@elementl.com>
2023-02-16 09:35:38 -0800

    guide to how assets relate to ops and graphs (#12204)
    ### Summary & Motivation
    
    New users often express confusion about how assets and ops relate and
    when to use one vs. the other. There's arguably more to say on this
    topic, but I wanted to do my best to lay out the basics in the
    somewhat-limited time I have to work on this.
    
    I don't think I have the bandwidth to do any big structural revisions,
    but of course open to any and all feedback on how to polish it.
    
    This meant to be kind of an updated version of the [Enriching with
    Software-Defined
    Assets](https://docs.dagster.io/guides/dagster/enriching-with-software-defined-assets#upgrading-to-software-defined-assets)
    page, but targeted at new users instead of users who are using Dagster
    with the pre-asset APIs.
    
    Preview:
    https://dagster-git-assets-relate-to-ops-elementl.vercel.app/guides/dagster/how-assets-relate-to-ops-and-graphs
    
    ### How I Tested These Changes
    
    ---------
    
    Co-authored-by: Erin Cochran <erin.k.cochran@gmail.com>

---
+++ examples/docs_snippets/docs_snippets/guides/dagster/assets_ops_graphs/op_graph_asset_input.py
@@ -0,0 +1,16 @@
+from dagster import asset, job, op
+
+
+@asset
+def emails_to_send():
+    ...
+
+
+@op
+def send_emails(emails) -> None:
+    ...
+
+
+@job
+def send_emails_job():
+    send_emails(emails_to_send.to_source_asset())
