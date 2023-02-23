8c9f54a2b4c63aa156d1aa328deecf8df90dac27
elementl <devtools@elementl.com>
2023-02-23 22:57:51 +0000

    1.1.20

--- python_modules/libraries/dagster-datahub/setup.py
+++ python_modules/libraries/dagster-datahub/setup.py
@@ -37,7 +37,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "acryl-datahub[datahub-rest, datahub-kafka]<0.8.41.2",
-        f"dagster{pin}",
+        "dagster==1.1.20",
         "packaging",
         "requests",
     ],
--- python_modules/libraries/dagster-duckdb-pandas/setup.py
+++ python_modules/libraries/dagster-duckdb-pandas/setup.py
@@ -34,8 +34,8 @@ setup(
     packages=find_packages(exclude=["dagster_duckdb_pandas_tests*"]),
     include_package_data=True,
     install_requires=[
-        f"dagster{pin}",
-        f"dagster-duckdb{pin}",
+        "dagster==1.1.20",
+        "dagster-duckdb==0.17.20",
         "pandas",
     ],
     zip_safe=False,
