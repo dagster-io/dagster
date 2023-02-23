4615ff832802bb78564b0f7d4fc41d17fad61845
Sean Han <xhan1@flexport.com>
2023-02-23 10:21:12 -0800

    Typo

--- python_modules/dagster/dagster/_core/definitions/time_window_partition_mapping.py
+++ python_modules/dagster/dagster/_core/definitions/time_window_partition_mapping.py
@@ -161,7 +161,7 @@ class TimeWindowPartitionMapping(
         if start_offset != 0 or end_offset != 0:
             check.invariant(
                 from_partitions_def.cron_schedule == to_partitions_def.cron_schedule,
-                "Offset can't be used when cron schedule don't match",
+                "Offset can't be used when cron schedules don't match",
             )
 
         if to_partitions_def.timezone != from_partitions_def.timezone:
