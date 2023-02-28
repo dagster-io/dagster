1c3633c587b6f1e5837f842f5addb52eb3f45fec
Marco polo <salazarm@users.noreply.github.com>
2023-02-28 06:39:09 -0500

    Storybook for LaunchAssetChoosePartitionsDialog (#12567)
    ### Summary & Motivation
    
    Adds a storybook with 2 examples. An empty state, and one with "ordinal"
    partitions. I need to add an example for a timeseries based one once I'm
    able to load one.
    
    ### How I Tested These Changes
    
    Storybook

---
+++ js_modules/dagit/packages/core/src/assets/__fixtures__/RunningBackfillsNoticeQuery.fixture.tsx
@@ -0,0 +1,20 @@
+import {MockedResponse} from '@apollo/client/testing';
+
+import {RUNNING_BACKFILLS_NOTICE_QUERY} from '../RunningBackfillsNotice';
+import {RunningBackfillsNoticeQuery} from '../types/RunningBackfillsNotice.types';
+
+export const NoRunningBackfills: MockedResponse<RunningBackfillsNoticeQuery> = {
+  request: {
+    query: RUNNING_BACKFILLS_NOTICE_QUERY,
+    variables: {},
+  },
+  result: {
+    data: {
+      __typename: 'DagitQuery',
+      partitionBackfillsOrError: {
+        __typename: 'PartitionBackfills',
+        results: [],
+      },
+    },
+  },
+};
