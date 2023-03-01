c187ee0a414a448f4c3d896a88e6d384ddb8b19b
odette-elementl <odette@elementl.com>
2023-03-01 09:01:32 -0500

    updating to fix dbt assets

--- examples/docs_snippets/docs_snippets/guides/dagster/automating_pipelines/airbyte_dbt_sensor.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/automating_pipelines/airbyte_dbt_sensor.py
@@ -32,7 +32,7 @@ defs = Definitions(
     assets=[airbyte_asset1, airbyte_asset2, dbt_asset1, dbt_asset2],
     sensors=[
         build_asset_reconciliation_sensor(
-            asset_selection=AssetSelection.keys("dbt_asset1", "dbt_asset2"),
+            asset_selection=AssetSelection.assets(dbt_asset1, dbt_asset2),
             name="asset_reconciliation_sensor",
         ),
     ],
