be945f065dd0b8de7ffc346d32ef83136cde6869
Marco Salazar <marco@elementl.com>
2023-03-01 11:29:45 -0500

    regenerate grpahql

--- js_modules/dagit/packages/core/src/asset-graph/types/useLiveDataForAssetKeys.types.ts
+++ js_modules/dagit/packages/core/src/asset-graph/types/useLiveDataForAssetKeys.types.ts
@@ -34,11 +34,6 @@ export type AssetGraphLiveQuery = {
     opNames: Array<string>;
     currentLogicalVersion: string | null;
     projectedLogicalVersion: string | null;
-    type:
-      | {__typename: 'ListDagsterType'; name: string | null}
-      | {__typename: 'NullableDagsterType'; name: string | null}
-      | {__typename: 'RegularDagsterType'; name: string | null}
-      | null;
     repository: {__typename: 'Repository'; id: string};
     assetKey: {__typename: 'AssetKey'; path: Array<string>};
     assetMaterializations: Array<{
