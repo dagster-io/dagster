418468d78e6a27d6c5f909e5a07e401d77c0015a
Sandy Ryza <sandy@elementl.com>
2023-03-09 10:12:15 -0800

    nothing input toy

--- python_modules/dagster-test/dagster_test/toys/repo.py
+++ python_modules/dagster-test/dagster_test/toys/repo.py
@@ -33,6 +33,7 @@ from dagster_test.toys.many_events import many_events, many_events_subset_job
 from dagster_test.toys.metadata import with_metadata
 from dagster_test.toys.multi_inputs_outputs import multi_inputs_outputs_job
 from dagster_test.toys.notebooks import hello_world_notebook_pipeline
+from dagster_test.toys.nothing_input import nothing_job
 from dagster_test.toys.retries import retry_job
 from dagster_test.toys.run_status_sensors import (
     cross_repo_job_sensor,
@@ -87,6 +88,7 @@ def toys_repository():
             longitudinal_job,
             many_events,
             many_events_subset_job,
+            nothing_job,
             sleepy_job,
             retry_job,
             branch_job,
