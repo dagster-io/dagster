d72d36ff5406c4299be6c01f8a6e049e42262fe6
Sean Mackesey <s.mackesey@gmail.com>
2023-03-10 05:21:05 -0500

    Update pyright 1.1.293 -> 1.1.298

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -129,7 +129,7 @@ setup(
             "mypy==0.991",
         ],
         "pyright": [
-            "pyright==1.1.293",
+            "pyright==1.1.298",
             ### Stub packages
             "pandas-stubs",  # version will be resolved against pandas
             "types-backports",  # version will be resolved against backports
