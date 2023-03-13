4b1eb52e9d51a70682ec4b8929e247eda8436031
Chris DeCarolis <cdguitar817@gmail.com>
2023-03-13 09:29:53 -0600

    Remove gating of run monitoring on run launcher type, and instead gate specific monitor behavior on run launcher type

--- python_modules/dagster/dagster/_daemon/monitoring/monitoring_daemon.py
+++ python_modules/dagster/dagster/_daemon/monitoring/monitoring_daemon.py
@@ -32,7 +32,10 @@ def monitor_starting_run(
     launch_time = check.not_none(
         run_stats.launch_time, "Run in status STARTING doesn't have a launch time."
     )
-    if time.time() - launch_time >= instance.run_monitoring_start_timeout_seconds:
+    if (
+        instance.run_launcher.supports_check_run_worker_health
+        and time.time() - launch_time >= instance.run_monitoring_start_timeout_seconds
+    ):
         msg = (
             f"Run {run.run_id} has been running for {time.time() - launch_time} seconds, which is"
             f" longer than the timeout of {instance.run_monitoring_start_timeout_seconds} seconds"
@@ -57,46 +60,48 @@ def monitor_started_run(
 ) -> None:
     run = run_record.dagster_run
     check.invariant(run.status == DagsterRunStatus.STARTED)
-    instance.handle_started_run(run_record, logger)
-    check_health_result = instance.run_launcher.check_run_worker_health(run)
-    if check_health_result.status not in [WorkerStatus.RUNNING, WorkerStatus.SUCCESS]:
-        num_prev_attempts = count_resume_run_attempts(instance, run.run_id)
-        recheck_run = check.not_none(instance.get_run_by_id(run.run_id))
-        status_changed = run.status != recheck_run.status
-        if status_changed:
-            msg = (
-                "Detected run status changed during monitoring loop: "
-                f"{run.status} -> {recheck_run.status}, disregarding for now"
-            )
-            logger.info(msg)
-            return
-        if num_prev_attempts < instance.run_monitoring_max_resume_run_attempts:
-            msg = (
-                f"Detected run worker status {check_health_result}. Resuming run {run.run_id} with "
-                "a new worker."
-            )
-            logger.info(msg)
-            instance.report_engine_event(msg, run)
-            attempt_number = num_prev_attempts + 1
-            instance.resume_run(
-                run.run_id,
-                workspace,
-                attempt_number,
-            )
-        else:
-            if instance.run_launcher.supports_resume_run:
+    if instance.run_launcher.supports_check_run_worker_health:
+        check_health_result = instance.run_launcher.check_run_worker_health(run)
+        if check_health_result.status not in [WorkerStatus.RUNNING, WorkerStatus.SUCCESS]:
+            num_prev_attempts = count_resume_run_attempts(instance, run.run_id)
+            recheck_run = check.not_none(instance.get_run_by_id(run.run_id))
+            status_changed = run.status != recheck_run.status
+            if status_changed:
                 msg = (
-                    f"Detected run worker status {check_health_result}. Marking run {run.run_id} as"
-                    " failed, because it has surpassed the configured maximum attempts to resume"
-                    f" the run: {instance.run_monitoring_max_resume_run_attempts}."
+                    "Detected run status changed during monitoring loop: "
+                    f"{run.status} -> {recheck_run.status}, disregarding for now"
+                )
+                logger.info(msg)
+                return
+            if num_prev_attempts < instance.run_monitoring_max_resume_run_attempts:
+                msg = (
+                    f"Detected run worker status {check_health_result}. Resuming run"
+                    f" {run.run_id} with a new worker."
+                )
+                logger.info(msg)
+                instance.report_engine_event(msg, run)
+                attempt_number = num_prev_attempts + 1
+                instance.resume_run(
+                    run.run_id,
+                    workspace,
+                    attempt_number,
                 )
             else:
-                msg = (
-                    f"Detected run worker status {check_health_result}. Marking run {run.run_id} as"
-                    " failed."
-                )
-            logger.info(msg)
-            instance.report_run_failed(run, msg)
+                if instance.run_launcher.supports_resume_run:
+                    msg = (
+                        f"Detected run worker status {check_health_result}. Marking run"
+                        f" {run.run_id} as failed, because it has surpassed the configured maximum"
+                        " attempts to resume the run:"
+                        f" {instance.run_monitoring_max_resume_run_attempts}."
+                    )
+                else:
+                    msg = (
+                        f"Detected run worker status {check_health_result}. Marking run"
+                        f" {run.run_id} as failed."
+                    )
+                logger.info(msg)
+                instance.report_run_failed(run, msg)
+    instance.handle_started_run(run_record, logger)
 
 
 def execute_monitoring_iteration(
@@ -105,9 +110,6 @@ def execute_monitoring_iteration(
     _debug_crash_flags: Optional[DebugCrashFlags] = None,
 ) -> Iterator[Optional[SerializableErrorInfo]]:
     instance = workspace_process_context.instance
-    check.invariant(
-        instance.run_launcher.supports_check_run_worker_health, "Must use a supported run launcher"
-    )
 
     # TODO: consider limiting number of runs to fetch
     run_records = list(
@@ -134,7 +136,9 @@ def execute_monitoring_iteration(
                 check.invariant(False, f"Unexpected run status: {run_record.dagster_run.status}")
         except Exception:
             error_info = serializable_error_info_from_exc_info(sys.exc_info())
-            logger.error(f"Hit error while monitoring run {run_record.dagster_run.run_id}: {str(error_info)}")
+            logger.error(
+                f"Hit error while monitoring run {run_record.dagster_run.run_id}: {str(error_info)}"
+            )
             yield error_info
         else:
             yield
