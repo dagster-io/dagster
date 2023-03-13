36a41927041732e5d13554a0e27f78a98bbe4eca
Sandy Ryza <sandy@elementl.com>
2023-03-13 10:16:57 -0700

    remove support for legacy APIs from validate_run_config (#12881)
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- python_modules/dagster/dagster/_core/execution/validate_run_config.py
+++ python_modules/dagster/dagster/_core/execution/validate_run_config.py
@@ -1,49 +1,31 @@
-from typing import Any, Mapping, Optional, cast
+from typing import Any, Mapping, Optional
 
 import dagster._check as check
-from dagster._core.definitions import JobDefinition, PipelineDefinition
+from dagster._core.definitions import JobDefinition
 from dagster._core.definitions.run_config import convert_config_input
 from dagster._core.system_config.objects import ResolvedRunConfig
 
 
 def validate_run_config(
-    job_def: Optional[JobDefinition] = None,
+    job_def: JobDefinition,
     run_config: Optional[Mapping[str, Any]] = None,
-    mode: Optional[str] = None,
-    pipeline_def: Optional[PipelineDefinition] = None,
 ) -> Mapping[str, Any]:
-    """Function to validate a provided run config blob against a given job. For legacy APIs, a
-    pipeline/mode can also be passed in.
+    """Function to validate a provided run config blob against a given job.
 
     If validation is successful, this function will return a dictionary representation of the
     validated config actually used during execution.
 
     Args:
-        job_def (Union[PipelineDefinition, JobDefinition]): The job definition to validate run
+        job_def (JobDefinition): The job definition to validate run
             config against
         run_config (Optional[Dict[str, Any]]): The run config to validate
-        mode (str): The mode of the pipeline to validate against (different modes may require
-            different config)
-        pipeline_def (PipelineDefinition): The pipeline definition to validate run config against.
 
     Returns:
         Dict[str, Any]: A dictionary representation of the validated config.
     """
-    job_def = check.opt_inst_param(job_def, "job_def", (JobDefinition, PipelineDefinition))
-    pipeline_def = check.opt_inst_param(pipeline_def, "pipeline_def", PipelineDefinition)
+    check.inst_param(job_def, "job_def", JobDefinition)
     run_config = check.opt_mapping_param(
         convert_config_input(run_config), "run_config", key_type=str
     )
 
-    if job_def and pipeline_def:
-        check.failed("Cannot specify both a job_def and a pipeline_def")
-
-    pipeline_or_job_def = pipeline_def or job_def
-
-    if pipeline_or_job_def is None:
-        check.failed("Must specify at least one of job_def and pipeline_def")
-
-    pipeline_or_job_def = cast(PipelineDefinition, pipeline_def or job_def)
-    mode = check.opt_str_param(mode, "mode", default=pipeline_or_job_def.get_default_mode_name())
-
-    return ResolvedRunConfig.build(pipeline_or_job_def, run_config, mode=mode).to_dict()
+    return ResolvedRunConfig.build(job_def, run_config).to_dict()
