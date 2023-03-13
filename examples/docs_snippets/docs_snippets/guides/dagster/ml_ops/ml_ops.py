226ee897768ea4f79e29933adf709230a64adcc3
odette-elementl <odette@elementl.com>
2023-03-13 17:29:37 -0400

    yarn test and mdx

--- examples/docs_snippets/docs_snippets/guides/dagster/ml_ops/ml_ops.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/ml_ops/ml_ops.py
@@ -1,25 +1,39 @@
 import pandas as pd
 import requests
 import numpy as np
-from dagster import MetadataValue, Output, asset, Definitions, DynamicPartitionsDefinition, Output, FreshnessPolicy
+from dagster import (
+    MetadataValue,
+    Output,
+    asset,
+    Definitions,
+    DynamicPartitionsDefinition,
+    Output,
+    FreshnessPolicy,
+)
 import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import linear_model
 
+
 @asset
 def previous_item():
-    previous_item=requests.get(f"https://hacker-news.firebaseio.com/v0/maxitem.json").json()
+    previous_item = requests.get(
+        f"https://hacker-news.firebaseio.com/v0/maxitem.json"
+    ).json()
     return previous_item
 
 
 ## data_ingestion_start
 
+
 @asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=10))
 def hackernews_stories(previous_item):
-    """Get the max ID number from hacker news """
+    """Get the max ID number from hacker news"""
 
-    latest_item = requests.get(f"https://hacker-news.firebaseio.com/v0/maxitem.json").json()
+    latest_item = requests.get(
+        f"https://hacker-news.firebaseio.com/v0/maxitem.json"
+    ).json()
 
     """Get items based on story ids from the HackerNews items endpoint"""
     results = []
@@ -31,16 +45,17 @@ def hackernews_stories(previous_item):
         results.append(item)
 
     df = pd.DataFrame(results)
-    if len(df)>0:
+    if len(df) > 0:
+        df = df[df.type == "story"]
+        df = df[~df.title.isna()]
 
-      df = df[df.type=='story']
-      df = df[~df.title.isna()]
-    
     return df, latest_item
+
 
 ## data_ingestion_end
 
 ## test_train_split_start
+
 
 @asset
 def test_train_split(hackernews_stories):
@@ -50,7 +65,9 @@ def test_train_split(hackernews_stories):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     return X_train, X_test, y_train, y_test
 
+
 ## test_train_split_end
+
 
 ## vectorizer_start
 @asset
@@ -64,6 +81,7 @@ def transform_train(test_train_split):
 
     return vectorizer, transformed_X_train, transformed_y_train
 
+
 @asset
 def transform_test(test_train_split, transform_train):
     X_train, X_test, y_train, y_test = test_train_split
@@ -73,42 +91,49 @@ def transform_test(test_train_split, transform_train):
     y_test = y_test.fillna(0)
     transformed_y_test = np.array(y_test)
     return transformed_X_test, transformed_y_test
+
+
 ## vectorizer_end
 
 ## models_start
 
 from sklearn.tree import DecisionTreeRegressor
 
+
 @asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=1440))
 def decision_tree(transform_train, transform_test):
-    vectorizer, transformed_X_train, transformed_y_train = transform_train 
+    vectorizer, transformed_X_train, transformed_y_train = transform_train
     decision_tree1 = DecisionTreeRegressor(max_depth=10)
     decision_tree1.fit(transformed_X_train, transformed_y_train)
     transformed_X_test, transformed_y_test = transform_test
     score = decision_tree1.score(transformed_X_test, transformed_y_test)
     return decision_tree1, decision_tree1.score(transformed_X_test, transformed_y_test)
 
+
 import xgboost as xg
 from sklearn.metrics import mean_absolute_error
 
+
 @asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=1440))
 def xgboost(transform_train, transform_test):
-    vectorizer, transformed_X_train, transformed_y_train = transform_train 
-    xgb_r = xg.XGBRegressor(objective ='reg:squarederror', eval_metric=mean_absolute_error,
-                  n_estimators = 20)
+    vectorizer, transformed_X_train, transformed_y_train = transform_train
+    xgb_r = xg.XGBRegressor(
+        objective="reg:squarederror", eval_metric=mean_absolute_error, n_estimators=20
+    )
     xgb_r.fit(transformed_X_train, transformed_y_train)
     transformed_X_test, transformed_y_test = transform_test
     score = xgb_r.score(transformed_X_test, transformed_y_test)
-    return xgb_r, score 
+    return xgb_r, score
+
 
 ## models_end
 
-@asset 
+
+@asset
 def reg_model(transform_train, transform_test):
-    vectorizer, transformed_X_train, transformed_y_train = transform_train 
+    vectorizer, transformed_X_train, transformed_y_train = transform_train
     reg = linear_model.LinearRegression()
     reg.fit(transformed_X_train, transformed_y_train)
     transformed_X_test, transformed_y_test = transform_test
     score = reg.score(transformed_X_test, transformed_y_test)
     return reg, score
-
