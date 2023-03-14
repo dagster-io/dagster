cc4d94012aeb5e90240e6ef4215dcd2b8487b40f
benpankow <ben@elementl.com>
2023-03-14 11:15:45 -0700

    Revert "fix test"
    This reverts commit 0fae5277f5367bc25d4a634bce405b0625385b00.

--- python_modules/dagster/dagster_tests/core_tests/resource_tests/test_required_resources.py
+++ python_modules/dagster/dagster_tests/core_tests/resource_tests/test_required_resources.py
@@ -448,6 +448,10 @@ def test_type_missing_resource_fails():
         def _type_check_job():
             custom_type_op()
 
+        @repository
+        def _repo():
+            return [_type_check_job]
+
 
 def test_loader_missing_resource_fails():
     @dagster_type_loader(String, required_resource_keys={"a"})
@@ -471,6 +475,10 @@ def test_loader_missing_resource_fails():
         @job
         def _type_check_job():
             custom_type_op()
+
+        @repository
+        def _repo():
+            return [_type_check_job]
 
 
 def test_extra_configured_resources():
@@ -537,6 +545,10 @@ def test_root_input_manager_missing_fails():
         def _invalid():
             requires_missing_root_input_manager()
 
+        @repository
+        def _repo():
+            return [_invalid]
+
 
 def test_io_manager_missing_fails():
     @op(out={"result": Out(int, io_manager_key="missing_io_manager")})
@@ -554,3 +566,7 @@ def test_io_manager_missing_fails():
         @job
         def _invalid():
             requires_missing_io_manager()
+
+        @repository
+        def _repo():
+            return [_invalid]
