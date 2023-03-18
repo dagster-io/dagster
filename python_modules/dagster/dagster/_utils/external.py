243144cdd8c20333570c4eef74230b7e19f3117d
Sean Mackesey <s.mackesey@gmail.com>
2023-03-18 15:53:41 -0400

    [refactor] _utils.external.external_pipeline_from_location(..., repo_location, ...) -> code_location

--- python_modules/dagster/dagster/_utils/external.py
+++ python_modules/dagster/dagster/_utils/external.py
@@ -8,29 +8,29 @@ from dagster._core.host_representation.origin import ExternalPipelineOrigin
 
 
 def external_pipeline_from_location(
-    repo_location: CodeLocation,
+    code_location: CodeLocation,
     external_pipeline_origin: ExternalPipelineOrigin,
     solid_selection: Optional[Sequence[str]],
 ) -> ExternalPipeline:
-    check.inst_param(repo_location, "repository_location", CodeLocation)
+    check.inst_param(code_location, "repository_location", CodeLocation)
     check.inst_param(external_pipeline_origin, "external_pipeline_origin", ExternalPipelineOrigin)
 
     repo_name = external_pipeline_origin.external_repository_origin.repository_name
     pipeline_name = external_pipeline_origin.pipeline_name
 
     check.invariant(
-        repo_location.has_repository(repo_name),
+        code_location.has_repository(repo_name),
         "Could not find repository {repo_name} in location {repo_location_name}".format(
-            repo_name=repo_name, repo_location_name=repo_location.name
+            repo_name=repo_name, repo_location_name=code_location.name
         ),
     )
-    external_repo = repo_location.get_repository(repo_name)
+    external_repo = code_location.get_repository(repo_name)
 
     pipeline_selector = PipelineSelector(
-        location_name=repo_location.name,
+        location_name=code_location.name,
         repository_name=external_repo.name,
         pipeline_name=pipeline_name,
         solid_selection=solid_selection,
     )
 
-    return repo_location.get_external_pipeline(pipeline_selector)
+    return code_location.get_external_pipeline(pipeline_selector)
