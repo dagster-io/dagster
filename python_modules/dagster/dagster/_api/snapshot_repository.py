1964c67be394a5870103b395a620fc13ea287126
Sean Mackesey <s.mackesey@gmail.com>
2023-03-18 17:09:45 -0400

    [refactor] _api.snapshot_repository(..., repository_location, ...) -> code_location

--- python_modules/dagster/dagster/_api/snapshot_repository.py
+++ python_modules/dagster/dagster/_api/snapshot_repository.py
@@ -14,18 +14,18 @@ if TYPE_CHECKING:
 
 
 def sync_get_streaming_external_repositories_data_grpc(
-    api_client: "DagsterGrpcClient", repository_location: "CodeLocation"
+    api_client: "DagsterGrpcClient", code_location: "CodeLocation"
 ) -> Mapping[str, ExternalRepositoryData]:
     from dagster._core.host_representation import CodeLocation, ExternalRepositoryOrigin
 
-    check.inst_param(repository_location, "repository_location", CodeLocation)
+    check.inst_param(code_location, "code_location", CodeLocation)
 
     repo_datas = {}
-    for repository_name in repository_location.repository_names:  # type: ignore
+    for repository_name in code_location.repository_names:  # type: ignore
         external_repository_chunks = list(
             api_client.streaming_external_repository(
                 external_repository_origin=ExternalRepositoryOrigin(
-                    repository_location.origin,
+                    code_location.origin,
                     repository_name,
                 )
             )
