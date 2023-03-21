10917fb45f1f232de2b188c075f1484c1de4caa9
odette-elementl <odette@elementl.com>
2023-03-21 16:42:36 -0400

    updates

--- examples/docs_snippets/docs_snippets/guides/dagster/ml_ops/ml_ops.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/ml_ops/ml_ops.py
@@ -1,8 +1,8 @@
-
 ## data_ingestion_start
 import requests
 from dagster import asset, FreshnessPolicy
 import pandas as pd
+
 
 @asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=10))
 def hackernews_stories():
@@ -12,7 +12,7 @@ def hackernews_stories():
     ).json()
     # Get items based on story ids from the HackerNews items endpoint
     results = []
-    scope = range(latest_item-1000, latest_item)
+    scope = range(latest_item - 1000, latest_item)
     for item_id in scope:
         item = requests.get(
             f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
@@ -34,14 +34,15 @@ def hackernews_stories():
 from sklearn.model_selection import train_test_split
 from dagster import multi_asset, AssetOut
 
-@multi_asset(outs={'training_data': AssetOut(), 'test_data': AssetOut()})
+
+@multi_asset(outs={"training_data": AssetOut(), "test_data": AssetOut()})
 def training_test_data(hackernews_stories):
     hackernews_stories = hackernews_stories
     X = hackernews_stories.title
     y = hackernews_stories.descendants
     # Split the dataset to reserve 20% of records as the test set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    return (X_train,  y_train), (X_test, y_test)
+    return (X_train, y_train), (X_test, y_test)
 
 
 ## test_train_split_end
@@ -51,9 +52,12 @@ def training_test_data(hackernews_stories):
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 
-@multi_asset(outs={'tfidf_Vectorizer': AssetOut(), 'transformed_training_data': AssetOut()})
+
+@multi_asset(
+    outs={"tfidf_Vectorizer": AssetOut(), "transformed_training_data": AssetOut()}
+)
 def transformed_train_data(training_data):
-    X_train,  y_train = training_data
+    X_train, y_train = training_data
     # Initiate and fit the tokenizer on the training data and transform the training dataset
     vectorizer = TfidfVectorizer()
     transformed_X_train = vectorizer.fit_transform(X_train)
@@ -61,6 +65,7 @@ def transformed_train_data(training_data):
     y_train = y_train.fillna(0)
     transformed_y_train = np.array(y_train)
     return vectorizer, (transformed_X_train, transformed_y_train)
+
 
 @asset
 def transformed_test_data(test_data, tfidf_Vectorizer):
@@ -71,6 +76,8 @@ def transformed_test_data(test_data, tfidf_Vectorizer):
     y_test = y_test.fillna(0)
     transformed_y_test = np.array(y_test)
     return transformed_X_test, transformed_y_test
+
+
 ## vectorizer_end
 
 
@@ -79,21 +86,26 @@ def transformed_test_data(test_data, tfidf_Vectorizer):
 import xgboost as xg
 from sklearn.metrics import mean_absolute_error
 
+
 @asset
 def xgboost_comments_model(transformed_training_data):
     transformed_X_train, transformed_y_train = transformed_training_data
-    # Train XGBoost model, which is a highly efficient and flexible model 
-    xgb_r = xg.XGBRegressor(objective ='reg:squarederror', eval_metric=mean_absolute_error,
-                  n_estimators = 20)
+    # Train XGBoost model, which is a highly efficient and flexible model
+    xgb_r = xg.XGBRegressor(
+        objective="reg:squarederror", eval_metric=mean_absolute_error, n_estimators=20
+    )
     xgb_r.fit(transformed_X_train, transformed_y_train)
     return xgb_r
 
-@asset 
-def comments_model_test_set_r_squared( transformed_test_data, xgboost_comments_model):
+
+@asset
+def comments_model_test_set_r_squared(transformed_test_data, xgboost_comments_model):
     transformed_X_test, transformed_y_test = transformed_test_data
     # Use the test set data to get a score of the XGBoost model
     score = xgboost_comments_model.score(transformed_X_test, transformed_y_test)
-    return score 
+    return score
+
+
 ## models_end
 
 
@@ -105,9 +117,9 @@ def latest_story_comment_predictions(xgboost, Tfidf_Vectorizer):
     latest_item = requests.get(
         f"https://hacker-news.firebaseio.com/v0/maxitem.json"
     ).json()
-    # Get items based on story ids from the HackerNews items endpoint 
+    # Get items based on story ids from the HackerNews items endpoint
     results = []
-    scope = range(latest_item-100, latest_item)
+    scope = range(latest_item - 100, latest_item)
     for item_id in scope:
         item = requests.get(
             f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
@@ -118,12 +130,10 @@ def latest_story_comment_predictions(xgboost, Tfidf_Vectorizer):
     if len(df) > 0:
         df = df[df.type == "story"]
         df = df[~df.title.isna()]
-    inference_x = df.title 
-    # Transform the new story titles using the existing vectorizer 
+    inference_x = df.title
+    # Transform the new story titles using the existing vectorizer
     inference_x = Tfidf_Vectorizer.transform(inference_x)
     return xgboost.predict(inference_x)
 
+
 ## inference_end
-
-    
-
