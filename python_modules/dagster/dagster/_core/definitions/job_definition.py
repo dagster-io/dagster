bfb4cd017ac755a11f27b66168b34be0270b01d5
benpankow <ben@elementl.com>
2023-03-21 09:58:08 -0700

    [fix] Fix behavior of BindResourcesToJobs when jobs have config specified

--- python_modules/dagster/dagster/_core/definitions/job_definition.py
+++ python_modules/dagster/dagster/_core/definitions/job_definition.py
@@ -110,6 +110,7 @@ class JobDefinition(PipelineDefinition):
         _executor_def_specified: Optional[bool] = None,
         _logger_defs_specified: Optional[bool] = None,
         _preset_defs: Optional[Sequence[PresetDefinition]] = None,
+        _did_user_provide_resources: bool = False,
     ):
         from dagster._core.definitions.run_config import RunConfig, convert_config_input
         from dagster._loggers import default_loggers
@@ -172,7 +173,11 @@ class JobDefinition(PipelineDefinition):
             _preset_defs, "preset_defs", of_type=PresetDefinition
         )
 
-        did_user_provide_resources = bool(resource_defs)
+        did_user_provide_resources = (
+            bool(resource_defs)
+            if _did_user_provide_resources is None
+            else _did_user_provide_resources
+        )
         if resource_defs and DEFAULT_IO_MANAGER_KEY in resource_defs:
             resource_defs_with_defaults = resource_defs
         else:
@@ -210,6 +215,7 @@ class JobDefinition(PipelineDefinition):
                         executor_def,
                         logger_defs,
                         asset_layer,
+                        did_user_provide_resources=did_user_provide_resources,
                     ),
                     config,
                     name,
@@ -1041,6 +1047,7 @@ def get_run_config_schema_for_job(
     executor_def: "ExecutorDefinition",
     logger_defs: Mapping[str, LoggerDefinition],
     asset_layer: Optional[AssetLayer],
+    did_user_provide_resources: bool = False,
 ) -> ConfigType:
     return (
         JobDefinition(
@@ -1050,6 +1057,7 @@ def get_run_config_schema_for_job(
             executor_def=executor_def,
             logger_defs=logger_defs,
             asset_layer=asset_layer,
+            _did_user_provide_resources=did_user_provide_resources,
         )
         .get_run_config_schema("default")
         .run_config_schema_type
