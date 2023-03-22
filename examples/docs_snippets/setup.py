b569e40fb99cc297fe3e2234e1d971079dadf80c
odette-elementl <odette@elementl.com>
2023-03-22 17:26:51 -0400

    Moved all the files from the other PR

--- examples/docs_snippets/setup.py
+++ examples/docs_snippets/setup.py
@@ -42,7 +42,7 @@ setup(
             # "'NoneType' object has no attribute 'canvas'" errors in the tests that involve
             # Jupyter notebooks
             "matplotlib-inline<=0.1.3",
-            "moto==1.3.16",
+            "moto",
             "numpy",
             "pandas",
             "pandera",
