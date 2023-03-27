429d8b953e74e4fde7d7f972a44d26154f75c552
JamieDeMaria <jamie@elementl.com>
2023-03-27 12:57:42 -0400

    clear db in unit test

--- python_modules/libraries/dagster-duckdb-pandas/dagster_duckdb_pandas_tests/test_type_handler.py
+++ python_modules/libraries/dagster-duckdb-pandas/dagster_duckdb_pandas_tests/test_type_handler.py
@@ -541,3 +541,7 @@ def test_self_dependent_asset(tmp_path, io_managers):
 
         assert out_df["a"].tolist() == ["1", "1", "1", "2", "2", "2"]
         duckdb_conn.close()
+
+        # drop table so we start with an empty db for the next io manager
+        duckdb_conn.execute("DELETE FROM my_schema.dynamic_partitioned")
+        duckdb_conn.close()
