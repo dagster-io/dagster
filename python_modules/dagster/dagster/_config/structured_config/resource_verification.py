01448723e98ef33166186bca33bf18eace81d311
benpankow <ben@elementl.com>
2023-03-27 09:34:42 -0700

    add helper classmethods

--- python_modules/dagster/dagster/_config/structured_config/resource_verification.py
+++ python_modules/dagster/dagster/_config/structured_config/resource_verification.py
@@ -15,6 +15,18 @@ class VerificationResult(NamedTuple):
     status: VerificationStatus
     message: Optional[str]
 
+    @classmethod
+    def success(cls, message: Optional[str] = None):
+        """Create a successful verification result.
+        """
+        return cls(VerificationStatus.SUCCESS, message)
+
+    @classmethod
+    def failure(cls, message: Optional[str] = None):
+        """Create a failed verification result.
+        """
+        return cls(VerificationStatus.FAILURE, message)
+
 
 class ConfigVerifiable:
     def verify_config(self) -> VerificationResult:
