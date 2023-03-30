8d5c99f038532f2264818556159b92ef45461c27
Marco polo <salazarm@users.noreply.github.com>
2023-03-29 23:03:05 -0400

    Add a reusable filtering component (#13175)
    ## Summary & Motivation
    
    Motivation is this Cloud issue:
    https://linear.app/elementl/issue/DSN-75/cloud-audit-logs-ui-follow-ups
    
    This PR implements a reusable filtering component. The API exposed to
    users of the API is a hook:
    
    ```
    interface UseFiltersProps {
      filters: Filter<any, any>[];
      activeFilters: Filter<any, any>[];
      setActiveFilters: (next: React.SetStateAction<Filter<any, any>[]>) => void;
    }
    function useFilters({filters, activeFilters, setActiveFilters}: UseFiltersProps): { button: JSX.Element, activeFiltersJsx: JSX.Eelment }
    
    ```
    
    This PR also implements 2 types of filters: TimeRangeFilter and
    SetFilter.
    The idea here is that we can extend the base Filter class to implement
    new types of filtering and support different behaviors (eg. supporting
    typeahead results from the server).
    
    The reason for a hook is because we need to return 2 separately
    renderable pieces of UI that share state.
    
    
    ## How I Tested These Changes
    
    
    Storybook + I used this in the audit log filtering UI
    https://www.loom.com/share/b271abf4a2bf48309029bdd205d5338e
    
    
    <img width="826" alt="Screen Shot 2023-03-27 at 10 41 35 AM"
    src="https://user-images.githubusercontent.com/2286579/227974279-9f151324-5d04-4a4b-9c18-f5780442a560.png">
    
    [INTERNAL_BRANCH=auditLogFiltering]

---
+++ js_modules/dagit/packages/core/src/ui/Filters/__tests__/useStaticSetFilter.test.tsx
@@ -0,0 +1,106 @@
+import {render} from '@testing-library/react';
+import {act, renderHook} from '@testing-library/react-hooks';
+import React from 'react';
+
+import {useStaticSetFilter} from '../useStaticSetFilter';
+
+describe('useStaticSetFilter', () => {
+  const allValues = [
+    {value: 'apple', match: ['apple']},
+    {value: 'banana', match: ['banana']},
+    {value: 'cherry', match: ['cherry']},
+  ];
+
+  function createTestFilter() {
+    return renderHook(() =>
+      useStaticSetFilter({
+        name: 'Test',
+        icon: 'asset',
+        allValues,
+        renderLabel: ({value, isActive}) => (
+          <span className={isActive ? 'active' : 'inactive'}>{value}</span>
+        ),
+        getStringValue: (value: string) => value,
+        initialState: ['banana'],
+      }),
+    );
+  }
+
+  it('creates filter object with the correct properties', () => {
+    const filter = createTestFilter();
+
+    expect(filter.result.current).toHaveProperty('name', 'Test');
+    expect(filter.result.current).toHaveProperty('icon', 'asset');
+    expect(filter.result.current).toHaveProperty('state', new Set(['banana']));
+  });
+
+  function select(filter: ReturnType<typeof createTestFilter>, value: string) {
+    const close = jest.fn();
+    const createPortal = jest.fn();
+    act(() => {
+      filter.result.current.onSelect({
+        value,
+        close,
+        createPortal,
+      });
+    });
+    return {close, createPortal};
+  }
+
+  it('adds and removes values from the state', () => {
+    const filter = createTestFilter();
+    const {close} = select(filter, 'apple');
+    expect(filter.result.current.state).toEqual(new Set(['banana', 'apple']));
+    expect(close.mock.calls.length).toEqual(0);
+
+    select(filter, 'banana');
+    expect(filter.result.current.state).toEqual(new Set(['apple']));
+  });
+
+  it('renders results with proper isActive state', () => {
+    const filter = createTestFilter();
+    const results = filter.result.current.getResults('');
+    const {getByText} = render(
+      <>
+        {results.map((r) => (
+          <span
+            key={r.key}
+            onClick={() => {
+              select(filter, r.value);
+            }}
+          >
+            {r.label}
+          </span>
+        ))}
+      </>,
+    );
+
+    const apple = getByText('apple');
+    const banana = getByText('banana');
+    const cherry = getByText('cherry');
+
+    expect(apple).not.toHaveClass('active');
+    expect(banana).toHaveClass('active');
+    expect(cherry).not.toHaveClass('active');
+  });
+
+  it('renders filtered results based on query', () => {
+    const filter = createTestFilter();
+    const results = filter.result.current.getResults('a');
+    const {getByText, queryByText} = render(
+      <>
+        {results.map((r) => (
+          <span key={r.key}>{r.label}</span>
+        ))}
+      </>,
+    );
+
+    const apple = getByText('apple');
+    const banana = getByText('banana');
+    const cherry = queryByText('cherry');
+
+    expect(apple).toBeInTheDocument();
+    expect(banana).toBeInTheDocument();
+    expect(cherry).not.toBeInTheDocument();
+  });
+});
