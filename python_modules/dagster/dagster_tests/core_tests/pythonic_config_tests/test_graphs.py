4f620de2c5273d41efccf6c90fc0ad41c10213d4
benpankow <ben@elementl.com>
2023-03-30 17:30:55 -0700

    another small test

--- python_modules/dagster/dagster_tests/core_tests/pythonic_config_tests/test_graphs.py
+++ python_modules/dagster/dagster_tests/core_tests/pythonic_config_tests/test_graphs.py
@@ -23,6 +23,40 @@ def test_binding_runconfig() -> None:
     assert result.output_for_node("do_something") == "foo"
 
 
+def test_binding_runconfig_more_complex() -> None:
+    class DoSomethingConfig(Config):
+        config_param: str
+
+    @op
+    def do_something(config: DoSomethingConfig) -> str:
+        return config.config_param
+
+    class DoSomethingElseConfig(Config):
+        x: int
+        y: int
+
+    @op
+    def do_something_else(config: DoSomethingElseConfig) -> int:
+        return config.x + config.y
+
+    @job(
+        config=RunConfig(  # type: ignore[call-arg]
+            ops={
+                "do_something": DoSomethingConfig(config_param="foo"),
+                "do_something_else": DoSomethingElseConfig(x=3, y=4),
+            }
+        )
+    )
+    def do_it_all_with_baked_in_config() -> None:
+        do_something()
+        do_something_else()
+
+    result = do_it_all_with_baked_in_config.execute_in_process()
+    assert result.success
+    assert result.output_for_node("do_something") == "foo"
+    assert result.output_for_node("do_something_else") == 7
+
+
 def test_config_mapping_return_config_dict() -> None:
     class DoSomethingConfig(Config):
         config_param: str
