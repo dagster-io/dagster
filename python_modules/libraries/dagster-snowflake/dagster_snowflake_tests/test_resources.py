2ce1f71eca7c7d72a11b51cfdef1ac4892321fbe
JamieDeMaria <jamie@elementl.com>
2023-04-04 12:50:12 -0400

    wip convert resource

--- python_modules/libraries/dagster-snowflake/dagster_snowflake_tests/test_resources.py
+++ python_modules/libraries/dagster-snowflake/dagster_snowflake_tests/test_resources.py
@@ -1,41 +1,46 @@
 from unittest import mock
 
 import pytest
-from dagster import DagsterResourceFunctionError, job, op
+from dagster import DagsterResourceFunctionError, EnvVar, job, op
 from dagster._core.test_utils import environ
-from dagster_snowflake import snowflake_resource
+from dagster_snowflake import SnowflakeResource, snowflake_resource
 
 from .utils import create_mock_connector
 
+pythonic_snowflake_resource = SnowflakeResource(
+    account="foo",
+    user="bar",
+    password="baz",
+    database="TESTDB",
+    schema="TESTSCHEMA",
+    warehouse="TINY_WAREHOUSE",
+)
+old_snowflake_resource = snowflake_resource.configured(
+    {
+        "account": "foo",
+        "user": "bar",
+        "password": "baz",
+        "database": "TESTDB",
+        "schema": "TESTSCHEMA",
+        "warehouse": "TINY_WAREHOUSE",
+    }
+)
+
 
 @mock.patch("snowflake.connector.connect", new_callable=create_mock_connector)
-def test_snowflake_resource(snowflake_connect):
+@pytest.mark.parametrize("resource", [(pythonic_snowflake_resource), (old_snowflake_resource)])
+def test_snowflake_resource(snowflake_connect, resource):
     @op(required_resource_keys={"snowflake"})
     def snowflake_op(context):
         assert context.resources.snowflake
         with context.resources.snowflake.get_connection() as _:
             pass
 
-    @job(resource_defs={"snowflake": snowflake_resource})
+    @job(resource_defs={"snowflake": resource})
     def snowflake_job():
         snowflake_op()
 
-    result = snowflake_job.execute_in_process(
-        run_config={
-            "resources": {
-                "snowflake": {
-                    "config": {
-                        "account": "foo",
-                        "user": "bar",
-                        "password": "baz",
-                        "database": "TESTDB",
-                        "schema": "TESTSCHEMA",
-                        "warehouse": "TINY_WAREHOUSE",
-                    }
-                }
-            }
-        }
-    )
+    result = snowflake_job.execute_in_process()
     assert result.success
     snowflake_connect.assert_called_once_with(
         account="foo",
@@ -47,15 +52,38 @@ def test_snowflake_resource(snowflake_connect):
     )
 
 
+pythonic_snowflake_resource_env = SnowflakeResource(
+    account=EnvVar("SNOWFLAKE_ACCOUNT"),
+    user=EnvVar("SNOWFLAKE_USER"),
+    password=EnvVar("SNOWFLAKE_PASSWORD"),
+    database=EnvVar("SNOWFLAKE_DATABASE"),
+    schema=EnvVar("SNOWFLAKE_SCHEMA"),
+    warehouse=EnvVar("SNOWFLAKE_WAREHOUSE"),
+)
+old_snowflake_resource_env = snowflake_resource.configured(
+    {
+        "account": {"env": "SNOWFLAKE_ACCOUNT"},
+        "user": {"env": "SNOWFLAKE_USER"},
+        "password": {"env": "SNOWFLAKE_PASSWORD"},
+        "database": {"env": "SNOWFLAKE_DATABASE"},
+        "schema": {"env": "SNOWFLAKE_SCHEMA"},
+        "warehouse": {"env": "SNOWFLAKE_WAREHOUSE"},
+    }
+)
+
+
 @mock.patch("snowflake.connector.connect", new_callable=create_mock_connector)
-def test_snowflake_resource_from_envvars(snowflake_connect):
+@pytest.mark.parametrize(
+    "resource", [(pythonic_snowflake_resource_env), (old_snowflake_resource_env)]
+)
+def test_snowflake_resource_from_envvars(snowflake_connect, resource):
     @op(required_resource_keys={"snowflake"})
     def snowflake_op(context):
         assert context.resources.snowflake
         with context.resources.snowflake.get_connection() as _:
             pass
 
-    @job(resource_defs={"snowflake": snowflake_resource})
+    @job(resource_defs={"snowflake": resource})
     def snowflake_job():
         snowflake_op()
 
@@ -68,22 +96,7 @@ def test_snowflake_resource_from_envvars(snowflake_connect):
         "SNOWFLAKE_WAREHOUSE": "TINY_WAREHOUSE",
     }
     with environ(env_vars):
-        result = snowflake_job.execute_in_process(
-            run_config={
-                "resources": {
-                    "snowflake": {
-                        "config": {
-                            "account": {"env": "SNOWFLAKE_ACCOUNT"},
-                            "user": {"env": "SNOWFLAKE_USER"},
-                            "password": {"env": "SNOWFLAKE_PASSWORD"},
-                            "database": {"env": "SNOWFLAKE_DATABASE"},
-                            "schema": {"env": "SNOWFLAKE_SCHEMA"},
-                            "warehouse": {"env": "SNOWFLAKE_WAREHOUSE"},
-                        }
-                    }
-                }
-            },
-        )
+        result = snowflake_job.execute_in_process()
         assert result.success
         snowflake_connect.assert_called_once_with(
             account="foo",
@@ -95,8 +108,25 @@ def test_snowflake_resource_from_envvars(snowflake_connect):
         )
 
 
+pythonic_snowflake_resource_no_auth = SnowflakeResource(
+    account="foo", user="bar", database="TESTDB", schema="TESTSCHEMA", warehouse="TINY_WAREHOUSE"
+)
+old_snowflake_resource_no_auth = snowflake_resource.configured(
+    {
+        "account": "foo",
+        "user": "bar",
+        "database": "TESTDB",
+        "schema": "TESTSCHEMA",
+        "warehouse": "TINY_WAREHOUSE",
+    }
+)
+
+
 @mock.patch("snowflake.connector.connect", new_callable=create_mock_connector)
-def test_snowflake_resource_no_auth(snowflake_connect):
+@pytest.mark.parametrize(
+    "resource", [(pythonic_snowflake_resource_no_auth), (old_snowflake_resource_no_auth)]
+)
+def test_snowflake_resource_no_auth(snowflake_connect, resource):
     @op(required_resource_keys={"snowflake"})
     def snowflake_op(context):
         assert context.resources.snowflake
@@ -108,81 +138,84 @@ def test_snowflake_resource_no_auth(snowflake_connect):
         snowflake_op()
 
     with pytest.raises(DagsterResourceFunctionError):
-        snowflake_job.execute_in_process(
-            run_config={
-                "resources": {
-                    "snowflake": {
-                        "config": {
-                            "account": "foo",
-                            "user": "bar",
-                            "database": "TESTDB",
-                            "schema": "TESTSCHEMA",
-                            "warehouse": "TINY_WAREHOUSE",
-                        }
-                    }
-                }
-            }
-        )
+        snowflake_job.execute_in_process()
+
+
+pythonic_snowflake_resource_auth = SnowflakeResource(
+    account="foo",
+    user="bar",
+    password="baz",
+    database="TESTDB",
+    schema="TESTSCHEMA",
+    warehouse="TINY_WAREHOUSE",
+    private_key="TESTKEY",
+)
+old_snowflake_resource_auth = snowflake_resource.configured(
+    {
+        "account": "foo",
+        "user": "bar",
+        "password": "baz",
+        "database": "TESTDB",
+        "schema": "TESTSCHEMA",
+        "warehouse": "TINY_WAREHOUSE",
+        "private_key": "TESTKEY",
+    }
+)
 
 
 @mock.patch("snowflake.connector.connect", new_callable=create_mock_connector)
-def test_snowflake_resource_duplicate_auth(snowflake_connect):
+@pytest.mark.parametrize(
+    "resource", [(pythonic_snowflake_resource_auth), (old_snowflake_resource_auth)]
+)
+def test_snowflake_resource_duplicate_auth(snowflake_connect, resource):
     @op(required_resource_keys={"snowflake"})
     def snowflake_op(context):
         assert context.resources.snowflake
         with context.resources.snowflake.get_connection() as _:
             pass
 
-    @job(resource_defs={"snowflake": snowflake_resource})
+    @job(resource_defs={"snowflake": resource})
     def snowflake_job():
         snowflake_op()
 
     with pytest.raises(DagsterResourceFunctionError):
-        snowflake_job.execute_in_process(
-            run_config={
-                "resources": {
-                    "snowflake": {
-                        "config": {
-                            "account": "foo",
-                            "user": "bar",
-                            "database": "TESTDB",
-                            "password": "baz",
-                            "schema": "TESTSCHEMA",
-                            "warehouse": "TINY_WAREHOUSE",
-                            "private_key": "TESTKEY",
-                        }
-                    }
-                }
-            }
-        )
+        snowflake_job.execute_in_process()
+
+
+pythonic_snowflake_resource_pk_password = SnowflakeResource(
+    account="foo",
+    user="bar",
+    database="TESTDB",
+    schema="TESTSCHEMA",
+    warehouse="TINY_WAREHOUSE",
+    private_key="TESTKEY",
+)
+old_snowflake_resource_pk_password = snowflake_resource.configured(
+    {
+        "account": "foo",
+        "user": "bar",
+        "database": "TESTDB",
+        "schema": "TESTSCHEMA",
+        "warehouse": "TINY_WAREHOUSE",
+        "private_key": "TESTKEY",
+    }
+)
 
 
 @mock.patch("snowflake.connector.connect", new_callable=create_mock_connector)
-def test_snowflake_resource_missing_private_key_password(snowflake_connect):
+@pytest.mark.parametrize(
+    "resource", [(pythonic_snowflake_resource_pk_password), (old_snowflake_resource_pk_password)]
+)
+def test_snowflake_resource_missing_private_key_password(snowflake_connect, resource):
     @op(required_resource_keys={"snowflake"})
     def snowflake_op(context):
         assert context.resources.snowflake
         with context.resources.snowflake.get_connection() as _:
             pass
 
-    @job(resource_defs={"snowflake": snowflake_resource})
+    @job(resource_defs={"snowflake": resource})
     def snowflake_job():
         snowflake_op()
 
     with pytest.raises(DagsterResourceFunctionError):
-        snowflake_job.execute_in_process(
-            run_config={
-                "resources": {
-                    "snowflake": {
-                        "config": {
-                            "account": "foo",
-                            "user": "bar",
-                            "database": "TESTDB",
-                            "schema": "TESTSCHEMA",
-                            "warehouse": "TINY_WAREHOUSE",
-                            "private_key": "TESTKEY",
-                        }
-                    }
-                }
-            }
-        )
+        snowflake_job.execute_in_process()
