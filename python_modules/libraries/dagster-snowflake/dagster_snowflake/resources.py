c2dddd6399f48674c502f05b710b35c7491ba112
JamieDeMaria <jamie@elementl.com>
2023-04-04 12:50:12 -0400

    wip convert resource

--- python_modules/libraries/dagster-snowflake/dagster_snowflake/resources.py
+++ python_modules/libraries/dagster-snowflake/dagster_snowflake/resources.py
@@ -1,7 +1,7 @@
 import sys
 import warnings
 from contextlib import closing, contextmanager
-from typing import Any, Dict, Iterator, List, Mapping, Optional, Sequence, Union
+from typing import Any, Dict, Iterator, List, Mapping, Optional, Sequence, Union, cast
 
 import dagster._check as check
 from cryptography.hazmat.backends import default_backend
@@ -37,6 +37,11 @@ class SnowflakeConnection:
     """
 
     def __init__(self, config: Mapping[str, str], log):
+        config = cast(Dict[str, str], config)
+        # with pydantic resources the schema config field will be named schema_
+        if config.get("schema_") is not None:
+            config["schema"] = config["schema_"]
+
         # Extract parameters from resource config. Note that we can't pass None values to
         # snowflake.connector.connect() because they will override the default values set within the
         # connector; remove them from the conn_args dict.
@@ -364,7 +369,7 @@ class SnowflakeResource(ConfigurableResourceFactory):
             "change the schema."
         ),
         alias="schema",
-    )
+    )  # schema is a reserved word for pydantic
 
     role: Optional[str] = Field(
         default=None,
