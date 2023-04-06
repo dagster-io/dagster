8e9140e8cafda406f73c2ec7d07ebe47d5a52599
benpankow <ben@elementl.com>
2023-04-06 12:05:49 -0700

    [2/n][dagster-aws] Migrate SecretsManager, SSM resources to new APIs

--- python_modules/libraries/dagster-aws/dagster_aws/secretsmanager/resources.py
+++ python_modules/libraries/dagster-aws/dagster_aws/secretsmanager/resources.py
@@ -1,22 +1,57 @@
 from contextlib import contextmanager
+from typing import Any, Dict, Iterable, List, Optional
 
 from dagster import (
-    Array,
-    Field,
-    Noneable,
-    _check as check,
     resource,
 )
+from dagster._core.execution.context.init import InitResourceContext
 from dagster._core.test_utils import environ
 from dagster._utils.merger import merge_dicts
+from pydantic import Field
 
-from dagster_aws.utils import BOTO3_SESSION_CONFIG
+from dagster_aws.utils import Boto3ResourceBase
 
 from .secrets import construct_secretsmanager_client, get_secrets_from_arns, get_tagged_secrets
 
 
-@resource(BOTO3_SESSION_CONFIG)
-def secretsmanager_resource(context):
+class SecretsManagerResource(Boto3ResourceBase[Any]):
+    """Resource that gives access to AWS SecretsManager.
+
+    The underlying SecretsManager session is created by calling
+    :py:func:`boto3.session.Session(profile_name) <boto3:boto3.session>`.
+    The returned resource object is a SecretsManager client, an instance of `botocore.client.SecretsManager`.
+
+    Example:
+        .. code-block:: python
+
+            from dagster import build_op_context, job, op
+            from dagster_aws.secretsmanager import SecretsManagerResource
+
+            @op(required_resource_keys={'secretsmanager'})
+            def example_secretsmanager_op(context):
+                return context.resources.secretsmanager.get_secret_value(
+                    SecretId='arn:aws:secretsmanager:region:aws_account_id:secret:appauthexample-AbCdEf'
+                )
+
+            @job(resource_defs={
+                'secretsmanager': SecretsManagerResource(region_name='us-west-1')
+            })
+            def example_job():
+                example_secretsmanager_op()
+
+            example_job.execute_in_process()
+    """
+
+    def create_resource(self, context: InitResourceContext) -> Any:
+        return construct_secretsmanager_client(
+            max_attempts=self.max_attempts,
+            region_name=self.region_name,
+            profile_name=self.profile_name,
+        )
+
+
+@resource(SecretsManagerResource.to_config_schema())
+def secretsmanager_resource(context) -> Any:
     """Resource that gives access to AWS SecretsManager.
 
     The underlying SecretsManager session is created by calling
@@ -51,10 +86,6 @@ def secretsmanager_resource(context):
                 }
             )
 
-    Note that your ops must also declare that they require this resource with
-    `required_resource_keys`, or it will not be initialized for the execution of their compute
-    functions.
-
     You may configure this resource as follows:
 
     .. code-block:: YAML
@@ -70,40 +101,77 @@ def secretsmanager_resource(context):
               # profile as specified in ~/.aws/credentials file
 
     """
-    return construct_secretsmanager_client(
-        max_attempts=context.resource_config["max_attempts"],
-        region_name=context.resource_config.get("region_name"),
-        profile_name=context.resource_config.get("profile_name"),
-    )
+    return SecretsManagerResource.from_resource_context(context)
 
 
-@resource(
-    merge_dicts(
-        BOTO3_SESSION_CONFIG,
-        {
-            "secrets": Field(
-                Array(str),
-                is_required=False,
-                default_value=[],
-                description="An array of AWS Secrets Manager secrets arns to fetch.",
-            ),
-            "secrets_tag": Field(
-                Noneable(str),
-                is_required=False,
-                default_value=None,
-                description=(
-                    "AWS Secrets Manager secrets with this tag will be fetched and made available."
-                ),
-            ),
-            "add_to_environment": Field(
-                bool,
-                is_required=False,
-                default_value=False,
-                description="Whether to mount the secrets as environment variables.",
-            ),
-        },
+class SecretsManagerSecretsResource(Boto3ResourceBase[Dict[str, str]]):
+    """Resource that provides a dict which maps selected SecretsManager secrets to
+    their string values. Also optionally sets chosen secrets as environment variables.
+
+    Example:
+        .. code-block:: python
+
+            import os
+            from dagster import build_op_context, job, op
+            from dagster_aws.secretsmanager import SecretsManagerSecretsResource
+
+            @op(required_resource_keys={'secrets'})
+            def example_secretsmanager_secrets_op(context):
+                return context.resources.secrets.get("my-secret-name")
+
+            @op(required_resource_keys={'secrets'})
+            def example_secretsmanager_secrets_op_2(context):
+                return os.getenv("my-other-secret-name")
+
+            @job(resource_defs={
+                'secrets': SecretsManagerSecretsResource(
+                    region_name='us-west-1',
+                    secrets_tag="dagster",
+                    add_to_environment=True,
+                )}
+            )
+            def example_job():
+                example_secretsmanager_secrets_op()
+                example_secretsmanager_secrets_op_2()
+
+            example_job.execute_in_process()
+
+    Note that your ops must also declare that they require this resource with or it will not be initialized
+    for the execution of their compute functions.
+    """
+
+    secrets: List[str] = Field(
+        default=[], description="An array of AWS Secrets Manager secrets arns to fetch."
     )
-)
+    secrets_tag: Optional[str] = Field(
+        default=None,
+        description="AWS Secrets Manager secrets with this tag will be fetched and made available.",
+    )
+    add_to_environment: bool = Field(
+        default=False, description="Whether to mount the secrets as environment variables."
+    )
+
+    def create_resource(self, context: InitResourceContext) -> Iterable[Dict[str, str]]:
+        secrets_manager = construct_secretsmanager_client(
+            max_attempts=self.max_attempts,
+            region_name=self.region_name,
+            profile_name=self.profile_name,
+        )
+
+        secret_arns = merge_dicts(
+            (get_tagged_secrets(secrets_manager, [self.secrets_tag]) if self.secrets_tag else {}),
+            get_secrets_from_arns(secrets_manager, self.secrets),
+        )
+
+        secrets_map = {
+            name: secrets_manager.get_secret_value(SecretId=arn).get("SecretString")
+            for name, arn in secret_arns.items()
+        }
+        with environ(secrets_map if self.add_to_environment else {}):
+            yield secrets_map
+
+
+@resource(config_schema=SecretsManagerSecretsResource.to_config_schema())
 @contextmanager
 def secretsmanager_secrets_resource(context):
     """Resource that provides a dict which maps selected SecretsManager secrets to
@@ -170,26 +238,4 @@ def secretsmanager_secrets_resource(context):
               # to false.
 
     """
-    add_to_environment = check.bool_param(
-        context.resource_config["add_to_environment"], "add_to_environment"
-    )
-    secrets_tag = check.opt_str_param(context.resource_config["secrets_tag"], "secrets_tag")
-    secrets = check.list_param(context.resource_config["secrets"], "secrets", of_type=str)
-
-    secrets_manager = construct_secretsmanager_client(
-        max_attempts=context.resource_config["max_attempts"],
-        region_name=context.resource_config.get("region_name"),
-        profile_name=context.resource_config.get("profile_name"),
-    )
-
-    secret_arns = merge_dicts(
-        (get_tagged_secrets(secrets_manager, [secrets_tag]) if secrets_tag else {}),
-        get_secrets_from_arns(secrets_manager, secrets),
-    )
-
-    secrets_map = {
-        name: secrets_manager.get_secret_value(SecretId=arn).get("SecretString")
-        for name, arn in secret_arns.items()
-    }
-    with environ(secrets_map if add_to_environment else {}):
-        yield secrets_map
+    return SecretsManagerSecretsResource.from_resource_context(context)
