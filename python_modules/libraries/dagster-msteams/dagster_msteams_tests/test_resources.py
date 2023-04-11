ff8d9e3b91bb597ab1ea0945be00ba7e2f38f0a3
benpankow <ben@elementl.com>
2023-04-10 21:17:46 -0700

    [dagster-msteams] Migrate to Configurable Resource

--- python_modules/libraries/dagster-msteams/dagster_msteams_tests/test_resources.py
+++ python_modules/libraries/dagster-msteams/dagster_msteams_tests/test_resources.py
@@ -2,14 +2,14 @@ import json
 
 from dagster import op
 from dagster._utils.test import wrap_op_in_graph_and_execute
-from dagster_msteams import msteams_resource
+from dagster_msteams import MSTeamsResource, msteams_resource
 from mock import patch
 
 
 @patch("dagster_msteams.client.TeamsClient.post_message")
 def test_msteams_resource(mock_teams_post_message, json_message, teams_client):
     @op(required_resource_keys={"msteams"})
-    def msteams_solid(context):
+    def msteams_op(context):
         assert context.resources.msteams
         body = {"ok": True}
         mock_teams_post_message.return_value = {
@@ -17,11 +17,11 @@ def test_msteams_resource(mock_teams_post_message, json_message, teams_client):
             "body": json.dumps(body),
             "headers": "",
         }
-        teams_client.post_message(json_message)
+        context.resources.msteams.post_message(json_message)
         assert mock_teams_post_message.called
 
     result = wrap_op_in_graph_and_execute(
-        msteams_solid,
+        msteams_op,
         run_config={
             "resources": {
                 "msteams": {
@@ -33,5 +33,35 @@ def test_msteams_resource(mock_teams_post_message, json_message, teams_client):
             }
         },
         resources={"msteams": msteams_resource},
+    )
+    assert result.success
+
+
+@patch("dagster_msteams.client.TeamsClient.post_message")
+def test_msteams_resource_pythonic(mock_teams_post_message, json_message, teams_client) -> None:
+    @op
+    def msteams_op(msteams: MSTeamsResource) -> None:
+        body = {"ok": True}
+        mock_teams_post_message.return_value = {
+            "status": 200,
+            "body": json.dumps(body),
+            "headers": "",
+        }
+        msteams.get_client().post_message(json_message)
+        assert mock_teams_post_message.called
+
+    result = wrap_op_in_graph_and_execute(
+        msteams_op,
+        run_config={
+            "resources": {
+                "msteams": {
+                    "config": {
+                        "hook_url": "https://some_url_here/",
+                        "https_proxy": "some_proxy",
+                    }
+                }
+            }
+        },
+        resources={"msteams": MSTeamsResource()},
     )
     assert result.success
