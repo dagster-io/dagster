7d231314ef3e1954920f1ef8f2ff876748209e92
Nick Schrock <schrockn@elementl.com>
2023-04-11 19:17:23 -0400

    Convert github to use non-factory resource (#13480)
    ## Summary & Motivation
    
    Per internal discussion we are moving aware from ConfigurableResourceFactory for our integrations. This moves Github.
    
    ## How I Tested These Changes
    
    BK

--- python_modules/libraries/dagster-github/dagster_github/__init__.py
+++ python_modules/libraries/dagster-github/dagster_github/__init__.py
@@ -1,8 +1,8 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-from .resources import github_resource
+from .resources import GithubClientResource, github_resource
 from .version import __version__
 
 DagsterLibraryRegistry.register("dagster-github", __version__)
 
-__all__ = ["github_resource"]
+__all__ = ["github_resource", "GithubClientResource"]
