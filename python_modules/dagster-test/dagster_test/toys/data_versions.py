4845c4f82b05f628c2728281121ec66b522702d3
Sandy Ryza <sandy@elementl.com>
2023-04-11 12:56:27 -0700

    add data_versions toy to code location and normalize it (#13513)
    ## Summary & Motivation
    
    This is part of a larger effort to make our toys comprehensive and look
    similar.
    
    - Make the data_versions toy show up in the toys code location
    - Give the assets names that makes their purpose more clear
    - Remove bits like the input manager that aren't strictly related to
    code versioning
    - Import from the top-level dagster project, like a user would
    - Make one of the observable sources never change version, to exercise
    that functionality
    
    ## How I Tested These Changes
    
    - Ran `dagster dev` from the repo and ensured everything loaded
    - Ran `dagster dev` on data_versions.py and ensured everything loaded
    - Ran "Materialize all"
    - Ran "Observe sources"

--- python_modules/dagster-test/dagster_test/toys/data_versions.py
+++ python_modules/dagster-test/dagster_test/toys/data_versions.py
@@ -1,111 +1,78 @@
 import datetime
 
 from dagster import (
-    AssetIn,
-    Definitions,
+    AssetOut,
+    DataVersion,
+    Output,
+    SourceAsset,
     asset,
-    input_manager,
+    multi_asset,
     observable_source_asset,
 )
-from dagster._core.definitions.asset_out import AssetOut
-from dagster._core.definitions.data_version import DataVersion
-from dagster._core.definitions.decorators.asset_decorator import multi_asset
-from dagster._core.definitions.events import Output
-from dagster._core.definitions.source_asset import SourceAsset
 
 
-@observable_source_asset(
-    key_prefix=["data_versions"],
-)
-def foo(_context):
+@observable_source_asset
+def observable_different_version():
     return DataVersion(str(datetime.datetime.now()))
 
 
-@observable_source_asset(
-    key_prefix=["data_versions"],
-)
-def bar(_context):
-    return DataVersion(str(datetime.datetime.now()))
+@observable_source_asset
+def observable_same_version():
+    return "5"
 
 
-baz = SourceAsset("baz")
+non_observable_source = SourceAsset("non_observable_source")
 
 
-@input_manager
-def source_asset_input_manager():
-    return 100
+@asset(code_version="1", non_argument_deps={"observable_different_version"})
+def has_code_version1(context):
+    ...
 
 
-@asset(
-    key_prefix=["data_versions"],
-    ins={"foo": AssetIn(input_manager_key="source_asset_input_manager")},
-    code_version="1",
-)
-def alpha(context, foo):
-    return foo + 100
+@asset(code_version="1", non_argument_deps={"observable_same_version"})
+def has_code_version2():
+    ...
 
 
 @asset(
-    key_prefix=["data_versions"],
-    ins={"bar": AssetIn(input_manager_key="source_asset_input_manager")},
-    code_version="1",
-)
-def beta(context, bar):
-    return bar + 100
-
-
-@asset(
-    key_prefix=["data_versions"],
-    ins={
-        "foo": AssetIn(input_manager_key="source_asset_input_manager"),
-        "bar": AssetIn(input_manager_key="source_asset_input_manager"),
-        "baz": AssetIn(input_manager_key="source_asset_input_manager"),
+    non_argument_deps={
+        "observable_different_version",
+        "observable_same_version",
+        "non_observable_source",
     },
     code_version="1",
 )
-def delta(context, foo, bar, baz):
-    return foo + bar + baz + 100
+def has_code_version_multiple_deps():
+    ...
 
 
-@asset(
-    key_prefix=["data_versions"],
-    code_version="1",
-)
-def epsilon(context, alpha):
-    return alpha + 100
+@asset(code_version="1", non_argument_deps={"has_code_version1"})
+def downstream_of_code_versioned():
+    ...
 
 
-@asset(
-    key_prefix=["data_versions"],
-)
-def gamma(context):
+@asset
+def root_asset_no_code_version(context):
     return 100
 
 
-@asset(
-    key_prefix=["data_versions"],
-)
-def rho(context, gamma):
-    return gamma + 100
+@asset(non_argument_deps={"root_asset_no_code_version"})
+def downstream_of_no_code_version():
+    ...
 
 
 @multi_asset(
     outs={
-        "sigma": AssetOut(key_prefix="data_versions", code_version="1"),
-        "tau": AssetOut(key_prefix="data_versions", code_version="3"),
+        "code_versioned_multi_asset1": AssetOut(code_version="1"),
+        "code_versioned_multi_asset2": AssetOut(code_version="3"),
     },
+    non_argument_deps={"downstream_of_no_code_version"},
 )
-def sigma_tau(context, rho):
-    yield Output(rho + 100, "sigma")
-    yield Output(rho + 200, "tau")
+def code_versioned_multi_asset():
+    yield Output(None, "code_versioned_multi_asset1")
+    yield Output(None, "code_versioned_multi_asset2")
 
 
-@asset(key_prefix=["data_versions"], ins={"tau": AssetIn("tau")})
-def zeta(context, tau):
-    return tau + 100
-
-
-definitions = Definitions(
-    assets=[foo, bar, baz, alpha, beta, delta, epsilon, gamma, rho, sigma_tau, zeta],
-    resources={"source_asset_input_manager": source_asset_input_manager},
-)
+@asset(non_argument_deps={"code_versioned_multi_asset2"})
+def downstream_of_code_versioned_multi_asset():
+    ...
