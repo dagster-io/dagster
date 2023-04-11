98a3ec67bd651fdc251154fea294c70dcb8dffa4
Ben Pankow <ben@elementl.com>
2023-04-11 16:53:18 -0700

    [dagster-msteams] Migrate to Configurable Resource (#13501)
    ## Summary
    
    Migrate `dagster-msteams` resource to use `ConfigurableResource`.
    
    ## Test Plan
    
    New unit test.

--- python_modules/libraries/dagster-msteams/dagster_msteams/__init__.py
+++ python_modules/libraries/dagster-msteams/dagster_msteams/__init__.py
@@ -5,7 +5,10 @@ from .hooks import (
     teams_on_failure as teams_on_failure,
     teams_on_success as teams_on_success,
 )
-from .resources import msteams_resource as msteams_resource
+from .resources import (
+    MSTeamsResource as MSTeamsResource,
+    msteams_resource as msteams_resource,
+)
 from .sensors import make_teams_on_run_failure_sensor as make_teams_on_run_failure_sensor
 from .version import __version__ as __version__
 
