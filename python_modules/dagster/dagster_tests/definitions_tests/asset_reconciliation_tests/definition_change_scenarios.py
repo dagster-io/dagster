2a08c5acfeb57edb60ef2cd4a8ba8d640cf475d5
Sandy Ryza <sandy@elementl.com>
2023-04-11 10:06:32 -0700

    take out partitions_def_added and partitions_def_removed scenarios

--- python_modules/dagster/dagster_tests/definitions_tests/asset_reconciliation_tests/definition_change_scenarios.py
+++ python_modules/dagster/dagster_tests/definitions_tests/asset_reconciliation_tests/definition_change_scenarios.py
@@ -1,7 +1,7 @@
 """Scenarios where the set of asset definitions changes between ticks.
 """
 
-from .asset_reconciliation_scenario import AssetReconciliationScenario, run_request
+from .asset_reconciliation_scenario import AssetReconciliationScenario
 from .basic_scenarios import one_asset, two_assets_in_sequence
 from .partition_scenarios import one_asset_one_partition
 
@@ -30,24 +30,6 @@ definition_change_scenarios = {
         expected_run_requests=[],
         cursor_from=AssetReconciliationScenario(
             assets=one_asset_one_partition,
-            unevaluated_runs=[],
-        ),
-    ),
-    "partitions_def_removed": AssetReconciliationScenario(
-        assets=one_asset,
-        unevaluated_runs=[],
-        expected_run_requests=[run_request(asset_keys=["asset1"])],
-        cursor_from=AssetReconciliationScenario(
-            assets=one_asset_one_partition,
-            unevaluated_runs=[],
-        ),
-    ),
-    "partitions_def_added": AssetReconciliationScenario(
-        assets=one_asset_one_partition,
-        unevaluated_runs=[],
-        expected_run_requests=[run_request(asset_keys=["asset1"], partition_key="a")],
-        cursor_from=AssetReconciliationScenario(
-            assets=one_asset,
             unevaluated_runs=[],
         ),
     ),
