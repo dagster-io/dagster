fd79c019e8df181c72129c47e84e906072f1bacf
yuhan <yuhan@elementl.com>
2023-04-12 00:12:28 -0700

    [pythonic resources] dagster-wand WandBResource

--- python_modules/libraries/dagster-wandb/dagster_wandb/resources.py
+++ python_modules/libraries/dagster-wandb/dagster_wandb/resources.py
@@ -1,28 +1,94 @@
 from typing import Any, Dict
 
 import wandb
-from dagster import Field, InitResourceContext, String, StringSource, resource
+from dagster import ConfigurableResource, InitResourceContext, resource
+from pydantic import Field
 from wandb.sdk.internal.internal_api import Api
 
 WANDB_CLOUD_HOST: str = "https://api.wandb.ai"
 
 
+class WandBResource(ConfigurableResource):
+    """Dagster resource used to communicate with the W&B API. It's useful when you want to use the
+    wandb client within your ops and assets. It's a required resources if you are using the W&B IO
+    Manager.
+
+    It automatically authenticates using the provided API key.
+
+    For a complete set of documentation, see `Dagster integration <https://docs.wandb.ai/guides/integrations/dagster>`_.
+
+    To configure this resource, we recommend using the `configured
+    <https://docs.dagster.io/concepts/configuration/configured>`_ method.
+
+    **Example:**
+
+    .. code-block:: python
+
+        from dagster import EnvVar, job
+        from dagster_wandb import WandBResource
+
+        @op
+        def my_wandb_op(wandb_resource: WandBResource):
+            wandb_resource.get_api()
+
+        @job
+        def my_wandb_job():
+            my_wandb_op()
+
+        defs = Definitions(
+            jobs=[my_wandb_job],
+            resources={
+                "wandb_resource": WandBResource(api_key=EnvVar("WANDB_API_KEY")),
+            },
+        )
+
+    """
+
+    api_key: str = Field(
+        description="W&B API key necessary to communicate with the W&B API.",
+    )
+    host: str = Field(
+        default=WANDB_CLOUD_HOST,
+        description="API host server you wish to use. Only required if you are using W&B Server.",
+    )
+
+    def login(self):
+        wandb.login(
+            key=self.api_key,
+            host=self.host,
+            anonymous="never",
+        )
+
+    def get_api(self) -> Api:
+        client_settings = wandb.Settings(
+            api_key=self.api_key,
+            base_url=self.host,
+            anonymous="never",
+            launch=True,
+        )
+        return Api(default_settings=client_settings, load_settings=False)
+
+    # api_key = context.resource_config["api_key"]
+    # host = context.resource_config["host"]
+    # wandb.login(
+    #     key=api_key,
+    #     host=host,
+    #     anonymous="never",
+    # )
+    # client_settings = wandb.Settings(
+    #     api_key=api_key,
+    #     base_url=host,
+    #     anonymous="never",
+    #     launch=True,
+    # )
+    # api = Api(default_settings=client_settings, load_settings=False)
+
+    def get_host(self) -> str:
+        return self.host
+
+
 @resource(
-    config_schema={
-        "api_key": Field(
-            StringSource,
-            description="W&B API key necessary to communicate with the W&B API.",
-            is_required=True,
-        ),
-        "host": Field(
-            String,
-            description=(
-                "API host server you wish to use. Only required if you are using W&B Server."
-            ),
-            is_required=False,
-            default_value=WANDB_CLOUD_HOST,
-        ),
-    },
+    config_schema=WandBResource.to_config_schema(),
     description="Resource for interacting with Weights & Biases",
 )
 def wandb_resource(context: InitResourceContext) -> Dict[str, Any]:
@@ -51,18 +117,11 @@ def wandb_resource(context: InitResourceContext) -> Dict[str, Any]:
             ...
 
     """
-    api_key = context.resource_config["api_key"]
-    host = context.resource_config["host"]
-    wandb.login(
-        key=api_key,
-        host=host,
-        anonymous="never",
-    )
-    client_settings = wandb.Settings(
-        api_key=api_key,
-        base_url=host,
-        anonymous="never",
-        launch=True,
-    )
-    api = Api(default_settings=client_settings, load_settings=False)
-    return {"sdk": wandb, "api": api, "host": host}
+    _wandb_resource = WandBResource.from_resource_context(context)
+    _wandb_resource.login()
+
+    return {
+        "sdk": wandb,
+        "api": _wandb_resource.get_api(),
+        "host": _wandb_resource.get_host(),
+    }
--- python_modules/libraries/dagster-wandb/dagster_wandb_tests/test_resources.py
+++ python_modules/libraries/dagster-wandb/dagster_wandb_tests/test_resources.py
@@ -1,7 +1,8 @@
 from unittest.mock import patch
 
-from dagster import build_init_resource_context
-from dagster_wandb.resources import WANDB_CLOUD_HOST, wandb_resource
+from dagster import build_init_resource_context, op
+from dagster._utils.test import wrap_op_in_graph_and_execute
+from dagster_wandb.resources import WANDB_CLOUD_HOST, WandBResource, wandb_resource
 
 API_KEY = "api_key"
 CUSTOM_HOST = "https://qa.platform.ai"
@@ -9,7 +10,7 @@ CUSTOM_HOST = "https://qa.platform.ai"
 
 @patch("dagster_wandb.resources.Api")
 @patch("dagster_wandb.resources.wandb")
-def test_wandb_resource(wandb, api):
+def test_legacy_wandb_resource(wandb, api):
     init_context = build_init_resource_context(config={"api_key": API_KEY})
     assert wandb_resource(init_context) == {
         "sdk": wandb,
@@ -42,6 +43,32 @@ def test_wandb_resource_custom_host(wandb, api):
     wandb.Settings.assert_called_with(
         api_key=API_KEY,
         base_url=CUSTOM_HOST,
+        anonymous="never",
+        launch=True,
+    )
+    api.assert_called_with(
+        default_settings=wandb.Settings.return_value,
+        load_settings=False,
+    )
+
+
+@patch("dagster_wandb.resources.Api")
+@patch("dagster_wandb.resources.wandb")
+def test_wandb_resource(wandb, api):
+    @op
+    def wandb_op(wandb_resource: WandBResource) -> None:
+        wandb_resource.login()
+        assert wandb_resource.get_api() == api.return_value
+
+    result = wrap_op_in_graph_and_execute(
+        wandb_op,
+        resources={"wandb_resource": WandBResource(api_key=API_KEY)},
+    )
+    assert result.success
+    wandb.login.assert_called_with(key=API_KEY, host=WANDB_CLOUD_HOST, anonymous="never")
+    wandb.Settings.assert_called_with(
+        api_key=API_KEY,
+        base_url=WANDB_CLOUD_HOST,
         anonymous="never",
         launch=True,
     )
