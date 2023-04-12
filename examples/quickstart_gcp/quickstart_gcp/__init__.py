3c37ae106a6dddc5f3cfa9ed5c2bc3f5399df58d
JamieDeMaria <jamie@elementl.com>
2023-04-12 12:23:46 -0400

    convert to using built in bigquery io manager

--- examples/quickstart_gcp/quickstart_gcp/__init__.py
+++ examples/quickstart_gcp/quickstart_gcp/__init__.py
@@ -4,9 +4,9 @@ from dagster import (
     define_asset_job,
     load_assets_from_package_module,
 )
+from dagster_gcp_pandas import bigquery_pandas_io_manager
 
 from . import assets
-from .io_managers import bigquery_pandas_io_manager
 
 daily_refresh_schedule = ScheduleDefinition(
     job=define_asset_job(name="all_assets_job"), cron_schedule="0 0 * * *"
@@ -19,10 +19,7 @@ defs = Definitions(
         # Read about using environment variables and secrets in Dagster:
         #   https://docs.dagster.io/guides/dagster/using-environment-variables-and-secrets
         "io_manager": bigquery_pandas_io_manager.configured(
-            {
-                "credentials": {"env": "BIGQUERY_SERVICE_ACCOUNT_CREDENTIALS"},
-                "project_id": {"env": "BIGQUERY_PROJECT_ID"},
-            }
+            {"project": {"env": "BIGQUERY_PROJECT_ID"}, "dataset": "hackernews"}
         ),
     },
     schedules=[daily_refresh_schedule],
