83ea1ae9a78ad88e86f150dd3ccf2d3eba8af7a3
Ben Pankow <ben@elementl.com>
2023-04-12 15:33:55 -0700

    [3/n][dagster-aws] Port S3Resource, S3IOManager to Pythonic resources (#13433)
    ## Summary
    
    Finishes off the `dagster-aws` migration by migrating the `S3IOManager`
    and `S3Resource` over.
    
    ## Test Plan
    
    Add new resources to existing unit tests.

--- python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_file_handle_to_s3.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/s3_tests/test_file_handle_to_s3.py
@@ -1,27 +1,55 @@
-from dagster import DagsterEventType, job, op
+import pytest
+from dagster import DagsterEventType, ResourceDefinition, job, op
 from dagster._core.definitions.metadata import MetadataValue
-from dagster_aws.s3 import S3FileHandle, file_handle_to_s3, s3_file_manager, s3_resource
+from dagster_aws.s3 import (
+    S3FileHandle,
+    S3FileManagerResource,
+    S3Resource,
+    file_handle_to_s3,
+    s3_file_manager,
+    s3_resource,
+)
 
 
-def create_file_handle_job(temp_file_handle):
+@pytest.fixture(name="s3_resource_type", params=[True, False])
+def s3_resource_type_fixture(request) -> ResourceDefinition:
+    if request.param:
+        return s3_resource
+    else:
+        return S3Resource.configure_at_launch()
+
+
+@pytest.fixture(name="s3_file_manager_resource_type", params=[True, False])
+def s3_file_manager_resource_type_fixture(request) -> ResourceDefinition:
+    if request.param:
+        return s3_file_manager
+    else:
+        return S3FileManagerResource.configure_at_launch()
+
+
+def create_file_handle_job(temp_file_handle, s3_resource_type, s3_file_manager_resource_type):
     @op
     def emit_temp_handle(_):
         return temp_file_handle
 
-    @job(resource_defs={"s3": s3_resource, "file_manager": s3_file_manager})
+    @job(resource_defs={"s3": s3_resource_type, "file_manager": s3_file_manager_resource_type})
     def test():
         file_handle_to_s3(emit_temp_handle())
 
     return test
 
 
-def test_successful_file_handle_to_s3(mock_s3_bucket):
+def test_successful_file_handle_to_s3(
+    mock_s3_bucket, s3_resource_type, s3_file_manager_resource_type
+):
     foo_bytes = b"foo"
     remote_s3_object = mock_s3_bucket.Object("some-key/foo")
     remote_s3_object.put(Body=foo_bytes)
 
     file_handle = S3FileHandle(mock_s3_bucket.name, "some-key/foo")
-    result = create_file_handle_job(file_handle).execute_in_process(
+    result = create_file_handle_job(
+        file_handle, s3_resource_type, s3_file_manager_resource_type
+    ).execute_in_process(
         run_config={
             "ops": {
                 "file_handle_to_s3": {"config": {"Bucket": mock_s3_bucket.name, "Key": "some-key"}}
