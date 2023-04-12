70e70092ff547265cc870bf810e5e68dd8fdfc29
JamieDeMaria <jamie@elementl.com>
2023-04-12 11:08:47 -0400

    update column names

--- examples/docs_snippets/docs_snippets/integrations/snowflake/pyspark_with_spark_session.py
+++ examples/docs_snippets/docs_snippets/integrations/snowflake/pyspark_with_spark_session.py
@@ -25,11 +25,11 @@ def iris_dataset() -> DataFrame:
 
     schema = StructType(
         [
-            StructField("Sepal length (cm)", DoubleType()),
-            StructField("Sepal width (cm)", DoubleType()),
-            StructField("Petal length (cm)", DoubleType()),
-            StructField("Petal width (cm)", DoubleType()),
-            StructField("Species", StringType()),
+            StructField("sepal_length_cm", DoubleType()),
+            StructField("sepal_width_cm", DoubleType()),
+            StructField("petal_length_cm", DoubleType()),
+            StructField("petal_width_cm", DoubleType()),
+            StructField("species", StringType()),
         ]
     )
 
