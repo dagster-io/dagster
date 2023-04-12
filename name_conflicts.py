c92110764ed1b85597d57d410ab6ece4ef64ada5
Nicholas Schrock <schrockn@elementl.com>
2023-04-12 14:27:05 -0400

    remove errant file

--- name_conflicts.py
+++
@@ -1,9 +0,0 @@
-from dagster import ConfigurableResource
-
-
-class IntVersionResource(ConfigurableResource):
-    required_resource_keys: int
-
-
-resource = IntVersionResource(required_resource_keys=3)
-print(resource.required_resource_keys)
