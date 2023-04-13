297dda20a705424b70d745ce96c1da91f17171d8
Johann Miller <johann@elementl.com>
2023-04-13 11:06:45 -0400

    Add AutoMaterailizePolicy to api docs (#13588)
    <img width="1209" alt="Screen Shot 2023-04-12 at 11 43 54 PM"
    src="https://user-images.githubusercontent.com/22018973/231643649-85985740-e5d1-4ce6-8732-09d2f228e64c.png">

--- python_modules/dagster/dagster/_core/definitions/auto_materialize_policy.py
+++ python_modules/dagster/dagster/_core/definitions/auto_materialize_policy.py
@@ -14,27 +14,35 @@ class AutoMaterializePolicy(NamedTuple):
 
     Regardless of this selection, there are some cases where an asset / partition will never be
     materialized:
-       - one or more of its parents is missing
-       - the partition is a time window partition with a start time that is more than
-            time_window_partition_scope older than the latest available time window partition. By
-            default, only the most recent time window partition will be materialized.
+
+    - one or more of its parents is missing
+    - the partition is a time window partition with a start time that is more than
+      time_window_partition_scope older than the latest available time window partition. By
+      default, only the most recent time window partition will be materialized.
 
     Outside of those cases...
 
     For eager reconciliation, an asset / partition will be (re)materialized when:
-       - it is missing
-       - it or any of its children have a FreshnessPolicy that require more up-to-date data than it
-            currently has
-       - any of its parent assets / partitions have been updated more recently than it has
+
+    - it is missing
+    - it or any of its children have a FreshnessPolicy that require more up-to-date data than it
+      currently has
+    - any of its parent assets / partitions have been updated more recently than it has
 
     For lazy reconciliation, an asset / partition will be (re)materialized when:
-       - it is missing
-       - it or any of its children have a FreshnessPolicy that require more up-to-date data than it
-            currently has
+
+    - it is missing
+    - it or any of its children have a FreshnessPolicy that require more up-to-date data than it
+      currently has
 
     In essence, an asset with eager reconciliation will always incorporate the most up-to-date
     version of its parents, while an asset with lazy reconciliation will only update when necessary
     in order to stay in line with the relevant FreshnessPolicies.
+
+    **Warning:**
+
+    Constructing an AutoMaterializePolicy directly is not recommended as the API is subject to change.
+    AutoMaterializePolicy.eager() and AutoMaterializePolicy.lazy() are the recommended API.
     """
 
     on_missing: bool
