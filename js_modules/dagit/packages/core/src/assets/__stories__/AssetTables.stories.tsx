b21ea8a6d864c3454073dbfd30a6625c30f2feb5
Ben Gotow <ben@foundry376.com>
2023-04-14 15:48:04 -0500

    [ui] Fix half-clipped headers on repo-scoped asset table (#13656)
    ## Summary & Motivation
    
    This PR renames the asset catalog table stories and adds a story for the
    repo-scoped asset table, which has slightly different columns and
    folding behavior. It fixes an issue with the repo-scoped asset table
    defining one too many columns and adjusts the column widths slightly.
    
    Thankfully these use the same virtualized rows and the storybooks are
    almost the same.
    
    Note: I also added an "id" value to some Workspace mocks, because the
    storybook was throwing exceptions without an id. I'm not 100% sure all
    of the places I added it are required, but I think specifying a constant
    value is correct and we shouldn't let our old faker-based Apollo mocks
    randomize the value.
    
    ## How I Tested These Changes
    
    ![image](https://user-images.githubusercontent.com/1037212/232147007-8fac39c3-6e67-4665-850a-f36109e5fec1.png)
    
    ![image](https://user-images.githubusercontent.com/1037212/232147025-0507aa79-83ee-4bbd-9dc0-edc68904a31c.png)
    
    ---------
    
    Co-authored-by: bengotow <bgotow@elementl.com>
    # Conflicts:
    #	js_modules/dagit/packages/core/src/assets/__fixtures__/AssetTables.fixtures.ts
    #	js_modules/dagit/packages/core/src/assets/__fixtures__/AssetsCatalogTable.fixtures.ts
    #	js_modules/dagit/packages/core/src/assets/__fixtures__/AssetsCatalogTable.mocks.ts
    #	js_modules/dagit/packages/core/src/assets/__stories__/AssetTables.stories.tsx

--- js_modules/dagit/packages/core/src/assets/__stories__/AssetTables.stories.tsx
+++ js_modules/dagit/packages/core/src/assets/__stories__/AssetTables.stories.tsx
@@ -1,16 +1,19 @@
 import {MockedProvider} from '@apollo/client/testing';
+import {Box} from '@dagster-io/ui';
 import React from 'react';
 
 import {StorybookProvider} from '../../testing/StorybookProvider';
+import {VirtualizedRepoAssetTable} from '../../workspace/VirtualizedRepoAssetTable';
 import {AssetsCatalogTable} from '../AssetsCatalogTable';
 import {
   AssetCatalogGroupTableMock,
   AssetCatalogTableMock,
+  AssetCatalogTableMockAssets,
   SingleAssetQueryLastRunFailed,
   SingleAssetQueryMaterializedStaleAndLate,
   SingleAssetQueryMaterializedWithLatestRun,
   SingleAssetQueryTrafficDashboard,
-} from '../__fixtures__/AssetsCatalogTable.mocks';
+} from '../__fixtures__/AssetTables.fixtures';
 
 // eslint-disable-next-line import/no-default-export
 export default {component: AssetsCatalogTable};
@@ -39,6 +42,25 @@ export const WithinAssetPath = () => {
     <StorybookProvider routerProps={{initialEntries: ['/']}}>
       <MockedProvider mocks={MOCKS}>
         <AssetsCatalogTable prefixPath={['dashboards']} setPrefixPath={() => {}} />
+      </MockedProvider>
+    </StorybookProvider>
+  );
+};
+
+export const RepoAssets = () => {
+  return (
+    <StorybookProvider routerProps={{initialEntries: ['/']}}>
+      <MockedProvider mocks={MOCKS}>
+        <Box flex={{direction: 'column'}} style={{height: '100%', overflow: 'hidden'}}>
+          <VirtualizedRepoAssetTable
+            repoAddress={{name: 'repo', location: 'test.py'}}
+            assets={AssetCatalogTableMockAssets.filter((a) => !!a.definition).map((a) => ({
+              id: a.id,
+              assetKey: a.key,
+              groupName: a.definition!.groupName,
+            }))}
+          />
+        </Box>
       </MockedProvider>
     </StorybookProvider>
   );
