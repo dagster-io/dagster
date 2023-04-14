4c9976c58bfef4a28f273ac267aad3a57ce01e4d
Ben Pankow <ben@elementl.com>
2023-04-14 15:24:50 -0700

    [docs][pythonic config] Add advanced config types page (#13210)
    ## Summary
    
    Adds a new page which covers more advanced config use-cases, most taken
    from the Pythonic config experimental guide.
    
    ---------
    
    Co-authored-by: Erin Cochran <erin.k.cochran@gmail.com>

--- examples/docs_snippets/docs_snippets/guides/dagster/pythonic_config/pythonic_config.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/pythonic_config/pythonic_config.py
@@ -27,6 +27,28 @@ def basic_resource_config() -> None:
     # end_basic_resource_config
 
 
+def permissive_schema_config() -> None:
+    # start_permissive_schema_config
+
+    from dagster import asset, PermissiveConfig
+    from typing import Optional
+    import requests
+
+    class FilterConfig(PermissiveConfig):
+        title: Optional[str] = None
+        description: Optional[str] = None
+
+    @asset
+    def filtered_listings(config: FilterConfig):
+        # extract all config fields, including those not defined in the schema
+        url_params = config.dict()
+        return requests.get("https://my-api.com/listings", params=url_params).json()
+
+    # can pass in any fields, including those not defined in the schema
+    filtered_listings(FilterConfig(title="hotel", beds=4))  # type: ignore
+    # end_permissive_schema_config
+
+
 def execute_with_config() -> None:
     # start_basic_op_config
 
@@ -170,8 +192,7 @@ def union_schema_config() -> None:
         barks: float
 
     class ConfigWithUnion(Config):
-        # Here, the ellipses `...` indicates that the field is required and has no default value.
-        pet: Union[Cat, Dog] = Field(..., discriminator="pet_type")
+        pet: Union[Cat, Dog] = Field(discriminator="pet_type")
 
     @asset
     def pet_stats(config: ConfigWithUnion):
@@ -199,13 +220,10 @@ def metadata_config() -> None:
     from pydantic import Field
 
     class MyMetadataConfig(Config):
-        # Here, the ellipses `...` indicates that the field is required and has no default value.
-        person_name: str = Field(..., description="The name of the person to greet")
-        age: int = Field(
-            ..., gt=0, lt=100, description="The age of the person to greet"
-        )
+        person_name: str = Field(description="The name of the person to greet")
+        age: int = Field(gt=0, lt=100, description="The age of the person to greet")
 
-    # errors!
+    # errors, since age is not in the valid range!
     MyMetadataConfig(person_name="Alice", age=200)
 
     # end_metadata_config
@@ -216,10 +234,15 @@ def optional_config() -> None:
 
     from typing import Optional
     from dagster import asset, Config, materialize, RunConfig
+    from pydantic import Field
 
     class MyAssetConfig(Config):
         person_name: Optional[str] = None
-        greeting_phrase: str = "hello"
+
+        # can pass default to pydantic.Field to attach metadata to the field
+        greeting_phrase: str = Field(
+            default="hello", description="The greeting phrase to use."
+        )
 
     @asset
     def greeting(config: MyAssetConfig) -> str:
@@ -271,3 +294,124 @@ def execute_with_bad_config() -> None:
     )
 
     # end_execute_with_bad_config
+
+
+def enum_schema_config() -> None:
+    # start_enum_schema_config
+
+    from dagster import Config, RunConfig, op, job
+    from enum import Enum
+
+    class UserPermissions(Enum):
+        GUEST = "guest"
+        MEMBER = "member"
+        ADMIN = "admin"
+
+    class ProcessUsersConfig(Config):
+        users_list: Dict[str, UserPermissions]
+
+    @op
+    def process_users(config: ProcessUsersConfig):
+        for user, permission in config.users_list.items():
+            if permission == UserPermissions.ADMIN:
+                print(f"{user} is an admin")  # noqa: T201
+
+    @job
+    def process_users_job():
+        process_users()
+
+    op_result = process_users_job.execute_in_process(
+        run_config=RunConfig(
+            {
+                "process_users": ProcessUsersConfig(
+                    users_list={
+                        "Bob": UserPermissions.GUEST,
+                        "Alice": UserPermissions.ADMIN,
+                    }
+                )
+            }
+        ),
+    )
+    # end_enum_schema_config
+
+
+def validated_schema_config() -> None:
+    # start_validated_schema_config
+
+    from dagster import Config, RunConfig, op, job
+    from pydantic import validator
+
+    class UserConfig(Config):
+        name: str
+        username: str
+
+        @validator("name")
+        def name_must_contain_space(cls, v):
+            if " " not in v:
+                raise ValueError("must contain a space")
+            return v.title()
+
+        @validator("username")
+        def username_alphanumeric(cls, v):
+            assert v.isalnum(), "must be alphanumeric"
+            return v
+
+    executed = {}
+
+    @op
+    def greet_user(config: UserConfig) -> None:
+        print(f"Hello {config.name}!")  # noqa: T201
+        executed["greet_user"] = True
+
+    @job
+    def greet_user_job() -> None:
+        greet_user()
+
+    # Input is valid, so this will work
+    op_result = greet_user_job.execute_in_process(
+        run_config=RunConfig(
+            {"greet_user": UserConfig(name="Alice Smith", username="alice123")}
+        ),
+    )
+
+    # Name has no space, so this will fail
+    op_result = greet_user_job.execute_in_process(
+        run_config=RunConfig(
+            {"greet_user": UserConfig(name="John", username="johndoe44")}
+        ),
+    )
+
+    # end_validated_schema_config
+
+
+def required_config() -> None:
+    # start_required_config
+    from typing import Optional, Callable
+    from dagster import asset, Config
+    from pydantic import Field
+
+    class MyAssetConfig(Config):
+        # ellipsis indicates that even though the type is Optional,
+        # an input is required
+        person_first_name: Optional[str] = ...  # type: ignore
+
+        # ellipsis can also be used with pydantic.Field to attach metadata
+        person_last_name: Optional[Callable] = Field(
+            default=..., description="The last name of the person to greet"
+        )
+
+    @asset
+    def goodbye(config: MyAssetConfig) -> str:
+        full_name = f"{config.person_first_name} {config.person_last_name}".strip()
+        if full_name:
+            return f"Goodbye, {full_name}"
+        else:
+            return "Goodbye"
+
+    # errors, since person_first_name and person_last_name are required
+    goodbye(MyAssetConfig())
+
+    # works, since both person_first_name and person_last_name are provided
+    goodbye(MyAssetConfig(person_first_name="Alice", person_last_name=None))
+
+    # end_required_config
