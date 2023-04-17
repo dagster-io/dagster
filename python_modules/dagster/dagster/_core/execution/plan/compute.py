f8a7417a504bcc08b7caf0be36c984e6687f758d
Alex Langenfeld <alex@elementl.com>
2023-04-17 15:53:22 -0500

    [asyncio] dont use asyncio.run (#13484)
    Overlooked in #12785, was the fact that that `run` finalizes async
    generators which in our use case may still be active.
    
    https://docs.python.org/3/library/asyncio-runner.html#asyncio.run
    
    
    ## How I Tested These Changes
    
    added tests that were failing

--- python_modules/dagster/dagster/_core/execution/plan/compute.py
+++ python_modules/dagster/dagster/_core/execution/plan/compute.py
@@ -117,11 +117,17 @@ def _validate_event(event: Any, step_context: StepExecutionContext) -> OpOutputU
 
 
 def gen_from_async_gen(async_gen: AsyncIterator[T]) -> Iterator[T]:
-    while True:
-        try:
-            yield asyncio.run(async_gen.__anext__())  # type: ignore # subtle awaitable vs coroutine issue
-        except StopAsyncIteration:
-            return
+    # prime use for asyncio.Runner, but new in 3.11 and did not find appealing backport
+    loop = asyncio.new_event_loop()
+    try:
+        while True:
+            try:
+                yield loop.run_until_complete(async_gen.__anext__())
+            except StopAsyncIteration:
+                return
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
 
 
 def _yield_compute_results(
