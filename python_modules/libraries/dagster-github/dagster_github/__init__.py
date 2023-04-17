70fff040d8baf3faba180ff6912f014dabfc5da7
Nick Schrock <schrockn@elementl.com>
2023-04-17 19:34:41 -0400

    GithubClientResource --> GithubResource (#13701)
    ## Summary & Motivation
    
    Per naming conventions, GithubClientResource --> GithubResource
    
    ## How I Tested These Changes
    
    BK

--- python_modules/libraries/dagster-github/dagster_github/__init__.py
+++ python_modules/libraries/dagster-github/dagster_github/__init__.py
@@ -1,8 +1,8 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-from .resources import GithubClientResource, github_resource
+from .resources import GithubResource, github_resource
 from .version import __version__
 
 DagsterLibraryRegistry.register("dagster-github", __version__)
 
-__all__ = ["github_resource", "GithubClientResource"]
+__all__ = ["github_resource", "GithubResource"]
