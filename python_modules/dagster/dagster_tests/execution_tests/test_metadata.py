dagster-io/dagster

commit 60a090e766fc3f2502499230b4f506ce7a32e2c1

[refactor] Update internal representation of metadata to always use metadata dict (#12770)
### Summary & Motivation

Internal companion PR: https://github.com/dagster-io/internal/pull/5257

Update internal representation of `metadata` from `metadata_entries` to
the dict form we have been pushing to users for almost a year.

- Remove `metadata_entries` from all non-public constructor args
- Introduce `MetadataSerializer` custom field serializer to serialize
metadata dict to list of metadata entries and deserialize list of
metadata entries to metadata dict (for backcompat). Apply to all serdes
classes holding `metadata_entries`

**Context**

More than a year ago, it was decided that Dagster's preferred
representation of metadata was to be the metadata dict, as opposed to
the metadata entries list:

```
### metadata dict (yes)
{
    "foo": MetadataValue.text("bar"),
    "baz": MetadataValue.int(1)
}

### metadata entries list (no)
[
    MetadataEntry("foo", value=MetadataValue.text("bar")),
    MetadataEntry("baz", value=MetadataValue.int(1)),
]
```

Reasons for this transition:

- metadata dict is more user-friendly
- we deprecated the `description` field on `MetadataEntry` so
`MetadataEntry` no longer had any greater expressive power over a dict

There was a hitch in this plan though-- `metadata_entries:
Sequence[MetadataEntry]` was widely used as an internal representation
of metadata throughout the codebase, including on many
serdes-whitelisted objects. So we couldn't just replace the metadata
entry list everywhere. Instead, we decided on a temporary solution of:

- offer `metadata` kwarg on user-facing APIs to allow them to pass a
metadata dict
- internally convert to list of metadata entries
- keep using list of metadata entries everywhere for internal repr

This has a few problems:

- we have a lot of constructors that take either `metadata` or

python_modules/dagster/dagster/_core/snap/node.py
@@ -12,7 +12,9 @@ from dagster._core.definitions import (
     PipelineDefinition,
 )
 from dagster._core.definitions.metadata import (
-    MetadataEntry,
+    MetadataFieldSerializer,
+    MetadataValue,
+    normalize_metadata,
 )
 from dagster._serdes import whitelist_for_serdes
 
@@ -22,7 +24,11 @@ from .dep_snapshot import (
 )
 
 
-@whitelist_for_serdes(skip_when_empty_fields={"metadata_entries"})
+@whitelist_for_serdes(
+    storage_field_names={"metadata": "metadata_entries"},
+    field_serializers={"metadata": MetadataFieldSerializer},
+    skip_when_empty_fields={"metadata"},
+)
 class InputDefSnap(
     NamedTuple(
         "_InputDefSnap",
@@ -30,7 +36,7 @@ class InputDefSnap(
             ("name", str),
             ("dagster_type_key", str),
             ("description", Optional[str]),
-            ("metadata_entries", Sequence[MetadataEntry]),
+            ("metadata", Mapping[str, MetadataValue]),
         ],
     )
 ):
@@ -39,20 +45,24 @@ class InputDefSnap(
         name: str,
         dagster_type_key: str,
         description: Optional[str],
-        metadata_entries: Optional[Sequence[MetadataEntry]] = None,
+        metadata: Optional[Mapping[str, MetadataValue]] = None,
     ):
         return super(InputDefSnap, cls).__new__(
             cls,
             name=check.str_param(name, "name"),
             dagster_type_key=check.str_param(dagster_type_key, "dagster_type_key"),
             description=check.opt_str_param(description, "description"),
-            metadata_entries=check.opt_sequence_param(
-                metadata_entries, "metadata_entries", of_type=MetadataEntry
+            metadata=normalize_metadata(
+                check.opt_mapping_param(metadata, "metadata", key_type=str)
             ),
         )
 
 
-@whitelist_for_serdes(skip_when_empty_fields={"metadata_entries"})
+@whitelist_for_serdes(
+    storage_field_names={"metadata": "metadata_entries"},
+    field_serializers={"metadata": MetadataFieldSerializer},
+    skip_when_empty_fields={"metadata"},
+)
 class OutputDefSnap(
     NamedTuple(
         "_OutputDefSnap",
@@ -61,7 +71,7 @@ class OutputDefSnap(
             ("dagster_type_key", str),
             ("description", Optional[str]),
             ("is_required", bool),
-            ("metadata_entries", Sequence[MetadataEntry]),
+            ("metadata", Mapping[str, MetadataValue]),
             ("is_dynamic", bool),
         ],
     )
@@ -72,7 +82,7 @@ class OutputDefSnap(
         dagster_type_key: str,
         description: Optional[str],
         is_required: bool,
-        metadata_entries: Optional[Sequence[MetadataEntry]] = None,
+        metadata: Optional[Mapping[str, MetadataValue]] = None,
         is_dynamic: bool = False,
     ):
         return super(OutputDefSnap, cls).__new__(
@@ -81,8 +91,8 @@ class OutputDefSnap(
             dagster_type_key=check.str_param(dagster_type_key, "dagster_type_key"),
             description=check.opt_str_param(description, "description"),
             is_required=check.bool_param(is_required, "is_required"),
-            metadata_entries=check.opt_sequence_param(
-                metadata_entries, "metadata_entries", of_type=MetadataEntry
+            metadata=normalize_metadata(
+                check.opt_mapping_param(metadata, "metadata", key_type=str)
             ),
             is_dynamic=check.bool_param(is_dynamic, "is_dynamic"),
         )
@@ -155,7 +165,7 @@ def build_input_def_snap(input_def: InputDefinition) -> InputDefSnap:
         name=input_def.name,
         dagster_type_key=input_def.dagster_type.key,
         description=input_def.description,
-        metadata_entries=input_def.metadata_entries,
+        metadata=input_def.metadata,
     )
 
 
@@ -166,7 +176,7 @@ def build_output_def_snap(output_def: OutputDefinition) -> OutputDefSnap:
         dagster_type_key=output_def.dagster_type.key,
         description=output_def.description,
         is_required=output_def.is_required,
-        metadata_entries=output_def.metadata_entries,
+        metadata=output_def.metadata,
         is_dynamic=output_def.is_dynamic,
     )
 

python_modules/dagster/dagster_tests/execution_tests/test_metadata.py
@@ -20,7 +20,7 @@ from dagster._check import CheckError
 from dagster._core.definitions.decorators import op
 from dagster._core.definitions.metadata import (
     DagsterInvalidMetadata,
-    MetadataEntry,
+    TableMetadataValue,
     normalize_metadata,
 )
 from dagster._core.definitions.metadata.table import (
@@ -45,12 +45,6 @@ def solid_events_for_type(
         for compute_step_event in solid_result.compute_step_events
         if compute_step_event.event_type == event_type
     ]
-
-
-def test_metadata_entry_construction():
-    entry = MetadataEntry("foo", value=MetadataValue.text("bar"))
-    assert entry.label == "foo"
-    assert entry.value == MetadataValue.text("bar")
 
 
 def test_metadata_asset_materialization():
@@ -82,8 +76,8 @@ def test_metadata_asset_materialization():
     )
     assert len(materialization_events) == 1
     materialization = materialization_events[0].event_specific_data.materialization
-    assert len(materialization.metadata_entries) == 6
-    entry_map = {entry.label: entry.value.__class__ for entry in materialization.metadata_entries}
+    assert len(materialization.metadata) == 6
+    entry_map = {k: v.__class__ for k, v in materialization.metadata.items()}
     assert entry_map["text"] == TextMetadataValue
     assert entry_map["int"] == IntMetadataValue
     assert entry_map["url"] == UrlMetadataValue
@@ -120,8 +114,8 @@ def test_metadata_asset_observation():
     )
     assert len(observation_events) == 1
     observation = observation_events[0].event_specific_data.asset_observation
-    assert len(observation.metadata_entries) == 5
-    entry_map = {entry.label: entry.value.__class__ for entry in observation.metadata_entries}
+    assert len(observation.metadata) == 5
+    entry_map = {k: v.__class__ for k, v in observation.metadata.items()}
     assert entry_map["text"] == TextMetadataValue
     assert entry_map["int"] == IntMetadataValue
     assert entry_map["url"] == UrlMetadataValue
@@ -154,37 +148,30 @@ def test_unknown_metadata_value():
 
 def test_parse_null_metadata():
     metadata = {"foo": None}
-    entries = normalize_metadata(metadata, [])
-    assert entries[0].label == "foo"
-    assert entries[0].value == NullMetadataValue()
+    normalized = normalize_metadata(metadata)
+    assert normalized["foo"] == NullMetadataValue()
 
 
 def test_parse_list_metadata():
     metadata = {"foo": ["bar"]}
-    entries = normalize_metadata(metadata, [])
-    assert entries[0].label == "foo"
-    assert entries[0].value == JsonMetadataValue(["bar"])
+    normalized = normalize_metadata(metadata)
+    assert normalized["foo"] == JsonMetadataValue(["bar"])
 
 
 def test_parse_invalid_metadata():
     metadata = {"foo": object()}
 
     with pytest.raises(DagsterInvalidMetadata) as _exc_info:
-        normalize_metadata(metadata, [])
+        normalize_metadata(metadata)
 
-    entries = normalize_metadata(metadata, [], allow_invalid=True)
-    assert len(entries) == 1
-    assert entries[0].label == "foo"
-    assert entries[0].value == TextMetadataValue("[object] (unserializable)")
+    normalized = normalize_metadata(metadata, allow_invalid=True)
+    assert normalized["foo"] == TextMetadataValue("[object] (unserializable)")
 
 
 def test_parse_path_metadata():
     metadata = {"path": Path("/a/b.csv")}
-
-    entries = normalize_metadata(metadata, [])
-    assert len(entries) == 1
-    assert entries[0].label == "path"
-    assert entries[0].value == PathMetadataValue("/a/b.csv")
+    normalized = normalize_metadata(metadata)
+    assert normalized["path"] == PathMetadataValue("/a/b.csv")
 
 
 def test_bad_json_metadata_value():
@@ -210,17 +197,14 @@ def test_bad_json_metadata_value():
 
 
 def test_table_metadata_value_schema_inference():
-    table_metadata_entry = MetadataEntry(
-        "foo",
-        value=MetadataValue.table(
-            records=[
-                TableRecord(data=dict(name="foo", status=False)),
-                TableRecord(data=dict(name="bar", status=True)),
-            ],
-        ),
+    table = MetadataValue.table(
+        records=[
+            TableRecord(data=dict(name="foo", status=False)),
+            TableRecord(data=dict(name="bar", status=True)),
+        ],
     )
 
-    schema = table_metadata_entry.value.schema
+    schema = table.schema
     assert isinstance(schema, TableSchema)
     assert schema.columns == [
         TableColumn(name="name", type="string"),
@@ -355,16 +339,13 @@ def test_table_schema_from_name_type_dict():
 
 
 def test_table_serialization():
-    entry = MetadataEntry(
-        "foo",
-        value=MetadataValue.table(
-            records=[
-                TableRecord(dict(foo=1, bar=2)),
-            ],
-        ),
+    table_metadata = MetadataValue.table(
+        records=[
+            TableRecord(dict(foo=1, bar=2)),
+        ],
     )
-    serialized = serialize_value(entry)
-    assert deserialize_value(serialized, MetadataEntry) == entry
+    serialized = serialize_value(table_metadata)
+    assert deserialize_value(serialized, TableMetadataValue) == table_metadata
 
 
 def test_bool_metadata_value():
@@ -389,6 +370,6 @@ def test_bool_metadata_value():
     )
     assert len(materialization_events) == 1
     materialization = materialization_events[0].event_specific_data.materialization
-    entry_map = {entry.label: entry.value.__class__ for entry in materialization.metadata_entries}
+    entry_map = {k: v.__class__ for k, v in materialization.metadata.items()}
     assert entry_map["first_bool"] == BoolMetadataValue
     assert entry_map["second_bool"] == BoolMetadataValue

commit e7e3a5b2d5e0082cc6d30e4854db91732c3ace06

[bugfix] Allow invalid metadata in {Input,Output}DefSnap (#13703)
## Summary & Motivation

Allow invalid metadata when normalizing metadata input to
`OutputDefSnap` and `InputDefSnap`. This is beacuse `InputDefinition`
and `OutputDefinition` allow invalid metadata. Fixes a bug where
arbitrary (non-json-serializable) metadata passed to an
`OutputDefinition` (e.g. via an `@asset` would cause an error during
snap creation, introduced by #12770 (prior to that PR no normalization
occurred at snap creation).

Fixes this:
https://elementl-workspace.slack.com/archives/C03D094F23G/p1681416720082509

## How I Tested These Changes

Added new test.

python_modules/dagster/dagster/_core/snap/node.py
@@ -53,7 +53,7 @@ class InputDefSnap(
             dagster_type_key=check.str_param(dagster_type_key, "dagster_type_key"),
             description=check.opt_str_param(description, "description"),
             metadata=normalize_metadata(
-                check.opt_mapping_param(metadata, "metadata", key_type=str)
+                check.opt_mapping_param(metadata, "metadata", key_type=str), allow_invalid=True
             ),
         )
 
@@ -92,7 +92,7 @@ class OutputDefSnap(
             description=check.opt_str_param(description, "description"),
             is_required=check.bool_param(is_required, "is_required"),
             metadata=normalize_metadata(
-                check.opt_mapping_param(metadata, "metadata", key_type=str)
+                check.opt_mapping_param(metadata, "metadata", key_type=str), allow_invalid=True
             ),
             is_dynamic=check.bool_param(is_dynamic, "is_dynamic"),
         )

python_modules/dagster/dagster_tests/execution_tests/test_metadata.py
@@ -19,6 +19,8 @@ from dagster import (
     op,
 )
 from dagster._check import CheckError
+from dagster._core.definitions.decorators.asset_decorator import asset
+from dagster._core.definitions.definitions_class import Definitions
 from dagster._core.definitions.metadata import (
     DagsterInvalidMetadata,
     TableMetadataValue,
@@ -32,6 +34,7 @@ from dagster._core.definitions.metadata.table import (
     TableSchema,
 )
 from dagster._core.execution.execution_result import ExecutionResult
+from dagster._core.snap.node import build_node_defs_snapshot
 from dagster._serdes.serdes import deserialize_value, serialize_value
 
 
@@ -367,3 +370,15 @@ def test_bool_metadata_value():
     entry_map = {k: v.__class__ for k, v in materialization.metadata.items()}
     assert entry_map["first_bool"] == BoolMetadataValue
     assert entry_map["second_bool"] == BoolMetadataValue
+
+
+def test_snapshot_arbitrary_metadata():
+    # Asset decorator accepts arbitrary metadata. Need to ensure this doesn't throw an error when a
+    # snap is created.
+    @asset(metadata={"my_key": [object()]})
+    def foo_asset():
+        pass
+
+    assert build_node_defs_snapshot(
+        Definitions(assets=[foo_asset]).get_implicit_global_asset_job_def()
+    )