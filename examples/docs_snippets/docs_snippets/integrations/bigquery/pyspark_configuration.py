b48dfa7b4a4e456b97778dd7cd8ce4d55cc81e0b
jamiedemaria <demaria.jamie@gmail.com>
2023-04-18 12:25:29 -0400

    [docs] bigqueryy tutorial and guide to new resource system (#13340)
    (cherry picked from commit e16935e94d3d3a0d2cf5178badeb607f6a74311e)

--- examples/docs_snippets/docs_snippets/integrations/bigquery/pyspark_configuration.py
+++ examples/docs_snippets/docs_snippets/integrations/bigquery/pyspark_configuration.py
@@ -1,21 +1,25 @@
-iris_data = None
+from dagster import asset
+
+
+@asset
+def iris_data():
+    return None
+
 
 # start_configuration
 
-from dagster_gcp_pyspark import bigquery_pyspark_io_manager
+from dagster_gcp_pyspark import BigQueryPySparkIOManager
 
 from dagster import Definitions
 
 defs = Definitions(
-    assets=[iris_data],  # type: ignore  # (didactic)
+    assets=[iris_data],
     resources={
-        "io_manager": bigquery_pyspark_io_manager.configured(
-            {
-                "project": "my-gcp-project",  # required
-                "location": "us-east5",  # optional, defaults to the default location for the project - see https://cloud.google.com/bigquery/docs/locations for a list of locations
-                "data": "IRIS",  # optional, defaults to PUBLIC
-                "temporary_gcs_bucket": "my-gcs-bucket",  # optional, defaults to None, which will result in a direct write to BigQuery
-            }
+        "io_manager": BigQueryPySparkIOManager(
+            project="my-gcp-project",  # required
+            location="us-east5",  # optional, defaults to the default location for the project - see https://cloud.google.com/bigquery/docs/locations for a list of locations
+            dataset="IRIS",  # optional, defaults to PUBLIC
+            temporary_gcs_bucket="my-gcs-bucket",  # optional, defaults to None, which will result in a direct write to BigQuery
         )
     },
 )
--- examples/docs_snippets/docs_snippets/integrations/bigquery/resource.py
+++ examples/docs_snippets/docs_snippets/integrations/bigquery/resource.py
@@ -10,8 +10,8 @@ from dagster import Definitions, asset
 def small_petals(context):
     return context.resources.bigquery.query(
         (
-            'SELECT * FROM IRIS.IRIS_DATA WHERE "Petal length (cm)" < 1 AND "Petal'
-            ' width (cm)" < 1'
+            'SELECT * FROM IRIS.IRIS_DATA WHERE "petal_length_cm" < 1 AND'
+            ' "petal_width_cm" < 1'
         ),
     ).result()
 
