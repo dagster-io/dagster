d21153b257c0466de1957488cd14cea674d315d5
benpankow <ben@elementl.com>
2023-04-18 14:16:59 -0700

    add old page

--- examples/docs_snippets/docs_snippets/concepts/assets/asset_io_manager.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_io_manager.py
@@ -25,3 +25,26 @@ defs = Definitions(
 
 
 # end_marker
+def old():
+    # start_old_marker
+    from dagster_aws.s3 import s3_pickle_io_manager, s3_resource
+
+    from dagster import Definitions, asset
+
+
+    @asset
+    def upstream_asset():
+        return [1, 2, 3]
+
+
+    @asset
+    def downstream_asset(upstream_asset):
+        return upstream_asset + [4]
+
+
+    defs = Definitions(
+        assets=[upstream_asset, downstream_asset],
+        resources={"io_manager": s3_pickle_io_manager, "s3": s3_resource},
+    )
+
+    # end_old_marker
