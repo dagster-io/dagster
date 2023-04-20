461e45f628a9755e57c99db7cd371c683809ce5f
Marco Salazar <marco@elementl.com>
2023-04-20 16:38:33 -0400

    rebase and update tests

--- js_modules/dagit/packages/core/src/runs/RunsFilterInput.tsx
+++ js_modules/dagit/packages/core/src/runs/RunsFilterInput.tsx
@@ -292,7 +292,7 @@ export const RUN_TAG_KEYS_QUERY = gql`
   }
 `;
 
-const RUN_TAG_VALUES_QUERY = gql`
+export const RUN_TAG_VALUES_QUERY = gql`
   query RunTagValuesQuery($tagKeys: [String!]!) {
     runTagsOrError(tagKeys: $tagKeys) {
       __typename
