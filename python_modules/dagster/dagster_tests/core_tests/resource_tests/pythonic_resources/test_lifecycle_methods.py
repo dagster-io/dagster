44ae3706e8871166dcd43668070b746760a6f70a
benpankow <ben@elementl.com>
2023-04-26 15:48:05 -0700

    more complex tests

--- python_modules/dagster/dagster_tests/core_tests/resource_tests/pythonic_resources/test_lifecycle_methods.py
+++ python_modules/dagster/dagster_tests/core_tests/resource_tests/pythonic_resources/test_lifecycle_methods.py
@@ -1,3 +1,4 @@
+import contextlib
 from typing import Any, Dict, Generator
 
 import pytest
@@ -42,6 +43,7 @@ def test_basic_yield() -> None:
     log = []
 
     class MyResource(ConfigurableResource):
+        @contextlib.contextmanager
         def yield_for_execution(
             self, context: InitResourceContext
         ) -> Generator["MyResource", None, None]:
@@ -102,6 +104,7 @@ def test_basic_yield_multi_op() -> None:
     log = []
 
     class MyResource(ConfigurableResource):
+        @contextlib.contextmanager
         def yield_for_execution(
             self, context: InitResourceContext
         ) -> Generator["MyResource", None, None]:
@@ -197,6 +200,36 @@ def test_pre_execute_with_error() -> None:
     ]
 
 
+def test_post_execute_with_error() -> None:
+    # If an error occurs in post_execute, this error will manifest as a DagsterResourceFunctionError
+
+    log = []
+
+    class MyResource(ConfigurableResource):
+        def pre_execute(self, context: InitResourceContext) -> None:
+            log.append("pre_execute")
+
+        def post_execute(self, context: InitResourceContext) -> None:
+            log.append("post_execute")
+            raise Exception("foo")
+
+    @op
+    def my_hello_world_op(res: MyResource):
+        log.append("my_hello_world_op")
+
+    @job(resource_defs={"res": MyResource()})
+    def hello_world_job() -> None:
+        my_hello_world_op()
+
+    hello_world_job.execute_in_process()
+
+    assert log == [
+        "pre_execute",
+        "my_hello_world_op",
+        "post_execute",
+    ]
+
+
 def test_basic_init_with_privateattr() -> None:
     log = []
 
@@ -235,4 +268,68 @@ def test_basic_init_with_privateattr() -> None:
     assert log == [
         "pre_execute with foo and bar",
         "query select * from table with foo and bar",
+    ]
+
+
+def test_nested_resources_init_with_privateattr() -> None:
+    log = []
+
+    def fetch_jwt(access_key: str, secret_key: str) -> str:
+        log.append(f"fetch_jwt with {access_key} and {secret_key}")
+        return "my_jwt"
+
+    class S3Client:
+        def __init__(self, jwt: str):
+            self.jwt = jwt
+
+    class AWSCredentialsResource(ConfigurableResource):
+        access_key: str
+        secret_key: str
+
+        _jwt: str = PrivateAttr()
+
+        def pre_execute(self, context: InitResourceContext) -> None:
+            self._jwt = fetch_jwt(self.access_key, self.secret_key)
+
+        @property
+        def jwt(self) -> str:
+            return self._jwt
+
+    class S3Resource(ConfigurableResource):
+        credentials: AWSCredentialsResource
+
+        _s3_client: Any = PrivateAttr()
+
+        def pre_execute(self, context: InitResourceContext) -> None:
+            log.append(f"pre_execute with jwt {self.credentials.jwt}")
+            self._s3_client = S3Client(self.credentials.jwt)
+
+        def get_object(self, bucket: str, key: str) -> Dict[str, Any]:
+            log.append(f"get_object {bucket} {key} with jwt {self.credentials.jwt}")
+            return {"foo": "bar"}
+
+    @op
+    def load_from_s3_op(s3: S3Resource) -> Dict[str, Any]:
+        log.append("load_from_s3_op")
+        res = s3.get_object("my-bucket", "my-key")
+        assert res == {"foo": "bar"}
+        return res
+
+    @job(
+        resource_defs={
+            "s3": S3Resource(
+                credentials=AWSCredentialsResource(access_key="my_key", secret_key="my_secret")
+            )
+        }
+    )
+    def load_from_s3_job() -> None:
+        load_from_s3_op()
+
+    result = load_from_s3_job.execute_in_process()
+    assert result.success
+    assert log == [
+        "fetch_jwt with my_key and my_secret",
+        "pre_execute with jwt my_jwt",
+        "load_from_s3_op",
+        "get_object my-bucket my-key with jwt my_jwt",
     ]
