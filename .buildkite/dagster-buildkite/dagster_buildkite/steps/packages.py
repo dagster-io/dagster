2daeb731b3176db367472d23f9b3bc6a12395a3d
gibsondan <daniel@elementl.com>
2023-04-27 21:28:07 -0500

    fix dagster-ge tests (#13978)
    summary:
    - update snapshots
    - remove py311 testing (ge does not claim to have py311 support)
    
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- .buildkite/dagster-buildkite/dagster_buildkite/steps/packages.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/packages.py
@@ -542,6 +542,13 @@ LIBRARY_PACKAGES_WITH_CUSTOM_CONFIG: List[PackageSpec] = [
         ],
     ),
     PackageSpec(
+        "python_modules/libraries/dagster-ge",
+        unsupported_python_versions=[
+            # great-expectations not yet supported on 3.11
+            AvailablePythonVersion.V3_11,
+        ],
+    ),
+    PackageSpec(
         "python_modules/libraries/dagster-k8s",
         env_vars=[
             "AWS_ACCOUNT_ID",
