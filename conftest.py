608a1864a5eca5a32290e81fe72c8f058a9a8197
Jordan Sanders <jordan@elementl.com>
2023-05-05 12:31:00 -0500

    Split pytest logs into their own Buildkite section
    [NO_SKIP=true]

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import os
+
+
+def pytest_configure():
+    # Create a section break in the logs any time Buildkite invokes pytest
+    # https://buildkite.com/docs/pipelines/managing-log-output
+    # https://docs.pytest.org/en/7.1.x/reference/reference.html?highlight=pytest_configure#pytest.hookspec.pytest_configure
+    if os.getenv("BUILDKITE"):
+        print("+++ Running :pytest: PyTest")
