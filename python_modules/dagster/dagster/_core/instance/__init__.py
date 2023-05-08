18ae6cd8bb4695f98fb78b6388a711f0b518db47
gibsondan <daniel@elementl.com>
2023-05-08 12:47:27 -0500

    Add a timeout for runs stuck in canceling (#14084)
    Summary:
    Just like for STARTING. only question I have is whether or not the point
    query on the event log to determine the CANCELING time is likely to run
    into perf issues, or if the fact that it's filtering on a specific event
    type will help.
    
    Test Plan: BK, simulate a real hanging cancel in `dagster dev` by
    commenting out the line that cancels the run and waiting 3 minutes
    
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- python_modules/dagster/dagster/_core/instance/__init__.py
+++ python_modules/dagster/dagster/_core/instance/__init__.py
@@ -799,6 +799,10 @@ class DagsterInstance(DynamicPartitionsStore):
         return self.run_monitoring_settings.get("start_timeout_seconds", 180)
 
     @property
+    def run_monitoring_cancel_timeout_seconds(self) -> int:
+        return self.run_monitoring_settings.get("cancel_timeout_seconds", 180)
+
+    @property
     def code_server_settings(self) -> Any:
         return self.get_settings("code_servers")
 
