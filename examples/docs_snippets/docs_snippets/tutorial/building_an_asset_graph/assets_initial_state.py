416b8a7b1aab7c63fd978e705fa65d80c9f30b83
Tim Castillo <tim@elementl.com>
2023-05-09 14:29:04 -0400

    [docs] [tutorial] Removing the dependency on wordcloud (#13947)
    ## Summary & Motivation
    
    The `wordcloud` package hasn't been updated in a while and some users
    are experiencing issues installing it.
    
    This PR replaces the `wordcloud` asset with a `most_frequent_words`
    asset, which is a dictionary of the most frequent words in the titles.
    Then, instead of generating a word cloud, it embeds a bar chart of those
    words as metadata.
    
    The reason why I made this change now is because we have a few more
    sections of the tutorial coming down the pipeline and holding off the
    refactor would've meant more work later.
    
    ## How I Tested These Changes
    
    tests
    
    ---------
    
    Co-authored-by: Erin Cochran <erin.k.cochran@gmail.com>

---
+++ examples/docs_snippets/docs_snippets/tutorial/building_an_asset_graph/assets_initial_state.py
@@ -0,0 +1,51 @@
+# start_topstories_asset
+import pandas as pd  # Add new imports to the top of `assets.py`
+import requests
+
+from dagster import asset
+
+
+@asset
+def topstories(topstory_ids):  # this asset is dependent on topstory_ids
+    results = []
+    for item_id in topstory_ids:
+        item = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
+        ).json()
+        results.append(item)
+
+        if len(results) % 20 == 0:
+            print(f"Got {len(results)} items so far.")  # noqa: T201
+
+    df = pd.DataFrame(results)
+
+    return df
+
+
+# end_topstories_asset
+
+
+# start_most_frequent_words_asset
+@asset
+def most_frequent_words(topstories):
+    stopwords = ["a", "the", "an", "of", "to", "in", "for", "and", "with", "on", "is"]
+
+    # loop through the titles and count the frequency of each word
+    word_counts = {}
+    for raw_title in topstories["title"]:
+        title = raw_title.lower()
+        for word in title.split():
+            cleaned_word = word.strip(".,-!?:;()[]'\"-")
+            if cleaned_word not in stopwords and len(cleaned_word) > 0:
+                word_counts[cleaned_word] = word_counts.get(cleaned_word, 0) + 1
+
+    # Get the top 25 most frequent words
+    top_words = {
+        pair[0]: pair[1]
+        for pair in sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:25]
+    }
+
+    return top_words
+
+
+# end_most_frequent_words_asset
