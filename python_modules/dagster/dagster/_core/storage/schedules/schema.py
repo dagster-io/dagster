7a57959a050e38610c66d675f22a3eb9585b5dd9
Johann Miller <johann@elementl.com>
2023-05-09 13:32:03 -0400

    Asset policy evaluations table

--- python_modules/dagster/dagster/_core/storage/schedules/schema.py
+++ python_modules/dagster/dagster/_core/storage/schedules/schema.py
@@ -1,4 +1,5 @@
 import sqlalchemy as db
+from sqlalchemy.dialects import sqlite
 
 from ..sql import MySQLCompatabilityTypes, get_current_timestamp
 
@@ -45,6 +46,27 @@ JobTickTable = db.Table(
     db.Column("update_timestamp", db.DateTime, server_default=get_current_timestamp()),
 )
 
+AssetPolicyEvaluationsTable = db.Table(
+    "asset_daemon_evaluations",
+    ScheduleStorageSqlMetadata,
+    db.Column(
+        "id",
+        db.BigInteger().with_variant(sqlite.INTEGER(), "sqlite"),
+        primary_key=True,
+        autoincrement=True,
+    ),
+    db.Column(
+        "evaluation_id", db.BigInteger().with_variant(sqlite.INTEGER(), "sqlite"), index=True
+    ),
+    db.Column("asset_key", db.Text),
+    db.Column("asset_evaluation_body", db.Text),
+    db.Column("num_requested", db.Integer),
+    db.Column("num_skipped", db.Integer),
+    db.Column("num_discarded", db.Integer),
+    db.Column("create_timestamp", db.DateTime, server_default=get_current_timestamp()),
+)
+
+
 # Secondary Index migration table, used to track data migrations, event_logs and runs.
 # This schema should match the schema in the event_log storage, run schema
 SecondaryIndexMigrationTable = db.Table(
@@ -64,3 +86,10 @@ db.Index(
 )
 db.Index("idx_job_tick_timestamp", JobTickTable.c.job_origin_id, JobTickTable.c.timestamp)
 db.Index("idx_tick_selector_timestamp", JobTickTable.c.selector_id, JobTickTable.c.timestamp)
+
+db.Index(
+    "idx_asset_daemon_evaluations_asset_key_evaluation_id",
+    AssetPolicyEvaluationsTable.c.asset_key,
+    AssetPolicyEvaluationsTable.c.evaluation_id,
+    unique=True,
+)
