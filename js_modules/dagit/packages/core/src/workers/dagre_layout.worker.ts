dagster-io/dagster

commit 770b73b98b6d107b7d9f287f99e905b64b5d43ba

[ui] Upgrade Storybook (#14074)
## Summary & Motivation

Upgrade Storybook in `core` and `ui`. This resolves a handful of
critical dependency issues flagged by dependabot.

## How I Tested These Changes

Run `core` and `ui` storybooks, verify that they load correctly without
any errors. Run `build-storybook` for both, verify success.

Load full app to sanity check that everything is working as expected.

js_modules/dagit/packages/core/src/workers/dagre_layout.worker.ts
@@ -15,7 +15,7 @@ self.addEventListener('message', (event) => {
   // Before we attempt any imports, manually set the Webpack public path to the static path root.
   // This allows us to import paths when a path-prefix value has been set.
   if (data.staticPathRoot) {
-    __webpack_public_path__ = data.staticPathRoot;
+    (window as any).__webpack_public_path__ = data.staticPathRoot;
   }
 
   switch (data.type) {

commit 0d1fd4251cc5f0826892d5f5c02e919a476bf873

[ui] Fix reference to window in webworker
This is a small regression from https://github.com/dagster-io/dagster/commit/770b73b98b6d107b7d9f287f99e905b64b5d43ba#diff-fbe12ed550f699366fbfc8dde7f98e1edc596ca64652ea4b73c954d4ae291aaeL18

js_modules/dagit/packages/core/src/workers/dagre_layout.worker.ts
@@ -15,7 +15,7 @@ self.addEventListener('message', (event) => {
   // Before we attempt any imports, manually set the Webpack public path to the static path root.
   // This allows us to import paths when a path-prefix value has been set.
   if (data.staticPathRoot) {
-    (window as any).__webpack_public_path__ = data.staticPathRoot;
+    (self as any).__webpack_public_path__ = data.staticPathRoot;
   }
 
   switch (data.type) {