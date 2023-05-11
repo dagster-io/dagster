802af5296b223259053a1b5d9051aff05d8f0caa
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2023-05-11 10:43:32 -0500

    [ui] Try sticky header on virtualized Asset table (#14232)
    ## Summary & Motivation
    
    Change virtualized asset table header to be sticky, in an effort to
    avoid grid alignment mismatches when the scrollbar of the virtualized
    table is present and occupying width.
    
    ## How I Tested These Changes
    
    View asset catalog list, verify that the table header is sticky, sits on
    top of the rows, and has no alignment problems when the scrollbar is
    occupying horizontal space.
    
    Type a search query to force the table to be smaller than the viewport
    height, verify that rows are no cut off and that the header still
    renders correctly.

--- js_modules/dagit/packages/core/src/workspace/VirtualizedAssetTable.tsx
+++ js_modules/dagit/packages/core/src/workspace/VirtualizedAssetTable.tsx
@@ -58,52 +58,50 @@ export const VirtualizedAssetTable: React.FC<Props> = (props) => {
   }, [prefixPath, groups]);
 
   return (
-    <>
-      <VirtualizedAssetCatalogHeader headerCheckbox={headerCheckbox} view={view} />
-      <div style={{overflow: 'hidden'}}>
-        <Container ref={parentRef}>
-          <Inner $totalHeight={totalHeight}>
-            {items.map(({index, key, size, start}) => {
-              const row: Row = rows[index];
-              const path = JSON.stringify(row.path);
-              const rowType = () => {
-                if (row.type === 'folder') {
-                  return 'folder';
-                }
-                return row.asset.definition ? 'asset' : 'asset_non_sda';
-              };
+    <div style={{overflow: 'hidden'}}>
+      <Container ref={parentRef}>
+        <VirtualizedAssetCatalogHeader headerCheckbox={headerCheckbox} view={view} />
+        <Inner $totalHeight={totalHeight}>
+          {items.map(({index, key, size, start}) => {
+            const row: Row = rows[index];
+            const path = JSON.stringify(row.path);
+            const rowType = () => {
+              if (row.type === 'folder') {
+                return 'folder';
+              }
+              return row.asset.definition ? 'asset' : 'asset_non_sda';
+            };
 
-              const repoAddress = () => {
-                if (row.type === 'folder' || !row.asset.definition) {
-                  return null;
-                }
-                const repository = row.asset.definition.repository;
-                return buildRepoAddress(repository.name, repository.location.name);
-              };
+            const repoAddress = () => {
+              if (row.type === 'folder' || !row.asset.definition) {
+                return null;
+              }
+              const repository = row.asset.definition.repository;
+              return buildRepoAddress(repository.name, repository.location.name);
+            };
 
-              const wipeableAssets = row.type === 'folder' ? row.assets : [row.asset];
+            const wipeableAssets = row.type === 'folder' ? row.assets : [row.asset];
 
-              return (
-                <VirtualizedAssetRow
-                  key={key}
-                  view={view}
-                  type={rowType()}
-                  path={row.path}
-                  definition={row.type === 'asset' ? row.asset.definition : null}
-                  repoAddress={repoAddress()}
-                  showCheckboxColumn
-                  showRepoColumn={showRepoColumn}
-                  height={size}
-                  start={start}
-                  checked={checkedPaths.has(path)}
-                  onToggleChecked={onToggleFactory(path)}
-                  onWipe={() => onWipe(wipeableAssets.map((a) => a.key))}
-                />
-              );
-            })}
-          </Inner>
-        </Container>
-      </div>
-    </>
+            return (
+              <VirtualizedAssetRow
+                key={key}
+                view={view}
+                type={rowType()}
+                path={row.path}
+                definition={row.type === 'asset' ? row.asset.definition : null}
+                repoAddress={repoAddress()}
+                showCheckboxColumn
+                showRepoColumn={showRepoColumn}
+                height={size}
+                start={start}
+                checked={checkedPaths.has(path)}
+                onToggleChecked={onToggleFactory(path)}
+                onWipe={() => onWipe(wipeableAssets.map((a) => a.key))}
+              />
+            );
+          })}
+        </Inner>
+      </Container>
+    </div>
   );
 };
