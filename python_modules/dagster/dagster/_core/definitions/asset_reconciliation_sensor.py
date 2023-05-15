be23af548a73565895c3ad1b5078b3fafd3f1663
Johann Miller <johann@elementl.com>
2023-05-15 16:05:14 -0400

    Change AutoMaterializeAssetEvaluation to store serialized subsets

--- python_modules/dagster/dagster/_core/definitions/asset_reconciliation_sensor.py
+++ python_modules/dagster/dagster/_core/definitions/asset_reconciliation_sensor.py
@@ -66,14 +66,14 @@ class AutoMaterializeAssetEvaluation(NamedTuple):
         asset_key (AssetKey): The asset key that was evaluated.
         conditions: The conditions that impact if the asset should be materialized, skipped, or
             discarded. If the asset is partitioned, this will be a list of tuples, where the first
-            element is the condition and the second element is the set of partitions that the
+            element is the condition and the second element is the serialized subset of partitions that the
             condition applies to.
     """
 
     asset_key: AssetKey
     conditions: Union[
         Sequence[AutoMaterializeCondition],
-        Sequence[Tuple[AutoMaterializeCondition, PartitionsSubset]],
+        Sequence[Tuple[AutoMaterializeCondition, str]],
     ]
     num_requested: int
     num_skipped: int
@@ -122,7 +122,12 @@ class AutoMaterializeAssetEvaluation(NamedTuple):
             return AutoMaterializeAssetEvaluation(
                 asset_key=asset_key,
                 conditions=[
-                    (condition, partitions_def.empty_subset().with_partition_keys(partition_keys))
+                    (
+                        condition,
+                        partitions_def.empty_subset()
+                        .with_partition_keys(partition_keys)
+                        .serialize(),
+                    )
                     for condition, partition_keys in partition_keys_by_condition.items()
                 ],
                 num_requested=num_requested,
