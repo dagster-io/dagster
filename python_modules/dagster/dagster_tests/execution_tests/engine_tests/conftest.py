92091715a76380a41a2f02214c047d70207752fa
prha <prha@elementl.com>
2023-05-16 08:43:01 -0700

    tests
    [INTERNAL_BRANCH=prha/concurrency]

---
+++ python_modules/dagster/dagster_tests/execution_tests/engine_tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from dagster import file_relative_path
+from dagster._core.test_utils import instance_for_test
+from dagster._core.workspace.context import WorkspaceProcessContext
+from dagster._core.workspace.load_target import PythonFileTarget
+
+
+@pytest.fixture(scope="module")
+def workspace(instance):
+    with WorkspaceProcessContext(
+        instance,
+        PythonFileTarget(
+            python_file=file_relative_path(__file__, "test_op_concurrency.py"),
+            attribute="concurrency_repo",
+            working_directory=None,
+            location_name="test",
+        ),
+    ) as workspace_process_context:
+        yield workspace_process_context.create_request_context()
+
+
+@pytest.fixture(scope="module")
+def instance():
+    with instance_for_test() as _instance:
+        yield _instance
