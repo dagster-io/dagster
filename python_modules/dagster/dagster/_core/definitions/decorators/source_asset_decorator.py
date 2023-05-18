68ff588ba36eb93732fe2adf44baa6bce9702672
Sandy Ryza <sandy@elementl.com>
2023-05-18 09:35:16 -0700

    DataVersionsByPartition return value for observable_source_asset

--- python_modules/dagster/dagster/_core/definitions/decorators/source_asset_decorator.py
+++ python_modules/dagster/dagster/_core/definitions/decorators/source_asset_decorator.py
@@ -6,6 +6,7 @@ from dagster._core.definitions.events import AssetKey, CoercibleToAssetKeyPrefix
 from dagster._core.definitions.metadata import (
     MetadataUserInput,
 )
+from dagster._core.definitions.partition import PartitionsDefinition
 from dagster._core.definitions.resource_annotation import get_resource_args
 from dagster._core.definitions.resource_definition import ResourceDefinition
 from dagster._core.definitions.source_asset import SourceAsset, SourceAssetObserveFunction
@@ -29,6 +30,7 @@ def observable_source_asset(
     group_name: Optional[str] = None,
     required_resource_keys: Optional[AbstractSet[str]] = None,
     resource_defs: Optional[Mapping[str, ResourceDefinition]] = None,
+    partitions_def: Optional[PartitionsDefinition] = None,
 ) -> "_ObservableSourceAsset":
     ...
 
@@ -46,6 +48,7 @@ def observable_source_asset(
     group_name: Optional[str] = None,
     required_resource_keys: Optional[AbstractSet[str]] = None,
     resource_defs: Optional[Mapping[str, ResourceDefinition]] = None,
+    partitions_def: Optional[PartitionsDefinition] = None,
 ) -> Union[SourceAsset, "_ObservableSourceAsset"]:
     """Create a `SourceAsset` with an associated observation function.
 
@@ -74,6 +77,8 @@ def observable_source_asset(
         resource_defs (Optional[Mapping[str, ResourceDefinition]]): (Experimental) resource
             definitions that may be required by the :py:class:`dagster.IOManagerDefinition` provided in
             the `io_manager_def` argument.
+        partitions_def (Optional[PartitionsDefinition]): Defines the set of partition keys that
+            compose the asset.
         observe_fn (Optional[SourceAssetObserveFunction]) Observation function for the source asset.
     """
     if observe_fn is not None:
@@ -89,6 +94,7 @@ def observable_source_asset(
         group_name,
         required_resource_keys,
         resource_defs,
+        partitions_def,
     )
 
 
@@ -104,6 +110,7 @@ class _ObservableSourceAsset:
         group_name: Optional[str] = None,
         required_resource_keys: Optional[AbstractSet[str]] = None,
         resource_defs: Optional[Mapping[str, ResourceDefinition]] = None,
+        partitions_def: Optional[PartitionsDefinition] = None,
     ):
         self.name = name
         if isinstance(key_prefix, str):
@@ -118,6 +125,7 @@ class _ObservableSourceAsset:
         self.group_name = group_name
         self.required_resource_keys = required_resource_keys
         self.resource_defs = resource_defs
+        self.partitions_def = partitions_def
 
     def __call__(self, observe_fn: SourceAssetObserveFunction) -> SourceAsset:
         source_asset_name = self.name or observe_fn.__name__
@@ -144,4 +152,5 @@ class _ObservableSourceAsset:
             _required_resource_keys=resolved_resource_keys,
             resource_defs=self.resource_defs,
             observe_fn=observe_fn,
+            partitions_def=self.partitions_def,
         )
