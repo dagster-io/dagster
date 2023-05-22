870c83d954b63c054b903ac25108cf9e8a1f658c
Rex Ledesma <rex@elementl.com>
2023-05-22 18:49:20 -0400

    feat(dbt-assets)[1/N]: add dbt_asset decorator (#14186)
    ## Summary & Motivation
    Begin the scaffold for a `dbt_asset` decorator. The decorator takes in a
    dbt manifest and dbt select statement syntax and creates the
    corresponding asset definitions for the dbt models. In later diffs in
    the stack, we provide support for the user to populate the compute
    function for the decorator.
    
    ## How I Tested These Changes
    pytest

--- python_modules/libraries/dagster-dbt/dagster_dbt/cli/__init__.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/cli/__init__.py
@@ -3,4 +3,5 @@ from .resources import (
     DbtCliResource as DbtCliResource,
     dbt_cli_resource as dbt_cli_resource,
 )
+from .resources_v2 import DbtManifest as DbtManifest
 from .types import DbtCliOutput as DbtCliOutput
