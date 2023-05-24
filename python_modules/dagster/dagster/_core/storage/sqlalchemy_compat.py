e2337f9e3774e1beb433420bf86a1eeea14f712d
prha <prha@elementl.com>
2023-05-24 08:40:21 -0700

    mapping
    runs mapping
    
    event mappings
    
    schedule mapping

--- python_modules/dagster/dagster/_core/storage/sqlalchemy_compat.py
+++ python_modules/dagster/dagster/_core/storage/sqlalchemy_compat.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Any, Iterable, Sequence
 
 import sqlalchemy as db
 
@@ -19,3 +19,11 @@ def db_subquery(query, name: str = "subquery"):
         return query.subquery(name)
 
     return query.alias(name)
+
+
+def db_fetch_mappings(conn, query: Any) -> Sequence[Any]:
+    """Utility class that allows compatibility between SqlAlchemy 1.3.x, 1.4.x, and 2.x."""
+    if not IS_SQLALCHEMY_VERSION_1:
+        return conn.execute(query).mappings().all()
+
+    return conn.execute(query).fetchall()
