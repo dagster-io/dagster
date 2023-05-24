df5c4b71b5121ca578b098620d0c512a881691f0
jamiedemaria <demaria.jamie@gmail.com>
2023-05-24 16:59:29 -0400

    [docs] dagstermill example uses definitions (#14125)

--- examples/tutorial_notebook_assets/tutorial_finished/assets/__init__.py
+++ examples/tutorial_notebook_assets/tutorial_finished/assets/__init__.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from dagster import AssetIn, asset, file_relative_path
 from dagstermill import define_dagstermill_asset
-from papermill_origami.noteable_dagstermill import define_noteable_dagster_asset
 
 
 # fetch the iris dataset
@@ -24,17 +23,6 @@ def iris_dataset_finished():
 iris_kmeans_jupyter_notebook_finished = define_dagstermill_asset(
     name="iris_kmeans_jupyter_finished",
     notebook_path=file_relative_path(__file__, "../notebooks/iris-kmeans.ipynb"),
-    ins={"iris": AssetIn("iris_dataset_finished")},
-    group_name="finished_tutorial",
-)
-
-
-# Asset backed by a Noteable notebook
-
-notebook_id = "<your-noteable-notebook-id>"
-iris_kmeans_noteable_notebook_finished = define_noteable_dagster_asset(
-    name="iris_kmeans_noteable_finished",
-    notebook_id=notebook_id,
     ins={"iris": AssetIn("iris_dataset_finished")},
     group_name="finished_tutorial",
 )
