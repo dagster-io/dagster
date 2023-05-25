14730fe2df1c540a752fdb0d6e689396abb82415
Rex Ledesma <rex@elementl.com>
2023-05-25 17:35:08 -0400

    refactor(dbt-assets): use DbtManifest.read (#14474)
    ## Summary & Motivation
    As the title.
    
    ## How I Tested These Changes
    local

--- examples/experimental/tutorial_dbt_dagster_v2/tutorial_dbt_dagster_v2/assets/customize_asset_keys.py
+++ examples/experimental/tutorial_dbt_dagster_v2/tutorial_dbt_dagster_v2/assets/customize_asset_keys.py
@@ -4,7 +4,7 @@ from dagster import AssetKey, OpExecutionContext
 from dagster_dbt.asset_decorator import dbt_assets
 from dagster_dbt.cli import DbtCli, DbtManifest
 
-from tutorial_dbt_dagster_v2.assets import raw_manifest
+from . import MANIFEST_PATH
 
 
 class CustomizedDbtManifest(DbtManifest):
@@ -13,7 +13,7 @@ class CustomizedDbtManifest(DbtManifest):
         return AssetKey(["prefix", node_info["alias"]])
 
 
-manifest = CustomizedDbtManifest(raw_manifest=raw_manifest)
+manifest = CustomizedDbtManifest.read(path=MANIFEST_PATH)
 
 
 @dbt_assets(manifest=manifest)
