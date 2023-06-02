3d97e2ec2226de3bc3107df1c4edfb23ca963d6b
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2023-06-02 09:21:25 -0500

    [ui] Clean up LeftNavRepositorySection.test (#14584)
    ## Summary & Motivation
    
    Cleaning up another notoriously flaky test,
    `LeftNavRepositorySection.test`.
    
    ## How I Tested These Changes
    
    ts, lint, jest

--- js_modules/dagit/packages/core/src/nav/__tests__/LeftNavRepositorySection.test.tsx
+++ js_modules/dagit/packages/core/src/nav/__tests__/LeftNavRepositorySection.test.tsx
@@ -1,130 +1,64 @@
+import {MockedProvider} from '@apollo/client/testing';
 import {act, render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from 'react';
+import {MemoryRouter} from 'react-router-dom';
 
-import {LocationStateChangeEventType} from '../../graphql/types';
-import {TestProvider} from '../../testing/TestProvider';
-import {HIDDEN_REPO_KEYS} from '../../workspace/WorkspaceContext';
+import {
+  HIDDEN_REPO_KEYS,
+  WorkspaceContext,
+  WorkspaceProvider,
+} from '../../workspace/WorkspaceContext';
 import {DUNDER_REPO_NAME} from '../../workspace/buildRepoAddress';
 import {LeftNavRepositorySection} from '../LeftNavRepositorySection';
+import {
+  buildWorkspaceQueryWithOneLocation,
+  buildWorkspaceQueryWithOneLocationAndAssetGroup,
+  buildWorkspaceQueryWithThreeLocations,
+  buildWorkspaceQueryWithZeroLocations,
+} from '../__fixtures__/LeftNavRepositorySection.fixtures';
+
+jest.mock('../RepositoryLocationStateObserver', () => ({
+  RepositoryLocationStateObserver: () => <div />,
+}));
 
 describe('Repository options', () => {
-  const defaultMocks = {
-    RepositoryLocation: () => ({
-      name: () => 'bar',
-      repositories: () => [...new Array(1)],
-    }),
-    Schedules: () => ({
-      results: () => [],
-    }),
-    Sensors: () => ({
-      results: () => [],
-    }),
-    LocationStateChangeEvent: () => ({
-      eventType: () => LocationStateChangeEventType.LOCATION_UPDATED,
-    }),
-  };
-
   const locationOne = 'ipsum';
   const repoOne = 'lorem';
-  const locationTwo = 'bar';
-  const repoTwo = 'foo';
-  const locationThree = 'abc_location';
-  const repoThree = DUNDER_REPO_NAME;
 
   afterEach(() => {
     window.localStorage.clear();
   });
 
   it('Correctly displays the current repository state', async () => {
-    const mocks = {
-      Repository: () => ({
-        name: () => 'foo',
-        pipelines: () => [...new Array(1)],
-        assetGroups: () => [],
-      }),
-      Pipeline: () => ({
-        id: () => 'my_pipeline',
-        name: () => 'my_pipeline',
-        modes: () => [...new Array(1)],
-        isAssetJob: () => false,
-      }),
-    };
-
     render(
-      <TestProvider
-        apolloProps={{mocks: [defaultMocks, mocks]}}
-        routerProps={{initialEntries: ['/locations/foo@bar/etc']}}
-      >
-        <LeftNavRepositorySection />
-      </TestProvider>,
+      <MemoryRouter initialEntries={['/locations/foo@bar/etc']}>
+        <MockedProvider mocks={[buildWorkspaceQueryWithOneLocation()]}>
+          <WorkspaceProvider>
+            <LeftNavRepositorySection />
+          </WorkspaceProvider>
+        </MockedProvider>
+      </MemoryRouter>,
     );
 
-    const repoHeader = await screen.findByRole('button', {name: /foo/i});
+    const repoHeader = await screen.findByRole('button', {name: /lorem/i});
     await userEvent.click(repoHeader);
 
     await waitFor(() => {
-      expect(
-        screen.getByRole('link', {
-          name: /my_pipeline/i,
-        }),
-      ).toBeVisible();
+      expect(screen.getByRole('link', {name: /my_pipeline/i})).toBeVisible();
     });
   });
 
   describe('localStorage', () => {
-    const mocksWithOne = {
-      Workspace: () => ({
-        id: 'workspace',
-        locationEntries: () => [
-          {
-            locationOrLoadError: {
-              __typename: 'RepositoryLocation',
-              name: locationOne,
-              repositories: [{name: repoOne, pipelines: [...new Array(2)], assetGroups: []}],
-            },
-          },
-        ],
-      }),
-    };
-
-    const mocksWithThree = {
-      Workspace: () => ({
-        id: 'workspace',
-        locationEntries: () => [
-          {
-            locationOrLoadError: {
-              __typename: 'RepositoryLocation',
-              name: locationOne,
-              repositories: [{name: repoOne, pipelines: [...new Array(2)], assetGroups: []}],
-            },
-          },
-          {
-            locationOrLoadError: {
-              __typename: 'RepositoryLocation',
-              name: locationTwo,
-              repositories: [{name: repoTwo, pipelines: [...new Array(4)], assetGroups: []}],
-            },
-          },
-          {
-            locationOrLoadError: {
-              __typename: 'RepositoryLocation',
-              name: locationThree,
-              repositories: [{name: repoThree, pipelines: [...new Array(6)], assetGroups: []}],
-            },
-          },
-        ],
-      }),
-    };
-
     it('initializes with first repo option, if one option and no localStorage', async () => {
       render(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocksWithOne]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
+        <MemoryRouter initialEntries={['/runs']}>
+          <MockedProvider mocks={[buildWorkspaceQueryWithOneLocation()]}>
+            <WorkspaceProvider>
+              <LeftNavRepositorySection />
+            </WorkspaceProvider>
+          </MockedProvider>
+        </MemoryRouter>,
       );
 
       const repoHeader = await screen.findByRole('button', {name: /lorem/i});
@@ -139,12 +73,13 @@ describe('Repository options', () => {
     it(`initializes with one repo if it's the only one, even though it's hidden`, async () => {
       window.localStorage.setItem(HIDDEN_REPO_KEYS, `["${repoOne}:${locationOne}"]`);
       render(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocksWithOne]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
+        <MemoryRouter initialEntries={['/runs']}>
+          <MockedProvider mocks={[buildWorkspaceQueryWithOneLocation()]}>
+            <WorkspaceProvider>
+              <LeftNavRepositorySection />
+            </WorkspaceProvider>
+          </MockedProvider>
+        </MemoryRouter>,
       );
 
       const repoHeader = await screen.findByRole('button', {name: /lorem/i});
@@ -158,12 +93,13 @@ describe('Repository options', () => {
 
     it('initializes with all repos visible, if multiple options and no localStorage', async () => {
       render(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocksWithThree]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
+        <MemoryRouter initialEntries={['/runs']}>
+          <MockedProvider mocks={[buildWorkspaceQueryWithThreeLocations()]}>
+            <WorkspaceProvider>
+              <LeftNavRepositorySection />
+            </WorkspaceProvider>
+          </MockedProvider>
+        </MemoryRouter>,
       );
 
       const loremHeader = await screen.findByRole('button', {name: /lorem/i});
@@ -190,12 +126,13 @@ describe('Repository options', () => {
       );
 
       render(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocksWithThree]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
+        <MemoryRouter initialEntries={['/runs']}>
+          <MockedProvider mocks={[buildWorkspaceQueryWithThreeLocations()]}>
+            <WorkspaceProvider>
+              <LeftNavRepositorySection />
+            </WorkspaceProvider>
+          </MockedProvider>
+        </MemoryRouter>,
       );
 
       const fooHeader = await screen.findByRole('button', {name: /foo/i});
@@ -211,12 +148,13 @@ describe('Repository options', () => {
       window.localStorage.setItem(HIDDEN_REPO_KEYS, '["hello:world"]');
 
       render(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocksWithThree]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
+        <MemoryRouter initialEntries={['/runs']}>
+          <MockedProvider mocks={[buildWorkspaceQueryWithThreeLocations()]}>
+            <WorkspaceProvider>
+              <LeftNavRepositorySection />
+            </WorkspaceProvider>
+          </MockedProvider>
+        </MemoryRouter>,
       );
 
       const loremHeader = await screen.findByRole('button', {name: /lorem/i});
@@ -248,12 +186,13 @@ describe('Repository options', () => {
       // `act` immediately because we are asserting null renders
       act(() => {
         render(
-          <TestProvider
-            apolloProps={{mocks: [defaultMocks, mocksWithThree]}}
-            routerProps={{initialEntries: ['/runs']}}
-          >
-            <LeftNavRepositorySection />
-          </TestProvider>,
+          <MemoryRouter initialEntries={['/runs']}>
+            <MockedProvider mocks={[buildWorkspaceQueryWithThreeLocations()]}>
+              <WorkspaceProvider>
+                <LeftNavRepositorySection />
+              </WorkspaceProvider>
+            </MockedProvider>
+          </MemoryRouter>,
         );
       });
 
@@ -269,33 +208,36 @@ describe('Repository options', () => {
     });
 
     it('initializes empty, then shows options when they are added', async () => {
-      const initialMocks = {
-        Workspace: () => ({
-          id: 'workspace',
-          locationEntries: () => [],
-        }),
+      const ReloadableTest = () => {
+        const {refetch} = React.useContext(WorkspaceContext);
+        return (
+          <>
+            <button onClick={() => refetch()}>Refetch workspace</button>
+            <LeftNavRepositorySection />
+          </>
+        );
       };
 
-      const {rerender} = render(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, initialMocks]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
+      const mocks = [
+        buildWorkspaceQueryWithZeroLocations(),
+        buildWorkspaceQueryWithThreeLocations(),
+      ];
+
+      render(
+        <MemoryRouter initialEntries={['/runs']}>
+          <MockedProvider mocks={mocks}>
+            <WorkspaceProvider>
+              <ReloadableTest />
+            </WorkspaceProvider>
+          </MockedProvider>
+        </MemoryRouter>,
       );
 
       // Zero repositories, so zero pipelines.
       expect(screen.queryAllByRole('link')).toHaveLength(0);
 
-      rerender(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocksWithThree]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
-      );
+      const reloadButton = screen.getByRole('button', {name: /refetch workspace/i});
+      await userEvent.click(reloadButton);
 
       const loremHeader = await screen.findByRole('button', {name: /lorem/i});
       await waitFor(() => {
@@ -318,20 +260,26 @@ describe('Repository options', () => {
     });
 
     it('initializes with options, then shows empty if they are removed', async () => {
-      const mocksAfterRemoval = {
-        Workspace: () => ({
-          id: 'workspace',
-          locationEntries: () => [],
-        }),
+      const ReloadableTest = () => {
+        const {refetch} = React.useContext(WorkspaceContext);
+        return (
+          <>
+            <button onClick={() => refetch()}>Refetch workspace</button>
+            <LeftNavRepositorySection />
+          </>
+        );
       };
 
-      const {rerender} = render(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocksWithOne]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
+      const mocks = [buildWorkspaceQueryWithOneLocation(), buildWorkspaceQueryWithZeroLocations()];
+
+      render(
+        <MemoryRouter initialEntries={['/runs']}>
+          <MockedProvider mocks={mocks}>
+            <WorkspaceProvider>
+              <ReloadableTest />
+            </WorkspaceProvider>
+          </MockedProvider>
+        </MemoryRouter>,
       );
 
       const loremHeader = await screen.findByRole('button', {name: /lorem/i});
@@ -341,14 +289,8 @@ describe('Repository options', () => {
       // Three links: two jobs, one repo link at bottom.
       expect(screen.queryAllByRole('link')).toHaveLength(3);
 
-      rerender(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocksAfterRemoval]}}
-          routerProps={{initialEntries: ['/runs']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
-      );
+      const reloadButton = screen.getByRole('button', {name: /refetch workspace/i});
+      await userEvent.click(reloadButton);
 
       await waitFor(() => {
         // After repositories are removed, there are none displayed.
@@ -358,40 +300,15 @@ describe('Repository options', () => {
   });
 
   describe('Asset groups', () => {
-    const mocks = {
-      AssetGroup: () => ({
-        groupName: () => 'my_asset_group',
-      }),
-      Pipeline: () => ({
-        id: () => 'my_pipeline',
-        name: () => 'my_pipeline',
-        modes: () => [...new Array(1)],
-        isAssetJob: () => false,
-      }),
-      Workspace: () => ({
-        id: 'workspace',
-        locationEntries: () => [
-          {
-            locationOrLoadError: {
-              __typename: 'RepositoryLocation',
-              name: locationOne,
-              repositories: [
-                {name: repoOne, pipelines: [...new Array(1)], assetGroups: [...new Array(1)]},
-              ],
-            },
-          },
-        ],
-      }),
-    };
-
     it('renders asset groups alongside jobs', async () => {
       render(
-        <TestProvider
-          apolloProps={{mocks: [defaultMocks, mocks]}}
-          routerProps={{initialEntries: ['/locations/foo@bar/etc']}}
-        >
-          <LeftNavRepositorySection />
-        </TestProvider>,
+        <MemoryRouter initialEntries={['/runs']}>
+          <MockedProvider mocks={[buildWorkspaceQueryWithOneLocationAndAssetGroup()]}>
+            <WorkspaceProvider>
+              <LeftNavRepositorySection />
+            </WorkspaceProvider>
+          </MockedProvider>
+        </MemoryRouter>,
       );
 
       const repoHeader = await screen.findByRole('button', {name: /lorem/i});
