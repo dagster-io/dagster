13dec446812226e961e60872e359b09a56aa3589
odette-elementl <odette@elementl.com>
2023-06-01 22:16:29 -0400

    updating tests

--- examples/docs_snippets/docs_snippets_tests/guides_tests/managing_ml_tests/managing_ml_tests.py
+++ examples/docs_snippets/docs_snippets_tests/guides_tests/managing_ml_tests/managing_ml_tests.py
@@ -1,32 +1,9 @@
-# isort: skip_file
-
-from dagster._core.definitions.load_assets_from_modules import assets_from_modules
-from dagster._core.definitions.materialize import materialize
-from docs_snippets.guides.dagster.managing_ml.managing_ml_code import (
-    my_data,
-    my_ml_model,
-    my_other_data,
-    my_other_ml_model,
-    some_data,
-    some_ml_model,
-    predictions,
-    conditional_machine_learning_model,
-    xgboost_comments_model,
-)
+from dagster import load_assets_from_modules
+from dagster import materialize
+from docs_snippets.guides.dagster.managing_ml import managing_ml_code
 
 
 def assets_test():
-    result = materialize(
-        [
-            my_data,
-            my_ml_model,
-            my_other_data,
-            my_other_ml_model,
-            some_data,
-            some_ml_model,
-            predictions,
-            conditional_machine_learning_model,
-            xgboost_comments_model,
-        ]
-    )
+    assets = load_assets_from_modules([managing_ml_code])
+    result = materialize(assets)
     assert result.success
