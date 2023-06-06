7f6802f66b3dd9c4f152a3afc530518fc2642025
JamieDeMaria <jamie@elementl.com>
2023-06-06 09:53:05 -0500

    restore project fully featured

--- examples/project_fully_featured/project_fully_featured/resources/__init__.py
+++ examples/project_fully_featured/project_fully_featured/resources/__init__.py
@@ -4,11 +4,7 @@ from dagster._utils import file_relative_path
 from dagster_aws.s3 import S3Resource
 from dagster_aws.s3.io_manager import ConfigurablePickledObjectS3IOManager
 from dagster_dbt import DbtCliClientResource
-from dagster_gcp import bigquery_resource, BigQueryResource
 from dagster_pyspark import pyspark_resource
-from dagster_snowflake import SnowflakeResource, snowflake_resource
-from dagster_snowflake_pandas import SnowflakePandasIOManager, snowflake_pandas_io_manager
-from dagster_duckdb_pandas import duckdb_pandas_io_manager, DuckDBPandasIOManager
 
 from .duckdb_parquet_io_manager import DuckDBPartitionedParquetIOManager
 from .hn_resource import HNAPIClient, HNAPISubsampleClient
@@ -98,20 +94,4 @@ RESOURCES_LOCAL = {
     ),
     "hn_client": HNAPIClient(),
     "dbt": dbt_local_resource,
-    "old_style_io": snowflake_pandas_io_manager,
-    "old_style_resource": snowflake_resource,
-    "old_style_resource_configured": bigquery_resource.configured({"project": "bar"}),
-    "old_style_io_configured": duckdb_pandas_io_manager.configured({"database": "bar"}),
-    "new_style_io_configured": SnowflakePandasIOManager(
-        database="foo", account="foo", password="foo", user="foo"
-    ),
-    "new_style_resource_configured": SnowflakeResource(
-        database="foo", account="foo", password="foo", user="foo"
-    ),
-    "new_style_io": DuckDBPandasIOManager.configure_at_launch(),
-    "new_style_resource": BigQueryResource.configure_at_launch(),
-    "custom_configure_at_launch": DuckDBPartitionedParquetIOManager.configure_at_launch(),
-    "duplicate_resource": SnowflakeResource(
-        database="foo", account="foo", password="foo", user="foo"
-    ),
 }
