82fa0ef39a5454d6de83696c04b2ce33d9f52690
Tim Castillo <tim@elementl.com>
2023-06-08 17:07:26 -0400

    adding doc_snippets. still need to add tests

---
+++ examples/docs_snippets/docs_snippets/tutorial/connecting/assets/assets.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from dagster import MetadataValue, Output, asset
+
+# start_add_signsup_asset
+from ..resources import DataGeneratorResource
+
+# ...
+
+
+@asset
+def signups(hackernews_api: DataGeneratorResource):
+    signups = pd.DataFrame(hackernews_api.get_signups())
+
+    return Output(
+        value=signups,
+        metadata={
+            "Record Count": len(signups),
+            "Preview": MetadataValue.md(signups.head().to_markdown()),
+            "Earliest Signup": signups["registered_at"].min(),
+            "Latest Signup": signups["registered_at"].max(),
+        },
+    )
+
+
+# end_add_signsup_asset
