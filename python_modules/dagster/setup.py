d5bf859c304187b8395c97523bd5c441ced63d2d
prha <prha@elementl.com>
2023-06-08 16:34:41 -0700

    unpin alembic

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -79,7 +79,7 @@ setup(
         "PyYAML>=5.1",
         # core (not explicitly expressed atm)
         # pin around issues in specific versions of alembic that broke our migrations
-        "alembic>=1.2.1,!=1.6.3,!=1.7.0,<1.11.0",
+        "alembic>=1.2.1,!=1.6.3,!=1.7.0,!=1.11.0",
         "croniter>=0.3.34",
         f"grpcio>={GRPC_VERSION_FLOOR},<{GRPC_VERSION_CAP}; python_version<'3.11'",
         f"grpcio>={GRPC_VERSION_FLOOR}; python_version>='3.11'",
