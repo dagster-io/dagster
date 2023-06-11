15dec33e4dbfdc9417c1fbc85a58c04d978780fe
Robert Pack <robstar.pack@gmail.com>
2023-06-12 00:00:46 +0200

    feat: basic partition support

--- python_modules/libraries/dagster-deltalake/dagster_deltalake/handler.py
+++ python_modules/libraries/dagster-deltalake/dagster_deltalake/handler.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Type, Union
+from typing import List, Optional, Sequence, Tuple, Type, Union, cast
 
 import pyarrow as pa
 import pyarrow.dataset as ds
@@ -9,14 +9,18 @@ from dagster import (
     TableColumn,
     TableSchema,
 )
-from dagster._core.storage.db_io_manager import (
-    DbTypeHandler,
-    TableSlice,
-)
+from dagster._core.definitions.time_window_partitions import TimeWindow
+from dagster._core.storage.db_io_manager import DbTypeHandler, TablePartitionDimension, TableSlice
 from deltalake import DeltaTable
+from deltalake.schema import (
+    Field as DeltaField,
+    PrimitiveType,
+    Schema,
+)
+from deltalake.table import FilterLiteralType
 from deltalake.writer import write_deltalake
 
-from .io_manager import TableConnection
+from .io_manager import DELTA_DATE_FORMAT, DELTA_DATETIME_FORMAT, TableConnection
 
 
 class DeltalakeArrowTypeHandler(DbTypeHandler[pa.Table]):
@@ -33,8 +37,25 @@ class DeltalakeArrowTypeHandler(DbTypeHandler[pa.Table]):
         if isinstance(obj, ds.Scanner):
             obj = obj.to_reader()
 
+        delta_schema = Schema.from_pyarrow(obj.schema)
+
+        partition_filters = None
+        partition_columns = None
+        if table_slice.partition_dimensions is not None:
+            partition_filters = partition_dimensions_to_dnf(
+                partition_dimensions=table_slice.partition_dimensions, table_schema=delta_schema
+            )
+
+            # TODO make robust and move to function
+            partition_columns = [dim.partition_expr for dim in table_slice.partition_dimensions]
+
         write_deltalake(
-            connection.table_uri, obj, storage_options=connection.storage_options, mode="overwrite"
+            connection.table_uri,
+            obj,
+            storage_options=connection.storage_options,
+            mode="overwrite",
+            partition_filters=partition_filters,
+            partition_by=partition_columns,
         )
 
         if isinstance(obj, pa.Table):
@@ -77,3 +98,47 @@ class DeltalakeArrowTypeHandler(DbTypeHandler[pa.Table]):
     @property
     def supported_types(self) -> Sequence[Type[object]]:
         return [pa.Table, ds.Scanner, pa.RecordBatchReader]
+
+
+def partition_dimensions_to_dnf(
+    partition_dimensions: Sequence[TablePartitionDimension], table_schema: Schema
+) -> Optional[List[FilterLiteralType]]:
+    parts = []
+    for partition_dimension in partition_dimensions:
+        field = _field_from_schema(partition_dimension.partition_expr, table_schema)
+        if field is None:
+            raise ValueError(
+                f"Field {partition_dimension.partition_expr} is not part of table schema.",
+                "Currently only column names are supported as partition expressions",
+            )
+        if isinstance(field.type, PrimitiveType):
+            if field.type.type in ["timestamp", "date"]:
+                parts.append(_time_window_partition_dnf(partition_dimension, field.type.type))
+            else:
+                raise ValueError(f"Unsupported partition type {field.type.type}")
+        else:
+            raise ValueError(f"Unsupported partition type {field.type}")
+
+    return parts
+
+
+def _time_window_partition_dnf(
+    table_partition: TablePartitionDimension, data_type: str
+) -> Tuple[str, str, str]:
+    partition = cast(TimeWindow, table_partition.partitions)
+    start_dt, _ = partition
+    if data_type == "timestamp":
+        start_dt_str = start_dt.strftime(DELTA_DATETIME_FORMAT)
+    elif data_type == "date":
+        start_dt_str = start_dt.strftime(DELTA_DATE_FORMAT)
+    else:
+        raise ValueError(f"Unknown primitive type: {data_type}")
+
+    return (table_partition.partition_expr, "=", start_dt_str)
+
+
+def _field_from_schema(field_name: str, schema: Schema) -> Optional[DeltaField]:
+    for field in schema.fields:
+        if field.name == field_name:
+            return field
+    return None
