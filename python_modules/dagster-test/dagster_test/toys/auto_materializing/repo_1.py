de74721b675a2f8519f17fe974e4343bfc1af979
Johann Miller <johann@elementl.com>
2023-06-21 06:28:48 -0700

    add patitioned auto materialize assets to toys (#14877)
    ![Uploading Screen Shot 2023-06-20 at 2.10.58 PM.png…]()

--- python_modules/dagster-test/dagster_test/toys/auto_materializing/repo_1.py
+++ python_modules/dagster-test/dagster_test/toys/auto_materializing/repo_1.py
@@ -1,6 +1,7 @@
-from dagster import AutoMaterializePolicy, asset, repository
+from dagster import AutoMaterializePolicy, DailyPartitionsDefinition, asset, repository
 
 
+### Non partitioned ##
 @asset(auto_materialize_policy=AutoMaterializePolicy.eager())
 def eager_upstream():
     return 3
@@ -21,6 +22,32 @@ def lazy_downstream_1(lazy_upstream):
     return lazy_upstream + 1
 
 
+### Partitioned ##
+
+
+daily_partitions_def = DailyPartitionsDefinition(start_date="2023-02-01")
+
+
+@asset(auto_materialize_policy=AutoMaterializePolicy.eager())
+def eager_upstream_partitioned():
+    return 3
+
+
+@asset(auto_materialize_policy=AutoMaterializePolicy.eager())
+def eager_downstream_1_partitioned(eager_upstream_partitioned):
+    return eager_upstream_partitioned + 1
+
+
+@asset(auto_materialize_policy=AutoMaterializePolicy.lazy())
+def lazy_upstream_partitioned():
+    return 1
+
+
+@asset(auto_materialize_policy=AutoMaterializePolicy.lazy())
+def lazy_downstream_1_partitioned(lazy_upstream_partitioned):
+    return lazy_upstream_partitioned + 1
+
+
 @repository
 def auto_materialize_repo_1():
     return [
@@ -28,4 +55,8 @@ def auto_materialize_repo_1():
         eager_downstream_1,
         lazy_upstream,
         lazy_downstream_1,
+        eager_upstream_partitioned,
+        eager_downstream_1_partitioned,
+        lazy_upstream_partitioned,
+        lazy_downstream_1_partitioned,
     ]
