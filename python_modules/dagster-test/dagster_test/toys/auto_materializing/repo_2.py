39d9a2bf6c8e295f7de28d754146756778fdb36c
Johann Miller <johann@elementl.com>
2023-06-21 10:54:23 -0700

    toy assets actually use partitions def (#14891)
    oops

--- python_modules/dagster-test/dagster_test/toys/auto_materializing/repo_2.py
+++ python_modules/dagster-test/dagster_test/toys/auto_materializing/repo_2.py
@@ -53,20 +53,22 @@ def lazy_downstream_4(lazy_downstream_2, lazy_downstream_3):
 
 daily_partitions_def = DailyPartitionsDefinition(start_date="2023-02-01")
 
-eager_downstream_1_source_partitioned = SourceAsset(AssetKey(["eager_downstream_1_partitioned"]))
+eager_downstream_1_source_partitioned = SourceAsset(
+    AssetKey(["eager_downstream_1_partitioned"]), partitions_def=daily_partitions_def
+)
 
 
-@asset(auto_materialize_policy=AutoMaterializePolicy.eager())
+@asset(partitions_def=daily_partitions_def, auto_materialize_policy=AutoMaterializePolicy.eager())
 def eager_downstream_2_partitioned(eager_downstream_1_partitioned):
     return eager_downstream_1_partitioned + 2
 
 
-@asset(auto_materialize_policy=AutoMaterializePolicy.eager())
+@asset(partitions_def=daily_partitions_def, auto_materialize_policy=AutoMaterializePolicy.eager())
 def eager_downstream_3_partitioned(eager_downstream_1_partitioned):
     return eager_downstream_1_partitioned + 3
 
 
-@asset(auto_materialize_policy=AutoMaterializePolicy.eager())
+@asset(partitions_def=daily_partitions_def, auto_materialize_policy=AutoMaterializePolicy.eager())
 def eager_downstream_4_partitioned(eager_downstream_2_partitioned, eager_downstream_3_partitioned):
     return eager_downstream_2_partitioned + eager_downstream_3_partitioned
 
@@ -74,17 +76,18 @@ def eager_downstream_4_partitioned(eager_downstream_2_partitioned, eager_downstr
 lazy_downstream_1_source_partitioned = SourceAsset(AssetKey(["lazy_downstream_1_partitioned"]))
 
 
-@asset(auto_materialize_policy=AutoMaterializePolicy.lazy())
+@asset(partitions_def=daily_partitions_def, auto_materialize_policy=AutoMaterializePolicy.lazy())
 def lazy_downstream_2_partitioned(lazy_downstream_1_partitioned):
     return lazy_downstream_1_partitioned + 2
 
 
-@asset(auto_materialize_policy=AutoMaterializePolicy.lazy())
+@asset(partitions_def=daily_partitions_def, auto_materialize_policy=AutoMaterializePolicy.lazy())
 def lazy_downstream_3_partitioned(lazy_downstream_1_partitioned):
     return lazy_downstream_1_partitioned + 3
 
 
 @asset(
+    partitions_def=daily_partitions_def,
     auto_materialize_policy=AutoMaterializePolicy.lazy(),
     freshness_policy=FreshnessPolicy(maximum_lag_minutes=5),
 )
