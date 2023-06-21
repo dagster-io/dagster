7afd96b5d2277fc728cf0ecc0d5ec3d8145929f0
Ben Gotow <ben@foundry376.com>
2023-06-21 17:43:41 -0500

    [ui] Fix asset stale causes to a popover, stop truncating the list (#14840)
    ## Summary & Motivation
    
    Resolves #14654
    
    This PR switches the on-hover display of stale causes from a Tooltip to
    a Popover, which allows you to move the mouse into the overlay without
    it closing. We already use this on the run timeline to show a list of
    runs on hover.
    
    This new rendering style means that the list can scroll when there are
    many stale causes and we can also linkify the references to other assets
    (they now link to those asset's detail pages).
    
    
    ![image](https://github.com/dagster-io/dagster/assets/1037212/b03dec56-9be3-40b2-ab50-380c30a82487)
    
    Note: These new ones are slightly larger, primarily because I made the
    styling match the padding, etc. of the run timeline popover:
    
    ![image](https://github.com/dagster-io/dagster/assets/1037212/32716b9c-8d77-48b4-a61d-bab80a82b92a)
    
    ![image](https://github.com/dagster-io/dagster/assets/1037212/e3af24a3-7b15-48c8-9577-41c8ecf6295a)
    
    ## How I Tested These Changes
    
    I tested these changes manually by viewing stale causes overlaid on the
    asset DAG and overlaid via the Stale tag in the asset catalog / asset
    details pages. I used a hack to give myself many stale causes to ensure
    that the max-height and scrolling behavior works as expected.
    
    ---------
    
    Co-authored-by: bengotow <bgotow@elementl.com>

--- js_modules/dagit/packages/core/src/assets/Stale.tsx
+++ js_modules/dagit/packages/core/src/assets/Stale.tsx
@@ -1,10 +1,25 @@
-import {Colors, Box, BaseTag, Tooltip, Icon, Body, ButtonLink, CaptionMono} from '@dagster-io/ui';
+import {
+  BaseTag,
+  Body,
+  Box,
+  ButtonLink,
+  Caption,
+  CaptionMono,
+  Colors,
+  Icon,
+  Popover,
+  Tooltip,
+} from '@dagster-io/ui';
 import groupBy from 'lodash/groupBy';
 import isEqual from 'lodash/isEqual';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import {displayNameForAssetKey, LiveDataForNode} from '../asset-graph/Utils';
+import {AssetNodeKeyFragment} from '../asset-graph/types/AssetNode.types';
 import {AssetKeyInput, StaleCauseCategory, StaleStatus} from '../graphql/types';
+
+import {assetDetailsPathForKey} from './assetDetailsPathForKey';
 
 export const isAssetMissing = (liveData?: LiveDataForNode) =>
   liveData && liveData.staleStatus === StaleStatus.MISSING;
@@ -58,7 +73,13 @@ export const StaleReasonsTags: React.FC<{
   return (
     <>
       {Object.entries(groupedCauses(assetKey, include, liveData)).map(([label, causes]) => (
-        <Tooltip key={label} position="top" content={<StaleCausesSummary causes={causes} />}>
+        <Popover
+          key={label}
+          content={<StaleCausesSummary causes={causes} />}
+          position="top"
+          interactionKind="hover"
+          className="chunk-popover-target"
+        >
           <BaseTag
             fillColor={Colors.Yellow50}
             textColor={Colors.Yellow700}
@@ -74,13 +95,11 @@ export const StaleReasonsTags: React.FC<{
               )
             }
           />
-        </Tooltip>
+        </Popover>
       ))}
     </>
   );
 };
-
-const MAX_DISPLAYED_REASONS = 4;
 
 function groupedCauses(
   assetKey: AssetKeyInput,
@@ -100,31 +119,65 @@ function groupedCauses(
 export const StaleCausesInfoDot: React.FC<{causes: LiveDataForNode['staleCauses']}> = ({
   causes,
 }) => (
-  <Tooltip
-    position="top"
+  <Popover
     content={causes && causes.length > 0 ? <StaleCausesSummary causes={causes} /> : NO_CAUSES}
+    position="top"
+    interactionKind="hover"
+    className="chunk-popover-target"
   >
     <Icon name="info" size={12} color={Colors.Yellow700} />
-  </Tooltip>
+  </Popover>
 );
 
 const StaleCausesSummary: React.FC<{causes: LiveDataForNode['staleCauses']}> = ({causes}) => (
-  <Box>
-    <strong>Changes since last materialization:</strong>
-    <ul style={{margin: 0, padding: '4px 12px'}}>
-      {causes.slice(0, MAX_DISPLAYED_REASONS).map((cause, idx) => (
-        <li key={idx}>
-          <CaptionMono>{displayNameForAssetKey(cause.key)}</CaptionMono> {cause.reason}{' '}
-          {cause.dependency ? `(${displayNameForAssetKey(cause.dependency)})` : ''}
-        </li>
+  <Box style={{width: '300px'}}>
+    <Box
+      padding={12}
+      border={{side: 'bottom', width: 1, color: Colors.KeylineGray}}
+      style={{fontWeight: 600}}
+    >
+      Changes since last materialization:
+    </Box>
+    <Box style={{maxHeight: '240px', overflowY: 'auto'}} onClick={(e) => e.stopPropagation()}>
+      {causes.map((cause, idx) => (
+        <Box
+          key={idx}
+          border={idx > 0 ? {side: 'top', width: 1, color: Colors.KeylineGray} : null}
+          padding={{vertical: 8, horizontal: 12}}
+        >
+          <Link to={assetDetailsPathForKey(cause.key)}>
+            <CaptionMono>{displayNameForAssetKey(cause.key)}</CaptionMono>
+          </Link>
+          <StaleReason reason={cause.reason} dependency={cause.dependency} />
+        </Box>
       ))}
-      {causes.length > MAX_DISPLAYED_REASONS ? (
-        <span style={{color: Colors.Gray400}}>{`and ${
-          causes.length - MAX_DISPLAYED_REASONS
-        } more...`}</span>
-      ) : (
-        ''
-      )}
-    </ul>
+    </Box>
   </Box>
 );
+
+const StaleReason: React.FC<{reason: string; dependency: AssetNodeKeyFragment | null}> = ({
+  reason,
+  dependency,
+}) => {
+  if (!dependency) {
+    return <Caption>{` ${reason}`}</Caption>;
+  }
+
+  const dependencyName = displayNameForAssetKey(dependency);
+  const dependencyPythonName = dependencyName.replace(/ /g, '');
+  if (reason.endsWith(`${dependencyPythonName}`)) {
+    const reasonUpToDep = reason.slice(0, -dependencyPythonName.length);
+    return (
+      <Caption>
+        {` ${reasonUpToDep}`}
+        <Link to={assetDetailsPathForKey(dependency)}>{dependencyName}</Link>
+      </Caption>
+    );
+  }
+
+  return (
+    <Caption>
+      {` ${reason} `}(<Link to={assetDetailsPathForKey(dependency)}>{dependencyName}</Link>)
+    </Caption>
+  );
+};
