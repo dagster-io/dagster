110b61576851a517c132ef3bdf8ba546d1785f20
Joe Van Drunen <joe@elementl.com>
2023-06-21 17:35:14 -0400

    [dagster-airflow] remove < 2.6.2 airflow contraint, ignore dynamic task mapping examples (#14899)

--- python_modules/libraries/dagster-airflow/setup.py
+++ python_modules/libraries/dagster-airflow/setup.py
@@ -44,8 +44,7 @@ setup(
     extras_require={
         "kubernetes": ["kubernetes>=3.0.0", "cryptography>=2.0.0"],
         "test_airflow_2": [
-            # 2.6.2 breaks how dagster-airflow fetches task instance info
-            "apache-airflow>=2.0.0,<2.6.2",
+            "apache-airflow>=2.0.0",
             "boto3>=1.26.7",
             "kubernetes>=10.0.1",
             "apache-airflow-providers-docker>=3.2.0,<4",
