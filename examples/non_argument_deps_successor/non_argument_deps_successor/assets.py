40328baff055fdac8a88544ef06c5ddcbfc04c75
JamieDeMaria <jamie@elementl.com>
2023-06-27 14:35:39 -0500

    update

--- examples/non_argument_deps_successor/non_argument_deps_successor/assets.py
+++ examples/non_argument_deps_successor/non_argument_deps_successor/assets.py
@@ -7,29 +7,30 @@ DBT_PROJECT_DIR = file_relative_path(__file__, "../jaffle_shop")
 DBT_PROFILES_DIR = file_relative_path(__file__, "../jaffle_shop/config")
 DBT_MANIFEST = file_relative_path(__file__, "../jaffle_shop/target/manifest.json")
 
-# dbt_assets = load_assets_from_dbt_project(project_dir=DBT_PROJECT_DIR, profiles_dir=DBT_PROFILES_DIR)
-
-# @asset(
-#     deps=[AssetKey(["customers"]), AssetKey(["stg_customers"])],
-#     group_name="staging"
-# )
-# def downstream_of_dbt():
-#     return None
-
-from dagster_dbt.cli import DbtCli, DbtManifest
-from dagster_dbt.asset_decorator import dbt_assets
-
-manifest = DbtManifest.read(path=DBT_MANIFEST)
-
-
-@dbt_assets(
-    manifest=manifest,
-    select="stg_customers stg_orders customers",
+dbt_assets = load_assets_from_dbt_project(
+    project_dir=DBT_PROJECT_DIR, profiles_dir=DBT_PROFILES_DIR, key_prefix=["some_schema"]
 )
-def jaffle_shop_dbt_assets(context, dbt: DbtCli):
-    yield from dbt.cli(["run"], manifest=manifest, context=context).stream()
 
 
-@asset(deps=[manifest.get_asset_key_for_model("customers")], group_name="staging")
+@asset(deps=dbt_assets, group_name="staging")
 def downstream_of_dbt():
     return None
+
+
+# from dagster_dbt.cli import DbtCli, DbtManifest
+# from dagster_dbt.asset_decorator import dbt_assets
+
+# manifest = DbtManifest.read(path=DBT_MANIFEST)
+
+
+# @dbt_assets(
+#     manifest=manifest,
+#     select="stg_customers stg_orders customers",
+# )
+# def jaffle_shop_dbt_assets(context, dbt: DbtCli):
+#     yield from dbt.cli(["run"], manifest=manifest, context=context).stream()
+
+
+# @asset(deps=[manifest.get_asset_key_for_model("customers")], group_name="staging")
+# def downstream_of_dbt():
+#     return None
