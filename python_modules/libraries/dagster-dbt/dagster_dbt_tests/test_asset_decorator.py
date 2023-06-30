9021f668e8d4470dc526b0bd9243908408a5ea5e
JamieDeMaria <jamie@elementl.com>
2023-06-30 14:51:22 -0500

    update exception match string in tests

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/test_asset_decorator.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/test_asset_decorator.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import re
 from typing import AbstractSet, Any, Mapping, Optional
 
 import pytest
@@ -314,11 +315,14 @@ def test_dbt_with_downstream_asset_errors():
 
     with pytest.raises(
         DagsterInvalidDefinitionError,
-        match=(
-            "Cannot pass a multi_asset AssetsDefinition as an argument to deps."
-            " Instead, specify dependencies on the assets created by the multi_asset via AssetKeys"
-            " or strings."
-            " For the multi_asset my_dbt_assets, the available keys are: "
+        match=re.escape(
+            "Cannot pass a multi_asset AssetsDefinition as an argument to deps. Instead, specify"
+            " dependencies on the assets created by the multi_asset via AssetKeys or strings. For"
+            " the multi_asset my_dbt_assets, the available keys are: {AssetKey(['raw_payments']),"
+            " AssetKey(['raw_customers']), AssetKey(['customized', 'staging', 'payments']),"
+            " AssetKey(['customized', 'staging', 'orders']), AssetKey(['raw_orders']),"
+            " AssetKey(['orders']), AssetKey(['customers']), AssetKey(['customized', 'staging',"
+            " 'customers'])}."
         ),
     ):
 
