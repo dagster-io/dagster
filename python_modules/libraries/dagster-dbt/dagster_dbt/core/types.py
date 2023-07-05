37b3dc3aeab06686b73ec5f780d5aab7a5485bea
Sandy Ryza <sandy@elementl.com>
2023-07-05 13:01:54 -0700

    unexperimentalize the DbtCli resource and deprecate DbtCliResource (#15068)
    ## Summary & Motivation
    
    I'm aiming to remove the experimental label from DbtCli, which will
    allow me to start switching over the tutorial.
    
    Depends on https://github.com/dagster-io/dagster/pull/15067 and
    https://github.com/dagster-io/dagster/pull/15066.
    
    ## How I Tested These Changes

--- python_modules/libraries/dagster-dbt/dagster_dbt/core/types.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/core/types.py
@@ -1,13 +1,18 @@
 from typing import Any, Mapping, Optional, Sequence
 
 import dagster._check as check
+from dagster._annotations import deprecated
 
 from ..types import DbtOutput
 
 
+@deprecated
 class DbtCliOutput(DbtOutput):
     """The results of executing a dbt command, along with additional metadata about the dbt CLI
     process that was run.
+
+    This class is deprecated, because it's only produced by methods of the DbtCliResource class,
+    which is deprecated in favor of DbtCli.
 
     Note that users should not construct instances of this class directly. This class is intended
     to be constructed from the JSON output of dbt commands.
