d08f62c847dbc6cf2bee186eb7cbcd4a6b515843
Sandy Ryza <sandy@elementl.com>
2023-07-10 08:23:13 -0700

    use auto_observe_interval_minutes in auto-materialize concept docs

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_auto_materialize.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_auto_materialize.py
@@ -3,6 +3,7 @@ from docs_snippets.concepts.assets import (
     auto_materialize_eager,
     auto_materialize_lazy,
     auto_materialize_lazy_transitive,
+    auto_materialize_observable_source_asset,
     auto_materialize_time_partitions,
 )
 
@@ -20,11 +21,9 @@ def test_auto_materialize_lazy_transitive_asset_defs():
 
 
 def test_auto_materialize_observable_source_asset():
-    from docs_snippets.concepts.assets.auto_materialize_observable_source_asset import (
-        defs,
+    Definitions(
+        assets=load_assets_from_modules([auto_materialize_observable_source_asset])
     )
-
-    assert defs
 
 
 def test_auto_materialize_time_partitions():
