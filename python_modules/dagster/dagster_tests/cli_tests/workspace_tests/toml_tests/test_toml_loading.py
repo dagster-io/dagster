3f8be1d347b934417acc2234b85c2cdf4b270b56
Rex Ledesma <rex@elementl.com>
2023-07-12 12:44:48 -0400

    feat(toml): allow code location name to be set from pyproject.toml

--- python_modules/dagster/dagster_tests/cli_tests/workspace_tests/toml_tests/test_toml_loading.py
+++ python_modules/dagster/dagster_tests/cli_tests/workspace_tests/toml_tests/test_toml_loading.py
@@ -6,6 +6,14 @@ def test_load_python_module_from_toml():
     origins = get_origins_from_toml(file_relative_path(__file__, "single_module.toml"))
     assert len(origins) == 1
     assert origins[0].loadable_target_origin.module_name == "baaz"
+    assert origins[0].location_name == "baaz"
+
+    origins = get_origins_from_toml(
+        file_relative_path(__file__, "single_module_with_code_location_name.toml")
+    )
+    assert len(origins) == 1
+    assert origins[0].loadable_target_origin.module_name == "baaz"
+    assert origins[0].location_name == "bar"
 
 
 def test_load_empty_toml():
