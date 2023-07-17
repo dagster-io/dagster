bb6dda2fe0b414547416eb0bd3d5468c960ecbd2
Johann Miller <johann@elementl.com>
2023-07-17 17:44:36 -0600

    Revert "Document k8s example jobs (#15255)"
    This reverts commit 3e4c7110b89ff3b8348e32a886ddce8b2fb71e02.

--- examples/deploy_k8s/tests/test_project.py
+++ examples/deploy_k8s/tests/test_project.py
@@ -1,5 +1,5 @@
-from ..example_project.example_repo.repo import example_job
+from ..example_project.example_repo.repo import single_pod_job
 
 
 def test_example_project():
-    assert example_job.execute_in_process().success
+    assert single_pod_job.execute_in_process().success
