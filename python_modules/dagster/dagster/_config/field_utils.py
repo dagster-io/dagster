072fae1327d013b5889ab2e20cf7602aa4cfdc4e
benpankow <ben@elementl.com>
2023-07-17 09:37:20 -0700

    [docstrings] Resources & config

--- python_modules/dagster/dagster/_config/field_utils.py
+++ python_modules/dagster/dagster/_config/field_utils.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 import hashlib
-from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Mapping, Sequence
+from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Mapping, Optional, Sequence
 
 import dagster._check as check
 from dagster._annotations import public
@@ -206,7 +206,8 @@ class Map(ConfigType):
 
     @public
     @property
-    def key_label_name(self):
+    def key_label_name(self) -> Optional[str]:
+        """Name which describes the role of keys in the map, if provided."""
         return self.given_name
 
     def type_iterator(self) -> Iterator["ConfigType"]:
