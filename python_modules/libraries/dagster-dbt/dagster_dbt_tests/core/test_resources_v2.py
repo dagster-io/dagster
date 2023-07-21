dagster-io/dagster

commit c06f1d7a01362f143edc442c51c014e45df18dc6

Remove DbtAssetsDefinition (#15344)
## Summary & Motivation

This removes the DbtAssetsDefinition class. In its stead, it adds a
`get_asset_key_for_model` function and similar functions that were
previously its methods as a top-level export of dagster_dbt.

## How I Tested These Changes

python_modules/libraries/dagster-dbt/dagster_dbt/core/resources_v2.py
@@ -34,9 +34,10 @@ from dbt.node_types import NodeType
 from pydantic import Field
 from typing_extensions import Literal
 
-from ..asset_utils import output_name_fn
+from ..asset_utils import get_manifest_and_translator_from_dbt_assets, output_name_fn
 from ..dagster_dbt_translator import DagsterDbtTranslator
 from ..errors import DagsterDbtCliRuntimeError
+from ..utils import ASSET_RESOURCE_TYPES, get_node_info_by_dbt_unique_id_from_manifest
 
 logger = get_dagster_logger()
 
@@ -465,25 +466,20 @@ class DbtCliResource(ConfigurableResource):
             "DBT_TARGET_PATH": target_path,
         }
 
-        from dagster_dbt.dbt_assets_definition import DbtAssetsDefinition
-
         assets_def: Optional[AssetsDefinition] = None
         with suppress(DagsterInvalidPropertyError):
             assets_def = context.assets_def if context else None
 
-        if context and isinstance(assets_def, DbtAssetsDefinition):
-            logger.info(
-                "A DbtAssetsDefinition was provided to the dbt CLI client. Selection arguments to"
-                " dbt will automatically be interpreted from the execution environment."
+        if context and assets_def is not None:
+            manifest, dagster_dbt_translator = get_manifest_and_translator_from_dbt_assets(
+                [assets_def]
             )
-
-            selection_args = assets_def.get_subset_selection_for_context(
+            selection_args = get_subset_selection_for_context(
                 context=context,
+                manifest=manifest,
                 select=context.op.tags.get("dagster-dbt/select"),
                 exclude=context.op.tags.get("dagster-dbt/exclude"),
             )
-            manifest = assets_def.manifest
-            dagster_dbt_translator = assets_def.dagster_dbt_translator
         else:
             selection_args: List[str] = []
             if manifest is None:
@@ -515,3 +511,75 @@ class DbtCliResource(ConfigurableResource):
             target_path=project_dir.joinpath(target_path),
             raise_on_error=raise_on_error,
         )
+
+
+def get_subset_selection_for_context(
+    context: OpExecutionContext,
+    manifest: Mapping[str, Any],
+    select: Optional[str],
+    exclude: Optional[str],
+) -> List[str]:
+    """Generate a dbt selection string to materialize the selected resources in a subsetted execution context.
+
+    See https://docs.getdbt.com/reference/node-selection/syntax#how-does-selection-work.
+
+    Args:
+        context (OpExecutionContext): The execution context for the current execution step.
+        select (Optional[str]): A dbt selection string to select resources to materialize.
+        exclude (Optional[str]): A dbt selection string to exclude resources from materializing.
+
+    Returns:
+        List[str]: dbt CLI arguments to materialize the selected resources in a
+            subsetted execution context.
+
+            If the current execution context is not performing a subsetted execution,
+            return CLI arguments composed of the inputed selection and exclusion arguments.
+    """
+    default_dbt_selection = []
+    if select:
+        default_dbt_selection += ["--select", select]
+    if exclude:
+        default_dbt_selection += ["--exclude", exclude]
+
+    node_info_by_output_name = get_node_info_by_output_name(manifest)
+
+    # TODO: this should be a property on the context if this is a permanent indicator for
+    # determining whether the current execution context is performing a subsetted execution.
+    is_subsetted_execution = len(context.selected_output_names) != len(context.assets_def.keys)
+    if not is_subsetted_execution:
+        logger.info(
+            "A dbt subsetted execution is not being performed. Using the default dbt selection"
+            f" arguments `{default_dbt_selection}`."
+        )
+        return default_dbt_selection
+
+    selected_dbt_resources = []
+    for output_name in context.selected_output_names:
+        node_info = node_info_by_output_name[output_name]
+
+        # Explicitly select a dbt resource by its fully qualified name (FQN).
+        # https://docs.getdbt.com/reference/node-selection/methods#the-file-or-fqn-method
+        fqn_selector = f"fqn:{'.'.join(node_info['fqn'])}"
+
+        selected_dbt_resources.append(fqn_selector)
+
+    # Take the union of all the selected resources.
+    # https://docs.getdbt.com/reference/node-selection/set-operators#unions
+    union_selected_dbt_resources = ["--select"] + [" ".join(selected_dbt_resources)]
+
+    logger.info(
+        "A dbt subsetted execution is being performed. Overriding default dbt selection"
+        f" arguments `{default_dbt_selection}` with arguments: `{union_selected_dbt_resources}`"
+    )
+
+    return union_selected_dbt_resources
+
+
+def get_node_info_by_output_name(manifest: Mapping[str, Any]) -> Mapping[str, Mapping[str, Any]]:
+    node_info_by_dbt_unique_id = get_node_info_by_dbt_unique_id_from_manifest(manifest)
+
+    return {
+        output_name_fn(node): node
+        for node in node_info_by_dbt_unique_id.values()
+        if node["resource_type"] in ASSET_RESOURCE_TYPES
+    }

examples/experimental/tutorial_dbt_dagster_v2/tutorial_dbt_dagster_v2/assets/dependencies.py
@@ -2,7 +2,13 @@ from typing import Any
 
 import pandas as pd
 from dagster import AssetOut, OpExecutionContext, Output, asset, multi_asset
-from dagster_dbt import DbtCliResource, dbt_assets
+from dagster_dbt import (
+    DbtCliResource,
+    dbt_assets,
+    get_asset_key_for_model,
+    get_asset_key_for_source,
+    get_asset_keys_by_output_name_for_source,
+)
 
 from ..constants import MANIFEST_PATH
 
@@ -13,7 +19,7 @@ def my_dbt_assets(context: OpExecutionContext, dbt: DbtCliResource):
 
 
 @asset(
-    key=my_dbt_assets.get_asset_key_for_source("raw_data"),
+    key=get_asset_key_for_source([my_dbt_assets], "raw_data"),
 )
 def raw_inventory() -> Any:
     return pd.DataFrame()
@@ -22,8 +28,8 @@ def raw_inventory() -> Any:
 @multi_asset(
     outs={
         name: AssetOut(key=asset_key)
-        for name, asset_key in my_dbt_assets.get_asset_keys_by_output_name_for_source(
-            "clients_data"
+        for name, asset_key in get_asset_keys_by_output_name_for_source(
+            [my_dbt_assets], "clients_data"
         ).items()
     }
 )
@@ -33,6 +39,6 @@ def clients_data(context):
     yield Output(value=pd.DataFrame(), output_name=output_names[1])
 
 
-@asset(deps=[my_dbt_assets.get_asset_key_for_model("customers")])
+@asset(deps=[get_asset_key_for_model([my_dbt_assets], "customers")])
 def cleaned_customers() -> Any:
     return pd.DataFrame()

examples/experimental/tutorial_dbt_dagster_v2/tutorial_dbt_dagster_v2/schedules/define_schedules.py
@@ -1,4 +1,4 @@
-from dagster_dbt import DbtCliResource, dbt_assets
+from dagster_dbt import DbtCliResource, build_schedule_from_dbt_selection, dbt_assets
 
 from ..constants import MANIFEST_PATH
 
@@ -8,13 +8,16 @@ def all_dbt_assets(context, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
 
-daily_dbt_assets_schedule = all_dbt_assets.build_schedule_from_dbt_selection(
+daily_dbt_assets_schedule = build_schedule_from_dbt_selection(
+    [all_dbt_assets],
     job_name="all_dbt_assets",
     cron_schedule="0 0 * * *",
     dbt_select="fqn:*",
 )
 
-hourly_staging_dbt_assets = all_dbt_assets.build_schedule_from_dbt_selection(
+
+hourly_staging_dbt_assets = build_schedule_from_dbt_selection(
+    [all_dbt_assets],
     job_name="staging_dbt_assets",
     cron_schedule="0 * * * *",
     dbt_select="fqn:staging.*",

python_modules/libraries/dagster-dbt/dagster_dbt/__init__.py
@@ -2,6 +2,13 @@ from .asset_defs import (
     load_assets_from_dbt_manifest as load_assets_from_dbt_manifest,
     load_assets_from_dbt_project as load_assets_from_dbt_project,
 )
+from .asset_utils import (
+    build_dbt_asset_selection as build_dbt_asset_selection,
+    build_schedule_from_dbt_selection as build_schedule_from_dbt_selection,
+    get_asset_key_for_model as get_asset_key_for_model,
+    get_asset_key_for_source as get_asset_key_for_source,
+    get_asset_keys_by_output_name_for_source as get_asset_keys_by_output_name_for_source,
+)
 from .cloud import (
     DbtCloudClientResource as DbtCloudClientResource,
     DbtCloudOutput as DbtCloudOutput,

python_modules/libraries/dagster-dbt/dagster_dbt/asset_decorator.py
@@ -15,23 +15,18 @@ from typing import (
 )
 
 import dagster._check as check
-from dagster import (
-    AssetKey,
-    AssetOut,
-    Nothing,
-    PartitionsDefinition,
-    multi_asset,
-)
+from dagster import AssetKey, AssetOut, AssetsDefinition, Nothing, PartitionsDefinition, multi_asset
 from dagster._annotations import experimental
 
 from .asset_utils import (
+    DAGSTER_DBT_TRANSLATOR_METADATA_KEY,
+    MANIFEST_METADATA_KEY,
     default_auto_materialize_policy_fn,
     default_code_version_fn,
     default_freshness_policy_fn,
     get_deps,
 )
 from .dagster_dbt_translator import DagsterDbtTranslator
-from .dbt_assets_definition import DbtAssetsDefinition
 from .utils import (
     ASSET_RESOURCE_TYPES,
     get_node_info_by_dbt_unique_id_from_manifest,
@@ -49,7 +44,7 @@ def dbt_assets(
     io_manager_key: Optional[str] = None,
     partitions_def: Optional[PartitionsDefinition] = None,
     dagster_dbt_translator: DagsterDbtTranslator = DagsterDbtTranslator(),
-) -> Callable[..., DbtAssetsDefinition]:
+) -> Callable[..., AssetsDefinition]:
     """Create a definition for how to compute a set of dbt resources, described by a manifest.json.
 
     Args:
@@ -101,10 +96,11 @@ def dbt_assets(
         dbt_nodes=node_info_by_dbt_unique_id,
         deps=deps,
         io_manager_key=io_manager_key,
+        manifest=manifest,
         dagster_dbt_translator=dagster_dbt_translator,
     )
 
-    def inner(fn) -> DbtAssetsDefinition:
+    def inner(fn) -> AssetsDefinition:
         asset_definition = multi_asset(
             outs=outs,
             internal_asset_deps=internal_asset_deps,
@@ -118,11 +114,7 @@ def dbt_assets(
             },
         )(fn)
 
-        return DbtAssetsDefinition.from_assets_def(
-            asset_definition,
-            manifest=manifest,
-            dagster_dbt_translator=dagster_dbt_translator,
-        )
+        return asset_definition
 
     return inner
 
@@ -131,6 +123,7 @@ def get_dbt_multi_asset_args(
     dbt_nodes: Mapping[str, Any],
     deps: Mapping[str, FrozenSet[str]],
     io_manager_key: Optional[str],
+    manifest: Mapping[str, Any],
     dagster_dbt_translator: DagsterDbtTranslator,
 ) -> Tuple[Sequence[AssetKey], Dict[str, AssetOut], Dict[str, Set[AssetKey]]]:
     non_argument_deps: Set[AssetKey] = set()
@@ -149,7 +142,11 @@ def get_dbt_multi_asset_args(
             io_manager_key=io_manager_key,
             description=dagster_dbt_translator.get_description(node_info),
             is_required=False,
-            metadata=dagster_dbt_translator.get_metadata(node_info),
+            metadata={  # type: ignore
+                **dagster_dbt_translator.get_metadata(node_info),
+                MANIFEST_METADATA_KEY: manifest,
+                DAGSTER_DBT_TRANSLATOR_METADATA_KEY: dagster_dbt_translator,
+            },
             group_name=dagster_dbt_translator.get_group_name(node_info),
             code_version=default_code_version_fn(node_info),
             freshness_policy=default_freshness_policy_fn(node_info),

python_modules/libraries/dagster-dbt/dagster_dbt/asset_defs.py
@@ -62,7 +62,6 @@ from dagster_dbt.core.resources_v2 import DbtCliResource
 from dagster_dbt.core.types import DbtCliOutput
 from dagster_dbt.core.utils import build_command_args_from_flags, execute_cli
 from dagster_dbt.dagster_dbt_translator import DagsterDbtTranslator
-from dagster_dbt.dbt_assets_definition import DbtAssetsDefinition
 from dagster_dbt.errors import DagsterDbtError
 from dagster_dbt.types import DbtOutput
 from dagster_dbt.utils import (
@@ -499,7 +498,7 @@ def load_assets_from_dbt_project(
     ] = default_metadata_fn,
     display_raw_sql: Optional[bool] = None,
     dbt_resource_key: str = "dbt",
-) -> Sequence[DbtAssetsDefinition]:
+) -> Sequence[AssetsDefinition]:
     """Loads a set of dbt models from a dbt project into Dagster assets.
 
     Creates one Dagster asset for each dbt model. All assets will be re-materialized using a single
@@ -664,7 +663,7 @@ def load_assets_from_dbt_manifest(
     node_info_to_definition_metadata_fn: Callable[
         [Mapping[str, Any]], Mapping[str, MetadataUserInput]
     ] = default_metadata_fn,
-) -> Sequence[DbtAssetsDefinition]:
+) -> Sequence[AssetsDefinition]:
     """Loads a set of dbt models, described in a manifest.json, into Dagster assets.
 
     Creates one Dagster asset for each dbt model. All assets will be re-materialized using a single
@@ -819,7 +818,7 @@ def _load_assets_from_dbt_manifest(
     node_info_to_definition_metadata_fn: Callable[
         [Mapping[str, Any]], Mapping[str, MetadataUserInput]
     ],
-) -> Sequence[DbtAssetsDefinition]:
+) -> Sequence[AssetsDefinition]:
     if partition_key_to_vars_fn:
         check.invariant(
             partitions_def is not None,
@@ -924,13 +923,7 @@ def _load_assets_from_dbt_manifest(
         manifest_json=manifest,
     )
 
-    return [
-        DbtAssetsDefinition.from_assets_def(
-            dbt_assets_def,
-            manifest=manifest,
-            dagster_dbt_translator=dagster_dbt_translator,
-        )
-    ]
+    return [dbt_assets_def]
 
 
 def _raise_warnings_for_deprecated_args(

python_modules/libraries/dagster-dbt/dagster_dbt/asset_utils.py
@@ -9,26 +9,305 @@ from typing import (
     List,
     Mapping,
     Optional,
+    Sequence,
     Set,
     Tuple,
 )
 
 from dagster import (
     AssetKey,
+    AssetsDefinition,
+    AssetSelection,
     AutoMaterializePolicy,
+    DagsterInvariantViolationError,
     FreshnessPolicy,
     In,
     MetadataValue,
     Nothing,
     Out,
+    RunConfig,
+    ScheduleDefinition,
     TableColumn,
     TableSchema,
+    _check as check,
+    define_asset_job,
 )
+from dagster._utils.merger import merge_dicts
 
 from .utils import input_name_fn, output_name_fn
 
 if TYPE_CHECKING:
     from .dagster_dbt_translator import DagsterDbtTranslator
+
+MANIFEST_METADATA_KEY = "dagster_dbt/manifest"
+DAGSTER_DBT_TRANSLATOR_METADATA_KEY = "dagster_dbt/dagster_dbt_translator"
+
+
+def get_asset_key_for_model(dbt_assets: Sequence[AssetsDefinition], model_name: str) -> AssetKey:
+    """Return the corresponding Dagster asset key for a dbt model.
+
+    Args:
+        dbt_assets (AssetsDefinition): An AssetsDefinition object produced by
+            load_assets_from_dbt_project, load_assets_from_dbt_manifest, or @dbt_assets.
+        model_name (str): The name of the dbt model.
+
+    Returns:
+        AssetKey: The corresponding Dagster asset key.
+
+    Examples:
+        .. code-block:: python
+
+            from dagster import asset
+            from dagster_dbt import dbt_assets, get_asset_key_for_model
+
+            @dbt_assets(manifest=...)
+            def all_dbt_assets():
+                ...
+
+
+            @asset(deps={get_asset_key_for_model([all_dbt_assets], "customers")})
+            def cleaned_customers():
+                ...
+    """
+    check.sequence_param(dbt_assets, "dbt_assets", of_type=AssetsDefinition)
+    check.str_param(model_name, "model_name")
+
+    manifest, dagster_dbt_translator = get_manifest_and_translator_from_dbt_assets(dbt_assets)
+
+    matching_models = [
+        value
+        for value in manifest["nodes"].values()
+        if value["name"] == model_name and value["resource_type"] == "model"
+    ]
+
+    if len(matching_models) == 0:
+        raise KeyError(f"Could not find a dbt model with name: {model_name}")
+
+    return dagster_dbt_translator.get_asset_key(next(iter(matching_models)))
+
+
+def get_asset_keys_by_output_name_for_source(
+    dbt_assets: Sequence[AssetsDefinition], source_name: str
+) -> Mapping[str, AssetKey]:
+    """Returns the corresponding Dagster asset keys for all tables in a dbt source.
+
+    This is a convenience method that makes it easy to define a multi-asset that generates
+    all the tables for a given dbt source.
+
+    Args:
+        source_name (str): The name of the dbt source.
+
+    Returns:
+        Mapping[str, AssetKey]: A mapping of the table name to corresponding Dagster asset key
+            for all tables in the given dbt source.
+
+    Examples:
+        .. code-block:: python
+
+            from dagster import AssetOut, multi_asset
+            from dagster_dbt import dbt_assets, get_asset_keys_by_output_name_for_source
+
+            @dbt_assets(manifest=...)
+            def all_dbt_assets():
+                ...
+
+            @multi_asset(
+                outs={
+                    name: AssetOut(key=asset_key)
+                    for name, asset_key in get_asset_keys_by_output_name_for_source(
+                        [all_dbt_assets], "raw_data"
+                    ).items()
+                },
+            )
+            def upstream_python_asset():
+                ...
+
+    """
+    check.sequence_param(dbt_assets, "dbt_assets", of_type=AssetsDefinition)
+    check.str_param(source_name, "source_name")
+
+    manifest, dagster_dbt_translator = get_manifest_and_translator_from_dbt_assets(dbt_assets)
+
+    matching_nodes = [
+        value for value in manifest["sources"].values() if value["source_name"] == source_name
+    ]
+
+    if len(matching_nodes) == 0:
+        raise KeyError(f"Could not find a dbt source with name: {source_name}")
+
+    return {
+        output_name_fn(value): dagster_dbt_translator.get_asset_key(value)
+        for value in matching_nodes
+    }
+
+
+def get_asset_key_for_source(dbt_assets: Sequence[AssetsDefinition], source_name: str) -> AssetKey:
+    """Returns the corresponding Dagster asset key for a dbt source with a singular table.
+
+    Args:
+        source_name (str): The name of the dbt source.
+
+    Raises:
+        DagsterInvalidInvocationError: If the source has more than one table.
+
+    Returns:
+        AssetKey: The corresponding Dagster asset key.
+
+    Examples:
+        .. code-block:: python
+
+            from dagster import asset
+            from dagster_dbt import dbt_assets, get_asset_key_for_source
+
+            @dbt_assets(manifest=...)
+            def all_dbt_assets():
+                ...
+
+            @asset(key=get_asset_key_for_source([all_dbt_assets], "my_source"))
+            def upstream_python_asset():
+                ...
+    """
+    asset_keys_by_output_name = get_asset_keys_by_output_name_for_source(dbt_assets, source_name)
+
+    if len(asset_keys_by_output_name) > 1:
+        raise KeyError(
+            f"Source {source_name} has more than one table:"
+            f" {asset_keys_by_output_name.values()}. Use"
+            " `get_asset_keys_by_output_name_for_source` instead to get all tables for a"
+            " source."
+        )
+
+    return list(asset_keys_by_output_name.values())[0]
+
+
+def build_dbt_asset_selection(
+    dbt_assets: Sequence[AssetsDefinition],
+    dbt_select: str = "fqn:*",
+    dbt_exclude: Optional[str] = None,
+) -> AssetSelection:
+    """Build an asset selection for a dbt selection string.
+
+    See https://docs.getdbt.com/reference/node-selection/syntax#how-does-selection-work for
+    more information.
+
+    Args:
+        dbt_select (str): A dbt selection string to specify a set of dbt resources.
+        dbt_exclude (Optional[str]): A dbt selection string to exclude a set of dbt resources.
+
+    Returns:
+        AssetSelection: An asset selection for the selected dbt nodes.
+
+    Examples:
+        .. code-block:: python
+
+            from dagster_dbt import dbt_assets, build_dbt_asset_selection
+
+            @dbt_assets(manifest=...)
+            def all_dbt_assets():
+                ...
+
+            # Select the dbt assets that have the tag "foo".
+            my_selection = build_dbt_asset_selection([dbt_assets], dbt_select="tag:foo")
+    """
+    manifest, dagster_dbt_translator = get_manifest_and_translator_from_dbt_assets(dbt_assets)
+    from .dbt_manifest_asset_selection import DbtManifestAssetSelection
+
+    return DbtManifestAssetSelection(
+        manifest=manifest,
+        dagster_dbt_translator=dagster_dbt_translator,
+        select=dbt_select,
+        exclude=dbt_exclude,
+    )
+
+
+def build_schedule_from_dbt_selection(
+    dbt_assets: Sequence[AssetsDefinition],
+    job_name: str,
+    cron_schedule: str,
+    dbt_select: str = "fqn:*",
+    dbt_exclude: Optional[str] = None,
+    tags: Optional[Mapping[str, str]] = None,
+    config: Optional[RunConfig] = None,
+    execution_timezone: Optional[str] = None,
+) -> ScheduleDefinition:
+    """Build a schedule to materialize a specified set of dbt resources from a dbt selection string.
+
+    See https://docs.getdbt.com/reference/node-selection/syntax#how-does-selection-work for
+    more information.
+
+    Args:
+        job_name (str): The name of the job to materialize the dbt resources.
+        cron_schedule (str): The cron schedule to define the schedule.
+        dbt_select (str): A dbt selection string to specify a set of dbt resources.
+        dbt_exclude (Optional[str]): A dbt selection string to exclude a set of dbt resources.
+        tags (Optional[Mapping[str, str]]): A dictionary of tags (string key-value pairs) to attach
+            to the scheduled runs.
+        config (Optional[RunConfig]): The config that parameterizes the execution of this schedule.
+        execution_timezone (Optional[str]): Timezone in which the schedule should run.
+            Supported strings for timezones are the ones provided by the
+            `IANA time zone database <https://www.iana.org/time-zones>` - e.g. "America/Los_Angeles".
+
+    Returns:
+        ScheduleDefinition: A definition to materialize the selected dbt resources on a cron schedule.
+
+    Examples:
+        .. code-block:: python
+
+            from dagster_dbt import dbt_assets, build_schedule_from_dbt_selection
+
+            @dbt_assets(manifest=...)
+            def all_dbt_assets():
+                ...
+
+            daily_dbt_assets_schedule = build_schedule_from_dbt_selection(
+                [all_dbt_assets],
+                job_name="all_dbt_assets",
+                cron_schedule="0 0 * * *",
+                dbt_select="fqn:*",
+            )
+    """
+    return ScheduleDefinition(
+        cron_schedule=cron_schedule,
+        job=define_asset_job(
+            name=job_name,
+            selection=build_dbt_asset_selection(
+                dbt_assets,
+                dbt_select=dbt_select,
+                dbt_exclude=dbt_exclude,
+            ),
+            config=config,
+            tags=tags,
+        ),
+        execution_timezone=execution_timezone,
+    )
+
+
+def get_manifest_and_translator_from_dbt_assets(
+    dbt_assets: Sequence[AssetsDefinition],
+) -> Tuple[Mapping[str, Any], "DagsterDbtTranslator"]:
+    check.invariant(len(dbt_assets) == 1, "Exactly one dbt AssetsDefinition is required")
+    dbt_assets_def = dbt_assets[0]
+    metadata_by_key = dbt_assets_def.metadata_by_key or {}
+    first_asset_key = next(iter(dbt_assets_def.keys))
+    first_metadata = metadata_by_key.get(first_asset_key, {})
+    manifest = first_metadata.get(MANIFEST_METADATA_KEY)
+    if manifest is None:
+        raise DagsterInvariantViolationError(
+            f"Expected to find dbt manifest metadata on asset {first_asset_key.to_user_string()},"
+            " but did not. Did you pass in assets that weren't generated by"
+            " load_assets_from_dbt_project, load_assets_from_dbt_manifest, or @dbt_assets?"
+        )
+
+    dagster_dbt_translator = first_metadata.get(DAGSTER_DBT_TRANSLATOR_METADATA_KEY)
+    if dagster_dbt_translator is None:
+        raise DagsterInvariantViolationError(
+            f"Expected to find dbt translator metadata on asset {first_asset_key.to_user_string()},"
+            " but did not. Did you pass in assets that weren't generated by"
+            " load_assets_from_dbt_project, load_assets_from_dbt_manifest, or @dbt_assets?"
+        )
+
+    return manifest, dagster_dbt_translator
+
 
 ###################
 # DEFAULT FUNCTIONS
@@ -288,7 +567,13 @@ def get_asset_deps(
 
         asset_deps[asset_key] = set()
 
-        metadata = dagster_dbt_translator.get_metadata(node_info)
+        metadata = merge_dicts(
+            dagster_dbt_translator.get_metadata(node_info),
+            {
+                MANIFEST_METADATA_KEY: manifest,
+                DAGSTER_DBT_TRANSLATOR_METADATA_KEY: dagster_dbt_translator,
+            },
+        )
         asset_outs[asset_key] = (
             output_name,
             Out(

commit b5122eeb4588e22b2cedf92d7df551f355f8d52f

fix(dbt): correctly determine if `DbtCliResource` is in a subselection context (#15466)
## Summary & Motivation
Fix a regression from https://github.com/dagster-io/dagster/pull/15344.

## How I Tested These Changes
add pytest

python_modules/libraries/dagster-dbt/dagster_dbt/core/resources_v2.py
@@ -549,7 +549,9 @@ def get_subset_selection_for_context(
 
     # TODO: this should be a property on the context if this is a permanent indicator for
     # determining whether the current execution context is performing a subsetted execution.
-    is_subsetted_execution = len(context.selected_output_names) != len(context.assets_def.keys)
+    is_subsetted_execution = len(context.selected_output_names) != len(
+        context.assets_def.node_keys_by_output_name
+    )
     if not is_subsetted_execution:
         logger.info(
             "A dbt subsetted execution is not being performed. Using the default dbt selection"

python_modules/libraries/dagster-dbt/dagster_dbt_tests/core/test_resources_v2.py
@@ -15,6 +15,7 @@ from dagster import (
 )
 from dagster._core.execution.context.compute import OpExecutionContext
 from dagster_dbt import dbt_assets
+from dagster_dbt.asset_utils import build_dbt_asset_selection
 from dagster_dbt.core.resources_v2 import (
     PARTIAL_PARSE_FILE_NAME,
     DbtCliEventMessage,
@@ -145,11 +146,8 @@ def test_dbt_cli_subsetted_execution() -> None:
             " fqn:dagster_dbt_test_project.sort_by_calories"
         ),
     )
-    def my_dbt_assets(context):
-        dbt = DbtCliResource(project_dir=TEST_PROJECT_DIR)
-        dbt_cli_invocation = dbt.cli(["run"], context=context)
-
-        dbt_cli_invocation.wait()
+    def my_dbt_assets(context, dbt: DbtCliResource):
+        dbt_cli_invocation = dbt.cli(["run"], context=context).wait()
 
         assert dbt_cli_invocation.process.args == [
             "dbt",
@@ -160,11 +158,49 @@ def test_dbt_cli_subsetted_execution() -> None:
                 " fqn:dagster_dbt_test_project.sort_by_calories"
             ),
         ]
-        assert dbt_cli_invocation.process.returncode is not None
 
         yield from dbt_cli_invocation.stream()
 
-    assert materialize([my_dbt_assets]).success
+    result = materialize(
+        [my_dbt_assets],
+        resources={
+            "dbt": DbtCliResource(project_dir=TEST_PROJECT_DIR),
+        },
+    )
+    assert result.success
+
+
+def test_dbt_cli_asset_selection() -> None:
+    @dbt_assets(manifest=manifest)
+    def my_dbt_assets(context, dbt: DbtCliResource):
+        dbt_cli_invocation = dbt.cli(["run"], context=context).wait()
+
+        assert dbt_cli_invocation.process.args == [
+            "dbt",
+            "run",
+            "--select",
+            (
+                "fqn:dagster_dbt_test_project.subdir.least_caloric"
+                " fqn:dagster_dbt_test_project.sort_by_calories"
+            ),
+        ]
+
+        yield from dbt_cli_invocation.stream()
+
+    result = materialize(
+        [my_dbt_assets],
+        resources={
+            "dbt": DbtCliResource(project_dir=TEST_PROJECT_DIR),
+        },
+        selection=build_dbt_asset_selection(
+            [my_dbt_assets],
+            dbt_select=(
+                "fqn:dagster_dbt_test_project.subdir.least_caloric"
+                " fqn:dagster_dbt_test_project.sort_by_calories"
+            ),
+        ),
+    )
+    assert result.success
 
 
 @pytest.mark.parametrize("exclude", [None, "fqn:dagster_dbt_test_project.subdir.least_caloric"])