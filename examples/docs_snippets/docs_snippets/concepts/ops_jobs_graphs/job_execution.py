8648c444a5cce2b791fce27cb8b64516cc9524b2
prha <1040172+prha@users.noreply.github.com>
2023-07-25 16:04:35 -0700

    add helm values for concurrency free run monitoring (#15465)
    ## Summary & Motivation
    We should have solid support / documentation of the concurrency slot
    freeing daemon for any user checking out global op concurrency
    
    ## How I Tested These Changes
    BK
    
    ---------
    
    Co-authored-by: Erin Cochran <erin.k.cochran@gmail.com>

--- examples/docs_snippets/docs_snippets/concepts/ops_jobs_graphs/job_execution.py
+++ examples/docs_snippets/docs_snippets/concepts/ops_jobs_graphs/job_execution.py
@@ -102,12 +102,3 @@ def tag_concurrency_job():
 
 
 # end_tag_concurrency
-
-
-# start_global_concurrency
-@op(tags={"dagster/concurrency_key": "redshift"})
-def my_redshift_op():
-    ...
-
-
-# end_global_concurrency
