a2ee756bbd9b227e94991a9693b57292079a6305
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2023-08-01 08:34:15 -0500

    [ui] Upgrade Storybook (#15609)
    ## Summary & Motivation
    
    Upgrade Storybook to 7.2. Eliminates the last remaining dependabot
    alert, https://github.com/dagster-io/dagster/security/dependabot/297.
    
    ## How I Tested These Changes
    
    Run Storybook in `core` and `ui`.

--- js_modules/dagit/packages/core/.storybook/main.js
+++ js_modules/dagit/packages/core/.storybook/main.js
@@ -1,10 +1,22 @@
-import path from 'path';
+import path, {dirname, join} from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
 
 const config = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-mdx-gfm'),
+  ],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: getAbsolutePath('@storybook/react-webpack5'),
     options: {},
   },
   // https://storybook.js.org/docs/react/configure/webpack#bundle-splitting
