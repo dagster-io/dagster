432d2db079325df9066f363f74c0a2b34b7f21f3
Tim Castillo <tim@elementl.com>
2023-08-02 13:58:27 -0400

    addressing PR comments and unleashing for review

--- examples/project_dagster_university_start/setup.py
+++ examples/project_dagster_university_start/setup.py
@@ -4,18 +4,14 @@ setup(
     name="dagster_university",
     packages=find_packages(exclude=["dagster_university_tests"]),
     install_requires=[
-        "dagster==1.3.*",
+        "dagster==1.4.*",
         "dagster-cloud",
-        "dagster-dbt",
         "dagster-duckdb",
-        "dagster-gcp",
-        "dbt-duckdb",
         "geopandas",
         "kaleido",
         "pandas",
         "plotly",
-        "psycopg2-binary",
         "shapely"
     ],
-    extras_require={"dev": ["dagit", "pytest"]},
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
