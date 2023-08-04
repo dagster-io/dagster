04860eddd0d19baa541411761e96b686fda217b3
Tim Castillo <tim@elementl.com>
2023-08-04 11:27:20 -0400

    except I pushed from the docs directory so the python files didn't get added

--- examples/docs_snippets/docs_snippets/tutorial/building_an_asset_graph/assets_initial_state.py
+++ examples/docs_snippets/docs_snippets/tutorial/building_an_asset_graph/assets_initial_state.py
@@ -27,6 +27,9 @@ import requests
 from dagster import asset
 
 
+# ... Keep the `topstory_ids` asset from the previous section
+
+
 @asset(deps=[topstory_ids])  # this asset is dependent on topstory_ids
 def topstories() -> None:
     with open("data/topstory_ids.json", "r") as f:
