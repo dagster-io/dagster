2e742dd5fcd80f34ae76a9628499388d5c94665d
Owen Kephart <owen@elementl.com>
2023-08-08 11:58:23 -0700

    safer generation of output names

--- python_modules/libraries/dagster-dbt/dagster_dbt/utils.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/utils.py
@@ -44,7 +44,7 @@ def input_name_fn(dbt_resource_props: Mapping[str, Any]) -> str:
 
 
 def output_name_fn(dbt_resource_props: Mapping[str, Any]) -> str:
-    return dbt_resource_props["unique_id"].split(".")[-1]
+    return dbt_resource_props["unique_id"].split(".")[-1].replace("-", "_")
 
 
 def _node_result_to_metadata(node_result: Mapping[str, Any]) -> Mapping[str, RawMetadataValue]:
