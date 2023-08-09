72275aeee666738cc7b72e7356e18139026e77ce
JamieDeMaria <jamie@elementl.com>
2023-08-09 16:33:55 -0400

    add a section on using ops

--- examples/docs_snippets/docs_snippets/integrations/duckdb/reference/resource.py
+++ examples/docs_snippets/docs_snippets/integrations/duckdb/reference/resource.py
@@ -2,25 +2,20 @@ from ..tutorial.resource.create_table import iris_dataset  # noqa: I001
 
 
 # start
-import pandas as pd
 from dagster_duckdb import DuckDBResource
 
 from dagster import asset
 
-# this example executes a query against the IRIS_DATASET table created in Step 2 of the
+# this example executes a query against the iris_dataset table created in Step 2 of the
 # Using Dagster with DuckDB tutorial
 
 
 @asset(deps=[iris_dataset])
-def small_petals(duckdb: DuckDBResource) -> pd.DataFrame:
+def small_petals(duckdb: DuckDBResource) -> None:
     with duckdb.get_connection() as conn:  # conn is a DuckDBPyConnection
-        return (
-            conn.cursor()
-            .execute(
-                "SELECT * FROM iris.iris_dataset WHERE 'petal_length_cm' < 1 AND"
-                " 'petal_width_cm' < 1"
-            )
-            .fetch_df()
+        conn.cursor().execute(
+            "CREATE TABLE iris.small_petals AS SELECT * FROM iris.iris_dataset WHERE"
+            " 'petal_length_cm' < 1 AND 'petal_width_cm' < 1"
         )
 
 
