c5a44affacf2ccaf30aeffa683536da72ef7c805
Isaac Hellendag <dish@elementl.com>
2023-08-11 11:17:57 -0500

    [ui] LS key migration fixes

--- js_modules/dagster-ui/packages/ui-core/src/app/migrateLocalStorageKeys.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/migrateLocalStorageKeys.tsx
@@ -1,8 +1,26 @@
-export const migrateLocalStorageKeys = ({from, to}: {from: RegExp; to: string}) => {
+type Input = {
+  from: RegExp;
+  to: string;
+  deleteExisting?: boolean;
+};
+
+export const migrateLocalStorageKeys = ({from, to, deleteExisting = false}: Input) => {
   Object.entries(window.localStorage).forEach(([key, value]) => {
     if (from.test(key)) {
       const newKey = key.replaceAll(from, to);
-      window.localStorage.setItem(newKey, value);
+
+      // If the new key doesn't exist yet, write it.
+      if (window.localStorage.getItem(newKey) === null) {
+        try {
+          window.localStorage.setItem(newKey, value);
+        } catch (e) {
+          // Failed to write. Probably a QuotaExceededError.
+        }
+      }
+
+      if (deleteExisting) {
+        window.localStorage.removeItem(key);
+      }
     }
   });
 };
