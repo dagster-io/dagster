e39ada9d0d122307c0b6bd5d1529f6324d471cf1
elementl <devtools@elementl.com>
2023-08-17 17:10:57 +0000

    1.4.6

--- python_modules/libraries/dagster-celery-docker/setup.py
+++ python_modules/libraries/dagster-celery-docker/setup.py
@@ -31,9 +31,9 @@ setup(
     ],
     packages=find_packages(exclude=["dagster_celery_docker_tests*"]),
     install_requires=[
-        f"dagster{pin}",
-        f"dagster-celery{pin}",
-        f"dagster-graphql{pin}",
+        "dagster==1.4.6",
+        "dagster-celery==0.20.6",
+        "dagster-graphql==1.4.6",
         "docker",
     ],
     zip_safe=False,
--- python_modules/libraries/dagster-gcp-pyspark/setup.py
+++ python_modules/libraries/dagster-gcp-pyspark/setup.py
@@ -35,8 +35,8 @@ setup(
     ],
     packages=find_packages(exclude=["dagster_gcp_pyspark_tests*"]),
     install_requires=[
-        f"dagster{pin}",
-        f"dagster-gcp{pin}",
+        "dagster==1.4.6",
+        "dagster-gcp==0.20.6",
         "pyspark",
     ],
     extras_require={"test": ["pandas-gbq"]},
