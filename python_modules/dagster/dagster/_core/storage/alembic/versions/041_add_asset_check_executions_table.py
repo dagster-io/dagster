20462701a9f823899901af7aa9cf4e5c93265676
Johann Miller <johann@elementl.com>
2023-08-21 12:13:03 -0400

    alembic

---
+++ python_modules/dagster/dagster/_core/storage/alembic/versions/041_add_asset_check_executions_table.py
@@ -0,0 +1,69 @@
+"""add asset_check_executions table
+
+Revision ID: ec80dd91891a
+Revises: 5771160a95ad
+Create Date: 2023-08-21 12:05:47.817280
+
+"""
+import sqlalchemy as db
+from alembic import op
+from dagster._core.storage.migration.utils import has_index, has_table
+from dagster._core.storage.sql import get_current_timestamp
+from sqlalchemy.dialects import sqlite
+
+# revision identifiers, used by Alembic.
+revision = "ec80dd91891a"
+down_revision = "5771160a95ad"
+branch_labels = None
+depends_on = None
+
+TABLE_NAME = "asset_check_executions"
+INDEX_NAME = "idx_asset_check_executions_unique"
+
+
+def upgrade():
+    if not has_table(TABLE_NAME):
+        op.create_table(
+            TABLE_NAME,
+            db.Column(
+                "id",
+                db.BigInteger().with_variant(sqlite.INTEGER(), "sqlite"),
+                primary_key=True,
+                autoincrement=True,
+            ),
+            db.Column("asset_key", db.Text),
+            db.Column("check_name", db.Text),
+            db.Column(
+                "partition", db.Text
+            ),  # Currently unused. Planned for future partition support
+            db.Column("run_id", db.Text),
+            db.Column("execution_status", db.Text),  # Planned, Success, or Failure
+            db.Column("event_record", db.Text),
+            db.Column("event_timestamp", db.DateTime),
+            db.Column(
+                "materialization_storage_id",
+                db.BigInteger().with_variant(sqlite.INTEGER(), "sqlite"),
+            ),
+            db.Column("create_timestamp", db.DateTime, server_default=get_current_timestamp()),
+        )
+
+    if not has_index(TABLE_NAME, INDEX_NAME):
+        op.create_index(
+            INDEX_NAME,
+            TABLE_NAME,
+            [
+                "asset_key",
+                "check_name",
+                "run_id",
+                "partition",
+            ],
+            unique=True,
+        )
+
+
+def downgrade():
+    if has_table(TABLE_NAME):
+        if has_index(TABLE_NAME, INDEX_NAME):
+            op.drop_index(INDEX_NAME, TABLE_NAME)
+
+        op.drop_table(TABLE_NAME)
