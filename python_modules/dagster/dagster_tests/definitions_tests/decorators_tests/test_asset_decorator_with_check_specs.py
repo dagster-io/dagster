c7e74f647ef6ffba3fe6a577a8e53b1a7ca80ac9
Sandy Ryza <sandy@elementl.com>
2023-08-22 08:01:29 -0700

    check_specs argument on @multi_asset (#15948)
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- python_modules/dagster/dagster_tests/definitions_tests/decorators_tests/test_asset_decorator_with_check_specs.py
+++ python_modules/dagster/dagster_tests/definitions_tests/decorators_tests/test_asset_decorator_with_check_specs.py
@@ -5,10 +5,12 @@ from dagster import (
     AssetCheckResult,
     AssetCheckSpec,
     AssetKey,
+    AssetOut,
     MetadataValue,
     Output,
     asset,
     materialize,
+    multi_asset,
 )
 from dagster._core.errors import (
     DagsterInvalidDefinitionError,
@@ -155,6 +157,27 @@ def test_asset_decorator_unexpected_asset_key():
         materialize(assets=[asset1])
 
 
+def test_result_missing_check_name():
+    @asset(
+        check_specs=[
+            AssetCheckSpec("check1", asset_key="asset1"),
+            AssetCheckSpec("check2", asset_key="asset1"),
+        ]
+    )
+    def asset1():
+        yield Output(None)
+        yield AssetCheckResult(success=True)
+
+    with pytest.raises(
+        DagsterInvariantViolationError,
+        match=re.escape(
+            "AssetCheckResult result didn't specify a check name, but there are multiple checks to"
+            " choose from for the this asset key:"
+        ),
+    ):
+        materialize(assets=[asset1])
+
+
 def test_asset_check_fails_downstream_still_executes():
     @asset(check_specs=[AssetCheckSpec("check1", asset_key=AssetKey("asset1"))])
     def asset1():
@@ -193,3 +216,68 @@ def test_duplicate_checks_same_asset():
         )
         def asset1():
             ...
+
+
+def test_multi_asset_with_check():
+    @multi_asset(
+        outs={"one": AssetOut("asset1"), "two": AssetOut("asset2")},
+        check_specs=[AssetCheckSpec("check1", asset_key="asset1", description="desc")],
+    )
+    def asset_1_and_2():
+        yield Output(None, output_name="one")
+        yield Output(None, output_name="two")
+        yield AssetCheckResult(check_name="check1", success=True, metadata={"foo": "bar"})
+
+    result = materialize(assets=[asset_1_and_2])
+    assert result.success
+
+    assert len(result.get_asset_materialization_events()) == 2
+
+    check_evals = result.get_asset_check_evaluations()
+    assert len(check_evals) == 1
+    check_eval = check_evals[0]
+    assert check_eval.asset_key == AssetKey("asset1")
+    assert check_eval.check_name == "check1"
+    assert check_eval.metadata == {"foo": MetadataValue.text("bar")}
+
+
+def test_multi_asset_no_result_for_check():
+    @multi_asset(
+        outs={"one": AssetOut("asset1"), "two": AssetOut("asset2")},
+        check_specs=[AssetCheckSpec("check1", asset_key="asset1", description="desc")],
+    )
+    def asset_1_and_2():
+        yield Output(None, output_name="one")
+        yield Output(None, output_name="two")
+
+    with pytest.raises(
+        DagsterStepOutputNotFoundError,
+        match=(
+            'Core compute for op "asset_1_and_2" did not return an output for non-optional output'
+            ' "asset1_check1"'
+        ),
+    ):
+        materialize(assets=[asset_1_and_2])
+
+
+def test_result_missing_asset_key():
+    @multi_asset(
+        outs={"one": AssetOut("asset1"), "two": AssetOut("asset2")},
+        check_specs=[
+            AssetCheckSpec("check1", asset_key="asset1"),
+            AssetCheckSpec("check2", asset_key="asset2"),
+        ],
+    )
+    def asset_1_and_2():
+        yield Output(None, output_name="one")
+        yield Output(None, output_name="two")
+        yield AssetCheckResult(success=True)
+
+    with pytest.raises(
+        DagsterInvariantViolationError,
+        match=re.escape(
+            "AssetCheckResult didn't specify an asset key, but there are multiple assets to choose"
+            " from: ['asset1', 'asset2']"
+        ),
+    ):
+        materialize(assets=[asset_1_and_2])
