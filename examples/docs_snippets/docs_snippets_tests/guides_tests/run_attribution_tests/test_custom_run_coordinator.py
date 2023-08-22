73d9345276d8f76de021b9b8549d09c484d0a90d
Sean Mackesey <s.mackesey@gmail.com>
2023-08-22 16:40:11 -0400

    misc

--- examples/docs_snippets/docs_snippets_tests/guides_tests/run_attribution_tests/test_custom_run_coordinator.py
+++ examples/docs_snippets/docs_snippets_tests/guides_tests/run_attribution_tests/test_custom_run_coordinator.py
@@ -48,7 +48,7 @@ class TestCustomRunCoordinator(TestQueuedRunCoordinator):
         run_id, jwt_header, expected_email = (
             "foo",
             "foo.eyJlbWFpbCI6ICJoZWxsb0BlbGVtZW50bC5jb20ifQ==.bar",
-            "hello@dagsterlabs.com",
+            "hello@elementl.com",
         )
         MockRequest = namedtuple("MockRequest", ["headers"])
         workspace._source = MockRequest(  # noqa: SLF001
