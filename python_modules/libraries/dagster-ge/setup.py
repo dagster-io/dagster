7a128d60d4d3843922ed14531d37265036945978
Shalabh Chaturvedi <shalabh@dagsterlabs.com>
2023-08-24 17:05:03 -0700

    Exclude great_expectations 0.17.12 to avoid FileDataContext bug (#16086)
    Seeing `AttributeError: 'FileDataContext' object has no attribute
    'get_batch'` with the new version.
    
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- python_modules/libraries/dagster-ge/setup.py
+++ python_modules/libraries/dagster-ge/setup.py
@@ -34,7 +34,7 @@ setup(
         f"dagster{pin}",
         f"dagster-pandas{pin}",
         "pandas",
-        "great_expectations >=0.11.9, !=0.12.8, !=0.13.17, !=0.13.27",
+        "great_expectations >=0.11.9, !=0.12.8, !=0.13.17, !=0.13.27, <0.17.12",
     ],
     extras_require={
         "test": [
