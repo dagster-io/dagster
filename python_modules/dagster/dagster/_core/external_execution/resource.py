468ac757fd67caa07d378abfb051c49bc8c373df
Sean Mackesey <s.mackesey@gmail.com>
2023-08-26 12:02:59 -0400

    [externals] Remove `ExternalExecutionTask`, add `ExternalExecutionOrchestrationContext` (#16125)
    ## Summary & Motivation
    
    - Remove `ExternalExecutionTask`
    - Fold logic of adapter subclasses of `ExternalExecutionTask` into
    resources (e.g. `SubprocessExecutionResource` now directly implements
    io/launch logic)
    - Create new `ExternalExecutionOrchestrationContext` class used by IO
    
    After the preceding refactors, `ExternalExecutionTask` had become
    hollowed out and was an unnecessary abstraction for purposes of
    launching an external process. However, it was still serving an
    important purpose that couldn't (cleanly) be served directly by
    `ExternalExecutionResource`: the binding of `OpExecutionContext`/extras
    and translation of incoming messages into equivalent calls to
    `OpExecutionContext`.
    
    In this PR, the process-launching and message-translation functionality
    has been decoupled. Process launching is now handled directly by
    `ExternalExecutionResource.run`. Message translation is handled by a new
    `ExternalExecutionOrchestrationContext` class. This means that adapters
    now only need to implement a single `ExternalExecutionResource` class,
    and can make use of the common `ExternalExecutionOrchestrationContext`.
    
    ## How I Tested These Changes
    
    Existing test suite.

--- python_modules/dagster/dagster/_core/external_execution/resource.py
+++ python_modules/dagster/dagster/_core/external_execution/resource.py
@@ -1,13 +1,20 @@
+import json
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Mapping, Optional
 
-from dagster_externals import ExternalExecutionExtras
+from dagster_externals import (
+    DAGSTER_EXTERNALS_ENV_KEYS,
+    ExternalExecutionExtras,
+)
 
 from dagster._config.pythonic_config import ConfigurableResource
 from dagster._core.execution.context.compute import OpExecutionContext
 
 
 class ExternalExecutionResource(ConfigurableResource, ABC):
+    def get_base_env(self) -> Mapping[str, str]:
+        return {DAGSTER_EXTERNALS_ENV_KEYS["is_orchestration_active"]: json.dumps(True)}
+
     @abstractmethod
     def run(
         self,
