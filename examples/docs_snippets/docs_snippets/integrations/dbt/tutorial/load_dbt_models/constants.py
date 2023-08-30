2eb5cda4c583fa5fb185866e5502fb61abe75fe3
Rex Ledesma <rex@dagsterlabs.com>
2023-08-30 15:43:56 -0400

    feat(dbt): remove empty `manifest` argument in scaffold

--- examples/docs_snippets/docs_snippets/integrations/dbt/tutorial/load_dbt_models/constants.py
+++ examples/docs_snippets/docs_snippets/integrations/dbt/tutorial/load_dbt_models/constants.py
@@ -8,10 +8,10 @@ from dagster_dbt import DbtCliResource
 dbt_project_dir = Path(__file__).joinpath("..", "..", "..").resolve()
 dbt = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
 
-# If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at runtime.
+# If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at run time.
 # Otherwise, we expect a manifest to be present in the project's target directory.
 if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
-    dbt_parse_invocation = dbt.cli(["parse"], manifest={}).wait()
+    dbt_parse_invocation = dbt.cli(["parse"]).wait()
     dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
 else:
     dbt_manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
