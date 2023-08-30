dagster-io/dagster

commit 862c5656541fd18b4ed8c8fd63c1a8532491db95

direct execution context cleanup (#15042)
* move more logic to `bind` time, removing the need for various context
chaining & argument passing
* refactor direct invocation processing to remove duplication of
`context` arg handling
* drop some error checking on `None` passed for `context` since it gets
pretty weird with how config/resources by arg work

## How I Tested These Changes

existing and updated tests

python_modules/dagster/dagster/_core/definitions/assets.py
@@ -19,13 +19,13 @@ from typing import (
 
 import dagster._check as check
 from dagster._annotations import experimental_param, public
-from dagster._core.decorator_utils import get_function_params
 from dagster._core.definitions.asset_layer import get_dep_node_handles_of_graph_backed_asset
 from dagster._core.definitions.auto_materialize_policy import AutoMaterializePolicy
 from dagster._core.definitions.backfill_policy import BackfillPolicy, BackfillPolicyType
 from dagster._core.definitions.freshness_policy import FreshnessPolicy
 from dagster._core.definitions.metadata import ArbitraryMetadataMapping
 from dagster._core.definitions.multi_dimensional_partitions import MultiPartitionsDefinition
+from dagster._core.definitions.op_invocation import direct_invocation_result
 from dagster._core.definitions.op_selection import get_graph_subset
 from dagster._core.definitions.partition_mapping import MultiPartitionMapping
 from dagster._core.definitions.time_window_partition_mapping import TimeWindowPartitionMapping
@@ -58,8 +58,6 @@ from .source_asset import SourceAsset
 from .utils import DEFAULT_GROUP_NAME, validate_group_name
 
 if TYPE_CHECKING:
-    from dagster._core.execution.context.compute import OpExecutionContext
-
     from .graph_definition import GraphDefinition
 
 
@@ -291,33 +289,14 @@ class AssetsDefinition(ResourceAddable, IHasInternalInit):
         )
 
     def __call__(self, *args: object, **kwargs: object) -> object:
-        from dagster._core.definitions.decorators.op_decorator import DecoratedOpFunction
-        from dagster._core.execution.context.compute import OpExecutionContext
-
         from .graph_definition import GraphDefinition
 
+        # defer to GraphDefinition.__call__ for graph backed assets
         if isinstance(self.node_def, GraphDefinition):
             return self._node_def(*args, **kwargs)
-        op_def = self.op
-        provided_context: Optional[OpExecutionContext] = None
-        if len(args) > 0 and isinstance(args[0], OpExecutionContext):
-            provided_context = _build_invocation_context_with_included_resources(self, args[0])
-            new_args = [provided_context, *args[1:]]
-            return op_def(*new_args, **kwargs)
-        elif (
-            isinstance(op_def.compute_fn, DecoratedOpFunction)
-            and op_def.compute_fn.has_context_arg()
-        ):
-            context_param_name = get_function_params(op_def.compute_fn.decorated_fn)[0].name
-            if context_param_name in kwargs:
-                provided_context = _build_invocation_context_with_included_resources(
-                    self, cast(OpExecutionContext, kwargs[context_param_name])
-                )
-                new_kwargs = dict(kwargs)
-                new_kwargs[context_param_name] = provided_context
-                return op_def(*args, **new_kwargs)
 
-        return op_def(*args, **kwargs)
+        # invoke against self to allow assets def information to be used
+        return direct_invocation_result(self, *args, **kwargs)
 
     @public
     @experimental_param(param="resource_defs")
@@ -1296,43 +1275,6 @@ def _infer_keys_by_output_names(
         if output_name not in inferred_keys_by_output_names:
             inferred_keys_by_output_names[output_name] = AssetKey([output_name])
     return inferred_keys_by_output_names
-
-
-def _build_invocation_context_with_included_resources(
-    assets_def: AssetsDefinition,
-    context: "OpExecutionContext",
-) -> "OpExecutionContext":
-    from dagster._core.execution.context.invocation import (
-        UnboundOpExecutionContext,
-        build_op_context,
-    )
-
-    resource_defs = assets_def.resource_defs
-    invocation_resources = context.resources._asdict()
-    for resource_key in sorted(list(invocation_resources.keys())):
-        if resource_key in resource_defs:
-            raise DagsterInvalidInvocationError(
-                f"Error when invoking {assets_def}: resource '{resource_key}' "
-                "provided on both the definition and invocation context. Please "
-                "provide on only one or the other."
-            )
-    all_resources = merge_dicts(resource_defs, invocation_resources)
-
-    if isinstance(context, UnboundOpExecutionContext):
-        return build_op_context(
-            resources=all_resources,
-            config=context.op_config,
-            resources_config=context._resources_config,  # noqa: SLF001
-            instance=context._instance,  # noqa: SLF001
-            partition_key=context._partition_key,  # noqa: SLF001
-            partition_key_range=context._partition_key_range,  # noqa: SLF001
-            mapping_key=context._mapping_key,  # noqa: SLF001
-            _assets_def=assets_def,
-        )
-    else:
-        # If user is mocking OpExecutionContext, send it through (we don't know
-        # what modifications they might be making, and we don't want to override)
-        return context
 
 
 def _validate_graph_def(graph_def: "GraphDefinition", prefix: Optional[Sequence[str]] = None):

python_modules/dagster/dagster/_core/definitions/composition.py
@@ -430,45 +430,16 @@ class PendingNodeInvocation(Generic[T_NodeDefinition]):
             current_context().add_pending_invocation(self)
 
     def __call__(self, *args, **kwargs) -> Any:
-        from ..execution.context.invocation import UnboundOpExecutionContext
-        from .decorators.op_decorator import DecoratedOpFunction
-        from .op_invocation import op_invocation_result
+        from .op_invocation import direct_invocation_result
 
         node_name = self.given_alias if self.given_alias else self.node_def.name
 
         # If PendingNodeInvocation is not within composition context, and underlying NodeDefinition
         # is an OpDefinition, then permit it to be invoked and executed like an OpDefinition.
         if not is_in_composition() and isinstance(self.node_def, OpDefinition):
-            node_label = self.node_def.node_type_str
-            # Pyright does not currently infer the typevar-bound type of self based on the above
-            # `isinstance` check. We need the correct type for passing to `op_invocation_result`.
-            _self = cast(PendingNodeInvocation[OpDefinition], self)
-            if not isinstance(self.node_def.compute_fn, DecoratedOpFunction):
-                raise DagsterInvalidInvocationError(
-                    f"Attemped to invoke {node_label} that was not constructed using the"
-                    f" `@{node_label}` decorator. Only {node_label}s constructed using the"
-                    f" `@{node_label}` decorator can be directly invoked."
-                )
-            if self.node_def.compute_fn.has_context_arg():
-                if len(args) == 0:
-                    raise DagsterInvalidInvocationError(
-                        f"Compute function of {node_label} '{self.given_alias}' has context"
-                        " argument, but no context was provided when invoking."
-                    )
-                elif args[0] is not None and not isinstance(args[0], UnboundOpExecutionContext):
-                    raise DagsterInvalidInvocationError(
-                        f"Compute function of {node_label} '{self.given_alias}' has context"
-                        " argument, but no context was provided when invoking."
-                    )
-                context = args[0]
-                return op_invocation_result(_self, context, *args[1:], **kwargs)
-            else:
-                if len(args) > 0 and isinstance(args[0], UnboundOpExecutionContext):
-                    raise DagsterInvalidInvocationError(
-                        f"Compute function of {node_label} '{self.given_alias}' has no context"
-                        " argument, but context was provided when invoking."
-                    )
-                return op_invocation_result(_self, None, *args, **kwargs)
+            return direct_invocation_result(
+                cast(PendingNodeInvocation[OpDefinition], self), *args, **kwargs
+            )
 
         assert_in_composition(node_name, self.node_def)
         input_bindings: Dict[str, InputSource] = {}

python_modules/dagster/dagster/_core/definitions/decorators/op_decorator.py
@@ -278,6 +278,10 @@ class DecoratedOpFunction(NamedTuple):
 
     decorated_fn: Callable[..., Any]
 
+    @property
+    def name(self):
+        return self.decorated_fn.__name__
+
     @lru_cache(maxsize=1)
     def has_context_arg(self) -> bool:
         return is_context_provided(get_function_params(self.decorated_fn))

python_modules/dagster/dagster/_core/definitions/op_definition.py
@@ -20,10 +20,9 @@ from typing_extensions import TypeAlias, get_args, get_origin
 import dagster._check as check
 from dagster._annotations import deprecated, deprecated_param, public
 from dagster._config.config_schema import UserConfigSchema
-from dagster._core.decorator_utils import get_function_params
 from dagster._core.definitions.dependency import NodeHandle, NodeInputHandle
 from dagster._core.definitions.node_definition import NodeDefinition
-from dagster._core.definitions.op_invocation import op_invocation_result
+from dagster._core.definitions.op_invocation import direct_invocation_result
 from dagster._core.definitions.policy import RetryPolicy
 from dagster._core.definitions.resource_requirement import (
     InputManagerRequirement,
@@ -442,56 +441,12 @@ class OpDefinition(NodeDefinition, IHasInternalInit):
         return [input_handle]
 
     def __call__(self, *args, **kwargs) -> Any:
-        from ..execution.context.invocation import UnboundOpExecutionContext
         from .composition import is_in_composition
-        from .decorators.op_decorator import DecoratedOpFunction
 
         if is_in_composition():
             return super(OpDefinition, self).__call__(*args, **kwargs)
-        else:
-            if not isinstance(self.compute_fn, DecoratedOpFunction):
-                raise DagsterInvalidInvocationError(
-                    "Attemped to invoke op that was not constructed using the"
-                    " `@op` decorator. Only ops constructed using the"
-                    " `@op` decorator can be directly invoked."
-                )
-            if self.compute_fn.has_context_arg():
-                if len(args) + len(kwargs) == 0:
-                    raise DagsterInvalidInvocationError(
-                        f"Compute function of op '{self.name}' has context argument, but"
-                        " no context was provided when invoking."
-                    )
-                if len(args) > 0:
-                    if args[0] is not None and not isinstance(args[0], UnboundOpExecutionContext):
-                        raise DagsterInvalidInvocationError(
-                            f"Compute function of op '{self.name}' has context argument, "
-                            "but no context was provided when invoking."
-                        )
-                    context = args[0]
-                    return op_invocation_result(self, context, *args[1:], **kwargs)
-                # Context argument is provided under kwargs
-                else:
-                    context_param_name = get_function_params(self.compute_fn.decorated_fn)[0].name
-                    if context_param_name not in kwargs:
-                        raise DagsterInvalidInvocationError(
-                            f"Compute function of op '{self.name}' has context argument "
-                            f"'{context_param_name}', but no value for '{context_param_name}' was "
-                            f"found when invoking. Provided kwargs: {kwargs}"
-                        )
-                    context = cast(UnboundOpExecutionContext, kwargs[context_param_name])
-                    kwargs_sans_context = {
-                        kwarg: val
-                        for kwarg, val in kwargs.items()
-                        if not kwarg == context_param_name
-                    }
-                    return op_invocation_result(self, context, *args, **kwargs_sans_context)
 
-            else:
-                context = None
-                if len(args) > 0 and isinstance(args[0], UnboundOpExecutionContext):
-                    context = cast(UnboundOpExecutionContext, args[0])
-                    args = args[1:]
-                return op_invocation_result(self, context, *args, **kwargs)
+        return direct_invocation_result(self, *args, **kwargs)
 
 
 def _resolve_output_defs_from_outs(

python_modules/dagster/dagster/_core/definitions/op_invocation.py
@@ -5,7 +5,6 @@ from typing import (
     Dict,
     Mapping,
     NamedTuple,
-    Optional,
     Tuple,
     TypeVar,
     Union,
@@ -30,7 +29,8 @@ from .events import (
 from .output import DynamicOutputDefinition
 
 if TYPE_CHECKING:
-    from ..execution.context.invocation import BoundOpExecutionContext, UnboundOpExecutionContext
+    from ..execution.context.invocation import BoundOpExecutionContext
+    from .assets import AssetsDefinition
     from .composition import PendingNodeInvocation
     from .decorators.op_decorator import DecoratedOpFunction
     from .op_definition import OpDefinition
@@ -98,30 +98,80 @@ def _separate_args_and_kwargs(
     )
 
 
-def op_invocation_result(
-    op_def_or_invocation: Union["OpDefinition", "PendingNodeInvocation[OpDefinition]"],
-    context: Optional["UnboundOpExecutionContext"],
+def direct_invocation_result(
+    def_or_invocation: Union[
+        "OpDefinition", "PendingNodeInvocation[OpDefinition]", "AssetsDefinition"
+    ],
     *args,
     **kwargs,
 ) -> Any:
-    from dagster._core.definitions.decorators.op_decorator import DecoratedOpFunction
-    from dagster._core.execution.context.invocation import build_op_context
-
-    from .composition import PendingNodeInvocation
-
-    op_def = (
-        op_def_or_invocation.node_def
-        if isinstance(op_def_or_invocation, PendingNodeInvocation)
-        else op_def_or_invocation
+    from dagster._config.pythonic_config import Config
+    from dagster._core.execution.context.invocation import (
+        UnboundOpExecutionContext,
+        build_op_context,
     )
+
+    from ..execution.plan.compute_generator import invoke_compute_fn
+    from .assets import AssetsDefinition
+    from .composition import PendingNodeInvocation
+    from .decorators.op_decorator import DecoratedOpFunction
+    from .op_definition import OpDefinition
+
+    if isinstance(def_or_invocation, OpDefinition):
+        op_def = def_or_invocation
+        pending_invocation = None
+        assets_def = None
+    elif isinstance(def_or_invocation, AssetsDefinition):
+        assets_def = def_or_invocation
+        op_def = assets_def.op
+        pending_invocation = None
+    elif isinstance(def_or_invocation, PendingNodeInvocation):
+        pending_invocation = def_or_invocation
+        op_def = def_or_invocation.node_def
+        assets_def = None
+    else:
+        check.failed(f"unexpected direct invocation target {def_or_invocation}")
 
     compute_fn = op_def.compute_fn
     if not isinstance(compute_fn, DecoratedOpFunction):
-        check.failed("op invocation only works with decorated op fns")
+        raise DagsterInvalidInvocationError(
+            "Attempted to directly invoke an op/asset that was not constructed using the `@op` or"
+            " `@asset` decorator. Only decorated functions can be directly invoked."
+        )
 
-    compute_fn = cast(DecoratedOpFunction, compute_fn)
-
-    from ..execution.plan.compute_generator import invoke_compute_fn
+    context = None
+    if compute_fn.has_context_arg():
+        if len(args) + len(kwargs) == 0:
+            raise DagsterInvalidInvocationError(
+                f"Decorated function '{compute_fn.name}' has context argument, but"
+                " no context was provided when invoking."
+            )
+        if len(args) > 0:
+            if args[0] is not None and not isinstance(args[0], UnboundOpExecutionContext):
+                raise DagsterInvalidInvocationError(
+                    f"Decorated function '{compute_fn.name}' has context argument, "
+                    "but no context was provided when invoking."
+                )
+            context = cast(UnboundOpExecutionContext, args[0])
+            # update args to omit context
+            args = args[1:]
+        else:  # context argument is provided under kwargs
+            context_param_name = get_function_params(compute_fn.decorated_fn)[0].name
+            if context_param_name not in kwargs:
+                raise DagsterInvalidInvocationError(
+                    f"Decorated function '{compute_fn.name}' has context argument "
+                    f"'{context_param_name}', but no value for '{context_param_name}' was "
+                    f"found when invoking. Provided kwargs: {kwargs}"
+                )
+            context = cast(UnboundOpExecutionContext, kwargs[context_param_name])
+            # update kwargs to remove context
+            kwargs = {
+                kwarg: val for kwarg, val in kwargs.items() if not kwarg == context_param_name
+            }
+    # allow passing context, even if the function doesn't have an arg for it
+    elif len(args) > 0 and isinstance(args[0], UnboundOpExecutionContext):
+        context = cast(UnboundOpExecutionContext, args[0])
+        args = args[1:]
 
     resource_arg_mapping = {arg.name: arg.name for arg in compute_fn.get_resource_args()}
 
@@ -142,30 +192,17 @@ def op_invocation_result(
     resources_by_param_name = extracted.resources_by_param_name
     config_input = extracted.config_arg
 
-    resources_provided_in_multiple_places = (
-        resources_by_param_name and context and context.resource_keys
-    )
-    if resources_provided_in_multiple_places:
-        raise DagsterInvalidInvocationError("Cannot provide resources in both context and kwargs")
-
-    if resources_by_param_name:
-        context = (context or build_op_context()).replace_resources(resources_by_param_name)
-
-    config_provided_in_multiple_places = config_input and context and context.op_config
-    if config_provided_in_multiple_places:
-        raise DagsterInvalidInvocationError("Cannot provide config in both context and kwargs")
-    if config_input:
-        from dagster._config.pythonic_config import Config
-
-        context = (context or build_op_context()).replace_config(
+    bound_context = (context or build_op_context()).bind(
+        op_def=op_def,
+        pending_invocation=pending_invocation,
+        assets_def=assets_def,
+        resources_from_args=resources_by_param_name,
+        config_from_args=(
             config_input._convert_to_config_dictionary()  # noqa: SLF001
             if isinstance(config_input, Config)
             else config_input
-        )
-
-    _check_invocation_requirements(op_def, context)
-
-    bound_context = (context or build_op_context()).bind(op_def_or_invocation)
+        ),
+    )
 
     input_dict = _resolve_inputs(op_def, input_args, input_kwargs, bound_context)
 
@@ -181,36 +218,6 @@ def op_invocation_result(
     )
 
     return _type_check_output_wrapper(op_def, result, bound_context)
-
-
-def _check_invocation_requirements(
-    op_def: "OpDefinition", context: Optional["UnboundOpExecutionContext"]
-) -> None:
-    """Ensure that provided context fulfills requirements of op definition.
-
-    If no context was provided, then construct an enpty UnboundOpExecutionContext
-    """
-    # Check resource requirements
-    if (
-        op_def.required_resource_keys
-        and cast("DecoratedOpFunction", op_def.compute_fn).has_context_arg()
-        and context is None
-    ):
-        node_label = op_def.node_type_str
-        raise DagsterInvalidInvocationError(
-            f'{node_label} "{op_def.name}" has required resources, but no context was provided.'
-            f" Use the `build_{node_label}_context` function to construct a context with the"
-            " required resources."
-        )
-
-    # Check config requirements
-    if not context and op_def.config_schema.as_field().is_required:
-        node_label = op_def.node_type_str
-        raise DagsterInvalidInvocationError(
-            f'{node_label} "{op_def.name}" has required config schema, but no context was'
-            f" provided. Use the `build_{node_label}_context` function to create a context with"
-            " config."
-        )
 
 
 def _resolve_inputs(

python_modules/dagster/dagster/_core/execution/context/compute.py
@@ -234,7 +234,7 @@ class OpExecutionContext(AbstractComputeExecutionContext):
 
     @property
     def op(self) -> Node:
-        """Solid: The current op object.
+        """Node: The object representing the invoked op within the graph.
 
         :meta private:
 

commit de483b946216833e6e263a0d86ebbd529c724aef

[bugfix] assets used in composition (#16204)
broken by https://github.com/dagster-io/dagster/pull/15042, this use
case had no test coverage, but reported here
https://discuss.dagster.io/t/15601462/i-m-trying-to-upgrade-from-1-13-13-to-1-4-10-but-i-m-getting#2a61ef0c-7ef0-4ae1-aa35-17868af5ad6f
is something done by some users

## How I Tested These Changes

added test

python_modules/dagster/dagster/_core/definitions/assets.py
@@ -317,10 +317,11 @@ class AssetsDefinition(ResourceAddable, RequiresResources, IHasInternalInit):
         )
 
     def __call__(self, *args: object, **kwargs: object) -> object:
+        from .composition import is_in_composition
         from .graph_definition import GraphDefinition
 
-        # defer to GraphDefinition.__call__ for graph backed assets
-        if isinstance(self.node_def, GraphDefinition):
+        # defer to GraphDefinition.__call__ for graph backed assets, or if invoked in composition
+        if isinstance(self.node_def, GraphDefinition) or is_in_composition():
             return self._node_def(*args, **kwargs)
 
         # invoke against self to allow assets def information to be used

python_modules/dagster/dagster_tests/definitions_tests/test_composition.py
@@ -12,6 +12,7 @@ from dagster import (
     OpDefinition,
     Out,
     Output,
+    asset,
     graph,
     job,
     op,
@@ -964,3 +965,17 @@ def test_aliasing_invoked_dynamic_output_fails():
         @job
         def _alias_invoked_dynamic_output_job():
             dynamic_output_op().alias("dynamic_output")
+
+
+def test_compose_asset():
+    @asset
+    def foo():
+        pass
+
+    @graph
+    def compose():
+        foo()
+
+    result = compose.execute_in_process()
+    assert result.success
+    assert result.events_for_node("foo")