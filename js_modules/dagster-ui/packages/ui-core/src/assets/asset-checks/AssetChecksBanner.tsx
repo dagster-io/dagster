ef3a9fad296388271909af798794db6a9f18950a
Johann Miller <johann@elementl.com>
2023-09-01 23:12:17 -0400

    update asset checks banner (#16267)
    Now that we have a dedicated discussion for the MVP
    https://github.com/dagster-io/dagster/discussions/16266

--- js_modules/dagster-ui/packages/ui-core/src/assets/asset-checks/AssetChecksBanner.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/asset-checks/AssetChecksBanner.tsx
@@ -5,12 +5,12 @@ export const AssetChecksBanner = () => {
   return (
     <Alert
       intent="info"
-      title="Asset Checks are still experimental"
+      title="Asset Checks are experimental"
       icon={<Icon name="info" color={Colors.Blue700} />}
       description={
         <span>
           You can learn more about this new feature and provide feedback{' '}
-          <a href="https://github.com/dagster-io/dagster/discussions/15880">here</a>.
+          <a href="https://github.com/dagster-io/dagster/discussions/16266">here</a>.
         </span>
       }
     />
