6d0073c585bc93e9a35d1c5d5efa17f3040af1d7
Francisco Garcia Florez <francisco@truevoid.dev>
2023-09-03 19:09:30 +0200

    Added better handling of failed jobs to dagster-k8s

--- python_modules/libraries/dagster-k8s/dagster_k8s/client.py
+++ python_modules/libraries/dagster-k8s/dagster_k8s/client.py
@@ -395,7 +395,9 @@ class DagsterKubernetesClient:
                 break
 
             # status.failed represents the number of pods which reached phase Failed.
-            if status.failed and status.failed > 0:
+            # if there are any active runs do not raise an exception. This happens when the job
+            # is created with a backoff_limit > 0.
+            if (status.active is None or status.active == 0) and status.failed and status.failed > 0:
                 raise DagsterK8sError(
                     "Encountered failed job pods for job {job_name} with status: {status}, "
                     "in namespace {namespace}".format(
