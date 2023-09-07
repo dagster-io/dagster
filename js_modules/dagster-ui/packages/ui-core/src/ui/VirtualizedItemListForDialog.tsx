5e6b2592b239d663d3550ea4bbbcf9b69f235c72
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2023-09-07 09:29:21 -0500

    [ui] Some backfill page cleanup (#16310)
    ## Summary & Motivation
    
    A handful of changes and cleanup items on the backfill table and
    backfill page.
    
    - Virtualize the partition and asset key tables from these pages,
    reusing the component created for auto-materialization. It's just a
    generic virtualized item list anyway, so we can go ahead and reuse it
    here. (I moved and renamed the component to give it generic
    characteristics.)
    - When linking to an asset from the backfill page where a backfill is
    for a range, link to that default range on the asset page itself.
    - When a backfill header just shows the names of partitions that have
    been backfilled (with no dialog) use a `Tag` to give the partition names
    some separation. It's not perfect, but it seemed a little weird to just
    stick a comma between them.
    - Some alignment tweaks.
    
    <img width="557" alt="Screenshot 2023-09-05 at 9 51 06 AM"
    src="https://github.com/dagster-io/dagster/assets/2823852/e70676ab-e89b-4b36-89d2-8ad2fe337dc2">
    <img width="580" alt="Screenshot 2023-09-05 at 9 50 58 AM"
    src="https://github.com/dagster-io/dagster/assets/2823852/c995a16f-7345-4a9f-a22e-2031e269b661">
    
    ## How I Tested These Changes
    
    View Backfill page. Click to view assets affected by a backfill, verify
    dialog with virtualized contents. Verify same when clicking the link to
    see the affected partitions.
    
    Click into a backfill, verify same dialog and virtualization behavior
    when looking at the list of affected partitions.
    
    Click into a backfill that uses a partition range. Click to the asset,
    verify that the range is applied to the querystring of the URL and loads
    as the selected range on the asset page.

--- js_modules/dagster-ui/packages/ui-core/src/ui/VirtualizedItemListForDialog.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/ui/VirtualizedItemListForDialog.tsx
@@ -2,38 +2,38 @@ import {Box, Colors} from '@dagster-io/ui-components';
 import {useVirtualizer} from '@tanstack/react-virtual';
 import * as React from 'react';
 
-import {Container, Inner, Row} from '../../ui/VirtualizedTable';
+import {Container, Inner, Row} from './VirtualizedTable';
 
-interface Props<A> {
-  assetKeys: A[];
-  renderItem: (assetKey: A) => React.ReactNode;
+interface Props<T> {
+  items: T[];
+  renderItem: (item: T) => React.ReactNode;
 }
 
-export function VirtualizedAssetListForDialog<A>({assetKeys, renderItem}: Props<A>) {
+export function VirtualizedItemListForDialog<A>({items, renderItem}: Props<A>) {
   const container = React.useRef<HTMLDivElement | null>(null);
 
   const rowVirtualizer = useVirtualizer({
-    count: assetKeys.length,
+    count: items.length,
     getScrollElement: () => container.current,
     estimateSize: () => 40,
     overscan: 10,
   });
 
   const totalHeight = rowVirtualizer.getTotalSize();
-  const items = rowVirtualizer.getVirtualItems();
+  const virtualItems = rowVirtualizer.getVirtualItems();
 
   return (
     <Container ref={container} style={{padding: '8px 24px'}}>
       <Inner $totalHeight={totalHeight}>
-        {items.map(({index, key, size, start}) => {
-          const assetKey = assetKeys[index]!;
+        {virtualItems.map(({index, key, size, start}) => {
+          const assetKey = items[index]!;
           return (
             <Row $height={size} $start={start} key={key}>
               <Box
                 style={{height: '100%'}}
                 flex={{direction: 'row', alignItems: 'center'}}
                 border={
-                  index < assetKeys.length - 1
+                  index < items.length - 1
                     ? {side: 'bottom', width: 1, color: Colors.KeylineGray}
                     : null
                 }
