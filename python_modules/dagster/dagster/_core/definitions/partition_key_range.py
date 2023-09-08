ba05a8ad1681204b72e5afc04e6ab2fc56c89201
Claire Lin <claire@elementl.com>
2023-09-08 16:59:23 -0700

    add docstring and build apidocs for partition key range

--- python_modules/dagster/dagster/_core/definitions/partition_key_range.py
+++ python_modules/dagster/dagster/_core/definitions/partition_key_range.py
@@ -4,6 +4,13 @@ from dagster._annotations import PublicAttr
 
 
 class PartitionKeyRange(NamedTuple):
+    """Defines a range of partitions.
+
+    Attributes:
+        start (str): The starting partition key in the range (inclusive).
+        end (str): The ending partition key in the range (inclusive).
+    """
+
     # Inclusive on both sides
     start: PublicAttr[str]
     end: PublicAttr[str]
