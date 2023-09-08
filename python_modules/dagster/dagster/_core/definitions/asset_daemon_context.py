b89816f90092056eeec4690dff637e3d90e4cd41
OwenKephart <owen@elementl.com>
2023-09-08 11:58:13 -0700

    [bug] Fix issue in which incorrect run requests would be made for partitioned observable source assets with different partitions defs (#16403)
    ## Summary & Motivation
    
    The get_implicit_job_def_for_assets function does not take into account
    the fact that ALL asset jobs have ALL partitioned observable source
    assets within them. This updates the function to make sure we're getting
    a job that matches the expected partitions definition
    
    
    ## How I Tested These Changes

--- python_modules/dagster/dagster/_core/definitions/asset_daemon_context.py
+++ python_modules/dagster/dagster/_core/definitions/asset_daemon_context.py
@@ -788,8 +788,17 @@ def get_auto_observe_run_requests(
         ):
             assets_to_auto_observe.add(asset_key)
 
+    # create groups of asset keys that share the same repository AND the same partitions definition
+    partitions_def_and_asset_key_groups: List[Sequence[AssetKey]] = []
+    for repository_asset_keys in asset_graph.split_asset_keys_by_repository(assets_to_auto_observe):
+        asset_keys_by_partitions_def = defaultdict(list)
+        for asset_key in repository_asset_keys:
+            partitions_def = asset_graph.get_partitions_def(asset_key)
+            asset_keys_by_partitions_def[partitions_def].append(asset_key)
+        partitions_def_and_asset_key_groups.extend(asset_keys_by_partitions_def.values())
+
     return [
         RunRequest(asset_selection=list(asset_keys), tags=run_tags)
-        for asset_keys in asset_graph.split_asset_keys_by_repository(assets_to_auto_observe)
+        for asset_keys in partitions_def_and_asset_key_groups
         if len(asset_keys) > 0
     ]
