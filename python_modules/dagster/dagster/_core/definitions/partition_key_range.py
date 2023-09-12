256f051991f5488d88f593b4d2b28f941841e5ee
Claire Lin <claire@elementl.com>
2023-09-12 14:18:31 -0700

    add example

--- python_modules/dagster/dagster/_core/definitions/partition_key_range.py
+++ python_modules/dagster/dagster/_core/definitions/partition_key_range.py
@@ -9,6 +9,12 @@ class PartitionKeyRange(NamedTuple):
     Attributes:
         start (str): The starting partition key in the range (inclusive).
         end (str): The ending partition key in the range (inclusive).
+
+    Examples:
+        .. code-block:: python
+
+            partitions_def = StaticPartitionsDefinition(["a", "b", "c", "d"])
+            partition_key_range = PartitionKeyRange(start="a", end="c") # Represents ["a", "b", "c"]
     """
 
     # Inclusive on both sides
