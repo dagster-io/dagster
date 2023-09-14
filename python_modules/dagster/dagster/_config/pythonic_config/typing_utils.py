74239a905c431429f7b6d7f27807bb72ae5e14dc
Ben Pankow <ben@elementl.com>
2023-09-14 16:47:59 -0700

    [1/n][pythonic config] Restructure Pythonic config & resources into modules (#16434)
    ## Summary
    
    PR stack which re-implements #16257, in hopes of making it easier to
    review.
    
    This PR restructures the pythonic resources+config module into a few
    smaller, easier to grasp files - one for the "Config" class, one for
    Resources, one for IO managers, and one for misc Pydantic & Dagster
    Config Type conversion utilities.
    
    ## Test Plan
    
    Impl does not change, existing unit tests.

--- python_modules/dagster/dagster/_config/pythonic_config/typing_utils.py
+++ python_modules/dagster/dagster/_config/pythonic_config/typing_utils.py
@@ -6,7 +6,7 @@ from typing_extensions import Annotated, dataclass_transform, get_origin
 
 from dagster._core.errors import DagsterInvalidDagsterTypeInPythonicConfigDefinitionError
 
-from .utils import safe_is_subclass
+from .inheritance_utils import safe_is_subclass
 
 if TYPE_CHECKING:
     from dagster._config.pythonic_config import PartialResource
