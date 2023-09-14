f10c8b308bb7d5a346c976008487ea412339cbc9
Jordan Sanders <jordan@elementl.com>
2023-09-14 14:16:29 -0500

    Test that dagster-buildkite gets added to build
    And do some refactoring around commonly used fixtures:
    
    - `dagster_repo.touch()` to touch and commit a file
    - `dagster_buildkite()` to execute dagster-buildkite and return a
      summary of the pipeline

--- .buildkite/dagster-buildkite/dagster_buildkite/steps/packages.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/packages.py
@@ -630,6 +630,6 @@ LIBRARY_PACKAGES_WITH_CUSTOM_CONFIG: List[PackageSpec] = [
     ),
     PackageSpec(
         ".buildkite/dagster-buildkite",
-        run_pytest=False,
+        run_pytest=True,
     ),
 ]
--- .buildkite/dagster-buildkite/dagster_buildkite_tests/test_pipeline.py
+++ .buildkite/dagster-buildkite/dagster_buildkite_tests/test_pipeline.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 import tempfile
 import venv
@@ -9,14 +10,14 @@ import pytest
 import yaml
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def temporary_directory():
     with tempfile.TemporaryDirectory() as path:
         os.chdir(path)
         yield path
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def python(temporary_directory):
     path = os.path.join(temporary_directory, ".venv")
     venv.create(path, with_pip=True)
@@ -26,28 +27,41 @@ def python(temporary_directory):
     yield python
 
 
-@pytest.fixture(scope="session")
-def dagster_repo(temporary_directory, python):
+@pytest.fixture
+def dagster_repo(temporary_directory):
     path = os.path.join(temporary_directory, "dagster")
 
+    # Do a fresh clone so our repo has no changes. This should largely isolate
+    # tests from failing because of unrelated commits on a branch
     repo = git.Repo.clone_from(
         "https://github.com/dagster-io/dagster.git",
         path,
         filter=["tree:0"],
     )
 
+    # But copy in our .buildkite directory so we can still test our
+    # changes to our pipeline generation logic. This has the somewhat odd side
+    # often recognizing `dagster-buildkite` as a changed package in all tests
+    # if you're doing active development on it.
+    dagster_buildkite_src = Path(os.getenv("DAGSTER_GIT_REPO_DIR")) / ".buildkite"
+    dagster_buildkite_dst = Path(path) / ".buildkite"
+
+    shutil.copytree(
+        dagster_buildkite_src,
+        dagster_buildkite_dst,
+        dirs_exist_ok=True,
+    )
+
     os.chdir(path)
 
+    def touch(path: Path):
+        path.touch()
+        repo.index.add(str(path))
+        repo.index.commit(f"Change {path}")
+
+    repo.touch = touch
+
     yield repo
-
-
-@pytest.fixture(scope="session")
-def dagster_buildkite(python, dagster_repo):
-    subprocess.run(
-        [python, "-m", "pip", "install", "-e", ".buildkite/dagster-buildkite"],
-        check=True,
-    )
-    yield python.replace("python", "dagster-buildkite")
 
 
 @pytest.fixture
@@ -58,6 +72,35 @@ def env(monkeypatch):
     monkeypatch.setenv("BUILDKITE_WINDOWS_QUEUE", "fake")
     monkeypatch.setenv("BUILDKITE_COMMIT", "fake")
     monkeypatch.setenv("BUILDKITE_MESSAGE", "fake")
+
+
+@pytest.fixture
+def dagster_buildkite(env, python, dagster_repo):
+    subprocess.run(
+        [python, "-m", "pip", "install", "-e", ".buildkite/dagster-buildkite"],
+        check=True,
+    )
+    executable = python.replace("python", "dagster-buildkite")
+
+    yield lambda: PipelineSummary(
+        yaml.safe_load(
+            subprocess.run(
+                executable,
+                capture_output=True,
+            ).stdout
+        )
+    )
+
+
+@pytest.fixture
+def libraries(dagster_repo):
+    return [
+        library.parts[-1]
+        for library in (Path(dagster_repo.working_tree_dir) / "python_modules" / "libraries").glob(
+            "*"
+        )
+        if library.is_dir()
+    ]
 
 
 class StepSummary:
@@ -78,15 +121,15 @@ class PipelineSummary:
         steps = pipeline["steps"]
         for step in steps:
             if step.get("label"):
-                summary = StepSummary(step)
-                if summary.skipped:
-                    skipped.append(summary)
+                output = StepSummary(step)
+                if output.skipped:
+                    skipped.append(output)
                 else:
-                    planned.append(summary)
+                    planned.append(output)
             elif step.get("steps"):
-                recursed = PipelineSummary(step)
-                planned.extend(recursed.planned)
-                skipped.extend(recursed.skipped)
+                output = PipelineSummary(step)
+                planned.extend(output.planned)
+                skipped.extend(output.skipped)
             else:
                 raise
 
@@ -94,98 +137,71 @@ class PipelineSummary:
         self.skipped = skipped
 
 
-def test_dagster_buildkite(env, dagster_buildkite):
-    subprocess.run(dagster_buildkite, check=True)
-
-
-def test_release_branch(env, dagster_buildkite, monkeypatch):
+def test_release_branch(dagster_buildkite, monkeypatch):
     monkeypatch.setenv("BUILDKITE_BRANCH", "release-0.0.1")
 
-    pipeline = yaml.safe_load(
-        subprocess.run(
-            dagster_buildkite,
-            capture_output=True,
-        ).stdout,
-    )
-    summary = PipelineSummary(pipeline)
+    output = dagster_buildkite()
 
     # Nothing gets skipped
-    assert not summary.skipped
+    assert not output.skipped
 
     # We test multiple python versions
-    assert any(["3.8" in step.name for step in summary.planned])
-    assert any(["3.10" in step.name for step in summary.planned])
+    assert any(["3.8" in step.name for step in output.planned])
+    assert any(["3.10" in step.name for step in output.planned])
 
 
 @pytest.mark.xfail(reason="dagster-airflow is running 3.8 tests", strict=True)
-def test_main_branch(env, dagster_buildkite, monkeypatch):
+def test_main_branch(dagster_buildkite, monkeypatch):
     monkeypatch.setenv("BUILDKITE_BRANCH", "master")
 
-    pipeline = yaml.safe_load(
-        subprocess.run(
-            dagster_buildkite,
-            capture_output=True,
-        ).stdout,
-    )
-    summary = PipelineSummary(pipeline)
+    output = dagster_buildkite()
 
     # We only test latest python versions
-    assert not any(["3.8" in step.name for step in summary.planned])
-    assert any(["3.10" in step.name for step in summary.planned])
+    assert not any(["3.8" in step.name for step in output.planned])
+    assert any(["3.10" in step.name for step in output.planned])
 
     # Only our test-project builds are skipped
-    assert all(["test-project" in step.name for step in summary.skipped])
+    assert all(["test-project" in step.name for step in output.skipped])
 
 
-def test_python_change_no_dependencies(env, dagster_repo, dagster_buildkite):
-    change = (
+def test_python_change_no_dependencies(dagster_repo, dagster_buildkite, libraries):
+    dagster_repo.touch(
         Path(dagster_repo.working_tree_dir)
         / "python_modules"
         / "libraries"
         / "dagster-twilio"
         / "change.py"
     )
-    change.touch()
-    dagster_repo.index.add(change)
-    dagster_repo.index.commit("Change dagster-twilio")
 
-    pipeline = yaml.safe_load(
-        subprocess.run(
-            dagster_buildkite,
-            capture_output=True,
-        ).stdout,
-    )
-    summary = PipelineSummary(pipeline)
+    output = dagster_buildkite()
 
     # The only python package test suite we run is dagster-twilio because
     # nothing depends on it.
-    assert any([":pytest: dagster-twilio" in step.name for step in summary.planned])
-    assert sum([":pytest:" in step.name for step in summary.planned]) == 1
-    assert any([":pytest:" in step.name for step in summary.skipped])
+    assert any([":pytest: dagster-twilio" in step.name for step in output.planned])
+    assert not any([":pytest: dagster-twilio" in step.name for step in output.skipped])
+
+    for library in libraries:
+        if library != "dagster-twilio":
+            assert not any([f":pytest: {library} " in step.name for step in output.planned])
+            assert any([f":pytest: {library} " in step.name for step in output.skipped])
 
 
-def test_python_change_dagster(env, dagster_repo, dagster_buildkite):
-    change = Path(dagster_repo.working_tree_dir) / "python_modules" / "dagster" / "change.py"
-    change.touch()
-    dagster_repo.index.add(change)
-    dagster_repo.index.commit("Change dagster")
-
-    pipeline = yaml.safe_load(
-        subprocess.run(
-            dagster_buildkite,
-            capture_output=True,
-        ).stdout,
+def test_python_change_dagster(dagster_repo, dagster_buildkite, libraries):
+    dagster_repo.touch(
+        Path(dagster_repo.working_tree_dir) / "python_modules" / "dagster" / "change.py"
     )
-    summary = PipelineSummary(pipeline)
+
+    output = dagster_buildkite()
 
     # Every library test suite depends on dagster
-    libraries = [
-        library.parts[-1]
-        for library in (Path(dagster_repo.working_tree_dir) / "python_modules" / "libraries").glob(
-            "*"
-        )
-        if library.is_dir()
-    ]
     for library in libraries:
-        assert any([f":pytest: {library} " in step.name for step in summary.planned])
-        assert not any([f":pytest: {library} " in step.name for step in summary.skipped])
+        assert any([f":pytest: {library} " in step.name for step in output.planned])
+        assert not any([f":pytest: {library} " in step.name for step in output.skipped])
+
+
+def test_python_change_dagster_buildkite(dagster_repo, dagster_buildkite):
+    dagster_repo.touch(
+        Path(dagster_repo.working_tree_dir) / ".buildkite" / "dagster-buildkite" / "change.py"
+    )
+
+    assert any([":pytest: dagster-buildkite" in step.name for step in dagster_buildkite().planned])
