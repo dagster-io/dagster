7ebe89d21650692988cc7213537748dd959f5a9d
Nicholas Schrock <schrockn@elementl.com>
2023-09-16 13:03:36 -0400

    Add runless events
    cleanup
    
    cp

--- python_modules/dagster/dagster/_core/definitions/observable_asset.py
+++ python_modules/dagster/dagster/_core/definitions/observable_asset.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Optional, Sequence
 
 from dagster._core.definitions.asset_spec import (
     SYSTEM_METADATA_KEY_ASSET_VARIETAL,
@@ -7,6 +7,15 @@ from dagster._core.definitions.asset_spec import (
     ObservableAssetSpec,
 )
 from dagster._core.definitions.decorators.asset_decorator import multi_asset
+from dagster._core.definitions.events import AssetMaterialization, AssetObservation
+from dagster._core.events import (
+    AssetObservationData,
+    DagsterEvent,
+    DagsterEventType,
+    EventSpecificData,
+    StepMaterializationData,
+)
+from dagster._core.instance import DagsterInstance
 
 
 def create_unexecutable_observable_assets_def(specs: Sequence[ObservableAssetSpec]):
@@ -29,3 +38,45 @@ def create_unexecutable_observable_assets_def(specs: Sequence[ObservableAssetSpe
         raise NotImplementedError()
 
     return an_asset
+
+
+# Our internal guts can handle empty strings for job name and run id
+# However making thse named constants for documentation, to encode where we are making the assumption,
+# and to allow us to change this more easily in the future, provided we are disciplined about
+# actually using this constants.
+RUNLESS_JOB_NAME = ""
+RUNLESS_RUN_ID = ""
+
+
+def report_runless_event(
+    instance: DagsterInstance, event_type: DagsterEventType, event_specific_data: EventSpecificData
+) -> None:
+    dagster_event = DagsterEvent(
+        event_type_value=event_type.value,
+        job_name=RUNLESS_JOB_NAME,
+        event_specific_data=event_specific_data,
+    )
+
+    instance.report_dagster_event(dagster_event=dagster_event, run_id=RUNLESS_RUN_ID)
+
+
+def report_runless_asset_materialization(
+    asset_materialization: AssetMaterialization,
+    instance: Optional[DagsterInstance] = None,
+) -> None:
+    report_runless_event(
+        instance or DagsterInstance.get(),
+        DagsterEventType.ASSET_MATERIALIZATION,
+        StepMaterializationData(asset_materialization),
+    )
+
+
+def report_runless_asset_observation(
+    asset_observation: AssetObservation,
+    instance: Optional[DagsterInstance] = None,
+) -> None:
+    report_runless_event(
+        instance or DagsterInstance.get(),
+        DagsterEventType.ASSET_OBSERVATION,
+        AssetObservationData(asset_observation),
+    )
--- python_modules/dagster/dagster_tests/definitions_tests/test_observable_assets.py
+++ python_modules/dagster/dagster_tests/definitions_tests/test_observable_assets.py
@@ -1,6 +1,13 @@
-from dagster import AssetKey, AssetsDefinition, asset
+from dagster import AssetKey, AssetsDefinition, DagsterInstance, asset
 from dagster._core.definitions.asset_spec import ObservableAssetSpec
-from dagster._core.definitions.observable_asset import create_unexecutable_observable_assets_def
+from dagster._core.definitions.events import AssetMaterialization, AssetObservation
+from dagster._core.definitions.observable_asset import (
+    create_unexecutable_observable_assets_def,
+    report_runless_asset_materialization,
+    report_runless_asset_observation,
+)
+from dagster._core.event_api import EventRecordsFilter
+from dagster._core.events import DagsterEventType
 
 
 def test_observable_asset_basic_creation() -> None:
@@ -51,3 +58,42 @@ def test_observable_asset_creation_with_deps() -> None:
     assert assets_def.asset_deps[expected_key] == {
         AssetKey(["observable_asset_two"]),
     }
+
+
+def test_report_runless_materialization() -> None:
+    instance = DagsterInstance.ephemeral()
+
+    report_runless_asset_materialization(
+        asset_materialization=AssetMaterialization(asset_key="observable_asset_one"),
+        instance=instance,
+    )
+
+    mat_event = instance.get_latest_materialization_event(
+        asset_key=AssetKey("observable_asset_one")
+    )
+
+    assert mat_event
+    assert mat_event.asset_materialization
+    assert mat_event.asset_materialization.asset_key == AssetKey("observable_asset_one")
+
+
+def test_report_runless_observation() -> None:
+    instance = DagsterInstance.ephemeral()
+
+    report_runless_asset_observation(
+        asset_observation=AssetObservation(asset_key="observable_asset_one"),
+        instance=instance,
+    )
+
+    observations = instance.get_event_records(
+        EventRecordsFilter(
+            event_type=DagsterEventType.ASSET_OBSERVATION,
+            asset_key=AssetKey("observable_asset_one"),
+        ),
+        limit=1,
+    )
+
+    observation = next(iter(observations), None)
+    assert observation
+
+    assert observation.asset_key == AssetKey("observable_asset_one")
