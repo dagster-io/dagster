15c53d78ae815a715125bd1acd17576b8346ae9d
Nick Schrock <schrockn@dagsterlabs.com>
2023-09-18 14:23:24 -0400

    Add system metadata key to determine executability (#16576)
    ## Summary & Motivation
    
    For UIs or calling code to determine if an asset key is executable, we
    need to add this information to `AssetsDefinition`. Rather than add a
    property and to go through the associated insanity of adding said
    property to >10 layers of passing around a new dictionary indexed by
    assets key, I'm just going to jam it in the user-defined metadata with a
    new system-level metadata key.
    
    The metadata key is an enum that contains information about the
    "varietal" of this assets definition. This allows for extension later. I
    have a near-term extension in mind, as new varietal––executable
    observable assets––will be reintroduced in short order to supplant
    observable source assets.
    
    We can reassess this later, but I do not want to introduce API and code
    thrash for now.
    
    This also makes `create_unexecutable_observable_assets_def` use
    `multi_asset` instead of an `asset`
    
    ## How I Tested These Changes
    
    BK
    
    ---------
    
    Co-authored-by: Nicholas Schrock <schrockn@elementl.com>

--- python_modules/dagster/dagster/_core/definitions/observable_asset.py
+++ python_modules/dagster/dagster/_core/definitions/observable_asset.py
@@ -1,24 +1,48 @@
-from dagster._core.definitions.asset_spec import AssetSpec
-from dagster._core.definitions.decorators import asset
+from typing import Sequence
+
+from dagster import _check as check
+from dagster._core.definitions.asset_spec import (
+    SYSTEM_METADATA_KEY_ASSET_VARIETAL,
+    AssetSpec,
+    AssetVarietal,
+)
+from dagster._core.definitions.decorators.asset_decorator import multi_asset
 from dagster._core.errors import DagsterInvariantViolationError
 
 
-def create_unexecutable_observable_assets_def(asset_spec: AssetSpec):
-    # This function is to be used in the internals of repository definition to coerce
-    # an AssetSpec into an AssetsDefinition
-    # TODO: will make this use multi_asset later in the stack
-    @asset(
-        key=asset_spec.key,
-        description=asset_spec.description,
-        metadata=asset_spec.metadata,
-        group_name=asset_spec.group_name,
-        deps=[
-            dep.asset_key for dep in asset_spec.deps
-        ],  # switch to not using .asset_key once jamie's diff lands
-    )
+def create_unexecutable_observable_assets_def(specs: Sequence[AssetSpec]):
+    new_specs = []
+    for spec in specs:
+        check.invariant(
+            spec.auto_materialize_policy is None,
+            "auto_materialize_policy must be None since it is ignored",
+        )
+        check.invariant(spec.code_version is None, "code_version must be None since it is ignored")
+        check.invariant(
+            spec.freshness_policy is None, "freshness_policy must be None since it is ignored"
+        )
+        check.invariant(
+            spec.skippable is False,
+            "skippable must be False since it is ignored and False is the default",
+        )
+
+        new_specs.append(
+            AssetSpec(
+                key=spec.key,
+                description=spec.description,
+                group_name=spec.group_name,
+                metadata={
+                    **(spec.metadata or {}),
+                    **{SYSTEM_METADATA_KEY_ASSET_VARIETAL: AssetVarietal.UNEXECUTABLE.value},
+                },
+                deps=spec.deps,
+            )
+        )
+
+    @multi_asset(specs=new_specs)
     def an_asset() -> None:
         raise DagsterInvariantViolationError(
-            f"You have attempted to execute an unexecutable asset {asset_spec.key}"
+            f"You have attempted to execute an unexecutable asset {[spec.key for spec in specs]}"
         )
 
     return an_asset
