bd1a7a2482a941c12945ed884c920510164625dd
Nicholas Schrock <schrockn@elementl.com>
2023-09-18 16:42:53 -0400

    Pin k8s to < 28.1.0

--- python_modules/libraries/dagster-k8s/setup.py
+++ python_modules/libraries/dagster-k8s/setup.py
@@ -32,6 +32,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=find_packages(exclude=["dagster_k8s_tests*"]),
-    install_requires=[f"dagster{pin}", "kubernetes"],
+    install_requires=[f"dagster{pin}", "kubernetes<28.1.0"],
     zip_safe=False,
 )
