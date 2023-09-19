dccfc4257291ffb7eea9e4f10133b99e8616f152
Nicholas Schrock <schrockn@dagsterlabs.com>
2023-09-19 10:42:39 -0400

    observable assets in a user space sensor
    feedabck
    
    Extract out observe_fn_to_op_compute_fn to it can be reused in other contexts
    
    cp
    
    feedback
    
    experimental
    
    cp

--- python_modules/dagster/dagster_tests/definitions_tests/test_observable_assets.py
+++ python_modules/dagster/dagster_tests/definitions_tests/test_observable_assets.py
@@ -1,4 +1,4 @@
-from typing import AbstractSet, Iterable
+from typing import AbstractSet, Any, Callable, Iterable
 
 import pytest
 from dagster import (
@@ -11,18 +11,36 @@ from dagster import (
     Definitions,
     IOManager,
     JobDefinition,
+    SensorResult,
     SourceAsset,
     _check as check,
     asset,
     observable_source_asset,
 )
-from dagster._core.definitions.asset_spec import AssetSpec
+from dagster._core.definitions import materialize
+from dagster._core.definitions.asset_spec import (
+    SYSTEM_METADATA_KEY_ASSET_EXECUTION_TYPE,
+    AssetExecutionType,
+    AssetSpec,
+)
+from dagster._core.definitions.data_version import DATA_VERSION_TAG
+from dagster._core.definitions.decorators.sensor_decorator import sensor
+from dagster._core.definitions.events import AssetObservation
 from dagster._core.definitions.freshness_policy import FreshnessPolicy
 from dagster._core.definitions.observable_asset import (
     create_assets_def_from_source_asset,
     create_unexecutable_observable_assets_def,
 )
+from dagster._core.definitions.sensor_definition import (
+    SensorDefinition,
+    build_sensor_context,
+)
+from dagster._core.definitions.source_asset import (
+    observe_fn_to_op_compute_fn,
+)
 from dagster._core.definitions.time_window_partitions import DailyPartitionsDefinition
+from dagster._core.event_api import EventRecordsFilter
+from dagster._core.events import DagsterEventType
 
 
 def test_observable_asset_basic_creation() -> None:
@@ -217,3 +235,151 @@ def test_observable_source_asset_decorator() -> None:
 
     all_materializations = result.get_asset_materialization_events()
     assert len(all_materializations) == 0
+
+
+def get_latest_asset_observation(
+    instance: DagsterInstance, asset_key: AssetKey
+) -> AssetObservation:
+    event_records = instance.get_event_records(
+        EventRecordsFilter(
+            event_type=DagsterEventType.ASSET_OBSERVATION,
+            asset_key=asset_key,
+        ),
+        limit=1,
+    )
+
+    assert len(event_records) == 1
+
+    event_record = event_records[0]
+
+    return check.not_none(event_record.asset_observation)
+
+
+def test_demonstrate_explicit_sensor_in_user_space() -> None:
+    def compute_data_version() -> str:
+        return "data_version"
+
+    observing_only_asset_key = AssetKey("observing_only_asset")
+
+    @asset(
+        key=observing_only_asset_key,
+        metadata={SYSTEM_METADATA_KEY_ASSET_EXECUTION_TYPE: AssetExecutionType.OBSERVATION.value},
+    )
+    def observing_only_asset(context: AssetExecutionContext) -> None:
+        context.log_event(
+            AssetObservation(
+                asset_key=observing_only_asset_key, tags={DATA_VERSION_TAG: compute_data_version()}
+            )
+        )
+
+    asset_execution_instance = DagsterInstance.ephemeral()
+
+    assert materialize(assets=[observing_only_asset], instance=asset_execution_instance).success
+
+    assert (
+        get_latest_asset_observation(
+            asset_execution_instance, observing_only_asset_key
+        ).data_version
+        == "data_version"
+    )
+
+    @sensor(job_name="observing_only_sensor")
+    def observing_only_asset_sensor() -> SensorResult:
+        return SensorResult(
+            asset_events=[
+                AssetObservation(
+                    asset_key=observing_only_asset_key,
+                    tags={DATA_VERSION_TAG: compute_data_version()},
+                )
+            ]
+        )
+
+    sensor_instance = DagsterInstance.ephemeral()
+
+    sensor_execution_data = observing_only_asset_sensor.evaluate_tick(
+        build_sensor_context(instance=sensor_instance)
+    )
+
+    assert len(sensor_execution_data.asset_events) == 1
+
+    asset_event = sensor_execution_data.asset_events[0]
+
+    assert isinstance(asset_event, AssetObservation)
+
+
+def create_observation_with_version(
+    asset_key: AssetKey, data_version: DataVersion
+) -> AssetObservation:
+    return AssetObservation(
+        asset_key=asset_key,
+        tags={DATA_VERSION_TAG: data_version.value},
+    )
+
+
+def assets_def_from_observe_fn(asset_key: AssetKey, observe_fn: Callable[..., Any]):
+    @asset(
+        key=asset_key,
+        metadata={SYSTEM_METADATA_KEY_ASSET_EXECUTION_TYPE: AssetExecutionType.OBSERVATION.value},
+    )
+    def _asset(context: AssetExecutionContext, **kwargs) -> None:
+        assets_def = context.job_def.asset_layer.assets_def_for_asset(context.asset_key)
+        op_compute_fn = observe_fn_to_op_compute_fn(
+            observe_fn=observe_fn,
+            partitions_def=assets_def.partitions_def,
+            asset_key=context.asset_key,
+        )
+        return op_compute_fn.decorated_fn(context, **kwargs)
+
+    return _asset
+
+
+def sensor_def_from_observe_fn(
+    asset_key: AssetKey, job_name: str, observe_fn: Callable[..., Any]
+) -> SensorDefinition:
+    @sensor(job_name=job_name)
+    def _sensor(context, **kwargs) -> SensorResult:
+        return SensorResult(
+            asset_events=[
+                create_observation_with_version(asset_key, observe_fn(context, **kwargs)),
+            ]
+        )
+
+    return _sensor
+
+
+def test_framework_support_for_observable_source_assets_on_assets_def() -> None:
+    def compute_data_version(context) -> DataVersion:
+        return DataVersion("data_version")
+
+    observing_only_asset_key = AssetKey("observing_only_asset")
+
+    observing_only_asset = assets_def_from_observe_fn(
+        asset_key=observing_only_asset_key, observe_fn=compute_data_version
+    )
+
+    asset_execution_instance = DagsterInstance.ephemeral()
+
+    assert materialize(assets=[observing_only_asset], instance=asset_execution_instance).success
+
+    assert (
+        get_latest_asset_observation(
+            asset_execution_instance, observing_only_asset_key
+        ).data_version
+        == "data_version"
+    )
+
+    observing_only_asset_sensor = sensor_def_from_observe_fn(
+        asset_key=observing_only_asset_key, job_name="some_job", observe_fn=compute_data_version
+    )
+
+    sensor_instance = DagsterInstance.ephemeral()
+
+    sensor_execution_data = observing_only_asset_sensor.evaluate_tick(
+        build_sensor_context(instance=sensor_instance)
+    )
+
+    assert len(sensor_execution_data.asset_events) == 1
+
+    asset_event = sensor_execution_data.asset_events[0]
+
+    assert isinstance(asset_event, AssetObservation)
