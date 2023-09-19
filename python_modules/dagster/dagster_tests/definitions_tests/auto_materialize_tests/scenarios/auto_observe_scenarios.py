5d87ca95cf3f7b96204479225f6fed8a32e0c772
OwenKephart <owen@elementl.com>
2023-09-19 11:28:39 -0700

    [observable-source-assets] Fix observable job resolution logic (#16626)
    ## Summary & Motivation
    
    Fixes: https://github.com/dagster-io/dagster/issues/16611
    
    The prior logic made sure to appropriately match observable source
    assets to asset jobs that shared an identical underlying
    PartitionsDefinition. However, if your code location consists of only
    partitioned assets, and your observable source asset is unpartitioned,
    this means that no job can be found to materialize it.
    
    Any unpartitioned asset can be materialized by any partitioned job, so
    this PR updates the logic to allow for unpartitioned assets to be
    materialized by partitioned base asset jobs.
    
    ## How I Tested These Changes
    
    The added test case is a bit goofy, but it did reproduce the error.

--- python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/scenarios/auto_observe_scenarios.py
+++ python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/scenarios/auto_observe_scenarios.py
@@ -5,8 +5,10 @@ from dagster import (
     DataVersion,
     DataVersionsByPartition,
     StaticPartitionsDefinition,
+    asset,
     observable_source_asset,
 )
+from dagster._core.definitions.auto_materialize_policy import AutoMaterializePolicy
 
 from ..base_scenario import AssetReconciliationScenario, run_request
 
@@ -19,6 +21,15 @@ def asset1():
 @observable_source_asset(auto_observe_interval_minutes=30)
 def asset2():
     return DataVersion("5")
+
+
+@asset(
+    partitions_def=StaticPartitionsDefinition(["a", "b", "c"]),
+    # this is just a dummy policy, as we don't want this to actually impact the logic
+    auto_materialize_policy=AutoMaterializePolicy(rules=set()),
+)
+def partitioned_dummy_asset():
+    return 1
 
 
 @observable_source_asset(
@@ -86,6 +97,13 @@ auto_observe_scenarios = {
         expected_run_requests=[
             run_request(asset_keys=["partitioned_observable_source_asset"]),
             run_request(asset_keys=["partitioned_observable_source_asset2"]),
+        ],
+    ),
+    "auto_observe_unpartitioned_source_to_partitioned_asset": AssetReconciliationScenario(
+        unevaluated_runs=[],
+        assets=[asset1, partitioned_dummy_asset],
+        expected_run_requests=[
+            run_request(asset_keys=["asset1"]),
         ],
     ),
 }
