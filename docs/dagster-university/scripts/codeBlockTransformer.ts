4589fe5b6232dfcd52c569402114444eb607c3b9
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2023-09-22 11:32:52 -0400

    fix yarn prettier

--- docs/dagster-university/scripts/codeBlockTransformer.ts
+++ docs/dagster-university/scripts/codeBlockTransformer.ts
@@ -1,18 +1,14 @@
-import { promises as fs } from "fs";
-import path from "path";
+import {promises as fs} from 'fs';
+import path from 'path';
 
-import { Node } from "hast";
-import visit from "unist-util-visit";
+import {Node} from 'hast';
+import visit from 'unist-util-visit';
 
-import { limitSnippetLines } from "./limit";
+import {limitSnippetLines} from './limit';
 
-const DAGSTER_REPO =
-  process.env.DAGSTER_REPO || path.join(__dirname, "../../../");
+const DAGSTER_REPO = process.env.DAGSTER_REPO || path.join(__dirname, '../../../');
 
-const DOCS_SNIPPET = path.join(
-  DAGSTER_REPO,
-  "/examples/docs_snippets/docs_snippets"
-);
+const DOCS_SNIPPET = path.join(DAGSTER_REPO, '/examples/docs_snippets/docs_snippets');
 
 export interface CodeBlockStats {
   totalCodeBlocks: number;
@@ -24,25 +20,23 @@ interface CodeTransformerOptions {
 }
 
 const normalizeOptionValue = (value: string): string | boolean => {
-  if (value === "true") {
+  if (value === 'true') {
     return true;
-  } else if (value === "false") {
+  } else if (value === 'false') {
     return false;
   } else {
     return value;
   }
 };
 
-export default ({
-    setCodeBlockStats: setCodeBlockStats,
-  }: CodeTransformerOptions) =>
+export default ({setCodeBlockStats: setCodeBlockStats}: CodeTransformerOptions) =>
   async (tree: Node) => {
     const codes: [Node, number][] = [];
-    visit(tree, "code", (node, index) => {
+    visit(tree, 'code', (node, index) => {
       codes.push([node, index]);
     });
 
-    const optionKeys = ["lines", "startafter", "endbefore", "dedent", "trim"];
+    const optionKeys = ['lines', 'startafter', 'endbefore', 'dedent', 'trim'];
 
     const stats: CodeBlockStats = {
       totalCodeBlocks: 0,
@@ -50,8 +44,8 @@ export default ({
     };
 
     for (const [node] of codes) {
-      const meta = ((node["meta"] as string) || "").split(" ");
-      const fileMeta = meta.find((m) => m.startsWith("file="));
+      const meta = ((node['meta'] as string) || '').split(' ');
+      const fileMeta = meta.find((m) => m.startsWith('file='));
       if (!fileMeta) {
         continue;
       }
@@ -70,45 +64,37 @@ export default ({
         const needle = `${option}=`;
         const value = meta.find((m) => m.startsWith(needle));
         if (value) {
-          metaOptions[option] = normalizeOptionValue(
-            value.slice(needle.length)
-          );
+          metaOptions[option] = normalizeOptionValue(value.slice(needle.length));
         }
       }
 
-      const filePath = fileMeta.slice("file=".length);
+      const filePath = fileMeta.slice('file='.length);
       const fileAbsPath = path.join(DOCS_SNIPPET, filePath);
       try {
-        const content = await fs.readFile(fileAbsPath, "utf8");
+        const content = await fs.readFile(fileAbsPath, 'utf8');
         let contentWithLimit = limitSnippetLines(
           content,
           metaOptions.lines,
           metaOptions.dedent,
           metaOptions.startafter,
-          metaOptions.endbefore
+          metaOptions.endbefore,
         );
 
         // remove pragmas
-        contentWithLimit = contentWithLimit.replace(
-          /^\s*# (type|ruff|isort|noqa):.*$/g,
-          ""
-        );
-        contentWithLimit = contentWithLimit.replace(
-          /  # (type|ruff|isort|noqa):.*$/g,
-          ""
-        );
+        contentWithLimit = contentWithLimit.replace(/^\s*# (type|ruff|isort|noqa):.*$/g, '');
+        contentWithLimit = contentWithLimit.replace(/  # (type|ruff|isort|noqa):.*$/g, '');
 
         if (metaOptions.trim) {
           contentWithLimit = contentWithLimit.trim();
         }
 
         stats.totalCodeBlocks++;
-        if (node["value"] !== contentWithLimit) {
-          stats.updatedCodeBlocks.push(node["meta"] as string);
-          node["value"] = `${contentWithLimit}`;
+        if (node['value'] !== contentWithLimit) {
+          stats.updatedCodeBlocks.push(node['meta'] as string);
+          node['value'] = `${contentWithLimit}`;
         }
       } catch (err) {
-        node["value"] = err.message;
+        node['value'] = err.message;
       }
     }
 
