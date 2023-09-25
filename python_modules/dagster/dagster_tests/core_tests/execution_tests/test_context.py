b447c3b962712c1168a8d0128c77d44dd03150b8
JamieDeMaria <jamie@dagsterlabs.com>
2023-09-25 14:06:13 -0400

    move test here

--- python_modules/dagster/dagster_tests/core_tests/execution_tests/test_context.py
+++ python_modules/dagster/dagster_tests/core_tests/execution_tests/test_context.py
@@ -1,5 +1,8 @@
+import warnings
+
 import dagster._check as check
-from dagster import OpExecutionContext, job, op
+import pytest
+from dagster import AssetExecutionContext, OpExecutionContext, job, op
 from dagster._core.definitions.job_definition import JobDefinition
 from dagster._core.definitions.op_definition import OpDefinition
 from dagster._core.storage.dagster_run import DagsterRun
@@ -20,3 +23,24 @@ def test_op_execution_context():
         ctx_op()
 
     assert foo.execute_in_process().success
+
+
+def test_instance_check():
+    # turn off any outer warnings filters, e.g. ignores that are set in pyproject.toml
+    warnings.resetwarnings()
+    warnings.filterwarnings("error")
+
+    @op
+    def test_op_context_instance_check(context: OpExecutionContext):
+        step_context = context._step_execution_context  # noqa: SLF001
+        asset_context = AssetExecutionContext(step_execution_context=step_context)
+        op_context = OpExecutionContext(step_execution_context=step_context)
+        with pytest.raises(DeprecationWarning):
+            isinstance(asset_context, OpExecutionContext)
+        assert isinstance(op_context, OpExecutionContext)
+
+    @job
+    def test_isinstance():
+        test_op_context_instance_check()
+
+    test_isinstance.execute_in_process()
