c0dbfb5a7a39b1b72634f3bb6697991a0488c2c2
Nick Schrock <schrockn@dagsterlabs.com>
2023-09-27 06:55:00 -0400

    PipesSubprocess --> PipesSubprocessClient (#16813)
    ## Summary & Motivation
    
    Renaming `PipesSubprocess` to `PipesSubprocessClient` in line with the
    naming conventions of the other `PipesClient` classes. Flagged prior PR
    and followed up on here.
    
    ## How I Tested These Changes

--- examples/experimental/assets_yaml_dsl/assets_yaml_dsl/__init__.py
+++ examples/experimental/assets_yaml_dsl/assets_yaml_dsl/__init__.py
@@ -1,10 +1,10 @@
 from dagster import Definitions
-from dagster._core.pipes.subprocess import PipesSubprocess
+from dagster._core.pipes.subprocess import PipesSubprocessClient
 
 from .domain_specific_dsl.stocks_dsl import get_stocks_dsl_example_defs
 from .pure_assets_dsl.assets_dsl import get_asset_dsl_example_defs
 
 defs = Definitions(
     assets=get_asset_dsl_example_defs() + get_stocks_dsl_example_defs(),
-    resources={"subprocess_resource": PipesSubprocess()},
+    resources={"pipes_subprocess_client": PipesSubprocessClient()},
 )
