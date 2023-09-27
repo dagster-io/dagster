46351fa356f2921361d7c6be8dde8f26e60d7b7c
Isaac Hellendag <dish@elementl.com>
2023-09-27 11:27:31 -0500

    [docs] Use GTM snippet

--- docs/next/pages/_app.tsx
+++ docs/next/pages/_app.tsx
@@ -7,10 +7,13 @@ import {PersistentTabProvider} from 'components/PersistentTabContext';
 import {DefaultSeo} from 'next-seo';
 import {AppProps} from 'next/app';
 import {useRouter} from 'next/router';
+import Script from 'next/script';
 import * as React from 'react';
 
 import Layout from '../layouts/MainLayout';
 import * as gtag from '../util/gtag';
+
+const GTM_TRACKING_ID = process.env.NEXT_PUBLIC_DOCS_GTM_ID;
 
 const BASE_URL = 'https://docs.dagster.io';
 const DEFAULT_SEO = {
@@ -58,6 +61,25 @@ const MyApp = ({Component, pageProps}: AppProps) => {
   return (
     <>
       <DefaultSeo canonical={canonicalUrl} {...DEFAULT_SEO} />
+      {/* {process.env.NODE_ENV === 'production' ? ( */}
+      <>
+        <Script id="gtm">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_TRACKING_ID}');`}
+        </Script>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_TRACKING_ID}`}
+            height="0"
+            width="0"
+            style={{display: 'none', visibility: 'hidden'}}
+          ></iframe>
+        </noscript>
+      </>
+      {/* ) : null} */}
       <PersistentTabProvider>
         <Layout>
           <Component {...pageProps} />
