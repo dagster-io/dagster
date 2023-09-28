ea57fb2aba805ab7e0c4b615699274da4983da75
bengotow <bgotow@elementl.com>
2023-09-28 15:05:05 -0500

    [ui] Front-end updates for “Terminate all” queued / in progress runs

--- js_modules/dagster-ui/packages/ui-core/src/runs/types/RunsRoot.types.ts
+++ js_modules/dagster-ui/packages/ui-core/src/runs/types/RunsRoot.types.ts
@@ -58,24 +58,3 @@ export type RunsRootQuery = {
         }>;
       };
 };
-
-export type QueueDaemonStatusQueryVariables = Types.Exact<{[key: string]: never}>;
-
-export type QueueDaemonStatusQuery = {
-  __typename: 'Query';
-  instance: {
-    __typename: 'Instance';
-    id: string;
-    daemonHealth: {
-      __typename: 'DaemonHealth';
-      id: string;
-      daemonStatus: {
-        __typename: 'DaemonStatus';
-        id: string;
-        daemonType: string;
-        healthy: boolean | null;
-        required: boolean;
-      };
-    };
-  };
-};
