635eb7e7da6ade16c21db76f053294523b6bb5d8
Marco Salazar <marco@elementl.com>
2023-09-28 06:46:50 -0400

    basic refactoring

---
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/sidebar/util.ts
@@ -0,0 +1,7 @@
+export function nodeId(node: {path: string; id: string} | {id: string}) {
+  return 'path' in node ? node.path : node.id;
+}
+
+export function getDisplayName(node: GraphNode) {
+  return node.assetKey.path[node.assetKey.path.length - 1]!;
+}
