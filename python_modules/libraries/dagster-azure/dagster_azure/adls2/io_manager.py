2064e73e6b7c3ba4067a4e926e271a13c36ad355
Fábio <dacostaferreirafabio@googlemail.com>
2023-09-28 22:18:56 +0200

    Update io_manager.py to suggested comment

--- python_modules/libraries/dagster-azure/dagster_azure/adls2/io_manager.py
+++ python_modules/libraries/dagster-azure/dagster_azure/adls2/io_manager.py
@@ -63,7 +63,7 @@ class PickledObjectADLS2IOManager(UPathIOManager):
             file_client.delete_file(lease=lease, recursive=True)
 
     def make_directory(self, path: UPath) -> None:
-        # It is not necessary to create directories in adls
+        # It is not necessary to create directories in ADLS2
         return None
     
     def path_exists(self, path: UPath) -> bool:
