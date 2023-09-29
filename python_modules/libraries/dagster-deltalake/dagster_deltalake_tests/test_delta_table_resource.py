f5e60652df57d86e5f2a69af7db1501307ca1dfa
Robert Pack <robstar.pack@gmail.com>
2023-09-29 07:44:07 +0200

    chore: update configuration

--- python_modules/libraries/dagster-deltalake/dagster_deltalake_tests/test_delta_table_resource.py
+++ python_modules/libraries/dagster-deltalake/dagster_deltalake_tests/test_delta_table_resource.py
@@ -1,6 +1,6 @@
 import os
 
-import pandas as pd
+import pyarrow as pa
 from dagster import asset, materialize
 from dagster_deltalake import DeltaTableResource
 from dagster_deltalake.config import LocalConfig
@@ -8,16 +8,21 @@ from deltalake import write_deltalake
 
 
 def test_resource(tmp_path):
-    df = pd.DataFrame({"a": [1, 2, 3], "b": [5, 6, 7]})
+    data = pa.table(
+        {
+            "a": pa.array([1, 2, 3], type=pa.int32()),
+            "b": pa.array([5, 6, 7], type=pa.int32()),
+        }
+    )
 
     @asset
     def create_table(delta_table: DeltaTableResource):
-        write_deltalake(delta_table.url, df, storage_options=delta_table.storage_options.dict())
+        write_deltalake(delta_table.url, data, storage_options=delta_table.storage_options.dict())
 
     @asset
     def read_table(delta_table: DeltaTableResource):
-        res = delta_table.load().to_pandas()
-        assert res.equals(df)
+        res = delta_table.load().to_pyarrow_table()
+        assert res.equals(data)
 
     materialize(
         [create_table, read_table],
