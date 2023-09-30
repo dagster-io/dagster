85a7026bd7f7ad0048109eb42a46d05926ae0e43
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2023-09-29 22:14:00 -0700

    update imports

--- python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/__init__.py
+++ python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/__init__.py
@@ -2,10 +2,14 @@ from dagster_embedded_elt.sling.asset_defs import build_sling_asset
 from dagster_embedded_elt.sling.resources import (
     SlingMode,
     SlingResource,
+    SlingSourceConnection,
+    SlingTargetConnection,
 )
 
 __all__ = [
     "SlingResource",
     "SlingMode",
     "build_sling_asset",
+    "SlingSourceConnection",
+    "SlingTargetConnection",
 ]
