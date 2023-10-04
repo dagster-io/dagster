b7118ab78d1bbd6ca9e191b3a375428eeb44bd25
Daniel Gibson <daniel@elementl.com>
2023-10-04 16:13:49 -0500

    Feature flags to let dagit vary features for certain code locations
    Summary:
    For users on versions that are able to use thew new backfill policy, we want to hide the backfill dialog from the backfill page. For users still on older versions, we want to leave it. This gives the frontend the tools to check that.
    
    Test Plan: BK

--- python_modules/dagster/dagster_tests/core_tests/workspace_tests/test_request_context.py
+++ python_modules/dagster/dagster_tests/core_tests/workspace_tests/test_request_context.py
@@ -1,10 +1,15 @@
 import time
+from typing import Mapping
 from unittest import mock
 
 import pytest
 from dagster._core.errors import (
     DagsterCodeLocationLoadError,
     DagsterCodeLocationNotFoundError,
+)
+from dagster._core.host_representation.feature_flags import (
+    CodeLocationFeatureFlags,
+    get_feature_flags_for_location,
 )
 from dagster._core.host_representation.origin import RegisteredCodeLocationOrigin
 from dagster._core.workspace.context import WorkspaceRequestContext
@@ -15,12 +20,13 @@ from dagster._core.workspace.workspace import (
 from dagster._utils.error import SerializableErrorInfo
 
 
-def test_get_code_location():
+@pytest.fixture
+def workspace_request_context():
     mock_loc = mock.MagicMock()
 
     error_info = SerializableErrorInfo(message="oopsie", stack=[], cls_name="Exception")
 
-    context = WorkspaceRequestContext(
+    return WorkspaceRequestContext(
         instance=mock.MagicMock(),
         workspace_snapshot={
             "loading_loc": CodeLocationEntry(
@@ -54,7 +60,10 @@ def test_get_code_location():
         read_only=True,
     )
 
-    assert context.get_code_location("loaded_loc") == mock_loc
+
+def test_get_code_location(workspace_request_context):
+    context = workspace_request_context
+    assert context.get_code_location("loaded_loc")
     with pytest.raises(DagsterCodeLocationLoadError, match="oopsie"):
         context.get_code_location("error_loc")
 
@@ -68,3 +77,80 @@ def test_get_code_location():
         match="Location missing_loc does not exist in workspace",
     ):
         context.get_code_location("missing_loc")
+
+
+def _location_with_mocked_versions(dagster_library_versions: Mapping[str, str]):
+    code_location = mock.MagicMock()
+    code_location.get_dagster_library_versions = mock.MagicMock(
+        return_value=dagster_library_versions
+    )
+
+    return CodeLocationEntry(
+        origin=RegisteredCodeLocationOrigin("loaded_loc"),
+        code_location=code_location,
+        load_error=None,
+        load_status=CodeLocationLoadStatus.LOADED,
+        display_metadata={},
+        update_timestamp=time.time(),
+    )
+
+
+def test_feature_flags(workspace_request_context):
+    workspace_snapshot = workspace_request_context.get_workspace_snapshot()
+
+    error_loc = workspace_snapshot["error_loc"]
+    assert get_feature_flags_for_location(error_loc) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: False
+    }
+
+    loading_loc = workspace_snapshot["loading_loc"]
+
+    assert get_feature_flags_for_location(loading_loc) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: False
+    }
+
+    # Old version that didn't even have it set
+    really_old_version_loc = _location_with_mocked_versions({})
+
+    assert get_feature_flags_for_location(really_old_version_loc) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: True
+    }
+
+    # old pre 1.5.0 version
+    pre_10_version_loc = _location_with_mocked_versions({"dagster": "0.15.5"})
+
+    assert get_feature_flags_for_location(pre_10_version_loc) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: True
+    }
+
+    # old pre 1.5.0 version
+    old_version_loc = _location_with_mocked_versions({"dagster": "1.4.5"})
+
+    assert get_feature_flags_for_location(old_version_loc) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: True
+    }
+
+    # Post 1.5.0 version
+    new_version_loc = _location_with_mocked_versions({"dagster": "1.5.0"})
+
+    assert get_feature_flags_for_location(new_version_loc) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: False
+    }
+
+    future_version_loc = _location_with_mocked_versions({"dagster": "2.5.0"})
+
+    assert get_feature_flags_for_location(future_version_loc) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: False
+    }
+
+    gibberish_version = _location_with_mocked_versions({"dagster": "BLAHBLAHBLAH"})
+
+    assert get_feature_flags_for_location(gibberish_version) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: False
+    }
+
+    dev_version = _location_with_mocked_versions({"dagster": "1!0+dev"})
+
+    assert get_feature_flags_for_location(dev_version) == {
+        CodeLocationFeatureFlags.SHOW_SINGLE_RUN_BACKFILL_TOGGLE: False
+    }
