48eac89a53a25b84b509697953885f675458d02f
Nick Schrock <schrockn@dagsterlabs.com>
2023-10-04 20:58:10 -0400

    intermediate result

--- python_modules/dagster/dagster_tests/execution_tests/pipes_tests/test_in_process_client.py
+++ python_modules/dagster/dagster_tests/execution_tests/pipes_tests/test_in_process_client.py
@@ -1,4 +1,6 @@
+from re import M
 from dagster import AssetExecutionContext, Definitions, ExecuteInProcessResult, asset
+from dagster._core.definitions.result import MaterializeResult
 from dagster_pipes import PipesContext
 
 from .in_process_client import InProcessPipesClient
@@ -22,6 +24,28 @@ def test_basic_materialization() -> None:
     @asset
     def an_asset(context: AssetExecutionContext, inprocess_client: InProcessPipesClient):
         return inprocess_client.run(context=context, fn=_impl).get_results()
+
+    result = execute_asset_through_def(
+        an_asset, resources={"inprocess_client": InProcessPipesClient()}
+    )
+    assert result.success
+    mat_events = result.get_asset_materialization_events()
+    assert len(mat_events) ==1
+    assert mat_events[0].materialization.metadata["some_key"].value == "some_value"
+    assert called["yes"]
+
+
+
+def test_get_materialize_result():
+    called = {}
+
+    def _impl(context: PipesContext):
+        context.report_asset_materialization(metadata={"some_key": "some_value"})
+        called["yes"] = True
+
+    @asset
+    def an_asset(context: AssetExecutionContext, inprocess_client: InProcessPipesClient) -> MaterializeResult:
+        return inprocess_client.run(context=context, fn=_impl).get_materialize_result()
 
     result = execute_asset_through_def(
         an_asset, resources={"inprocess_client": InProcessPipesClient()}
