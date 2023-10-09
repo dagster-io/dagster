17d079353ab4d1ccd7491b5fc801df8648314628
bengotow <bgotow@elementl.com>
2023-10-09 16:25:30 -0500

    Read dagster/reporting_user instead of user

--- js_modules/dagster-ui/packages/ui-core/src/assets/RunlessEventTag.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/RunlessEventTag.tsx
@@ -6,7 +6,9 @@ import {DagsterTag} from '../runs/RunTag';
 export const RunlessEventTag: React.FC<{
   tags: Array<{__typename: 'EventTag'; key: string; value: string}>;
 }> = ({tags}) => {
-  const user = tags.find((t) => t.key === DagsterTag.User);
+  const user = tags.find((t) => t.key === DagsterTag.ReportingUser);
 
+  // Note: This does not use UserDisplay because in cloud, the UserDisplay component is hardcoded to a
+  // 14pt font size. I think it'd be nice to make that component more flexible and revisit.
   return user ? <Tag>Reported by {user.value}</Tag> : <Tag>Reported</Tag>;
 };
