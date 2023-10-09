f2fce59dff6f1212b1991d3f55209a1280aacc0e
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2023-10-09 16:32:44 -0700

    [docs][pipes] RFC: Subprocess section

---
+++ examples/docs_snippets/docs_snippets/guides/dagster/dagster_pipes/subprocess/dagster_subprocess_env_extras.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+from dagster import (
+    AssetExecutionContext,
+    MaterializeResult,
+    PipesSubprocessClient,
+    asset,
+    file_relative_path,
+)
+
+
+@asset
+def subprocess_asset(
+    context: AssetExecutionContext, pipes_subprocess_client: PipesSubprocessClient
+) -> MaterializeResult:
+    cmd = [shutil.which("python"), file_relative_path(__file__, "external_code.py")]
+    return pipes_subprocess_client.run(
+        command=cmd,
+        context=context,
+        extras={"foo": "bar"},
+        env={
+            "MY_ENV_VAR_IN_SUBPROCESS": os.environ["MY_ENV_VAR"],
+        },
+    ).get_materialize_result()
