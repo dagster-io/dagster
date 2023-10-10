5beb9a3c1c0d88dca091948c0d4c8a58f9883b8f
Joseph Van Drunen <josephvandrunen@gmail.com>
2023-10-10 13:56:47 -0400

    fixup

--- examples/experimental/external_assets/airflow_example.py
+++ examples/experimental/external_assets/airflow_example.py
@@ -1,6 +1,3 @@
-import json
-
-import requests
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
@@ -10,17 +7,6 @@ from pendulum import datetime
 default_args = {
     "start_date": datetime(2023, 10, 1),
 }
-
-
-def create_asset_materialization(context):
-    response = requests.post(
-        "http://hooli.dagster.cloud/test/report_asset_materialization/external_k8s_pod_operator_asset",
-        data=json.dumps({}),
-        headers={"content-type": "application/json", "Dagster-Cloud-Api-Token": "API_TOKEN"},
-    )
-    response.raise_for_status()
-
-
 in_cluster = False
 config_file = "/opt/airflow/.kube/config"
 
@@ -35,6 +21,5 @@ with DAG(dag_id="example_kubernetes_pod", schedule="@daily", default_args=defaul
         cluster_context="docker-desktop",  # is ignored when in_cluster is set to True
         config_file=config_file,
         is_delete_operator_pod=True,
-        on_success_callback=create_asset_materialization,
         get_logs=True,
     )
