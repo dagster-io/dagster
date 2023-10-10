9463cc3559f27d33ebd2d74b8185a40a13dcce63
Joseph Van Drunen <josephvandrunen@gmail.com>
2023-10-10 13:59:16 -0400

    fixup

--- examples/experimental/external_assets/airflow_example.py
+++ examples/experimental/external_assets/airflow_example.py
@@ -7,19 +7,17 @@ from pendulum import datetime
 default_args = {
     "start_date": datetime(2023, 10, 1),
 }
-in_cluster = False
-config_file = "/opt/airflow/.kube/config"
 
 with DAG(dag_id="example_kubernetes_pod", schedule="@daily", default_args=default_args) as dag:
     KubernetesPodOperator(
         image="debian:stable-slim",
         cmds=["bash", "-cx"],
-        arguments=["echo", "10"],
+        arguments=["python", "create_asset.py", "{{ ds }}"],
         name="airflow-test-pod",
-        task_id="task-one",
-        in_cluster=in_cluster,  # if set to true, will look in the cluster, if false, looks for file
-        cluster_context="docker-desktop",  # is ignored when in_cluster is set to True
-        config_file=config_file,
+        task_id="asset-materialization-task",
+        in_cluster=False,
+        cluster_context="docker-desktop",
+        config_file="/opt/airflow/.kube/config",
         is_delete_operator_pod=True,
         get_logs=True,
     )
