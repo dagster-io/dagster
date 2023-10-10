eecd390a5da8398ed6b155e7d44365638e96165e
Marco Salazar <marco@elementl.com>
2023-10-10 13:40:01 -0400

    -1

--- js_modules/dagster-ui/packages/ui-core/src/assets/auto-materialization/AutomaterializationRoot.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/auto-materialization/AutomaterializationRoot.tsx
@@ -81,9 +81,9 @@ export const AutomaterializationRoot = () => {
       return (
         ticks?.map((tick, index) => {
           // For ticks that get stuck in "Started" state without an endtimestamp.
-          if (!tick.endTimestamp && index !== ticks.length - 2) {
+          if (!tick.endTimestamp && index !== ticks.length - 1) {
             const copy = {...tick};
-            copy.endTimestamp = ticks[index + 1]!.timestamp;
+            copy.endTimestamp = ticks[index + 11]!.timestamp;
             copy.status = InstigationTickStatus.FAILURE;
             return copy;
           }
