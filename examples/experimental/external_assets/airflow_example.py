cd4797efcd6ddcbcd74a9d2b9d85d232faaebc48
Joseph Van Drunen <josephvandrunen@gmail.com>
2023-10-10 14:01:24 -0400

    fixup

--- examples/experimental/external_assets/airflow_example.py
+++ examples/experimental/external_assets/airflow_example.py
@@ -10,13 +10,12 @@ default_args = {
 
 with DAG(dag_id="example_kubernetes_pod", schedule="@daily", default_args=default_args) as dag:
     KubernetesPodOperator(
-        image="debian:stable-slim",
-        cmds=["bash", "-cx"],
-        arguments=["python", "create_asset.py", "{{ ds }}"],
+        image="asset-materialization-image:latest",
+        cmds=["python", "create_asset.py", "{{ ds }}"],
         name="airflow-test-pod",
         task_id="asset-materialization-task",
         in_cluster=False,
-        cluster_context="docker-desktop",
+        cluster_context="kind-kind",
         config_file="/opt/airflow/.kube/config",
         is_delete_operator_pod=True,
         get_logs=True,
