2c03d474897cdd9b728421871fd280f9487b6a5e
Joe Van Drunen <joe@elementl.com>
2023-10-10 22:23:16 -0400

    [demo][external-assets] add external yaml for dep structure (#17135)
    ## Summary & Motivation
    
    this pr adds a rough example of how you can ingest an external
    definition of what would be considered an asset into a collection of
    asset_specs.
    
    ## How I Tested These Changes

--- examples/experimental/external_assets/external_assets/external_assets/__init__.py
+++ examples/experimental/external_assets/external_assets/external_assets/__init__.py
@@ -1,60 +1,29 @@
+import os
+
+import yaml
 from dagster import AssetKey
 from dagster._core.definitions.asset_spec import AssetSpec
 from dagster._core.definitions.external_asset import (
     external_assets_from_specs,
 )
 
+
+def build_asset_specs_from_external_definitions():
+    specs = []
+    with open(os.path.join(os.path.dirname(__file__), "asset_defs.yaml"), "r") as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+        for asset in data["assets"]:
+            deps = []
+            for dep in asset.get("dependsOn", []):
+                deps.append(AssetKey(dep.split("/")))
+            specs.append(
+                AssetSpec(
+                    key=AssetKey(asset["name"].split("/")), group_name="external_assets", deps=deps
+                )
+            )
+    return specs
+
+
 external_asset_defs = external_assets_from_specs(
-    specs=[
-        AssetSpec(
-            key=AssetKey(["s3", "iot_raw_telem_eu"]),
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["s3", "iot_scrubbed_telem_eu"]),
-            deps=[AssetKey(["s3", "iot_raw_telem_eu"])],
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["s3", "iot_raw_telem_americas"]),
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["s3", "iot_scrubbed_telem_americas"]),
-            deps=[AssetKey(["s3", "iot_raw_telem_americas"])],
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["s3", "iot_raw_telem_apac"]),
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["s3", "iot_scrubbed_telem_apac"]),
-            deps=[AssetKey(["s3", "iot_raw_telem_apac"])],
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["vendors", "telem_vendor_foo"]),
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["vendors", "telem_vendor_bar"]),
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["s3", "joined_sensor_telem"]),
-            deps=[
-                AssetKey(["vendors", "telem_vendor_foo"]),
-                AssetKey(["vendors", "telem_vendor_bar"]),
-                AssetKey(["s3", "iot_scrubbed_telem_apac"]),
-                AssetKey(["s3", "iot_scrubbed_telem_americas"]),
-                AssetKey(["s3", "iot_scrubbed_telem_eu"]),
-            ],
-            group_name="external_assets",
-        ),
-        AssetSpec(
-            key=AssetKey(["static", "admin_boundaries"]),
-            group_name="external_assets",
-        ),
-    ]
+    specs=build_asset_specs_from_external_definitions()
 )
