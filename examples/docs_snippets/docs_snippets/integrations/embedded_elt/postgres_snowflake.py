465df031114a57b2016d8322d334453a5a238ed3
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2023-10-11 16:40:48 -0700

    This uses tested snippets for the embedded-elt docs and fixes some formatting issues with the Python code blocks

---
+++ examples/docs_snippets/docs_snippets/integrations/embedded_elt/postgres_snowflake.py
@@ -0,0 +1,39 @@
+import os
+
+from dagster_embedded_elt.sling import (
+    SlingMode,
+    SlingResource,
+    SlingSourceConnection,
+    SlingTargetConnection,
+    build_sling_asset,
+)
+
+from dagster import AssetSpec
+
+source = SlingSourceConnection(
+    type="postgres",
+    host="localhost",
+    port=5432,
+    database="my_database",
+    user="my_user",
+    password=os.getenv("PG_PASS"),
+)
+
+target = SlingTargetConnection(
+    type="snowflake",
+    host="hostname.snowflake",
+    user="username",
+    database="database",
+    password=os.getenv("SF_PASSWORD"),
+    role="role",
+)
+
+sling = SlingResource(source_connection=source, target_connection=target)
+
+asset_def = build_sling_asset(
+    asset_spec=AssetSpec("my_asset_name"),
+    source_stream="public.my_table",
+    target_object="marts.my_table",
+    mode=SlingMode.INCREMENTAL,
+    primary_key="id",
+)
---
+++ examples/docs_snippets/docs_snippets/integrations/embedded_elt/s3_snowflake.py
@@ -0,0 +1,39 @@
+import os
+
+from dagster_embedded_elt.sling import (
+    SlingResource,
+    SlingSourceConnection,
+    SlingTargetConnection,
+    build_sling_asset,
+)
+
+from dagster import AssetSpec
+
+target = SlingTargetConnection(
+    type="snowflake",
+    host="hostname.snowflake",
+    user="username",
+    database="database",
+    password=os.getenv("SF_PASSWORD"),
+    role="role",
+)
+
+
+# start_storage_config
+source = SlingSourceConnection(
+    type="s3",
+    bucket="sling-bucket",
+    access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
+
+sling = SlingResource(source_connection=source, target_connection=target)
+
+asset_def = build_sling_asset(
+    asset_spec=AssetSpec("my_asset_name"),
+    source_stream="s3://my-bucket/my_file.parquet",
+    target_object="marts.my_table",
+    primary_key="id",
+)
+
+# end_storage_config
