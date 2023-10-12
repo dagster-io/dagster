cb24457e3a2aefc0e3c340f0fa9a421a7263c602
Owen Kephart <owen@elementl.com>
2023-10-12 14:17:03 -0700

    update comment

--- python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/scenarios/auto_materialize_policy_scenarios.py
+++ python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/scenarios/auto_materialize_policy_scenarios.py
@@ -988,7 +988,6 @@ auto_materialize_policy_scenarios = {
             # C must wait for B to be materialized
             expected_run_requests=[],
         ),
-        # manually run C
         expected_run_requests=[],
         unevaluated_runs=[],
     ),
