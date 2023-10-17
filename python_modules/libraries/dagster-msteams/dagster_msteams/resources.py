76c45e64f45fbf2571015f51ef64746a7912b98d
benpankow <ben@elementl.com>
2023-10-17 16:00:40 -0700

    fix optional fields

--- python_modules/libraries/dagster-msteams/dagster_msteams/resources.py
+++ python_modules/libraries/dagster-msteams/dagster_msteams/resources.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from dagster import ConfigurableResource, resource
 from dagster._core.definitions.resource_definition import dagster_maintained_resource
 from pydantic import Field
@@ -44,7 +46,7 @@ class MSTeamsResource(ConfigurableResource):
             )
     """
 
-    hook_url: str = Field(
+    hook_url: Optional[str] = Field(
         default=None,
         description=(
             "To send messages to MS Teams channel, an incoming webhook has to be created. The"
@@ -54,8 +56,8 @@ class MSTeamsResource(ConfigurableResource):
             " https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook"
         ),
     )
-    http_proxy: str = Field(default=None, description="HTTP proxy URL")
-    https_proxy: str = Field(default=None, description="HTTPS proxy URL")
+    http_proxy: Optional[str] = Field(default=None, description="HTTP proxy URL")
+    https_proxy: Optional[str] = Field(default=None, description="HTTPS proxy URL")
     timeout: float = Field(default=60, description="Timeout for requests to MS Teams")
     verify: bool = Field(
         default=True, description="Whether to verify SSL certificates, defaults to True"
