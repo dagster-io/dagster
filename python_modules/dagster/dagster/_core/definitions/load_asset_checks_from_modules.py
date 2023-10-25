39b014bcd924d6d93a0307947fdb76445b3e4700
Johann Miller <johann@elementl.com>
2023-10-25 16:10:11 -0400

    load_asset_checks_from_package_* (#17381)
    load_asset_checks_from_package_module and
    load_asset_checks_from_package_name

--- python_modules/dagster/dagster/_core/definitions/load_asset_checks_from_modules.py
+++ python_modules/dagster/dagster/_core/definitions/load_asset_checks_from_modules.py
@@ -1,4 +1,5 @@
 import inspect
+from importlib import import_module
 from types import ModuleType
 from typing import Iterable, Optional, Sequence
 
@@ -9,7 +10,7 @@ from .events import (
     CoercibleToAssetKeyPrefix,
     check_opt_coercible_to_asset_key_prefix_param,
 )
-from .load_assets_from_modules import find_objects_in_module_of_types
+from .load_assets_from_modules import find_modules_in_package, find_objects_in_module_of_types
 
 
 def _checks_from_modules(modules: Iterable[ModuleType]) -> Sequence[AssetChecksDefinition]:
@@ -38,14 +39,14 @@ def load_asset_checks_from_modules(
     modules: Iterable[ModuleType],
     asset_key_prefix: Optional[CoercibleToAssetKeyPrefix] = None,
 ) -> Sequence[AssetChecksDefinition]:
-    """Constructs a list of asset checks from the given modules. This is most often used in conjunction
-    with a call to `load_assets_from_modules`.
+    """Constructs a list of asset checks from the given modules. This is most often used in
+    conjunction with a call to `load_assets_from_modules`.
 
     Args:
         modules (Iterable[ModuleType]): The Python modules to look for checks inside.
         asset_key_prefix (Optional[Union[str, Sequence[str]]]):
-            The prefix for the asset keys targeted by the loaded checks. This should match the key_prefix
-            argument to load_assets_from_modules.
+            The prefix for the asset keys targeted by the loaded checks. This should match the
+            key_prefix argument to load_assets_from_modules.
 
     Returns:
         Sequence[AssetChecksDefinition]:
@@ -59,15 +60,15 @@ def load_asset_checks_from_modules(
 
 
 def load_asset_checks_from_current_module(
-    asset_key_prefix: Optional[CoercibleToAssetKeyPrefix] = None,
+    asset_key_prefix: Optional[CoercibleToAssetKeyPrefix] = None
 ) -> Sequence[AssetChecksDefinition]:
-    """Constructs a list of asset checks from the module where this function is called. This is most often used in conjunction
-    with a call to `load_assets_from_current_module`.
+    """Constructs a list of asset checks from the module where this function is called. This is most
+    often used in conjunction with a call to `load_assets_from_current_module`.
 
     Args:
         asset_key_prefix (Optional[Union[str, Sequence[str]]]):
-            The prefix for the asset keys targeted by the loaded checks. This should match the key_prefix
-            argument to load_assets_from_modules.
+            The prefix for the asset keys targeted by the loaded checks. This should match the
+            key_prefix argument to load_assets_from_current_module.
 
     Returns:
         Sequence[AssetChecksDefinition]:
@@ -84,4 +85,57 @@ def load_asset_checks_from_current_module(
 
     return _checks_with_attributes(
         _checks_from_modules([module]), asset_key_prefix=asset_key_prefix
+    )
+
+
+def load_asset_checks_from_package_module(
+    package_module: ModuleType, asset_key_prefix: Optional[CoercibleToAssetKeyPrefix] = None
+) -> Sequence[AssetChecksDefinition]:
+    """Constructs a list of asset checks from all sub-modules of the given package module. This is
+    most often used in conjunction with a call to `load_assets_from_package_module`.
+
+    Args:
+        package_module (ModuleType): The Python module to look for checks inside.
+        asset_key_prefix (Optional[Union[str, Sequence[str]]]):
+            The prefix for the asset keys targeted by the loaded checks. This should match the
+            key_prefix argument to load_assets_from_package_module.
+
+    Returns:
+        Sequence[AssetChecksDefinition]:
+            A list containing asset checks defined in the package.
+    """
+    asset_key_prefix = check_opt_coercible_to_asset_key_prefix_param(
+        asset_key_prefix, "asset_key_prefix"
+    )
+
+    return _checks_with_attributes(
+        _checks_from_modules(find_modules_in_package(package_module)),
+        asset_key_prefix=asset_key_prefix,
+    )
+
+
+def load_asset_checks_from_package_name(
+    package_name: str, asset_key_prefix: Optional[CoercibleToAssetKeyPrefix] = None
+) -> Sequence[AssetChecksDefinition]:
+    """Constructs a list of asset checks from all sub-modules of the given package. This is most
+    often used in conjunction with a call to `load_assets_from_package_name`.
+
+    Args:
+        package_name (str): The name of the Python package to look for checks inside.
+        asset_key_prefix (Optional[Union[str, Sequence[str]]]):
+            The prefix for the asset keys targeted by the loaded checks. This should match the
+            key_prefix argument to load_assets_from_package_name.
+
+    Returns:
+        Sequence[AssetChecksDefinition]:
+            A list containing asset checks defined in the package.
+    """
+    asset_key_prefix = check_opt_coercible_to_asset_key_prefix_param(
+        asset_key_prefix, "asset_key_prefix"
+    )
+
+    package_module = import_module(package_name)
+    return _checks_with_attributes(
+        _checks_from_modules(find_modules_in_package(package_module)),
+        asset_key_prefix=asset_key_prefix,
     )
