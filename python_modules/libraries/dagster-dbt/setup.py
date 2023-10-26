beb55237d46a80fbaa17c29a58bd766f2cf1f6d5
Rex Ledesma <rex@dagsterlabs.com>
2023-10-26 12:15:24 -0400

    feat(dbt): support `dbt-core==1.7.*`

--- python_modules/libraries/dagster-dbt/setup.py
+++ python_modules/libraries/dagster-dbt/setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
         f"dagster{pin}",
         # Follow the version support constraints for dbt Core: https://docs.getdbt.com/docs/dbt-versions/core
-        "dbt-core<1.7",
+        "dbt-core<1.8",
         "Jinja2",
         "networkx",
         "orjson",
