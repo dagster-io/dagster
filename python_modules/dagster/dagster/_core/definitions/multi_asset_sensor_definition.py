ac6222d3df991e50ac7bc7a6d2dbc95b3e398ec7
Maxime Lagresle <maxime@wellfound.com>
2023-10-26 09:20:44 +0200

    better cursor handling switching from asset to multi-asset

--- python_modules/dagster/dagster/_core/definitions/multi_asset_sensor_definition.py
+++ python_modules/dagster/dagster/_core/definitions/multi_asset_sensor_definition.py
@@ -125,7 +125,7 @@ class MultiAssetSensorContextCursor:
     # Must call MultiAssetSensorEvaluationContext._update_cursor_after_evaluation at end of tick
     # to serialize the cursor.
     def __init__(self, cursor: Optional[str], context: "MultiAssetSensorEvaluationContext"):
-        loaded_cursor = json.loads(cursor) if cursor else {}
+        loaded_cursor = json.loads(cursor) if cursor and hasattr(cursor, "items") else {}
         self._cursor_component_by_asset_key: Dict[str, MultiAssetSensorAssetCursorComponent] = {}
 
         # The initial latest consumed event ID at the beginning of the tick
