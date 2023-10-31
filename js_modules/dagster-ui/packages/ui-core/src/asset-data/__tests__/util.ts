2840be698478756ac809e2bd2ee2227325c4ef26
Marco polo <salazarm@users.noreply.github.com>
2023-10-31 10:32:27 -0400

    [AssetLiveDataProvider] Retry asset keys that fail to fetch after the poll interval. (#17393)
    ## Summary & Motivation
    
    Currently if an asset key fails to fetch we never retry fetching it
    again because it gets stuck in the requested state. This PR fixes that
    and marks them as "fetched" when this happens. This has the consequence
    of (1) letting us move on to the next set of asset keys which could
    succeed and (2) it means we'll retry the failed asset keys again after
    the polling interval threshold is crossed.
    
    ## How I Tested These Changes
    jest. Locally threw an error and saw that we did end up retrying the
    failed asset keys after the polling interval was met.

--- js_modules/dagster-ui/packages/ui-core/src/asset-data/__tests__/util.ts
+++ js_modules/dagster-ui/packages/ui-core/src/asset-data/__tests__/util.ts
@@ -1,3 +1,5 @@
+import {GraphQLError} from 'graphql/error';
+
 import {
   AssetKeyInput,
   buildAssetNode,
@@ -11,20 +13,35 @@ import {
   AssetGraphLiveQueryVariables,
 } from '../types/AssetLiveDataProvider.types';
 
-export function buildMockedAssetGraphLiveQuery(assetKeys: AssetKeyInput[]) {
+export function buildMockedAssetGraphLiveQuery(
+  assetKeys: AssetKeyInput[],
+  data:
+    | Parameters<
+        typeof buildQueryMock<AssetGraphLiveQuery, AssetGraphLiveQueryVariables>
+      >[0]['data']
+    | undefined = undefined,
+  errors: readonly GraphQLError[] | undefined = undefined,
+) {
   return buildQueryMock<AssetGraphLiveQuery, AssetGraphLiveQueryVariables>({
     query: ASSETS_GRAPH_LIVE_QUERY,
     variables: {
       // strip __typename
       assetKeys: assetKeys.map((assetKey) => ({path: assetKey.path})),
     },
-    data: {
-      assetNodes: assetKeys.map((assetKey) =>
-        buildAssetNode({assetKey: buildAssetKey(assetKey), id: JSON.stringify(assetKey)}),
-      ),
-      assetsLatestInfo: assetKeys.map((assetKey) =>
-        buildAssetLatestInfo({assetKey: buildAssetKey(assetKey), id: JSON.stringify(assetKey)}),
-      ),
-    },
+    data:
+      data === undefined && errors === undefined
+        ? {
+            assetNodes: assetKeys.map((assetKey) =>
+              buildAssetNode({assetKey: buildAssetKey(assetKey), id: JSON.stringify(assetKey)}),
+            ),
+            assetsLatestInfo: assetKeys.map((assetKey) =>
+              buildAssetLatestInfo({
+                assetKey: buildAssetKey(assetKey),
+                id: JSON.stringify(assetKey),
+              }),
+            ),
+          }
+        : data,
+    errors,
   });
 }
