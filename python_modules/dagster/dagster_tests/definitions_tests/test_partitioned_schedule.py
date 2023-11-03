5905832792989c768fd88d7920cce451029c5663
Sandy Ryza <sandy@elementl.com>
2023-11-03 16:05:41 -0700

    fix schedule with job partitions inferred from asset with multi-partitions

--- python_modules/dagster/dagster_tests/definitions_tests/test_partitioned_schedule.py
+++ python_modules/dagster/dagster_tests/definitions_tests/test_partitioned_schedule.py
@@ -482,6 +482,44 @@ def test_unresolved_partitioned_schedule():
     assert run_requests[0].partition_key == "2020-01-01"
 
 
+def test_unresolved_multi_partitioned_schedule():
+    time_window_partitions = DailyPartitionsDefinition(start_date="2020-01-01")
+    static_partitions = StaticPartitionsDefinition(["a", "b", "c", "d"])
+    partitions_def = MultiPartitionsDefinition(
+        {"static": static_partitions, "date": time_window_partitions}
+    )
+
+    @asset(partitions_def=partitions_def)
+    def asset1():
+        return 1
+
+    job1 = define_asset_job("job1")
+    schedule1 = build_schedule_from_partitioned_job(job1)
+
+    @repository
+    def my_repo():
+        return [asset1, job1, schedule1]
+
+    run_requests = (
+        my_repo.get_schedule_def("job1_schedule")
+        .evaluate_tick(
+            build_schedule_context(
+                scheduled_execution_time=datetime.strptime("2020-01-02", DATE_FORMAT)
+            )
+        )
+        .run_requests
+    )
+    assert len(run_requests) == 4
+    assert set([req.partition_key for req in run_requests]) == set(
+        [
+            "2020-01-01|a",
+            "2020-01-01|b",
+            "2020-01-01|c",
+            "2020-01-01|d",
+        ]
+    )
+
+
 def test_dynamic_multipartitioned_job_schedule():
     time_window_partitions = DailyPartitionsDefinition(start_date="2020-01-01")
     dynamic_partitions = DynamicPartitionsDefinition(name="dummy")
