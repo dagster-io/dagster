71ef8da5f4c867e489d07dcd38cd71e3f45dcbe5
Owen Kephart <owen@elementl.com>
2023-11-09 15:53:55 -0800

    update example

--- examples/docs_snippets/docs_snippets/concepts/assets/auto_materialize_on_cron.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/auto_materialize_on_cron.py
@@ -3,12 +3,9 @@ from dagster import AutoMaterializePolicy, AutoMaterializeRule, asset
 materialize_on_cron_policy = AutoMaterializePolicy.eager().with_rules(
     # try to materialize this asset if it hasn't been materialized since the last cron tick
     AutoMaterializeRule.materialize_on_cron("0 9 * * *", timezone="US/Central"),
-    # to make sure assets in the middle of the graph wait for all their parents to be materialized,
-    # the skip_on_not_all_parents_updated rule is added
-    AutoMaterializeRule.skip_on_not_all_parents_updated(),
 )
 
 
 @asset(auto_materialize_policy=materialize_on_cron_policy)
-def asset1(upstream1, upstream2):
+def root_asset():
     ...
