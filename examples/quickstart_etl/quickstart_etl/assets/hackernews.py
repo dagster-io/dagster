925e9f9cda78ff26caf6268d554edf2860c89acd
Tim Castillo <tim@dagsterlabs.com>
2023-11-09 10:16:23 -0500

    Updating quickstart_etl to match 1.5

--- examples/quickstart_etl/quickstart_etl/assets/hackernews.py
+++ examples/quickstart_etl/quickstart_etl/assets/hackernews.py
@@ -1,83 +1,93 @@
 import base64
 from io import BytesIO
-from typing import List
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
+import json
+import os
 from dagster import AssetExecutionContext, MetadataValue, asset
-from wordcloud import STOPWORDS, WordCloud
 
 
 @asset(group_name="hackernews", compute_kind="HackerNews API")
-def hackernews_topstory_ids() -> List[int]:
-    """Get up to 500 top stories from the HackerNews topstories endpoint.
-
-    API Docs: https://github.com/HackerNews/API#new-top-and-best-stories
-    """
+def topstory_ids() -> None:
     newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-    top_500_newstories = requests.get(newstories_url).json()
-    return top_500_newstories
+    top_new_story_ids = requests.get(newstories_url).json()[:100]
+
+    os.makedirs("data", exist_ok=True)
+    with open("data/topstory_ids.json", "w") as f:
+        json.dump(top_new_story_ids, f)
 
 
-@asset(group_name="hackernews", compute_kind="HackerNews API")
-def hackernews_topstories(
-    context: AssetExecutionContext, hackernews_topstory_ids: List[int]
-) -> pd.DataFrame:
-    """Get items based on story ids from the HackerNews items endpoint. It may take 1-2 minutes to fetch all 500 items.
+@asset(
+    deps=[topstory_ids],
+    group_name="hackernews",
+    compute_kind="HackerNews API")
+def topstories(context: AssetExecutionContext) -> None:
+    with open("data/topstory_ids.json", "r") as f:
+        topstory_ids = json.load(f)
 
-    API Docs: https://github.com/HackerNews/API#items
-    """
     results = []
-    for item_id in hackernews_topstory_ids:
-        item = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json").json()
+    for item_id in topstory_ids:
+        item = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
+        ).json()
         results.append(item)
+
         if len(results) % 20 == 0:
             context.log.info(f"Got {len(results)} items so far.")
 
     df = pd.DataFrame(results)
+    df.to_csv("data/topstories.csv")
 
-    # Dagster supports attaching arbitrary metadata to asset materializations. This metadata will be
-    # shown in the run logs and also be displayed on the "Activity" tab of the "Asset Details" page in the UI.
-    # This metadata would be useful for monitoring and maintaining the asset as you iterate.
-    # Read more about in asset metadata in https://docs.dagster.io/concepts/assets/software-defined-assets#recording-materialization-metadata
     context.add_output_metadata(
-        {
-            "num_records": len(df),
+        metadata={
+            "num_records": len(df),  # Metadata can be any key-value pair
             "preview": MetadataValue.md(df.head().to_markdown()),
+            # The `MetadataValue` class has useful static methods to build Metadata
         }
     )
-    return df
 
 
-@asset(group_name="hackernews", compute_kind="Plot")
-def hackernews_topstories_word_cloud(
-    context: AssetExecutionContext, hackernews_topstories: pd.DataFrame
-) -> bytes:
-    """Exploratory analysis: Generate a word cloud from the current top 500 HackerNews top stories.
-    Embed the plot into a Markdown metadata for quick view.
+@asset(deps=[topstories], group_name="hackernews", compute_kind="Plot")
+def most_frequent_words(context: AssetExecutionContext) -> None:
+    stopwords = ["a", "the", "an", "of", "to", "in", "for", "and", "with", "on", "is"]
 
-    Read more about how to create word clouds in http://amueller.github.io/word_cloud/.
-    """
-    stopwords = set(STOPWORDS)
-    stopwords.update(["Ask", "Show", "HN"])
-    titles_text = " ".join([str(item) for item in hackernews_topstories["title"]])
-    titles_cloud = WordCloud(stopwords=stopwords, background_color="white").generate(titles_text)
+    topstories = pd.read_csv("data/topstories.csv")
 
-    # Generate the word cloud image
-    plt.figure(figsize=(8, 8), facecolor=None)
-    plt.imshow(titles_cloud, interpolation="bilinear")
-    plt.axis("off")
-    plt.tight_layout(pad=0)
+    # loop through the titles and count the frequency of each word
+    word_counts = {}
+    for raw_title in topstories["title"]:
+        title = raw_title.lower() # type: ignore
+        for word in title.split():
+            cleaned_word = word.strip(".,-!?:;()[]'\"-")
+            if cleaned_word not in stopwords and len(cleaned_word) > 0:
+                word_counts[cleaned_word] = word_counts.get(cleaned_word, 0) + 1
 
-    # Save the image to a buffer and embed the image into Markdown content for quick view
+    # Get the top 25 most frequent words
+    top_words = {
+        pair[0]: pair[1]
+        for pair in sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:25]
+    }
+
+    # Make a bar chart of the top 25 words
+    plt.figure(figsize=(10, 6))
+    plt.bar(list(top_words.keys()), list(top_words.values())) # type: ignore
+    plt.xticks(rotation=45, ha="right")
+    plt.title("Top 25 Words in Hacker News Titles")
+    plt.tight_layout()
+
+    # Convert the image to a saveable format
     buffer = BytesIO()
     plt.savefig(buffer, format="png")
     image_data = base64.b64encode(buffer.getvalue())
+
+    # Convert the image to Markdown to preview it within Dagster
     md_content = f"![img](data:image/png;base64,{image_data.decode()})"
 
-    # Attach the Markdown content as metadata to the asset
-    # Read about more metadata types in https://docs.dagster.io/_apidocs/ops#metadata-types
-    context.add_output_metadata({"plot": MetadataValue.md(md_content)})
+    with open("data/most_frequent_words.json", "w") as f:
+        json.dump(top_words, f)
 
-    return image_data
+    # Attach the Markdown content as metadata to the asset
+    context.add_output_metadata(metadata={"plot": MetadataValue.md(md_content)})
+
