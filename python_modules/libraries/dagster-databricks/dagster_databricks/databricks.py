f518256b8f88ab6a5a8643bc9eb5c78fe4e84431
zyd14 <romerzs14@gmail.com>
2023-11-09 17:36:47 +0000

    Feature/databricks additional api config (#17799)
    ## Summary & Motivation
    Fix #17796 , and extend the step launcher config API to better match the
    options of the Databricks REST API
    
    ## How I Tested These Changes
    
    Added unit tests to ensure existing and extended config parameters get
    passed through to mocked out `submit_run` call. Ran Databricks jobs via
    the `databricks_pyspark_step_launcher` on a local deployment with the
    new configuration options set and observed the configs being set on the
    Databricks job run.

--- python_modules/libraries/dagster-databricks/dagster_databricks/databricks.py
+++ python_modules/libraries/dagster-databricks/dagster_databricks/databricks.py
@@ -1,7 +1,7 @@
 import base64
 import logging
 import time
-from typing import IO, Any, Mapping, Optional, Tuple, Union, cast
+from typing import IO, Any, List, Mapping, Optional, Tuple, Union, cast
 
 import dagster
 import dagster._check as check
@@ -349,8 +349,19 @@ class DatabricksJobRunner:
             new_cluster = new_cluster.copy()
 
             nodes = new_cluster.pop("nodes")
+
+            if "driver_instance_pool_id" in nodes:
+                check.invariant(
+                    "instance_pool_id" in nodes,
+                    "Usage of `driver_instance_pool_id` requires that `instance_pool_id` be specified"
+                    " for worker nodes",
+                )
+
             if "instance_pool_id" in nodes:
                 new_cluster["instance_pool_id"] = nodes["instance_pool_id"]
+
+                if "driver_instance_pool_id" in nodes:
+                    new_cluster["driver_node_type_id"] = nodes["driver_node_type_id"]
             else:
                 node_types = nodes["node_types"]
                 new_cluster["node_type_id"] = node_types["node_type_id"]
@@ -411,6 +422,9 @@ class DatabricksJobRunner:
             "Multiple tasks specified in Databricks run",
         )
 
+        notification_settings = self._get_notification_settings(run_config)
+        job_health_settings = self._get_job_health_settings(run_config)
+
         return self.client.workspace_client.jobs.submit(
             run_name=run_config.get("run_name"),
             tasks=[
@@ -425,7 +439,46 @@ class DatabricksJobRunner:
                     },
                 )
             ],
+            idempotency_token=run_config.get("idempotency_token"),
+            timeout_seconds=run_config.get("timeout_seconds"),
+            health=job_health_settings,
+            **notification_settings,
         ).bind()["run_id"]
+
+    def _get_job_health_settings(
+        self, run_config: Mapping[str, Any]
+    ) -> Optional[List[jobs.JobsHealthRule]]:
+        if "job_health_settings" in run_config:
+            job_health_settings = [
+                jobs.JobsHealthRule.from_dict(h) for h in run_config["job_health_settings"]
+            ]
+        else:
+            job_health_settings = None
+        return job_health_settings
+
+    def _get_notification_settings(
+        self, run_config: Mapping[str, Any]
+    ) -> Mapping[
+        str,
+        Union[jobs.JobEmailNotifications, jobs.JobNotificationSettings, jobs.WebhookNotifications],
+    ]:
+        notification_configs = {}
+        if "email_notifications" in run_config:
+            email_notifications = jobs.JobEmailNotifications.from_dict(
+                run_config["email_notifications"]
+            )
+            notification_configs.update({"email_notifications": email_notifications})
+        if "notification_settings" in run_config:
+            notification_settings = jobs.JobNotificationSettings.from_dict(
+                run_config["notification_settings"]
+            )
+            notification_configs.update({"notification_settings": notification_settings})
+        if "webhook_notifications" in run_config:
+            webhook_notifications = jobs.WebhookNotifications.from_dict(
+                run_config["webhook_notifications"]
+            )
+            notification_configs.update({"webhook_notifications": webhook_notifications})
+        return notification_configs
 
     def retrieve_logs_for_run_id(
         self, log: logging.Logger, databricks_run_id: int
