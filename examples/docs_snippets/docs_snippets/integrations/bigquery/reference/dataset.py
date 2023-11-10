a583154079755d12838a1540153509f2d1589677
JamieDeMaria <jamie@dagsterlabs.com>
2023-11-10 16:16:18 -0500

    update for pyright

--- examples/docs_snippets/docs_snippets/integrations/bigquery/reference/dataset.py
+++ examples/docs_snippets/docs_snippets/integrations/bigquery/reference/dataset.py
@@ -2,45 +2,42 @@ import pandas as pd
 
 from dagster import SourceAsset, asset
 
-# start_asset_key
 
-daffodil_data = SourceAsset(key=["gcp", "bigquery", "daffodil", "daffodil_data"])
+def scppe_asset_key():
+    # start_asset_key
+    daffodil_data = SourceAsset(key=["gcp", "bigquery", "daffodil", "daffodil_data"])
 
+    @asset(key_prefix=["gcp", "bigquery", "iris"])
+    def iris_data() -> pd.DataFrame:
+        return pd.read_csv(
+            "https://docs.dagster.io/assets/iris.csv",
+            names=[
+                "sepal_length_cm",
+                "sepal_width_cm",
+                "petal_length_cm",
+                "petal_width_cm",
+                "species",
+            ],
+        )
 
-@asset(key_prefix=["gcp", "bigquery", "iris"])
-def iris_data() -> pd.DataFrame:
-    return pd.read_csv(
-        "https://docs.dagster.io/assets/iris.csv",
-        names=[
-            "sepal_length_cm",
-            "sepal_width_cm",
-            "petal_length_cm",
-            "petal_width_cm",
-            "species",
-        ],
-    )
-
-
-# end_asset_key
-
-
-# start_metadata
-
-daffodil_data = SourceAsset(key=["daffodil_data"], metadata={"schema": "daffodil"})
+    # end_asset_key
 
 
-@asset(metadata={"schema": "iris"})
-def iris_data() -> pd.DataFrame:
-    return pd.read_csv(
-        "https://docs.dagster.io/assets/iris.csv",
-        names=[
-            "sepal_length_cm",
-            "sepal_width_cm",
-            "petal_length_cm",
-            "petal_width_cm",
-            "species",
-        ],
-    )
+def scope_metadata():
+    # start_metadata
+    daffodil_data = SourceAsset(key=["daffodil_data"], metadata={"schema": "daffodil"})
 
+    @asset(metadata={"schema": "iris"})
+    def iris_data() -> pd.DataFrame:
+        return pd.read_csv(
+            "https://docs.dagster.io/assets/iris.csv",
+            names=[
+                "sepal_length_cm",
+                "sepal_width_cm",
+                "petal_length_cm",
+                "petal_width_cm",
+                "species",
+            ],
+        )
 
-# end_metadata
+    # end_metadata
--- examples/docs_snippets/docs_snippets/integrations/duckdb/reference/schema.py
+++ examples/docs_snippets/docs_snippets/integrations/duckdb/reference/schema.py
@@ -2,46 +2,45 @@ import pandas as pd
 
 from dagster import SourceAsset, asset
 
-# start_asset_key
 
-daffodil_dataset = SourceAsset(key=["daffodil", "daffodil_dataset"])
+def scope_asset_key():
+    # start_asset_key
+    daffodil_dataset = SourceAsset(key=["daffodil", "daffodil_dataset"])
+
+    @asset(key_prefix=["iris"])
+    def iris_dataset() -> pd.DataFrame:
+        return pd.read_csv(
+            "https://docs.dagster.io/assets/iris.csv",
+            names=[
+                "sepal_length_cm",
+                "sepal_width_cm",
+                "petal_length_cm",
+                "petal_width_cm",
+                "species",
+            ],
+        )
+
+    # end_asset_key
 
 
-@asset(key_prefix=["iris"])
-def iris_dataset() -> pd.DataFrame:
-    return pd.read_csv(
-        "https://docs.dagster.io/assets/iris.csv",
-        names=[
-            "sepal_length_cm",
-            "sepal_width_cm",
-            "petal_length_cm",
-            "petal_width_cm",
-            "species",
-        ],
+def scope_metadata():
+    # start_metadata
+
+    daffodil_dataset = SourceAsset(
+        key=["daffodil_dataset"], metadata={"schema": "daffodil"}
     )
 
+    @asset(metadata={"schema": "iris"})
+    def iris_dataset() -> pd.DataFrame:
+        return pd.read_csv(
+            "https://docs.dagster.io/assets/iris.csv",
+            names=[
+                "sepal_length_cm",
+                "sepal_width_cm",
+                "petal_length_cm",
+                "petal_width_cm",
+                "species",
+            ],
+        )
 
-# end_asset_key
-
-# start_metadata
-
-daffodil_dataset = SourceAsset(
-    key=["daffodil_dataset"], metadata={"schema": "daffodil"}
-)
-
-
-@asset(metadata={"schema": "iris"})
-def iris_dataset() -> pd.DataFrame:
-    return pd.read_csv(
-        "https://docs.dagster.io/assets/iris.csv",
-        names=[
-            "sepal_length_cm",
-            "sepal_width_cm",
-            "petal_length_cm",
-            "petal_width_cm",
-            "species",
-        ],
-    )
-
-
-# end_metadata
+    # end_metadata
