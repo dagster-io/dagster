37573e7d5ff192e4e8c3e89b18393ca69bab49b4
Marco polo <salazarm@users.noreply.github.com>
2023-11-14 14:44:13 -0800

    remove menu option

--- js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetNodeMenu.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetNodeMenu.tsx
@@ -1,22 +1,5 @@
-import {
-  Menu,
-  MenuItem,
-  Spinner,
-  MenuDivider,
-  Box,
-  Dialog,
-  DialogBody,
-  DialogFooter,
-  Button,
-  Checkbox,
-  Colors,
-  Icon,
-  MiddleTruncate,
-} from '@dagster-io/ui-components';
-import {useVirtualizer} from '@tanstack/react-virtual';
+import {Menu, MenuItem, Spinner, MenuDivider, Box} from '@dagster-io/ui-components';
 import React from 'react';
-import {Link} from 'react-router-dom';
-import styled from 'styled-components';
 
 import {showSharedToaster} from '../app/DomUtils';
 import {
@@ -25,21 +8,11 @@ import {
   AssetKeysDialogEmptyState,
 } from '../assets/AutoMaterializePolicyPage/AssetKeysDialog';
 import {useMaterializationAction} from '../assets/LaunchAssetExecutionButton';
-import {assetDetailsPathForKey} from '../assets/assetDetailsPathForKey';
 import {ExplorerPath} from '../pipelines/PipelinePathUtils';
 import {VirtualizedItemListForDialog} from '../ui/VirtualizedItemListForDialog';
-import {Container, Inner, Row} from '../ui/VirtualizedTable';
 
-import {
-  GraphData,
-  GraphNode,
-  displayNameForAssetKey,
-  getUpstreamNodes,
-  toGraphId,
-  tokenForAssetKey,
-} from './Utils';
+import {GraphData, GraphNode, tokenForAssetKey} from './Utils';
 import {StatusDot} from './sidebar/StatusDot';
-import {AssetNodeKeyFragment} from './types/AssetNode.types';
 
 type Props = {
   graphData: GraphData;
@@ -98,9 +71,6 @@ export const useAssetNodeMenu = ({
     );
   }
 
-  const [showMaterializeWithUpstreamDialog, setShowMaterializeWithUpstreamDialog] =
-    React.useState(false);
-
   return {
     menu: (
       <Menu>
@@ -121,20 +91,6 @@ export const useAssetNodeMenu = ({
             onClick([node.assetKey], e, false);
           }}
         />
-        {upstream.length ? (
-          <MenuItem
-            icon="materialization"
-            text={
-              <Box flex={{direction: 'row', alignItems: 'center', gap: 4}}>
-                <span>Materialize with upstream</span>
-                {loading ? <Spinner purpose="body-text" /> : null}
-              </Box>
-            }
-            onClick={() => {
-              setShowMaterializeWithUpstreamDialog(true);
-            }}
-          />
-        ) : null}
         {upstream.length || downstream.length ? <MenuDivider /> : null}
         {upstream.length ? (
           <MenuItem
@@ -159,15 +115,6 @@ export const useAssetNodeMenu = ({
     ),
     dialog: (
       <>
-        <MaterializeWithUpstreamDialog
-          assetKeys={
-            showMaterializeWithUpstreamDialog ? getUpstreamNodes(node.assetKey, graphData) : []
-          }
-          isOpen={showMaterializeWithUpstreamDialog}
-          close={() => setShowMaterializeWithUpstreamDialog(false)}
-          onMaterialize={onClick}
-          graphData={graphData}
-        />
         <UpstreamDownstreamDialog
           title="Parent assets"
           graphData={graphData}
@@ -264,133 +211,3 @@ const UpstreamDownstreamDialog = ({
     />
   );
 };
-
-const MaterializeWithUpstreamDialog = React.memo(
-  ({
-    assetKeys,
-    close,
-    isOpen,
-    onMaterialize,
-    graphData,
-  }: {
-    assetKeys: AssetNodeKeyFragment[];
-    isOpen: boolean;
-    close: () => void;
-    onMaterialize: ReturnType<typeof useMaterializationAction>['onClick'];
-    graphData: GraphData;
-  }) => {
-    const materialize = async (e: React.MouseEvent<HTMLElement>) => {
-      await showSharedToaster({
-        intent: 'primary',
-        message: 'Initiating materialization',
-        icon: 'materialization',
-      });
-      onMaterialize(assetKeys, e, false);
-    };
-    const containerRef = React.useRef<HTMLDivElement | null>(null);
-    const virtualizer = useVirtualizer({
-      count: assetKeys?.length ?? 0,
-      getScrollElement: () => containerRef.current,
-      estimateSize: () => 28,
-    });
-    const totalHeight = virtualizer.getTotalSize();
-    const items = virtualizer.getVirtualItems();
-
-    const [checked, setChecked] = React.useState<Set<AssetNodeKeyFragment>>(new Set());
-    React.useLayoutEffect(() => {
-      setChecked(new Set(assetKeys));
-    }, [assetKeys]);
-
-    return (
-      <Dialog
-        isOpen={isOpen}
-        onClose={close}
-        title="Materialize with upstream"
-        icon="materialization"
-        style={{transform: 'scale(1)'}}
-      >
-        {/* Change they key after the openning animation to force MiddleTruncates to re-render and re-measure */}
-        <DialogBody>
-          <div style={{scale: 1}}>
-            <RowGrid border="bottom" padding={{bottom: 8}}>
-              <Checkbox
-                id="check-all"
-                checked={checked.size === assetKeys.length}
-                onChange={() => {
-                  setChecked((checked) => {
-                    if (checked.size === assetKeys.length) {
-                      return new Set();
-                    } else {
-                      return new Set(assetKeys);
-                    }
-                  });
-                }}
-              />
-              <label htmlFor="check-all" style={{color: Colors.Gray500, cursor: 'pointer'}}>
-                Asset Name
-              </label>
-            </RowGrid>
-            <Container ref={containerRef} style={{maxHeight: '400px'}}>
-              <Inner $totalHeight={totalHeight}>
-                {items.map(({index, key, size, start, measureElement}) => {
-                  const item = assetKeys[index]!;
-                  return (
-                    <Row $height={size} $start={start} key={key} ref={measureElement}>
-                      <RowGrid border="bottom">
-                        <Checkbox
-                          id={`checkbox-${key}`}
-                          checked={checked.has(item)}
-                          onChange={() => {
-                            setChecked((checked) => {
-                              const copy = new Set(checked);
-                              if (copy.has(item)) {
-                                copy.delete(item);
-                              } else {
-                                copy.add(item);
-                              }
-                              return copy;
-                            });
-                          }}
-                        />
-                        <Box
-                          as="label"
-                          htmlFor={`checkbox-${key}`}
-                          flex={{alignItems: 'center', gap: 4}}
-                          style={{cursor: 'pointer'}}
-                        >
-                          <Box style={{overflow: 'hidden'}}>
-                            <MiddleTruncate text={displayNameForAssetKey(item)} />
-                          </Box>
-                          <Link to={assetDetailsPathForKey(item)} target="_blank">
-                            <Icon name="open_in_new" color={Colors.Link} />
-                          </Link>
-                        </Box>
-                        <StatusDot node={graphData.nodes[toGraphId(item)]!} />
-                      </RowGrid>
-                    </Row>
-                  );
-                })}
-              </Inner>
-            </Container>
-          </div>
-        </DialogBody>
-        <DialogFooter topBorder>
-          <Button onClick={close}>Cancel</Button>
-          <Button intent="primary" onClick={materialize}>
-            Materialize
-          </Button>
-        </DialogFooter>
-      </Dialog>
-    );
-  },
-);
-
-const TEMPLATE_COLUMNS = '20px minmax(0, 1fr) 32px ';
-
-const RowGrid = styled(Box)`
-  display: grid;
-  grid-template-columns: ${TEMPLATE_COLUMNS};
-  gap: 8px;
-  height: 100%;
-  align-items: center;
-`;
