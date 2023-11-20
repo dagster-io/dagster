fb5d3bb412a11ec3a9e48dc6f48c78012d8079f2
JamieDeMaria <jamie@dagsterlabs.com>
2023-11-20 15:28:01 -0500

    run props on init

--- python_modules/dagster/dagster/_core/execution/context/compute.py
+++ python_modules/dagster/dagster/_core/execution/context/compute.py
@@ -1420,7 +1420,12 @@ class AssetExecutionContext(OpExecutionContext):
         )
         self._step_execution_context = self._op_execution_context._step_execution_context  # noqa: SLF001
 
-        self._run_props = None
+        self._run_props = RunProperties(
+            run_id=self._op_execution_context.run_id,
+            run_config=self._op_execution_context.run_config,
+            dagster_run=self._op_execution_context.run,
+            retry_number=self._op_execution_context.retry_number,
+        )
 
     @staticmethod
     def get() -> "AssetExecutionContext":
@@ -1437,13 +1442,6 @@ class AssetExecutionContext(OpExecutionContext):
 
     @property
     def run_properties(self) -> RunProperties:
-        if self._run_props is None:
-            self._run_props = RunProperties(
-                run_id=self._op_execution_context.run_id,
-                run_config=self._op_execution_context.run_config,
-                dagster_run=self._op_execution_context.run,
-                retry_number=self._op_execution_context.retry_number,
-            )
         return self._run_props
 
     ######## Deprecated methods
