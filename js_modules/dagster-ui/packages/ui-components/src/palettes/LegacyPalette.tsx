176111fc42a3b232bf60523b4f68eae0e0c13392
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2023-11-22 13:08:38 -0600

    Add legacy palette (#18230)
    ## Summary & Motivation
    
    Add a legacy theme to keep the colors mostly unchanged while working on
    the new palettes.
    
    ## How I Tested These Changes

---
+++ js_modules/dagster-ui/packages/ui-components/src/palettes/LegacyPalette.tsx
@@ -0,0 +1,65 @@
+import {LegacyColors} from '../components/Colors';
+
+import {ColorName} from './ColorName';
+
+export const LegacyPalette = {
+  [ColorName.KeylineDefault]: LegacyColors.KeylineGray,
+  [ColorName.LinkDefault]: LegacyColors.Link,
+  [ColorName.LinkHover]: LegacyColors.Link,
+  [ColorName.LinkDisabled]: LegacyColors.Link,
+  [ColorName.TextDefault]: LegacyColors.Dark,
+  [ColorName.TextLight]: LegacyColors.Gray500,
+  [ColorName.TextLighter]: LegacyColors.Gray400,
+  [ColorName.TextDisabled]: LegacyColors.Gray300,
+  [ColorName.TextRed]: LegacyColors.Red700,
+  [ColorName.TextYellow]: LegacyColors.Yellow700,
+  [ColorName.TextGreen]: LegacyColors.Green700,
+  [ColorName.TextBlue]: LegacyColors.Blue700,
+  [ColorName.TextOlive]: LegacyColors.Olive700,
+  [ColorName.TextCyan]: LegacyColors.Blue700, // No cyan in Legacy
+  [ColorName.TextLime]: LegacyColors.HighlightGreen, // No lime in Legacy
+  [ColorName.BackgroundDefault]: LegacyColors.White,
+  [ColorName.BackgroundDefaultHover]: LegacyColors.Gray50,
+  [ColorName.BackgroundLight]: LegacyColors.Gray50,
+  [ColorName.BackgroundLightHover]: LegacyColors.Gray100,
+  [ColorName.BackgroundLighter]: LegacyColors.Gray10,
+  [ColorName.BackgroundLighterHover]: LegacyColors.Gray50,
+  [ColorName.BackgroundDisabled]: LegacyColors.Gray100,
+  [ColorName.BackgroundRed]: LegacyColors.Red50,
+  [ColorName.BackgroundRedHover]: LegacyColors.Red50,
+  [ColorName.BackgroundYellow]: LegacyColors.Yellow50,
+  [ColorName.BackgroundYellowHover]: LegacyColors.Yellow50,
+  [ColorName.BackgroundGreen]: LegacyColors.Green50,
+  [ColorName.BackgroundGreenHover]: LegacyColors.Green50,
+  [ColorName.BackgroundBlue]: LegacyColors.Blue50,
+  [ColorName.BackgroundBlueHover]: LegacyColors.Blue50,
+  [ColorName.BackgroundOlive]: LegacyColors.Olive50,
+  [ColorName.BackgroundOliverHover]: LegacyColors.Olive50,
+  [ColorName.BackgroundCyan]: LegacyColors.Blue50, // No cyan
+  [ColorName.BackgroundCyanHover]: LegacyColors.Blue50, // No cyan
+  [ColorName.BackgroundLime]: LegacyColors.Green50, // No lime
+  [ColorName.BackgroundLimeHover]: LegacyColors.Green50, // No lime
+  [ColorName.BackgroundGray]: LegacyColors.Gray50,
+  [ColorName.BackgroundGrayHover]: LegacyColors.Gray50,
+  [ColorName.BorderDefault]: LegacyColors.Gray300,
+  [ColorName.BorderHover]: LegacyColors.Gray200,
+  [ColorName.BorderDisabled]: LegacyColors.Gray100,
+  [ColorName.AccentPrimary]: LegacyColors.Gray900,
+  [ColorName.AccentPrimaryHover]: LegacyColors.Gray800,
+  [ColorName.AccentReversed]: LegacyColors.White,
+  [ColorName.AccentReversedHover]: LegacyColors.White,
+  [ColorName.AccentRed]: LegacyColors.Red500,
+  [ColorName.AccentRedHover]: LegacyColors.Red500,
+  [ColorName.AccentYellow]: LegacyColors.Yellow500,
+  [ColorName.AccentYellowHover]: LegacyColors.Yellow500,
+  [ColorName.AccentGreen]: LegacyColors.Green500,
+  [ColorName.AccentGreenHover]: LegacyColors.Green500,
+  [ColorName.AccentBlue]: LegacyColors.Blue500,
+  [ColorName.AccentBlueHover]: LegacyColors.Blue500,
+  [ColorName.AccentCyan]: LegacyColors.Blue200, // No cyan
+  [ColorName.AccentCyanHover]: LegacyColors.Blue200, // No cyan
+  [ColorName.AccentLime]: LegacyColors.HighlightGreen, // No lime
+  [ColorName.AccentLimeHover]: LegacyColors.HighlightGreen, // No lime
+  [ColorName.AccentGray]: LegacyColors.Gray500,
+  [ColorName.AccentGrayHover]: LegacyColors.Gray400,
+};
