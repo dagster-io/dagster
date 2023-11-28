7be90db277f45f07ea6adf5f8813ed6f4f7aa5b5
Erin Cochran <erin.k.cochran@gmail.com>
2023-11-28 12:23:07 -0500

    Run format

--- examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/partitioned_asset.py
+++ examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/partitioned_asset.py
@@ -1,13 +1,19 @@
 import urllib.request
+import os
+
+# Create a new 'nasa' directory if needed
+dir_name = "nasa"
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
 
 from dagster import AssetExecutionContext, DailyPartitionsDefinition, asset
 
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"))
+@asset(partitions_def=DailyPartitionsDefinition(start_date="2023-10-01"))
 def my_daily_partitioned_asset(context: AssetExecutionContext) -> None:
     partition_date_str = context.asset_partition_key_for_output()
 
-    url = f"coolweatherwebsite.com/weather_obs&date={partition_date_str}"
-    target_location = f"weather_observations/{partition_date_str}.csv"
+    url = f"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={partition_date_str}"
+    target_location = f"nasa/{partition_date_str}.csv"
 
     urllib.request.urlretrieve(url, target_location)
