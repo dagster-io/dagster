b006b631385f9605552cea2216d5e1563a6a3f24
Ben Gotow <ben@foundry376.com>
2023-11-29 15:42:56 -0600

    [ui] Clean up RunRow, move to a separate file (#18364)
    ## Summary & Motivation
    
    This PR doesn't contain any logic changes, just wanted to clean up a few
    things before I work on #17035:
    
    - Moves RunRow out to it's own file for consistency with our other
    tables (SensorRow.tsx, AssetRow.tsx etc) Keep trying to get to it via
    Cmd-P
    
    - Moves all the technical bits of tag pinning out of the run row into a
    hook that hides the logic
    
    ## How I Tested These Changes
    
    CI still happy 🙏
    
    Co-authored-by: bengotow <bgotow@elementl.com>

---
+++ js_modules/dagster-ui/packages/ui-core/src/runs/useTagPinning.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import {useStateWithStorage} from '../hooks/useStateWithStorage';
+
+import {DagsterTag} from './RunTag';
+
+export function useTagPinning() {
+  // Most system tags are unpinned by default so we track pinned for these.
+  const [pinnedSystemTags, setPinnedSystemTags] = useStateWithStorage(
+    'pinned-system-tags',
+    validateIsArray,
+  );
+
+  // All user tags are pinned by default so we track unpinned.
+  const [unpinnedTags, setUnpinnedTags] = useStateWithStorage('unpinned-tags', validateIsArray);
+
+  const onToggleTagPin = React.useCallback(
+    (tagKey: string) => {
+      if (isUnpinnedByDefaultSystemTag(tagKey)) {
+        setPinnedSystemTags((pinnedSystemTags) => toggleTag(pinnedSystemTags, tagKey));
+      } else {
+        setUnpinnedTags((unpinnedTags) => toggleTag(unpinnedTags, tagKey));
+      }
+    },
+    [setUnpinnedTags, setPinnedSystemTags],
+  );
+
+  const isTagPinned = React.useCallback(
+    (tag: {key: string}) => {
+      return isUnpinnedByDefaultSystemTag(tag.key)
+        ? pinnedSystemTags.indexOf(tag.key) !== -1
+        : unpinnedTags.indexOf(tag.key) === -1;
+    },
+    [pinnedSystemTags, unpinnedTags],
+  );
+
+  return {
+    isTagPinned,
+    onToggleTagPin,
+  };
+}
+
+function validateIsArray(value: any) {
+  if (Array.isArray(value)) {
+    return value;
+  }
+  return [];
+}
+
+function isUnpinnedByDefaultSystemTag(key: string) {
+  return (
+    (key.startsWith(DagsterTag.Namespace) &&
+      key !== DagsterTag.Partition &&
+      key !== DagsterTag.Backfill) ||
+    key === 'mode'
+  );
+}
+
+function toggleTag(tagsArr: string[] | undefined, tagKey: string): string[] {
+  const tags = tagsArr || [];
+  if (tags.indexOf(tagKey) !== -1) {
+    return tags.filter((key) => key !== tagKey);
+  } else {
+    return [...tags, tagKey];
+  }
+}
