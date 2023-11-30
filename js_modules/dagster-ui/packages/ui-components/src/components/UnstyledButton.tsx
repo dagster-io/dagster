affdf2227ba9034a39ea67a44b4e9a7fc7956a86
Josh Braun <josh.braun.4@gmail.com>
2023-11-30 10:45:54 -0500

    legacy theme updates

--- js_modules/dagster-ui/packages/ui-components/src/components/UnstyledButton.tsx
+++ js_modules/dagster-ui/packages/ui-components/src/components/UnstyledButton.tsx
@@ -1,6 +1,6 @@
 import styled, {css} from 'styled-components';
 
-import {colorTextDefault} from '../theme/color';
+import {colorTextDefault, colorBorderFocused} from '../theme/color';
 
 interface Props {
   $expandedClickPx?: number;
@@ -33,7 +33,8 @@ export const UnstyledButton = styled.button<Props>`
   :active {
     outline: none;
     ${({$showFocusOutline}) =>
-      $showFocusOutline ? `box-shadow: rgba(58, 151, 212, 0.6) 0 0 0 3px;` : null}
+      $showFocusOutline ? `box-shadow: ${colorBorderFocused()} 0 0 0 2px;
+      ` : null}
   }
 
   &:disabled {
