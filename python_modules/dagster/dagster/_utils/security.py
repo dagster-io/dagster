5817c35fa04b5bda079fbcfa7925e3f34b990b59
Josh lloyd <josh.lloyd@acquia.com>
2023-12-01 09:37:27 -0700

    fixed input usage

--- python_modules/dagster/dagster/_utils/security.py
+++ python_modules/dagster/dagster/_utils/security.py
@@ -6,6 +6,6 @@ def non_secure_md5(s):
     """Drop in replacement md5 hash function marking it for a non-security purpose."""
     # check python version
     if sys.version_info[0] <= 3 and sys.version_info[1] <= 8:
-        return hashlib.md5(s.encode("utf-8"))
+        return hashlib.md5(s)
     else:
-        return hashlib.md5(s.encode("utf-8"), usedforsecurity=False)
+        return hashlib.md5(s, usedforsecurity=False)
