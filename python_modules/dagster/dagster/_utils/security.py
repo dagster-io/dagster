4139f51e8a832bb2437e1da7fda75a649ba2047e
Josh lloyd <josh.lloyd@acquia.com>
2023-12-01 10:26:48 -0700

    added type hints

--- python_modules/dagster/dagster/_utils/security.py
+++ python_modules/dagster/dagster/_utils/security.py
@@ -1,8 +1,9 @@
 import hashlib
 import sys
+from typing import Union
 
 
-def non_secure_md5(s):
+def non_secure_md5(s: Union[bytes, bytearray, memoryview]) -> hashlib._Hash:
     """Drop in replacement md5 hash function marking it for a non-security purpose."""
     # check python version
     if sys.version_info[0] <= 3 and sys.version_info[1] <= 8:
