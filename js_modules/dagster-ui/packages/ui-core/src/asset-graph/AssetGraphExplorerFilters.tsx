e8f8ddda21600bb9f27a014df790805bcbc77a70
Marco polo <salazarm@users.noreply.github.com>
2023-12-05 09:53:57 -0500

    [2/n][Asset graph] Filter bar (#18443)
    ## Summary & Motivation
    
    Adds the active filters bar in the asset graph.
    
    I added some custom logic to make it so that if 0 code locations are
    selected then we actually make all of them visible but we don't tick all
    of the checkboxes so that the user can still go from 0 -> 1 selected
    
    
    ## How I Tested These Changes
    <img width="1680" alt="Screenshot 2023-12-01 at 1 36 30 PM"
    src="https://github.com/dagster-io/dagster/assets/2286579/7967190b-0a02-43ff-8d70-10c4549d5899">

--- js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetGraphExplorerFilters.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetGraphExplorerFilters.tsx
@@ -9,106 +9,121 @@ import {useStaticSetFilter} from '../ui/Filters/useStaticSetFilter';
 import {DagsterRepoOption, WorkspaceContext} from '../workspace/WorkspaceContext';
 import {buildRepoAddress, buildRepoPathForHuman} from '../workspace/buildRepoAddress';
 
-export const AssetGraphExplorerFilters = React.memo(
-  ({
-    assetGroups,
-    visibleAssetGroups,
-    setGroupFilters,
-  }:
-    | {
-        assetGroups: AssetGroupSelector[];
-        visibleAssetGroups: AssetGroupSelector[];
-        setGroupFilters: (groups: AssetGroupSelector[]) => void;
+const emptySet = new Set<any>();
+
+export function useAssetGraphExplorerFilters({
+  assetGroups,
+  visibleAssetGroups,
+  setGroupFilters,
+}:
+  | {
+      assetGroups: AssetGroupSelector[];
+      visibleAssetGroups: AssetGroupSelector[];
+      setGroupFilters: (groups: AssetGroupSelector[]) => void;
+    }
+  | {assetGroups?: null; setGroupFilters?: null; visibleAssetGroups?: null}) {
+  const {allRepos, visibleRepos, toggleVisible, setVisible} = React.useContext(WorkspaceContext);
+
+  const visibleReposSet = React.useMemo(() => new Set(visibleRepos), [visibleRepos]);
+
+  const reposFilter = useStaticSetFilter<DagsterRepoOption>({
+    name: 'Code location',
+    icon: 'repo',
+    allValues: allRepos.map((repo) => ({
+      key: repo.repository.id,
+      value: repo,
+      match: [buildRepoPathForHuman(repo.repository.name, repo.repositoryLocation.name)],
+    })),
+    menuWidth: '300px',
+    renderLabel: ({value}) => (
+      <Box flex={{direction: 'row', gap: 4, alignItems: 'center'}}>
+        <Icon name="repo" />
+        <TruncatedTextWithFullTextOnHover
+          text={buildRepoPathForHuman(value.repository.name, value.repositoryLocation.name)}
+        />
+      </Box>
+    ),
+    getStringValue: (value) =>
+      buildRepoPathForHuman(value.repository.name, value.repositoryLocation.name),
+    initialState: visibleReposSet.size === allRepos.length ? emptySet : visibleReposSet,
+    onStateChanged: (values) => {
+      let areAllVisible = false;
+      if (values.size === 0) {
+        areAllVisible = true;
       }
-    | {assetGroups?: null; setGroupFilters?: null; visibleAssetGroups?: null}) => {
-    const {allRepos, visibleRepos, toggleVisible} = React.useContext(WorkspaceContext);
-
-    const visibleReposSet = React.useMemo(() => new Set(visibleRepos), [visibleRepos]);
-
-    const reposFilter = useStaticSetFilter<DagsterRepoOption>({
-      name: 'Code location',
-      icon: 'repo',
-      allValues: allRepos.map((repo) => ({
-        key: repo.repository.id,
-        value: repo,
-        match: [buildRepoPathForHuman(repo.repository.name, repo.repositoryLocation.name)],
-      })),
-      menuWidth: '300px',
-      renderLabel: ({value}) => (
-        <Box flex={{direction: 'row', gap: 4, alignItems: 'center'}}>
-          <Icon name="repo" />
-          <TruncatedTextWithFullTextOnHover
-            text={buildRepoPathForHuman(value.repository.name, value.repositoryLocation.name)}
-          />
-        </Box>
-      ),
-      getStringValue: (value) =>
-        buildRepoPathForHuman(value.repository.name, value.repositoryLocation.name),
-      initialState: visibleReposSet,
-      onStateChanged: (values) => {
-        allRepos.forEach((repo) => {
-          if (visibleReposSet.has(repo) !== values.has(repo)) {
-            toggleVisible([buildRepoAddress(repo.repository.name, repo.repositoryLocation.name)]);
-          }
-        });
-      },
-    });
-
-    const groupsFilter = useStaticSetFilter<AssetGroupSelector>({
-      name: 'Asset Groups',
-      icon: 'asset_group',
-      allValues: (assetGroups || []).map((group) => ({
-        key: group.groupName,
-        value:
-          visibleAssetGroups?.find(
-            (visibleGroup) =>
-              visibleGroup.groupName === group.groupName &&
-              visibleGroup.repositoryName === group.repositoryName &&
-              visibleGroup.repositoryLocationName === group.repositoryLocationName,
-          ) ?? group,
-        match: [group.groupName],
-      })),
-      menuWidth: '300px',
-      renderLabel: ({value}) => (
-        <Box flex={{direction: 'row', gap: 4, alignItems: 'center'}}>
-          <Icon name="repo" />
-          <TruncatedTextWithFullTextOnHover
-            tooltipText={
-              value.groupName +
-              ' - ' +
-              buildRepoPathForHuman(value.repositoryName, value.repositoryLocationName)
-            }
-            text={
-              <>
-                {value.groupName}
-                <span style={{opacity: 0.5, paddingLeft: '4px'}}>
-                  {buildRepoPathForHuman(value.repositoryName, value.repositoryLocationName)}
-                </span>
-              </>
-            }
-          />
-        </Box>
-      ),
-      getStringValue: (group) => group.groupName,
-      initialState: React.useMemo(() => new Set(visibleAssetGroups ?? []), [visibleAssetGroups]),
-      onStateChanged: (values) => {
-        if (setGroupFilters) {
-          setGroupFilters(Array.from(values));
+      allRepos.forEach((repo) => {
+        const address = buildRepoAddress(repo.repository.name, repo.repositoryLocation.name);
+        if (areAllVisible) {
+          setVisible([address]);
+        } else if (visibleReposSet.has(repo) !== values.has(repo)) {
+          toggleVisible([address]);
         }
-      },
-    });
+      });
+    },
+  });
 
-    const filters: FilterObject[] = [];
-    if (allRepos.length > 1) {
-      filters.push(reposFilter);
-    }
-    if (assetGroups) {
-      filters.push(groupsFilter);
-    }
-    const {button} = useFilters({filters});
-    if (allRepos.length <= 1 && !assetGroups) {
-      return null;
-    }
-    return button;
-  },
-);
+  const groupsFilter = useStaticSetFilter<AssetGroupSelector>({
+    name: 'Asset Groups',
+    icon: 'asset_group',
+    allValues: (assetGroups || []).map((group) => ({
+      key: group.groupName,
+      value:
+        visibleAssetGroups?.find(
+          (visibleGroup) =>
+            visibleGroup.groupName === group.groupName &&
+            visibleGroup.repositoryName === group.repositoryName &&
+            visibleGroup.repositoryLocationName === group.repositoryLocationName,
+        ) ?? group,
+      match: [group.groupName],
+    })),
+    menuWidth: '300px',
+    renderLabel: ({value}) => (
+      <Box flex={{direction: 'row', gap: 4, alignItems: 'center'}}>
+        <Icon name="repo" />
+        <TruncatedTextWithFullTextOnHover
+          tooltipText={
+            value.groupName +
+            ' - ' +
+            buildRepoPathForHuman(value.repositoryName, value.repositoryLocationName)
+          }
+          text={
+            <>
+              {value.groupName}
+              <span style={{opacity: 0.5, paddingLeft: '4px'}}>
+                {buildRepoPathForHuman(value.repositoryName, value.repositoryLocationName)}
+              </span>
+            </>
+          }
+        />
+      </Box>
+    ),
+    getStringValue: (group) => group.groupName,
+    initialState: React.useMemo(() => new Set(visibleAssetGroups ?? []), [visibleAssetGroups]),
+    onStateChanged: (values) => {
+      if (setGroupFilters) {
+        setGroupFilters(Array.from(values));
+      }
+    },
+  });
+
+  const filters: FilterObject[] = [];
+  if (allRepos.length > 1) {
+    filters.push(reposFilter);
+  }
+  if (assetGroups) {
+    filters.push(groupsFilter);
+  }
+  const {button, activeFiltersJsx} = useFilters({filters});
+  if (allRepos.length <= 1 && !assetGroups) {
+    return {button: null, activeFiltersJsx: null};
+  }
+  return {
+    button,
+    filterBar: activeFiltersJsx.length ? (
+      <Box padding={12} flex={{gap: 12}}>
+        {' '}
+        {activeFiltersJsx}
+      </Box>
+    ) : null,
+  };
+}
