4c8d05f5d748fc780f6352c851df8ba775a3a75b
Claire Lin <claire@dagsterlabs.com>
2023-12-12 10:02:14 +0800

    switch default to 3

--- python_modules/dagster/dagster/_core/execution/asset_backfill.py
+++ python_modules/dagster/dagster/_core/execution/asset_backfill.py
@@ -1250,7 +1250,7 @@ def execute_asset_backfill_iteration_inner(
         next_latest_storage_id = instance_queryer.instance.event_log_storage.get_maximum_record_id()
     else:
         next_latest_storage_id = instance_queryer.instance.event_log_storage.get_maximum_record_id()
-        cursor_delay_time = int(os.getenv("ASSET_BACKFILL_CURSOR_DELAY_TIME", "0"))
+        cursor_delay_time = int(os.getenv("ASSET_BACKFILL_CURSOR_DELAY_TIME", "3"))
         # Events are not guaranteed to be written to the event log in monotonic increasing order,
         # so we wait to ensure all events up until next_latest_storage_id have been written.
         time.sleep(cursor_delay_time)
