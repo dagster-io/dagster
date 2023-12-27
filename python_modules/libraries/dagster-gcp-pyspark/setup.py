4f670d7a8da27da9fbf795d31bd8a53b8272b9b3
Johann Miller <johann@elementl.com>
2023-12-26 23:31:22 -0500

    Change dagster_webserver to dagster-webserver
    [NO_SKIP]

--- python_modules/libraries/dagster-gcp-pyspark/setup.py
+++ python_modules/libraries/dagster-gcp-pyspark/setup.py
@@ -16,7 +16,7 @@ ver = get_version()
 # dont pin dev installs to avoid pip dep resolver issues
 pin = "" if ver == "1!0+dev" else f"=={ver}"
 setup(
-    name="dagster_gcp_pyspark",
+    name="dagster-gcp-pyspark",
     version=get_version(),
     author="Dagster Labs",
     author_email="hello@dagsterlabs.com",
