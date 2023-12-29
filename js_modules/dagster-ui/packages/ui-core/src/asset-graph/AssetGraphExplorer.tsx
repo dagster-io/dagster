d0bdee665567bf7ab92a5e578d73d8eab34d23bd
Marco Salazar <marco@elementl.com>
2023-12-29 10:08:42 -0500

    .

--- js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetGraphExplorer.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetGraphExplorer.tsx
@@ -407,7 +407,7 @@ const AssetGraphExplorerWithData = ({
   const allowGroupsOnlyZoomLevel =
     !flagDAGSidebar && !!(layout && Object.keys(layout.groups).length);
 
-  const [showSidebar, setShowSidebar] = React.useState(allGroups.length > 1);
+  const [showSidebar, setShowSidebar] = React.useState(isGlobalGraph);
 
   const toggleGroupsButton = flagDAGSidebar && allGroups.length > 1 && (
     <ShortcutHandler
