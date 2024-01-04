604eea40a66b88d957399a3c873f3d7bac44d7ba
JamieDeMaria <jamie@dagsterlabs.com>
2024-01-03 16:09:23 -0800

    test cleanup

--- python_modules/dagster/dagster_tests/storage_tests/test_no_io_manager.py
+++ python_modules/dagster/dagster_tests/storage_tests/test_no_io_manager.py
@@ -67,27 +67,10 @@ def test_downstream_deps_with_type_annotation():
     assert not io_mgr.loaded_input
 
 
-def test_downstream_managed_deps():
-    @asset
-    def returns_none():
-        return None
-
-    @asset
-    def downstream(returns_none):
-        assert returns_none == 1
-
-    io_mgr = TestIOManager(return_value=1)
-
-    materialize([returns_none, downstream], resources={"io_manager": io_mgr})
-
-    assert io_mgr.handled_output
-    assert io_mgr.loaded_input
-
-
 def test_downstream_managed_deps_with_type_annotation():
     # Tests that the return type None annotation does not use the I/O manager, but you can still
     # use an I/O manager to load it as a downstream input if the I/O manager is set up to handle that
-    # case, i.e. a third party writes data to the place the I/O manager expects it to exist
+    # case, for example a third party writes data to the place the I/O manager expects it to exist
     @asset
     def returns_none() -> None:
         return None
@@ -104,31 +87,10 @@ def test_downstream_managed_deps_with_type_annotation():
     assert io_mgr.loaded_input
 
 
-def test_ops_no_type_annotation():
-    @op
-    def returns_none():
-        return None
-
-    @op
-    def asserts_none(x):
-        assert x == 1
-
-    @job
-    def return_none_job():
-        asserts_none(returns_none())
-
-    io_mgr = TestIOManager(return_value=1)
-
-    result = return_none_job.execute_in_process(resources={"io_manager": io_mgr})
-    assert result.success
-    assert io_mgr.handled_output
-    assert io_mgr.loaded_input
-
-
 def test_ops_with_type_annotation():
     # Tests that the return type None annotation does not use the I/O manager, but you can still
     # use an I/O manager to load it as a downstream input if the I/O manager is set up to handle that
-    # case, i.e. a third party writes data to the place the I/O manager expects it to exist
+    # case, for example a third party writes data to the place the I/O manager expects it to exist
     @op
     def returns_none() -> None:
         return None
