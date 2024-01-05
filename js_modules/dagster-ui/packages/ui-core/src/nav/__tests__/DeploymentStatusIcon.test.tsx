f788fdb77e7c08c2fa38ed2fe358abf277e4f478
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2024-01-05 10:31:01 -0600

    [ui] Clean up AppTopNav.test (#19034)
    ## Summary & Motivation
    
    Break apart AppTopNav.test, which is doing more work than necessary and
    is a remaining `TestProvider` callsite.
    
    ## How I Tested These Changes
    
    yarn jest

---
+++ js_modules/dagster-ui/packages/ui-core/src/nav/__tests__/DeploymentStatusIcon.test.tsx
@@ -0,0 +1,79 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as React from 'react';
+
+import {DeploymentStatus, DeploymentStatusContext} from '../../instance/DeploymentStatusProvider';
+import {DeploymentStatusIcon} from '../DeploymentStatusIcon';
+
+describe('DeploymentStatusIcon', () => {
+  it('does not show warning icon when no loading state or warnings', async () => {
+    const value = {codeLocations: null, daemons: null};
+    render(
+      <DeploymentStatusContext.Provider value={value}>
+        <DeploymentStatusIcon />
+      </DeploymentStatusContext.Provider>,
+    );
+
+    expect(await screen.findByText(/no errors/i)).toBeInTheDocument();
+  });
+
+  it('shows the spinner when repo locations are loading', async () => {
+    const event = userEvent.setup();
+    const value: DeploymentStatus = {
+      codeLocations: {type: 'spinner', content: 'Wait patiently!'},
+      daemons: null,
+    };
+
+    render(
+      <DeploymentStatusContext.Provider value={value}>
+        <DeploymentStatusIcon />
+      </DeploymentStatusContext.Provider>,
+    );
+
+    const spinner = await screen.findByTitle(/loading/i);
+    expect(spinner).toBeVisible();
+
+    await event.hover(spinner);
+    expect(await screen.findByText(/wait patiently/i)).toBeVisible();
+  });
+
+  it('shows the error message when repo location warnings are found', async () => {
+    const event = userEvent.setup();
+    const value: DeploymentStatus = {
+      codeLocations: {type: 'warning', content: 'Oh no!'},
+      daemons: null,
+    };
+
+    render(
+      <DeploymentStatusContext.Provider value={value}>
+        <DeploymentStatusIcon />
+      </DeploymentStatusContext.Provider>,
+    );
+
+    const warningIcon = await screen.findByRole('img', {name: /warning/i});
+    expect(warningIcon).toBeVisible();
+
+    await event.hover(warningIcon);
+    expect(await screen.findByText(/oh no/i)).toBeVisible();
+  });
+
+  it('shows the error message when daemon warnings are found', async () => {
+    const event = userEvent.setup();
+    const value: DeploymentStatus = {
+      codeLocations: null,
+      daemons: {type: 'warning', content: 'Daemon problem!'},
+    };
+
+    render(
+      <DeploymentStatusContext.Provider value={value}>
+        <DeploymentStatusIcon />
+      </DeploymentStatusContext.Provider>,
+    );
+
+    const warningIcon = await screen.findByRole('img', {name: /warning/i});
+    expect(warningIcon).toBeVisible();
+
+    await event.hover(warningIcon);
+    expect(await screen.findByText(/daemon problem/i)).toBeVisible();
+  });
+});
