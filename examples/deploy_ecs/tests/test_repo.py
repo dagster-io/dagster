fb23b39a72ac2e6b5063d018dc06d4770a786ccf
Maxime Armstrong <maxime@retain.ai>
2024-01-05 12:45:49 -0500

    Update tests

--- examples/deploy_ecs/tests/test_repo.py
+++ examples/deploy_ecs/tests/test_repo.py
@@ -1,5 +1,11 @@
-from ..defs import my_job
+from ..defs import my_job, defs
 
 
-def test_repo():
-    assert my_job.execute_in_process().output_for_node("my_op")
+def test_defs():
+    # The job name corresponds to the name of the function defining the job.
+    # In our example, the job is created from a graph,
+    # which is defined by the function `my_graph()`,
+    # therefore the name of the job is `my_graph`.
+    test_job = defs.get_job_def("my_graph")
+    assert test_job == my_job
+    assert test_job.execute_in_process().output_for_node("my_op")
