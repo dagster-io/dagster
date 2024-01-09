751faaadfceca7b841b9983b47eb00beded5a13a
Marco Salazar <marco@elementl.com>
2024-01-09 15:46:35 -0500

    feedback

--- js_modules/dagster-ui/packages/ui-core/src/app/__tests__/formatElapsedTime.test.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/__tests__/formatElapsedTime.test.tsx
@@ -1,5 +1,9 @@
 import {formatElapsedTimeWithoutMsec, formatElapsedTimeWithMsec} from '../Util';
 
+const SECOND = 1 * 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+
 describe('Elapsed time formatters', () => {
   describe('formatElapsedTimeWithoutMsec', () => {
     it('formats times between -1 and 1 seconds', () => {
@@ -24,12 +28,19 @@ describe('Elapsed time formatters', () => {
 
   describe('formatElapsedTimeWithMsec', () => {
     it('formats times', () => {
-      expect(formatElapsedTimeWithMsec(50)).toBe('0:00:00.050');
-      expect(formatElapsedTimeWithMsec(50000)).toBe('0:00:50.000');
-      expect(formatElapsedTimeWithMsec(500000)).toBe('0:08:20.000');
-      expect(formatElapsedTimeWithMsec(-500000)).toBe('-0:08:20.000');
+      expect(formatElapsedTimeWithMsec(50)).toBe('0.050');
+      expect(formatElapsedTimeWithMsec(50000)).toBe('50.000');
+      expect(formatElapsedTimeWithMsec(500000)).toBe('8:20.000');
+      expect(formatElapsedTimeWithMsec(-500000)).toBe('-8:20.000');
       expect(formatElapsedTimeWithMsec(363599999)).toBe('100:59:59.999');
       expect(formatElapsedTimeWithMsec(-363599999)).toBe('-100:59:59.999');
+
+      expect(formatElapsedTimeWithMsec(HOUR + MINUTE + SECOND + 69)).toBe('1:01:01.069');
+      expect(formatElapsedTimeWithMsec(HOUR + SECOND + 69)).toBe('1:00:01.069');
+      expect(formatElapsedTimeWithMsec(HOUR + 69)).toBe('1:00:00.069');
+      expect(formatElapsedTimeWithMsec(MINUTE + 69)).toBe('1:00.069');
+      expect(formatElapsedTimeWithMsec(SECOND + 69)).toBe('1.069');
+      expect(formatElapsedTimeWithMsec(69)).toBe('0.069');
     });
   });
 
@@ -53,8 +64,8 @@ describe('Elapsed time formatters', () => {
     });
 
     it('handles decimal correctly in `formatElapsedTimeWithMsec`', () => {
-      expect(formatElapsedTimeWithMsec(500000)).toBe('0:08:20,000');
-      expect(formatElapsedTimeWithMsec(-500000)).toBe('-0:08:20,000');
+      expect(formatElapsedTimeWithMsec(500000)).toBe('8:20,000');
+      expect(formatElapsedTimeWithMsec(-500000)).toBe('-8:20,000');
       expect(formatElapsedTimeWithMsec(363599999)).toBe('100:59:59,999');
     });
   });
