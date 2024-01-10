95135739bc684cc29fa87bc105f556393126975f
Marco Salazar <marco@elementl.com>
2024-01-10 09:24:10 -0500

    test var names

--- js_modules/dagster-ui/packages/ui-core/src/instigation/__tests__/util.test.ts
+++ js_modules/dagster-ui/packages/ui-core/src/instigation/__tests__/util.test.ts
@@ -5,17 +5,17 @@ const DAY = 1000 * 60 * 60 * 24;
 
 describe('isStuckStarted', () => {
   it('identifies stuck started ticks', () => {
-    const startedTickTodayStarted = buildInstigationTick({
+    const todayTickStarted = buildInstigationTick({
       status: InstigationTickStatus.STARTED,
       timestamp: Date.now(),
       endTimestamp: null,
     });
 
     // First index so can't tell if its stuck
-    expect(isStuckStartedTick(startedTickTodayStarted, 0)).toBe(false);
+    expect(isStuckStartedTick(todayTickStarted, 0)).toBe(false);
 
     // Second index so definitely stuck
-    expect(isStuckStartedTick(startedTickTodayStarted, 1)).toBe(true);
+    expect(isStuckStartedTick(todayTickStarted, 1)).toBe(true);
 
     const overThreeDaysOldTickStarted = buildInstigationTick({
       status: InstigationTickStatus.STARTED,
@@ -26,13 +26,13 @@ describe('isStuckStarted', () => {
     // Over three days old so probably stuck
     expect(isStuckStartedTick(overThreeDaysOldTickStarted, 0)).toBe(true);
 
-    const startedTickTodayFailure = buildInstigationTick({
+    const todayTickFailure = buildInstigationTick({
       status: InstigationTickStatus.FAILURE,
       timestamp: Date.now() / 1000,
       endTimestamp: Date.now() / 1000,
     });
 
-    expect(isStuckStartedTick(startedTickTodayFailure, 0)).toBe(false);
-    expect(isStuckStartedTick(startedTickTodayFailure, 1)).toBe(false);
+    expect(isStuckStartedTick(todayTickFailure, 0)).toBe(false);
+    expect(isStuckStartedTick(todayTickFailure, 1)).toBe(false);
   });
 });
