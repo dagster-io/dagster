ef258b05862739bc113f76f0d3bd7eafdabe83e1
Erin Cochran <erin.k.cochran@gmail.com>
2024-01-11 15:01:51 -0500

    Fix test for ruff

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/partitions_schedules_sensors_tests/test_schedules.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/partitions_schedules_sensors_tests/test_schedules.py
@@ -1,15 +1,15 @@
 from dagster import ScheduleDefinition
 from docs_snippets.concepts.partitions_schedules_sensors.schedules.schedules import (
-    ecommerce_schedule,
     configurable_job_schedule,
+    ecommerce_schedule,
     my_timezone_schedule,
 )
 
 
 def test_schedule_definitions():
     for schedule in [
-        ecommerce_schedule,
         configurable_job_schedule,
+        ecommerce_schedule,
         my_timezone_schedule,
     ]:
         assert isinstance(schedule, ScheduleDefinition)
