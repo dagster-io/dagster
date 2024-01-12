7011430b25c933a3cc04ecc76815c0e3505b88d6
Erin Cochran <erin.k.cochran@gmail.com>
2024-01-12 14:46:59 -0500

    Fix import in example test

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/partitions_schedules_sensors_tests/test_schedules.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/partitions_schedules_sensors_tests/test_schedules.py
@@ -1,8 +1,10 @@
 from dagster import ScheduleDefinition
 from docs_snippets.concepts.partitions_schedules_sensors.schedules.schedules import (
     configurable_job_schedule,
-    ecommerce_schedule,
     my_timezone_schedule,
+)
+from docs_snippets.concepts.partitions_schedules_sensors.schedules.basic_asset_schedule import (
+    ecommerce_schedule,
 )
 
 
