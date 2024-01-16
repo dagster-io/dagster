c5db490fd341465256e612df89b6b12dedb302a5
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-01-16 14:58:27 -0800

    Move translator to its own file

--- python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/dagster_sling_translator.py
+++ python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/dagster_sling_translator.py
@@ -1,4 +1,5 @@
 import re
+from typing import Any, Mapping
 
 from dagster import AssetKey
 from dagster._annotations import public
@@ -31,11 +32,18 @@ class DagsterSlingTranslator:
 
     @public
     @classmethod
-    def get_asset_key(cls, stream_name: str, target_prefix: str = "target") -> AssetKey:
-        """A function that takes a stream name from a Sling replication config and returns a
+    def get_asset_key(
+        cls, stream_definition: Mapping[str, Any], target_prefix: str = "target"
+    ) -> AssetKey:
+        """A function that takes a stream definition from a Sling replication config and returns a
         Dagster AssetKey.
 
-        By default, this returns concatenates `target_` with the stream name. For example, a stream
+        The stream definition is a dictionary key/value pair where the key is the stream name and
+        the value is a dictionary representing the stream definition. For example:
+
+        stream_definition = {"public.users": {'sql': 'select all_user_id, name from public."all_Users"', 'object': 'public.all_users'}}
+
+        By default, this returns the target_prefix concatenated with the stream name. For example, a stream
         named "public.accounts" will create an AssetKey named "target_public_accounts".
 
         Override this function to customize how to map a Sling stream to a Dagster AssetKey.
@@ -51,8 +59,8 @@ class DagsterSlingTranslator:
                    asset_key: "mydb_users"
 
         Args:
-            stream_name (str): The name of the stream.
-            target_prefix (str): The prefix for the target stream.
+            stream_definition (Mapping[str, Any]): A dictionary representing the stream definition
+            target_prefix (str): The prefix to use when creating the AssetKey
 
         Returns:
             AssetKey: The Dagster AssetKey for the replication stream.
@@ -60,20 +68,29 @@ class DagsterSlingTranslator:
         Examples:
             Using a custom mapping for streams:
 
-            .. code-block:: python
-
-                class CustomSlingTranslator(DagsterSlingTranslator):
-                    @classmethod
-                    def get_asset_key(cls, stream_name: str) -> AssetKey:
-                        map = {"stream1": "asset1", "stream2": "asset2"}
-                        return AssetKey(map[stream_name])
+            class CustomSlingTranslator(DagsterSlingTranslator):
+                @classmethod
+                def get_asset_key_for_target(cls, stream_definition) -> AssetKey:
+                    map = {"stream1": "asset1", "stream2": "asset2"}
+                    return AssetKey(map[stream_name])
         """
+        meta = stream_definition.get("meta", {}).get("dagster", {})
+        asset_key = meta.get("asset_key")
+        if asset_key and cls.sanitize_stream_name(asset_key) != asset_key:
+            raise ValueError(
+                f"Asset key {asset_key} for stream {stream_definition['name']} is not "
+                "sanitized. Please use only alphanumeric characters and underscores."
+            )
+        elif asset_key:
+            return AssetKey(asset_key)
+
+        stream_name = next(iter(stream_definition.keys()))
         components = cls.sanitize_stream_name(stream_name).split(".")
         return AssetKey([target_prefix] + components)
 
     @public
     @classmethod
-    def get_deps_asset_key(cls, stream_name: str) -> AssetKey:
+    def get_deps_asset_key(cls, stream_definition: Mapping[str, Any]) -> AssetKey:
         """A function that takes a stream name from a Sling replication config and returns a
         Dagster AssetKey for the dependencies of the replication stream.
 
@@ -107,5 +124,16 @@ class DagsterSlingTranslator:
 
 
         """
+        meta = stream_definition.get("meta", {}).get("dagster", {})
+        asset_key = meta.get("deps")
+        if asset_key and cls.sanitize_stream_name(asset_key) != asset_key:
+            raise ValueError(
+                f"Asset key {asset_key} for stream {stream_definition['name']} is not "
+                "sanitized. Please use only alphanumeric characters and underscores."
+            )
+        elif asset_key:
+            return AssetKey(asset_key)
+
+        stream_name = next(iter(stream_definition.keys()))
         components = cls.sanitize_stream_name(stream_name).split(".")
         return AssetKey(components)
