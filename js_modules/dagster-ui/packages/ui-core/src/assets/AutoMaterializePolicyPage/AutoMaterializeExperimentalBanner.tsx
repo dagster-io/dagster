12149e032e24e3e507ddab83f77f7d5a9169da64
Marco Salazar <marco@elementl.com>
2024-01-17 16:50:42 -0500

    .

--- js_modules/dagster-ui/packages/ui-core/src/assets/AutoMaterializePolicyPage/AutoMaterializeExperimentalBanner.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/AutoMaterializePolicyPage/AutoMaterializeExperimentalBanner.tsx
@@ -1,4 +1,4 @@
-import {Alert, Icon, Tag, Tooltip, colorAccentBlue} from '@dagster-io/ui-components';
+import {Alert, Box, Icon, Tag, Tooltip, colorAccentBlue} from '@dagster-io/ui-components';
 import React from 'react';
 
 import {useStateWithStorage} from '../../hooks/useStateWithStorage';
