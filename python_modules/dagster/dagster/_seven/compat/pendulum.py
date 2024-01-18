5ea301fef2a29d02dc9cef77b67f910b46ebdc79
Daniel Gibson <daniel@elementl.com>
2024-01-18 16:54:24 -0600

    Add pendulum 3 support
    Summary:
    pendulum 3 didn't have any truly breaking changes so this is as simple as inverting our version check (which is probably how it should have worked originally). I think testing all three versions is warranted given how critical the package working correctly is for things like DST transitions and AMP working correctly.
    
    Test Plan: BK

--- python_modules/dagster/dagster/_seven/compat/pendulum.py
+++ python_modules/dagster/dagster/_seven/compat/pendulum.py
@@ -5,24 +5,29 @@ import packaging.version
 import pendulum
 from typing_extensions import TypeAlias
 
-_IS_PENDULUM_2 = (
+_IS_PENDULUM_1 = (
     hasattr(pendulum, "__version__")
-    and getattr(packaging.version.parse(getattr(pendulum, "__version__")), "major") == 2
+    and getattr(packaging.version.parse(getattr(pendulum, "__version__")), "major") == 1
+)
+
+_IS_PENDULUM_3 = (
+    hasattr(pendulum, "__version__")
+    and getattr(packaging.version.parse(getattr(pendulum, "__version__")), "major") == 3
 )
 
 
 @contextmanager
 def mock_pendulum_timezone(override_timezone):
-    if _IS_PENDULUM_2:
-        with pendulum.tz.test_local_timezone(pendulum.tz.timezone(override_timezone)):
+    if _IS_PENDULUM_1:
+        with pendulum.tz.LocalTimezone.test(pendulum.Timezone.load(override_timezone)):
             yield
     else:
-        with pendulum.tz.LocalTimezone.test(pendulum.Timezone.load(override_timezone)):
+        with pendulum.tz.test_local_timezone(pendulum.tz.timezone(override_timezone)):
             yield
 
 
 def create_pendulum_time(year, month, day, *args, **kwargs):
-    if "tz" in kwargs and "dst_rule" in kwargs and not _IS_PENDULUM_2:
+    if "tz" in kwargs and "dst_rule" in kwargs and _IS_PENDULUM_1:
         tz = pendulum.timezone(kwargs.pop("tz"))
         dst_rule = kwargs.pop("dst_rule")
 
@@ -40,14 +45,18 @@ def create_pendulum_time(year, month, day, *args, **kwargs):
         )
 
     return (
-        pendulum.datetime(year, month, day, *args, **kwargs)
-        if _IS_PENDULUM_2
-        else pendulum.create(year, month, day, *args, **kwargs)
+        pendulum.create(year, month, day, *args, **kwargs)
+        if _IS_PENDULUM_1
+        else pendulum.datetime(year, month, day, *args, **kwargs)
     )
 
 
 PendulumDateTime: TypeAlias = (
-    pendulum.DateTime if _IS_PENDULUM_2 else pendulum.Pendulum  # type: ignore[attr-defined]
+    pendulum.Pendulum if _IS_PENDULUM_1 else pendulum.DateTime  # type: ignore[attr-defined]
+)
+
+PendulumInterval: TypeAlias = (
+    pendulum.Interval if _IS_PENDULUM_3 else pendulum.Period  # type: ignore[attr-defined]
 )
 
 
