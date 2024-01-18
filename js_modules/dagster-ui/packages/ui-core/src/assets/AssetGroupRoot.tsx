dagster-io/dagster

commit ca81e1d36ab1a23f28e985d6338993cc873f6b6c

Replace 'automationPolicySensor' with 'targetingInstigators' on AssetNode (#19083)
Summary:
Generalize the logic for mapping from an asset to its AMP sensor to make
it include any sensor that either has a (non-implicit) job with that
asset's jobs in it, or an AMP sensor.

Test Plan: Bk

## Summary & Motivation

## How I Tested These Changes

js_modules/dagster-ui/packages/ui-core/src/assets/AssetGroupRoot.tsx
@@ -1,6 +1,5 @@
 import {gql, useQuery} from '@apollo/client';
 import {Page, PageHeader, Heading, Box, Tag, Tabs} from '@dagster-io/ui-components';
-import uniqBy from 'lodash/uniqBy';
 import * as React from 'react';
 import {useHistory, useParams} from 'react-router-dom';
 
@@ -10,14 +9,12 @@ import {AssetLocation} from '../asset-graph/useFindAssetLocation';
 import {AssetGroupSelector} from '../graphql/types';
 import {useDocumentTitle} from '../hooks/useDocumentTitle';
 import {RepositoryLink} from '../nav/RepositoryLink';
-import {ScheduleOrSensorTag} from '../nav/ScheduleOrSensorTag';
 import {
   ExplorerPath,
   explorerPathFromString,
   explorerPathToString,
 } from '../pipelines/PipelinePathUtils';
 import {SENSOR_SWITCH_FRAGMENT} from '../sensors/SensorSwitch';
-import {SensorSwitchFragment} from '../sensors/types/SensorSwitch.types';
 import {TabLink} from '../ui/TabLink';
 import {ReloadAllButton} from '../workspace/ReloadAllButton';
 import {RepoAddress} from '../workspace/types';
@@ -135,10 +132,6 @@ export const ASSET_GROUP_METADATA_QUERY = gql`
       autoMaterializePolicy {
         policyType
       }
-      automationPolicySensor {
-        id
-        ...SensorSwitchFragment
-      }
     }
   }
 
@@ -169,17 +162,6 @@ export const AssetGroupTags = ({
       assetNodes.some((a) => !!a.autoMaterializePolicy)
     ) {
       return <AutomaterializeDaemonStatusTag />;
-    }
-
-    if (automaterializeSensorsFlagState === 'has-sensor-amp') {
-      const sensors = assetNodes
-        .map((node) => node.automationPolicySensor)
-        .filter((sensor): sensor is SensorSwitchFragment => !!sensor);
-      const uniqueSensors = uniqBy(sensors, 'id');
-
-      if (sensors.length) {
-        return <ScheduleOrSensorTag repoAddress={repoAddress} sensors={uniqueSensors} />;
-      }
     }
 
     return null;

js_modules/dagster-ui/packages/ui-core/src/assets/AssetNodeInstigatorTag.tsx
@@ -1,14 +1,13 @@
 import {gql} from '@apollo/client';
-import flatMap from 'lodash/flatMap';
-import uniqBy from 'lodash/uniqBy';
 import React from 'react';
 
 import {ScheduleOrSensorTag} from '../nav/ScheduleOrSensorTag';
 import {SCHEDULE_SWITCH_FRAGMENT} from '../schedules/ScheduleSwitch';
+import {ScheduleSwitchFragment} from '../schedules/types/ScheduleSwitch.types';
 import {SENSOR_SWITCH_FRAGMENT} from '../sensors/SensorSwitch';
+import {SensorSwitchFragment} from '../sensors/types/SensorSwitch.types';
 import {RepoAddress} from '../workspace/types';
 
-import {useAutomationPolicySensorFlag} from './AutomationPolicySensorFlag';
 import {AssetNodeInstigatorsFragment} from './types/AssetNodeInstigatorTag.types';
 
 export const AssetNodeInstigatorTag = ({
@@ -18,26 +17,17 @@ export const AssetNodeInstigatorTag = ({
   assetNode: AssetNodeInstigatorsFragment;
   repoAddress: RepoAddress;
 }) => {
-  const automaterializeSensorsFlagState = useAutomationPolicySensorFlag();
   const {schedules, sensors} = React.useMemo(() => {
-    const schedules = uniqBy(
-      flatMap(assetNode.jobs, (j) => j.schedules),
-      'id',
+    const instigators = assetNode.targetingInstigators;
+    const schedules = instigators.filter(
+      (instigator): instigator is ScheduleSwitchFragment => instigator.__typename === 'Schedule',
     );
-    const sensors = uniqBy(
-      flatMap(assetNode.jobs, (j) => j.sensors),
-      'id',
+    const sensors = instigators.filter(
+      (instigator): instigator is SensorSwitchFragment => instigator.__typename === 'Sensor',
     );
-
-    if (automaterializeSensorsFlagState === 'has-sensor-amp') {
-      const ampSensor = assetNode.automationPolicySensor;
-      if (ampSensor) {
-        sensors.push(ampSensor);
-      }
-    }
 
     return {schedules, sensors};
-  }, [assetNode, automaterializeSensorsFlagState]);
+  }, [assetNode]);
 
   return (
     <ScheduleOrSensorTag
@@ -52,24 +42,15 @@ export const AssetNodeInstigatorTag = ({
 export const ASSET_NODE_INSTIGATORS_FRAGMENT = gql`
   fragment AssetNodeInstigatorsFragment on AssetNode {
     id
-    jobs {
-      id
-      name
-      schedules {
-        id
+    targetingInstigators {
+      ... on Schedule {
         ...ScheduleSwitchFragment
       }
-      sensors {
-        id
+      ... on Sensor {
         ...SensorSwitchFragment
       }
     }
-    automationPolicySensor {
-      id
-      ...SensorSwitchFragment
-    }
   }
-
   ${SCHEDULE_SWITCH_FRAGMENT}
   ${SENSOR_SWITCH_FRAGMENT}
 `;

js_modules/dagster-ui/packages/ui-core/src/assets/__fixtures__/AssetGroupTags.fixtures.ts
@@ -1,6 +1,6 @@
 import {MockedResponse} from '@apollo/client/testing';
 
-import {buildAssetNode, buildAutoMaterializePolicy, buildSensor} from '../../graphql/types';
+import {buildAssetNode, buildAutoMaterializePolicy} from '../../graphql/types';
 import {DUNDER_REPO_NAME} from '../../workspace/buildRepoAddress';
 import {ASSET_GROUP_METADATA_QUERY} from '../AssetGroupRoot';
 import {
@@ -33,7 +33,6 @@ export const assetGroupWithoutAMP: MockedResponse<
         buildAssetNode({
           id: 'node_vanilla',
           autoMaterializePolicy: null,
-          automationPolicySensor: null,
         }),
       ],
     },
@@ -61,85 +60,6 @@ export const assetGroupWithAMP: MockedResponse<
         buildAssetNode({
           id: 'node_with_amp',
           autoMaterializePolicy: buildAutoMaterializePolicy(),
-          automationPolicySensor: null,
-        }),
-      ],
-    },
-  },
-};
-
-export const assetGroupWithAMPSensor: MockedResponse<
-  AssetGroupMetadataQuery,
-  AssetGroupMetadataQueryVariables
-> = {
-  request: {
-    query: ASSET_GROUP_METADATA_QUERY,
-    variables: {
-      selector: {
-        groupName: GROUP_NAME,
-        repositoryName: DUNDER_REPO_NAME,
-        repositoryLocationName: LOCATION_NAME,
-      },
-    },
-  },
-  result: {
-    data: {
-      __typename: 'Query',
-      assetNodes: [
-        buildAssetNode({
-          id: 'node_with_amp_sensor',
-          autoMaterializePolicy: null,
-          automationPolicySensor: buildSensor({
-            id: AMP_SENSOR_ID,
-            name: AMP_SENSOR_ID,
-          }),
-        }),
-      ],
-    },
-  },
-};
-
-export const assetGroupWithManyAMPSensors: MockedResponse<
-  AssetGroupMetadataQuery,
-  AssetGroupMetadataQueryVariables
-> = {
-  request: {
-    query: ASSET_GROUP_METADATA_QUERY,
-    variables: {
-      selector: {
-        groupName: GROUP_NAME,
-        repositoryName: DUNDER_REPO_NAME,
-        repositoryLocationName: LOCATION_NAME,
-      },
-    },
-  },
-  result: {
-    data: {
-      __typename: 'Query',
-      assetNodes: [
-        buildAssetNode({
-          id: 'node_with_amp_sensor_a',
-          autoMaterializePolicy: null,
-          automationPolicySensor: buildSensor({
-            id: `${AMP_SENSOR_ID}-A`,
-            name: `${AMP_SENSOR_ID}-A`,
-          }),
-        }),
-        buildAssetNode({
-          id: 'node_with_amp_sensor_b',
-          autoMaterializePolicy: null,
-          automationPolicySensor: buildSensor({
-            id: `${AMP_SENSOR_ID}-B`,
-            name: `${AMP_SENSOR_ID}-B`,
-          }),
-        }),
-        buildAssetNode({
-          id: 'node_with_amp_sensor_c',
-          autoMaterializePolicy: null,
-          automationPolicySensor: buildSensor({
-            id: `${AMP_SENSOR_ID}-C`,
-            name: `${AMP_SENSOR_ID}-C`,
-          }),
         }),
       ],
     },

js_modules/dagster-ui/packages/ui-core/src/assets/__fixtures__/AssetViewDefinition.fixtures.ts
@@ -79,7 +79,6 @@ export const AssetViewDefinitionSourceAsset: MockedResponse<AssetViewDefinitionQ
             },
             __typename: 'Repository',
           },
-          jobs: [],
           __typename: 'AssetNode',
           description: null,
           graphName: null,
@@ -107,7 +106,7 @@ export const AssetViewDefinitionSourceAsset: MockedResponse<AssetViewDefinitionQ
               resourceKey: 'foo',
             },
           ],
-          automationPolicySensor: null,
+          targetingInstigators: [],
         },
         __typename: 'Asset',
       },
@@ -149,7 +148,6 @@ export const AssetViewDefinitionSDA: MockedResponse<AssetViewDefinitionQuery> = 
             },
             __typename: 'Repository',
           },
-          jobs: [],
           __typename: 'AssetNode',
           description: null,
           graphName: null,
@@ -177,7 +175,7 @@ export const AssetViewDefinitionSDA: MockedResponse<AssetViewDefinitionQuery> = 
               resourceKey: 'foo',
             },
           ],
-          automationPolicySensor: null,
+          targetingInstigators: [],
         },
         __typename: 'Asset',
       },

js_modules/dagster-ui/packages/ui-core/src/assets/__tests__/AssetGroupTags.test.tsx
@@ -8,12 +8,9 @@ import {DUNDER_REPO_NAME, buildRepoAddress} from '../../workspace/buildRepoAddre
 import {AssetGroupTags} from '../AssetGroupRoot';
 import * as AutomationPolicySensorFlag from '../AutomationPolicySensorFlag';
 import {
-  AMP_SENSOR_ID,
   GROUP_NAME,
   LOCATION_NAME,
   assetGroupWithAMP,
-  assetGroupWithAMPSensor,
-  assetGroupWithManyAMPSensors,
   assetGroupWithoutAMP,
 } from '../__fixtures__/AssetGroupTags.fixtures';
 
@@ -73,14 +70,14 @@ describe('AssetGroupTags', () => {
     expect(await screen.findByRole('link', {name: /auto\-materialize on/i})).toBeVisible();
   });
 
-  it('renders AMP sensor tag', async () => {
+  it('does not render global AMP tag when sensors are turned on', async () => {
     jest
       .spyOn(AutomationPolicySensorFlag, 'useAutomationPolicySensorFlag')
       .mockReturnValue('has-sensor-amp');
 
     render(
       <MemoryRouter>
-        <MockedProvider mocks={[assetGroupWithAMPSensor]}>
+        <MockedProvider mocks={[assetGroupWithAMP]}>
           <AssetGroupTags repoAddress={repoAddress} groupSelector={groupSelector} />
         </MockedProvider>
       </MemoryRouter>,
@@ -88,28 +85,6 @@ describe('AssetGroupTags', () => {
 
     expect(await screen.findByText(/asset group in/i)).toBeVisible();
     expect(await screen.findByRole('link', {name: /my_location/i})).toBeVisible();
-    expect(
-      await screen.findByRole('link', {name: new RegExp(`${AMP_SENSOR_ID}`, 'i')}),
-    ).toBeVisible();
-  });
-
-  it('renders link for many AMP sensors', async () => {
-    jest
-      .spyOn(AutomationPolicySensorFlag, 'useAutomationPolicySensorFlag')
-      .mockReturnValue('has-sensor-amp');
-
-    render(
-      <MemoryRouter>
-        <MockedProvider mocks={[assetGroupWithManyAMPSensors]}>
-          <AssetGroupTags repoAddress={repoAddress} groupSelector={groupSelector} />
-        </MockedProvider>
-      </MemoryRouter>,
-    );
-
-    expect(await screen.findByText(/asset group in/i)).toBeVisible();
-    expect(await screen.findByRole('link', {name: /my_location/i})).toBeVisible();
-
-    // Renders a ButtonLink that opens a dialog
-    expect(await screen.findByRole('button', {name: /3 sensors/i})).toBeVisible();
+    expect(await screen.queryByRole('link', {name: /auto\-materialize on/i})).toBeNull();
   });
 });

js_modules/dagster-ui/packages/ui-core/src/assets/__tests__/buildAssetTabs.test.tsx
@@ -46,19 +46,10 @@ describe('buildAssetTabs', () => {
       },
       __typename: 'Repository',
     },
-    jobs: [
-      {
-        id: 'c2c2f713745b2a2a671094c846b1786ecddab4ce',
-        name: '__ASSET_JOB_0',
-        schedules: [],
-        sensors: [],
-        __typename: 'Pipeline',
-      },
-    ],
     __typename: 'AssetNode',
     description: null,
     graphName: null,
-    automationPolicySensor: null,
+    targetingInstigators: [],
     opNames: ['eager_downstream_3_partitioned'],
     opVersion: null,
     jobNames: ['__ASSET_JOB_0'],
@@ -192,19 +183,10 @@ describe('buildAssetTabs', () => {
       },
       __typename: 'Repository',
     },
-    jobs: [
-      {
-        id: '198e5820c136b6d5655b12b6415b5af1295bef53',
-        name: '__ASSET_JOB_0',
-        schedules: [],
-        sensors: [],
-        __typename: 'Pipeline',
-      },
-    ],
     __typename: 'AssetNode',
     description: null,
     graphName: null,
-    automationPolicySensor: null,
+    targetingInstigators: [],
     opNames: ['lazy_downstream_1'],
     opVersion: null,
     jobNames: ['__ASSET_JOB_0'],

js_modules/dagster-ui/packages/ui-core/src/assets/types/AssetGroupRoot.types.ts
@@ -15,17 +15,5 @@ export type AssetGroupMetadataQuery = {
       __typename: 'AutoMaterializePolicy';
       policyType: Types.AutoMaterializePolicyType;
     } | null;
-    automationPolicySensor: {
-      __typename: 'Sensor';
-      id: string;
-      jobOriginId: string;
-      name: string;
-      sensorState: {
-        __typename: 'InstigationState';
-        id: string;
-        selectorId: string;
-        status: Types.InstigationStatus;
-      };
-    } | null;
   }>;
 };

js_modules/dagster-ui/packages/ui-core/src/assets/types/AssetNodeInstigatorTag.types.ts
@@ -5,46 +5,31 @@ import * as Types from '../../graphql/types';
 export type AssetNodeInstigatorsFragment = {
   __typename: 'AssetNode';
   id: string;
-  jobs: Array<{
-    __typename: 'Pipeline';
-    id: string;
-    name: string;
-    schedules: Array<{
-      __typename: 'Schedule';
-      id: string;
-      name: string;
-      cronSchedule: string;
-      executionTimezone: string | null;
-      scheduleState: {
-        __typename: 'InstigationState';
+  targetingInstigators: Array<
+    | {
+        __typename: 'Schedule';
         id: string;
-        selectorId: string;
-        status: Types.InstigationStatus;
-      };
-    }>;
-    sensors: Array<{
-      __typename: 'Sensor';
-      id: string;
-      jobOriginId: string;
-      name: string;
-      sensorState: {
-        __typename: 'InstigationState';
+        name: string;
+        cronSchedule: string;
+        executionTimezone: string | null;
+        scheduleState: {
+          __typename: 'InstigationState';
+          id: string;
+          selectorId: string;
+          status: Types.InstigationStatus;
+        };
+      }
+    | {
+        __typename: 'Sensor';
         id: string;
-        selectorId: string;
-        status: Types.InstigationStatus;
-      };
-    }>;
-  }>;
-  automationPolicySensor: {
-    __typename: 'Sensor';
-    id: string;
-    jobOriginId: string;
-    name: string;
-    sensorState: {
-      __typename: 'InstigationState';
-      id: string;
-      selectorId: string;
-      status: Types.InstigationStatus;
-    };
-  } | null;
+        jobOriginId: string;
+        name: string;
+        sensorState: {
+          __typename: 'InstigationState';
+          id: string;
+          selectorId: string;
+          status: Types.InstigationStatus;
+        };
+      }
+  >;
 };

commit 5f6e2c61be4f88d632111a461daa7f7ba49aefff

Remove unused "SensorSwitchFragment" (#19293)
## Summary & Motivation

broken in https://github.com/dagster-io/dagster/pull/19083

Fixes https://github.com/dagster-io/dagster/issues/19292

## How I Tested These Changes

Loaded the page after removing

js_modules/dagster-ui/packages/ui-core/src/assets/AssetGroupRoot.tsx
@@ -14,7 +14,6 @@ import {
   explorerPathFromString,
   explorerPathToString,
 } from '../pipelines/PipelinePathUtils';
-import {SENSOR_SWITCH_FRAGMENT} from '../sensors/SensorSwitch';
 import {TabLink} from '../ui/TabLink';
 import {ReloadAllButton} from '../workspace/ReloadAllButton';
 import {RepoAddress} from '../workspace/types';
@@ -134,8 +133,6 @@ export const ASSET_GROUP_METADATA_QUERY = gql`
       }
     }
   }
-
-  ${SENSOR_SWITCH_FRAGMENT}
 `;
 
 export const AssetGroupTags = ({