dagster-io/dagster

commit 9aa58e22e68e8455573cc04c7fb6a3448ce933c5

[Asset Graph] Use groupIdForNode in Sidebar for group node IDs (#19240)
## Summary & Motivation

The AssetGraph layout algorithm ID's group nodes using the
`groupIdForNode` function, this PR updates the Sidebar to also do that
to fix an issue where the id's aren't aligned when launching with
`dagster dev -f` or `-m`

## How I Tested These Changes
dagster dev -f
python_modules/dagster-test/dagster_test/toys/module_with_groups.py -p
3333

js_modules/dagster-ui/packages/ui-core/src/asset-graph/sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ import {useQueryAndLocalStoragePersistedState} from '../../hooks/useQueryAndLoca
 import {ExplorerPath} from '../../pipelines/PipelinePathUtils';
 import {Container, Inner, Row} from '../../ui/VirtualizedTable';
 import {buildRepoPathForHuman} from '../../workspace/buildRepoAddress';
-import {GraphData, GraphNode, tokenForAssetKey} from '../Utils';
+import {GraphData, GraphNode, groupIdForNode, tokenForAssetKey} from '../Utils';
 import {SearchFilter} from '../sidebar/SearchFilter';
 
 import {AssetSidebarNode} from './AssetSidebarNode';
@@ -139,35 +139,36 @@ export const AssetGraphExplorerSidebar = React.memo(
         const locationName = node.definition.repository.location.name;
         const repositoryName = node.definition.repository.name;
         const groupName = node.definition.groupName || 'default';
+        const groupId = groupIdForNode(node);
         const codeLocation = buildRepoPathForHuman(repositoryName, locationName);
         codeLocationNodes[codeLocation] = codeLocationNodes[codeLocation] || {
           locationName: codeLocation,
           groups: {},
         };
-        if (!codeLocationNodes[codeLocation]!.groups[groupName]!) {
+        if (!codeLocationNodes[codeLocation]!.groups[groupId]!) {
           groupsCount += 1;
         }
-        codeLocationNodes[codeLocation]!.groups[groupName] = codeLocationNodes[codeLocation]!
-          .groups[groupName] || {
+        codeLocationNodes[codeLocation]!.groups[groupId] = codeLocationNodes[codeLocation]!.groups[
+          groupName
+        ] || {
           groupName,
           assets: [],
         };
-        codeLocationNodes[codeLocation]!.groups[groupName]!.assets.push(id);
+        codeLocationNodes[codeLocation]!.groups[groupId]!.assets.push(id);
       });
       const codeLocationsCount = Object.keys(codeLocationNodes).length;
       Object.entries(codeLocationNodes).forEach(([locationName, locationNode]) => {
         folderNodes.push({locationName, id: locationName, level: 1});
         if (openNodes.has(locationName) || codeLocationsCount === 1) {
-          Object.entries(locationNode.groups).forEach(([groupName, groupNode]) => {
-            const groupId = locationName + ':' + groupName;
-            folderNodes.push({groupName, id: groupId, level: 2});
-            if (openNodes.has(groupId) || groupsCount === 1) {
+          Object.entries(locationNode.groups).forEach(([id, groupNode]) => {
+            folderNodes.push({groupName: groupNode.groupName, id, level: 2});
+            if (openNodes.has(id) || groupsCount === 1) {
               groupNode.assets
                 .sort((a, b) => COLLATOR.compare(a, b))
                 .forEach((assetKey) => {
                   folderNodes.push({
                     id: assetKey,
-                    path: locationName + ':' + groupName + ':' + assetKey,
+                    path: locationName + ':' + groupNode.groupName + ':' + assetKey,
                     level: 3,
                   });
                 });

python_modules/dagster-test/dagster_test/toys/module_with_groups.py
@@ -1,0 +1,29 @@
+from dagster import Definitions, asset
+
+assets = []
+for i in range(4):
+
+    @asset(name=f"a_asset_{i}", group_name="my_asset_group_1")
+    def a_asset():
+        pass
+
+    @asset(
+        name=f"b_asset_{i}",
+        group_name="my_asset_group_2",
+        non_argument_deps={f"a_asset_{i}"},
+    )
+    def b_asset():
+        pass
+
+    @asset(
+        name=f"c_asset_{i}",
+        group_name="my_asset_group_3",
+        non_argument_deps={f"b_asset_{i}"},
+    )
+    def c_asset():
+        pass
+
+    assets += [a_asset, b_asset, c_asset]
+
+
+defs = Definitions(assets=assets)

commit 22121dfaf17c4b80a0fb7cb3541d29a245e94e22

[Asset graph] Fix sidebar showing assets for group (#19317)
## Summary & Motivation

This object is keyed by groupId

broken in https://github.com/dagster-io/dagster/pull/19240

## How I Tested These Changes

loaded the asset graph while viewing only a single group

js_modules/dagster-ui/packages/ui-core/src/asset-graph/sidebar/Sidebar.tsx
@@ -148,7 +148,7 @@ export const AssetGraphExplorerSidebar = React.memo(
           groupsCount += 1;
         }
         codeLocationNodes[codeLocation]!.groups[groupId] = codeLocationNodes[codeLocation]!.groups[
-          groupName
+          groupId
         ] || {
           groupName,
           assets: [],