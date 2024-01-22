408793076423ba7e4697887d239af190b6a05760
Matthew Kuzyk <matthew.kuzyk@woven-planet.global>
2024-01-22 13:52:09 +0900

    feat: Add test to daemon

--- helm/dagster/schema/schema_tests/test_dagster_daemon.py
+++ helm/dagster/schema/schema_tests/test_dagster_daemon.py
@@ -537,6 +537,32 @@ def test_scheduler_name(template: HelmTemplate):
     assert daemon_deployment.spec.template.spec.scheduler_name == "custom"
 
 
+def test_init_container_resources(template: HelmTemplate):
+    init_container_resources = {
+        "limits": {
+            "cpu": "200m"
+        },
+        "requests": {
+            "memory": "1Gi"
+        }
+    }
+    helm_values = DagsterHelmValues.construct(
+        dagsterDaemon=Daemon.construct(initContainerResources=init_container_resources)
+    )
+
+    [webserver_deployment] = template.render(helm_values)
+
+    assert len(webserver_deployment.spec.template.spec.init_containers) == 2
+
+    assert all(
+        container.resources
+        == k8s_model_from_dict(
+            k8s_client.models.v1_resource_requirements.V1ResourceRequirements,
+            k8s_snake_case_dict(k8s_client.models.v1_resource_requirements.V1ResourceRequirements, init_container_resources),
+        )
+        for container in webserver_deployment.spec.template.spec.init_containers
+    )
+
 def test_env(template: HelmTemplate):
     helm_values = DagsterHelmValues.construct(dagsterDaemon=Daemon.construct())
     [daemon_deployment] = template.render(helm_values)
