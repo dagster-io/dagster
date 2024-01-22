34b21d68ac26f03357c362ffe47bab8cb738c1a1
Johann Miller <johann@elementl.com>
2024-01-22 14:42:09 -0800

    verify no duplicate dependencies on AssetCheckSpec (#19358)
    https://github.com/dagster-io/dagster/pull/19319/files#r1460110077

--- python_modules/dagster/dagster_tests/definitions_tests/test_asset_check_spec.py
+++ python_modules/dagster/dagster_tests/definitions_tests/test_asset_check_spec.py
@@ -1,3 +1,6 @@
+import re
+
+import pytest
 from dagster import AssetCheckSpec, AssetKey, SourceAsset, asset
 
 
@@ -17,3 +20,13 @@ def test_source_asset():
     foo = SourceAsset("foo")
 
     assert AssetCheckSpec(asset=foo, name="check1").asset_key == AssetKey("foo")
+
+
+def test_additional_deps():
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            'Asset check check1 for asset ["foo"] cannot have an additional dependency on asset ["foo"].'
+        ),
+    ):
+        AssetCheckSpec(asset="foo", name="check1", additional_deps=["foo"])
