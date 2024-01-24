4443f8f91582e5dc6d2b56a7f1ce1d9b0e40bef1
Marco Salazar <marco@elementl.com>
2024-01-24 10:00:43 -0800

    .

--- python_modules/dagster-test/dagster_test/toys/simple_config.py
+++ python_modules/dagster-test/dagster_test/toys/simple_config.py
@@ -1,11 +1,22 @@
 from dagster import job, op
 
 
-@op(config_schema={"num": int})
+@op(
+    config_schema={"num": int},
+    tags={
+        "dagster-k8s/config": {
+            "container_config": {
+                "resources": {
+                    "requests": {"cpu": "3"},
+                },
+            }
+        }
+    },
+)
 def requires_config(context):
     return context.op_config
 
 
-@job
+@job()
 def simple_config_job():
     requires_config()
