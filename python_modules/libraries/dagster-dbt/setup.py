998b54e8dae3b953cc491a253898d3bd06888778
Rex Ledesma <rex@dagsterlabs.com>
2024-01-29 14:51:42 -0500

    feat(dbt): remove support for `dbt-core==1.4.*`

--- python_modules/libraries/dagster-dbt/setup.py
+++ python_modules/libraries/dagster-dbt/setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
         f"dagster{pin}",
         # Follow the version support constraints for dbt Core: https://docs.getdbt.com/docs/dbt-versions/core
-        "dbt-core>=1.4,<1.8",
+        "dbt-core>=1.5,<1.8",
         "Jinja2",
         "networkx",
         "orjson",
