f68c067067e4a8776d160211156663af605fb4b9
chrishiste <chrishiste@gmail.com>
2024-02-01 10:25:07 -0300

    Fix wandb version

--- python_modules/libraries/dagster-wandb/setup.py
+++ python_modules/libraries/dagster-wandb/setup.py
@@ -34,7 +34,7 @@ setup(
     packages=find_packages(exclude=["dagster_wandb_tests*"]),
     install_requires=[
         f"dagster{pin}",
-        "wandb>=0.13.5,<0.15.5",
+        "wandb>=0.13.5,<1.0",
     ],
     extras_require={"dev": ["cloudpickle", "joblib", "callee", "dill"]},
     zip_safe=False,
