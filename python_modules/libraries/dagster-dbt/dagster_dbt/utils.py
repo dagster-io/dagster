48380a250218ab6088671398be7d193a44d05969
kmontag42 <kmontag@salsify.com>
2024-02-06 11:12:16 -0500

    🐛 bugfix for asset selection unique keys

--- python_modules/libraries/dagster-dbt/dagster_dbt/utils.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/utils.py
@@ -44,7 +44,7 @@ def input_name_fn(dbt_resource_props: Mapping[str, Any]) -> str:
 
 def output_name_fn(dbt_resource_props: Mapping[str, Any]) -> str:
     # hyphens are valid in dbt model names, but not in output names
-    return dbt_resource_props["unique_id"].split(".")[-1].replace("-", "_")
+    return dbt_resource_props["unique_id"].replace(".", "_").replace("-", "_")
 
 
 def _node_result_to_metadata(node_result: Mapping[str, Any]) -> Mapping[str, RawMetadataValue]:
