c7f7d309ef331a1e037dd455a16617559807201b
elementl <devtools@elementl.com>
2024-02-08 22:10:58 +0000

    1.6.4

--- python_modules/libraries/dagster-snowflake-pandas/setup.py
+++ python_modules/libraries/dagster-snowflake-pandas/setup.py
@@ -34,8 +34,8 @@ setup(
     ],
     packages=find_packages(exclude=["dagster_snowflake_pandas_tests*"]),
     install_requires=[
-        f"dagster{pin}",
-        f"dagster-snowflake{pin}",
+        "dagster==1.6.4",
+        "dagster-snowflake==0.22.4",
         "pandas",
         "requests",
         "snowflake-connector-python[pandas]>=2.1.0",
