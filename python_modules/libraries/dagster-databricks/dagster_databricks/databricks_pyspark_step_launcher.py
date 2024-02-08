ef0143559c2e6155051ba6d5daed2717b9929d99
zyd14 <romerzs14@gmail.com>
2024-02-07 19:13:31 -0700

    Removing unnecessary method

--- python_modules/libraries/dagster-databricks/dagster_databricks/databricks_pyspark_step_launcher.py
+++ python_modules/libraries/dagster-databricks/dagster_databricks/databricks_pyspark_step_launcher.py
@@ -588,9 +588,6 @@ class DatabricksPySparkStepLauncher(StepLauncher):
         )
         return f"/dbfs/{path}"
 
-    def get_run_url(self, run_id: int) -> str:
-        return self.databricks_runner.get_run_url(run_id)
-
 
 class DatabricksConfig:
     """Represents configuration required by Databricks to run jobs.
