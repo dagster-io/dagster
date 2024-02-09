bd71e49ebf4797ce3c0741354011d545742f0a03
Maxime Armstrong <maxime@retain.ai>
2024-02-09 16:05:06 -0500

    Update dagster-openai integration

---
+++ python_modules/libraries/dagster-openai/dagster_openai/__init__.py
@@ -0,0 +1,10 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from .resources import (
+    ApiResourceEnum as ApiResourceEnum,
+    OpenAIResource as OpenAIResource,
+    with_usage_metadata as with_usage_metadata,
+)
+from .version import __version__
+
+DagsterLibraryRegistry.register("dagster-openai", __version__)
--- python_modules/libraries/dagster-openai/dagster_openai/resources.py
+++ python_modules/libraries/dagster-openai/dagster_openai/resources.py
@@ -1,18 +1,60 @@
 from collections import defaultdict
 from contextlib import contextmanager
+from enum import Enum
 from functools import wraps
-from typing import Generator
+from typing import Generator, List, Optional
 from weakref import WeakKeyDictionary
 
 from dagster import AssetExecutionContext, ConfigurableResource, InitResourceContext
 from openai import Client
 from pydantic import Field, PrivateAttr
 
+
+class ApiResourceEnum(Enum):
+    """Legacy and current resources of the OpenAI API v1."""
+
+    COMPLETIONS = "completions"
+    CHAT = "chat"
+    EMBEDDINGS = "embeddings"
+    FILES = "files"
+    IMAGES = "images"
+    AUDIO = "audio"
+    MODERATIONS = "moderations"
+    MODELS = "models"
+    FINE_TUNING = "fine_tuning"
+
+
+API_RESOURCES_TO_ENDPOINT_METHODS_MAPPING = {
+    "completions": [["create"]],
+    "chat": [["completions", "create"]],
+    "embeddings": [["create"]],
+    "files": [
+        ["create"],
+        ["retrieve"],
+        ["list"],
+        ["delete"],
+        ["content"],
+        ["retrieve_content"],
+        ["wait_for_processing"],
+    ],
+    "images": [["create_variation"], ["edit"], ["generate"]],
+    "audio": [["transcriptions", "create"], ["translations", "create"], ["speech", "create"]],
+    "moderations": [["create"]],
+    "models": [["retrieve"], ["list"], ["delete"]],
+    "fine_tuning": [
+        ["jobs", "create"],
+        ["jobs", "retrieve"],
+        ["jobs", "list"],
+        ["jobs", "cancel"],
+        ["jobs", "list_events"],
+    ],
+}
+
 context_to_counters = WeakKeyDictionary()
 
 
 def add_to_asset_metadata(
-    context: AssetExecutionContext, metadata_key: str, delta: int, output_name: str | None = None
+    context: AssetExecutionContext, metadata_key: str, delta: int, output_name: Optional[str] = None
 ):
     if context not in context_to_counters:
         context_to_counters[context] = defaultdict(lambda: 0)
@@ -21,7 +63,41 @@ def add_to_asset_metadata(
     context.add_output_metadata(dict(counters), output_name)
 
 
-def with_usage_metadata(context: AssetExecutionContext, output_name: str | None, func):
+def with_usage_metadata(context: AssetExecutionContext, output_name: Optional[str], func):
+    """This wrapped can be used on any endpoint of the
+    `openai library <https://github.com/openai/openai-python>`
+    to log the OpenAI API usage to asset metadata.
+
+    Examples:
+    .. code-block:: python
+
+        import os
+
+        from dagster import asset, define_asset_job, EnvVar
+        from dagster_openai import OpenAIResource, with_usage_metadata
+
+
+        @asset(compute_kind="OpenAI")
+        def openai_asset(context: AssetExecutionContext, openai: OpenAIResource):
+            with openai.get_client(context) as client:
+                client.fine_tuning.jobs.create = with_usage_metadata(
+                    client.fine_tuning.jobs.create
+                )
+                client.fine_tuning.jobs.create(
+                    model="gpt-3.5-turbo",
+                    training_file="some_training_file"
+                )
+
+        openai_asset_job = define_asset_job(name="openai_asset_job", selection="openai_asset")
+
+        defs = Definitions(
+            jobs=[openai_asset_job],
+            resources={
+                "slack": OpenAIResource(api_key=EnvVar("OPENAI_API_KEY")),
+            },
+        )
+    """
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         response = func(*args, **kwargs)
@@ -39,8 +115,37 @@ def with_usage_metadata(context: AssetExecutionContext, output_name: str | None,
 
 
 class OpenAIResource(ConfigurableResource):
-    """This resource is a thin wrapper over the
+    """This resource is wrapper over the
     `openai library <https://github.com/openai/openai-python>`_.
+
+    By configuring this OpenAI resource, you can interact with OpenAI API
+    and log its usage to asset metadata.
+
+    Examples:
+        .. code-block:: python
+
+            import os
+
+            from dagster import asset, define_asset_job, EnvVar
+            from dagster_openai import OpenAIResource
+
+
+            @asset(compute_kind="OpenAI")
+            def openai_asset(context: AssetExecutionContext, openai: OpenAIResource):
+                with openai.get_client(context) as client:
+                    client.chat.completions.create(
+                        model="gpt-3.5-turbo",
+                        messages=[{"role": "user", "content": "Say this is a test"}]
+                    )
+
+            openai_asset_job = define_asset_job(name="openai_asset_job", selection="openai_asset")
+
+            defs = Definitions(
+                jobs=[openai_asset_job],
+                resources={
+                    "slack": OpenAIResource(api_key=EnvVar("OPENAI_API_KEY")),
+                },
+            )
     """
 
     api_key: str = Field(description=("OpenAI API key. See https://platform.openai.com/api-keys"))
@@ -51,41 +156,98 @@ class OpenAIResource(ConfigurableResource):
     def _is_dagster_maintained(cls) -> bool:
         return True
 
+    def _wrap_with_usage_metadata(
+        self,
+        client_resource: ApiResourceEnum,
+        context: AssetExecutionContext,
+        output_name: Optional[str],
+    ):
+        for attribute_names in API_RESOURCES_TO_ENDPOINT_METHODS_MAPPING[client_resource.value]:
+            curr = self._client.__getattribute__(client_resource.value)
+            # Get the second to last attribute from the attribute list to reach the method.
+            i = 0
+            while i < len(attribute_names) - 1:
+                curr = curr.__getattribute__(attribute_names[i])
+                i += 1
+            # Wrap the method.
+            curr.__setattr__(
+                attribute_names[i],
+                with_usage_metadata(
+                    context=context,
+                    output_name=output_name,
+                    func=curr.__getattribute__(attribute_names[i]),
+                ),
+            )
+
     def setup_for_execution(self, context: InitResourceContext) -> None:
         # Set up an OpenAI client based on the API key.
         self._client = Client(api_key=self.api_key)
 
     @contextmanager
     def get_client(
-        self, context: AssetExecutionContext, output_name: str | None = None
+        self,
+        context: AssetExecutionContext,
+        output_name: Optional[str] = None,
+        api_resources: Optional[List[ApiResourceEnum]] = None,
     ) -> Generator[Client, None, None]:
+        """Returns an ``openai.Client`` for interacting with the OpenAI API.
+
+        By default, the client comes with wrapped endpoints
+        for three API resources, Completions, Embeddings and Chat,
+        allowing to log the API usage in the asset metadata.
+
+        Note that, if required by your use case, you can specify the API resources
+        for which you would like the endpoints to be wrapped.
+        Legacy and current resources of OpenAI API v1 can be specified using `ApiResourceEnum`.
+
+        Examples:
+        .. code-block:: python
+
+            import os
+
+            from dagster import asset, define_asset_job, EnvVar
+            from dagster_openai import OpenAIResource, ApiResourceEnum
+
+
+            @asset(compute_kind="OpenAI")
+            def openai_asset(context: AssetExecutionContext, openai: OpenAIResource):
+                api_resources = [
+                    ApiResourceEnum.CHAT,
+                    ApiResourceEnum.FINE_TUNING,
+                ]
+                with openai.get_client(context, api_resources=api_resources) as client:
+                    client.chat.completions.create(
+                        model="gpt-3.5-turbo",
+                        messages=[{"role": "user", "content": "Say this is a test"}]
+                    )
+
+            openai_asset_job = define_asset_job(name="openai_asset_job", selection="openai_asset")
+
+            defs = Definitions(
+                jobs=[openai_asset_job],
+                resources={
+                    "slack": OpenAIResource(api_key=EnvVar("OPENAI_API_KEY")),
+                },
+            )
+        """
+        # If none of the OpenAI API resource types are specified,
+        # we wrap by default the methods of
+        # `openai.resources.Completions`, `openai.resources.Embeddings`
+        # and `openai.resources.chat.Completions`.
+        # This allows the usage metadata to be captured.
+        if not api_resources:
+            api_resources = [
+                ApiResourceEnum.COMPLETIONS,
+                ApiResourceEnum.CHAT,
+                ApiResourceEnum.EMBEDDINGS,
+            ]
+        for client_resource in api_resources:
+            self._wrap_with_usage_metadata(
+                client_resource=client_resource,
+                context=context,
+                output_name=output_name,
+            )
         yield self._client
-
-    @contextmanager
-    def get_client_for_usage_metadata(
-            self, context: AssetExecutionContext, output_name: str | None = None
-    ) -> Generator[Client, None, None]:
-
-        original_completions_create = self._client.completions.create
-        original_embeddings_create = self._client.embeddings.create
-        original_completions_create = self._client.chat.completions.create
-
-        self._client.completions.create = with_usage_metadata(
-            context, output_name, self._client.completions.create
-        )
-        self._client.embeddings.create = with_usage_metadata(
-            context, output_name, self._client.embeddings.create
-        )
-        self._client.chat.completions.create = with_usage_metadata(
-            context, output_name, self._client.chat.completions.create
-        )
-
-        try:
-            yield self._client
-        finally:
-            self._client.completions.create = original_completions_create
-            self._client.embeddings.create = original_embeddings_create
-            self._client.chat.completions.create = original_completions_create
 
     def teardown_after_execution(self, context: InitResourceContext) -> None:
         # Close OpenAI client.
