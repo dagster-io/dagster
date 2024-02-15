591bb958ab87d6c04ebdd39f398049b6eb102514
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-02-15 09:40:17 -0800

    Revert "[embedded-elt] Decoupling connections from resource allocation (#18270)"
    This reverts commit 822d6539e539ca15b4929aa6d5745938a6fe6ed6.

--- python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/__init__.py
+++ python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/__init__.py
@@ -1,5 +1,5 @@
 from dagster_embedded_elt.sling.asset_decorator import sling_assets
-from dagster_embedded_elt.sling.asset_defs import build_assets_from_sling_stream, build_sling_asset
+from dagster_embedded_elt.sling.asset_defs import build_sling_asset
 from dagster_embedded_elt.sling.dagster_sling_translator import DagsterSlingTranslator
 from dagster_embedded_elt.sling.resources import (
     SlingConnectionResource,
@@ -14,7 +14,6 @@ __all__ = [
     "SlingResource",
     "SlingMode",
     "build_sling_asset",
-    "build_assets_from_sling_stream",
     "SlingSourceConnection",
     "SlingTargetConnection",
     "SlingConnectionResource",
