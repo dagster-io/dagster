069b7d0090f3d8a024138e03d273aade877c7c4c
gibsondan <daniel@elementl.com>
2024-02-15 11:08:27 -0600

    Add python 3.12 marker to setup.py files (#19780)
    Summary:
    Added the 3.12 marker for modules that we now have under test
    
    part of https://github.com/dagster-io/dagster/issues/17350
    
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- python_modules/libraries/dagster-pandas/setup.py
+++ python_modules/libraries/dagster-pandas/setup.py
@@ -40,6 +40,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
