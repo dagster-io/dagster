6b81144ee528ea8801b942d6e2830764f3bba7ac
alangenfeld <alex@elementl.com>
2024-02-20 12:47:59 -0600

    [dbt] utility class for managing artifacts

--- python_modules/libraries/dagster-dbt/dagster_dbt/__init__.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/__init__.py
@@ -27,6 +27,7 @@ from .dagster_dbt_translator import (
     DagsterDbtTranslatorSettings as DagsterDbtTranslatorSettings,
     KeyPrefixDagsterDbtTranslator as KeyPrefixDagsterDbtTranslator,
 )
+from .dbt_artifacts import DbtArtifacts as DbtArtifacts
 from .dbt_manifest_asset_selection import DbtManifestAssetSelection as DbtManifestAssetSelection
 from .errors import (
     DagsterDbtCliRuntimeError as DagsterDbtCliRuntimeError,
