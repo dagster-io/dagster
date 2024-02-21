c9fc279dd4a3131f6905e1bfeb170d8c7c68b3fa
bengotow <bgotow@elementl.com>
2024-02-21 14:41:52 -0600

    Fix lineage view

--- js_modules/dagster-ui/packages/ui-core/src/assets/AssetNodeLineageGraph.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/AssetNodeLineageGraph.tsx
@@ -44,7 +44,7 @@ export const AssetNodeLineageGraph = ({
 
   // Use the pathname as part of the key so that different deployments don't invalidate each other's cached layout
   // and so that different assets dont invalidate each others layout
-  const {layout, loading} = useAssetLayout(assetGraphData, allGroups);
+  const {layout, loading} = useAssetLayout(assetGraphData, allGroups, 'horizontal');
   const viewportEl = useRef<SVGViewport>();
   const history = useHistory();
 
@@ -107,6 +107,7 @@ export const AssetNodeLineageGraph = ({
             highlighted={highlighted}
             edges={layout.edges}
             viewportRect={viewportRect}
+            direction="horizontal"
           />
 
           {Object.values(layout.nodes)
