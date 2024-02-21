2f1ff454ec0a9173e3b8adedf9c132981f22c952
Marco Salazar <marco@elementl.com>
2024-02-21 00:00:12 -0500

    rm console.logs

--- js_modules/dagster-ui/packages/ui-core/src/nav/useRepositoryLocationReload.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/nav/useRepositoryLocationReload.tsx
@@ -1,7 +1,7 @@
 import {ApolloClient, ApolloError, gql, useApolloClient, useQuery} from '@apollo/client';
 // eslint-disable-next-line no-restricted-imports
 import {Intent} from '@blueprintjs/core';
-import React, {useCallback, useMemo, useReducer} from 'react';
+import {useCallback, useMemo, useReducer} from 'react';
 
 import {
   ReloadRepositoryLocationMutation,
@@ -95,14 +95,6 @@ export const useRepositoryLocationReload = ({
 
   const invalidateConfigs = useInvalidateConfigsForRepo();
 
-  console.log('rerender');
-  React.useLayoutEffect(() => {
-    console.log('first render');
-    return () => {
-      console.log('remove');
-    };
-  }, []);
-
   const {startPolling, stopPolling} = useQuery<
     RepositoryLocationStatusQuery,
     RepositoryLocationStatusQueryVariables
@@ -180,7 +172,6 @@ export const useRepositoryLocationReload = ({
         // Otherwise, we have no errors left.
         dispatch({type: 'finish-polling'});
         stopPolling();
-        console.log('show1');
 
         // On success, show the successful toast, hide the dialog (if open), and reset Apollo.
         await showSharedToaster({
