9948bd69d7298835318bd134d13c528550f9b32c
Marco Salazar <marco@elementl.com>
2024-02-21 18:42:35 -0500

    update fixtures

--- js_modules/dagster-ui/packages/ui-core/src/live-data-provider/LiveDataThread.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/live-data-provider/LiveDataThread.tsx
@@ -84,6 +84,7 @@ export class LiveDataThread<T> {
       this._batchedQueryKeys();
     };
     try {
+      console.log('fetching', {keys});
       const data = await this.queryKeys(keys);
       this.manager._updateFetchedKeys(keys, data);
       doNextFetch();
