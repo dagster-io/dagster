e4596aaa6a56f18f81990fa30775413c43a64faa
Colton Padden <colton@dagsterlabs.com>
2024-02-22 13:40:31 -0500

    make mdx-format

---
+++ examples/docs_snippets/docs_snippets/getting-started/quickstart/assets.py
@@ -0,0 +1,46 @@
+import json
+
+import pandas as pd
+import requests
+from dagster_quickstart.configurations import HNStoriesConfig
+
+from dagster import (
+    MaterializeResult,
+    MetadataValue,
+    asset,
+)
+
+
+@asset
+def hackernews_top_story_ids(config: HNStoriesConfig):
+    """Get top stories from the HackerNews top stories endpoint."""
+    top_story_ids = requests.get(
+        "https://hacker-news.firebaseio.com/v0/topstories.json"
+    ).json()
+
+    with open(config.hn_top_story_ids_path, "w") as f:
+        json.dump(top_story_ids[: config.top_stories_limit], f)
+
+
+@asset(deps=[hackernews_top_story_ids])
+def hackernews_top_stories(config: HNStoriesConfig) -> MaterializeResult:
+    """Get items based on story ids from the HackerNews items endpoint."""
+    with open(config.hn_top_story_ids_path, "r") as f:
+        hackernews_top_story_ids = json.load(f)
+
+    results = []
+    for item_id in hackernews_top_story_ids:
+        item = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
+        ).json()
+        results.append(item)
+
+    df = pd.DataFrame(results)
+    df.to_csv(config.hn_top_stories_path)
+
+    return MaterializeResult(
+        metadata={
+            "num_records": len(df),
+            "preview": MetadataValue.md(str(df[["title", "by", "url"]].to_markdown())),
+        }
+    )
