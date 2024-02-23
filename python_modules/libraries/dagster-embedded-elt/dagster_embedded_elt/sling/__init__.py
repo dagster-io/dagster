068696e7e8f47b0044dd90c072f359d573e50a73
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-02-22 21:46:32 -0800

    [embedded-elt] Revert #18270 Sling Replication changes. (#19957)
    ## Summary & Motivation
    
    This reverts #18270 in preparation for adding the new asset decorator approach. I had initially created this revert after the implementation but it made the diff very hard to parse, moving it before the merge makes it a little easier to review the incoming Sling changes.
    
    ## How I Tested These Changes
    bk

--- python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/__init__.py
+++ python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/__init__.py
@@ -1,6 +1,5 @@
-from dagster_embedded_elt.sling.asset_defs import build_assets_from_sling_stream, build_sling_asset
+from dagster_embedded_elt.sling.asset_defs import build_sling_asset
 from dagster_embedded_elt.sling.resources import (
-    SlingConnectionResource,
     SlingMode,
     SlingResource,
     SlingSourceConnection,
@@ -11,8 +10,6 @@ __all__ = [
     "SlingResource",
     "SlingMode",
     "build_sling_asset",
-    "build_assets_from_sling_stream",
     "SlingSourceConnection",
     "SlingTargetConnection",
-    "SlingConnectionResource",
 ]
