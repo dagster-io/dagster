628c6914aa0aef3a57999e65cde17a6f0ab771f0
jamiedemaria <demaria.jamie@gmail.com>
2024-02-26 12:07:48 -0500

    [docs] remove I/O managers from SDA page (DOC-108) (#19269)

--- examples/docs_snippets/docs_snippets/concepts/assets/asset_group_argument.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_group_argument.py
@@ -1,11 +1,18 @@
 from dagster import asset
 
+
+def execute_query(query):
+    del query
+
+
 # start_example
 
 
 @asset(group_name="cereal_assets")
 def nabisco_cereals():
-    return [1, 2, 3]
+    execute_query(
+        "CREATE TABLE nabisco_cereals AS SELECT * FROM cereals WHERE manufacturer = 'Nabisco'"
+    )
 
 
 # end_example
