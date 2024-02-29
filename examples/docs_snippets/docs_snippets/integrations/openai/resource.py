56779be37cf27709418dba455098ec06c9c854a2
Maxime Armstrong <maxime@retain.ai>
2024-02-29 17:03:42 -0500

    Update code snippets

---
+++ examples/docs_snippets/docs_snippets/integrations/openai/resource.py
@@ -0,0 +1,11 @@
+# ruff: isort: skip_file
+
+# start_example
+from dagster_openai import OpenAIResource
+from dagster import EnvVar
+
+# Pull API key from environment variables
+openai = OpenAIResource(
+    api_key=EnvVar("OPENAI_API_KEY"),
+)
+# end_example
