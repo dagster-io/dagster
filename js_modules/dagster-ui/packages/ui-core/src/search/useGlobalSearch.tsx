451899334c45abc1d7695f7e051adcc7aa7c13cd
Marco polo <salazarm@users.noreply.github.com>
2024-02-29 20:32:15 -0500

    Fix KH's laggy search input with this one weird trick (#20172)
    ## Summary & Motivation
    
    This fixes very long multi-second freeze ups that occur due to the
    apollo cache being very slow. We actually don't need the caching at all
    here because (1) we never make this query a second time, (2) there are
    no variables to the query, and (3) the worker already caches the results
    in memory.
    
    ## How I Tested These Changes
    
    shadow-dagit, type a bunch of searches and observe that we never lock up
    the main thread for an extended period of time

--- js_modules/dagster-ui/packages/ui-core/src/search/useGlobalSearch.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/search/useGlobalSearch.tsx
@@ -187,8 +187,8 @@ export const useGlobalSearch = () => {
   const secondarySearch = useRef<WorkerSearchResult | null>(null);
 
   const primary = useLazyQuery<SearchPrimaryQuery>(SEARCH_PRIMARY_QUERY, {
-    // Try to make aggressive use of workspace values from the Apollo cache.
-    fetchPolicy: 'cache-first',
+    // Don't use the cache because it is slow and we only make this request once so we don't need the cache
+    fetchPolicy: 'no-cache',
     onCompleted: (data: SearchPrimaryQuery) => {
       const results = primaryDataToSearchResults({data});
       if (!primarySearch.current) {
@@ -200,8 +200,8 @@ export const useGlobalSearch = () => {
   });
 
   const secondary = useLazyQuery<SearchSecondaryQuery>(SEARCH_SECONDARY_QUERY, {
-    // As above, try to aggressively use asset information from Apollo cache if possible.
-    fetchPolicy: 'cache-first',
+    // Don't use the cache because it is slow and we only make this request once so we don't need the cache
+    fetchPolicy: 'no-cache',
     onCompleted: (data: SearchSecondaryQuery) => {
       const results = secondaryDataToSearchResults({data});
       if (!secondarySearch.current) {
@@ -230,15 +230,14 @@ export const useGlobalSearch = () => {
     [],
   );
 
-  // If we already have WebWorkers set up, initialization is complete and this will be a no-op.
   const initialize = useCallback(() => {
-    if (!primarySearch.current) {
+    if (!primaryResult.data && !primaryResult.loading) {
       performPrimaryLazyQuery();
     }
-    if (!secondarySearch.current) {
+    if (!secondaryResult.data && !secondaryResult.loading) {
       performSecondaryLazyQuery();
     }
-  }, [performPrimaryLazyQuery, performSecondaryLazyQuery]);
+  }, [performPrimaryLazyQuery, performSecondaryLazyQuery, primaryResult, secondaryResult]);
 
   const searchIndex = useCallback(
     (
