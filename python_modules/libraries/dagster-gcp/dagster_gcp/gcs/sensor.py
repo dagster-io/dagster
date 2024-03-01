b6dc2d834477054bd48bc2d082ccb19e28a805c1
Mathieu Larose <mathieu@retain.ai>
2024-03-01 16:19:27 -0500

    simplify get_gcs_keys by leveraging google cloud page iterator logic

--- python_modules/libraries/dagster-gcp/dagster_gcp/gcs/sensor.py
+++ python_modules/libraries/dagster-gcp/dagster_gcp/gcs/sensor.py
@@ -12,26 +12,12 @@ def get_gcs_keys(bucket, prefix="", since_key=None, gcs_session=None):
     if not gcs_session:
         gcs_session = storage.Client()
 
-    cursor = ""
-    contents = []
-
-    while True:
-        response = list(
-            gcs_session.list_blobs(
-                bucket_or_name=bucket,
-                delimiter="",
-                max_results=MAX_KEYS,
-                prefix=prefix,
-                start_offset=cursor,
-            )
-        )
-        contents.extend(response)
-        if len(response) < MAX_KEYS:
-            break
-
-        contents.pop()
-
-        cursor = response[-1].name
+    contents = list(gcs_session.list_blobs(
+        bucket_or_name=bucket,
+        delimiter="",
+        page_size=MAX_KEYS,
+        prefix=prefix,
+    ))
 
     sorted_keys = [obj.name for obj in sorted(contents, key=lambda x: x.updated)]
 
@@ -40,6 +26,6 @@ def get_gcs_keys(bucket, prefix="", since_key=None, gcs_session=None):
 
     for idx, key in enumerate(sorted_keys):
         if key == since_key:
-            return sorted_keys[idx + 1 :]
+            return sorted_keys[idx + 1:]
 
     return []
