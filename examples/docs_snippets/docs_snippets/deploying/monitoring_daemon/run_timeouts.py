7851156ebc38bdd92886db56886059d1e7585abc
gibsondan <daniel@elementl.com>
2024-03-04 15:05:04 -0600

    Reference dagster/max_runtime tag in docs instead of the python constant (#20243)
    Summary:
    Since you might apply this tag in places other than python, the Python
    constant indirection here is causing more confusion than doing good.
    
    Test Plan: Inspect docs page
    
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- examples/docs_snippets/docs_snippets/deploying/monitoring_daemon/run_timeouts.py
+++ examples/docs_snippets/docs_snippets/deploying/monitoring_daemon/run_timeouts.py
@@ -1,13 +1,13 @@
 # start_timeout
-from dagster import MAX_RUNTIME_SECONDS_TAG, define_asset_job, job
+from dagster import define_asset_job, job
 
 
-@job(tags={MAX_RUNTIME_SECONDS_TAG: 10})
+@job(tags={"dagster/max_runtime": 10})
 def my_job():
     ...
 
 
 asset_job = define_asset_job(
-    name="some_job", selection="*", tags={MAX_RUNTIME_SECONDS_TAG: 10}
+    name="some_job", selection="*", tags={"dagster/max_runtime": 10}
 )
 # end_timeout
