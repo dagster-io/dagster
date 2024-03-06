17a44fe7fc6150873de29a7e5b2dcf018bcd2f31
Rex Ledesma <rex@dagsterlabs.com>
2024-03-06 10:52:26 -0500

    feat(dbt): add setting to allow duplicate source asset keys

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/dbt_projects/__init__.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/dbt_projects/__init__.py
@@ -9,6 +9,9 @@ test_dbt_alias_path = projects_path.joinpath("test_dagster_dbt_alias")
 test_dbt_model_versions_path = projects_path.joinpath("test_dagster_dbt_model_versions")
 test_dbt_python_interleaving_path = projects_path.joinpath("test_dagster_dbt_python_interleaving")
 test_dbt_semantic_models_path = projects_path.joinpath("test_dagster_dbt_semantic_models")
+test_duplicate_source_asset_key_path = projects_path.joinpath(
+    "test_dagster_duplicate_source_asset_key"
+)
 test_exceptions_path = projects_path.joinpath("test_dagster_exceptions")
 test_meta_config_path = projects_path.joinpath("test_dagster_meta_config")
 test_metadata_path = projects_path.joinpath("test_dagster_metadata")
