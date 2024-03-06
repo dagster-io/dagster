5487ee29d7cc8d84ab5a8b8ec6c76e6c2273ec28
Marco polo <salazarm@users.noreply.github.com>
2024-03-06 15:59:15 -0500

    Asset checks polish (#20263)
    ## Summary & Motivation
    <img width="1727" alt="Screenshot 2024-03-05 at 1 42 03 PM"
    src="https://github.com/dagster-io/dagster/assets/2286579/e1f51883-2b00-4d49-be73-f16c6c2f821b">
    
    1. Lowercase "result" in "Evaluation result".
    2. Move the collapse arrows to the right side
    3. Don't show the latest execution in the table since we already show it
    above
    
    ## How I Tested These Changes

--- js_modules/dagster-ui/packages/ui-components/src/components/CollapsibleSection.tsx
+++ js_modules/dagster-ui/packages/ui-components/src/components/CollapsibleSection.tsx
@@ -8,28 +8,48 @@ export const CollapsibleSection = ({
   headerWrapperProps,
   children,
   isInitiallyCollapsed = false,
+  arrowSide = 'left',
 }: {
   header: React.ReactNode;
   headerWrapperProps?: React.ComponentProps<typeof Box>;
   children: React.ReactNode;
   isInitiallyCollapsed?: boolean;
+  arrowSide?: 'left' | 'right';
 }) => {
   const [isCollapsed, setIsCollapsed] = React.useState(isInitiallyCollapsed);
   return (
     <Box flex={{direction: 'column'}}>
       <Box
         {...headerWrapperProps}
-        flex={{direction: 'row', alignItems: 'center', gap: 6, ...(headerWrapperProps?.flex || {})}}
+        flex={{
+          direction: 'row',
+          alignItems: 'center',
+          gap: 6,
+          grow: 1,
+          ...(headerWrapperProps?.flex || {}),
+        }}
         onClick={() => {
           setIsCollapsed(!isCollapsed);
           headerWrapperProps?.onClick?.();
         }}
       >
-        <Icon
-          name="arrow_drop_down"
-          style={{transform: isCollapsed ? 'rotate(-90deg)' : 'rotate(0deg)'}}
-        />
-        <div>{header}</div>
+        {arrowSide === 'left' ? (
+          <>
+            <Icon
+              name="arrow_drop_down"
+              style={{transform: isCollapsed ? 'rotate(-90deg)' : 'rotate(0deg)'}}
+            />
+            <div>{header}</div>
+          </>
+        ) : (
+          <Box flex={{justifyContent: 'space-between', alignItems: 'center'}}>
+            <div>{header}</div>
+            <Icon
+              name="arrow_drop_down"
+              style={{transform: isCollapsed ? 'rotate(-90deg)' : 'rotate(0deg)'}}
+            />
+          </Box>
+        )}
       </Box>
       {isCollapsed ? null : children}
     </Box>
