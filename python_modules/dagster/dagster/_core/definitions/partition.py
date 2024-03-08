e3d64fd6cda299a6a794c65d62305c36b9b3b874
Nick Schrock <schrockn@dagsterlabs.com>
2024-03-08 16:01:08 -0500

    Short circuit PartitionsSet operations regardless of order
    ## Summary & Motivation
    
    Owen made the sensible suggestion (https://github.com/dagster-io/dagster/pull/20312#discussion_r1516977615) to take advantage of native partition set features to avoid materializing partition keys with the AllPartitionsSubset is involved. This required changing operation order.
    
    Instead, this PR proposes to also special case the base class operators when "other" is AllPartitionsSubset so we don't have to think about order.
    
    
    ## How I Tested These Changes
    
    BK

--- python_modules/dagster/dagster/_core/definitions/partition.py
+++ python_modules/dagster/dagster/_core/definitions/partition.py
@@ -969,20 +969,29 @@ class PartitionsSubset(ABC, Generic[T_str]):
             )
         )
 
-    def __or__(self, other: "PartitionsSubset") -> "PartitionsSubset[T_str]":
+    def __or__(self, other: "PartitionsSubset") -> "PartitionsSubset":
         if self is other:
             return self
+        # Anything | AllPartitionsSubset = AllPartitionsSubset
+        if isinstance(other, AllPartitionsSubset):
+            return other
         return self.with_partition_keys(other.get_partition_keys())
 
-    def __sub__(self, other: "PartitionsSubset") -> "PartitionsSubset[T_str]":
+    def __sub__(self, other: "PartitionsSubset") -> "PartitionsSubset":
         if self is other:
+            return self.empty_subset()
+        # Anything - AllPartitionsSubset = Empty
+        if isinstance(other, AllPartitionsSubset):
             return self.empty_subset()
         return self.empty_subset().with_partition_keys(
             set(self.get_partition_keys()).difference(set(other.get_partition_keys()))
         )
 
-    def __and__(self, other: "PartitionsSubset") -> "PartitionsSubset[T_str]":
+    def __and__(self, other: "PartitionsSubset") -> "PartitionsSubset":
         if self is other:
+            return self
+        # Anything & AllPartitionsSubset = Anything
+        if isinstance(other, AllPartitionsSubset):
             return self
         return self.empty_subset().with_partition_keys(
             set(self.get_partition_keys()) & set(other.get_partition_keys())
