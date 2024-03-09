f70782ee2fbd815b597e26ae8b846e574e162d23
Nick Schrock <schrockn@dagsterlabs.com>
2024-03-09 13:54:47 -0500

    cp'

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
+# from typing import Iterable
 from dagster import Definitions, asset
-from dagster._core.definitions.code_server import CodeLocation, CodeServer
+from dagster._core.definitions.code_server import CodeLocation, CodeServer  # , ICodeServer
 
 
 @asset
-def an_asset() -> None:
+def an_asset_a() -> None:
     pass
 
 
@@ -11,7 +12,38 @@ class ACodeLocation(CodeLocation):
     name = "a_code_location"
 
     def load_definitions(self) -> Definitions:
-        return Definitions([an_asset])
+        return Definitions([an_asset_a])
 
 
-code_server = CodeServer(code_locations=[ACodeLocation()])
+@asset
+def an_asset_b() -> None:
+    pass
+
+
+class BCodeLocation(CodeLocation):
+    name = "b_code_location"
+
+    def load_definitions(self) -> Definitions:
+        return Definitions([an_asset_b])
+
+
+code_server = CodeServer(code_locations=[ACodeLocation(), BCodeLocation()])
+
+# class CustomCodeServer(ICodeServer):
+#     def load_code_locations(self) -> Iterable[CodeLocation]:
+#         return [
+#             ACodeLocationWithDefsBuiltFromPersistedState()
+#         ]
+
+# class ACodeLocationWithDefsBuiltFromPersistedState(CodeLocation):
+#     name = "a_code_location"
+
+#     def load_definitions(self) -> Definitions:
+#         return Definitions(
+#             build_assets_defs(
+#                 get_serialized_representation_of_assets_from_storage()
+#             )
+#         )
+
+
+# code_server = CustomCodeServer()
--- python_modules/dagster/dagster/_core/definitions/code_server.py
+++ python_modules/dagster/dagster/_core/definitions/code_server.py
@@ -1,17 +1,27 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import Iterable, List
 
 from dagster._core.definitions.definitions_class import Definitions
 
 
-class CodeServer:
+class ICodeServer(ABC):
+    # User can override this method to perform any necessary setup when the code server starts
+    def on_code_server_start(self) -> None: ...
+
+    @abstractmethod
+    def load_code_locations(self) -> Iterable["CodeLocation"]: ...
+
+
+class CodeServer(ICodeServer):
     code_locations: List["CodeLocation"]
 
     def __init__(self, code_locations: List["CodeLocation"]):
         self.code_locations = code_locations
 
-    # User can override this method to perform any necessary setup when the code server starts
     def on_code_server_start(self) -> None: ...
+
+    def load_code_locations(self) -> Iterable["CodeLocation"]:
+        return self.code_locations
 
 
 class CodeLocation(ABC):
