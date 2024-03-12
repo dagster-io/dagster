4c445405365169c657cecb7a12a6d872d5a6946e
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2024-03-12 13:51:54 -0700

    make Ask AI fab more prominent in the UI (#20439)
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- docs/next/pages/_document.js
+++ docs/next/pages/_document.js
@@ -31,7 +31,28 @@ export default class MyDocument extends Document {
               />
             </>
           )}
-          <co-pilot copilot_id="dagster"></co-pilot>
+          <co-pilot copilot_id="dagster">
+            <div
+              slot="fab"
+              className="bg-lavender text-gray-700 px-24 py-2 rounded-full flex flex-row items-center"
+            >
+              <div className="pt-1 pr-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <path
+                    d="M6 14H14V12H6V14ZM6 11H18V9H6V11ZM6 8H18V6H6V8ZM2 22V4C2 3.45 2.19583 2.97917 2.5875 2.5875C2.97917 2.19583 3.45 2 4 2H20C20.55 2 21.0208 2.19583 21.4125 2.5875C21.8042 2.97917 22 3.45 22 4V16C22 16.55 21.8042 17.0208 21.4125 17.4125C21.0208 17.8042 20.55 18 20 18H6L2 22ZM5.15 16H20V4H4V17.125L5.15 16Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </div>
+              <div>Ask AI</div>
+            </div>
+          </co-pilot>
           <script type="module" src="https://ui.scoutos.com/copilot.js"></script>
         </Head>
         <body>
