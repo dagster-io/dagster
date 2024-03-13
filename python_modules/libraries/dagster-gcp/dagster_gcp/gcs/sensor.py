c1482b601d30a8fdf399af731e937db70bc5abb8
Mathieu Larose <m.larose@dagsterlabs.com>
2024-03-13 15:15:53 -0400

    docs: dagster_gcp.gcs.sensor.get_gcs_keys code example (#20326)
    ## Summary & Motivation
    
    Add documentation to get_gcs_keys.
    
    ## How I Tested These Changes
    
    code example derived from what was used to test #20191
    
    ---------
    
    Co-authored-by: ajohnson5 <agjohnson1111@gmail.com>

--- python_modules/libraries/dagster-gcp/dagster_gcp/gcs/sensor.py
+++ python_modules/libraries/dagster-gcp/dagster_gcp/gcs/sensor.py
@@ -4,10 +4,54 @@ from google.cloud import storage
 MAX_KEYS = 1000
 
 
-def get_gcs_keys(bucket, prefix="", since_key=None, gcs_session=None):
-    """Credits to @ajohnson5 for the original implementation of this method."""
+def get_gcs_keys(bucket, prefix=None, since_key=None, gcs_session=None):
+    """Return a list of updated keys in a GCS bucket.
+
+    Args:
+        bucket (str): The name of the GCS bucket.
+        prefix (Optional[str]): The prefix to filter the keys by.
+        since_key (Optional[str]): The key to start from. If provided, only keys updated after this key will be returned.
+        gcs_session (Optional[google.cloud.storage.client.Client]): A GCS client session. If not provided, a new session will be created.
+
+    Returns:
+        List[str]: A list of keys in the bucket, sorted by update time, that are newer than the `since_key`.
+
+    Example:
+        .. code-block:: python
+
+            @resource
+            def google_cloud_storage_client(context):
+                return storage.Client().from_service_account_json("my-service-account.json")
+
+            @sensor(job=my_job, required_resource_keys={"google_cloud_storage_client"})
+            def my_gcs_sensor(context):
+                since_key = context.cursor or None
+                new_gcs_keys = get_gcs_keys(
+                    "my-bucket",
+                    prefix="data",
+                    since_key=since_key,
+                    gcs_session=context.resources.google_cloud_storage_client
+                )
+
+                if not new_gcs_keys:
+                    return SkipReason("No new gcs files found for bucket 'my-bucket'.")
+
+                for gcs_key in new_gcs_keys:
+                    yield RunRequest(run_key=gcs_key, run_config={
+                        "ops": {
+                            "gcs_files": {
+                                "config": {
+                                    "gcs_key": gcs_key
+                                }
+                            }
+                        }
+                    })
+
+                last_key = new_gcs_keys[-1]
+                context.update_cursor(last_key)
+    """
     check.str_param(bucket, "bucket")
-    check.str_param(prefix, "prefix")
+    check.opt_str_param(prefix, "prefix")
     check.opt_str_param(since_key, "since_key")
 
     if not gcs_session:
