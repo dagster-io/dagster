5f961bf949c9f78a2a67ae81346fe227defa7add
Jordan Sanders <jordan@dagsterlabs.com>
2024-03-20 22:12:38 -0500

    Revert "[RFC] feat(metadata): add table spec metadata" (#20613)
    Reverts dagster-io/dagster#20597
    
    I think this introduced a circular import that must not be fully under
    test?
    
    ```
    from dagster_graphql.implementation.events import iterate_metadata_entries
    ```
    
    ```
    ImportError: cannot import name 'iterate_metadata_entries' from partially initialized module 'dagster_graphql.implementation.events' (most likely due to a circular import) (/dagster/python_modules/dagster-graphql/dagster_graphql/implementation/events.py)
    
    ```

--- python_modules/dagster/dagster_tests/definitions_tests/metadata_tests/test_table_metadata_entries.py
+++ python_modules/dagster/dagster_tests/definitions_tests/metadata_tests/test_table_metadata_entries.py
@@ -1,11 +1,5 @@
-from dagster import AssetKey, AssetMaterialization, TableColumn, TableSchema
-from dagster._core.definitions.metadata import (
-    TableMetadataEntries,
-)
-from dagster._core.definitions.metadata.table import (
-    TableColumnDep,
-    TableColumnLineage,
-)
+from dagster import AssetMaterialization, TableColumn, TableSchema
+from dagster._core.definitions.metadata import TableMetadataEntries
 
 
 def test_table_metadata_entries():
@@ -24,33 +18,3 @@ def test_table_metadata_entries():
 
     assert dict(TableMetadataEntries()) == {}
     assert TableMetadataEntries.extract(dict(TableMetadataEntries())) == TableMetadataEntries()
-
-
-def test_column_specs() -> None:
-    expected_column_lineage = TableColumnLineage(
-        {
-            "column": [
-                TableColumnDep(
-                    asset_key=AssetKey("upstream"),
-                    column_name="upstream_column",
-                )
-            ]
-        }
-    )
-    expected_metadata = {"dagster/column_lineage": expected_column_lineage}
-
-    table_metadata_entries = TableMetadataEntries(column_lineage=expected_column_lineage)
-
-    dict_table_metadata_entries = dict(table_metadata_entries)
-    assert dict_table_metadata_entries == expected_metadata
-
-    materialization = AssetMaterialization(asset_key="foo", metadata=dict_table_metadata_entries)
-    extracted_table_metadata_entries = TableMetadataEntries.extract(materialization.metadata)
-    assert extracted_table_metadata_entries.column_lineage == expected_column_lineage
-
-    splat_table_metadata_entries = {**table_metadata_entries}
-    assert splat_table_metadata_entries == expected_metadata
-
-    materialization = AssetMaterialization(asset_key="foo", metadata=splat_table_metadata_entries)
-    extracted_table_metadata_entries = TableMetadataEntries.extract(materialization.metadata)
-    assert extracted_table_metadata_entries.column_lineage == expected_column_lineage
