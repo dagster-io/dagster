cb8f22cfe6b75b160ecc35353e46b299318f510b
Colton Padden <colton@dagsterlabs.com>
2024-03-21 13:00:03 -0400

    add sling metadata to ui ignore set

--- js_modules/dagster-ui/packages/ui-core/src/metadata/MetadataEntry.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/metadata/MetadataEntry.tsx
@@ -39,6 +39,8 @@ export const HIDDEN_METADATA_ENTRY_LABELS = new Set([
   'dagster-dbt/exclude',
   'dagster_dbt/manifest',
   'dagster_dbt/dagster_dbt_translator',
+  'dagster_sling/translator',
+  'dagster_sling/replication_config',
 ]);
 
 export const LogRowStructuredContentTable = ({
