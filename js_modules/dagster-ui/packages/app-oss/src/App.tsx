d27e821c2e634230be766e191e8246fb26a8cb35
Marco polo <salazarm@users.noreply.github.com>
2024-03-22 13:06:12 -0400

    Refactor `getNavLinks` and `rightOfSearchBar` into a single DI point for cloud (#20608)
    ## Summary & Motivation
    For cloud we want to optionally render only a single nav link and also
    render a custom catalog mode button. This can be achieved using the
    existing `getNavLinks` and `rightOfSearchbar` props but instead I think
    we can combine these into a single concept "AppTopNavRightOfOfLogo"
    (really rolls off the tongue)
    
    To allow cloud to override this new DI point easily I added
    `CloudComponentContext`, The context helps reduce some boilerplate with
    the `componentStub` method. I'm proposing we adopt a pattern of creating
    stub files wherever cloud has an injection point and using`.oss.tsx` for
    the actual oss implementation. This will help us identify where our DI
    points which will somewhat help us figure out if changes need to be
    tested in cloud, but I don't feel strongly and am open to dropping this
    convention or using a different one.
    
    ## How I Tested These Changes
    
    local host cloud

--- js_modules/dagster-ui/packages/app-oss/src/App.tsx
+++ js_modules/dagster-ui/packages/app-oss/src/App.tsx
@@ -2,7 +2,7 @@ import {App} from '@dagster-io/ui-core/app/App';
 import {createAppCache} from '@dagster-io/ui-core/app/AppCache';
 import {errorLink, setupErrorToasts} from '@dagster-io/ui-core/app/AppError';
 import {AppProvider} from '@dagster-io/ui-core/app/AppProvider';
-import {AppTopNav} from '@dagster-io/ui-core/app/AppTopNav';
+import {AppTopNav} from '@dagster-io/ui-core/app/AppTopNav/AppTopNav';
 import {ContentRoot} from '@dagster-io/ui-core/app/ContentRoot';
 import {HelpMenu} from '@dagster-io/ui-core/app/HelpMenu';
 import {UserSettingsButton} from '@dagster-io/ui-core/app/UserSettingsButton';
@@ -10,6 +10,7 @@ import {logLink, timeStartLink} from '@dagster-io/ui-core/app/apolloLinks';
 import {DeploymentStatusType} from '@dagster-io/ui-core/instance/DeploymentStatusProvider';
 import {LiveDataPollRateContext} from '@dagster-io/ui-core/live-data-provider/LiveDataProvider';
 
+import {InjectedComponents} from './InjectedComponents';
 import {CommunityNux} from './NUX/CommunityNux';
 import {extractInitializationData} from './extractInitializationData';
 import {telemetryLink} from './telemetryLink';
@@ -38,17 +39,19 @@ const appCache = createAppCache();
 // eslint-disable-next-line import/no-default-export
 export default function AppPage() {
   return (
-    <LiveDataPollRateContext.Provider value={liveDataPollRate ?? 2000}>
-      <AppProvider appCache={appCache} config={config}>
-        <AppTopNav searchPlaceholder="Search…" allowGlobalReload>
-          <HelpMenu showContactSales={false} />
-          <UserSettingsButton />
-        </AppTopNav>
-        <App>
-          <ContentRoot />
-          <CommunityNux />
-        </App>
-      </AppProvider>
-    </LiveDataPollRateContext.Provider>
+    <InjectedComponents>
+      <LiveDataPollRateContext.Provider value={liveDataPollRate ?? 2000}>
+        <AppProvider appCache={appCache} config={config}>
+          <AppTopNav searchPlaceholder="Search…" allowGlobalReload>
+            <HelpMenu showContactSales={false} />
+            <UserSettingsButton />
+          </AppTopNav>
+          <App>
+            <ContentRoot />
+            <CommunityNux />
+          </App>
+        </AppProvider>
+      </LiveDataPollRateContext.Provider>
+    </InjectedComponents>
   );
 }
