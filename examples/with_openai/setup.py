938ceb7bd90b1774d1e9811c43de83a7bb98a1b4
Maxime Armstrong <maxime@retain.ai>
2024-03-25 12:52:15 -0400

    Update requirements

--- examples/with_openai/setup.py
+++ examples/with_openai/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="with_openai",
     packages=find_packages(exclude=["with_openai_tests"]),
-    install_requires=["dagster", "dagster-cloud", "dagster-openai", "langchain==0.1.11"],
+    install_requires=["dagster", "dagster-openai", "langchain==0.1.11"],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
