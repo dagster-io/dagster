b4587db06e361986a54b2f68b53ac2e70826908b
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-03-24 20:32:36 -0700

    [docs] Use normal font-feature-settings for AskAI bot

--- docs/next/pages/_document.js
+++ docs/next/pages/_document.js
@@ -1,19 +1,19 @@
-import Document, {Head, Html, Main, NextScript} from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-import {GA_TRACKING_ID} from '../util/gtag';
+import { GA_TRACKING_ID } from '../util/gtag';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          {process.env.NODE_ENV === 'production' && (
-            <>
-              {/* Global Site Tag (gtag.js) - Google Analytics */}
-              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
+    render() {
+        return (
+            <Html>
+                <Head>
+                    {process.env.NODE_ENV === 'production' && (
+                        <>
+                            {/* Global Site Tag (gtag.js) - Google Analytics */}
+                            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -21,45 +21,45 @@ export default class MyDocument extends Document {
               page_path: window.location.pathname,
             });
           `,
-                }}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
+                                }}
+                            />
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
                   (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('bElvO73vM0E8ZMqj');`,
-                }}
-              />
-            </>
-          )}
-          <co-pilot copilot_id="dagster">
-            <div
-              slot="fab"
-              className="bg-lavender text-gray-700 px-24 py-2 rounded-full flex flex-row items-center"
-            >
-              <div className="pt-1 pr-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M6 14H14V12H6V14ZM6 11H18V9H6V11ZM6 8H18V6H6V8ZM2 22V4C2 3.45 2.19583 2.97917 2.5875 2.5875C2.97917 2.19583 3.45 2 4 2H20C20.55 2 21.0208 2.19583 21.4125 2.5875C21.8042 2.97917 22 3.45 22 4V16C22 16.55 21.8042 17.0208 21.4125 17.4125C21.0208 17.8042 20.55 18 20 18H6L2 22ZM5.15 16H20V4H4V17.125L5.15 16Z"
-                    fill="currentColor"
-                  />
-                </svg>
-              </div>
-              <div>Ask AI</div>
-            </div>
-          </co-pilot>
-          <script type="module" src="https://ui.scoutos.com/copilot.js"></script>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+                                }}
+                            />
+                        </>
+                    )}
+                    <co-pilot copilot_id="dagster" style={{ fontFeatureSettings: 'normal' }}>
+                        <div
+                            slot="fab"
+                            className="bg-lavender text-gray-700 px-24 py-2 rounded-full flex flex-row items-center"
+                        >
+                            <div className="pt-1 pr-1">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                >
+                                    <path
+                                        d="M6 14H14V12H6V14ZM6 11H18V9H6V11ZM6 8H18V6H6V8ZM2 22V4C2 3.45 2.19583 2.97917 2.5875 2.5875C2.97917 2.19583 3.45 2 4 2H20C20.55 2 21.0208 2.19583 21.4125 2.5875C21.8042 2.97917 22 3.45 22 4V16C22 16.55 21.8042 17.0208 21.4125 17.4125C21.0208 17.8042 20.55 18 20 18H6L2 22ZM5.15 16H20V4H4V17.125L5.15 16Z"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                            </div>
+                            <div>Ask AI</div>
+                        </div>
+                    </co-pilot>
+                    <script type="module" src="https://ui.scoutos.com/copilot.js"></script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
