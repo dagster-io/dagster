7466f059d6599c744f4593f34ce248dff3d1988c
Claire Lin <claire@dagsterlabs.com>
2024-03-26 09:20:27 -0700

    [ui] Rename asset catalog / overview files (#20719)
    Previously the new asset details page component was titled
    `AssetsCatalogRoot`, this PR renames it to `AssetsOverview` for clarity.

--- js_modules/dagster-ui/packages/ui-core/src/app/__tests__/AppTopNav.test.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/__tests__/AppTopNav.test.tsx
@@ -2,7 +2,7 @@ import {MockedProvider} from '@apollo/client/testing';
 import {render, screen} from '@testing-library/react';
 import {MemoryRouter} from 'react-router-dom';
 
-import AssetsCatalogRoot from '../../assets/AssetsCatalogRoot';
+import AssetsOverviewRoot from '../../assets/AssetsOverviewRoot';
 import {AppTopNav} from '../AppTopNav/AppTopNav';
 import {AppTopNavRightOfLogo} from '../AppTopNav/AppTopNavRightOfLogo.oss';
 import {InjectedComponentContext} from '../InjectedComponentContext';
@@ -16,7 +16,7 @@ describe('AppTopNav', () => {
   it('renders links and controls', async () => {
     render(
       <InjectedComponentContext.Provider
-        value={{AppTopNavRightOfLogo, AssetsOverview: AssetsCatalogRoot}}
+        value={{AppTopNavRightOfLogo, AssetsOverview: AssetsOverviewRoot}}
       >
         <MockedProvider>
           <MemoryRouter>
--- js_modules/dagster-ui/packages/ui-core/src/assets/types/AssetsOverviewRoot.types.ts
+++ js_modules/dagster-ui/packages/ui-core/src/assets/types/AssetsOverviewRoot.types.ts
@@ -2,11 +2,11 @@
 
 import * as Types from '../../graphql/types';
 
-export type AssetsCatalogRootQueryVariables = Types.Exact<{
+export type AssetsOverviewRootQueryVariables = Types.Exact<{
   assetKey: Types.AssetKeyInput;
 }>;
 
-export type AssetsCatalogRootQuery = {
+export type AssetsOverviewRootQuery = {
   __typename: 'Query';
   assetOrError:
     | {__typename: 'Asset'; id: string; key: {__typename: 'AssetKey'; path: Array<string>}}
