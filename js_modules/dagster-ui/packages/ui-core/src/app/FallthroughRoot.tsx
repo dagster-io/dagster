37044319c2546f80903b940985a99fd1ce97ffcb
Marco Salazar <marco@elementl.com>
2024-03-27 15:25:38 -0400

    default export

--- js_modules/dagster-ui/packages/ui-core/src/app/FallthroughRoot.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/FallthroughRoot.tsx
@@ -1,3 +1,6 @@
 import {componentStub} from './InjectedComponentContext';
 
 export const FallthroughRoot = componentStub('FallthroughRoot');
+
+// eslint-disable-next-line import/no-default-export
+export default FallthroughRoot;
