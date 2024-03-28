09ff794900ff6568e5b27bda709f54515b97bc19
Erin Cochran <erin.k.cochran@gmail.com>
2024-03-28 18:53:31 -0400

    Ruff

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/partitions_schedules_sensors_tests/backfills_tests/test_single_run_backfill_io_manager.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/partitions_schedules_sensors_tests/backfills_tests/test_single_run_backfill_io_manager.py
@@ -4,8 +4,8 @@ from dagster._core.storage.tags import (
     ASSET_PARTITION_RANGE_START_TAG,
 )
 from docs_snippets.concepts.partitions_schedules_sensors.backfills.single_run_backfill_io_manager import (
-    daily_partition,
     MyIOManager,
+    daily_partition,
     events,
     raw_events,
 )
