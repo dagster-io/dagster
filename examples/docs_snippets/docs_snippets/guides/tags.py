f60888604b3b105917449dc37565e8e53ace95ff
Erin Cochran <erin.k.cochran@gmail.com>
2024-03-28 15:11:50 -0400

    Fix code snippets

--- examples/docs_snippets/docs_snippets/guides/tags.py
+++ examples/docs_snippets/docs_snippets/guides/tags.py
@@ -1,4 +1,4 @@
-from dagster import asset, define_asset_job, job, to_job
+from dagster import asset, define_asset_job, graph, job, op, to_job
 
 # start_asset_tags
 
@@ -18,18 +18,30 @@ asset_job = define_asset_job(
 
 # end_asset_job_tags
 
+
+@op
+def send_email():
+    ...
+
+
 # start_op_job_tags
 
 
-@job(tags={"foo": "bar"})
-def my_job_with_tags():
-    my_op()
+@job(tags={"domain": "marketing"})
+def email_job():
+    send_email()
 
 
 # end_op_job_tags
 
+
+@graph
+def email_graph():
+    send_email()
+
+
 # start_tags_on_graph_to_job
 
-my_second_job_with_tags = my_graph.to_job(tags={"foo": "bar"})
+my_second_job_with_tags = email_graph.to_job(tags={"domain": "marketing"})
 
 # end_tags_on_graph_to_job
