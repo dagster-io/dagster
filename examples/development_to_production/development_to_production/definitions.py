7271ee7e0f466c83f5353fd3e7a2568df310a120
Erin Cochran <erin.k.cochran@gmail.com>
2024-03-28 20:03:10 -0400

    Update example files

--- examples/development_to_production/development_to_production/definitions.py
+++ examples/development_to_production/development_to_production/definitions.py
@@ -1,15 +1,15 @@
 import os
 
 from dagster import Definitions, EnvVar
-from dagster_snowflake_pandas import SnowflakePandasIOManager
+from dagster_snowflake import SnowflakeResource
 
 from development_to_production.assets import comments, items, stories
 from development_to_production.resources import HNAPIClient
 
-resource_defs = {
+resources = {
     "local": {
         "hn_client": HNAPIClient(),
-        "snowflake_io_manager": SnowflakePandasIOManager(
+        "snowflake_resource": SnowflakeResource(
             account="abc1234.us-east-1",
             user=EnvVar("DEV_SNOWFLAKE_USER"),
             password=EnvVar("DEV_SNOWFLAKE_PASSWORD"),
@@ -17,19 +17,9 @@ resource_defs = {
             schema=EnvVar("DEV_SNOWFLAKE_SCHEMA"),
         ),
     },
-    "staging": {
-        "hn_client": HNAPIClient(),
-        "snowflake_io_manager": SnowflakePandasIOManager(
-            account="abc1234.us-east-1",
-            user="system@company.com",
-            password=EnvVar("SYSTEM_SNOWFLAKE_PASSWORD"),
-            database="STAGING",
-            schema="HACKER_NEWS",
-        ),
-    },
     "production": {
         "hn_client": HNAPIClient(),
-        "snowflake_io_manager": SnowflakePandasIOManager(
+        "snowflake_resource": SnowflakeResource(
             account="abc1234.us-east-1",
             user="system@company.com",
             password=EnvVar("SYSTEM_SNOWFLAKE_PASSWORD"),
@@ -37,10 +27,19 @@ resource_defs = {
             schema="HACKER_NEWS",
         ),
     },
+    "staging": {
+        "hn_client": HNAPIClient(),
+        "snowflake_resource": SnowflakeResource(
+            account="abc1234.us-east-1",
+            user="system@company.com",
+            password=EnvVar("SYSTEM_SNOWFLAKE_PASSWORD"),
+            database="STAGING",
+            schema="HACKER_NEWS",
+        ),
+    },
 }
 deployment_name = os.getenv("DAGSTER_DEPLOYMENT", "local")
 
 defs = Definitions(
-    assets=[items, comments, stories],
-    resources=resource_defs[deployment_name],
+    assets=[items, comments, stories], resources=resources[deployment_name]
 )
