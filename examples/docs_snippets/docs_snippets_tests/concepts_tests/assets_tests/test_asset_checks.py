35e2e67b9f1d481455f511823037a1f167e67cab
Chris DeCarolis <cdguitar817@gmail.com>
2024-03-29 17:23:29 -0700

    docs snippets data freshness

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_asset_checks.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_asset_checks.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+from importlib import resources
+
+import mock
 import pytest
 from dagster_tests.cli_tests.command_tests.assets import fail_asset
 from mock import MagicMock
 
+from dagster import Definitions
 from dagster._core.definitions.asset_check_spec import AssetCheckKey
 from dagster._core.definitions.events import AssetKey
 from docs_snippets.concepts.assets.asset_checks.asset_with_check import (
@@ -13,14 +18,33 @@ from docs_snippets.concepts.assets.asset_checks.factory import (
     defs as factory_defs,
     make_check,
 )
+from docs_snippets.concepts.assets.asset_checks.materializable_freshness_complete import (
+    defs as materializable_freshness_complete_defs,
+)
+from docs_snippets.concepts.assets.asset_checks.materializable_freshness_in_pieces import (
+    defs as materializable_freshness_in_pieces_defs,
+)
 from docs_snippets.concepts.assets.asset_checks.metadata import defs as metadata_defs
 from docs_snippets.concepts.assets.asset_checks.orders_check import defs as orders_defs
 from docs_snippets.concepts.assets.asset_checks.severity import defs as severity_defs
+from docs_snippets.concepts.assets.asset_checks.source_data_freshness_complete import (
+    defs as source_data_freshness_complete_defs,
+)
+from docs_snippets.concepts.assets.asset_checks.source_data_freshness_in_pieces import (
+    defs as source_data_freshness_in_pieces_defs,
+)
 
 
 @pytest.mark.parametrize(
     "defs",
-    [orders_defs, asset_with_check_defs, severity_defs, metadata_defs],
+    [
+        orders_defs,
+        asset_with_check_defs,
+        severity_defs,
+        metadata_defs,
+        materializable_freshness_in_pieces_defs,
+        materializable_freshness_complete_defs,
+    ],
 )
 def test_execute(defs):
     job_def = defs.get_implicit_global_asset_job_def()
@@ -62,3 +86,25 @@ def test_blocking_execute():
     result = job_def.execute_in_process(raise_on_error=False)
     assert not result.success
     assert len(result.get_asset_materialization_events()) == 1
+
+
+@pytest.fixture
+def mock_fetch_last_updated_timestamps():
+    with mock.patch(
+        "docs_snippets.concepts.assets.asset_checks.source_data_freshness_in_pieces.fetch_last_updated_timestamps"
+    ) as m:
+        with mock.patch(
+            "docs_snippets.concepts.assets.asset_checks.source_data_freshness_complete.fetch_last_updated_timestamps"
+        ) as m_complete:
+            m.return_value = {"charges": 0.0, "customers": 0.0}
+            m_complete.return_value = {"charges": 0.0, "customers": 0.0}
+            yield
+
+
+def test_source_data_freshness(mock_fetch_last_updated_timestamps: None):
+    job_def = source_data_freshness_in_pieces_defs.get_implicit_global_asset_job_def()
+    result = job_def.execute_in_process(resources={"snowflake": MagicMock()})
+    assert result.success
+    job_def = source_data_freshness_complete_defs.get_implicit_global_asset_job_def()
+    result = job_def.execute_in_process(resources={"snowflake": MagicMock()})
+    assert result.success
