e04796f11ad2ad385b036dd017d833502ceead28
Isaac Hellendag <dish@elementl.com>
2024-04-02 12:12:28 -0500

    [ui] Top nav: iterate to build shortcut handling

--- js_modules/dagster-ui/packages/ui-core/src/app/AppTopNav/AppTopNavLinks.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/AppTopNav/AppTopNavLinks.tsx
@@ -1,5 +1,5 @@
 import {Box} from '@dagster-io/ui-components';
-import React from 'react';
+import {ReactNode} from 'react';
 import {useHistory} from 'react-router-dom';
 
 import {TopNavLink} from './AppTopNav';
@@ -9,113 +9,97 @@ import {FeatureFlag, featureEnabled} from '../Flags';
 import {ShortcutHandler} from '../ShortcutHandler';
 
 export type AppNavLinkType = {
-  title: string;
-  element: React.ReactNode;
+  key: string;
+  path: string;
+  element: ReactNode;
 };
 
 export const AppTopNavLinks = ({links}: {links: AppNavLinkType[]}) => {
+  const history = useHistory();
   return (
     <Box margin={{left: 8}} flex={{direction: 'row', alignItems: 'center', gap: 16}}>
-      {links.map((link) => link.element)}
+      {links.map((link, ii) => {
+        const {key, path, element} = link;
+        return (
+          <ShortcutHandler
+            key={key}
+            onShortcut={() => history.push(path)}
+            shortcutLabel={`⌥${ii + 1}`}
+            shortcutFilter={(e) => e.altKey && e.code === `Digit${ii + 1}`}
+          >
+            {element}
+          </ShortcutHandler>
+        );
+      })}
     </Box>
   );
 };
 
-export const navLinks = (history: ReturnType<typeof useHistory>) => {
+export const navLinks = () => {
   return [
     {
-      title: 'overview' as const,
+      key: 'overview',
+      path: '/overview',
       element: (
-        <ShortcutHandler
-          key="overview"
-          onShortcut={() => history.push('/overview')}
-          shortcutLabel="⌥1"
-          shortcutFilter={(e) => e.altKey && e.code === 'Digit1'}
-        >
-          <TopNavLink to="/overview" data-cy="AppTopNav_StatusLink">
-            Overview
-          </TopNavLink>
-        </ShortcutHandler>
+        <TopNavLink to="/overview" data-cy="AppTopNav_StatusLink">
+          Overview
+        </TopNavLink>
       ),
     },
     {
-      title: 'runs' as const,
+      key: 'runs',
+      path: '/runs',
       element: (
-        <ShortcutHandler
-          key="runs"
-          onShortcut={() => history.push('/runs')}
-          shortcutLabel="⌥2"
-          shortcutFilter={(e) => e.altKey && e.code === 'Digit2'}
-        >
-          <TopNavLink to="/runs" data-cy="AppTopNav_RunsLink">
-            Runs
-          </TopNavLink>
-        </ShortcutHandler>
+        <TopNavLink to="/runs" data-cy="AppTopNav_RunsLink">
+          Runs
+        </TopNavLink>
       ),
     },
     {
-      title: 'assets' as const,
+      key: 'assets',
+      path: '/assets',
       element: (
-        <ShortcutHandler
-          key="assets"
-          onShortcut={() => history.push('/assets')}
-          shortcutLabel="⌥3"
-          shortcutFilter={(e) => e.altKey && e.code === 'Digit3'}
+        <TopNavLink
+          to="/assets"
+          data-cy="AppTopNav_AssetsLink"
+          isActive={assetsPathMatcher}
+          exact={false}
         >
-          <TopNavLink
-            to="/assets"
-            data-cy="AppTopNav_AssetsLink"
-            isActive={assetsPathMatcher}
-            exact={false}
-          >
-            Assets
-          </TopNavLink>
-        </ShortcutHandler>
+          Assets
+        </TopNavLink>
       ),
     },
     featureEnabled(FeatureFlag.flagSettingsPage)
       ? {
-          title: 'settings' as const,
+          key: 'settings',
+          path: '/settings',
           element: (
-            <ShortcutHandler
-              key="settings"
-              onShortcut={() => history.push('/settings')}
-              shortcutLabel="⌥4"
-              shortcutFilter={(e) => e.altKey && e.code === 'Digit4'}
+            <TopNavLink
+              to="/settings"
+              data-cy="AppTopNav_SettingsLink"
+              isActive={settingsPathMatcher}
             >
-              <TopNavLink
-                to="/settings"
-                data-cy="AppTopNav_SettingsLink"
-                isActive={settingsPathMatcher}
-              >
-                <Box flex={{direction: 'row', alignItems: 'center', gap: 6}}>
-                  Settings
-                  <DeploymentStatusIcon />
-                </Box>
-              </TopNavLink>
-            </ShortcutHandler>
+              <Box flex={{direction: 'row', alignItems: 'center', gap: 6}}>
+                Settings
+                <DeploymentStatusIcon />
+              </Box>
+            </TopNavLink>
           ),
         }
       : {
-          title: 'deployment' as const,
+          key: 'deployment',
+          path: '/locations',
           element: (
-            <ShortcutHandler
-              key="deployment"
-              onShortcut={() => history.push('/locations')}
-              shortcutLabel="⌥4"
-              shortcutFilter={(e) => e.altKey && e.code === 'Digit4'}
+            <TopNavLink
+              to="/locations"
+              data-cy="AppTopNav_StatusLink"
+              isActive={locationPathMatcher}
             >
-              <TopNavLink
-                to="/locations"
-                data-cy="AppTopNav_StatusLink"
-                isActive={locationPathMatcher}
-              >
-                <Box flex={{direction: 'row', alignItems: 'center', gap: 6}}>
-                  Deployment
-                  <DeploymentStatusIcon />
-                </Box>
-              </TopNavLink>
-            </ShortcutHandler>
+              <Box flex={{direction: 'row', alignItems: 'center', gap: 6}}>
+                Deployment
+                <DeploymentStatusIcon />
+              </Box>
+            </TopNavLink>
           ),
         },
   ];
