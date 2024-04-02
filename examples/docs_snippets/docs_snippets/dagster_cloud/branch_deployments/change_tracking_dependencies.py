b87340e8a58511cf06412bc0bd163d08b2a554ee
JamieDeMaria <jamie@dagsterlabs.com>
2024-04-02 12:29:34 -0400

    images

--- examples/docs_snippets/docs_snippets/dagster_cloud/branch_deployments/change_tracking_dependencies.py
+++ examples/docs_snippets/docs_snippets/dagster_cloud/branch_deployments/change_tracking_dependencies.py
@@ -2,27 +2,24 @@ from dagster import asset
 
 
 @asset
-def orders():
-    ...
+def orders(): ...
 
 
 @asset
-def customers():
-    ...
+def customers(): ...
 
 
-# start_main_deployment
-@asset(deps=[orders])
-def returns():
-    ...
+def scope_main_deployment():
+    # start_main_deployment
+    @asset(deps=[orders])
+    def returns(): ...
 
-# end_main_deployment
-
-# start_branch_deployment
-
-@asset(deps=[orders, customers])
-def returns():
-    ...
+    # end_main_deployment
 
 
-# end_branch_deployment
+def scope_branch_deployment():
+    # start_branch_deployment
+    @asset(deps=[orders, customers])
+    def returns(): ...
+
+    # end_branch_deployment
--- examples/docs_snippets/docs_snippets/dagster_cloud/branch_deployments/change_tracking_partitions_definition.py
+++ examples/docs_snippets/docs_snippets/dagster_cloud/branch_deployments/change_tracking_partitions_definition.py
@@ -1,19 +1,17 @@
 from dagster import WeeklyPartitionsDefinition, asset
 
 
-# start_main_deployment
-@asset(partitions_def=WeeklyPartitionsDefinition(start_date="2024-01-01"))
-def weekly_orders():
-    ...
+def scope_main_deployment():
+    # start_main_deployment
+    @asset(partitions_def=WeeklyPartitionsDefinition(start_date="2024-01-01"))
+    def weekly_orders(): ...
+
+    # end_main_deployment
 
 
-# end_main_deployment
+def scope_branch_deployment():
+    # start_branch_deployment
+    @asset(partitions_def=WeeklyPartitionsDefinition(start_date="2023-01-01"))
+    def weekly_orders(): ...
 
-
-# start_branch_deployment
-@asset(partitions_def=WeeklyPartitionsDefinition(start_date="2023-01-01"))
-def weekly_orders():
-    ...
-
-
-# end_branch_deployment
+    # end_branch_deployment
