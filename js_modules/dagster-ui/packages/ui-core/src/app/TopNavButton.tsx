b590d3b3a5267d49e003ce063c049e80a41f2b08
Isaac Hellendag <dish@elementl.com>
2024-04-02 13:45:09 -0500

    [ui] Add tooltip with keyboard shortcut info on top nav items

--- js_modules/dagster-ui/packages/ui-core/src/app/TopNavButton.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/TopNavButton.tsx
@@ -1,4 +1,5 @@
-import {Colors, IconWrapper, UnstyledButton} from '@dagster-io/ui-components';
+import {Box, Colors, FontFamily, IconWrapper, UnstyledButton} from '@dagster-io/ui-components';
+import {ReactNode} from 'react';
 import styled from 'styled-components';
 
 export const TopNavButton = styled(UnstyledButton)`
@@ -27,4 +28,26 @@ export const TopNavButton = styled(UnstyledButton)`
   :focus ${IconWrapper}, :hover ${IconWrapper} {
     background-color: ${Colors.navTextHover()};
   }
+`;
+
+interface TooltipWithShortcutInfoProps {
+  label: ReactNode;
+  shortcutKey: string;
+}
+
+export const TooltipShortcutInfo = ({label, shortcutKey}: TooltipWithShortcutInfoProps) => {
+  return (
+    <Box flex={{direction: 'row', gap: 8, alignItems: 'center'}}>
+      <div>{label}</div>
+      <TooltipShortcutKey>{shortcutKey}</TooltipShortcutKey>
+    </Box>
+  );
+};
+
+export const TooltipShortcutKey = styled.div`
+  background-color: ${Colors.navButton()};
+  border-radius: 3px;
+  font-family: ${FontFamily.monospace};
+  padding: 1px 6px;
+  box-shadow: inset 0 0 0 1px ${Colors.borderHover()};
 `;
