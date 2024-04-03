9d2ab0c3a2bb5da33dd56209d8f0f0ec5f5bc699
Erin Cochran <erin.k.cochran@gmail.com>
2024-04-03 10:59:18 -0400

    Add draft

--- examples/docs_snippets/docs_snippets/concepts/assets/asset_definition_metadata.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_definition_metadata.py
@@ -1,11 +1,26 @@
+from integrations.duckdb.tutorial.resource.create_table import (
+    iris_dataset,
+)  # noqa: I001
+
+# start
+
+from dagster_duckdb import DuckDBResource
+
 from dagster import asset
 
-# start_example
+
+@asset(
+    deps=[iris_dataset],
+    metadata={
+        "Expected columns": "sepal_length_cm, sepal_width_cm, petal_length_cm, petal_width_cm, species"
+    },
+)
+def small_petals(duckdb: DuckDBResource) -> None:
+    with duckdb.get_connection() as conn:
+        conn.execute(
+            "CREATE TABLE iris.small_petals AS SELECT * FROM iris.iris_dataset WHERE"
+            " 'petal_length_cm' < 1 AND 'petal_width_cm' < 1"
+        )
 
 
-@asset(metadata={"priority": "high"})
-def my_asset():
-    return 5
-
-
-# end_example
+# end
