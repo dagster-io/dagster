d8d6de6e49b65818af6a18d8a901e50785d735be
Marco Salazar <marco@elementl.com>
2024-04-08 16:16:28 -0400

    feedback

--- js_modules/dagster-ui/packages/ui-core/src/app/DagsterPlusLaunchPromotion.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/DagsterPlusLaunchPromotion.tsx
@@ -1,16 +1,15 @@
 import {
-  Body,
+  Body1,
+  Box,
+  Button,
   DagsterTheme,
   Dialog,
-  DialogBody,
-  DialogFooter,
   ExternalAnchorButton,
   getTheme,
 } from '@dagster-io/ui-components';
 import styled from 'styled-components';
 
-import dagster_plus_light from './dagster_plus-primary-horizontal.svg';
-import dagster_plus_dark from './dagster_plus-reversed-horizontal.svg';
+import dagster_plus from './dagster_plus.png';
 import {useStateWithStorage} from '../hooks/useStateWithStorage';
 
 // April 17th at 12:00 ET
@@ -33,63 +32,49 @@ export const DagsterPlusLaunchPromotion = () => {
       }}
       canOutsideClickClose
       canEscapeKeyClose
-      title={
-        <Header $isDarkMode={isDarkMode}>
-          Join us for the unveiling of <span style={{fontWeight: 800}}>Dagster+</span>, the next
-          generation of Dagster Cloud.
-        </Header>
-      }
+      style={{width: 800, maxWidth: '80%'}}
     >
-      <DialogBody>
-        <ImageWrapper>
-          <img src={isDarkMode ? dagster_plus_dark.src : dagster_plus_light.src} />
-        </ImageWrapper>
-        <Body className="text-xl font-normal pt-2 text-gable-green md:px-8">
-          On <span style={{fontWeight: 600}}>April 17th at 12:00 ET</span> the Dagster Labs team
-          will unveil the next generation of Dagster Cloud.
-        </Body>
-      </DialogBody>
-      <DialogFooter topBorder>
-        <ExternalAnchorButton
-          href="https://dagster.io/events/dagster-plus-launch-event"
-          target="_blank"
-          rel="noreferrer"
+      <ImageWrapper>
+        <img src={dagster_plus.src} />
+      </ImageWrapper>
+      <Box flex={{direction: 'column', alignItems: 'center'}} padding={24}>
+        <Header>Introducing Dagster+</Header>
+        <Body1>Join us for the unveiling of the next generation of Dagster Cloud.</Body1>
+        <Box
+          flex={{direction: 'row', alignItems: 'center', justifyContent: 'center', gap: 8}}
+          padding={{top: 12}}
         >
-          Learn more
-        </ExternalAnchorButton>
-      </DialogFooter>
+          <ExternalAnchorButton
+            href="https://dagster.io/events/dagster-plus-launch-event"
+            target="_blank"
+            rel="noreferrer"
+            intent="primary"
+          >
+            Learn more
+          </ExternalAnchorButton>
+          <Button
+            onClick={() => {
+              setDidDismissPromotion(true);
+            }}
+          >
+            Close
+          </Button>
+        </Box>
+      </Box>
     </Dialog>
   );
 };
 
-const Header = styled.div<{$isDarkMode: boolean}>`
-  ${({$isDarkMode}) => {
-    if ($isDarkMode) {
-      return `
-      background: linear-gradient(0deg, #4f43dd, #ac00fc 40%, #5756ab);
-      background: -webkit-linear-gradient(0deg, #4f43dd, #ac00fc 40%, #5756ab);
-      `;
-    }
-    return `
-    background: linear-gradient(0deg, #413a9b, #7e12b0 40%, #434278);
-    background: -webkit-linear-gradient(0deg, #413a9b, #7e12b0 40%, #434278);
-    `;
-  }}
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-  -webkit-text-fill-color: transparent;
-  font-weight: 400;
-  font-size: 18px;
-  white-space: normal;
+const Header = styled.div`
+  font-size: 32px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
 `;
 
 const ImageWrapper = styled.div`
-  background-attachment: fixed;
-  border-radius: 36px;
-  padding: 16px 40px;
   img {
-    max-height: 176px;
     max-width: 100%;
   }
 }
