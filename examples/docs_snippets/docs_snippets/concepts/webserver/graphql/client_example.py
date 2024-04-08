2d1fbd0979c38fce011dc52604b64528d388f607
Erin Cochran <erin.k.cochran@gmail.com>
2024-04-08 13:25:14 -0400

    Cloud > Dagster+ in copy

--- examples/docs_snippets/docs_snippets/concepts/webserver/graphql/client_example.py
+++ examples/docs_snippets/docs_snippets/concepts/webserver/graphql/client_example.py
@@ -106,7 +106,7 @@ else:
 
 # start_cloud_usage
 url = "yourorg.dagster.cloud/prod"  # Your deployment-scoped url
-user_token = (  # a User Token generated from the Organization Settings page in Dagster Cloud.
+user_token = (  # a User Token generated from the Organization Settings page in Dagster+.
     "your_token_here"
 )
 client = DagsterGraphQLClient(url, headers={"Dagster-Cloud-Api-Token": user_token})
