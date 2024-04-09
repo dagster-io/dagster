ff793c884bd235283bd8098bb7d65e522b3109e5
Marco polo <salazarm@users.noreply.github.com>
2024-04-09 14:28:40 -0400

    Kill old asset automation page (#21098)
    ## Summary & Motivation
    Removing feature gate
    
    ## How I Tested These Changes
    <img width="1728" alt="Screenshot 2024-04-09 at 12 56 58 PM"
    src="https://github.com/dagster-io/dagster/assets/2286579/b82f8f17-d26c-43c0-b5d6-812cf352ac62">
    
    locally:

--- js_modules/dagster-ui/packages/ui-core/src/assets/AssetView.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/AssetView.tsx
@@ -20,7 +20,6 @@ import {AssetPartitions} from './AssetPartitions';
 import {AssetPlotsPage} from './AssetPlotsPage';
 import {AssetTabs} from './AssetTabs';
 import {AssetAutomaterializePolicyPage} from './AutoMaterializePolicyPage/AssetAutomaterializePolicyPage';
-import {AssetAutomaterializePolicyPageOld} from './AutoMaterializePolicyPageOld/AssetAutomaterializePolicyPage';
 import {useAutoMaterializeSensorFlag} from './AutoMaterializeSensorFlag';
 import {AutomaterializeDaemonStatusTag} from './AutomaterializeDaemonStatusTag';
 import {ChangedReasonsTag} from './ChangedReasons';
@@ -66,7 +65,7 @@ interface Props {
 export const AssetView = ({assetKey, trace, headerBreadcrumbs}: Props) => {
   const [params, setParams] = useQueryPersistedState<AssetViewParams>({});
   const {useTabBuilder, renderFeatureView} = useContext(AssetFeatureContext);
-  const {flagUseNewOverviewPage, flagUseNewAutomationPage} = useFeatureFlags();
+  const {flagUseNewOverviewPage} = useFeatureFlags();
 
   // Load the asset definition
   const {definition, definitionQueryResult, lastMaterialization} =
@@ -222,15 +221,7 @@ export const AssetView = ({assetKey, trace, headerBreadcrumbs}: Props) => {
     if (definitionQueryResult.loading && !definitionQueryResult.previousData) {
       return <AssetLoadingDefinitionState />;
     }
-    if (flagUseNewAutomationPage) {
-      return <AssetAutomaterializePolicyPage assetKey={assetKey} definition={definition} />;
-    }
-    return (
-      <AssetAutomaterializePolicyPageOld
-        assetKey={assetKey}
-        assetHasDefinedPartitions={!!definition?.partitionDefinition}
-      />
-    );
+    return <AssetAutomaterializePolicyPage assetKey={assetKey} definition={definition} />;
   };
 
   const renderChecksTab = () => {
