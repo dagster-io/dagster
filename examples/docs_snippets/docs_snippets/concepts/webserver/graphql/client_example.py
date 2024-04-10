9bb5721cf553ca1c21cfee5100ecdf009b68eb08
Erin Cochran <erin.k.cochran@gmail.com>
2024-04-10 12:47:39 -0400

    dagster.cloud to dagster.plus

--- examples/docs_snippets/docs_snippets/concepts/webserver/graphql/client_example.py
+++ examples/docs_snippets/docs_snippets/concepts/webserver/graphql/client_example.py
@@ -105,7 +105,7 @@ else:
 # end_shutdown_repo_location_marker
 
 # start_cloud_usage
-url = "yourorg.dagster.cloud/prod"  # Your deployment-scoped url
+url = "yourorg.dagster.plus/prod"  # Your deployment-scoped url
 user_token = (  # a User Token generated from the Organization Settings page in Dagster+.
     "your_token_here"
 )
