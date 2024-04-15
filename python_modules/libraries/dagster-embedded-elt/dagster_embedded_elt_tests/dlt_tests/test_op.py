e4de62c82b9e471438a5337c23fb88bda588553e
colton <cmpadden@users.noreply.github.com>
2024-04-15 16:49:58 -0400

    [dagster-embedded-elt][dlt] refactor unit tests to use shared fixtures (#21199)

--- python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt_tests/dlt_tests/test_op.py
+++ python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt_tests/dlt_tests/test_op.py
@@ -1,37 +1,33 @@
-import dlt
 import duckdb
 from dagster import OpExecutionContext, job, op
 from dagster_embedded_elt.dlt import DagsterDltResource
+from dlt import Pipeline
 
 from .dlt_test_sources.duckdb_with_transformer import pipeline
 
-EXAMPLE_PIPELINE_DUCKDB = "example_pipeline.duckdb"
 
-
-def test_base_dlt_op():
+def test_base_dlt_op(dlt_pipeline: Pipeline) -> None:
     @op(out={})
     def my_dlt_op_yield_events(context: OpExecutionContext, dlt_resource: DagsterDltResource):
         yield from dlt_resource.run(
             context=context,
             dlt_source=pipeline(),
-            dlt_pipeline=dlt.pipeline(
-                pipeline_name="example_pipeline",
-                dataset_name="example",
-                destination="duckdb",
-            ),
+            dlt_pipeline=dlt_pipeline,
         )
 
     @job
-    def my_dlt_op_yield_events_job():
+    def my_dlt_op_yield_events_job() -> None:
         my_dlt_op_yield_events()
 
     res = my_dlt_op_yield_events_job.execute_in_process(
         resources={"dlt_resource": DagsterDltResource()}
     )
+
     assert res.success
     assert len(res.get_asset_materialization_events()) == 2
 
-    with duckdb.connect(database=EXAMPLE_PIPELINE_DUCKDB, read_only=True) as conn:
+    duckdb_path = f"{dlt_pipeline.pipeline_name}.duckdb"
+    with duckdb.connect(database=duckdb_path, read_only=True) as conn:
         row = conn.execute("select count(*) from example.repos").fetchone()
         assert row and row[0] == 3
 
