b1f98f8211a29a2f27ecc85f3686c936d703d01a
elementl <devtools@elementl.com>
2024-04-16 20:25:30 +0000

    1.7.2rc2

--- python_modules/libraries/dagster-duckdb-pyspark/setup.py
+++ python_modules/libraries/dagster-duckdb-pyspark/setup.py
@@ -34,8 +34,8 @@ setup(
     include_package_data=True,
     python_requires=">=3.8,<3.13",
     install_requires=[
-        f"dagster{pin}",
-        f"dagster-duckdb{pin}",
+        "dagster==1.7.2rc2",
+        "dagster-duckdb==0.23.2rc2",
         "pyspark>=3",
         # Pinned pending duckdb removal of broken pandas import. Pin can be
         # removed as soon as it produces a working build.
