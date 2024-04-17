6d7bae38af3c528014ecc3fc641660a456ffd16b
Erin Cochran <erin.k.cochran@gmail.com>
2024-04-17 11:55:46 -0400

    Update test example

--- examples/docs_snippets/docs_snippets/guides/dagster/development_to_production/test_assets.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/development_to_production/test_assets.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from dagster import ItemsConfig, SnowflakeResource
+
 from .assets_v2 import ItemsConfig, items
 from .resources.resources_v2 import StubHNClient
 
@@ -8,17 +10,32 @@ from .resources.resources_v2 import StubHNClient
 
 
 def test_items():
-    hn_dataset = items(
-        config=ItemsConfig(base_item_id=StubHNClient().fetch_max_item_id()),
-        hn_client=StubHNClient(),
+    test_snowflake = (
+        SnowflakeResource(
+            account="abc1234.us-east-1",
+            user="test@company.com",
+            password=os.getenv("TEST_SNOWFLAKE_PASSWORD"),
+            database="TEST",
+            schema="HACKER_NEWS",
+        ),
     )
-    assert isinstance(hn_dataset, pd.DataFrame)
+    try:
+        items(
+            config=ItemsConfig(base_item_id=StubHNClient().fetch_max_item_id()),
+            hn_client=StubHNClient(),
+            snowflake_resource=test_snowflake,
+        )
+        with test_snowflake.get_connection() as conn:
+            hn_dataset = conn.cursor.execute("select * from ITEMS").fetch_pandas_all()
+            assert isinstance(hn_dataset, pd.DataFrame)
+            expected_data = pd.DataFrame(StubHNClient().data.values()).rename(
+                columns={"by": "user_id"}
+            )
+            assert (hn_dataset == expected_data).all().all()
 
-    expected_data = pd.DataFrame(StubHNClient().data.values()).rename(
-        columns={"by": "user_id"}
-    )
-
-    assert (hn_dataset == expected_data).all().all()
+    finally:
+        with test_snowflake.get_connection() as conn:
+            conn.cursor.execute("drop table if exists ITEMS")
 
 
 # end
