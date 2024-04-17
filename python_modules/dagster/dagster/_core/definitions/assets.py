58867b71c5f48b1da0e31ce36a3d14ab37826abb
Edvard Lindelöf <edvardlindelof@gmail.com>
2024-04-17 17:22:16 +0200

    annotate AssetsDefinition.__call__ with Any instead of object

--- python_modules/dagster/dagster/_core/definitions/assets.py
+++ python_modules/dagster/dagster/_core/definitions/assets.py
@@ -427,7 +427,7 @@ class AssetsDefinition(ResourceAddable, RequiresResources, IHasInternalInit):
             owners_by_key=owners_by_key,
         )
 
-    def __call__(self, *args: object, **kwargs: object) -> object:
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
         from .composition import is_in_composition
         from .graph_definition import GraphDefinition
 
