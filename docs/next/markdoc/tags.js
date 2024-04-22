f054fa9f661680cae83dd5f97f1182c847a0aef8
Niko <39569270+nikomancy@users.noreply.github.com>
2024-04-22 15:45:07 -0600

    Translated mdx buttons into markdoc component

--- docs/next/markdoc/tags.js
+++ docs/next/markdoc/tags.js
@@ -1,3 +1,4 @@
+import {Button} from '../components/markdoc/Button';
 import {Note, Warning} from '../components/markdoc/Callouts';
 
 export const note = {
@@ -6,4 +7,19 @@ export const note = {
 
 export const warning = {
   render: Warning,
+};
+
+export const button = {
+  render: Button,
+  children: ['text'],
+  attributes: {
+    link: {
+      type: String,
+    },
+    style: {
+      type: String,
+      default: 'primary',
+      matches: ['primary', 'secondary', 'blurple'],
+    },
+  },
 };
