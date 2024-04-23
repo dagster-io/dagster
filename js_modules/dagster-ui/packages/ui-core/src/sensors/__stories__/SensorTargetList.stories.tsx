8f4fb5cd73def9532f5478f41ade55f90beb5146
Marco Salazar <marco@elementl.com>
2024-04-23 14:07:43 -0400

    storybook

--- js_modules/dagster-ui/packages/ui-core/src/sensors/__stories__/SensorTargetList.stories.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/sensors/__stories__/SensorTargetList.stories.tsx
@@ -1,7 +1,9 @@
 import {MockedProvider} from '@apollo/client/testing';
 import {Meta} from '@storybook/react';
 
+import {CustomAlertProvider} from '../../app/CustomAlertProvider';
 import {SensorType, buildAssetSelection, buildPythonError, buildSensor} from '../../graphql/types';
+import {WorkspaceProvider} from '../../workspace/WorkspaceContext';
 import {buildRepoAddress} from '../../workspace/buildRepoAddress';
 import {SensorTargetList} from '../SensorTargetList';
 import {
@@ -17,24 +19,29 @@ export default {
 
 export const SensorTargetListWithError = () => {
   return (
-    <MockedProvider mocks={[buildStartKansasSuccess(1000), buildStartLouisianaError(1000)]}>
-      <SensorTargetList
-        sensorType={SensorType.ASSET}
-        targets={[{pipelineName: 'any'}]}
-        repoAddress={buildRepoAddress('a', 'b')}
-        selectionQueryResult={
-          {
-            data: {
-              __typename: 'Query',
-              sensorOrError: buildSensor({
-                assetSelection: buildAssetSelection({
-                  assetsOrError: buildPythonError(),
-                }),
-              }),
-            },
-          } as any
-        }
-      />
-    </MockedProvider>
+    <>
+      <CustomAlertProvider />
+      <MockedProvider mocks={[buildStartKansasSuccess(1000), buildStartLouisianaError(1000)]}>
+        <WorkspaceProvider>
+          <SensorTargetList
+            sensorType={SensorType.ASSET}
+            targets={[{pipelineName: 'pipelineName'}]}
+            repoAddress={buildRepoAddress('a', 'b')}
+            selectionQueryResult={
+              {
+                data: {
+                  __typename: 'Query',
+                  sensorOrError: buildSensor({
+                    assetSelection: buildAssetSelection({
+                      assetsOrError: buildPythonError(),
+                    }),
+                  }),
+                },
+              } as any
+            }
+          />
+        </WorkspaceProvider>
+      </MockedProvider>
+    </>
   );
 };
