de31a154b951e824b6cf15698ada44f75bd290a4
Maxime Armstrong <maxime@retain.ai>
2024-04-23 10:58:14 -0400

    Fix tests for dbt 1.5

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/cli/test_prepare_for_deployment.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/cli/test_prepare_for_deployment.py
@@ -84,7 +84,7 @@ def test_prepare_for_deployment_with_dependencies(
     dependencies_path = dbt_project_dir.joinpath("dependencies.yml")
     packages_path = dbt_project_dir.joinpath("packages.yml")
     packages_install_path = dbt_project_dir.joinpath("dbt_packages")
-    dbt_utils_dbt_project_path = packages_install_path.joinpath("dbt_utils/dbt_project.yml")
+    manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
 
     # Scaffold doesn't include a dependencies.yml file, let's create one
     with open(dependencies_path, "w") as file:
@@ -99,7 +99,7 @@ def test_prepare_for_deployment_with_dependencies(
     assert dependencies_path.exists()
     assert not packages_path.exists()
     assert not packages_install_path.exists()
-    assert not dbt_utils_dbt_project_path.exists()
+    assert not manifest_path.exists()
 
     result = runner.invoke(
         app,
@@ -113,7 +113,7 @@ def test_prepare_for_deployment_with_dependencies(
 
     assert result.exit_code == 0
     assert packages_install_path.exists()
-    assert dbt_utils_dbt_project_path.exists()
+    assert manifest_path.exists()
 
 
 def test_prepare_for_deployment_with_packages(
@@ -142,7 +142,7 @@ def test_prepare_for_deployment_with_packages(
     dependencies_path = dbt_project_dir.joinpath("dependencies.yml")
     packages_path = dbt_project_dir.joinpath("packages.yml")
     packages_install_path = dbt_project_dir.joinpath("dbt_packages")
-    dbt_utils_dbt_project_path = packages_install_path.joinpath("dbt_utils/dbt_project.yml")
+    manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
 
     # Scaffold doesn't include a packages.yml file, let's create one
     with open(packages_path, "w") as file:
@@ -157,7 +157,7 @@ def test_prepare_for_deployment_with_packages(
     assert not dependencies_path.exists()
     assert packages_path.exists()
     assert not packages_install_path.exists()
-    assert not dbt_utils_dbt_project_path.exists()
+    assert not manifest_path.exists()
 
     result = runner.invoke(
         app,
@@ -171,7 +171,7 @@ def test_prepare_for_deployment_with_packages(
 
     assert result.exit_code == 0
     assert packages_install_path.exists()
-    assert dbt_utils_dbt_project_path.exists()
+    assert manifest_path.exists()
 
 
 def test_prepare_for_deployment_with_state(
