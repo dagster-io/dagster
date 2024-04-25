ce67fd37742756014dd78053d76dd7a274e71e9f
Maxime Armstrong <maxime@retain.ai>
2024-04-24 20:46:52 -0400

    Update dependencies

--- examples/with_openai/setup.py
+++ examples/with_openai/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="with_openai",
     packages=find_packages(exclude=["with_openai_tests"]),
-    install_requires=["dagster", "dagster-openai", "langchain==0.1.11"],
+    install_requires=["dagster", "dagster-openai", "langchain==0.1.11", "filelock", "git"],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
