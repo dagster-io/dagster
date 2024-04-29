c4b328c9cc3b005b7ccdf093171a8e23b1b1770b
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2024-04-29 12:07:34 -0400

    Update sources in with_openai (#21405)
    ## Summary & Motivation
    
    This PR updates and add doc sources in the `with_openai` example so that
    we can showcase a more complete example in the blog post.
    
    This PR also fixes the summaries of the sources provided to the model.
    Long story short, passing a list of summaries caused the model to always
    answer "I don't know", although it was working just fine before. Passing
    a string created by joining the summaries fixed the problem.
    
    ## How I Tested These Changes
    
    dagster dev
    BK

--- examples/with_openai/setup.py
+++ examples/with_openai/setup.py
@@ -3,6 +3,15 @@ from setuptools import find_packages, setup
 setup(
     name="with_openai",
     packages=find_packages(exclude=["with_openai_tests"]),
-    install_requires=["dagster", "dagster-openai", "langchain==0.1.11"],
+    install_requires=[
+        "dagster",
+        "dagster-openai",
+        "dagster-cloud",
+        "faiss-cpu==1.8.0",
+        "filelock",
+        "langchain==0.1.11",
+        "langchain-community==0.0.34",
+        "langchain-openai==0.1.3",
+    ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
