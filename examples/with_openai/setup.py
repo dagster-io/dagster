d3e018ceaf03eeeedec9e2a046fb8e590d94bc9b
Maxime Armstrong <maxime@retain.ai>
2024-04-29 15:04:34 -0400

    Lint setup.py

--- examples/with_openai/setup.py
+++ examples/with_openai/setup.py
@@ -6,9 +6,9 @@ setup(
     install_requires=[
         "dagster",
         "dagster-aws",
-        "dagster-openai",
         # Commented to avoid version conflicts with Pyright in the Dagster repo, uncomment when using Dagster+
         # "dagster-cloud",
+        "dagster-openai",
         "faiss-cpu==1.8.0",
         "filelock",
         "langchain==0.1.11",
