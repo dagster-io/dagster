c16fab7fb010208ed97718bad37a798cfdbd09fc
Marco Salazar <marco@elementl.com>
2024-05-01 12:14:03 -0400

    .

--- js_modules/dagster-ui/packages/ui-core/src/hooks/usePrevious.tsx
+++
@@ -1,8 +0,0 @@
-import {useRef} from 'react';
-
-export function usePrevious<T>(value: T) {
-  const ref = useRef<T | undefined>(undefined);
-  const current = ref.current;
-  ref.current = value;
-  return current;
-}
