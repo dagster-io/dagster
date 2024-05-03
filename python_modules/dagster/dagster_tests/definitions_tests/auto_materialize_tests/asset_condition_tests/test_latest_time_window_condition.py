3715293552b8fdd453e99d9ef5dce557899e6528
OwenKephart <owen@elementl.com>
2024-05-03 14:49:49 -0700

    [DS][9/n] Add lookback_timedelta to InLatestTimeWindow condition (#21508)
    ## Summary & Motivation
    
    This diverges slightly from past conversations we've had about separating out this functionality into two separate conditions.
    
    That said, I think this definition actually ends up making more sense overall.
    
    The most annoying bit with the originally-proposed definition is that if you define a "RecentPartitionsCondition" to mean "all partitions within the time window (current_time - 3 days, current_time)" you end up with the annoying result that this time range actually only contains 2 full daily partitions, as you are inevitably going to call this function at some time other than "exactly midnight", meaning that you have (partital_day, full_day, full_day, part of today) as your range, leaving you with only 2 partitions. I think this would be a persistant annoyance for users that would simply want to express "the last 3 daily partitions" in a natural way.
    
    I think this formulation ends up being more intuitive, and has the added bonus of consolidating two very-similar concepts into a single condition.
    
    This definition also handles partitions deifnitions with an end date nicely, which is nice.
    
    
    ## How I Tested These Changes

--- python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/asset_condition_tests/test_latest_time_window_condition.py
+++ python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/asset_condition_tests/test_latest_time_window_condition.py
@@ -1,3 +1,5 @@
+import datetime
+
 from dagster._core.definitions.asset_key import AssetKey
 from dagster._core.definitions.declarative_scheduling.asset_condition import (
     AssetCondition,
@@ -22,9 +24,33 @@ def test_in_latest_time_window_unpartitioned() -> None:
     assert result.true_subset.size == 1
 
 
+def test_in_latest_time_window_unpartitioned_lookback() -> None:
+    state = AssetConditionScenarioState(
+        one_asset,
+        asset_condition=AssetCondition.in_latest_time_window(
+            lookback_delta=datetime.timedelta(days=3)
+        ),
+    )
+
+    state, result = state.evaluate("A")
+    assert result.true_subset.size == 1
+
+
 def test_in_latest_time_window_static_partitioned() -> None:
     state = AssetConditionScenarioState(
         one_asset, asset_condition=AssetCondition.in_latest_time_window()
+    ).with_asset_properties(partitions_def=two_partitions_def)
+
+    state, result = state.evaluate("A")
+    assert result.true_subset.size == 2
+
+
+def test_in_latest_time_window_static_partitioned_lookback() -> None:
+    state = AssetConditionScenarioState(
+        one_asset,
+        asset_condition=AssetCondition.in_latest_time_window(
+            lookback_delta=datetime.timedelta(days=3)
+        ),
     ).with_asset_properties(partitions_def=two_partitions_def)
 
     state, result = state.evaluate("A")
@@ -53,4 +79,36 @@ def test_in_latest_time_window_time_partitioned() -> None:
     assert result.true_subset.size == 1
     assert result.true_subset.asset_partitions == {
         AssetKeyPartitionKey(AssetKey("A"), "2020-02-06")
+    }
+
+
+def test_in_latest_time_window_time_partitioned_lookback() -> None:
+    state = AssetConditionScenarioState(
+        one_asset,
+        asset_condition=AssetCondition.in_latest_time_window(
+            lookback_delta=datetime.timedelta(days=3)
+        ),
+    ).with_asset_properties(partitions_def=daily_partitions_def)
+
+    # no partitions exist yet
+    state = state.with_current_time(time_partitions_start_datetime)
+    state, result = state.evaluate("A")
+    assert result.true_subset.size == 0
+
+    state = state.with_current_time("2020-02-07T01:00:00")
+    state, result = state.evaluate("A")
+    assert result.true_subset.size == 3
+    assert result.true_subset.asset_partitions == {
+        AssetKeyPartitionKey(AssetKey("A"), "2020-02-06"),
+        AssetKeyPartitionKey(AssetKey("A"), "2020-02-05"),
+        AssetKeyPartitionKey(AssetKey("A"), "2020-02-04"),
+    }
+
+    state = state.with_current_time_advanced(days=5)
+    state, result = state.evaluate("A")
+    assert result.true_subset.size == 3
+    assert result.true_subset.asset_partitions == {
+        AssetKeyPartitionKey(AssetKey("A"), "2020-02-11"),
+        AssetKeyPartitionKey(AssetKey("A"), "2020-02-10"),
+        AssetKeyPartitionKey(AssetKey("A"), "2020-02-09"),
     }
