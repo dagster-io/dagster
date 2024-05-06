e9d3547db5c92d66e57c27ba827150daf9d85657
prha <1040172+prha@users.noreply.github.com>
2024-05-06 08:50:52 -0700

    remove get_event_records call for planned materializations in event log storage tests (#21598)
    ## Summary & Motivation
    Trying to remove get_event_records calls in the storage tests,
    especially for event types that are not backed by a derived table in
    Cloud
    
    ## How I Tested These Changes
    BK

--- python_modules/dagster/dagster_tests/storage_tests/utils/event_log_storage.py
+++ python_modules/dagster/dagster_tests/storage_tests/utils/event_log_storage.py
@@ -3746,21 +3746,33 @@ class TestEventLogStorage:
                 assert asset_entry.last_run_id == result.run_id
                 assert asset_entry.asset_details is None
 
-                event_log_record = storage.get_event_records(
-                    EventRecordsFilter(
-                        event_type=DagsterEventType.ASSET_MATERIALIZATION,
-                        asset_key=my_asset_key,
-                    )
-                )[0]
+                # get the materialization from the one_asset_job run
+                event_log_record = storage.get_records_for_run(
+                    run_id_1,
+                    of_type=DagsterEventType.ASSET_MATERIALIZATION,
+                    limit=1,
+                    ascending=False,
+                ).records[0]
 
-                assert asset_entry.last_materialization_record == event_log_record
-
-                materialization_planned_record = storage.get_event_records(
-                    EventRecordsFilter(
-                        event_type=DagsterEventType.ASSET_MATERIALIZATION_PLANNED,
-                        asset_key=my_asset_key,
+                if self.is_sqlite(storage):
+                    # sqlite storages are run sharded, so the storage ids in the run-shard will be
+                    # different from the storage ids in the index shard.  We therefore cannot
+                    # compare records directly for sqlite. But we can compare event log entries.
+                    assert (
+                        asset_entry.last_materialization_record
+                        and asset_entry.last_materialization_record.event_log_entry
+                        == event_log_record.event_log_entry
                     )
-                )[0]
+                else:
+                    assert asset_entry.last_materialization_record == event_log_record
+
+                # get the planned materialization from the one asset_job run
+                materialization_planned_record = storage.get_records_for_run(
+                    run_id_1,
+                    of_type=DagsterEventType.ASSET_MATERIALIZATION_PLANNED,
+                    limit=1,
+                    ascending=False,
+                ).records[0]
 
                 if storage.asset_records_have_last_planned_materialization_storage_id:
                     assert (
