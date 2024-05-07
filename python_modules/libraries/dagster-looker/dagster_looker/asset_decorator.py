fc3e8c2b679cdc2105823950cb394adbf30b3a4f
Rex Ledesma <rex@dagsterlabs.com>
2024-05-07 14:24:00 -0400

    feat(looker): represent looker explores as assets (#21635)
    ## Summary & Motivation
    Model the basic Looker explore where an explore is joined off of existing Looker views.
    
    To parse the Looker specifications, we use [`lkml`](https://lkml.readthedocs.io/en/latest/simple.html).
    
    A Looker explore will always be based on an existing sql table. By default, this is the `explore_name`, but it can be overridden with the [`from`](https://cloud.google.com/looker/docs/reference/param-explore-from) attribute.
    
    Joins on this table are specified using [`join`](https://cloud.google.com/looker/docs/reference/param-explore-join).
    
    The combination of these two lists will give the upstream dependencies for the Looker view.
    
    
    
    ## How I Tested These Changes
    pytest

--- python_modules/libraries/dagster-looker/dagster_looker/asset_decorator.py
+++ python_modules/libraries/dagster-looker/dagster_looker/asset_decorator.py
@@ -1,16 +1,44 @@
 import itertools
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, Mapping, Sequence
 
+import lkml
 import yaml
 from dagster import AssetKey, AssetsDefinition, AssetSpec, multi_asset
+
+
+def build_looker_explore_specs(project_dir: Path) -> Sequence[AssetSpec]:
+    looker_explore_specs = []
+
+    # https://cloud.google.com/looker/docs/reference/param-explore
+    for model_path in project_dir.rglob("*.model.lkml"):
+        for explore in lkml.load(model_path.read_text()).get("explores", []):
+            explore_asset_key = AssetKey(["explore", explore["name"]])
+
+            # https://cloud.google.com/looker/docs/reference/param-explore-from
+            explore_base_view = [{"name": explore.get("from") or explore["name"]}]
+
+            # https://cloud.google.com/looker/docs/reference/param-explore-join
+            explore_join_views: Sequence[Mapping[str, Any]] = explore.get("joins", [])
+
+            looker_explore_specs.append(
+                AssetSpec(
+                    key=explore_asset_key,
+                    deps={
+                        AssetKey(["view", view["name"]])
+                        for view in itertools.chain(explore_base_view, explore_join_views)
+                    },
+                )
+            )
+
+    return looker_explore_specs
 
 
 def looker_assets(*, project_dir: Path) -> Callable[[Callable[..., Any]], AssetsDefinition]:
     lookml_dashboard_specs = [
         AssetSpec(
-            key=AssetKey(lookml_dashboard["dashboard"]),
-            deps={AssetKey(dashboard_element["explore"])},
+            key=AssetKey(["dashboard", lookml_dashboard["dashboard"]]),
+            deps={AssetKey(["explore", dashboard_element["explore"]])},
         )
         for dashboard_path in project_dir.rglob("*.dashboard.lookml")
         # Each dashboard file can contain multiple dashboards.
@@ -27,5 +55,6 @@ def looker_assets(*, project_dir: Path) -> Callable[[Callable[..., Any]], Assets
         compute_kind="looker",
         specs=[
             *lookml_dashboard_specs,
+            *build_looker_explore_specs(project_dir),
         ],
     )
