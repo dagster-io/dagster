eab52cda85c8576fe53ea342421f0047ee27b813
Ben Pankow <ben@elementl.com>
2024-05-07 14:15:27 -0700

    Update python_modules/dagster/dagster/_core/definitions/metadata/metadata_set.py
    Co-authored-by: Sandy Ryza <sandy@elementl.com>

--- python_modules/dagster/dagster/_core/definitions/metadata/metadata_set.py
+++ python_modules/dagster/dagster/_core/definitions/metadata/metadata_set.py
@@ -150,7 +150,7 @@ class TableMetadataSet(NamespacedMetadataSet):
         column_lineage (Optional[TableColumnLineage]): The lineage of column inputs to column
             outputs for the table.
         row_count (Optional[int]): The number of rows in the table.
-        partition_row_count (Optional[int]): The number of rows in the materialized partition.
+        partition_row_count (Optional[int]): The number of rows in the materialized or observed partition.
     """
 
     column_schema: Optional[TableSchema] = None
