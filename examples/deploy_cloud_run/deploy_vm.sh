8bad21fb316ca94088156fd27261b12b7ca02241
Thomas Baumann <tbthomas.baumann@gmail.com>
2024-05-07 17:00:11 -0400

    added example of how to deploy dagster for cloud run jobs

---
+++ examples/deploy_cloud_run/deploy_vm.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+VM_NAME="dagster-vm"
+ZONE="northamerica-northeast1-a"
+PROJECT_ID="" # add your project id
+LOCAL_FILE_PATH="./vm_config/*"
+DAGSTER_GCP_PATH="../../python_modules/libraries/dagster-gcp/*"
+REMOTE_DAGSTER_GCP_PATH="/opt/dagster/app/python_modules/libraries/dagster_gcp"
+REMOTE_DIR="/opt/dagster/app"
+SERVICE_ACCOUNT_EMAIL="" # service account used must have the right to launch a cloud run job and access secrets from secret manager
+SCOPES="cloud-platform"
+
+gcloud compute instances create $VM_NAME \
+    --zone=$ZONE \
+    --machine-type=e2-micro \
+    --image-family=ubuntu-2204-lts \
+    --image-project=ubuntu-os-cloud \
+    --project=$PROJECT_ID \
+    --service-account=$SERVICE_ACCOUNT_EMAIL \
+    --scopes=$SCOPES
+
+echo "waiting for VM to be created..."
+sleep 40
+
+gcloud compute ssh $VM_NAME --zone=$ZONE --command="
+    sudo mkdir -p $REMOTE_DIR $REMOTE_DAGSTER_GCP_PATH
+    sudo chown -R $USER $REMOTE_DIR $REMOTE_DAGSTER_GCP_PATH
+" --project=$PROJECT_ID
+
+gcloud compute scp $LOCAL_FILE_PATH ${VM_NAME}:$REMOTE_DIR \
+    --zone=$ZONE \
+    --project=$PROJECT_ID
+
+gcloud compute scp --recurse $DAGSTER_GCP_PATH ${VM_NAME}:$REMOTE_DAGSTER_GCP_PATH \
+    --zone=$ZONE \
+    --project=$PROJECT_ID
+
+gcloud compute ssh $VM_NAME --zone=$ZONE --command="
+    # Update package list and install prerequisites
+    sudo apt-get update
+    sudo apt-get install -y python3.10 python3-venv python3-pip python3.10-distutils
+
+    python3 -m pip install --upgrade pip
+
+    # Create and activate a virtual environment with Python 3.10
+    python3 -m venv .venv
+    source .venv/bin/activate
+
+    # fetch secrets for postgres db from secret manager
+    export DAGSTER_PG_HOST=\$(gcloud secrets versions access latest --secret='DAGSTER_PG_HOST' --project=$PROJECT_ID)
+    export DAGSTER_PG_USERNAME=\$(gcloud secrets versions access latest --secret='DAGSTER_PG_USERNAME' --project=$PROJECT_ID)
+    export DAGSTER_PG_PASSWORD=\$(gcloud secrets versions access latest --secret='DAGSTER_PG_PASSWORD' --project=$PROJECT_ID)
+    export DAGSTER_PG_DB=\$(gcloud secrets versions access latest --secret='DAGSTER_PG_DB' --project=$PROJECT_ID)
+
+    # Install Dagster
+    pip install dagster-postgres dagster-webserver
+    # install dagster-gcp
+    pip install $REMOTE_DAGSTER_GCP_PATH
+
+    echo 'installed dagster'
+
+    export DAGSTER_HOME=$REMOTE_DIR
+    cd $REMOTE_DIR
+
+    echo 'starting dagster daemon'
+    # Start the Dagster daemon
+    nohup dagster-daemon run &
+
+    echo 'starting dagster daemon webserver'
+    # Start the dagster webserver
+    nohup dagster-webserver -h 0.0.0.0 -p 3000
+
+" --project=$PROJECT_ID
