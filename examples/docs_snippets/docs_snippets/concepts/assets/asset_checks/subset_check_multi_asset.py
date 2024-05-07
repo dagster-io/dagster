43495f3f83536683b387d8d2d652b303e9ce4526
Erin Cochran <erin.k.cochran@gmail.com>
2024-05-07 16:43:29 -0400

    Add example and copy for multi_asset

--- examples/docs_snippets/docs_snippets/concepts/assets/asset_checks/subset_check_multi_asset.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_checks/subset_check_multi_asset.py
@@ -4,31 +4,29 @@ from dagster import (
     AssetCheckSpec,
     AssetExecutionContext,
     AssetKey,
-    AssetOut,
-    Output,
+    AssetSpec,
+    MaterializeResult,
     multi_asset,
 )
 
 
 @multi_asset(
-    outs={
-        "one": AssetOut(
-            key="multi_asset_piece_1", group_name="asset_checks", is_required=False
-        ),
-        "two": AssetOut(
-            key="multi_asset_piece_2", group_name="asset_checks", is_required=False
-        ),
-    },
+    specs=[
+        AssetSpec("multi_asset_piece_1", group_name="asset_checks", skippable=True),
+        AssetSpec("multi_asset_piece_2", group_name="asset_checks", skippable=True),
+    ],
     check_specs=[AssetCheckSpec("my_check", asset="multi_asset_piece_1")],
     can_subset=True,
 )
 def multi_asset_1_and_2(context: AssetExecutionContext):
     if AssetKey("multi_asset_piece_1") in context.selected_asset_keys:
-        yield Output(1, output_name="one")
+        yield MaterializeResult(asset_key="multi_asset_piece_1")
+    # The check will only execute when multi_asset_piece_1 is materialized
     if (
         AssetCheckKey(AssetKey("multi_asset_piece_1"), "my_check")
         in context.selected_asset_check_keys
     ):
         yield AssetCheckResult(passed=True, metadata={"foo": "bar"})
     if AssetKey("multi_asset_piece_2") in context.selected_asset_keys:
-        yield Output(1, output_name="two")
+        # No check on multi_asset_piece_2
+        yield MaterializeResult(asset_key="multi_asset_piece_2")
