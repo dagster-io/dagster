4a4bf865af92b11e9883116bc9d3b02f96434460
Erin Cochran <erin.k.cochran@gmail.com>
2024-05-07 17:16:53 -0400

    Review comments

--- examples/docs_snippets/docs_snippets/concepts/assets/asset_checks/subset_multi_asset_check.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_checks/subset_multi_asset_check.py
@@ -1,39 +1,34 @@
-import random
 from typing import Iterable
 
 from dagster import (
     AssetCheckExecutionContext,
+    AssetCheckKey,
     AssetCheckResult,
-    AssetCheckSeverity,
     AssetCheckSpec,
+    AssetKey,
     multi_asset_check,
 )
 
 
 @multi_asset_check(
     specs=[
-        AssetCheckSpec(name="asset_check_one", asset="my_asset_one"),
-        AssetCheckSpec(name="asset_check_two", asset="my_asset_two"),
+        AssetCheckSpec(name="asset_check_one", asset="asset_one"),
+        AssetCheckSpec(name="asset_check_two", asset="asset_two"),
     ],
     can_subset=True,
 )
 def the_check(context: AssetCheckExecutionContext) -> Iterable[AssetCheckResult]:
-    for check_key in context.selected_asset_check_keys:
-        asset_key = check_key.asset_key
-
-        # TODO: populate metadata
-        metadata = {}
-        if random.random() > 0.5:
-            yield AssetCheckResult(
-                passed=False,
-                severity=AssetCheckSeverity.WARN,
-                description="The asset is over 0.5",
-                asset_key=asset_key,
-            )
-        else:
-            yield AssetCheckResult(
-                passed=True,
-                metadata=metadata,
-                description="The asset is fresh.",
-                asset_key=asset_key,
-            )
+    if (
+        AssetCheckKey(AssetKey("asset_one"), "asset_check_one")
+        in context.selected_asset_check_keys
+    ):
+        yield AssetCheckResult(
+            passed=True, metadata={"foo": "bar"}, name="asset_check_one"
+        )
+    if (
+        AssetCheckKey(AssetKey("asset_two"), "asset_check_two")
+        in context.selected_asset_check_keys
+    ):
+        yield AssetCheckResult(
+            passed=True, metadata={"foo": "bar"}, name="asset_check_two"
+        )
