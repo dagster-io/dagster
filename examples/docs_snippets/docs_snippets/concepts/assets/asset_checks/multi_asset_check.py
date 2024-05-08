9b126db43a94d9978fb350bad9cf0b4b5a45551d
Erin Cochran <erin.k.cochran@gmail.com>
2024-05-08 15:28:25 -0400

    [docs] - Subsetting asset checks (DOC-162) (#21564)
    ## Summary & Motivation
    
    Originally based on #21426, which went haywire for unknown reasons. RIP.
    
    ## How I Tested These Changes

---
+++ examples/docs_snippets/docs_snippets/concepts/assets/asset_checks/multi_asset_check.py
@@ -0,0 +1,30 @@
+from typing import Iterable
+
+from dagster import (
+    AssetCheckExecutionContext,
+    AssetCheckResult,
+    AssetCheckSeverity,
+    AssetCheckSpec,
+    multi_asset_check,
+)
+
+
+@multi_asset_check(
+    specs=[
+        AssetCheckSpec(name="asset_check_one", asset="my_asset_one"),
+        AssetCheckSpec(name="asset_check_two", asset="my_asset_two"),
+    ]
+)
+def the_check(context: AssetCheckExecutionContext) -> Iterable[AssetCheckResult]:
+    yield AssetCheckResult(
+        passed=False,
+        severity=AssetCheckSeverity.WARN,
+        description="The asset is over 0.5",
+        asset_key="asset_check_one",
+    )
+
+    yield AssetCheckResult(
+        passed=True,
+        description="The asset is fresh.",
+        asset_key="asset_check_two",
+    )
