14c99c93711f87a4b608bcfec7ad6733849cc6aa
benpankow <ben@dagsterlabs.com>
2024-05-09 10:34:48 -0700

    [docs] Add guide for code references

---
+++ examples/docs_snippets/docs_snippets/guides/dagster/code_references/with_source_code_references.py
@@ -0,0 +1,13 @@
+from dagster import Definitions, asset
+from dagster._core.definitions.metadata import with_source_code_references
+
+
+@asset
+def my_asset(): ...
+
+
+@asset
+def another_asset(): ...
+
+
+defs = Definitions(assets=with_source_code_references([my_asset, another_asset]))
