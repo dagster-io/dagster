fc4966f4c4c2422489d80601f58f0b316ace58d7
Owen Kephart <owen@elementl.com>
2024-05-10 11:35:53 -0700

    Create an as_auto_materialize_policy method on SchedulingCondition

---
+++ python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/asset_condition_tests/test_auto_materialize_policy_conversion.py
@@ -0,0 +1,32 @@
+import dagster._check as check
+from dagster import (
+    AutoMaterializePolicy,
+    Definitions,
+    SchedulingCondition,
+    asset,
+    deserialize_value,
+    serialize_value,
+)
+
+
+def test_round_trip_conversion() -> None:
+    policy = SchedulingCondition.eager_with_rate_limit().as_auto_materialize_policy()
+    serialized_policy = serialize_value(policy)
+    deserialized_policy = deserialize_value(serialized_policy, AutoMaterializePolicy)
+    assert policy == deserialized_policy
+    assert deserialized_policy.asset_condition == SchedulingCondition.eager_with_rate_limit()
+
+
+def test_defs() -> None:
+    @asset(
+        auto_materialize_policy=SchedulingCondition.eager_with_rate_limit().as_auto_materialize_policy()
+    )
+    def my_asset() -> None: ...
+
+    defs = Definitions(assets=[my_asset])
+
+    asset_graph_amp = defs.get_asset_graph().get(my_asset.key).auto_materialize_policy
+    assert (
+        check.not_none(asset_graph_amp).to_scheduling_condition()
+        == SchedulingCondition.eager_with_rate_limit()
+    )
