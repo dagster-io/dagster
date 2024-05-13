6d73b4a08bd2bfd096b51dadc02f26bca17a6e18
Marco Salazar <marco@elementl.com>
2024-05-13 12:39:09 -0400

    inject oss telemetry script if telemetry enabled

--- js_modules/dagster-ui/packages/app-oss/src/extractInitializationData.ts
+++ js_modules/dagster-ui/packages/app-oss/src/extractInitializationData.ts
@@ -30,5 +30,10 @@ export const extractInitializationData = (): {
       }
     }
   }
+  if (value.telemetryEnabled) {
+    const script = document.createElement('script');
+    script.src = 'https://dagster.io/oss-telemetry.js';
+    document.head.appendChild(script);
+  }
   return value;
 };
