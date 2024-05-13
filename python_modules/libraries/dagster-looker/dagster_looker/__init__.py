fc8930b28fa23269261cfcf7846db9e55f958ef0
Rex Ledesma <rex@dagsterlabs.com>
2024-05-13 11:09:39 -0400

    chore(looker): add `dagster-looker` to release pipeline

--- python_modules/libraries/dagster-looker/dagster_looker/__init__.py
+++ python_modules/libraries/dagster-looker/dagster_looker/__init__.py
@@ -1,7 +1,5 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-# Move back to version.py and edit setup.py once we are ready to publish.
-__version__ = "1!0+dev"
-
+from .version import __version__ as __version__
 
 DagsterLibraryRegistry.register("dagster-looker", __version__)
