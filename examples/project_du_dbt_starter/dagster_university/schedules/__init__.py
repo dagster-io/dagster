04569562fea0e96fa92d94e6a6741193ac70c1ee
Tim Castillo <tim@elementl.com>
2024-05-13 13:34:37 -0400

    [du] updating dbt+du starter kit (#21605)
    ## Summary & Motivation
    
    ## How I Tested These Changes

--- examples/project_du_dbt_starter/dagster_university/schedules/__init__.py
+++ examples/project_du_dbt_starter/dagster_university/schedules/__init__.py
@@ -2,7 +2,6 @@ from dagster import ScheduleDefinition
 
 from ..jobs import trip_update_job, weekly_update_job
 
-## Lesson 7
 trip_update_schedule = ScheduleDefinition(
     job=trip_update_job,
     cron_schedule="0 0 5 * *",  # every 5th of the month at midnight
