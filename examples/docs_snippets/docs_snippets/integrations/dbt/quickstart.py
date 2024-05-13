8e2ecb5e1893e9c2717c449917cc3049b79374b2
Maxime Armstrong <maxime@retain.ai>
2024-05-13 14:42:34 -0400

    Add dbt quickstart example for already existing Dagster project

--- examples/docs_snippets/docs_snippets/integrations/dbt/quickstart.py
+++ examples/docs_snippets/docs_snippets/integrations/dbt/quickstart.py
@@ -1,3 +1,4 @@
+# pyright: reportMissingImports=false
 # ruff: isort: skip_file
 
 # start_example
@@ -40,3 +41,50 @@ defs = Definitions(
     },
 )
 # end_example
+
+
+def dbt_project_example():
+    # start_dbt_project_example
+    from pathlib import Path
+
+    from dagster_dbt import DbtProject
+
+    RELATIVE_PATH_TO_MY_DBT_PROJECT = "./my-dbt-project"
+
+    my_project = DbtProject(
+        project_dir=Path(__file__)
+        .joinpath("..", RELATIVE_PATH_TO_MY_DBT_PROJECT)
+        .resolve(),
+    )
+    # end_dbt_project_example
+
+
+def dbt_assets_example():
+    # start_dbt_assets_example
+    from dagster import AssetExecutionContext
+    from dagster_dbt import DbtCliResource, dbt_assets
+
+    from .project import my_project
+
+    @dbt_assets(manifest=my_project.manifest_path)
+    def my_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+        yield from dbt.cli(["build"], context=context).stream()
+
+    # end_dbt_assets_example
+
+
+def dbt_definitions_example():
+    # start_dbt_definitions_example
+    from dagster import Definitions
+    from dagster_dbt import DbtCliResource
+
+    from .assets import my_dbt_assets
+    from .project import my_project
+
+    defs = Definitions(
+        assets=[my_dbt_assets],
+        resources={
+            "dbt": DbtCliResource(project_dir=my_project),
+        },
+    )
+    # end_dbt_definitions_example
