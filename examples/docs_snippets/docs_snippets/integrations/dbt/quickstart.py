b79360cf9483d292f50583d75fff88cc6d5772dc
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2024-05-14 15:00:11 -0400

    Add existing Dagster project example to dbt quickstart (#21817)
    ## Summary & Motivation
    
    This PR updates the quickstart example for dbt + Dagster - it adds the
    steps to update an existing Dagster project to wrap a dbt project.
    
    This work is the continuation of #21240  and #21816
    
    ## How I Tested These Changes
    
    BK 
    +
    local
    ```
    make mdx-full-format
    make next-watch-build
    ```

--- examples/docs_snippets/docs_snippets/integrations/dbt/quickstart.py
+++ examples/docs_snippets/docs_snippets/integrations/dbt/quickstart.py
@@ -1,3 +1,4 @@
+# pyright: reportMissingImports=false
 # ruff: isort: skip_file
 
 # start_example
@@ -40,3 +41,60 @@ defs = Definitions(
     },
 )
 # end_example
+
+
+# start_dbt_project_example
+from pathlib import Path
+
+from dagster_dbt import DbtProject
+
+RELATIVE_PATH_TO_MY_DBT_PROJECT = "./my_dbt_project"
+
+my_project = DbtProject(
+    project_dir=Path(__file__)
+    .joinpath("..", RELATIVE_PATH_TO_MY_DBT_PROJECT)
+    .resolve(),
+)
+# end_dbt_project_example
+
+
+# Using a string to avoid ruff adding a second blank line before the dbt_assets.
+"""
+# start_dbt_assets_example
+from dagster import AssetExecutionContext
+from dagster_dbt import DbtCliResource, dbt_assets
+
+from .project import my_project
+
+@dbt_assets(manifest=my_project.manifest_path)
+def my_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
+# end_dbt_assets_example
+"""
+
+
+# Using a string to avoid compilation error caused by the `...` placeholders
+"""# start_dbt_definitions_example
+
+from dagster import Definitions
+from dagster_dbt import DbtCliResource
+
+from .assets import my_dbt_assets
+from .project import my_project
+
+defs = Definitions(
+    ...,
+    assets=[
+        ...,
+        # Add the dbt assets alongside your other asset
+        my_dbt_assets
+    ],
+    resources={
+        ...,
+        # Add the dbt resource alongside your other resources
+        "dbt": DbtCliResource(project_dir=my_project),
+    },
+)
+
+# end_dbt_definitions_example
+"""
