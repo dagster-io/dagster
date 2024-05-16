1c7b3f70ccd112682024f032530f187217ed187b
Nick Schrock <schrockn@dagsterlabs.com>
2024-05-16 04:01:27 -0400

    rebase

--- examples/docs_snippets/docs_snippets/concepts/declarative_scheduling/eager_asset.py
+++ examples/docs_snippets/docs_snippets/concepts/declarative_scheduling/eager_asset.py
@@ -1,12 +1,12 @@
-from dagster import SchedulingCondition
 from dagster._core.definitions.declarative_scheduling.ds_asset import ds_asset as asset
+from dagster._core.definitions.declarative_scheduling.scheduling_condition import (
+    Scheduling,
+)
 
 
 @asset()
 def upstream() -> None: ...
 
 
-@asset(
-    deps=[upstream], scheduling_condition=SchedulingCondition.eager()
-)
+@asset(deps=[upstream], scheduling=Scheduling.eager())
 def downstream() -> None: ...
