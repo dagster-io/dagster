6584a002e40aba4e0b9f8164fe062d4e9ceed13c
Nick Schrock <schrockn@dagsterlabs.com>
2024-05-16 04:00:35 -0400

    change to Scheduling

--- examples/docs_snippets/docs_snippets/concepts/declarative_scheduling/many_asset_cron.py
+++ examples/docs_snippets/docs_snippets/concepts/declarative_scheduling/many_asset_cron.py
@@ -1,12 +1,14 @@
-from dagster import SchedulingCondition
 from dagster._core.definitions.declarative_scheduling.ds_asset import ds_asset as asset
+from dagster._core.definitions.declarative_scheduling.scheduling_condition import (
+    Scheduling,
+)
 
-daily_cron = SchedulingCondition.on_cron("0 0 * * *")
+daily_cron = Scheduling.on_cron("0 0 * * *")
 
 
-@asset(scheduling_condition=daily_cron)
+@asset(scheduling=daily_cron)
 def asset_one() -> None: ...
 
 
-@asset(scheduling_condition=daily_cron)
+@asset(scheduling=daily_cron)
 def asset_two() -> None: ...
