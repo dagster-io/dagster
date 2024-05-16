dead722392e3875666e9428e6fdb979cf53a6bf5
Maxime Armstrong <maxime@retain.ai>
2024-05-16 13:11:00 -0400

    Fix import in resources_v2

--- python_modules/libraries/dagster-dbt/dagster_dbt/core/resources_v2.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/core/resources_v2.py
@@ -76,7 +76,7 @@ from sqlglot.optimizer import optimize
 from typing_extensions import Final, Literal, TypeVar
 
 if dbt_version >= "1.8":
-    from dbt_common.events.event_manager_client import cleanup_event_loger  # noqa
+    from dbt_common.events.event_manager_client import cleanup_event_logger  # noqa
 else:
     from dbt.events.functions import cleanup_event_logger
 
