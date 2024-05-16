3a19068058e1f8ee10d31a629839247d76cc964b
Marco polo <salazarm@users.noreply.github.com>
2024-05-16 11:03:45 -0400

    [2/3] Paginate, Chunk and Parallelize RunTimeline queries (#21850)
    ## Summary & Motivation
    
    - Break up our query into 1 hour buckets
    - Serially paginate queries within each of those buckets
    
    ## How I Tested These Changes
    
    jest +
    
    
    https://app.datadoghq.com/rum/sessions?query=%40type%3Aview%20%40application.id%3A6d12cca2-0263-463b-a90e-cc6c524e14bd%20%40usr.email%3Amarco%40dagsterlabs.com&agg_m=count&agg_m_source=base&agg_q=%40issue.id&agg_q_source=base&agg_t=count&cols=&event=AgAAAY96S1BumB6VlgAAAAAAAAAYAAAAAEFZOTZTMUJ1QUFCUG1QVUp1MW9ZRHRxWAAAACQAAAAAMDE4ZjdhNGMtMmViYy00ODE5LTlkOWQtYTQwZmU2ZTJiYTY4&fromUser=false&tab=error&top_n=10&top_o=top&viz=stream&x_missing=true&from_ts=1715657384997&to_ts=1715743784997&live=true

---
+++ js_modules/dagster-ui/packages/ui-core/src/runs/fetchPaginatedBucketData.ts
@@ -0,0 +1,80 @@
+export type QueryResultData<DataType> = {
+  data: DataType[] | undefined;
+  loading: boolean;
+  error: any;
+  called: boolean;
+};
+
+export async function fetchPaginatedBucketData<BucketType, DataType, CursorType, ErrorType>({
+  buckets,
+  fetchData,
+  setQueryData,
+}: {
+  buckets: BucketType[];
+  fetchData: (
+    bucket: BucketType,
+    cursor: CursorType | undefined,
+  ) => Promise<{
+    data: DataType[];
+    hasMore: boolean;
+    cursor: CursorType | undefined;
+    error: ErrorType;
+  }>;
+  setQueryData: React.Dispatch<React.SetStateAction<QueryResultData<DataType>>>;
+}) {
+  setQueryData(({data}) => ({
+    data, // preserve existing data
+    loading: true,
+    called: true,
+    error: undefined,
+  }));
+  try {
+    const results = await Promise.all(
+      buckets.map((bucket) =>
+        fetchPaginatedData<DataType, CursorType, ErrorType>({
+          fetchData: (cursor) => fetchData(bucket, cursor),
+        }),
+      ),
+    );
+    setQueryData({
+      data: results.flat(),
+      loading: false,
+      called: true,
+      error: undefined,
+    });
+  } catch (error) {
+    setQueryData(({data}) => ({
+      data, // preserve existing data
+      loading: false,
+      called: true,
+      error,
+    }));
+  }
+}
+
+export async function fetchPaginatedData<DataType, CursorType, ErrorType>({
+  fetchData,
+}: {
+  fetchData: (cursor: CursorType | undefined) => Promise<{
+    data: DataType[];
+    hasMore: boolean;
+    cursor: CursorType | undefined;
+    error: ErrorType;
+  }>;
+}): Promise<DataType[]> {
+  let hasMoreData = true;
+  const dataSoFar: DataType[] = [];
+  let currentCursor: CursorType | undefined = undefined;
+
+  while (hasMoreData) {
+    const {cursor, hasMore, data, error} = await fetchData(currentCursor);
+    if (error) {
+      throw error;
+    }
+    dataSoFar.push(...data);
+    currentCursor = cursor;
+    hasMoreData = hasMore;
+  }
+
+  return dataSoFar;
+}
