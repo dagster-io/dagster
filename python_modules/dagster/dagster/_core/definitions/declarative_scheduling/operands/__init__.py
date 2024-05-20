e7e9ffa451827d919052ec5023b20fcfdf3e8289
Owen Kephart <owen@elementl.com>
2024-05-20 07:25:45 -0700

    Create CronTickPassedCondition

--- python_modules/dagster/dagster/_core/definitions/declarative_scheduling/operands/__init__.py
+++ python_modules/dagster/dagster/_core/definitions/declarative_scheduling/operands/__init__.py
@@ -1,5 +1,6 @@
 from .parent_newer_condition import ParentNewerCondition as ParentNewerCondition
 from .slice_conditions import (
+    CronTickPassedCondition as CronTickPassedCondition,
     FailedSchedulingCondition as FailedSchedulingCondition,
     InLatestTimeWindowCondition as InLatestTimeWindowCondition,
     InProgressSchedulingCondition as InProgressSchedulingCondition,
