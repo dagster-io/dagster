dda6dd318439d4c9d7d9c012c9d251f58d113b6a
Ben Pankow <ben@elementl.com>
2024-05-20 08:58:09 -0700

    [bk] add nightly oss pipeline (#21956)
    ## Summary
    
    Templates out a basic nightly OSS test to use for Snowflake integration
    tests w/ `dagster-dbt`, to avoid unnecessary cost.

---
+++ .buildkite/dagster-buildkite/dagster_buildkite/pipelines/dagster_oss_nightly_pipeline.py
@@ -0,0 +1,9 @@
+from typing import List
+
+from dagster_buildkite.utils import BuildkiteStep
+
+
+def build_dagster_oss_nightly_steps() -> List[BuildkiteStep]:
+    steps: List[BuildkiteStep] = []
+
+    return steps
