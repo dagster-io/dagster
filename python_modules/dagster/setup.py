697e104ff7ec5659af1fa79bfdeee944ea96557e
Jordan Sanders <jordan@elementl.com>
2024-05-20 12:34:15 -0500

    Move pin into dagster-docker

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -96,7 +96,7 @@ setup(
         "python-dateutil",
         "python-dotenv",
         "pytz",
-        "requests<2.32.0",  # 2.32.0 breaks our docker tests https://buildkite.com/dagster/dagster-dagster/builds/83562
+        "requests",
         "setuptools",
         "tabulate",
         "tomli<3",
