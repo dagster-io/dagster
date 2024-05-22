c9ff8f14523858d6a07fb808e23f0b571d80d6e9
benpankow <ben@dagsterlabs.com>
2024-05-22 11:48:25 -0700

    introduce dagster/storage_kind tag

--- python_modules/dagster/dagster/_core/definitions/tags/__init__.py
+++ python_modules/dagster/dagster/_core/definitions/tags/__init__.py
@@ -1,1 +1,13 @@
+from typing import Optional
+
 from .tag_set import NamespacedTagSet as NamespacedTagSet
+
+
+class StorageKindTagSet(NamespacedTagSet):
+    """Metadata entries which describe how an asset is stored."""
+
+    storage_kind: Optional[str]
+
+    @classmethod
+    def namespace(cls) -> str:
+        return "dagster"
