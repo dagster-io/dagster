e48701ca9e154501a39285729ad0c778f45f734e
benpankow <ben@dagsterlabs.com>
2024-05-23 14:02:46 -0700

    update docstr

--- python_modules/dagster/dagster/_core/definitions/tags/__init__.py
+++ python_modules/dagster/dagster/_core/definitions/tags/__init__.py
@@ -4,7 +4,12 @@ from .tag_set import NamespacedTagSet as NamespacedTagSet
 
 
 class StorageKindTagSet(NamespacedTagSet):
-    """Metadata entries which describe how an asset is stored."""
+    """Tag entries which describe how an asset is stored.
+
+    Args:
+        storage_kind (Optional[str]): The storage type of the asset.
+            For example, "snowflake" or "s3".
+    """
 
     storage_kind: Optional[str] = None
 
