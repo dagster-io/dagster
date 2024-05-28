5ad3b82982f6e1cb911c9538f245fbb5ecc79c39
Ben Pankow <ben@elementl.com>
2024-05-28 16:22:16 -0700

    [ui] introduce pill for `storage_kind` tag across UI (#22031)
    ## Summary
    
    Implements a very basic visual tag for `storage_kind` on assets. Right now, this special rendering only displays on the catalog page, asset list, and asset graph. A stacked PR handles adding the filtering.
    
    Uses the same iconset as compute kind.
    
    <img width="304" alt="Screenshot 2024-05-22 at 2 09 55 PM" src="https://github.com/dagster-io/dagster/assets/10215173/7988910f-642d-4f00-9753-3b0a156079a3">
    
    
    <img width="391" alt="Screenshot 2024-05-22 at 1 33 08 PM" src="https://github.com/dagster-io/dagster/assets/10215173/0845ce88-e198-4dfb-a347-9cd7a949ea08">
    
    
    <img width="1449" alt="Screenshot 2024-05-22 at 1 33 04 PM" src="https://github.com/dagster-io/dagster/assets/10215173/1e255c51-a5f1-4365-9b96-16121247a648">
    
    <img width="258" alt="Screenshot 2024-05-22 at 2 09 43 PM" src="https://github.com/dagster-io/dagster/assets/10215173/8918e281-2bc0-48fb-ae03-748ef7a92fce">
    
    
    ## Test Plan
    
    Tested locally.

--- js_modules/dagster-ui/packages/ui-core/src/assets/AssetNodeOverview.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/AssetNodeOverview.tsx
@@ -51,7 +51,11 @@ import {
 import {StatusDot} from '../asset-graph/sidebar/StatusDot';
 import {AssetNodeForGraphQueryFragment} from '../asset-graph/types/useAssetGraphData.types';
 import {DagsterTypeSummary} from '../dagstertype/DagsterType';
-import {AssetComputeKindTag} from '../graph/OpTags';
+import {
+  AssetComputeKindTag,
+  AssetStorageKindTag,
+  isCanonicalStorageKindTag,
+} from '../graph/KindTags';
 import {IntMetadataEntry} from '../graphql/types';
 import {useLaunchPadHooks} from '../launchpad/LaunchpadHooksContext';
 import {isCanonicalRowCountMetadataEntry} from '../metadata/MetadataEntry';
@@ -216,6 +220,9 @@ export const AssetNodeOverview = ({
     </>
   );
 
+  const storageKindTag = assetNode.tags?.find(isCanonicalStorageKindTag);
+  const filteredTags = assetNode.tags?.filter((tag) => tag.key !== 'dagster/storage_kind');
+
   const renderDefinitionSection = () => (
     <Box flex={{direction: 'column', gap: 12}}>
       <AttributeAndValue label="Group">
@@ -260,10 +267,19 @@ export const AssetNodeOverview = ({
           <AssetComputeKindTag style={{position: 'relative'}} definition={assetNode} reduceColor />
         )}
       </AttributeAndValue>
+      <AttributeAndValue label="Storage kind">
+        {storageKindTag && (
+          <AssetStorageKindTag
+            style={{position: 'relative'}}
+            storageKind={storageKindTag.value}
+            reduceColor
+          />
+        )}
+      </AttributeAndValue>
       <AttributeAndValue label="Tags">
-        {assetNode.tags &&
-          assetNode.tags.length > 0 &&
-          assetNode.tags.map((tag, idx) => <Tag key={idx}>{buildTagString(tag)}</Tag>)}
+        {filteredTags &&
+          filteredTags.length > 0 &&
+          filteredTags.map((tag, idx) => <Tag key={idx}>{buildTagString(tag)}</Tag>)}
       </AttributeAndValue>
     </Box>
   );
--- js_modules/dagster-ui/packages/ui-core/src/graph/OpTags.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/graph/OpTags.tsx
@@ -557,34 +557,6 @@ function coerceToStandardLabel(label: string) {
   return label.replace(/[ _-]/g, '').toLowerCase();
 }
 
-export const AssetComputeKindTag = ({
-  definition,
-  ...rest
-}: {
-  definition: {computeKind: string | null};
-  style: React.CSSProperties;
-  reduceColor?: boolean;
-  reduceText?: boolean;
-  reversed?: boolean;
-}) => {
-  if (!definition.computeKind) {
-    return null;
-  }
-  return (
-    <OpTags
-      {...rest}
-      tags={[
-        {
-          label: definition.computeKind,
-          onClick: () => {
-            window.requestAnimationFrame(() => document.dispatchEvent(new Event('show-kind-info')));
-          },
-        },
-      ]}
-    />
-  );
-};
-
 export const extractIconSrc = (knownTag: KnownTag | undefined) => {
   // Storybook imports SVGs are string but nextjs imports them as object.
   // This is a temporary work around until we can get storybook to import them the same way as nextjs
