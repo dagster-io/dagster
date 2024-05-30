7ef15aca18764564c3a94083d06dd94a6b41cb03
benpankow <ben@dagsterlabs.com>
2024-05-30 14:51:42 -0700

    [blueprints] Add CLI command to generate JSON schemas

---
+++ examples/experimental/dagster-blueprints/dagster_blueprints_tests/test_cli/sample_blueprint_project/blueprint_project/__init__.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from dagster import asset
+from dagster_blueprints.blueprint import Blueprint, BlueprintDefinitions
+from dagster_blueprints.load_from_yaml import YamlBlueprintsLoader
+
+
+class SimpleAssetBlueprint(Blueprint):
+    key: str
+
+    def build_defs(self) -> BlueprintDefinitions:
+        @asset(key=self.key)
+        def blueprint_asset(): ...
+
+        return BlueprintDefinitions(assets=[blueprint_asset])
+
+
+loader = YamlBlueprintsLoader(
+    path=Path(__file__).parent / "blueprints", per_file_blueprint_type=SimpleAssetBlueprint
+)
+other_loader = YamlBlueprintsLoader(
+    path=Path(__file__).parent / "other_blueprints", per_file_blueprint_type=SimpleAssetBlueprint
+)
