a913501cb0fc9bd909faa7d857324282aa7f64a5
Erin Cochran <erin.k.cochran@gmail.com>
2024-05-31 12:10:51 -0400

    Add missing import in example

--- examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/schedules/schedules.py
+++ examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/schedules/schedules.py
@@ -14,7 +14,8 @@ from dagster import (
 
 # start_basic_schedule
 @job
-def my_job(): ...
+def my_job():
+    ...
 
 
 basic_schedule = ScheduleDefinition(job=my_job, cron_schedule="0 0 * * *")
@@ -27,7 +28,7 @@ def my_asset():
 
 
 # start_basic_asset_schedule
-from dagster import AssetSelection, define_asset_job
+from dagster import AssetSelection, ScheduleDefinition, define_asset_job
 
 asset_job = define_asset_job("asset_job", AssetSelection.groups("some_asset_group"))
 
