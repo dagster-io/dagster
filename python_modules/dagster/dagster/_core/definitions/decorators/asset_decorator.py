080989892cf809142992eb5dbe30b7b4dcec4e2d
Nick Schrock <schrockn@dagsterlabs.com>
2024-06-02 15:25:51 -0400

    Factor out spec-based construction path into factory area

--- python_modules/dagster/dagster/_core/definitions/decorators/asset_decorator.py
+++ python_modules/dagster/dagster/_core/definitions/decorators/asset_decorator.py
@@ -1,11 +1,9 @@
-from inspect import Parameter
 from typing import (
     AbstractSet,
     Any,
     Callable,
     Dict,
     Iterable,
-    List,
     Mapping,
     Optional,
     Sequence,
@@ -20,7 +18,6 @@ import dagster._check as check
 from dagster._annotations import deprecated_param, experimental_param
 from dagster._builtins import Nothing
 from dagster._config import UserConfigSchema
-from dagster._core.decorator_utils import get_function_params, get_valid_name_permutations
 from dagster._core.definitions.asset_dep import AssetDep, CoercibleToAssetDep
 from dagster._core.definitions.auto_materialize_policy import AutoMaterializePolicy
 from dagster._core.definitions.config import ConfigMapping
@@ -59,6 +56,7 @@ from ..utils import (
     NoValueSentinel,
     validate_tags_strict,
 )
+from .assets_definition_factory import build_asset_ins
 
 
 @overload
@@ -661,20 +659,8 @@ def multi_asset(
 
         if asset_out_map and specs:
             raise DagsterInvalidDefinitionError("Must specify only outs or specs but not both.")
-        elif specs:
-            output_tuples_by_asset_key = {}
-            for asset_spec in specs:
-                output_name = asset_spec.key.to_python_identifier()
-                output_tuples_by_asset_key[asset_spec.key] = (
-                    output_name,
-                    Out(
-                        Nothing,
-                        is_required=not (can_subset or asset_spec.skippable),
-                        description=asset_spec.description,
-                        code_version=asset_spec.code_version,
-                        metadata=asset_spec.metadata,
-                    ),
-                )
+
+        if specs:
             if upstream_asset_deps:
                 raise DagsterInvalidDefinitionError(
                     "Can not pass deps and specs to @multi_asset, specify deps on the AssetSpecs"
@@ -686,31 +672,13 @@ def multi_asset(
                     " the AssetSpecs directly."
                 )
 
-            upstream_keys = set()
-            for spec in specs:
-                for dep in spec.deps:
-                    if dep.asset_key not in output_tuples_by_asset_key:
-                        upstream_keys.add(dep.asset_key)
-                    if (
-                        dep.asset_key in output_tuples_by_asset_key
-                        and dep.partition_mapping is not None
-                    ):
-                        # self-dependent asset also needs to be considered an upstream_key
-                        upstream_keys.add(dep.asset_key)
-
-            explicit_ins = ins or {}
-            # get which asset keys have inputs set
-            loaded_upstreams = build_asset_ins(fn, explicit_ins, deps=set())
-            unexpected_upstreams = {
-                key for key in loaded_upstreams.keys() if key not in upstream_keys
-            }
-            if unexpected_upstreams:
-                raise DagsterInvalidDefinitionError(
-                    f"Asset inputs {unexpected_upstreams} do not have dependencies on the passed"
-                    " AssetSpec(s). Set the deps on the appropriate AssetSpec(s)."
-                )
-            remaining_upstream_keys = {key for key in upstream_keys if key not in loaded_upstreams}
-            asset_ins = build_asset_ins(fn, explicit_ins, deps=remaining_upstream_keys)
+            in_out_mapper = InOutMapper.from_specs(
+                specs=specs,
+                check_specs=check_specs or [],
+                can_subset=can_subset,
+                ins=ins or {},
+                fn=fn,
+            )
         else:
             asset_ins = build_asset_ins(
                 fn,
@@ -751,12 +719,12 @@ def multi_asset(
                     f" {list(valid_asset_deps)[:20]}",
                 )
 
-        in_out_mapper = InOutMapper.from_asset_ins_and_asset_outs(
-            asset_ins=asset_ins,
-            asset_outs=output_tuples_by_asset_key,
-            check_specs=check_specs or [],
-            can_subset=can_subset,
-        )
+            in_out_mapper = InOutMapper.from_asset_ins_and_asset_outs(
+                asset_ins=asset_ins,
+                asset_outs=output_tuples_by_asset_key,
+                check_specs=check_specs or [],
+                can_subset=can_subset,
+            )
 
         arg_resource_keys = {arg.name for arg in get_resource_args(fn)}
         check.param_invariant(
@@ -778,7 +746,8 @@ def multi_asset(
             }
         else:
             internal_deps = {
-                in_out_mapper.keys_by_output_name[name]: asset_deps[name] for name in asset_deps
+                in_out_mapper.asset_keys_by_output_name[name]: asset_deps[name]
+                for name in asset_deps
             }
 
         # when a subsettable multi-asset is defined, it is possible that it will need to be
@@ -786,9 +755,11 @@ def multi_asset(
         # this in the op execution graph, inputs need to be added to connect these possible deps
         if can_subset and internal_deps:
             asset_ins = {
-                **asset_ins,
+                **in_out_mapper.input_tuples_by_asset_key,
                 **build_subsettable_asset_ins(
-                    asset_ins, output_tuples_by_asset_key, internal_deps.values()
+                    in_out_mapper.input_tuples_by_asset_key,
+                    in_out_mapper.output_tuples_by_asset_key,
+                    internal_deps.values(),
                 ),
             }
 
@@ -798,7 +769,7 @@ def multi_asset(
             op = _Op(
                 name=op_name,
                 description=description,
-                ins=dict(asset_ins.values()),
+                ins=in_out_mapper.asset_ins_by_input_names,
                 out=in_out_mapper.combined_outs_by_output_name,
                 required_resource_keys=op_required_resource_keys,
                 tags={
@@ -810,11 +781,8 @@ def multi_asset(
                 code_version=code_version,
             )(fn)
 
-        keys_by_input_name = {
-            input_name: asset_key for asset_key, (input_name, _) in asset_ins.items()
-        }
         partition_mappings = {
-            keys_by_input_name[input_name]: asset_in.partition_mapping
+            in_out_mapper.asset_keys_by_input_names[input_name]: asset_in.partition_mapping
             for input_name, asset_in in (ins or {}).items()
             if asset_in.partition_mapping is not None
         }
@@ -830,11 +798,11 @@ def multi_asset(
             resolved_specs = []
             input_deps_by_key = {
                 key: AssetDep(asset=key, partition_mapping=partition_mappings.get(key))
-                for key in keys_by_input_name.values()
+                for key in in_out_mapper.asset_keys_by_input_names.values()
             }
             input_deps = list(input_deps_by_key.values())
             for output_name, asset_out in asset_out_map.items():
-                key = in_out_mapper.keys_by_output_name[output_name]
+                key = in_out_mapper.asset_keys_by_output_name[output_name]
                 if internal_asset_deps:
                     deps = [
                         input_deps_by_key.get(
@@ -857,8 +825,8 @@ def multi_asset(
             resolved_specs = [spec._replace(group_name=group_name) for spec in resolved_specs]
 
         return AssetsDefinition.dagster_internal_init(
-            keys_by_input_name=keys_by_input_name,
-            keys_by_output_name=in_out_mapper.keys_by_output_name,
+            keys_by_input_name=in_out_mapper.asset_keys_by_input_names,
+            keys_by_output_name=in_out_mapper.asset_keys_by_output_name,
             node_def=op,
             partitions_def=partitions_def,
             can_subset=can_subset,
@@ -872,94 +840,6 @@ def multi_asset(
         )
 
     return inner
-
-
-def get_function_params_without_context_or_config_or_resources(
-    fn: Callable[..., Any],
-) -> List[Parameter]:
-    params = get_function_params(fn)
-    is_context_provided = len(params) > 0 and params[0].name in get_valid_name_permutations(
-        "context"
-    )
-    input_params = params[1:] if is_context_provided else params
-
-    resource_arg_names = {arg.name for arg in get_resource_args(fn)}
-
-    new_input_args = []
-    for input_arg in input_params:
-        if input_arg.name != "config" and input_arg.name not in resource_arg_names:
-            new_input_args.append(input_arg)
-
-    return new_input_args
-
-
-def stringify_asset_key_to_input_name(asset_key: AssetKey) -> str:
-    return "_".join(asset_key.path).replace("-", "_")
-
-
-def build_asset_ins(
-    fn: Callable[..., Any],
-    asset_ins: Mapping[str, AssetIn],
-    deps: Optional[AbstractSet[AssetKey]],
-) -> Mapping[AssetKey, Tuple[str, In]]:
-    """Creates a mapping from AssetKey to (name of input, In object)."""
-    deps = check.opt_set_param(deps, "deps", AssetKey)
-
-    new_input_args = get_function_params_without_context_or_config_or_resources(fn)
-
-    non_var_input_param_names = [
-        param.name for param in new_input_args if param.kind == Parameter.POSITIONAL_OR_KEYWORD
-    ]
-    has_kwargs = any(param.kind == Parameter.VAR_KEYWORD for param in new_input_args)
-
-    all_input_names = set(non_var_input_param_names) | asset_ins.keys()
-
-    if not has_kwargs:
-        for in_key, asset_in in asset_ins.items():
-            if in_key not in non_var_input_param_names and (
-                not isinstance(asset_in.dagster_type, DagsterType)
-                or not asset_in.dagster_type.is_nothing
-            ):
-                raise DagsterInvalidDefinitionError(
-                    f"Key '{in_key}' in provided ins dict does not correspond to any of the names "
-                    "of the arguments to the decorated function"
-                )
-
-    ins_by_asset_key: Dict[AssetKey, Tuple[str, In]] = {}
-    for input_name in all_input_names:
-        asset_key = None
-
-        if input_name in asset_ins:
-            asset_key = asset_ins[input_name].key
-            metadata = asset_ins[input_name].metadata or {}
-            key_prefix = asset_ins[input_name].key_prefix
-            input_manager_key = asset_ins[input_name].input_manager_key
-            dagster_type = asset_ins[input_name].dagster_type
-        else:
-            metadata = {}
-            key_prefix = None
-            input_manager_key = None
-            dagster_type = NoValueSentinel
-
-        asset_key = asset_key or AssetKey(list(filter(None, [*(key_prefix or []), input_name])))
-
-        ins_by_asset_key[asset_key] = (
-            input_name.replace("-", "_"),
-            In(metadata=metadata, input_manager_key=input_manager_key, dagster_type=dagster_type),
-        )
-
-    for asset_key in deps:
-        if asset_key in ins_by_asset_key:
-            raise DagsterInvalidDefinitionError(
-                f"deps value {asset_key} also declared as input/AssetIn"
-            )
-            # mypy doesn't realize that Nothing is a valid type here
-        ins_by_asset_key[asset_key] = (
-            stringify_asset_key_to_input_name(asset_key),
-            In(cast(type, Nothing)),
-        )
-
-    return ins_by_asset_key
 
 
 def build_subsettable_asset_ins(
--- python_modules/dagster/dagster/_core/definitions/decorators/assets_definition_factory.py
+++ python_modules/dagster/dagster/_core/definitions/decorators/assets_definition_factory.py
@@ -1,20 +1,126 @@
 from collections import Counter
 from functools import cached_property
+from inspect import Parameter
 from typing import (
+    AbstractSet,
+    Any,
+    Callable,
+    Dict,
+    List,
     Mapping,
     NamedTuple,
     Optional,
     Sequence,
     Set,
     Tuple,
+    cast,
 )
 
+import dagster._check as check
+from dagster._core.decorator_utils import get_function_params, get_valid_name_permutations
+from dagster._core.definitions.asset_in import AssetIn
 from dagster._core.definitions.asset_key import AssetKey
+from dagster._core.definitions.asset_spec import AssetSpec
 from dagster._core.definitions.input import In
 from dagster._core.definitions.output import Out
+from dagster._core.definitions.resource_annotation import (
+    get_resource_args,
+)
 from dagster._core.errors import DagsterInvalidDefinitionError
+from dagster._core.types.dagster_type import DagsterType, Nothing
 
 from ..asset_check_spec import AssetCheckSpec
+from ..utils import (
+    NoValueSentinel,
+)
+
+
+def stringify_asset_key_to_input_name(asset_key: AssetKey) -> str:
+    return "_".join(asset_key.path).replace("-", "_")
+
+
+def get_function_params_without_context_or_config_or_resources(
+    fn: Callable[..., Any],
+) -> List[Parameter]:
+    params = get_function_params(fn)
+    is_context_provided = len(params) > 0 and params[0].name in get_valid_name_permutations(
+        "context"
+    )
+    input_params = params[1:] if is_context_provided else params
+
+    resource_arg_names = {arg.name for arg in get_resource_args(fn)}
+
+    new_input_args = []
+    for input_arg in input_params:
+        if input_arg.name != "config" and input_arg.name not in resource_arg_names:
+            new_input_args.append(input_arg)
+
+    return new_input_args
+
+
+def build_asset_ins(
+    fn: Callable[..., Any],
+    asset_ins: Mapping[str, AssetIn],
+    deps: Optional[AbstractSet[AssetKey]],
+) -> Mapping[AssetKey, Tuple[str, In]]:
+    """Creates a mapping from AssetKey to (name of input, In object)."""
+    deps = check.opt_set_param(deps, "deps", AssetKey)
+
+    new_input_args = get_function_params_without_context_or_config_or_resources(fn)
+
+    non_var_input_param_names = [
+        param.name for param in new_input_args if param.kind == Parameter.POSITIONAL_OR_KEYWORD
+    ]
+    has_kwargs = any(param.kind == Parameter.VAR_KEYWORD for param in new_input_args)
+
+    all_input_names = set(non_var_input_param_names) | asset_ins.keys()
+
+    if not has_kwargs:
+        for in_key, asset_in in asset_ins.items():
+            if in_key not in non_var_input_param_names and (
+                not isinstance(asset_in.dagster_type, DagsterType)
+                or not asset_in.dagster_type.is_nothing
+            ):
+                raise DagsterInvalidDefinitionError(
+                    f"Key '{in_key}' in provided ins dict does not correspond to any of the names "
+                    "of the arguments to the decorated function"
+                )
+
+    ins_by_asset_key: Dict[AssetKey, Tuple[str, In]] = {}
+    for input_name in all_input_names:
+        asset_key = None
+
+        if input_name in asset_ins:
+            asset_key = asset_ins[input_name].key
+            metadata = asset_ins[input_name].metadata or {}
+            key_prefix = asset_ins[input_name].key_prefix
+            input_manager_key = asset_ins[input_name].input_manager_key
+            dagster_type = asset_ins[input_name].dagster_type
+        else:
+            metadata = {}
+            key_prefix = None
+            input_manager_key = None
+            dagster_type = NoValueSentinel
+
+        asset_key = asset_key or AssetKey(list(filter(None, [*(key_prefix or []), input_name])))
+
+        ins_by_asset_key[asset_key] = (
+            input_name.replace("-", "_"),
+            In(metadata=metadata, input_manager_key=input_manager_key, dagster_type=dagster_type),
+        )
+
+    for asset_key in deps:
+        if asset_key in ins_by_asset_key:
+            raise DagsterInvalidDefinitionError(
+                f"deps value {asset_key} also declared as input/AssetIn"
+            )
+            # mypy doesn't realize that Nothing is a valid type here
+        ins_by_asset_key[asset_key] = (
+            stringify_asset_key_to_input_name(asset_key),
+            In(cast(type, Nothing)),
+        )
+
+    return ins_by_asset_key
 
 
 class InMapping(NamedTuple):
@@ -41,6 +147,56 @@ class InOutMapper:
         self.can_subset = can_subset
 
     @staticmethod
+    def from_specs(
+        *,
+        specs: Sequence[AssetSpec],
+        check_specs: Sequence[AssetCheckSpec],
+        can_subset: bool,
+        ins: Mapping[str, AssetIn],
+        fn: Callable[..., Any],
+    ):
+        output_tuples_by_asset_key = {}
+        for asset_spec in specs:
+            output_name = asset_spec.key.to_python_identifier()
+            output_tuples_by_asset_key[asset_spec.key] = (
+                output_name,
+                Out(
+                    Nothing,
+                    is_required=not (can_subset or asset_spec.skippable),
+                    description=asset_spec.description,
+                    code_version=asset_spec.code_version,
+                    metadata=asset_spec.metadata,
+                ),
+            )
+
+        upstream_keys = set()
+        for spec in specs:
+            for dep in spec.deps:
+                if dep.asset_key not in output_tuples_by_asset_key:
+                    upstream_keys.add(dep.asset_key)
+                if (
+                    dep.asset_key in output_tuples_by_asset_key
+                    and dep.partition_mapping is not None
+                ):
+                    # self-dependent asset also needs to be considered an upstream_key
+                    upstream_keys.add(dep.asset_key)
+
+        explicit_ins = ins or {}
+        # get which asset keys have inputs set
+        loaded_upstreams = build_asset_ins(fn, explicit_ins, deps=set())
+        unexpected_upstreams = {key for key in loaded_upstreams.keys() if key not in upstream_keys}
+        if unexpected_upstreams:
+            raise DagsterInvalidDefinitionError(
+                f"Asset inputs {unexpected_upstreams} do not have dependencies on the passed"
+                " AssetSpec(s). Set the deps on the appropriate AssetSpec(s)."
+            )
+        remaining_upstream_keys = {key for key in upstream_keys if key not in loaded_upstreams}
+        asset_ins = build_asset_ins(fn, explicit_ins, deps=remaining_upstream_keys)
+        return InOutMapper.from_asset_ins_and_asset_outs(
+            asset_ins, output_tuples_by_asset_key, check_specs, can_subset
+        )
+
+    @staticmethod
     def from_asset_ins_and_asset_outs(
         asset_ins: Mapping[AssetKey, Tuple[str, In]],
         asset_outs: Mapping[AssetKey, Tuple[str, Out]],
@@ -62,7 +218,7 @@ class InOutMapper:
         return dict(self.out_mappings.values())
 
     @cached_property
-    def keys_by_output_name(self) -> Mapping[str, AssetKey]:
+    def asset_keys_by_output_name(self) -> Mapping[str, AssetKey]:
         return {
             out_mapping.output_name: asset_key
             for asset_key, out_mapping in self.out_mappings.items()
@@ -97,6 +253,30 @@ class InOutMapper:
         return set(self.asset_outs_by_output_name.keys()) & set(
             self.check_outs_by_output_name.keys()
         )
+
+    @cached_property
+    def asset_ins_by_input_names(self) -> Mapping[str, In]:
+        return {in_name: in_obj for in_name, in_obj in self.in_mappings.values()}
+
+    @cached_property
+    def asset_keys_by_input_names(self) -> Mapping[str, AssetKey]:
+        return {
+            in_mapping.input_name: asset_key for asset_key, in_mapping in self.in_mappings.items()
+        }
+
+    @cached_property
+    def input_tuples_by_asset_key(self) -> Mapping[AssetKey, Tuple[str, In]]:
+        return {
+            asset_key: (in_mapping.input_name, in_mapping.input)
+            for asset_key, in_mapping in self.in_mappings.items()
+        }
+
+    @cached_property
+    def output_tuples_by_asset_key(self) -> Mapping[AssetKey, Tuple[str, Out]]:
+        return {
+            asset_key: (out_mapping.output_name, out_mapping.output)
+            for asset_key, out_mapping in self.out_mappings.items()
+        }
 
 
 def validate_and_assign_output_names_to_check_specs(
