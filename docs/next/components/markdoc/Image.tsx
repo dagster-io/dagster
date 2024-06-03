f79a4681d06c6a2330efb1280c709e0bfde482dd
Niko <39569270+nikomancy@users.noreply.github.com>
2024-06-03 15:13:00 -0600

    Committing with half working client side solution.

--- docs/next/components/markdoc/Image.tsx
+++ docs/next/components/markdoc/Image.tsx
@@ -1,13 +1,31 @@
 import NextImage from 'next/image';
 import Zoom from 'react-medium-image-zoom';
 
-export const Image = ({children, ...props}) => {
+const getImageDimensions = (src) => {
+  console.log("getImageDimensions src works?", src);
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    console.log('inside getImageDimensions Img', img);
+    img.onload = () => {
+      resolve({ width: img.naturalWidth, height: img.naturalHeight });
+    };
+    img.onerror = (err) => {
+      reject(err);
+    };
+    img.src = src;
+  });
+};
+
+export const MyImage = ({children, ...props}) => {
+  console.log('Image', props);
   /* Only version images when all conditions meet:
    * - use <Image> component in mdx
    * - on non-master version
    * - in public/images/ dir
    */
   const {src} = props;
+  console.log('src', src);
+  // Handle External Images
   if (!src.startsWith('/images/')) {
     return (
       <span className="block mx-auto">
@@ -15,11 +33,10 @@ export const Image = ({children, ...props}) => {
       </span>
     );
   }
+  // Handle Internal Images
   return (
-    <Zoom wrapElement="span" wrapStyle={{display: 'block'}}>
-      <span className="block mx-auto">
-        <NextImage src={src} width={props.width} height={props.height} alt={props.alt} />
-      </span>
-    </Zoom>
+    <span className="block mx-auto">
+      <NextImage src={src} width={props.width} height={props.height} alt={props.alt} />
+    </span>
   );
 };
