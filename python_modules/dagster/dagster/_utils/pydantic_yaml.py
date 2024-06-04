85e84fa49d86e38c422bde0660c56757f3d8a0dd
benpankow <ben@dagsterlabs.com>
2024-06-04 11:07:22 -0700

    DagsterInvariantViolationError

--- python_modules/dagster/dagster/_utils/pydantic_yaml.py
+++ python_modules/dagster/dagster/_utils/pydantic_yaml.py
@@ -2,6 +2,7 @@ from typing import Sequence, Type, TypeVar
 
 from pydantic import BaseModel, ValidationError, parse_obj_as
 
+from dagster._core.errors import DagsterInvariantViolationError
 from dagster._model.pydantic_compat_layer import USING_PYDANTIC_1
 
 from .source_position import (
@@ -116,7 +117,7 @@ def parse_yaml_file_to_pydantic_sequence(
     parsed = parse_yaml_with_source_positions(src, filename)
 
     if not isinstance(parsed.value, list):
-        raise ValueError(
+        raise DagsterInvariantViolationError(
             f"Error parsing YAML file {filename}: Expected a list of objects at document root, but got {type(parsed.value)}"
         )
 
