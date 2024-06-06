838486e380f041ef8a0c96f5d9586fed8c1cd80e
Nick Schrock <schrockn@dagsterlabs.com>
2024-06-06 05:19:36 -0400

    fix tests

--- python_modules/dagster/dagster_tests/definitions_tests/automation_tests/test_basic_schedules.py
+++ python_modules/dagster/dagster_tests/definitions_tests/automation_tests/test_basic_schedules.py
@@ -3,9 +3,9 @@ from typing import Sequence
 from dagster import ScheduleDefinition, asset, job, op
 from dagster._core.definitions.asset_key import AssetKey
 from dagster._core.definitions.assets import AssetsDefinition
+from dagster._core.definitions.automation_target import AutomationTarget, make_synthetic_job_name
 from dagster._core.definitions.definitions_class import Definitions
 from dagster._core.definitions.job_definition import JobDefinition
-from dagster._core.definitions.schedule_definition import AutomationTarget, make_syntetic_job_name
 
 
 def make_automations_aware_definitions(schedules: Sequence[ScheduleDefinition]):
@@ -29,7 +29,7 @@ def test_basic_schedule_single_asset() -> None:
 
     assert isinstance(defs, Definitions)
 
-    assert isinstance(defs.get_job_def(make_syntetic_job_name(schedule.name)), JobDefinition)
+    assert isinstance(defs.get_job_def(make_synthetic_job_name(schedule.name)), JobDefinition)
     assert isinstance(defs.get_assets_def("my_asset"), AssetsDefinition)
 
 
@@ -48,7 +48,7 @@ def test_basic_schedule_multiple_assets() -> None:
 
     assert isinstance(defs, Definitions)
 
-    assert isinstance(defs.get_job_def(make_syntetic_job_name(schedule.name)), JobDefinition)
+    assert isinstance(defs.get_job_def(make_synthetic_job_name(schedule.name)), JobDefinition)
     assert isinstance(defs.get_assets_def("asset_one"), AssetsDefinition)
     assert isinstance(defs.get_assets_def("asset_two"), AssetsDefinition)
     assert not defs.get_asset_graph().has(AssetKey.from_coercible("slkjdfklsjdfl"))
