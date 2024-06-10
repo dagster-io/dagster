0d9d1d15866716de31b0b0443e25a1d56a591aff
bengotow <bgotow@elementl.com>
2024-06-10 18:17:44 -0500

    [ui] Switch to a class-based data fetcher outside React hook

--- js_modules/dagster-ui/packages/ui-core/src/runs/useCursorAccumulatedQuery.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/runs/useCursorAccumulatedQuery.tsx
@@ -1,45 +1,111 @@
-import {useQuery} from '@apollo/client';
+import {useApolloClient} from '@apollo/client';
 import {DocumentNode} from 'graphql';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
+import {EventEmitter} from 'ws';
 
-import {useQueryRefreshAtInterval} from '../app/QueryRefresh';
+import {useRefreshAtInterval} from '../app/QueryRefresh';
 
 type FetchState = {
   hasMore: boolean;
   cursor: string | null | undefined;
 };
 
-export function useCursorAccumulatedQuery<T, TVars extends {cursor?: string | null}, U>(options: {
+type FetcherFunction<DataType, CursorType, ErrorType> = (
+  cursor: CursorType | undefined,
+) => Promise<{
+  data: DataType[];
+  hasMore: boolean;
+  cursor: CursorType | undefined;
+  error: ErrorType;
+}>;
+
+class PaginatingDataFetcher<DataType, CursorType, ErrorType> extends EventEmitter {
+  private hasMoreData = true;
+  private dataSoFar: DataType[] = [];
+  private currentCursor: CursorType | undefined = undefined;
+  private stopped: boolean = false;
+  private fetchData: FetcherFunction<DataType, CursorType, ErrorType>;
+
+  constructor(fetchData: FetcherFunction<DataType, CursorType, ErrorType>) {
+    super();
+    this.fetchData = fetchData;
+  }
+
+  async start() {
+    if (this.stopped) {
+      throw new Error('PaginatingDataFetcher cannot be restarted');
+    }
+    this.emit('status-change', true);
+
+    while (this.hasMoreData) {
+      const {cursor, hasMore, data, error} = await this.fetchData(this.currentCursor);
+      if (error) {
+        this.emit('status-change', false);
+        throw error;
+      }
+      if (this.stopped) {
+        break;
+      }
+      this.currentCursor = cursor;
+      this.hasMoreData = hasMore;
+      if (data.length > 0) {
+        this.dataSoFar = this.dataSoFar.concat(data);
+        this.emit('data', this.dataSoFar);
+      }
+    }
+    this.emit('status-change', false);
+  }
+
+  stop() {
+    this.stopped = true;
+  }
+}
+
+export function useCursorAccumulatedQuery<T, TVars extends {cursor?: string | null}, U>({
+  query,
+  variables,
+  getResultArray,
+  getNextFetchState,
+}: {
   query: DocumentNode;
-  skip?: boolean;
   variables: Omit<TVars, 'cursor'>;
   getResultArray: (result: T | undefined) => U[];
   getNextFetchState: (result: T | undefined) => FetchState | null;
 }) {
   const [fetched, setFetched] = useState<U[] | null>(null);
-  const [fetchState, setFetchState] = useState<FetchState>({hasMore: true, cursor: undefined});
-  const {query, skip, variables, getResultArray, getNextFetchState} = options;
+  const [fetching, setFetching] = useState(false);
+  const client = useApolloClient();
 
-  const queryResult = useQuery<T, TVars>(query, {
-    variables: {...variables, cursor: fetchState.cursor} as TVars,
-    notifyOnNetworkStatusChange: true,
-    skip,
+  const refresh = useRef<() => Promise<void>>(() => Promise.resolve());
+  const refreshState = useRefreshAtInterval({
+    refresh: refresh.current,
+    enabled: !fetching,
+    intervalMs: 10000,
   });
 
   useEffect(() => {
-    if (!queryResult.data) {
-      return;
-    }
-    const result = getResultArray(queryResult.data);
-    setFetched((fetched) => (fetched || []).concat(result));
-    const next = getNextFetchState(queryResult.data);
-    if (next) {
-      setFetchState(next);
-    }
-  }, [queryResult, getResultArray, getNextFetchState]);
+    const fetcher = new PaginatingDataFetcher(async (cursor: string | undefined) => {
+      const resp = await client.query<T, TVars>({
+        variables: {...variables, cursor},
+        query,
+      });
 
-  // When we have reached the last event, switch to refreshing every 10s
-  const refreshState = useQueryRefreshAtInterval(queryResult, 10 * 1000, !fetchState.hasMore);
+      // Todo align this with the data fetcher better
+      return {...getNextFetchState(resp.data), data: getResultArray(resp.data), error: null};
+    });
+
+    refresh.current = () => fetcher.start();
+    fetcher.on('data', setFetched);
+    fetcher.on('status-change', setFetching);
+    fetcher.on('finish', () => setFetching(false));
+    void fetcher.start();
+    return () => {
+      fetcher.removeAllListeners('data');
+      fetcher.removeAllListeners('status-change');
+      fetcher.removeAllListeners('finish');
+      fetcher.stop();
+    };
+  }, [query, client, variables, getResultArray, getNextFetchState]);
 
   return {fetched, refreshState};
 }
