4c01aea63ebe3346b5d5fc7aa5e5a926d96b33e6
Marco Salazar <marco@elementl.com>
2024-06-09 21:45:22 -0400

    clear cache data

--- js_modules/dagster-ui/packages/ui-core/src/workspace/WorkspaceContext.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/workspace/WorkspaceContext.tsx
@@ -26,6 +26,7 @@ import {useStateWithStorage} from '../hooks/useStateWithStorage';
 import {useUpdatingRef} from '../hooks/useUpdatingRef';
 import {codeLocationStatusAtom} from '../nav/useCodeLocationsStatus';
 import {
+  useClearCachedData,
   useGetCachedData,
   useGetData,
   useIndexedDBCachedQuery,
@@ -114,6 +115,7 @@ export const WorkspaceProvider = ({children}: {children: React.ReactNode}) => {
 
   const getCachedData = useGetCachedData();
   const getData = useGetData();
+  const clearCachedData = useClearCachedData();
 
   useLayoutEffect(() => {
     // Load data from the cache
@@ -250,12 +252,12 @@ export const WorkspaceProvider = ({children}: {children: React.ReactNode}) => {
     const copy = {...locationsData};
     locationsRemoved.forEach((loc) => {
       delete copy[loc.name];
-      indexedDB.deleteDatabase(`${localCacheIdPrefix}${locationWorkspaceKey(loc.name)}`);
+      clearCachedData({key: `${localCacheIdPrefix}${locationWorkspaceKey(loc.name)}`});
     });
     if (Object.keys(copy).length !== Object.keys(locationsData).length) {
       setLocationsData(copy);
     }
-  }, [localCacheIdPrefix, locationsData, locationsRemoved]);
+  }, [clearCachedData, localCacheIdPrefix, locationsData, locationsRemoved]);
 
   const locationEntries = useMemo(
     () =>
