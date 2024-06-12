1475f62f6e808f45f9fb22e05d8ec0c56bf29c05
Maxime Armstrong <maxime@retain.ai>
2024-06-12 15:33:34 -0400

    Implement prepared() in DbtProject

--- python_modules/libraries/dagster-dbt/dagster_dbt/dbt_project.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/dbt_project.py
@@ -64,6 +64,12 @@ class DagsterDbtManifestPreparer(DbtManifestPreparer):
                     f"after running '{self.prepare.__qualname__}'. Ensure the implementation respects "
                     "all DbtProject properties."
                 )
+        else:
+            if not project.manifest_path.exists():
+                raise DagsterDbtManifestNotFoundError(
+                    f"Did not find manifest.json at expected path {project.manifest_path} "
+                    f"when deploying'. Ensure manifest.json is created at build time."
+                )
 
     def prepare(self, project: "DbtProject") -> None:
         # guard against multiple Dagster processes trying to update this at the same time
@@ -146,7 +152,7 @@ class DbtProject(IHaveNew):
 
             from dagster_dbt import DbtProject
 
-            my_project = DbtProject(project_dir=Path("path/to/dbt_project"))
+            my_project = DbtProject(project_dir=Path("path/to/dbt_project")).prepared()
 
         Creating a DbtProject that changes target based on environment variables and uses manged state artifacts:
 
@@ -169,7 +175,7 @@ class DbtProject(IHaveNew):
                 project_dir=Path('path/to/dbt_project'),
                 state_path="target/managed_state",
                 target=get_env(),
-            )
+            ).prepared()
 
     """
 
@@ -221,7 +227,7 @@ class DbtProject(IHaveNew):
             dependencies_path.exists() or packages_path.exists()
         ) and not packages_install_path.exists()
 
-        val = super().__new__(
+        super().__new__(
             cls,
             project_dir=project_dir,
             target_path=target_path,
@@ -232,6 +238,44 @@ class DbtProject(IHaveNew):
             has_uninstalled_deps=has_uninstalled_deps,
             manifest_preparer=manifest_preparer,
         )
-        if manifest_preparer:
-            manifest_preparer.on_load(val)
-        return val
+
+    def prepared(self) -> "DbtProject":
+        """Execute the preparation process for a dbt project and return the DbtProject object when complete.
+
+        Examples:
+            Preparing a DbtProject on creation (recommended):
+
+            .. code-block:: python
+
+                from pathlib import Path
+
+                from dagster import Definitions
+                from dagster_dbt import DbtProject
+
+                my_project = DbtProject(project_dir=Path("path/to/dbt_project")).prepared()
+
+                defs = Definitions(
+                    resources={
+                        "dbt": DbtCliResource(project_dir=my_project),
+                    },
+                    ...
+                )
+
+            Preparing a dbt project on usage:
+
+            .. code-block:: python
+
+                from pathlib import Path
+
+                from dagster_dbt import DbtProject, dbt_assets
+
+                my_project = DbtProject(project_dir=Path("path/to/dbt_project"))
+
+
+                @dbt_assets(manifest=my_project.prepared())
+                def my_dbt_asset():
+                    ...
+        """
+        if self.manifest_preparer:
+            self.manifest_preparer.on_load(self)
+        return self
--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/core/test_project.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/core/test_project.py
@@ -5,6 +5,7 @@ from dagster._core.test_utils import environ
 from dagster._utils.test import copy_directory
 from dagster_dbt.dbt_manifest import validate_manifest
 from dagster_dbt.dbt_project import DbtProject
+from dagster_dbt.errors import DagsterDbtManifestNotFoundError
 
 from ..dbt_projects import test_jaffle_shop_path
 
@@ -17,7 +18,7 @@ def shared_project_dir():
 
 @pytest.fixture(scope="function")
 def project_dir(shared_project_dir):
-    manifest_path = DbtProject(shared_project_dir).manifest_path
+    manifest_path = DbtProject(shared_project_dir).prepared().manifest_path
     if manifest_path.exists():
         manifest_path.unlink()
     yield shared_project_dir
@@ -26,21 +27,29 @@ def project_dir(shared_project_dir):
 def test_local_dev(project_dir) -> None:
     my_project = DbtProject(project_dir)
     assert not my_project.manifest_path.exists()
+    with pytest.raises(DagsterDbtManifestNotFoundError):
+        # The preparation process will fail locally
+        # if no env var are provided and the manifest does not exist
+        DbtProject(project_dir).prepared()
     with environ({"DAGSTER_IS_DEV_CLI": "1"}):
-        my_project = DbtProject(project_dir)
+        my_project = DbtProject(project_dir).prepared()
         assert my_project.manifest_path.exists()
 
 
 def test_opt_in_env_var(project_dir) -> None:
     my_project = DbtProject(project_dir)
     assert not my_project.manifest_path.exists()
+    with pytest.raises(DagsterDbtManifestNotFoundError):
+        # The preparation process will fail locally
+        # if no env var are provided and the manifest does not exist
+        DbtProject(project_dir).prepared()
     with environ({"DAGSTER_DBT_PARSE_PROJECT_ON_LOAD": "1"}):
-        my_project = DbtProject(project_dir)
+        my_project = DbtProject(project_dir).prepared()
         assert my_project.manifest_path.exists()
 
 
 def _init(project_dir):
-    my_project = DbtProject(project_dir)
+    my_project = DbtProject(project_dir).prepared()
     assert my_project.manifest_path.exists()
     assert validate_manifest(my_project.manifest_path)
     return
@@ -49,6 +58,10 @@ def _init(project_dir):
 def test_concurrent_processes(project_dir):
     my_project = DbtProject(project_dir)
     assert not my_project.manifest_path.exists()
+    with pytest.raises(DagsterDbtManifestNotFoundError):
+        # The preparation process will fail locally
+        # if no env var are provided and the manifest does not exist
+        DbtProject(project_dir).prepared()
     with environ({"DAGSTER_IS_DEV_CLI": "1"}):
         procs = [multiprocessing.Process(target=_init, args=(project_dir,)) for _ in range(4)]
         for proc in procs:
