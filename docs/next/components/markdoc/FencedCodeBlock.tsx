381341c3c61f394fcb5052602c05c6bf55a0e70f
Niko <39569270+nikomancy@users.noreply.github.com>
2024-06-20 10:00:21 -0600

    Adding working copy to clipboard function.

--- docs/next/components/markdoc/FencedCodeBlock.tsx
+++ docs/next/components/markdoc/FencedCodeBlock.tsx
@@ -1,24 +1,37 @@
+import {Transition} from '@headlessui/react';
 import Prism from 'prismjs';
 import React from 'react';
 import 'prismjs/components/prism-python';
+import { useCopyToClipboard } from 'react-use';
 
 Prism.manual = true;
 
-// Define possible options for code blocks
 
-export const Fence = ({children, 'data-language': language}) => {
-  console.log(children, '\n', language);
+export const Fence = (props) => {
+  const text = props.children;
+  const language = props['data-language'];
+  const [copied, setCopied] = React.useState(false);
+  const [state, copy] = useCopyToClipboard();
+
   React.useEffect(() => {
-    console.log('highlighting');
     Prism.highlightAll();
   }, []);
 
+
+  const copyToClipboard = React.useCallback(() => {
+    if (typeof text === 'string') {
+      copy(text);
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+    }
+  }, [copy, text]);
+
   return (
     <div aria-live="polite">
-      <pre className="line-numbers">
-        <code className={`language-${language}`}>
-          {children}
-        </code>
+      <pre className="line-numbers" onClick={copyToClipboard}>
+        <code className={`language-${language}`}> {text} </code>
       </pre>
     </div>
   );
