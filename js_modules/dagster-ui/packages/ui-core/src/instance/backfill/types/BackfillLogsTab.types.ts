fc79aafb3e59ca1e6bd204e3b29b84575bba5d9b
Ben Gotow <ben@foundry376.com>
2024-06-21 15:29:47 -0500

    [ui] Switch useCursorAccumulatedQuery to a less hook-specific solution (#22451)
    ## Summary & Motivation
    
    Follow-up from PR feedback in #22409
    
    ## How I Tested These Changes
    
    Expanded the existing test case to verify the "Refreshing..." label
    disappears, and also tested it manually in the new backfill logs UI
    
    ---------
    
    Co-authored-by: bengotow <bgotow@elementl.com>
    Co-authored-by: Marco Salazar <marco@elementl.com>

--- js_modules/dagster-ui/packages/ui-core/src/instance/backfill/types/BackfillLogsTab.types.ts
+++ js_modules/dagster-ui/packages/ui-core/src/instance/backfill/types/BackfillLogsTab.types.ts
@@ -10,7 +10,7 @@ export type BackfillLogsPageQueryVariables = Types.Exact<{
 export type BackfillLogsPageQuery = {
   __typename: 'Query';
   partitionBackfillOrError:
-    | {__typename: 'BackfillNotFoundError'}
+    | {__typename: 'BackfillNotFoundError'; message: string}
     | {
         __typename: 'PartitionBackfill';
         id: string;
@@ -26,5 +26,5 @@ export type BackfillLogsPageQuery = {
           }>;
         };
       }
-    | {__typename: 'PythonError'};
+    | {__typename: 'PythonError'; message: string};
 };
