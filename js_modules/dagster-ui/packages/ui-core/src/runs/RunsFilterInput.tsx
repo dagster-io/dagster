fcb05f006fa775820c76783480568a71654adb24
Marco Salazar <marco@elementl.com>
2024-06-25 11:47:47 -0400

    allow free-form partition

--- js_modules/dagster-ui/packages/ui-core/src/runs/RunsFilterInput.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/runs/RunsFilterInput.tsx
@@ -417,28 +417,25 @@ export const useRunsFilterInput = ({tokens, onChange, enabledFilters}: RunsFilte
     matchType: 'any-of',
   });
 
-  const partitionsFilter = useStaticSetFilter({
+  const partitionsFilter = useSuggestionFilter({
     name: 'Partition',
     icon: 'partition',
-    allValues: partitionValues,
-    allowMultipleSelections: false,
+    initialSuggestions: partitionValues,
+    getNoSuggestionsPlaceholder: (query) => (query ? 'Invalid ID' : 'Type or paste a backfill ID'),
+
     state: useMemo(() => {
-      return new Set(
-        tokens
-          .filter(
-            ({token, value}) => token === 'tag' && value.split('=')[0] === DagsterTag.Partition,
-          )
-          .map(({value}) => tagValueToFilterObject(value)),
-      );
+      return tokens
+        .filter(({token, value}) => token === 'tag' && value.split('=')[0] === DagsterTag.Partition)
+        .map(({value}) => tagValueToFilterObject(value));
     }, [tokens]),
-    renderLabel: ({value}) => (
-      <Box flex={{direction: 'row', gap: 4, alignItems: 'center'}}>
-        <Icon name="job" />
-        <TruncatedTextWithFullTextOnHover text={value.value!} />
-      </Box>
-    ),
-    getStringValue: ({value}) => value!,
-    onStateChanged: (values) => {
+
+    freeformSearchResult: (query) => {
+      return {
+        value: tagValueToFilterObject(`${DagsterTag.Partition}=${query.trim()}`),
+        final: true,
+      };
+    },
+    setState: (values) => {
       onChange([
         ...tokens.filter(({token, value}) => {
           if (token !== 'tag') {
@@ -452,6 +449,26 @@ export const useRunsFilterInput = ({tokens, onChange, enabledFilters}: RunsFilte
         })),
       ]);
     },
+    getStringValue: ({value}) => value,
+    getKey: ({value}) => value,
+    renderLabel: ({value}) => (
+      <Box flex={{direction: 'row', gap: 4, alignItems: 'center'}}>
+        <Icon name="partition" />
+        <TruncatedTextWithFullTextOnHover text={value.value} />
+      </Box>
+    ),
+    onSuggestionClicked: async (value) => {
+      return [{value}];
+    },
+    renderActiveStateLabel: ({value}) => (
+      <Box flex={{direction: 'row', gap: 4, alignItems: 'center'}}>
+        <Icon name="partition" />
+        <TruncatedTextWithFullTextOnHover text={value.value} />
+        {value.value!}
+      </Box>
+    ),
+    isMatch: ({value}, query) => value.toLowerCase().includes(query.toLowerCase()),
+    matchType: 'any-of',
   });
 
   const launchedByFilter = useStaticSetFilter({
