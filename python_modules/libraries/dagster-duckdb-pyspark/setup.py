28aa0a8ade20ccc42cc53ff3436fea48ff25582e
elementl <devtools@elementl.com>
2024-06-27 19:58:49 +0000

    1.7.11

--- python_modules/libraries/dagster-duckdb-pyspark/setup.py
+++ python_modules/libraries/dagster-duckdb-pyspark/setup.py
@@ -34,8 +34,8 @@ setup(
     include_package_data=True,
     python_requires=">=3.8,<3.13",
     install_requires=[
-        f"dagster{pin}",
-        f"dagster-duckdb{pin}",
+        "dagster==1.7.11",
+        "dagster-duckdb==0.23.11",
         "pyspark>=3",
         "pandas",
         "pyarrow",
