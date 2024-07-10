dagster-io/dagster

commit aaffc523ae7c12b2ad42f8c0494ed754080e9777

[DA-Internals][3/n] Decorator updates (#22890)
## Summary & Motivation

Updates decorators to accept AutomationConditions in addition to AutoMaterializePolicy objects. As with other PRs in the stack, this introduces no breaking changes.

## How I Tested These Changes

python_modules/dagster/dagster/__init__.py
@@ -163,9 +163,10 @@ from dagster._core.definitions.data_version import (
     DataVersion as DataVersion,
     DataVersionsByPartition as DataVersionsByPartition,
 )
-from dagster._core.definitions.declarative_automation import (
-    AssetCondition as AssetCondition,
+from dagster._core.definitions.declarative_automation.automation_condition import (
     AutomationCondition as AutomationCondition,
+)
+from dagster._core.definitions.declarative_automation.automation_condition_tester import (
     evaluate_automation_conditions as evaluate_automation_conditions,
 )
 from dagster._core.definitions.decorators.asset_check_decorator import (

python_modules/dagster/dagster/_core/definitions/asset_checks.py
@@ -133,7 +133,7 @@ def build_asset_with_blocking_check(
         tags=None,
         owners=None,
         freshness_policy=spec.freshness_policy,
-        auto_materialize_policy=spec.auto_materialize_policy,
+        automation_condition=spec.automation_condition,
         backfill_policy=asset_def.backfill_policy,
         config=None,  # gets config from asset_def.op
     )

python_modules/dagster/dagster/_core/definitions/asset_out.py
@@ -6,6 +6,9 @@ from dagster._core.definitions.asset_dep import AssetDep
 from dagster._core.definitions.asset_spec import AssetSpec
 from dagster._core.definitions.auto_materialize_policy import AutoMaterializePolicy
 from dagster._core.definitions.backfill_policy import BackfillPolicy
+from dagster._core.definitions.declarative_automation.automation_condition import (
+    AutomationCondition,
+)
 from dagster._core.definitions.events import (
     AssetKey,
     CoercibleToAssetKey,
@@ -18,7 +21,7 @@ from dagster._core.definitions.utils import DEFAULT_IO_MANAGER_KEY
 from dagster._core.types.dagster_type import DagsterType, resolve_dagster_type
 from dagster._utils.warnings import disable_dagster_warnings
 
-from .utils import validate_tags_strict
+from .utils import resolve_automation_condition, validate_tags_strict
 
 
 @experimental_param(param="owners")
@@ -37,7 +40,7 @@ class AssetOut(
             ("group_name", PublicAttr[Optional[str]]),
             ("code_version", PublicAttr[Optional[str]]),
             ("freshness_policy", PublicAttr[Optional[FreshnessPolicy]]),
-            ("auto_materialize_policy", PublicAttr[Optional[AutoMaterializePolicy]]),
+            ("automation_condition", PublicAttr[Optional[AutomationCondition]]),
             ("backfill_policy", PublicAttr[Optional[BackfillPolicy]]),
             ("owners", PublicAttr[Optional[Sequence[str]]]),
             ("tags", PublicAttr[Optional[Mapping[str, str]]]),
@@ -69,8 +72,8 @@ class AssetOut(
         code_version (Optional[str]): The version of the code that generates this asset.
         freshness_policy (Optional[FreshnessPolicy]): (Deprecated) A policy which indicates how up
             to date this asset is intended to be.
-        auto_materialize_policy (Optional[AutoMaterializePolicy]): AutoMaterializePolicy to apply to
-            the specified asset.
+        automation_condition (Optional[AutomationCondition]): AutomationCondition to apply to the
+            specified asset.
         backfill_policy (Optional[BackfillPolicy]): BackfillPolicy to apply to the specified asset.
         owners (Optional[Sequence[str]]): A list of strings representing owners of the asset. Each
             string can be a user's email address, or a team name prefixed with `team:`,
@@ -91,10 +94,12 @@ class AssetOut(
         group_name: Optional[str] = None,
         code_version: Optional[str] = None,
         freshness_policy: Optional[FreshnessPolicy] = None,
-        auto_materialize_policy: Optional[AutoMaterializePolicy] = None,
+        automation_condition: Optional[AutomationCondition] = None,
         backfill_policy: Optional[BackfillPolicy] = None,
         owners: Optional[Sequence[str]] = None,
         tags: Optional[Mapping[str, str]] = None,
+        # TODO: FOU-243
+        auto_materialize_policy: Optional[AutoMaterializePolicy] = None,
     ):
         if isinstance(key_prefix, str):
             key_prefix = [key_prefix]
@@ -119,8 +124,10 @@ class AssetOut(
             freshness_policy=check.opt_inst_param(
                 freshness_policy, "freshness_policy", FreshnessPolicy
             ),
-            auto_materialize_policy=check.opt_inst_param(
-                auto_materialize_policy, "auto_materialize_policy", AutoMaterializePolicy
+            automation_condition=check.opt_inst_param(
+                resolve_automation_condition(automation_condition, auto_materialize_policy),
+                "automation_condition",
+                AutomationCondition,
             ),
             backfill_policy=check.opt_inst_param(
                 backfill_policy, "backfill_policy", BackfillPolicy
@@ -149,11 +156,17 @@ class AssetOut(
                 group_name=self.group_name,
                 code_version=self.code_version,
                 freshness_policy=self.freshness_policy,
-                automation_condition=self.auto_materialize_policy.to_automation_condition()
-                if self.auto_materialize_policy
-                else None,
+                automation_condition=self.automation_condition,
                 owners=self.owners,
                 tags=self.tags,
                 deps=deps,
                 auto_materialize_policy=None,
             )
+
+    @property
+    def auto_materialize_policy(self) -> Optional[AutoMaterializePolicy]:
+        return (
+            self.automation_condition.as_auto_materialize_policy()
+            if self.automation_condition
+            else None
+        )

python_modules/dagster/dagster/_core/definitions/asset_spec.py
@@ -4,6 +4,9 @@ from typing import TYPE_CHECKING, Any, Iterable, Mapping, NamedTuple, Optional, 
 
 import dagster._check as check
 from dagster._annotations import PublicAttr, experimental_param
+from dagster._core.definitions.declarative_automation.automation_condition import (
+    AutomationCondition,
+)
 from dagster._core.definitions.utils import validate_asset_owner
 from dagster._serdes.serdes import whitelist_for_serdes
 from dagster._utils.internal_init import IHasInternalInit
@@ -16,9 +19,6 @@ from .utils import resolve_automation_condition, validate_tags_strict
 
 if TYPE_CHECKING:
     from dagster._core.definitions.asset_dep import AssetDep, CoercibleToAssetDep
-    from dagster._core.definitions.declarative_automation.automation_condition import (
-        AutomationCondition,
-    )
 
 # SYSTEM_METADATA_KEY_ASSET_EXECUTION_TYPE lives on the metadata of an asset
 # (which currently ends up on the Output associated with the asset key)
@@ -78,7 +78,7 @@ class AssetSpec(
             ("skippable", PublicAttr[bool]),
             ("code_version", PublicAttr[Optional[str]]),
             ("freshness_policy", PublicAttr[Optional[FreshnessPolicy]]),
-            ("automation_condition", PublicAttr[Optional["AutomationCondition"]]),
+            ("automation_condition", PublicAttr[Optional[AutomationCondition]]),
             ("owners", PublicAttr[Sequence[str]]),
             ("tags", PublicAttr[Mapping[str, str]]),
         ],
@@ -125,14 +125,13 @@ class AssetSpec(
         group_name: Optional[str] = None,
         code_version: Optional[str] = None,
         freshness_policy: Optional[FreshnessPolicy] = None,
-        automation_condition: Optional["AutomationCondition"] = None,
+        automation_condition: Optional[AutomationCondition] = None,
         owners: Optional[Sequence[str]] = None,
         tags: Optional[Mapping[str, str]] = None,
         # TODO: FOU-243
         auto_materialize_policy: Optional[AutoMaterializePolicy] = None,
     ):
         from dagster._core.definitions.asset_dep import coerce_to_deps_and_check_duplicates
-        from dagster._core.definitions.declarative_automation import AutomationCondition
 
         key = AssetKey.from_coercible(key)
         asset_deps = coerce_to_deps_and_check_duplicates(deps, key)
@@ -175,7 +174,7 @@ class AssetSpec(
         group_name: Optional[str],
         code_version: Optional[str],
         freshness_policy: Optional[FreshnessPolicy],
-        automation_condition: Optional["AutomationCondition"],
+        automation_condition: Optional[AutomationCondition],
         owners: Optional[Sequence[str]],
         tags: Optional[Mapping[str, str]],
         auto_materialize_policy: Optional[AutoMaterializePolicy],

python_modules/dagster/dagster/_core/definitions/assets.py
@@ -33,6 +33,9 @@ from dagster._core.definitions.asset_spec import (
 )
 from dagster._core.definitions.auto_materialize_policy import AutoMaterializePolicy
 from dagster._core.definitions.backfill_policy import BackfillPolicy, BackfillPolicyType
+from dagster._core.definitions.declarative_automation.automation_condition import (
+    AutomationCondition,
+)
 from dagster._core.definitions.dependency import NodeHandle, NodeOutputHandle
 from dagster._core.definitions.freshness_policy import FreshnessPolicy
 from dagster._core.definitions.graph_definition import SubselectedGraphDefinition
@@ -83,7 +86,6 @@ from .source_asset import SourceAsset
 from .utils import DEFAULT_GROUP_NAME, validate_tags_strict
 
 if TYPE_CHECKING:
-    from .declarative_automation import AutomationCondition
     from .graph_definition import GraphDefinition
 
 ASSET_SUBSET_INPUT_PREFIX = "__subset_input__"
@@ -526,7 +528,7 @@ class AssetsDefinition(ResourceAddable, RequiresResources, IHasInternalInit):
         tags_by_output_name: Optional[Mapping[str, Optional[Mapping[str, str]]]] = None,
         freshness_policies_by_output_name: Optional[Mapping[str, Optional[FreshnessPolicy]]] = None,
         automation_conditions_by_output_name: Optional[
-            Mapping[str, Optional["AutomationCondition"]]
+            Mapping[str, Optional[AutomationCondition]]
         ] = None,
         backfill_policy: Optional[BackfillPolicy] = None,
         can_subset: bool = False,
@@ -588,8 +590,8 @@ class AssetsDefinition(ResourceAddable, RequiresResources, IHasInternalInit):
                 FreshnessPolicy to be associated with some or all of the output assets for this node.
                 Keys are the names of the outputs, and values are the FreshnessPolicies to be attached
                 to the associated asset.
-            automation_conditions_by_output_name (Optional[Mapping[str, Optional["AutomationCondition"]]]): Defines an
-                "AutomationCondition" to be associated with some or all of the output assets for this node.
+            automation_conditions_by_output_name (Optional[Mapping[str, Optional[AutomationCondition]]]): Defines an
+                AutomationCondition to be associated with some or all of the output assets for this node.
                 Keys are the names of the outputs, and values are the AutoMaterializePolicies to be attached
                 to the associated asset.
             backfill_policy (Optional[BackfillPolicy]): Defines this asset's BackfillPolicy
@@ -641,7 +643,7 @@ class AssetsDefinition(ResourceAddable, RequiresResources, IHasInternalInit):
         tags_by_output_name: Optional[Mapping[str, Optional[Mapping[str, str]]]] = None,
         freshness_policies_by_output_name: Optional[Mapping[str, Optional[FreshnessPolicy]]] = None,
         automation_conditions_by_output_name: Optional[
-            Mapping[str, Optional["AutomationCondition"]]
+            Mapping[str, Optional[AutomationCondition]]
         ] = None,
         backfill_policy: Optional[BackfillPolicy] = None,
         can_subset: bool = False,
@@ -696,8 +698,8 @@ class AssetsDefinition(ResourceAddable, RequiresResources, IHasInternalInit):
                 FreshnessPolicy to be associated with some or all of the output assets for this node.
                 Keys are the names of the outputs, and values are the FreshnessPolicies to be attached
                 to the associated asset.
-            automation_conditions_by_output_name (Optional[Mapping[str, Optional["AutomationCondition"]]]): Defines an
-                "AutomationCondition" to be associated with some or all of the output assets for this node.
+            automation_conditions_by_output_name (Optional[Mapping[str, Optional[AutomationCondition]]]): Defines an
+                AutomationCondition to be associated with some or all of the output assets for this node.
                 Keys are the names of the outputs, and values are the AutoMaterializePolicies to be attached
                 to the associated asset.
             backfill_policy (Optional[BackfillPolicy]): Defines this asset's BackfillPolicy
@@ -743,7 +745,7 @@ class AssetsDefinition(ResourceAddable, RequiresResources, IHasInternalInit):
         freshness_policies_by_output_name: Optional[Mapping[str, Optional[FreshnessPolicy]]] = None,
         code_versions_by_output_name: Optional[Mapping[str, Optional[str]]] = None,
         automation_conditions_by_output_name: Optional[
-            Mapping[str, Optional["AutomationCondition"]]
+            Mapping[str, Optional[AutomationCondition]]
         ] = None,
         backfill_policy: Optional[BackfillPolicy] = None,
         can_subset: bool = False,
@@ -1081,7 +1083,7 @@ class AssetsDefinition(ResourceAddable, RequiresResources, IHasInternalInit):
         }
 
     @property
-    def automation_conditions_by_key(self) -> Mapping[AssetKey, "AutomationCondition"]:
+    def automation_conditions_by_key(self) -> Mapping[AssetKey, AutomationCondition]:
         return {
             key: spec.automation_condition
             for key, spec in self._specs_by_key.items()
@@ -1798,11 +1800,11 @@ def _validate_graph_def(graph_def: "GraphDefinition", prefix: Optional[Sequence[
 
 
 def _resolve_automation_conditions_by_output_name(
-    automation_conditions_by_output_name: Optional[Mapping[str, Optional["AutomationCondition"]]],
+    automation_conditions_by_output_name: Optional[Mapping[str, Optional[AutomationCondition]]],
     auto_materialize_policies_by_output_name: Optional[
         Mapping[str, Optional[AutoMaterializePolicy]]
     ],
-) -> Optional[Mapping[str, Optional["AutomationCondition"]]]:
+) -> Optional[Mapping[str, Optional[AutomationCondition]]]:
     if auto_materialize_policies_by_output_name is not None:
         check.param_invariant(
             automation_conditions_by_output_name is None,
@@ -1868,13 +1870,11 @@ def _asset_specs_from_attr_key_params(
     metadata_by_key: Optional[Mapping[AssetKey, ArbitraryMetadataMapping]],
     tags_by_key: Optional[Mapping[AssetKey, Mapping[str, str]]],
     freshness_policies_by_key: Optional[Mapping[AssetKey, FreshnessPolicy]],
-    automation_conditions_by_key: Optional[Mapping[AssetKey, "AutomationCondition"]],
+    automation_conditions_by_key: Optional[Mapping[AssetKey, AutomationCondition]],
     code_versions_by_key: Optional[Mapping[AssetKey, str]],
     descriptions_by_key: Optional[Mapping[AssetKey, str]],
     owners_by_key: Optional[Mapping[AssetKey, Sequence[str]]],
 ) -> Sequence[AssetSpec]:
-    from dagster._core.definitions.declarative_automation import AutomationCondition
-
     validated_group_names_by_key = check.opt_mapping_param(
         group_names_by_key, "group_names_by_key", key_type=AssetKey, value_type=str
     )

python_modules/dagster/dagster/_core/definitions/auto_materialize_policy.py
@@ -300,7 +300,11 @@ class AutoMaterializePolicy(
     def to_automation_condition(self) -> "AutomationCondition":
         """Converts a set of materialize / skip rules into a single binary expression."""
         from .auto_materialize_rule_impls import DiscardOnMaxMaterializationsExceededRule
-        from .declarative_automation import AndAssetCondition, NotAssetCondition, OrAssetCondition
+        from .declarative_automation.operators import (
+            AndAssetCondition,
+            NotAssetCondition,
+            OrAssetCondition,
+        )
 
         if self.asset_condition is not None:
             return self.asset_condition

python_modules/dagster/dagster/_core/definitions/base_asset_graph.py
@@ -47,7 +47,9 @@ from .time_window_partitions import get_time_partition_key, get_time_partitions_
 if TYPE_CHECKING:
     from dagster._core.definitions.asset_graph_subset import AssetGraphSubset
     from dagster._core.definitions.auto_materialize_policy import AutoMaterializePolicy
-    from dagster._core.definitions.declarative_automation import AutomationCondition
+    from dagster._core.definitions.declarative_automation.automation_condition import (
+        AutomationCondition,
+    )
 
 
 class ParentsPartitionsResult(NamedTuple):

python_modules/dagster/dagster/_core/definitions/declarative_automation/__init__.py
@@ -1,31 +1,0 @@
-from .automation_condition import AutomationCondition as AutomationCondition
-from .automation_condition_tester import (
-    evaluate_automation_conditions as evaluate_automation_conditions,
-)
-from .legacy import RuleCondition as RuleCondition
-from .legacy.asset_condition import AssetCondition as AssetCondition
-from .operands import (
-    CronTickPassedCondition as CronTickPassedCondition,
-    FailedAutomationCondition as FailedAutomationCondition,
-    InLatestTimeWindowCondition as InLatestTimeWindowCondition,
-    InProgressAutomationCondition as InProgressAutomationCondition,
-    MissingAutomationCondition as MissingAutomationCondition,
-    NewlyRequestedCondition as NewlyRequestedCondition,
-    NewlyUpdatedCondition as NewlyUpdatedCondition,
-    WillBeRequestedCondition as WillBeRequestedCondition,
-)
-from .operators import (
-    AllDepsCondition as AllDepsCondition,
-    AndAssetCondition as AndAssetCondition,
-    AnyDepsCondition as AnyDepsCondition,
-    NewlyTrueCondition as NewlyTrueCondition,
-    NotAssetCondition as NotAssetCondition,
-    OrAssetCondition as OrAssetCondition,
-    SinceCondition as SinceCondition,
-)
-from .serialized_objects import (
-    AssetConditionEvaluationState as AssetConditionEvaluationState,
-    AssetConditionSnapshot as AssetConditionSnapshot,
-    AssetSubsetWithMetadata as AssetSubsetWithMetadata,
-    HistoricalAllPartitionsSubsetSentinel as HistoricalAllPartitionsSubsetSentinel,
-)

commit d4fcd726db90bd413730957db1206e010140eae1

Augment dagster dev sanity check test to actually verify that it could load the toys code location
Summary:
This test previously detected dagit crashes on startup, but not issues with loading the toys repo. Now it does both.

Test Plan: BK (this test is failing in master currently due to an issue introduced by https://github.com/dagster-io/dagster/pull/22890)

integration_tests/test_suites/daemon-test-suite/dagster_dev_command_tests/test_dagster_dev_command.py
@@ -78,6 +78,14 @@ def test_dagster_dev_command_loads_toys():
                 )
                 try:
                     _wait_for_dagit_running(dagit_port)
+
+                    client = DagsterGraphQLClient(hostname="localhost", port_number=dagit_port)
+                    locations_and_names = client._get_repo_locations_and_names_with_pipeline(  # noqa
+                        "hammer"
+                    )
+                    assert (
+                        len(locations_and_names) > 0
+                    ), "toys repo failed to load or was missing a job called 'hammer'"
                 finally:
                     dev_process.send_signal(signal.SIGINT)
                     dev_process.communicate()

integration_tests/test_suites/daemon-test-suite/tox.ini
@@ -22,6 +22,8 @@ deps =
   -e ../../../python_modules/libraries/dagster-celery-k8s
   -e ../../../python_modules/libraries/dagster-postgres
   -e ../../../python_modules/libraries/dagster-docker
+  -e ../../../python_modules/libraries/dagstermill
+  -e ../../../python_modules/libraries/dagster-slack
 allowlist_externals =
   /bin/bash
   uv