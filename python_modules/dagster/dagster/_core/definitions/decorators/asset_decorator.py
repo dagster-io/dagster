e327fae54bf4a796baa066f4eb9316f0eeb21805
Owen Kephart <owen@elementl.com>
2024-07-16 15:33:01 -0700

    add automation_condition_param

--- python_modules/dagster/dagster/_core/definitions/decorators/asset_decorator.py
+++ python_modules/dagster/dagster/_core/definitions/decorators/asset_decorator.py
@@ -104,13 +104,17 @@ def asset(
 
 @experimental_param(param="resource_defs")
 @experimental_param(param="io_manager_def")
-@experimental_param(param="auto_materialize_policy")
 @experimental_param(param="automation_condition")
 @experimental_param(param="backfill_policy")
 @experimental_param(param="owners")
 @experimental_param(param="tags")
 @deprecated_param(
     param="non_argument_deps", breaking_version="2.0.0", additional_warn_text="use `deps` instead."
+)
+@deprecated_param(
+    param="auto_materialize_policy",
+    breaking_version="1.9.0",
+    additional_warn_text="use `automation_condition` instead.",
 )
 def asset(
     compute_fn: Optional[Callable[..., Any]] = None,
--- python_modules/dagster/dagster/_core/definitions/load_assets_from_modules.py
+++ python_modules/dagster/dagster/_core/definitions/load_assets_from_modules.py
@@ -5,9 +5,14 @@ from types import ModuleType
 from typing import Dict, Iterable, Iterator, List, Optional, Sequence, Set, Tuple, Union, cast
 
 import dagster._check as check
+from dagster._annotations import deprecated_param
 from dagster._core.definitions.auto_materialize_policy import AutoMaterializePolicy
 from dagster._core.definitions.backfill_policy import BackfillPolicy
+from dagster._core.definitions.declarative_automation.automation_condition import (
+    AutomationCondition,
+)
 from dagster._core.definitions.freshness_policy import FreshnessPolicy
+from dagster._core.definitions.utils import resolve_automation_condition
 from dagster._core.errors import DagsterInvalidDefinitionError
 
 from .asset_key import (
@@ -92,6 +97,11 @@ def assets_from_modules(
     return list(set(assets.values())), source_assets, cacheable_assets
 
 
+@deprecated_param(
+    param="auto_materialize_policy",
+    breaking_version="1.9",
+    additional_warn_text="use `automation_condition` instead.",
+)
 def load_assets_from_modules(
     modules: Iterable[ModuleType],
     group_name: Optional[str] = None,
@@ -99,6 +109,7 @@ def load_assets_from_modules(
     *,
     freshness_policy: Optional[FreshnessPolicy] = None,
     auto_materialize_policy: Optional[AutoMaterializePolicy] = None,
+    automation_condition: Optional[AutomationCondition] = None,
     backfill_policy: Optional[BackfillPolicy] = None,
     source_key_prefix: Optional[CoercibleToAssetKeyPrefix] = None,
 ) -> Sequence[Union[AssetsDefinition, SourceAsset, CacheableAssetsDefinition]]:
@@ -114,7 +125,7 @@ def load_assets_from_modules(
             of the loaded objects, with the prefix prepended.
         freshness_policy (Optional[FreshnessPolicy]): FreshnessPolicy to apply to all the loaded
             assets.
-        auto_materialize_policy (Optional[AutoMaterializePolicy]): AutoMaterializePolicy to apply
+        automation_condition (Optional[AutomationCondition]): AutomationCondition to apply
             to all the loaded assets.
         backfill_policy (Optional[AutoMaterializePolicy]): BackfillPolicy to apply to all the loaded assets.
         source_key_prefix (bool): Prefix to prepend to the keys of loaded SourceAssets. The returned
@@ -127,9 +138,6 @@ def load_assets_from_modules(
     group_name = check.opt_str_param(group_name, "group_name")
     key_prefix = check_opt_coercible_to_asset_key_prefix_param(key_prefix, "key_prefix")
     freshness_policy = check.opt_inst_param(freshness_policy, "freshness_policy", FreshnessPolicy)
-    auto_materialize_policy = check.opt_inst_param(
-        auto_materialize_policy, "auto_materialize_policy", AutoMaterializePolicy
-    )
     backfill_policy = check.opt_inst_param(backfill_policy, "backfill_policy", BackfillPolicy)
 
     (
@@ -145,18 +153,26 @@ def load_assets_from_modules(
         key_prefix=key_prefix,
         group_name=group_name,
         freshness_policy=freshness_policy,
-        auto_materialize_policy=auto_materialize_policy,
+        automation_condition=resolve_automation_condition(
+            automation_condition, auto_materialize_policy
+        ),
         backfill_policy=backfill_policy,
         source_key_prefix=source_key_prefix,
     )
 
 
+@deprecated_param(
+    param="auto_materialize_policy",
+    breaking_version="1.9",
+    additional_warn_text="use `automation_condition` instead.",
+)
 def load_assets_from_current_module(
     group_name: Optional[str] = None,
     key_prefix: Optional[CoercibleToAssetKeyPrefix] = None,
     *,
     freshness_policy: Optional[FreshnessPolicy] = None,
     auto_materialize_policy: Optional[AutoMaterializePolicy] = None,
+    automation_condition: Optional[AutomationCondition] = None,
     backfill_policy: Optional[BackfillPolicy] = None,
     source_key_prefix: Optional[CoercibleToAssetKeyPrefix] = None,
 ) -> Sequence[Union[AssetsDefinition, SourceAsset, CacheableAssetsDefinition]]:
@@ -172,7 +188,7 @@ def load_assets_from_current_module(
             of the loaded objects, with the prefix prepended.
         freshness_policy (Optional[FreshnessPolicy]): FreshnessPolicy to apply to all the loaded
             assets.
-        auto_materialize_policy (Optional[AutoMaterializePolicy]): AutoMaterializePolicy to apply
+        automation_condition (Optional[AutomationCondition]): AutomationCondition to apply
             to all the loaded assets.
         backfill_policy (Optional[AutoMaterializePolicy]): BackfillPolicy to apply to all the loaded assets.
         source_key_prefix (bool): Prefix to prepend to the keys of loaded SourceAssets. The returned
@@ -192,7 +208,9 @@ def load_assets_from_current_module(
         group_name=group_name,
         key_prefix=key_prefix,
         freshness_policy=freshness_policy,
-        auto_materialize_policy=auto_materialize_policy,
+        automation_condition=resolve_automation_condition(
+            automation_condition, auto_materialize_policy
+        ),
         backfill_policy=backfill_policy,
         source_key_prefix=source_key_prefix,
     )
@@ -220,6 +238,11 @@ def assets_from_package_module(
     )
 
 
+@deprecated_param(
+    param="auto_materialize_policy",
+    breaking_version="1.9",
+    additional_warn_text="use `automation_condition` instead.",
+)
 def load_assets_from_package_module(
     package_module: ModuleType,
     group_name: Optional[str] = None,
@@ -227,6 +250,7 @@ def load_assets_from_package_module(
     *,
     freshness_policy: Optional[FreshnessPolicy] = None,
     auto_materialize_policy: Optional[AutoMaterializePolicy] = None,
+    automation_condition: Optional[AutomationCondition] = None,
     backfill_policy: Optional[BackfillPolicy] = None,
     source_key_prefix: Optional[CoercibleToAssetKeyPrefix] = None,
 ) -> Sequence[Union[AssetsDefinition, SourceAsset, CacheableAssetsDefinition]]:
@@ -245,7 +269,7 @@ def load_assets_from_package_module(
             of the loaded objects, with the prefix prepended.
         freshness_policy (Optional[FreshnessPolicy]): FreshnessPolicy to apply to all the loaded
             assets.
-        auto_materialize_policy (Optional[AutoMaterializePolicy]): AutoMaterializePolicy to apply
+        automation_condition (Optional[AutomationCondition]): AutomationCondition to apply
             to all the loaded assets.
         backfill_policy (Optional[AutoMaterializePolicy]): BackfillPolicy to apply to all the loaded assets.
         source_key_prefix (bool): Prefix to prepend to the keys of loaded SourceAssets. The returned
@@ -258,9 +282,6 @@ def load_assets_from_package_module(
     group_name = check.opt_str_param(group_name, "group_name")
     key_prefix = check_opt_coercible_to_asset_key_prefix_param(key_prefix, "key_prefix")
     freshness_policy = check.opt_inst_param(freshness_policy, "freshness_policy", FreshnessPolicy)
-    auto_materialize_policy = check.opt_inst_param(
-        auto_materialize_policy, "auto_materialize_policy", AutoMaterializePolicy
-    )
     backfill_policy = check.opt_inst_param(backfill_policy, "backfill_policy", BackfillPolicy)
 
     (
@@ -275,7 +296,9 @@ def load_assets_from_package_module(
         key_prefix=key_prefix,
         group_name=group_name,
         freshness_policy=freshness_policy,
-        auto_materialize_policy=auto_materialize_policy,
+        automation_condition=resolve_automation_condition(
+            automation_condition, auto_materialize_policy
+        ),
         backfill_policy=backfill_policy,
         source_key_prefix=source_key_prefix,
     )
@@ -441,7 +464,7 @@ def assets_with_attributes(
     key_prefix: Optional[Sequence[str]],
     group_name: Optional[str],
     freshness_policy: Optional[FreshnessPolicy],
-    auto_materialize_policy: Optional[AutoMaterializePolicy],
+    automation_condition: Optional[AutomationCondition],
     backfill_policy: Optional[BackfillPolicy],
     source_key_prefix: Optional[Sequence[str]],
 ) -> Sequence[Union[AssetsDefinition, SourceAsset, CacheableAssetsDefinition]]:
@@ -457,7 +480,7 @@ def assets_with_attributes(
             cached_asset.with_prefix_for_all(key_prefix) for cached_asset in cacheable_assets
         ]
 
-    if group_name or freshness_policy or auto_materialize_policy or backfill_policy:
+    if group_name or freshness_policy or automation_condition or backfill_policy:
         assets_defs = [
             asset.with_attributes(
                 group_names_by_key=(
@@ -466,7 +489,7 @@ def assets_with_attributes(
                     else {}
                 ),
                 freshness_policy=freshness_policy,
-                auto_materialize_policy=auto_materialize_policy,
+                automation_condition=automation_condition,
                 backfill_policy=backfill_policy,
             )
             for asset in assets_defs
@@ -480,7 +503,9 @@ def assets_with_attributes(
             cached_asset.with_attributes_for_all(
                 group_name,
                 freshness_policy=freshness_policy,
-                auto_materialize_policy=auto_materialize_policy,
+                auto_materialize_policy=automation_condition.as_auto_materialize_policy()
+                if automation_condition
+                else None,
                 backfill_policy=backfill_policy,
             )
             for cached_asset in cacheable_assets
