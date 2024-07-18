1a03d774b78b8ca7e8539825f58390e0d00bc2c9
Rex Ledesma <rex@dagsterlabs.com>
2024-07-18 13:57:03 -0400

    chore: enable `include_package_data`

--- python_modules/libraries/dagster-mlflow/setup.py
+++ python_modules/libraries/dagster-mlflow/setup.py
@@ -31,6 +31,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=find_packages(exclude=["dagster_mlflow_tests*"]),
+    include_package_data=True,
     python_requires=">=3.8,<3.13",
     install_requires=[f"dagster{pin}", "mlflow", "pandas"],
     zip_safe=False,
