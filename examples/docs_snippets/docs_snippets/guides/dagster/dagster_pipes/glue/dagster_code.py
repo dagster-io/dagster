c4328ab4a12c7ae1e34841aee89ff720f840b7f2
Daniel Gafni <danielgafni16@gmail.com>
2024-07-18 09:43:18 +0300

    [docs] [dagster-aws] add docs for PipesGlueClient (#22969)
    ## Summary & Motivation
    Adding docs for Glue Dagster Pipes
    
    
    ## How I Tested These Changes
    
    ---------
    
    Co-authored-by: Marco polo <salazarm@users.noreply.github.com>
    Co-authored-by: prha <1040172+prha@users.noreply.github.com>
    Co-authored-by: Erin Cochran <erin.k.cochran@gmail.com>

---
+++ examples/docs_snippets/docs_snippets/guides/dagster/dagster_pipes/glue/dagster_code.py
@@ -0,0 +1,49 @@
+# start_asset_marker
+import os
+
+# dagster_glue_pipes.py
+import boto3
+from dagster_aws.pipes import PipesGlueClient
+
+from dagster import AssetExecutionContext, asset
+
+
+@asset
+def glue_pipes_asset(
+    context: AssetExecutionContext, pipes_glue_client: PipesGlueClient
+):
+    return pipes_glue_client.run(
+        context=context,
+        job_name="Example Job",
+        arguments={"some_parameter_value": "1"},
+    ).get_materialize_result()
+
+
+# end_asset_marker
+
+# start_definitions_marker
+
+from dagster import Definitions  # noqa
+from dagster_aws.pipes import PipesGlueContextInjector, PipesS3MessageReader
+
+
+bucket = os.environ["DAGSTER_GLUE_S3_CONTEXT_BUCKET"]
+
+
+defs = Definitions(
+    assets=[glue_pipes_asset],
+    resources={
+        "pipes_glue_client": PipesGlueClient(
+            client=boto3.client("glue"),
+            context_injector=PipesGlueContextInjector(
+                client=boto3.client("s3"),
+                bucket=bucket,
+            ),
+            message_reader=PipesS3MessageReader(
+                client=boto3.client("s3"), bucket=bucket
+            ),
+        )
+    },
+)
+
+# end_definitions_marker
