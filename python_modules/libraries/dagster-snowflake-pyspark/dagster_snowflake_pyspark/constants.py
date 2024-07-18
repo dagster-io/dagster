b25c7dca61617a741672b9a7e14bf524b034e3c4
Mathieu Larose <mathieu@retain.ai>
2024-07-18 12:50:08 -0400

    feat: add snowflake partner connection id to dagster-snowflake

---
+++ python_modules/libraries/dagster-snowflake-pyspark/dagster_snowflake_pyspark/constants.py
@@ -0,0 +1,5 @@
+# Description: This file contains the Snowflake connection identifiers for the Snowflake partner account.
+# The connection identifiers are used to identify the partner account when connecting to Snowflake.
+# We use different connection identifiers for different connection code paths to ensure that each is
+# working as expected.
+SNOWFLAKE_PARTNER_CONNECTION_IDENTIFIER_PYSPARK = "DagsterLabs_Dagster_Pyspark"
