dagster-io/dagster

commit 96651d468b60f95d04180ee6da7dbe8425c0c89a

Guard against undefined entry in WorkspaceContext (#23105)
## Summary & Motivation

fixes
https://linear.app/dagster-labs/issue/FE-468/[bug]-deleting-a-code-location-in-dagster-causes-application-error-a.

js_modules/dagster-ui/packages/ui-core/src/workspace/WorkspaceContext.tsx
@@ -113,7 +113,7 @@ export const WorkspaceProvider = ({children}: {children: React.ReactNode}) => {
   const [didLoadStatusData, setDidLoadStatusData] = useState(false);
 
   const [locationEntriesData, setLocationEntriesData] = React.useState<
-    Record<string, WorkspaceLocationNodeFragment | PythonErrorFragment>
+    Record<string, WorkspaceLocationNodeFragment | PythonErrorFragment | undefined>
   >({});
 
   const getCachedData = useGetCachedData();
@@ -265,7 +265,7 @@ export const WorkspaceProvider = ({children}: {children: React.ReactNode}) => {
     () =>
       Object.values(locationEntriesData).filter(
         (entry): entry is WorkspaceLocationNodeFragment =>
-          entry.__typename === 'WorkspaceLocationEntry',
+          !!entry && entry.__typename === 'WorkspaceLocationEntry',
       ),
     [locationEntriesData],
   );

commit 2d6808a66017822d44d18c2c6cf31a133ae9d4ab

Fix typescript error (#23139)
Fix ts error introduced in
https://github.com/dagster-io/dagster/pull/23105
## How I Tested These Changes
buildkite

js_modules/dagster-ui/packages/ui-core/src/workspace/WorkspaceContext.tsx
@@ -113,7 +113,7 @@ export const WorkspaceProvider = ({children}: {children: React.ReactNode}) => {
   const [didLoadStatusData, setDidLoadStatusData] = useState(false);
 
   const [locationEntriesData, setLocationEntriesData] = React.useState<
-    Record<string, WorkspaceLocationNodeFragment | PythonErrorFragment | undefined>
+    Record<string, WorkspaceLocationNodeFragment | PythonErrorFragment>
   >({});
 
   const getCachedData = useGetCachedData();
@@ -184,11 +184,13 @@ export const WorkspaceProvider = ({children}: {children: React.ReactNode}) => {
         bypassCache: true,
       });
       const entry = locationData.data?.workspaceLocationEntryOrError;
-      setLocationEntriesData((locationsData) =>
-        Object.assign({}, locationsData, {
-          [name]: entry,
-        }),
-      );
+      if (entry) {
+        setLocationEntriesData((locationsData) =>
+          Object.assign({}, locationsData, {
+            [name]: entry,
+          }),
+        );
+      }
       return locationData;
     },
     [client, getData, localCacheIdPrefix],
@@ -236,11 +238,11 @@ export const WorkspaceProvider = ({children}: {children: React.ReactNode}) => {
       Array.from(
         new Set([
           ...Object.values(prevLocationStatuses.current).filter(
-            (loc) => !locationStatuses[loc.name],
+            (loc) => loc && !locationStatuses[loc.name],
           ),
           ...Object.values(locationEntriesData).filter(
             (loc): loc is WorkspaceLocationNodeFragment =>
-              loc?.__typename === 'WorkspaceLocationEntry' && !locationStatuses[loc.name],
+              loc && loc?.__typename === 'WorkspaceLocationEntry' && !locationStatuses[loc.name],
           ),
         ]),
       ),