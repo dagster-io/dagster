857001d8ecc39feb4e4502e72a7bc2cf418a4832
Isaac Hellendag <dish@elementl.com>
2024-07-24 15:01:29 -0500

    [ui] Make automation target a tag

--- js_modules/dagster-ui/packages/ui-core/src/automation/__stories__/AutomationTargetList.stories.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/automation/__stories__/AutomationTargetList.stories.tsx
@@ -1,8 +1,16 @@
 import {MockedProvider} from '@apollo/client/testing';
 import {Meta} from '@storybook/react';
+import {RecoilRoot} from 'recoil';
 
 import {CustomAlertProvider} from '../../app/CustomAlertProvider';
-import {SensorType} from '../../graphql/types';
+import {
+  SensorType,
+  buildAsset,
+  buildAssetConnection,
+  buildAssetKey,
+  buildAssetSelection,
+  buildPythonError,
+} from '../../graphql/types';
 import {
   buildStartKansasSuccess,
   buildStartLouisianaError,
@@ -17,10 +25,9 @@ export default {
   component: AutomationTargetList,
 } as Meta;
 
-export const AutomationTargetListWithError = () => {
+export const SingleJob = () => {
   return (
-    <>
-      <CustomAlertProvider />
+    <RecoilRoot>
       <MockedProvider mocks={[buildStartKansasSuccess(1000), buildStartLouisianaError(1000)]}>
         <WorkspaceProvider>
           <AutomationTargetList
@@ -31,6 +38,101 @@ export const AutomationTargetListWithError = () => {
           />
         </WorkspaceProvider>
       </MockedProvider>
+    </RecoilRoot>
+  );
+};
+
+export const MultipleJobs = () => {
+  return (
+    <RecoilRoot>
+      <MockedProvider mocks={[buildStartKansasSuccess(1000), buildStartLouisianaError(1000)]}>
+        <WorkspaceProvider>
+          <AutomationTargetList
+            automationType={SensorType.ASSET}
+            targets={[{pipelineName: 'foo'}, {pipelineName: 'bar'}, {pipelineName: 'baz'}]}
+            repoAddress={buildRepoAddress('a', 'b')}
+            assetSelection={null}
+          />
+        </WorkspaceProvider>
+      </MockedProvider>
+    </RecoilRoot>
+  );
+};
+
+export const AssetSelection = () => {
+  const assetSelection = buildAssetSelection({
+    assetSelectionString: 'asset_one',
+    assetsOrError: buildAssetConnection({
+      nodes: [buildAsset({id: 'asset_one', key: buildAssetKey({path: ['a', 'b']})})],
+    }),
+  });
+
+  return (
+    <RecoilRoot>
+      <MockedProvider mocks={[buildStartKansasSuccess(1000), buildStartLouisianaError(1000)]}>
+        <WorkspaceProvider>
+          <AutomationTargetList
+            automationType={SensorType.ASSET}
+            targets={null}
+            repoAddress={buildRepoAddress('a', 'b')}
+            assetSelection={assetSelection}
+          />
+        </WorkspaceProvider>
+      </MockedProvider>
+    </RecoilRoot>
+  );
+};
+
+export const MultipleAssetSelection = () => {
+  const assetSelection = buildAssetSelection({
+    assetSelectionString: 'asset_one and asset_two',
+    assetsOrError: buildAssetConnection({
+      nodes: [
+        buildAsset({id: 'asset_one', key: buildAssetKey({path: ['a', 'b']})}),
+        buildAsset({id: 'asset_two', key: buildAssetKey({path: ['c', 'd']})}),
+      ],
+    }),
+  });
+
+  return (
+    <RecoilRoot>
+      <MockedProvider mocks={[buildStartKansasSuccess(1000), buildStartLouisianaError(1000)]}>
+        <WorkspaceProvider>
+          <AutomationTargetList
+            automationType={SensorType.ASSET}
+            targets={null}
+            repoAddress={buildRepoAddress('a', 'b')}
+            assetSelection={assetSelection}
+          />
+        </WorkspaceProvider>
+      </MockedProvider>
+    </RecoilRoot>
+  );
+};
+
+export const PythonError = () => {
+  const assetSelection = buildAssetSelection({
+    assetSelectionString: 'asset_one and asset_two',
+    assetsOrError: buildPythonError({
+      message: 'oh nooo',
+    }),
+  });
+
+  return (
+    <>
+      <CustomAlertProvider />
+      <RecoilRoot>
+        <MockedProvider mocks={[buildStartKansasSuccess(1000), buildStartLouisianaError(1000)]}>
+          <WorkspaceProvider>
+            <AutomationTargetList
+              automationType={SensorType.ASSET}
+              targets={null}
+              repoAddress={buildRepoAddress('a', 'b')}
+              assetSelection={assetSelection}
+            />
+          </WorkspaceProvider>
+        </MockedProvider>
+      </RecoilRoot>
     </>
   );
 };
