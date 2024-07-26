2b01a753bca370207b3ecc829263ea9593b3b85b
Maxime Armstrong <maxime@retain.ai>
2024-07-26 12:33:47 -0400

    Update tests

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/cli/test_scaffold.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/cli/test_scaffold.py
@@ -25,8 +25,6 @@ def _assert_scaffold_invocation(
     project_name: str,
     dbt_project_dir: Path,
     dagster_project_dir: Path,
-    use_dbt_project_package_data_dir: bool,
-    use_dbt_project: bool,
 ) -> None:
     result = runner.invoke(
         app,
@@ -37,8 +35,6 @@ def _assert_scaffold_invocation(
             project_name,
             "--dbt-project-dir",
             os.fspath(dbt_project_dir),
-            *(["--use-dbt-project-package-data-dir"] if use_dbt_project_package_data_dir else []),
-            *(["--use-dbt-project"] if use_dbt_project else []),
         ],
     )
 
@@ -53,12 +49,8 @@ def _assert_scaffold_invocation(
         in dagster_project_dir.joinpath("setup.py").read_text()
     )
 
-    if use_dbt_project_package_data_dir or use_dbt_project:
-        assert dagster_project_dir.joinpath(project_name, "project.py").exists()
-        assert not dagster_project_dir.joinpath(project_name, "constants.py").exists()
-    else:
-        assert not dagster_project_dir.joinpath(project_name, "project.py").exists()
-        assert dagster_project_dir.joinpath(project_name, "constants.py").exists()
+    assert dagster_project_dir.joinpath(project_name, "project.py").exists()
+    assert not dagster_project_dir.joinpath(project_name, "constants.py").exists()
 
 
 def _assert_scaffold_defs(project_name: str, dagster_project_dir: Path) -> None:
@@ -78,43 +70,32 @@ def _assert_scaffold_defs(project_name: str, dagster_project_dir: Path) -> None:
 
 
 def _update_dbt_project_path(
-    dbt_project_dir: Path,
     dagster_project_dir: Path,
-    use_dbt_project_package_data_dir: bool,
 ) -> Path:
-    if use_dbt_project_package_data_dir:
-        dbt_project_dir = dagster_project_dir.joinpath("dbt-project")
-        shutil.copytree(src=test_jaffle_shop_path, dst=dbt_project_dir)
+    dbt_project_dir = dagster_project_dir.joinpath("dbt-project")
+    shutil.copytree(src=test_jaffle_shop_path, dst=dbt_project_dir)
 
     return dbt_project_dir
 
 
-@pytest.mark.parametrize("use_dbt_project", [True, False])
-@pytest.mark.parametrize("use_dbt_project_package_data_dir", [True, False])
 def test_project_scaffold_command_with_precompiled_manifest(
     monkeypatch: pytest.MonkeyPatch,
     tmp_path: Path,
     dbt_project_dir: Path,
-    use_dbt_project_package_data_dir: bool,
-    use_dbt_project: bool,
 ) -> None:
     monkeypatch.chdir(tmp_path)
 
-    project_name = f"test_dagster_scaffold_precompiled_manifest_{use_dbt_project}"
+    project_name = f"test_dagster_scaffold_precompiled_manifest"
     dagster_project_dir = tmp_path.joinpath(project_name)
 
     _assert_scaffold_invocation(
         project_name=project_name,
         dbt_project_dir=dbt_project_dir,
         dagster_project_dir=dagster_project_dir,
-        use_dbt_project_package_data_dir=use_dbt_project_package_data_dir,
-        use_dbt_project=use_dbt_project,
     )
 
     dbt_project_dir = _update_dbt_project_path(
-        dbt_project_dir=dbt_project_dir,
         dagster_project_dir=dagster_project_dir,
-        use_dbt_project_package_data_dir=use_dbt_project_package_data_dir,
     )
 
     subprocess.run(["dbt", "compile"], cwd=dbt_project_dir, check=True)
@@ -127,71 +108,24 @@ def test_project_scaffold_command_with_precompiled_manifest(
     _assert_scaffold_defs(project_name=project_name, dagster_project_dir=dagster_project_dir)
 
 
-@pytest.mark.parametrize("use_dbt_project", [False])
-@pytest.mark.parametrize("use_dbt_project_package_data_dir", [False])
-def test_project_scaffold_command_without_dbt_project_with_runtime_manifest(
-    monkeypatch: pytest.MonkeyPatch,
-    tmp_path: Path,
-    dbt_project_dir: Path,
-    use_dbt_project_package_data_dir: bool,
-    use_dbt_project: bool,
-) -> None:
-    monkeypatch.chdir(tmp_path)
-
-    project_name = f"test_dagster_scaffold_runtime_manifest_{use_dbt_project}"
-    dagster_project_dir = tmp_path.joinpath(project_name)
-
-    _assert_scaffold_invocation(
-        project_name=project_name,
-        dbt_project_dir=dbt_project_dir,
-        dagster_project_dir=dagster_project_dir,
-        use_dbt_project_package_data_dir=use_dbt_project_package_data_dir,
-        use_dbt_project=use_dbt_project,
-    )
-
-    dbt_project_dir = _update_dbt_project_path(
-        dbt_project_dir=dbt_project_dir,
-        dagster_project_dir=dagster_project_dir,
-        use_dbt_project_package_data_dir=use_dbt_project_package_data_dir,
-    )
-
-    monkeypatch.setenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD", "1")
-    monkeypatch.chdir(tmp_path)
-    sys.path.append(os.fspath(tmp_path))
-
-    _assert_scaffold_defs(project_name=project_name, dagster_project_dir=dagster_project_dir)
-
-
-@pytest.mark.parametrize("use_dbt_project", [True, False])
-@pytest.mark.parametrize("use_dbt_project_package_data_dir", [True, False])
 def test_project_scaffold_command_with_dbt_project_with_runtime_manifest(
     monkeypatch: pytest.MonkeyPatch,
     tmp_path: Path,
     dbt_project_dir: Path,
-    use_dbt_project_package_data_dir: bool,
-    use_dbt_project: bool,
 ) -> None:
-    # Skip the case where we don't use DbtProject
-    if not use_dbt_project_package_data_dir and not use_dbt_project:
-        pytest.skip()
-
     monkeypatch.chdir(tmp_path)
 
-    project_name = f"test_dagster_scaffold_runtime_manifest_{use_dbt_project}"
+    project_name = f"test_dagster_scaffold_runtime_manifest"
     dagster_project_dir = tmp_path.joinpath(project_name)
 
     _assert_scaffold_invocation(
         project_name=project_name,
         dbt_project_dir=dbt_project_dir,
         dagster_project_dir=dagster_project_dir,
-        use_dbt_project_package_data_dir=use_dbt_project_package_data_dir,
-        use_dbt_project=use_dbt_project,
     )
 
     dbt_project_dir = _update_dbt_project_path(
-        dbt_project_dir=dbt_project_dir,
         dagster_project_dir=dagster_project_dir,
-        use_dbt_project_package_data_dir=use_dbt_project_package_data_dir,
     )
 
     # DbtProject does not support the opt-in env var anymore, only the local dev env var
@@ -202,39 +136,32 @@ def test_project_scaffold_command_with_dbt_project_with_runtime_manifest(
     _assert_scaffold_defs(project_name=project_name, dagster_project_dir=dagster_project_dir)
 
 
-@pytest.mark.parametrize("use_dbt_project", [True, False])
-@pytest.mark.parametrize("use_dbt_project_package_data_dir", [True, False])
 def test_project_scaffold_command_with_runtime_manifest_without_env_var(
     monkeypatch: pytest.MonkeyPatch,
     tmp_path: Path,
     dbt_project_dir: Path,
-    use_dbt_project_package_data_dir: bool,
-    use_dbt_project: bool,
 ) -> None:
     monkeypatch.chdir(tmp_path)
 
-    project_name = f"test_scaffold_runtime_without_env_var_{use_dbt_project}"
+    monkeypatch.delenv("DAGSTER_IS_DEV_CLI", raising=False)
+
+    project_name = f"test_scaffold_runtime_without_env_var"
     dagster_project_dir = tmp_path.joinpath(project_name)
 
     _assert_scaffold_invocation(
         project_name=project_name,
         dbt_project_dir=dbt_project_dir,
         dagster_project_dir=dagster_project_dir,
-        use_dbt_project_package_data_dir=use_dbt_project_package_data_dir,
-        use_dbt_project=use_dbt_project,
     )
 
     dbt_project_dir = _update_dbt_project_path(
-        dbt_project_dir=dbt_project_dir,
         dagster_project_dir=dagster_project_dir,
-        use_dbt_project_package_data_dir=use_dbt_project_package_data_dir,
     )
 
     monkeypatch.chdir(tmp_path)
     sys.path.append(os.fspath(tmp_path))
 
     with pytest.raises(DagsterDbtManifestNotFoundError):
-        monkeypatch.delenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD", raising=False)
         monkeypatch.delenv("DAGSTER_IS_DEV_CLI", raising=False)
         importlib.import_module(f"{project_name}.{project_name}.definitions")
 
