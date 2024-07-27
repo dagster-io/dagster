dd458ac53fd01dcac97766b8cd70710a46487e4e
Sandy Ryza <sandy@elementl.com>
2024-07-27 08:17:53 -0700

    create_external_run accept asset graph, not partitions def
    branch-name: siab/create-external-run-asset-graph

--- python_modules/dagster/dagster/_scheduler/scheduler.py
+++ python_modules/dagster/dagster/_scheduler/scheduler.py
@@ -984,7 +984,9 @@ def _create_scheduler_run(
             frozenset(run_request.asset_selection) if run_request.asset_selection else None
         ),
         asset_check_selection=None,
-        asset_job_partitions_def=code_location.get_asset_job_partitions_def(external_job),
+        asset_graph=code_location.get_repository(
+            external_job.repository_handle.repository_name
+        ).asset_graph,
     )
 
 
