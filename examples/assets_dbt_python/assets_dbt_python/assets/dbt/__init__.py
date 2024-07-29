4e2e4b7a057f88be95686610d9ca28600b260214
benpankow <ben@dagsterlabs.com>
2024-07-29 15:21:36 -0700

    Revert "Update examples to remove Dagster code from __init__.py (part 2) (#20809)"
    This reverts commit 8bd0d1e8617d1d4adefad6543e7e334bdb5faec6.

--- examples/assets_dbt_python/assets_dbt_python/assets/dbt/__init__.py
+++ examples/assets_dbt_python/assets_dbt_python/assets/dbt/__init__.py
@@ -3,7 +3,7 @@ from typing import Any, Mapping
 from dagster import AssetExecutionContext, AssetKey, file_relative_path
 from dagster_dbt import DagsterDbtTranslator, DbtCliResource, dbt_assets, get_asset_key_for_model
 
-DBT_PROJECT_DIR = file_relative_path(__file__, "../../dbt_project")
+DBT_PROJECT_DIR = file_relative_path(__file__, "../../../dbt_project")
 
 dbt_resource = DbtCliResource(project_dir=DBT_PROJECT_DIR)
 dbt_parse_invocation = dbt_resource.cli(["parse"]).wait()
