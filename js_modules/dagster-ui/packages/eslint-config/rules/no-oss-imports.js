74eecc418bdd9ec3a20f0cf173b2e82f4ac54878
Marco Salazar <marco@elementl.com>
2024-08-05 14:26:43 -0400

    update lint rule

--- js_modules/dagster-ui/packages/eslint-config/rules/no-oss-imports.js
+++ js_modules/dagster-ui/packages/eslint-config/rules/no-oss-imports.js
@@ -31,23 +31,17 @@ module.exports = {
           });
         }
       },
-      CallExpression(node) {
-        if (
-          node.callee.type === 'Import' &&
-          node.arguments[0] &&
-          node.arguments[0].type === 'Literal' &&
-          node.arguments[0].value.endsWith('.oss') &&
-          node.arguments[0].value.startsWith('.')
-        ) {
+      ImportExpression(node) {
+        if (node.source.value.endsWith('.oss') && node.source.value.startsWith('.')) {
           context.report({
             node,
             message: 'Relative dynamic importing files that end with ".oss.tsx" is not allowed.',
             fix: (fixer) => {
               const absolutePath = path.relative(
                 context.getCwd(),
-                path.resolve(path.dirname(context.getFilename()), node.arguments[0].value),
+                path.resolve(path.dirname(context.getFilename()), node.source.value),
               );
-              return fixer.replaceText(node.arguments[0], `'${absolutePath}'`);
+              return fixer.replaceText(node.source, `'${absolutePath}'`);
             },
           });
         }
