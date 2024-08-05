148a6ad6d1eedef8111e3304daef90bfac5a98d6
Nick Schrock <schrockn@dagsterlabs.com>
2024-08-05 16:04:43 -0700

    helper functino

--- examples/experimental/dagster-airlift/examples/peering-with-dbt/peering_with_dbt/dagster_defs/definitions.py
+++ examples/experimental/dagster-airlift/examples/peering-with-dbt/peering_with_dbt/dagster_defs/definitions.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-from dagster import Definitions
 from dagster._core.definitions.asset_key import AssetKey
 from dagster._core.definitions.asset_spec import AssetSpec
 from dagster_airlift import (
@@ -11,6 +10,7 @@ from dagster_airlift import (
     create_defs_from_airflow_instance,
     load_migration_state_from_yaml,
 )
+from dagster_airlift.core.def_factory import defs_from_factories
 from dagster_airlift.dbt import DbtProjectDefs
 
 from .constants import (
@@ -37,17 +37,17 @@ def dbt_project_path() -> Path:
 
 defs = create_defs_from_airflow_instance(
     airflow_instance=airflow_instance,
-    orchestrated_defs=Definitions.merge(
+    orchestrated_defs=defs_from_factories(
         PythonDefs(
             name="load_lakehouse__load_iris",
             specs=[AssetSpec(key=AssetKey.from_user_string("iris_dataset/iris_lakehouse_table"))],
             python_fn=lambda: None,
-        ).build_defs(),
+        ),
         DbtProjectDefs(
             name="dbt_dag__build_dbt_models",
             dbt_project_path=dbt_project_path(),
             group="dbt",
-        ).build_defs(),
+        ),
     ),
     migration_state_override=load_migration_state_from_yaml(
         migration_yaml_path=MIGRATION_STATE_PATH
