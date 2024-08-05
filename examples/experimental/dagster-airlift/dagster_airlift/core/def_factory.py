bea0af64934d5871cd47004f313fddc2777d53d3
Nick Schrock <schrockn@dagsterlabs.com>
2024-08-05 16:22:38 -0700

    Add defs_from_factories helper to clean up code (#23433)
    ## Summary & Motivation
    
    Add a simple helper defs_from_factories to make defs cosntruction more ergonomic
    
    ## How I Tested These Changes
    
    BK

--- examples/experimental/dagster-airlift/dagster_airlift/core/def_factory.py
+++ examples/experimental/dagster-airlift/dagster_airlift/core/def_factory.py
@@ -6,3 +6,7 @@ from dagster._core.definitions.definitions_class import Definitions
 class DefsFactory(ABC):
     @abstractmethod
     def build_defs(self) -> Definitions: ...
+
+
+def defs_from_factories(*factories: DefsFactory) -> Definitions:
+    return Definitions.merge(*[factory.build_defs() for factory in factories])
