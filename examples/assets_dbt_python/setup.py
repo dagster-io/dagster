be47b6adaf600cf3c260857b9e931fec8673d783
Maxime Armstrong <maxime@retain.ai>
2024-08-06 16:00:22 -0400

    Use DbtProject in assets_dbt_python

--- examples/assets_dbt_python/setup.py
+++ examples/assets_dbt_python/setup.py
@@ -1,14 +1,10 @@
-import glob
-
 from setuptools import find_packages, setup
 
 setup(
     name="assets_dbt_python",
     packages=find_packages(exclude=["assets_dbt_python_tests"]),
     # package data paths are relative to the package key
-    package_data={
-        "assets_dbt_python": ["../" + path for path in glob.glob("dbt_project/**", recursive=True)]
-    },
+    package_data={"assets_dbt_python": ["dbt-project/**/*"]},
     install_requires=[
         "dagster",
         "dagster-cloud",
