10b07618a9c75647637393b57b59dd9d46efb95d
colton <cmpadden@users.noreply.github.com>
2024-08-06 17:50:02 -0400

    [sling] rm experimental decorator on SlingResource (#23452)

--- python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/resources.py
+++ python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/resources.py
@@ -21,7 +21,7 @@ from dagster import (
     PermissiveConfig,
     get_dagster_logger,
 )
-from dagster._annotations import deprecated, experimental, public
+from dagster._annotations import deprecated, public
 from dagster._utils.env import environ
 from dagster._utils.warnings import deprecation_warning
 from pydantic import Field
@@ -191,7 +191,6 @@ class SlingConnectionResource(PermissiveConfig):
     )
 
 
-@experimental
 class SlingResource(ConfigurableResource):
     """Resource for interacting with the Sling package. This resource can be used to run Sling replications.
 
