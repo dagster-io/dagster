ec6b5200f8c20ea87434e722bfa9a39e11545161
bengotow <bgotow@elementl.com>
2024-08-05 22:21:09 -0500

    [ui] Allow filtering run logs for metadata entry keys and values

--- js_modules/dagster-ui/packages/ui-core/src/runs/filterLogs.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/runs/filterLogs.tsx
@@ -1,6 +1,7 @@
 import {LogFilter, LogsProviderLogs} from './LogsProvider';
 import {eventTypeToDisplayType} from './getRunFilterProviders';
 import {logNodeLevel} from './logNodeLevel';
+import {LogNode} from './types';
 
 export function filterLogs(logs: LogsProviderLogs, filter: LogFilter, filterStepKeys: string[]) {
   const filteredNodes = logs.allNodes.filter((node) => {
@@ -26,6 +27,7 @@ export function filterLogs(logs: LogsProviderLogs, filter: LogFilter, filterStep
 
   const textMatchNodes = hasTextFilter
     ? filteredNodes.filter((node) => {
+        const nodeTexts = [node.message.toLowerCase(), ...metadataEntryKeyValueStrings(node)];
         return (
           filter.logQuery.length > 0 &&
           filter.logQuery.every((f) => {
@@ -38,7 +40,7 @@ export function filterLogs(logs: LogsProviderLogs, filter: LogFilter, filterStep
             if (f.token === 'type') {
               return node.eventType && f.value === eventTypeToDisplayType(node.eventType);
             }
-            return node.message.toLowerCase().includes(f.value.toLowerCase());
+            return nodeTexts.some((text) => text.toLowerCase().includes(f.value.toLowerCase()));
           })
         );
       })
@@ -48,4 +50,24 @@ export function filterLogs(logs: LogsProviderLogs, filter: LogFilter, filterStep
     filteredNodes: hasTextFilter && filter.hideNonMatches ? textMatchNodes : filteredNodes,
     textMatchNodes,
   };
+}
+
+// Given an array of metadata entries, returns searchable text in the format:
+// [`label1:value1`, ...], where "value1" is the top-level value(s) present in the
+// metadata entry object converted to JSON. All of our metadata entry types contain
+// different top-level keys, such as "intValue", "mdStr" and "tableSchema", and
+// the searchable text is the value of these keys.
+//
+function metadataEntryKeyValueStrings(node: LogNode) {
+  if (!('metadataEntries' in node)) {
+    return [];
+  }
+  const s = node.metadataEntries.map(
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    ({__typename, label, description, ...rest}) =>
+      `${label}:${Object.values(rest)
+        .map((v) => (typeof v === 'string' ? v : JSON.stringify(v)))
+        .join(' ')}`,
+  );
+  return s;
 }
