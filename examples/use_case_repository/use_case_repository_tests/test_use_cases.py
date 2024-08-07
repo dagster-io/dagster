571ae3d246b9b7afda1183995f4779e8adff6373
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-08-06 22:04:31 -0700

    add use case test

--- examples/use_case_repository/use_case_repository_tests/test_use_cases.py
+++ examples/use_case_repository/use_case_repository_tests/test_use_cases.py
@@ -1,6 +1,14 @@
-from use_case_repository.snowflake_to_s3_embedded_elt import ingest_s3_to_snowflake, sling_resource
+from use_case_repository.guides.pipes_cli_command import cli_command_asset
+from use_case_repository.guides.snowflake_to_s3_embedded_elt import (
+    ingest_s3_to_snowflake,
+    sling_resource,
+)
 
 
 def test_snowflake_to_s3_embedded_elt():
     assert ingest_s3_to_snowflake
     assert sling_resource
+
+
+def test_pipes_cli_command():
+    assert cli_command_asset
