fdd5f734cfe6a44fa920279d062bb0d831bfe2ca
Marco Salazar <marco@elementl.com>
2024-08-08 10:13:27 -0400

    update jest and next.config.js aliases

--- js_modules/dagster-ui/packages/ui-core/jest.config.js
+++ js_modules/dagster-ui/packages/ui-core/jest.config.js
@@ -78,7 +78,7 @@ module.exports = {
     '\\.(css|less)$': 'identity-obj-proxy',
     '^worker-loader(.*)/workers/(.*)$': '<rootDir>/jest/mocks/$2',
     '^@dagster-io/ui-components$': '<rootDir>/../ui-components/src/index',
-    '^src/(.*)$': '<rootDir>/src/$1',
+    '^shared/(.*)$': '<rootDir>/src/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
