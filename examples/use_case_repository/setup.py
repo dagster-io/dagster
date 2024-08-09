bed5fdcf789970d3b2c0995f8b795e9983ceeb8f
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-08-09 10:07:12 -0700

    Add code for a use-case repository (#23460)
    ## Summary & Motivation
    
    This adds the `use_case_repository` package under examples which will be
    used by `dagster-website` to populate the the Repository on our website.
    
    <img width="1311" alt="image"
    src="https://github.com/user-attachments/assets/476a720a-1120-4935-a613-c3d6c749acf3">
    
    All `.md` and `.py` files in `use_case_repository/guides` from the
    master branch will be loaded to our website during the build process.
    
    A local server is provided in order to quickly view your markdown files
    using `make webserver`. Refer to the `README.md` for more information.
    <img width="872" alt="image"
    src="https://github.com/user-attachments/assets/9d13d2c5-9631-46b4-aecd-820cfd420b22">
    
    <img width="727" alt="image"
    src="https://github.com/user-attachments/assets/3a14ffd0-754c-4231-98b6-3ea0b0cecae3">
    
    
    ## How I Tested These Changes
    
    local testing

---
+++ examples/use_case_repository/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="use_case_repository",
+    packages=find_packages(exclude=["use_case_repository_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-embedded-elt",
+        "dagster-pipes",
+        "python-frontmatter",
+        "pymdown-extensions",
+        "markdown",
+        "flask",
+        "sling",
+    ],
+    extras_require={
+        "dev": [
+            "dagster-webserver",
+            "pytest",
+            "ruff",
+        ]
+    },
+)
