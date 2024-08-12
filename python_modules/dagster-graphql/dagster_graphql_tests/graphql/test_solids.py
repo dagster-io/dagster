dagster-io/dagster

commit 65612ed4753368982955257f045792f3f1cd98c0

Attempt to fix issue where asset nodes were getting incorrectly mapped to solids (#23446)
Summary:
This locally fixes an issue introduced in
https://github.com/dagster-io/dagster/pull/22747 that we were seeing
where two separate dbt_assets with different subsets of the same
manifest resulted in jobs without any assets attached to them. Still
need to create a test that reproduces the problem.

## Summary & Motivation

## How I Tested These Changes

python_modules/dagster-graphql/dagster_graphql/schema/solids.py
@@ -440,7 +440,7 @@ class ISolidDefinitionMixin:
             nodes = [
                 node
                 for node in ext_repo.get_external_asset_nodes()
-                if node.op_name == self.solid_def_name
+                if node.node_definition_name == self.solid_def_name
             ]
             asset_checks_loader = AssetChecksLoader(
                 context=graphene_info.context, asset_keys=[node.asset_key for node in nodes]

commit d4ed34fd3e6bfe8d5ee0d8929d0161d1aee50b15

Fix solid to asset mapping for graph-backed assets
Summary:
Issue introduced by https://github.com/dagster-io/dagster/pull/23446 - when graphs are in the mix, we actually were relying implicitly on the fact that the graph name is equal to the op name here: https://github.com/dagster-io/dagster/blob/master/python_modules/dagster/dagster/_core/remote_representation/external_data.py#L1418 (h/t Alex)

So now check both and consider it a match if either the graph name or the node definition name matches.

Test Plan: Render an asset job created from a graph-based asset, "View asset graph" link now appears as expected
Render the previous test case with two different subsets of the same dbt manifest that was incorrectly not getting returned

python_modules/dagster-graphql/dagster_graphql/schema/solids.py
@@ -440,7 +440,10 @@ class ISolidDefinitionMixin:
             nodes = [
                 node
                 for node in ext_repo.get_external_asset_nodes()
-                if node.node_definition_name == self.solid_def_name
+                if (
+                    (node.node_definition_name == self.solid_def_name)
+                    or (node.graph_name and node.graph_name == self.solid_def_name)
+                )
             ]
             asset_checks_loader = AssetChecksLoader(
                 context=graphene_info.context, asset_keys=[node.asset_key for node in nodes]

python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_solids.py
@@ -20,11 +20,18 @@ def all_solids_query():
 
 def get_solid_query_exists():
     return """
-    query SolidsQuery($repositorySelector: RepositorySelector!) {
+    query SolidsQuery($repositorySelector: RepositorySelector!, $solidName: String!) {
         repositoryOrError(repositorySelector: $repositorySelector) {
             ... on Repository {
-                usedSolid(name: "sum_op") {
-                    definition { name }
+                usedSolid(name: $solidName) {
+                    definition {
+                        name
+                        assetNodes {
+                            assetKey {
+                                path
+                            }
+                        }
+                    }
                 }
             }
             ... on PythonError {
@@ -47,9 +54,31 @@ def test_query_all_solids(graphql_context: WorkspaceRequestContext, snapshot):
 def test_query_get_solid_exists(graphql_context: WorkspaceRequestContext):
     selector = infer_repository_selector(graphql_context)
     result = execute_dagster_graphql(
-        graphql_context, get_solid_query_exists(), variables={"repositorySelector": selector}
+        graphql_context,
+        get_solid_query_exists(),
+        variables={"repositorySelector": selector, "solidName": "sum_op"},
     )
 
     assert not result.errors
     print(result.data["repositoryOrError"])  # noqa: T201
     assert result.data["repositoryOrError"]["usedSolid"]["definition"]["name"] == "sum_op"
+
+
+def test_asset_graph_node_asset_defs(graphql_context: WorkspaceRequestContext):
+    # Verify that the node for an asset graph is correctly associated with its assets
+
+    selector = infer_repository_selector(graphql_context)
+    result = execute_dagster_graphql(
+        graphql_context,
+        get_solid_query_exists(),
+        variables={"repositorySelector": selector, "solidName": "hanging_graph"},
+    )
+
+    assert not result.errors
+    print(result.data["repositoryOrError"])  # noqa: T201
+    assert result.data["repositoryOrError"]["usedSolid"]["definition"]["name"] == "hanging_graph"
+
+    nodes = result.data["repositoryOrError"]["usedSolid"]["definition"]["assetNodes"]
+
+    assert len(nodes) == 1
+    assert nodes[0]["assetKey"]["path"] == ["hanging_graph"]