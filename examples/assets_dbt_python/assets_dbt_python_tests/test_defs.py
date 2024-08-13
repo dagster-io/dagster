a931ee091eee90f80f2e597a990049f3d73407cf
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2024-08-13 10:18:20 -0400

    Update assets_dbt_python example to use DbtProject (#23454)
    ## Summary & Motivation
    
    Use `DbtProject` instead of `DbtCliResource` to generate manifest in
    `assets_dbt_python`. The `package_data` also updated in `setup.py`.
    
    ## How I Tested These Changes
    
    dagster dev
    BK with updated tests

--- examples/assets_dbt_python/assets_dbt_python_tests/test_defs.py
+++ examples/assets_dbt_python/assets_dbt_python_tests/test_defs.py
@@ -1,6 +1,9 @@
-from assets_dbt_python.definitions import defs
+from dagster._core.test_utils import environ
 
 
 def test_def_can_load():
-    assert defs.get_job_def("everything_everywhere_job")
-    assert defs.get_job_def("refresh_forecast_model_job")
+    with environ({"DAGSTER_IS_DEV_CLI": "1"}):
+        from assets_dbt_python.definitions import defs
+
+        assert defs.get_job_def("everything_everywhere_job")
+        assert defs.get_job_def("refresh_forecast_model_job")
