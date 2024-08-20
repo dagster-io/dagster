929d1c575be2db887c9678140c9cf0fab9cf5565
colton <cmpadden@users.noreply.github.com>
2024-08-20 12:55:39 -0400

    [usecase] monitor and process files landing in s3 (#23646)
    ## Summary & Motivation
    
    - Sensor to detect new files landing in S3
    - Asset to read / process file landing in S3
    
    ## How I Tested These Changes
    
    - Confirmed local execution using _dev_ account
    - Copied file to S3 bucket, after 30 second tick file was detected and
    read

--- examples/use_case_repository/setup.py
+++ examples/use_case_repository/setup.py
@@ -5,13 +5,15 @@ setup(
     packages=find_packages(exclude=["use_case_repository_tests"]),
     install_requires=[
         "dagster",
+        "dagster-aws",
         "dagster-embedded-elt",
         "dagster-pipes",
         "dagster-snowflake",
-        "python-frontmatter",
-        "pymdown-extensions",
-        "markdown",
         "flask",
+        "markdown",
+        "polars",
+        "pymdown-extensions",
+        "python-frontmatter",
         "sling",
     ],
     extras_require={
