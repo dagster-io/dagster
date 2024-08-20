2311c74a259bf9f6cf9eeeef1cd7ef594ee32e24
bengotow <bgotow@elementl.com>
2024-08-20 11:31:28 -0500

    [ui] Remove Observe Sources button + add to materialize all dropdown

--- js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetGraphExplorer.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetGraphExplorer.tsx
@@ -51,7 +51,6 @@ import {
 import {AssetLocation, useFindAssetLocation} from './useFindAssetLocation';
 import {AssetLiveDataRefreshButton} from '../asset-data/AssetLiveDataProvider';
 import {LaunchAssetExecutionButton} from '../assets/LaunchAssetExecutionButton';
-import {LaunchAssetObservationButton} from '../assets/LaunchAssetObservationButton';
 import {AssetKey} from '../assets/types';
 import {DEFAULT_MAX_ZOOM, SVGViewport} from '../graph/SVGViewport';
 import {useAssetLayout} from '../graph/asyncGraphLayout';
@@ -734,14 +733,6 @@ const AssetGraphExplorerWithData = ({
                     />
                   </GraphQueryInputFlexWrap>
                   <AssetLiveDataRefreshButton />
-                  <LaunchAssetObservationButton
-                    preferredJobName={explorerPath.pipelineName}
-                    scope={
-                      selectedDefinitions.length
-                        ? {selected: selectedDefinitions.filter((a) => a.isObservable)}
-                        : {all: allDefinitionsForMaterialize.filter((a) => a.isObservable)}
-                    }
-                  />
                   <LaunchAssetExecutionButton
                     preferredJobName={explorerPath.pipelineName}
                     scope={
