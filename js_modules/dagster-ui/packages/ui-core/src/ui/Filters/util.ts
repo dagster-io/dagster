403be732f3d50a8bf1251589ad6ff3b94ac9c51f
benpankow <ben@dagsterlabs.com>
2024-08-22 09:45:33 -0700

    fx fmt

--- js_modules/dagster-ui/packages/ui-core/src/ui/Filters/util.ts
+++ js_modules/dagster-ui/packages/ui-core/src/ui/Filters/util.ts
@@ -1,12 +1,12 @@
-import { BaseConfig as AssetGroupsFilterBaseConfig } from './useAssetGroupFilter';
-import { BaseConfig as AssetOwnerFilterBaseConfig } from './useAssetOwnerFilter';
-import { BaseConfig as AssetTagFilterBaseConfig } from './useAssetTagFilter';
-import { BaseConfig as ChangedFilterBaseConfig } from './useChangedFilter';
-import { BaseConfig as CodeLocationFilterBaseConfig } from './useCodeLocationFilter';
-import { BaseConfig as ComputeKindTagFilterBaseConfig } from './useComputeKindTagFilter';
-import { AssetGroupSelector, AssetOwner, ChangeReason, DefinitionTag } from '../../graphql/types';
-import { RepoAddress } from '../../workspace/types';
-import { StaticBaseConfig } from '../BaseFilters/useStaticSetFilter';
+import {BaseConfig as AssetGroupsFilterBaseConfig} from './useAssetGroupFilter';
+import {BaseConfig as AssetOwnerFilterBaseConfig} from './useAssetOwnerFilter';
+import {BaseConfig as AssetTagFilterBaseConfig} from './useAssetTagFilter';
+import {BaseConfig as ChangedFilterBaseConfig} from './useChangedFilter';
+import {BaseConfig as CodeLocationFilterBaseConfig} from './useCodeLocationFilter';
+import {BaseConfig as ComputeKindTagFilterBaseConfig} from './useComputeKindTagFilter';
+import {AssetGroupSelector, AssetOwner, ChangeReason, DefinitionTag} from '../../graphql/types';
+import {RepoAddress} from '../../workspace/types';
+import {StaticBaseConfig} from '../BaseFilters/useStaticSetFilter';
 
 export const STATIC_FILTER_CONFIGS: {
   groups: StaticBaseConfig<AssetGroupSelector>;
