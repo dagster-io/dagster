3aa3ff6a13b9a9f4c5abb0b9c6282ac96093fb18
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-08-22 12:48:31 -0700

    [docs-beta] Add snippets folder

---
+++ examples/docs_beta_snippets/README.md
@@ -0,0 +1,4 @@
+# Docs Beta Examples
+
+This module exists only to enable testing docs snippets/examples in CI, and should not be installed
+otherwise.
---
+++ examples/docs_beta_snippets/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="docs_beta_snippets",
+    author="Dagster Labs",
+    author_email="hello@dagsterlabs.com",
+    license="Apache-2.0",
+    url="https://github.com/dagster-io/dagster/tree/master/examples/docs_beta_snippets",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=["test"]),
+    install_requires=[
+        "dagster",
+        "dagster-webserver",
+        "dagster-embedded-elt",
+    ],
+    extras_require={
+        "full": [
+            "pytest",
+            "dagster-webserver[test]",
+        ]
+    },
+)
