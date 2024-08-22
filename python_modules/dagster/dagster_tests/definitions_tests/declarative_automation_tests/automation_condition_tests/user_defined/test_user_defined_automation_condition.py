6cf3234d292276ebf2e685f770fa83c577054642
OwenKephart <owen@elementl.com>
2024-08-22 15:18:39 -0700

    [DA][12/n][user-code] Improve log handling (#23625)
    ## Summary & Motivation
    
    Two things:
    
    1. Change it from `context.logger` to `context.log` to better align with our existing interfaces
    2. Make the log you get on each context object specific to the class that's being logged from (this is just quality of life)
    
    ## How I Tested These Changes
    
    Before this change:
    
    ```
    2024-08-14 19:05:37 -0700 - dagster.daemon.SensorDaemon - INFO - Checking for new runs for sensor: tryin_to_make_a_change
    2024-08-14 19:05:37 -0700 - dagster - INFO - __repository__ - tryin_to_make_a_change - Prefetching asset records for 1 records.
    2024-08-14 19:05:37 -0700 - dagster - INFO - __repository__ - tryin_to_make_a_change - Done prefetching asset records.
    2024-08-14 19:05:37 -0700 - dagster - ERROR - some msg
    ```
    
    After this change:
    
    ```
    2024-08-14 19:05:37 -0700 - dagster.daemon.SensorDaemon - INFO - Checking for new runs for sensor: tryin_to_make_a_change
    2024-08-14 19:05:37 -0700 - dagster - INFO - __repository__ - tryin_to_make_a_change - Prefetching asset records for 1 records.
    2024-08-14 19:05:37 -0700 - dagster - INFO - __repository__ - tryin_to_make_a_change - Done prefetching asset records.
    2024-08-14 19:05:37 -0700 - dagster.MyCustomAutomationCondition - ERROR - some msg
    ```

--- python_modules/dagster/dagster_tests/definitions_tests/declarative_automation_tests/automation_condition_tests/user_defined/test_user_defined_automation_condition.py
+++ python_modules/dagster/dagster_tests/definitions_tests/declarative_automation_tests/automation_condition_tests/user_defined/test_user_defined_automation_condition.py
@@ -1,3 +1,5 @@
+import logging
+
 from dagster import AutomationCondition, DagsterInstance, asset, evaluate_automation_conditions
 from dagster._core.definitions.declarative_automation.automation_condition import AutomationResult
 from dagster._core.definitions.declarative_automation.automation_context import AutomationContext
@@ -30,3 +32,26 @@ def test_cursoring() -> None:
             assert result.get_num_requested(my_asset.key) == 0
         else:
             assert result.get_num_requested(my_asset.key) == 1
+
+
+def test_logging(caplog) -> None:
+    class MyAutomationCondition(AutomationCondition):
+        def evaluate(self, context: AutomationContext) -> AutomationResult:
+            context.log.debug("DEBUG_THING")
+            context.log.info("INFO_THING")
+
+            return AutomationResult(context, true_slice=context.get_empty_slice())
+
+    @asset(automation_condition=MyAutomationCondition())
+    def my_asset() -> None: ...
+
+    caplog.set_level(logging.INFO)
+    evaluate_automation_conditions(defs=[my_asset], instance=DagsterInstance.ephemeral())
+
+    assert "INFO_THING" in caplog.text
+    assert "DEBUG_THING" not in caplog.text
+    assert "MyAutomationCondition" in caplog.text
+
+    caplog.set_level(logging.DEBUG)
+    evaluate_automation_conditions(defs=[my_asset], instance=DagsterInstance.ephemeral())
+    assert "DEBUG_THING" in caplog.text
