94d6dc5d030a2692cc306a6f073665c943e948ce
Christopher DeCarolis <cdguitar817@gmail.com>
2024-08-22 16:19:42 -0700

    [dagster-airlift] allow all retrieval methods of task/dag (#23812)
    Allow task/dag to be set by name, op tag, or asset tag.
    
    Simple unit tests for the util methods.

--- examples/experimental/dagster-airlift/dagster_airlift/core/utils.py
+++ examples/experimental/dagster-airlift/dagster_airlift/core/utils.py
@@ -1,6 +1,9 @@
 from typing import Optional
 
-from dagster import AssetsDefinition
+from dagster import (
+    AssetsDefinition,
+    _check as check,
+)
 
 MIGRATED_TAG = "airlift/task_migrated"
 DAG_ID_TAG = "airlift/dag_id"
@@ -8,16 +11,51 @@ TASK_ID_TAG = "airlift/task_id"
 
 
 def get_task_id_from_asset(assets_def: AssetsDefinition) -> Optional[str]:
-    if assets_def.node_def.tags and "airlift/task_id" in assets_def.node_def.tags:
-        return assets_def.node_def.tags["airlift/task_id"]
-    if len(assets_def.node_def.name.split("__")) == 2:
-        return assets_def.node_def.name.split("__")[1]
-    return None
+    return _get_prop_from_asset(assets_def, TASK_ID_TAG, 1)
 
 
 def get_dag_id_from_asset(assets_def: AssetsDefinition) -> Optional[str]:
-    if assets_def.node_def.tags and "airlift/dag_id" in assets_def.node_def.tags:
-        return assets_def.node_def.tags["airlift/dag_id"]
+    return _get_prop_from_asset(assets_def, DAG_ID_TAG, 0)
+
+
+def _get_prop_from_asset(
+    assets_def: AssetsDefinition, prop_tag: str, position: int
+) -> Optional[str]:
+    prop_from_tags = None
+    if any(prop_tag in spec.tags for spec in assets_def.specs):
+        prop = None
+        for spec in assets_def.specs:
+            if prop is None:
+                prop = spec.tags[prop_tag]
+            else:
+                if spec.tags.get(prop_tag) is None:
+                    check.failed(
+                        f"Missing {prop_tag} tag in spec {spec.key} for {assets_def.node_def.name}"
+                    )
+                check.invariant(
+                    prop == spec.tags[prop_tag],
+                    f"Task ID mismatch within same AssetsDefinition: {prop} != {spec.tags[prop_tag]}",
+                )
+        prop_from_tags = prop
+    prop_from_op_tags = None
+    if assets_def.node_def.tags and prop_tag in assets_def.node_def.tags:
+        prop_from_op_tags = assets_def.node_def.tags[prop_tag]
+    prop_from_name = None
     if len(assets_def.node_def.name.split("__")) == 2:
-        return assets_def.node_def.name.split("__")[0]
-    return None
+        prop_from_name = assets_def.node_def.name.split("__")[position]
+    if prop_from_tags and prop_from_op_tags:
+        check.invariant(
+            prop_from_tags == prop_from_op_tags,
+            f"ID mismatch between asset tags and op tags: {prop_from_tags} != {prop_from_op_tags}",
+        )
+    if prop_from_tags and prop_from_name:
+        check.invariant(
+            prop_from_tags == prop_from_name,
+            f"ID mismatch between tags and name: {prop_from_tags} != {prop_from_name}",
+        )
+    if prop_from_op_tags and prop_from_name:
+        check.invariant(
+            prop_from_op_tags == prop_from_name,
+            f"ID mismatch between op tags and name: {prop_from_op_tags} != {prop_from_name}",
+        )
+    return prop_from_tags or prop_from_op_tags or prop_from_name
