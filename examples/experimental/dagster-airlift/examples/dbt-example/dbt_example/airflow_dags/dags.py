52abfa9561eaad8fb61ba88c0c1dd2c64aa2e27c
Christopher DeCarolis <cdguitar817@gmail.com>
2024-08-23 12:04:18 -0700

    [dagster-airlift] consolidate dags (#23856)
    consolidate dags into a single file. Makes demo flow a bit easier.

--- examples/experimental/dagster-airlift/examples/dbt-example/dbt_example/airflow_dags/dags.py
+++ examples/experimental/dagster-airlift/examples/dbt-example/dbt_example/airflow_dags/dags.py
@@ -5,9 +5,17 @@ from typing import List
 
 from airflow import DAG
 from airflow.models.operator import BaseOperator
+from airflow.operators.bash import BashOperator
 from dagster_airlift.in_airflow import mark_as_dagster_migrating
 from dagster_airlift.migration_state import load_migration_state_from_yaml
-from dbt_example.shared.load_iris import load_csv_to_duckdb
+from dbt_example.shared.load_iris import iris_path, load_csv_to_duckdb
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 7, 18),
+    "retries": 0,
+}
 
 
 class LoadToLakehouseOperator(BaseOperator):
@@ -25,18 +33,19 @@ class LoadToLakehouseOperator(BaseOperator):
         )
 
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2023, 1, 1),
-    "retries": 0,
-}
+DBT_DIR = os.getenv("DBT_PROJECT_DIR")
+# Create the DAG with the specified schedule interval
+dbt_dag = DAG("dbt_dag", default_args=default_args, schedule_interval=None)
+args = f"--project-dir {DBT_DIR} --profiles-dir {DBT_DIR}"
+run_dbt_model = BashOperator(
+    task_id="build_dbt_models", bash_command=f"dbt build {args}", dag=dbt_dag
+)
 
 dag = DAG("load_lakehouse", default_args=default_args, schedule_interval=None)
 load_iris = LoadToLakehouseOperator(
     task_id="load_iris",
     dag=dag,
-    csv_path=Path(__file__).parent / "iris.csv",
+    csv_path=iris_path(),
     db_path=Path(os.environ["AIRFLOW_HOME"]) / "jaffle_shop.duckdb",
     columns=[
         "sepal_length_cm",
@@ -46,6 +55,7 @@ load_iris = LoadToLakehouseOperator(
         "species",
     ],
 )
+
 mark_as_dagster_migrating(
     global_vars=globals(),
     migration_state=load_migration_state_from_yaml(Path(__file__).parent / "migration_state"),
