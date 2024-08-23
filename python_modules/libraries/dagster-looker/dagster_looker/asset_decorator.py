eff7254f67cb22c9d14aab4d17ccd666f03a75ba
Maxime Armstrong <maxime@retain.ai>
2024-08-23 12:25:08 -0400

    Implement looker_assets decorator

---
+++ python_modules/libraries/dagster-looker/dagster_looker/asset_decorator.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import Any, Callable, Optional
+
+from dagster import (
+    AssetsDefinition,
+    PartitionsDefinition,
+    _check as check,
+    multi_asset,
+)
+
+from .asset_specs import build_looker_asset_specs
+from .dagster_looker_translator import DagsterLookerTranslator
+
+
+def looker_assets(
+    *,
+    project_dir: Path,
+    name: Optional[str] = None,
+    group_name: Optional[str] = None,
+    dagster_looker_translator: Optional[DagsterLookerTranslator] = None,
+    partitions_def: Optional[PartitionsDefinition] = None,
+) -> Callable[[Callable[..., Any]], AssetsDefinition]:
+    dagster_looker_translator = check.inst_param(
+        dagster_looker_translator or DagsterLookerTranslator(),
+        "dagster_looker_translator",
+        DagsterLookerTranslator,
+        )
+    return multi_asset(
+        name=name,
+        group_name=group_name,
+        compute_kind="looker",
+        can_subset=True,
+        partitions_def=partitions_def,
+        specs=build_looker_asset_specs(
+            project_dir=project_dir,
+            dagster_looker_translator=dagster_looker_translator,
+        ),
+    )
