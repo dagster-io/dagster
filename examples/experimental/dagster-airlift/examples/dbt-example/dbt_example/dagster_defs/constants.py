8fa1b6ec9b9b30260c7a48d23145327070700db5
Christopher DeCarolis <cdguitar817@gmail.com>
2024-08-23 17:26:49 -0700

    [dagster-airlift] add freshness check (#23875)
    ## Summary & Motivation
    
    ## How I Tested These Changes
    
    ## Changelog [New | Bug | Docs]
    
    NOCHANGELOG

--- examples/experimental/dagster-airlift/examples/dbt-example/dbt_example/dagster_defs/constants.py
+++ examples/experimental/dagster-airlift/examples/dbt-example/dbt_example/dagster_defs/constants.py
@@ -2,6 +2,8 @@
 import os
 from pathlib import Path
 
+from dagster import AssetKey
+
 AIRFLOW_BASE_URL = "http://localhost:8080"
 AIRFLOW_INSTANCE_NAME = "my_airflow_instance"
 
@@ -11,6 +13,7 @@ PASSWORD = "admin"
 
 ASSETS_PATH = Path(__file__).parent / "defs"
 MIGRATION_STATE_PATH = Path(__file__).parent / "migration"
+DBT_DAG_ASSET_KEY = AssetKey(["airflow_instance", "dag", "dbt_dag"])
 
 
 def dbt_project_path() -> Path:
