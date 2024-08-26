c7453ce53a30183015529b98a137e64543b3926e
Nick Schrock <schrockn@dagsterlabs.com>
2024-08-26 11:39:37 -0700

    Change tutorial to use dag_defs (#23883)
    ## Summary & Motivation
    
    Use `dbt_defs` and `task_defs` in tutorial
    
    ## How I Tested These Changes
    
    BK
    
    ## Changelog [New | Bug | Docs]
    
    NOCHANGELOG

--- examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example/dagster_defs/csv_to_duckdb_defs.py
+++ examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example/dagster_defs/csv_to_duckdb_defs.py
@@ -1,37 +1,28 @@
-from dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
 from dagster import AssetKey, AssetSpec, Definitions, multi_asset
-from dagster_airlift.core import DefsFactory
 
 from tutorial_example.shared.load_csv_to_duckdb import load_csv_to_duckdb
 
 
-@dataclass
-class CSVToDuckdbDefs(DefsFactory):
-    name: str
-    table_name: str
-    duckdb_schema: str
-    csv_path: Path
-    duckdb_path: Path
-    column_names: List[str]
-    duckdb_database_name: str
-
-    def build_defs(self) -> Definitions:
-        ...
-
-        @multi_asset(
-            specs=[AssetSpec(key=AssetKey([self.duckdb_schema, self.table_name]))], name=self.name
+def load_csv_to_duckdb_defs(
+    table_name: str,
+    csv_path: Path,
+    duckdb_path: Path,
+    column_names: List[str],
+    duckdb_schema: str,
+    duckdb_database_name: str,
+) -> Definitions:
+    @multi_asset(specs=[AssetSpec(key=AssetKey([duckdb_schema, table_name]))])
+    def _multi_asset() -> None:
+        load_csv_to_duckdb(
+            table_name=table_name,
+            csv_path=csv_path,
+            duckdb_path=duckdb_path,
+            names=column_names,
+            duckdb_schema=duckdb_schema,
+            duckdb_database_name=duckdb_database_name,
         )
-        def _multi_asset():
-            load_csv_to_duckdb(
-                table_name=self.table_name,
-                csv_path=self.csv_path,
-                duckdb_path=self.duckdb_path,
-                names=self.column_names,
-                duckdb_schema=self.duckdb_schema,
-                duckdb_database_name=self.duckdb_database_name,
-            )
 
-        return Definitions(assets=[_multi_asset])
+    return Definitions(assets=[_multi_asset])
