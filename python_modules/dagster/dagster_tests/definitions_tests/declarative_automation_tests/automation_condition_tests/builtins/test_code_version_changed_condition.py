624689804b71f4a5415bdf80c6c2b9b48041d3fc
Owen Kephart <owen@elementl.com>
2024-08-26 08:14:53 -0400

    Reduce AssetSubset usage

--- python_modules/dagster/dagster_tests/definitions_tests/declarative_automation_tests/automation_condition_tests/builtins/test_code_version_changed_condition.py
+++ python_modules/dagster/dagster_tests/definitions_tests/declarative_automation_tests/automation_condition_tests/builtins/test_code_version_changed_condition.py
@@ -15,31 +15,31 @@ def test_code_version_changed_condition() -> None:
 
     # not changed
     state, result = state.evaluate("A")
-    assert result.true_subset.size == 0
+    assert result.true_slice.size == 0
 
     # still not changed
     state, result = state.evaluate("A")
-    assert result.true_subset.size == 0
+    assert result.true_slice.size == 0
 
     # newly changed
     state = state.with_asset_properties(code_version="2")
     state, result = state.evaluate("A")
-    assert result.true_subset.size == 1
+    assert result.true_slice.size == 1
 
     # not newly changed
     state, result = state.evaluate("A")
-    assert result.true_subset.size == 0
+    assert result.true_slice.size == 0
 
     # newly changed
     state = state.with_asset_properties(code_version="3")
     state, result = state.evaluate("A")
-    assert result.true_subset.size == 1
+    assert result.true_slice.size == 1
 
     # newly changed
     state = state.with_asset_properties(code_version="2")
     state, result = state.evaluate("A")
-    assert result.true_subset.size == 1
+    assert result.true_slice.size == 1
 
     # not newly changed
     state, result = state.evaluate("A")
-    assert result.true_subset.size == 0
+    assert result.true_slice.size == 0
