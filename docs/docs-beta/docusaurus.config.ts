c93fe80686ad302fbebfbae7357fca69899ac1a4
Josh Braun <josh.braun.4@gmail.com>
2024-08-27 18:49:06 -0400

    more style updates

--- docs/docs-beta/docusaurus.config.ts
+++ docs/docs-beta/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -49,6 +49,7 @@ const config: Config = {
       maxHeadingLevel: 4,
     },
     navbar: {
+      hideOnScroll: true,
       logo: {
         alt: 'Dagster Logo',
         src: 'img/dagster-docs-logo.svg',
