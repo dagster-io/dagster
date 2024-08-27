1dd03f23d84c7ea9b9173cceaa1755a6d8cfe303
Nick Schrock <schrockn@dagsterlabs.com>
2024-08-27 13:23:29 -0700

    cp

--- examples/experimental/dagster-airlift/dagster_airlift/core/defs_from_airflow.py
+++ examples/experimental/dagster-airlift/dagster_airlift/core/defs_from_airflow.py
@@ -5,7 +5,6 @@ from dagster import Definitions
 from dagster_airlift.core.sensor import build_airflow_polling_sensor
 from dagster_airlift.migration_state import AirflowMigrationState
 
-from ..migration_state import AirflowMigrationState
 from .airflow_cacheable_assets_def import DEFAULT_POLL_INTERVAL, AirflowCacheableAssetsDefinition
 from .airflow_instance import AirflowInstance
 
@@ -46,9 +45,7 @@ def build_defs_from_airflow_instance(
         migration_state_override=migration_state_override,
     )
     # Now, we construct the sensor that will poll airflow for dag runs.
-    airflow_sensor = build_airflow_polling_sensor(
-        airflow_instance=airflow_instance,
-    )
+    airflow_sensor = build_airflow_polling_sensor(airflow_instance=airflow_instance)
     return Definitions(
         assets=[assets_defs],
         asset_checks=defs.asset_checks if defs else None,
