e940f5e85e6e83677d82737903b2a289cf615b52
Nick Schrock <schrockn@dagsterlabs.com>
2024-08-27 08:17:48 -0700

    Extract dags_def construction into separate file, write tests, and fix bugs

--- examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example_tests/unit_tests/test_duckdb_to_csv_defs.py
+++ examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example_tests/unit_tests/test_duckdb_to_csv_defs.py
@@ -1,5 +1,31 @@
-from tutorial_example.dagster_defs.duckdb_to_csv_defs import export_duckdb_to_csv_defs
+from pathlib import Path
+
+from dagster import Definitions
+from tutorial_example.dagster_defs.build_dag_defs import build_dag_defs
 
 
-def test_inclusion() -> None:
-    assert export_duckdb_to_csv_defs
+def example_root() -> Path:
+    return Path(__file__).parent.parent.parent
+
+
+def dummy_duck_db_path() -> Path:
+    return Path(__file__).parent / "does_not_exist.duckdb"
+
+
+def test_dag_defs_construction_and_node_defs() -> None:
+    defs = build_dag_defs(
+        dbt_project_path=example_root() / "tutorial_example" / "shared" / "dbt",
+        # not invoked during
+        duckdb_path=dummy_duck_db_path(),
+    )
+    assert isinstance(defs, Definitions)
+    Definitions.validate_loadable(defs)
+
+    load_raw_customers = defs.get_assets_def(["raw_data", "raw_customers"])
+    assert load_raw_customers.node_def.name == "load_csv_to_duckdb_raw_customers"
+
+    customers_csv = defs.get_assets_def(["customers_csv"])
+    assert customers_csv.node_def.name == "export_duckdb_to_csv_customers"
+
+    customers = defs.get_assets_def(["customers"])
+    assert customers.node_def.name == "build_jaffle_shop"
