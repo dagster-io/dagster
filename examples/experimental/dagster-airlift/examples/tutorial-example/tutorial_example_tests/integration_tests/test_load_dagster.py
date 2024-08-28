09043e9126cf1b9b67f0f2961b0eb4ac6528efaf
Ben Pankow <ben@elementl.com>
2024-08-28 13:47:46 -0700

    [airlift] get stages under test (#24024)
    ## Summary
    
    Updates airlift tutorial to test each stage file. Moves stages into a
    subfolder, and deletes the old un-staged tutorial code.
    
    ## Changelog [Docs]
    
    `NOCHANGELOG`

--- examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example_tests/integration_tests/test_load_dagster.py
+++ examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example_tests/integration_tests/test_load_dagster.py
@@ -1,9 +1,45 @@
-from dagster._core.definitions.definitions_class import Definitions
+from dagster import Definitions
 
 
-def test_defs_loads(airflow_instance) -> None:
-    from tutorial_example.dagster_defs.definitions import defs
+def test_peer_loads(airflow_instance) -> None:
+    from tutorial_example.dagster_defs.stages.peer import defs
 
     assert defs
 
     Definitions.validate_loadable(defs)
+
+    # representation of dag
+    assert len(defs.get_repository_def().assets_defs_by_key) == 1
+
+
+def test_observe_loads(airflow_instance) -> None:
+    from tutorial_example.dagster_defs.stages.observe import defs
+
+    assert defs
+
+    Definitions.validate_loadable(defs)
+
+    # representation of dag + 9 assets
+    assert len(defs.get_repository_def().assets_defs_by_key) == 10
+
+
+def test_migrate_loads(airflow_instance) -> None:
+    from tutorial_example.dagster_defs.stages.migrate import defs
+
+    assert defs
+
+    Definitions.validate_loadable(defs)
+
+    # representation of dag + 9 assets
+    assert len(defs.get_repository_def().assets_defs_by_key) == 10
+
+
+def test_standalone_loads(airflow_instance) -> None:
+    from tutorial_example.dagster_defs.stages.standalone import defs
+
+    assert defs
+
+    Definitions.validate_loadable(defs)
+
+    # 1 fewer, since no representation of the overall DAG
+    assert len(defs.get_repository_def().assets_defs_by_key) == 9
