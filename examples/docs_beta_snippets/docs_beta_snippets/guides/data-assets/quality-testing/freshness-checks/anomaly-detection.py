535f355d43c42f29c80088d5c7ff7031b92ae28d
Claire Lin <claire@dagsterlabs.com>
2024-08-28 10:28:48 -0700

    add materializable to vale keywords

--- examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/quality-testing/freshness-checks/anomaly-detection.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/quality-testing/freshness-checks/anomaly-detection.py
@@ -1,3 +1,12 @@
 from dagster_cloud.anomaly_detection import build_anomaly_detection_freshness_checks
 
-freshness_checks = build_anomaly_detection_freshness_checks(assets=..., params=None)
+import dagster as dg
+
+
+@dg.observable_source_asset
+def hourly_sales(): ...
+
+
+freshness_checks = build_anomaly_detection_freshness_checks(
+    assets=[hourly_sales], params=None
+)
