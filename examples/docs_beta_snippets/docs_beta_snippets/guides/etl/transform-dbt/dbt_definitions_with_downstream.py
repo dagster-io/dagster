c2e29eb977d064e90e38ce749e8c7f445a0c4bf2
Nick Roach <nickroach@dagsterlabs.com>
2024-08-28 15:42:55 -0400

    remove dbt compile

--- examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/dbt_definitions_with_downstream.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/dbt_definitions_with_downstream.py
@@ -25,7 +25,6 @@ def raw_customers(context: dg.AssetExecutionContext) -> None:
 dbt_project_directory = Path(__file__).absolute().parent / "jaffle_shop"
 dbt_project = DbtProject(project_dir=dbt_project_directory)
 dbt_resource = DbtCliResource(project_dir=dbt_project)
-dbt_project.prepare_if_dev()
 
 @dbt_assets(manifest=dbt_project.manifest_path)
 def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
