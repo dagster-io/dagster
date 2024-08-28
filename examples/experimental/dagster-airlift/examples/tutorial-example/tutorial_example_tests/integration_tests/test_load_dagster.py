d87b4311fdb9ad71cfdba76ef16ac28d7d470091
benpankow <ben@dagsterlabs.com>
2024-08-28 12:46:15 -0700

    asdf

--- examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example_tests/integration_tests/test_load_dagster.py
+++ examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example_tests/integration_tests/test_load_dagster.py
@@ -1,8 +1,40 @@
-from dagster._core.definitions.definitions_class import Definitions
+from dagster import AssetKey, Definitions
 
 
-def test_defs_loads(airflow_instance) -> None:
-    from tutorial_example.dagster_defs.definitions import defs
+def test_migrate_loads(airflow_instance) -> None:
+    from tutorial_example.dagster_defs.stages.migrate import defs
+
+    assert defs
+
+    Definitions.validate_loadable(defs)
+
+
+def test_peer_loads(airflow_instance) -> None:
+    from tutorial_example.dagster_defs.stages.peer import defs
+
+    assert defs
+
+    Definitions.validate_loadable(defs)
+
+    assert len(defs.assets) == 1
+    dag_asset = defs.assets[0]
+    assert dag_asset.key == AssetKey(["airflow_instance", "dag", "rebuild_customers_list"])
+
+
+def test_observe_loads(airflow_instance) -> None:
+    from tutorial_example.dagster_defs.stages.observe import defs
+
+    assert defs
+
+    Definitions.validate_loadable(defs)
+
+    assert len(defs.assets) == 4
+    dag_asset = defs.assets[0]
+    assert dag_asset.key == AssetKey(["airflow_instance", "dag", "rebuild_customers_list"])
+
+
+def test_standalone_loads(airflow_instance) -> None:
+    from tutorial_example.dagster_defs.stages.standalone import defs
 
     assert defs
 
