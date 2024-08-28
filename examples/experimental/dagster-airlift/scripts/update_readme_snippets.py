f9bda092141f55856c9a46a9231a48901700809c
benpankow <ben@dagsterlabs.com>
2024-08-28 14:51:09 -0700

    doc

--- examples/experimental/dagster-airlift/scripts/update_readme_snippets.py
+++ examples/experimental/dagster-airlift/scripts/update_readme_snippets.py
@@ -10,6 +10,20 @@ def _get_regex_match_snippet(file_name: str) -> re.Pattern[str]:
 
 
 def update_readme_snippets(readme_filepath_raw: str, *snippet_files_raw: str):
+    """Given a path to a README file and a list of paths to snippet files,
+    update any python code blocks in the README file that match the snippet file names
+    with the contents of the snippet files.
+
+    e.g.
+
+    ```python
+    # my_snippet.py
+
+    ...
+    ```
+
+    Will be replaced with the contents of `my_snippet.py`, if a file with that name is passed.
+    """
     readme_file = Path(readme_filepath_raw)
     snippet_files = [Path(snippet_file_raw) for snippet_file_raw in snippet_files_raw]
 
