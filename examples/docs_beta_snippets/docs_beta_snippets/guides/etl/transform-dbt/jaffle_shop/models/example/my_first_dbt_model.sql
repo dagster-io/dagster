1d3a2b13a4d71734b70eb1b6c3059f9bb45b66bf
Nick Roach <nickroach@dagsterlabs.com>
2024-08-29 14:46:50 -0400

    remove top comments from model

--- examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/jaffle_shop/models/example/my_first_dbt_model.sql
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/jaffle_shop/models/example/my_first_dbt_model.sql
@@ -1,12 +1,3 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
 with source_data as (
