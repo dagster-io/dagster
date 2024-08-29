73c64ba6cad6267269c40ba594a19c1b7e20319e
Nick Roach <nickroach@dagsterlabs.com>
2024-08-29 16:50:52 -0400

    swap out dbt project

--- examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/basic-dbt-project/models/example/customers.sql
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/basic-dbt-project/models/example/customers.sql
@@ -2,5 +2,4 @@ select
     id as customer_id,
     first_name,
     last_name
-
 from {{ source('raw', 'raw_customers') }}
--- examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/dbt_definitions_with_upstream.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/dbt_definitions_with_upstream.py
@@ -6,7 +6,7 @@ import pandas as pd
 import duckdb
 import os
 
-duckdb_database_path = "jaffle_shop/dev.duckdb"
+duckdb_database_path = "basic-dbt-project/dev.duckdb"
 
 @dg.asset(compute_kind="python")
 def raw_customers(context: dg.AssetExecutionContext) -> None:
@@ -21,9 +21,10 @@ def raw_customers(context: dg.AssetExecutionContext) -> None:
     context.add_output_metadata({"num_rows": data.shape[0]})
 # highlight-end
 
-dbt_project_directory = Path(__file__).absolute().parent / "jaffle_shop"
+dbt_project_directory = Path(__file__).absolute().parent / "basic-dbt-project"
 dbt_project = DbtProject(project_dir=dbt_project_directory)
 dbt_resource = DbtCliResource(project_dir=dbt_project)
+dbt_project.prepare_if_dev()
 
 @dbt_assets(manifest=dbt_project.manifest_path)
 def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
@@ -35,3 +36,6 @@ defs = dg.Definitions(
     # highlight-end
     resources={"dbt": dbt_resource}
 )
+
+if __name__ == '__main__':
+    dg.materialize(assets=[raw_customers, dbt_models], resources={"dbt": dbt_resource})
