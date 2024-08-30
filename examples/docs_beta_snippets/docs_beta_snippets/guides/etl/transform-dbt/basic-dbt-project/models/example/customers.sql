251b6f74480f586f2a3e5c4f5937116861d70c6b
Erin Cochran <erin.k.cochran@gmail.com>
2024-08-30 14:18:19 -0400

    Add highlights and comments to examples

--- examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/basic-dbt-project/models/example/customers.sql
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/basic-dbt-project/models/example/customers.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     first_name,
     last_name
-from {{ source('raw', 'raw_customers') }}
+from {{ source('raw', 'raw_customers') }}     # Define the raw_customers asset as a source
--- examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/dbt_definitions_with_upstream.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/etl/transform-dbt/dbt_definitions_with_upstream.py
@@ -1,10 +1,7 @@
 import dagster as dg
-from dagster_dbt import (
-    dbt_assets,
-    DbtCliResource,
-    DbtProject
-)
+from dagster_dbt import dbt_assets, DbtCliResource, DbtProject
 from pathlib import Path
+
 # highlight-start
 import pandas as pd
 import duckdb
@@ -12,16 +9,23 @@ import os
 
 duckdb_database_path = "basic-dbt-project/dev.duckdb"
 
+
 @dg.asset(compute_kind="python")
 def raw_customers(context: dg.AssetExecutionContext) -> None:
+
+    # Pull customer data from a CSV
     data = pd.read_csv("https://docs.dagster.io/assets/customers.csv")
     connection = duckdb.connect(os.fspath(duckdb_database_path))
+
+    # Create a schema named raw
     connection.execute("create schema if not exists raw")
+
+    # Create/replace table named raw_customers
     connection.execute(
         "create or replace table raw.raw_customers as select * from data"
     )
 
-    # Log some metadata about the table we just wrote. It will show up in the UI.
+    # Log some metadata about the new table. It will show up in the UI.
     context.add_output_metadata({"num_rows": data.shape[0]})
 # highlight-end
 
@@ -30,16 +34,19 @@ dbt_project = DbtProject(project_dir=dbt_project_directory)
 dbt_resource = DbtCliResource(project_dir=dbt_project)
 dbt_project.prepare_if_dev()
 
+
 @dbt_assets(manifest=dbt_project.manifest_path)
 def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
+
+# Add Dagster definitions to Definitions object
 defs = dg.Definitions(
     # highlight-start
-    assets=[raw_customers, dbt_models], 
+    assets=[raw_customers, dbt_models],
     # highlight-end
-    resources={"dbt": dbt_resource}
+    resources={"dbt": dbt_resource},
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dg.materialize(assets=[raw_customers, dbt_models], resources={"dbt": dbt_resource})
