32254508f56577d4b133ea38be8dea347ca78226
Nick Schrock <schrockn@dagsterlabs.com>
2024-08-30 09:48:57 -0700

    rename test

--- examples/experimental/dagster-airlift/dagster_airlift_tests/unit_tests/test_mwaa_auth.py
+++ examples/experimental/dagster-airlift/dagster_airlift_tests/unit_tests/test_mwaa_auth.py
@@ -3,7 +3,7 @@ import mock
 from dagster_airlift.mwaa import MwaaSessionAuthBackend
 
 
-def test_mwaa_session_auth() -> None:
+def test_mwaa_session_auth_from_profile() -> None:
     """Test output format of mwaa auth backend. Does not actually make any live requests to boto or exercise usage of aws apis in any way."""
     with mock.patch("dagster_airlift.mwaa.auth.get_session_info") as mock_get_session_info:
         mock_get_session_info.return_value = ("my-webserver-hostname", "my-session-cookie")
