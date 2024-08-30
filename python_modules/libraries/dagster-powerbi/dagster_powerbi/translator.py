dedac5a0044ab312add80f89bd03f9f8af592c6d
Ben Pankow <ben@elementl.com>
2024-08-30 10:57:04 -0700

    [2/n][dagster-powerbi] scaffold DagsterPowerBITranslator class (#23245)
    ## Summary
    
    Builds out a very barebones translator class to build specs for PowerBI
    assets.
    
    The structure of this translator is slightly different than our existing
    ones, curious for feedback. Instead of implementing many functions to
    define each property of an asset, it has a single function that returns
    an asset spec (which calls out to other functions for each object type).
    
    The user also supplies a translator class rather than a translator
    instance. The ontology in this model is:
    - A translator class defines the logic for how to translate API data
    into asset specs.
    - A user can subclass a translator class to override or customize this
    logic.
    - An instance of a translator class represents a translation "session,"
    provided with any global context information (`PowerBIWorkspaceData`, in
    this case).
    - An invocation of `get_asset_spec` represents translating an individual
    object, and is provided with local context (the object we're
    translating, `PowerBIContentData`, in this case).
    
    One hesitation is that `Type[DagsterPowerBITranslator]` is an awkward
    and potentially confusing type annotation vs passing an instance.
    
    For PowerBI (and I'd imagine many integrations), we need global context
    to resolve local translation because each local object has relationships
    with the global context - in this case we need to figure out the asset
    key of an upstream asset from its internal PowerBI ID.
    
    ## Test Plan
    
    n/a, subsequent PRs will test impl - this PR is mostly around settling
    on the translator API

---
+++ python_modules/libraries/dagster-powerbi/dagster_powerbi/translator.py
@@ -0,0 +1,76 @@
+from enum import Enum
+from typing import Any, Dict
+
+from dagster import _check as check
+from dagster._core.definitions.asset_key import AssetKey
+from dagster._core.definitions.asset_spec import AssetSpec
+from dagster._record import record
+
+
+class PowerBIContentType(Enum):
+    """Enum representing each object in PowerBI's ontology, generically referred to as "content" by the API."""
+
+    DASHBOARD = "dashboard"
+    REPORT = "report"
+    SEMANTIC_MODEL = "semantic_model"
+    DATA_SOURCE = "data_source"
+
+
+@record
+class PowerBIContentData:
+    """A record representing a piece of content in PowerBI.
+    Includes the content's type and data as returned from the API.
+    """
+
+    content_type: PowerBIContentType
+    properties: Dict[str, Any]
+
+
+@record
+class PowerBIWorkspaceData:
+    """A record representing all content in a PowerBI workspace.
+
+    Provided as context for the translator so that it can resolve dependencies between content.
+    """
+
+    dashboards_by_id: Dict[str, PowerBIContentData]
+    reports_by_id: Dict[str, PowerBIContentData]
+    semantic_models_by_id: Dict[str, PowerBIContentData]
+    data_sources_by_id: Dict[str, PowerBIContentData]
+
+
+class DagsterPowerBITranslator:
+    """Translator class which converts raw response data from the PowerBI API into AssetSpecs.
+    Subclass this class to implement custom logic for each type of PowerBI content.
+    """
+
+    def __init__(self, context: PowerBIWorkspaceData):
+        self._context = context
+
+    @property
+    def workspace_data(self) -> PowerBIWorkspaceData:
+        return self._context
+
+    def get_asset_spec(self, data: PowerBIContentData) -> AssetSpec:
+        if data.content_type == PowerBIContentType.DASHBOARD:
+            return self.get_dashboard_spec(data)
+        elif data.content_type == PowerBIContentType.REPORT:
+            return self.get_report_spec(data)
+        elif data.content_type == PowerBIContentType.SEMANTIC_MODEL:
+            return self.get_semantic_model_spec(data)
+        elif data.content_type == PowerBIContentType.DATA_SOURCE:
+            return self.get_data_source_spec(data)
+        else:
+            check.assert_never(data.content_type)
+
+    def get_dashboard_asset_key(self, data: PowerBIContentData) -> AssetKey: ...
+    def get_dashboard_spec(self, data: PowerBIContentData) -> AssetSpec: ...
+
+    def get_report_asset_key(self, data: PowerBIContentData) -> AssetKey: ...
+    def get_report_spec(self, data: PowerBIContentData) -> AssetSpec: ...
+
+    def get_semantic_model_asset_key(self, data: PowerBIContentData) -> AssetKey: ...
+    def get_semantic_model_spec(self, data: PowerBIContentData) -> AssetSpec: ...
+
+    def get_data_source_asset_key(self, data: PowerBIContentData) -> AssetKey: ...
+    def get_data_source_spec(self, data: PowerBIContentData) -> AssetSpec: ...
