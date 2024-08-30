76dceb46faa8174afa9e577047769623c26ac516
Erin Cochran <erin.k.cochran@gmail.com>
2024-08-30 17:03:29 -0400

    Update graph example

--- examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/data-assets/graph_asset_decorator.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/data-assets/graph_asset_decorator.py
@@ -1,19 +1,19 @@
 import dagster as dg
 
 
+@dg.op
+def step_one():
+    ...
+
+
+@dg.op
+def step_two():
+    ...
+
+
 @dg.graph_asset
 def complex_asset():
-    @dg.op
-    def step_one():
-        ...
-
-    @dg.op
-    def step_two():
-        ...
-
-    step_one()
-    step_two()
-    return
+    return step_two(step_one())
 
 
 defs = dg.Definitions(assets=[complex_asset])
