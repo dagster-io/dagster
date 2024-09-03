dafcee605315107793eca7c704f2a4f7c80dbdea
Colton Padden <colton@dagsterlabs.com>
2024-09-03 10:40:39 -0400

    fix(ruff) replace unused methods with context.log

--- examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/data-assets/asset_decorator.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/data-assets/asset_decorator.py
@@ -4,20 +4,18 @@ import dagster as dg
 
 
 @dg.asset
-def sugary_cereals() -> None:
-    ... 
+def sugary_cereals() -> None: ...
 
 
 @dg.asset(deps=[sugary_cereals], group_name="sales")
-def shopping_list() -> None:
-    ...
-    
+def shopping_list() -> None: ...
+
 
 @dg.asset(
     owners=["bighead@hooli.com", "team:roof", "team:corpdev"],
 )
-def my_dataset():
-    store_data(transform_data(load_data))
+def my_dataset(context: dg.AssetExecutionContext):
+    context.log.info("Loading data for my_dataset")
 
 
 defs = dg.Definitions(assets=[my_dataset])
