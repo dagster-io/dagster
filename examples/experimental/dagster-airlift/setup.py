d5b981a62d550d885808ccef93a6f1731c383677
Chris DeCarolis <cdguitar817@gmail.com>
2024-09-03 14:58:53 -0700

    [dagster-airlift]

--- examples/experimental/dagster-airlift/setup.py
+++ examples/experimental/dagster-airlift/setup.py
@@ -13,7 +13,7 @@ airflow_dep_list = [
 
 setup(
     name="dagster-airlift",
-    version="0.0.16",
+    version="0.0.19",
     author="Dagster Labs",
     author_email="hello@dagsterlabs.com",
     license="Apache-2.0",
