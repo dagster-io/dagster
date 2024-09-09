6fbf9d62444119fd28816d6fc10f2463817ce837
briantu <brianptu2@gmail.com>
2024-09-09 18:25:29 -0400

    cli-materialize-range-of-partitions-2

--- python_modules/dagster/dagster/_cli/asset.py
+++ python_modules/dagster/dagster/_cli/asset.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Mapping
 
 import click
@@ -5,21 +6,37 @@ import click
 import dagster._check as check
 from dagster._cli.utils import get_instance_for_cli, get_possibly_temporary_instance_for_cli
 from dagster._cli.workspace.cli_target import (
+    get_code_location_from_workspace,
+    get_external_job_from_external_repo,
+    get_external_repository_from_code_location,
     get_repository_python_origin_from_kwargs,
+    get_workspace_from_kwargs,
     python_origin_target_argument,
 )
 from dagster._core.definitions.asset_selection import AssetSelection
-from dagster._core.definitions.backfill_policy import BackfillPolicyType
+from dagster._core.definitions.backfill_policy import BackfillPolicyType, resolve_backfill_policy
 from dagster._core.definitions.events import AssetKey
-from dagster._core.errors import DagsterInvalidSubsetError, DagsterUnknownPartitionError
+from dagster._core.definitions.partition_key_range import PartitionKeyRange
+from dagster._core.errors import (
+    DagsterBackfillFailedError,
+    DagsterInvalidSubsetError,
+    DagsterUnknownPartitionError,
+)
 from dagster._core.execution.api import execute_job
+from dagster._core.execution.backfill import BulkActionStatus, PartitionBackfill
+from dagster._core.execution.job_backfill import create_backfill_run
 from dagster._core.instance import DagsterInstance
 from dagster._core.origin import JobPythonOrigin
+from dagster._core.remote_representation.external_data import ExternalPartitionSetExecutionParamData
+from dagster._core.remote_representation.handle import RepositoryHandle
 from dagster._core.storage.tags import (
     ASSET_PARTITION_RANGE_END_TAG,
     ASSET_PARTITION_RANGE_START_TAG,
 )
 from dagster._core.telemetry import telemetry_wrapper
+from dagster._core.utils import make_new_backfill_id
+from dagster._time import get_current_timestamp
+from dagster._utils.error import serializable_error_info_from_exc_info
 from dagster._utils.hosted_user_process import recon_job_from_origin, recon_repository_from_origin
 from dagster._utils.interrupts import capture_interrupts
 
@@ -100,15 +117,20 @@ def execute_materialize_command(instance: DagsterInstance, kwargs: Mapping[str, 
                 f" partition key `{partition}` or have no PartitionsDefinition."
             )
     elif partition_range_start:
-        for asset_key in asset_keys:
-            backfill_policy = implicit_job_def.asset_layer.get(asset_key).backfill_policy
-            if (
-                backfill_policy is not None
-                and backfill_policy.policy_type != BackfillPolicyType.SINGLE_RUN
-            ):
-                check.failed(
-                    "Provided partition range, but not all assets have a single-run backfill policy."
-                )
+        backfill_policy_type = resolve_backfill_policy(
+            [implicit_job_def.asset_layer.get(next(iter(asset_keys))).backfill_policy]
+        ).policy_type
+
+        if any(
+            resolve_backfill_policy(
+                [implicit_job_def.asset_layer.get(asset_key).backfill_policy]
+            ).policy_type
+            != backfill_policy_type
+            for asset_key in asset_keys
+        ):
+            check.failed(
+                f"Provided partition range, but not all assets have a {backfill_policy_type} backfill policy."
+            )
         try:
             implicit_job_def.validate_partition_key(
                 partition_range_start,
@@ -125,10 +147,95 @@ def execute_materialize_command(instance: DagsterInstance, kwargs: Mapping[str, 
                 "All selected assets must have a PartitionsDefinition containing the passed"
                 f" partition key `{partition_range_start}` or have no PartitionsDefinition."
             )
-        tags = {
-            ASSET_PARTITION_RANGE_START_TAG: partition_range_start,
-            ASSET_PARTITION_RANGE_END_TAG: partition_range_end,
-        }
+
+        if backfill_policy_type == BackfillPolicyType.SINGLE_RUN:
+            tags = {
+                ASSET_PARTITION_RANGE_START_TAG: partition_range_start,
+                ASSET_PARTITION_RANGE_END_TAG: partition_range_end,
+            }
+        elif backfill_policy_type == BackfillPolicyType.MULTI_RUN:
+            from dagster import __version__ as dagster_version
+
+            with get_workspace_from_kwargs(
+                instance, version=dagster_version, kwargs=kwargs
+            ) as workspace:
+                code_location = get_code_location_from_workspace(workspace, None)
+
+                external_repo = get_external_repository_from_code_location(
+                    code_location, repo_def.name
+                )
+
+                external_job = get_external_job_from_external_repo(
+                    external_repo, implicit_job_def.name
+                )
+
+            job_partition_set = next(
+                (
+                    external_partition_set
+                    for external_partition_set in external_repo.get_external_partition_sets()
+                    if external_partition_set.job_name == external_job.name
+                ),
+                None,
+            )
+
+            if not job_partition_set:
+                raise click.UsageError(f"Job `{external_job.name}` is not partitioned.")
+
+            repo_handle = RepositoryHandle(
+                repository_name=external_repo.name,
+                code_location=code_location,
+            )
+
+            partition_keys = check.not_none(
+                implicit_job_def.partitions_def
+            ).get_partition_keys_in_range(PartitionKeyRange(start=..., end=...))
+
+            backfill_id = make_new_backfill_id()
+            backfill_job = PartitionBackfill(
+                backfill_id=backfill_id,
+                partition_set_origin=job_partition_set.get_external_origin(),
+                status=BulkActionStatus.REQUESTED,
+                partition_names=partition_keys,
+                from_failure=False,
+                reexecution_steps=None,
+                tags={},
+                backfill_timestamp=get_current_timestamp(),
+            )
+            try:
+                partition_execution_data = (
+                    code_location.get_external_partition_set_execution_param_data(
+                        repository_handle=repo_handle,
+                        partition_set_name=job_partition_set.name,
+                        partition_names=partition_keys,
+                        instance=instance,
+                    )
+                )
+            except Exception:
+                error_info = serializable_error_info_from_exc_info(sys.exc_info())
+                instance.add_backfill(
+                    backfill_job.with_status(BulkActionStatus.FAILED).with_error(error_info)
+                )
+                raise DagsterBackfillFailedError(f"Backfill failed: {error_info}")
+
+            assert isinstance(partition_execution_data, ExternalPartitionSetExecutionParamData)
+
+            for partition_data in partition_execution_data.partition_data:
+                dagster_run = create_backfill_run(
+                    instance,
+                    code_location,
+                    external_job,
+                    job_partition_set,
+                    backfill_job,
+                    partition_data.name,
+                    partition_data.tags,
+                    partition_data.run_config,
+                )
+                if dagster_run:
+                    instance.submit_run(dagster_run.run_id, workspace)
+
+            instance.add_backfill(backfill_job.with_status(BulkActionStatus.COMPLETED))
+
+            return
     else:
         if any(
             implicit_job_def.asset_layer.get(asset_key).partitions_def is not None
