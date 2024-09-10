a4da1944cd5705152e6b07f1728ee124b5e0ed4c
Colton Padden <colton@dagsterlabs.com>
2024-09-10 14:09:31 -0400

    wip

--- docs/docs-beta/scripts/sync-api-docs.sh
+++ docs/docs-beta/scripts/sync-api-docs.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+#
+# **NOTE: this script is intended to by used from Vercel only!**
+#
 # Description: Builds and synchronizes MDX API docs from Sphinx
 # Usage: yarn sync-api-docs
+#
 
 pushd ../sphinx
 
-make install_uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source ~/.cargo/env
+
 make install
 make mdx
 make mdx_copy
