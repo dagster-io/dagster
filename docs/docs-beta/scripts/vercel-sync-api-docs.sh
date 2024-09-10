1d6cab3a97a547faa870bf88d102457aa3436972
Colton Padden <colton@dagsterlabs.com>
2024-09-10 14:23:13 -0400

    wip

--- docs/docs-beta/scripts/vercel-sync-api-docs.sh
+++ docs/docs-beta/scripts/vercel-sync-api-docs.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 #
 # **NOTE: this script is intended to by used from Vercel only!**
 #
 # Description: Builds and synchronizes MDX API docs from Sphinx
 # Usage: yarn sync-api-docs
 #
+
+set -e
 
 pushd ../sphinx
 
@@ -13,6 +15,17 @@ source ~/.cargo/env
 
 uv venv
 source .venv/bin/activate
+
+# File "/vercel/path0/docs/sphinx/.venv/bin/sphinx-build", line 8, in <module>
+#   sys.exit(main())
+#            ^^^^^^
+# File "/vercel/path0/docs/sphinx/.venv/lib/python3.12/site-packages/sphinx/cmd/build.py", line 369, in main
+#   locale.setlocale(locale.LC_ALL, '')
+# File "/python312/lib/python3.12/locale.py", line 615, in setlocale
+#   return _setlocale(category, locale)
+#          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# locale.Error: unsupported locale setting
+export LC_ALL=C.UTF-8
 
 make install
 make mdx
