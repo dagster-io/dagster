18bafa989b916b77d855ce21d027cba3ec2cf7e6
Colton Padden <colton@dagsterlabs.com>
2024-09-10 14:59:06 -0400

    wip

--- docs/docs-beta/scripts/vercel-sync-api-docs.sh
+++ docs/docs-beta/scripts/vercel-sync-api-docs.sh
@@ -25,7 +25,7 @@ source .venv/bin/activate
 #   return _setlocale(category, locale)
 #          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # locale.Error: unsupported locale setting
-export LC_ALL=en_US.UTF-8
+export LC_ALL=C.UTF-8
 
 uv pip install tox
 tox -e sphinx
