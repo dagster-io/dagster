f5a04b3fa78eb16202345611c941b8ec58e35a52
Erin Cochran <erin.k.cochran@gmail.com>
2024-09-11 12:17:38 -0400

    First pass

--- examples/docs_beta_snippets/docs_beta_snippets/guides/data-modeling/external-assets/creating-external-assets.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/data-modeling/external-assets/creating-external-assets.py
@@ -1,15 +1,18 @@
 import dagster as dg
 
-# We define a new external asset with the key "raw_transactions".
+# Define an external asset with the key "raw_transactions".
 # This will appear in the Dagster asset catalog, but cannot
 # be materialized by Dagster itself.
+# highlight-next-line
 raw_transactions = dg.AssetSpec("raw_transactions")
 
 
 # This asset is materialized by Dagster and depends on the
 # external asset.
 @dg.asset(deps=[raw_transactions])
-def cleaned_transactions(): ...
+def cleaned_transactions():
+    ...
 
 
+# Define the Definitions object
 defs = dg.Definitions(assets=[raw_transactions, cleaned_transactions])
