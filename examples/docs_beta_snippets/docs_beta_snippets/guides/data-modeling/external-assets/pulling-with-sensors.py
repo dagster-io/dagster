3fa4cf6f34dcc376400f01df69bf168b40692ab2
Erin Cochran <erin.k.cochran@gmail.com>
2024-09-11 15:31:14 -0400

    [docs-revamp] - Clean up External assets guide (#24402)
    ## Summary & Motivation
    
    ## How I Tested These Changes
    
    ## Changelog
    
    NOCHANGELOG
    
    ---------
    
    Co-authored-by: colton <cmpadden@users.noreply.github.com>

--- examples/docs_beta_snippets/docs_beta_snippets/guides/data-modeling/external-assets/pulling-with-sensors.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/data-modeling/external-assets/pulling-with-sensors.py
@@ -1,5 +1,6 @@
 import dagster as dg
 
+# Define the external asset
 raw_transactions = dg.AssetSpec("raw_transactions")
 
 
@@ -7,18 +8,18 @@ raw_transactions = dg.AssetSpec("raw_transactions")
 def raw_transactions_sensor(
     context: dg.SensorEvaluationContext,
 ) -> dg.SensorResult:
-    # This sensor polls the external system every 30 seconds
-    # for the last time the file was modified.
+    # Poll the external system every 30 seconds
+    # for the last time the file was modified
     file_last_modified_at_ms = ...
 
-    # We can use the cursor to store the last time the sensor updated the asset
+    # Use the cursor to store the last time the sensor updated the asset
     if context.cursor is not None:
         external_asset_last_updated_at_ms = float(context.cursor)
     else:
         external_asset_last_updated_at_ms = 0
 
     if file_last_modified_at_ms > external_asset_last_updated_at_ms:
-        # The external asset has been modified since we last updated it,
+        # The external asset has been modified since it was last updated,
         # so record a materialization and update the cursor.
         return dg.SensorResult(
             asset_events=[
@@ -31,8 +32,9 @@ def raw_transactions_sensor(
             cursor=str(file_last_modified_at_ms),
         )
     else:
-        # Nothing has happened since the last time we checked
+        # Nothing has happened since the last check
         return dg.SensorResult()
 
 
+# Define the Definitions object
 defs = dg.Definitions(assets=[raw_transactions], sensors=[raw_transactions_sensor])
