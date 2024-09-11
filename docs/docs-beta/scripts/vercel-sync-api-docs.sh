93bd7a1af431e1ba6e8f0db78199494d0b5864f5
Colton Padden <colton@dagsterlabs.com>
2024-09-11 11:26:58 -0400

    rm error

--- docs/docs-beta/scripts/vercel-sync-api-docs.sh
+++ docs/docs-beta/scripts/vercel-sync-api-docs.sh
@@ -16,15 +16,7 @@ source ~/.cargo/env
 uv venv
 source .venv/bin/activate
 
-# File "/vercel/path0/docs/sphinx/.venv/bin/sphinx-build", line 8, in <module>
-#   sys.exit(main())
-#            ^^^^^^
-# File "/vercel/path0/docs/sphinx/.venv/lib/python3.12/site-packages/sphinx/cmd/build.py", line 369, in main
-#   locale.setlocale(locale.LC_ALL, '')
-# File "/python312/lib/python3.12/locale.py", line 615, in setlocale
-#   return _setlocale(category, locale)
-#          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# locale.Error: unsupported locale setting
+# Required as is locale is not set by default in Vercel runner
 export LC_ALL=C.UTF-8
 
 uv pip install tox
