739374764214d7a2a9c7a89f66300d4551c2c659
Sean Mackesey <s.mackesey@gmail.com>
2024-09-11 12:19:46 -0400

    [lazy-defs] Add source metadata to PowerBI integration

--- python_modules/libraries/dagster-powerbi/dagster_powerbi/__init__.py
+++ python_modules/libraries/dagster-powerbi/dagster_powerbi/__init__.py
@@ -1,6 +1,9 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-from dagster_powerbi.resource import PowerBIWorkspace as PowerBIWorkspace
+from dagster_powerbi.resource import (
+    PowerBIWorkspace as PowerBIWorkspace,
+    load_powerbi_defs as load_powerbi_defs,
+)
 from dagster_powerbi.translator import DagsterPowerBITranslator as DagsterPowerBITranslator
 
 # Move back to version.py and edit setup.py once we are ready to publish.
