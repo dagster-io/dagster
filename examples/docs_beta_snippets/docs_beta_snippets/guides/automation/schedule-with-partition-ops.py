b399f60371b6197f87aef586efe2f77c6594b889
Erin Cochran <erin.k.cochran@gmail.com>
2024-09-12 14:41:29 -0400

    First pass

---
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/automation/schedule-with-partition-ops.py
@@ -0,0 +1,14 @@
+import dagster as dg
+
+
+# Define the job
+@dg.job(config=partitioned_config)
+def partitioned_op_job(): ...
+
+
+# highlight-start
+# Create the schedule from the partition
+partitioned_op_schedule = dg.build_schedule_from_partitioned_job(
+    partitioned_op_job,
+)
+# highlight-end
--- examples/docs_beta_snippets/docs_beta_snippets/guides/automation/schedule-with-partition.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/automation/schedule-with-partition.py
@@ -1,5 +1,6 @@
 import dagster as dg
 
+# Daily partition
 daily_partition = dg.DailyPartitionsDefinition(start_date="2024-05-20")
 
 
@@ -7,10 +8,11 @@ daily_partition = dg.DailyPartitionsDefinition(start_date="2024-05-20")
 def daily_asset(): ...
 
 
+# Define the asset job
 partitioned_asset_job = dg.define_asset_job("partitioned_job", selection=[daily_asset])
 
 # highlight-start
-# This partition will run daily
+# This schedule will run daily
 asset_partitioned_schedule = dg.build_schedule_from_partitioned_job(
     partitioned_asset_job,
 )
