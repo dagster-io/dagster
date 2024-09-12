7d59c61232d1ec99c08ff8e49a88f847823dc6ad
Erin Cochran <erin.k.cochran@gmail.com>
2024-09-12 17:27:07 -0400

    First pass

--- examples/docs_beta_snippets/docs_beta_snippets/guides/automation/multi-asset-sensor.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/automation/multi-asset-sensor.py
@@ -6,9 +6,12 @@ def target_asset():
     pass
 
 
+# Job to trigger
 downstream_job = dg.define_asset_job("downstream_job", [target_asset])
 
 
+# highlight-start
+# Sensor monitoring multiple assets
 @dg.multi_asset_sensor(
     monitored_assets=[
         dg.AssetKey("upstream_asset_1"),
@@ -22,7 +25,9 @@ def my_multi_asset_sensor(context: dg.MultiAssetSensorEvaluationContext):
         asset_key,
         materialization,
     ) in context.latest_materialization_records_by_key().items():
+        # Trigger downstream_job when any of the monitored assets are materialized
         if materialization:
             run_requests.append(dg.RunRequest(asset_selection=[asset_key]))
             context.advance_cursor({asset_key: materialization})
     return run_requests
+    # highlight-end
