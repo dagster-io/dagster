d8c5d0d58aadea5ca6b530722749bbf725120609
benpankow <ben@dagsterlabs.com>
2024-09-13 07:58:44 -0700

    [2/n][dagster-sigma] Scaffold Sigma resource and auth

---
+++ python_modules/libraries/dagster-sigma/dagster_sigma/resource.py
@@ -0,0 +1,62 @@
+from enum import Enum
+from typing import Any, Dict, Optional
+
+import requests
+from dagster import ConfigurableResource
+from pydantic import Field, PrivateAttr
+
+
+class SigmaCloudType(Enum):
+    AWS_US = "https://aws-api.sigmacomputing.com"
+    AWS_CANADA = "https://api.ca.aws.sigmacomputing.com"
+    AWS_EUROPE = "https://api.eu.aws.sigmacomputing.com"
+    AWS_UK = "https://api.uk.aws.sigmacomputing.com"
+    AZURE_US = "https://api.us.azure.sigmacomputing.com"
+    GCP = "https://api.sigmacomputing.com"
+
+
+class SigmaOrganization(ConfigurableResource):
+    """Represents a workspace in PowerBI and provides utilities
+    to interact with the PowerBI API.
+    """
+
+    cloud_type: SigmaCloudType = Field(
+        ...,
+        description="The cloud type for your Sigma organization, found under the Administration -> Account -> Site settings.",
+    )
+    client_id: str = Field(..., description="A client ID with access to the Sigma API.")
+    client_secret: str = Field(..., description="A client secret with access to the Sigma API.")
+
+    _api_token: Optional[str] = PrivateAttr(None)
+
+    def _fetch_api_token(self) -> str:
+        response = requests.post(
+            url=f"{self.cloud_type.value}/v2/auth/token",
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            data={
+                "grant_type": "client_credentials",
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+            },
+        )
+        response.raise_for_status()
+        return response.json()["access_token"]
+
+    @property
+    def api_token(self) -> str:
+        if self._api_token is None:
+            self._api_token = self._fetch_api_token()
+
+        return self._api_token
+
+    def fetch_json(self, endpoint: str, method: str = "GET") -> Dict[str, Any]:
+        response = requests.request(
+            method=method,
+            url=f"{self.cloud_type.value}/v2/{endpoint}",
+            headers={"Accept": "application/json", "Authorization": f"Bearer {self.api_token}"},
+        )
+        response.raise_for_status()
+        return response.json()
