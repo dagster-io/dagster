cdfd3c43e925cda638535a1717c3a116cd6425bf
Rex Ledesma <rex@dagsterlabs.com>
2024-09-16 15:58:03 -0400

    feat(looker): implement `LookerResource` (#24445)
    ## Summary & Motivation
    Super bare bones, but implements a resource that instantiates the Looker
    sdk.
    
    Next, we'll use this connection to create cacheable assets.
    
    ## How I Tested These Changes
    pytest, local
    
    ## Changelog
    NOCHANGELOG
    
    - [ ] `NEW` _(added new feature or capability)_
    - [ ] `BUGFIX` _(fixed a bug)_
    - [ ] `DOCS` _(added or updated documentation)_

---
+++ python_modules/libraries/dagster-looker/dagster_looker/resource.py
@@ -0,0 +1,31 @@
+from dagster import ConfigurableResource
+from dagster._utils.cached_method import cached_method
+from looker_sdk import init40
+from looker_sdk.rtl.api_settings import ApiSettings, SettingsConfig
+from looker_sdk.sdk.api40.methods import Looker40SDK
+from pydantic import Field
+
+
+class LookerResource(ConfigurableResource):
+    """Represents a connection to a Looker instance and provides methods
+    to interact with the Looker API.
+    """
+
+    base_url: str = Field(
+        ..., description="Base URL for the Looker API. For example, https://your.cloud.looker.com."
+    )
+    client_id: str = Field(..., description="Client ID for the Looker API.")
+    client_secret: str = Field(..., description="Client secret for the Looker API.")
+
+    @cached_method
+    def get_sdk(self) -> Looker40SDK:
+        class DagsterLookerApiSettings(ApiSettings):
+            def read_config(_self) -> SettingsConfig:
+                return {
+                    **super().read_config(),
+                    "base_url": self.base_url,
+                    "client_id": self.client_id,
+                    "client_secret": self.client_secret,
+                }
+
+        return init40(config_settings=DagsterLookerApiSettings())
