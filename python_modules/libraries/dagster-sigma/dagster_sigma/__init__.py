a1a7c81b007b0a35ce04e7ed8f158d378296c441
Ben Pankow <ben@elementl.com>
2024-09-16 10:17:35 -0700

    [1/n][dagster-sigma] Scaffold empty dagster-sigma package (#24475)
    ## Summary
    
    Introduces an empty `dagster-sigma` package for the Sigma integration.
    
    ## Changelog
    
    > NOCHANGELOG

---
+++ python_modules/libraries/dagster-sigma/dagster_sigma/__init__.py
@@ -0,0 +1,6 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+# Move back to version.py and edit setup.py once we are ready to publish.
+__version__ = "1!0+dev"
+
+DagsterLibraryRegistry.register("dagster-sigma", __version__)
