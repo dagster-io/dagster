8fe7016e7189a877fbe0a0f3abac229afdcd5ddd
benpankow <ben@dagsterlabs.com>
2024-09-17 15:17:04 -0700

    export

--- python_modules/libraries/dagster-sigma/dagster_sigma/__init__.py
+++ python_modules/libraries/dagster-sigma/dagster_sigma/__init__.py
@@ -4,6 +4,11 @@ from dagster_sigma.resource import (
     SigmaBaseUrl as SigmaBaseUrl,
     SigmaOrganization as SigmaOrganization,
 )
+from dagster_sigma.translator import (
+    DagsterSigmaTranslator as DagsterSigmaTranslator,
+    SigmaDataset as SigmaDataset,
+    SigmaWorkbook as SigmaWorkbook,
+)
 
 # Move back to version.py and edit setup.py once we are ready to publish.
 __version__ = "1!0+dev"
