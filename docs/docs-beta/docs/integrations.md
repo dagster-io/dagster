27d00939f4e41b7c4de40571605186274aff4023
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2024-09-18 13:02:41 -0700

    i think its looking good

--- docs/docs-beta/docs/integrations.md
+++
@@ -1,8 +0,0 @@
----
-title: 'Integrations'
-displayed_sidebar: 'integrations'
----
-
-# Integrations
-
-imagine a nice-looking list of all integrations with icons and links to the integration pages
--- docs/docs-beta/docusaurus.config.ts
+++ docs/docs-beta/docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
         {
           label: 'Integrations',
           type: 'doc',
-          docId: 'integrations',
+          docId: 'integrations/index',
           position: 'left',
         },
         {
