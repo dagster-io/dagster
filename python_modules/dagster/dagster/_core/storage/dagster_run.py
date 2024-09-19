38dff9c4a313726771ba39da860e5e3e4d54dd89
JamieDeMaria <jamie@dagsterlabs.com>
2024-09-19 15:32:20 -0400

    mark filter experimental

--- python_modules/dagster/dagster/_core/storage/dagster_run.py
+++ python_modules/dagster/dagster/_core/storage/dagster_run.py
@@ -17,7 +17,7 @@ from typing import (
 from typing_extensions import Self
 
 import dagster._check as check
-from dagster._annotations import PublicAttr, public
+from dagster._annotations import PublicAttr, experimental_param, public
 from dagster._core.definitions.asset_check_spec import AssetCheckKey
 from dagster._core.definitions.events import AssetKey
 from dagster._core.loader import InstanceLoadableBy
@@ -543,6 +543,7 @@ class RunsFilter(IHaveNew, LegacyNamedTupleMixin):
     created_before: Optional[datetime]
     exclude_subruns: Optional[bool]
 
+    @experimental_param(param="exclude_subruns")
     def __new__(
         cls,
         run_ids: Optional[Sequence[str]] = None,
