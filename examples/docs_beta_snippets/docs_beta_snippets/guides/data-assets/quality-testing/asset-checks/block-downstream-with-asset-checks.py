9075bbc9cf27d8f2b758d55af101cdc745a581c0
Erin Cochran <erin.k.cochran@gmail.com>
2024-09-19 12:18:45 -0400

    [docs-revamp] - Clean up Asset Checks guide (#24483)
    ## Summary & Motivation
    
    ## How I Tested These Changes
    
    ## Changelog
    
    NOCHANGELOG
    
    ---------
    
    Co-authored-by: Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
    Co-authored-by: Colton Padden <colton@dagsterlabs.com>

--- examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/quality-testing/asset-checks/block-downstream-with-asset-checks.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/quality-testing/asset-checks/block-downstream-with-asset-checks.py
@@ -9,8 +9,10 @@ def orders():
     orders_df.to_csv("orders.csv")
 
 
-# highlight-next-line
+# highlight-start
+# Check that targets `orders`; block materialization of `augmented_orders` on failure
 @dg.asset_check(asset=orders, blocking=True)
+# highlight-end
 def orders_id_has_no_nulls():
     orders_df = pd.read_csv("orders.csv")
     num_null_order_ids = orders_df["order_id"].isna().sum()
@@ -19,6 +21,7 @@ def orders_id_has_no_nulls():
     )
 
 
+# Asset downstream of `orders`
 @dg.asset(deps=[orders])
 def augmented_orders():
     orders_df = pd.read_csv("orders.csv")
