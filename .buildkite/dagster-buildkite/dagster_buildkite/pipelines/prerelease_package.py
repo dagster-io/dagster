1ac4eacd0494805e2458b0dd453555a5323104c3
benpankow <ben@dagsterlabs.com>
2024-09-19 15:29:37 -0700

    sigma3

--- .buildkite/dagster-buildkite/dagster_buildkite/pipelines/prerelease_package.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/pipelines/prerelease_package.py
@@ -22,7 +22,7 @@ def build_prerelease_package_steps() -> List[BuildkiteStep]:
     for package in packages:
         setup_file = Path(package) / "setup.py"
         contents = setup_file.read_text()
-        if re.match(r"version=\"[\d\.]+\"", contents):
+        if re.findall(r"version=\"[\d\.]+\"", contents):
             filtered_packages.append(package)
 
     input_step: BlockStep = {
