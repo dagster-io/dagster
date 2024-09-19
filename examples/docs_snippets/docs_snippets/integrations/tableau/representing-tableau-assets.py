6fcce59e8627ee6e30cd60361ffae918a12095fa
Maxime Armstrong <maxime@retain.ai>
2024-09-19 18:20:00 -0400

    Add code snippets

---
+++ examples/docs_snippets/docs_snippets/integrations/tableau/representing-tableau-assets.py
@@ -0,0 +1,25 @@
+from dagster_tableau import TableauCloudWorkspace, TableauServerWorkspace
+
+from dagster import EnvVar
+
+# Connect to Tableau Cloud using the connected app credentials
+workspace = TableauCloudWorkspace(
+    connected_app_client_id=EnvVar("TABLEAU_CONNECTED_APP_CLIENT_ID"),
+    connected_app_secret_id=EnvVar("TABLEAU_CONNECTED_APP_SECRET_ID"),
+    connected_app_secret_value=EnvVar("TABLEAU_CONNECTED_APP_SECRET_VALUE"),
+    username=EnvVar("TABLEAU_USER_NAME"),
+    site_name=EnvVar("TABLEAU_SITE_NAME"),
+    pod_name=EnvVar("TABLEAU_POD_NAME"),
+)
+
+# Connect to Tableau Server using the same approach
+workspace = TableauServerWorkspace(
+    connected_app_client_id=EnvVar("TABLEAU_CONNECTED_APP_CLIENT_ID"),
+    connected_app_secret_id=EnvVar("TABLEAU_CONNECTED_APP_SECRET_ID"),
+    connected_app_secret_value=EnvVar("TABLEAU_CONNECTED_APP_SECRET_VALUE"),
+    username=EnvVar("TABLEAU_USER_NAME"),
+    site_name=EnvVar("TABLEAU_SITE_NAME"),
+    pod_name=EnvVar("TABLEAU_POD_NAME"),
+)
+
+defs = workspace.build_defs()
