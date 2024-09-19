a48b3e6d6f5626e579435f39d3227817107eda7d
Erin Cochran <erin.k.cochran@gmail.com>
2024-09-19 09:37:44 -0400

    [docs-revamp] - Clean up Sensors guide (#24462)
    ## Summary & Motivation
    
    ## How I Tested These Changes
    
    ## Changelog
    
    NOCHANGELOG
    
    ---------
    
    Co-authored-by: Colton Padden <colton@dagsterlabs.com>

--- examples/docs_beta_snippets/docs_beta_snippets/guides/automation/simple-sensor-example.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/automation/simple-sensor-example.py
@@ -4,27 +4,37 @@ from typing import List
 import dagster as dg
 
 
+# Define the asset
 @dg.asset
 def my_asset(context: dg.AssetExecutionContext):
     context.log.info("Hello, world!")
 
 
+# Define asset job
 my_job = dg.define_asset_job("my_job", selection=[my_asset])
 
 
 # highlight-start
+# Define file check
 def check_for_new_files() -> List[str]:
     if random.random() > 0.5:
         return ["file1", "file2"]
     return []
 
 
-@dg.sensor(job=my_job, minimum_interval_seconds=5)
+# Define the sensor
+@dg.sensor(
+    job=my_job,
+    minimum_interval_seconds=5,
+    default_status=dg.DefaultSensorStatus.RUNNING,  # Sensor is turned on by default
+)
 def new_file_sensor():
     new_files = check_for_new_files()
+    # New files, run `my_job`
     if new_files:
         for filename in new_files:
             yield dg.RunRequest(run_key=filename)
+    # No new files, skip the run and log the reason
     else:
         yield dg.SkipReason("No new files found")
         # highlight-end
