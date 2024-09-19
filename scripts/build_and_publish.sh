eeba61f9a9119a3b93f08c75cc5dfc2fd1cff24e
benpankow <ben@dagsterlabs.com>
2024-09-19 14:17:31 -0700

    bestmode

--- scripts/build_and_publish.sh
+++ scripts/build_and_publish.sh
@@ -12,15 +12,15 @@ fi
 
 mkdir -p package_prerelease
 cp -R $PACKAGE_TO_RELEASE_PATH/* package_prerelease
-pushd package_prerelease
+cd package_prerelease
 
 # Update both a hardcoded version, if set, in setup.py, and
 # find where __version__ is set and update it
-sed -i "" "s|return \"1!0+dev\"|return \"$VERSION_TO_RELEASE\"|" setup.py
-grep -rl "__version__ = \"1!0+dev\"" ./ | xargs sed -i "" "s|\"1!0+dev\"|\"$VERSION_TO_RELEASE\"|"
+sed -i "s|return \"1!0+dev\"|return \"$VERSION_TO_RELEASE\"|" setup.py
+grep -rl "__version__ = \"1!0+dev\"" ./ | xargs sed -i "s|\"1!0+dev\"|\"$VERSION_TO_RELEASE\"|"
 
 echo "Building package..."
 python3 -m build
 echo "Uploading to pypi..."
 # Capture the output of the twine upload command
-python3 -m twine upload --username "__token__" --password $PYPI_TOKEN --repository pypi dist/* --verbose
+python3 -m twine upload --username "__token__" --password "$PYPI_TOKEN" --repository pypi dist/* --verbose
