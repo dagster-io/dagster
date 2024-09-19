6fdd8d4e914938db1a2323b85bf463da4a7e2330
Christopher DeCarolis <cdguitar817@gmail.com>
2024-09-19 08:28:38 -0700

    [dagster-airlift] fix test fixture inheritance (#24582)
    Fix some nonsense about pytest fixture inheritance that was causing a
    misleading error to appear in the output. Also use better convention for
    makefile format.

--- examples/experimental/dagster-airlift/examples/perf-harness/perf_harness_tests/conftest.py
+++ examples/experimental/dagster-airlift/examples/perf-harness/perf_harness_tests/conftest.py
@@ -5,20 +5,24 @@ from typing import Generator
 
 import pytest
 from dagster._core.test_utils import environ
+from dagster_airlift.test.shared_fixtures import stand_up_airflow
+
+
+def makefile_dir() -> Path:
+    return Path(__file__).parent.parent
 
 
 @pytest.fixture(name="local_env")
 def local_env_fixture() -> Generator[None, None, None]:
-    makefile_dir = Path(__file__).parent.parent
-    subprocess.run(["make", "setup_local_env"], cwd=makefile_dir, check=True)
+    subprocess.run(["make", "setup_local_env"], cwd=makefile_dir(), check=True)
     with environ(
         {
-            "AIRFLOW_HOME": str(makefile_dir / ".airflow_home"),
-            "DAGSTER_HOME": str(makefile_dir / ".dagster_home"),
+            "AIRFLOW_HOME": str(makefile_dir() / ".airflow_home"),
+            "DAGSTER_HOME": str(makefile_dir() / ".dagster_home"),
         }
     ):
         yield
-    subprocess.run(["make", "wipe"], cwd=makefile_dir, check=True)
+    subprocess.run(["make", "wipe"], cwd=makefile_dir(), check=True)
 
 
 @pytest.fixture(name="dags_dir")
@@ -29,3 +33,11 @@ def dags_dir_fixture() -> Path:
 @pytest.fixture(name="airflow_home")
 def airflow_home_fixture(local_env: None) -> Path:
     return Path(os.environ["AIRFLOW_HOME"])
+
+
+@pytest.fixture(name="airflow_instance")
+def airflow_instance_fixture(local_env: None) -> Generator[subprocess.Popen, None, None]:
+    with stand_up_airflow(
+        airflow_cmd=["make", "run_airflow"], env=os.environ, cwd=makefile_dir()
+    ) as process:
+        yield process
