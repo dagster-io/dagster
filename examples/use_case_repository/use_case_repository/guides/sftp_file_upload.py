2dfe9586314bb49e5edc22287b69482dac185924
Colton Padden <colton@dagsterlabs.com>
2024-09-20 11:20:31 -0400

    tested sftp usecase

--- examples/use_case_repository/use_case_repository/guides/sftp_file_upload.py
+++ examples/use_case_repository/use_case_repository/guides/sftp_file_upload.py
@@ -1,20 +1,18 @@
-from dagster import asset, Definitions, AssetExecutionContext
+from dagster import AssetExecutionContext, Definitions, asset
 from dagster_ssh import SSHResource
 
-
 sftp_resource = SSHResource(
-    remote_host="your.server.com",
-    remote_port=2121,
-    username="your-username",
-    password="your-password",  # or use 'key_file': 'path/to/id_rsa' for key-based authentication
-
+    remote_host="your.hostname",
+    remote_port=22,
+    username="example",
+    key_file="/Users/example/.ssh/id_ed25519",
 )
 
 
 @asset
 def sftp_file(context: AssetExecutionContext, sftp: SSHResource):
-    local_file_path = "sftp_example_file.txt"
-    remote_file_path = "/path/to/remote/file.txt"
+    local_file_path = "hello.txt"
+    remote_file_path = "/path/to/destination/hello.txt"
     sftp.sftp_put(local_file_path, remote_file_path)
     context.log.info(f"Uploaded {local_file_path} to {remote_file_path} on SFTP server")
 
