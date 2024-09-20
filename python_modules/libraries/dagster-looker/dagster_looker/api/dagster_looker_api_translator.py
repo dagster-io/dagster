444e6db14bcead3d9785ed1fe297544bd5aa4f55
Rex Ledesma <rex@dagsterlabs.com>
2024-09-20 12:19:39 -0400

    feat(looker): implement dashboards as cacheable assets (#24606)
    ## Summary & Motivation
    
    Models [Looker
    dashboards](https://developers.looker.com/api/explorer/4.0/methods/Dashboard/all_dashboards?s=dashboard)
    as cacheable assets. To get a Looker dashboard's dependencies, we parse
    out its [dashboard
    filters](https://developers.looker.com/api/explorer/4.0/methods/Dashboard/dashboard?s=dashboard)
    for explores.
    
    ## How I Tested These Changes
    pytest, local
    
    ![Screenshot 2024-09-19 at
    3.16.15 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/3paPzMeByK6VgE9uQe4F/e795c7cf-9777-48e1-8fcb-453d309fcde6.png)
    
    ## Changelog
    NOCHANGELOG

--- python_modules/libraries/dagster-looker/dagster_looker/api/dagster_looker_api_translator.py
+++ python_modules/libraries/dagster-looker/dagster_looker/api/dagster_looker_api_translator.py
@@ -1,11 +1,11 @@
-from typing import Dict
+from typing import Dict, Union
 
 from dagster import (
     AssetSpec,
     _check as check,
 )
 from dagster._record import record
-from looker_sdk.sdk.api40.models import LookmlModelExplore
+from looker_sdk.sdk.api40.models import Dashboard, DashboardFilter, LookmlModelExplore
 
 
 @record
@@ -13,17 +13,44 @@ class LookerInstanceData:
     """A record representing all content in a Looker instance."""
 
     explores_by_id: Dict[str, LookmlModelExplore]
+    dashboards_by_id: Dict[str, Dashboard]
 
 
 class DagsterLookerApiTranslator:
-    def get_explore_asset_spec(self, lookml_explore: LookmlModelExplore) -> AssetSpec:
+    def get_explore_asset_spec(
+        self, lookml_explore: Union[LookmlModelExplore, DashboardFilter]
+    ) -> AssetSpec:
+        if isinstance(lookml_explore, LookmlModelExplore):
+            return AssetSpec(
+                key=check.not_none(lookml_explore.id),
+                tags={
+                    "dagster/kind/looker": "",
+                    "dagster/kind/explore": "",
+                },
+            )
+        elif isinstance(lookml_explore, DashboardFilter):
+            lookml_model_name = check.not_none(lookml_explore.model)
+            lookml_explore_name = check.not_none(lookml_explore.explore)
+
+            return AssetSpec(key=f"{lookml_model_name}::{lookml_explore_name}")
+
+    def get_dashboard_asset_spec(self, looker_dashboard: Dashboard) -> AssetSpec:
         return AssetSpec(
-            key=check.not_none(lookml_explore.id),
+            key=f"{check.not_none(looker_dashboard.title)}_{looker_dashboard.id}",
+            deps=list(
+                {
+                    self.get_explore_asset_spec(dashboard_filter).key
+                    for dashboard_filter in looker_dashboard.dashboard_filters or []
+                }
+            ),
             tags={
                 "dagster/kind/looker": "",
-                "dagster/kind/explore": "",
+                "dagster/kind/dashboard": "",
             },
         )
 
-    def get_asset_spec(self, api_model: LookmlModelExplore) -> AssetSpec:
-        return self.get_explore_asset_spec(api_model)
+    def get_asset_spec(self, api_model: Union[LookmlModelExplore, Dashboard]) -> AssetSpec:
+        if isinstance(api_model, Dashboard):
+            return self.get_dashboard_asset_spec(api_model)
+        elif isinstance(api_model, LookmlModelExplore):
+            return self.get_explore_asset_spec(api_model)
