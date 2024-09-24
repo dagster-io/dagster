cd100a5aca36ef9d818ac7cd910163f8d779ef13
Nick Schrock <schrockn@dagsterlabs.com>
2024-09-24 13:01:40 -0400

    Delete unused MIGRATED TAG

--- examples/experimental/dagster-airlift/dagster_airlift/core/task_asset.py
+++ examples/experimental/dagster-airlift/dagster_airlift/core/task_asset.py
@@ -8,7 +8,6 @@ from dagster._core.definitions.asset_spec import AssetSpec
 from dagster._core.definitions.metadata.metadata_value import UrlMetadataValue
 from dagster._record import record
 
-from dagster_airlift.constants import MIGRATED_TAG
 from dagster_airlift.core.airflow_instance import TaskInfo
 from dagster_airlift.core.serialization.serialized_data import SerializedAssetKeyScopedAirflowData
 from dagster_airlift.core.utils import airflow_kind_dict
@@ -50,8 +49,6 @@ def get_airflow_data_for_task_mapped_spec(
     task_info = mapped_task.task_info
 
     tags = airflow_kind_dict() if not migration_state else {}
-    if migration_state is not None:
-        tags[MIGRATED_TAG] = str(bool(migration_state))
 
     return SerializedAssetKeyScopedAirflowData(
         additional_metadata=task_asset_metadata(task_info, migration_state),
