f363009670baca5e6b989a4946e53154f4ef5735
Sean Mackesey <s.mackesey@gmail.com>
2024-09-24 23:38:48 -0400

    [lazy-defs] Move StateBackedDefinitionsLoader from airlift to core

--- python_modules/dagster/dagster/_core/definitions/definitions_load_context.py
+++ python_modules/dagster/dagster/_core/definitions/definitions_load_context.py
@@ -1,8 +1,11 @@
+from abc import ABC, abstractmethod
 from enum import Enum
 from functools import cached_property
-from typing import TYPE_CHECKING, Any, ClassVar, Mapping, Optional
+from typing import TYPE_CHECKING, Any, ClassVar, Generic, Mapping, Optional, TypeVar, cast
 
+from dagster._core.definitions.definitions_class import Definitions
 from dagster._core.errors import DagsterInvariantViolationError
+from dagster._serdes.serdes import PackableValue, deserialize_value, serialize_value
 
 if TYPE_CHECKING:
     from dagster._core.definitions.repository_definition import RepositoryLoadData
@@ -77,4 +80,86 @@ class DefinitionsLoadContext:
             {k: v.data for k, v in self._repository_load_data.reconstruction_metadata.items()}
             if self._repository_load_data
             else {}
+        )
+
+
+TState = TypeVar("TState", bound=PackableValue)
+
+
+class StateBackedDefinitionsLoader(ABC, Generic[TState]):
+    """A class for building state-backed definitions. In a structured way. It
+    handles manipulation of the DefinitionsLoadContext on the user's behalf.
+
+    The goal here is so that an unreliable backing source is fetched only
+    at code server load time, which defines the code location. When, for example,
+    a run worker is launched, the backing source is not queryed again, and
+    only `defs_from_state` is called.
+
+    Current meant for internal usage only hence TState must be a marked as whitelist_for_serdes.
+
+    Args:
+        defs_key (str): The unique key for the definitions. Must be unique per code location.
+
+    Examples:
+
+    .. code-block:: python
+
+        @whitelist_for_serdes
+        @record
+        class ExampleDefState:
+            a_string: str
+
+        class ExampleStateBackedDefinitionsLoader(StateBackedDefinitionsLoader[ExampleDefState]):
+            def fetch_state(self) -> ExampleDefState:
+                # Fetch from potentially unreliable API (e.g. Rest API)
+                return ExampleDefState(a_string="foo")
+
+            def defs_from_state(self, state: ExampleDefState) -> Definitions:
+                # Construct or reconstruct the Definitions from the previously
+                # fetched state.
+                return Definitions([AssetSpec(key=state.a_string)])
+    """
+
+    @property
+    @abstractmethod
+    def defs_key(self) -> str:
+        """The unique key for the definitions. Must be unique per code location."""
+        ...
+
+    @abstractmethod
+    def fetch_state(self) -> TState:
+        """Subclasses must implement this method. This is where the integration runs
+        code that fetches the backing state from the source of truth for the definitions.
+        This is only called when the code location is initializing, for example on
+        code server load, or when loading via dagster dev.
+        """
+        ...
+
+    @abstractmethod
+    def defs_from_state(self, state: TState) -> Definitions:
+        """Subclasses must implement this method. It is invoked whenever the code location
+        is loading, whether it be initializaton or reconstruction. In the case of
+        intialization, it takes the result of fetch_backing state that just happened.
+        When reconstructing, it takes the state that was previously fetched and attached
+        as metadata.
+
+        This method also takes responsibility for attaching the state to the definitions
+        on its metadata, with the key passed in as defs_key.
+        """
+        ...
+
+    def build_defs(self) -> Definitions:
+        context = DefinitionsLoadContext.get()
+
+        state = (
+            cast(TState, deserialize_value(context.reconstruction_metadata[self.defs_key]))
+            if (
+                context.load_type == DefinitionsLoadType.RECONSTRUCTION
+                and self.defs_key in context.reconstruction_metadata
+            )
+            else self.fetch_state()
+        )
+
+        return self.defs_from_state(state).with_reconstruction_metadata(
+            {self.defs_key: serialize_value(state)}
         )
