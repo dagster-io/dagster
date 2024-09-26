98f47fecc96218667ed262551355241a47ada29a
Chris DeCarolis <cdguitar817@gmail.com>
2024-09-26 15:18:23 -0700

    [dagster-airlift] introduce effective timestamp

--- examples/experimental/dagster-airlift/dagster_airlift/constants.py
+++ examples/experimental/dagster-airlift/dagster_airlift/constants.py
@@ -1,3 +1,5 @@
 STANDALONE_DAG_ID_METADATA_KEY = "airlift/dag_id"
 AIRFLOW_SOURCE_METADATA_KEY_PREFIX = "dagster-airlift/source"
 TASK_MAPPING_METADATA_KEY = "dagster-airlift/task_mapping"
+# This represents the timestamp used in ordering the materializatons.
+EFFECTIVE_TIMESTAMP_METADATA_KEY = "dagster-airlift/effective_timestamp"
