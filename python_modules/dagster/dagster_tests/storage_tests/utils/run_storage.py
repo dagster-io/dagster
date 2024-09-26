4ce7c24952a1c66c4efce580b66c09a6d4c40dd7
JamieDeMaria <jamie@dagsterlabs.com>
2024-09-26 12:38:57 -0400

    cleanup

--- python_modules/dagster/dagster_tests/storage_tests/utils/run_storage.py
+++ python_modules/dagster/dagster_tests/storage_tests/utils/run_storage.py
@@ -98,10 +98,6 @@ class TestRunStorage:
     def supports_backfill_job_name_filtering_queries(self):
         return False
 
-    def assert_results_match_count_match_expected(results, count, expected):
-        assert len(results) == count
-        assert count == expected
-
     def get_backfills_and_assert_expected_count(self, storage, filters, expected_count):
         backfills = storage.get_backfills(filters)
         count = storage.get_backfills_count(filters)
