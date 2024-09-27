31c6f365d9e6c69dd478e35ddac3cc77a1481fef
Nick Schrock <schrockn@dagsterlabs.com>
2024-09-27 16:09:35 -0400

    Add integration test for automapped tasks

--- examples/experimental/dagster-airlift/examples/kitchen-sink/kitchen_sink/dagster_defs/automapped_defs.py
+++ examples/experimental/dagster-airlift/examples/kitchen-sink/kitchen_sink/dagster_defs/automapped_defs.py
@@ -1,20 +1,12 @@
 from dagster._core.definitions.definitions_class import Definitions
-from dagster_airlift.core import AirflowInstance, BasicAuthBackend
 from dagster_airlift.core.load_defs import build_full_automapped_dags_from_airflow_instance
 
-from .constants import AIRFLOW_BASE_URL, AIRFLOW_INSTANCE_NAME, PASSWORD, USERNAME
-
-airflow_instance = AirflowInstance(
-    auth_backend=BasicAuthBackend(
-        webserver_url=AIRFLOW_BASE_URL, username=USERNAME, password=PASSWORD
-    ),
-    name=AIRFLOW_INSTANCE_NAME,
-)
+from .airflow_instance import local_airflow_instance
 
 
 def build_automapped_defs() -> Definitions:
     return build_full_automapped_dags_from_airflow_instance(
-        airflow_instance=airflow_instance,
+        airflow_instance=local_airflow_instance(),
     )
 
 
--- examples/experimental/dagster-airlift/examples/kitchen-sink/kitchen_sink/dagster_defs/mapped_defs.py
+++ examples/experimental/dagster-airlift/examples/kitchen-sink/kitchen_sink/dagster_defs/mapped_defs.py
@@ -1,21 +1,14 @@
 # ruff: noqa: T201
 from dagster import Definitions, asset
-from dagster_airlift.core import (
-    AirflowInstance,
-    BasicAuthBackend,
-    build_defs_from_airflow_instance,
-    dag_defs,
-    task_defs,
-)
+from dagster_airlift.core import build_defs_from_airflow_instance, dag_defs, task_defs
 
-from .constants import AIRFLOW_BASE_URL, AIRFLOW_INSTANCE_NAME, PASSWORD, USERNAME
-
-airflow_instance = AirflowInstance(
-    auth_backend=BasicAuthBackend(
-        webserver_url=AIRFLOW_BASE_URL, username=USERNAME, password=PASSWORD
-    ),
-    name=AIRFLOW_INSTANCE_NAME,
-)
+# airflow_instance = AirflowInstance(
+#     auth_backend=BasicAuthBackend(
+#         webserver_url=AIRFLOW_BASE_URL, username=USERNAME, password=PASSWORD
+#     ),
+#     name=AIRFLOW_INSTANCE_NAME,
+# )
+from .airflow_instance import local_airflow_instance
 
 
 @asset
@@ -25,7 +18,7 @@ def print_asset():
 
 def build_basic_mapped_defs() -> Definitions:
     return build_defs_from_airflow_instance(
-        airflow_instance=airflow_instance,
+        airflow_instance=local_airflow_instance(),
         defs=dag_defs(
             "print_dag",
             task_defs("print_task", Definitions(assets=[print_asset])),
