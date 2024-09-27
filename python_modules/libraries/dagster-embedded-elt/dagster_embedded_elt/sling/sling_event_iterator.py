e5f00ffe9a39800bb677e5ef860604f891d43cf2
benpankow <ben@dagsterlabs.com>
2024-09-27 08:38:59 -0700

    fstring

--- python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/sling_event_iterator.py
+++ python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt/sling/sling_event_iterator.py
@@ -90,9 +90,9 @@ def fetch_row_count_metadata(
         try:
             row_count = sling_cli.get_row_count_for_table(target_name, target_table_name)
             return dict(TableMetadataSet(row_count=row_count))
-        except Exception:
+        except Exception as e:
             context.log.warning(
-                "Failed to fetch row count for stream %s\nException: {e}",
+                f"Failed to fetch row count for stream %s\nException: {e}",
                 stream_name,
                 exc_info=True,
             )
@@ -158,9 +158,9 @@ def fetch_column_metadata(
                     column_lineage=column_lineage,
                 )
             )
-        except Exception:
+        except Exception as e:
             context.log.warning(
-                "Failed to fetch column metadata for stream %s\nException: {e}",
+                f"Failed to fetch column metadata for stream %s\nException: {e}",
                 stream_name,
                 exc_info=True,
             )
