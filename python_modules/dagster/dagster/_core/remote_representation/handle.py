e8b501816e425403fcb088aab6aa028a6be1b956
Alex Langenfeld <alex@elementl.com>
2024-09-30 14:03:00 -0500

    [record] handle.py (#24822)
    ## How I Tested These Changes
    
    tests and pyright

--- python_modules/dagster/dagster/_core/remote_representation/handle.py
+++ python_modules/dagster/dagster/_core/remote_representation/handle.py
@@ -1,59 +1,59 @@
-from typing import TYPE_CHECKING, Mapping, NamedTuple
+from typing import TYPE_CHECKING, Mapping
 
 import dagster._check as check
 from dagster._core.definitions.selector import JobSubsetSelector
 from dagster._core.origin import RepositoryPythonOrigin
 from dagster._core.remote_representation.origin import CodeLocationOrigin, RemoteRepositoryOrigin
+from dagster._record import IHaveNew, record, record_custom
 
 if TYPE_CHECKING:
     from dagster._core.remote_representation.code_location import CodeLocation
 
 
-class RepositoryHandle(
-    NamedTuple(
-        "_RepositoryHandle",
-        [
-            ("repository_name", str),
-            ("code_location_origin", CodeLocationOrigin),
-            ("repository_python_origin", RepositoryPythonOrigin),
-            ("display_metadata", Mapping[str, str]),
-        ],
-    )
-):
+@record_custom
+class RepositoryHandle(IHaveNew):
+    repository_name: str
+    code_location_origin: CodeLocationOrigin
+    repository_python_origin: RepositoryPythonOrigin
+    display_metadata: Mapping[str, str]
+
     def __new__(cls, repository_name: str, code_location: "CodeLocation"):
         from dagster._core.remote_representation.code_location import CodeLocation
 
         check.inst_param(code_location, "code_location", CodeLocation)
-        return super(RepositoryHandle, cls).__new__(
+        return super().__new__(
             cls,
-            check.str_param(repository_name, "repository_name"),
-            code_location.origin,
-            code_location.get_repository_python_origin(repository_name),
-            code_location.get_display_metadata(),
+            repository_name=repository_name,
+            code_location_origin=code_location.origin,
+            repository_python_origin=code_location.get_repository_python_origin(repository_name),
+            display_metadata=code_location.get_display_metadata(),
         )
 
     @property
     def location_name(self) -> str:
         return self.code_location_origin.location_name
 
-    def get_remote_origin(self):
+    def get_remote_origin(self) -> RemoteRepositoryOrigin:
         return RemoteRepositoryOrigin(
-            self.code_location_origin,
-            self.repository_name,
+            code_location_origin=self.code_location_origin,
+            repository_name=self.repository_name,
         )
 
-    def get_python_origin(self):
+    def get_python_origin(self) -> RepositoryPythonOrigin:
         return self.repository_python_origin
 
 
-class JobHandle(
-    NamedTuple("_PipelineHandle", [("job_name", str), ("repository_handle", RepositoryHandle)])
-):
+@record_custom
+class JobHandle(IHaveNew):
+    job_name: str
+    repository_handle: RepositoryHandle
+
+    # allow posargs
     def __new__(cls, job_name: str, repository_handle: RepositoryHandle):
-        return super(JobHandle, cls).__new__(
+        return super().__new__(
             cls,
-            check.str_param(job_name, "job_name"),
-            check.inst_param(repository_handle, "repository_handle", RepositoryHandle),
+            job_name=job_name,
+            repository_handle=repository_handle,
         )
 
     def to_string(self):
@@ -73,28 +73,34 @@ class JobHandle(
     def get_python_origin(self):
         return self.repository_handle.get_python_origin().get_job_origin(self.job_name)
 
-    def to_selector(self):
-        return JobSubsetSelector(self.location_name, self.repository_name, self.job_name, None)
+    def to_selector(self) -> JobSubsetSelector:
+        return JobSubsetSelector(
+            location_name=self.location_name,
+            repository_name=self.repository_name,
+            job_name=self.job_name,
+            op_selection=None,
+        )
 
 
-class InstigatorHandle(
-    NamedTuple(
-        "_InstigatorHandle", [("instigator_name", str), ("repository_handle", RepositoryHandle)]
-    )
-):
+@record_custom
+class InstigatorHandle(IHaveNew):
+    instigator_name: str
+    repository_handle: RepositoryHandle
+
+    # allow posargs
     def __new__(cls, instigator_name: str, repository_handle: RepositoryHandle):
-        return super(InstigatorHandle, cls).__new__(
+        return super().__new__(
             cls,
-            check.str_param(instigator_name, "instigator_name"),
-            check.inst_param(repository_handle, "repository_handle", RepositoryHandle),
+            instigator_name=instigator_name,
+            repository_handle=repository_handle,
         )
 
     @property
-    def repository_name(self):
+    def repository_name(self) -> str:
         return self.repository_handle.repository_name
 
     @property
-    def location_name(self):
+    def location_name(self) -> str:
         return self.repository_handle.location_name
 
     def get_remote_origin(self):
@@ -103,18 +109,10 @@ class InstigatorHandle(
         )
 
 
-class PartitionSetHandle(
-    NamedTuple(
-        "_PartitionSetHandle",
-        [("partition_set_name", str), ("repository_handle", RepositoryHandle)],
-    )
-):
-    def __new__(cls, partition_set_name: str, repository_handle: RepositoryHandle):
-        return super(PartitionSetHandle, cls).__new__(
-            cls,
-            check.str_param(partition_set_name, "partition_set_name"),
-            check.inst_param(repository_handle, "repository_handle", RepositoryHandle),
-        )
+@record
+class PartitionSetHandle:
+    partition_set_name: str
+    repository_handle: RepositoryHandle
 
     @property
     def repository_name(self):
