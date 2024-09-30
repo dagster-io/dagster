0c769e6d6fe0bf56f3919971071d2626768d8653
bengotow <bgotow@elementl.com>
2024-09-30 13:39:20 -0500

    Tests to separate file

--- js_modules/dagster-ui/packages/ui-core/src/asset-data/__tests__/AssetLiveDataProvider.test.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-data/__tests__/AssetLiveDataProvider.test.tsx
@@ -5,15 +5,7 @@ import {act, render, waitFor} from '@testing-library/react';
 import {GraphQLError} from 'graphql/error';
 
 import {buildMockedAssetGraphLiveQuery} from './util';
-import {shouldDisplayRunFailure} from '../../asset-graph/Utils';
-import {
-  AssetKey,
-  AssetKeyInput,
-  RunStatus,
-  buildAssetKey,
-  buildMaterializationEvent,
-  buildRun,
-} from '../../graphql/types';
+import {AssetKey, AssetKeyInput, buildAssetKey} from '../../graphql/types';
 import {LiveDataThreadID} from '../../live-data-provider/LiveDataThread';
 import {BATCH_SIZE, SUBSCRIPTION_IDLE_POLL_RATE} from '../../live-data-provider/util';
 import {getMockResultFn} from '../../testing/mocking';
@@ -526,41 +518,6 @@ describe('AssetLiveDataProvider', () => {
     await waitFor(() => {
       expect(resultFn3).toHaveBeenCalled();
       expect(resultFn4).toHaveBeenCalled();
-    });
-  });
-
-  describe('shouldDisplayRunFailure', () => {
-    it('should return false if the latest materialization is from the latest run', () => {
-      expect(
-        shouldDisplayRunFailure(
-          buildRun({id: '1', status: RunStatus.FAILURE, endTime: 1673301346}),
-          buildMaterializationEvent({runId: '1', timestamp: `1673300346000`}),
-        ),
-      ).toEqual(false);
-    });
-    it('should return false if the latest materialization is newer than the failure (manually reported event case)', () => {
-      expect(
-        shouldDisplayRunFailure(
-          buildRun({id: '1', status: RunStatus.FAILURE, endTime: 1673301346}),
-          buildMaterializationEvent({runId: undefined, timestamp: `1674400000000`}),
-        ),
-      ).toEqual(false);
-    });
-    it('should return false if the run succeeded', () => {
-      expect(
-        shouldDisplayRunFailure(
-          buildRun({id: '2', status: RunStatus.SUCCESS, endTime: 1673301346}),
-          buildMaterializationEvent({runId: '1', timestamp: `1673300346000`}),
-        ),
-      ).toEqual(false);
-    });
-    it('should return true if the latest run status is failed and other conditions are not met', () => {
-      expect(
-        shouldDisplayRunFailure(
-          buildRun({id: '2', status: RunStatus.FAILURE, endTime: 1673301346}),
-          buildMaterializationEvent({runId: '1', timestamp: `1673300000000`}),
-        ),
-      ).toEqual(true);
     });
   });
 });
