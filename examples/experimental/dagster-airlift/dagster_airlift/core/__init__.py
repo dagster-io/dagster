de5e370ddc50b2374ee7c895a3d9276cfff9cc18
Nick Schrock <schrockn@dagsterlabs.com>
2024-10-02 17:25:35 -0400

    Deletes specs_from_task (#25009)
    ## Summary & Motivation
    
    ## How I Tested These Changes
    
    ## Changelog
    
    Insert changelog entry or "NOCHANGELOG" here.
    
    - [ ] `NEW` _(added new feature or capability)_
    - [ ] `BUGFIX` _(fixed a bug)_
    - [ ] `DOCS` _(added or updated documentation)_

--- examples/experimental/dagster-airlift/dagster_airlift/core/__init__.py
+++ examples/experimental/dagster-airlift/dagster_airlift/core/__init__.py
@@ -4,7 +4,6 @@ from .dag_defs import (
     dag_defs as dag_defs,
     task_defs as task_defs,
 )
-from .defs_builders import specs_from_task as specs_from_task
 from .load_defs import (
     AirflowInstance as AirflowInstance,
     build_defs_from_airflow_instance as build_defs_from_airflow_instance,
