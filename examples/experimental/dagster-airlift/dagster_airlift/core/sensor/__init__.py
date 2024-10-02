5249f96b7e17085490123c53046253c89f26d064
Chris DeCarolis <cdguitar817@gmail.com>
2024-10-02 14:00:21 -0700

    [dagster-airlift][sensor-pluggability] rename file

--- examples/experimental/dagster-airlift/dagster_airlift/core/sensor/__init__.py
+++ examples/experimental/dagster-airlift/dagster_airlift/core/sensor/__init__.py
@@ -1,4 +1,4 @@
-from .builder import (
+from .sensor_builder import (
     DEFAULT_AIRFLOW_SENSOR_INTERVAL_SECONDS as DEFAULT_AIRFLOW_SENSOR_INTERVAL_SECONDS,
     AirflowPollingSensorCursor as AirflowPollingSensorCursor,
     build_airflow_polling_sensor_defs as build_airflow_polling_sensor_defs,
