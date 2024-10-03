9e1bd40f02d54563eda13f0cb488e88d01a05888
Chris DeCarolis <cdguitar817@gmail.com>
2024-10-03 15:32:06 -0700

    [dagster-airlift] top level sensor builder

--- examples/experimental/dagster-airlift/dagster_airlift/core/airflow_defs_data.py
+++ examples/experimental/dagster-airlift/dagster_airlift/core/airflow_defs_data.py
@@ -24,7 +24,7 @@ from dagster_airlift.core.utils import get_metadata_key, is_mapped_asset_spec, t
 @record
 class AirflowDefinitionsData:
     airflow_instance: AirflowInstance
-    resolved_airflow_defs: Definitions
+    mapped_defs: Definitions
 
     @property
     def instance_name(self) -> str:
@@ -32,9 +32,7 @@ class AirflowDefinitionsData:
 
     @cached_property
     def mapping_info(self) -> AirliftMetadataMappingInfo:
-        return AirliftMetadataMappingInfo(
-            asset_specs=list(self.resolved_airflow_defs.get_all_asset_specs())
-        )
+        return AirliftMetadataMappingInfo(asset_specs=list(self.mapped_defs.get_all_asset_specs()))
 
     def task_ids_in_dag(self, dag_id: str) -> Set[str]:
         return self.mapping_info.task_id_map[dag_id]
@@ -45,15 +43,15 @@ class AirflowDefinitionsData:
         automapped_metadata_key = regular_metadata_key + "/full_automapped_dags"
         check.invariant(
             any(
-                metadata_key in self.resolved_airflow_defs.metadata
+                metadata_key in self.mapped_defs.metadata
                 for metadata_key in [regular_metadata_key, automapped_metadata_key]
             ),
             "Expected at least one of the possible metadata keys to be present",
         )
         serialized_data_str = (
-            self.resolved_airflow_defs.metadata[regular_metadata_key].value
-            if regular_metadata_key in self.resolved_airflow_defs.metadata
-            else self.resolved_airflow_defs.metadata[automapped_metadata_key].value
+            self.mapped_defs.metadata[regular_metadata_key].value
+            if regular_metadata_key in self.mapped_defs.metadata
+            else self.mapped_defs.metadata[automapped_metadata_key].value
         )
         return deserialize_value(
             cast(str, serialized_data_str), as_type=SerializedAirflowDefinitionsData
@@ -66,7 +64,7 @@ class AirflowDefinitionsData:
     @cached_property
     def asset_keys_per_task_handle(self) -> Mapping[TaskHandle, AbstractSet[AssetKey]]:
         asset_keys_per_handle = defaultdict(set)
-        for spec in self.resolved_airflow_defs.get_all_asset_specs():
+        for spec in self.mapped_defs.get_all_asset_specs():
             if is_mapped_asset_spec(spec):
                 task_handles = task_handles_for_spec(spec)
                 for task_handle in task_handles:
@@ -76,7 +74,7 @@ class AirflowDefinitionsData:
     @cached_property
     def asset_key_per_dag(self) -> Mapping[str, AssetKey]:
         dag_id_to_asset_key = {}
-        for spec in self.resolved_airflow_defs.get_all_asset_specs():
+        for spec in self.mapped_defs.get_all_asset_specs():
             if STANDALONE_DAG_ID_METADATA_KEY in spec.metadata:
                 dag_id = spec.metadata[STANDALONE_DAG_ID_METADATA_KEY]
                 dag_id_to_asset_key[dag_id] = spec.key
