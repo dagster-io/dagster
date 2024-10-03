4cc156a43d75c63163526ea0b60f9b7cd5b17888
Alex Noonan <a.noonan@dagsterlabs.com>
2024-10-03 15:38:10 -0400

    removed dagster project files to just have code snippet

--- examples/docs_beta_snippets/docs_beta_snippets/guides/tutorials/etl_tutorial/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="etl_tutorial",
-    packages=find_packages(exclude=["etl_tutorial_tests"]),
-    install_requires=["dagster", "dagster-cloud", "duckdb"],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
-)
