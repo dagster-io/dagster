cc5e8b6621aba620ede8c9ce12224b8f61ca0ce1
Colton Padden <colton@dagsterlabs.com>
2024-10-03 12:02:24 -0400

    [dagster-aws] add `since_last_modified` parameter to get_s3_keys

--- python_modules/libraries/dagster-aws/dagster_aws/s3/sensor.py
+++ python_modules/libraries/dagster-aws/dagster_aws/s3/sensor.py
@@ -1,43 +1,98 @@
-from typing import Optional
+from collections import namedtuple
+from datetime import datetime
+from typing import List, Optional
 
 import boto3
 import dagster._check as check
+from dagster._annotations import deprecated
+
+Object = namedtuple("Object", ["key", "last_modified"])
 
 
 class ClientException(Exception):
     pass
 
 
+def get_objects(
+    bucket: str,
+    prefix: str = "",
+    since_key: Optional[str] = None,
+    since_last_modified: Optional[str] = None,
+    client=None,
+) -> List[Object]:
+    """Retrieves a list of object keys in S3 for a given `bucket`, `prefix`, and filter option.
+
+    Args:
+        bucket (str): s3 bucket
+        prefix (str): s3 object prefix
+        since_key (Optional[str]): retrieve objects after the modified date of this key
+        since_last_modified (Optional[str]): retrieve objects after this timestamp
+        client (Optional[boto3.Client]): s3 client
+
+    Returns:
+        List of object keys in S3.
+
+    """
+    check.str_param(bucket, "bucket")
+    check.str_param(prefix, "prefix")
+    check.opt_str_param(since_key, "since_key")
+    check.opt_str_param(since_last_modified, "since_last_modified")
+
+    if not client:
+        raise ClientException("Failed to initialize s3 client")
+
+    paginator = client.get_paginator("list_objects_v2")
+    page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix, Delimiter="")
+
+    objects = []
+    for page in page_iterator:
+        contents = page.get("Contents", [])
+        objects.extend([Object(obj.get("Key"), obj.get("LastModified")) for obj in contents])
+
+    if since_key and not any(obj.key == since_key for obj in objects):
+        raise Exception("Provided `since_key` is not present in list of objects")
+
+    sorted_objects = [obj for obj in sorted(objects, key=lambda x: x.last_modified)]
+
+    if since_key and since_key:
+        for idx, obj in enumerate(sorted_objects):
+            if obj.key == since_key:
+                return sorted_objects[idx + 1 :]
+
+    if since_last_modified:
+        since_last_modified_dt = datetime.fromisoformat(since_last_modified)
+        for idx, obj in enumerate(sorted_objects):
+            if obj.last_modified >= since_last_modified_dt:
+                return sorted_objects[idx + 1 :]
+
+    return sorted_objects
+
+
+@deprecated(breaking_version="2.0", additional_warn_text="Use get_objects instead.")
 def get_s3_keys(
     bucket: str,
     prefix: str = "",
     since_key: Optional[str] = None,
     s3_session: Optional[boto3.Session] = None,
-):
-    check.str_param(bucket, "bucket")
-    check.str_param(prefix, "prefix")
-    check.opt_str_param(since_key, "since_key")
+) -> List[str]:
+    """Retrieves a list of object keys in S3 for a given `bucket`, `prefix`, and filter option.
 
-    if not s3_session:
-        s3_session = boto3.client("s3", use_ssl=True, verify=True)
+    Note: when using the `since_key` it is possible to miss records if that key has been modified,
+    as sorting is done by the `LastModified` property of the S3 object. For more information, see
+    the following GitHub issue:
 
-    if not s3_session:
-        raise ClientException("Failed to initialize s3 client")
+        https://github.com/dagster-io/dagster/issues/22892
 
-    paginator = s3_session.get_paginator("list_objects_v2")  # type: ignore
-    page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix)
+    Args:
+        bucket (str): s3 bucket
+        prefix (str): s3 object prefix
+        since_key (Optional[str]): retrieve objects after the modified date of this key
+        since_last_modified (Optional[str]): retrieve objects after this timestamp
+        s3_session (Optional[boto3.Session]): s3 client
 
-    contents = []
-    for page in page_iterator:
-        contents.extend(page.get("Contents", []))
+    Returns:
+        List of object keys in S3.
 
-    sorted_keys = [obj["Key"] for obj in sorted(contents, key=lambda x: x["LastModified"])]
-
-    if not since_key or since_key not in sorted_keys:
-        return sorted_keys
-
-    for idx, key in enumerate(sorted_keys):
-        if key == since_key:
-            return sorted_keys[idx + 1 :]
-
-    return []
+    """
+    objects = get_objects(bucket=bucket, prefix=prefix, since_key=since_key, client=s3_session)
+    return [obj.key for obj in objects]
