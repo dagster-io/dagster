7cdc21ccb0325b2a24c0d61d4b74252e8dc0313b
JamieDeMaria <jamie@dagsterlabs.com>
2024-10-04 10:32:25 -0400

    wip

--- js_modules/dagster-ui/packages/ui-core/src/runs/RunsFeedTabs.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/runs/RunsFeedTabs.tsx
@@ -6,7 +6,7 @@ import styled, {css} from 'styled-components';
 
 import {failedStatuses, inProgressStatuses, queuedStatuses} from './RunStatuses';
 import {getRunFeedPath} from './RunsFeedUtils';
-import {runsPathWithFilters, useQueryPersistedRunFilters} from './RunsFilterInput';
+import {runsPathWithFilters, useQueryPersistedRunFilters, RunFilterTokenType} from './RunsFilterInput';
 import {RunFeedTabsCountQuery, RunFeedTabsCountQueryVariables} from './types/RunsFeedTabs.types';
 import {gql, useQuery} from '../apollo-client';
 import {RunStatus, RunsFilter} from '../graphql/types';
@@ -53,13 +53,26 @@ export const useRunsFeedTabs = (filter: RunsFilter = {}, includeRunsFromBackfill
           : null,
     };
   }, [countData]);
-
-  const [filterTokens] = useQueryPersistedRunFilters();
+  const enabledFilters: RunFilterTokenType[] = [
+    'status',
+    'tag',
+    'id',
+    'created_date_before',
+    'created_date_after',
+    'pipeline',
+    'partition',
+    'job',
+    'snapshotId',
+    'backfill'
+  ];
+  const [filterTokens] = useQueryPersistedRunFilters(enabledFilters);
   const selectedTab = useSelectedRunsFeedTab(filterTokens);
 
   useDocumentTitle(getDocumentTitle(selectedTab));
 
   const urlForStatus = (statuses: RunStatus[]) => {
+    console.log('filter tokens', filterTokens)
+
     const tokensMinusStatus = filterTokens.filter((token) => token.token !== 'status');
     const statusTokens = statuses.map((status) => ({token: 'status' as const, value: status}));
     return runsPathWithFilters([...statusTokens, ...tokensMinusStatus], getRunFeedPath(), includeRunsFromBackfills);
