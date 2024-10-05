35897a45c94f87565bfd939cfdbd4d7f8d39ece0
Nick Schrock <schrockn@dagsterlabs.com>
2024-10-04 22:31:06 -0400

    [dagster-airlift] Move standalone.py away from Definitions-centric appraoch

--- examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example/dagster_defs/stages/standalone.py
+++ examples/experimental/dagster-airlift/examples/tutorial-example/tutorial_example/dagster_defs/stages/standalone.py
@@ -1,13 +1,14 @@
 import os
 from pathlib import Path
 
-from dagster import AssetSelection, AssetSpec, Definitions, ScheduleDefinition, multi_asset
-from dagster_airlift.dbt import dbt_defs
-from dagster_dbt import DbtProject
+from dagster import AssetSpec, Definitions, ScheduleDefinition, multi_asset
+from dagster._core.definitions.assets import AssetsDefinition
 
 # Code also invoked from Airflow
 from tutorial_example.shared.export_duckdb_to_csv import ExportDuckDbToCsvArgs, export_duckdb_to_csv
 from tutorial_example.shared.load_csv_to_duckdb import LoadCsvToDuckDbArgs, load_csv_to_duckdb
+
+from .jaffle_shop import jaffle_shop_assets, jaffle_shop_resource
 
 
 def dbt_project_path() -> Path:
@@ -20,25 +21,27 @@ def airflow_dags_path() -> Path:
     return Path(os.environ["TUTORIAL_EXAMPLE_DIR"]) / "tutorial_example" / "airflow_dags"
 
 
-def load_csv_to_duckdb_defs(spec: AssetSpec, args: LoadCsvToDuckDbArgs) -> Definitions:
+def load_csv_to_duckdb_assets_def(spec: AssetSpec, args: LoadCsvToDuckDbArgs) -> AssetsDefinition:
     @multi_asset(name=f"load_{args.table_name}", specs=[spec])
     def _multi_asset() -> None:
         load_csv_to_duckdb(args)
 
-    return Definitions(assets=[_multi_asset])
+    return _multi_asset
 
 
-def export_duckdb_to_csv_defs(spec: AssetSpec, args: ExportDuckDbToCsvArgs) -> Definitions:
+def export_duckdb_to_csv_assets_def(
+    spec: AssetSpec, args: ExportDuckDbToCsvArgs
+) -> AssetsDefinition:
     @multi_asset(name=f"export_{args.table_name}", specs=[spec])
     def _multi_asset() -> None:
         export_duckdb_to_csv(args)
 
-    return Definitions(assets=[_multi_asset])
+    return _multi_asset
 
 
-def rebuild_customers_list_defs() -> Definitions:
-    merged_defs = Definitions.merge(
-        load_csv_to_duckdb_defs(
+defs = Definitions(
+    assets=[
+        load_csv_to_duckdb_assets_def(
             AssetSpec(key=["raw_data", "raw_customers"]),
             LoadCsvToDuckDbArgs(
                 table_name="raw_customers",
@@ -49,11 +52,8 @@ def rebuild_customers_list_defs() -> Definitions:
                 duckdb_database_name="jaffle_shop",
             ),
         ),
-        dbt_defs(
-            manifest=dbt_project_path() / "target" / "manifest.json",
-            project=DbtProject(dbt_project_path().absolute()),
-        ),
-        export_duckdb_to_csv_defs(
+        jaffle_shop_assets,
+        export_duckdb_to_csv_assets_def(
             AssetSpec(key="customers_csv", deps=["customers"]),
             ExportDuckDbToCsvArgs(
                 table_name="customers",
@@ -62,18 +62,13 @@ def rebuild_customers_list_defs() -> Definitions:
                 duckdb_database_name="jaffle_shop",
             ),
         ),
-    )
-
-    rebuild_customers_list_schedule = ScheduleDefinition(
-        name="rebuild_customers_list_schedule",
-        target=AssetSelection.assets(*merged_defs.get_asset_graph().all_asset_keys),
-        cron_schedule="0 0 * * *",
-    )
-
-    return Definitions.merge(
-        merged_defs,
-        Definitions(schedules=[rebuild_customers_list_schedule]),
-    )
-
-
-defs = rebuild_customers_list_defs()
+    ],
+    resources={"dbt": jaffle_shop_resource()},
+    schedules=[
+        ScheduleDefinition(
+            name="rebuild_customers_list_schedule",
+            target="*",
+            cron_schedule="0 0 * * *",
+        )
+    ],
+)
