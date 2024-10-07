505af6c0bbdfde211b0ece334af905db4ced09fe
Sandy Ryza <sandy@elementl.com>
2024-10-07 12:39:54 -0700

    add dagster-powerbi/asset-type tag (#24801)
    ## Summary & Motivation
    
    This can be used inside `map_asset_spec` to control which assets to
    customize.
    
    ## How I Tested These Changes
    
    ## Changelog
    
    NOCHANGELOG
    
    - [ ] `NEW` _(added new feature or capability)_
    - [ ] `BUGFIX` _(fixed a bug)_
    - [ ] `DOCS` _(added or updated documentation)_

--- python_modules/libraries/dagster-powerbi/dagster_powerbi_tests/test_translator.py
+++ python_modules/libraries/dagster-powerbi/dagster_powerbi_tests/test_translator.py
@@ -20,6 +20,7 @@ def test_translator_dashboard_spec(workspace_data: PowerBIWorkspaceData) -> None
             "https://app.powerbi.com/groups/a2122b8f-d7e1-42e8-be2b-a5e636ca3221/dashboards/efee0b80-4511-42e1-8ee0-2544fd44e122"
         )
     }
+    assert asset_spec.tags == {"dagster-powerbi/asset_type": "dashboard"}
 
 
 def test_translator_report_spec(workspace_data: PowerBIWorkspaceData) -> None:
@@ -37,6 +38,7 @@ def test_translator_report_spec(workspace_data: PowerBIWorkspaceData) -> None:
             "https://app.powerbi.com/groups/a2122b8f-d7e1-42e8-be2b-a5e636ca3221/reports/8b7f815d-4e64-40dd-993c-cfa4fb12edee"
         )
     }
+    assert asset_spec.tags == {"dagster-powerbi/asset_type": "report"}
 
 
 def test_translator_semantic_model(workspace_data: PowerBIWorkspaceData) -> None:
@@ -55,6 +57,7 @@ def test_translator_semantic_model(workspace_data: PowerBIWorkspaceData) -> None
             "https://app.powerbi.com/groups/a2122b8f-d7e1-42e8-be2b-a5e636ca3221/datasets/8e9c85a1-7b33-4223-9590-76bde70f9a20"
         )
     }
+    assert asset_spec.tags == {"dagster-powerbi/asset_type": "semantic_model"}
 
 
 class MyCustomTranslator(DagsterPowerBITranslator):
