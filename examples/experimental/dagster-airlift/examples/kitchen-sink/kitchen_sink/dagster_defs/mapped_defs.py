f03a596a654262a743434faed938d262f235b982
Chris DeCarolis <cdguitar817@gmail.com>
2024-10-07 13:55:39 -0700

    [dagster-airlift] master

--- examples/experimental/dagster-airlift/examples/kitchen-sink/kitchen_sink/dagster_defs/mapped_defs.py
+++ examples/experimental/dagster-airlift/examples/kitchen-sink/kitchen_sink/dagster_defs/mapped_defs.py
@@ -11,6 +11,11 @@ def print_asset() -> None:
     print("Hello, world!")
 
 
+@asset
+def another_print_asset() -> None:
+    print("Hello, world!")
+
+
 @asset(description="Asset one is materialized by multiple airflow tasks")
 def asset_one() -> None:
     # ruff: noqa: T201
@@ -24,6 +29,7 @@ def build_mapped_defs() -> Definitions:
             dag_defs(
                 "print_dag",
                 task_defs("print_task", Definitions(assets=[print_asset])),
+                task_defs("downstream_print_task", Definitions(assets=[another_print_asset])),
             ),
             targeted_by_multiple_tasks(
                 Definitions([asset_one]),
