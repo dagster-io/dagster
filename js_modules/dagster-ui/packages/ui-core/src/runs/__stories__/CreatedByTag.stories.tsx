d7266145aee4e53044d38dcb0f4bc2a54f2b4439
JamieDeMaria <jamie@dagsterlabs.com>
2024-10-15 09:19:17 -0400

    fix test

--- js_modules/dagster-ui/packages/ui-core/src/runs/__stories__/CreatedByTag.stories.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/runs/__stories__/CreatedByTag.stories.tsx
@@ -4,7 +4,6 @@ import {Meta} from '@storybook/react';
 import {buildLaunchedBy, buildPipelineTag} from '../../graphql/types';
 import {CreatedByTag} from '../CreatedByTag';
 import {DagsterTag} from '../RunTag';
-import {RunTagsFragment} from '../types/RunTagsFragment.types';
 import {LaunchedByFragment} from '../types/launchedByFragment.types';
 
 // eslint-disable-next-line import/no-default-export
@@ -21,10 +20,15 @@ export const Default = () => {
           <td>User launched</td>
           <td>
             <CreatedByTag
-              launchedBy={buildLaunchedBy({kind: "user", tag: buildPipelineTag({
-                key: DagsterTag.User,
-                value: 'foo@dagsterlabs.com',
-              })}) as LaunchedByFragment}
+              launchedBy={
+                buildLaunchedBy({
+                  kind: 'user',
+                  tag: buildPipelineTag({
+                    key: DagsterTag.User,
+                    value: 'foo@dagsterlabs.com',
+                  }),
+                }) as LaunchedByFragment
+              }
             />
           </td>
         </tr>
@@ -32,10 +36,15 @@ export const Default = () => {
           <td>Schedule</td>
           <td>
             <CreatedByTag
-              launchedBy={buildLaunchedBy({kind: "schedule", tag: buildPipelineTag({
-                key: DagsterTag.ScheduleName,
-                value: 'my_cool_schedule',
-              })}) as LaunchedByFragment}
+              launchedBy={
+                buildLaunchedBy({
+                  kind: 'schedule',
+                  tag: buildPipelineTag({
+                    key: DagsterTag.ScheduleName,
+                    value: 'my_cool_schedule',
+                  }),
+                }) as LaunchedByFragment
+              }
             />
           </td>
         </tr>
@@ -43,10 +52,15 @@ export const Default = () => {
           <td>Sensor</td>
           <td>
             <CreatedByTag
-              launchedBy={buildLaunchedBy({kind: "sensor", tag: buildPipelineTag({
-                key: DagsterTag.SensorName,
-                value: 'my_cool_sensor',
-              })}) as LaunchedByFragment}
+              launchedBy={
+                buildLaunchedBy({
+                  kind: 'sensor',
+                  tag: buildPipelineTag({
+                    key: DagsterTag.SensorName,
+                    value: 'my_cool_sensor',
+                  }),
+                }) as LaunchedByFragment
+              }
             />
           </td>
         </tr>
@@ -54,10 +68,15 @@ export const Default = () => {
           <td>Auto-materialize</td>
           <td>
             <CreatedByTag
-              launchedBy={buildLaunchedBy({kind: "auto-materialize", tag: buildPipelineTag({
-                key: DagsterTag.Automaterialize,
-                value: 'auto',
-              })}) as LaunchedByFragment}
+              launchedBy={
+                buildLaunchedBy({
+                  kind: 'auto-materialize',
+                  tag: buildPipelineTag({
+                    key: DagsterTag.Automaterialize,
+                    value: 'auto',
+                  }),
+                }) as LaunchedByFragment
+              }
             />
           </td>
         </tr>
@@ -65,17 +84,29 @@ export const Default = () => {
           <td>Auto-observation</td>
           <td>
             <CreatedByTag
-              launchedBy={buildLaunchedBy({kind: "auto-observe", tag: buildPipelineTag({
-                key: DagsterTag.AutoObserve,
-                value: 'auto',
-              })}) as LaunchedByFragment}
+              launchedBy={
+                buildLaunchedBy({
+                  kind: 'auto-observe',
+                  tag: buildPipelineTag({
+                    key: DagsterTag.AutoObserve,
+                    value: 'auto',
+                  }),
+                }) as LaunchedByFragment
+              }
             />
           </td>
         </tr>
         <tr>
           <td>Manually launched</td>
           <td>
-            <CreatedByTag launchedBy={[] as LaunchedByFragment} />
+            <CreatedByTag
+              launchedBy={
+                buildLaunchedBy({
+                  kind: 'manual',
+                  tag: buildPipelineTag({key: '', value: ''}),
+                }) as LaunchedByFragment
+              }
+            />
           </td>
         </tr>
       </tbody>
