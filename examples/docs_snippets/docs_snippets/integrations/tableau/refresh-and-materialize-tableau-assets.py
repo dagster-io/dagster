b0adf742e20ecc91d7425ede44ee4f3d50687c1e
Alex Noonan <a.noonan@dagsterlabs.com>
2024-10-17 15:13:37 -0400

    updated docs for tableau using dg and pyobject for api references.

--- examples/docs_snippets/docs_snippets/integrations/tableau/refresh-and-materialize-tableau-assets.py
+++ examples/docs_snippets/docs_snippets/integrations/tableau/refresh-and-materialize-tableau-assets.py
@@ -1,14 +1,14 @@
 from dagster_tableau import TableauCloudWorkspace
 
-from dagster import EnvVar
+import dagster as dg
 
 workspace = TableauCloudWorkspace(
-    connected_app_client_id=EnvVar("TABLEAU_CONNECTED_APP_CLIENT_ID"),
-    connected_app_secret_id=EnvVar("TABLEAU_CONNECTED_APP_SECRET_ID"),
-    connected_app_secret_value=EnvVar("TABLEAU_CONNECTED_APP_SECRET_VALUE"),
-    username=EnvVar("TABLEAU_USERNAME"),
-    site_name=EnvVar("TABLEAU_SITE_NAME"),
-    pod_name=EnvVar("TABLEAU_POD_NAME"),
+    connected_app_client_id=dg.EnvVar("TABLEAU_CONNECTED_APP_CLIENT_ID"),
+    connected_app_secret_id=dg.EnvVar("TABLEAU_CONNECTED_APP_SECRET_ID"),
+    connected_app_secret_value=dg.EnvVar("TABLEAU_CONNECTED_APP_SECRET_VALUE"),
+    username=dg.EnvVar("TABLEAU_USERNAME"),
+    site_name=dg.EnvVar("TABLEAU_SITE_NAME"),
+    pod_name=dg.EnvVar("TABLEAU_POD_NAME"),
 )
 
 defs = workspace.build_defs(
