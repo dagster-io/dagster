ca7b2e7c161ca39e4653c526858cd3980e294455
Chris DeCarolis <cdguitar817@gmail.com>
2024-10-17 10:14:18 -0700

    [dagster-airlift] Multi code locations working

---
+++ examples/experimental/dagster-airlift/examples/kitchen-sink/kitchen_sink/dagster_multi_code_locations/first_dag_defs.py
@@ -0,0 +1,17 @@
+from dagster import AssetSpec, Definitions
+from dagster_airlift.core import assets_with_task_mappings, build_defs_from_airflow_instance
+
+from kitchen_sink.airflow_instance import local_airflow_instance
+
+defs = build_defs_from_airflow_instance(
+    airflow_instance=local_airflow_instance(),
+    defs=Definitions(
+        assets=assets_with_task_mappings(
+            dag_id="dag_first_code_location",
+            task_mappings={
+                "task": [AssetSpec(key="dag_first_code_location__asset")],
+            },
+        ),
+    ),
+    dag_selector_fn=lambda dag_info: dag_info.dag_id == "dag_first_code_location",
+)
