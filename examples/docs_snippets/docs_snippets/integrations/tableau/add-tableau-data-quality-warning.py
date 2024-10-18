2f11030b84e6b4a75db57991558fc2fc2da8e828
Colton Padden <colton@dagsterlabs.com>
2024-10-18 13:57:22 -0400

    use dagster as dg

--- examples/docs_snippets/docs_snippets/integrations/tableau/add-tableau-data-quality-warning.py
+++ examples/docs_snippets/docs_snippets/integrations/tableau/add-tableau-data-quality-warning.py
@@ -1,34 +1,28 @@
 from dagster_tableau import TableauCloudWorkspace
 
-from dagster import (
-    Definitions,
-    EnvVar,
-    RunFailureSensorContext,
-    asset,
-    run_failure_sensor,
-)
+import dagster as dg
 
 # Connect to Tableau Cloud using the connected app credentials
 workspace = TableauCloudWorkspace(
-    connected_app_client_id=EnvVar("TABLEAU_CONNECTED_APP_CLIENT_ID"),
-    connected_app_secret_id=EnvVar("TABLEAU_CONNECTED_APP_SECRET_ID"),
-    connected_app_secret_value=EnvVar("TABLEAU_CONNECTED_APP_SECRET_VALUE"),
-    username=EnvVar("TABLEAU_USERNAME"),
-    site_name=EnvVar("TABLEAU_SITE_NAME"),
-    pod_name=EnvVar("TABLEAU_POD_NAME"),
+    connected_app_client_id=dg.EnvVar("TABLEAU_CONNECTED_APP_CLIENT_ID"),
+    connected_app_secret_id=dg.EnvVar("TABLEAU_CONNECTED_APP_SECRET_ID"),
+    connected_app_secret_value=dg.EnvVar("TABLEAU_CONNECTED_APP_SECRET_VALUE"),
+    username=dg.EnvVar("TABLEAU_USERNAME"),
+    site_name=dg.EnvVar("TABLEAU_SITE_NAME"),
+    pod_name=dg.EnvVar("TABLEAU_POD_NAME"),
 )
 
 
-@asset(
+@dg.asset(
     # Define which Tableau data source this upstream asset corresponds to
     metadata={"dagster/tableau_data_source_id": "f5660c7-2b05-4ff0-90ce-3199226956c6"}
 )
 def upstream_asset(): ...
 
 
-@run_failure_sensor
+@dg.run_failure_sensor
 def tableau_run_failure_sensor(
-    context: RunFailureSensorContext, tableau: TableauCloudWorkspace
+    context: dg.RunFailureSensorContext, tableau: TableauCloudWorkspace
 ):
     asset_keys = context.dagster_run.asset_selection or set()
     for asset_key in asset_keys:
@@ -45,10 +39,10 @@ def tableau_run_failure_sensor(
 # We use Definitions.merge to combine the definitions from the Tableau workspace
 # and the Dagster definitions into a single set of definitions to load
 tableau_defs = workspace.build_defs()
-upstream_defs = Definitions(
+upstream_defs = dg.Definitions(
     assets=[upstream_asset],
     sensors=[tableau_run_failure_sensor],
     resources={"tableau": workspace},
 )
 
-defs = Definitions.merge(tableau_defs, upstream_defs)
+defs = dg.Definitions.merge(tableau_defs, upstream_defs)
