b6a284d0bc88ca2e618f12d666aaec7a058e182c
Daniel Gafni <danielgafni16@gmail.com>
2024-10-21 22:26:43 +0200

    [dagster-aws] remove deprecated arguments from PipesGlueClient

--- python_modules/libraries/dagster-aws/dagster_aws/pipes/clients/glue.py
+++ python_modules/libraries/dagster-aws/dagster_aws/pipes/clients/glue.py
@@ -1,11 +1,11 @@
 import time
-from typing import TYPE_CHECKING, Any, Dict, Literal, Mapping, Optional, cast
+from typing import TYPE_CHECKING, Any, Dict, Optional, cast
 
 import boto3
 import dagster._check as check
 from botocore.exceptions import ClientError
 from dagster import PipesClient
-from dagster._annotations import deprecated_param, experimental, public
+from dagster._annotations import experimental, public
 from dagster._core.definitions.resource_annotation import TreatAsResourceParam
 from dagster._core.errors import DagsterExecutionInterruptedError
 from dagster._core.execution.context.compute import OpExecutionContext
@@ -16,26 +16,11 @@ from dagster._core.pipes.client import (
 )
 from dagster._core.pipes.utils import open_pipes_session
 
-from dagster_aws.pipes.context_injectors import PipesS3ContextInjector
 from dagster_aws.pipes.message_readers import PipesCloudWatchLogReader, PipesCloudWatchMessageReader
 
 if TYPE_CHECKING:
     from mypy_boto3_glue.client import GlueClient
     from mypy_boto3_glue.type_defs import StartJobRunRequestRequestTypeDef
-
-RUN_PARAMS_BREAKING_VERSION = "1.9.0"
-
-
-def _param_deprecation_message(param: str) -> str:
-    return f"Set a corresponding (pascal-case) key in `start_job_run_params` argument instead of passing `{param}` directly."
-
-
-def _deprecated_run_param(param: str):
-    return deprecated_param(
-        param=param,
-        breaking_version=RUN_PARAMS_BREAKING_VERSION,
-        additional_warn_text=_param_deprecation_message(param),
-    )
 
 
 @experimental
@@ -71,35 +56,13 @@ class PipesGlueClient(PipesClient, TreatAsResourceParam):
     def _is_dagster_maintained(cls) -> bool:
         return True
 
-    @_deprecated_run_param("job_name")
-    @_deprecated_run_param("arguments")
-    @_deprecated_run_param("job_run_id")
-    @_deprecated_run_param("allocated_capacity")
-    @_deprecated_run_param("timeout")
-    @_deprecated_run_param("max_capacity")
-    @_deprecated_run_param("security_configuration")
-    @_deprecated_run_param("notification_property")
-    @_deprecated_run_param("worker_type")
-    @_deprecated_run_param("number_of_workers")
-    @_deprecated_run_param("execution_class")
     @public
     def run(
         self,
         *,
         context: OpExecutionContext,
-        start_job_run_params: Optional["StartJobRunRequestRequestTypeDef"] = None,
+        start_job_run_params: "StartJobRunRequestRequestTypeDef",
         extras: Optional[Dict[str, Any]] = None,
-        job_name: Optional[str] = None,
-        arguments: Optional[Mapping[str, Any]] = None,
-        job_run_id: Optional[str] = None,
-        allocated_capacity: Optional[int] = None,
-        timeout: Optional[int] = None,
-        max_capacity: Optional[float] = None,
-        security_configuration: Optional[str] = None,
-        notification_property: Optional[Mapping[str, Any]] = None,
-        worker_type: Optional[str] = None,
-        number_of_workers: Optional[int] = None,
-        execution_class: Optional[Literal["FLEX", "STANDARD"]] = None,
     ) -> PipesClientCompletedInvocation:
         """Start a Glue job, enriched with the pipes protocol.
 
@@ -107,72 +70,16 @@ class PipesGlueClient(PipesClient, TreatAsResourceParam):
 
         Args:
             context (OpExecutionContext): The context of the currently executing Dagster op or asset.
-            start_job_run_params (Optional[dict]): Parameters for the ``start_job_run`` boto3 Glue client call.
+            start_job_run_params (Dict): Parameters for the ``start_job_run`` boto3 Glue client call.
             extras (Optional[Dict[str, Any]]): Additional Dagster metadata to pass to the Glue job.
-            job_name (Optional[str]): The name of the job to use.
-            arguments (Optional[Dict[str, str]]): Arguments to pass to the Glue job Command
-            job_run_id (Optional[str]): The ID of the previous job run to retry.
-            allocated_capacity (Optional[int]): The amount of DPUs (Glue data processing units) to allocate to this job.
-            timeout (Optional[int]): The job run timeout in minutes.
-            max_capacity (Optional[float]): The maximum capacity for the Glue job in DPUs (Glue data processing units).
-            security_configuration (Optional[str]): The name of the Security Configuration to be used with this job run.
-            notification_property (Optional[Mapping[str, Any]]): Specifies configuration properties of a job run notification.
-            worker_type (Optional[str]): The type of predefined worker that is allocated when a job runs.
-            number_of_workers (Optional[int]): The number of workers that are allocated when a job runs.
-            execution_class (Optional[Literal["FLEX", "STANDARD"]]): The execution property of a job run.
 
         Returns:
             PipesClientCompletedInvocation: Wrapper containing results reported by the external
             process.
         """
-        arguments = arguments or {}
-
         params = start_job_run_params
 
-        if params is None:
-            if job_name is None:
-                raise ValueError("`JobName` key in `start_job_run_params` argument is required")
-            else:
-                params = {
-                    "JobName": job_name,
-                }
-
-        params = cast("StartJobRunRequestRequestTypeDef", params)
-
-        params["Arguments"] = arguments
-
-        if job_run_id is not None:
-            params["JobRunId"] = job_run_id
-
-        if allocated_capacity is not None:
-            params["AllocatedCapacity"] = allocated_capacity
-
-        if timeout is not None:
-            params["Timeout"] = timeout
-
-        if max_capacity is not None:
-            params["MaxCapacity"] = max_capacity
-
-        if security_configuration is not None:
-            params["SecurityConfiguration"] = security_configuration
-
-        if notification_property is not None:
-            params["NotificationProperty"] = notification_property  # pyright: ignore (reportGeneralTypeIssues)
-
-        if worker_type is not None:
-            params["WorkerType"] = worker_type  # pyright: ignore ()
-
-        if number_of_workers is not None:
-            params["NumberOfWorkers"] = number_of_workers
-
-        if execution_class is not None:
-            params["ExecutionClass"] = execution_class
-
-        # boto3 does not accept None as defaults for some of the parameters
-        # let's make sure they are not passed
-        params = {k: v for k, v in params.items() if v is not None}
-
-        # this variable is used in the code below, let's set it here
+        params["Arguments"] = params.get("Arguments") or {}
         job_name = cast(str, params["JobName"])
 
         with open_pipes_session(
@@ -183,11 +90,10 @@ class PipesGlueClient(PipesClient, TreatAsResourceParam):
         ) as session:
             pipes_args = session.get_bootstrap_cli_arguments()
 
-            if isinstance(self._context_injector, PipesS3ContextInjector):
-                params["Arguments"].update(pipes_args)  # pyright: ignore (reportAttributeAccessIssue)
+            params["Arguments"].update(pipes_args)  # pyright: ignore (reportAttributeAccessIssue)
 
             try:
-                run_id = self._client.start_job_run(**params)["JobRunId"]  # pyright: ignore (reportArgumentType)
+                run_id = self._client.start_job_run(**params)["JobRunId"]
 
             except ClientError as err:
                 context.log.error(
