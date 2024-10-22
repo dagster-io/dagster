75a7f9529ff42c575b9f8992f2e76f690c5ba7cd
JamieDeMaria <jamie@dagsterlabs.com>
2024-10-22 09:39:22 -0400

    cleanup

--- python_modules/dagster/dagster/_core/storage/alembic/versions/044_63d7a8ec641a_add_backfill_id_column_to_runs_table.py
+++ python_modules/dagster/dagster/_core/storage/alembic/versions/044_63d7a8ec641a_add_backfill_id_column_to_runs_table.py
@@ -8,7 +8,6 @@ Create Date: 2024-10-18 15:56:30.002452
 
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy import inspect
 
 # revision identifiers, used by Alembic.
 revision = "63d7a8ec641a"
@@ -18,22 +17,19 @@ depends_on = None
 
 
 def upgrade():
-    inspector = inspect(op.get_bind())
+    inspector = sa.inspect(op.get_bind())
     has_tables = inspector.get_table_names()
 
     if "runs" in has_tables:
         columns = [x.get("name") for x in inspector.get_columns("runs")]
-        with op.batch_alter_table("runs") as batch_op:
-            if "backfill_id" not in columns:
-                batch_op.add_column(sa.Column("backfill_id", sa.String(255), nullable=True))
+        if "backfill_id" not in columns:
+            op.add_column(sa.Column("backfill_id", sa.String(255), nullable=True))
 
 
 def downgrade():
-    inspector = inspect(op.get_bind())
+    inspector = sa.inspect(op.get_bind())
     has_tables = inspector.get_table_names()
     if "runs" in has_tables:
         columns = [x.get("name") for x in inspector.get_columns("runs")]
-
-        with op.batch_alter_table("runs") as batch_op:
-            if "backfill_id" in columns:
-                batch_op.drop_column("backfill_id")
+        if "backfill_id" in columns:
+            op.drop_column("backfill_id")
