015f898ab6ed47b567816a8b66c6925da60bc925
Brian Tu <brianptu2@gmail.com>
2024-10-22 18:15:41 -0400

    Make automation condition evaluator async (#25014)
    ## Summary & Motivation
    Start to making the automation condition evaluator evaluate asynchronously. This won't reduce any latency yet, but will be useful once we can actually batch database queries instead of doing `AssetRecord.blocking_get`.
    
    ## How I Tested These Changes
    Existing tests should pass

--- python_modules/dagster/dagster/_core/definitions/declarative_automation/automation_condition_evaluator.py
+++ python_modules/dagster/dagster/_core/definitions/declarative_automation/automation_condition_evaluator.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 import logging
 from collections import defaultdict
@@ -102,15 +103,18 @@ class AutomationConditionEvaluator:
         self.logger.info("Done prefetching asset records.")
 
     def evaluate(self) -> Tuple[Sequence[AutomationResult], Sequence[EntitySubset[EntityKey]]]:
+        return asyncio.run(self.async_evaluate())
+
+    async def async_evaluate(
+        self,
+    ) -> Tuple[Sequence[AutomationResult], Sequence[EntitySubset[EntityKey]]]:
         self.prefetch()
         num_conditions = len(self.entity_keys)
         num_evaluated = 0
-        for entity_key in self.asset_graph.toposorted_entity_keys:
-            if entity_key not in self.entity_keys:
-                continue
 
+        async def _evaluate_entity_async(entity_key: EntityKey, offset: int):
             self.logger.debug(
-                f"Evaluating {entity_key.to_user_string()} ({num_evaluated+1}/{num_conditions})"
+                f"Evaluating {entity_key.to_user_string()} ({num_evaluated+offset}/{num_conditions})"
             )
 
             try:
@@ -132,7 +136,16 @@ class AutomationConditionEvaluator:
                 f"requested ({requested_str}) "
                 f"({format(result.end_timestamp - result.start_timestamp, '.3f')} seconds)"
             )
-            num_evaluated += 1
+
+        for topo_level in self.asset_graph.toposorted_entity_keys_by_level:
+            coroutines = [
+                _evaluate_entity_async(entity_key, offset)
+                for offset, entity_key in enumerate(topo_level)
+                if entity_key in self.entity_keys
+            ]
+            await asyncio.gather(*coroutines)
+            num_evaluated += len(coroutines)
+
         return list(self.current_results_by_key.values()), [
             v for v in self.request_subsets_by_key.values() if not v.is_empty
         ]
