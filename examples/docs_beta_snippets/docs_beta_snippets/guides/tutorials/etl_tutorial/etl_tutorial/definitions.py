6b079056e35834cf763b48be24f45b5f24d28c39
OwenKephart <owen@elementl.com>
2024-10-24 16:01:40 -0700

    [1.9] Update references to AutoMaterializePolicy in docs (#25520)
    ## Summary & Motivation
    
    Removes / updates any references to AutoMaterializePolicy in the docs
    
    ## How I Tested These Changes
    
    ## Changelog
    
    > Insert changelog entry or delete this section.

--- examples/docs_beta_snippets/docs_beta_snippets/guides/tutorials/etl_tutorial/etl_tutorial/definitions.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/tutorials/etl_tutorial/etl_tutorial/definitions.py
@@ -176,7 +176,7 @@ monthly_partition = dg.MonthlyPartitionsDefinition(start_date="2024-01-01")
     compute_kind="duckdb",
     group_name="analysis",
     deps=[joined_data],
-    auto_materialize_policy=dg.AutoMaterializePolicy.eager(),
+    automation_condition=dg.AutomationCondition.eager(),
 )
 def monthly_sales_performance(
     context: dg.AssetExecutionContext, duckdb: DuckDBResource
@@ -237,7 +237,7 @@ product_category_partition = dg.StaticPartitionsDefinition(
     partitions_def=product_category_partition,
     group_name="analysis",
     compute_kind="duckdb",
-    auto_materialize_policy=dg.AutoMaterializePolicy.eager(),
+    automation_condition=dg.AutomationCondition.eager(),
 )
 def product_performance(context: dg.AssetExecutionContext, duckdb: DuckDBResource):
     product_category_str = context.partition_key
