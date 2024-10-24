b30cac922759160ed266c6c93e32f222e39c0cb6
benpankow <ben@dagsterlabs.com>
2024-10-24 10:07:50 -0700

    fix

--- python_modules/libraries/dagster-sigma/setup.py
+++ python_modules/libraries/dagster-sigma/setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def get_version() -> str:
     version: Dict[str, str] = {}
-    with open(Path(__file__).parent / "dagster_looker/version.py", encoding="utf8") as fp:
+    with open(Path(__file__).parent / "dagster_sigma/version.py", encoding="utf8") as fp:
         exec(fp.read(), version)
 
     return version["__version__"]
