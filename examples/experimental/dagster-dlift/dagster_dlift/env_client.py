04fdd3d03982710814f5eeb298b2cbfc9fef52a4
Chris DeCarolis <cdguitar817@gmail.com>
2024-10-23 21:20:16 -0700

    [dagster-dlift] runtime event translation

---
+++ examples/experimental/dagster-dlift/dagster_dlift/env_client.py
@@ -0,0 +1,56 @@
+from typing import Any, Iterator, Mapping, Sequence, Union
+
+from dagster import AssetCheckResult, AssetMaterialization
+from dagster._record import record
+
+from dagster_dlift.client import UnscopedDbtCloudClient
+from dagster_dlift.translator import DagsterDbtCloudTranslator, DbtCloudProjectEnvironmentData
+
+
+@record
+class DbtCloudJobRun:
+    """Represents a dbt Cloud job run."""
+
+    job_id: int
+    run_id: int
+    client: UnscopedDbtCloudClient
+    translator: DagsterDbtCloudTranslator
+    env_data: DbtCloudProjectEnvironmentData
+
+    def wait_for_success(self) -> int:
+        return self.client.poll_for_run_completion(self.run_id)
+
+    def get_run_results(self) -> Mapping[str, Any]:
+        return self.client.get_run_results_json(self.run_id)
+
+    def get_asset_events(
+        self,
+    ) -> Iterator[Union[AssetMaterialization, AssetCheckResult]]:
+        self.wait_for_success()
+        run_results = self.get_run_results()
+        asset_events = []
+        for result in run_results["results"]:
+            data = self.env_data.get_from_unique_id(result["unique_id"])
+            spec = self.translator.get_spec(data)
+            asset_events.append(self.translator.get_asset_event(spec, result))
+        yield from asset_events
+
+
+@record
+class EnvScopedDbtCloudClient:
+    dbt_client: UnscopedDbtCloudClient
+    env_data: DbtCloudProjectEnvironmentData
+    translator: DagsterDbtCloudTranslator
+
+    def cli(self, args: Sequence[str]) -> DbtCloudJobRun:
+        """Run a dbt cli command with the dbt Cloud client."""
+        response = self.dbt_client.trigger_job(
+            self.env_data.job_id, steps=[" ".join(["dbt", *args])]
+        )
+        return DbtCloudJobRun(
+            job_id=self.env_data.job_id,
+            run_id=response["data"]["id"],
+            client=self.dbt_client,
+            translator=self.translator,
+            env_data=self.env_data,
+        )
--- examples/experimental/dagster-dlift/kitchen-sink/dlift_kitchen_sink_tests/test_instance_methods.py
+++ examples/experimental/dagster-dlift/kitchen-sink/dlift_kitchen_sink_tests/test_instance_methods.py
@@ -1,16 +1,12 @@
-import pytest
-from dagster_dlift.client import DbtCloudClient
+from dagster_dlift.client import UnscopedDbtCloudClient
 from dagster_dlift.utils import get_job_name
 from dlift_kitchen_sink.constants import EXPECTED_TAG
-from dlift_kitchen_sink.instance import get_instance
 
 
-def test_get_models(instance: DbtCloudClient, environment_id: int) -> None:
+def test_get_models(instance: UnscopedDbtCloudClient, environment_id: int) -> None:
     # Filter to only the models that we use for testing.
     models_response = [
-        model
-        for model in get_instance().get_dbt_models(environment_id)
-        if EXPECTED_TAG in model["tags"]
+        model for model in instance.get_dbt_models(environment_id) if EXPECTED_TAG in model["tags"]
     ]
 
     assert len(models_response) == 3
@@ -56,11 +52,11 @@ def test_get_models(instance: DbtCloudClient, environment_id: int) -> None:
     }
 
 
-def test_get_sources(instance: DbtCloudClient, environment_id: int) -> None:
+def test_get_sources(instance: UnscopedDbtCloudClient, environment_id: int) -> None:
     """Test that we can get sources from the instance."""
     sources_response = [
         source
-        for source in get_instance().get_dbt_sources(environment_id)
+        for source in instance.get_dbt_sources(environment_id)
         if EXPECTED_TAG in source["tags"]
     ]
     assert len(sources_response) == 2
@@ -70,7 +66,7 @@ def test_get_sources(instance: DbtCloudClient, environment_id: int) -> None:
     }
 
 
-def test_get_tests(instance: DbtCloudClient, environment_id: int) -> None:
+def test_get_tests(instance: UnscopedDbtCloudClient, environment_id: int) -> None:
     """Test that we can get tests from the instance."""
     tests_response = [
         test for test in instance.get_dbt_tests(environment_id) if EXPECTED_TAG in test["tags"]
@@ -88,8 +84,9 @@ def test_get_tests(instance: DbtCloudClient, environment_id: int) -> None:
     }
 
 
-@pytest.mark.skip("This test is failing")
-def test_cloud_job_apis(instance: DbtCloudClient, environment_id: int, project_id: int) -> None:
+def test_cloud_job_apis(
+    instance: UnscopedDbtCloudClient, environment_id: int, project_id: int
+) -> None:
     """Tests that we can create / destroy a dagster job."""
     job_id = instance.create_dagster_job(project_id, environment_id)
     job_info = instance.get_job_info_by_id(job_id)
