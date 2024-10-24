dd66c37d99afb81294050d6826f61494c4ca45f9
Ben Pankow <ben@elementl.com>
2024-10-24 13:09:40 -0700

    [dagster-sigma] Detach spec loading from Sigma resource (#25431)
    ## Summary
    
    As part of migrating to the APIs mocked out in https://www.notion.so/dagster/Asset-integration-customization-options-11a18b92e462807c94c2f84675021120  and https://www.notion.so/dagster/Power-BI-semantic-model-API-options-11918b92e4628006a25af3f58a6b48d9, detaches the asset spec loading process from the resource.
    
    ```python
    resource = SigmaOrganization(
        base_url=SigmaBaseUrl.AWS_US,
        client_id=EnvVar("SIGMA_CLIENT_ID"),
        client_secret=EnvVar("SIGMA_CLIENT_SECRET"),
    )
    
    sigma_specs = load_sigma_asset_specs(resource, dagster_sigma_translator=MyCoolTranslator)
    ```
    
    ## How I Tested These Changes
    
    New unit test, updated existing unit tests.

--- python_modules/libraries/dagster-sigma/dagster_sigma_tests/test_asset_specs.py
+++ python_modules/libraries/dagster-sigma/dagster_sigma_tests/test_asset_specs.py
@@ -44,3 +44,20 @@ def test_load_assets_organization_data(sigma_auth_token: str, sigma_sample_data:
         assert len(recon_repository_def.assets_defs_by_key) == 2 + 1
 
         assert len(responses.calls) == calls
+
+
+@responses.activate
+def test_load_assets_organization_data_translator(
+    sigma_auth_token: str, sigma_sample_data: None
+) -> None:
+    with instance_for_test() as _instance:
+        repository_def = initialize_repository_def_from_pointer(
+            CodePointer.from_python_file(
+                str(Path(__file__).parent / "pending_repo_with_translator.py"), "defs", None
+            ),
+        )
+
+        assert len(repository_def.assets_defs_by_key) == 2
+        assert all(
+            key.path[0] == "my_prefix" for key in repository_def.assets_defs_by_key.keys()
+        ), repository_def.assets_defs_by_key
