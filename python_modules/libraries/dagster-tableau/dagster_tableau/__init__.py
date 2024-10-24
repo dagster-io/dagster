265dfc38b19c39b4967448f763b9070aeaebab78
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2024-10-24 15:29:50 -0400

    [dagster-tableau] Detach spec loading, assets def building from Tableau resource (#25459)
    ## Summary
    
    As part of migrating to the APIs mocked out in
    https://www.notion.so/dagster/Asset-integration-customization-options-11a18b92e462807c94c2f84675021120
    and
    https://www.notion.so/dagster/Power-BI-semantic-model-API-options-11918b92e4628006a25af3f58a6b48d9,
    detaches the asset spec loading process from the Tableau resource, and
    moves building asset definitions explicitly into user code.
    
    ```python
    resource = TableauCloudWorkspace(
        connected_app_client_id=EnvVar("CONNECTED_APP_CLIENT_ID"),
        connected_app_secret_id=EnvVar("CONNECTED_APP_SECRET_ID"),
        connected_app_secret_value=EnvVar("CONNECTED_APP_SECRET_VALUE"),
        username=EnvVar("USERNAME"),
        site_name=EnvVar("SITE_NAME"),
        pod_name=EnvVar("POD_NAME"),
    )
    
    tableau_specs = load_tableau_asset_specs(
        workspace=resource,
    )
    
    non_executable_asset_specs = [
        spec for spec in tableau_specs if spec.tags.get("dagster-tableau/asset_type") == "data_source"
    ]
    
    executable_asset_specs = [
        spec
        for spec in tableau_specs
        if spec.tags.get("dagster-tableau/asset_type") in ["dashboard", "sheet"]
    ]
    
    defs = Definitions(
        assets=[
            build_tableau_executable_assets_definition(
                resource_key="tableau",
                workspace=resource,
                specs=executable_asset_specs,
                refreshable_workbook_ids=["b75fc023-a7ca-4115-857b-4342028640d0"],
            ),
            *non_executable_asset_specs,
        ],
        resources={"tableau": resource},
    )
    
    ```
    
    
    ## How I Tested These Changes
    
    New unit test, updated existing unit tests.

--- python_modules/libraries/dagster-tableau/dagster_tableau/__init__.py
+++ python_modules/libraries/dagster-tableau/dagster_tableau/__init__.py
@@ -1,8 +1,12 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
+from dagster_tableau.assets import (
+    build_tableau_executable_assets_definition as build_tableau_executable_assets_definition,
+)
 from dagster_tableau.resources import (
     TableauCloudWorkspace as TableauCloudWorkspace,
     TableauServerWorkspace as TableauServerWorkspace,
+    load_tableau_asset_specs as load_tableau_asset_specs,
 )
 from dagster_tableau.translator import DagsterTableauTranslator as DagsterTableauTranslator
 from dagster_tableau.version import __version__ as __version__
