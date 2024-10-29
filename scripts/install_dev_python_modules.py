45c6f1a7b71c2c841e44b92c5989683389c6bc7e
Daniel Bartley <dbrtly00@gmail.com>
2024-10-30 04:24:56 +1100

    feat(cli.project): enable optional minimalist configuration in templates (#25245)
    PEP 621 (Nov 2020) introduced pyproject.toml.
    Setuptools is fully compatible with pyroject. 
    All the config in setup.cfg and setup.py will also be in pyproject.toml
    project template if this is merged.
    Minimalist config to make adopting dagster easy.
    
    ## Summary & Motivation
    See #25244 
    
    ## How I Tested These Changes
    
    ![image](https://github.com/user-attachments/assets/75473daa-a282-4564-93fd-db1d95cf2691)
    Last time I tried the run the test suite it failed because my macbook
    has an intel chipset.
    
    ## Changelog
    (CLI) 
    `dagster project scaffold`: Add option to create dagster projects from
    templates with excluded files/filepaths
    `dagster project scaffold-code-location`: Refactored to leverage
    scaffold command with excludes flag. Deprecated.
    
    ---------
    
    Co-authored-by: Colton Padden <colton@dagsterlabs.com>

--- scripts/install_dev_python_modules.py
+++ scripts/install_dev_python_modules.py
@@ -49,16 +49,22 @@ def main(
 
     # Supported on all Python versions.
     editable_target_paths = [
+        ".buildkite/dagster-buildkite",
+        "examples/experimental/dagster-blueprints",
+        "examples/experimental/dagster-airlift[core,in-airflow,mwaa,dbt,test]",
+        "integration_tests/python_modules/dagster-k8s-test-infra",
+        "helm/dagster/schema[test]",
+        "python_modules/automation",
         "python_modules/dagster[pyright,ruff,test]",
         "python_modules/dagster-pipes",
         "python_modules/dagster-graphql",
         "python_modules/dagster-test",
         "python_modules/dagster-webserver",
         "python_modules/dagit",
-        "python_modules/automation",
         "python_modules/libraries/dagster-managed-elements",
         "python_modules/libraries/dagster-airbyte",
         "python_modules/libraries/dagster-aws[stubs,test]",
+        "python_modules/libraries/dagster-azure",
         "python_modules/libraries/dagster-celery",
         "python_modules/libraries/dagster-celery-docker",
         "python_modules/libraries/dagster-dask[yarn,pbs,kube]",
@@ -66,6 +72,9 @@ def main(
         "python_modules/libraries/dagster-datadog",
         "python_modules/libraries/dagster-datahub",
         "python_modules/libraries/dagster-dbt",
+        "python_modules/libraries/dagster-deltalake",
+        "python_modules/libraries/dagster-deltalake-pandas",
+        "python_modules/libraries/dagster-deltalake-polars",
         "python_modules/libraries/dagster-docker",
         "python_modules/libraries/dagster-gcp",
         "python_modules/libraries/dagster-gcp-pandas",
@@ -77,31 +86,26 @@ def main(
         "python_modules/libraries/dagster-celery-k8s",
         "python_modules/libraries/dagster-github",
         "python_modules/libraries/dagster-mlflow",
+        "python_modules/libraries/dagster-msteams",
         "python_modules/libraries/dagster-mysql",
         "python_modules/libraries/dagster-looker",
         "python_modules/libraries/dagster-openai",
         "python_modules/libraries/dagster-pagerduty",
         "python_modules/libraries/dagster-pandas",
+        "python_modules/libraries/dagster-pandera",
+        "python_modules/libraries/dagster-polars[deltalake,gcp,test]",
         "python_modules/libraries/dagster-papertrail",
         "python_modules/libraries/dagster-postgres",
         "python_modules/libraries/dagster-prometheus",
         "python_modules/libraries/dagster-pyspark",
         "python_modules/libraries/dagster-shell",
         "python_modules/libraries/dagster-slack",
+        "python_modules/libraries/dagster-snowflake",
+        "python_modules/libraries/dagster-snowflake-pandas",
         "python_modules/libraries/dagster-spark",
         "python_modules/libraries/dagster-ssh",
         "python_modules/libraries/dagster-twilio",
         "python_modules/libraries/dagstermill",
-        "integration_tests/python_modules/dagster-k8s-test-infra",
-        "python_modules/libraries/dagster-azure",
-        "python_modules/libraries/dagster-msteams",
-        "python_modules/libraries/dagster-deltalake",
-        "python_modules/libraries/dagster-deltalake-pandas",
-        "python_modules/libraries/dagster-deltalake-polars",
-        "helm/dagster/schema[test]",
-        ".buildkite/dagster-buildkite",
-        "examples/experimental/dagster-blueprints",
-        "examples/experimental/dagster-airlift[core,in-airflow,mwaa,dbt,test]",
     ]
 
     if sys.version_info <= (3, 12):
@@ -114,13 +118,8 @@ def main(
             "python_modules/libraries/dagster-airflow",
         ]
 
-    if sys.version_info > (3, 7):
-        editable_target_paths += [
-            "python_modules/libraries/dagster-pandera",
-            "python_modules/libraries/dagster-snowflake",
-            "python_modules/libraries/dagster-snowflake-pandas",
-            "python_modules/libraries/dagster-polars[deltalake,gcp,test]",
-        ]
+    # if sys.version_info > (3, 7):
+    #     editable_target_paths += []
 
     install_targets += list(
         itertools.chain.from_iterable(
