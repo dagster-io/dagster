30b627cfa04521dc149f030c19da3b8fef29c5f2
jamiedemaria <demaria.jamie@gmail.com>
2024-10-29 12:52:10 -0400

    [sdf] modify context type signatures and usage for making AssetExecutionContext a standalone class (#25544)
    ## Summary & Motivation
    https://github.com/dagster-io/dagster/pull/25541 makes
    `AssetExecutionContext` not a subclass of `OpExecutionContext`. This PR
    is the changes to the sdf integration required to make that change.
    
    Includes updating type signatures to allow for `AssetExecutionContext`,
    since previously, type annotation of `OpExecutionContext` would have
    allowed `AssetExecutionContext` to be passed
    
    For any `context` calls that needed to be updated, I've added a comment
    explaining the change
    
    ## How I Tested These Changes
    existing tests

--- python_modules/libraries/dagster-sdf/dagster_sdf/sdf_cli_event.py
+++ python_modules/libraries/dagster-sdf/dagster_sdf/sdf_cli_event.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
-from typing import AbstractSet, Any, Dict, Iterator, Optional
+from typing import AbstractSet, Any, Dict, Iterator, Optional, Union
 
 from dagster import (
     AssetCheckResult,
     AssetCheckSeverity,
+    AssetExecutionContext,
     AssetMaterialization,
     OpExecutionContext,
     Output,
@@ -49,12 +50,12 @@ class SdfCliEventMessage:
     def to_default_asset_events(
         self,
         dagster_sdf_translator: DagsterSdfTranslator = DagsterSdfTranslator(),
-        context: Optional[OpExecutionContext] = None,
+        context: Optional[Union[OpExecutionContext, AssetExecutionContext]] = None,
     ) -> Iterator[SdfDagsterEventType]:
         """Convert an sdf CLI event to a set of corresponding Dagster events.
 
         Args:
-            context (Optional[OpExecutionContext]): The execution context.
+            context (Optional[Union[OpExecutionContext, AssetExecutionContext]]): The execution context.
 
         Returns:
             Iterator[Union[Output, AssetMaterialization]]:
@@ -74,7 +75,7 @@ class SdfCliEventMessage:
             return
 
         selected_output_names: AbstractSet[str] = (
-            context.selected_output_names if context else set()
+            context.op_execution_context.selected_output_names if context else set()
         )
 
         catalog = self.raw_event["ev_tb_catalog"]
