1b790dbaab142eba8498fc93ef9e5420067df360
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2024-10-30 13:53:47 -0400

    [dagster-looker] Update Looker documentation to showcase new API patterns (#25601)
    ## Summary
    
    Updates our Looker docs to incorporate the new specs-building pattern,
    norm of passing the resource to `Definitions`, and the new pattern for
    building executable asset definitions.

--- examples/docs_snippets/docs_snippets/integrations/looker/materializing-looker-pdts.py
+++ examples/docs_snippets/docs_snippets/integrations/looker/materializing-looker-pdts.py
@@ -1,18 +1,29 @@
-from dagster_looker import LookerResource, RequestStartPdtBuild
+from dagster_looker import (
+    LookerResource,
+    RequestStartPdtBuild,
+    build_looker_pdt_assets_definitions,
+    load_looker_asset_specs,
+)
 
 import dagster as dg
 
-resource = LookerResource(
+looker_resource = LookerResource(
     client_id=dg.EnvVar("LOOKERSDK_CLIENT_ID"),
     client_secret=dg.EnvVar("LOOKERSDK_CLIENT_SECRET"),
     base_url=dg.EnvVar("LOOKERSDK_HOST_URL"),
 )
 
-defs = resource.build_defs(
+looker_specs = load_looker_asset_specs(looker_resource=looker_resource)
+
+pdts = build_looker_pdt_assets_definitions(
+    resource_key="looker",
     request_start_pdt_builds=[
-        RequestStartPdtBuild(
-            model_name="analytics",
-            view_name="page_keyword_performance",
-        ),
-    ]
+        RequestStartPdtBuild(model_name="my_model", view_name="my_view")
+    ],
+)
+
+
+defs = dg.Definitions(
+    assets=[*pdts, *looker_specs],
+    resources={"looker": looker_resource},
 )
