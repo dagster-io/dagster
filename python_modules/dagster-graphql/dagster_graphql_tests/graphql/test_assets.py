dagster-io/dagster

commit 4ed1bb14bd456b1a1ad4fc410f6494ae906e6539

[graphql] move asset node fetches back to asset_graph (#25580)
theres is a great deal of code that does `asset_graph.get` so instead of
trying to reroute things through a new context method, lets just focus
on improving that code path

## How I Tested These Changes

existing tests

python_modules/dagster-graphql/dagster_graphql/schema/roots/query.py
@@ -1016,11 +1016,10 @@ class GrapheneQuery(graphene.ObjectType):
             ]
         else:
             if not use_all_asset_keys and resolved_asset_keys:
-                fetched_nodes = [
-                    graphene_info.context.get_asset_node(asset_key)
+                remote_nodes = [
+                    graphene_info.context.asset_graph.get(asset_key)
                     for asset_key in resolved_asset_keys
                 ]
-                remote_nodes = [node for node in fetched_nodes if node]
             else:
                 remote_nodes = [
                     remote_node for remote_node in graphene_info.context.asset_graph.asset_nodes
@@ -1160,7 +1159,9 @@ class GrapheneQuery(graphene.ObjectType):
     ):
         asset_keys = set(AssetKey.from_graphql_input(asset_key) for asset_key in assetKeys)
 
-        remote_nodes = {graphene_info.context.get_asset_node(asset_key) for asset_key in asset_keys}
+        remote_nodes = {
+            graphene_info.context.asset_graph.get(asset_key) for asset_key in asset_keys
+        }
 
         # Build mapping of asset key to the step keys required to generate the asset
         step_keys_by_asset: Dict[AssetKey, Sequence[str]] = {

python_modules/dagster-graphql/dagster_graphql/implementation/fetch_assets.py
@@ -154,20 +154,19 @@ def get_asset_node_definition_collisions(
     from dagster_graphql.schema.external import GrapheneRepository
 
     repos: Dict[AssetKey, List[GrapheneRepository]] = defaultdict(list)
+    for asset_key in asset_keys:
+        remote_asset_node = graphene_info.context.asset_graph.get(asset_key)
+        for info in remote_asset_node.repo_scoped_asset_infos:
+            asset_node_snap = info.asset_node.asset_node_snap
+            is_defined = (
+                asset_node_snap.node_definition_name
+                or asset_node_snap.graph_name
+                or asset_node_snap.op_name
+            )
+            if not is_defined:
+                continue
 
-    for remote_asset_node in graphene_info.context.asset_graph.asset_nodes:
-        if remote_asset_node.key in asset_keys:
-            for info in remote_asset_node.repo_scoped_asset_infos:
-                asset_node_snap = info.asset_node.asset_node_snap
-                is_defined = (
-                    asset_node_snap.node_definition_name
-                    or asset_node_snap.graph_name
-                    or asset_node_snap.op_name
-                )
-                if not is_defined:
-                    continue
-
-                repos[asset_node_snap.asset_key].append(GrapheneRepository(info.handle))
+            repos[asset_node_snap.asset_key].append(GrapheneRepository(info.handle))
 
     results: List[GrapheneAssetNodeDefinitionCollision] = []
     for asset_key in repos.keys():
@@ -251,10 +250,10 @@ def get_asset_node(
 
     check.inst_param(asset_key, "asset_key", AssetKey)
 
-    remote_node = graphene_info.context.get_asset_node(asset_key)
-    if not remote_node:
+    if not graphene_info.context.asset_graph.has(asset_key):
         return GrapheneAssetNotFoundError(asset_key=asset_key)
 
+    remote_node = graphene_info.context.asset_graph.get(asset_key)
     return _graphene_asset_node(
         graphene_info,
         remote_node,
@@ -282,14 +281,15 @@ def get_asset(
 
     check.inst_param(asset_key, "asset_key", AssetKey)
     instance = graphene_info.context.instance
-    remote_node = graphene_info.context.get_asset_node(asset_key)
 
-    if not remote_node and not instance.has_asset_key(asset_key):
+    has_remote_node = graphene_info.context.asset_graph.has(asset_key)
+    if not has_remote_node and not instance.has_asset_key(asset_key):
         return GrapheneAssetNotFoundError(asset_key=asset_key)
-    elif remote_node:
+
+    if has_remote_node:
         def_node = _graphene_asset_node(
             graphene_info,
-            remote_node,
+            graphene_info.context.asset_graph.get(asset_key),
             stale_status_loader=None,
             asset_checks_loader=AssetChecksLoader(
                 context=graphene_info.context,
@@ -331,7 +331,9 @@ def get_asset_materializations(
         cursor = None
         while True:
             event_records_result = instance.fetch_materializations(
-                records_filter=records_filter, cursor=cursor, limit=get_max_event_records_limit()
+                records_filter=records_filter,
+                cursor=cursor,
+                limit=get_max_event_records_limit(),
             )
             cursor = event_records_result.cursor
             event_records.extend(event_records_result.records)
@@ -370,7 +372,9 @@ def get_asset_observations(
         cursor = None
         while True:
             event_records_result = instance.fetch_observations(
-                records_filter=records_filter, cursor=cursor, limit=get_max_event_records_limit()
+                records_filter=records_filter,
+                cursor=cursor,
+                limit=get_max_event_records_limit(),
             )
             cursor = event_records_result.cursor
             event_records.extend(event_records_result.records)
@@ -566,7 +570,8 @@ def build_partition_statuses(
         graphene_ranges = []
         for r in ranges:
             partition_key_range = cast(
-                TimeWindowPartitionsDefinition, materialized_partitions_subset.partitions_def
+                TimeWindowPartitionsDefinition,
+                materialized_partitions_subset.partitions_def,
             ).get_partition_key_range_for_time_window(r.time_window)
             graphene_ranges.append(
                 GrapheneTimePartitionRangeStatus(
@@ -597,7 +602,8 @@ def build_partition_statuses(
             - set(in_progress_keys),
             failedPartitions=failed_keys,
             unmaterializedPartitions=materialized_partitions_subset.get_partition_keys_not_in_subset(
-                partitions_def=partitions_def, dynamic_partitions_store=dynamic_partitions_store
+                partitions_def=partitions_def,
+                dynamic_partitions_store=dynamic_partitions_store,
             ),
             materializingPartitions=in_progress_keys,
         )

python_modules/dagster-graphql/dagster_graphql/implementation/fetch_runs.py
@@ -190,10 +190,8 @@ def get_assets_latest_info(
         return []
 
     asset_nodes = {
-        asset_key: graphene_info.context.get_asset_node(asset_key) for asset_key in asset_keys
+        asset_key: graphene_info.context.asset_graph.get(asset_key) for asset_key in asset_keys
     }
-
-    # asset_nodes = get_asset_nodes_by_asset_key(graphene_info, set(asset_keys))
 
     asset_records = AssetRecord.blocking_get_many(graphene_info.context, asset_keys)
 

python_modules/dagster-graphql/dagster_graphql/schema/asset_graph.py
@@ -171,7 +171,7 @@ class GrapheneAssetDependency(graphene.ObjectType):
         super().__init__()
 
     def resolve_asset(self, graphene_info: ResolveInfo):
-        remote_node = check.not_none(graphene_info.context.get_asset_node(self._asset_key))
+        remote_node = graphene_info.context.asset_graph.get(self._asset_key)
         return GrapheneAssetNode(
             remote_node=remote_node,
             asset_checks_loader=self._asset_checks_loader,

python_modules/dagster/dagster/_core/definitions/base_asset_graph.py
@@ -236,7 +236,10 @@ class BaseAssetGraph(ABC, Generic[T_AssetNode]):
 
     @property
     def nodes(self) -> Iterable[BaseEntityNode]:
-        return [*self._asset_nodes_by_key.values(), *self._asset_check_nodes_by_key.values()]
+        return [
+            *self._asset_nodes_by_key.values(),
+            *self._asset_check_nodes_by_key.values(),
+        ]
 
     def has(self, key: EntityKey) -> bool:
         return key in self._asset_nodes_by_key or key in self._asset_check_nodes_by_key
@@ -344,7 +347,8 @@ class BaseAssetGraph(ABC, Generic[T_AssetNode]):
     def root_executable_asset_keys(self) -> AbstractSet[AssetKey]:
         """Executable asset keys that have no executable parents."""
         return fetch_sources(
-            self.asset_dep_graph, self.observable_asset_keys | self.materializable_asset_keys
+            self.asset_dep_graph,
+            self.observable_asset_keys | self.materializable_asset_keys,
         )
 
     @property
@@ -354,7 +358,8 @@ class BaseAssetGraph(ABC, Generic[T_AssetNode]):
     @cached_property
     def all_partitions_defs(self) -> Sequence[PartitionsDefinition]:
         return sorted(
-            set(node.partitions_def for node in self.asset_nodes if node.partitions_def), key=repr
+            set(node.partitions_def for node in self.asset_nodes if node.partitions_def),
+            key=repr,
         )
 
     @cached_property
@@ -673,7 +678,8 @@ class BaseAssetGraph(ABC, Generic[T_AssetNode]):
 
         all_assets = set(initial_subset.asset_keys)
         check.invariant(
-            len(initial_subset.asset_keys) == 1, "Multiple initial assets not yet supported"
+            len(initial_subset.asset_keys) == 1,
+            "Multiple initial assets not yet supported",
         )
         initial_asset_key = next(iter(initial_subset.asset_keys))
         queue = deque([initial_asset_key])
@@ -743,12 +749,14 @@ class BaseAssetGraph(ABC, Generic[T_AssetNode]):
         self,
         dynamic_partitions_store: DynamicPartitionsStore,
         condition_fn: Callable[
-            [Iterable[AssetKeyPartitionKey], AbstractSet[AssetKeyPartitionKey]], Tuple[bool, str]
+            [Iterable[AssetKeyPartitionKey], AbstractSet[AssetKeyPartitionKey]],
+            Tuple[bool, str],
         ],
         initial_asset_partitions: Iterable[AssetKeyPartitionKey],
         evaluation_time: datetime,
     ) -> Tuple[
-        AbstractSet[AssetKeyPartitionKey], Sequence[Tuple[Iterable[AssetKeyPartitionKey], str]]
+        AbstractSet[AssetKeyPartitionKey],
+        Sequence[Tuple[Iterable[AssetKeyPartitionKey], str]],
     ]:
         """Returns asset partitions within the graph that satisfy supplied criteria.
 

python_modules/dagster/dagster/_core/definitions/remote_asset_graph.py
@@ -403,7 +403,9 @@ class RemoteAssetGraph(BaseAssetGraph[TRemoteAssetNode], ABC, Generic[TRemoteAss
 
     @property
     @abstractmethod
-    def remote_asset_check_nodes_by_key(self) -> Mapping[AssetCheckKey, RemoteAssetCheckNode]: ...
+    def remote_asset_check_nodes_by_key(
+        self,
+    ) -> Mapping[AssetCheckKey, RemoteAssetCheckNode]: ...
 
     ##### COMMON ASSET GRAPH INTERFACE
     @cached_property
@@ -546,10 +548,24 @@ class RemoteRepositoryAssetGraph(RemoteAssetGraph[RemoteRepositoryAssetNode]):
         )
 
 
-@record
 class RemoteWorkspaceAssetGraph(RemoteAssetGraph[RemoteWorkspaceAssetNode]):
-    remote_asset_nodes_by_key: Mapping[AssetKey, RemoteWorkspaceAssetNode]
-    remote_asset_check_nodes_by_key: Mapping[AssetCheckKey, RemoteAssetCheckNode]
+    def __init__(
+        self,
+        remote_asset_nodes_by_key: Mapping[AssetKey, RemoteWorkspaceAssetNode],
+        remote_asset_check_nodes_by_key: Mapping[AssetCheckKey, RemoteAssetCheckNode],
+    ):
+        self._remote_asset_nodes_by_key = remote_asset_nodes_by_key
+        self._remote_asset_check_nodes_by_key = remote_asset_check_nodes_by_key
+
+    @property
+    def remote_asset_nodes_by_key(self) -> Mapping[AssetKey, RemoteWorkspaceAssetNode]:
+        return self._remote_asset_nodes_by_key
+
+    @property
+    def remote_asset_check_nodes_by_key(
+        self,
+    ) -> Mapping[AssetCheckKey, RemoteAssetCheckNode]:
+        return self._remote_asset_check_nodes_by_key
 
     @property
     def _asset_nodes_by_key(self) -> Mapping[AssetKey, RemoteWorkspaceAssetNode]:
@@ -641,7 +657,9 @@ class RemoteWorkspaceAssetGraph(RemoteAssetGraph[RemoteWorkspaceAssetNode]):
         )
 
 
-def _warn_on_duplicate_nodes(nodes_with_multiple: Sequence[RemoteWorkspaceAssetNode]) -> None:
+def _warn_on_duplicate_nodes(
+    nodes_with_multiple: Sequence[RemoteWorkspaceAssetNode],
+) -> None:
     # Split the nodes into materializable, observable, and unexecutable nodes. Observable and
     # unexecutable `AssetNodeSnap` represent both source and external assets-- the
     # "External" in "AssetNodeSnap" is unrelated to the "external" in "external asset", this
@@ -650,7 +668,8 @@ def _warn_on_duplicate_nodes(nodes_with_multiple: Sequence[RemoteWorkspaceAssetN
     observable_duplicates: Mapping[AssetKey, Sequence[str]] = {}
     for node in nodes_with_multiple:
         check.invariant(
-            len(node.repo_scoped_asset_infos) > 1, "only perform check on nodes with multiple defs"
+            len(node.repo_scoped_asset_infos) > 1,
+            "only perform check on nodes with multiple defs",
         )
         observable_locations = []
         materializable_locations = []

python_modules/dagster/dagster/_core/workspace/context.py
@@ -79,10 +79,7 @@ from dagster._utils.aiodataloader import DataLoader
 from dagster._utils.error import SerializableErrorInfo, serializable_error_info_from_exc_info
 
 if TYPE_CHECKING:
-    from dagster._core.definitions.remote_asset_graph import (
-        RemoteWorkspaceAssetGraph,
-        RemoteWorkspaceAssetNode,
-    )
+    from dagster._core.definitions.remote_asset_graph import RemoteWorkspaceAssetGraph
     from dagster._core.remote_representation import (
         PartitionConfigSnap,
         PartitionExecutionErrorSnap,
@@ -348,12 +345,6 @@ class BaseWorkspaceRequestContext(LoadingContext):
 
     def get_base_deployment_context(self) -> Optional["BaseWorkspaceRequestContext"]:
         return None
-
-    def get_asset_node(self, asset_key: AssetKey) -> Optional["RemoteWorkspaceAssetNode"]:
-        if not self.get_workspace_snapshot().asset_graph.has(asset_key):
-            return None
-
-        return self.get_workspace_snapshot().asset_graph.get(asset_key)
 
     def get_repository(
         self, selector: Union[RepositorySelector, RepositoryHandle]

commit 9b9e6a70a8665689b180de7efaae51624a46b2e2

[graphql] handle asset key that does not exist in assetNodes (#25645)
broken in https://github.com/dagster-io/dagster/pull/25580 because not
tested, discovered in manual QA

## How I Tested These Changes

added test

python_modules/dagster-graphql/dagster_graphql/schema/roots/query.py
@@ -1018,6 +1018,7 @@ class GrapheneQuery(graphene.ObjectType):
                 remote_nodes = [
                     graphene_info.context.asset_graph.get(asset_key)
                     for asset_key in resolved_asset_keys
+                    if graphene_info.context.asset_graph.has(asset_key)
                 ]
             else:
                 remote_nodes = [

python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_assets.py
@@ -254,7 +254,7 @@ GET_ASSET_DATA_VERSIONS_BY_PARTITION = """
 
 
 GET_ASSET_NODES_FROM_KEYS = """
-    query AssetNodeQuery($pipelineSelector: PipelineSelector!, $assetKeys: [AssetKeyInput!]) {
+    query AssetNodeQuery($pipelineSelector: PipelineSelector, $assetKeys: [AssetKeyInput!]) {
         assetNodes(pipeline: $pipelineSelector, assetKeys: $assetKeys) {
             id
             hasMaterializePermission
@@ -1189,6 +1189,17 @@ class TestAssetAwareEventLog(ExecutingGraphQLContextTestMatrix):
             variables={
                 "pipelineSelector": selector,
                 "assetKeys": [{"path": ["asset_one"]}],
+            },
+        )
+        assert result.data
+        assert len(result.data["assetNodes"]) == 0
+
+    def test_asset_node_does_not_exist(self, graphql_context: WorkspaceRequestContext):
+        result = execute_dagster_graphql(
+            graphql_context,
+            GET_ASSET_NODES_FROM_KEYS,
+            variables={
+                "assetKeys": [{"path": ["does_not_exist"]}],
             },
         )
         assert result.data