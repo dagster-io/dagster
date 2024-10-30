a397264a660d47c50a568a38946508a9c41217a1
Sean Mackesey <s.mackesey@gmail.com>
2024-10-30 16:45:06 -0400

    [docs] [jobless-automation] Update tutorial (#25495)
    ## Summary & Motivation
    
    - Update tutorial to use jobless automation via the `target` argument to
    `ScheduleDefinition` instead of defining `hackernews_job`.
    - Remove references to `hackernews_job` after schedule phase of
    tutorial.
    
    ## How I Tested These Changes
    
    BK, read through tutorial.

--- examples/docs_snippets/docs_snippets/tutorial/scheduling/with_schedule/with_schedule.py
+++ examples/docs_snippets/docs_snippets/tutorial/scheduling/with_schedule/with_schedule.py
@@ -2,7 +2,6 @@ from dagster import (
     AssetSelection,
     Definitions,
     ScheduleDefinition,
-    define_asset_job,
     load_assets_from_modules,
 )
 
@@ -10,12 +9,10 @@ from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
-# Define a job that will materialize the assets
-hackernews_job = define_asset_job("hackernews_job", selection=AssetSelection.all())
-
-# Addition: a ScheduleDefinition the job it should run and a cron schedule of how frequently to run it
+# Addition: a ScheduleDefinition targeting all assets and a cron schedule of how frequently to run it
 hackernews_schedule = ScheduleDefinition(
-    job=hackernews_job,
+    name="hackernews_schedule",
+    target=AssetSelection.all(),
     cron_schedule="0 * * * *",  # every hour
 )
 
