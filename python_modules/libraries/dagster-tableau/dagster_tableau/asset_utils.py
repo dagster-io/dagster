0838c352b94b274cfd99a6a213b0337a872c62d5
Maxime Armstrong <maxime@retain.ai>
2024-10-31 16:10:06 -0400

    Return namedtuple

--- python_modules/libraries/dagster-tableau/dagster_tableau/asset_utils.py
+++ python_modules/libraries/dagster-tableau/dagster_tableau/asset_utils.py
@@ -1,13 +1,36 @@
-from typing import Sequence, Tuple
+from collections import namedtuple
+from typing import Sequence
 
-from dagster import AssetSpec
+from dagster import (
+    AssetSpec,
+    _check as check,
+)
 
 from dagster_tableau.translator import TableauTagSet
 
 
+class ParsedTableauAssetSpecs(
+    namedtuple("_ParsedTableauAssetSpecs", ["external_asset_specs", "materializable_asset_specs"])
+):
+    """Used to represent the parsed Tableau asset specs
+    as returned by the `parse_tableau_external_and_materializable_asset_specs` function below.
+    """
+
+    def __new__(cls, external_asset_specs, materializable_asset_specs):
+        return super(ParsedTableauAssetSpecs, cls).__new__(
+            cls,
+            external_asset_specs=check.list_param(
+                external_asset_specs, "external_asset_specs", AssetSpec
+            ),
+            materializable_asset_specs=check.list_param(
+                materializable_asset_specs, "materializable_asset_specs", AssetSpec
+            ),
+        )
+
+
 def parse_tableau_external_and_materializable_asset_specs(
     specs: Sequence[AssetSpec],
-) -> Tuple[Sequence[AssetSpec], Sequence[AssetSpec]]:
+) -> ParsedTableauAssetSpecs:
     external_asset_specs = [
         spec for spec in specs if TableauTagSet.extract(spec.tags).asset_type == "data_source"
     ]
@@ -18,4 +41,7 @@ def parse_tableau_external_and_materializable_asset_specs(
         if TableauTagSet.extract(spec.tags).asset_type in ["dashboard", "sheet"]
     ]
 
-    return external_asset_specs, materializable_asset_specs
+    return ParsedTableauAssetSpecs(
+        external_asset_specs=external_asset_specs,
+        materializable_asset_specs=materializable_asset_specs,
+    )
