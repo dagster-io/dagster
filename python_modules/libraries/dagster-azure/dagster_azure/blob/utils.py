74dddebcbac1054cb955f8b5861ede31228186a1
lilygrierjil <87042447+lilygrierjil@users.noreply.github.com>
2024-11-01 21:19:56 +0000

    EDIT to take in cloud type for blob client url

--- python_modules/libraries/dagster-azure/dagster_azure/blob/utils.py
+++ python_modules/libraries/dagster-azure/dagster_azure/blob/utils.py
@@ -16,13 +16,16 @@ except ImportError:
     raise
 
 
-def _create_url(storage_account, subdomain):
-    return f"https://{storage_account}.{subdomain}.core.windows.net/"
+def _create_url(storage_account, subdomain, cloud_type: str = "public"):
+    if cloud_type == "public":
+        return f"https://{storage_account}.{subdomain}.core.windows.net/"
+    elif cloud_type == "government":
+        return f"https://{storage_account}.{subdomain}.core.usgovcloudapi.net/"
 
 
-def create_blob_client(storage_account, credential) -> BlobServiceClient:
+def create_blob_client(storage_account, credential, cloud_type: str = "public") -> BlobServiceClient:
     """Create a Blob Storage client."""
-    account_url = _create_url(storage_account, "blob")
+    account_url = _create_url(storage_account, "blob", cloud_type)
     if hasattr(credential, "account_key"):
         credential = credential.account_key
     return BlobServiceClient(account_url, credential)
