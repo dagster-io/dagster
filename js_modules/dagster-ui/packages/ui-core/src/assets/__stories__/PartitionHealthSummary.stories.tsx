eecde30a8ef88d4f0efcbd641daec0e635df1f46
bengotow <bgotow@elementl.com>
2024-11-03 18:53:45 -0600

    [ui] Fix poor performance when selecting assets with 100k+ partitions

--- js_modules/dagster-ui/packages/ui-core/src/assets/__stories__/PartitionHealthSummary.stories.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/__stories__/PartitionHealthSummary.stories.tsx
@@ -10,7 +10,7 @@ import {
   SingleDimensionTimePartitionHealthQuery,
 } from '../__fixtures__/PartitionHealthSummary.fixtures';
 import {AssetKey} from '../types';
-import {PartitionDimensionSelection, usePartitionHealthData} from '../usePartitionHealthData';
+import {usePartitionHealthData} from '../usePartitionHealthData';
 
 // eslint-disable-next-line import/no-default-export
 export default {
@@ -20,36 +20,15 @@ export default {
 
 const PartitionHealthSummaryWithData = ({
   assetKey,
-  ranges,
 }: {
   assetKey: AssetKey;
   ranges?: [string, string][];
 }) => {
   const data = usePartitionHealthData([assetKey]);
 
-  // Convert the convenient test shorthand into a PartitionDimensionSelection
-  const selections =
-    data[0] && ranges
-      ? ranges.map((range, idx) => {
-          const dim = data[0]!.dimensions[idx]!;
-          const idx0 = dim.partitionKeys.indexOf(range[0]);
-          const idx1 = dim.partitionKeys.indexOf(range[1]);
-          return {
-            dimension: dim,
-            selectedKeys: dim.partitionKeys.slice(idx0, idx1 + 1),
-            selectedRanges: [{start: {idx: idx0, key: range[0]}, end: {idx: idx1, key: range[1]}}],
-          } as PartitionDimensionSelection;
-        })
-      : undefined;
-
   return (
     <Box style={{width: '50%'}}>
-      <PartitionHealthSummary
-        assetKey={assetKey}
-        showAssetKey
-        data={data}
-        selections={selections}
-      />
+      <PartitionHealthSummary assetKey={assetKey} showAssetKey data={data} partitionStats={null} />
     </Box>
   );
 };
@@ -67,47 +46,23 @@ export const States = () => (
     <Box flex={{direction: 'column', gap: 60}}>
       <Box flex={{direction: 'row', gap: 30}}>
         <PartitionHealthSummaryWithData assetKey={{path: ['single_dimension_time']}} />
-        <PartitionHealthSummaryWithData
-          assetKey={{path: ['single_dimension_time']}}
-          ranges={[['2023-01-01-00:00', '2023-02-01-00:00']]}
-        />
+        <PartitionHealthSummaryWithData assetKey={{path: ['single_dimension_time']}} />
       </Box>
       <Box flex={{direction: 'row', gap: 30}}>
         <PartitionHealthSummaryWithData assetKey={{path: ['single_dimension_static']}} />
-        <PartitionHealthSummaryWithData
-          assetKey={{path: ['single_dimension_static']}}
-          ranges={[['KY', 'FL']]}
-        />
+        <PartitionHealthSummaryWithData assetKey={{path: ['single_dimension_static']}} />
       </Box>
       <Box flex={{direction: 'row', gap: 30}}>
         <PartitionHealthSummaryWithData assetKey={{path: ['multi_dimension_static']}} />
-        <PartitionHealthSummaryWithData
-          assetKey={{path: ['multi_dimension_static']}}
-          ranges={[
-            ['May', 'May'],
-            ['TN', 'VA'],
-          ]}
-        />
+        <PartitionHealthSummaryWithData assetKey={{path: ['multi_dimension_static']}} />
       </Box>
       <Box flex={{direction: 'row', gap: 30}}>
         <PartitionHealthSummaryWithData assetKey={{path: ['multi_dimension_time_first']}} />
-        <PartitionHealthSummaryWithData
-          assetKey={{path: ['multi_dimension_time_first']}}
-          ranges={[
-            ['2022-09-10', '2022-09-16'],
-            ['TN', 'VA'],
-          ]}
-        />
+        <PartitionHealthSummaryWithData assetKey={{path: ['multi_dimension_time_first']}} />
       </Box>
       <Box flex={{direction: 'row', gap: 30}}>
         <PartitionHealthSummaryWithData assetKey={{path: ['multi_dimension_time_second']}} />
-        <PartitionHealthSummaryWithData
-          assetKey={{path: ['multi_dimension_time_second']}}
-          ranges={[
-            ['TN', 'VA'],
-            ['2022-09-28', '2022-11-16'],
-          ]}
-        />
+        <PartitionHealthSummaryWithData assetKey={{path: ['multi_dimension_time_second']}} />
       </Box>
     </Box>
   </MockedProvider>
