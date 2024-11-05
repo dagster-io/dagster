633f1410b1e48609898a82a2e3aa9f7b730e3c4a
Maxime Armstrong <maxime@retain.ai>
2024-11-05 17:33:02 -0500

    [dagster-fivetran] Implement FivetranClient for rework

---
+++ python_modules/libraries/dagster-fivetran/dagster_fivetran_tests/v2/test_resources.py
@@ -0,0 +1,34 @@
+import responses
+import uuid
+from typing import Callable
+
+from dagster_fivetran.v2 import FivetranWorkspace
+
+
+@responses.activate
+def test_basic_resource_request(
+    connector_id: str,
+    destination_id: str,
+    group_id: str,
+    workspace_data_api_mocks_fn: Callable
+) -> None:
+    api_key = uuid.uuid4().hex
+    api_secret = uuid.uuid4().hex
+
+    resource = FivetranWorkspace(api_key=api_key, api_secret=api_secret)
+
+    with workspace_data_api_mocks_fn() as response:
+
+        client = resource.get_client()
+        client.get_connector_details(connector_id=connector_id)
+        client.get_connectors_for_group(group_id=group_id)
+        client.get_destination_details(destination_id=destination_id)
+        client.get_groups()
+
+        assert len(response.calls) == 4
+
+        assert "Basic" in response.calls[0].request.headers["Authorization"]
+        assert connector_id in response.calls[0].request.url
+        assert group_id in response.calls[1].request.url
+        assert destination_id in response.calls[2].request.url
+        assert "groups" in response.calls[3].request.url
