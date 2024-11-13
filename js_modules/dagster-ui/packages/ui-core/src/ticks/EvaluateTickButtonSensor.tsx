4406df662ede1a13297a7157806467d9552bd1a7
David Liu <d.liu@dagsterlabs.com>
2024-11-13 13:12:55 -0500

    add manual tick button to rows in automation table

---
+++ js_modules/dagster-ui/packages/ui-core/src/ticks/EvaluateTickButtonSensor.tsx
@@ -0,0 +1,49 @@
+import {Box, Button, Tooltip} from '@dagster-io/ui-components';
+import {useState} from 'react';
+
+import {SensorDryRunDialog} from './SensorDryRunDialog';
+import {SensorType} from '../graphql/types';
+import {RepoAddress} from '../workspace/types';
+
+interface EvaluateTickButtonSensorProps {
+  cursor: string;
+  name: string;
+  repoAddress: RepoAddress;
+  jobName: string;
+  sensorType: SensorType;
+}
+
+export const EvaluateTickButtonSensor = ({
+  cursor,
+  name,
+  repoAddress,
+  jobName,
+  sensorType,
+}: EvaluateTickButtonSensorProps) => {
+  const [showTestTickDialog, setShowTestTickDialog] = useState(false);
+
+  return (
+    <Box flex={{direction: 'row', alignItems: 'center', gap: 8}}>
+      <Tooltip
+        canShow={sensorType !== SensorType.STANDARD}
+        content="Testing not available for this sensor type"
+        placement="top-end"
+      >
+        <Button
+          disabled={sensorType !== SensorType.STANDARD}
+          onClick={() => setShowTestTickDialog(true)}
+        >
+          Evaluate tick
+        </Button>
+      </Tooltip>
+      <SensorDryRunDialog
+        isOpen={showTestTickDialog}
+        onClose={() => setShowTestTickDialog(false)}
+        currentCursor={cursor}
+        name={name}
+        repoAddress={repoAddress}
+        jobName={jobName}
+      />
+    </Box>
+  );
+};
