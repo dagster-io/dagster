ce1c6e6670349e2a1047e03cfef5ce38dad7f152
Maxime Armstrong <maxime@retain.ai>
2024-11-15 09:40:12 -0500

    Add comments in test

--- python_modules/libraries/dagster-fivetran/dagster_fivetran_tests/experimental/test_resources.py
+++ python_modules/libraries/dagster-fivetran/dagster_fivetran_tests/experimental/test_resources.py
@@ -19,8 +19,9 @@ def test_basic_resource_request(
     resource = FivetranWorkspace(
         account_id=TEST_ACCOUNT_ID, api_key=TEST_API_KEY, api_secret=TEST_API_SECRET
     )
-
     client = resource.get_client()
+
+    # fetch workspace data calls
     client.get_connectors_for_group(group_id=group_id)
     client.get_destination_details(destination_id=destination_id)
     client.get_groups()
@@ -33,7 +34,7 @@ def test_basic_resource_request(
     assert "groups" in all_api_mocks.calls[2].request.url
     assert f"{connector_id}/schemas" in all_api_mocks.calls[3].request.url
 
-    # reset calls
+    # connector details calls
     all_api_mocks.calls.reset()
     client.get_connector_details(connector_id=connector_id)
     client.update_schedule_type_for_connector(connector_id=connector_id, schedule_type="auto")
@@ -43,21 +44,25 @@ def test_basic_resource_request(
     assert connector_id in all_api_mocks.calls[1].request.url
     assert all_api_mocks.calls[1].request.method == "PATCH"
 
+    # sync calls
     all_api_mocks.calls.reset()
     client.start_sync(connector_id=connector_id)
     assert len(all_api_mocks.calls) == 3
     assert f"{connector_id}/force" in all_api_mocks.calls[2].request.url
 
+    # resync calls
     all_api_mocks.calls.reset()
     client.start_resync(connector_id=connector_id, resync_parameters=None)
     assert len(all_api_mocks.calls) == 3
     assert f"{connector_id}/resync" in all_api_mocks.calls[2].request.url
 
+    # resync calls with parameters
     all_api_mocks.calls.reset()
     client.start_resync(connector_id=connector_id, resync_parameters={"property1": ["string"]})
     assert len(all_api_mocks.calls) == 3
     assert f"{connector_id}/schemas/tables/resync" in all_api_mocks.calls[2].request.url
 
+    # poll calls
     all_api_mocks.calls.reset()
     client.poll_sync(
         connector_id=connector_id, previous_sync_completed_at=parser.parse(MIN_TIME_STR)
