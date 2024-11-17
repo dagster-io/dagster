6d20966e67c563095a52810fae201b2f3fc804c6
bengotow <bgotow@elementl.com>
2024-11-17 08:48:47 -0600

    [ui] Fix vertical centering of arrows on the asset graph, asset name wrapping

--- js_modules/dagster-ui/packages/ui-core/src/asset-graph/__stories__/AssetNode.stories.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/__stories__/AssetNode.stories.tsx
@@ -56,24 +56,32 @@ export const LiveStates = () => {
       <>
         <SetCacheEntry />
         <Box flex={{direction: 'column', gap: 8, alignItems: 'flex-start'}}>
+          <code style={{marginTop: 20}}>
+            <strong>{scenario.title}</strong>
+          </code>
           <div
             style={{
               position: 'relative',
               width: dimensions.width,
               height: dimensions.height,
+              background: `linear-gradient(to bottom, rgba(100,100,100,0.15) 49%, rgba(100,100,100,1) 50%, rgba(100,100,100,0.15) 51%)`,
               overflowY: 'hidden',
             }}
           >
             <AssetNode definition={definitionCopy} selected={false} />
           </div>
-          <div style={{position: 'relative', width: dimensions.width, height: 104}}>
-            <div style={{position: 'absolute', width: dimensions.width}}>
+          <div
+            style={{
+              position: 'relative',
+              width: dimensions.width,
+              background: `rgba(100,100,100,0.15)`,
+              height: 90,
+            }}
+          >
+            <div style={{position: 'absolute', width: dimensions.width, transform: 'scale(0.4)'}}>
               <AssetNodeMinimal definition={definitionCopy} selected={false} height={82} />
             </div>
           </div>
-          <code>
-            <strong>{scenario.title}</strong>
-          </code>
         </Box>
       </>
     );
@@ -82,13 +90,15 @@ export const LiveStates = () => {
   return (
     <MockedProvider>
       <AssetLiveDataProvider>
+        <h2>Base Assets</h2>
         <Box flex={{gap: 20, wrap: 'wrap', alignItems: 'flex-start'}}>
           {Mocks.AssetNodeScenariosBase.map(caseWithLiveData)}
         </Box>
-
+        <h2>Source Assets</h2>
         <Box flex={{gap: 20, wrap: 'wrap', alignItems: 'flex-start'}}>
           {Mocks.AssetNodeScenariosSource.map(caseWithLiveData)}
         </Box>
+        <h2>Partitioned Assets</h2>
         <Box flex={{gap: 20, wrap: 'wrap', alignItems: 'flex-start'}}>
           {Mocks.AssetNodeScenariosPartitioned.map(caseWithLiveData)}
         </Box>
@@ -107,9 +117,10 @@ export const Links = () => {
     </Box>
   );
 };
+
 export const PartnerTags = () => {
   const caseWithComputeKind = (computeKind: string) => {
-    const def = {...Mocks.AssetNodeFragmentBasic, computeKind};
+    const def = {...Mocks.AssetNodeFragmentBasic, kinds: [computeKind]};
     const liveData = Mocks.LiveDataForNodeMaterialized;
 
     function SetCacheEntry() {
@@ -142,6 +153,7 @@ export const PartnerTags = () => {
               width: 280,
               height: dimensions.height,
               overflowY: 'hidden',
+              background: `linear-gradient(to bottom, transparent 49%, gray 50%, transparent 51%)`,
             }}
           >
             <AssetNode definition={def} selected={false} />
