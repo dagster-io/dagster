e4e569b084ae27f09c3a044e22358ddb4b6b2600
Maxime Armstrong <maxime@retain.ai>
2024-11-20 15:30:08 -0500

    [dagster-looker] Update custom API translator example in Looker docs

--- examples/docs_snippets/docs_snippets/integrations/looker/customize-looker-assets.py
+++ examples/docs_snippets/docs_snippets/integrations/looker/customize-looker-assets.py
@@ -7,6 +7,7 @@ from dagster_looker import (
 )
 
 import dagster as dg
+from dagster._core.definitions.asset_spec import replace_attributes
 
 looker_resource = LookerResource(
     client_id=dg.EnvVar("LOOKERSDK_CLIENT_ID"),
@@ -17,16 +18,18 @@ looker_resource = LookerResource(
 
 class CustomDagsterLookerApiTranslator(DagsterLookerApiTranslator):
     def get_asset_spec(self, looker_structure: LookerStructureData) -> dg.AssetSpec:
-        asset_spec = super().get_asset_spec(looker_structure)
-
-        # Add a team owner tag for all Looker assets
-        asset_spec = asset_spec._replace(owners=["team:my_team"])
-
-        # For only Looker dashboard, prefix the asset key with "looker" for organizational purposes
-        if looker_structure.structure_type == LookerStructureType.DASHBOARD:
-            asset_spec = asset_spec._replace(key=asset_spec.key.with_prefix("looker"))
-
-        return asset_spec
+        # We create the default asset spec using super()
+        default_spec = super().get_asset_spec(looker_structure)
+        # We customize the team owner tag for all assets,
+        # and we customize the asset key prefix only for dashboards.
+        return replace_attributes(
+            default_spec,
+            key=default_spec.key.with_prefix("looker")
+            if looker_structure.structure_type == LookerStructureType.DASHBOARD
+            else default_spec.key,
+            metadata={**default_spec.metadata, "custom": "metadata"},
+            owners=["team:my_team"],
+        )
 
 
 looker_specs = load_looker_asset_specs(
