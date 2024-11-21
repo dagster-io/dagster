1e976b3d21ac554bed2ff7db617d3e09c4e8998c
Chris DeCarolis <cdguitar817@gmail.com>
2024-11-21 15:38:17 -0800

    [dagster-airlift] Operator Migration guides

--- examples/docs_snippets/docs_snippets/integrations/airlift/operator_migration/using_dbt_assets.py
+++ examples/docs_snippets/docs_snippets/integrations/airlift/operator_migration/using_dbt_assets.py
@@ -5,6 +5,6 @@ from dagster import AssetExecutionContext
 project = DbtProject(project_dir="path/to/dbt_project")
 
 
-@dbt_assets(manifest=project.manifest_path, project=project)
+@dbt_assets(manifest=project.manifest_path)
 def my_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["run"], context=context).stream()
