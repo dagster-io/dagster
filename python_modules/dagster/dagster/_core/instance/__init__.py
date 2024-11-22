21fa9f02e9985e064ba8e84f629f3ddf5a9ddce2
JamieDeMaria <jamie@dagsterlabs.com>
2024-11-22 11:22:59 -0500

    move run retries enabled condition

--- python_modules/dagster/dagster/_core/instance/__init__.py
+++ python_modules/dagster/dagster/_core/instance/__init__.py
@@ -2414,7 +2414,7 @@ class DagsterInstance(DynamicPartitionsStore):
     def should_retry_run(self, run: DagsterRun) -> bool:
         from dagster._core.events import RunFailureReason
 
-        if run.status == DagsterRunStatus.FAILURE and self.run_retries_enabled:
+        if run.status == DagsterRunStatus.FAILURE:
             raw_max_retries_tag = run.tags.get(MAX_RETRIES_TAG)
             if raw_max_retries_tag is None:
                 max_retries = self.run_retries_max_retries
@@ -2505,7 +2505,7 @@ class DagsterInstance(DynamicPartitionsStore):
             ):
                 self._run_storage.handle_run_event(run_id, event.get_dagster_event())
                 run = self.get_run_by_id(run_id)
-                if run and event.get_dagster_event().is_run_failure:
+                if run and event.get_dagster_event().is_run_failure and self.run_retries_enabled:
                     self.add_run_tags(
                         run_id, {WILL_RETRY_TAG: str(self.should_retry_run(run)).lower()}
                     )
