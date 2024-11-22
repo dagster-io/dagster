cbe8c03b7aa60968c53ee433766b0e2726664f77
Chris DeCarolis <cdguitar817@gmail.com>
2024-11-22 13:08:37 -0800

    [dagster-airlift] Airflow-dagster equivalents

--- examples/docs_snippets/docs_snippets/integrations/airlift/operator_migration/bash_operator_general.py
+++ examples/docs_snippets/docs_snippets/integrations/airlift/operator_migration/bash_operator_general.py
@@ -1,6 +1,6 @@
 from airflow.operators.bash import BashOperator
 
 execute_script = BashOperator(
-    task_id='execute_script',
-    bash_command='python /path/to/script.py',
+    task_id="execute_script",
+    bash_command="python /path/to/script.py",
 )
