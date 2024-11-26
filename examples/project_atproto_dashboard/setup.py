0714d0d138eb0c7abee25220d5c70ac58fa5ca6d
Colton Padden <colton@dagsterlabs.com>
2024-11-26 10:11:15 -0500

    make rebuild_pyright_pins

--- examples/project_atproto_dashboard/setup.py
+++ examples/project_atproto_dashboard/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="project_atproto_dashboard",
     packages=find_packages(exclude=["project_atproto_dashboard_tests"]),
-    install_requires=["dagster", "dagster-cloud", "dagster-aws", "atproto", "tenacity"],
+    install_requires=["dagster", "dagster-aws", "atproto", "tenacity"],
     extras_require={"dev": ["dagster-webserver", "pytest", "ruff"]},
 )
