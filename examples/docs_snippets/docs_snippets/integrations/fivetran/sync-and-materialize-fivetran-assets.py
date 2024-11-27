3eff2f53017105dbeca1810ab6a87a4058874f4a
Maxime Armstrong <maxime@retain.ai>
2024-11-27 14:23:37 -0500

    Add materialize fivetran assets section

---
+++ examples/docs_snippets/docs_snippets/integrations/fivetran/sync-and-materialize-fivetran-assets.py
@@ -0,0 +1,27 @@
+from dagster_fivetran import FivetranWorkspace, fivetran_assets
+
+import dagster as dg
+
+fivetran_workspace = FivetranWorkspace(
+    account_id=dg.EnvVar("FIVETRAN_ACCOUNT_ID"),
+    api_key=dg.EnvVar("FIVETRAN_API_KEY"),
+    api_secret=dg.EnvVar("FIVETRAN_API_SECRET"),
+)
+
+
+@fivetran_assets(
+    connector_id="fivetran_connector_id",
+    name="fivetran_connector_id",
+    group_name="fivetran_connector_id",
+    workspace=fivetran_workspace,
+)
+def fivetran_connector_assets(
+    context: dg.AssetExecutionContext, fivetran: FivetranWorkspace
+):
+    yield from fivetran.sync_and_poll(context=context)
+
+
+defs = dg.Definitions(
+    assets=[fivetran_connector_assets],
+    resources={"fivetran": fivetran_workspace},
+)
