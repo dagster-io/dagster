fddda656b0c9fbfcd00c5a81a6a7b2d00a9e993a
Chris DeCarolis <cdguitar817@gmail.com>
2024-11-27 11:24:07 -0800

    [dagster-azure] Generate URL from compute log manager

--- integration_tests/test_suites/dagster-azure-live-tests/integration_tests/test_compute_log_manager.py
+++ integration_tests/test_suites/dagster-azure-live-tests/integration_tests/test_compute_log_manager.py
@@ -4,34 +4,69 @@ from typing import Generator
 
 import pytest
 from azure.identity import ClientSecretCredential
-from azure.storage.blob import ContainerClient
+from azure.storage.blob import BlobClient, ContainerClient
+from dagster import (
+    DagsterEventType,
+    DagsterInstance,
+    EventRecordsFilter,
+    _check as check,
+)
 from dagster_azure.blob.utils import create_blob_client
 
 
 @pytest.fixture
-def container_client() -> Generator[ContainerClient, None, None]:
+def credentials() -> ClientSecretCredential:
+    return ClientSecretCredential(
+        tenant_id=os.environ["TEST_AZURE_TENANT_ID"],
+        client_id=os.environ["TEST_AZURE_CLIENT_ID"],
+        client_secret=os.environ["TEST_AZURE_CLIENT_SECRET"],
+    )
+
+
+@pytest.fixture
+def container_client(credentials: ClientSecretCredential) -> Generator[ContainerClient, None, None]:
     yield create_blob_client(
         storage_account="chriscomplogmngr",
-        credential=ClientSecretCredential(
-            tenant_id=os.environ["TEST_AZURE_TENANT_ID"],
-            client_id=os.environ["TEST_AZURE_CLIENT_ID"],
-            client_secret=os.environ["TEST_AZURE_CLIENT_SECRET"],
-        ),
+        credential=credentials,
     ).get_container_client("mycontainer")
 
 
 def test_compute_log_manager(
-    dagster_dev: subprocess.Popen, container_client: ContainerClient, prefix_env: str
+    dagster_dev: subprocess.Popen,
+    container_client: ContainerClient,
+    prefix_env: str,
+    credentials: ClientSecretCredential,
 ) -> None:
     subprocess.run(
         ["dagster", "asset", "materialize", "--select", "my_asset", "-m", "azure_test_proj.defs"],
         check=True,
     )
-    blobs = list(container_client.list_blobs(name_starts_with=f"{prefix_env}/storage"))
-    assert len(blobs) == 2
-    assert len([blob for blob in blobs if blob.name.endswith(".err")]) == 1
-    assert len([blob for blob in blobs if blob.name.endswith(".out")]) == 1
-    stdout = container_client.download_blob(blob=blobs[0].name).readall().decode()
-    stderr = container_client.download_blob(blob=blobs[1].name).readall().decode()
-    assert stdout.count("Logging using context") == 10
-    assert stderr.count("Printing without context") == 10
+    logs_captured_data = check.not_none(
+        DagsterInstance.get()
+        .get_event_records(
+            EventRecordsFilter(
+                event_type=DagsterEventType.LOGS_CAPTURED,
+            )
+        )[0]
+        .event_log_entry.dagster_event
+    ).logs_captured_data
+
+    assert logs_captured_data.external_stderr_url
+    assert logs_captured_data.external_stdout_url
+
+    stderr = (
+        BlobClient.from_blob_url(logs_captured_data.external_stderr_url, credential=credentials)
+        .download_blob()
+        .readall()
+        .decode()
+    )
+
+    stdout = (
+        BlobClient.from_blob_url(logs_captured_data.external_stdout_url, credential=credentials)
+        .download_blob()
+        .readall()
+        .decode()
+    )
+
+    assert stdout.count("Printing without context") == 10
+    assert stderr.count("Logging using context") == 10
