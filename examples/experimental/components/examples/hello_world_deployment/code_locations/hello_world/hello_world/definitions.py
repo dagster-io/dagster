340f97d448d38701a5e38d1f64ab47bb014d556d
Nick Schrock <schrockn@dagsterlabs.com>
2024-12-01 16:40:43 -0500

    [components] Rename to be pipes-specific and provide resource out of the box (#26200)
    ## Summary & Motivation
    
    Renames PythonScriptCollection to PipesSubprocessScriptCollection which is more accurate and specific. Also have it provide its own pipes client for now for convenience.
    
    ## How I Tested These Changes
    
    dagster dev hello and BK

--- examples/experimental/components/examples/hello_world_deployment/code_locations/hello_world/hello_world/definitions.py
+++ examples/experimental/components/examples/hello_world_deployment/code_locations/hello_world/hello_world/definitions.py
@@ -1,14 +1,16 @@
 from pathlib import Path
 
 from dagster._components import ComponentRegistry, build_defs_from_toplevel_components_folder
-from dagster._components.impls.python_script_component import PythonScriptCollection
+from dagster._components.impls.pipes_subprocess_script_collection import (
+    PipesSubprocessScriptCollection,
+)
 from dagster._core.definitions.definitions_class import Definitions
-from dagster._core.pipes.subprocess import PipesSubprocessClient
 
 defs = build_defs_from_toplevel_components_folder(
     path=Path(__file__).parent,
-    registry=ComponentRegistry({"python_script_collection": PythonScriptCollection}),
-    resources={"pipes_client": PipesSubprocessClient()},
+    registry=ComponentRegistry(
+        {"pipes_subprocess_script_collection": PipesSubprocessScriptCollection}
+    ),
 )
 
 if __name__ == "__main__":
--- python_modules/dagster/dagster_tests/components_tests/test_pipes_subprocess_script_collection.py
+++ python_modules/dagster/dagster_tests/components_tests/test_pipes_subprocess_script_collection.py
@@ -6,28 +6,32 @@ from dagster._components import (
     ComponentLoadContext,
     build_defs_from_component_folder,
 )
-from dagster._components.impls.python_script_component import PythonScriptCollection
+from dagster._components.impls.pipes_subprocess_script_collection import (
+    PipesSubprocessScriptCollection,
+)
 from dagster._core.pipes.subprocess import PipesSubprocessClient
 
 LOCATION_PATH = Path(__file__).parent / "code_locations" / "python_script_location"
 init_context = ComponentInitContext(path=LOCATION_PATH / "scripts")
-init_context.registry.register("python_script_collection", PythonScriptCollection)
+init_context.registry.register(
+    "pipes_subprocess_script_collection", PipesSubprocessScriptCollection
+)
 
 
 def _assert_assets(component: Component, expected_assets: int) -> None:
-    defs = component.build_defs(ComponentLoadContext({"pipes_client": PipesSubprocessClient()}))
+    defs = component.build_defs(ComponentLoadContext())
     assert len(defs.get_asset_graph().get_all_asset_keys()) == expected_assets
     result = defs.get_implicit_global_asset_job_def().execute_in_process()
     assert result.success
 
 
 def test_python_native() -> None:
-    component = PythonScriptCollection(LOCATION_PATH / "scripts")
+    component = PipesSubprocessScriptCollection(LOCATION_PATH / "scripts")
     _assert_assets(component, 3)
 
 
 def test_python_params() -> None:
-    component = PythonScriptCollection.from_component_params(
+    component = PipesSubprocessScriptCollection.from_component_params(
         init_context=init_context,
         component_params={
             "script_one": {
