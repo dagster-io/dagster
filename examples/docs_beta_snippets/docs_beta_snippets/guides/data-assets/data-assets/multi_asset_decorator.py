69f6953fb7585b2493f6e3e02cfca106101a48e3
alangenfeld <alex@elementl.com>
2024-12-02 17:26:30 -0600

    [new docs] fix multi_asset

--- examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/data-assets/multi_asset_decorator.py
+++ examples/docs_beta_snippets/docs_beta_snippets/guides/data-assets/data-assets/multi_asset_decorator.py
@@ -3,8 +3,8 @@ import dagster as dg
 
 @dg.multi_asset(specs=[dg.AssetSpec("asset_one"), dg.AssetSpec("asset_two")])
 def my_multi_asset():
-    dg.MaterializeResult(asset_key="asset_one", metadata={"num_rows": 10})
-    dg.MaterializeResult(asset_key="asset_two", metadata={"num_rows": 24})
+    yield dg.MaterializeResult(asset_key="asset_one", metadata={"num_rows": 10})
+    yield dg.MaterializeResult(asset_key="asset_two", metadata={"num_rows": 24})
 
 
 defs = dg.Definitions(assets=[my_multi_asset])
