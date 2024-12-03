d288e73ce427fb296b6d17a6cd47e67009d23ec3
prha <philrha@gmail.com>
2024-12-02 16:53:43 -0800

    thread concurrency key into execution step

--- python_modules/dagster/dagster/_core/execution/plan/step.py
+++ python_modules/dagster/dagster/_core/execution/plan/step.py
@@ -90,6 +90,11 @@ class IExecutionStep:
 
     @property
     @abstractmethod
+    def concurrency_key(self) -> Optional[str]:
+        pass
+
+    @property
+    @abstractmethod
     def step_inputs(
         self,
     ) -> Sequence[Union[StepInput, UnresolvedCollectStepInput, UnresolvedMappedStepInput]]:
@@ -132,6 +137,7 @@ class ExecutionStep(
             ("tags", Mapping[str, str]),
             ("logging_tags", Mapping[str, str]),
             ("key", str),
+            ("concurrency_key", Optional[str]),
         ],
     ),
     IExecutionStep,
@@ -145,6 +151,7 @@ class ExecutionStep(
         step_inputs: Sequence[StepInput],
         step_outputs: Sequence[StepOutput],
         tags: Optional[Mapping[str, str]],
+        concurrency_key: Optional[str],
         logging_tags: Optional[Mapping[str, str]] = None,
         key: Optional[str] = None,
     ):
@@ -161,6 +168,7 @@ class ExecutionStep(
                 for so in check.sequence_param(step_outputs, "step_outputs", of_type=StepOutput)
             },
             tags=tags or {},
+            concurrency_key=check.opt_str_param(concurrency_key, "concurrency_key"),
             logging_tags=merge_dicts(
                 {
                     "step_key": handle.to_key(),
@@ -231,6 +239,7 @@ class UnresolvedMappedExecutionStep(
             ("step_input_dict", Mapping[str, Union[StepInput, UnresolvedMappedStepInput]]),
             ("step_output_dict", Mapping[str, StepOutput]),
             ("tags", Mapping[str, str]),
+            ("concurrency_key", Optional[str]),
         ],
     ),
     IExecutionStep,
@@ -244,6 +253,7 @@ class UnresolvedMappedExecutionStep(
         step_inputs: Sequence[Union[StepInput, UnresolvedMappedStepInput]],
         step_outputs: Sequence[StepOutput],
         tags: Optional[Mapping[str, str]],
+        concurrency_key: Optional[str],
     ):
         return super(UnresolvedMappedExecutionStep, cls).__new__(
             cls,
@@ -260,6 +270,7 @@ class UnresolvedMappedExecutionStep(
                 for so in check.sequence_param(step_outputs, "step_outputs", of_type=StepOutput)
             },
             tags=check.opt_mapping_param(tags, "tags", key_type=str),
+            concurrency_key=check.opt_str_param(concurrency_key, "concurrency_key"),
         )
 
     @property
@@ -364,6 +375,7 @@ class UnresolvedMappedExecutionStep(
                     step_inputs=resolved_inputs,
                     step_outputs=self.step_outputs,
                     tags=self.tags,
+                    concurrency_key=self.concurrency_key,
                 )
             )
 
@@ -389,6 +401,7 @@ class UnresolvedCollectExecutionStep(
             ("step_input_dict", Mapping[str, Union[StepInput, UnresolvedCollectStepInput]]),
             ("step_output_dict", Mapping[str, StepOutput]),
             ("tags", Mapping[str, str]),
+            ("concurrency_key", Optional[str]),
         ],
     ),
     IExecutionStep,
@@ -402,6 +415,7 @@ class UnresolvedCollectExecutionStep(
         step_inputs: Sequence[Union[StepInput, UnresolvedCollectStepInput]],
         step_outputs: Sequence[StepOutput],
         tags: Optional[Mapping[str, str]],
+        concurrency_key: Optional[str],
     ):
         return super(UnresolvedCollectExecutionStep, cls).__new__(
             cls,
@@ -418,6 +432,7 @@ class UnresolvedCollectExecutionStep(
                 for so in check.sequence_param(step_outputs, "step_outputs", of_type=StepOutput)
             },
             tags=check.opt_mapping_param(tags, "tags", key_type=str),
+            concurrency_key=check.opt_str_param(concurrency_key, "concurrency_key"),
         )
 
     @property
@@ -499,4 +514,5 @@ class UnresolvedCollectExecutionStep(
             step_inputs=resolved_inputs,
             step_outputs=self.step_outputs,
             tags=self.tags,
+            concurrency_key=self.concurrency_key,
         )
