86b8e31d34ced1692523a0f8b0027c771e228675
JamieDeMaria <jamie@dagsterlabs.com>
2024-12-06 12:04:24 -0500

    fix indent

--- python_modules/dagster/dagster/_core/execution/job_backfill.py
+++ python_modules/dagster/dagster/_core/execution/job_backfill.py
@@ -73,7 +73,7 @@ def execute_job_backfill_iteration(
 
         if not isinstance(all_runs_canceled, bool):
             check.failed(
-                "Expected cancel_backfill_runs_and_cancellation_complete to return an boolean"
+                "Expected cancel_backfill_runs_and_cancellation_complete to return a boolean"
             )
 
         if all_runs_canceled:
