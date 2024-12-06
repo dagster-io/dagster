0847cd67335e5c02665dccb0fd0d73bdb0c165c0
Owen Kephart <owenkephart@gmail.com>
2024-12-06 14:47:05 -0800

    [components] undelete examples/components

---
+++ examples/experimental/components/examples/hello_world_deployment/code_locations/hello_world/hello_world/definitions.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import dagster as dg
+from dagster_components import ComponentRegistry, build_defs_from_toplevel_components_folder
+from dagster_components.impls.pipes_subprocess_script_collection import (
+    PipesSubprocessScriptCollection,
+)
+
+defs = build_defs_from_toplevel_components_folder(
+    path=Path(__file__).parent,
+    registry=ComponentRegistry(
+        {"pipes_subprocess_script_collection": PipesSubprocessScriptCollection}
+    ),
+)
+
+if __name__ == "__main__":
+    dg.Definitions.validate_loadable(defs)
