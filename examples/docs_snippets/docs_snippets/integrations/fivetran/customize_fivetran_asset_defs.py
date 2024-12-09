23c93cc625900cade75e0190bcd528a9e29ae52f
Maxime Armstrong <maxime@retain.ai>
2024-12-09 17:30:04 -0500

    Update docs

--- examples/docs_snippets/docs_snippets/integrations/fivetran/customize_fivetran_asset_defs.py
+++ examples/docs_snippets/docs_snippets/integrations/fivetran/customize_fivetran_asset_defs.py
@@ -1,10 +1,7 @@
 from dagster_fivetran import (
-    DagsterFivetranTranslator,
-    FivetranConnectorTableProps,
     FivetranWorkspace,
     build_fivetran_assets_definitions,
     fivetran_assets,
-    load_fivetran_asset_specs,
 )
 
 import dagster as dg
@@ -16,45 +13,20 @@ fivetran_workspace = FivetranWorkspace(
 )
 
 
-# A translator class lets us customize properties of the built
-# Fivetran assets, such as the owners or asset key
-class MyCustomFivetranTranslator(DagsterFivetranTranslator):
-    def get_asset_spec(self, props: FivetranConnectorTableProps) -> dg.AssetSpec:
-        # We create the default asset spec using super()
-        default_spec = super().get_asset_spec(props)
-        # We customize the metadata, asset key prefix and team owner tag for all assets
-        return default_spec.replace_attributes(
-            key=default_spec.key.with_prefix("prefix"),
-            owners=["team:my_team"],
-        ).merge_attributes(metadata={"custom": "metadata"})
-
-
-# When loading asset specs
-fivetran_specs = load_fivetran_asset_specs(
-    fivetran_workspace, dagster_fivetran_translator=MyCustomFivetranTranslator()
-)
-
-
-# Alternatively, when creating assets definition for a given connector using the `@fivetran_assets` decorator
 @fivetran_assets(
     connector_id="fivetran_connector_id",
     name="fivetran_connector_id",
     group_name="fivetran_connector_id",
     workspace=fivetran_workspace,
-    dagster_fivetran_translator=MyCustomFivetranTranslator(),
 )
 def fivetran_connector_assets(
     context: dg.AssetExecutionContext, fivetran: FivetranWorkspace
 ):
+    # Do something before the materialization...
     yield from fivetran.sync_and_poll(context=context)
 
 
-# Alternatively, when creating all assets definitions for the Fivetran workspace
-# using the `build_fivetran_assets_definitions` factory
-all_fivetran_assets = build_fivetran_assets_definitions(
-    fivetran_workspace, dagster_fivetran_translator=MyCustomFivetranTranslator()
-)
-
 defs = dg.Definitions(
-    assets=all_fivetran_assets, resources={"fivetran": fivetran_workspace}
+    assets=[fivetran_connector_assets],
+    resources={"fivetran": fivetran_workspace},
 )
