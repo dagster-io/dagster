0ecfb10622847b62413494623fafb83244cbab06
Maxime Armstrong <maxime@retain.ai>
2024-12-10 17:52:41 -0500

    [dagster-fivetran] Implement FivetranEventIterator

---
+++ python_modules/libraries/dagster-fivetran/dagster_fivetran/fivetran_event_iterator.py
@@ -0,0 +1,32 @@
+from collections import abc
+from typing import TYPE_CHECKING, Generic, Iterator, Union
+
+from dagster import AssetMaterialization, MaterializeResult
+from typing_extensions import TypeVar
+
+if TYPE_CHECKING:
+    from dagster_fivetran.resources import FivetranWorkspace
+
+
+FivetranEventType = Union[AssetMaterialization, MaterializeResult]
+T = TypeVar("T", bound=FivetranEventType)
+
+
+class FivetranEventIterator(Generic[T], abc.Iterator):
+    """A wrapper around an iterator of Fivetran events which contains additional methods for
+    post-processing the events, such as fetching column metadata.
+    """
+
+    def __init__(
+        self,
+        events: Iterator[T],
+        fivetran_workspace: "FivetranWorkspace",
+    ) -> None:
+        self._inner_iterator = events
+        self._fivetran_workspace = fivetran_workspace
+
+    def __next__(self) -> T:
+        return next(self._inner_iterator)
+
+    def __iter__(self) -> "FivetranEventIterator[T]":
+        return self
