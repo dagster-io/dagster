54b39a2595408ae723e77830919fc3219f268c70
Christopher DeCarolis <cdguitar817@gmail.com>
2024-12-11 08:10:02 -0800

    [dagster-airlift] Standardize asset key interpolation (#26141)
    In response to recent product review where we decided to standardize the
    interpolation of asset keys.

--- python_modules/libraries/dagster-airlift/dagster_airlift/in_airflow/materialize_assets_operator.py
+++ python_modules/libraries/dagster-airlift/dagster_airlift/in_airflow/materialize_assets_operator.py
@@ -1,8 +1,12 @@
+import re
 from typing import Any, Iterable, Mapping, Sequence, Union
 
 from airflow.utils.context import Context
 
 from dagster_airlift.in_airflow.base_asset_operator import BaseDagsterAssetsOperator
+
+UNESCAPED_SLASH_RE = re.compile(r"(?<!\\)/")
+ESCAPED_SLASH = "\\/"
 
 
 class BaseMaterializeAssetsOperator(BaseDagsterAssetsOperator):
@@ -12,13 +16,14 @@ class BaseMaterializeAssetsOperator(BaseDagsterAssetsOperator):
     Args:
         asset_key_paths (Sequence[Union[str, Sequence[str]]]): A sequence of asset key paths to materialize.
             Each path in the sequence can be a string, which is treated as an asset key path with a single
-            component, or a sequence of strings representing a path with multiple components. For more,
+            component per "/" key, or a sequence of strings representing a path with multiple components. For more,
             see the docs on asset keys: https://docs.dagster.io/concepts/assets/software-defined-assets#multi-component-asset-keys
     """
 
     def __init__(self, asset_key_paths: Sequence[Union[str, Sequence[str]]], *args, **kwargs):
         self.asset_key_paths = [
-            (path,) if isinstance(path, str) else tuple(path) for path in asset_key_paths
+            _get_path_from_str(path) if isinstance(path, str) else tuple(path)
+            for path in asset_key_paths
         ]
         super().__init__(*args, **kwargs)
 
@@ -31,3 +36,8 @@ class BaseMaterializeAssetsOperator(BaseDagsterAssetsOperator):
                 f"Could not find all asset key paths {self.asset_key_paths} in the asset nodes. Found: {list(hashable_path_to_node.keys())}"
             )
         yield from [hashable_path_to_node[path] for path in self.asset_key_paths]
+
+
+def _get_path_from_str(path_str: str) -> tuple:
+    parts = re.split(UNESCAPED_SLASH_RE, path_str)
+    return tuple(part.replace(ESCAPED_SLASH, "/") for part in parts)
