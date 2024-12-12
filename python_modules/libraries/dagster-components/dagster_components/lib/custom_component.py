7f1736fac4a850699158f374eb1f67ef84be8d0f
Nick Schrock <schrockn@dagsterlabs.com>
2024-12-12 06:32:15 -0500

    cp

--- python_modules/libraries/dagster-components/dagster_components/lib/custom_component.py
+++ python_modules/libraries/dagster-components/dagster_components/lib/custom_component.py
@@ -1,30 +1,34 @@
 from abc import abstractmethod
-from typing import Any, Optional, Mapping
-from typing_extensions import Self
-from dagster._core.definitions.definitions_class import Definitions
+from typing import TYPE_CHECKING, Any, Mapping, Optional
 
+from pydantic import BaseModel
 
 from dagster_components.core.component import (
     Component,
+    ComponentGenerateRequest,
     ComponentLoadContext,
     component,
-    ComponentDeclNode,
-    ComponentGenerateRequest,
 )
+from dagster_components.generate import generate_component_yaml
+
+if TYPE_CHECKING:
+    from dagster._core.definitions.definitions_class import Definitions
+
+CUSTOM_COMPONENT_TEMPLATE = """
+"""
+
+
+class GenerateCustomComponentParams(BaseModel):
+    class_name: str
 
 
 @component(name="custom_component")
 class CustomComponent(Component):
     @classmethod
     def generate_files(
-        cls, request: ComponentGenerateRequest, params: Any
-    ) -> Optional[Mapping[str, Any]]: ...
+        cls, request: ComponentGenerateRequest, params: GenerateCustomComponentParams
+    ) -> Optional[Mapping[str, Any]]:
+        generate_component_yaml(request, {})
 
     @abstractmethod
     def build_defs(self, context: "ComponentLoadContext") -> "Definitions": ...
-
-    @classmethod
-    @abstractmethod
-    def from_decl_node(
-        cls, context: "ComponentLoadContext", decl_node: "ComponentDeclNode"
-    ) -> Self: ...
