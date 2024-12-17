a0cf2c91c9091c821a1bdf452971e183c3c59b8f
Alex Noonan <a.noonan@dagsterlabs.com>
2024-12-16 20:45:36 -0500

    changed dbt model group

--- examples/project_atproto_dashboard/dbt_project/models/modeling/latest_feed.sql
+++ examples/project_atproto_dashboard/dbt_project/models/modeling/latest_feed.sql
@@ -19,10 +19,11 @@ WITH max_update AS (
 final AS (
     SELECT
         json_extract_string(sfs.json, '$.post.author.handle') AS author_handle,
+        json_extract_string(sfs.json, '$.post.author.did') AS author_id,
         cast(sfs.json.post.like_count AS int) AS likes,
         cast(sfs.json.post.quote_count AS int) AS quotes,
         cast(sfs.json.post.reply_count AS int) AS replies,
-        sfs.json.post.record.text AS post_text,
+        json_extract_string(sfs.json, '$.post.record.text') AS post_text,
         sfs.json.post.record.embed,
         json_extract_string(
             sfs.json, '$.post.record.embed.external.description'
@@ -31,6 +32,10 @@ final AS (
             AS external_embed_link,
         sfs.json.post.record.embed.external.thumb AS external_embed_thumbnail,
         cast(sfs.json.post.record.created_at AS timestamp) AS created_at,
+        CASE 
+	        WHEN json_extract_string(sfs.json.post.record.embed, '$.images[0].image.ref.link') IS NULL THEN NULL
+            ELSE concat('https://cdn.bsky.app/img/feed_thumbnail/plain/', json_extract_string(sfs.json, '$.post.author.did') ,'/' ,json_extract_string(sfs.json.post.record.embed, '$.images[0].image.ref.link'), '@jpeg')
+        END AS image_url,
         max_update.max_extracted_timestamp,
         max_update.profile_id
     FROM {{ ref("stg_feed_snapshots") }} sfs
