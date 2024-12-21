afa3bffd9680ff4473cf2241ce4f4229bfa0f4ad
Nick Schrock <schrockn@dagsterlabs.com>
2024-12-20 16:47:47 -0800

    better test structure

--- python_modules/libraries/dagster-components/dagster_components_tests/integration_tests/test_definitions_component.py
+++ python_modules/libraries/dagster-components/dagster_components_tests/integration_tests/test_definitions_component.py
@@ -2,6 +2,7 @@ import importlib
 from pathlib import Path
 
 from dagster._core.definitions.asset_key import AssetKey
+from dagster._core.definitions.definitions_class import Definitions
 from dagster_components.core.component import ComponentRegistry, get_registered_components_in_module
 from dagster_components.core.component_defs_builder import (
     build_defs_from_component_path,
@@ -10,7 +11,16 @@ from dagster_components.core.component_defs_builder import (
 from dagster_components.core.deployment import CodeLocationProjectContext
 
 
-def test_basic_load() -> None:
+def load_test_component_defs(name: str) -> Definitions:
+    context = load_test_component_project_context()
+    return build_defs_from_component_path(
+        path=Path(__file__).parent / "components" / name,
+        registry=context.component_registry,
+        resources={},
+    )
+
+
+def load_test_component_project_context() -> CodeLocationProjectContext:
     package_name = "dagster_components.lib"
     dc_module = importlib.import_module(package_name)
 
@@ -19,21 +29,14 @@ def test_basic_load() -> None:
         key = f"dagster_components.{get_component_name(component)}"
         components[key] = component
 
-    # import code
-
-    # code.interact(local=locals())
-
-    context = CodeLocationProjectContext(
+    return CodeLocationProjectContext(
         root_path=str(Path(__file__).parent),
         name="test",
         component_registry=ComponentRegistry(components),
-        components_folder=(Path(__file__).parent / "components"),
+        components_folder=Path(__file__).parent / "components",
     )
 
-    defs = build_defs_from_component_path(
-        path=Path(__file__).parent / "components" / "definitions_instance_one_default_file",
-        registry=context.component_registry,
-        resources={},
-    )
 
+def test_definitions_component_with_default_file() -> None:
+    defs = load_test_component_defs("definitions_instance_one_default_file")
     assert {spec.key for spec in defs.get_all_asset_specs()} == {AssetKey("an_asset")}
