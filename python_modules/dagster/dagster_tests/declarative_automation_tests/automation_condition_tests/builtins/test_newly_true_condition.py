ff687740eb71a9ae9c833a020271ade59a5b97e3
Daniel Gibson <daniel@elementl.com>
2024-12-31 15:16:03 -0600

    Move declarative_automation_tests into its own test suite
    Summary:
    definitions_tests is semi-regularly timing out and this appears to be the source of the slowest tests. I thin kthis feature is grown up enough now that it deserves its own test suite.
    
    Bk, monitor runtimes
    
    > Insert changelog entry or delete this section.

---
+++ python_modules/dagster/dagster_tests/declarative_automation_tests/automation_condition_tests/builtins/test_newly_true_condition.py
@@ -0,0 +1,58 @@
+import pytest
+from dagster._core.definitions.asset_key import AssetKey
+from dagster._core.definitions.events import AssetKeyPartitionKey
+
+from dagster_tests.declarative_automation_tests.automation_condition_tests.builtins.test_dep_condition import (
+    get_hardcoded_condition,
+)
+from dagster_tests.declarative_automation_tests.scenario_utils.automation_condition_scenario import (
+    AutomationConditionScenarioState,
+)
+from dagster_tests.declarative_automation_tests.scenario_utils.scenario_specs import one_asset
+
+
+@pytest.mark.asyncio
+async def test_newly_true_condition() -> None:
+    inner_condition, true_set = get_hardcoded_condition()
+
+    condition = inner_condition.newly_true()
+    state = AutomationConditionScenarioState(one_asset, automation_condition=condition)
+
+    # nothing true
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 0
+
+    # becomes true
+    true_set.add(AssetKeyPartitionKey(AssetKey("A")))
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 1
+
+    # now on the next tick, this asset is no longer newly true
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 0
+
+    # see above
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 0
+
+    # see above
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 0
+
+    # now condition becomes false, result still false
+    true_set.remove(AssetKeyPartitionKey(AssetKey("A")))
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 0
+
+    # see above
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 0
+
+    # becomes true again
+    true_set.add(AssetKeyPartitionKey(AssetKey("A")))
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 1
+
+    # no longer newly true
+    state, result = await state.evaluate("A")
+    assert result.true_subset.size == 0
---
+++ python_modules/dagster/dagster_tests/declarative_automation_tests/legacy_tests/scenarios/scenarios.py
@@ -0,0 +1,66 @@
+from dagster import Definitions
+from dagster._core.definitions.executor_definition import in_process_executor
+
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.active_run_scenarios import (
+    active_run_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.auto_materialize_policy_scenarios import (
+    auto_materialize_policy_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.auto_observe_scenarios import (
+    auto_observe_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.basic_scenarios import (
+    basic_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.definition_change_scenarios import (
+    definition_change_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.freshness_policy_scenarios import (
+    freshness_policy_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.multi_code_location_scenarios import (
+    multi_code_location_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.observable_source_asset_scenarios import (
+    observable_source_asset_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.partition_scenarios import (
+    partition_scenarios,
+)
+from dagster_tests.declarative_automation_tests.legacy_tests.scenarios.version_scenarios import (
+    version_scenarios,
+)
+
+ASSET_RECONCILIATION_SCENARIOS = {
+    **partition_scenarios,
+    **basic_scenarios,
+    **freshness_policy_scenarios,
+    **auto_materialize_policy_scenarios,
+    **observable_source_asset_scenarios,
+    **definition_change_scenarios,
+    **active_run_scenarios,
+    **version_scenarios,
+}
+
+DAEMON_ONLY_SCENARIOS = {
+    **multi_code_location_scenarios,
+    **auto_observe_scenarios,
+}
+
+
+# put repos in the global namespace so that the daemon can load them with LoadableTargetOrigin
+for scenario_name, scenario in {**ASSET_RECONCILIATION_SCENARIOS, **DAEMON_ONLY_SCENARIOS}.items():
+    if scenario.code_locations is not None:
+        assert scenario.assets is None
+
+        for location_name, assets in scenario.code_locations.items():
+            d = Definitions(assets=assets, executor=in_process_executor)
+            globals()["hacky_daemon_repo_" + scenario_name + "_" + location_name] = (
+                d.get_repository_def()
+            )
+    else:
+        assert scenario.code_locations is None
+
+        d = Definitions(assets=scenario.assets, executor=in_process_executor)
+        globals()["hacky_daemon_repo_" + scenario_name] = d.get_repository_def()
