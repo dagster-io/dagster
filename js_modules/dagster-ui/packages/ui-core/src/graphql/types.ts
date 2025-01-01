7b225c285f99c1985af171b7165bdae2accbf3c8
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-01-01 07:32:35 -0700

    Generate after making changes to GraphQL fragments

--- js_modules/dagster-ui/packages/ui-core/src/graphql/types.ts
+++ js_modules/dagster-ui/packages/ui-core/src/graphql/types.ts
@@ -16,6 +16,7 @@ export type Scalars = {
   Boolean: {input: boolean; output: boolean};
   Int: {input: number; output: number};
   Float: {input: number; output: number};
+  BigInt: {input: any; output: any};
   Cursor: {input: any; output: any};
   GenericScalar: {input: any; output: any};
   RunConfigData: {input: any; output: any};
@@ -223,7 +224,7 @@ export type AssetCheckEvaluationPlannedEvent = MessageEvent &
 export type AssetCheckEvaluationTargetMaterializationData = {
   __typename: 'AssetCheckEvaluationTargetMaterializationData';
   runId: Scalars['String']['output'];
-  storageId: Scalars['Int']['output'];
+  storageId: Scalars['BigInt']['output'];
   timestamp: Scalars['Float']['output'];
 };
 
@@ -3375,7 +3376,7 @@ export type PendingConcurrencyStep = {
   __typename: 'PendingConcurrencyStep';
   assignedTimestamp: Maybe<Scalars['Float']['output']>;
   enqueuedTimestamp: Scalars['Float']['output'];
-  priority: Maybe<Scalars['Int']['output']>;
+  priority: Maybe<Scalars['BigInt']['output']>;
   runId: Scalars['String']['output'];
   stepKey: Scalars['String']['output'];
 };
@@ -6133,7 +6134,8 @@ export const buildAssetCheckEvaluationTargetMaterializationData = (
   return {
     __typename: 'AssetCheckEvaluationTargetMaterializationData',
     runId: overrides && overrides.hasOwnProperty('runId') ? overrides.runId! : 'exercitationem',
-    storageId: overrides && overrides.hasOwnProperty('storageId') ? overrides.storageId! : 3254,
+    storageId:
+      overrides && overrides.hasOwnProperty('storageId') ? overrides.storageId! : 'voluptas',
     timestamp: overrides && overrides.hasOwnProperty('timestamp') ? overrides.timestamp! : 3.87,
   };
 };
@@ -11487,7 +11489,7 @@ export const buildPendingConcurrencyStep = (
       overrides && overrides.hasOwnProperty('enqueuedTimestamp')
         ? overrides.enqueuedTimestamp!
         : 1.74,
-    priority: overrides && overrides.hasOwnProperty('priority') ? overrides.priority! : 8863,
+    priority: overrides && overrides.hasOwnProperty('priority') ? overrides.priority! : 'rerum',
     runId: overrides && overrides.hasOwnProperty('runId') ? overrides.runId! : 'facere',
     stepKey: overrides && overrides.hasOwnProperty('stepKey') ? overrides.stepKey! : 'fuga',
   };
