82bb0802ab332a4e73f7920b99b6923a192c06e4
Maxime Armstrong <maxime@retain.ai>
2025-01-02 12:05:51 -0500

    Add deprecation warning for asset factory

--- python_modules/libraries/dagster-looker/dagster_looker/api/assets.py
+++ python_modules/libraries/dagster-looker/dagster_looker/api/assets.py
@@ -1,7 +1,8 @@
-from typing import Optional, Sequence, cast
+from typing import Optional, Sequence, cast, Union, Type
 
 from dagster import AssetExecutionContext, AssetsDefinition, Failure, multi_asset
 from dagster._annotations import experimental
+from dagster._utils.warnings import deprecation_warning
 
 from dagster_looker.api.dagster_looker_api_translator import (
     DagsterLookerApiTranslator,
@@ -18,7 +19,9 @@ from dagster_looker.api.resource import LookerResource
 def build_looker_pdt_assets_definitions(
     resource_key: str,
     request_start_pdt_builds: Sequence[RequestStartPdtBuild],
-    dagster_looker_translator: Optional[DagsterLookerApiTranslator] = None,
+    dagster_looker_translator: Optional[
+        Union[DagsterLookerApiTranslator, Type[DagsterLookerApiTranslator]]
+    ] = None,
 ) -> Sequence[AssetsDefinition]:
     """Returns the AssetsDefinitions of the executable assets for the given the list of refreshable PDTs.
 
@@ -27,13 +30,23 @@ def build_looker_pdt_assets_definitions(
         request_start_pdt_builds (Optional[Sequence[RequestStartPdtBuild]]): A list of requests to start PDT builds.
             See https://developers.looker.com/api/explorer/4.0/types/DerivedTable/RequestStartPdtBuild?sdk=py
             for documentation on all available fields.
-        dagster_looker_translator (Optional[DagsterLookerApiTranslator]): The translator to use
-            to convert Looker structures into :py:class:`dagster.AssetSpec`.
+        dagster_looker_translator (Optional[Union[DagsterLookerApiTranslator, Type[DagsterLookerApiTranslator]]]):
+            The translator to use to convert Looker structures into :py:class:`dagster.AssetSpec`.
             Defaults to :py:class:`DagsterLookerApiTranslator`.
 
     Returns:
         AssetsDefinition: The AssetsDefinitions of the executable assets for the given the list of refreshable PDTs.
     """
+    if isinstance(dagster_looker_translator, type):
+        deprecation_warning(
+            subject="Support of `dagster_looker_translator` as a Type[DagsterLookerApiTranslator]",
+            breaking_version="1.10",
+            additional_warn_text=(
+                "Pass an instance of DagsterLookerApiTranslator or subclass to `dagster_looker_translator` instead."
+            ),
+        )
+        dagster_looker_translator = dagster_looker_translator()
+
     translator = dagster_looker_translator or DagsterLookerApiTranslator()
     result = []
     for request_start_pdt_build in request_start_pdt_builds:
