6f350cc5eb33cfb0b6a815a899c405b012fed862
Jordan Sanders <jordan@elementl.com>
2025-01-02 16:12:40 -0600

    Install dagster-aws[test]
    Nevermind - moto is only included as a test extra. But we can install it
    as an extra here so we inherit the same pins.

--- examples/docs_snippets/setup.py
+++ examples/docs_snippets/setup.py
@@ -49,6 +49,7 @@ setup(
             # "'NoneType' object has no attribute 'canvas'" errors in the tests that involve
             # Jupyter notebooks
             "matplotlib-inline<=0.1.3",
+            "dagster-aws[test]",
             "numpy",
             "pandas",
             "pandera",
