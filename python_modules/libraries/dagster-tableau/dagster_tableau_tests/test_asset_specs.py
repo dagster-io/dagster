10d88948e0405de589e0896217d63e435502861f
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-01-02 14:52:22 -0500

    [dagster-tableau] Move contextual data from DagsterTableauTranslator to TableauTranslatorData (#26739)
    ## Summary & Motivation
    
    Same as #26654 but for Tableau.
    
    ## How I Tested These Changes
    
    Updated tests with BK
    
    ## Changelog
    
    [dagster-tableau] Type hints in the signature of
    `DagsterTableauTranslator.get_asset_spec` have been updated - the
    parameter `data` is now of type `TableauTranslatorData` instead of
    `TableauContentData`. Custom Tableau translators should be updated.

--- python_modules/libraries/dagster-tableau/dagster_tableau_tests/test_asset_specs.py
+++ python_modules/libraries/dagster-tableau/dagster_tableau_tests/test_asset_specs.py
@@ -6,8 +6,10 @@ import pytest
 import responses
 from dagster._check.functions import CheckError
 from dagster._config.field_utils import EnvVar
+from dagster._core.definitions.asset_spec import AssetSpec
 from dagster._core.test_utils import environ
 from dagster_tableau import TableauCloudWorkspace, TableauServerWorkspace, load_tableau_asset_specs
+from dagster_tableau.translator import DagsterTableauTranslator, TableauTranslatorData
 
 
 @responses.activate
@@ -162,3 +164,61 @@ def test_translator_spec(
 
         data_source_asset_key = next(key for key in all_assets_keys if "datasource" in key.path[0])
         assert data_source_asset_key.path == ["superstore_datasource"]
+
+
+class MyCustomTranslator(DagsterTableauTranslator):
+    def get_asset_spec(self, data: TableauTranslatorData) -> AssetSpec:
+        default_spec = super().get_asset_spec(data)
+        return default_spec.replace_attributes(
+            key=default_spec.key.with_prefix("prefix"),
+        ).merge_attributes(metadata={"custom": "metadata"})
+
+
+@responses.activate
+@pytest.mark.parametrize(
+    "clazz,host_key,host_value",
+    [
+        (TableauServerWorkspace, "server_name", "fake_server_name"),
+        (TableauCloudWorkspace, "pod_name", "fake_pod_name"),
+    ],
+)
+@pytest.mark.usefixtures("site_name")
+@pytest.mark.usefixtures("sign_in")
+@pytest.mark.usefixtures("get_workbooks")
+@pytest.mark.usefixtures("get_workbook")
+def test_translator_custom_metadata(
+    clazz: Union[Type[TableauCloudWorkspace], Type[TableauServerWorkspace]],
+    host_key: str,
+    host_value: str,
+    site_name: str,
+    sign_in: MagicMock,
+    get_workbooks: MagicMock,
+    get_workbook: MagicMock,
+) -> None:
+    connected_app_client_id = uuid.uuid4().hex
+    connected_app_secret_id = uuid.uuid4().hex
+    connected_app_secret_value = uuid.uuid4().hex
+    username = "fake_username"
+
+    with environ({"TABLEAU_CLIENT_ID": connected_app_client_id}):
+        resource_args = {
+            "connected_app_client_id": EnvVar("TABLEAU_CLIENT_ID"),
+            "connected_app_secret_id": connected_app_secret_id,
+            "connected_app_secret_value": connected_app_secret_value,
+            "username": username,
+            "site_name": site_name,
+            host_key: host_value,
+        }
+
+        resource = clazz(**resource_args)
+        resource.build_client()
+
+        all_asset_specs = load_tableau_asset_specs(
+            workspace=resource, dagster_tableau_translator=MyCustomTranslator
+        )
+        asset_spec = next(spec for spec in all_asset_specs)
+
+        assert "custom" in asset_spec.metadata
+        assert asset_spec.metadata["custom"] == "metadata"
+        assert asset_spec.key.path == ["prefix", "superstore_datasource"]
+        assert asset_spec.tags["dagster/storage_kind"] == "tableau"
