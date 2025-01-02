5d205598e66fcf93b3973e7e2ea7d0acea7ef183
Marco polo <salazarm@users.noreply.github.com>
2025-01-02 12:48:54 -0500

    [Asset selection input] Show auto-complete suggestions immediately on focus. (#26755)
    ## Summary & Motivation
    
    As titled

--- js_modules/dagster-ui/packages/ui-core/src/assets/AssetsCatalogTable.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/AssetsCatalogTable.tsx
@@ -211,7 +211,10 @@ export const AssetsCatalogTable = ({
     kindFilter,
   } = useAssetCatalogFiltering({assets});
   const {filterInput, filtered, loading, assetSelection, setAssetSelection} =
-    useAssetSelectionInput(partiallyFiltered, !assets);
+    useAssetSelectionInput({
+      assets: partiallyFiltered,
+      assetsLoading: !assets || filteredAssetsLoading,
+    });
 
   useBlockTraceUntilTrue('useAllAssets', !!assets?.length && !loading);
 
