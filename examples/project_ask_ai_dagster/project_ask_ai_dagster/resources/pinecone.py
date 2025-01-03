23c614e3b08808327b6417e409809b8d1bfbe906
Alex Noonan <a.noonan@dagsterlabs.com>
2025-01-03 11:51:08 -0500

    updated pinecone resource and openai resource

--- examples/project_ask_ai_dagster/project_ask_ai_dagster/resources/pinecone.py
+++ examples/project_ask_ai_dagster/project_ask_ai_dagster/resources/pinecone.py
@@ -1,22 +1,16 @@
 from typing import Optional
 
 import dagster as dg
-from langchain_openai import OpenAIEmbeddings
 from pinecone import Pinecone, ServerlessSpec
-from pydantic import Field, PrivateAttr
+from pydantic import Field
 
 
 class PineconeResource(dg.ConfigurableResource):
     pinecone_api_key: str = Field(description="Pinecone API key")
     openai_api_key: str = Field(description="OpenAI API key")
-    pinecone_env: str = Field(description="Pinecone environment")
-    _embeddings: OpenAIEmbeddings = PrivateAttr()
 
     def setup_for_execution(self, context: dg.InitResourceContext) -> None:
         self._pinecone = Pinecone(api_key=self.pinecone_api_key)
-        self._embeddings = OpenAIEmbeddings(
-            model="text-embedding-3-small", openai_api_key=self.openai_api_key
-        )
 
     def create_index(self, index_name: str, dimension: int = 1536):
         if index_name not in self._pinecone.list_indexes().names():
@@ -33,12 +27,7 @@ class PineconeResource(dg.ConfigurableResource):
             return index, {"namespace": namespace}
         return index, {}
 
-    def embed_texts(self, texts: list[str]) -> list[list[float]]:
-        return self._embeddings.embed_documents(texts)
-
 
 pinecone_resource = PineconeResource(
-    pinecone_api_key=dg.EnvVar("PINECONE_API_KEY"),
-    openai_api_key=dg.EnvVar("OPENAI_API_KEY"),
-    pinecone_env=dg.EnvVar("PINECONE_ENV"),
+    pinecone_api_key=dg.EnvVar("PINECONE_API_KEY"), openai_api_key=dg.EnvVar("OPENAI_API_KEY")
 )
