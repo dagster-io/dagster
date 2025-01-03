727706822b3cd175417a933a535c8e63e78d1463
Colton Padden <colton@dagsterlabs.com>
2025-01-03 09:38:16 -0500

    rm tutorial_pinecone

--- examples/tutorial_pinecone/README.md
+++
@@ -1,1 +0,0 @@
-## Dagster × Pinecone Demo
--- examples/tutorial_pinecone/tutorial_pinecone/schedules.py
+++
@@ -1,8 +0,0 @@
-import dagster as dg
-from . import jobs
-
-goodreads_pinecone_schedule = dg.ScheduleDefinition(
-    name="pinecone_schedule",
-    target=jobs.goodreads_pinecone,
-    cron_schedule="0 12 * * *",
-)
