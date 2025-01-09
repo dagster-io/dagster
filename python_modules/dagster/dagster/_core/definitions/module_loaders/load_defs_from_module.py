62f54d66ef073597c017465ca87317c02e503ac9
Maxime Armstrong <maxime@retain.ai>
2025-01-09 17:17:33 -0500

    [module-loaders] Add load_definitions_from_modules

--- python_modules/dagster/dagster/_core/definitions/module_loaders/load_defs_from_module.py
+++ python_modules/dagster/dagster/_core/definitions/module_loaders/load_defs_from_module.py
@@ -1,5 +1,5 @@
 import inspect
-from collections.abc import Mapping
+from collections.abc import Iterable, Mapping
 from types import ModuleType
 from typing import Any, Optional, Union
 
@@ -13,17 +13,46 @@ from dagster._core.executor.base import Executor
 
 
 @experimental
+def load_definitions_from_modules(
+    modules: Iterable[ModuleType],
+    resources: Optional[Mapping[str, Any]] = None,
+    loggers: Optional[Mapping[str, LoggerDefinition]] = None,
+    executor: Optional[Union[Executor, ExecutorDefinition]] = None,
+) -> Definitions:
+    """Constructs the :py:class:`dagster.Definitions` from the given modules.
+
+    Args:
+        modules (Iterable[ModuleType]): The Python modules to look for :py:class:`dagster.Definitions` inside.
+        resources (Optional[Mapping[str, Any]]):
+            Dictionary of resources to bind to assets in the loaded :py:class:`dagster.Definitions`.
+        loggers (Optional[Mapping[str, LoggerDefinition]]):
+            Default loggers for jobs in the loaded :py:class:`dagster.Definitions`. Individual jobs
+            can define their own loggers by setting them explicitly.
+        executor (Optional[Union[Executor, ExecutorDefinition]]):
+            Default executor for jobs in the loaded :py:class:`dagster.Definitions`. Individual jobs
+            can define their own executors by setting them explicitly.
+
+    Returns:
+        Definitions:
+            The :py:class:`dagster.Definitions` defined in the given modules.
+    """
+    return Definitions(
+        **ModuleScopedDagsterDefs.from_modules(modules).get_object_list().to_definitions_args(),
+        resources=resources,
+        loggers=loggers,
+        executor=executor,
+    )
+
+
+@experimental
 def load_definitions_from_module(
     module: ModuleType,
     resources: Optional[Mapping[str, Any]] = None,
     loggers: Optional[Mapping[str, LoggerDefinition]] = None,
     executor: Optional[Union[Executor, ExecutorDefinition]] = None,
 ) -> Definitions:
-    return Definitions(
-        **ModuleScopedDagsterDefs.from_modules([module]).get_object_list().to_definitions_args(),
-        resources=resources,
-        loggers=loggers,
-        executor=executor,
+    return load_definitions_from_modules(
+        modules=[module], resources=resources, loggers=loggers, executor=executor
     )
 
 
@@ -54,6 +83,6 @@ def load_definitions_from_current_module(
     if module is None:
         check.failed("Could not find a module for the caller")
 
-    return load_definitions_from_module(
-        module=module, resources=resources, loggers=loggers, executor=executor
+    return load_definitions_from_modules(
+        modules=[module], resources=resources, loggers=loggers, executor=executor
     )
