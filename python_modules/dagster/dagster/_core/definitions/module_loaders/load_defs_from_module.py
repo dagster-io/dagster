61eef2772fcc4713c3310ee760749376474f2eae
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-01-10 15:14:01 -0500

    [module-loaders] Add docstring for load_definitions_from_module (#26994)
    ## Summary & Motivation
    
    As title

--- python_modules/dagster/dagster/_core/definitions/module_loaders/load_defs_from_module.py
+++ python_modules/dagster/dagster/_core/definitions/module_loaders/load_defs_from_module.py
@@ -53,6 +53,23 @@ def load_definitions_from_module(
     loggers: Optional[Mapping[str, LoggerDefinition]] = None,
     executor: Optional[Union[Executor, ExecutorDefinition]] = None,
 ) -> Definitions:
+    """Constructs the :py:class:`dagster.Definitions` from the given module.
+
+    Args:
+        module (ModuleType): The Python module to look for :py:class:`dagster.Definitions` inside.
+        resources (Optional[Mapping[str, Any]]):
+            Dictionary of resources to bind to assets in the loaded :py:class:`dagster.Definitions`.
+        loggers (Optional[Mapping[str, LoggerDefinition]]):
+            Default loggers for jobs in the loaded :py:class:`dagster.Definitions`. Individual jobs
+            can define their own loggers by setting them explicitly.
+        executor (Optional[Union[Executor, ExecutorDefinition]]):
+            Default executor for jobs in the loaded :py:class:`dagster.Definitions`. Individual jobs
+            can define their own executors by setting them explicitly.
+
+    Returns:
+        Definitions:
+            The :py:class:`dagster.Definitions` defined in the given module.
+    """
     return load_definitions_from_modules(
         modules=[module], resources=resources, loggers=loggers, executor=executor
     )
