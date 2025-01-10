8b5c275f3c7b9a6d7220f5537c622e5d008137d6
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-01-10 14:38:57 -0500

    [module-loaders] Add load_definitions_from_package_name (#26993)
    ## Summary & Motivation
    
    Same as https://github.com/dagster-io/dagster/pull/26546 but for a
    package module given its name.
    
    ## How I Tested These Changes
    
    Additional test with BK

--- python_modules/dagster/dagster/_core/definitions/module_loaders/load_defs_from_module.py
+++ python_modules/dagster/dagster/_core/definitions/module_loaders/load_defs_from_module.py
@@ -1,5 +1,6 @@
 import inspect
 from collections.abc import Iterable, Mapping
+from importlib import import_module
 from types import ModuleType
 from typing import Any, Optional, Union
 
@@ -116,6 +117,40 @@ def load_definitions_from_package_module(
     """
     return load_definitions_from_modules(
         modules=[*find_modules_in_package(package_module)],
+        resources=resources,
+        loggers=loggers,
+        executor=executor,
+    )
+
+
+@experimental
+def load_definitions_from_package_name(
+    package_name: str,
+    resources: Optional[Mapping[str, Any]] = None,
+    loggers: Optional[Mapping[str, LoggerDefinition]] = None,
+    executor: Optional[Union[Executor, ExecutorDefinition]] = None,
+) -> Definitions:
+    """Constructs the :py:class:`dagster.Definitions` from the package module for the given package name.
+
+    Args:
+        package_name (str):
+            The name of the package module to look for :py:class:`dagster.Definitions` inside.
+        resources (Optional[Mapping[str, Any]]):
+            Dictionary of resources to bind to assets in the loaded :py:class:`dagster.Definitions`.
+        loggers (Optional[Mapping[str, LoggerDefinition]]):
+            Default loggers for jobs in the loaded :py:class:`dagster.Definitions`. Individual jobs
+            can define their own loggers by setting them explicitly.
+        executor (Optional[Union[Executor, ExecutorDefinition]]):
+            Default executor for jobs in the loaded :py:class:`dagster.Definitions`. Individual jobs
+            can define their own executors by setting them explicitly.
+
+    Returns:
+        Definitions:
+            The :py:class:`dagster.Definitions` defined in the package module for the given package name.
+    """
+    package_module = import_module(package_name)
+    return load_definitions_from_package_module(
+        package_module=package_module,
         resources=resources,
         loggers=loggers,
         executor=executor,
