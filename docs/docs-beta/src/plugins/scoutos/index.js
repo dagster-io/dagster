03ba87fdeb158cff37f10a43d740976d5718b6ed
Colton Padden <colton@dagsterlabs.com>
2025-01-20 22:17:29 -0500

    add sparkle logo

--- docs/docs-beta/src/plugins/scoutos/index.js
+++ docs/docs-beta/src/plugins/scoutos/index.js
@@ -73,7 +73,43 @@ const SCOUTOS_COPILOT_INNER_HTML = `
   document.addEventListener('DOMContentLoaded', function() {
     var copilot = document.createElement('scout-copilot');
     copilot.setAttribute('copilot_id', 'copilot_cm61kissp00010es61qxro4dq');
-    
+    copilot.innerHTML = \`
+      <div
+        slot="fab"
+        style="
+          padding: 12px;
+          border-radius: 72px;
+          background: white;
+          background: var(--prism-background-color);
+          box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12),
+                      0px 1px 2px rgba(0, 0, 0, 0.24);
+        "
+      >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="url(#iconGradient)"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            stroke-width="2.0"
+            stroke="currentColor"
+          >
+            <defs>
+              <linearGradient id="iconGradient" x1="80%" x2="20%" y1="0%" y2="100%">
+                <stop offset="0%" stop-color="#A7FFBF"/>
+                <stop offset="100%" stop-color="#4F43DD"/>
+              </linearGradient>
+            </defs>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              fill="url(#iconGradient)"
+              stroke="url(#iconGradient)"
+              d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+          </svg>
+      </div>
+    \`;
+
     copilot.initial_activities = [
       {
         activity_type: 'llm.chat.message',
