b4587054207522de2c61bf3371bddeb9cf94e92a
Colton Padden <colton@dagsterlabs.com>
2025-01-21 15:49:58 -0500

    light/dark theming

--- docs/docs-beta/src/plugins/scoutos/index.js
+++ docs/docs-beta/src/plugins/scoutos/index.js
@@ -77,36 +77,39 @@ const SCOUTOS_COPILOT_INNER_HTML = `
       <div
         slot="fab"
         style="
-          padding: 12px;
+          padding: 6px 24px 6px 24px;
           border-radius: 72px;
-          background: white;
-          background: var(--prism-background-color);
-          box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12),
-                      0px 1px 2px rgba(0, 0, 0, 0.24);
+          background: var(--theme-color-background-default); 
+          color: var(--theme-color-text-blue);
+          box-shadow: var(--shadow-elevation-medium);
+          display: flex;
+          flex-direction: row;
+          gap: 6px;
+          align-items: center;
+          font-weight: 400;
+          font-family: "Geist", "Inter", "Arial", sans-serif;
         "
       >
+        <div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="url(#iconGradient)"
-            width="32"
-            height="32"
+            fill="none"
+            stroke="url(#iconGradient)"
+            stroke-width="2.5"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
-            stroke-width="2.0"
-            stroke="currentColor"
           >
             <defs>
               <linearGradient id="iconGradient" x1="80%" x2="20%" y1="0%" y2="100%">
-                <stop offset="0%" stop-color="#A7FFBF"/>
+                <stop offset="0%" stop-color="#DEDDFF"/>
                 <stop offset="100%" stop-color="#4F43DD"/>
               </linearGradient>
             </defs>
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill="url(#iconGradient)"
-              stroke="url(#iconGradient)"
-              d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
           </svg>
+        </div>
+        <div>Ask Dagster AI</div>
       </div>
     \`;
 
