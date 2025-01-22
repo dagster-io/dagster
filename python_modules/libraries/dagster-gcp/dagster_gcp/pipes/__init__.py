1c6454ea64289bc02917eb6f725127dd2671bfbe
danielgafni <danielgafni16@gmail.com>
2025-01-22 17:34:40 +0100

    [dagster-gcp] PipesDataprocClient

--- python_modules/libraries/dagster-gcp/dagster_gcp/pipes/__init__.py
+++ python_modules/libraries/dagster-gcp/dagster_gcp/pipes/__init__.py
@@ -1,4 +1,10 @@
+from dagster_gcp.pipes.clients.dataproc_job import PipesDataprocJobClient
 from dagster_gcp.pipes.context_injectors import PipesGCSContextInjector
 from dagster_gcp.pipes.message_readers import PipesGCSLogReader, PipesGCSMessageReader
 
-__all__ = ["PipesGCSContextInjector", "PipesGCSLogReader", "PipesGCSMessageReader"]
+__all__ = [
+    "PipesDataprocJobClient",
+    "PipesGCSContextInjector",
+    "PipesGCSLogReader",
+    "PipesGCSMessageReader",
+]
