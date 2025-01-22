50999b44011c528f2b0bddb23eb343e6e4330d2a
Marco Salazar <marco@elementl.com>
2025-01-22 16:18:42 -0500

    query separately

--- js_modules/dagster-ui/packages/ui-core/src/asset-graph/Utils.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/Utils.tsx
@@ -6,7 +6,6 @@ import {AssetNodeForGraphQueryFragment} from './types/useAssetGraphData.types';
 import {COMMON_COLLATOR} from '../app/Util';
 import {
   AssetCheckLiveFragment,
-  AssetGraphLiveQuery,
   AssetLatestInfoFragment,
   AssetLatestInfoRunFragment,
   AssetNodeLiveFragment,
@@ -33,7 +32,9 @@ export enum AssetGraphViewType {
 
 type AssetNode = AssetNodeForGraphQueryFragment;
 type AssetKey = AssetNodeKeyFragment;
-type AssetLiveNode = AssetNodeLiveFragment;
+type AssetLiveNode = AssetNodeLiveFragment & {
+  freshnessInfo: AssetNodeLiveFreshnessInfoFragment | null | undefined;
+};
 type AssetLatestInfo = AssetLatestInfoFragment;
 
 export const __ASSET_JOB_PREFIX = '__ASSET_JOB';
@@ -157,7 +158,7 @@ export interface LiveDataForNode {
   runWhichFailedToMaterialize: AssetLatestInfoRunFragment | null;
   lastMaterialization: AssetNodeLiveMaterializationFragment | null;
   lastMaterializationRunStatus: RunStatus | null; // only available if runWhichFailedToMaterialize is null
-  freshnessInfo: AssetNodeLiveFreshnessInfoFragment | null;
+  freshnessInfo: AssetNodeLiveFreshnessInfoFragment | null | undefined;
   lastObservation: AssetNodeLiveObservationFragment | null;
   assetChecks: AssetCheckLiveFragment[];
   partitionStats: {
@@ -193,24 +194,6 @@ export const MISSING_LIVE_DATA: LiveDataForNodeWithStaleData = {
 export interface LiveData {
   [assetId: GraphId]: LiveDataForNode;
 }
-
-export const buildLiveData = ({
-  assetNodes,
-  assetsLatestInfo,
-}: Pick<AssetGraphLiveQuery, 'assetNodes' | 'assetsLatestInfo'>) => {
-  const data: LiveData = {};
-
-  for (const liveNode of assetNodes) {
-    const graphId = toGraphId(liveNode.assetKey);
-    const assetLatestInfo = assetsLatestInfo.find(
-      (r) => JSON.stringify(r.assetKey) === JSON.stringify(liveNode.assetKey),
-    );
-
-    data[graphId] = buildLiveDataForNode(liveNode, assetLatestInfo);
-  }
-
-  return data;
-};
 
 export const buildLiveDataForNode = (
   assetNode: AssetLiveNode,
