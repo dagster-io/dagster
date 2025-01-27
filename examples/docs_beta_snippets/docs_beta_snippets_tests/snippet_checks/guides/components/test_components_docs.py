599174693c9c9ca837ef2b62b5cb3d44d32a0a0a
benpankow <ben@dagsterlabs.com>
2025-01-27 13:53:46 -0800

    [components docs] Add dg components check to docs

--- examples/docs_beta_snippets/docs_beta_snippets_tests/snippet_checks/guides/components/test_components_docs.py
+++ examples/docs_beta_snippets/docs_beta_snippets_tests/snippet_checks/guides/components/test_components_docs.py
@@ -280,7 +280,16 @@ params:
     project_dir: ../../../dbt/jdbt
   asset_attributes:
     key: "target/main/{{ node.name }}"
-    """,
+""",
+            )
+            run_command_and_snippet_output(
+                cmd="dg component check --no-use-dg-managed-environment",
+                snippet_path=COMPONENTS_SNIPPETS_DIR
+                / f"{next_snip_no()}-dg-component-check.txt",
+                update_snippets=update_snippets,
+                snippet_replace_regex=[
+                    ("--no-use-dg-managed-environment", ""),
+                ],
             )
 
             # Run dbt, check works
