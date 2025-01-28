ac7f11d9acdc67de6e7f594fef077747aa6fe943
Chris DeCarolis <cdguitar817@gmail.com>
2025-01-27 16:13:03 -0800

    Handle source code scaling

--- python_modules/libraries/dagster-airlift/kitchen-sink/kitchen_sink_tests/integration_tests/test_api.py
+++ python_modules/libraries/dagster-airlift/kitchen-sink/kitchen_sink_tests/integration_tests/test_api.py
@@ -1,4 +1,9 @@
+from typing import cast
+
 import requests
+from dagster._core.definitions.assets import AssetsDefinition
+from dagster_airlift.constants import SOURCE_CODE_METADATA_KEY
+from dagster_airlift.core import build_defs_from_airflow_instance
 from dagster_airlift.core.airflow_instance import AirflowInstance
 from dagster_airlift.core.basic_auth import AirflowBasicAuthBackend
 from kitchen_sink.airflow_instance import (
@@ -6,6 +11,7 @@ from kitchen_sink.airflow_instance import (
     AIRFLOW_INSTANCE_NAME,
     PASSWORD,
     USERNAME,
+    local_airflow_instance,
 )
 from pytest_mock import MockFixture
 
@@ -24,3 +30,29 @@ def test_configure_dag_list_limit(airflow_instance: None, mocker: MockFixture) -
     assert len(af_instance.list_dags()) == 16
     # 16 with actual results, 1 with no results
     assert spy.call_count == 17
+
+
+def change_dag_limit_source_code(limit: int) -> None:
+    import dagster_airlift.core.serialization.compute
+
+    dagster_airlift.core.serialization.compute.MAX_NUM_DAGS_SOURCE_CODE_RETRIEVAL = limit
+
+
+def test_disable_source_code_retrieval_at_scale(airflow_instance: None) -> None:
+    """Test that source code retrieval is disabled at scale."""
+    change_dag_limit_source_code(1)
+    af_instance = local_airflow_instance()
+    defs = build_defs_from_airflow_instance(airflow_instance=af_instance)
+    assert defs.assets
+    for assets_def in defs.assets:
+        metadata = next(iter(cast(AssetsDefinition, assets_def).specs)).metadata
+        assert SOURCE_CODE_METADATA_KEY not in metadata
+
+    change_dag_limit_source_code(200)
+    # Also force re-retrieval of state by giving the airflow instance a different name.
+    af_instance = local_airflow_instance(name="different_name")
+    defs = build_defs_from_airflow_instance(airflow_instance=af_instance)
+    assert defs.assets
+    for assets_def in defs.assets:
+        metadata = next(iter(cast(AssetsDefinition, assets_def).specs)).metadata
+        assert SOURCE_CODE_METADATA_KEY in metadata
