c1748949207c0589e99f99cd2b02e41b1f879804
benpankow <ben@dagsterlabs.com>
2025-02-03 15:35:28 -0800

    mask check

--- examples/docs_beta_snippets/docs_beta_snippets_tests/snippet_checks/guides/components/test_components_docs.py
+++ examples/docs_beta_snippets/docs_beta_snippets_tests/snippet_checks/guides/components/test_components_docs.py
@@ -292,6 +292,7 @@ params:
                 update_snippets=update_snippets,
                 snippet_replace_regex=[
                     ("--no-use-dg-managed-environment", ""),
+                    MASK_JAFFLE_PLATFORM,
                 ],
             )
 
