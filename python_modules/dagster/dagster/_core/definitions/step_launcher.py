d57153609c079c08056f8a015ac0d78bbfd2f267
colton <cmpadden@users.noreply.github.com>
2025-02-03 11:38:09 -0500

    Update python_modules/dagster/dagster/_core/definitions/step_launcher.py
    Co-authored-by: Nikki Everett <neverett@users.noreply.github.com>

--- python_modules/dagster/dagster/_core/definitions/step_launcher.py
+++ python_modules/dagster/dagster/_core/definitions/step_launcher.py
@@ -60,7 +60,7 @@ class StepRunRef(
 
 _step_launcher_supersession = superseded(
     subject="StepLauncher",
-    additional_warn_text="This API has been superseded, but there is no plan to remove this functionality. For new projects, we recommend using Dagster Pipes.",
+    additional_warn_text="This API has been superseded, but there is no plan to remove this functionality. For new projects, we recommend using Dagster Pipes. For more information, see https://docs.dagster.io/guides/build/external-pipelines/",
 )
 
 
