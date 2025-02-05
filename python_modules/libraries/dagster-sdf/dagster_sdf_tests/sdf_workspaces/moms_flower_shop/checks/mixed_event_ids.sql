650ec10f971a20e1bebd27ce3dd713dd8593ef59
prha <1040172+prha@users.noreply.github.com>
2025-02-04 17:24:52 -0800

    remove dagster-sdf from dagster repo (#27572)
    ## Summary & Motivation
    We are moving `dagster-sdf` from the main `dagster` repo to the
    community-integrations repo
    
    ## How I Tested These Changes
    BK
    
    ## Changelog
    
    > Moved `dagster-sdf` library to the community-supported repo

--- python_modules/libraries/dagster-sdf/dagster_sdf_tests/sdf_workspaces/moms_flower_shop/checks/mixed_event_ids.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    DISTINCT c.table_name as "table_name",
-    c.column_name as "column name",
-    c.classifiers
-FROM
-    sdf.information_schema.columns c
-WHERE
-    -- more than one EVENT classifier is assigned
-    CAST(c.classifiers AS VARCHAR) LIKE '%EVENT%EVENT%'
--- python_modules/libraries/dagster-sdf/dagster_sdf_tests/sdf_workspaces/moms_flower_shop/reports/README.txt
+++
@@ -1,1 +0,0 @@
-This folder will contain data warehouse reports based on SDF's information schema
