7c777fcbc020413e68daf94a2fb2526d533b587a
Marco Salazar <marco@elementl.com>
2025-02-05 09:19:27 -0500

    web-worker utilities part 1

--- js_modules/dagster-ui/packages/ui-core/src/asset-graph/ComputeGraphData.worker.ts
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/ComputeGraphData.worker.ts
@@ -3,6 +3,7 @@ import {createWorkerThread} from 'shared/workers/WorkerThread.oss';
 import {computeGraphData} from './ComputeGraphData';
 import {BuildGraphDataMessageType, ComputeGraphDataMessageType} from './ComputeGraphData.types';
 import {buildGraphData} from './Utils';
+import {assertUnreachable} from '../app/Util';
 
 type WorkerMessageData = ComputeGraphDataMessageType | BuildGraphDataMessageType;
 
@@ -13,6 +14,8 @@ createWorkerThread(
       postMessage({...state, id: data.id});
     } else if (data.type === 'buildGraphData') {
       postMessage({...buildGraphData(data.nodes), id: data.id});
+    } else {
+      assertUnreachable(data);
     }
   },
   (_postMessage: (message: any) => void, error: Error) => {
