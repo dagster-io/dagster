85eec9b06ab0c8e765327d567e72d8a628f35efa
colton <cmpadden@users.noreply.github.com>
2025-02-06 09:53:02 -0800

    [dagster-university] removes DU content as it has been migrated to project-dagster-university (#27621)
    ## Summary & Motivation
    
    https://github.com/dagster-io/project-dagster-university/pull/53
    
    ## How I Tested These Changes
    
    ## Changelog
    
    NOCHANGELOG

--- docs/dagster-university/next.config.js
+++
@@ -1,5 +0,0 @@
-const withMarkdoc = require('@markdoc/next.js');
-
-module.exports = withMarkdoc({schemaPath: './markdoc', mode: 'static'})({
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
-});
