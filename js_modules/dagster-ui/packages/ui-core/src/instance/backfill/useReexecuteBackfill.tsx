a80ff5d2ae0eabeace49a51173aa74de2707952c
Ben Gotow <ben@foundry376.com>
2025-02-11 22:22:57 -0600

    [ui] Update backfill actions menus to include Re-execute options (#27709)
    ## Summary & Motivation
    
    Related: https://github.com/dagster-io/dagster/pull/23679
    Ticket:
    https://linear.app/dagster-labs/issue/FE-709/update-backfill-actions-menu-to-allow-for-retries
    
    Screenshots of the new options in action. Note: the tooltips were not in
    the spec but I think it's helpful to explain exactly what "failure"
    means in this context.
    
    <img width="637" alt="image"
    src="https://github.com/user-attachments/assets/632eb3fa-6bee-426a-bca8-8909e62a20b2"
    />
    
    <img width="880" alt="image"
    src="https://github.com/user-attachments/assets/9f8f7feb-8445-490d-93f2-7cbdf6200e8f"
    />
    
    
    ### Updates to the "Resume failed backfill" option
    
    With the new "Re-execute from Failure" option in place, I found the
    "Resume failed backfill" option pretty confusing. The old label said
    "Submits runs for all partitions in the backfill that do not have a
    corresponding run. Does not retry failed runs." It's confusing because
    it's only enabled in the FAILED state and not the COMPLETED_FAILED
    state, which means the backfill itself must fail to queue runs for it to
    be enabled (I think?) Notice in my screenshot it's disabled because the
    red "Failed" tag means that one partition failed.
    
    I'd like to propose this slight wording change and new tooltip to help
    differentiate this from "Re-execute from failure":
    
    <img width="631" alt="image"
    src="https://github.com/user-attachments/assets/d8a3aaba-4908-4c19-922a-dcc13f3d1e31"
    />
    
    <img width="995" alt="image"
    src="https://github.com/user-attachments/assets/11c73aab-2dba-460f-b90e-9e2efce09225"
    />
    
    
    I'm also not sure why this option would be used INSTEAD of the new
    re-execute from failure. I think the new one is sort of a superset of
    the functionality - you get to queue remaining runs from your
    interrupted backfill AND failed materializations. Maybe we just remove
    it?
    
    ## How I Tested These Changes
    
    - New tests! 
    - Manual testing with a variety of backfills
    
    ## Changelog
    
    [ui] The backfill actions menu now includes "Re-execute" and "Re-execute
    from failure", which create new backfills targeting the same partitions,
    and the partitions which failed to materialize, respectively.
    
    Co-authored-by: bengotow <bgotow@elementl.com>

---
+++ js_modules/dagster-ui/packages/ui-core/src/instance/backfill/useReexecuteBackfill.tsx
@@ -0,0 +1,79 @@
+import {Group} from '@dagster-io/ui-components';
+import {useHistory} from 'react-router';
+
+import {gql, useMutation} from '../../apollo-client';
+import {BackfillActionsBackfillFragment} from './types/BackfillFragments.types';
+import {
+  ReexecuteBackfillMutation,
+  ReexecuteBackfillMutationVariables,
+} from './types/useReexecuteBackfill.types';
+import {showCustomAlert} from '../../app/CustomAlertProvider';
+import {showSharedToaster} from '../../app/DomUtils';
+import {PYTHON_ERROR_FRAGMENT} from '../../app/PythonErrorFragment';
+import {PythonErrorInfo} from '../../app/PythonErrorInfo';
+import {ReexecutionStrategy} from '../../graphql/types';
+import {showBackfillSuccessToast} from '../../partitions/BackfillMessaging';
+
+export function useReexecuteBackfill(
+  backfill: BackfillActionsBackfillFragment,
+  refetch: () => void,
+) {
+  const history = useHistory();
+  const [reexecuteBackfill] = useMutation<
+    ReexecuteBackfillMutation,
+    ReexecuteBackfillMutationVariables
+  >(REEXECUTE_BACKFILL_MUTATION);
+
+  return async (strategy: ReexecutionStrategy) => {
+    const {data} = await reexecuteBackfill({
+      variables: {reexecutionParams: {parentRunId: backfill.id, strategy}},
+    });
+    if (data && data.reexecutePartitionBackfill.__typename === 'LaunchBackfillSuccess') {
+      showBackfillSuccessToast(history, data.reexecutePartitionBackfill.backfillId, true);
+      refetch();
+    } else if (data && data.reexecutePartitionBackfill.__typename === 'UnauthorizedError') {
+      await showSharedToaster({
+        message: (
+          <Group direction="column" spacing={4}>
+            <div>
+              Attempted to re-execute the backfill in read-only mode. This backfill was not
+              launched.
+            </div>
+          </Group>
+        ),
+        icon: 'error',
+        intent: 'danger',
+      });
+    } else if (data && data.reexecutePartitionBackfill.__typename === 'PythonError') {
+      const error = data.reexecutePartitionBackfill;
+      await showSharedToaster({
+        message: <div>An unexpected error occurred. This backfill was not re-executed.</div>,
+        icon: 'error',
+        intent: 'danger',
+        action: {
+          text: 'View error',
+          onClick: () =>
+            showCustomAlert({
+              body: <PythonErrorInfo error={error} />,
+            }),
+        },
+      });
+    }
+  };
+}
+
+export const REEXECUTE_BACKFILL_MUTATION = gql`
+  mutation reexecuteBackfill($reexecutionParams: ReexecutionParams!) {
+    reexecutePartitionBackfill(reexecutionParams: $reexecutionParams) {
+      ... on LaunchBackfillSuccess {
+        backfillId
+      }
+      ... on UnauthorizedError {
+        message
+      }
+      ...PythonErrorFragment
+    }
+  }
+
+  ${PYTHON_ERROR_FRAGMENT}
+`;
