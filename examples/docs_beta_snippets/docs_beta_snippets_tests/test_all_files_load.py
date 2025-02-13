b1fe01ad61713b83bb777050687596856ed2fdfe
benpankow <ben@dagsterlabs.com>
2025-02-13 13:26:53 -0800

    addd ignores

--- examples/docs_beta_snippets/docs_beta_snippets_tests/test_all_files_load.py
+++ examples/docs_beta_snippets/docs_beta_snippets_tests/test_all_files_load.py
@@ -27,10 +27,10 @@ EXCLUDED_FILES = {
     f"{snippets_folder}/guides/components/existing-project/6-initial-definitions.py",
     f"{snippets_folder}/guides/components/existing-project/7-updated-definitions.py",
     f"{snippets_folder}/guides/components/migrating-definitions/2-definitions-before.py",
-    f"{snippets_folder}/guides/components/migrating-definitions/3-inner-definitions-before.py",
+    f"{snippets_folder}/guides/components/migrating-definitions/5-elt-nested-definitions.py",
     f"{snippets_folder}/guides/components/migrating-definitions/7-definitions-after.py",
+    f"{snippets_folder}/guides/components/migrating-definitions/10-definitions-after-all.py",
 }
-
 EXCLUDED_DIRS = {
     # integrations are excluded because they have external dependencies that are easier to manage in
     # a separate tox environment
