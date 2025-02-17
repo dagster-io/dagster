09da639409fd5fb66c01750f47aee9d22673f7b5
Brent Jericho Uyguangco <89530434+brentjericho@users.noreply.github.com>
2025-02-17 09:04:22 +0000

    Added location field to asset metadata and updated asset decorator

--- python_modules/dagster/dagster/_core/assets.py
+++ python_modules/dagster/dagster/_core/assets.py
@@ -7,14 +7,15 @@ from dagster._serdes.serdes import deserialize_value
 
 
 @whitelist_for_serdes
-class AssetDetails(NamedTuple("_AssetDetails", [("last_wipe_timestamp", Optional[float])])):
+class AssetDetails(NamedTuple("_AssetDetails", [("last_wipe_timestamp", Optional[float]), ("location", Optional[str])])):
     """Set of asset fields that do not change with every materialization.  These are generally updated
     on some non-materialization action (e.g. wipe).
     """
 
-    def __new__(cls, last_wipe_timestamp: Optional[float] = None):
+    def __new__(cls, last_wipe_timestamp: Optional[float] = None, location: Optional[str] = None):
         check.opt_float_param(last_wipe_timestamp, "last_wipe_timestamp")
-        return super().__new__(cls, last_wipe_timestamp)
+        check.opt_str_param(location, "location")
+        return super().__new__(cls, last_wipe_timestamp, location)
 
     @staticmethod
     def from_db_string(db_string):
