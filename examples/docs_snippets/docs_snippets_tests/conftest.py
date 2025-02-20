9ab51b3eede53421d3b5aecb2c09e717847dbed2
Colton Padden <colton@dagsterlabs.com>
2025-02-20 11:16:54 -0500

    add mock_s3_resource

--- examples/docs_snippets/docs_snippets_tests/conftest.py
+++ examples/docs_snippets/docs_snippets_tests/conftest.py
@@ -1,4 +1,6 @@
+import boto3
 import pytest
+from moto import mock_s3
 
 from dagster import file_relative_path
 
@@ -6,6 +8,12 @@ from dagster import file_relative_path
 @pytest.fixture
 def docs_snippets_folder():
     return file_relative_path(__file__, "../docs_snippets/")
+
+
+@pytest.fixture
+def mock_s3_resource():
+    with mock_s3():
+        yield boto3.resource("s3", region_name="us-east-1")
 
 
 @pytest.fixture
