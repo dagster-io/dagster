dagster-io/dagster

commit 43c5240f82fd2dc43dff80673d5dc9f862c424af

[branch deployments] change from base context to base asset graph (#27872)
The full context is designed around serving the current live deployment,
but if we want to be able to represent base deployments at a specific
point and time we need more constrained target. Opted for the asset
graph in this case.

## How I Tested These Changes

bk

python_modules/dagster-graphql/dagster_graphql/schema/asset_graph.py
@@ -388,14 +388,11 @@ class GrapheneAssetNode(graphene.ObjectType):
         if self._asset_graph_differ is not None:
             return self._asset_graph_differ
 
-        base_deployment_context = graphene_info.context.get_base_deployment_context()
-
-        if base_deployment_context is None:
-            return None
+        base_deployment_asset_graph = graphene_info.context.get_base_deployment_asset_graph()
 
         self._asset_graph_differ = AssetGraphDiffer(
             branch_asset_graph=graphene_info.context.asset_graph,
-            base_asset_graph=base_deployment_context.asset_graph,
+            base_asset_graph=base_deployment_asset_graph,
         )
         return self._asset_graph_differ
 

python_modules/dagster/dagster/_core/workspace/context.py
@@ -350,7 +350,7 @@ class BaseWorkspaceRequestContext(LoadingContext):
         code_location = self.get_code_location(code_location_name)
         return code_location.get_notebook_data(notebook_path=notebook_path)
 
-    def get_base_deployment_context(self) -> Optional["BaseWorkspaceRequestContext"]:
+    def get_base_deployment_asset_graph(self) -> Optional["RemoteWorkspaceAssetGraph"]:
         return None
 
     def get_repository(

commit 5b31001a0d79b675e1f3f7b070c9f010b974835e

fix asset graph diffing (#27954)
fix issues caused by https://github.com/dagster-io/dagster/pull/27872

and update AssetGraphDiffer to always expect a graph (which may be
empty) instead of being able to take `None`

## How I Tested These Changes

existing coverage

python_modules/dagster-graphql/dagster_graphql/schema/asset_graph.py
@@ -390,6 +390,9 @@ class GrapheneAssetNode(graphene.ObjectType):
 
         base_deployment_asset_graph = graphene_info.context.get_base_deployment_asset_graph()
 
+        if base_deployment_asset_graph is None:
+            return None
+
         self._asset_graph_differ = AssetGraphDiffer(
             branch_asset_graph=graphene_info.context.asset_graph,
             base_asset_graph=base_deployment_asset_graph,

python_modules/dagster/dagster/_core/definitions/asset_graph_differ.py
@@ -77,13 +77,10 @@ class AssetGraphDiffer:
     we will consider every asset New.
     """
 
-    _branch_asset_graph: "RemoteAssetGraph"
-    _base_asset_graph: Optional["RemoteAssetGraph"]
-
     def __init__(
         self,
         branch_asset_graph: "RemoteAssetGraph",
-        base_asset_graph: Optional["RemoteAssetGraph"],
+        base_asset_graph: "RemoteAssetGraph",
     ):
         self._branch_asset_graph = branch_asset_graph
         self._base_asset_graph = base_asset_graph
@@ -94,7 +91,7 @@ class AssetGraphDiffer:
         """Computes the diff between a branch deployment asset and the
         corresponding base deployment asset.
         """
-        if not self._base_asset_graph or not self._base_asset_graph.has(asset_key):
+        if not self._base_asset_graph.has(asset_key):
             # if the base asset graph is None, it is because the asset graph in the branch deployment
             # is new and doesn't exist in the base deployment. Thus all assets are new.
             return AssetDefinitionDiffDetails(change_types={AssetDefinitionChangeType.NEW})

python_modules/dagster/dagster/_core/definitions/remote_asset_graph.py
@@ -579,6 +579,13 @@ class RemoteRepositoryAssetGraph(RemoteAssetGraph[RemoteRepositoryAssetNode]):
             remote_asset_check_nodes_by_key=asset_checks_by_key,
         )
 
+    @classmethod
+    def empty(cls):
+        return cls(
+            remote_asset_nodes_by_key={},
+            remote_asset_check_nodes_by_key={},
+        )
+
 
 class RemoteWorkspaceAssetGraph(RemoteAssetGraph[RemoteWorkspaceAssetNode]):
     def __init__(