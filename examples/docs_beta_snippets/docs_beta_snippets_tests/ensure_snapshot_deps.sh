726b8f335c75e94b1b6eb52e7eb0ba37950b1cfa
benpankow <ben@dagsterlabs.com>
2025-02-20 13:15:21 -0800

    [ez] Brew install tree when rebuilding component docs on  mac

--- examples/docs_beta_snippets/docs_beta_snippets_tests/ensure_snapshot_deps.sh
+++ examples/docs_beta_snippets/docs_beta_snippets_tests/ensure_snapshot_deps.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-which tree || apt-get install tree
+# Validate that tree is installed, install it if not (for bk purposes)
+(which tree > /dev/null && echo "tree command found") ||
+(which brew > /dev/null && echo "tree command not found, installing using brew..." && brew install tree) ||
+(echo "tree command not found, installing using apt-get..." && apt-get install tree)
