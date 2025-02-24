a65019dc753f224b52c153d8e3c60977820ccaa8
Marco Salazar <marco@elementl.com>
2025-02-24 09:58:32 -0500

    update error styles

--- js_modules/dagster-ui/packages/ui-core/src/selection/CustomErrorListener.ts
+++ js_modules/dagster-ui/packages/ui-core/src/selection/CustomErrorListener.ts
@@ -1,33 +1,54 @@
-import {ANTLRErrorListener, RecognitionException, Recognizer} from 'antlr4ts';
-import {Token} from 'antlr4ts/Token';
+import {ANTLRErrorListener, Parser, RecognitionException, Recognizer} from 'antlr4ts';
 
-export interface SyntaxError {
+export type SyntaxError = {
   message: string;
-  line: number;
-  column: number;
-  offendingSymbol: Token | null;
-}
+  from: number;
+  to: number;
+} & (
+  | {
+      mismatchedInput?: undefined;
+      expectedInput?: undefined;
+    }
+  | {
+      mismatchedInput: string;
+      expectedInput: string[];
+    }
+);
 
 export class CustomErrorListener implements ANTLRErrorListener<any> {
   private errors: SyntaxError[];
+  private parser: Parser | undefined;
 
-  constructor() {
+  constructor({parser}: {parser?: Parser}) {
+    this.parser = parser;
     this.errors = [];
   }
 
   syntaxError(
     _recognizer: Recognizer<any, any>,
     offendingSymbol: any,
-    line: number,
+    _line: number,
     charPositionInLine: number,
     msg: string,
     _e: RecognitionException | undefined,
   ): void {
+    const expectedTokens = this.parser?.getExpectedTokens()?.toArray();
+    const message = msg;
+    let mismatchedInput;
+    let expectedInput;
+    if (this.parser && expectedTokens?.length && expectedTokens[0] !== -1 && offendingSymbol) {
+      mismatchedInput = offendingSymbol.text;
+      expectedInput = expectedTokens
+        .map((token) => this.parser?.vocabulary.getLiteralName(token))
+        .filter(Boolean) as string[];
+    }
+
     this.errors.push({
-      message: msg,
-      line,
-      column: charPositionInLine,
-      offendingSymbol,
+      message,
+      from: charPositionInLine,
+      to: charPositionInLine + (offendingSymbol?.text?.length ?? Infinity),
+      mismatchedInput,
+      expectedInput,
     });
   }
 
---
+++ js_modules/dagster-ui/packages/ui-core/src/selection/useSelectionInputLintingAndHighlighting.tsx
@@ -0,0 +1,136 @@
+import {
+  BodySmall,
+  Box,
+  Colors,
+  Icon,
+  MonoSmall,
+  PopoverContentStyle,
+  PopoverWrapperStyle,
+} from '@dagster-io/ui-components';
+import {useLayoutEffect, useMemo, useState} from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+import {SyntaxError} from './CustomErrorListener';
+import {applyStaticSyntaxHighlighting} from './SelectionInputHighlighter';
+import {useUpdatingRef} from '../hooks/useUpdatingRef';
+
+export const useSelectionInputLintingAndHighlighting = ({
+  cmInstance,
+  value,
+  linter,
+}: {
+  cmInstance: React.MutableRefObject<CodeMirror.Editor | null>;
+  value: string;
+  linter: (content: string) => SyntaxError[];
+}) => {
+  const errors = useMemo(() => {
+    const errors = linter(value);
+    return errors.map((error, idx) => ({
+      ...error,
+      idx,
+    }));
+  }, [linter, value]);
+
+  useLayoutEffect(() => {
+    if (!cmInstance.current) {
+      return;
+    }
+    applyStaticSyntaxHighlighting(cmInstance.current, errors);
+  }, [cmInstance, errors]);
+
+  const errorsRef = useUpdatingRef(errors);
+
+  const [error, setError] = useState<{
+    error: SyntaxError;
+    x: number;
+    y: number;
+  } | null>(null);
+
+  const errorRef = useUpdatingRef(error);
+
+  useLayoutEffect(() => {
+    document.body.addEventListener('mousemove', (ev) => {
+      if (!(ev.target instanceof HTMLElement)) {
+        return;
+      }
+      const error = ev.target.closest('.selection-input-error') as HTMLElement | null;
+      if (error) {
+        const regex = /selection-input-error-(\d+)/;
+        const errorIdx = parseInt(error.className.match(regex)?.[1] ?? '0', 10);
+        const errorAnnotation = errorsRef.current[errorIdx];
+        if (errorAnnotation) {
+          setError({
+            error: errorAnnotation,
+            x: ev.clientX,
+            y: ev.clientY,
+          });
+          return;
+        }
+      }
+      if (errorRef.current) {
+        setError(null);
+      }
+    });
+  }, [cmInstance, errorsRef, errorRef]);
+
+  const message = useMemo(() => {
+    if (!error) {
+      return null;
+    }
+    if (error.error.mismatchedInput && error.error.expectedInput) {
+      return (
+        <Box>
+          Mismatched input{' '}
+          <MonoSmall color={Colors.textRed()}>&apos;{error.error.mismatchedInput}&apos;</MonoSmall>.
+          Expected{' '}
+          {error.error.expectedInput.map((expectedInput, idx) => (
+            <>
+              {idx > 0 && ', '}
+              <MonoSmall key={expectedInput} color={Colors.textBlue()}>
+                {expectedInput}
+              </MonoSmall>
+            </>
+          ))}
+        </Box>
+      );
+    }
+    if (error.error.message) {
+      return error.error.message;
+    }
+    return null;
+  }, [error]);
+
+  if (!error) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
+    <PortalElement $bottom={error.y} $left={error.x}>
+      <Box
+        as={Content}
+        padding={{horizontal: 12, vertical: 8}}
+        flex={{direction: 'row', gap: 4}}
+        color={Colors.textLight()}
+      >
+        <Icon name="run_failed" color={Colors.accentRed()} />
+        <BodySmall color={Colors.textLight()}>{message}</BodySmall>
+      </Box>
+    </PortalElement>,
+    document.body,
+  );
+};
+
+const PortalElement = styled.div<{$bottom: number; $left: number}>`
+  position: absolute;
+  top: ${({$bottom}) => $bottom - 32}px;
+  left: ${({$left}) => $left + 16}px;
+  min-width: 320px;
+  max-width: 600px;
+  z-index: 20; // Z-index 20 to match bp5-overlay
+  ${PopoverWrapperStyle}
+`;
+
+const Content = styled.div`
+  ${PopoverContentStyle}
+`;
