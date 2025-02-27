46fb280e9e346e45a1b394ae14d140a67382e935
Alex Langenfeld <alex@elementl.com>
2025-02-27 15:12:54 -0600

    [compute logs] fix download link in limit exceeded message (#28102)
    ## How I Tested These Changes
    ![Screenshot 2025-02-27 at 1 53
    56 PM](https://github.com/user-attachments/assets/98bde43b-0f7b-41ef-80a0-2ba34a02ac63)

--- js_modules/dagster-ui/packages/ui-core/src/runs/CapturedLogPanel.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/runs/CapturedLogPanel.tsx
@@ -317,6 +317,9 @@ const CapturedLogPanel = React.memo(
 
     const stdoutLocation = queryResult.data?.capturedLogsMetadata.stdoutLocation || undefined;
     const stderrLocation = queryResult.data?.capturedLogsMetadata.stderrLocation || undefined;
+    const stdoutDownloadUrl = queryResult.data?.capturedLogsMetadata.stdoutDownloadUrl;
+    const stderrDownloadUrl = queryResult.data?.capturedLogsMetadata.stderrDownloadUrl;
+
     const websocketsUnavailabile = availability === 'unavailable' || disabled;
     const Component = websocketsUnavailabile
       ? CapturedLogsQueryProvider
@@ -331,12 +334,14 @@ const CapturedLogPanel = React.memo(
                 isLoading={_state.isLoading}
                 location={stdoutLocation}
                 isVisible={visibleIOType === 'stdout'}
+                downloadUrl={stdoutDownloadUrl}
               />
               <RawLogContent
                 logData={_state.stderr}
                 isLoading={_state.isLoading}
                 location={stderrLocation}
                 isVisible={visibleIOType === 'stderr'}
+                downloadUrl={stderrDownloadUrl}
               />
             </>
           )}
