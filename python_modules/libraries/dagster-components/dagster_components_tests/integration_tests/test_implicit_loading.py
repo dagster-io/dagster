20fd68ba1be0e9c5f70bd240cfd8b38a7f5045f4
Nick Schrock <schrockn@dagsterlabs.com>
2025-02-27 17:33:37 -0500

    cp

---
+++ python_modules/libraries/dagster-components/dagster_components_tests/integration_tests/test_implicit_loading.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+
+import pytest
+from dagster._core.definitions.asset_key import AssetKey
+from dagster._utils import pushd
+
+from dagster_components_tests.integration_tests.component_loader import load_test_component_defs
+
+
+@pytest.fixture(autouse=True)
+def chdir():
+    with pushd(str(Path(__file__).parent.parent)):
+        sys.path.append(str(Path(__file__).parent))
+        yield
+
+
+def test_implicit_single_file() -> None:
+    defs = load_test_component_defs("implicit/single_file")
+    assert {spec.key for spec in defs.get_all_asset_specs()} == {AssetKey("an_implicit_asset")}
