029aa0ef581d37718fda1cf8c1409f27d094c3b7
JamieDeMaria <jamie@dagsterlabs.com>
2025-02-27 11:43:12 -0500

    other updates

--- python_modules/dagster/dagster/_core/events/__init__.py
+++ python_modules/dagster/dagster/_core/events/__init__.py
@@ -80,6 +80,7 @@ EventSpecificData = Union[
     "AssetMaterializationPlannedData",
     "AssetCheckEvaluation",
     "AssetCheckEvaluationPlanned",
+    "PlannedAssetMaterializationFailureData",
 ]
 
 
