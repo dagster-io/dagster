c7c5c8eb236109213401e7c8568281179f717b6f
nikki everett <nikki@dagsterlabs.com>
2025-02-28 15:19:12 -0600

    add 1.10.2 versioned docs and set latest to 1.10.3
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -177,7 +177,7 @@ const config: Config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '1.10.2 (master)',
+              label: '1.10.3 (master)',
               path: '/',
             },
           },
