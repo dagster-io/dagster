71b61704ba5c733936e7fd820f95aab4e02584a3
Dennis Hume <dehume@users.noreply.github.com>
2025-02-28 08:25:07 -0600

    [du] Change to `import dg` (#28119)
    ## Summary & Motivation
    Update the starter projects for both Dagster University modules to use
    `import dagster as dg` statements
    
    ## How I Tested These Changes
    
    ## Changelog
    
    > Insert changelog entry or delete this section.

--- examples/project_du_dbt_starter/dagster_university/__init__.py
+++ examples/project_du_dbt_starter/dagster_university/__init__.py
@@ -1,4 +1,4 @@
-from dagster import Definitions, load_assets_from_modules
+import dagster as dg
 
 from .assets import metrics, requests, trips
 from .jobs import adhoc_request_job, trip_update_job, weekly_update_job
@@ -6,12 +6,12 @@ from .resources import database_resource
 from .schedules import trip_update_schedule, weekly_update_schedule
 from .sensors import adhoc_request_sensor
 
-trip_assets = load_assets_from_modules([trips])
-metric_assets = load_assets_from_modules(
+trip_assets = dg.load_assets_from_modules([trips])
+metric_assets = dg.load_assets_from_modules(
     modules=[metrics],
     group_name="metrics",
 )
-requests_assets = load_assets_from_modules(
+requests_assets = dg.load_assets_from_modules(
     modules=[requests],
     group_name="requests",
 )
@@ -20,7 +20,7 @@ all_jobs = [trip_update_job, weekly_update_job, adhoc_request_job]
 all_schedules = [trip_update_schedule, weekly_update_schedule]
 all_sensors = [adhoc_request_sensor]
 
-defs = Definitions(
+defs = dg.Definitions(
     assets=trip_assets + metric_assets + requests_assets,
     resources={
         "database": database_resource,
