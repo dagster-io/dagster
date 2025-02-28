b09cecc3f026ade44d569c4ac802040b82c5d21c
Maxime Armstrong <maxime@retain.ai>
2025-02-28 18:08:42 -0500

    Move DbtCloudWorkspaceData class

--- python_modules/libraries/dagster-dbt/dagster_dbt/cloud/resources_v2.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/cloud/resources_v2.py
@@ -2,187 +2,23 @@ from typing import NamedTuple
 
 from dagster._annotations import preview
 from dagster._config.pythonic_config import ConfigurableResource
-<<<<<<< HEAD
 from dagster._config.pythonic_config.resource import ResourceDependency
-=======
 from dagster._model import DagsterModel
 from dagster._record import record
 from dagster._serdes import whitelist_for_serdes
->>>>>>> c584056d68 ([4/n][dagster-dbt] Implement fetch_workspace_data for DbtCloudWorkspace)
 from dagster._utils.cached_method import cached_method
 from pydantic import Field
 
-<<<<<<< HEAD
 from dagster_dbt.cloud.client_v2 import DbtCloudClient
 
-=======
 from dagster_dbt.cloud.dbt_cloud_job_run import DbtCloudJobRun
 
 LIST_JOBS_INDIVIDUAL_REQUEST_LIMIT = 100
->>>>>>> c584056d68 ([4/n][dagster-dbt] Implement fetch_workspace_data for DbtCloudWorkspace)
 DAGSTER_ADHOC_PREFIX = "DAGSTER_ADHOC_JOB__"
 
 
 def get_job_name(environment_id: int, project_id: int) -> str:
     return f"{DAGSTER_ADHOC_PREFIX}{project_id}__{environment_id}"
-
-
-@preview
-<<<<<<< HEAD
-=======
-class DbtCloudClient(DagsterModel):
-    account_id: int = Field(
-        ...,
-        description="The dbt Cloud Account ID. Can be found on the Account Info page of dbt Cloud.",
-    )
-    token: int = Field(
-        ...,
-        description="The token to access the dbt Cloud API. Can be either a personal token or a service token.",
-    )
-    access_url: int = Field(
-        ...,
-        description="The access URL for your dbt Cloud workspace.",
-    )
-    request_max_retries: int = Field(
-        ...,
-        description=(
-            "The maximum number of times requests to the dbt Cloud API should be retried "
-            "before failing."
-        ),
-    )
-    request_retry_delay: float = Field(
-        ...,
-        description="Time (in seconds) to wait between each request retry.",
-    )
-    request_timeout: int = Field(
-        ...,
-        description="Time (in seconds) after which the requests to dbt Cloud are declared timed out.",
-    )
-
-    @property
-    def api_v2_url(self) -> str:
-        return f"{self.access_url}/api/v2/accounts/{self.account_id}"
-
-    def _get_session(self) -> requests.Session:
-        session = requests.Session()
-        session.headers.update(
-            {
-                "Accept": "application/json",
-                "Authorization": f"Token {self.token}",
-            }
-        )
-        return session
-
-    def _make_request(
-        self,
-        method: str,
-        endpoint: str,
-        base_url: str,
-        data: Optional[Mapping[str, Any]] = None,
-        params: Optional[Mapping[str, Any]] = None,
-    ) -> Mapping[str, Any]:
-        url = f"{base_url}/{endpoint}"
-
-        num_retries = 0
-        while True:
-            try:
-                session = self._get_session()
-                response = session.request(
-                    method=method,
-                    url=f"{self.api_v2_url}/{endpoint}",
-                    json=data,
-                    params=params,
-                    timeout=self.request_timeout,
-                )
-                response.raise_for_status()
-                return response.json()
-            except RequestException as e:
-                self._log.error(
-                    f"Request to dbt Cloud API failed for url {url} with method {method} : {e}"
-                )
-                if num_retries == self.request_max_retries:
-                    break
-                num_retries += 1
-                time.sleep(self.request_retry_delay)
-
-        raise Failure(f"Max retries ({self.request_max_retries}) exceeded with url: {url}.")
-
-    def create_job(
-        self, *, project_id: int, environment_id: int, job_name: str
-    ) -> Mapping[str, Any]:
-        """Creates a dbt cloud job spec'ed to do what dagster expects."""
-        return self._make_request(
-            method="post",
-            endpoint="jobs",
-            base_url=self.api_v2_url,
-            data={
-                "account_id": self.account_id,
-                "environment_id": environment_id,
-                "project_id": project_id,
-                "name": job_name,
-                "description": "A job that runs dbt models, sources, and tests.",
-                "job_type": "other",
-            },
-        )
-
-    def list_jobs(
-        self,
-        project_id: int,
-        environment_id: int,
-    ) -> Sequence[Mapping[str, Any]]:
-        results = []
-        while jobs := self._make_request(
-            method="get",
-            endpoint="jobs",
-            params={
-                "account_id": self.account_id,
-                "environment_id": environment_id,
-                "project_id": project_id,
-                "limit": LIST_JOBS_INDIVIDUAL_REQUEST_LIMIT,
-                "offset": len(results),
-            },
-        )["data"]:
-            results.extend(jobs)
-            if len(jobs) < LIST_JOBS_INDIVIDUAL_REQUEST_LIMIT:
-                break
-        return results
-
-    def trigger_job(self, job_id: int, steps: Optional[Sequence[str]] = None) -> Mapping[str, Any]:
-        return self._make_request(
-            method="post",
-            endpoint=f"jobs/{job_id}/run",
-            base_url=self.api_v2_url,
-            data={"steps_override": steps, "cause": "Triggered by dagster."},
-        )
-
-    def _get_job_run_details(self, job_run_id: int) -> Mapping[str, Any]:
-        return self._make_request(
-            method="get",
-            endpoint=f"runs/{job_run_id}",
-            base_url=self.api_v2_url,
-        )
-
-    def poll_run(self, job_run_id: int, poll_timeout: Optional[float] = None) -> Mapping[str, Any]:
-        if not poll_timeout:
-            poll_timeout = DEFAULT_POLL_TIMEOUT
-        start_time = time.time()
-        while time.time() - start_time < poll_timeout:
-            run_details = self._get_job_run_details(job_run_id)
-            if run_details["data"]["status"] in {10, 20, 30}:
-                return run_details
-            time.sleep(0.1)
-        raise Exception(f"Run {job_run_id} did not complete within {poll_timeout} seconds.")
-
-    def get_run_artifact(self, job_run_id: int, path: str) -> Mapping[str, Any]:
-        return self._make_request(
-            method="get", endpoint=f"runs/{job_run_id}/artifacts/{path}", base_url=self.api_v2_url
-        )
-
-    def get_run_results_json(self, job_run_id: int) -> Mapping[str, Any]:
-        return self.get_run_artifact(job_run_id=job_run_id, path="run_results.json")
-
-    def get_run_manifest_json(self, job_run_id: int) -> Mapping[str, Any]:
-        return self.get_run_artifact(job_run_id=job_run_id, path="manifest.json")
 
 
 @preview
@@ -199,7 +35,6 @@ class DbtCloudWorkspaceData:
 
 
 @preview
->>>>>>> c584056d68 ([4/n][dagster-dbt] Implement fetch_workspace_data for DbtCloudWorkspace)
 class DbtCloudCredentials(NamedTuple):
     """The DbtCloudCredentials to access your dbt Cloud Workspace."""
 
