38890ab705fb6b52e059aeaede8fdd6714f90125
Owen Kephart <owenkephart@gmail.com>
2025-03-04 14:35:39 -0800

    [components] Allow Definitions component to pick up raw Definitions objects

--- python_modules/libraries/dagster-components/dagster_components/components/definitions_component/component.py
+++ python_modules/libraries/dagster-components/dagster_components/components/definitions_component/component.py
@@ -5,6 +5,7 @@ from dagster._core.definitions.definitions_class import Definitions
 from dagster._core.definitions.module_loaders.load_defs_from_module import (
     load_definitions_from_module,
 )
+from dagster._core.definitions.module_loaders.utils import find_objects_in_module_of_types
 from pydantic import Field
 
 from dagster_components import Component, ComponentLoadContext
@@ -24,8 +25,15 @@ class DefinitionsComponent(Component, ResolvableModel):
     )
 
     def build_defs(self, context: ComponentLoadContext) -> Definitions:
-        return load_definitions_from_module(
-            context.load_component_relative_python_module(
-                Path(self.definitions_path) if self.definitions_path else Path("definitions.py")
+        defs_path = Path(self.definitions_path) if self.definitions_path else Path("definitions.py")
+        defs_module = context.load_component_relative_python_module(defs_path)
+        defs_attrs = list(find_objects_in_module_of_types(defs_module, Definitions))
+        if len(defs_attrs) > 1:
+            raise ValueError(
+                f"Found multiple Definitions objects in {defs_path}. "
+                "At most one Definitions object may be specified when using the DefinitionsComponent."
             )
-        )
+        elif len(defs_attrs) == 1:
+            return defs_attrs[0]
+        else:
+            return load_definitions_from_module(defs_module)
