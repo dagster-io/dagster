7fcc18eeaede21a9cad5280b0886ea82c91e2728
Maxime Armstrong <maxime@retain.ai>
2025-03-06 16:40:52 -0500

    Add tests

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/cloud/preview/test_specs.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/cloud/preview/test_specs.py
@@ -18,3 +18,36 @@ def test_fetch_dbt_cloud_workspace_data(
     assert workspace_data.environment_id == TEST_ENVIRONMENT_ID
     assert workspace_data.job_id == TEST_JOB_ID
     assert workspace_data.manifest == get_sample_manifest_json()
+
+
+def test_load_asset_specs(
+    workspace: DbtCloudWorkspace,
+    fetch_workspace_data_api_mocks: responses.RequestsMock,
+) -> None:
+    all_assets = workspace.load_asset_specs()
+    all_assets_keys = [asset.key for asset in all_assets]
+
+    # 8 dbt models
+    assert len(all_assets) == 8
+    assert len(all_assets_keys) == 8
+
+    # Sanity check outputs
+    first_asset_key = next(key for key in sorted(all_assets_keys))
+    assert first_asset_key.path == ["customers"]
+
+
+def test_load_check_specs(
+    workspace: DbtCloudWorkspace,
+    fetch_workspace_data_api_mocks: responses.RequestsMock,
+) -> None:
+    all_checks = workspace.load_check_specs()
+    all_checks_keys = [check.key for check in all_checks]
+
+    # 8 dbt models
+    assert len(all_checks) == 20
+    assert len(all_checks_keys) == 20
+
+    # Sanity check outputs
+    first_check_key = next(key for key in sorted(all_checks_keys))
+    assert first_check_key.name == "not_null_customers_customer_id"
+    assert first_check_key.asset_key.path == ["customers"]
