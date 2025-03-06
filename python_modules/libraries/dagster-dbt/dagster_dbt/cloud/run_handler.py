b846c2570247d533e263f71fa2bb363b477e1ba4
Maxime Armstrong <maxime@retain.ai>
2025-03-06 14:59:14 -0500

    Fix var

--- python_modules/libraries/dagster-dbt/dagster_dbt/cloud/run_handler.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/cloud/run_handler.py
@@ -14,7 +14,7 @@ class DbtCloudJobRunHandler:
 
     job_id: int
     args: Sequence[str]
-    run: DbtCloudRun
+    dbt_cloud_run: DbtCloudRun
     client: DbtCloudWorkspaceClient
 
     @classmethod
@@ -22,15 +22,15 @@ class DbtCloudJobRunHandler:
         cls, job_id: int, args: Sequence[str], client: DbtCloudWorkspaceClient
     ) -> "DbtCloudJobRunHandler":
         run_details = client.trigger_job_run(job_id, steps_override=[" ".join(["dbt", *args])])
-        run = DbtCloudRun.from_run_details(run_details=run_details)
+        dbt_cloud_run = DbtCloudRun.from_run_details(run_details=run_details)
         return DbtCloudJobRunHandler(
             job_id=job_id,
             args=args,
-            run=run,
+            dbt_cloud_run=dbt_cloud_run,
             client=client,
         )
 
     def wait_for_success(self) -> DbtCloudJobRunStatusType:
-        run_details = self.client.poll_run(run_id=self.run.id)
-        self.run = DbtCloudRun.from_run_details(run_details=run_details)
-        return self.run.status
+        run_details = self.client.poll_run(run_id=self.dbt_cloud_run.id)
+        self.dbt_cloud_run = DbtCloudRun.from_run_details(run_details=run_details)
+        return self.dbt_cloud_run.status
