051b84e812070bef6cad0cae58318451a3ada430
Nick Schrock <schrockn@dagsterlabs.com>
2025-03-07 14:28:42 -0500

    cp

--- examples/experimental/assets_yaml_dsl_components/setup.py
+++ examples/experimental/assets_yaml_dsl_components/setup.py
@@ -6,7 +6,7 @@ setup(
     install_requires=["dagster", "pandas", "dagster-pipes"],
     license="Apache-2.0",
     description="Dagster example of yaml dsl for building asset graphs",
-    url="https://github.com/dagster-io/dagster/tree/master/examples/assets_yaml_dsl",
+    url="https://github.com/dagster-io/dagster/tree/master/examples/assets_yaml_components_dsl",
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
