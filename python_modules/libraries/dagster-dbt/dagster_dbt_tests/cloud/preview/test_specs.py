d198150b8a221956db8f69b026e1787d70def8e7
Maxime Armstrong <maxime@retain.ai>
2025-03-10 17:19:12 -0400

    Update test

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/cloud/preview/test_specs.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/cloud/preview/test_specs.py
@@ -2,10 +2,10 @@ import responses
 from dagster_dbt.cloud.resources import DbtCloudWorkspace
 
 from dagster_dbt_tests.cloud.preview.conftest import (
-    SAMPLE_MANIFEST_JSON,
     TEST_ENVIRONMENT_ID,
     TEST_JOB_ID,
     TEST_PROJECT_ID,
+    get_sample_manifest_json,
 )
 
 
@@ -17,4 +17,4 @@ def test_fetch_dbt_cloud_workspace_data(
     assert workspace_data.project_id == TEST_PROJECT_ID
     assert workspace_data.environment_id == TEST_ENVIRONMENT_ID
     assert workspace_data.job_id == TEST_JOB_ID
-    assert workspace_data.manifest == SAMPLE_MANIFEST_JSON
+    assert workspace_data.manifest == get_sample_manifest_json()
