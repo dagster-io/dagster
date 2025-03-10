dagster-io/dagster

commit 5a139ce5d46f42625de5a555fb0171c690830e6b

[Selection syntax] Update logic using old syntax to use new syntax when feature flag is enabled (#28337)
## Summary & Motivation

As titled.

js_modules/dagster-ui/packages/ui-core/src/assets/globalAssetGraphPathToString.tsx
@@ -1,3 +1,6 @@
+import {FeatureFlag} from 'shared/app/FeatureFlags.oss';
+
+import {featureEnabled} from '../app/Flags';
 import {tokenForAssetKey} from '../asset-graph/Utils';
 import {AssetKeyInput} from '../graphql/types';
 import {
@@ -20,10 +23,15 @@ export function globalAssetGraphPathFromString(pathName: string) {
 }
 
 export function globalAssetGraphPathForAssetsAndDescendants(assetKeys: AssetKeyInput[]) {
-  // In a perfect world we populate ops query to "asset1*,asset2*" and then select the roots
-  // by passing opNames. If we don't have enough characters to do both, just populate the ops
-  // query. It might still be too long, but we tried.
-  const opsQuery = assetKeys.map((a) => `${tokenForAssetKey(a)}*`).join(', ');
+  let opsQuery;
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    opsQuery = assetKeys.map((a) => `key:"${tokenForAssetKey(a)}"+`).join(' or ');
+  } else {
+    // In a perfect world we populate ops query to "asset1*,asset2*" and then select the roots
+    // by passing opNames. If we don't have enough characters to do both, just populate the ops
+    // query. It might still be too long, but we tried.
+    opsQuery = assetKeys.map((a) => `${tokenForAssetKey(a)}*`).join(', ');
+  }
   const opNames =
     opsQuery.length > URL_MAX_LENGTH / 2 ? [] : [assetKeys.map(tokenForAssetKey).join(',')];
   return globalAssetGraphPathToString({opNames, opsQuery});

js_modules/dagster-ui/packages/ui-core/src/gantt/GanttChart.tsx
@@ -169,8 +169,13 @@ export const GanttChart = (props: GanttChartProps) => {
 
   const onDoubleClickStep = React.useCallback(
     (stepKey: string) => {
-      const query = `*${stepKey}*`;
-      onUpdateQuery(selection.query !== query ? query : '*');
+      if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+        const query = `+name:"${stepKey}"+`;
+        onUpdateQuery(selection.query !== query ? query : '*');
+      } else {
+        const query = `*${stepKey}*`;
+        onUpdateQuery(selection.query !== query ? query : '*');
+      }
     },
     [onUpdateQuery, selection.query],
   );

js_modules/dagster-ui/packages/ui-core/src/ui/GraphQueryInput.tsx
@@ -21,8 +21,10 @@ import isEqual from 'lodash/isEqual';
 import uniq from 'lodash/uniq';
 import * as React from 'react';
 import {Link} from 'react-router-dom';
+import {FeatureFlag} from 'shared/app/FeatureFlags.oss';
 import styled from 'styled-components';
 
+import {featureEnabled} from '../app/Flags';
 import {GraphQueryItem, filterByQuery} from '../app/GraphQueryImpl';
 import {dynamicKeyWithoutIndex, isDynamicStep} from '../gantt/DynamicStepSupport';
 import {GraphExplorerSolidFragment} from '../pipelines/types/GraphExplorer.types';
@@ -93,16 +95,36 @@ export const placeholderTextForItems = (base: string, items: GraphQueryItem[]) =
 
   if (seed === 0) {
     const example = ranked.sort((a, b) => b.outcount - a.outcount)[0];
-    placeholder = `${placeholder} (ex: ${example!.name}+*)`;
+    placeholder = `${placeholder} (ex: ${key(example!.name)}${traversal('down')})`;
   } else if (seed === 1) {
     const example = ranked.sort((a, b) => b.outcount - a.outcount)[0];
-    placeholder = `${placeholder} (ex: ${example!.name}+)`;
+    placeholder = `${placeholder} (ex: ${key(example!.name)}${traversal('down', 1)})`;
   } else if (seed === 2) {
     const example = ranked.sort((a, b) => b.incount - a.incount)[0];
-    placeholder = `${placeholder} (ex: ++${example!.name})`;
+    placeholder = `${placeholder} (ex: ${traversal('up', 2)}${key(example!.name)})`;
   }
   return placeholder;
 };
+
+function key(name: string) {
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    return `name:"${name}"`;
+  }
+  return name;
+}
+
+function traversal(direction: 'up' | 'down', levels?: number) {
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    if (levels === undefined) {
+      return '+';
+    }
+    return direction === 'up' ? `+${levels}` : `${levels}+`;
+  }
+  if (levels === undefined) {
+    return '*';
+  }
+  return '+'.repeat(levels);
+}
 
 const intentToStrokeColor = (intent: Intent | undefined) => {
   switch (intent) {

commit 65240b18a02eada720291dc7e55bdce249a1bf95

fix test (#28353)
## Summary & Motivation

Broken by https://github.com/dagster-io/dagster/pull/28337/

## How I Tested These Changes
jest

js_modules/dagster-ui/packages/ui-core/src/assets/globalAssetGraphPathToString.tsx
@@ -34,5 +34,6 @@ export function globalAssetGraphPathForAssetsAndDescendants(assetKeys: AssetKeyI
   }
   const opNames =
     opsQuery.length > URL_MAX_LENGTH / 2 ? [] : [assetKeys.map(tokenForAssetKey).join(',')];
+
   return globalAssetGraphPathToString({opNames, opsQuery});
 }

js_modules/dagster-ui/packages/ui-core/src/assets/__tests__/globalAssetGraphPathToString.test.tsx
@@ -20,17 +20,17 @@ describe('Global Graph URLs', () => {
   describe('globalAssetGraphPathForAssetsAndDescendants', () => {
     it('should return a valid path for a single asset', async () => {
       const url = globalAssetGraphPathForAssetsAndDescendants([{path: ['asset_0']}]);
-      expect(url).toEqual(`/asset-groups~asset_0*/asset_0`);
+      expect(url).toEqual(`/asset-groups~${encodeURIComponent('key:"asset_0"+')}/asset_0`);
     });
 
     it('should avoid exceeding a 32k character URL', async () => {
-      const keysLarge = new Array(900).fill(0).map((_, idx) => ({path: [`asset_${idx}`]}));
+      const keysLarge = new Array(800).fill(0).map((_, idx) => ({path: [`asset_${idx}`]}));
       const urlLarge = globalAssetGraphPathForAssetsAndDescendants(keysLarge);
-      expect(urlLarge.length).toEqual(24986);
+      expect(urlLarge.length).toEqual(34984);
 
-      const keysHuge = new Array(1500).fill(0).map((_, idx) => ({path: [`asset_${idx}`]}));
+      const keysHuge = new Array(900).fill(0).map((_, idx) => ({path: [`asset_${idx}`]}));
       const urlHuge = globalAssetGraphPathForAssetsAndDescendants(keysHuge);
-      expect(urlHuge.length).toEqual(24399); // smaller because the selection is not passed
+      expect(urlHuge.length).toEqual(28697); // smaller because the selection is not passed
     });
   });
 });