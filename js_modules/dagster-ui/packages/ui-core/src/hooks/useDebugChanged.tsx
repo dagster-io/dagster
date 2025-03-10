c6113dd7151357c8ca44776067132ad87d90f517
Marco Salazar <marco@elementl.com>
2025-03-10 11:46:06 -0400

    [INTERNAL_BRANCH=salazarm/fix-moving-owner-filter-catalog]

--- js_modules/dagster-ui/packages/ui-core/src/hooks/useDebugChanged.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/hooks/useDebugChanged.tsx
@@ -18,7 +18,7 @@ const getDifferences = <T extends Record<any, any>>(
   obj2: T | undefined,
 ): Partial<Record<keyof T, Difference<any>>> => {
   const diffs: Partial<Record<keyof T, Difference<any>>> = {};
-  Object.keys({...obj1, ...obj2}).forEach((key) => {
+  Object.keys({...(obj1 || {}), ...(obj2 || {})}).forEach((key) => {
     if (!isEqual(obj1?.[key], obj2?.[key])) {
       diffs[key as keyof T] = {previous: obj1?.[key], current: obj2?.[key]};
     }
