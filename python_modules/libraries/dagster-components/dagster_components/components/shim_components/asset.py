57aa9b92cfd38ddbfec5d92dc189d63f1b0dbaa4
Owen Kephart <owenkephart@gmail.com>
2025-03-11 08:23:49 -0700

    [dg] Update output location of sensor / schedule / asset

--- python_modules/libraries/dagster-components/dagster_components/components/shim_components/asset.py
+++ python_modules/libraries/dagster-components/dagster_components/components/shim_components/asset.py
@@ -1,21 +1,15 @@
-from pathlib import Path
-
-from dagster._utils import pushd
-
-from dagster_components import Blueprint, ScaffoldRequest
 from dagster_components.blueprint import scaffold_with
-from dagster_components.components.shim_components.base import ShimComponent
+from dagster_components.components.shim_components.base import ShimBlueprint, ShimComponent
 
 
-class AssetBlueprint(Blueprint):
-    def scaffold(self, request: ScaffoldRequest, params: None) -> None:
-        with pushd(str(request.target_path)):
-            Path("definitions.py").write_text("""# import dagster as dg
+class AssetBlueprint(ShimBlueprint):
+    def get_text(self) -> str:
+        return """# import dagster as dg
 # 
 #
 # @dg.asset
 # def my_asset(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
-""")
+"""
 
 
 @scaffold_with(AssetBlueprint)
