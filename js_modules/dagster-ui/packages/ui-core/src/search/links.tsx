3bd0769898aac6c5f7bcf9a425d9f631ada9fbfb
Marco polo <salazarm@users.noreply.github.com>
2025-03-11 13:02:11 -0500

    [Selection syntax] Update search links (#28388)
    ## Summary & Motivation
    As titled.
    
    ## How I Tested These Changes
    
    Manually tested asset search in cloud.

--- js_modules/dagster-ui/packages/ui-core/src/search/links.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/search/links.tsx
@@ -1,39 +1,71 @@
 import qs from 'qs';
+import {FeatureFlag} from 'shared/app/FeatureFlags.oss';
 
 import {GroupMetadata} from './BuildAssetSearchResults';
+import {featureEnabled} from '../app/Flags';
 import {AssetOwner, DefinitionTag} from '../graphql/types';
 import {repoAddressAsURLString} from '../workspace/repoAddressAsString';
 import {RepoAddress} from '../workspace/types';
 
 export const linkToAssetTableWithGroupFilter = (groupMetadata: GroupMetadata) => {
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    return `/assets?${qs.stringify({
+      'asset-selection': `group:${groupMetadata.groupName} and code_location:${groupMetadata.repositoryLocationName}`,
+    })}`;
+  }
   return `/assets?${qs.stringify({groups: JSON.stringify([groupMetadata])})}`;
 };
 
 export const linkToAssetTableWithKindFilter = (kind: string) => {
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    return `/assets?${qs.stringify({
+      'asset-selection': `kind:${kind}`,
+    })}`;
+  }
   return `/assets?${qs.stringify({
     kinds: JSON.stringify([kind]),
   })}`;
 };
 
 export const linkToAssetTableWithTagFilter = (tag: Omit<DefinitionTag, '__typename'>) => {
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    return `/assets?${qs.stringify({
+      'asset-selection': `tag:"${tag.key}"${tag.value ? `="${tag.value}"` : ''}`,
+    })}`;
+  }
   return `/assets?${qs.stringify({
     tags: JSON.stringify([tag]),
   })}`;
 };
 
 export const linkToAssetTableWithAssetOwnerFilter = (owner: AssetOwner) => {
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    return `/assets?${qs.stringify({
+      'asset-selection': `owner:${owner.__typename === 'TeamAssetOwner' ? owner.team : owner.email}`,
+    })}`;
+  }
   return `/assets?${qs.stringify({
     owners: JSON.stringify([owner]),
   })}`;
 };
 
 export const linkToAssetTableWithColumnsFilter = (columns: string[]) => {
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    return `/assets?${qs.stringify({
+      'asset-selection': `column:${columns.join(',')}`,
+    })}`;
+  }
   return `/assets?${qs.stringify({
     columns: JSON.stringify(columns),
   })}`;
 };
 
 export const linkToAssetTableWithColumnTagFilter = (tag: Omit<DefinitionTag, '__typename'>) => {
+  if (featureEnabled(FeatureFlag.flagSelectionSyntax)) {
+    return `/assets?${qs.stringify({
+      'asset-selection': `column_tag:"${tag.key}"${tag.value ? `="${tag.value}"` : ''}`,
+    })}`;
+  }
   return `/assets?${qs.stringify({
     columnTags: JSON.stringify([tag]),
   })}`;
