8ceaab059d9f74d7c0004fefeebad4ed72661fb3
Tomasz Zbrozek <tomasz.zbrozek@gmail.com>
2025-03-12 17:53:50 +0100

    patch: fallback in case _event_status is missing

--- python_modules/libraries/dagster-dbt/dagster_dbt/core/dbt_cli_event.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/core/dbt_cli_event.py
@@ -332,7 +332,10 @@ class DbtCliEventMessage:
             "invocation_id": invocation_id,
         }
 
-        if event_node_info.get("node_started_at") == "" and event_node_info.get("node_finished_at") == "":
+        if (
+                event_node_info.get("node_started_at") in ["", "None", None] and
+                event_node_info.get("node_finished_at") in ["", "None", None]
+        ):
             # if model materialization is incremental microbatch, node_started_at and node_finished_at are empty strings
             # and require fallback to data.execution_time
             default_metadata["Execution Duration"] = self.raw_event["data"]["execution_time"]
@@ -347,7 +350,7 @@ class DbtCliEventMessage:
         # if model materialization is incremental microbatch, node_status property is "None", hence fall back to status
         node_status: str = (
             self.raw_event["data"]["status"].lower()
-            if event_node_info["node_status"] == "None"
+            if event_node_info["node_status"] in ["", "None", None]
             else event_node_info["node_status"]
         )
         node_materialization: str = self.raw_event["data"]["node_info"]["materialized"]
