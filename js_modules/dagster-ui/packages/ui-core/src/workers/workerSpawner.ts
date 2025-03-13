2b7197c63d78e36a1ccbf4e79f431726617879b5
Marco Salazar <marco@elementl.com>
2025-03-13 09:56:26 -0400

    ts

--- js_modules/dagster-ui/packages/ui-core/src/workers/workerSpawner.ts
+++ js_modules/dagster-ui/packages/ui-core/src/workers/workerSpawner.ts
@@ -1,6 +1,6 @@
 import {Worker} from 'shared/workers/Worker.oss';
 export function workerSpawner(createWorker: () => Worker) {
-  let nextWorker: Worker | null = createWorker();
+  let nextWorker: Worker = createWorker();
   let currentWorker: Worker | null = null;
   return function spawnNewWorker() {
     // Make sure we only have one worker at a time
