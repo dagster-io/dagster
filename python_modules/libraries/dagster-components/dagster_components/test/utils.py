d2e229e7dbe38094ae4d61c080032a59db7c97a3
alangenfeld <alex@elementl.com>
2025-03-14 10:29:51 -0500

    [resolved] handle ResolvableModel fields on ResolvedFrom types

---
+++ python_modules/libraries/dagster-components/dagster_components/test/utils.py
@@ -0,0 +1,21 @@
+from typing_extensions import TypeVar
+
+from dagster_components.core.component import Component, ComponentLoadContext
+from dagster_components.core.component_decl_builder import DirectForTestComponentDecl
+
+TComponent = TypeVar("TComponent", bound=Component)
+
+
+def load_direct(
+    component_type: type[TComponent],
+    attribute_yaml: str,
+) -> TComponent:
+    decl_node = DirectForTestComponentDecl(
+        component_type=component_type,
+        attributes_yaml=attribute_yaml,
+    )
+    context = ComponentLoadContext.for_test(
+        decl_node=decl_node,
+    )
+    components = decl_node.load(context)
+    return components[0]  # type: ignore
--- python_modules/libraries/dagster-components/dagster_components_tests/unit_tests/test_component_invariant_errors.py
+++ python_modules/libraries/dagster-components/dagster_components_tests/unit_tests/test_component_invariant_errors.py
@@ -1,4 +1,9 @@
+from dataclasses import dataclass
+from typing import Annotated, Optional
+
 from dagster_components import Component, ComponentLoadContext
+from dagster_components.resolved.model import ResolvableModel, ResolvedFrom, Resolver
+from dagster_components.test.utils import load_direct
 
 
 def test_component_does_not_implement_resolved_anything():
@@ -6,3 +11,69 @@ def test_component_does_not_implement_resolved_anything():
         def build_defs(self, context): ...
 
     assert AComponent.load(attributes=None, context=ComponentLoadContext.for_test())
+
+
+def test_nested_resolvable():
+    class MyModel(ResolvableModel):
+        foo: str
+
+    class ResolvableComponent(Component, ResolvableModel):
+        thing: MyModel
+
+        def build_defs(self, _):
+            return []
+
+    c = load_direct(
+        ResolvableComponent,
+        """
+thing:
+  foo: hi
+        """,
+    )
+    assert c.thing.foo
+
+    class ComponentModel(ResolvableModel):
+        thing: MyModel
+        num: str
+
+    @dataclass
+    class ResolveFromComponent(Component, ResolvedFrom[ComponentModel]):
+        thing: MyModel
+        num: Annotated[int, Resolver(lambda _, v: int(v))]
+
+        def build_defs(self, _):
+            return []
+
+    c = load_direct(
+        ResolveFromComponent,
+        """
+num: '123'
+thing:
+  foo: hi
+        """,
+    )
+    assert c.thing.foo
+
+    class ListComponentModel(ResolvableModel):
+        thing: Optional[list[MyModel]]
+        num: str
+
+    @dataclass
+    class ResolveFromListComponent(Component, ResolvedFrom[ListComponentModel]):
+        thing: Optional[list[MyModel]]
+        num: Annotated[int, Resolver(lambda _, v: int(v))]
+
+        def build_defs(self, _):
+            return []
+
+    c = load_direct(
+        ResolveFromListComponent,
+        """
+num: '123'
+thing:
+    - foo: hi
+    - foo: bye
+        """,
+    )
+    assert c.thing
+    assert c.thing[0].foo
