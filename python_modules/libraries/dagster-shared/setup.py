b53c7ccc2b41f69facaef1513ea394cdb7b9f1c7
gibsondan <daniel@elementl.com>
2025-03-16 22:40:39 -0500

    [1/n] Add empty dagster-shared package (#28404)
    Summary:
    Not hooked up to anything yet, just creating the empty package (and
    updating the template package to use the latest syntax)
    
    I have this in libraries currently (which means it would be 0.26.x not
    1.10.x) but not wed to that.
    
    NOCHANGELOG
    
    ## Summary & Motivation
    
    ## How I Tested These Changes
    
    ## Changelog
    
    > Insert changelog entry or delete this section.

--- python_modules/libraries/dagster-shared/setup.py
+++ python_modules/libraries/dagster-shared/setup.py
@@ -5,26 +5,25 @@ from setuptools import find_packages, setup
 
 def get_version() -> str:
     version: dict[str, str] = {}
-    with open(Path(__file__).parent / "{{ underscore_name }}/version.py", encoding="utf8") as fp:
+    with open(Path(__file__).parent / "dagster_shared/version.py", encoding="utf8") as fp:
         exec(fp.read(), version)
 
     return version["__version__"]
 
-# TODO - add your package to scripts/install_dev_python_modules.py
 
 ver = get_version()
 # dont pin dev installs to avoid pip dep resolver issues
 pin = "" if ver == "1!0+dev" else f"=={ver}"
 setup(
-    name="{{ underscore_name }}",
+    name="dagster_shared",
     version=get_version(),
     author="Dagster Labs",
     author_email="hello@dagsterlabs.com",
     license="Apache-2.0",
-    description="",  # TODO - fill out description
+    description="Shared code between dagster and dagster-dg.",
     url=(
         "https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/"
-        "{{ hyphen_name }}"
+        "dagster-shared"
     ),
     classifiers=[
         "Programming Language :: Python :: 3.9",
@@ -34,11 +33,11 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    packages=find_packages(exclude=["{{ underscore_name }}_tests*"]),
+    packages=find_packages(exclude=["dagster_shared_tests*"]),
     include_package_data=True,
-    install_requires=[
-        f"dagster{pin}",
-        # TODO - fill in remaining dependencies
-    ],
+    install_requires=[],
+    extras_require={
+        "test": ["pytest"],
+    },
     zip_safe=False,
 )
