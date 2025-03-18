dagster-io/dagster

commit 7862ae82ee9e4cbcea50acbb1ea5c4f1ff1636e9

Stash, retrieve `StateBackedDefinitionsLoader` reconstruction data in `DefinitionsLoadContext`  (#25436)
## Summary

Currently, `StateBackedDefinitionsLoader` stashes its reconstruction
data in the output `Definitions`. This means that in order for it to be
persisted, that `Definitions` object must make its way to the terminal
user-specified `defs`. In the case that we build APIs which emit
non-Definitions objects as a result of cached computation (e.g. specs),
we lose that reconstruction data.

This PR moves to a paradigm where we store this reconstruction data in
the `DefinitionsLoadContext`, where it's retrieved and attached to the
final `RepositoryDefinition`.

## Test Plan

Existing tests.

python_modules/dagster/dagster/_core/definitions/definitions_load_context.py
@@ -46,6 +46,7 @@ class DefinitionsLoadContext:
     ):
         self._load_type = load_type
         self._repository_load_data = repository_load_data
+        self._pending_reconstruction_metadata = {}
 
     @classmethod
     def get(cls) -> "DefinitionsLoadContext":
@@ -63,6 +64,12 @@ class DefinitionsLoadContext:
     def load_type(self) -> DefinitionsLoadType:
         """DefinitionsLoadType: Classifier for scenario in which Definitions are being loaded."""
         return self._load_type
+
+    def add_to_pending_reconstruction_metadata(self, key: str, metadata: Any) -> None:
+        self._pending_reconstruction_metadata[key] = metadata
+
+    def get_pending_reconstruction_metadata(self) -> Mapping[str, Any]:
+        return self._pending_reconstruction_metadata
 
     @cached_property
     def reconstruction_metadata(self) -> Mapping[str, Any]:
@@ -160,6 +167,6 @@ class StateBackedDefinitionsLoader(ABC, Generic[TState]):
             else self.fetch_state()
         )
 
-        return self.defs_from_state(state).with_reconstruction_metadata(
-            {self.defs_key: serialize_value(state)}
-        )
+        context.add_to_pending_reconstruction_metadata(self.defs_key, serialize_value(state))
+
+        return self.defs_from_state(state)

commit 09d088a2e335d6416725448b663e99366bb21d33

Fix reconstruction metadata routing through DefinitionsLoadContext (#28416)
## Summary & Motivation

Fix a longstanding regression in reconstruction metadata routing that was introduced in #25436. Prior to that PR, reconstruction metadata during initial repository load was stored on `Definitions` objects in the `metadata` field. Integrations that used the reconstruction metadata API were expected to return a `Definitions` object that would be merged into the final `Definitions` object for the code location. This final `Definitions` object would then contained the merged reconstruction metadata for any integrations using the API:

```
my_integration_resource = MyIntegrationResource(...)
integration_defs = build_my_integration_defs(my_integration_resource)

# final defs object merges together metadata from all passed in defs objects
defs = Definitions.merge(
  integration_defs,
  # ... other definitions
)
```

#25436 aimed to eliminate the restriction that integrations using reconstruction metadata needed to return `Definitions` objects, so that they could return e.g. `AssetSpecs` without any wrapper. It did this by switching the channel by which reconstruction metadata was routed from `Definitions.metadata` to `DefinitionsLoadContext`. New methods `DefinitionsLoadContext.{add,get}_pending_reconstruction_metadata` were added. The idea was that integrations would stash their reconstruction metadata on a process-scoped `DefinitionsLoadContext`. Then the final bundle of reconstruction metadata would be pulled off the global `DefinitionsLoadContext` where previously it had been pulled off the final `Definitions` object:

```
my_integration_resource = MyIntegrationResource(...)
# build_my_integration_assets modified globally available `DefinitionsLoadContext`
integration_assets = build_my_integration_assets(my_integration_resource)

# final defs object merges together metadata from all passed in defs objects
defs = Definitions.merge(

python_modules/dagster/dagster/_core/definitions/decorators/repository_decorator.py
@@ -9,9 +9,6 @@ from dagster._core.definitions.graph_definition import GraphDefinition
 from dagster._core.definitions.job_definition import JobDefinition
 from dagster._core.definitions.logger_definition import LoggerDefinition
 from dagster._core.definitions.metadata import RawMetadataValue, normalize_metadata
-from dagster._core.definitions.metadata.metadata_value import (
-    CodeLocationReconstructionMetadataValue,
-)
 from dagster._core.definitions.partitioned_schedule import (
     UnresolvedPartitionedAssetScheduleDefinition,
 )
@@ -87,7 +84,10 @@ class _Repository:
     ) -> RepositoryDefinition:
         from dagster._core.definitions import AssetsDefinition, SourceAsset
         from dagster._core.definitions.cacheable_assets import CacheableAssetsDefinition
-        from dagster._core.definitions.definitions_load_context import DefinitionsLoadContext
+        from dagster._core.definitions.definitions_load_context import (
+            DefinitionsLoadContext,
+            DefinitionsLoadType,
+        )
 
         check.callable_param(fn, "fn")
 
@@ -95,14 +95,14 @@ class _Repository:
             self.name = fn.__name__
 
         cacheable_asset_data: dict[str, Sequence[AssetsDefinitionCacheableData]] = {}
-        reconstruction_metadata = {
-            k: v
-            for k, v in self.metadata.items()
-            if isinstance(v, CodeLocationReconstructionMetadataValue)
-        }
 
         repository_definitions = fn()
         context = DefinitionsLoadContext.get()
+
+        if context.load_type == DefinitionsLoadType.INITIALIZATION:
+            reconstruction_metadata = context.get_pending_reconstruction_metadata()
+        else:
+            reconstruction_metadata = context.reconstruction_metadata
 
         if isinstance(repository_definitions, list):
             bad_defns = []

python_modules/dagster/dagster/_core/definitions/definitions_load_context.py
@@ -6,6 +6,9 @@ from typing import TYPE_CHECKING, Any, ClassVar, Generic, Optional, TypeVar, cas
 
 from dagster._core.definitions.cacheable_assets import AssetsDefinitionCacheableData
 from dagster._core.definitions.definitions_class import Definitions
+from dagster._core.definitions.metadata.metadata_value import (
+    CodeLocationReconstructionMetadataValue,
+)
 from dagster._core.errors import DagsterInvariantViolationError
 from dagster._serdes.serdes import PackableValue, deserialize_value, serialize_value
 
@@ -62,6 +65,11 @@ class DefinitionsLoadContext:
         """Get the current DefinitionsLoadContext."""
         cls._instance = instance
 
+    @classmethod
+    def is_set(cls) -> bool:
+        """bool: Whether the context has been set."""
+        return cls._instance is not None
+
     @property
     def load_type(self) -> DefinitionsLoadType:
         """DefinitionsLoadType: Classifier for scenario in which Definitions are being loaded."""
@@ -99,7 +107,10 @@ class DefinitionsLoadContext:
             )
         # Expose the wrapped metadata values so that users access exactly what they put in.
         return (
-            {k: v.data for k, v in self._repository_load_data.reconstruction_metadata.items()}
+            {
+                k: v.data if isinstance(v, CodeLocationReconstructionMetadataValue) else v
+                for k, v in self._repository_load_data.reconstruction_metadata.items()
+            }
             if self._repository_load_data
             else {}
         )

python_modules/dagster/dagster/_core/definitions/reconstruct.py
@@ -785,13 +785,38 @@ def reconstruct_repository_def_from_pointer(
         DefinitionsLoadContext,
         DefinitionsLoadType,
     )
-    from dagster._core.definitions.repository_definition import RepositoryDefinition
-
-    DefinitionsLoadContext.set(
-        DefinitionsLoadContext(
-            load_type=DefinitionsLoadType.RECONSTRUCTION, repository_load_data=repository_load_data
-        )
+    from dagster._core.definitions.repository_definition import (
+        RepositoryDefinition,
+        RepositoryLoadData,
     )
+
+    # This method is called when generating the RepositoryDefinition from a
+    # ReconstructableRepository. It is sometimes called in the initialization context, in which case
+    # we need to make sure that any data present on `pending_reconstruction_metadata` gets set as
+    # "plain" reconstruction metadata in the new context. The distinction between "pending" and
+    # "regular" reconstruction metadata on the same class is confusing and should be eliminated in a
+    # future refactor, but this is a working solution.
+    curr_context = DefinitionsLoadContext.get() if DefinitionsLoadContext.is_set() else None
+    if repository_load_data or not curr_context:
+        context = DefinitionsLoadContext(
+            load_type=DefinitionsLoadType.RECONSTRUCTION,
+            repository_load_data=repository_load_data,
+        )
+    elif curr_context.load_type == DefinitionsLoadType.INITIALIZATION:
+        curr_repo_load_data = curr_context._repository_load_data  # noqa: SLF001
+        context = DefinitionsLoadContext(
+            load_type=DefinitionsLoadType.RECONSTRUCTION,
+            repository_load_data=RepositoryLoadData(
+                cacheable_asset_data=curr_repo_load_data.cacheable_asset_data
+                if curr_repo_load_data
+                else {},
+                reconstruction_metadata=curr_context.get_pending_reconstruction_metadata(),
+            ),
+        )
+    else:
+        context = curr_context
+
+    DefinitionsLoadContext.set(context)
     target = def_from_pointer(pointer)
     repo_def = _repository_def_from_target_def_inner(
         target,

python_modules/dagster/dagster/_grpc/server.py
@@ -24,6 +24,10 @@ from grpc_health.v1 import health, health_pb2, health_pb2_grpc
 import dagster._check as check
 import dagster._seven as seven
 from dagster._core.code_pointer import CodePointer
+from dagster._core.definitions.definitions_load_context import (
+    DefinitionsLoadContext,
+    DefinitionsLoadType,
+)
 from dagster._core.definitions.reconstruct import ReconstructableRepository
 from dagster._core.definitions.repository_definition import RepositoryDefinition
 from dagster._core.errors import (
@@ -239,6 +243,13 @@ class LoadedRepositories:
         self._loadable_repository_symbols: list[LoadableRepositorySymbol] = []
 
         self._container_context = container_context
+
+        # Make sure we have a persistent load context before loading any repositories.
+        DefinitionsLoadContext.set(
+            DefinitionsLoadContext(
+                DefinitionsLoadType.INITIALIZATION,
+            )
+        )
 
         if not loadable_target_origin:
             # empty workspace

python_modules/libraries/dagster-airlift/kitchen-sink/kitchen_sink/dagster_defs/mapped_defs.py
@@ -168,8 +168,13 @@ def build_mapped_defs() -> Definitions:
     )
 
 
+# We need to use a different name for this instance because the reconstruction metadata key is
+# derived from the instance name, and we don't want the reconstruction metadata from the mapped defs
+# instance to clobber the reconstruction metadata from this.
+UNMAPPED_SPECS_INSTANCE_NAME = "unmapped_specs_instance"
+
 unmapped_specs = load_airflow_dag_asset_specs(
-    airflow_instance=local_airflow_instance(),
+    airflow_instance=local_airflow_instance(UNMAPPED_SPECS_INSTANCE_NAME),
     dag_selector_fn=lambda dag: dag.dag_id.startswith("unmapped"),
 )
 

python_modules/dagster-graphql/dagster_graphql/test/utils.py
@@ -3,10 +3,12 @@ import sys
 from collections.abc import Iterator, Mapping, Sequence
 from contextlib import contextmanager
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any, Optional, Union, cast
 
 import dagster._check as check
 import graphene
+from dagster._core.definitions.asset_job import IMPLICIT_ASSET_JOB_NAME
+from dagster._core.definitions.asset_key import AssetKey
 from dagster._core.instance import DagsterInstance
 from dagster._core.remote_representation.external import RemoteRepository
 from dagster._core.test_utils import wait_for_runs_to_finish
@@ -121,7 +123,7 @@ def execute_dagster_graphql_and_finish_runs(
 @contextmanager
 def define_out_of_process_context(
     python_file: str,
-    fn_name: str,
+    fn_name: Optional[str],
     instance: DagsterInstance,
     read_only: bool = False,
     read_only_locations: Optional[Mapping[str, bool]] = None,
@@ -143,7 +145,7 @@ def define_out_of_process_context(
 
 
 def define_out_of_process_workspace(
-    python_file: str, fn_name: str, instance: DagsterInstance, read_only: bool = False
+    python_file: str, fn_name: Optional[str], instance: DagsterInstance, read_only: bool = False
 ) -> WorkspaceProcessContext:
     return WorkspaceProcessContext(
         instance,
@@ -238,3 +240,54 @@ def ensure_dagster_graphql_tests_import() -> None:
         dagster_package_root / "dagster_graphql_tests"
     ).exists(), "Could not find dagster_graphql_tests where expected"
     sys.path.append(dagster_package_root.as_posix())
+
+
+def materialize_assets(
+    context: WorkspaceRequestContext,
+    asset_selection: Optional[Sequence[AssetKey]] = None,
+    partition_keys: Optional[Sequence[str]] = None,
+    run_config_data: Optional[Mapping[str, Any]] = None,
+) -> Union[GqlResult, Sequence[GqlResult]]:
+    from dagster_graphql.client.query import LAUNCH_PIPELINE_EXECUTION_MUTATION
+
+    gql_asset_selection = (
+        cast(Sequence[GqlAssetKey], [key.to_graphql_input() for key in asset_selection])
+        if asset_selection
+        else None
+    )
+    selector = infer_job_selector(
+        context, IMPLICIT_ASSET_JOB_NAME, asset_selection=gql_asset_selection
+    )
+    if partition_keys:
+        results = []
+        for key in partition_keys:
+            results.append(
+                execute_dagster_graphql(
+                    context,
+                    LAUNCH_PIPELINE_EXECUTION_MUTATION,
+                    variables={
+                        "executionParams": {
+                            "selector": selector,
+                            "executionMetadata": {
+                                "tags": [{"key": "dagster/partition", "value": key}]
+                            },
+                            "runConfigData": run_config_data,
+                        }
+                    },
+                )
+            )
+        return results
+    else:
+        selector = infer_job_selector(
+            context, IMPLICIT_ASSET_JOB_NAME, asset_selection=gql_asset_selection
+        )
+        return execute_dagster_graphql(
+            context,
+            LAUNCH_PIPELINE_EXECUTION_MUTATION,
+            variables={
+                "executionParams": {
+                    "selector": selector,
+                    "runConfigData": run_config_data,
+                }
+            },
+        )

python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_data_versions.py
@@ -1,5 +1,5 @@
 from collections.abc import Mapping, Sequence
-from typing import Any, Optional, Union, cast
+from typing import Any, Optional
 
 from dagster import (
     AssetIn,
@@ -21,14 +21,12 @@ from dagster._core.definitions.partition import StaticPartitionsDefinition
 from dagster._core.definitions.unresolved_asset_job_definition import define_asset_job
 from dagster._core.test_utils import instance_for_test, wait_for_runs_to_finish
 from dagster._core.workspace.context import WorkspaceRequestContext
-from dagster_graphql.client.query import LAUNCH_PIPELINE_EXECUTION_MUTATION
 from dagster_graphql.test.utils import (
-    GqlAssetKey,
-    GqlResult,
     define_out_of_process_context,
     execute_dagster_graphql,
     infer_job_selector,
     infer_repository_selector,
+    materialize_assets,
 )
 
 from dagster_graphql_tests.graphql.test_assets import (
@@ -67,12 +65,11 @@ def get_repo_v2():
 
 
 def test_dependencies_changed():
-    repo_v1 = get_repo_v1()
     repo_v2 = get_repo_v2()
 
     with instance_for_test(synchronous_run_coordinator=True) as instance:
         with define_out_of_process_context(__file__, "get_repo_v1", instance) as context_v1:
-            assert _materialize_assets(context_v1, repo_v1)
+            assert materialize_assets(context_v1)
             wait_for_runs_to_finish(context_v1.instance)
         with define_out_of_process_context(__file__, "get_repo_v2", instance) as context_v2:
             assert _fetch_data_versions(context_v2, repo_v2)
@@ -89,7 +86,7 @@ def test_stale_status():
             assert foo["staleStatus"] == "MISSING"
             assert foo["staleCauses"] == []
 
-            assert _materialize_assets(context, repo)
+            assert materialize_assets(context)
             wait_for_runs_to_finish(context.instance)
 
             result = _fetch_data_versions(context, repo)
@@ -98,7 +95,7 @@ def test_stale_status():
             assert foo["staleStatus"] == "FRESH"
             assert foo["staleCauses"] == []
 
-            assert _materialize_assets(context, repo, asset_selection=[AssetKey(["foo"])])
+            assert materialize_assets(context, asset_selection=[AssetKey(["foo"])])
             wait_for_runs_to_finish(context.instance)
 
             result = _fetch_data_versions(context, repo)
@@ -139,8 +136,6 @@ def get_repo_partitioned():
 
 
 def test_stale_status_partitioned():
-    repo = get_repo_partitioned()
-
     with instance_for_test(synchronous_run_coordinator=True) as instance:
         with define_out_of_process_context(__file__, "get_repo_partitioned", instance) as context:
             for key in ["foo", "bar"]:
@@ -155,8 +150,8 @@ def test_stale_status_partitioned():
                 assert node["staleCauses"] == []  # always returns empty for this undefined field
                 assert node["staleCausesByPartition"] == [[], []]
 
-            assert _materialize_assets(
-                context, repo, [AssetKey(["foo"]), AssetKey(["bar"])], ["alpha", "beta"]
+            assert materialize_assets(
+                context, [AssetKey(["foo"]), AssetKey(["bar"])], ["alpha", "beta"]
             )
             wait_for_runs_to_finish(context.instance)
 
@@ -170,9 +165,8 @@ def test_stale_status_partitioned():
                 assert node["staleCauses"] == []
                 assert node["staleCausesByPartition"] == [[], []]
 
-            assert _materialize_assets(
+            assert materialize_assets(
                 context,
-                repo,
                 [AssetKey(["foo"])],
                 ["alpha", "beta"],
                 run_config_data={"ops": {"foo": {"config": {"prefix": "from_config"}}}},
@@ -226,7 +220,7 @@ def test_data_version_from_tags():
     repo_v1 = get_repo_v1()
     with instance_for_test(synchronous_run_coordinator=True) as instance:
         with define_out_of_process_context(__file__, "get_repo_v1", instance) as context_v1:
-            assert _materialize_assets(context_v1, repo_v1)
+            assert materialize_assets(context_v1)
             wait_for_runs_to_finish(context_v1.instance)
             result = _fetch_data_versions(context_v1, repo_v1)
             tags = result.data["assetNodes"][0]["assetMaterializations"][0]["tags"]
@@ -344,56 +338,6 @@ def test_source_asset_job_name():
 
             # Make sure observable source asset appears in explicit observation job
             assert "bar_job" in bar_jobs
-
-
-def _materialize_assets(
-    context: WorkspaceRequestContext,
-    repo: RepositoryDefinition,
-    asset_selection: Optional[Sequence[AssetKey]] = None,
-    partition_keys: Optional[Sequence[str]] = None,
-    run_config_data: Optional[Mapping[str, Any]] = None,
-) -> Union[GqlResult, Sequence[GqlResult]]:
-    gql_asset_selection = (
-        cast(Sequence[GqlAssetKey], [key.to_graphql_input() for key in asset_selection])
-        if asset_selection
-        else None
-    )
-    selector = infer_job_selector(
-        context, repo.get_implicit_asset_job_names()[0], asset_selection=gql_asset_selection
-    )
-    if partition_keys:
-        results = []
-        for key in partition_keys:
-            results.append(
-                execute_dagster_graphql(
-                    context,
-                    LAUNCH_PIPELINE_EXECUTION_MUTATION,
-                    variables={
-                        "executionParams": {
-                            "selector": selector,
-                            "executionMetadata": {
-                                "tags": [{"key": "dagster/partition", "value": key}]
-                            },
-                            "runConfigData": run_config_data,
-                        }
-                    },
-                )
-            )
-        return results
-    else:
-        selector = infer_job_selector(
-            context, repo.get_implicit_asset_job_names()[0], asset_selection=gql_asset_selection
-        )
-        return execute_dagster_graphql(
-            context,
-            LAUNCH_PIPELINE_EXECUTION_MUTATION,
-            variables={
-                "executionParams": {
-                    "selector": selector,
-                    "runConfigData": run_config_data,
-                }
-            },
-        )
 
 
 def _fetch_data_versions(context: WorkspaceRequestContext, repo: RepositoryDefinition):

python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_reconstruction_metadata.py
@@ -1,0 +1,84 @@
+import inspect
+import os
+import textwrap
+from collections.abc import Iterator
+from contextlib import contextmanager
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Any, Callable
+
+from dagster._core.instance_for_test import instance_for_test
+from dagster_graphql.test.utils import define_out_of_process_context, materialize_assets
+
+_STATE_BACKED_DEFINITIONS_TEST_LOG_FILE_VAR = "DAGSTER_STATE_BACKED_DEFINITIONS_TEST_LOG_FILE"
+
+
+def test_state_backed_defs_loader(monkeypatch) -> None:
+    with NamedTemporaryFile(delete=False) as f:
+        log_file_path = f.name
+    try:
+        monkeypatch.setenv(_STATE_BACKED_DEFINITIONS_TEST_LOG_FILE_VAR, log_file_path)
+        with (
+            instance_for_test(synchronous_run_coordinator=True) as instance,
+            _temp_script(_state_backed_defs) as repo_path,
+        ):
+            with define_out_of_process_context(repo_path, None, instance) as context:
+                assert _get_num_calls(log_file_path) == 1
+                materialize_assets(context)
+                assert _get_num_calls(log_file_path) == 1
+    finally:
+        os.unlink(log_file_path)
+
+
+def _get_num_calls(log_file_path: str) -> int:
+    lines = Path(log_file_path).read_text().strip().split("\n")
+    return len(lines)
+
+
+@contextmanager
+def _temp_script(script_fn: Callable[[], Any]) -> Iterator[str]:
+    # drop the signature line
+    source = textwrap.dedent(inspect.getsource(script_fn).split("\n", 1)[1])
+    with NamedTemporaryFile(suffix=".py") as file:
+        file.write(source.encode())
+        file.flush()
+        yield file.name
+
+
+# This is written to a temporary file for the test. It is important that this is written to a
+# standalone file and not wrapped in @lazy_definitions, because we are specifically testing the
+# effects of executing this at import time.
+def _state_backed_defs() -> None:
+    import os
+
+    from dagster._core.definitions.decorators.asset_decorator import asset
+    from dagster._core.definitions.definitions_class import Definitions
+    from dagster._core.definitions.definitions_load_context import StateBackedDefinitionsLoader
+    from dagster._record import record
+    from dagster._serdes.serdes import whitelist_for_serdes
+
+    @whitelist_for_serdes
+    @record
+    class ExampleDefState:
+        a_string: str
+
+    class ExampleStateBackedDefinitionsLoader(StateBackedDefinitionsLoader[ExampleDefState]):
+        @property
+        def defs_key(self) -> str:
+            return "test_key"
+
+        def fetch_state(self) -> ExampleDefState:
+            log_file = os.environ["DAGSTER_STATE_BACKED_DEFINITIONS_TEST_LOG_FILE"]
+            with open(log_file, "a") as f:
+                f.write("fetch_state\n")
+            return ExampleDefState(a_string="foo")
+
+        def defs_from_state(self, state: ExampleDefState) -> Definitions:
+            @asset(key=state.a_string)
+            def foo_asset(_):
+                return 1
+
+            return Definitions(assets=[foo_asset])
+
+    assets = ExampleStateBackedDefinitionsLoader().build_defs().assets
+    defs = Definitions(assets=assets)  # noqa: F841

python_modules/dagster/dagster_tests/execution_tests/engine_tests/test_step_delegating_executor.py
@@ -419,7 +419,7 @@ def test_execute_using_repository_data():
                 call_counts = instance.run_storage.get_cursor_values(
                     {"compute_cacheable_data_called", "get_definitions_called"}
                 )
-                assert call_counts.get("compute_cacheable_data_called") == "2"
+                assert call_counts.get("compute_cacheable_data_called") == "1"
 
                 assert call_counts.get("get_definitions_called") == "9"
 

python_modules/libraries/dagster-airlift/kitchen-sink/kitchen_sink_tests/integration_tests/test_e2e_mapped.py
@@ -285,9 +285,9 @@ def test_respect_airflow_retries(
     dagster_home: str,
 ) -> None:
     """Airflow doesn't actually have dag-level retries; only task-level retries. This test just ensures that we handle task-level retries gracefully."""
-    from kitchen_sink.dagster_defs.mapped_defs import materialize_dags
+    from kitchen_sink.dagster_defs.mapped_defs import UNMAPPED_SPECS_INSTANCE_NAME, materialize_dags
 
-    af_instance = local_airflow_instance()
+    af_instance = local_airflow_instance(UNMAPPED_SPECS_INSTANCE_NAME)
 
     dagster_instance = DagsterInstance.get()
     result = materialize(