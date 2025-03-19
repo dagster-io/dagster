e513e2b652d28a238238c39327ab1843182c9615
Maxime Armstrong <maxime@retain.ai>
2025-03-19 10:51:52 -0400

    Update code

--- python_modules/libraries/dagster-dbt/kitchen-sink/dagster_dbt_cloud_kitchen_sink/resources.py
+++ python_modules/libraries/dagster-dbt/kitchen-sink/dagster_dbt_cloud_kitchen_sink/resources.py
@@ -2,12 +2,14 @@ import os
 
 from dagster_dbt.cloud_v2.resources import DbtCloudCredentials, DbtCloudWorkspace
 
-dbt_cloud_workspace = DbtCloudWorkspace(
-    credentials=DbtCloudCredentials(
-        account_id=os.getenv("KS_DBT_CLOUD_ACCOUNT_ID"),
-        access_url=os.getenv("KS_DBT_CLOUD_ACCESS_URL"),
-        token=os.getenv("KS_DBT_CLOUD_TOKEN"),
-    ),
-    project_id=os.getenv("KS_DBT_CLOUD_PROJECT_ID"),
-    environment_id=os.getenv("KS_DBT_CLOUD_ENVIRONMENT_ID"),
-)
+
+def get_dbt_cloud_workspace() -> DbtCloudWorkspace:
+    return DbtCloudWorkspace(
+        credentials=DbtCloudCredentials(
+            account_id=os.getenv("KS_DBT_CLOUD_ACCOUNT_ID"),
+            access_url=os.getenv("KS_DBT_CLOUD_ACCESS_URL"),
+            token=os.getenv("KS_DBT_CLOUD_TOKEN"),
+        ),
+        project_id=os.getenv("KS_DBT_CLOUD_PROJECT_ID"),
+        environment_id=os.getenv("KS_DBT_CLOUD_ENVIRONMENT_ID"),
+    )
