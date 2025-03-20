4690fab9e4d5fc2c736bddb51c9c6d4ea4aa43e5
Maxime Armstrong <maxime@retain.ai>
2025-03-20 16:46:24 -0400

    Update comment

--- python_modules/libraries/dagster-dbt/kitchen-sink/dagster_dbt_cloud_kitchen_sink_tests/test_dagster_assets.py
+++ python_modules/libraries/dagster-dbt/kitchen-sink/dagster_dbt_cloud_kitchen_sink_tests/test_dagster_assets.py
@@ -2,7 +2,7 @@ from dagster_dbt_cloud_kitchen_sink.defs import dbt_cloud_specs
 
 
 def test_dbt_cloud_specs(ensure_cleanup: None) -> None:
-    """Test that dbt models can be successfully materialized, and that a run and job exist in the dbt cloud project."""
+    """Test that dbt Cloud asset specs are correctly loaded with the expected number of assets."""
     all_assets_keys = [asset.key for asset in dbt_cloud_specs]
 
     # 5 dbt models
