e129768b1d13861ec4e9ec82cd5b4fbe3c4df782
Maxime Armstrong <maxime@retain.ai>
2025-03-20 17:52:17 -0400

    [dagster-dbt] Add project and environment endpoints to dbt Cloud kitchen sink

--- python_modules/libraries/dagster-dbt/kitchen-sink/dagster_dbt_cloud_kitchen_sink_tests/conftest.py
+++ python_modules/libraries/dagster-dbt/kitchen-sink/dagster_dbt_cloud_kitchen_sink_tests/conftest.py
@@ -2,6 +2,7 @@ from collections.abc import Generator
 
 import pytest
 from dagster_dbt.cloud_v2.resources import DbtCloudWorkspace, get_dagster_adhoc_job_name
+from dagster_dbt.cloud_v2.types import DbtCloudEnvironment, DbtCloudProject
 from dagster_dbt_cloud_kitchen_sink.resources import (
     get_dbt_cloud_workspace,
     get_environment_id,
@@ -36,15 +37,21 @@ def ensure_cleanup(
     finally:
         client = workspace.get_client()
         jobs = client.list_jobs(project_id=project_id, environment_id=environment_id)
+        project = DbtCloudProject.from_project_details(
+            project_details=client.get_project_details(project_id=project_id)
+        )
+        environment = DbtCloudEnvironment.from_environment_details(
+            environment_details=client.get_environment_details(environment_id=environment_id)
+        )
         adhoc_job_ids = {
             job["id"]
             for job in jobs
             if job["name"]
             == get_dagster_adhoc_job_name(
-                project_id=project_id,
-                project_name=None,
-                environment_id=environment_id,
-                environment_name=None,
+                project_id=project.id,
+                project_name=project.name,
+                environment_id=environment.id,
+                environment_name=environment.name,
             )
         }
         for job_id in adhoc_job_ids:
