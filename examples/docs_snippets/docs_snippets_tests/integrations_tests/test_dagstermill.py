08cac74bb26922ee7226bf5f0de476a9710ee8d9
Alex Langenfeld <alex@elementl.com>
2025-03-24 16:10:15 -0500

    [dagstermill] fix test imports (#28701)
    I switched over to uv while fixing the setuptools issue which caused
    these test import to surface.
    
    This just moves things to `dagstermill.test_utils`
    
    ## How I Tested These Changes
    
    bk

--- examples/docs_snippets/docs_snippets_tests/integrations_tests/test_dagstermill.py
+++ examples/docs_snippets/docs_snippets_tests/integrations_tests/test_dagstermill.py
@@ -1,7 +1,7 @@
 import os
 from contextlib import contextmanager
 
-from dagstermill_tests.test_ops import cleanup_result_notebook
+from dagstermill.test_utils import cleanup_result_notebook
 
 from dagster import execute_job
 from dagster._core.definitions.reconstruct import ReconstructableJob
