52043005f0f575976326bb73ae725bd028776669
benpankow <ben@dagsterlabs.com>
2025-03-25 15:01:06 -0700

    clarify name

--- python_modules/libraries/dagster-dg/dagster_dg/cli/env.py
+++ python_modules/libraries/dagster-dg/dagster_dg/cli/env.py
@@ -7,7 +7,7 @@ from rich.table import Table
 from dagster_dg.cli.shared_options import dg_global_options
 from dagster_dg.config import normalize_cli_config
 from dagster_dg.context import DgContext
-from dagster_dg.env import Env
+from dagster_dg.env import ProjectEnvVars
 from dagster_dg.utils import DgClickCommand, DgClickGroup
 
 
@@ -24,11 +24,11 @@ def env_group():
 @env_group.command(name="list", cls=DgClickCommand)
 @dg_global_options
 def list_env_command(**global_options: object) -> None:
-    """List environment variables defined in the current project."""
+    """List environment variables from the .env file of the current project."""
     cli_config = normalize_cli_config(global_options, click.get_current_context())
     dg_context = DgContext.for_project_environment(Path.cwd(), cli_config)
 
-    env = Env.from_ctx(dg_context)
+    env = ProjectEnvVars.from_ctx(dg_context)
     if not env.values:
         click.echo("No environment variables are defined for this project.")
         return
--- python_modules/libraries/dagster-dg/dagster_dg/env.py
+++ python_modules/libraries/dagster-dg/dagster_dg/env.py
@@ -1,3 +1,4 @@
+from collections.abc import Mapping
 from typing import Optional
 
 from dagster_shared import check
@@ -7,28 +8,36 @@ from typing_extensions import Self
 from dagster_dg.context import DgContext
 
 
-class Env:
-    def __init__(self, ctx: DgContext, values: dict[str, Optional[str]]):
+class ProjectEnvVars:
+    """Represents the environment for a project, stored in the .env file of a
+    project root.
+    """
+
+    def __init__(self, ctx: DgContext, values: Mapping[str, Optional[str]]):
         self.ctx = ctx
-        self.values = values
+        self._values = values
 
     @classmethod
     def from_ctx(cls, ctx: DgContext) -> "Self":
-        check.invariant(ctx.is_project, "Env can only be created from a project context")
+        check.invariant(ctx.is_project, "ProjectEnvVars can only be created from a project context")
         env_path = ctx.root_path / ".env"
         if not env_path.exists():
             return cls(ctx, values={})
         env = dotenv_values(env_path)
         return cls(ctx, values=env)
 
+    @property
+    def values(self) -> Mapping[str, Optional[str]]:
+        return self._values
+
     def get(self, key: str) -> Optional[str]:
         return self.values.get(key)
 
-    def with_values(self, values: dict[str, Optional[str]]) -> "Env":
-        return Env(self.ctx, {**self.values, **values})
+    def with_values(self, values: Mapping[str, Optional[str]]) -> "ProjectEnvVars":
+        return ProjectEnvVars(self.ctx, {**self.values, **values})
 
-    def without_values(self, keys: set[str]) -> "Env":
-        return Env(self.ctx, {k: v for k, v in self.values.items() if k not in keys})
+    def without_values(self, keys: set[str]) -> "ProjectEnvVars":
+        return ProjectEnvVars(self.ctx, {k: v for k, v in self.values.items() if k not in keys})
 
     def write(self) -> None:
         env_path = self.ctx.root_path / ".env"
