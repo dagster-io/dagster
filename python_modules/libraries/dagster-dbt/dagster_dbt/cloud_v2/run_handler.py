f5da792bc331a850f91e806c81b07674e9c491be
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-03-27 09:19:25 -0400

    [dagster-dbt] Implement DbtCloudJobRunResults.to_default_asset_events for asset materializations (#28731)
    ## Summary & Motivation
    
    This PR implements the class and method required to parse
    `run_results.json` and generate Dagster events. Asset materializations
    are supported in this PR, asset check evaluations are supported in the
    next PR.
    
    ## How I Tested These Changes
    
    New tests with BK.

--- python_modules/libraries/dagster-dbt/dagster_dbt/cloud_v2/run_handler.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/cloud_v2/run_handler.py
@@ -1,11 +1,24 @@
-from collections.abc import Mapping, Sequence
+from collections.abc import Iterator, Mapping, Sequence
 from typing import Any, Optional
 
+from dagster import AssetMaterialization
 from dagster._annotations import preview
 from dagster._record import record
+from dbt.contracts.results import NodeStatus
+from dbt.node_types import NodeType
+from dbt.version import __version__ as dbt_version
+from packaging import version
 
+from dagster_dbt.asset_utils import build_dbt_specs
 from dagster_dbt.cloud_v2.client import DbtCloudWorkspaceClient
-from dagster_dbt.cloud_v2.types import DbtCloudJobRunStatusType, DbtCloudRun
+from dagster_dbt.cloud_v2.types import DbtCloudJobRunStatusType, DbtCloudRun, DbtCloudWorkspaceData
+from dagster_dbt.dagster_dbt_translator import DagsterDbtTranslator
+
+IS_DBT_CORE_VERSION_LESS_THAN_1_8_0 = version.parse(dbt_version) < version.parse("1.8.0")
+if IS_DBT_CORE_VERSION_LESS_THAN_1_8_0:
+    REFABLE_NODE_TYPES = NodeType.refable()  # type: ignore
+else:
+    from dbt.node_types import REFABLE_NODE_TYPES as REFABLE_NODE_TYPES
 
 
 @preview
@@ -41,3 +54,77 @@ class DbtCloudJobRunHandler:
 
     def get_manifest(self) -> Mapping[str, Any]:
         return self.client.get_run_manifest_json(run_id=self.run_id)
+
+
+@preview
+@record
+class DbtCloudJobRunResults:
+    """Represents the run results of a dbt Cloud job run."""
+
+    run_id: int
+    run_results: Mapping[str, Any]
+
+    @classmethod
+    def from_run_results_json(cls, run_results_json: Mapping[str, Any]) -> "DbtCloudJobRunResults":
+        return cls(
+            run_id=int(run_results_json["metadata"]["env"]["DBT_CLOUD_RUN_ID"]),
+            run_results=run_results_json,
+        )
+
+    def to_default_asset_events(
+        self,
+        workspace_data: DbtCloudWorkspaceData,
+        dagster_dbt_translator: Optional[DagsterDbtTranslator] = None,
+    ) -> Iterator[AssetMaterialization]:
+        """Convert the run results of a dbt Cloud job run to a set of corresponding Dagster events.
+
+        Args:
+            workspace_data (DbtCloudWorkspaceData): The data of the dbt Cloud workspace.
+            dagster_dbt_translator (DagsterDbtTranslator): Optionally, a custom translator for
+                linking dbt nodes to Dagster assets.
+
+        Returns:
+            Iterator[AssetMaterialization]:
+                A set of corresponding Dagster events.
+
+                The following are yielded:
+                - AssetMaterialization for refables (e.g. models, seeds, snapshots.)
+        """
+        dagster_dbt_translator = dagster_dbt_translator or DagsterDbtTranslator()
+        manifest = workspace_data.manifest
+
+        invocation_id: str = self.run_results["metadata"]["invocation_id"]
+        for result in self.run_results["results"]:
+            unique_id: str = result["unique_id"]
+            dbt_resource_props: Mapping[str, Any] = manifest["nodes"][unique_id]
+            selector: str = ".".join(dbt_resource_props["fqn"])
+
+            default_metadata = {
+                "unique_id": unique_id,
+                "invocation_id": invocation_id,
+                "Execution Duration": result["execution_time"],
+            }
+
+            resource_type: str = dbt_resource_props["resource_type"]
+            result_status: str = result["status"]
+            materialization: str = dbt_resource_props["config"]["materialized"]
+
+            is_ephemeral = materialization == "ephemeral"
+            is_successful = result_status == NodeStatus.Success
+
+            # Build the specs for the given unique ID
+            asset_specs, check_specs = build_dbt_specs(
+                manifest=manifest,
+                translator=dagster_dbt_translator,
+                select=selector,
+                exclude="",
+                io_manager_key=None,
+                project=None,
+            )
+
+            if resource_type in REFABLE_NODE_TYPES and is_successful and not is_ephemeral:
+                spec = asset_specs[0]
+                yield AssetMaterialization(
+                    asset_key=spec.key,
+                    metadata=default_metadata,
+                )
