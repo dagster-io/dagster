92803c147d576b360caeec53d90230417bcb4805
Colton Padden <colton@dagsterlabs.com>
2025-03-28 10:53:38 -0400

    [docs] add prettier formatting

--- docs/.prettierrc.js
+++ docs/.prettierrc.js
@@ -1,6 +1,18 @@
 module.exports = {
   bracketSpacing: false,
-  printWidth: 100,
+  bracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  overrides: [
+    {
+      files: ['docs/**/*.md', 'docs/**/*.mdx'],
+      options: {
+        parser: 'mdx',
+        proseWrap: 'preserve',
+        htmlWhitespaceSensitivity: 'ignore',
+        printWidth: 1000, // needed to prevent wrapping of CodeExamples
+        singleAttributePerLine: false, // needed to prevent wrapping of CodeExamples
+      },
+    },
+  ],
 };
