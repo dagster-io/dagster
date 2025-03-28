d8c68c8fdd7c497309ead2d30cd27860af07bd7d
Anurag Anjaria <anurag@dagsterlabs.com>
2025-03-27 23:35:06 -0400

    cleanup

--- python_modules/dagster/dagster/_core/definitions/new_freshness_thing.py
+++ python_modules/dagster/dagster/_core/definitions/new_freshness_thing.py
@@ -7,13 +7,48 @@ from dagster._serdes import whitelist_for_serdes
 
 
 class NewFreshnessThing(ABC):
-    """A thing that describes how to check the freshness of an asset."""
+    """A condition that delineates when an asset is considered fresh. The asset can either pass, violate, or warn on this condition.
+
+    `NewFreshnessThing` can be specified as a parameter to `@asset`, `@multi_asset`, or in `AssetSpec`.
+
+    Do not use this class directly. Instead, use one of the subclasses.
+
+    A proper name for this concept is under discussion here:
+        https://dagsterlabs.slack.com/archives/C047L6H0LF4/p1743087033186109.
+
+    """
 
 
 @whitelist_for_serdes
 @record_custom
 class TimeWindowFreshnessThing(NewFreshnessThing, IHaveNew):
-    """Consider an asset fresh if the last successful materialization was within a time window of max_lag_minutes."""
+    """A freshness condition that considers an asset fresh if it was materialized within a specified time window (time_window_minutes).
+
+    The asset is considered stale if its last materialization is older than time_window_minutes, and in a warning state
+    if its last materialization is older than the warning time window but newer than the critical time window.
+
+    Example usage:
+
+    .. code-block:: python
+
+        from dagster import TimeWindowFreshnessThing
+
+        # Asset must be materialized within last 24 hours (1440 minutes)
+        @asset(new_freshness_thing=TimeWindowFreshnessThing(time_window_minutes=1440))
+        def my_asset():
+            ...
+
+        # Asset must be materialized within last 24 hours (1440 minutes), with warning after 12 hours
+        @asset(new_freshness_thing=TimeWindowFreshnessThing(time_window_minutes=1440, warning_time_window_minutes=720))
+        def my_asset():
+            ...
+
+    Args:
+        time_window_minutes (int): The maximum age in minutes for an asset to be considered fresh.
+            Must be positive.
+        warning_time_window_minutes (Optional[int]): If provided, the age in minutes at which to start
+            showing warnings about asset freshness. Must be positive and less than time_window_minutes.
+    """
 
     time_window_minutes: int
     warning_time_window_minutes: Optional[int] = None
@@ -42,11 +77,3 @@ class TimeWindowFreshnessThing(NewFreshnessThing, IHaveNew):
             time_window_minutes=time_window_minutes,
             warning_time_window_minutes=warning_time_window_minutes,
         )
-
-
-# TODO this will become a type union once we add more freshness things
-# TODO would prefer this to be an abstract base class, but can't figure out how to make it work with serdes and record
-
-# from typing import Union
-
-# NewFreshnessThing = Union[TimeWindowFreshnessThing]
