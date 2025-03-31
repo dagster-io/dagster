ef86d48a5822aa6b25bd2040afa1162a19fe5b32
nikki everett <nikki@dagsterlabs.com>
2025-03-31 12:47:11 -0500

    fix pyright error
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

--- examples/oss-metadata-to-plus/metadata_to_plus/migrate_metadata_job.py
+++ examples/oss-metadata-to-plus/metadata_to_plus/migrate_metadata_job.py
@@ -79,7 +79,7 @@ def migrate_metadata_asset(context: AssetExecutionContext, config: MetadataMigra
             partition=partition_to_report,  # type: ignore
             metadata=metadata_to_report,  # type: ignore
         )
-        print(f"Migrated asset {asset_key} for partition {partition_to_report}")  # noqa: T201
+        print(f"Migrated asset {asset_key} for partition {partition_to_report}")  # type: ignore # noqa: T201
 
 
 migrate_metadata_job = define_asset_job(
