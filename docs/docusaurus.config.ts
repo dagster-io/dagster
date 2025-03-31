e78217baa7a7fcdbc194d4bddb79ecab512634e0
Nikki Everett <neverett@users.noreply.github.com>
2025-03-31 11:04:18 -0500

    DOC-976 Bump docs version to 1.10.8 (#28883)
    ## Summary & Motivation
    
    Updates docs version dropdown for 1.10.8 release
    
    ## How I Tested These Changes
    
    ## Changelog
    
    > Insert changelog entry or delete this section.
    
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -185,7 +185,7 @@ const config: Config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '1.10.7',
+              label: '1.10.8',
               path: '/',
             },
           },
