eca5c1884863892d917862344f06282f4afc62de
Isaac Hellendag <dish@elementl.com>
2025-04-02 15:52:39 -0500

    [ui] Marketplace: Build step to pull content from docs

--- js_modules/dagster-ui/packages/ui-core/src/integrations/__stories__/IntegrationPage.stories.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/integrations/__stories__/IntegrationPage.stories.tsx
@@ -2,8 +2,7 @@ import {Box, Colors} from '@dagster-io/ui-components';
 import {Meta} from '@storybook/react';
 
 import {IntegrationPage} from '../IntegrationPage';
-import {IntegrationTag} from '../IntegrationTag';
-import {IntegrationDetails} from '../types';
+import {airbyte} from '../__generated__';
 
 // eslint-disable-next-line import/no-default-export
 export default {
@@ -11,58 +10,7 @@ export default {
   component: IntegrationPage,
 } as Meta;
 
-const MARKDOWN = `
-Using this integration, you can trigger Airbyte syncs and orchestrate your Airbyte connections from within Dagster, making it easy to chain an Airbyte sync with upstream or downstream steps in your workflow.
-
-### Installation
-
-\`\`\`bash
-pip install dagster-airbyte
-\`\`\`
-
-### Example
-
-\`\`\`python
-from dagster import EnvVar
-from dagster_airbyte import AirbyteResource, load_assets_from_airbyte_instance
-import os
-
-# Connect to your OSS Airbyte instance
-airbyte_instance = AirbyteResource(
-    host="localhost",
-    port="8000",
-    # If using basic auth, include username and password:
-    username="airbyte",
-    password=EnvVar("AIRBYTE_PASSWORD")
-)
-
-# Load all assets from your Airbyte instance
-airbyte_assets = load_assets_from_airbyte_instance(airbyte_instance)
-\`\`\`
-
-### About Airbyte
-
-**Airbyte** is an open source data integration engine that helps you consolidate your SaaS application and database data into your data warehouses, lakes and databases.
-`;
-
 export const Default = () => {
-  const integration: IntegrationDetails = {
-    id: 'airbyte',
-    name: 'Airbyte',
-    logo: 'airbyte',
-    tags: [IntegrationTag.EltTools],
-    status: 'published',
-    title: 'Airbyte',
-    excerpt: 'Airbyte is a tool for syncing data between databases.',
-    date: '2025-03-14',
-    apireflink: 'https://docs.airbyte.com/api',
-    docslink: 'https://docs.airbyte.com',
-    partnerlink: 'https://airbyte.com',
-    categories: ['data-integration'],
-    enabledBy: ['dagster'],
-    markdown: MARKDOWN,
-  };
-
   return (
     <Box
       style={{
@@ -72,7 +20,7 @@ export const Default = () => {
         backgroundColor: Colors.backgroundLight(),
       }}
     >
-      <IntegrationPage integration={integration} />
+      <IntegrationPage integration={airbyte} />
     </Box>
   );
 };
--- js_modules/dagster-ui/packages/ui-core/src/integrations/__stories__/MarketplaceHome.stories.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/integrations/__stories__/MarketplaceHome.stories.tsx
@@ -1,9 +1,8 @@
 import {Box, Colors} from '@dagster-io/ui-components';
 import {Meta} from '@storybook/react';
 
-import {IntegrationTag} from '../IntegrationTag';
 import {MarketplaceHome} from '../MarketplaceHome';
-import {IntegrationConfig} from '../types';
+import * as allIntegrations from '../__generated__';
 
 // eslint-disable-next-line import/no-default-export
 export default {
@@ -12,111 +11,7 @@ export default {
 } as Meta;
 
 export const Default = () => {
-  const integrations: IntegrationConfig[] = [
-    {
-      id: 'airbyte',
-      name: 'Airbyte',
-      icon: 'airbyte',
-      tags: [IntegrationTag.EltTools],
-    },
-    {
-      id: 'aws',
-      name: 'AWS',
-      icon: 'aws',
-      tags: [IntegrationTag.Storage, IntegrationTag.Compute],
-    },
-    {
-      id: 'azure',
-      name: 'Azure',
-      icon: 'azure',
-      tags: [IntegrationTag.Storage, IntegrationTag.Compute],
-    },
-    {
-      id: 'bigquery',
-      name: 'BigQuery',
-      icon: 'bigquery',
-      tags: [IntegrationTag.Storage],
-    },
-    {
-      id: 'dbt',
-      name: 'dbt',
-      icon: 'dbt',
-      tags: [IntegrationTag.EltTools],
-    },
-    {
-      id: 'duckdb',
-      name: 'DuckDB',
-      icon: 'duckdb',
-      tags: [IntegrationTag.Storage],
-    },
-    {
-      id: 'fivetran',
-      name: 'Fivetran',
-      icon: 'fivetran',
-      tags: [IntegrationTag.EltTools],
-    },
-    {
-      id: 'gcp',
-      name: 'Google Cloud Platform',
-      icon: 'gcp',
-      tags: [IntegrationTag.Storage, IntegrationTag.Compute],
-    },
-    {
-      id: 'great-expectations',
-      name: 'Great Expectations',
-      icon: 'greatexpectations',
-      tags: [IntegrationTag.Monitoring],
-    },
-    {
-      id: 'jupyter',
-      name: 'Jupyter',
-      icon: 'jupyter',
-      tags: [IntegrationTag.BiTools],
-    },
-    {
-      id: 'mlflow',
-      name: 'MLflow',
-      icon: 'mlflow',
-      tags: [IntegrationTag.Metadata],
-    },
-    {
-      id: 'noteable',
-      name: 'Noteable',
-      icon: 'noteable',
-      tags: [IntegrationTag.BiTools],
-    },
-    {
-      id: 'pandas',
-      name: 'pandas',
-      icon: 'pandas',
-      tags: [IntegrationTag.EltTools],
-    },
-    {
-      id: 'pyspark',
-      name: 'PySpark',
-      icon: 'pyspark',
-      tags: [IntegrationTag.Compute],
-    },
-    {
-      id: 'redshift',
-      name: 'Redshift',
-      icon: 'redshift',
-      tags: [IntegrationTag.Storage],
-    },
-    {
-      id: 'sling',
-      name: 'Sling',
-      icon: 'sling',
-      tags: [IntegrationTag.EltTools],
-    },
-    {
-      id: 'snowflake',
-      name: 'Snowflake',
-      icon: 'snowflake',
-      tags: [IntegrationTag.Storage],
-    },
-  ];
-
+  const integrations = Object.values(allIntegrations);
   return (
     <Box padding={64} style={{width: '1300px', backgroundColor: Colors.backgroundLight()}}>
       <MarketplaceHome integrations={integrations} />
