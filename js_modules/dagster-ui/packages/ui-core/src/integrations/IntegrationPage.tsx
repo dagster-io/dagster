d7fc66b686ea1745b60b97626a39db9c8d798d28
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2025-04-02 09:08:47 -0500

    [dg] Integrations Marketplace in Storybook (#28926)
    ## Summary & Motivation
    
    Add Storybook components for first pass at Integrations Marketplace and individual Integration pages.
    
    <img width="1291" alt="Screenshot 2025-04-01 at 13 59 58" src="https://github.com/user-attachments/assets/1ae1edae-6c1e-4bdb-af09-85505ce7da27" />
    
    <img width="1294" alt="Screenshot 2025-04-01 at 13 59 49" src="https://github.com/user-attachments/assets/2b171944-8e62-48b4-a1ac-aceea56f8eac" />
    
    
    ## How I Tested These Changes
    
    yarn storybook, verify hover, text entry, copy behavior.

---
+++ js_modules/dagster-ui/packages/ui-core/src/integrations/IntegrationPage.tsx
@@ -0,0 +1,81 @@
+import {Body, Box, Colors, Heading, PageHeader} from '@dagster-io/ui-components';
+import {useLayoutEffect, useRef, useState} from 'react';
+import ReactMarkdown from 'react-markdown';
+import {CodeComponent} from 'react-markdown/lib/ast-to-react';
+import {Link} from 'react-router-dom';
+import rehypeHighlight from 'rehype-highlight';
+import remarkGfm from 'remark-gfm';
+
+import {IntegrationIcon} from './IntegrationIcon';
+import {CopyIconButton} from '../ui/CopyButton';
+import styles from './css/IntegrationPage.module.css';
+import {IntegrationDetails} from './types';
+
+interface Props {
+  integration: IntegrationDetails;
+}
+
+export const IntegrationPage = ({integration}: Props) => {
+  const {name, title, logo, excerpt, markdown} = integration;
+  return (
+    <div>
+      <PageHeader
+        title={
+          <Heading>
+            <Box flex={{direction: 'row', gap: 8}}>
+              <Link to="/integrations">Integrations Marketplace</Link>
+              <span> / </span>
+              <div>{title}</div>
+            </Box>
+          </Heading>
+        }
+      />
+      <Box
+        padding={{vertical: 24}}
+        flex={{direction: 'column', gap: 24}}
+        style={{width: '1100px', margin: '0 auto'}}
+      >
+        <Box flex={{direction: 'row', gap: 12, alignItems: 'flex-start'}}>
+          <IntegrationIcon name={name} knownTag={logo} padding={12} />
+          <Box flex={{direction: 'column', gap: 2}} margin={{top: 4}}>
+            <div style={{fontSize: 18, fontWeight: 600}}>{title}</div>
+            <Body color={Colors.textLight()}>{excerpt}</Body>
+          </Box>
+        </Box>
+        <div className={styles.markdownOutput}>
+          <ReactMarkdown
+            className={styles.integrationPage}
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[[rehypeHighlight, {ignoreMissing: true}]]}
+            components={{
+              code: Code,
+            }}
+          >
+            {markdown}
+          </ReactMarkdown>
+        </div>
+      </Box>
+    </div>
+  );
+};
+
+const Code: CodeComponent = (props) => {
+  const {children, className, ...rest} = props;
+  const codeRef = useRef<HTMLElement>(null);
+  const [value, setValue] = useState('');
+
+  useLayoutEffect(() => {
+    setValue(codeRef.current?.textContent?.trim() ?? '');
+  }, [children]);
+
+  return (
+    <div className={styles.codeBlock}>
+      <code className={className} {...rest} ref={codeRef}>
+        {children}
+      </code>
+      <div className={styles.copyButton}>
+        <CopyIconButton value={value} iconSize={16} iconColor={Colors.accentPrimary()} />
+      </div>
+    </div>
+  );
+};
