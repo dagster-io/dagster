1fb79a82cbcf861b4b671b2353eed11fbfd81efa
Jordan Sanders <jordan@dagsterlabs.com>
2025-04-03 14:42:28 -0500

    Also pass through BUILDKITE_PIPELINE_SLUG
    I think these are all env vars that aren't available at the time our
    buildkite_envvars list comprehension is happening during pipeline
    generation.

--- .buildkite/dagster-buildkite/dagster_buildkite/step_builder.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/step_builder.py
@@ -88,6 +88,7 @@ class CommandStepBuilder:
         buildkite_envvars.append("BUILDKITE_TEST_QUARANTINE_TOKEN")
         buildkite_envvars.append("BUILDKITE_BRANCH")
         buildkite_envvars.append("BUILDKITE_COMMIT")
+        buildkite_envvars.append("BUILDKITE_PIPELINE_SLUG")
 
         # Set PYTEST_DEBUG_TEMPROOT to our mounted /tmp volume. Any time the
         # pytest `tmp_path` or `tmpdir` fixtures are used used, the temporary
