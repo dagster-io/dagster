d7889443809013c7faf5dc41ca08c4d889062533
Ben Pankow <ben@elementl.com>
2025-04-03 09:29:37 -0700

    [dg docs] Add tooling to move dg docs webapp into dg module, tests (#28959)
    ## Summary
    
    #28760 moved the `dg docs` webapp out of the `dagster_dg` folder, which
    means it is no longer published along with the Python package. This PR
    introduces tooling to copy it back with `make
    ready_dagster_dg_docs_for_publish`, and adds some branching logic to `dg
    docs` to search for a packaged webapp before falling back to the dev
    location.
    
    Also adds a `dg docs build` command, mainly because it's easier to test
    than `dg docs serve` but tests the same underlying yarn interactions.
    
    ## Test Plan
    
    New tests for `dg docs build` (dev env) and `dg docs build` (published
    wheel env)

--- python_modules/libraries/dagster-dg/dagster_dg/cli/docs.py
+++ python_modules/libraries/dagster-dg/dagster_dg/cli/docs.py
@@ -1,5 +1,6 @@
 import json
 import re
+import shutil
 import subprocess
 import time
 import webbrowser
@@ -18,14 +19,17 @@ from dagster_dg.utils import DgClickCommand, DgClickGroup, exit_with_error, push
 from dagster_dg.utils.telemetry import cli_telemetry_wrapper
 
 # from pathlib import Path
-DOCS_DIR = (
+DEV_DOCS_DIR = (
     Path(__file__).parent.parent.parent.parent.parent.parent
     / "js_modules"
     / "dagster-ui"
     / "packages"
     / "dg-docs-site"
 )
-DOCS_JSON_PATH = DOCS_DIR / "contents" / "generated.json"
+DOCS_DIR = Path(__file__).parent.parent / "docs" / "packages" / "dg-docs-site"
+ACTIVE_DOCS_DIR = DOCS_DIR if DOCS_DIR.exists() else DEV_DOCS_DIR
+
+DOCS_JSON_PATH = ACTIVE_DOCS_DIR / "contents" / "generated.json"
 
 SHOULD_DOCS_EXIT = False
 
@@ -48,7 +52,7 @@ def serve_docs_command(
     port: int,
     **global_options: object,
 ) -> None:
-    """Get detailed information on a registered Dagster component type."""
+    """Serve the Dagster components docs, to be viewed in a browser."""
     cli_config = normalize_cli_config(global_options, click.get_current_context())
     dg_context = DgContext.for_defined_registry_environment(Path.cwd(), cli_config)
     registry = RemoteLibraryObjectRegistry.from_dg_context(dg_context)
@@ -59,7 +63,7 @@ def serve_docs_command(
         if not component_key or not registry.has(component_key):
             exit_with_error(f"Component type `{component_type}` not found.")
 
-    with pushd(DOCS_DIR):
+    with pushd(ACTIVE_DOCS_DIR):
         DOCS_JSON_PATH.parent.mkdir(parents=True, exist_ok=True)
         DOCS_JSON_PATH.write_text(json.dumps(json_for_all_components(registry), indent=2))
         with yaspin(text="Verifying docs dependencies", color="blue") as spinner:
@@ -101,3 +105,35 @@ def serve_docs_command(
 
         finally:
             yarn_dev.terminate()  # pyright: ignore[reportPossiblyUnboundVariable]
+
+
+@docs_group.command(name="build", cls=DgClickCommand)
+@click.argument("output_dir", type=click.Path(exists=False))
+@dg_global_options
+@cli_telemetry_wrapper
+def build_docs_command(
+    output_dir: str,
+    **global_options: object,
+) -> None:
+    """Build a static version of the Dagster components docs, to be served by a static file server."""
+    cli_config = normalize_cli_config(global_options, click.get_current_context())
+    dg_context = DgContext.for_defined_registry_environment(Path.cwd(), cli_config)
+    registry = RemoteLibraryObjectRegistry.from_dg_context(dg_context)
+
+    with pushd(ACTIVE_DOCS_DIR):
+        DOCS_JSON_PATH.parent.mkdir(parents=True, exist_ok=True)
+        DOCS_JSON_PATH.write_text(json.dumps(json_for_all_components(registry), indent=2))
+        with yaspin(text="Verifying docs dependencies", color="blue") as spinner:
+            yes = subprocess.Popen(["yes", "y"], stdout=subprocess.PIPE)
+            try:
+                subprocess.check_output(["yarn", "install"], stdin=yes.stdout)
+            finally:
+                yes.terminate()
+            spinner.ok("✓")
+
+        spinner = yaspin(text="Building docs", color="blue")
+        subprocess.check_output(["yarn", "build"])
+        spinner.ok("✓")
+
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
+        shutil.copytree(ACTIVE_DOCS_DIR / "out", output_dir, dirs_exist_ok=True)
