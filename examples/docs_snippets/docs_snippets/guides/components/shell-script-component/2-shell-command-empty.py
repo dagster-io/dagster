9537646d7b8919515c62828b96fc8a30081061da
colton <cmpadden@users.noreply.github.com>
2025-04-04 11:11:21 -0400

    [docs] updates docs to favor @dataclass for component authoring (#28900)
    ## Summary & Motivation
    
    Current docs promote `__init__` methods for defining YAML schema. This
    updates the tutorial to use data classes.
    
    ## How I Tested These Changes
    
    ```
    yarn start
    ```
    
    ## Changelog
    
    NOCHANGELOG

--- examples/docs_snippets/docs_snippets/guides/components/shell-script-component/2-shell-command-empty.py
+++ examples/docs_snippets/docs_snippets/guides/components/shell-script-component/2-shell-command-empty.py
@@ -11,6 +11,8 @@ class ShellCommand(Component, Resolvable):
     COMPONENT DESCRIPTION HERE.
     """
 
+    # added fields here will define yaml schema via Resolvable
+
     def build_defs(self, context: ComponentLoadContext) -> dg.Definitions:
         # Add definition construction logic here.
         return dg.Definitions()
