4517f4576ca3451e5350569b23bd73936b00c0b1
bengotow <bgotow@elementl.com>
2025-04-04 12:42:52 -0500

    [ui] Explicitly define the optional label present on feature flags

--- js_modules/dagster-ui/packages/ui-core/src/app/useVisibleFeatureFlagRows.oss.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/useVisibleFeatureFlagRows.oss.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import {FeatureFlag} from 'shared/app/FeatureFlags.oss';
+
+type FeatureFlagRow = {key: string; flagType: FeatureFlag; label?: React.ReactNode};
 
 /**
  * Open-source feature flags to be displayed in Dagster UI "User settings"
  */
-export const useVisibleFeatureFlagRows = () => [
+export const useVisibleFeatureFlagRows = (): FeatureFlagRow[] => [
   {
     key: 'Display resources in navigation sidebar',
     flagType: FeatureFlag.flagSidebarResources,
