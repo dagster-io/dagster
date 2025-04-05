04890c01e4d8d5bfa58792d5f5f4613535001aa4
Nick Schrock <schrockn@dagsterlabs.com>
2025-04-05 10:09:02 -0400

    cp

--- examples/components_yaml_checks_dsl/components_yaml_checks_dsl_tests/test_checks.py
+++ examples/components_yaml_checks_dsl/components_yaml_checks_dsl_tests/test_checks.py
@@ -1,26 +1,19 @@
 import dagster as dg
 import pandas as pd
 from dagster._core.definitions.asset_check_spec import AssetCheckSpec
-from dagster._core.definitions.asset_checks import AssetChecksDefinition
 from dagster._core.definitions.asset_key import AssetCheckKey, AssetKey
 from dagster._core.definitions.definitions_class import Definitions
 from dagster._core.definitions.materialize import materialize
-from dagster.components import ComponentLoadContext
+from dagster.components.core.context import component_defs_for_test
 
 from components_yaml_checks_dsl.definitions import defs
 from components_yaml_checks_dsl.lib import HooliAssetChecksComponent
 from components_yaml_checks_dsl.lib.hooli_asset_checks.component import StaticThresholdCheck
 
 
-def checks_specs_by_key(defs: Definitions) -> dict[str, AssetCheckSpec]:
+def checks_specs_by_key(defs: Definitions) -> dict[AssetCheckKey, AssetCheckSpec]:
     """Return a dictionary of check specs by key."""
-    check_specs = {}
-    asset_graph = defs.get_asset_graph()
-
-    for check_key in asset_graph.asset_check_keys:
-        check_spec = asset_graph.get_check_spec(check_key)
-        check_specs[check_key] = check_spec
-    return check_specs
+    return {spec.key: spec for spec in defs.get_all_asset_check_specs()}
 
 
 def raw_ck(table: str, check_name: str) -> AssetCheckKey:
@@ -43,20 +36,6 @@ def test_load_defs() -> None:
     assert raw_ck("users", "user_count_static_threshold") in check_specs
 
 
-def get_check_def_from_component(
-    component: HooliAssetChecksComponent, key: AssetCheckKey
-) -> AssetChecksDefinition:
-    context = ComponentLoadContext.for_test()
-    defs = component.build_defs(context)
-    return get_check_def(defs, key)
-
-
-def get_check_def(defs: Definitions, key: AssetCheckKey) -> AssetChecksDefinition:
-    assets_def = defs.get_asset_graph().assets_def_for_key(key)
-    assert isinstance(assets_def, AssetChecksDefinition)
-    return assets_def
-
-
 def test_execute_component() -> None:
     @dg.asset
     def upstream() -> pd.DataFrame:
@@ -74,9 +53,7 @@ def test_execute_component() -> None:
         ]
     )
 
-    defs = Definitions.merge(
-        component.build_defs(ComponentLoadContext.for_test()), Definitions([upstream])
-    )
+    defs = Definitions.merge(component_defs_for_test(component), Definitions([upstream]))
 
     assert AssetKey("upstream") in {spec.key for spec in defs.get_all_asset_specs()}
 
