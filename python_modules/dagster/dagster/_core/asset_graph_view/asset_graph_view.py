58dca858dbadedc334911ddc2ac3a3ca3725b334
prha <1040172+prha@users.noreply.github.com>
2025-04-05 10:47:26 -0700

    Revert "partition key pagination APIs (#28311)" (#29033)
    ## Summary & Motivation
    
    This reverts commit 5134d4b7c2cbd225b90b59044c4f208fe83144b0.
    
    I'm not sure why this would affect the automaterialize daemon's perf
    tests, but it seems to. Reverting to keep the build green (at least
    potentially) until I can dig into this more.
    
    ## How I Tested These Changes
    BK

--- python_modules/dagster/dagster/_core/asset_graph_view/asset_graph_view.py
+++ python_modules/dagster/dagster/_core/asset_graph_view/asset_graph_view.py
@@ -23,7 +23,7 @@ from dagster._core.definitions.multi_dimensional_partitions import (
     MultiPartitionsDefinition,
     PartitionDimensionDefinition,
 )
-from dagster._core.definitions.partition import AllPartitionsSubset, TemporalContext
+from dagster._core.definitions.partition import AllPartitionsSubset
 from dagster._core.definitions.partition_mapping import UpstreamPartitionsResult
 from dagster._core.definitions.time_window_partitions import (
     TimeWindow,
@@ -51,6 +51,31 @@ if TYPE_CHECKING:
 
 
 U_EntityKey = TypeVar("U_EntityKey", AssetKey, AssetCheckKey, EntityKey)
+
+
+class TemporalContext(NamedTuple):
+    """TemporalContext represents an effective time, used for business logic, and last_event_id
+    which is used to identify that state of the event log at some point in time. Put another way,
+    the value of a TemporalContext represents a point in time and a snapshot of the event log.
+
+    Effective time: This is the effective time of the computation in terms of business logic,
+    and it impacts the behavior of partitioning and partition mapping. For example,
+    the "last" partition window of a given partitions definition, it is with
+    respect to the effective time.
+
+    Last event id: Our event log has a monotonically increasing event id. This is used to
+    cursor the event log. This event_id is also propogated to derived tables to indicate
+    when that record is valid.  This allows us to query the state of the event log
+    at a given point in time.
+
+    Note that insertion time of the last_event_id is not the same as the effective time.
+
+    A last_event_id of None indicates that the reads will be volatile will immediately
+    reflect any subsequent writes.
+    """
+
+    effective_dt: datetime
+    last_event_id: Optional[int]
 
 
 class AssetGraphView(LoadingContext):
