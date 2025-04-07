c98ee88a89eedc1de705473a13741540d0d8cb77
Nick Schrock <schrockn@dagsterlabs.com>
2025-04-07 10:51:04 -0400

    cp

--- examples/docs_snippets/docs_snippets/guides/components/shell-script-component/2-shell-command-empty.py
+++ examples/docs_snippets/docs_snippets/guides/components/shell-script-component/2-shell-command-empty.py
@@ -2,14 +2,13 @@ import dagster as dg
 from dagster.components import Component, ComponentLoadContext, Model, Resolvable
 
 
-@dataclass
 class ShellCommand(Component, Model, Resolvable):
     """COMPONENT SUMMARY HERE.
 
     COMPONENT DESCRIPTION HERE.
     """
 
-    # added fields here will define yaml schema via Resolvable
+    # added fields here will define yaml schema via Model
 
     def build_defs(self, context: ComponentLoadContext) -> dg.Definitions:
         # Add definition construction logic here.
