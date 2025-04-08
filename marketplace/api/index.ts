784a565b4a9b5add72068497073255d6768b877b
Colton Padden <colton@dagsterlabs.com>
2025-04-08 15:46:09 -0400

    [marketplace] scaffold express API

---
+++ marketplace/api/index.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Dagster Marketplace"));
+
+app.get("/api/registry", (req, res) => res.json(
+    {
+        message: 'Hello, world!',
+        elements: [1, 2, 3, 4, 5],
+    }
+));
+
+app.listen(8080, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
