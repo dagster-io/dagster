47980d905f19c4b12795887d60de4a2dbd2a90dd
Jordan Sanders <jordan@dagsterlabs.com>
2025-04-08 08:50:35 -0500

    Import op

--- python_modules/dagster-test/dagster_test/toys/notebooks.py
+++ python_modules/dagster-test/dagster_test/toys/notebooks.py
@@ -1,13 +1,15 @@
-from dagster import job
+from dagster import job, op
 
 try:
     from dagstermill.factory import define_dagstermill_op
 
     hello_world_notebook_op = define_dagstermill_op("hello_world_notebook_op", "hello_world.ipynb")
 except ImportError:
+
     @op(name="hello_world_notebook_op")
     def mock_notebook_op():
         return None
+
     hello_world_notebook_op = mock_notebook_op
 
 
