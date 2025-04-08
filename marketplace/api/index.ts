2ef56b7555bc16ee0d1237de074e2dc20a298bec
Colton Padden <colton@dagsterlabs.com>
2025-04-08 18:28:28 -0400

    attempt serve of __generated__

--- marketplace/api/index.ts
+++ marketplace/api/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => { res.send('Dagster Marketplace API') })
+
 //
-// Serve `__generated__` files (e.g. `index.json` at `/integrations/index.json`)
+// Serve `__generated__` files (e.g. `index.json` at `/api/integrations/index.json`)
 //
-app.use("/integrations", express.static("__generated__"));
+app.use("/api/integrations", express.static("__generated__"));
 
 // Servce static assets, for example:
 //
