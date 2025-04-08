9b7224da6ab0fcf70d1c89fd3bf36a9f1c706ebc
Jordan Sanders <jordan@dagsterlabs.com>
2025-04-08 15:54:19 -0500

    debug

--- conftest.py
+++ conftest.py
@@ -30,24 +30,28 @@ def buildkite_quarantined_tests() -> set[TestId]:
         if os.getenv("BUILDKITE_BRANCH", "").startswith("release-"):
             return quarantined_tests
 
-        import requests
+        try:
+            import requests
 
-        token = os.getenv("BUILDKITE_TEST_QUARANTINE_TOKEN")
-        org_slug = os.getenv("BUILDKITE_ORGANIZATION_SLUG")
-        suite_slug = os.getenv("BUILDKITE_TEST_SUITE_SLUG")
+            token = os.getenv("BUILDKITE_TEST_QUARANTINE_TOKEN")
+            org_slug = os.getenv("BUILDKITE_ORGANIZATION_SLUG")
+            suite_slug = os.getenv("BUILDKITE_TEST_SUITE_SLUG")
 
-        headers = {"Authorization": f"Bearer {token}"}
-        url = f"https://api.buildkite.com/v2/analytics/organizations/{org_slug}/suites/{suite_slug}/tests/muted"
+            headers = {"Authorization": f"Bearer {token}"}
+            url = f"https://api.buildkite.com/v2/analytics/organizations/{org_slug}/suites/{suite_slug}/tests/muted"
 
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
 
-        for test in response.json():
-            scope = test.get("scope", "")
-            name = test.get("name", "")
-            quarantined_test = TestId(scope, name)
+            for test in response.json():
+                scope = test.get("scope", "")
+                name = test.get("name", "")
+                quarantined_test = TestId(scope, name)
 
-            quarantined_tests.add(quarantined_test)
+                quarantined_tests.add(quarantined_test)
+
+        except Exception as e:
+            print(e)  # noqa
 
     return quarantined_tests
 
@@ -70,6 +74,7 @@ def pytest_runtest_setup(item):
     # Buildkite Test Engine marks unreliable tests as muted and triages them out to owning teams to improve.
     # We pull this list of tests at the beginning of each pytest session and add soft xfail markers to each
     # quarantined test.
+    print(buildkite_quarantined_tests())
     try:
         if buildkite_quarantined_tests():
             # https://github.com/buildkite/test-collector-python/blob/6fba081a2844d6bdec8607942eee48a03d60cd40/src/buildkite_test_collector/pytest_plugin/buildkite_plugin.py#L22-L27
@@ -78,6 +83,7 @@ def pytest_runtest_setup(item):
             name = chunks[-1]
 
             test = TestId(scope, name)
+            print(test)
 
             if test in buildkite_quarantined_tests():
                 item.add_marker(pytest.mark.xfail(reason="Test muted in Buildkite.", strict=False))
