bc6a6664ea747b64ab4b074fa699a49d1cccb30e
Isaac Hellendag <2823852+hellendag@users.noreply.github.com>
2025-04-09 10:14:01 -0500

    [marketplace] Try again with eslint/prettier config (#29141)
    ## Summary & Motivation
    
    Run prettier with eslint.
    
    ## How I Tested These Changes
    
    yarn lint, verify that `api/index.ts` is prettiered.

--- marketplace/api/index.ts
+++ marketplace/api/index.ts
@@ -1,21 +1,23 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
 
 const app = express();
 
-app.get('/', (req, res) => { res.send('Dagster Marketplace API') })
+app.get('/', (req, res) => {
+  res.send('Dagster Marketplace API');
+});
 
 //
 // Serve `__generated__` files (e.g. `index.json` at `/api/integrations/index.json`)
 //
-app.use("/api/integrations", express.static(path.join(__dirname, "__generated__")));
+app.use('/api/integrations', express.static(path.join(__dirname, '__generated__')));
 
 // Servce static assets, for example:
 //
 // public/images/dagster-primary-horizontal.svg -> /assets/images/dagster-primary-horizontal.svg
 //
-app.use("/assets", express.static(path.join(__dirname, "public")));
+app.use('/assets', express.static(path.join(__dirname, 'public')));
 
-app.listen(8080, () => console.log("Server ready on port 8080."));
+app.listen(8080, () => console.log('Server ready on port 8080.'));
 
 module.exports = app;
