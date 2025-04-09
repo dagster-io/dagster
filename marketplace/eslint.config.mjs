7814c4c606df7f1e4a07342e7db1fd6c70da3de2
Isaac Hellendag <dish@elementl.com>
2025-04-09 09:15:04 -0500

    [marketplace] Clean up yarn etc

--- marketplace/eslint.config.mjs
+++ marketplace/eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
-  {files: ['**/*.{ts}'], plugins: {js}, extends: ['js/recommended']},
+  {files: ['**/*.{ts,json}'], plugins: {js}, extends: ['js/recommended']},
   tseslint.configs.recommended,
   eslintConfigPrettier,
 ]);
