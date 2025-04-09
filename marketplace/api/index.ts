670135d5a6a1cffc61a7f34988474129898a4946
Colton Padden <colton@dagsterlabs.com>
2025-04-09 10:56:46 -0400

    [marketplace] use dirname in paths

--- marketplace/api/index.ts
+++ marketplace/api/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
@@ -7,13 +8,13 @@ app.get('/', (req, res) => { res.send('Dagster Marketplace API') })
 //
 // Serve `__generated__` files (e.g. `index.json` at `/api/integrations/index.json`)
 //
-app.use("/api/integrations", express.static("__generated__"));
+app.use("/api/integrations", express.static(path.join(__dirname, "__generated__")));
 
 // Servce static assets, for example:
 //
 // public/images/dagster-primary-horizontal.svg -> /assets/images/dagster-primary-horizontal.svg
 //
-app.use("/assets", express.static("public"));
+app.use("/assets", express.static(path.join(__dirname, "public")));
 
 app.listen(8080, () => console.log("Server ready on port 8080."));
 
