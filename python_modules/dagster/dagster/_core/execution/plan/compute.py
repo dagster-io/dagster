be222b92e5858038361c5fa370db1c85cd27b139
gibsondan <daniel@elementl.com>
2025-04-10 08:43:50 -0500

    Deprecate/remove 'should_materiailze' field on StepOutput (#29147)
    ## Summary & Motivation
    This appears to be unused in 2025. Retain the serialized field for any
    old versions that might still be using it, but make the name clearer
    that it is unused.
    
    ## How I Tested These Changes
    New back-compat test case

--- python_modules/dagster/dagster/_core/execution/plan/compute.py
+++ python_modules/dagster/dagster/_core/execution/plan/compute.py
@@ -76,7 +76,7 @@ def create_step_outputs(
                     is_required=output_def.is_required,
                     is_dynamic=output_def.is_dynamic,
                     is_asset=asset_key is not None,
-                    should_materialize=output_def.name in config_output_names,
+                    should_materialize_DEPRECATED=output_def.name in config_output_names,
                     asset_key=asset_node.key
                     if asset_node and asset_node.key in asset_layer.asset_keys_for_node(handle)
                     else None,
