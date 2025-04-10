7b27d18668650478dd6c911729521e52f3a66c03
Jordan Sanders <jordan@dagsterlabs.com>
2025-04-10 16:30:43 -0500

    Modernize postgres docker compose fixtures (#29186)

--- python_modules/libraries/dagster-postgres/dagster_postgres_tests/conftest.py
+++ python_modules/libraries/dagster-postgres/dagster_postgres_tests/conftest.py
@@ -1,16 +1,12 @@
 import pytest
-from dagster._utils import file_relative_path
-from dagster._utils.test.postgres_instance import TestPostgresInstance
+from dagster_postgres.test_fixtures import postgres_conn_str, postgres_hostname  # noqa: F401
 
 
-@pytest.fixture(scope="session")
-def hostname(conn_string):
-    return TestPostgresInstance.get_hostname()
+@pytest.fixture
+def hostname(postgres_hostname):  # noqa: F811
+    yield postgres_hostname
 
 
-@pytest.fixture(scope="session")
-def conn_string():
-    with TestPostgresInstance.docker_service_up_or_skip(
-        file_relative_path(__file__, "docker-compose.yml"), "test-postgres-db"
-    ) as conn_str:
-        yield conn_str
+@pytest.fixture
+def conn_string(postgres_conn_str):  # noqa: F811
+    yield postgres_conn_str
