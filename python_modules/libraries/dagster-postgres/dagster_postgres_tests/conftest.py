ac59315b90333b6f1f9e9bbbc1ef3d5f791a7784
Jordan Sanders <jordan@dagsterlabs.com>
2025-04-10 15:18:56 -0500

    Explicitly import fixtures

--- python_modules/libraries/dagster-postgres/dagster_postgres_tests/conftest.py
+++ python_modules/libraries/dagster-postgres/dagster_postgres_tests/conftest.py
@@ -1,13 +1,15 @@
 import pytest
-
-pytest_plugins = ["dagster_postgres.test_fixtures"]
+from dagster_postgres.test_fixtures import (  # noqa: F401 #pyright:ignore reportMissingImports
+    postgres_conn_str,
+    postgres_hostname,
+)
 
 
 @pytest.fixture
-def hostname(postgres_hostname):
+def hostname(postgres_hostname):  # noqa: F811
     yield postgres_hostname
 
 
 @pytest.fixture
-def conn_string(postgres_conn_str):
+def conn_string(postgres_conn_str):  # noqa: F811
     yield postgres_conn_str
