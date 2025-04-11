68b2bfaebc9d052f92f4dc70d3162e885b0fefba
Jordan Sanders <jordan@dagsterlabs.com>
2025-04-11 14:05:23 -0500

    Run monitoring integration test against fake S3 (#29205)

--- integration_tests/test_suites/daemon-test-suite/monitoring_daemon_tests/test_monitoring.py
+++ integration_tests/test_suites/daemon-test-suite/monitoring_daemon_tests/test_monitoring.py
@@ -1,15 +1,17 @@
 import os
 import time
 from contextlib import contextmanager
+from pathlib import Path
 
+import boto3
 import pytest
 from dagster._core.storage.dagster_run import DagsterRunStatus
 from dagster._core.test_utils import instance_for_test, poll_for_finished_run
 from dagster._daemon.controller import all_daemons_healthy
 from dagster._utils.merger import merge_dicts
-from dagster_aws.utils import ensure_dagster_aws_tests_import
 from dagster_shared.ipc import interrupt_ipc_subprocess, open_ipc_subprocess
 from dagster_shared.yaml_utils import load_yaml_from_path
+from dagster_test.fixtures import docker_compose_cm
 from dagster_test.test_project import (
     ReOriginatedExternalJobForTest,
     find_local_test_image,
@@ -20,12 +22,48 @@ from dagster_test.test_project import (
     get_test_project_workspace_and_remote_job,
 )
 
-ensure_dagster_aws_tests_import()
-from dagster_aws_tests.aws_credential_test_utils import get_aws_creds
-
 IS_BUILDKITE = os.getenv("BUILDKITE") is not None
 
 pytest_plugins = ["dagster_postgres.test_fixtures"]
+
+compose_file = Path(__file__).parent / "docker-compose.yml"
+
+
+@pytest.fixture(scope="session")
+def hostnames():
+    with docker_compose_cm(compose_file) as hostnames:
+        yield hostnames
+
+
+@pytest.fixture(scope="session")
+def postgres_hostname(hostnames):
+    yield hostnames["postgres"]
+
+
+@pytest.fixture(scope="session")
+def aws_env(hostnames):
+    region = "us-east-1"
+    hostname = hostnames["s3"]
+    endpoint_url_from_pytest = f"http://{hostname}:4566"
+    endpoint_url_from_dagster_container = (
+        endpoint_url_from_pytest if IS_BUILDKITE else "http://s3:4566"
+    )
+    access_key_id = "fake"
+    secret_access_key = "fake"
+
+    boto3.client(
+        "s3",
+        region_name=region,
+        endpoint_url=endpoint_url_from_pytest,
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+    ).create_bucket(Bucket="dagster-scratch-80542c2")
+
+    yield [
+        f"AWS_ENDPOINT_URL={endpoint_url_from_dagster_container}",
+        f"AWS_ACCESS_KEY_ID={access_key_id}",
+        f"AWS_SECRET_ACCESS_KEY={secret_access_key}",
+    ]
 
 
 @pytest.fixture
@@ -154,15 +192,6 @@ def test_docker_monitoring(docker_postgres_instance, aws_env):
                     # daemon resumes the run
                     poll_for_finished_run(instance, run.run_id, timeout=300)
                     assert instance.get_run_by_id(run.run_id).status == DagsterRunStatus.SUCCESS
-
-
-@pytest.fixture
-def aws_env():
-    aws_creds = get_aws_creds()
-    return [
-        f"AWS_ACCESS_KEY_ID={aws_creds['aws_access_key_id']}",
-        f"AWS_SECRET_ACCESS_KEY={aws_creds['aws_secret_access_key']}",
-    ]
 
 
 def test_docker_monitoring_run_out_of_attempts(docker_postgres_instance, aws_env):
