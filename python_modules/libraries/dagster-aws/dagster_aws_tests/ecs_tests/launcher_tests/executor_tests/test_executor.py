f0124116777d2869cd3cb026f72a38c92a2d2470
James Keys <james@soji.ai>
2025-04-15 20:59:37 +0700

    Remove invalid networkConfiguration before call to run_task API in ECS executor

--- python_modules/libraries/dagster-aws/dagster_aws_tests/ecs_tests/launcher_tests/executor_tests/test_executor.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/ecs_tests/launcher_tests/executor_tests/test_executor.py
@@ -114,6 +114,7 @@ def test_executor_init(instance_cm: Callable[..., ContextManager[DagsterInstance
                         }
                     ],
                 },
+                "run_task_kwargs": {"networkConfiguration": None},
             },
         )
 
@@ -153,6 +154,8 @@ def test_executor_init(instance_cm: Callable[..., ContextManager[DagsterInstance
             )
 
             assert run_task_kwargs["launchType"] == "FARGATE"  # this comes from the Run Launcher
+
+            assert "networkConfiguration" not in run_task_kwargs
 
             overrides = run_task_kwargs["overrides"]
 
