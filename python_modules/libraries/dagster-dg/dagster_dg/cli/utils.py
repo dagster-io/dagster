e456c01f8e4347f040019d0a6e3f080650aa50dd
Brett Polivka <brett.polivka@magnetar.com>
2025-04-15 11:19:58 -0500

    [dg] make temp workspace creation work on Windows
    On Windows, the NamedTemporaryFile function requires that delete_on_close=False if delete=True, otherwise a Permission denied error will occur

--- python_modules/libraries/dagster-dg/dagster_dg/cli/utils.py
+++ python_modules/libraries/dagster-dg/dagster_dg/cli/utils.py
@@ -150,7 +150,7 @@ MIN_ENV_VAR_INJECTION_VERSION = "1.10.8"
 
 @contextmanager
 def create_temp_workspace_file(dg_context: DgContext) -> Iterator[str]:
-    with NamedTemporaryFile(mode="w+", delete=True) as temp_workspace_file:
+    with NamedTemporaryFile(mode="w+", delete=True, delete_on_close=False) as temp_workspace_file:
         entries = []
         if dg_context.is_project:
             entries.append(_workspace_entry_for_project(dg_context))
