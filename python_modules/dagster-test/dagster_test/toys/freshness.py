1bc88d97833903ef632cfe31ea034b61a37858c6
Anurag Anjaria <anurag@dagsterlabs.com>
2025-04-16 13:12:44 -0400

    add asset with 60s fail window, 30s warn window

--- python_modules/dagster-test/dagster_test/toys/freshness.py
+++ python_modules/dagster-test/dagster_test/toys/freshness.py
@@ -27,9 +27,19 @@ def asset_with_time_window_freshness_and_warning():
     return 1
 
 
+@dg.asset(
+    internal_freshness_policy=InternalFreshnessPolicy.time_window(
+        fail_window=timedelta(seconds=60), warn_window=timedelta(seconds=30)
+    )
+)
+def asset_with_time_window_freshness_fast_fail():
+    return 1
+
+
 def get_freshness_assets():
     return [
         asset_with_time_window_freshness_60m,
         asset_with_time_window_freshness_30m,
         asset_with_time_window_freshness_and_warning,
+        asset_with_time_window_freshness_fast_fail,
     ]
