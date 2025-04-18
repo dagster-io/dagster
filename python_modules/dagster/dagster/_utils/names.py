804cc507600379ec3f958beba29d42ec96d7bc97
Maxime Armstrong <maxime@retain.ai>
2025-04-18 10:45:29 -0400

    [utils] Rename clean_asset_name to clean_name

--- python_modules/dagster/dagster/_utils/names.py
+++ python_modules/dagster/dagster/_utils/names.py
@@ -1,6 +1,6 @@
 import re
 
 
-def clean_asset_name(name: str) -> str:
-    """Cleans an input to be a valid Dagster asset name by replacing invalid characters with underscores."""
+def clean_name(name: str) -> str:
+    """Cleans an input to be a valid Dagster name by replacing invalid characters with underscores."""
     return re.sub(r"[^A-Za-z0-9_]+", "_", name)
