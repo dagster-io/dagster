0b0e117c8476291a8ebddd76682377e6829b463f
Maxime Armstrong <maxime@retain.ai>
2025-04-18 12:46:47 -0400

    Update tableau and sling

--- python_modules/dagster/dagster/_utils/names.py
+++ python_modules/dagster/dagster/_utils/names.py
@@ -11,3 +11,10 @@ def clean_name_lower(name: str) -> str:
     and converting uppercase characters into lowercase characters.
     """
     return clean_name(name=name).lower()
+
+
+def clean_name_lower_with_dots(name: str) -> str:
+    """Cleans an input to be a valid Dagster name by replacing invalid characters with underscores
+    and converting uppercase characters into lowercase characters. This function keeps dots from the original string.
+    """
+    return re.sub(r"[^A-Za-z0-9_.]+", "_", name).lower()
