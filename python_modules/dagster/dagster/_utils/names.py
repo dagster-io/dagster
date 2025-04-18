9ad0c7f9ed6cf051041b60c40dc9225866e8d8c0
Maxime Armstrong <maxime@retain.ai>
2025-04-18 12:31:30 -0400

    Remove clean_name_upper from utils

--- python_modules/dagster/dagster/_utils/names.py
+++ python_modules/dagster/dagster/_utils/names.py
@@ -11,10 +11,3 @@ def clean_name_lower(name: str) -> str:
     and converting uppercase characters into lowercase characters.
     """
     return clean_name(name=name).lower()
-
-
-def clean_name_upper(name: str) -> str:
-    """Cleans an input to be a valid Dagster name by replacing invalid characters with underscores
-    and converting lowercase characters into uppercase characters.
-    """
-    return clean_name(name=name).upper()
