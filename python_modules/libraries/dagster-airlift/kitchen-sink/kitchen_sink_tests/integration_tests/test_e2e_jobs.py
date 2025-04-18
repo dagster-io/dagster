90ac654272ce6ecaad3454f4f7c7f5da98d71acd
Chris DeCarolis <cdguitar817@gmail.com>
2025-04-18 10:14:37 -0700

    address feedback

--- python_modules/libraries/dagster-airlift/kitchen-sink/kitchen_sink_tests/integration_tests/test_e2e_jobs.py
+++ python_modules/libraries/dagster-airlift/kitchen-sink/kitchen_sink_tests/integration_tests/test_e2e_jobs.py
@@ -8,6 +8,6 @@ def test_job_based_defs(
     from kitchen_sink.dagster_defs.job_based_defs import defs
 
     assert len(defs.jobs) == 19
-    assert len(defs.assets) == 2
+    assert len(defs.assets) == 1
     for key in ["print_asset", "another_print_asset", "example1", "example2"]:
         assert asset_spec(key, defs)
