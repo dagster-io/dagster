e4dbc8b70ce2129ef557c4ecca945cf1d42f0fd4
Dennis Hume <dennis@dagsterlabs.com>
2025-04-22 10:34:40 -0500

    Exclude definitions.py

--- examples/docs_snippets/docs_snippets_tests/test_all_files_load.py
+++ examples/docs_snippets/docs_snippets_tests/test_all_files_load.py
@@ -24,8 +24,7 @@ EXCLUDED_FILES = {
     f"{snippets_folder}/dagster-plus/deployment/serverless/runtime-environment/data_files_setup.py",
     f"{snippets_folder}/dagster-plus/deployment/serverless/runtime-environment/example_setup.py",
     # these files are part of a completed project and the import references are failing the tests
-    f"{snippets_folder}/guides/tutorials/etl_tutorial_completed/etl_tutorial/assets.py",
-    f"{snippets_folder}/guides/tutorials/etl_tutorial_completed/etl_tutorial/definitions.py",
+    f"{snippets_folder}/guides/tutorials/etl_tutorial/definitions.py",
     # there are no components defined in the snippets and so it would fail to load
     f"{snippets_folder}/guides/components/existing-project/definitions-after.py",
     # there are no components defined in the snippets and so it would fail to load
