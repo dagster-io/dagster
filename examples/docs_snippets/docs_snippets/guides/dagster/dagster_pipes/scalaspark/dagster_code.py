346f250f78079a88bf1a1dc42d24bf8d96c17d4a
Colton Padden <colton@dagsterlabs.com>
2025-04-28 11:04:50 -0400

    make ruff

--- examples/docs_snippets/docs_snippets/guides/dagster/dagster_pipes/scalaspark/dagster_code.py
+++ examples/docs_snippets/docs_snippets/guides/dagster/dagster_pipes/scalaspark/dagster_code.py
@@ -1,31 +1,33 @@
-import dagster as dg
-import boto3
-import subprocess
-from dagster_aws.pipes import PipesS3MessageReader, PipesS3ContextInjector
 import os
-from typing import Sequence
+import subprocess
+from collections.abc import Sequence
 
-@dg.asset(check_specs=[dg.AssetCheckSpec(
-            name="demo_check",
-            asset="scala_spark_demo"
-        )
-    ]
-)
+import boto3
+from dagster_aws.pipes import PipesS3ContextInjector, PipesS3MessageReader
+
+import dagster as dg
+
+
+@dg.asset(check_specs=[dg.AssetCheckSpec(name="demo_check", asset="scala_spark_demo")])
 def scala_spark_demo(context: dg.AssetExecutionContext) -> dg.MaterializeResult:
     s3_client = boto3.client("s3")
     s3_bucket_name = os.environ["DAGSTER_PIPES_BUCKET"]
-    
-    jar_path = dg.file_relative_path(__file__, "external_scala/build/libs/external_scala-all.jar")
+
+    jar_path = dg.file_relative_path(
+        __file__, "external_scala/build/libs/external_scala-all.jar"
+    )
 
     with dg.open_pipes_session(
         context=context,
         message_reader=PipesS3MessageReader(bucket=s3_bucket_name, client=s3_client),
-        context_injector=PipesS3ContextInjector(bucket=s3_bucket_name, client=s3_client),
+        context_injector=PipesS3ContextInjector(
+            bucket=s3_bucket_name, client=s3_client
+        ),
     ) as session:
         args = []
         for key, value in session.get_bootstrap_cli_arguments().items():
             args.extend([key, str(value)])
-        
+
         subprocess.run(
             ["spark-submit", jar_path] + args,
             shell=False,
@@ -34,6 +36,7 @@ def scala_spark_demo(context: dg.AssetExecutionContext) -> dg.MaterializeResult:
 
     return session.get_results()
 
+
 defs = dg.Definitions(
-  assets=[scala_spark_demo],
+    assets=[scala_spark_demo],
 )
