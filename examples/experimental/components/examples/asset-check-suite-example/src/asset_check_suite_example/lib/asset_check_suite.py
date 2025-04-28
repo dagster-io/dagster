3714370198ba72fe673e2998b378def8872686d0
Colton Padden <colton@dagsterlabs.com>
2025-04-28 16:34:17 -0400

    refactor test harness

--- examples/experimental/components/examples/asset-check-suite-example/src/asset_check_suite_example/lib/asset_check_suite.py
+++ examples/experimental/components/examples/asset-check-suite-example/src/asset_check_suite_example/lib/asset_check_suite.py
@@ -1,22 +1,51 @@
+from collections.abc import Sequence
 from dataclasses import dataclass
+from typing import Literal, Union
 
 import dagster as dg
 import polars as pl
 from dagster.components import Component, ComponentLoadContext, Resolvable
+from pydantic import BaseModel
+
+
+class BaseCheck(BaseModel):
+    columns: list[str]
+
+
+class CheckIsBetween(BaseCheck):
+    type: Literal["is_between"]
+    min: int
+    max: int
+
+    def check(self, df: pl.DataFrame, column: str) -> bool:
+        return dg.AssetCheckResult(passed=df[column].is_between(self.min, self.max).all())
+
+
+class CheckIsNotNull(BaseCheck):
+    type: Literal["is_not_null"]
+
+    def check(self, df: pl.DataFrame, column: str) -> bool:
+        return df[column].is_null().not_().all()
 
 
 @dataclass
 class AssetCheckSuite(Component, Resolvable):
     asset: str
-    column: str
+    checks: Sequence[Union[CheckIsBetween, CheckIsNotNull]]
 
     def build_defs(self, context: ComponentLoadContext) -> dg.Definitions:
-        @dg.asset_check(
-            asset=self.asset,
-            name=f"check_{self.column}_between",
-            description="Check that values are within a defined range.",
-        )
-        def _check(df: pl.DataFrame) -> dg.AssetCheckResult:
-            return dg.AssetCheckResult(passed=df[self.target_column].is_between(0, 99).all())
+        def _build_checks():
+            for check in self.checks:
+                for column in check.columns:
 
-        return dg.Definitions(asset_checks=[_check])
+                    @dg.asset_check(
+                        asset=self.asset,
+                        name=f"check_{column}_{check.type}",
+                        description="Check that values are within a defined range.",
+                    )
+                    def _check(df: pl.DataFrame) -> dg.AssetCheckResult:
+                        return dg.AssetCheckresult(passed=check.check(df, column))
+
+                    yield _check
+
+        return dg.Definitions(asset_checks=list(_build_checks()))
