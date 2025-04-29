af5a304ff59b510c412038cc9967985ed910f6f2
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-04-29 10:26:50 -0600

    Pin `marshmallow` from 3.23.1 back to 3.20.1 again

--- python_modules/libraries/dagster-airlift/setup.py
+++ python_modules/libraries/dagster-airlift/setup.py
@@ -25,7 +25,7 @@ pin = "" if ver == "1!0+dev" or "rc" in ver else f"=={ver}"
 AIRFLOW_REQUIREMENTS = [
     # Requirements for python versions under 3.12.
     "apache-airflow==2.10.5; python_version < '3.12'",
-    "marshmallow==3.23.1; python_version < '3.12'",
+    "marshmallow==3.20.1; python_version < '3.12'",
     "pendulum>=2.0.0,<4.0.0; python_version < '3.12'",
     # Requirements for python versions 3.12 and above.
     "apache-airflow>=2.9.0; python_version >= '3.12'",
