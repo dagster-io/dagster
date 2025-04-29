1a4f8fe0ccee28cd13beb4b905b5ae1c98a6b1a7
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-04-28 21:06:14 -0600

    Bump Apache Airflow and Pendulum versions for docs

--- python_modules/libraries/dagster-airlift/setup.py
+++ python_modules/libraries/dagster-airlift/setup.py
@@ -24,9 +24,9 @@ pin = "" if ver == "1!0+dev" or "rc" in ver else f"=={ver}"
 # to ensure compatibility.
 AIRFLOW_REQUIREMENTS = [
     # Requirements for python versions under 3.12.
-    "apache-airflow==2.7.3; python_version < '3.12'",
-    "marshmallow==3.20.1; python_version < '3.12'",
-    "pendulum>=2.0.0,<3.0.0; python_version < '3.12'",
+    "apache-airflow==2.10.5; python_version < '3.12'",
+    "marshmallow==3.23.1; python_version < '3.12'",
+    "pendulum>=2.0.0,<4.0.0; python_version < '3.12'",
     # Requirements for python versions 3.12 and above.
     "apache-airflow>=2.9.0; python_version >= '3.12'",
     "marshmallow==3.23.1; python_version >= '3.12'",
