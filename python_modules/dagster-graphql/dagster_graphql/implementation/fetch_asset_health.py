aa8da371118d728f984791756de06c63c003f3ce
JamieDeMaria <jamie@dagsterlabs.com>
2025-04-30 11:26:58 -0400

    make regeneration a class method

--- python_modules/dagster-graphql/dagster_graphql/implementation/fetch_asset_health.py
+++ python_modules/dagster-graphql/dagster_graphql/implementation/fetch_asset_health.py
@@ -1,86 +1,12 @@
 from typing import TYPE_CHECKING, Optional
 
-from dagster._core.definitions.asset_check_spec import AssetCheckSeverity
 from dagster._core.definitions.asset_key import AssetKey
-from dagster._core.storage.asset_check_execution_record import AssetCheckExecutionResolvedStatus
-from dagster._core.storage.event_log.base import AssetCheckSummaryRecord
 from dagster._streamline.asset_check_health import AssetCheckHealthState
 from dagster._streamline.asset_health import AssetHealthStatus
 
 if TYPE_CHECKING:
     from dagster_graphql.schema.asset_health import GrapheneAssetHealthCheckMeta
     from dagster_graphql.schema.util import ResolveInfo
-
-
-async def _compute_asset_check_health_state(
-    graphene_info: "ResolveInfo", asset_key: AssetKey
-) -> AssetCheckHealthState:
-    """Computes the number of asset checks in each terminal state for an asset so that the overall
-    health can be computed in get_asset_check_status_and_metadata. Does this by fetching the
-    asset check summary record for each check and checking the status of the latest completed execution.
-    """
-    remote_check_nodes = graphene_info.context.asset_graph.get_checks_for_asset(asset_key)
-    if not remote_check_nodes or len(remote_check_nodes) == 0:
-        # asset doesn't have checks defined
-        return AssetCheckHealthState.default()
-
-    failed_checks = set()
-    warning_checks = set()
-    passing_checks = set()
-    asset_check_summary_records = await AssetCheckSummaryRecord.gen_many(
-        graphene_info.context,
-        [remote_check_node.asset_check.key for remote_check_node in remote_check_nodes],
-    )
-    for summary_record in asset_check_summary_records:
-        if summary_record is None or summary_record.last_check_execution_record is None:
-            # the check has never been executed.
-            continue
-
-        # if the last_check_execution_record is completed, it will be the same as last_completed_check_execution_record,
-        # but we check the last_check_execution_record status first since there is an edge case
-        # where the record will have status PLANNED, but the resolve_status will be EXECUTION_FAILED
-        # because the run for the check failed.
-        last_check_execution_status = (
-            await summary_record.last_check_execution_record.resolve_status(graphene_info.context)
-        )
-        last_check_evaluation = summary_record.last_check_execution_record.evaluation
-
-        if last_check_execution_status in [
-            AssetCheckExecutionResolvedStatus.IN_PROGRESS,
-            AssetCheckExecutionResolvedStatus.SKIPPED,
-        ]:
-            # the last check is still in progress or is skipped, so we want to check the status of
-            # the latest completed check instead
-            if summary_record.last_completed_check_execution_record is None:
-                # the check hasn't been executed prior to this in progress check
-                continue
-            last_check_execution_status = (
-                await summary_record.last_completed_check_execution_record.resolve_status(
-                    graphene_info.context
-                )
-            )
-            last_check_evaluation = summary_record.last_completed_check_execution_record.evaluation
-
-        if last_check_execution_status == AssetCheckExecutionResolvedStatus.FAILED:
-            # failed checks should always have an evaluation, but default to ERROR if not
-            if last_check_evaluation and last_check_evaluation.severity == AssetCheckSeverity.WARN:
-                warning_checks.add(summary_record.asset_check_key)
-            else:
-                failed_checks.add(summary_record.asset_check_key)
-        elif last_check_execution_status == AssetCheckExecutionResolvedStatus.EXECUTION_FAILED:
-            # EXECUTION_FAILED checks may not have an evaluation, and we want to show these as
-            # degraded health anyway.
-            failed_checks.add(summary_record.asset_check_key)
-        else:
-            # asset check passed
-            passing_checks.add(summary_record.asset_check_key)
-
-    return AssetCheckHealthState(
-        passing_checks=passing_checks,
-        failing_checks=failed_checks,
-        warning_checks=warning_checks,
-        all_checks={node.asset_check.key for node in remote_check_nodes},
-    )
 
 
 async def get_asset_check_status_and_metadata(
@@ -105,7 +31,11 @@ async def get_asset_check_status_and_metadata(
             # asset_check_health_state_for is only None if no are defined on the asset
             return GrapheneAssetHealthStatus.NOT_APPLICABLE, None
     else:
-        asset_check_health_state = await _compute_asset_check_health_state(graphene_info, asset_key)
+        remote_check_nodes = graphene_info.context.asset_graph.get_checks_for_asset(asset_key)
+        asset_check_health_state = await AssetCheckHealthState.compute_for_asset_checks(
+            {remote_check_node.asset_check.key for remote_check_node in remote_check_nodes},
+            graphene_info.context,
+        )
 
     if asset_check_health_state.health_status == AssetHealthStatus.HEALTHY:
         return GrapheneAssetHealthStatus.HEALTHY, None
