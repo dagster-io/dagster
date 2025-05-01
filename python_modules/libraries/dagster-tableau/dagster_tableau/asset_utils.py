87a8c8a0d9d31b3e056d1ae364057c0ba9122dea
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-05-01 19:15:36 -0400

    [2/n][dagster-tableau] Update spec parser to include extracts as materializable assets (#29680)
    ## Summary & Motivation
    
    This PR updates the
    `parse_tableau_external_and_materializable_asset_specs` fn to let users
    select data sources with extracts as materializable Tableau assets.
    
    ## How I Tested These Changes
    
    New test with BK

--- python_modules/libraries/dagster-tableau/dagster_tableau/asset_utils.py
+++ python_modules/libraries/dagster-tableau/dagster_tableau/asset_utils.py
@@ -6,7 +6,7 @@ from dagster import (
     _check as check,
 )
 
-from dagster_tableau.translator import TableauTagSet
+from dagster_tableau.translator import TableauDataSourceMetadataSet, TableauTagSet
 
 
 class ParsedTableauAssetSpecs(
@@ -30,22 +30,48 @@ class ParsedTableauAssetSpecs(
 
 def parse_tableau_external_and_materializable_asset_specs(
     specs: Sequence[AssetSpec],
+    include_data_sources_with_extracts: bool = False,
 ) -> ParsedTableauAssetSpecs:
     """Parses a list of Tableau AssetSpecs provided as input and return two lists of AssetSpecs,
     one for the Tableau external assets and another one for the Tableau materializable assets.
 
     In Tableau, data sources are considered external assets,
     while sheets and dashboards are considered materializable assets.
+
+    Args:
+        specs (Sequence[AssetSpec]): The asset specs of the assets in the Tableau workspace.
+        include_data_sources_with_extracts (bool):
+            Whether to include data sources with extracts in materializable assets.
+
+    Returns:
+        ParsedTableauAssetSpecs: A named tuple representing the parsed Tableau asset specs
+            as `external_asset_specs` and `materializable_asset_specs`.
     """
-    external_asset_specs = [
+    data_source_asset_specs = [
         spec for spec in specs if TableauTagSet.extract(spec.tags).asset_type == "data_source"
     ]
 
-    materializable_asset_specs = [
+    extract_asset_specs, non_extract_asset_specs = [], []
+    for spec in data_source_asset_specs:
+        if TableauDataSourceMetadataSet.extract(spec.metadata).has_extracts:
+            extract_asset_specs.append(spec)
+        else:
+            non_extract_asset_specs.append(spec)
+
+    view_asset_specs = [
         spec
         for spec in specs
         if TableauTagSet.extract(spec.tags).asset_type in ["dashboard", "sheet"]
     ]
+
+    external_asset_specs = (
+        non_extract_asset_specs if include_data_sources_with_extracts else data_source_asset_specs
+    )
+    materializable_asset_specs = (
+        view_asset_specs + extract_asset_specs
+        if include_data_sources_with_extracts
+        else view_asset_specs
+    )
 
     return ParsedTableauAssetSpecs(
         external_asset_specs=external_asset_specs,
