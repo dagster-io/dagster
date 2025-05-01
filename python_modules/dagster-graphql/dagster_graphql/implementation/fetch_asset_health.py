69fb78716c39fa3eaebff75c2c6984fd0fdc4e50
JamieDeMaria <jamie@dagsterlabs.com>
2025-05-01 15:58:33 -0400

    use freshness streamline

--- python_modules/dagster-graphql/dagster_graphql/implementation/fetch_asset_health.py
+++ python_modules/dagster-graphql/dagster_graphql/implementation/fetch_asset_health.py
@@ -1,11 +1,17 @@
 from typing import TYPE_CHECKING, Optional
 
 from dagster._core.definitions.asset_key import AssetKey
+from dagster._core.definitions.freshness import FreshnessState
+from dagster._core.storage.event_log.base import AssetRecord
 from dagster._streamline.asset_check_health import AssetCheckHealthState
+from dagster._streamline.asset_freshness_health import AssetFreshnessHealthState
 from dagster._streamline.asset_health import AssetHealthStatus
 
 if TYPE_CHECKING:
-    from dagster_graphql.schema.asset_health import GrapheneAssetHealthCheckMeta
+    from dagster_graphql.schema.asset_health import (
+        GrapheneAssetHealthCheckMeta,
+        GrapheneAssetHealthFreshnessMeta,
+    )
     from dagster_graphql.schema.util import ResolveInfo
 
 
@@ -69,3 +75,56 @@ async def get_asset_check_status_and_metadata(
         )
     else:  # asset_check_health_state.health_status == AssetHealthStatus.NOT_APPLICABLE
         return GrapheneAssetHealthStatus.NOT_APPLICABLE, None
+
+
+async def get_freshness_status_and_metadata(
+    graphene_info: "ResolveInfo", asset_key: AssetKey
+) -> tuple[str, Optional["GrapheneAssetHealthFreshnessMeta"]]:
+    """Gets an AssetFreshnessHealthState object for an asset, either via streamline or by computing
+    it based on the state of the DB. Then converts it to a GrapheneAssetHealthStatus and the metdata
+    needed to power the UIs. Metadata is fetched from the AssetLatestMaterializationState object, again
+    either via streamline or by computing it based on the state of the DB.
+    """
+    from dagster_graphql.schema.asset_health import (
+        GrapheneAssetHealthFreshnessMeta,
+        GrapheneAssetHealthStatus,
+    )
+
+    if graphene_info.context.instance.streamline_read_asset_health_supported():
+        asset_freshness_health_state = (
+            graphene_info.context.instance.get_asset_freshness_health_state_for_asset(asset_key)
+        )
+        if asset_freshness_health_state is None:
+            # if the freshness state is None, it means that the asset has no freshness policy, or streamline
+            # has not been run yet to process the asset. Return NOT_APPLICABLE, since if the asset
+            # does have a policy, streamline will process it in the future and the state will update.
+            return GrapheneAssetHealthStatus.NOT_APPLICABLE, None
+    else:
+        if graphene_info.context.asset_graph.get(asset_key).internal_freshness_policy is None:
+            return GrapheneAssetHealthStatus.NOT_APPLICABLE, None
+        asset_freshness_health_state = AssetFreshnessHealthState.compute_for_asset(
+            asset_key,
+            graphene_info.context,
+        )
+
+    asset_record = await AssetRecord.gen(graphene_info.context, asset_key)
+    materialization_timestamp = (
+        asset_record.asset_entry.last_materialization.timestamp
+        if asset_record
+        and asset_record.asset_entry
+        and asset_record.asset_entry.last_materialization
+        else None
+    )
+
+    if asset_freshness_health_state.freshness_state == FreshnessState.PASS:
+        return GrapheneAssetHealthStatus.HEALTHY, None
+    if asset_freshness_health_state.freshness_state == FreshnessState.WARN:
+        return GrapheneAssetHealthStatus.WARNING, GrapheneAssetHealthFreshnessMeta(
+            lastMaterializedTimestamp=materialization_timestamp,
+        )
+    if asset_freshness_health_state.freshness_state == FreshnessState.FAIL:
+        return GrapheneAssetHealthStatus.DEGRADED, GrapheneAssetHealthFreshnessMeta(
+            lastMaterializedTimestamp=materialization_timestamp,
+        )
+    else:  # asset_freshness_health_state.freshness_state == FreshnessState.UNKNOWN:
+        return GrapheneAssetHealthStatus.UNKNOWN, None
