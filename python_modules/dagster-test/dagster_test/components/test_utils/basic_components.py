bf205cb64ff1f27cf3525f8e11eb5bdb3447437f
Nick Schrock <schrockn@dagsterlabs.com>
2025-05-03 11:46:06 -0400

    cp

--- python_modules/dagster-test/dagster_test/components/test_utils/basic_components.py
+++ python_modules/dagster-test/dagster_test/components/test_utils/basic_components.py
@@ -6,13 +6,12 @@ from collections.abc import Mapping
 from dataclasses import dataclass
 from typing import Annotated, Any
 
-from pydantic import BaseModel, ConfigDict
-
 from dagster._core.definitions.definitions_class import Definitions
 from dagster.components.component.component import Component
 from dagster.components.core.context import ComponentLoadContext
 from dagster.components.resolved.base import Resolvable
 from dagster.components.resolved.model import Resolver
+from pydantic import BaseModel, ConfigDict
 
 
 def _inner_error():
