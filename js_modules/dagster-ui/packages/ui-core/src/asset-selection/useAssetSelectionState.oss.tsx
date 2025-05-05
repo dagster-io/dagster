0df4cb78e94508427c3a09ec01704ef7dafbba74
Marco polo <salazarm@users.noreply.github.com>
2025-05-05 12:55:38 -0400

    Update js_modules/dagster-ui/packages/ui-core/src/asset-selection/useAssetSelectionState.oss.tsx
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- js_modules/dagster-ui/packages/ui-core/src/asset-selection/useAssetSelectionState.oss.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-selection/useAssetSelectionState.oss.tsx
@@ -2,7 +2,9 @@ import {useQueryPersistedState} from '../hooks/useQueryPersistedState';
 
 export function getAssetSelectionQueryString(search: string = location.search) {
   const params = new URLSearchParams(search);
-  return `&asset-selection=${params.get('asset-selection')}`;
+  return params.get('asset-selection') 
+    ? `&asset-selection=${params.get('asset-selection')}` 
+    : '';
 }
 export function useAssetSelectionState() {
   return useQueryPersistedState<string>({
