2a52bb48f1a27a6f4bcd5531c475344abeffc281
benpankow <ben@dagsterlabs.com>
2025-05-06 10:21:07 -0700

    test util

--- python_modules/dagster/dagster_tests/components_tests/integration_tests/test_definitions_autoload.py
+++ python_modules/dagster/dagster_tests/components_tests/integration_tests/test_definitions_autoload.py
@@ -6,11 +6,7 @@ from dagster import AssetKey, Definitions
 from dagster._core.errors import DagsterInvalidDefinitionError
 from dagster._utils.env import environ
 from dagster.components.core.context import ComponentLoadContext
-from dagster.components.core.defs_module import (
-    CompositeYamlComponent,
-    DefsFolderComponent,
-    get_component,
-)
+from dagster.components.core.defs_module import CompositeYamlComponent, DefsFolderComponent
 from dagster_shared import check
 from pydantic import ValidationError
 
@@ -18,7 +14,10 @@ from dagster_tests.components_tests.integration_tests.component_loader import (
     chdir as chdir,
     sync_load_test_component_defs,
 )
-from dagster_tests.components_tests.utils import create_project_from_components
+from dagster_tests.components_tests.utils import (
+    create_project_from_components,
+    get_underlying_component,
+)
 
 
 @pytest.mark.parametrize("defs", ["definitions/explicit_file_relative_imports"], indirect=True)
@@ -156,8 +155,7 @@ def test_ignored_empty_dir():
     with create_project_from_components(path_str) as (project_root, project_name):
         module = importlib.import_module(f"{project_name}.defs.{src_path.stem}")
         context = ComponentLoadContext.for_module(module, project_root)
-        root = check.inst(get_component(context), CompositeYamlComponent)
-        defs_root = check.inst(root.components[0], DefsFolderComponent)
+        defs_root = check.inst(get_underlying_component(context), DefsFolderComponent)
         for comp in defs_root.iterate_components():
             if isinstance(comp, DefsFolderComponent):
                 assert comp.children
