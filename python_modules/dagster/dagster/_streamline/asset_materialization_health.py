132f2167d120e23e0a45a971c6335b53f8cd393b
JamieDeMaria <jamie@dagsterlabs.com>
2025-05-05 20:16:54 -0400

    update

--- python_modules/dagster/dagster/_streamline/asset_materialization_health.py
+++ python_modules/dagster/dagster/_streamline/asset_materialization_health.py
@@ -19,7 +19,10 @@ from dagster._core.storage.partition_status_cache import get_partition_subsets
 class AssetMaterializationHealthState:
     """For tracking the materialization health of an asset, we only care about the most recent
     completed materialization attempt for each asset/partition. This record keeps track of the
-    assets/partitions that are currently in a successful state and those that are in a failed state.
+    assets/partitions that have ever been successfully materialized and those that are currently in
+    a failed state. From this information we can derive the subset that is currently in a successfully
+    materialized state.
+
     If an asset/partition is currently being materialized, it will not move to a new state until after
     the materialization attempt is complete.
 
@@ -37,6 +40,10 @@ class AssetMaterializationHealthState:
         if self.partitions_snap is None:
             return None
         return self.partitions_snap.get_partitions_definition()
+
+    @property
+    def currently_materialized_subset(self) -> SerializableEntitySubset[AssetKey]:
+        return self.materialized_subset.compute_difference(self.failed_subset)
 
     @classmethod
     async def compute_for_asset(
@@ -90,12 +97,9 @@ class AssetMaterializationHealthState:
         has_ever_materialized = asset_entry.last_materialization is not None
         is_currently_failed = await _get_is_currently_failed(loading_context, asset_record)
 
-        # it's possible that the asset is not materialized and not failed (ie if it has never been run)
-        is_currently_materialized = has_ever_materialized and not is_currently_failed
-
         return cls(
             materialized_subset=SerializableEntitySubset(
-                key=asset_key, value=is_currently_materialized
+                key=asset_key, value=has_ever_materialized
             ),
             failed_subset=SerializableEntitySubset(key=asset_key, value=is_currently_failed),
             partitions_snap=None,
@@ -114,15 +118,22 @@ async def _get_is_currently_failed(
             ],
             key=lambda record: -1 if record is None else record.storage_id,
         )
-        return latest_record.storage_id == asset_entry.last_failed_to_materialize_storage_id
+        return (
+            latest_record.storage_id == asset_entry.last_failed_to_materialize_storage_id
+            if latest_record
+            else False
+        )
 
     # if failure events are not stored, we usually have to fetch the run record to check if the
     # asset is currently failed. However, if the latest run id is the same as the last materialization run id,
     # then we know the asset is in a successfully materialized state.
-    if asset_entry.last_run_id == asset_entry.last_materialization.run_id:
+    if (
+        asset_entry.last_materialization
+        and asset_entry.last_run_id == asset_entry.last_materialization.run_id
+    ):
         return False
 
-    run_record = await RunRecord.gen(loading_context, asset_entry.last_run_id)
+    run_record = await RunRecord.gen(loading_context, check.not_none(asset_entry.last_run_id))
     if run_record is None or not run_record.dagster_run.is_finished:
         # the run is deleted or in progress. With the information we have available, we cannot know
         # if the asset is in a failed state prior to this run, so we report it as not failed
