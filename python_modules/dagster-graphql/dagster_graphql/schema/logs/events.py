2a16c3db1e2b44f1ead3796615d15b15b87a046c
JamieDeMaria <jamie@dagsterlabs.com>
2025-05-07 13:11:00 -0400

    tmp

--- python_modules/dagster-graphql/dagster_graphql/schema/logs/events.py
+++ python_modules/dagster-graphql/dagster_graphql/schema/logs/events.py
@@ -470,6 +470,16 @@ class GrapheneObservationEvent(graphene.ObjectType, AssetEventMixin):
         )
 
 
+class GrapheneAssetEventType(graphene.Union):
+    class Meta:
+        types = (
+            GrapheneFailedToMaterializeEvent,
+            GrapheneMaterializationEvent,
+            GrapheneObservationEvent,
+        )
+        name = "AssetEventType"
+
+
 class GrapheneAssetMaterializationPlannedEvent(graphene.ObjectType):
     assetKey = graphene.Field(GrapheneAssetKey)
     runOrError = graphene.NonNull("dagster_graphql.schema.pipelines.pipeline.GrapheneRunOrError")
