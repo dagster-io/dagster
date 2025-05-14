9bff356c753e7df82c681d0abe0d5f53eac21266
prha <1040172+prha@users.noreply.github.com>
2025-05-14 12:08:34 -0700

    add automation condition information on the global asset graph (#29583) (#29985)
    ## Summary & Motivation
    Recommit of https://github.com/dagster-io/dagster/pull/29583, which had
    to be reverted to avoid gql / frontend cross-push backcompat issues
    
    ## How I Tested These Changes
    BK
    
    ## Changelog
    [ui] Added an asset graph node facet for viewing automation conditions
    and the most recent evaluation

--- js_modules/dagster-ui/packages/ui-core/src/asset-data/AssetLiveDataProvider.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-data/AssetLiveDataProvider.tsx
@@ -1,6 +1,10 @@
 import uniq from 'lodash/uniq';
 import React, {useCallback, useMemo, useReducer, useRef} from 'react';
 
+import {
+  AssetAutomationData,
+  __resetForJest as __resetAutomationData,
+} from './AssetAutomationDataProvider';
 import {AssetBaseData, __resetForJest as __resetBaseData} from './AssetBaseDataProvider';
 import {AssetHealthData, __resetForJest as __resetHealthData} from './AssetHealthDataProvider';
 import {
@@ -115,12 +119,14 @@ export const AssetLiveDataProvider = ({children}: {children: React.ReactNode}) =
     AssetStaleStatusData.manager.setPollRate(pollRate);
     AssetBaseData.manager.setPollRate(pollRate);
     AssetHealthData.manager.setPollRate(pollRate);
+    AssetAutomationData.manager.setPollRate(pollRate);
   }, [pollRate]);
 
   useDidLaunchEvent(() => {
     AssetStaleStatusData.manager.invalidateCache();
     AssetBaseData.manager.invalidateCache();
     AssetHealthData.manager.invalidateCache();
+    AssetAutomationData.manager.invalidateCache();
   }, SUBSCRIPTION_MAX_POLL_RATE);
 
   useThrottledEffect(
@@ -152,6 +158,7 @@ export const AssetLiveDataProvider = ({children}: {children: React.ReactNode}) =
           AssetBaseData.manager.invalidateCache();
           AssetStaleStatusData.manager.invalidateCache();
           AssetHealthData.manager.invalidateCache();
+          AssetAutomationData.manager.invalidateCache();
         }
       });
       return unobserve;
@@ -161,11 +168,13 @@ export const AssetLiveDataProvider = ({children}: {children: React.ReactNode}) =
   );
 
   return (
-    <AssetHealthData.LiveDataProvider>
-      <AssetBaseData.LiveDataProvider>
-        <AssetStaleStatusData.LiveDataProvider>{children}</AssetStaleStatusData.LiveDataProvider>
-      </AssetBaseData.LiveDataProvider>
-    </AssetHealthData.LiveDataProvider>
+    <AssetAutomationData.LiveDataProvider>
+      <AssetHealthData.LiveDataProvider>
+        <AssetBaseData.LiveDataProvider>
+          <AssetStaleStatusData.LiveDataProvider>{children}</AssetStaleStatusData.LiveDataProvider>
+        </AssetBaseData.LiveDataProvider>
+      </AssetHealthData.LiveDataProvider>
+    </AssetAutomationData.LiveDataProvider>
   );
 };
 
@@ -177,4 +186,5 @@ export function __resetForJest() {
   __resetBaseData();
   __resetStaleData();
   __resetHealthData();
+  __resetAutomationData();
 }
