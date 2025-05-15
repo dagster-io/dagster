8c300bf446cf06219d1c04fdfcd668d7589aa6aa
benpankow <ben@dagsterlabs.com>
2025-05-15 10:17:05 -0700

    [resolved] out-of-the-box support for union types

--- python_modules/dagster/dagster_tests/components_tests/resolution_tests/test_resolved_from.py
+++ python_modules/dagster/dagster_tests/components_tests/resolution_tests/test_resolved_from.py
@@ -1,5 +1,6 @@
+from collections.abc import Sequence
 from dataclasses import dataclass
-from typing import Annotated, Optional
+from typing import Annotated, Literal, Optional, Union
 
 from dagster.components import Component
 from dagster.components.resolved.base import Model, Resolvable
@@ -16,7 +17,7 @@ def test_nested_resolvable():
     class ResolvableComponent(Component, Resolvable, Model):
         thing: MyModel
 
-        def build_defs(self, _): ...  # type: ignore
+        def build_defs(self, _): ...
 
     c = load_component_for_test(
         ResolvableComponent,
@@ -38,7 +39,7 @@ thing:
             ),
         ]
 
-        def build_defs(self, _): ...  # type: ignore
+        def build_defs(self, _): ...
 
     c = load_component_for_test(
         ResolveFromComponent,
@@ -61,7 +62,7 @@ thing:
             ),
         ]
 
-        def build_defs(self, _): ...  # type: ignore
+        def build_defs(self, _): ...
 
     c = load_component_for_test(
         ResolveFromListComponent,
@@ -86,7 +87,7 @@ def test_class():
             self.thing = thing
             self.num = num
 
-        def build_defs(self, _):  # type: ignore
+        def build_defs(self, _):
             return []
 
     c = load_component_for_test(
@@ -99,3 +100,113 @@ thing:
     )
     assert c.thing.foo
     assert c.num == 123
+
+
+def test_union_resolvable():
+    class FooModel(Model):
+        foo: str
+
+    class BarModel(Model):
+        bar: str
+
+    @dataclass
+    class ResolveFromListComponent(Component, Resolvable):
+        thing: Union[FooModel, BarModel]
+
+        def build_defs(self, _): ...
+
+    c = load_component_for_test(
+        ResolveFromListComponent,
+        """
+thing:
+  foo: hi
+        """,
+    )
+    assert isinstance(c.thing, FooModel)
+    assert c.thing.foo == "hi"
+
+    c = load_component_for_test(
+        ResolveFromListComponent,
+        """
+thing:
+  bar: hello
+        """,
+    )
+    assert isinstance(c.thing, BarModel)
+    assert c.thing.bar == "hello"
+
+
+def test_union_resolvable_complex():
+    class FooModel(Model):
+        foo: str
+
+    # Test a nested model, in a sequence, with a custom resolver
+    class NumModel(Model):
+        num: Annotated[int, Resolver(lambda _, v: int(v), model_field_type=str)]
+
+    @dataclass
+    class ResolveFromListComponent(Component, Resolvable):
+        thing: Union[FooModel, Sequence[NumModel]]
+
+        def build_defs(self, _): ...
+
+    c = load_component_for_test(
+        ResolveFromListComponent,
+        """
+thing:
+  foo: hi
+        """,
+    )
+    assert isinstance(c.thing, FooModel)
+    assert c.thing.foo == "hi"
+
+    c = load_component_for_test(
+        ResolveFromListComponent,
+        """
+thing:
+  - num: '123'
+  - num: '456'
+        """,
+    )
+    assert isinstance(c.thing, list)
+    assert len(c.thing) == 2
+    assert c.thing[0].num == 123
+    assert c.thing[1].num == 456
+
+
+def test_union_resolvable_discriminator():
+    class FooModel(Model):
+        type: Literal["foo"] = "foo"
+        value: str
+
+    class BarModel(Model):
+        type: Literal["bar"] = "bar"
+        value: str
+
+    @dataclass
+    class ResolveFromListComponent(Component, Resolvable):
+        thing: Union[FooModel, BarModel]
+
+        def build_defs(self, _): ...
+
+    c = load_component_for_test(
+        ResolveFromListComponent,
+        """
+thing:
+  type: foo
+  value: hi
+        """,
+    )
+    assert isinstance(c.thing, FooModel)
+    assert c.thing.value == "hi"
+
+    c = load_component_for_test(
+        ResolveFromListComponent,
+        """
+thing:
+  type: bar
+  value: hello
+        """,
+    )
+    assert isinstance(c.thing, BarModel)
+    assert c.thing.value == "hello"
