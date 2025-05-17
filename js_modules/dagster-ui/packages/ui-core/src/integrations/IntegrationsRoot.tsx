ad5d8c3443a52427ba804f01471d3fe01a04ad79
bengotow <bgotow@elementl.com>
2025-05-17 10:21:45 -0500

    Additional fixes for use in dagster-internal

--- js_modules/dagster-ui/packages/ui-core/src/integrations/IntegrationsRoot.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/integrations/IntegrationsRoot.tsx
@@ -1,4 +1,4 @@
-import {Redirect, Route, Switch} from 'react-router';
+import {Redirect, Route, Switch} from 'react-router-dom';
 
 import {MarketplaceRoot} from './MarketplaceRoot';
 import {SingleIntegrationRoot} from './SingleIntegrationRoot';
