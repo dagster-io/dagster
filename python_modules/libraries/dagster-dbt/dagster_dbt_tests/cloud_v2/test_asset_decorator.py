81ab90686cb15575f8b175c2e2d918bbd2f635be
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-05-16 20:20:25 -0400

    [dagster-dbt] Raise custom exception on conflicting group_name in dbt Cloud asset decorator (#29990)
    ## Summary & Motivation
    
    Same as #29976 but for the new dbt Cloud integration
    
    ## How I Tested These Changes
    
    Additional tests with BK

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/cloud_v2/test_asset_decorator.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/cloud_v2/test_asset_decorator.py
@@ -3,7 +3,9 @@ from typing import Any, Optional, Union
 
 import pytest
 import responses
-from dagster import AssetExecutionContext, AssetKey, OpExecutionContext
+from dagster import AssetExecutionContext, AssetKey, AssetSpec, OpExecutionContext
+from dagster._core.errors import DagsterInvariantViolationError
+from dagster_dbt import DbtProject
 from dagster_dbt.asset_utils import build_dbt_specs
 from dagster_dbt.cloud_v2.asset_decorator import dbt_cloud_assets
 from dagster_dbt.cloud_v2.resources import DbtCloudWorkspace
@@ -58,6 +60,39 @@ def test_asset_defs_with_custom_metadata(
     asset_spec = next(iter(assets_def_specs))
     assert "custom" in asset_spec.metadata
     assert asset_spec.metadata["custom"] == "metadata"
+
+    # Clearing cache for other tests
+    workspace.load_specs.cache_clear()
+
+
+class MyCustomTranslatorWithGroupName(DagsterDbtTranslator):
+    def get_asset_spec(
+        self,
+        manifest: Mapping[str, Any],
+        unique_id: str,
+        project: Optional[DbtProject],
+    ) -> AssetSpec:
+        default_spec = super().get_asset_spec(
+            manifest=manifest, unique_id=unique_id, project=project
+        )
+        return default_spec.replace_attributes(group_name="my_group_name")
+
+
+def test_translator_invariant_group_name_with_asset_decorator(
+    workspace: DbtCloudWorkspace,
+    fetch_workspace_data_api_mocks: responses.RequestsMock,
+) -> None:
+    with pytest.raises(
+        DagsterInvariantViolationError,
+        match="Cannot set group_name parameter on dbt_cloud_assets",
+    ):
+
+        @dbt_cloud_assets(
+            workspace=workspace,
+            group_name="my_asset_decorator_group_name",
+            dagster_dbt_translator=MyCustomTranslatorWithGroupName(),
+        )
+        def my_dbt_cloud_assets(): ...
 
     # Clearing cache for other tests
     workspace.load_specs.cache_clear()
