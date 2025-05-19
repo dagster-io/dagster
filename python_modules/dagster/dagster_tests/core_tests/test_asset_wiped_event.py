25f8272e1270a892b0a84bccdd925be0ed311da1
JamieDeMaria <jamie@dagsterlabs.com>
2025-05-19 17:22:03 -0400

    test outline

---
+++ python_modules/dagster/dagster_tests/core_tests/test_asset_wiped_event.py
@@ -0,0 +1,3 @@
+# test that materializes some assets, then calls wipe_assets, then confirms that the ASSET_WIPED event is in the DB
+
+# test that does the same as ^ but with a partitioned asset. Confirm that the wiped partitions are in the event
