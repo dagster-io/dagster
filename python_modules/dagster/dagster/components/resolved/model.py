1f136c39003740712894ed14cb36574fd73fd020
Ben Pankow <ben@elementl.com>
2025-05-19 16:54:41 -0700

    [resolved] out-of-the-box support for union types (#30014)
    ## Summary
    
    Natively support `Union` types in `Resolvable` models, ended up being
    quite straightforward.
    
    ## Test Plan
    
    Unit tests.

--- python_modules/dagster/dagster/components/resolved/model.py
+++ python_modules/dagster/dagster/components/resolved/model.py
@@ -1,5 +1,8 @@
+import functools
 import sys
+import textwrap
 import traceback
+from collections.abc import Sequence
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Annotated, Any, Callable, Optional, TypeVar, Union
 
@@ -7,6 +10,7 @@ from pydantic import BaseModel, ConfigDict
 
 from dagster import _check as check
 from dagster._annotations import preview, public
+from dagster.components.resolved.errors import ResolutionException
 
 try:
     # this type only exists in python 3.10+
@@ -39,6 +43,30 @@ class AttrWithContextFn:
 default_resolver = AttrWithContextFn(
     lambda context, field_value: context.resolve_value(field_value)
 )
+
+
+def resolve_union(resolvers: Sequence["Resolver"], context: "ResolutionContext", field_value: Any):
+    """Resolve a union typed field by trying each resolver in order until one succeeds.
+    This attempts to mirror the behavior of the Union type in Pydantic using the left-to-right
+    strategy. If all resolvers fail, a ResolutionException is raised.
+    """
+    accumulated_errors = []
+    for r in resolvers:
+        try:
+            result = r.fn.callable(context, field_value)
+            if result is not None:
+                return result
+        except Exception:
+            accumulated_errors.append(traceback.format_exc())
+
+    raise ResolutionException(
+        "No resolver matched the field value"
+        + "\n"
+        + textwrap.indent(
+            "\n".join(accumulated_errors),
+            prefix="  ",
+        ),
+    )
 
 
 @public
@@ -82,6 +110,14 @@ class Resolver:
     def from_model(fn: Callable[["ResolutionContext", Any], Any], **kwargs):
         """Resolve this field by invoking the function which will receive the entire parent model."""
         return Resolver(ParentFn(fn), **kwargs)
+
+    @staticmethod
+    def union(*resolvers: "Resolver"):
+        field_types = tuple(r.model_field_type for r in resolvers)
+        return Resolver(
+            fn=functools.partial(resolve_union, resolvers),
+            model_field_type=Union[field_types],  # type: ignore
+        )
 
     @staticmethod
     def default(
