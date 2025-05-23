0edf8a1123d89c4fac684a10111646162339f4c8
Nick Schrock <schrockn@dagsterlabs.com>
2025-05-23 07:21:15 -0400

    Make @sensor loadable after scaffolding

--- python_modules/dagster/dagster_tests/components_tests/shim_components/test_sensor.py
+++ python_modules/dagster/dagster_tests/components_tests/shim_components/test_sensor.py
@@ -1,7 +1,9 @@
+from dagster._core.definitions.sensor_definition import SensorDefinition
 from dagster.components.lib.shim_components.sensor import SensorScaffolder
 
 from dagster_tests.components_tests.shim_components.shim_test_utils import (
     execute_ruff_compliance_test,
+    execute_scaffolder_and_get_symbol,
     make_test_scaffold_request,
 )
 
@@ -14,6 +16,10 @@ def test_sensor_scaffolder():
     assert isinstance(code, str)
     assert "sensor" in code
     assert "SensorEvaluationContext" in code
+
+    sensor_fn = execute_scaffolder_and_get_symbol(scaffolder, "my_sensor")
+    assert isinstance(sensor_fn, SensorDefinition)
+    assert sensor_fn.name == "my_sensor"
 
 
 def test_sensor_scaffolder_ruff_compliance():
