deaee2b5a3ce3daff35bdd313f87377fb81f2e23
Claire Lin <claire@dagsterlabs.com>
2025-05-27 22:43:27 -0700

    handle different unit types in rate card

--- js_modules/dagster-ui/packages/ui-core/src/assets/insights/AssetCatalogRateCard.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/assets/insights/AssetCatalogRateCard.tsx
@@ -9,6 +9,52 @@ export interface AssetCatalogRateCardProps {
   value: number | null; // e.g., 0.981 for 98.1%
   prevValue: number | null; // e.g., 1 for 100%
   loading: boolean;
+  unit: 'percent' | 'seconds';
+}
+
+function pct_change(x: number, y: number): number {
+  if (x === 0) {
+    if (y === 0) {
+      return 0.0; // No change
+    }
+    return Number.POSITIVE_INFINITY;
+  }
+
+  if (y > x) {
+    return Math.round(1000 * (y / x - 1)) / 10;
+  }
+  return Math.round(-(1000 * (1 - y / x))) / 10;
+}
+
+function formatValues(
+  valueOrNull: number | null,
+  prevValueOrNull: number | null,
+  unit: 'percent' | 'seconds',
+): {
+  currValueString: string;
+  prevValueString: string;
+  absDeltaString: string;
+  isDown: boolean;
+} {
+  let value = valueOrNull ?? 0;
+  let prevValue = prevValueOrNull ?? 0;
+
+  if (unit === 'percent') {
+    value = Math.round(value * 1000) / 10;
+    prevValue = Math.round(prevValue * 1000) / 10;
+  }
+
+  const deltaPct = pct_change(prevValue, value);
+  const isDown = deltaPct < 0;
+  const absDeltaPct = Math.abs(deltaPct);
+  const unitString = unit === 'percent' ? '%' : ' seconds';
+
+  return {
+    currValueString: numberFormatter.format(value) + unitString,
+    prevValueString: numberFormatter.format(prevValue) + unitString,
+    absDeltaString: numberFormatter.format(absDeltaPct) + '%', // delta is always in percent
+    isDown,
+  };
 }
 
 export function AssetCatalogRateCard({
@@ -16,12 +62,13 @@ export function AssetCatalogRateCard({
   value,
   prevValue,
   loading,
+  unit,
 }: AssetCatalogRateCardProps) {
-  const pct = value ? Math.round(value * 1000) / 10 : 0;
-  const prevPct = prevValue ? Math.round(prevValue * 1000) / 10 : 0;
-  const delta = pct - prevPct;
-  const isDown = delta < 0;
-  const absDelta = Math.abs(delta);
+  const {currValueString, prevValueString, absDeltaString, isDown} = formatValues(
+    value,
+    prevValue,
+    unit,
+  );
 
   const noDataAvailableCard = (
     <div className={styles.rateCardNoDataContainer}>
@@ -47,7 +94,7 @@ export function AssetCatalogRateCard({
     <div className={styles.rateCardContainer}>
       <BodyLarge>{title}</BodyLarge>
       {value !== null ? (
-        <div className={styles.rateCardValue}>{numberFormatter.format(pct) + '%'}</div>
+        <div className={styles.rateCardValue}>{currValueString}</div>
       ) : (
         noDataAvailableCard
       )}
@@ -66,9 +113,7 @@ export function AssetCatalogRateCard({
           prevValue !== null &&
           value !== null && (
             <>
-              <div className={styles.rateCardPrev}>
-                {numberFormatter.format(prevPct) + '% last period'}
-              </div>
+              <div className={styles.rateCardPrev}>{prevValueString + ' last period'}</div>
               <Box
                 className={styles.rateCardDeltaRow}
                 flex={{direction: 'row', alignItems: 'center', gap: 4}}
@@ -79,7 +124,7 @@ export function AssetCatalogRateCard({
                   size={16}
                 />
                 <span className={styles.rateCardDelta}>
-                  <>{numberFormatter.format(absDelta)}%</>
+                  <>{absDeltaString}</>
                 </span>
               </Box>
             </>
