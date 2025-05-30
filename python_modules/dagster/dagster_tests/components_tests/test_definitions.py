0e8db43cdfbc923af03a13ece3ac34d424b452f7
Alex Langenfeld <alex@elementl.com>
2025-05-30 09:12:53 -0500

    Reapply "[dg] Make dagster-test a dg project (#30022)" (#30044) (#30326)
    try again to have standing dg/components project in dagster-test
    
    removes the 
    ```
        # to be a dg-compatible project
        entry_points={
            "dagster_dg.plugin": [
                "dagster_test = dagster_test.components",
            ],
        },
    ```
    section that cause issues before
    
    
    ## How I Tested These Changes
    
    no_skip bk run

--- python_modules/dagster/dagster_tests/components_tests/test_definitions.py
+++ python_modules/dagster/dagster_tests/components_tests/test_definitions.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 from typing import TYPE_CHECKING, cast
 
 import pytest
 from dagster import AssetSpec, ComponentLoadContext, Definitions
 from dagster._core.errors import DagsterInvariantViolationError
+from dagster._core.remote_representation.external_data import RepositorySnap
+from dagster.components.core.tree import ComponentTree
 from dagster.components.definitions import definitions
 from dagster_shared import check
 
@@ -90,3 +93,20 @@ def test_definitions_decorator_with_context_using_context():
     assert len(assets) == 1
 
     assert check.inst(next(iter(assets)), AssetSpec).key.path[0] == "asset1"
+
+
+def test_component_tree():
+    dagster_test_path = Path(__file__).joinpath("../../../../dagster-test").resolve()
+    assert dagster_test_path.exists()
+    defs = ComponentTree.load(dagster_test_path).load_defs()
+
+    repo_snap = RepositorySnap.from_def(defs.get_repository_def())
+    assert repo_snap.component_tree
+
+    inst_map = {snap.key: snap.full_type_name for snap in repo_snap.component_tree.leaf_instances}
+    assert inst_map == {
+        "composites/python[first]": "dagster_test.dg_defs.composites.python.component.PyComponent",
+        "composites/python[second]": "dagster_test.dg_defs.composites.python.component.PyComponent",
+        "composites/yaml[0]": "dagster_test.components.simple_asset.SimpleAssetComponent",
+        "composites/yaml[1]": "dagster_test.components.simple_asset.SimpleAssetComponent",
+    }
