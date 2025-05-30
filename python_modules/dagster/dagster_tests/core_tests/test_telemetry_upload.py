fefb7881412ec99a8baf36c83efcb2373376e7b4
gibsondan <daniel@elementl.com>
2025-05-30 11:09:08 -0500

    Don't propagate the telemetry logger to the root logger (#30334)
    ## Summary & Motivation
    Without this, if anybody adjusts the log level of the root logger, this logger might also write to stdout.
    
    ## How I Tested These Changes
    With dbt installed, dg list defs no longer prints telemetry logger output
    
    ## Changelog
    Fixed an issue where `dg` commands would sometimes output extra dagster_telemetry_logger ines to stdout at the end of commands.

--- python_modules/dagster/dagster_tests/core_tests/test_telemetry_upload.py
+++ python_modules/dagster/dagster_tests/core_tests/test_telemetry_upload.py
@@ -6,10 +6,10 @@ import pytest
 import responses
 from click.testing import CliRunner
 from dagster._cli.job import job_execute_command
-from dagster._core.telemetry import cleanup_telemetry_logger
 from dagster._core.telemetry_upload import get_dagster_telemetry_url, upload_logs
 from dagster._core.test_utils import environ, instance_for_test
 from dagster._utils import pushd, script_relative_path
+from dagster_shared.telemetry import cleanup_telemetry_logger
 
 
 def path_to_file(path):
