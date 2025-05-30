ade31ded147e473e623774f8bfbe7e1b0942ee6d
benpankow <ben@dagsterlabs.com>
2025-05-30 09:29:24 -0700

    fix

--- python_modules/dagster/dagster_tests/components_tests/integration_tests/integration_test_defs/definitions/special_names_at_levels/folder_with_component_py/component.py
+++ python_modules/dagster/dagster_tests/components_tests/integration_tests/integration_test_defs/definitions/special_names_at_levels/folder_with_component_py/component.py
@@ -1,4 +1,4 @@
-from dagster import Component, ComponentLoadContext, component
+from dagster import Component, ComponentLoadContext, component_instance
 from dagster._core.definitions.decorators.asset_decorator import asset
 from dagster._core.definitions.definitions_class import Definitions
 
@@ -13,7 +13,7 @@ class AComponent(Component):
         return Definitions(assets=[asset_in_component_py])
 
 
-@component
+@component_instance
 def load(context: ComponentLoadContext) -> Component:
     """A component that loads a component from the same module."""
     return AComponent()
