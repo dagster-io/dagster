27ea93a23db90e25e36f9f86f2caa76c4da2ba8a
benpankow <ben@dagsterlabs.com>
2025-05-30 13:45:05 -0700

    cleanup posix comparison

--- python_modules/dagster/dagster/components/core/tree.py
+++ python_modules/dagster/dagster/components/core/tree.py
@@ -57,13 +57,22 @@ class ComponentTree:
         )
 
     def load_defs_at_path(self, defs_path: Path) -> Definitions:
+        """Loads definitions from the given defs subdirectory. Currently
+        does not incorporate postprocessing from parent defs modules.
+
+        Args:
+            defs_path: Path to the defs module to load. If relative, resolves relative to the defs root.
+
+        Returns:
+            Definitions: The definitions loaded from the given path.
+        """
         # impl to be fleshed out to flexibly handle different path types (str, list[str], ...)
         if self.root.path.absolute().as_posix() == defs_path.absolute().as_posix():
             return self.root.build_defs(self.load_context.for_path(self.root.path))
         for cp, c in self.root.iterate_path_component_pairs():
             if (
-                cp.file_path.absolute().as_posix() == defs_path.absolute().as_posix()
-                or cp.file_path.relative_to(self.root.path) == defs_path
+                cp.file_path.absolute().as_posix()
+                == (self.root.path / defs_path).absolute().as_posix()
             ):
                 ctx = self.load_context.for_path(cp.file_path)
                 return c.build_defs(ctx)
