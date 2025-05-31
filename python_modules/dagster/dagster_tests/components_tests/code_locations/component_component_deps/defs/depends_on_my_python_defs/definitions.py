154ddec15fd4cdafa65804d670a80de3f9ffebca
Nick Schrock <schrockn@dagsterlabs.com>
2025-05-31 17:58:14 -0400

    cp

--- python_modules/dagster/dagster_tests/components_tests/code_locations/component_component_deps/defs/depends_on_my_python_defs/definitions.py
+++ python_modules/dagster/dagster_tests/components_tests/code_locations/component_component_deps/defs/depends_on_my_python_defs/definitions.py
@@ -1,10 +1,6 @@
-from typing import TYPE_CHECKING, cast
-
 import dagster as dg
 from dagster.components.core.context import ComponentLoadContext
-
-if TYPE_CHECKING:
-    from collections.abc import Sequence
+from dagster_shared import check
 
 
 @dg.definitions
@@ -12,10 +8,7 @@ def defs(context: ComponentLoadContext) -> dg.Definitions:
     from component_component_deps.defs import my_python_defs  # type:ignore
 
     @dg.asset(
-        deps=cast(
-            "Sequence[dg.AssetsDefinition]",
-            context.load_defs(my_python_defs).assets,
-        )
+        deps=check.is_list(context.load_defs(my_python_defs).assets, of_type=dg.AssetsDefinition)
     )
     def downstream_of_all_my_python_defs(): ...
 
