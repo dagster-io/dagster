e93cea516cf377081448e33c4f638c2560d6d5ab
Daniel Gibson <daniel@elementl.com>
2025-06-02 16:40:44 -0500

    Add create-dagster cli section to docs alongside dg cli

--- docs/scripts/build-api-docs.sh
+++ docs/scripts/build-api-docs.sh
@@ -39,6 +39,8 @@ if [ "$VERCEL" = "1" ]; then
   cp -rf sphinx/_build/mdx/sections/api/apidocs/dagster docs/api
   cp -rf sphinx/_build/mdx/sections/api/apidocs/libraries docs/api
   cp -rf sphinx/_build/mdx/sections/api/apidocs/dagster-dg-cli.mdx docs/guides/labs/dg
+  cp -rf sphinx/_build/mdx/sections/api/apidocs/create-dagster.mdx docs/guides/labs/dg
+
 
   # Parallelize production sphinx-inv build -- see tox.ini
   echo "Running sphinx and copying \`object.inv\` to \`static/\`"
@@ -51,7 +53,8 @@ else
   cp -rf sphinx/_build/mdx/sections/api/apidocs/dagster docs/api
   cp -rf sphinx/_build/mdx/sections/api/apidocs/libraries docs/api
   cp -rf sphinx/_build/mdx/sections/api/apidocs/dagster-dg-cli.mdx docs/guides/labs/dg
-  
+  cp -rf sphinx/_build/mdx/sections/api/apidocs/create-dagster.mdx docs/guides/labs/dg
+
   # Do not parallelize local sphinx-inv build -- see tox.ini
   echo "Running sphinx and copying \`object.inv\` to \`static/\`"
   tox -e sphinx-inv-local
