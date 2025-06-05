cd4a3c5a8a1dfab1a550e123f050b19834267caf
Daniel Gibson <daniel@elementl.com>
2025-06-05 16:02:30 -0500

    Add automation condition to AssetSpec docstring.
    Summary:
    Unless this was intentionally omited for some resaon, this just seems like an oversight.
    
    Eyes
    
    > Insert changelog entry or delete this section.

--- python_modules/dagster/dagster/_core/definitions/asset_spec.py
+++ python_modules/dagster/dagster/_core/definitions/asset_spec.py
@@ -143,6 +143,8 @@ class AssetSpec(IHasInternalInit, IHaveNew, LegacyNamedTupleMixin):
         owners (Optional[Sequence[str]]): A list of strings representing owners of the asset. Each
             string can be a user's email address, or a team name prefixed with `team:`,
             e.g. `team:finops`.
+        automation_condition (Optional[AutomationCondition]): The automation condition to apply to
+            the asset.
         tags (Optional[Mapping[str, str]]): Tags for filtering and organizing. These tags are not
             attached to runs of the asset.
         kinds: (Optional[Set[str]]): A set of strings representing the kinds of the asset. These
