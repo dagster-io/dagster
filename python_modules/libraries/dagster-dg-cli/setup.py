8b31f995905a04c1b42d4b0bf72d78088d2359fd
elementl <devtools@elementl.com>
2025-06-05 17:32:33 +0000

    1.10.19

--- python_modules/libraries/dagster-dg-cli/setup.py
+++ python_modules/libraries/dagster-dg-cli/setup.py
@@ -35,7 +35,7 @@ setup(
     packages=find_packages(exclude=["dagster_dg_cli_tests*"]),
     include_package_data=True,
     install_requires=[
-        f"dagster-dg-core{pin}",
+        "dagster-dg-core==1.10.19",
     ],
     entry_points={
         "console_scripts": [
@@ -44,7 +44,7 @@ setup(
     },
     extras_require={
         "local": [
-            f"dagster=={ver}",
+            f"dagster==1.10.19",
         ],
         "mcp": [
             "mcp",
--- python_modules/libraries/dagster-pyspark/setup.py
+++ python_modules/libraries/dagster-pyspark/setup.py
@@ -34,8 +34,8 @@ setup(
     include_package_data=True,
     python_requires=">=3.9,<3.13",
     install_requires=[
-        f"dagster{pin}",
-        f"dagster_spark{pin}",
+        "dagster==1.10.19",
+        "dagster_spark==0.26.19",
         "pyspark>=3,<4",
     ],
     zip_safe=False,
