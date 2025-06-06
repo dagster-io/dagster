71efa1a675e4e72180f56466f5cf755cb4bd7f1a
benpankow <ben@dagsterlabs.com>
2025-06-06 12:07:09 -0700

    add passthru to ctx

--- python_modules/dagster/dagster/components/core/defs_module.py
+++ python_modules/dagster/dagster/components/core/defs_module.py
@@ -169,7 +169,7 @@ def get_component(context: ComponentLoadContext) -> Optional[Component]:
 
     component_decl = build_component_decl_from_context(context)
     if component_decl:
-        return context.component_tree.load_component_at_path(component_decl.path)
+        return context.load_component_at_path(component_decl.path)
     return None
 
 
@@ -400,7 +400,7 @@ def load_yaml_component_from_path(context: ComponentLoadContext, component_def_p
     from dagster.components.core.decl import build_component_decl_from_yaml_file
 
     decl = build_component_decl_from_yaml_file(context, component_def_path)
-    return context.component_tree.load_component_at_path(decl.path)
+    return context.load_component_at_path(decl.path)
 
 
 # When we remove component.yaml, we can remove this function for just a defs.yaml check
