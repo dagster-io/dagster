705a7df9c3c0957e7dea6ee11ed2f2039e3a30c5
benpankow <ben@dagsterlabs.com>
2025-06-06 12:07:09 -0700

    add passthru to ctx

--- python_modules/dagster/dagster/components/core/decl.py
+++ python_modules/dagster/dagster/components/core/decl.py
@@ -83,7 +83,7 @@ class CompositePythonDecl(ComponentDecl[CompositeComponent]):
     def _load_component(self) -> "CompositeComponent":
         return CompositeComponent(
             components={
-                attr: self.context.component_tree.load_component_at_path(decl.path)
+                attr: self.context.load_component_at_path(decl.path)
                 for attr, decl in self.decls.items()
             }
         )
@@ -186,9 +186,7 @@ class YamlFileDecl(ComponentDecl[CompositeYamlComponent]):
 
     def _load_component(self) -> "CompositeYamlComponent":
         return CompositeYamlComponent(
-            components=[
-                self.context.component_tree.load_component_at_path(decl.path) for decl in self.decls
-            ],
+            components=[self.context.load_component_at_path(decl.path) for decl in self.decls],
             source_positions=self.source_positions,
         )
 
@@ -247,7 +245,7 @@ class DefsFolderDecl(ComponentDecl[DefsFolderComponent]):
         return DefsFolderComponent(
             path=self.path.file_path,
             children={
-                subpath: self.context.component_tree.load_component_at_path(decl.path)
+                subpath: self.context.load_component_at_path(decl.path)
                 for subpath, decl in self.children.items()
             },
             asset_post_processors=resolved_attributes.asset_post_processors
