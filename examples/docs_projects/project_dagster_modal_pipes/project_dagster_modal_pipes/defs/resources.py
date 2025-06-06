1af5df33f2173d3249007707980345d9ae26e432
Nick Schrock <schrockn@dagsterlabs.com>
2025-06-06 13:23:42 -0400

    Full dg-ification

---
+++ examples/docs_projects/project_dagster_modal_pipes/project_dagster_modal_pipes/defs/resources.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import dagster as dg
+from dagster_aws.s3 import S3Resource
+from dagster_modal import ModalClient
+from dagster_openai import OpenAIResource
+
+
+@dg.definitions
+def resources() -> dg.Definitions:
+    return dg.Definitions(
+        resources={
+            "s3": S3Resource(
+                endpoint_url=dg.EnvVar("CLOUDFLARE_R2_API"),
+                aws_access_key_id=dg.EnvVar("CLOUDFLARE_R2_ACCESS_KEY_ID"),
+                aws_secret_access_key=dg.EnvVar("CLOUDFLARE_R2_SECRET_ACCESS_KEY"),
+                region_name="auto",
+            ),
+            "modal": ModalClient(project_directory=Path(__file__).parent.parent),
+            "openai": OpenAIResource(api_key=dg.EnvVar("OPENAI_API_KEY")),
+        }
+    )
