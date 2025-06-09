b602d1344858c82099992e7280d02845ec33d2e2
Nick Schrock <schrockn@dagsterlabs.com>
2025-06-09 08:54:52 -0700

    cp

--- python_modules/dagster/dagster/_core/execution/plan/compute_generator.py
+++ python_modules/dagster/dagster/_core/execution/plan/compute_generator.py
@@ -105,10 +105,9 @@ def invoke_compute_fn(
     if config_arg_cls:
         # config_arg_cls is either a Config class or a primitive type
         if issubclass(config_arg_cls, Config):
-            to_pass = config_arg_cls._get_non_default_public_field_values_cls(  # noqa: SLF001
-                context.op_execution_context.op_config
+            args_to_pass["config"] = construct_config_from_context(
+                config_arg_cls, context.op_execution_context
             )
-            args_to_pass["config"] = config_arg_cls(**to_pass)
         else:
             args_to_pass["config"] = context.op_execution_context.op_config
     if resource_args:
