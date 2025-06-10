69eb6fb204b30f038e2e8a04e07ebc0e7991a987
Sean Mackesey <s.mackesey@gmail.com>
2025-06-10 15:49:18 -0400

    [dg] Remove cache (#30618)
    ## Summary & Motivation
    
    Code for the `dg` remains in place, but it isn't used anywhere since the
    transition to a single-process architecture.
    
    Though it is very possible we will reintroduce some form of caching in
    the future, we can safely remove this for now. If we readd the cache
    this PR can be looked at for inspiration.
    
    Note that this also removes several user-facing CLI and config file
    options.
    
    ## How I Tested These Changes
    
    Existing test suite.
    
    ## Changelog
    
    The `dg` cache is no longer operative, since `dg` now operates in the
    same python environment as the projects it manipulates. Config options
    for the cache have been removed.

--- python_modules/libraries/dagster-dg-core/dagster_dg_core/shared_options.py
+++ python_modules/libraries/dagster-dg-core/dagster_dg_core/shared_options.py
@@ -51,17 +51,6 @@ GLOBAL_OPTIONS = {
     not_none(option.name): option
     for option in [
         click.Option(
-            ["--cache-dir"],
-            default=str(DgCliConfig.cache_dir),
-            help="Specify a directory to use for the cache.",
-        ),
-        click.Option(
-            ["--disable-cache"],
-            is_flag=True,
-            default=DgCliConfig.disable_cache,
-            help="Disable the cache..",
-        ),
-        click.Option(
             ["--verbose"],
             is_flag=True,
             default=DgCliConfig.verbose,
