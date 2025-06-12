e9b29edda2fbe67b8bbfd0af02e1d26a6f09c96d
Alex Noonan <48368867+C00ldudeNoonan@users.noreply.github.com>
2025-06-12 15:28:05 -0400

    Revert "Add MLOps demo (#30587)"
    This reverts commit f2fcc1b1d7ea1e7423fde9e24336e826e60c2356.

--- examples/mlops-demo/mlops_demo/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="mlops_demo",
-    packages=find_packages(exclude=["mlops_demo_tests"]),
-    install_requires=["dagster", "dagster-cloud"],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
-)
