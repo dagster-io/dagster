da04b6561e794b25581db6a2339925d86f0724dc
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-06-12 14:48:09 -0400

    [dagster-fivetran] Move snapshot_path to FivetranWorkspace (#30663)
    ## Summary & Motivation
    
    `snapshot_path` is now passed to FivetranWorkspace instead. This allows
    to load the snapshot behind the scenes whenever users are using
    `load_asset_specs`. Subsequent PR to also support the snapshot when
    using `get_or_fetch_workspace_data`
    
    ## How I Tested These Changes
    
    Updated tests with BK

--- python_modules/libraries/dagster-fivetran/dagster_fivetran_tests/pending_repo_snapshot.py
+++ python_modules/libraries/dagster-fivetran/dagster_fivetran_tests/pending_repo_snapshot.py
@@ -5,11 +5,15 @@ from dagster_fivetran import FivetranWorkspace, load_fivetran_asset_specs
 
 from dagster_fivetran_tests.conftest import TEST_ACCOUNT_ID, TEST_API_KEY, TEST_API_SECRET
 
+snapshot_path = os.getenv("FIVETRAN_SNAPSHOT_PATH")
+
 workspace = FivetranWorkspace(
-    account_id=TEST_ACCOUNT_ID, api_key=TEST_API_KEY, api_secret=TEST_API_SECRET
+    account_id=TEST_ACCOUNT_ID,
+    api_key=TEST_API_KEY,
+    api_secret=TEST_API_SECRET,
+    snapshot_path=snapshot_path,
 )
 
-snapshot_path = os.getenv("FIVETRAN_SNAPSHOT_PATH")
-fivetran_specs = load_fivetran_asset_specs(workspace=workspace, snapshot_path=snapshot_path)
+fivetran_specs = load_fivetran_asset_specs(workspace=workspace)
 
 defs = Definitions(assets=fivetran_specs)
