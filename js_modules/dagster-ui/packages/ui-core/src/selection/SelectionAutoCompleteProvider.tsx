74c5adfb161f00c1a6d38ccecb2434ba74095412
Marco polo <salazarm@users.noreply.github.com>
2025-06-13 10:33:59 -0400

    Treat * as wildcard in auto-complete results for key attribute (#30677)
    ## Summary & Motivation
    As titled
    
    ## How I Tested These Changes
    jest + manual testing
    
    <img width="628" alt="Screenshot 2025-06-12 at 5 15 49 PM"
    src="https://github.com/user-attachments/assets/74fb398f-9347-4bbe-8e7f-12d20aa00202"
    />

--- js_modules/dagster-ui/packages/ui-core/src/selection/SelectionAutoCompleteProvider.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/selection/SelectionAutoCompleteProvider.tsx
@@ -376,9 +376,20 @@ export const createProvider = <
     },
     getAttributeValueResultsMatchingQuery: ({attribute, query, textCallback}) => {
       const values = attributesMap[attribute as keyof typeof attributesMap];
+      const shouldTreatAsteriskAsWildcard = attribute === primaryAttributeKey;
+      const queryToUseAsRegex = shouldTreatAsteriskAsWildcard ? query.replace(/\*/g, '.*') : query;
+      const regex = new RegExp(queryToUseAsRegex, 'i');
       const results =
         values
-          ?.filter((value) => doesValueIncludeQuery({value, query}))
+          ?.filter((value) => {
+            if (shouldTreatAsteriskAsWildcard) {
+              if (typeof value === 'string') {
+                return regex.test(value);
+              }
+              return regex.test(value.key) || (value.value && regex.test(value.value));
+            }
+            return doesValueIncludeQuery({value, query});
+          })
           .map((value) =>
             createAttributeValueSuggestion({
               value,
