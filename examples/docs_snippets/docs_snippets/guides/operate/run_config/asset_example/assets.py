75ef6b2869b9c07123b962183d08d45f174d5d70
nikki everett <nikki@dagsterlabs.com>
2025-06-13 12:35:26 -0500

    update examples for run config doc
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

---
+++ examples/docs_snippets/docs_snippets/guides/operate/run_config/asset_example/assets.py
@@ -0,0 +1,7 @@
+import dagster as dg
+from .resources import MyAssetConfig
+
+
+@dg.asset
+def greeting(config: MyAssetConfig) -> str:
+    return f"hello {config.person_name}"
---
+++ examples/docs_snippets/docs_snippets/guides/operate/run_config/asset_example/resources.py
@@ -0,0 +1,13 @@
+import dagster as dg
+
+class MyAssetConfig(dg.Config):
+    person_name: str
+
+
+@dg.definitions
+def resources() -> dg.Definitions:
+    return dg.Definitions(
+        resources = {
+            "config": MyAssetConfig(person_name="")
+        }
+    )
