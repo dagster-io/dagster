9f4930f19e29f16590114ae05ed7afb3e57fc660
Dennis Hume <dennis@dagsterlabs.com>
2025-06-16 09:56:22 -0500

    Fix test

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_jobs_to_definitions.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_jobs_to_definitions.py
@@ -1,8 +1,14 @@
 import dagster as dg
-from docs_snippets.concepts.assets.jobs_to_definitions import number_asset_job
+from docs_snippets.concepts.assets.jobs_to_definitions import (
+    number_asset,
+    number_asset_job,
+)
 
 
 def test_build_job_doc_snippet():
-    defs = dg.Definitions(jobs=[number_asset_job])
+    defs = dg.Definitions(
+        assets=[number_asset],
+        jobs=[number_asset_job],
+    )
 
     assert defs.get_job_def("number_asset_job").execute_in_process().success
