d83b0b35a38c8be1ef773b39d46bdd89c0a88f4b
Dennis Hume <dennis@dagsterlabs.com>
2025-06-16 09:42:46 -0500

    Tests

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_jobs_to_definitions.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_jobs_to_definitions.py
@@ -1,5 +1,8 @@
+import dagster as dg
 from docs_snippets.concepts.assets.jobs_to_definitions import number_asset_job
 
 
 def test_build_job_doc_snippet():
-    assert number_asset_job.execute_in_process().success
+    defs = dg.Definitions(jobs=[number_asset_job])
+
+    assert defs.get_job_def("number_asset_job").execute_in_process().success
