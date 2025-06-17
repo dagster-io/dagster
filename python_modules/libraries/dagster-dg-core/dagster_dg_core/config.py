1d023162bceec64a8f5952c33228fef155d8c541
benpankow <ben@dagsterlabs.com>
2025-06-17 10:26:58 -0400

    rm warn

--- python_modules/libraries/dagster-dg-core/dagster_dg_core/config.py
+++ python_modules/libraries/dagster-dg-core/dagster_dg_core/config.py
@@ -1,6 +1,5 @@
 import functools
 import textwrap
-import warnings
 from collections.abc import Iterator, Mapping, Sequence
 from contextlib import contextmanager
 from dataclasses import dataclass, field
@@ -239,11 +238,6 @@ class DgProjectConfig:
 
     @classmethod
     def from_raw(cls, raw: "DgRawProjectConfig") -> Self:
-        if raw.get("autoload_defs"):
-            warnings.warn(
-                "Using autoload_defs in pyproject.toml is deprecated."
-                " Use a definitions.py file with @definitions and load_defs_folder instead."
-            )
         return cls(
             root_module=raw["root_module"],
             autoload_defs=raw.get("autoload_defs", DgProjectConfig.autoload_defs),
