4bb3768c764fe79585c3d7835436e2f987d7dcb1
Daniel Gibson <daniel@elementl.com>
2025-06-17 13:50:55 -0500

    Source StaleStatusLoader from the workspace context instead of passing it into GrapheneAssetNdoe
    Summary:
    Similar to https://github.com/dagster-io/dagster/pull/29717/files but for this other field. Makes it easier to construct a new GrapheneAssetNode without having to pass in a bunch of stuff.
    
    BK

--- python_modules/dagster-graphql/dagster_graphql/schema/external.py
+++ python_modules/dagster-graphql/dagster_graphql/schema/external.py
@@ -21,7 +21,7 @@ from dagster._core.workspace.workspace import CodeLocationEntry, CodeLocationLoa
 from dagster.components.core.load_defs import PLUGIN_COMPONENT_TYPES_JSON_METADATA_KEY
 
 from dagster_graphql.implementation.fetch_solids import get_solid, get_solids
-from dagster_graphql.implementation.loader import RepositoryScopedBatchLoader, StaleStatusLoader
+from dagster_graphql.implementation.loader import RepositoryScopedBatchLoader
 from dagster_graphql.implementation.utils import capture_error
 from dagster_graphql.schema.asset_graph import GrapheneAssetGroup, GrapheneAssetNode
 from dagster_graphql.schema.env_vars import (
@@ -385,16 +385,9 @@ class GrapheneRepository(graphene.ObjectType):
     def resolve_assetNodes(self, graphene_info: ResolveInfo):
         remote_nodes = self.get_repository(graphene_info).asset_graph.asset_nodes
 
-        stale_status_loader = StaleStatusLoader(
-            instance=graphene_info.context.instance,
-            asset_graph=lambda: self.get_repository(graphene_info).asset_graph,
-            loading_context=graphene_info.context,
-        )
-
         return [
             GrapheneAssetNode(
                 remote_node=remote_node,
-                stale_status_loader=stale_status_loader,
             )
             for remote_node in remote_nodes
         ]
