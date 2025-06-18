f9690646bfff9c63e4c4f855475a7ed58815c6f7
nikki everett <nikki@dagsterlabs.com>
2025-06-18 15:40:27 -0500

    update example
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

--- examples/docs_snippets/docs_snippets/guides/operate/configuration/env_vars_and_secrets/per_env_config.py
+++ examples/docs_snippets/docs_snippets/guides/operate/configuration/env_vars_and_secrets/per_env_config.py
@@ -1,36 +1,33 @@
 import os
 
 import dagster as dg
-
 from dagster_snowflake_pandas import SnowflakePandasIOManager
+
+
+def resources_by_deployment() -> dict:
+    return {
+        "local": {
+            "snowflake_io_manager": SnowflakePandasIOManager(
+                account="abc1234.us-east-1",
+                user=dg.EnvVar("DEV_SNOWFLAKE_USER"),
+                password=dg.EnvVar("DEV_SNOWFLAKE_PASSWORD"),
+                database="LOCAL",
+                schema=dg.EnvVar("DEV_SNOWFLAKE_SCHEMA"),
+            ),
+        },
+        "production": {
+            "snowflake_io_manager": SnowflakePandasIOManager(
+                account="abc1234.us-east-1",
+                user="system@company.com",
+                password=dg.EnvVar("SYSTEM_SNOWFLAKE_PASSWORD"),
+                database="PRODUCTION",
+                schema="HACKER_NEWS",
+            ),
+        },
+    }
 
 
 @dg.definitions
 def resources() -> dg.Definitions:
-    return dg.Definitions(
-        resources={
-            "local": {
-                "snowflake_io_manager": SnowflakePandasIOManager(
-                    account="abc1234.us-east-1",
-                    user=dg.EnvVar("DEV_SNOWFLAKE_USER"),
-                    password=dg.EnvVar("DEV_SNOWFLAKE_PASSWORD"),
-                    database="LOCAL",
-                    schema=dg.EnvVar("DEV_SNOWFLAKE_SCHEMA"),
-                ),
-            },
-            "production": {
-                "snowflake_io_manager": SnowflakePandasIOManager(
-                    account="abc1234.us-east-1",
-                    user="system@company.com",
-                    password=dg.EnvVar("SYSTEM_SNOWFLAKE_PASSWORD"),
-                    database="PRODUCTION",
-                    schema="HACKER_NEWS",
-                ),
-            },
-        }
-    )
-
-
-deployment_name = os.getenv("DAGSTER_DEPLOYMENT", "local")
-
-defs = dg.Definitions(resources=resources[deployment_name])
+    deployment_name = os.getenv("DAGSTER_DEPLOYMENT", "local")
+    return dg.Definitions(resources=resources_by_deployment()[deployment_name])
