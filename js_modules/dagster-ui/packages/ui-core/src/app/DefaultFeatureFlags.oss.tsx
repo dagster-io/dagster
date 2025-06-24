d075fb3ca725e541df2cac11d1f2f2004dcd83e2
benpankow <ben@dagsterlabs.com>
2025-06-24 09:41:33 -0700

    [ui] enable in-app docs by default

--- js_modules/dagster-ui/packages/ui-core/src/app/DefaultFeatureFlags.oss.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/DefaultFeatureFlags.oss.tsx
@@ -6,6 +6,7 @@ import {FeatureFlag} from 'shared/app/FeatureFlags.oss';
 export const DEFAULT_FEATURE_FLAG_VALUES: Partial<Record<FeatureFlag, boolean>> = {
   [FeatureFlag.flagAssetSelectionWorker]: true,
   [FeatureFlag.flagAssetNodeFacets]: true,
+  [FeatureFlag.flagDocsInApp]: true,
 
   // Flags for tests
   [FeatureFlag.__TestFlagDefaultTrue]: true,
