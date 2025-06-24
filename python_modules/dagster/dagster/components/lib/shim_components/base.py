b515ebb9b59897ac698d622be6d779aedce7e830
Nick Schrock <schrockn@dagsterlabs.com>
2025-06-24 12:13:33 -0400

    Update base.py

--- python_modules/dagster/dagster/components/lib/shim_components/base.py
+++ python_modules/dagster/dagster/components/lib/shim_components/base.py
@@ -12,7 +12,7 @@ class ShimScaffolder(Scaffolder[TModel]):
             raise ValueError("Invalid target path suffix. Expected a path ending in `.py`.")
         # temporary hack as currently all scaffold requests target directories
         # that are auto-created
-        # TODO this seems extremely dangerous -- schrockn 06-24-2025
+        # Note: rmdir() only removes empty directories, so this is safe - it will error if dir contains files
         # https://linear.app/dagster-labs/issue/ADOPT-1614/bad-error-and-scary-code-when-scaffolding-shim-components
         request.target_path.rmdir()
         request.target_path.write_text(self.get_text(request))
