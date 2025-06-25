df7f82d45b12e9b4c1a9d3d855892295d4821fd1
Anurag Anjaria <anuthebananu@users.noreply.github.com>
2025-06-24 20:09:32 -0400

    Update freshness docs code samples for `1.11` (#30862)
    ## Summary & Motivation
    Update freshness docs code samples to be consistent with our recommended imports and usage for `1.11`.
    
    - `from dagster._core.definitions.freshness import InternalFreshnessPolicy` -> `from dagster.preview.freshness import FreshnessPolicy`
    - `from dagster._core.definitions.asset_spec import attach_internal_freshness_policy` -> `from dagster.preview.freshness import apply_freshness_policy`
    
    ## How I Tested These Changes
    
    ## Changelog
    
    > Insert changelog entry or delete this section.

--- examples/docs_snippets/docs_snippets/guides/freshness/multiple_assets_policy.py
+++ examples/docs_snippets/docs_snippets/guides/freshness/multiple_assets_policy.py
@@ -1,8 +1,7 @@
 from datetime import timedelta
 
 from dagster import Definitions, asset
-from dagster._core.definitions.asset_spec import attach_internal_freshness_policy
-from dagster._core.definitions.freshness import InternalFreshnessPolicy
+from dagster.preview.freshness import FreshnessPolicy, apply_freshness_policy
 
 
 @asset
@@ -20,17 +19,15 @@ def asset_2():
     pass
 
 
-policy = InternalFreshnessPolicy.time_window(fail_window=timedelta(hours=24))
+policy = FreshnessPolicy.time_window(fail_window=timedelta(hours=24))
 
 defs = Definitions(assets=[parent_asset, child_asset, asset_2])
 
 # Apply the policy to multiple assets - in this case, all assets in defs
-defs = defs.map_asset_specs(
-    func=lambda spec: attach_internal_freshness_policy(spec, policy)
-)
+defs = defs.map_asset_specs(func=lambda spec: apply_freshness_policy(spec, policy))
 
 # Use map_resolved_asset_specs to apply the policy to a selection
 defs = defs.map_resolved_asset_specs(
-    func=lambda spec: attach_internal_freshness_policy(spec, policy),
+    func=lambda spec: apply_freshness_policy(spec, policy),
     selection='key:"parent_asset"+',  # will apply policy to parent_asset and its downstream dependencies
 )
