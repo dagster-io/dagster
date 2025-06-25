641d4fbdd9da414f94bc434515571e04171e3941
OwenKephart <owen@elementl.com>
2025-06-25 11:00:32 -0700

    [freshness] Resolver for freshnessStatusInfo (#30881)
    ## Summary & Motivation
    
    Adds a resolver that does not rely on AssetHealth to fetch freshness status info.
    
    We should consider if we can rework how the AssetHealthStatus graphql layer works so that it can be more compatible with instances that don't support all of those queries, but for now we'll special-case this path
    
    ## How I Tested These Changes
    
    ## Changelog
    
    NOCHANGELOG

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_internal_freshness.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_internal_freshness.py
@@ -2,11 +2,16 @@ from datetime import timedelta
 
 from dagster._core.definitions.decorators.asset_decorator import asset
 from dagster._core.definitions.definitions_class import Definitions
-from dagster._core.definitions.freshness import InternalFreshnessPolicy
+from dagster._core.definitions.freshness import (
+    FreshnessState,
+    FreshnessStateChange,
+    InternalFreshnessPolicy,
+)
 from dagster._core.definitions.repository_definition.repository_definition import (
     RepositoryDefinition,
 )
 from dagster._core.test_utils import instance_for_test
+from dagster._time import get_current_timestamp
 from dagster_graphql.test.utils import define_out_of_process_context, execute_dagster_graphql
 
 GET_INTERNAL_FRESHNESS_POLICY = """
@@ -21,6 +26,21 @@ query GetInternalFreshnessPolicy($assetKey: AssetKeyInput!) {
                 deadlineCron
                 lowerBoundDeltaSeconds
                 timezone
+            }
+        }
+    }
+}
+"""
+
+GET_FRESHNESS_STATUS_INFO = """
+query GetFreshnessStatusInfo($assetKey: AssetKeyInput!) {
+    assetNodes(assetKeys: [$assetKey]) {
+        freshnessStatusInfo {
+            freshnessStatus
+            freshnessStatusMetadata {
+                ... on AssetHealthFreshnessMeta {
+                    lastMaterializedTimestamp
+                }
             }
         }
     }
@@ -74,3 +94,35 @@ query getFreshnessEnabled {
                 variables={},
             )
             assert result.data["instance"]["freshnessEvaluationEnabled"] is False
+
+            # starts off with no status
+            result = execute_dagster_graphql(
+                graphql_context,
+                GET_FRESHNESS_STATUS_INFO,
+                variables={"assetKey": asset_with_freshness.key.to_graphql_input()},
+            )
+            assert result.data["assetNodes"][0]["freshnessStatusInfo"] is not None
+            assert (
+                result.data["assetNodes"][0]["freshnessStatusInfo"]["freshnessStatus"] == "UNKNOWN"
+            )
+
+            # now it's healthy
+            instance._report_runless_asset_event(  # noqa: SLF001
+                asset_event=FreshnessStateChange(
+                    key=asset_with_freshness.key,
+                    previous_state=FreshnessState.UNKNOWN,
+                    new_state=FreshnessState.PASS,
+                    state_change_timestamp=get_current_timestamp(),
+                )
+            )
+
+            # make sure it comes out as healthy
+            result = execute_dagster_graphql(
+                graphql_context,
+                GET_FRESHNESS_STATUS_INFO,
+                variables={"assetKey": asset_with_freshness.key.to_graphql_input()},
+            )
+            assert result.data["assetNodes"][0]["freshnessStatusInfo"] is not None
+            assert (
+                result.data["assetNodes"][0]["freshnessStatusInfo"]["freshnessStatus"] == "HEALTHY"
+            )
