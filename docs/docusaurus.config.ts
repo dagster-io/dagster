f5c7d39bd123e96c2602ec30193c673125931c78
nikki everett <nikki@dagsterlabs.com>
2025-06-26 14:07:13 -0500

    update for 1.11.0
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -207,7 +207,7 @@ const config: Config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: 'Latest (1.10.21)',
+              label: 'Latest (1.11.0)',
               path: '/',
             },
           },
