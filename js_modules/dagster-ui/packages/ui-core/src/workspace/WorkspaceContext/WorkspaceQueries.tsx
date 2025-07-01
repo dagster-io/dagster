c6595b184dc6cbe2816f0af2cda317d70b16b41b
Marco polo <salazarm@users.noreply.github.com>
2025-07-01 18:04:03 -0400

    [Perf] Fetch assets and asset groups separately from other workspace data (#31003)
    ## Summary & Motivation
    
    Updates our WorkspaceContext to fetch assetNodes/assetGroups separately
    from the other workspace fields in order to reduce peak server memory
    usage (a streaming solution here would be ideal for overall sum
    efficiency).
    
    
    ## How I Tested These Changes
    
    Updated jest tests + local app proxy

--- js_modules/dagster-ui/packages/ui-core/src/workspace/WorkspaceContext/WorkspaceQueries.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/workspace/WorkspaceContext/WorkspaceQueries.tsx
@@ -6,104 +6,14 @@ import {RESOURCE_ENTRY_FRAGMENT} from '../../resources/WorkspaceResourcesQuery';
 import {SENSOR_SWITCH_FRAGMENT} from '../../sensors/SensorSwitchFragment';
 import {REPOSITORY_INFO_FRAGMENT} from '../RepositoryInformation';
 
-export const LOCATION_WORKSPACE_QUERY = gql`
-  query LocationWorkspaceQuery($name: String!) {
-    workspaceLocationEntryOrError(name: $name) {
-      ...WorkspaceLocationNode
-      ...PythonErrorFragment
-    }
-  }
-
-  fragment WorkspaceLocationNode on WorkspaceLocationEntry {
-    id
-    name
-    loadStatus
-    displayMetadata {
-      ...WorkspaceDisplayMetadata
-    }
-    updatedTimestamp
-    versionKey
-    featureFlags {
-      name
-      enabled
-    }
-    locationOrLoadError {
-      ... on RepositoryLocation {
-        id
-        ...WorkspaceLocation
-      }
-      ...PythonErrorFragment
-    }
-  }
-
+export const WORKSPACE_DISPLAY_METADATA_FRAGMENT = gql`
   fragment WorkspaceDisplayMetadata on RepositoryMetadata {
     key
     value
   }
+`;
 
-  fragment WorkspaceLocation on RepositoryLocation {
-    id
-    isReloadSupported
-    serverId
-    name
-    dagsterLibraryVersions {
-      name
-      version
-    }
-    repositories {
-      id
-      ...WorkspaceRepository
-    }
-  }
-
-  fragment WorkspaceRepository on Repository {
-    id
-    name
-    pipelines {
-      id
-      ...WorkspacePipeline
-    }
-    schedules {
-      id
-      ...WorkspaceSchedule
-    }
-    sensors {
-      id
-      ...WorkspaceSensor
-    }
-    partitionSets {
-      id
-      mode
-      pipelineName
-    }
-    assetGroups {
-      id
-      groupName
-    }
-    assetNodes {
-      id
-      ...WorkspaceAsset
-    }
-    allTopLevelResourceDetails {
-      id
-      ...ResourceEntryFragment
-    }
-    ...RepositoryInfoFragment
-  }
-
-  fragment WorkspaceAsset on AssetNode {
-    id
-    ...AssetTableDefinitionFragment
-    graphName
-    opVersion
-    dependencyKeys {
-      path
-    }
-    dependedByKeys {
-      path
-    }
-  }
-
+export const WORKSPACE_PIPELINE_FRAGMENT = gql`
   fragment WorkspacePipeline on Pipeline {
     id
     name
@@ -112,7 +22,9 @@ export const LOCATION_WORKSPACE_QUERY = gql`
     externalJobSource
     pipelineSnapshotId
   }
+`;
 
+export const WORKSPACE_SCHEDULE_FRAGMENT = gql`
   fragment WorkspaceSchedule on Schedule {
     id
     cronSchedule
@@ -130,7 +42,10 @@ export const LOCATION_WORKSPACE_QUERY = gql`
       value
     }
   }
+  ${BASIC_INSTIGATION_STATE_FRAGMENT}
+`;
 
+export const WORKSPACE_SENSOR_FRAGMENT = gql`
   fragment WorkspaceSensor on Sensor {
     id
     name
@@ -152,24 +67,88 @@ export const LOCATION_WORKSPACE_QUERY = gql`
   }
   ${BASIC_INSTIGATION_STATE_FRAGMENT}
   ${SENSOR_SWITCH_FRAGMENT}
-  ${PYTHON_ERROR_FRAGMENT}
-  ${REPOSITORY_INFO_FRAGMENT}
-  ${RESOURCE_ENTRY_FRAGMENT}
-  ${PYTHON_ERROR_FRAGMENT}
-  ${ASSET_TABLE_DEFINITION_FRAGMENT}
 `;
 
-export const CODE_LOCATION_STATUS_QUERY = gql`
-  query CodeLocationStatusQuery {
-    locationStatusesOrError {
-      ... on WorkspaceLocationStatusEntries {
-        entries {
-          ...LocationStatusEntryFragment
-        }
-      }
+export const PARTIAL_WORKSPACE_REPOSITORY_FRAGMENT = gql`
+  fragment PartialWorkspaceRepository on Repository {
+    id
+    name
+    pipelines {
+      id
+      ...WorkspacePipeline
+    }
+    schedules {
+      id
+      ...WorkspaceSchedule
+    }
+    sensors {
+      id
+      ...WorkspaceSensor
+    }
+    partitionSets {
+      id
+      mode
+      pipelineName
+    }
+    allTopLevelResourceDetails {
+      id
+      ...ResourceEntryFragment
+    }
+    ...RepositoryInfoFragment
+  }
+  ${WORKSPACE_PIPELINE_FRAGMENT}
+  ${WORKSPACE_SCHEDULE_FRAGMENT}
+  ${WORKSPACE_SENSOR_FRAGMENT}
+  ${REPOSITORY_INFO_FRAGMENT}
+  ${RESOURCE_ENTRY_FRAGMENT}
+`;
+
+export const PARTIAL_WORKSPACE_LOCATION_FRAGMENT = gql`
+  fragment PartialWorkspaceLocation on RepositoryLocation {
+    id
+    isReloadSupported
+    serverId
+    name
+    dagsterLibraryVersions {
+      name
+      version
+    }
+    repositories {
+      id
+      ...PartialWorkspaceRepository
     }
   }
+  ${PARTIAL_WORKSPACE_REPOSITORY_FRAGMENT}
+`;
 
+export const PARTIAL_WORKSPACE_LOCATION_NODE_FRAGMENT = gql`
+  fragment PartialWorkspaceLocationNode on WorkspaceLocationEntry {
+    id
+    name
+    loadStatus
+    displayMetadata {
+      ...WorkspaceDisplayMetadata
+    }
+    updatedTimestamp
+    versionKey
+    featureFlags {
+      name
+      enabled
+    }
+    locationOrLoadError {
+      ... on RepositoryLocation {
+        id
+        ...PartialWorkspaceLocation
+      }
+      ...PythonErrorFragment
+    }
+  }
+  ${WORKSPACE_DISPLAY_METADATA_FRAGMENT}
+  ${PARTIAL_WORKSPACE_LOCATION_FRAGMENT}
+  ${PYTHON_ERROR_FRAGMENT}
+`;
+
+export const LOCATION_STATUS_ENTRY_FRAGMENT = gql`
   fragment LocationStatusEntryFragment on WorkspaceLocationStatusEntry {
     id
     name
@@ -177,4 +156,156 @@ export const CODE_LOCATION_STATUS_QUERY = gql`
     updateTimestamp
     versionKey
   }
+`;
+
+export const WORKSPACE_ASSET_FRAGMENT = gql`
+  fragment WorkspaceAsset on AssetNode {
+    id
+    ...AssetTableDefinitionFragment
+    graphName
+    opVersion
+    dependencyKeys {
+      path
+    }
+    dependedByKeys {
+      path
+    }
+  }
+  ${ASSET_TABLE_DEFINITION_FRAGMENT}
+`;
+
+export const WORKSPACE_REPOSITORY_ASSETS_FRAGMENT = gql`
+  fragment WorkspaceRepositoryAssets on Repository {
+    id
+    name
+    assetNodes {
+      id
+      ...WorkspaceAsset
+    }
+    assetGroups {
+      id
+      groupName
+    }
+  }
+  ${WORKSPACE_ASSET_FRAGMENT}
+`;
+
+export const WORKSPACE_LOCATION_ASSETS_FRAGMENT = gql`
+  fragment WorkspaceLocationAssets on RepositoryLocation {
+    id
+    name
+    repositories {
+      id
+      ...WorkspaceRepositoryAssets
+    }
+  }
+  ${WORKSPACE_REPOSITORY_ASSETS_FRAGMENT}
+`;
+
+export const WORKSPACE_LOCATION_FRAGMENT = gql`
+  fragment WorkspaceLocation on RepositoryLocation {
+    ...PartialWorkspaceLocation
+    ...WorkspaceLocationAssets
+  }
+  ${PARTIAL_WORKSPACE_LOCATION_FRAGMENT}
+  ${WORKSPACE_LOCATION_ASSETS_FRAGMENT}
+`;
+
+export const WORKSPACE_LOCATION_ASSETS_ENTRY_FRAGMENT = gql`
+  fragment WorkspaceLocationAssetsEntry on WorkspaceLocationEntry {
+    id
+    name
+    loadStatus
+    updatedTimestamp
+    versionKey
+    locationOrLoadError {
+      ... on RepositoryLocation {
+        id
+        ...WorkspaceLocationAssets
+      }
+      ...PythonErrorFragment
+    }
+  }
+  ${WORKSPACE_LOCATION_ASSETS_FRAGMENT}
+  ${PYTHON_ERROR_FRAGMENT}
+`;
+
+// Queries
+export const LOCATION_WORKSPACE_QUERY = gql`
+  query LocationWorkspaceQuery($name: String!) {
+    workspaceLocationEntryOrError(name: $name) {
+      ...PartialWorkspaceLocationNode
+      ...PythonErrorFragment
+    }
+  }
+  ${PARTIAL_WORKSPACE_LOCATION_NODE_FRAGMENT}
+  ${PYTHON_ERROR_FRAGMENT}
+`;
+
+export const CODE_LOCATION_STATUS_QUERY = gql`
+  query CodeLocationStatusQuery {
+    locationStatusesOrError {
+      ... on WorkspaceLocationStatusEntries {
+        entries {
+          id
+          ...LocationStatusEntryFragment
+        }
+      }
+    }
+  }
+  ${LOCATION_STATUS_ENTRY_FRAGMENT}
+`;
+
+export const LOCATION_WORKSPACE_ASSETS_QUERY = gql`
+  query LocationWorkspaceAssetsQuery($name: String!) {
+    workspaceLocationEntryOrError(name: $name) {
+      ...WorkspaceLocationAssetsEntry
+      ...PythonErrorFragment
+    }
+  }
+  ${WORKSPACE_LOCATION_ASSETS_ENTRY_FRAGMENT}
+  ${PYTHON_ERROR_FRAGMENT}
+`;
+
+/*
+ * This fragment isn't used to make a query, but the type is used when merging
+ * PartialWorkspaceRepository and WorkspaceRepositoryAssets together
+ */
+export const WORKSPACE_LOCATION_NODE_FRAGMENT = gql`
+  fragment WorkspaceLocationNode on WorkspaceLocationEntry {
+    id
+    name
+    loadStatus
+    displayMetadata {
+      ...WorkspaceDisplayMetadata
+    }
+    updatedTimestamp
+    versionKey
+    featureFlags {
+      name
+      enabled
+    }
+    locationOrLoadError {
+      ...WorkspaceRepositoryLocation
+      ...PythonErrorFragment
+    }
+  }
+  fragment WorkspaceRepositoryLocation on RepositoryLocation {
+    id
+    ...WorkspaceLocation
+    ...WorkspaceLocationAssets
+  }
+  ${WORKSPACE_DISPLAY_METADATA_FRAGMENT}
+  ${WORKSPACE_LOCATION_FRAGMENT}
+  ${WORKSPACE_LOCATION_ASSETS_FRAGMENT}
+  ${PYTHON_ERROR_FRAGMENT}
+`;
+
+export const WORKSPACE_REPOSITORY_FRAGMENT = gql`
+  fragment WorkspaceRepository on Repository {
+    ...PartialWorkspaceRepository
+    ...WorkspaceRepositoryAssets
+  }
+  ${PARTIAL_WORKSPACE_REPOSITORY_FRAGMENT}
+  ${WORKSPACE_REPOSITORY_ASSETS_FRAGMENT}
 `;
