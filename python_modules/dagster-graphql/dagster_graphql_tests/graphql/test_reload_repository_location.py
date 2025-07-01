1e1096c5275ecf546f60b21b2c223e7760380159
gibsondan <daniel@elementl.com>
2025-07-01 13:01:31 -0400

    Add async versions of code location and execution plan APIs (#29818)
    ## Summary & Motivation
    Laying groundwork for parallelizing IO-bound grpc/agent calls in DA. Not happy about the amount of duplicated code here, but sharing code between async and non-async functions is surprisingly tricky.
    
    ## How I Tested These Changes
    BK

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_reload_repository_location.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_reload_repository_location.py
@@ -291,13 +291,13 @@ class TestReloadRepositoriesOutOfProcess(OutOfProcessTestSuite):
             # note it where the function is *used* that needs to mocked, not
             # where it is defined.
             # see https://docs.python.org/3/library/unittest.mock.html#where-to-patch
-            "dagster._core.remote_representation.code_location.sync_list_repositories_grpc"
+            "dagster._api.list_repositories.sync_list_repositories_grpc"
         ) as cli_command_mock:
             with mock.patch(
                 # note it where the function is *used* that needs to mocked, not
                 # where it is defined.
                 # see https://docs.python.org/3/library/unittest.mock.html#where-to-patch
-                "dagster._core.remote_representation.code_location.sync_get_streaming_external_repositories_data_grpc"
+                "dagster._api.snapshot_repository.sync_get_streaming_external_repositories_data_grpc"
             ) as remote_repository_mock:
 
                 @repository
@@ -360,7 +360,7 @@ class TestReloadRepositoriesOutOfProcess(OutOfProcessTestSuite):
             # note it where the function is *used* that needs to mocked, not
             # where it is defined.
             # see https://docs.python.org/3/library/unittest.mock.html#where-to-patch
-            "dagster._core.remote_representation.code_location.sync_list_repositories_grpc"
+            "dagster._api.list_repositories.sync_list_repositories_grpc"
         ) as cli_command_mock:
             cli_command_mock.side_effect = Exception("Mocked repository load failure")
 
