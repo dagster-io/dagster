cae05543ddd29ee6a0a06fdddbe60d07e52fa4d8
Hynek Blaha <hynek.blaha@second-foundation.eu>
2025-07-07 18:16:38 +0200

    [ui] fix storybook: example for run page

--- js_modules/dagster-ui/packages/ui-core/src/runs/__stories__/RunsFeedRoot.stories.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/runs/__stories__/RunsFeedRoot.stories.tsx
@@ -1,6 +1,7 @@
 import {MockedProvider} from '@apollo/client/testing';
 import {Meta} from '@storybook/react';
 
+import {InMemoryCache} from '../../apollo-client';
 import {WorkspaceProvider} from '../../workspace/WorkspaceContext/WorkspaceContext';
 import RunsFeedRoot from '../RunsFeedRoot';
 import {RunsFeedRootMock} from '../__fixtures__/RunsFeedRoot.fixtures';
@@ -11,9 +12,26 @@ export default {
   component: RunsFeedRoot,
 } as Meta;
 
+// This cache prevents Apollo Client from stripping fields in mock data.
+const createFixingRunsCache = () => {
+  return new InMemoryCache({
+    possibleTypes: {
+      RunsFeedEntry: ['Run', 'PartitionBackfill'],
+    },
+    typePolicies: {
+      Run: {
+        keyFields: false,
+      },
+      PartitionBackfill: {
+        keyFields: false,
+      },
+    },
+  });
+};
+
 export const Example = () => {
   return (
-    <MockedProvider mocks={[RunsFeedRootMock]}>
+    <MockedProvider mocks={[RunsFeedRootMock]} cache={createFixingRunsCache()}>
       <WorkspaceProvider>
         <RunsFeedRoot />
       </WorkspaceProvider>
