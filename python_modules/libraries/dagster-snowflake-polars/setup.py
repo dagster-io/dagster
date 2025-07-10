1454ab160a9a84565a147639170a47dad30a6b4e
Daniel Gibson <daniel@elementl.com>
2025-07-10 16:53:55 -0400

    [TEST_PYTHON_VERSIONS=test-all][NO_SKIP] Make all dagster integration pins available on 3.12 and 3.13
    Summary:
    For now i just matched our old behavior and allowed everything on the python version - I suspect if these do not work it is because of an issue with the underlying library rather than somethign in the dagster-xxx integration specifically?
    
    Test Plan: BK

--- python_modules/libraries/dagster-snowflake-polars/setup.py
+++ python_modules/libraries/dagster-snowflake-polars/setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     packages=find_packages(exclude=["dagster_snowflake_polars_tests*"]),
     include_package_data=True,
-    python_requires=">=3.9,<3.13",
+    python_requires=">=3.9,<3.14",
     install_requires=[
         f"dagster{pin}",
         f"dagster-snowflake{pin}",
