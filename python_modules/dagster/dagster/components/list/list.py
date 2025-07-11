8363177b41b803bc22985243793354428af77c08
benpankow <ben@dagsterlabs.com>
2025-07-11 14:53:30 -0700

    no ctx manager

--- python_modules/dagster/dagster/components/list/list.py
+++ python_modules/dagster/dagster/components/list/list.py
@@ -27,12 +27,15 @@ from dagster._cli.workspace.cli_target import get_repository_python_origin_from_
 from dagster._config.pythonic_config.resource import get_resource_type_name
 from dagster._core.definitions.asset_selection import AssetSelection
 from dagster._core.definitions.assets.job.asset_job import is_reserved_asset_job_name
+from dagster._core.definitions.definitions_load_context import (
+    DefinitionsLoadContext,
+    DefinitionsLoadType,
+)
 from dagster._core.definitions.repository_definition.repository_definition import (
     RepositoryDefinition,
 )
 from dagster._utils.error import serializable_error_info_from_exc_info
 from dagster._utils.hosted_user_process import recon_repository_from_origin
-from dagster._utils.test.definitions import scoped_definitions_load_context
 from dagster.components.component.component import Component
 from dagster.components.core.defs_module import ComponentRequirementsModel
 from dagster.components.core.package_entry import (
@@ -89,24 +92,29 @@ def _load_defs_at_path(dg_context: DgContext, path: Optional[Path]) -> Repositor
     """Attempts to load the component tree from the context project root, falling back to
     resolving the entire repository and using the attached component tree.
     """
-    with scoped_definitions_load_context():
-        if not path:
-            repository_origin = get_repository_python_origin_from_cli_opts(
-                PythonPointerOpts.extract_from_cli_options(dict(dg_context.target_args))
-            )
-            recon_repo = recon_repository_from_origin(repository_origin)
-            repo_def = recon_repo.get_definition()
-            return repo_def
+    DefinitionsLoadContext.set(
+        DefinitionsLoadContext(
+            load_type=DefinitionsLoadType.INITIALIZATION, repository_load_data=None
+        )
+    )
 
-        tree = ComponentTree.load(dg_context.root_path)
+    if not path:
+        repository_origin = get_repository_python_origin_from_cli_opts(
+            PythonPointerOpts.extract_from_cli_options(dict(dg_context.target_args))
+        )
+        recon_repo = recon_repository_from_origin(repository_origin)
+        repo_def = recon_repo.get_definition()
+        return repo_def
 
-        try:
-            defs = tree.build_defs_at_path(path) if path else tree.build_defs()
-        except Exception as e:
-            path_text = f" at {path}" if path else ""
-            raise click.ClickException(f"Unable to load definitions{path_text}: {e}") from e
+    tree = ComponentTree.load(dg_context.root_path)
 
-        return defs.get_repository_def()
+    try:
+        defs = tree.build_defs_at_path(path) if path else tree.build_defs()
+    except Exception as e:
+        path_text = f" at {path}" if path else ""
+        raise click.ClickException(f"Unable to load definitions{path_text}: {e}") from e
+
+    return defs.get_repository_def()
 
 
 def _tag_filter(tag_key: str) -> bool:
