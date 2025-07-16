6191008ecbbcd70e8239c6e3ef693690475245de
Marco Salazar <salazarm93@gmail.com>
2025-07-16 15:41:55 -0700

    .

--- js_modules/dagster-ui/packages/ui-core/src/live-data-provider/util.ts
+++ js_modules/dagster-ui/packages/ui-core/src/live-data-provider/util.ts
@@ -4,18 +4,15 @@ const urlParams = new URLSearchParams(window.location.search);
 const urlLiveDataBatchSize = parseInt(urlParams.get('live-data-batch-size') ?? '0', 10);
 const urlLiveDataBatchThreads = parseInt(urlParams.get('live-data-parallel-fetches') ?? '0', 10);
 
-const liveDataBatchSize = urlLiveDataBatchSize ?? 10;
-const batchThreads = urlLiveDataBatchThreads ?? 2;
-
-export const BATCH_SIZE = isNaN(liveDataBatchSize) ? 10 : liveDataBatchSize;
-export const BATCH_PARALLEL_FETCHES = isNaN(batchThreads) ? 2 : batchThreads;
+export const BATCH_SIZE = urlLiveDataBatchSize || 10;
+export const BATCH_PARALLEL_FETCHES = urlLiveDataBatchThreads || 2;
 
 export const SUBSCRIPTION_IDLE_POLL_RATE = 30 * 1000;
 export const SUBSCRIPTION_MAX_POLL_RATE = 2 * 1000;
 
 export const threadIDToLimits = {
   ['AssetHealth' as string]: {
-    batchSize: urlLiveDataBatchSize ?? 250,
-    parallelThreads: urlLiveDataBatchThreads ?? 4,
+    batchSize: urlLiveDataBatchSize || 250,
+    parallelThreads: urlLiveDataBatchThreads || 4,
   },
 };
