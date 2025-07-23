32f4f65e92cb300ac8468bdb0c686f2e5e5a924b
ahmetsahiner <119397720+albertfast@users.noreply.github.com>
2025-07-23 19:40:16 +0000

    updated buildkite-integration.yml

---
+++ test_project/definitions.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+from dagster import definitions, load_from_defs_folder
+
+@definitions
+def defs():
+    return load_from_defs_folder(project_root=Path(__file__).parent.parent.parent)
