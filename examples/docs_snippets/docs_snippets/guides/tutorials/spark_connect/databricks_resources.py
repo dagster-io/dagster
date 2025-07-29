b9c42f7da9288b5ceff472f1afa3735a65f38183
benpankow <ben@dagsterlabs.com>
2025-07-29 13:56:32 -0700

    szession

--- examples/docs_snippets/docs_snippets/guides/tutorials/spark_connect/databricks_resources.py
+++ examples/docs_snippets/docs_snippets/guides/tutorials/spark_connect/databricks_resources.py
@@ -4,16 +4,16 @@ from databricks.connect import DatabricksSession
 
 import dagster as dg
 
-databricks_session = (
-    DatabricksSession.builder.remote(
-        host=os.environ["DATABRICKS_HOST"],
-        token=os.environ["DATABRICKS_TOKEN"],
-    )
-    .serverless()
-    .getOrCreate()
-)
-
 
 @dg.definitions
 def resources():
+    databricks_session = (
+        DatabricksSession.builder.remote(
+            host=os.environ["DATABRICKS_HOST"],
+            token=os.environ["DATABRICKS_TOKEN"],
+        )
+        .serverless()
+        .getOrCreate()
+    )
+
     return dg.Definitions(resources={"spark": databricks_session})
--- examples/docs_snippets/docs_snippets/guides/tutorials/spark_connect/spark_resources.py
+++ examples/docs_snippets/docs_snippets/guides/tutorials/spark_connect/spark_resources.py
@@ -4,11 +4,11 @@ from pyspark.sql import SparkSession
 
 import dagster as dg
 
-spark_session = SparkSession.builder.remote(
-    os.environ["SPARK_REMOTE"],
-).getOrCreate()
-
 
 @dg.definitions
 def resources():
+    spark_session = SparkSession.builder.remote(
+        os.environ["SPARK_REMOTE"],
+    ).getOrCreate()
+
     return dg.Definitions(resources={"spark": spark_session})
