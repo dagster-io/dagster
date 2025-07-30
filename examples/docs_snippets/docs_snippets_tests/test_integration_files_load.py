32dd1b7667c374dc2747474dabaeeb400332b2bb
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-07-30 14:29:19 -0400

    [docs] Add example on how to create a job for Airbyte Cloud assets (#31431)
    ## Summary & Motivation
    
    ## How I Tested These Changes
    
    ## Changelog
    
    > Insert changelog entry or delete this section.

--- examples/docs_snippets/docs_snippets_tests/test_integration_files_load.py
+++ examples/docs_snippets/docs_snippets_tests/test_integration_files_load.py
@@ -38,6 +38,7 @@ EXCLUDED_FILES = {
     f"{snippets_folder}/airbyte_cloud/multiple_airbyte_cloud_workspaces.py",
     f"{snippets_folder}/airbyte_cloud/representing_airbyte_cloud_assets.py",
     f"{snippets_folder}/airbyte_cloud/sync_and_materialize_airbyte_cloud_assets.py",
+    f"{snippets_folder}/airbyte_cloud/create_airbyte_cloud_all_assets_job.py",
     # FIXME: this breaks on py3.8 and seems related to the non-dagster dependencies
     f"{snippets_folder}/pandera.py",
     # FIXME: include tests
