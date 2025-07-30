0a9a4cba810ad1474fa98aedec8bf01750ab394e
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-07-30 14:33:28 -0400

    [docs] Add example on how to create dlt asset job (#31436)
    ## Summary & Motivation
    
    ## How I Tested These Changes
    
    ## Changelog
    
    > Insert changelog entry or delete this section.

--- examples/docs_snippets/docs_snippets_tests/test_integration_files_load.py
+++ examples/docs_snippets/docs_snippets_tests/test_integration_files_load.py
@@ -82,6 +82,8 @@ EXCLUDED_FILES = {
     f"{snippets_folder}/dlt/dlt_dagster_translator.py",
     f"{snippets_folder}/dlt/dlt_partitions.py",
     f"{snippets_folder}/dlt/dlt_source_assets.py",
+    f"{snippets_folder}/dlt/dlt_source_assets.py",
+    f"{snippets_folder}/dlt/create_dlt_asset_job.py",
     f"{snippets_folder}/duckdb/reference/resource.py",
     f"{snippets_folder}/duckdb/tutorial/resource/downstream.py",
     f"{snippets_folder}/looker/customize-looker-assets.py",
