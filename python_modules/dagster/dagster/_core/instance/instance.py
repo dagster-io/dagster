35043dd35cef67578a19b2999e15985aa397f53c
Nick Schrock <schrockn@dagsterlabs.com>
2025-08-06 11:20:42 -0700

    cp

--- python_modules/dagster/dagster/_core/instance/instance.py
+++ python_modules/dagster/dagster/_core/instance/instance.py
@@ -1049,6 +1049,12 @@ class DagsterInstance(DynamicPartitionsStore):
 
         return RunLauncherDomain(self)
 
+    @cached_property
+    def _daemon_domain(self):
+        from dagster._core.instance.daemon.daemon_domain import DaemonDomain
+
+        return DaemonDomain(self)
+
     def create_run(
         self,
         *,
@@ -2047,48 +2053,17 @@ class DagsterInstance(DynamicPartitionsStore):
     # dagster daemon
     def add_daemon_heartbeat(self, daemon_heartbeat: "DaemonHeartbeat") -> None:
         """Called on a regular interval by the daemon."""
-        self._run_storage.add_daemon_heartbeat(daemon_heartbeat)
+        return self._daemon_domain.add_daemon_heartbeat(daemon_heartbeat)
 
     def get_daemon_heartbeats(self) -> Mapping[str, "DaemonHeartbeat"]:
         """Latest heartbeats of all daemon types."""
-        return self._run_storage.get_daemon_heartbeats()
+        return self._daemon_domain.get_daemon_heartbeats()
 
     def wipe_daemon_heartbeats(self) -> None:
-        self._run_storage.wipe_daemon_heartbeats()
+        return self._daemon_domain.wipe_daemon_heartbeats()
 
     def get_required_daemon_types(self) -> Sequence[str]:
-        from dagster._core.run_coordinator import QueuedRunCoordinator
-        from dagster._core.scheduler import DagsterDaemonScheduler
-        from dagster._daemon.asset_daemon import AssetDaemon
-        from dagster._daemon.auto_run_reexecution.event_log_consumer import EventLogConsumerDaemon
-        from dagster._daemon.daemon import (
-            BackfillDaemon,
-            MonitoringDaemon,
-            SchedulerDaemon,
-            SensorDaemon,
-        )
-        from dagster._daemon.freshness import FreshnessDaemon
-        from dagster._daemon.run_coordinator.queued_run_coordinator_daemon import (
-            QueuedRunCoordinatorDaemon,
-        )
-
-        if self.is_ephemeral:
-            return []
-
-        daemons = [SensorDaemon.daemon_type(), BackfillDaemon.daemon_type()]
-        if isinstance(self.scheduler, DagsterDaemonScheduler):
-            daemons.append(SchedulerDaemon.daemon_type())
-        if isinstance(self.run_coordinator, QueuedRunCoordinator):
-            daemons.append(QueuedRunCoordinatorDaemon.daemon_type())
-        if self.run_monitoring_enabled:
-            daemons.append(MonitoringDaemon.daemon_type())
-        if self.run_retries_enabled:
-            daemons.append(EventLogConsumerDaemon.daemon_type())
-        if self.auto_materialize_enabled or self.auto_materialize_use_sensors:
-            daemons.append(AssetDaemon.daemon_type())
-        if self.freshness_enabled:
-            daemons.append(FreshnessDaemon.daemon_type())
-        return daemons
+        return self._daemon_domain.get_required_daemon_types()
 
     def get_daemon_statuses(
         self, daemon_types: Optional[Sequence[str]] = None
@@ -2096,21 +2071,11 @@ class DagsterInstance(DynamicPartitionsStore):
         """Get the current status of the daemons. If daemon_types aren't provided, defaults to all
         required types. Returns a dict of daemon type to status.
         """
-        from dagster._daemon.controller import get_daemon_statuses
-
-        check.opt_sequence_param(daemon_types, "daemon_types", of_type=str)
-        return get_daemon_statuses(
-            self,
-            daemon_types=daemon_types or self.get_required_daemon_types(),
-            ignore_errors=True,
-        )
+        return self._daemon_domain.get_daemon_statuses(daemon_types)
 
     @property
     def daemon_skip_heartbeats_without_errors(self) -> bool:
-        # If enabled, daemon threads won't write heartbeats unless they encounter an error. This is
-        # enabled in cloud, where we don't need to use heartbeats to check if daemons are running, but
-        # do need to surface errors to users. This is an optimization to reduce DB writes.
-        return False
+        return self._daemon_domain.daemon_skip_heartbeats_without_errors
 
     # backfill
     def get_backfills(
