a8b61a31084582adb486ae43f52fb7112343d4ff
Nick Schrock <schrockn@dagsterlabs.com>
2025-08-06 19:37:35 -0700

    Remove deprecated scheduling_implementation alias (#31603)
    ## Summary & Motivation
    
    This change removes a deprecated backward compatibility alias `scheduling_implementation` from the scheduling module. The alias was pointing to `SchedulingDomain` but is no longer needed as code should reference `SchedulingDomain` directly.
    
    ## How I Tested These Changes
    
    Existing test suite confirms no active usage of the removed alias.

--- python_modules/dagster/dagster/_core/instance/scheduling/__init__.py
+++ python_modules/dagster/dagster/_core/instance/scheduling/__init__.py
@@ -1,6 +1,3 @@
 from dagster._core.instance.scheduling.scheduling_domain import SchedulingDomain
 
 __all__ = ["SchedulingDomain"]
-
-# For backward compatibility, provide scheduling_implementation as SchedulingDomain
-scheduling_implementation = SchedulingDomain
