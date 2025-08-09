56ab75cc92b0cfbbfd71293ea74956f111247c79
JamieDeMaria <jamie@dagsterlabs.com>
2025-08-09 12:32:52 -0400

    switch stub asset default

--- js_modules/dagster-ui/packages/ui-core/src/app/UserSettingsDialog/useShowStubAssets.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/UserSettingsDialog/useShowStubAssets.tsx
@@ -5,7 +5,7 @@ const SHOW_STUB_ASSETS_STORAGE_KEY = 'showStubAssets';
 export const useShowStubAssets = () => {
   const [showStubAssets, setShowStubAssets] = useStateWithStorage(
     SHOW_STUB_ASSETS_STORAGE_KEY,
-    (value: any) => (typeof value === 'boolean' ? value : false),
+    (value: any) => (typeof value === 'boolean' ? value : true),
   );
 
   return {showStubAssets, setShowStubAssets};
