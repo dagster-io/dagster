007984583b69cd8a5a5c9de7c71fe91f78d3219c
Nick Schrock <schrockn@dagsterlabs.com>
2025-08-11 16:22:46 -0700

    Update unlaunched.py

--- python_modules/libraries/dagster-dg-cli/dagster_dg_cli/utils/unlaunched.py
+++ python_modules/libraries/dagster-dg-cli/dagster_dg_cli/utils/unlaunched.py
@@ -6,7 +6,7 @@ import os
 
 
 def show_dg_unlaunched_commands() -> bool:
-    """We hide cli commands that we have not launchedyet. Override this by setting
+    """We hide cli commands that we have not launched yet. Override this by setting
     the DG_SHOW_UNLAUNCHED_COMMANDS environment variable to any value.
     """
     return os.getenv("DG_SHOW_UNLAUNCHED_COMMANDS") is not None
