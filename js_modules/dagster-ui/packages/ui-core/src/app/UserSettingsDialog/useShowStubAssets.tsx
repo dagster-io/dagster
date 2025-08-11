5b778e2ffd1feeb39ae7385b075f5f43c6e2a839
gibsondan <daniel@elementl.com>
2025-08-10 20:07:09 -0400

    fix issue where an asset was declared a stub if it was a stub in any code location (#31693)
    ## Summary & Motivation
    Unlike these other properties, an asset should only be called a stub if it is a stub in the actual asset definition that we picked.
    
    ## How I Tested These Changes
    Load an asset job with the 'filter stubs' toggle on when it defined in another code location as a stub, the toggle no longer excludes it.

--- js_modules/dagster-ui/packages/ui-core/src/app/UserSettingsDialog/useShowStubAssets.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/app/UserSettingsDialog/useShowStubAssets.tsx
@@ -5,7 +5,7 @@ const SHOW_STUB_ASSETS_STORAGE_KEY = 'showStubAssets';
 export const useShowStubAssets = () => {
   const [showStubAssets, setShowStubAssets] = useStateWithStorage(
     SHOW_STUB_ASSETS_STORAGE_KEY,
-    (value: any) => (typeof value === 'boolean' ? value : true),
+    (value: any) => (typeof value === 'boolean' ? value : false),
   );
 
   return {showStubAssets, setShowStubAssets};
