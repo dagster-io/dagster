1ecb4eca7bbc88c77f90c93f093c5cf895a956dd
Maxime Armstrong <maxime@retain.ai>
2025-08-12 14:29:29 -0400

    [dagster-databricks] Add Databricks resource for DatabricksAssetBundleComponent

---
+++ python_modules/libraries/dagster-databricks/dagster_databricks/components/databricks_asset_bundle/resource.py
@@ -0,0 +1,20 @@
+from dagster import ConfigurableResource
+from dagster_shared.utils import cached_method
+from databricks.sdk import WorkspaceClient
+from pydantic import Field
+
+
+class DatabricksWorkspace(ConfigurableResource):
+    """Represents a workspace in Databricks and provides utilities
+    to interact with the Databricks SDK.
+    """
+
+    host: str = Field(description="The host used to connect to the Databricks Workspace.")
+    token: str = Field(description="The token used to connect to Databricks Workspace.")
+
+    @cached_method
+    def get_client(self) -> WorkspaceClient:
+        return WorkspaceClient(
+            host=self.host,
+            token=self.token,
+        )
