eef6e7a86b4492679124b26d8fe784acc47c70dd
Owen Kephart <owenkephart@gmail.com>
2025-08-13 10:35:06 -0700

    [dagster-omni] initial commit

---
+++ python_modules/libraries/dagster-omni/setup.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+def get_version() -> str:
+    version: dict[str, str] = {}
+    with open(Path(__file__).parent / "dagster_omni/version.py", encoding="utf8") as fp:
+        exec(fp.read(), version)
+
+    return version["__version__"]
+
+
+# TODO - add your package to scripts/install_dev_python_modules.py
+
+ver = get_version()
+# dont pin dev installs to avoid pip dep resolver issues
+pin = "" if ver == "1!0+dev" else f"=={ver}"
+setup(
+    name="dagster_omni",
+    version=get_version(),
+    author="Dagster Labs",
+    author_email="hello@dagsterlabs.com",
+    license="Apache-2.0",
+    description="",  # TODO - fill out description
+    url=("https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-omni"),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=["dagster_omni_tests*"]),
+    include_package_data=True,
+    install_requires=[
+        f"dagster{pin}",
+        # TODO - fill in remaining dependencies
+    ],
+    zip_safe=False,
+)
