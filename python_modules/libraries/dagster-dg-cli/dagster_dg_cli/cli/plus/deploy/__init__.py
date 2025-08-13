ece8b564d94894802e5c4432210697deaf26bc5d
Nick Schrock <schrockn@dagsterlabs.com>
2025-08-12 18:10:28 -0700

    cp

--- python_modules/libraries/dagster-dg-cli/dagster_dg_cli/cli/plus/deploy/__init__.py
+++ python_modules/libraries/dagster-dg-cli/dagster_dg_cli/cli/plus/deploy/__init__.py
@@ -1,3 +1,3 @@
-from dagster_dg_cli.cli.plus.deploy.commands import deploy_group
+from dagster_dg_cli.cli.plus.deploy.commands import DEFAULT_STATEDIR_PATH, deploy_group
 
-__all__ = ["deploy_group"]
+__all__ = ["DEFAULT_STATEDIR_PATH", "deploy_group"]
