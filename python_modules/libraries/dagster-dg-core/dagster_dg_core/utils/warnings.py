cc2c0f9225c1eeaee795e5bbc74ae3b1e764f60b
Sean Mackesey <s.mackesey@gmail.com>
2025-08-14 12:31:28 -0400

    [dg] Remove autoload_defs settings from dg project settings (ADOPT-1785) (#31764)
    ## Summary & Motivation
    
    Remove `autoload_defs` setting from dg project settings-- this was
    actually deprecated for removal in 1.11.0 but that never happened.
    
    ## How I Tested These Changes
    
    Existing test suite.

--- python_modules/libraries/dagster-dg-core/dagster_dg_core/utils/warnings.py
+++ python_modules/libraries/dagster-dg-core/dagster_dg_core/utils/warnings.py
@@ -8,7 +8,6 @@ from typing_extensions import TypeAlias
 from dagster_dg_core.utils import format_multiline_str
 
 DgWarningIdentifier: TypeAlias = Literal[
-    "autoload_defs_with_definitions_py",
     "cli_config_in_workspace_project",
     "deprecated_user_config_location",
     "deprecated_python_environment",
