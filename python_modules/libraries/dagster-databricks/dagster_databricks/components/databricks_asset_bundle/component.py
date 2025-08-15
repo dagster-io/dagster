05dd72d53665fc850c3aaf26a38ac37fd2446f97
Maxime Armstrong <maxime@retain.ai>
2025-08-15 14:08:50 -0400

    fix rebase

--- python_modules/libraries/dagster-databricks/dagster_databricks/components/databricks_asset_bundle/component.py
+++ python_modules/libraries/dagster-databricks/dagster_databricks/components/databricks_asset_bundle/component.py
@@ -134,7 +134,7 @@ class DatabricksAssetBundleComponent(Component, Resolvable):
 
     def build_defs(self, context: ComponentLoadContext) -> Definitions:
         @multi_asset(
-            name="databricks_multi_asset",
+            name=f"{self.custom_configs.job_name_prefix}_multi_asset",
             specs=[self.get_asset_spec(task) for task in self.databricks_config.tasks],
             can_subset=True,
         )
