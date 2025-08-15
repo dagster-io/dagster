a17ce042d088c08a9207f1008012b9f252b489fb
Maxime Armstrong <46797220+maximearmstrong@users.noreply.github.com>
2025-08-14 20:46:39 -0400

    [dagster-databricks] Support Databricks Python wheel tasks in asset bundle component (#31682)
    ## Summary & Motivation
    
    Support Python wheel tasks in DatabricksConfigs.
    
    ## How I Tested These Changes
    
    BK

--- python_modules/libraries/dagster-databricks/dagster_databricks/components/databricks_asset_bundle/databricks_configs.py
+++ python_modules/libraries/dagster-databricks/dagster_databricks/components/databricks_asset_bundle/databricks_configs.py
@@ -144,6 +144,46 @@ class DatabricksSparkPythonTask:
         )
 
 
+@record
+class DatabricksPythonWheelTask:
+    task_key: str
+    task_config: Mapping[str, Any]
+    task_parameters: list[str]
+    depends_on: list[str]
+    job_name: str
+    libraries: list[Mapping[str, Any]]
+
+    @property
+    def task_type(self) -> str:
+        return "python_wheel"
+
+    @cached_property
+    def task_config_metadata(self) -> Mapping[str, Any]:
+        task_config_metadata = {}
+        wheel_config = self.task_config["python_wheel_task"]
+        task_config_metadata["package_name"] = wheel_config["package_name"]
+        task_config_metadata["entry_point"] = wheel_config["entry_point"]
+        task_config_metadata["parameters"] = self.task_parameters
+        return task_config_metadata
+
+    @classmethod
+    def from_job_task_config(
+        cls, job_task_config: Mapping[str, Any]
+    ) -> "DatabricksPythonWheelTask":
+        python_wheel_task = job_task_config["python_wheel_task"]
+        task_config = {"python_wheel_task": python_wheel_task}
+        # Python wheel tasks use parameters differently
+        task_parameters = python_wheel_task.get("parameters", [])
+        return cls(
+            task_key=job_task_config["task_key"],
+            task_config=task_config,
+            task_parameters=task_parameters,
+            depends_on=parse_depends_on(job_task_config.get("depends_on", [])),
+            job_name=job_task_config["job_name"],
+            libraries=job_task_config.get("libraries", []),
+        )
+
+
 @record_custom
 class DatabricksConfigs(IHaveNew):
     databricks_configs_path: Path
@@ -231,6 +271,13 @@ class DatabricksConfigs(IHaveNew):
                 elif "spark_python_task" in job_task_config:
                     tasks.append(
                         DatabricksSparkPythonTask.from_job_task_config(
+                            job_task_config=augmented_job_task_config
+                        )
+                    )
+
+                elif "python_wheel_task" in job_task_config:
+                    tasks.append(
+                        DatabricksPythonWheelTask.from_job_task_config(
                             job_task_config=augmented_job_task_config
                         )
                     )
