c1e2f0564ce2f03192516ee08374f4d4d41189f4
Maxime Armstrong <maxime@retain.ai>
2025-08-15 14:01:19 -0400

    Lint

--- python_modules/libraries/dagster-databricks/dagster_databricks/components/databricks_asset_bundle/component.py
+++ python_modules/libraries/dagster-databricks/dagster_databricks/components/databricks_asset_bundle/component.py
@@ -11,9 +11,7 @@ from dagster.components.resolved.core_models import ResolutionContext
 from dagster.components.resolved.model import Resolver
 from dagster.components.scaffold.scaffold import scaffold_with
 
-from dagster_databricks.components.databricks_asset_bundle.configs import (
-    DatabricksConfig,
-)
+from dagster_databricks.components.databricks_asset_bundle.configs import DatabricksConfig
 from dagster_databricks.components.databricks_asset_bundle.scaffolder import (
     DatabricksAssetBundleScaffolder,
 )
