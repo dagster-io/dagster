196b69dd8aaf2e86866bd924f63a0b34facff0d2
Nick Schrock <schrockn@dagsterlabs.com>
2025-08-16 04:09:46 -0700

    fix

--- python_modules/libraries/dagster-dg-cli/dagster_dg_cli/utils/claude_utils.py
+++ python_modules/libraries/dagster-dg-cli/dagster_dg_cli/utils/claude_utils.py
@@ -8,17 +8,7 @@ from time import perf_counter
 from typing import Any, Optional, Protocol
 
 # typer and pydantic imports moved to functions to defer import costs
-
 from dagster_dg_cli.cli.scaffold.branch.diagnostics import AIInteraction, ClaudeDiagnosticsService
-from dagster_dg_cli.cli.scaffold.branch.validation import (
-    ClaudeAssistantMessage,
-    ClaudeErrorResult,
-    ClaudeSuccessResult,
-    ClaudeSystemInit,
-    ClaudeUserMessage,
-    MCPServer,
-    SDKMessage,
-)
 from dagster_dg_core.context import DgContext
 
 
