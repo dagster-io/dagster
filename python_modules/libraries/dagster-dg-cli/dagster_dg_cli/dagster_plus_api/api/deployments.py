4faba95657e4aa402460dfae1637aa425587478a
Nick Schrock <schrockn@dagsterlabs.com>
2025-08-27 06:39:43 -0700

    cp

--- python_modules/libraries/dagster-dg-cli/dagster_dg_cli/dagster_plus_api/api/deployments.py
+++ python_modules/libraries/dagster-dg-cli/dagster_dg_cli/dagster_plus_api/api/deployments.py
@@ -11,10 +11,6 @@ if TYPE_CHECKING:
 
 
 class DeploymentAPI:
-    """Deployment API matching future FastAPI endpoints.
-    This is the interface that will remain stable when we switch from GraphQL to REST.
-    """
-
     def __init__(self, config: DagsterPlusCliConfig):
         self.config = config
 
@@ -22,7 +18,4 @@ class DeploymentAPI:
         self,
         limit: Optional[int] = None,
     ) -> "DeploymentList":
-        """GET /api/v1/deployments
-        List all deployments in the organization.
-        """
         return list_deployments_via_graphql(self.config, limit=limit)
