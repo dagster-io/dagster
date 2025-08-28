e821ada1b40033c82c9e122b95f42a0e97abf483
Daniel Gibson <daniel@elementl.com>
2025-08-27 20:48:33 -0500

    Optimize set operations on two DefaultPartitionSubsets and is_empty on DefaultPartitionsSubset
    Summary:
    The base implementation creates a new copy of the underlying set and uses with_partition_keys which can create new copies and make a real difference for large subsets. Manipulate the underlying sets directly when possible.
    
    BK, microbenchmark script that takes two DefaultPartitionSubsets and subtracts them:
    
    ```
    from dagster._core.definitions.partitions.subset.default import DefaultPartitionsSubset
    
    seta = DefaultPartitionsSubset({str(a) for a in range(100000)})
    
    setb = DefaultPartitionsSubset({str(a) * 2 for a in range(100000)})
    
    for i in range(200):
        print(str(i))
        setc = seta - setb
    ```
    
    > Insert changelog entry or delete this section.

--- python_modules/dagster/dagster/_core/definitions/partitions/subset/default.py
+++ python_modules/dagster/dagster/_core/definitions/partitions/subset/default.py
@@ -28,6 +28,10 @@ class DefaultPartitionsSubset(
         check.opt_set_param(subset, "subset")
         return super().__new__(cls, subset or set())
 
+    @property
+    def is_empty(self) -> bool:
+        return len(self.subset) == 0
+
     def get_partition_keys_not_in_subset(
         self, partitions_def: PartitionsDefinition
     ) -> Iterable[str]:
@@ -35,6 +39,42 @@ class DefaultPartitionsSubset(
 
     def get_partition_keys(self) -> Iterable[str]:
         return self.subset
+
+    def __sub__(self, other: "PartitionsSubset") -> "PartitionsSubset":
+        if not isinstance(other, DefaultPartitionsSubset):
+            return super().__sub__(other)
+
+        if self is other:
+            return self.empty_subset()
+        if other.is_empty:
+            return self
+
+        return DefaultPartitionsSubset(self.subset - other.subset)
+
+    def __or__(self, other: "PartitionsSubset") -> "PartitionsSubset":
+        if not isinstance(other, DefaultPartitionsSubset):
+            return super().__or__(other)
+
+        if self is other or other.is_empty:
+            return self
+
+        if self.is_empty:
+            return other
+
+        return DefaultPartitionsSubset(self.subset | other.subset)
+
+    def __and__(self, other: "PartitionsSubset") -> "PartitionsSubset":
+        if not isinstance(other, DefaultPartitionsSubset):
+            return super().__and__(other)
+
+        if self is other:
+            return self
+        if other.is_empty:
+            return other
+        if self.is_empty:
+            return self
+
+        return DefaultPartitionsSubset(self.subset & other.subset)
 
     def get_ranges_for_keys(self, partition_keys: Sequence[str]) -> Sequence[PartitionKeyRange]:
         cur_range_start = None
