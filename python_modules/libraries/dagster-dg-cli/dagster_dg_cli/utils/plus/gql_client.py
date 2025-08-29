e95d8c68319584b1cedf90d4a0305172eb15aaa2
Nick Schrock <schrockn@dagsterlabs.com>
2025-08-29 07:23:22 -0700

    cp

--- python_modules/libraries/dagster-dg-cli/dagster_dg_cli/utils/plus/gql_client.py
+++ python_modules/libraries/dagster-dg-cli/dagster_dg_cli/utils/plus/gql_client.py
@@ -6,12 +6,6 @@ from typing import TYPE_CHECKING, Any, Optional
 import click
 from dagster_shared.plus.config import DagsterPlusCliConfig
 
-from dagster_dg_cli.cli.api.shared import (
-    DgApiError,
-    get_default_error_mapping,
-    get_graphql_error_mappings,
-)
-
 if TYPE_CHECKING:
     from dagster_cloud_cli.commands.ci.state import LocationState
 
@@ -96,7 +90,13 @@ class DagsterPlusGraphQLClient(IGraphQLClient):
         # defer for import performance
         from gql import gql
 
-        from dagster_dg_cli.cli.api.shared import get_graphql_error_types
+        # Import error handling classes locally to avoid circular imports
+        from dagster_dg_cli.cli.api.shared import (
+            DgApiError,
+            get_default_error_mapping,
+            get_graphql_error_mappings,
+            get_graphql_error_types,
+        )
 
         result = self.client.execute(gql(query), variable_values=dict(variables or {}))
         value = next(iter(result.values()))
