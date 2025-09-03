SWE-bench-repos/dagster-io__dagster

[computation] initial computation class and tests

python_modules/dagster/dagster/_core/definitions/assets/definition/computation.py
@@ -1,0 +1,135 @@
+from collections.abc import Iterator, Mapping, Sequence
+from typing import TYPE_CHECKING, Callable, Union
+
+from dagster_shared import check
+from dagster_shared.record import record
+from typing_extensions import TypeAlias
+
+from dagster._core.definitions.asset_checks.asset_check_result import AssetCheckResult
+from dagster._core.definitions.asset_checks.asset_check_spec import AssetCheckSpec
+from dagster._core.definitions.asset_key import AssetKey
+from dagster._core.definitions.assets.definition.asset_effect import AssetEffect, Effect
+from dagster._core.definitions.assets.definition.asset_spec import AssetSpec
+from dagster._core.definitions.assets.definition.assets_definition import AssetsDefinition
+from dagster._core.definitions.decorators.decorator_assets_definition_builder import (
+    build_and_validate_named_ins,
+)
+from dagster._core.definitions.decorators.op_decorator import DecoratedOpFunction
+from dagster._core.definitions.inference import get_config_param_type
+from dagster._core.definitions.node_definition import NodeDefinition
+from dagster._core.definitions.op_definition import OpDefinition
+from dagster._core.definitions.result import AssetResult
+from dagster.components.resolved.core_models import OpSpec
+
+if TYPE_CHECKING:
+    from dagster._core.definitions.assets.definition.asset_dep import AssetDep
+
+ComputationFn: TypeAlias = Callable[..., Iterator[Union[AssetResult, AssetCheckResult]]]
+
+
+@record
+class Computation:
+    """Generalized class for represting a single computation that has effects on entities in the
+    asset graph.
+
+    For now, this is a simple translation layer that swiftly gets turned into an AssetsDefinition,
+    but in the future this relationship will flip, and this Computation class will take over the
+    role of AssetsDefinition in the internals of the system.
+    """
+
+    node_def: NodeDefinition
+    input_mappings: Mapping[str, AssetKey]
+    output_mappings: Mapping[str, Effect]
+
+    can_subset: bool
+
+    @staticmethod
+    def from_fn(
+        fn: ComputationFn, *, op_spec: OpSpec, effects: Sequence[Effect], can_subset: bool
+    ) -> "Computation":
+        """Create a computation from a single function. Output definitions are created from the
+        provided effects. Input definitions are inferred from the function signature and provided
+        dependencies. The underlying node will be an OpDefinition that wraps the provided function.
+
+        Args:
+            fn: The function to create a computation from.
+            op_spec: The spec for the op that will be created.
+            effects: The effects that the function will have when executed.
+            can_subset: Whether the computation can be subsetted.
+        """
+        wrapped_fn = DecoratedOpFunction(fn)
+
+        # construct inputs from the deps of the specs and the params of the function
+        deps: set[AssetDep] = set().union(*(effect.spec.deps for effect in effects))
+        if not can_subset:
+            # if the computation is not subsettable, then we can avoid creating inputs for
+            # any upstream assets that are produced by this computation and are not self
+            # dependencies
+            output_keys = {effect.spec.key for effect in effects}
+            deps = {
+                dep
+                for dep in deps
+                if dep.asset_key not in output_keys and dep.partition_mapping is None
+            }
+
+        named_ins = build_and_validate_named_ins(fn=fn, deps=deps, passed_asset_ins={})
+
+        # construct outputs from the specs that this function affects
+        output_mappings = {effect.spec.key.to_python_identifier(): effect for effect in effects}
+
+        config_type = get_config_param_type(fn)
+        op_def = OpDefinition(
+            compute_fn=wrapped_fn,
+            name=op_spec.name or wrapped_fn.name,
+            ins={named_in.input_name: named_in.input for named_in in named_ins.values()},
+            outs={
+                output_name: effect.to_out(can_subset=can_subset)
+                for output_name, effect in output_mappings.items()
+            },
+            description=op_spec.description,
+            tags=op_spec.tags,
+            config_schema=config_type.to_config_schema() if config_type else None,
+            required_resource_keys={arg.name for arg in wrapped_fn.get_resource_args()},
+            pool=op_spec.pool,
+        )
+        return Computation(
+            node_def=op_def,
+            input_mappings={named_in.input_name: key for key, named_in in named_ins.items()},
+            output_mappings=output_mappings,
+            can_subset=can_subset,
+        )
+
+    def to_assets_def(self) -> AssetsDefinition:
+        """Convert this Computation into an AssetsDefinition."""
+        execution_types = {
+            effect.execution_type
+            for effect in self.output_mappings.values()
+            if isinstance(effect, AssetEffect)
+        }
+        check.invariant(
+            len(execution_types) <= 1,
+            f"All output effects must have the same execution type, found: {execution_types}",
+        )
+        execution_type = next(iter(execution_types), None)
+
+        return AssetsDefinition(
+            node_def=self.node_def,
+            execution_type=execution_type,
+            specs=[
+                effect.spec
+                for effect in self.output_mappings.values()
+                if isinstance(effect, AssetEffect)
+            ],
+            keys_by_input_name={input_name: key for input_name, key in self.input_mappings.items()},
+            keys_by_output_name={
+                output_name: effect.spec.key
+                for output_name, effect in self.output_mappings.items()
+                if isinstance(effect.spec, AssetSpec)
+            },
+            check_specs_by_output_name={
+                output_name: effect.spec
+                for output_name, effect in self.output_mappings.items()
+                if isinstance(effect.spec, AssetCheckSpec)
+            },
+            can_subset=self.can_subset,
+        )

python_modules/dagster/dagster_tests/definitions_tests/test_computation.py
@@ -1,0 +1,211 @@
+from collections.abc import Iterator
+from typing import Annotated
+
+import dagster as dg
+import pytest
+from dagster._core.definitions.assets.definition.asset_effect import (
+    AssetCheckEffect,
+    AssetMaterializationEffect,
+    Effect,
+)
+from dagster._core.definitions.assets.definition.computation import Computation
+from dagster.components.resolved.core_models import OpSpec
+
+
+class MyConfig(dg.Config):
+    a: int
+    b: str
+
+
+class MyResource(dg.ConfigurableResource):
+    c: float
+    d: bool
+
+
+def test_computation_from_fn_no_params() -> None:
+    def _fn(): ...  # -> Iterator[dg.MaterializeResult]: ...
+
+    specs = [
+        dg.AssetSpec(key=dg.AssetKey(["prefix", "a"])),
+        dg.AssetCheckSpec(name="asset_check1", asset=dg.AssetKey(["prefix", "a"])),
+    ]
+    computation = Computation.from_fn(
+        fn=_fn,  # type: ignore
+        op_spec=OpSpec(name="test_op"),
+        effects=[Effect.from_coercible(spec) for spec in specs],
+        can_subset=False,
+    )
+
+    assert computation.node_def.name == "test_op"
+
+    # Test Outputs
+    output_defs = computation.node_def.output_dict
+    assert len(output_defs) == 2
+
+    asset_output = output_defs["prefix__a"]
+    assert asset_output.name == "prefix__a"
+    assert asset_output.dagster_type.is_any
+    assert asset_output.description is None
+    assert asset_output.is_required
+
+    asset_check_output = output_defs["prefix__a_asset_check1"]
+    assert asset_check_output.name == "prefix__a_asset_check1"
+    assert asset_check_output.dagster_type.is_nothing
+    assert asset_check_output.description is None
+    assert asset_check_output.is_required
+
+    assert len(computation.output_mappings) == 2
+    assert computation.output_mappings["prefix__a"] == AssetMaterializationEffect(
+        spec=dg.AssetSpec(key=dg.AssetKey(["prefix", "a"]))
+    )
+    assert computation.output_mappings["prefix__a_asset_check1"] == AssetCheckEffect(
+        spec=dg.AssetCheckSpec(name="asset_check1", asset=dg.AssetKey(["prefix", "a"]))
+    )
+
+    # Test Inputs
+    input_defs = computation.node_def.input_defs
+    assert len(input_defs) == 0
+
+
+def test_computation_from_fn_with_complex_deps_and_additional_args() -> None:
+    def _fn(
+        context: dg.AssetExecutionContext,
+        config: MyConfig,
+        my_res: MyResource,
+        # asset inputs
+        a: int,
+        b: str,
+    ):  # -> Iterator[Union[dg.MaterializeResult, dg.AssetCheckResult]]:
+        yield dg.MaterializeResult(asset_key="asset1", value=1)
+        yield dg.MaterializeResult(asset_key="asset2")
+        yield dg.MaterializeResult(asset_key="asset3", value=3)
+        yield dg.AssetCheckResult(asset_key="asset1", passed=True)
+        yield dg.AssetCheckResult(asset_key="asset2", passed=True)
+
+    specs = [
+        dg.AssetSpec(key="asset1", deps=["a"]).with_dagster_type(int),
+        dg.AssetSpec(key="asset2", deps=["a", "b"]),
+        dg.AssetSpec(key="asset3", deps=["asset1", "asset2", "b", "d"]),
+        dg.AssetCheckSpec(name="asset_check1", asset="asset1", additional_deps=["a", "c"]),
+        dg.AssetCheckSpec(name="asset_check2", asset="asset2"),
+    ]
+    computation = Computation.from_fn(
+        fn=_fn,
+        op_spec=OpSpec(name="test_op"),
+        effects=[Effect.from_coercible(spec) for spec in specs],
+        can_subset=False,
+    )
+
+    assert computation.node_def.name == "test_op"
+
+    # Test Outputs
+    output_defs = computation.node_def.output_dict
+    assert len(output_defs) == 5
+
+    asset1_output = output_defs["asset1"]
+    assert asset1_output.name == "asset1"
+    assert asset1_output.dagster_type.display_name == "Int"
+
+    asset2_output = output_defs["asset2"]
+    assert asset2_output.name == "asset2"
+    assert asset2_output.dagster_type.is_any
+
+    asset3_output = output_defs["asset3"]
+    assert asset3_output.name == "asset3"
+    assert asset3_output.dagster_type.is_any
+
+    asset_check1_output = output_defs["asset1_asset_check1"]
+    assert asset_check1_output.name == "asset1_asset_check1"
+    assert asset_check1_output.dagster_type.is_nothing
+
+    asset_check2_output = output_defs["asset2_asset_check2"]
+    assert asset_check2_output.name == "asset2_asset_check2"
+    assert asset_check2_output.dagster_type.is_nothing
+
+    # Test Inputs
+    input_defs = computation.node_def.input_dict
+    assert len(input_defs) == 4
+
+    a_input = input_defs["a"]
+    assert a_input.dagster_type.display_name == "Int"
+
+    b_input = input_defs["b"]
+    assert b_input.dagster_type.display_name == "String"
+
+    c_input = input_defs["c"]
+    assert c_input.dagster_type.is_nothing
+
+    d_input = input_defs["d"]
+    assert d_input.dagster_type.is_nothing
+
+    # Test Execution
+    @dg.asset
+    def a() -> int:
+        return 1
+
+    @dg.asset
+    def b() -> str:
+        return "b"
+
+    assets_def = computation.to_assets_def()
+    result = dg.materialize_to_memory(
+        [a, b, assets_def],
+        resources={"my_res": MyResource(c=1, d=True)},
+        run_config=dg.RunConfig(ops={"test_op": {"config": {"a": 1, "b": "b"}}}),
+    )
+    assert result.success
+
+
+@pytest.mark.skip
+def test_computation_from_fn_with_complex_deps_and_asset_ins() -> None:
+    # currently not supported
+    def _fn(
+        config: MyConfig,
+        my_res: MyResource,
+        a: int,
+        b_renamed: Annotated[str, dg.AssetIn(key=dg.AssetKey("b"))],
+    ) -> Iterator[dg.MaterializeResult]: ...
+
+
+def test_computation_from_fn_spec_properties() -> None:
+    def _fn(): ...  # -> Iterator[dg.MaterializeResult]: ...
+
+    metadata = {"foo": 1, "bar": "baz"}
+    tags = {"test_tag": "test_value"}
+    specs = [
+        dg.AssetSpec(key="asset1", metadata=metadata, code_version="1a"),
+        dg.AssetCheckSpec(name="asset_check1", metadata=metadata, asset="asset1"),
+    ]
+    effects = [Effect.from_coercible(spec) for spec in specs]
+    computation = Computation.from_fn(
+        fn=_fn,  # type: ignore
+        op_spec=OpSpec(
+            name="test_op",
+            description="test_description",
+            tags=tags,
+            pool="test_pool",
+        ),
+        effects=effects,
+        can_subset=False,
+    )
+
+    # Test Node Properties
+    assert computation.node_def.name == "test_op"
+    assert computation.node_def.description == "test_description"
+    assert computation.node_def.tags == {"test_tag": "test_value"}
+    assert computation.node_def.pools == {"test_pool"}
+
+    # Test Outputs
+    output_defs = computation.node_def.output_dict
+    assert len(output_defs) == 2
+
+    asset_output = output_defs["asset1"]
+    assert asset_output.name == "asset1"
+    assert asset_output.description is None
+    assert asset_output.code_version == "1a"
+    assert asset_output.metadata == metadata
+
+    asset_check_output = output_defs["asset1_asset_check1"]
+    assert asset_check_output.name == "asset1_asset_check1"
+    assert asset_check_output.description is None
+    assert asset_check_output.metadata == metadata

dagster-io/dagster

python_modules/dagster/dagster/_core/definitions/assets/definition/computation.py
@@ -1,0 +1,135 @@
+from collections.abc import Iterator, Mapping, Sequence
+from typing import TYPE_CHECKING, Callable, Union
+
+from dagster_shared import check
+from dagster_shared.record import record
+from typing_extensions import TypeAlias
+
+from dagster._core.definitions.asset_checks.asset_check_result import AssetCheckResult
+from dagster._core.definitions.asset_checks.asset_check_spec import AssetCheckSpec
+from dagster._core.definitions.asset_key import AssetKey
+from dagster._core.definitions.assets.definition.asset_spec import AssetSpec
+from dagster._core.definitions.assets.definition.assets_definition import AssetsDefinition
+from dagster._core.definitions.assets.definition.entity_effect import AssetEffect, EntityEffect
+from dagster._core.definitions.decorators.decorator_assets_definition_builder import (
+    build_and_validate_named_ins,
+)
+from dagster._core.definitions.decorators.op_decorator import DecoratedOpFunction
+from dagster._core.definitions.inference import get_config_param_type
+from dagster._core.definitions.node_definition import NodeDefinition
+from dagster._core.definitions.op_definition import OpDefinition
+from dagster._core.definitions.result import AssetResult
+from dagster.components.resolved.core_models import OpSpec
+
+if TYPE_CHECKING:
+    from dagster._core.definitions.assets.definition.asset_dep import AssetDep
+
+ComputationFn: TypeAlias = Callable[..., Iterator[Union[AssetResult, AssetCheckResult]]]
+
+
+@record
+class Computation:
+    """Generalized class for represting a single computation that has effects on entities in the
+    asset graph.
+
+    For now, this is a simple translation layer that swiftly gets turned into an AssetsDefinition,
+    but in the future this relationship will flip, and this Computation class will take over the
+    role of AssetsDefinition in the internals of the system.
+    """
+
+    node_def: NodeDefinition
+    input_mappings: Mapping[str, AssetKey]
+    output_mappings: Mapping[str, EntityEffect]
+
+    can_subset: bool
+
+    @staticmethod
+    def from_fn(
+        fn: ComputationFn, *, op_spec: OpSpec, effects: Sequence[EntityEffect], can_subset: bool
+    ) -> "Computation":
+        """Create a computation from a single function. Output definitions are created from the
+        provided effects. Input definitions are inferred from the function signature and provided
+        dependencies. The underlying node will be an OpDefinition that wraps the provided function.
+
+        Args:
+            fn: The function to create a computation from.
+            op_spec: The spec for the op that will be created.
+            effects: The effects that the function will have when executed.
+            can_subset: Whether the computation can be subsetted.
+        """
+        wrapped_fn = DecoratedOpFunction(fn)
+
+        # construct inputs from the deps of the specs and the params of the function
+        deps: set[AssetDep] = set().union(*(effect.spec.deps for effect in effects))
+        if not can_subset:
+            # if the computation is not subsettable, then we can avoid creating inputs for
+            # any upstream assets that are produced by this computation and are not self
+            # dependencies
+            output_keys = {effect.spec.key for effect in effects}
+            deps = {
+                dep
+                for dep in deps
+                if dep.asset_key not in output_keys and dep.partition_mapping is None
+            }
+
+        named_ins = build_and_validate_named_ins(fn=fn, deps=deps, passed_asset_ins={})
+
+        # construct outputs from the specs that this function affects
+        output_mappings = {effect.spec.key.to_python_identifier(): effect for effect in effects}
+
+        config_type = get_config_param_type(fn)
+        op_def = OpDefinition(
+            compute_fn=wrapped_fn,
+            name=op_spec.name or wrapped_fn.name,
+            ins={named_in.input_name: named_in.input for named_in in named_ins.values()},
+            outs={
+                output_name: effect.to_out(can_subset=can_subset)
+                for output_name, effect in output_mappings.items()
+            },
+            description=op_spec.description,
+            tags=op_spec.tags,
+            config_schema=config_type.to_config_schema() if config_type else None,
+            required_resource_keys={arg.name for arg in wrapped_fn.get_resource_args()},
+            pool=op_spec.pool,
+        )
+        return Computation(
+            node_def=op_def,
+            input_mappings={named_in.input_name: key for key, named_in in named_ins.items()},
+            output_mappings=output_mappings,
+            can_subset=can_subset,
+        )
+
+    def to_assets_def(self) -> AssetsDefinition:
+        """Convert this Computation into an AssetsDefinition."""
+        execution_types = {
+            effect.execution_type
+            for effect in self.output_mappings.values()
+            if isinstance(effect, AssetEffect)
+        }
+        check.invariant(
+            len(execution_types) <= 1,
+            f"All output effects must have the same execution type, found: {execution_types}",
+        )
+        execution_type = next(iter(execution_types), None)
+
+        return AssetsDefinition(
+            node_def=self.node_def,
+            execution_type=execution_type,
+            specs=[
+                effect.spec
+                for effect in self.output_mappings.values()
+                if isinstance(effect, AssetEffect)
+            ],
+            keys_by_input_name={input_name: key for input_name, key in self.input_mappings.items()},
+            keys_by_output_name={
+                output_name: effect.spec.key
+                for output_name, effect in self.output_mappings.items()
+                if isinstance(effect.spec, AssetSpec)
+            },
+            check_specs_by_output_name={
+                output_name: effect.spec
+                for output_name, effect in self.output_mappings.items()
+                if isinstance(effect.spec, AssetCheckSpec)
+            },
+            can_subset=self.can_subset,
+        )

python_modules/dagster/dagster_tests/definitions_tests/test_computation.py
@@ -1,0 +1,211 @@
+from collections.abc import Iterator
+from typing import Annotated
+
+import dagster as dg
+import pytest
+from dagster._core.definitions.assets.definition.computation import Computation
+from dagster._core.definitions.assets.definition.entity_effect import (
+    AssetCheckEffect,
+    AssetMaterializationEffect,
+    EntityEffect,
+)
+from dagster.components.resolved.core_models import OpSpec
+
+
+class MyConfig(dg.Config):
+    a: int
+    b: str
+
+
+class MyResource(dg.ConfigurableResource):
+    c: float
+    d: bool
+
+
+def test_computation_from_fn_no_params() -> None:
+    def _fn(): ...  # -> Iterator[dg.MaterializeResult]: ...
+
+    specs = [
+        dg.AssetSpec(key=dg.AssetKey(["prefix", "a"])),
+        dg.AssetCheckSpec(name="asset_check1", asset=dg.AssetKey(["prefix", "a"])),
+    ]
+    computation = Computation.from_fn(
+        fn=_fn,  # type: ignore
+        op_spec=OpSpec(name="test_op"),
+        effects=[EntityEffect.from_coercible(spec) for spec in specs],
+        can_subset=False,
+    )
+
+    assert computation.node_def.name == "test_op"
+
+    # Test Outputs
+    output_defs = computation.node_def.output_dict
+    assert len(output_defs) == 2
+
+    asset_output = output_defs["prefix__a"]
+    assert asset_output.name == "prefix__a"
+    assert asset_output.dagster_type.is_any
+    assert asset_output.description is None
+    assert asset_output.is_required
+
+    asset_check_output = output_defs["prefix__a_asset_check1"]
+    assert asset_check_output.name == "prefix__a_asset_check1"
+    assert asset_check_output.dagster_type.is_nothing
+    assert asset_check_output.description is None
+    assert asset_check_output.is_required
+
+    assert len(computation.output_mappings) == 2
+    assert computation.output_mappings["prefix__a"] == AssetMaterializationEffect(
+        spec=dg.AssetSpec(key=dg.AssetKey(["prefix", "a"]))
+    )
+    assert computation.output_mappings["prefix__a_asset_check1"] == AssetCheckEffect(
+        spec=dg.AssetCheckSpec(name="asset_check1", asset=dg.AssetKey(["prefix", "a"]))
+    )
+
+    # Test Inputs
+    input_defs = computation.node_def.input_defs
+    assert len(input_defs) == 0
+
+
+def test_computation_from_fn_with_complex_deps_and_additional_args() -> None:
+    def _fn(
+        context: dg.AssetExecutionContext,
+        config: MyConfig,
+        my_res: MyResource,
+        # asset inputs
+        a: int,
+        b: str,
+    ):  # -> Iterator[Union[dg.MaterializeResult, dg.AssetCheckResult]]:
+        yield dg.MaterializeResult(asset_key="asset1", value=1)
+        yield dg.MaterializeResult(asset_key="asset2")
+        yield dg.MaterializeResult(asset_key="asset3", value=3)
+        yield dg.AssetCheckResult(asset_key="asset1", passed=True)
+        yield dg.AssetCheckResult(asset_key="asset2", passed=True)
+
+    specs = [
+        dg.AssetSpec(key="asset1", deps=["a"]).with_dagster_type(int),
+        dg.AssetSpec(key="asset2", deps=["a", "b"]),
+        dg.AssetSpec(key="asset3", deps=["asset1", "asset2", "b", "d"]),
+        dg.AssetCheckSpec(name="asset_check1", asset="asset1", additional_deps=["a", "c"]),
+        dg.AssetCheckSpec(name="asset_check2", asset="asset2"),
+    ]
+    computation = Computation.from_fn(
+        fn=_fn,
+        op_spec=OpSpec(name="test_op"),
+        effects=[EntityEffect.from_coercible(spec) for spec in specs],
+        can_subset=False,
+    )
+
+    assert computation.node_def.name == "test_op"
+
+    # Test Outputs
+    output_defs = computation.node_def.output_dict
+    assert len(output_defs) == 5
+
+    asset1_output = output_defs["asset1"]
+    assert asset1_output.name == "asset1"
+    assert asset1_output.dagster_type.display_name == "Int"
+
+    asset2_output = output_defs["asset2"]
+    assert asset2_output.name == "asset2"
+    assert asset2_output.dagster_type.is_any
+
+    asset3_output = output_defs["asset3"]
+    assert asset3_output.name == "asset3"
+    assert asset3_output.dagster_type.is_any
+
+    asset_check1_output = output_defs["asset1_asset_check1"]
+    assert asset_check1_output.name == "asset1_asset_check1"
+    assert asset_check1_output.dagster_type.is_nothing
+
+    asset_check2_output = output_defs["asset2_asset_check2"]
+    assert asset_check2_output.name == "asset2_asset_check2"
+    assert asset_check2_output.dagster_type.is_nothing
+
+    # Test Inputs
+    input_defs = computation.node_def.input_dict
+    assert len(input_defs) == 4
+
+    a_input = input_defs["a"]
+    assert a_input.dagster_type.display_name == "Int"
+
+    b_input = input_defs["b"]
+    assert b_input.dagster_type.display_name == "String"
+
+    c_input = input_defs["c"]
+    assert c_input.dagster_type.is_nothing
+
+    d_input = input_defs["d"]
+    assert d_input.dagster_type.is_nothing
+
+    # Test Execution
+    @dg.asset
+    def a() -> int:
+        return 1
+
+    @dg.asset
+    def b() -> str:
+        return "b"
+
+    assets_def = computation.to_assets_def()
+    result = dg.materialize_to_memory(
+        [a, b, assets_def],
+        resources={"my_res": MyResource(c=1, d=True)},
+        run_config=dg.RunConfig(ops={"test_op": {"config": {"a": 1, "b": "b"}}}),
+    )
+    assert result.success
+
+
+@pytest.mark.skip
+def test_computation_from_fn_with_complex_deps_and_asset_ins() -> None:
+    # currently not supported
+    def _fn(
+        config: MyConfig,
+        my_res: MyResource,
+        a: int,
+        b_renamed: Annotated[str, dg.AssetIn(key=dg.AssetKey("b"))],
+    ) -> Iterator[dg.MaterializeResult]: ...
+
+
+def test_computation_from_fn_spec_properties() -> None:
+    def _fn(): ...  # -> Iterator[dg.MaterializeResult]: ...
+
+    metadata = {"foo": 1, "bar": "baz"}
+    tags = {"test_tag": "test_value"}
+    specs = [
+        dg.AssetSpec(key="asset1", metadata=metadata, code_version="1a"),
+        dg.AssetCheckSpec(name="asset_check1", metadata=metadata, asset="asset1"),
+    ]
+    effects = [EntityEffect.from_coercible(spec) for spec in specs]
+    computation = Computation.from_fn(
+        fn=_fn,  # type: ignore
+        op_spec=OpSpec(
+            name="test_op",
+            description="test_description",
+            tags=tags,
+            pool="test_pool",
+        ),
+        effects=effects,
+        can_subset=False,
+    )
+
+    # Test Node Properties
+    assert computation.node_def.name == "test_op"
+    assert computation.node_def.description == "test_description"
+    assert computation.node_def.tags == {"test_tag": "test_value"}
+    assert computation.node_def.pools == {"test_pool"}
+
+    # Test Outputs
+    output_defs = computation.node_def.output_dict
+    assert len(output_defs) == 2
+
+    asset_output = output_defs["asset1"]
+    assert asset_output.name == "asset1"
+    assert asset_output.description is None
+    assert asset_output.code_version == "1a"
+    assert asset_output.metadata == metadata
+
+    asset_check_output = output_defs["asset1_asset_check1"]
+    assert asset_check_output.name == "asset1_asset_check1"
+    assert asset_check_output.description is None
+    assert asset_check_output.metadata == metadata