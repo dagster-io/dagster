995ec8eb3087036e89d5235873609bce44bb0c99
gibsondan <daniel@elementl.com>
2025-09-17 15:11:02 -0500

    Add AllNewExecutedWithTagsCondition, to correctly handle filtering out runs when multiple runs by tag happen between ticks (#32141)
    ## Summary & Motivation
    
    Instead of LatestRunExecutedWithTagsCondition, which only looks at the
    most recent run even if there have been multiple runs since the last DA
    tick, a condition that checks all new materializations since the
    previous tick and returns all partitions that match the supplied tags.
    
    ## How I Tested These Changes
    New test case
    
    ## Changelog
    Added `AutomationCondition.all_new_executed_with_tags`, which allows
    automation conditions to be filtered to partitions that have been
    materialized since the last tick from runs with certain tags. This
    condition can be used to require or prevent certain run tags from
    triggering downstream declarative automation conditions.

--- python_modules/dagster/dagster/_core/definitions/declarative_automation/operands/__init__.py
+++ python_modules/dagster/dagster/_core/definitions/declarative_automation/operands/__init__.py
@@ -6,11 +6,15 @@ from dagster._core.definitions.declarative_automation.operands.operands import (
     ExecutionFailedAutomationCondition as ExecutionFailedAutomationCondition,
     InitialEvaluationCondition as InitialEvaluationCondition,
     InLatestTimeWindowCondition as InLatestTimeWindowCondition,
-    LatestRunExecutedWithRootTargetCondition as LatestRunExecutedWithRootTargetCondition,
-    LatestRunExecutedWithTagsCondition as LatestRunExecutedWithTagsCondition,
     MissingAutomationCondition as MissingAutomationCondition,
     NewlyRequestedCondition as NewlyRequestedCondition,
     NewlyUpdatedCondition as NewlyUpdatedCondition,
     RunInProgressAutomationCondition as RunInProgressAutomationCondition,
     WillBeRequestedCondition as WillBeRequestedCondition,
+)
+from dagster._core.definitions.declarative_automation.operands.run_operands import (
+    AllNewUpdatesHaveRunTagsCondition as AllNewUpdatesHaveRunTagsCondition,
+    AnyNewUpdateHasRunTagsCondition as AnyNewUpdateHasRunTagsCondition,
+    LatestRunExecutedWithRootTargetCondition as LatestRunExecutedWithRootTargetCondition,
+    LatestRunExecutedWithTagsCondition as LatestRunExecutedWithTagsCondition,
 )
