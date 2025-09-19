3885c40d16f3021d3965dc0461a01a6ed62c59ee
nikki everett <nikki@dagsterlabs.com>
2025-09-19 12:54:58 -0500

    add to other exclude list
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

--- python_modules/automation/automation/dagster_docs/exclude_lists.py
+++ python_modules/automation/automation/dagster_docs/exclude_lists.py
@@ -26,6 +26,8 @@ EXCLUDE_MISSING_RST = {
     "dagster.components.scaffold.scaffold.Scaffolder",
     "dagster.components.lib.sql_component.sql_component.SqlComponent",
     "dagster.components.core.component_tree.ComponentTree",
+    "dagster.components.testing.create_defs_folder_sandbox",
+    "dagster.components.testing.DefsFolderSandbox",
     # Core internal functionality
     "dagster._core.definitions.definitions_class.create_repository_using_definitions_args",
     # Core storage and compute management - internal functionality
