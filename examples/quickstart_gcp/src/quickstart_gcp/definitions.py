9aa7022e06518bd280ad5b4728c22dc7c6341f76
Colton Padden <colton@dagsterlabs.com>
2025-09-22 14:27:01 -0400

    [nux] make quickstart_gcp compatible with uv / dg

--- examples/quickstart_gcp/src/quickstart_gcp/definitions.py
+++ examples/quickstart_gcp/src/quickstart_gcp/definitions.py
@@ -1,13 +1,11 @@
-from dagster import (
-    Definitions,
-    EnvVar,
-    ScheduleDefinition,
-    define_asset_job,
-    load_assets_from_package_module,
-)
+from dagster import Definitions, EnvVar, ScheduleDefinition, define_asset_job
 from dagster_gcp_pandas import BigQueryPandasIOManager
 
-from . import assets
+from .defs.assets import (
+    hackernews_topstories,
+    hackernews_topstories_word_cloud,
+    hackernews_topstory_ids,
+)
 
 daily_refresh_schedule = ScheduleDefinition(
     job=define_asset_job(name="all_assets_job"), cron_schedule="0 0 * * *"
@@ -15,7 +13,11 @@ daily_refresh_schedule = ScheduleDefinition(
 
 
 defs = Definitions(
-    assets=load_assets_from_package_module(assets),
+    assets=[
+        hackernews_topstory_ids,
+        hackernews_topstories,
+        hackernews_topstories_word_cloud,
+    ],
     resources={
         # Read about using environment variables and secrets in Dagster:
         #   https://docs.dagster.io/guides/dagster/using-environment-variables-and-secrets
