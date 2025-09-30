c2afc6ee93472b18acac3fd8b7c9e14526bb72e9
Kyle P <kylespalermo@users.noreply.github.com>
2025-09-30 10:43:52 -0700

    client module disables slash hotkey
    Check if Scout is in focus and then, if so, stop "/" event from opening Algolia window.
    
    I explored opening a PR with docusaurus to add more native support for Algolia keyboardShortcuts but learned docusaurus is still running an Algolia version that doesn't support that param, and so we'll likely need to wait. Because this only blocks events that fire within scout widget, I think it's pretty harmless.

---
+++ docs/src/clientModules/disableSlashSearch.js
@@ -0,0 +1,14 @@
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
+if (ExecutionEnvironment.canUseDOM) {
+  document.addEventListener('keydown', (event) => {
+    if (event.key === '/' && !event.ctrlKey && !event.metaKey) {
+      const activeElement = document.activeElement;
+
+      // Check if focus is within scout-copilot element
+      if (activeElement?.closest('scout-copilot')) {
+        event.stopPropagation();
+      }
+    }
+  }, true);
+}
