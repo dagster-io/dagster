dagster-io/dagster

commit bae286c9f9990d9043b5a6393a78687a15a8f8eb

[ui] Update lint in OSS packages (#32255)
## Summary & Motivation

Update lint in OSS, clean up resulting errors.

Also updating Storybook in `ui-core`​ and `ui-components`​ to eliminate some remaining dependency warnings that show up during yarn install.

## How I Tested These Changes

TS, lint.

Load storybook in `ui-core`​ and `ui-components`​, verify that it loads and renders correctly.

js_modules/dagster-ui/packages/ui-components/src/components/configeditor/codemirror-yaml/mode.tsx
@@ -39,18 +39,22 @@ interface IParseState {
 // Helper methods that mutate parser state. These must return new JavaScript objects.
 //
 function parentsPoppingItemsDeeperThan(parents: IParseStateParent[], indent: number) {
-  while (parents.length > 0 && parents[parents.length - 1]!.indent >= indent) {
+  while (parents.length > 0) {
+    const immediateParent = parents[parents.length - 1];
+    if (!immediateParent || immediateParent.indent >= indent) {
+      break;
+    }
     parents = parents.slice(0, parents.length - 1);
   }
   return parents;
 }
 
 function parentsAddingChildKeyToLast(parents: IParseStateParent[], key: string) {
-  if (parents.length === 0) {
+  const immediateParent = parents[parents.length - 1];
+  if (!immediateParent) {
     return [];
   }
 
-  const immediateParent = parents[parents.length - 1]!;
   return [
     ...parents.slice(0, parents.length - 1),
     {
@@ -235,8 +239,8 @@ const defineYamlMode = () => {
         // in case the dict key has subkeys.
         if (!state.inValue) {
           const match = stream.match(RegExps.DICT_KEY);
-          if (match) {
-            const key = match[0]!;
+          if (match && match[0]) {
+            const key = match[0];
             const keyIndent = stream.pos - key.length;
             state.parents = parentsAddingChildKeyAtIndent(state.parents, key, keyIndent);
             return 'atom';
@@ -252,8 +256,8 @@ const defineYamlMode = () => {
           const match = !stream.string.match(/[^\s]:[^\s]/)
             ? stream.match(RegExps.DICT_KEY)
             : false;
-          if (match) {
-            const key = match[0]!;
+          if (match && match[0]) {
+            const key = match[0];
             const keyIndent = stream.pos - key.length;
             state.inValue = false;
             state.parents = parentsAddingChildKeyAtIndent(state.parents, key, keyIndent);
@@ -280,7 +284,7 @@ const defineYamlMode = () => {
                     ? stream.match(/^[^,\}]+/)
                     : stream.match(/^.+$/);
             }
-            const value = match ? match[0]! : '';
+            const value = match && match[0] ? match[0] : '';
             if (value.match(RegExps.VARIABLE)) {
               result = 'variable-2';
             } else if (value.match(RegExps.NUMBER)) {
@@ -602,7 +606,7 @@ function findAutocompletionContext(
   // Tracks the type key to be used for the next depth level
   // Used for Map config types, which specify the type key for their values, otherwise is null
   let nextTypeKey: string | null =
-    type.__typename === 'MapConfigType' ? type.typeParamKeys[1]! : null;
+    type.__typename === 'MapConfigType' && type.typeParamKeys[1] ? type.typeParamKeys[1] : null;
 
   if ((available || type.__typename === 'MapConfigType') && parents.length > 0) {
     for (const parent of parents) {
@@ -619,27 +623,27 @@ function findAutocompletionContext(
       const typeKey = nextTypeKey ? nextTypeKey : parentTypeDef?.configTypeKey;
       nextTypeKey = null;
 
-      let parentConfigType = schema.allConfigTypes.find((t) => t.key === typeKey)!;
+      let parentConfigType = schema.allConfigTypes.find((t) => t.key === typeKey);
 
       // If nullable, extract the inner type.
-      if (parentConfigType.__typename === 'NullableConfigType') {
+      if (parentConfigType?.__typename === 'NullableConfigType') {
         const innerType = parentConfigType.typeParamKeys[0];
-        parentConfigType = schema.allConfigTypes.find((t) => t.key === innerType)!;
+        parentConfigType = schema.allConfigTypes.find((t) => t.key === innerType);
       }
 
-      let childTypeKey = parentConfigType.key;
+      let childTypeKey = parentConfigType?.key;
       let childEntriesUnique = true;
 
-      inArray = parentConfigType.__typename === 'ArrayConfigType';
+      inArray = parentConfigType?.__typename === 'ArrayConfigType';
       if (inArray) {
-        childTypeKey = parentConfigType.typeParamKeys[0]!;
+        childTypeKey = parentConfigType?.typeParamKeys[0];
         childEntriesUnique = false;
       }
 
       // Maps provide no direct autocompletions, but they do act as the closestMappingType,
       // meaning they show up in the schema sidebar
-      if (parentConfigType.__typename === 'MapConfigType') {
-        nextTypeKey = parentConfigType.typeParamKeys[1]!;
+      if (parentConfigType?.__typename === 'MapConfigType') {
+        nextTypeKey = parentConfigType.typeParamKeys[1] ?? '';
         closestMappingType = parentConfigType;
         available = [];
         continue;
@@ -881,7 +885,11 @@ export function findRangeInDocumentFromPath(
 function nodeAtPath(doc: yaml.Document, path: Array<string>) {
   let node: any = doc.contents;
   for (let i = 0; i < path.length; i++) {
-    const part = path[i]!;
+    const part = path[i];
+    if (!part) {
+      return null;
+    }
+
     if (node && node.type && node.type === 'PAIR') {
       node = node.value;
     }

commit 41b6146a12ddb7947cb3d611c472192e298106b2

fix break condition in parentsPoppingItemsDeeperThan preventing auto-complete from working
Summary:
Fixes an issue introduced in https://github.com/dagster-io/dagster/pull/32255. The old while loop condition was >=, so the break condition should be <, not >=.

Launchpad autocomplete for nested config fields now works as expected.

Fixed an issue introduced in the 1.11.12 release where auto-complete in the Launchpad for nested fields stopped working.

> Insert changelog entry or delete this section.

js_modules/dagster-ui/packages/ui-components/src/components/configeditor/codemirror-yaml/mode.tsx
@@ -41,7 +41,7 @@ interface IParseState {
 function parentsPoppingItemsDeeperThan(parents: IParseStateParent[], indent: number) {
   while (parents.length > 0) {
     const immediateParent = parents[parents.length - 1];
-    if (!immediateParent || immediateParent.indent >= indent) {
+    if (!immediateParent || immediateParent.indent < indent) {
       break;
     }
     parents = parents.slice(0, parents.length - 1);