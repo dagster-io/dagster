e0ba5fd0817c3a0a01f4f26554b80d8a26796186
Daniel Gibson <daniel@elementl.com>
2025-10-01 20:31:18 -0500

    combine two separate runConfigSchema queries that the launchpad was doing into one
    Summary:
    Once https://app.graphite.dev/github/pr/dagster-io/dagster/32365/Pass-in-asset-selection-to-launchpad-query lands, these two queries are essentially identical, and the underlying data can be somewhat expensive to fetch. So lets do it all in one big fragment?
    
    > Insert changelog entry or delete this section.

--- js_modules/dagster-ui/packages/ui-core/src/launchpad/types/LaunchpadAllowedRoot.types.ts
+++ js_modules/dagster-ui/packages/ui-core/src/launchpad/types/LaunchpadAllowedRoot.types.ts
@@ -65,10 +65,90 @@ export type LaunchpadRootQuery = {
       };
   runConfigSchemaOrError:
     | {__typename: 'InvalidSubsetError'}
-    | {__typename: 'ModeNotFoundError'}
+    | {__typename: 'ModeNotFoundError'; message: string}
     | {__typename: 'PipelineNotFoundError'}
     | {__typename: 'PythonError'}
-    | {__typename: 'RunConfigSchema'; rootDefaultYaml: string};
+    | {
+        __typename: 'RunConfigSchema';
+        rootDefaultYaml: string;
+        rootConfigType:
+          | {__typename: 'ArrayConfigType'; key: string}
+          | {__typename: 'CompositeConfigType'; key: string}
+          | {__typename: 'EnumConfigType'; key: string}
+          | {__typename: 'MapConfigType'; key: string}
+          | {__typename: 'NullableConfigType'; key: string}
+          | {__typename: 'RegularConfigType'; key: string}
+          | {__typename: 'ScalarUnionConfigType'; key: string};
+        allConfigTypes: Array<
+          | {
+              __typename: 'ArrayConfigType';
+              key: string;
+              description: string | null;
+              isSelector: boolean;
+              typeParamKeys: Array<string>;
+            }
+          | {
+              __typename: 'CompositeConfigType';
+              key: string;
+              description: string | null;
+              isSelector: boolean;
+              typeParamKeys: Array<string>;
+              fields: Array<{
+                __typename: 'ConfigTypeField';
+                name: string;
+                description: string | null;
+                isRequired: boolean;
+                configTypeKey: string;
+                defaultValueAsJson: string | null;
+              }>;
+            }
+          | {
+              __typename: 'EnumConfigType';
+              givenName: string;
+              key: string;
+              description: string | null;
+              isSelector: boolean;
+              typeParamKeys: Array<string>;
+              values: Array<{
+                __typename: 'EnumConfigValue';
+                value: string;
+                description: string | null;
+              }>;
+            }
+          | {
+              __typename: 'MapConfigType';
+              keyLabelName: string | null;
+              key: string;
+              description: string | null;
+              isSelector: boolean;
+              typeParamKeys: Array<string>;
+            }
+          | {
+              __typename: 'NullableConfigType';
+              key: string;
+              description: string | null;
+              isSelector: boolean;
+              typeParamKeys: Array<string>;
+            }
+          | {
+              __typename: 'RegularConfigType';
+              givenName: string;
+              key: string;
+              description: string | null;
+              isSelector: boolean;
+              typeParamKeys: Array<string>;
+            }
+          | {
+              __typename: 'ScalarUnionConfigType';
+              key: string;
+              scalarTypeKey: string;
+              nonScalarTypeKey: string;
+              description: string | null;
+              isSelector: boolean;
+              typeParamKeys: Array<string>;
+            }
+        >;
+      };
 };
 
 export type LaunchpadSessionPartitionSetsFragment = {
@@ -100,4 +180,109 @@ export type LaunchpadSessionPipelineFragment = {
   tags: Array<{__typename: 'PipelineTag'; key: string; value: string}>;
 };
 
-export const LaunchpadRootQueryVersion = 'f3ec9d433b578615d066d6f8e3eb6d4f651d230a9ab155113103d42868f9aec2';
+export type LaunchpadSessionRunConfigSchemaFragment_InvalidSubsetError = {
+  __typename: 'InvalidSubsetError';
+};
+
+export type LaunchpadSessionRunConfigSchemaFragment_ModeNotFoundError = {
+  __typename: 'ModeNotFoundError';
+  message: string;
+};
+
+export type LaunchpadSessionRunConfigSchemaFragment_PipelineNotFoundError = {
+  __typename: 'PipelineNotFoundError';
+};
+
+export type LaunchpadSessionRunConfigSchemaFragment_PythonError = {__typename: 'PythonError'};
+
+export type LaunchpadSessionRunConfigSchemaFragment_RunConfigSchema = {
+  __typename: 'RunConfigSchema';
+  rootDefaultYaml: string;
+  rootConfigType:
+    | {__typename: 'ArrayConfigType'; key: string}
+    | {__typename: 'CompositeConfigType'; key: string}
+    | {__typename: 'EnumConfigType'; key: string}
+    | {__typename: 'MapConfigType'; key: string}
+    | {__typename: 'NullableConfigType'; key: string}
+    | {__typename: 'RegularConfigType'; key: string}
+    | {__typename: 'ScalarUnionConfigType'; key: string};
+  allConfigTypes: Array<
+    | {
+        __typename: 'ArrayConfigType';
+        key: string;
+        description: string | null;
+        isSelector: boolean;
+        typeParamKeys: Array<string>;
+      }
+    | {
+        __typename: 'CompositeConfigType';
+        key: string;
+        description: string | null;
+        isSelector: boolean;
+        typeParamKeys: Array<string>;
+        fields: Array<{
+          __typename: 'ConfigTypeField';
+          name: string;
+          description: string | null;
+          isRequired: boolean;
+          configTypeKey: string;
+          defaultValueAsJson: string | null;
+        }>;
+      }
+    | {
+        __typename: 'EnumConfigType';
+        givenName: string;
+        key: string;
+        description: string | null;
+        isSelector: boolean;
+        typeParamKeys: Array<string>;
+        values: Array<{__typename: 'EnumConfigValue'; value: string; description: string | null}>;
+      }
+    | {
+        __typename: 'MapConfigType';
+        keyLabelName: string | null;
+        key: string;
+        description: string | null;
+        isSelector: boolean;
+        typeParamKeys: Array<string>;
+      }
+    | {
+        __typename: 'NullableConfigType';
+        key: string;
+        description: string | null;
+        isSelector: boolean;
+        typeParamKeys: Array<string>;
+      }
+    | {
+        __typename: 'RegularConfigType';
+        givenName: string;
+        key: string;
+        description: string | null;
+        isSelector: boolean;
+        typeParamKeys: Array<string>;
+      }
+    | {
+        __typename: 'ScalarUnionConfigType';
+        key: string;
+        scalarTypeKey: string;
+        nonScalarTypeKey: string;
+        description: string | null;
+        isSelector: boolean;
+        typeParamKeys: Array<string>;
+      }
+  >;
+};
+
+export type LaunchpadSessionRunConfigSchemaFragment =
+  | LaunchpadSessionRunConfigSchemaFragment_InvalidSubsetError
+  | LaunchpadSessionRunConfigSchemaFragment_ModeNotFoundError
+  | LaunchpadSessionRunConfigSchemaFragment_PipelineNotFoundError
+  | LaunchpadSessionRunConfigSchemaFragment_PythonError
+  | LaunchpadSessionRunConfigSchemaFragment_RunConfigSchema;
+
+export type LaunchpadSessionModeNotFoundFragment = {
+  __typename: 'ModeNotFoundError';
+  message: string;
+};
+
+export const LaunchpadRootQueryVersion = '527627287343c3a482b43445b66c55de58b667dadb36218161575caffbc78348';
