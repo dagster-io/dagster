4080a23d07b7a589fc16ad8881be1942af0ca8ca
Soeren Stahlmann <soeren.stahlmann@henkel.com>
2025-10-02 16:02:28 +0200

    add env vars to databricks pipe client when notebook task is used

--- python_modules/libraries/dagster-databricks/dagster_databricks/pipes.py
+++ python_modules/libraries/dagster-databricks/dagster_databricks/pipes.py
@@ -328,7 +328,15 @@ class PipesDatabricksClient(BasePipesDatabricksClient, TreatAsResourceParam):
                         f'Passing Pipes bootstrap parameters via Databricks parameters as "{key}.parameters". Make sure to use the PipesCliArgsParamsLoader in the task.'  # pyright: ignore[reportPossiblyUnboundVariable]
                     )
                     break
-
+                
+            # use env vars to pass pipes context in case of notebook_task
+            pipes_env_vars = session.get_bootstrap_env_vars()
+            if submit_task_dict.get("notebook_task"):
+                existing_params = submit_task_dict["notebook_task"].get("base_parameters", {})
+                # merge the existing parameters with the CLI arguments
+                existing_params = {**existing_params, **session.get_bootstrap_env_vars()}
+                submit_task_dict["notebook_task"]["base_parameters"] = existing_params
+                
         else:
             pipes_env_vars = session.get_bootstrap_env_vars()
 
