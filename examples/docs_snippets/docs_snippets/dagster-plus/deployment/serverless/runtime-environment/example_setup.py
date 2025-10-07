6b66f14fc5d3d45c8a6724b70ecac5a433a2f932
Dennis Hume <dennis@dagsterlabs.com>
2025-10-07 10:17:21 -0500

    Check

--- examples/docs_snippets/docs_snippets/dagster-plus/deployment/serverless/runtime-environment/example_setup.py
+++ examples/docs_snippets/docs_snippets/dagster-plus/deployment/serverless/runtime-environment/example_setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="quickstart_etl",
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(exclude=["quickstart_etl_tests"]),
     install_requires=[
         "dagster",
         "dagster-cloud",
