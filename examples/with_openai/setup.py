a6cbe9c9921f92dd92d458813e775e69d7073411
Daniel Gibson <daniel@elementl.com>
2025-10-08 15:18:02 -0500

    Remove hard pins from examples/with_openai example
    Load example locally
    
    > Insert changelog entry or delete this section.

--- examples/with_openai/setup.py
+++ examples/with_openai/setup.py
@@ -9,9 +9,9 @@ setup(
         "dagster-openai",
         "faiss-cpu==1.8.0",
         "filelock",
-        "langchain==0.3.7",
-        "langchain-community==0.3.5",
-        "langchain-openai==0.2.5",
+        "langchain<0.4",
+        "langchain-community<0.4",
+        "langchain-openai<0.4",
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
