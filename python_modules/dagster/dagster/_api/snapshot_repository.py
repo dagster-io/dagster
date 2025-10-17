be9ae69208a62e744d64fb38ce7f15c48aa7c7ad
Daniel Gibson <daniel@elementl.com>
2025-10-17 15:35:30 -0400

    set defer_snapshots=True in local dev when loading repositories
    Summary:
    The recent change that took out the streaming grpc call meant that calls that could previously return 100MB of data due to large repos and large job snapshots now can hit the grpc message limit. Use the defer_snapshots argument that breaks it into multiple grpc calls to work around this and make all our repository code logic use similar logic and limits.

--- python_modules/dagster/dagster/_api/snapshot_repository.py
+++ python_modules/dagster/dagster/_api/snapshot_repository.py
@@ -5,28 +5,38 @@ import dagster._check as check
 from dagster._core.errors import DagsterUserCodeProcessError
 from dagster._core.remote_representation.external_data import RepositoryErrorSnap, RepositorySnap
 from dagster._serdes import deserialize_value
+from dagster._utils.error import SerializableErrorInfo
 
 if TYPE_CHECKING:
     from dagster._core.remote_representation.code_location import CodeLocation
+    from dagster._core.remote_representation.external_data import JobDataSnap
     from dagster._grpc.client import DagsterGrpcClient
 
 
 def sync_get_external_repositories_data_grpc(
-    api_client: "DagsterGrpcClient", code_location: "CodeLocation"
-) -> Mapping[str, RepositorySnap]:
+    api_client: "DagsterGrpcClient", code_location: "CodeLocation", defer_snapshots: bool
+) -> Mapping[str, tuple[RepositorySnap, Mapping[str, "JobDataSnap"]]]:
     from dagster._core.remote_origin import RemoteRepositoryOrigin
     from dagster._core.remote_representation.code_location import CodeLocation
+    from dagster._core.remote_representation.external_data import (
+        extract_serialized_job_snap_from_serialized_job_data_snap,
+    )
 
     check.inst_param(code_location, "code_location", CodeLocation)
 
     repo_datas = {}
     for repository_name in code_location.repository_names:  # type: ignore
+        repo_origin = RemoteRepositoryOrigin(
+            code_location.origin,
+            repository_name,
+        )
         result = deserialize_value(
             api_client.external_repository(
                 remote_repository_origin=RemoteRepositoryOrigin(
                     code_location.origin,
                     repository_name,
-                )
+                ),
+                defer_snapshots=defer_snapshots,
             ),
             (RepositorySnap, RepositoryErrorSnap),
         )
@@ -34,32 +44,28 @@ def sync_get_external_repositories_data_grpc(
         if isinstance(result, RepositoryErrorSnap):
             raise DagsterUserCodeProcessError.from_error_info(result.error)
 
-        repo_datas[repository_name] = result
-    return repo_datas
+        job_data_snaps = {}
 
-
-async def gen_external_repositories_data_grpc(
-    api_client: "DagsterGrpcClient", code_location: "CodeLocation"
-) -> Mapping[str, RepositorySnap]:
-    from dagster._core.remote_origin import RemoteRepositoryOrigin
-    from dagster._core.remote_representation.code_location import CodeLocation
-
-    check.inst_param(code_location, "code_location", CodeLocation)
-
-    repo_datas = {}
-    for repository_name in code_location.repository_names:  # type: ignore
-        result = deserialize_value(
-            await api_client.gen_external_repository(
-                remote_repository_origin=RemoteRepositoryOrigin(
-                    code_location.origin,
-                    repository_name,
+        if not result.has_job_data():
+            for job_ref in result.get_job_refs():
+                job_response = api_client.external_job(
+                    repo_origin,
+                    job_ref.name,
                 )
-            ),
-            (RepositorySnap, RepositoryErrorSnap),
-        )
+                if not job_response.serialized_job_data:
+                    error = (
+                        deserialize_value(job_response.serialized_error, SerializableErrorInfo)
+                        if job_response.serialized_error
+                        else "no captured error"
+                    )
+                    raise Exception(
+                        f"Error fetching job data for {job_ref.name} in code server:\n{error}"
+                    )
+                job_data_snaps[job_ref.name] = (
+                    extract_serialized_job_snap_from_serialized_job_data_snap(
+                        job_response.serialized_job_data
+                    )
+                )
 
-        if isinstance(result, RepositoryErrorSnap):
-            raise DagsterUserCodeProcessError.from_error_info(result.error)
-
-        repo_datas[repository_name] = result
+        repo_datas[repository_name] = result, job_data_snaps
     return repo_datas
