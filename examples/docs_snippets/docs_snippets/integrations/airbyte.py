e18dc423dd0f4e3f24e09d121abbd38ad97039da
colton <cmpadden@users.noreply.github.com>
2025-10-24 13:57:55 -0400

    Revert "update airbyte revamp documentation and deprecation flag changes" (#32622)
    Reverts dagster-io/dagster#32568
    
    Seeing some test failures. Confirming that reverting this resolves it.

--- examples/docs_snippets/docs_snippets/integrations/airbyte.py
+++ examples/docs_snippets/docs_snippets/integrations/airbyte.py
@@ -1,21 +1,19 @@
-from dagster_airbyte import AirbyteWorkspace, build_airbyte_assets_definitions
+from dagster_airbyte import AirbyteResource, load_assets_from_airbyte_instance
 
 import dagster as dg
 
-# Connect to your OSS Airbyte instance
-airbyte_workspace = AirbyteWorkspace(
-    rest_api_base_url="http://localhost:8000/api/public/v1",
-    configuration_api_base_url="http://localhost:8000/api/v1",
-    workspace_id=dg.EnvVar("AIRBYTE_WORKSPACE_ID"),
-    # If using basic auth, include username and password:
-    username="airbyte",
-    password=dg.EnvVar("AIRBYTE_PASSWORD"),
+# Load all assets from your Airbyte instance
+airbyte_assets = load_assets_from_airbyte_instance(
+    # Connect to your OSS Airbyte instance
+    AirbyteResource(
+        host="localhost",
+        port="8000",
+        # If using basic auth, include username and password:
+        username="airbyte",
+        password=dg.EnvVar("AIRBYTE_PASSWORD"),
+    )
 )
 
-# Load all assets from your Airbyte workspace
-airbyte_assets = build_airbyte_assets_definitions(workspace=airbyte_workspace)
-
 defs = dg.Definitions(
-    assets=airbyte_assets,
-    resources={"airbyte": airbyte_workspace},
+    assets=[airbyte_assets],
 )
