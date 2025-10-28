2fd7c438bf7f39f16e6e476fbe77db118b10d744
Maxime Armstrong <maxime@retain.ai>
2025-10-28 15:47:41 -0400

    Add test

--- python_modules/libraries/dagster-cloud-cli/dagster_cloud_cli/commands/integration/atlan/__init__.py
+++ python_modules/libraries/dagster-cloud-cli/dagster_cloud_cli/commands/integration/atlan/__init__.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from typer import Argument, Typer
 
-from dagster_cloud_cli import gql, ui
+from dagster_cloud_cli import gql
 from dagster_cloud_cli.config_utils import dagster_cloud_options
 
 app = Typer(help="Customize your Atlan integration.")
@@ -24,15 +24,8 @@ def set_atlan_settings_command(
         ),
     ],
     api_token: str,
-    organization: str,
     url: str,
 ):
     """Upload your Atlan settings to enable the Dagster<>Atlan integration in Dagster Cloud."""
-    if not url and not organization:
-        raise ui.error("Must provide either organization name or URL.")
-
-    if not url:
-        url = gql.url_from_config(organization=organization)
-
     with gql.graphql_client_from_url(url, api_token) as client:
         gql.set_atlan_integration_settings(client, token=atlan_token, domain=atlan_domain)
