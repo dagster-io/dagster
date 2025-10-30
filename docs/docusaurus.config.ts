50ea8f213ef9ab74fda4b3d206b8ac42287df045
Nikki Everett <neverett@users.noreply.github.com>
2025-10-30 13:42:33 -0500

    DOC-1533 Update docs version dropdown for 1.12.0 release (#32660)
    ## Summary & Motivation
    
    See title
    
    ## How I Tested These Changes
    
    ## Changelog
    
    > Insert changelog entry or delete this section.
    
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -222,7 +222,7 @@ const config: Config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: 'Latest (1.11.16)',
+              label: 'Latest (1.12.0)',
               path: '/',
             },
           },
