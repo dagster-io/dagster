500276da0db7c6eb13b2db34f8b51aab151a1c22
OctavioValdiviaMendoza <octavio.valdiviamendoza@sjsu.edu>
2025-11-01 11:20:52 -0700

    removed unecessary print statements in base.py and cleaned up the code and added one test case in test_resolved.py called test_dataclass_default_factory_dict()
    Co-authored-by: THIEN PHU QUACH <quachphuwork@gmail.com>
    Co-authored-by: Tyler Field <tyler.feelgood.field@gmail.com>

--- python_modules/dagster/dagster_tests/components_tests/resolution_tests/test_resolved.py
+++ python_modules/dagster/dagster_tests/components_tests/resolution_tests/test_resolved.py
@@ -1,7 +1,7 @@
 from collections.abc import Sequence
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Annotated, Literal, NamedTuple, Optional, TypeAlias, Union
+from typing import Annotated, Any, Literal, NamedTuple, Optional, TypeAlias, Union
 
 import dagster as dg
 import pytest
@@ -267,6 +267,9 @@ def test_component_docs():
     assert json_schema["$defs"]["RangeTest"]["properties"]["type"]["description"]
     assert json_schema["$defs"]["SumTest"]["properties"]["type"]["description"]
 
+    json_schema = model_cls.model_json_schema()
+    assert json_schema["$defs"]["RangeTest"]["properties"]["type"]["description"]
+    assert json_schema["$defs"]["SumTest"]["properties"]["type"]["description"]
 
 def test_nested_not_resolvable():
     @dataclass
@@ -341,7 +344,8 @@ bar: bar
 """)
     assert w.foo == "cool"
 
-def test_dataclass_default_factory_dict():
+def test_default_factory_dict():
+    #Test for default_factory in dataclass
     @dataclass
     class MyThing(dg.Resolvable):
         items: dict = field(default_factory=dict)
@@ -359,6 +363,18 @@ items:
 """
     )
     assert t2.items == {"a": 1}
+
+    # Test for default_factory in pydantic model
+    class DefaultFactoryDictTestComponent(dg.Component, dg.Model, dg.Resolvable):
+        config: dict[str, Any] = Field(default_factory=dict)
+
+        def build_defs(self, context):
+            return dg.Definitions()
+
+    model_cls = DefaultFactoryDictTestComponent.get_model_cls()
+    assert model_cls
+    instance = DefaultFactoryDictTestComponent.resolve_from_yaml("")
+    assert instance.config == {}
 
 
 def test_scope():
