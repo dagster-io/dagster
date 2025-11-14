28cbd2ad4a95c2dfecbc628dbbf7069d1fa46801
Anurag Anjaria <anurag@dagsterlabs.com>
2025-11-13 23:27:27 -0500

    wip

--- python_modules/dagster-test/dagster_test/toys/snowflake_connection.py
+++ python_modules/dagster-test/dagster_test/toys/snowflake_connection.py
@@ -7,12 +7,10 @@ import dagster as dg
 @dg.observable_source_asset
 def fake_snowflake_connection_asset(context: dg.OpExecutionContext):
     """Pretend this is a Snowflake connection asset."""
-    bar = random.randint(1, 100)
-
     return dg.ObserveResult(
         data_version=dg.DataVersion(str(uuid4())),
         metadata={
-            "dagster/my_metric": bar,
+            "dagster/my_metric": dg.MetadataValue.int(random.randint(1, 100)),
         },
     )
 
