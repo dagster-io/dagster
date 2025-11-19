4ea1fd7e387d4025744e04258024b32b6328e5b2
Maxime Armstrong <maxime@retain.ai>
2025-11-18 19:37:11 -0500

    [ui] Add NavItemWithExternalLink

---
+++ js_modules/dagster-ui/packages/ui-core/src/app/navigation/NavItemWithExternalLink.tsx
@@ -0,0 +1,25 @@
+import {Tooltip} from '@dagster-io/ui-components';
+import clsx from 'clsx';
+import {ReactNode, useContext} from 'react';
+
+import {NavCollapseContext} from './NavCollapseProvider';
+import {NavItemContent} from './NavItemContent';
+import styles from './css/MainNavigation.module.css';
+
+interface NavItemWithExternalLinkProps {
+  icon: ReactNode;
+  href: string;
+  label: string;
+}
+
+export const NavItemWithExternalLink = ({icon, href, label}: NavItemWithExternalLinkProps) => {
+  const {isCollapsed} = useContext(NavCollapseContext);
+
+  return (
+    <Tooltip content={label} placement="right" canShow={isCollapsed}>
+      <a href={href} target="_blank" rel="noreferrer" className={clsx(styles.link, null)}>
+        <NavItemContent icon={icon} label={label} collapsed={isCollapsed} />
+      </a>
+    </Tooltip>
+  );
+};
