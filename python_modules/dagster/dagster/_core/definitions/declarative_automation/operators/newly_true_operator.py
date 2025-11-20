a781e0b6fa150d255e61f96a1c3b4784650ef1fe
OwenKephart <owen@elementl.com>
2025-11-20 09:43:19 -0800

    [da] swap to new cursoring scheme (#32856)
    ## Summary & Motivation
    
    Previous PR made it so that we could read cursors written with this scheme for forwards compat. Now start writing with this scheme.
    
    ## How I Tested These Changes
    
    ## Changelog
    
    NOCHANGELOG

--- python_modules/dagster/dagster/_core/definitions/declarative_automation/operators/newly_true_operator.py
+++ python_modules/dagster/dagster/_core/definitions/declarative_automation/operators/newly_true_operator.py
@@ -39,6 +39,18 @@ class NewlyTrueCondition(BuiltinAutomationCondition[T_EntityKey]):
             return None
         return context.asset_graph_view.get_subset_from_serializable_subset(true_subset)
 
+    def get_node_unique_id(
+        self,
+        *,
+        parent_unique_id: Optional[str],
+        index: Optional[int],
+        target_key: Optional[EntityKey],
+    ) -> str:
+        # newly true conditions should have stable cursoring logic regardless of where they
+        # exist in the broader condition tree, as they're always evaluated over the entire
+        # subset
+        return self._get_stable_unique_id(target_key)
+
     def get_backcompat_node_unique_ids(
         self,
         *,
@@ -46,7 +58,12 @@ class NewlyTrueCondition(BuiltinAutomationCondition[T_EntityKey]):
         index: Optional[int] = None,
         target_key: Optional[EntityKey] = None,
     ) -> Sequence[str]:
-        return [self._get_stable_unique_id(target_key)]
+        return [
+            # get the standard globally-aware unique id for backcompat purposes
+            super().get_node_unique_id(
+                parent_unique_id=parent_unique_id, index=index, target_key=target_key
+            )
+        ]
 
     async def evaluate(self, context: AutomationContext) -> AutomationResult:  # pyright: ignore[reportIncompatibleMethodOverride]
         # evaluate child condition
