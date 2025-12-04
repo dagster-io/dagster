a8e7398a9a34ccfb1733db7a25b765d801ed484d
JamieDeMaria <jamie@dagsterlabs.com>
2025-12-04 10:30:17 -0500

    add latest run id to all asset health meta

--- python_modules/dagster/dagster/_core/definitions/asset_health/asset_materialization_health.py
+++ python_modules/dagster/dagster/_core/definitions/asset_health/asset_materialization_health.py
@@ -331,6 +331,7 @@ class AssetHealthMaterializationDegradedPartitionedMeta:
     num_failed_partitions: int
     num_missing_partitions: int
     total_num_partitions: int
+    failed_run_id: Optional[str]
 
 
 @whitelist_for_serdes
@@ -338,6 +339,7 @@ class AssetHealthMaterializationDegradedPartitionedMeta:
 class AssetHealthMaterializationHealthyPartitionedMeta:
     num_missing_partitions: int
     total_num_partitions: int
+    latest_run_id: Optional[str]
 
 
 @whitelist_for_serdes
@@ -439,6 +441,7 @@ async def get_materialization_status_and_metadata(
             meta = AssetHealthMaterializationHealthyPartitionedMeta(
                 num_missing_partitions=num_missing,
                 total_num_partitions=total_num_partitions,
+                latest_run_id=asset_materialization_health_state.latest_terminal_run_id,
             )
         else:
             # captures the case when asset is not partitioned, or the asset is partitioned and all partitions are materialized
@@ -459,6 +462,7 @@ async def get_materialization_status_and_metadata(
                 num_failed_partitions=asset_materialization_health_state.num_failed_partitions,
                 num_missing_partitions=num_missing,
                 total_num_partitions=total_num_partitions,
+                failed_run_id=asset_materialization_health_state.latest_terminal_run_id,
             )
         else:
             meta = AssetHealthMaterializationDegradedNotPartitionedMeta(
