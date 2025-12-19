dda7c5c67821a79a5d91672feb3d5d3af037cbc5
prha <philrha@gmail.com>
2025-12-19 11:45:17 -0800

    launcher tests

--- python_modules/libraries/dagster-aws/dagster_aws_tests/ecs_tests/launcher_tests/test_launching.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/ecs_tests/launcher_tests/test_launching.py
@@ -39,7 +39,8 @@ def test_default_launcher(
     task_long_arn_format,
 ):
     ecs.put_account_setting(name="taskLongArnFormat", value=task_long_arn_format)
-    assert not run.tags
+    assert "ecs/task_arn" not in run.tags
+    assert "ecs/cluster" not in run.tags
 
     initial_task_definitions = ecs.list_task_definitions()["taskDefinitionArns"]
     initial_tasks = ecs.list_tasks()["taskArns"]
@@ -1445,7 +1446,8 @@ def test_custom_launcher(
     custom_workspace,
     custom_run,
 ):
-    assert not custom_run.tags
+    assert "ecs/task_arn" not in custom_run.tags
+    assert "ecs/cluster" not in custom_run.tags
 
     initial_tasks = ecs.list_tasks()["taskArns"]
 
