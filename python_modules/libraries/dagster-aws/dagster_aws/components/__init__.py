545f9ed4f104be993390c7be447b69cb0736016a
michalcabir-ui <michal.cabir@cherrypick-consulting.com>
2025-12-27 18:29:17 +0200

    extend init file

--- python_modules/libraries/dagster-aws/dagster_aws/components/__init__.py
+++ python_modules/libraries/dagster-aws/dagster_aws/components/__init__.py
@@ -1,4 +1,5 @@
 from dagster_aws.components.credentials import (
     Boto3CredentialsComponent as Boto3CredentialsComponent,
+    S3CredentialsComponent as S3CredentialsComponent,
 )
 from dagster_aws.components.s3 import S3ResourceComponent as S3ResourceComponent
