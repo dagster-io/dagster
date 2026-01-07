fb19b8cf64dd91aab54648866a468ace3e081ebf
michalcabir-ui <michal.cabir@cherrypick-consulting.com>
2026-01-07 14:42:25 +0200

    create vertion.py, refactor setup.py

--- python_modules/libraries/dagster-hightouch/setup.py
+++ python_modules/libraries/dagster-hightouch/setup.py
@@ -1,20 +1,16 @@
 from pathlib import Path
-
 from setuptools import find_packages, setup
-
 
 def get_version() -> str:
     version: dict[str, str] = {}
-    with open(
-        Path(__file__).parent / "dagster_hightouch/version.py", encoding="utf8"
-    ) as fp:
+    with open(Path(__file__).parent / "dagster_hightouch/version.py", encoding="utf8") as fp:
         exec(fp.read(), version)
-
     return version["__version__"]
 
-
 ver = get_version()
+
 pin = "" if ver == "1!0+dev" else f"=={ver}"
+
 setup(
     name="dagster-hightouch",
     version=ver,
@@ -22,9 +18,7 @@ setup(
     author_email="hello@dagsterlabs.com",
     license="Apache-2.0",
     description="Package for integrating Hightouch with Dagster.",
-    url=(
-        "https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-hightouch"
-    ),
+    url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-hightouch",
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -33,6 +27,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
+
     packages=find_packages(exclude=["dagster_hightouch_tests*"]),
     include_package_data=True,
     python_requires=">=3.10,<3.14",
@@ -41,10 +36,11 @@ setup(
         "requests",
         "python-dateutil",
     ],
-    zip_safe=False,
+
     entry_points={
-        "dagster_dg_cli.plugin": [
+        "dagster_dg_cli.registry_modules": [
             "dagster_hightouch = dagster_hightouch",
         ],
     },
+    zip_safe=False,
 )
