681d4603c94ac1d94f0663390272a10d0f493092
Ben Gotow <ben@foundry376.com>
2026-01-08 13:41:57 -0600

    [ui] Remove orphaned components and front-end source (#33177)
    Since we've been doing a lot of code cleanup, I figured I would run
    `knip` and see if there are unused sources we can remove. A bunch of
    stuff turned up and I had Claude grep for usages across Dagster and
    Dagster internal since the two repo setup led to a lot of false
    positives.
    
    There are some things in here that feel like they could be vaguely
    useful in the future but I think it'd be best to keep the codebase
    tightly scoped to the things we're actually using
    
    ---------
    
    Co-authored-by: bengotow <bgotow@elementl.com>

--- js_modules/dagster-ui/packages/ui-core/src/schedules/makeScheduleKey.tsx
+++
@@ -1,6 +0,0 @@
-import {repoAddressAsHumanString} from '../workspace/repoAddressAsString';
-import {RepoAddress} from '../workspace/types';
-
-export const makeScheduleKey = (repoAddress: RepoAddress, scheduleName: string) => {
-  return `${repoAddressAsHumanString(repoAddress)}-${scheduleName}`;
-};
