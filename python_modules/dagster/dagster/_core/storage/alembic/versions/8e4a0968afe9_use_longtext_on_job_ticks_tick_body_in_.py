e4f1f4cc758e9a2d26791230ebc732eb3e95b1f0
Ian Jenkins <ian.jenkins@liverpoolfc.com>
2026-01-09 16:56:34 +0000

    Use LONGTEXT for job_ticks.tick_body on mysql
    Otherwise will create errors on sensors/schedules for an asset with
    thousands of partitions

---
+++ python_modules/dagster/dagster/_core/storage/alembic/versions/8e4a0968afe9_use_longtext_on_job_ticks_tick_body_in_.py
@@ -0,0 +1,46 @@
+"""use longtext on job_ticks.tick_body in mysql
+
+Revision ID: 8e4a0968afe9
+Revises: 29b539ebc72a
+Create Date: 2026-01-09 16:53:34.657286
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import inspect
+from sqlalchemy.dialects.mysql import LONGTEXT
+
+# revision identifiers, used by Alembic.
+revision = "8e4a0968afe9"
+down_revision = "29b539ebc72a"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    inspector = inspect(op.get_bind())
+    if "mysql" not in inspector.dialect.dialect_description:
+        return
+
+    op.alter_column(
+        table_name="job_ticks",
+        column_name="tick_body",
+        nullable=True,
+        type_=sa.types.Text().with_variant(LONGTEXT, "mysql"),
+        existing_type=sa.types.Text(),
+    )
+
+
+def downgrade():
+    inspector = inspect(op.get_bind())
+    if "mysql" not in inspector.dialect.dialect_description:
+        return
+
+    op.alter_column(
+        table_name="job_ticks",
+        column_name="tick_body",
+        nullable=True,
+        type_=sa.types.Text(),
+        existing_type=sa.types.Text().with_variant(LONGTEXT, "mysql"),
+    )
