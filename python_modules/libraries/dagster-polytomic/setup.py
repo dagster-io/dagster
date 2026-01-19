503bc25b7b91b219ed66008ae036ef840f84b7d6
Maxime Armstrong <maxime@retain.ai>
2026-01-19 18:14:22 -0500

    [dagster-polytomic][1/n] Scaffold dagster-polytomic folder

---
+++ python_modules/libraries/dagster-polytomic/setup.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+def get_version() -> str:
+    version: dict[str, str] = {}
+    with open(Path(__file__).parent / "dagster_polytomic/version.py", encoding="utf8") as fp:
+        exec(fp.read(), version)
+
+    return version["__version__"]
+
+
+ver = get_version()
+# dont pin dev installs to avoid pip dep resolver issues
+pin = "" if ver == "1!0+dev" else f"=={ver}"
+setup(
+    name="dagster-polytomic",
+    version=ver,
+    author="Dagster Labs",
+    author_email="hello@dagsterlabs.com",
+    license="Apache-2.0",
+    description="Package for integrating Polytomic with Dagster.",
+    url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-polytomic",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=["dagster_polytomic_tests*"]),
+    include_package_data=True,
+    python_requires=">=3.10,<3.15",
+    install_requires=[f"dagster{pin}", "polytomic>=1.11.3"],
+    zip_safe=False,
+)
