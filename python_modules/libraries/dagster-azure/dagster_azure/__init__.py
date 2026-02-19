a41695b853f39eceea0693524ffe9c21b8f6f67c
michalcabir-ui <michal.cabir@cherrypick-consulting.com>
2026-02-19 12:01:33 +0200

    CI FIX

--- python_modules/libraries/dagster-azure/dagster_azure/__init__.py
+++ python_modules/libraries/dagster-azure/dagster_azure/__init__.py
@@ -1,7 +1,9 @@
 from dagster_shared.libraries import DagsterLibraryRegistry
 
-from dagster_azure.components.adls2 import ADLS2ResourceComponent
-from dagster_azure.components.blob import AzureBlobStorageResourceComponent
+from dagster_azure.components.adls2 import ADLS2ResourceComponent as ADLS2ResourceComponent
+from dagster_azure.components.blob import (
+    AzureBlobStorageResourceComponent as AzureBlobStorageResourceComponent,
+)
 from dagster_azure.version import __version__
 
 DagsterLibraryRegistry.register("dagster-azure", __version__)
--- python_modules/libraries/dagster-azure/dagster_azure/components/__init__.py
+++ python_modules/libraries/dagster-azure/dagster_azure/components/__init__.py
@@ -1,4 +1,6 @@
 """Dagster Azure Components - YAML-configurable resource components for Azure services."""
 
-from dagster_azure.components.adls2 import ADLS2ResourceComponent
-from dagster_azure.components.blob import AzureBlobStorageResourceComponent
+from dagster_azure.components.adls2 import ADLS2ResourceComponent as ADLS2ResourceComponent
+from dagster_azure.components.blob import (
+    AzureBlobStorageResourceComponent as AzureBlobStorageResourceComponent,
+)
