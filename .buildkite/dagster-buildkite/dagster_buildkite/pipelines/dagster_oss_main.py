6f14367775c1a58320507abfc5f2e608ba8a979a
mfiliba <mfiliba@gmail.com>
2026-02-20 13:38:22 -0800

    Remove trigger to oss-internal-compatibility and internal BK runs from Dagster BK run (#20408)
    Synced-From-Internal
    
    GitOrigin-RevId: fb29f3fe7a23a8fa4730a0b427758d6967d577f1

--- .buildkite/dagster-buildkite/dagster_buildkite/pipelines/dagster_oss_main.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/pipelines/dagster_oss_main.py
@@ -3,18 +3,15 @@ import os
 import re
 
 from buildkite_shared.environment import safe_getenv
-from buildkite_shared.packages import run_all_tests
 from buildkite_shared.quarantine import (
     filter_and_print_steps_by_quarantined,
     get_buildkite_quarantined_objects,
 )
 from buildkite_shared.step_builders.step_builder import StepConfiguration
-from buildkite_shared.step_builders.trigger_step_builder import TriggerStepBuilder
 from dagster_buildkite.steps.dagster import build_dagster_steps, build_repo_wide_steps
 from dagster_buildkite.steps.dagster_ui import (
     build_dagster_ui_components_steps,
     build_dagster_ui_core_steps,
-    skip_if_no_dagster_ui_changes,
 )
 from dagster_buildkite.steps.docs import build_docs_steps
 
@@ -69,65 +66,11 @@ def filter_steps_by_quarantined(steps, skip_quarantined_steps, mute_quarantined_
 
 
 def build_dagster_oss_main_steps() -> list[StepConfiguration]:
-    branch_name = safe_getenv("BUILDKITE_BRANCH")
-    commit_hash = safe_getenv("BUILDKITE_COMMIT")
-    oss_contribution = os.getenv("OSS_CONTRIBUTION")
-
     steps: list[StepConfiguration] = []
 
-    # Trigger a build on the internal pipeline for dagster PRs.
-    # master/release branches always trigger
-    # Feature branches only trigger if [INTERNAL_BRANCH=<branch>] is in the commit message
-    if (
-        not oss_contribution
-        and not os.getenv("CI_DISABLE_INTEGRATION_TESTS")
-        and not os.getenv("TRIGGERED_BY_INTERNAL")
-    ):
-        if branch_name == "master":
-            pipeline_name = "internal"
-            trigger_branch = branch_name  # build on matching internal release branch
-            async_step = True
-            oss_compat_slim = False
-        else:  # feature branch
-            pipeline_name = "oss-internal-compatibility"
-            trigger_branch = (
-                _get_setting("INTERNAL_BRANCH")
-                or _get_setting("BUILDKITE_FOLDER_BRANCH")
-                or "master"
-            )
-            async_step = False
-            # Use OSS_COMPAT_SLIM by default unless an internal branch is explicitly specified or
-            # the commit message contains "NO_SKIP"
-            oss_compat_slim = _get_setting("OSS_COMPAT_SLIM") or not (
-                _get_setting("INTERNAL_BRANCH")
-                or _get_setting("BUILDKITE_FOLDER_BRANCH")
-                or run_all_tests()
-            )
-
-        dagster_commit_hash = safe_getenv("BUILDKITE_COMMIT")
-        steps.append(
-            TriggerStepBuilder(
-                label=f":link: {pipeline_name} from dagster@{dagster_commit_hash[:10]}",
-                pipeline=pipeline_name,
-            )
-            .with_build_params(
-                {
-                    "env": {
-                        "DAGSTER_BRANCH": branch_name,
-                        "DAGSTER_COMMIT_HASH": commit_hash,
-                        "DAGSTER_UI_ONLY_OSS_CHANGE": (
-                            "1" if not skip_if_no_dagster_ui_changes() else ""
-                        ),
-                        "DAGSTER_CHECKOUT_DEPTH": _get_setting("DAGSTER_CHECKOUT_DEPTH") or "100",
-                        "OSS_COMPAT_SLIM": "1" if oss_compat_slim else "",
-                        "DAGSTER_FROM_OSS": "1" if pipeline_name == "internal" else "0",
-                    },
-                    "branch": trigger_branch,
-                }
-            )
-            .with_async(async_step)
-            .build(),
-        )
+    # Note: we used to trigger an internal build (oss-internal-compatibility/internal),
+    # but with monorepo, that became unnecessary because the majority of edits
+    # now come from the internal repo
 
     # Full pipeline.
     steps += build_repo_wide_steps()
