3d4d8170f2d2e8339320c792ad7a0aa77f60afe1
elementl <devtools@elementl.com>
2026-02-27 14:31:09 +0000

    1.12.17
    Synced-From-Internal
    
    GitOrigin-RevId: 4676ca1b45e6807e69e6280a30210e8dce52c264

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -227,7 +227,7 @@ const config: Config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: 'Latest (1.12.17rc0)',
+              label: 'Latest (1.12.17)',
               path: '/',
             },
           },
