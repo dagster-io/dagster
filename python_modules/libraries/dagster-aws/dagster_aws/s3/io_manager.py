106aeb90ccd314ffa1b8ff5117b6413f3f2a215d
andrewwkimm <andrewkimka@gmail.com>
2026-03-04 20:19:44 -0500

    Fix S3 IO Manager to work with dynamic outputs (#6238)

--- python_modules/libraries/dagster-aws/dagster_aws/s3/io_manager.py
+++ python_modules/libraries/dagster-aws/dagster_aws/s3/io_manager.py
@@ -73,7 +73,15 @@ class PickledObjectS3IOManager(UPathIOManager):
         return {"uri": MetadataValue.path(self._uri_for_path(path))}
 
     def get_op_output_relative_path(self, context: InputContext | OutputContext) -> UPath:
-        return UPath("storage", super().get_op_output_relative_path(context))
+        sanitized = self._sanitize_path(super().get_op_output_relative_path(context))
+        return UPath("storage", sanitized)
+
+    @staticmethod
+    def _sanitize_path(path: UPath) -> UPath:
+        sanitized_parts = []
+        for part in path.parts:
+            sanitized_parts.append(part.replace("[", "_").replace("]", ""))
+        return UPath(*sanitized_parts)
 
     def _uri_for_path(self, path: UPath) -> str:
         return f"s3://{self.bucket}/{path.as_posix()}"
