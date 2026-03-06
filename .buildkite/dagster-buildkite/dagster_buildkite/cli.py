3cb1f9ce5a178b16d7bf3575f2932250e0a9437d
Sean Mackesey <s.mackesey@gmail.com>
2026-03-06 16:43:49 -0500

    [buildkite] nightly pipeline for monorepo integrity tests (#21435)
    - Delete the old wiring for the OSS nightly pipeline (which has been
    defunct for a while)
    - Add new copybara-integrity pipeline in internal. The only thing it
    runs (currently) is the monorepo integrity tests, which don't run
    anywhere else. It runs them nightly.
    
    Internal-RevId: e76c24e27ce79af547d21028418f2eaa9b2e7f80

--- .buildkite/dagster-buildkite/dagster_buildkite/cli.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/cli.py
@@ -14,7 +14,6 @@ logging.basicConfig(
 
 from buildkite_shared.context import BuildkiteContext
 from dagster_buildkite.pipelines.dagster_oss_main import build_dagster_oss_main_steps
-from dagster_buildkite.pipelines.dagster_oss_nightly_pipeline import build_dagster_oss_nightly_steps
 from dagster_buildkite.pipelines.prerelease_package import build_prerelease_package_steps
 
 CLI_HELP = """This CLI is used for generating Buildkite YAML. Each function corresponds to an entry
@@ -26,13 +25,6 @@ a pipeline.
 def dagster() -> None:
     ctx = BuildkiteContext.create()
     steps = build_dagster_oss_main_steps(ctx)
-    pipeline = _build_pipeline(steps)
-    print(dump_pipeline_yaml(pipeline))  # noqa: T201
-
-
-def dagster_nightly() -> None:
-    ctx = BuildkiteContext.create()
-    steps = build_dagster_oss_nightly_steps(ctx)
     pipeline = _build_pipeline(steps)
     print(dump_pipeline_yaml(pipeline))  # noqa: T201
 
