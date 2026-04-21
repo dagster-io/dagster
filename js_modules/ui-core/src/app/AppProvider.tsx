5184e5eabcf830d32cf6f32c1c8314d6edac12a0
Mohd Saif Ansari <mohdsaifansari8888@gmail.com>
2026-04-22 00:23:22 +0530

    Move toast to top-center to prevent UI blocking

--- js_modules/ui-core/src/app/AppProvider.tsx
+++ js_modules/ui-core/src/app/AppProvider.tsx
@@ -183,7 +183,7 @@ export const AppProvider = (props: AppProviderProps) => {
           <PermissionsProvider>
             <BrowserRouter basename={basePath || ''}>
               <GlobalStyleProvider />
-              <Toaster richColors />
+              <Toaster richColors position="top-center"/>
               <CompatRouter>
                 <TimeProvider>
                   <CodeLinkProtocolProvider>
