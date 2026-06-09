49bfcbc71015505c2650a6bd426acc186bc81aec
onjamm <75151498+onjamm@users.noreply.github.com>
2026-06-09 18:39:54 +0000

    added test for dagsterLibaryRegistry (from dagster import)

--- python_modules/dagster/dagster_tests/utils_tests/test_libraries.py
+++ python_modules/dagster/dagster_tests/utils_tests/test_libraries.py
@@ -1,2 +1,5 @@
 def test_legacy_registry_import_still_works():
     from dagster._core.libraries import DagsterLibraryRegistry  # noqa
+
+def test_dagster_library_registry_import_from_dagster():
+    from dagster import DagsterLibraryRegistry # noqa
