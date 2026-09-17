8393e28c8f649a894fdf744b3aa31e17b9c1908b
Matt Krukowski <m.krukowski@dagsterlabs.com>
2026-09-17 16:24:12 -0500

    [docs] Consistently use job (instead of job_name) in asset_sensor docs (#26942)
    ## Summary & Motivation
    A user in [the community
    slack](https://dagster.slack.com/archives/C01U5LFUZJS/p1789054175091879)
    called out this confusing inconsistency in the docs compared to the
    other sensor examples
    ([example](https://github.com/dagster-io/internal/blob/bef50e8d143e4680230beace9fa30777f2c14c12/dagster-oss/examples/docs_snippets/docs_snippets/guides/automate/asset-sensor-custom-eval.py#L18)).
    It was technically fine but worth unifying.
    
    Internal-RevId: 3b868f73515e5cd280e38c1fca86cc631eff5ebf

--- examples/docs_snippets/docs_snippets/guides/automate/simple-asset-sensor-example.py
+++ examples/docs_snippets/docs_snippets/guides/automate/simple-asset-sensor-example.py
@@ -17,7 +17,7 @@ my_job = dg.define_asset_job("my_job", [weekly_report])
 
 # highlight-start
 # Trigger `my_job` when the `daily_sales_data` asset is materialized
-@dg.asset_sensor(asset_key=dg.AssetKey("daily_sales_data"), job_name="my_job")
+@dg.asset_sensor(asset_key=dg.AssetKey("daily_sales_data"), job=my_job)
 def daily_sales_data_sensor():
     return dg.RunRequest()
     # highlight-end
